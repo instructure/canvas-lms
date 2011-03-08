0c50014ffef5b05e53a00467fad7c335af716dc6
Zach Wily <zach@instructure.com>
2011-03-08 15:28:15 -0700

    capitalize the plugin author names
    Change-Id: I77bbbbbf68b7e5bc30b9e50d36a5b1937a966a5d
    Reviewed-on: https://gerrit.instructure.com/2561
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Brian Whitmer <brian@instructure.com>

--- lib/canvas/plugins/default_plugins.rb
+++ lib/canvas/plugins/default_plugins.rb
@@ -5,7 +5,7 @@ end
 Canvas::Plugin.register('kaltura', nil, {
   :description => 'Kaltura video/audio recording and playback',
   :website => 'http://corp.kaltura.com',
-  :author => 'instructure',
+  :author => 'Instructure',
   :author_website => 'http://www.instructure.com',
   :version => 1.0,
   :settings_partial => 'plugins/kaltura_settings',
@@ -14,7 +14,7 @@ Canvas::Plugin.register('kaltura', nil, {
 Canvas::Plugin.register('dim_dim', :web_conferencing, {
   :description => 'DimDim web conferencing support',
   :website => 'http://www.dimdim.com',
-  :author => 'instructure',
+  :author => 'Instructure',
   :author_website => 'http://www.instructure.com',
   :version => 1.0,
   :settings_partial => 'plugins/dim_dim_settings'
@@ -22,7 +22,7 @@ Canvas::Plugin.register('dim_dim', :web_conferencing, {
 Canvas::Plugin.register('wimba', :web_conferencing, {
   :description => 'Wimba web conferencing support',
   :website => 'http://www.wimba.com',
-  :author => 'instructure',
+  :author => 'Instructure',
   :author_website => 'http://www.instructure.com',
   :version => 1.0,
   :settings_partial => 'plugins/wimba_settings',
