45f54e35ac2bfe174c5bb6acf1e6b50a5f338192
Brian Palmer <brianp@instructure.com>
2011-03-23 18:06:42 -0600

    update our preinitializer.rb
    this'll let you run script/console and such without having to cd to the
    rails root dir first.
    
    Change-Id: Ic2ff10652afc92ac1e5dc4ebfef97a9a27059e29
    Reviewed-on: https://gerrit.instructure.com/2790
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,15 +1,20 @@
 begin
-  # Require the preresolved locked set of gems.
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime.
   require "rubygems"
   require "bundler"
-  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
-    raise RuntimeError, "Bundler incompatible.\n" +
-      "Your bundler version is incompatible with Rails 2.3 and an unlocked bundle.\n" +
-      "Run `gem install bundler` to upgrade or `bundle lock` to lock."
-  else
-    Bundler.setup
-  end
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
+end
+
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("1.0.0")
+  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
+   "Run `gem install bundler` to upgrade."
+end
+
+begin
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
 end
