793cd7243438c53b0097eb6ee628da9a5368cd00
JT Olds <jt@instructure.com>
2011-04-13 15:08:19 -0600

    don't monkey patch attachment_fu when we have so many local mods already
    Change-Id: I0dde24272381f3ae1e8ba321ec31f8822731d5c5
    Reviewed-on: https://gerrit.instructure.com/3066
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

--- vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu.rb
+++ vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu.rb
@@ -240,6 +240,8 @@ module Technoweenie # :nodoc:
     end
 
     module InstanceMethods
+      require 'rack'
+
       def self.included(base)
         base.define_callbacks *[:after_resize, :after_attachment_saved, :before_thumbnail_saved] if base.respond_to?(:define_callbacks)
       end
@@ -303,7 +305,14 @@ module Technoweenie # :nodoc:
 
       # Returns true if the attachment data will be written to the storage system on the next save
       def save_attachment?
-        File.file?(temp_path.to_s)
+        if self.is_a?(Attachment)
+          if self.root_attachment_id && self.new_record?
+            return false
+          end
+          self.filename && File.file?(temp_path.to_s)
+        else
+          File.file?(temp_path.to_s)
+        end
       end
 
       # nil placeholder in case this field is used in a form.
@@ -321,17 +330,91 @@ module Technoweenie # :nodoc:
       #
       # TODO: Allow it to work with Merb tempfiles too.
       def uploaded_data=(file_data)
-        return nil if file_data.nil? || file_data.size == 0
-        self.content_type = file_data.content_type
-        self.filename     = file_data.original_filename if respond_to?(:filename)
-        if file_data.is_a?(StringIO)
-          file_data.rewind
-          self.temp_data = file_data.read
+        if self.is_a?(Attachment)
+          return nil if file_data.nil? || (file_data.respond_to?(:size) && file_data.size == 0)
+          # glean information from the file handle
+          self.content_type = detect_mimetype(file_data) rescue 'unknown/unknown'
+          self.filename     = file_data.original_filename if respond_to?(:filename) && file_data.respond_to?(:original_filename)
+          file_from_path = true
+          unless file_data.respond_to?(:path)
+            file_data.rewind
+            self.temp_data = file_data.read
+            file_from_path = false
+          else
+            self.temp_paths.unshift file_data
+          end
+          # If we're overwriting an existing file, we need to take serious
+          # precautions, since other Attachment records could be using this file.
+          # We first remove any root references for this file, and then we generate
+          # a new unique filename for this file so anybody children of this attachment
+          # will still be able to get at the original.
+          if !self.new_record?
+            self.root_attachment = nil
+            self.root_attachment_id = nil
+            self.scribd_mime_type_id = nil
+            self.scribd_user = nil
+            self.submitted_to_scribd_at = nil
+            self.workflow_state = nil
+            self.scribd_doc = nil
+            self.filename = filename.sub(/\A\d+_\d+__/, "")
+            self.filename = "#{Time.now.to_i}_#{rand(999)}__#{self.filename}" if self.filename
+          end
+          read_bytes = false
+          digest = Digest::MD5.new
+          begin
+            io = file_data
+            if file_from_path
+              io = File.open(self.temp_path, 'rb')
+            end
+            io.rewind
+            io.each_line do |line| 
+              digest.update(line)
+              read_bytes = true
+            end
+          rescue => e
+          end
+          self.md5 = read_bytes ? digest.hexdigest : nil
+          if self.md5 && ns = self.infer_namespace
+            if existing_attachment = Attachment.find_all_by_md5_and_namespace(self.md5, ns).detect{|a| (self.new_record? || a.id != self.id) && !a.root_attachment_id && a.content_type == self.content_type }
+              self.temp_path = nil if respond_to?(:temp_path=)
+              self.temp_data = nil if respond_to?(:temp_data=)
+              write_attribute(:filename, nil) if respond_to?(:filename=)
+              self.root_attachment = existing_attachment
+            end
+          end
+          file_data
         else
-          self.temp_path = file_data
+          return nil if file_data.nil? || file_data.size == 0
+          self.content_type = file_data.content_type
+          self.filename     = file_data.original_filename if respond_to?(:filename)
+          unless file_data.responds_to?(:path)
+            file_data.rewind
+            self.temp_data = file_data.read
+          else
+            self.temp_path = file_data
+          end
         end
       end
 
+      def detect_mimetype(file_data)
+        if file_data && file_data.respond_to?(:content_type) && (!file_data.content_type || file_data.content_type.strip == "application/octet-stream")
+          res = nil
+          res = File.mime_type?(file_data.original_filename) if !res || res == 'unknown/unknown'
+          res = File.mime_type?(file_data) if !res || res == 'unknown/unknown'
+          res = "text/plain" if !file_data.responds_to?(:path) && res == 'unknown/unknown'
+          # if res == 'unknown/unknown'
+            # require 'mime/types'
+            # res = MIME::Types.type_for(file_data.original_filename)[0].to_s
+          # end
+          res
+          # return File.mime_typMIME::Types.type_for(file_data.original_filename)[0].to_s
+        elsif file_data.respond_to?(:content_type)
+          return file_data.content_type
+        else
+          'unknown/unknown'
+        end
+      end
+      
       # Gets the latest temp path from the collection of temp paths.  While working with an attachment,
       # multiple Tempfile objects may be created for various processing purposes (resizing, for example).
       # An array of all the tempfile objects is stored so that the Tempfile instance is held on to until
