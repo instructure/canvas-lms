d27a6c24afd5f3a24bdbbd83cd01a11d49f12bf5
tom metge <tom@instructure.com>
2011-05-18 10:48:36 -0600

    sqlite compatibility in finding subaccounts
    Change-Id: I6dbeb1251d9fd3272589c18cf3b69334119b9c08
    Reviewed-on: https://gerrit.instructure.com/3701
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Jon Jensen <jon@instructure.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -435,7 +435,7 @@ class Account < ActiveRecord::Base
   end
   
   def self_and_all_sub_accounts
-    @self_and_all_sub_accounts ||= ActiveRecord::Base.connection.send(:select, "SELECT id FROM accounts WHERE accounts.root_account_id = #{self.id} OR accounts.parent_account_id = #{self.id}").map{|ref| ref['id'].to_i}.uniq + [self.id] #(self.all_accounts + [self]).map &:id
+    @self_and_all_sub_accounts ||= ActiveRecord::Base.connection.select_all("SELECT id FROM accounts WHERE accounts.root_account_id = #{self.id} OR accounts.parent_account_id = #{self.id}").map{|ref| ref['id'].to_i}.uniq + [self.id] #(self.all_accounts + [self]).map &:id
   end
   
   def abstract_courses
