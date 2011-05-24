c8117edbfb0cbcebba4c9a34555bee92854c6aa1
Zach Wily <zach@instructure.com>
2011-05-24 08:47:41 -0600

    fix typo in section switcher
    Change-Id: Icf07a17ae5cf16812eb1b8fc22641999e55588df
    Reviewed-on: https://gerrit.instructure.com/3821
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- spec/selenium/courses_sel.rb
+++ spec/selenium/courses_sel.rb
@@ -17,31 +17,41 @@ shared_examples_for "course selenium tests" do
     c = e.course
     student = user :active_user => true
     se = c.enroll_student(student)
+    
+    observer = user :active_user => true
+    oe = c.enroll_student(observer)
+    
     section = c.course_sections.create!(:name => 'M/W/F')
     login_as(username, password)
 
     get "/courses/#{e.course_id}/details"
     driver.find_element(:css, '#tab-users-link').click
-    student_user = driver.find_element(:css, ".user_list #enrollment_#{se.id}")
-    section_label = student_user.find_element(:css, ".section")
-    section_dropdown = student_user.find_element(:css, ".enrollment_course_section_form #course_section_id")
-    section_label.displayed?.should be_true
-    section_dropdown.displayed?.should be_false
-    # hover over the user to make the links appear
-    driver.execute_script("$('.user_list #enrollment_#{se.id} .links').css('visibility', 'visible')")
-    edit_section_link = student_user.find_element(:css, ".edit_section_link")
-    edit_section_link.displayed?.should be_true
-    edit_section_link.click
-    section_label.displayed?.should be_false
-    section_dropdown.displayed?.should be_true
-    section_dropdown.find_element(:css, "option[value=\"#{section.id.to_s}\"]").select
+    
+    def move_section_for_enrollment(enrollment, section)
+      student_user = driver.find_element(:css, ".user_list #enrollment_#{enrollment.id}")
+      section_label = student_user.find_element(:css, ".section")
+      section_dropdown = student_user.find_element(:css, ".enrollment_course_section_form #course_section_id")
+      section_label.displayed?.should be_true
+      section_dropdown.displayed?.should be_false
+      # hover over the user to make the links appear
+      driver.execute_script("$('.user_list #enrollment_#{enrollment.id} .links').css('visibility', 'visible')")
+      edit_section_link = student_user.find_element(:css, ".edit_section_link")
+      edit_section_link.displayed?.should be_true
+      edit_section_link.click
+      section_label.displayed?.should be_false
+      section_dropdown.displayed?.should be_true
+      section_dropdown.find_element(:css, "option[value=\"#{section.id.to_s}\"]").select
 
-    keep_trying { !section_dropdown.displayed? }
+      keep_trying { !section_dropdown.displayed? }
 
-    se.reload
-    se.course_section_id.should == section.id
-    section_label.displayed?.should be_true
-    section_label.text.should == section.name
+      enrollment.reload
+      enrollment.course_section_id.should == section.id
+      section_label.displayed?.should be_true
+      section_label.text.should == section.name
+    end
+    
+    move_section_for_enrollment(se, section)
+    move_section_for_enrollment(oe, section)
   end
 end
 
