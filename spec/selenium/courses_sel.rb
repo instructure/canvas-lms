51ade644bdeed2aaa3e78bae478565d91a1c6d0c
Cody Cutrer <cody@instructure.com>
2011-05-24 13:46:30 -0600

    more intensive selenium tests for inline editing of enrollments
    Change-Id: Ica06283044e2360bc7f6a65853ec909912e81c99
    Reviewed-on: https://gerrit.instructure.com/3839
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Cody Cutrer <cody@instructure.com>

--- spec/selenium/courses_sel.rb
+++ spec/selenium/courses_sel.rb
@@ -4,54 +4,112 @@ shared_examples_for "course selenium tests" do
   it_should_behave_like "in-process server selenium tests"
 
   it "should allow moving a student to a different section" do
-    username = "nobody@example.com"
-    password = "asdfasdf"
-    t = user_with_pseudonym :active_user => true,
-                            :username => username,
-                            :password => password
-    t.save!
-    e = course_with_teacher :active_course => true,
-                            :user => t,
-                            :active_enrollment => true
-    e.save!
-    c = e.course
-    student = user :active_user => true
-    se = c.enroll_student(student)
-    
-    observer = user :active_user => true
-    oe = c.enroll_student(observer)
-    
-    section = c.course_sections.create!(:name => 'M/W/F')
-    login_as(username, password)
-
-    get "/courses/#{e.course_id}/details"
-    driver.find_element(:css, '#tab-users-link').click
-    
-    def move_section_for_enrollment(enrollment, section)
-      student_user = driver.find_element(:css, ".user_list #enrollment_#{enrollment.id}")
-      section_label = student_user.find_element(:css, ".section")
-      section_dropdown = student_user.find_element(:css, ".enrollment_course_section_form #course_section_id")
-      section_label.displayed?.should be_true
-      section_dropdown.displayed?.should be_false
-      # hover over the user to make the links appear
-      driver.execute_script("$('.user_list #enrollment_#{enrollment.id} .links').css('visibility', 'visible')")
-      edit_section_link = student_user.find_element(:css, ".edit_section_link")
-      edit_section_link.displayed?.should be_true
-      edit_section_link.click
-      section_label.displayed?.should be_false
-      section_dropdown.displayed?.should be_true
-      section_dropdown.find_element(:css, "option[value=\"#{section.id.to_s}\"]").select
-
-      keep_trying { !section_dropdown.displayed? }
-
-      enrollment.reload
-      enrollment.course_section_id.should == section.id
-      section_label.displayed?.should be_true
-      section_label.text.should == section.name
+    c = course :active_course => true
+    users = {:plain => {}, :sis => {}}
+    [:plain, :sis].each do |sis_type|
+      [:student, :observer, :ta, :teacher].each do |enrollment_type|
+        user = {
+            :username => "#{enrollment_type}+#{sis_type}@example.com",
+            :password => "#{enrollment_type}#{sis_type}1"
+        }
+        user[:user] = user_with_pseudonym :active_user => true,
+          :username => user[:username],
+          :password => user[:password]
+        user[:enrollment] = c.enroll_user(user[:user], "#{enrollment_type.to_s.capitalize}Enrollment", :enrollment_state => 'active')
+        if sis_type == :sis
+          user[:enrollment].sis_source_id = "#{enrollment_type}.sis.1"
+          user[:enrollment].save!
+        end
+        users[sis_type][enrollment_type] = user
+      end
     end
-    
-    move_section_for_enrollment(se, section)
-    move_section_for_enrollment(oe, section)
+    admin = {
+      :username => 'admin@example.com',
+      :password => 'admin1'
+    }
+    admin[:user] = account_admin_user :active_user => true
+    user_with_pseudonym :user=> admin[:user],
+      :username => admin[:username],
+      :password => admin[:password]
+    users[:plain][:admin] = admin
+
+    section = c.course_sections.create!(:name => 'M/W/F')
+
+    users[:plain].each do |user_type, logged_in_user|
+      # Students and Observers can't do anything
+      next if user_type == :student || user_type == :observer
+      login_as(logged_in_user[:username], logged_in_user[:password])
+
+      get "/courses/#{c.id}/details"
+
+      driver.find_element(:css, '#tab-users-link').click
+
+      users.each do |sis_type, users2|
+        users2.each do |enrollment_type, user|
+          # Admin isn't actually enrolled
+          next if enrollment_type == :admin
+          # You can't move yourself
+          next if user == logged_in_user
+
+          enrollment = user[:enrollment]
+          enrollment_element = driver.find_element(:css, "#enrollment_#{enrollment.id}")
+          section_label = enrollment_element.find_element(:css, ".section") rescue nil
+          section_dropdown = enrollment_element.find_element(:css, ".enrollment_course_section_form #course_section_id") rescue nil
+          edit_section_link = enrollment_element.find_element(:css, ".edit_section_link") rescue nil
+          unenroll_user_link = enrollment_element.find_element(:css, ".unenroll_user_link") rescue nil
+
+          # Observers don't have a section
+          if enrollment_type == :observer
+            edit_section_link.nil?.should be_true
+            section_label.nil?.should be_true
+            next
+          end
+          section_label.nil?.should be_false
+          section_label.displayed?.should be_true
+
+          # "hover" over the user to make the links appear
+          driver.execute_script("$('.user_list #enrollment_#{enrollment.id} .links').css('visibility', 'visible')")
+          # All users can manage students; admins and teachers can manage all enrollment types
+          can_modify = enrollment_type == :student || [:admin, :teacher].include?(user_type)
+          if sis_type == :plain || logged_in_user == admin
+            section_dropdown.displayed?.should be_false
+
+            if can_modify
+              edit_section_link.nil?.should be_false
+              unenroll_user_link.nil?.should be_false
+
+              # Move sections
+              edit_section_link.click
+              section_label.displayed?.should be_false
+              section_dropdown.displayed?.should be_true
+              section_dropdown.find_element(:css, "option[value=\"#{section.id.to_s}\"]").select
+
+              keep_trying { !section_dropdown.displayed? }
+
+              enrollment.reload
+              enrollment.course_section_id.should == section.id
+              section_label.displayed?.should be_true
+              section_label.text.should == section.name
+
+              # reset this enrollment for the next user
+              enrollment.course_section = c.default_section
+              enrollment.save!
+            else
+              edit_section_link.nil?.should be_true
+              unenroll_user_link.nil?.should be_true
+            end
+          else
+            edit_section_link.nil?.should be_true
+            if can_modify
+              unenroll_user_link.nil?.should be_false
+              unenroll_user_link.attribute(:class).should match(/cant_unenroll/)
+            else
+              unenroll_user_link.nil?.should be_true
+            end
+          end
+        end
+      end
+    end
   end
 end
 
