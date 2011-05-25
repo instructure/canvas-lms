ae78629cfd6f36c41644a48321b41bb7a3fb4a88
Brian Palmer <brianp@instructure.com>
2011-05-25 15:20:50 -0600

    global setting to list authentication ip addresses, closes #4049
    Change-Id: Iac6762bf876d67fb87146f3cf10d5e4cf994556a
    Reviewed-on: https://gerrit.instructure.com/3866
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

---
+++ spec/views/account_authorization_configs/show.html.erb_spec.rb
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2011 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../views_helper')
+
+describe "account_authorization_configs/show" do
+  it "should list the auth ips" do
+    Setting.set('account_authorization_config_ip_addresses', "192.168.0.1,192.168.0.2")
+    assigns[:context] = assigns[:account] = Account.default
+    assigns[:account_config] = Account.default.build_account_authorization_config
+    assigns[:current_user] = user_model
+    assigns[:saml_identifiers] = []
+    render 'account_authorization_configs/show'
+    response.body.should match("192.168.0.1\n192.168.0.2")
+  end
+end
