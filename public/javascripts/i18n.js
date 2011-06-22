b388e018d3ba806dcc27ffb0a39dc3bf59402c90
Jacob Fugal <jacob@instructure.com>
2011-06-22 17:04:08 -0600

    javascript I18n.beforeLabel
    Change-Id: Ifc3b7a6c568a3e8662f8ab2f67afbf904535979d
    Reviewed-on: https://gerrit.instructure.com/4340
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- public/javascripts/i18n.js
+++ public/javascripts/i18n.js
@@ -447,6 +447,12 @@ I18n.scope.prototype = {
   },
   pluralize: function(count, scope, options) {
     return I18n.pluralize(count, this.resolveScope(scope), options);
+  },
+  beforeLabel: function(textOrKey, defaultValue) {
+    if (typeof defaultValue != "undefined") {
+      textOrKey = this.t('labels.' + textOrKey, defaultValue)
+    }
+    return this.t("#before_label_wrapper", "%{text}:", {'text': textOrKey})
   }
 }
 I18n.scope.prototype.t = I18n.scope.prototype.translate;
