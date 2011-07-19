f9892cd70db8fd3109ddce91f7519bc85e257a2c
Ryan Shaw <ryan@instructure.com>
2011-07-19 14:22:06 -0600

    dont render wiki_page body at all if it was deleted
    Change-Id: I15d765572fdbd5cfa36160839bc7a2e7d9a5ed20
    Reviewed-on: https://gerrit.instructure.com/4727
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

---
+++ spec/integration/wiki_page_spec.rb
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2011 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe WikiPagesController do
+  before do
+    course_with_teacher_logged_in(:active_all => true)
+  end
+
+  it "should not render wiki page body at all if it was deleted" do
+    @wiki_page = @course.wiki.wiki_pages.create :title => "Some random wiki page",
+                                                :body => "this is the content of the wikipage body asdfasdf"
+    @wiki_page.destroy
+    get course_wiki_page_url(@course, @wiki_page)
+    response.body.should_not have_text @wiki_page.body
+  end
+end
+
