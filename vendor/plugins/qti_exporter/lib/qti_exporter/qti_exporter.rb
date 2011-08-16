be34b3043929595fb7289c1be139b16b92ffcf8b
Brian Palmer <brianp@instructure.com>
2011-08-16 10:17:39 -0600

    typo causing qti/respondus question groups to fail to import
    Change-Id: I3752d278cdc7ba78892429b7a79ab08992d95649
    Reviewed-on: https://gerrit.instructure.com/5095
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- vendor/plugins/qti_exporter/lib/qti_exporter/qti_exporter.rb
+++ vendor/plugins/qti_exporter/lib/qti_exporter/qti_exporter.rb
@@ -108,7 +108,7 @@ class QtiExporter < Canvas::Migrator
     raise "The QTI must be converted to 2.1 before converting to JSON" unless @converted
     begin
       manifest_file = File.join(@dest_dir_2_1, MANIFEST_FILE)
-      @quizzes[:assessments] = Qti.convert_assessments(manifest_file, :questions => questions)
+      @quizzes[:assessments] = Qti.convert_assessments(manifest_file, :converted_questions => questions)
     rescue => e
       message = "Error processing assessment QTI data: #{$!}: #{$!.backtrace.join("\n")}"
       add_error "qti_assessments", message, @questions, e
