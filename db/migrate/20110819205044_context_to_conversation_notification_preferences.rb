ccbf57557d41561dbf1de7a96aeda7c2fdee0365
Jon Jensen <jon@instructure.com>
2011-08-20 07:34:06 -0600

    fix notification pref migration
    Change-Id: Iae2a29bed902a4868a64032c02fae9493445fc0b
    Reviewed-on: https://gerrit.instructure.com/5179
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- db/migrate/20110819205044_context_to_conversation_notification_preferences.rb
+++ db/migrate/20110819205044_context_to_conversation_notification_preferences.rb
@@ -1,12 +1,12 @@
 class ContextToConversationNotificationPreferences < ActiveRecord::Migration
   def self.up
-    if context_message = Notification.find_by_category("Context Message")
+    if message = Notification.find_by_category_and_name("Message", "Teacher Context Message")
       if conversation_message = Notification.find_by_category("Conversation Message")
         execute <<-SQL
           INSERT INTO notification_policies
             (notification_id, user_id, communication_channel_id, broadcast, frequency)
             SELECT #{conversation_message.id}, user_id, communication_channel_id, broadcast, frequency
-              FROM notification_policies WHERE notification_id=#{context_message.id};
+              FROM notification_policies WHERE notification_id=#{message.id};
         SQL
       end
       if added_to_conversation = Notification.find_by_category("Added To Conversation")
@@ -14,7 +14,7 @@ class ContextToConversationNotificationPreferences < ActiveRecord::Migration
           INSERT INTO notification_policies
             (notification_id, user_id, communication_channel_id, broadcast, frequency)
             SELECT #{added_to_conversation.id}, user_id, communication_channel_id, broadcast, frequency
-              FROM notification_policies WHERE notification_id=#{context_message.id}
+              FROM notification_policies WHERE notification_id=#{message.id}
         SQL
       end
     end
