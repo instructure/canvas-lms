f673176f012d7669b5b43c7a6ac7d3a7cf93501c
Cody Cutrer <cody@instructure.com>
2011-08-23 13:25:25 -0600

    fix deleting ajax-loaded questions from question banks
    fixes #5004
    
    Change-Id: I79e31ac3569fedd1ca7e14c62dfea6964d730e8c
    Reviewed-on: https://gerrit.instructure.com/5223
    Tested-by: Hudson <hudson@instructure.com>
    Tested-by: Selenium <selenium@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

---
+++ spec/selenium/question_banks_sel.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/common')
+
+shared_examples_for "question bank selenium tests" do
+  it_should_behave_like "in-process server selenium tests"
+
+  it "deleting AJAX-loaded questions should work" do
+    course_with_teacher_logged_in
+    @bank = @course.assessment_question_banks.create!(:title=>'Test Bank')
+    (1..60).each { |idx| @bank.assessment_questions.create!(:question_data => {'question_name' => "test question #{idx}", 'answers' => [{'id' => 1}, {'id' => 2}]}) { |aq| aq.context = @course } }
+    get "/courses/#{@course.id}/question_banks/#{@bank.id}"
+    driver.find_element(:css, ".more_questions_link").click
+    keep_trying_until { find_all_with_jquery('.question_teaser:visible').length == 60 }
+    driver.execute_script("$('.question_teaser .links').css('visibility', 'visible')")
+    driver.execute_script("window.confirm = function(msg) { return true; };")
+    find_with_jquery(".question_teaser:visible:last .delete_question_link").click
+    keep_trying_until { find_all_with_jquery('.question_teaser:visible').length == 59 }
+    @bank.reload
+    @bank.assessment_questions.select { |aq| !aq.deleted? }.length.should == 59
+  end
+end
+
+describe "question bank Windows-Firefox-Tests" do
+  it_should_behave_like "question bank selenium tests"
+end
