b193fee7fd25765ee39bb88453bce1605b41a547
JT Olds <jt@instructure.com>
2011-08-26 23:00:00 -0600

    treesame commit of origin/stable/2011-08-20
    Change-Id: Iee396db20f5895232c9ffc67f5faf4f67cccf4ed
    Treesame-Commit-Id: 417d11741479173f8658bbeeb3bdea7f293596d9

--- app/models/enrollment_term.rb
+++ app/models/enrollment_term.rb
@@ -29,23 +29,27 @@ class EnrollmentTerm < ActiveRecord::Base
   has_many :course_sections
   before_validation :verify_unique_sis_source_id
   validates_length_of :sis_data, :maximum => maximum_text_length, :allow_nil => true, :allow_blank => true
-  before_save :update_enrollments_later
+  before_save :update_courses_later_if_necessary
 
-  def update_enrollments_later
-    self.send_later_if_production(:touch_all_enrollments) if !self.new_record? && (self.start_at_changed? || self.end_at_changed?)
+  def update_courses_later_if_necessary
+    self.update_courses_later if !self.new_record? && (self.start_at_changed? || self.end_at_changed?)
   end
 
-  def touch_all_enrollments
+  # specifically for use in specs
+  def reset_touched_courses_flag
+    @touched_courses = false
+  end
+
+  def touch_all_courses
     return if new_record?
-    case Enrollment.connection.adapter_name
-    when 'MySQL'
-      Enrollment.connection.execute("UPDATE users, enrollments, courses SET users.updated_at=NOW(), enrollments.updated_at=NOW() WHERE users.id=enrollments.user_id AND enrollments.course_id=courses.id AND courses.enrollment_term_id=#{self.id}")
-    else
-      Enrollment.update_all({:updated_at => Time.now}, "course_id IN (SELECT id FROM courses WHERE enrollment_term_id=#{self.id})")
-      User.update_all({:updated_at => Time.now}, "id IN (SELECT user_id FROM enrollments INNER JOIN courses ON enrollments.course_id=courses.id WHERE courses.enrollment_term_id=#{self.id})")
-    end
+    Course.update_all({:updated_at => Time.now}, "enrollment_term_id=#{self.id}")
   end
-  
+
+  def update_courses_later
+    self.send_later_if_production(:touch_all_courses) unless @touched_courses
+    @touched_courses = true
+  end
+
   def self.i18n_default_term_name
     t '#account.default_term_name', "Default Term"
   end
@@ -75,7 +79,10 @@ class EnrollmentTerm < ActiveRecord::Base
     params.map do |type, values|
       type = type.classify
       enrollment_type = Enrollment.typed_enrollment(type).to_s
-      override = self.enrollment_dates_overrides.find_or_create_by_enrollment_type(enrollment_type)
+      override = self.enrollment_dates_overrides.find_by_enrollment_type(enrollment_type)
+      override ||= self.enrollment_dates_overrides.build(:enrollment_type => enrollment_type)
+      # preload the reverse association - VERY IMPORTANT so that @touched_enrollments is shared
+      override.enrollment_term = self
       override.start_at = values[:start_at]
       override.end_at = values[:end_at]
       override.context = context
