fb7dec9c34d40bb7ceaadf57da9d5f22f309b448
Brian Palmer <brianp@instructure.com>
2011-09-26 14:31:07 -0600

    hide soon-to-be-dropped columns from activerecord, closes #5758
    Whenever we create a migration that drops or renames a column, in that
    same commit we need to add that column to the list in this initializer.
    
    Then, when we deploy and restart, AR will already see the column as
    removed, and we can safely run the drop migration after deploy without
    any window where users will get page errors due to stale column
    information.
    
    Change-Id: I1c95fd146ac8046af579256cccf95b42f01442b7
    Reviewed-on: https://gerrit.instructure.com/5823
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

---
+++ config/initializers/dropped_columns.rb
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2011 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+class ActiveRecord::Base
+  DROPPED_COLUMNS = {
+    'courses' => %w(section hidden_tabs),
+    'users' => %w(type),
+    'role_overrides' => %w(context_code),
+  }.freeze
+
+  def self.columns_with_remove_dropped_columns
+    @columns_with_dropped ||= self.columns_without_remove_dropped_columns.reject { |c|
+      (DROPPED_COLUMNS[self.table_name] || []).include?(c.name)
+    }
+  end
+
+  def self.reset_column_information_with_remove_dropped_columns
+    @columns_with_dropped = nil
+    self.reset_column_information_without_remove_dropped_columns
+  end
+
+  class << self
+    alias_method_chain :columns, :remove_dropped_columns
+    alias_method_chain :reset_column_information, :remove_dropped_columns
+  end
+end
