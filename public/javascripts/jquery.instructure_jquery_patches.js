2682d0ebbbe422f14c367c04e242b1112e69044a
Ryan Shaw <ryan@instructure.com>
2011-09-27 12:51:26 -0600

    break instructure_helper.js into smaller, modular chunks
    NOTE: a previous commit tried to do this, but it
    broke specs, so it was reverted
    
    this was to introduce sanity where we don't have
    one file with ~3600 lines of javascript:
    
    * if something looked like it was worthy of its own
      plugin file, or should have been one in the first
      place, it got it. (ex: jquery.keycodes.js)
    * if there were a bunch of methods that obviously
      belong together, I put them all together in their
      own file (ex: jquery.instructure_forms.js)
    * all of our monkeypatching of jQuery went into one
      file so it is easier to see where that is happening
      when we choose to upgrade.
    * move the remaining jQuery helpers (things like $.blah)
      into jquery.instructure_misc_helpers.js
    * move remaining jQuery plugins (things like $.fn.blah)
      into jquery.instructure_misc_plugins.js
    
    Change-Id: I2978c79299ec8b73e5e14d941c20ffe35c4bba99
    Reviewed-on: https://gerrit.instructure.com/5862
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Tested-by: Bryan Madsen <bryan@instructure.com>

---
+++ public/javascripts/jquery.instructure_jquery_patches.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2011 Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function($){
+
+  // This is so that if you disable an element, that it also gives it the class disabled.  
+  // that way you can add css classes for our friend IE6. so rather than using selector:disabled, 
+  // you can do selector.disabled.
+  // works on both $(elem).attr('disabled', ...) AND $(elem).prop('disabled', ...)
+  $.each([ "prop", "attr" ], function(i, propOrAttr ) {
+    // set the `disabled.set` hook like this so we don't override any existing `get` hook
+    $[propOrAttr+'Hooks'].disabled = jQuery.extend( $[propOrAttr+'Hooks'].disabled, {
+      set: function( elem, value, name ) {
+        $(elem).toggleClass('disabled', !!value);
+  
+        // have to replicate wat jQuery's boolHook does because once you define your own hook
+        // for an attribute/property it wont fall back to boolHook. and it is not exposed externally.
+        elem[value ? 'setAttribute' : 'removeAttribute' ]('disabled', 'disabled');
+        if ( 'disabled' in elem ) {
+          // Only set the IDL specifically if it already exists on the element
+          // ie for an <input> but not a <div> 
+          elem.disabled = !!value;
+        }
+        return value;
+      }
+    });
+  });
+
+  // this is a patch so you can set the "method" atribute on rails' REST-ful forms.
+  $.attrHooks.method = $.extend($.attrHooks.method, {
+    set: function( elem, value ) {
+      var orginalVal = value;
+      value = value.toUpperCase() === 'GET' ? 'GET' : 'POST';
+      if ( value === 'POST' ) {
+        var $input = $(elem).find("input[name='_method']");
+        if ( !$input.length ) {
+          $input = $("<input type='hidden' name='_method'/>").prependTo(elem);
+        }
+        $input.val(orginalVal);
+      }
+      elem.setAttribute('method', value);
+      return value;
+    }
+  });
+
+  $.fn.originalScrollTop = $.fn.scrollTop;
+  $.fn.scrollTop = function() {
+    if(this.selector == "html,body" && arguments.length === 0) {
+      console.error("$('html,body').scrollTop() is not cross-browser compatible... use $.windowScrollTop() instead");
+    }
+    return $.fn.originalScrollTop.apply(this, arguments);
+  };
+  $.windowScrollTop = function() {
+    return ($.browser.safari ? $("body") : $("html")).scrollTop();
+  };
+  
+
+
+})(jQuery);
