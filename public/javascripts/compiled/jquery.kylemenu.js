36a8e3be77991c75c73c78a84676cafbe510a1e6
Ryan Shaw <ryan@instructure.com>
2011-10-09 00:33:22 -0600

    make kylemenu look like coffeescript
    Change-Id: I30d020984947249b4cf0e8a65c1c36cd100aebac
    Reviewed-on: https://gerrit.instructure.com/6058
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Ryan Florence <ryanf@instructure.com>

--- public/javascripts/compiled/jquery.kylemenu.js
+++ public/javascripts/compiled/jquery.kylemenu.js
@@ -1,46 +1,46 @@
 (function() {
-  (function($) {
-    $.fn.kyleMenu = function(options) {
-      return this.each(function() {
-        var $menu, opts;
-        opts = $.extend(true, {}, $.fn.kyleMenu.defaults, options);
-        if (!opts.noButton) {
-          $(this).button(opts.buttonOpts);
-        }
-        $menu = $(this).next().menu(opts.menuOpts).popup(opts.popupOpts).addClass("ui-kyle-menu use-css-transitions-for-show-hide");
-        return $menu.bind("menuselect", function() {
-          return $(this).removeClass("ui-state-open");
-        });
-      });
-    };
-    return $.fn.kyleMenu.defaults = {
-      popupOpts: {
-        position: {
-          my: 'center top',
-          at: 'center bottom',
-          offset: '0 10px'
-        },
-        open: function(event) {
-          var $trigger, actualOffset, caratOffset, differenceInWidth, triggerWidth;
-          $(this).find(".ui-menu-carat").remove();
-          $trigger = $(this).popup("option", "trigger");
-          triggerWidth = $trigger.width();
-          differenceInWidth = $(this).width() - triggerWidth;
-          actualOffset = event.pageX - $trigger.offset().left;
-          caratOffset = Math.min(Math.max(20, actualOffset), triggerWidth - 20) + differenceInWidth / 2;
-          $('<span class="ui-menu-carat"><span /></span>').css('left', caratOffset).prependTo(this);
-          return $(this).css('-webkit-transform-origin-x', caratOffset + 'px').addClass('ui-state-open');
-        },
-        close: function() {
-          return $(this).removeClass("ui-state-open");
-        }
-      },
-      buttonOpts: {
-        icons: {
-          primary: "ui-icon-home",
-          secondary: "ui-icon-droparrow"
-        }
+  var $;
+  $ = jQuery;
+  $.fn.kyleMenu = function(options) {
+    return this.each(function() {
+      var $menu, opts;
+      opts = $.extend(true, {}, $.fn.kyleMenu.defaults, options);
+      if (!opts.noButton) {
+        $(this).button(opts.buttonOpts);
       }
-    };
-  })(this.jQuery);
+      $menu = $(this).next().menu(opts.menuOpts).popup(opts.popupOpts).addClass("ui-kyle-menu use-css-transitions-for-show-hide");
+      return $menu.bind("menuselect", function() {
+        return $(this).removeClass("ui-state-open");
+      });
+    });
+  };
+  $.fn.kyleMenu.defaults = {
+    popupOpts: {
+      position: {
+        my: 'center top',
+        at: 'center bottom',
+        offset: '0 10px'
+      },
+      open: function(event) {
+        var $trigger, actualOffset, caratOffset, differenceInWidth, triggerWidth;
+        $(this).find(".ui-menu-carat").remove();
+        $trigger = $(this).popup("option", "trigger");
+        triggerWidth = $trigger.width();
+        differenceInWidth = $(this).width() - triggerWidth;
+        actualOffset = event.pageX - $trigger.offset().left;
+        caratOffset = Math.min(Math.max(20, actualOffset), triggerWidth - 20) + differenceInWidth / 2;
+        $('<span class="ui-menu-carat"><span /></span>').css('left', caratOffset).prependTo(this);
+        return $(this).css('-webkit-transform-origin-x', caratOffset + 'px').addClass('ui-state-open');
+      },
+      close: function() {
+        return $(this).removeClass("ui-state-open");
+      }
+    },
+    buttonOpts: {
+      icons: {
+        primary: "ui-icon-home",
+        secondary: "ui-icon-droparrow"
+      }
+    }
+  };
 }).call(this);
