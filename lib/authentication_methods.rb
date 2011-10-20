9fefc7a264a34467ca2de1c95cf6462b6766bf25
Zach Pendleton <zachp@instructure.com>
2011-10-20 13:28:32 -0600

    add sis_user_id option to as_user_id param in API.
    Admins can now masquerade users by giving an SIS id in
    the as_user_id param, e.g.: as_user_id=sis_user_id:1234.
    
    Change-Id: I9bb03ecf53c4ceba574dd4d196c0281ac8dd3141
    Reviewed-on: https://gerrit.instructure.com/6335
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- lib/authentication_methods.rb
+++ lib/authentication_methods.rb
@@ -128,10 +128,14 @@ module AuthenticationMethods
 
     as_user_id = api_request? && params[:as_user_id]
     as_user_id ||= session[:become_user_id]
-    if as_user_id && (user = User.find_by_id(as_user_id)) && user.grants_right?(@current_user, session, :become_user)
-      @real_current_user = @current_user
-      @current_user = user
-      logger.warn "#{@real_current_user.name}(#{@real_current_user.id}) impersonating #{@current_user.name} on page #{request.url}"
+    begin
+      if as_user_id && (user = api_find(User, as_user_id)) && user.grants_right?(@current_user, session, :become_user)
+        @real_current_user = @current_user
+        @current_user = user
+        logger.warn "#{@real_current_user.name}(#{@real_current_user.id}) impersonating #{@current_user.name} on page #{request.url}"
+      end
+    rescue ActiveRecord::RecordNotFound
+      # fail silently
     end
 
     @current_user
