52d33b968b933dfec5c66dfc327c9da386d25ffc
Bracken Mosbacker <bracken@instructure.com>
2011-12-02 12:39:45 -0700

    add ability to add designers on course settings page
    Before this you could only add designers through SIS, now
    you can add it like any of the other roles
    
    Test Plan:
    * add a designer to a course
    * Note, because of #6194 designers can't see unpublished courses right now
    
    closes #4057
    
    Change-Id: Ia1fc0dba52605430097c663de876bdc4b951b09b
    Reviewed-on: https://gerrit.instructure.com/7253
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

--- spec/selenium/user_lists_spec.rb
+++ spec/selenium/user_lists_spec.rb
@@ -9,7 +9,7 @@ describe "user lists Windows-Firefox-Tests" do
     account.save!
   end
 
-  def add_users_to_user_list(include_short_name = true)
+  def add_users_to_user_list(include_short_name = true, enrollment_type = 'StudentEnrollment')
     user = User.create!(:name => 'login_name user')
     user.pseudonyms.create!(:unique_id => "A124123", :account => @course.root_account)
     user.communication_channels.create!(:path => "A124123")
@@ -17,29 +17,36 @@ describe "user lists Windows-Firefox-Tests" do
     user_list = <<eolist
 user1@example.com, "bob sagat" <bob@thesagatfamily.name>, A124123
 eolist
+    if driver.find_elements(:id, 'enrollment_type').length > 0
+      driver.find_element(:id, 'enrollment_type').find_element(:css, "option[value=#{enrollment_type}]").click
+    end
     driver.find_element(:css, "textarea.user_list").send_keys(user_list)
     driver.find_element(:css, "button.verify_syntax_button").click
     driver.find_element(:css, "button.add_users_button").click
     wait_for_ajax_requests
     wait_for_animations
     enrollment = user.reload.enrollments.last
-    keep_trying_until {driver.find_element(:css, "#enrollment_#{enrollment.id}").text.should == ("user, login_name" + (include_short_name ? "\nlogin_name user" : "") + "\nA124123") }
+    extra_line = ""
+    extra_line = "\nlink to a student" if enrollment_type == 'ObserverEnrollment' 
+    keep_trying_until {driver.find_element(:css, "#enrollment_#{enrollment.id}").text.should == ("user, login_name" + (include_short_name ? "\nlogin_name user" : "") + "\nA124123" + extra_line) }
     
     unique_ids = ["user1@example.com", "bob@thesagatfamily.name", "A124123"]
     browser_text = ["user1@example.com\nuser1@example.com\nuser1@example.com", "sagat, bob\nbob sagat\nbob@thesagatfamily.name", "user, login_name\nlogin_name user\nA124123"] if include_short_name
     browser_text = ["user1@example.com\nuser1@example.com", "sagat, bob\nbob@thesagatfamily.name", "user, login_name\nA124123"] unless include_short_name
     Enrollment.all.last(3).sort_by { |e| unique_ids.index(e.user.communication_channels.first.path) }.each do |e|
       e.user.communication_channels.first.path.should == unique_ids.shift
-      driver.find_element(:css, "#enrollment_#{e.id}").text.should == browser_text.shift
+      driver.find_element(:css, "#enrollment_#{e.id}").text.should == browser_text.shift + extra_line
     end
   end
 
-  it "should support both email addresses and user names on the course details page" do
-    course_with_teacher_logged_in(:active_all => true)
-    get "/courses/#{@course.id}/details"
-    driver.find_element(:css, "a#tab-users-link").click
-    driver.find_element(:css, "div#tab-users a.add_users_link").click
-    add_users_to_user_list
+  ['StudentEnrollment', 'TeacherEnrollment', 'TaEnrollment', 'ObserverEnrollment', 'DesignerEnrollment'].each do |type|
+    it "should support adding #{type} enrollments by both email addresses and user names on the course details page" do
+      course_with_teacher_logged_in(:active_all => true)
+      get "/courses/#{@course.id}/details"
+      driver.find_element(:css, "a#tab-users-link").click
+      driver.find_element(:css, "div#tab-users a.add_users_link").click
+      add_users_to_user_list(true, type)
+    end
   end
 
   it "should support both email addresses and user names on the getting started page" do
