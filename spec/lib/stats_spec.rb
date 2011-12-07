6ddcb223e434c2edb005a450c566c04da51965f0
John Harrison <john@instructure.com>
2011-12-07 15:34:37 -0700

    updated stats lib to have histogram
    test plan: n/a
    
    Change-Id: I1d6ee94a1f5917761c5fae6b276539ce32c78a93
    Reviewed-on: https://gerrit.instructure.com/7344
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: JT Olds <jt@instructure.com>

--- spec/lib/stats_spec.rb
+++ spec/lib/stats_spec.rb
@@ -21,7 +21,7 @@ require 'lib/stats'
 
 describe Stats do
   context Stats::Counter do
-    def check_stats_with_matchers(c, empty, size, max, min, sum, mean, var, stddev)
+    def check_stats_with_matchers(c, empty, size, max, min, sum, mean, var, stddev, histogram)
       c.empty?.should empty
       c.size.should size
       c.count.should size
@@ -35,9 +35,10 @@ describe Stats do
       c.variance.should var
       c.stddev.should stddev
       c.standard_deviation.should stddev
+      c.histogram.should histogram
     end
 
-    def check_stats(c, size, max, min, sum, mean, var)
+    def check_stats(c, size, max, min, sum, mean, var, histogram)
       check_stats_with_matchers c,
                                 (size > 0 ? be_false : be_true),
                                 eql(size),
@@ -46,7 +47,8 @@ describe Stats do
                                 eql(sum),
                                 be_close(mean, 0.0000000001),
                                 be_close(var, 0.0000000001),
-                                be_close(Math::sqrt(var), 0.0000000001)
+                                be_close(Math::sqrt(var), 0.0000000001),
+                                eql(histogram)
     end
 
     it "should be able to initialize with an array" do
@@ -55,15 +57,15 @@ describe Stats do
     
     it "should return some basic statistics" do
       c = Stats::Counter.new([1,2,4,9])
-      check_stats(c, 4, 9, 1, 16, 4.0, 9.5)
+      check_stats(c, 4, 9, 1, 16, 4.0, 9.5, {:bin_width => 1.0, :bin_base => 0.0, :data =>{1.0=>1,2.0=>1,4.0=>1,9.0=>1}})
       c << 6
-      check_stats(c, 5, 9, 1, 22, 4.4, 8.24)
+      check_stats(c, 5, 9, 1, 22, 4.4, 8.24, {:bin_width => 1.0, :bin_base => 0.0, :data =>{1.0=>1,2.0=>1,4.0=>1,9.0=>1,6.0=>1}})
       c << -1
-      check_stats(c, 6, 9, -1, 21, 3.5, 139.0/6 - 12.25)
+      check_stats(c, 6, 9, -1, 21, 3.5, 139.0/6 - 12.25, {:bin_width => 1.0, :bin_base => 0.0, :data =>{1.0=>1,2.0=>1,4.0=>1,9.0=>1,6.0=>1,-1.0=>1}})
       c << 3
-      check_stats(c, 7, 9, -1, 24, 24.0/7, 148.0/7 - 576.0/49)
+      check_stats(c, 7, 9, -1, 24, 24.0/7, 148.0/7 - 576.0/49, {:bin_width => 1.0, :bin_base => 0.0, :data =>{1.0=>1,2.0=>1,4.0=>1,9.0=>1,6.0=>1,-1.0=>1,3.0=>1}})
       c << 21
-      check_stats(c, 8, 21, -1, 45, 5.625, 41.984375)
+      check_stats(c, 8, 21, -1, 45, 5.625, 41.984375, {:bin_width => 1.0, :bin_base => 0.0, :data =>{1.0=>1,2.0=>1,4.0=>1,9.0=>1,6.0=>1,-1.0=>1,3.0=>1,21.0=>1}})
     end
 
     it "should return the right things with no values" do
@@ -76,11 +78,13 @@ describe Stats do
                                 eql(0),
                                 be_nil,
                                 be_nil,
-                                be_nil
+                                be_nil,
+                                eql({:bin_width => 1.0, :bin_base => 0.0, :data =>{}})
+                                
       c << -5
-      check_stats(c, 1, -5, -5, -5, -5.0, 0)
+      check_stats(c, 1, -5, -5, -5, -5.0, 0,{:bin_width => 1.0, :bin_base => 0.0, :data =>{-5.0=>1}})
       c << 5
-      check_stats(c, 2, 5, -5, 0, 0.0, 25.0)
+      check_stats(c, 2, 5, -5, 0, 0.0, 25.0,{:bin_width => 1.0, :bin_base => 0.0, :data =>{-5.0=>1,5.0=>1}})
     end
 
     it "should support .each, .<<, and .push" do
@@ -92,5 +96,24 @@ describe Stats do
       c.each { |item| test << item }
       test.should == [1,2,3,1,2,3,4,5]
     end
+    
+    it "should put negative numbers in the proper bin in histograms" do
+      c = Stats::Counter.new([-1, -0.5, 0, 0.5, 1])
+      h = c.histogram
+      h.should == {:bin_width => 1.0, :bin_base => 0.0, :data =>{-1.0=>2, 0.0=>2, 1.0=>1 }}
+    end
+    
+    it "should work with strange bin widths in histogram" do
+      c = Stats::Counter.new([-7,-3,0,1,2,3,4,5,6])
+      h = c.histogram(bin_width = 2.5, bin_base = 0.0)
+      h.should == {:bin_width=>2.5, :bin_base=>0.0, :data=>{0.0=>3, -5.0=>1, 5.0=>2, -7.5=>1, 2.5=>2}}
+    end
+    
+    it "should work with strange bin bases in histogram" do
+      c = Stats::Counter.new([-7,-3,0,1,2,3,4,5,6])
+      h = c.histogram(bin_width = 2.5, bin_base = 1.5)
+      h.should == {:bin_width=>2.5, :bin_base=>1.5, :data=>{1.5=>2, 4.0=>3, -3.5=>1, -8.5=>1, -1.0=>2}}
+    end
+    
   end
 end
