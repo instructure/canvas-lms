f31ee556fa7874bfbc0e767ff13b6f270c183727
Jon Jensen <jon@instructure.com>
2011-12-09 15:58:20 -0700

    recalculate grades on csv download and activity report load
    kicks off a synchronous grade recalculation for the course (or
    user) when downloading the csv or viewing the activity report. we cache
    when we last generated grades, so we can avoid doing it repeatedly on
    page refreshes
    
    Change-Id: I023aa574f08a81c1150018502a8065c2e70ff8dd
    Reviewed-on: https://gerrit.instructure.com/7413
    Reviewed-by: Zach Wily <zach@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -537,6 +537,14 @@ class Enrollment < ActiveRecord::Base
     GradeCalculator.recompute_final_score(user_ids, course_id)
   end
 
+  def self.recompute_final_score_if_stale(user_ids, course_id)
+    Rails.cache.fetch(['recompute_final_scores', course_id, user_ids].cache_key, :expires_in => Setting.get_cached('recompute_grades_window', 600).to_i) do
+      recompute_final_score user_ids, course_id
+      yield if block_given?
+      true
+    end
+  end
+
   def computed_final_grade
     self.course.score_to_grade(self.computed_final_score)
   end
