b40e0641a7fc6aca8d1469544740fdec2e009bd3
Jon Jensen <jon@instructure.com>
2011-12-14 08:16:06 -0700

    stricter checks to avoid casting error
    test plan:
    
    ensure migration runs correctly. in production this should update ~14k
    rows
    
    Change-Id: I623a3fa4fffe6faff069fb44cb2c053a97d3e4c4
    Reviewed-on: https://gerrit.instructure.com/7478
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- db/migrate/20111122162607_submission_points_possible_fix_data.rb
+++ db/migrate/20111122162607_submission_points_possible_fix_data.rb
@@ -5,14 +5,14 @@ class SubmissionPointsPossibleFixData < ActiveRecord::Migration
         execute <<-SQL
           UPDATE quiz_submissions, quizzes
           SET quiz_points_possible = points_possible
-          WHERE quiz_id = quizzes.id AND quiz_points_possible <> points_possible AND (quiz_points_possible = CAST(points_possible AS SIGNED) OR quiz_points_possible = 2147483647)
+          WHERE quiz_id = quizzes.id AND quiz_points_possible <> points_possible AND (points_possible < 2147483647 AND quiz_points_possible = CAST(points_possible AS SIGNED) OR points_possible >= 2147483647 AND quiz_points_possible = 2147483647)
         SQL
       when 'PostgreSQL'
         execute <<-SQL
           UPDATE quiz_submissions
           SET quiz_points_possible = points_possible
           FROM quizzes
-          WHERE quiz_id = quizzes.id AND quiz_points_possible <> points_possible AND quiz_points_possible = CAST(points_possible AS INTEGER)
+          WHERE quiz_id = quizzes.id AND quiz_points_possible <> points_possible AND (points_possible < 2147483647 AND quiz_points_possible = CAST(points_possible AS INTEGER) OR points_possible >= 2147483647 AND quiz_points_possible = 2147483647)
         SQL
       # no fix needed for sqlite
     end
