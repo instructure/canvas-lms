95a8d305ee4ef60d972d14b9e6f9dbbd9b119399
Jacob Fugal <jacob@instructure.com>
2011-12-14 17:37:06 -0700

    improve data hygiene in GradingStandards
    when setting a standard's data, don't unexpectedly modify the argument.
    when reading data from an older version standard, make note of the fact
    that we've upgraded the data after the first access, so we don't
    "upgrade" (read: corrupt) it over and over again.
    
    fixes #6649
    
    test-plan:
      - create a course. give it a v1 grading standard through the console
        (load the GradingStandard, then set it's data via write_attribute
        and set the version to 1)
      - add several students to the course.
      - download the gradebook csv
      - letter grades should not drift upwards as you move down the list of
        students
    
    Change-Id: I9a50a882b669fe4c991433e14660995cee28b222
    Reviewed-on: https://gerrit.instructure.com/7517
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- spec/models/grading_standard_spec.rb
+++ spec/models/grading_standard_spec.rb
@@ -19,8 +19,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 describe GradingStandard do
-  it "should upgrade the standard scheme from v1 to v2" do
-    default_standard_v1 = {
+  before do
+    @default_standard_v1 = {
       "A" => 1.0,
       "A-" => 0.93,
       "B+" => 0.89,
@@ -34,12 +34,44 @@ describe GradingStandard do
       "D-" => 0.63,
       "F" => 0.6
     }.to_a.sort_by { |i| i[1] }.reverse
-    converted = GradingStandard.upgrade_data(default_standard_v1, 1)
-    default = GradingStandard.default_grading_standard
-    converted.size.should == default.size
-    converted.each_with_index do |row, i|
-      row[0].should == default[i][0]
-      row[1].should be_close(default[i][1], 0.001)
+  end
+
+  def compare_schemes(subject, expected)
+    subject.size.should == expected.size
+    subject.each_with_index do |row, i|
+      row[0].should == expected[i][0]
+      row[1].should be_close(expected[i][1], 0.001)
     end
+  end
+
+  it "should upgrade the standard scheme from v1 to v2" do
+    converted = GradingStandard.upgrade_data(@default_standard_v1, 1)
+    default = GradingStandard.default_grading_standard
+    compare_schemes(converted, default)
+  end
+
+  it "should not the argument to data=" do
+    input = [['A', 0.9999]]
+    standard = GradingStandard.new
+    standard.data = input
+    standard.data[0][1].should be_close(0.999, 0.00001)
+    input[0][1].should be_close(0.9999, 0.00001)
+  end
+
+  it "should upgrade in memory when accessing data" do
+    standard = GradingStandard.new
+    standard.write_attribute(:data, @default_standard_v1)
+    standard.write_attribute(:version, 1)
+    compare_schemes(standard.data, GradingStandard.default_grading_standard)
+    standard.version.should == GradingStandard::VERSION
+  end
+
+  it "should not upgrade repeatedly when accessing data repeatedly" do
+    standard = GradingStandard.new
+    standard.write_attribute(:data, @default_standard_v1)
+    standard.write_attribute(:version, 1)
+    compare_schemes(standard.data, GradingStandard.default_grading_standard)
+    compare_schemes(standard.data, GradingStandard.default_grading_standard)
+    compare_schemes(standard.data, GradingStandard.default_grading_standard)
   end
 end
