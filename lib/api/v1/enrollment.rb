5d6cbfae2bb32c606bd7e32a8953873ace86914f
Cody Cutrer <cody@instructure.com>
2012-01-05 15:00:18 -0700

    remove sis_source_id from enrollments API
    this hasn't been released yet, and doesn't actually mean anything
    
    test plan: run the specs
    
    Change-Id: I391623e63fa7710717c26f33dac9c5ae220577e3
    Reviewed-on: https://gerrit.instructure.com/7902
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Cody Cutrer <cody@instructure.com>

--- lib/api/v1/enrollment.rb
+++ lib/api/v1/enrollment.rb
@@ -2,7 +2,7 @@ module Api::V1::Enrollment
   include Api::V1::Json
 
   API_ENROLLMENT_JSON_OPTS = [:root_account_id, :id, :user_id, :course_section_id,
-    :limit_privileges_to_course_section, :workflow_state, :course_id, :sis_source_id, :type]
+    :limit_privileges_to_course_section, :workflow_state, :course_id, :type]
 
   def enrollment_json(enrollment, user, session)
     api_json(enrollment, user, session, :only => API_ENROLLMENT_JSON_OPTS).tap do |json|
