8005fcbe3b8fcd561ce9e263dfc0679bb852908e
Brian Palmer <brianp@instructure.com>
2012-01-10 19:12:36 -0700

    upgrade nokogiri and sanitize gems
    Updating to nokogiri 1.5.0 requires updating to sanitize 2.0.3 as well.
    
    Since the API for sanitize changed, we take this opportunity to remove
    the monkey patching in config/initializers, and use the actual
    transformers plugin interface for sanitize.
    
    The changes to html in the specs are due to nokogiri making a couple
    changes around empty tags -- html5 wants <img> , not <img />
    
    test plan: The existing specs exercise both gems, to ensure
    compatibility.
    
    Change-Id: Id04d017dda056e03205b373ac9bfbf71bd338cb9
    Reviewed-on: https://gerrit.instructure.com/7988
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

--- spec/lib/cc/importer/common_cartridge_converter_spec.rb
+++ spec/lib/cc/importer/common_cartridge_converter_spec.rb
@@ -39,11 +39,11 @@ describe "Standard Common Cartridge importing" do
     file2_id = @course.attachments.find_by_migration_id("I_00006_Media").id
     
     dt =  @course.discussion_topics.find_by_migration_id("I_00006_R")
-    dt.message.should == %{<p>Your face is ugly. <br /><img src="/courses/#{@course.id}/files/#{file1_id}/preview" /></p>}
+    dt.message.should == %{<p>Your face is ugly. <br><img src="/courses/#{@course.id}/files/#{file1_id}/preview"></p>}
     dt.attachment_id = file2_id
     
     dt =  @course.discussion_topics.find_by_migration_id("I_00009_R")
-    dt.message.should == %{<p>Monkeys: Go!</p>\n<ul><li>\n<a href="/courses/#{@course.id}/files/#{file2_id}/preview">angry_person.jpg</a>\n</li>\n<li>\n<a href="/courses/#{@course.id}/files/#{file1_id}/preview">smiling_dog.jpg</a>\n</li>\n</ul>} 
+    dt.message.should == %{<p>Monkeys: Go!</p>\n<ul>\n<li>\n<a href="/courses/#{@course.id}/files/#{file2_id}/preview">angry_person.jpg</a>\n</li>\n<li>\n<a href="/courses/#{@course.id}/files/#{file1_id}/preview">smiling_dog.jpg</a>\n</li>\n</ul>} 
   end
 
   # This also tests the WebLinks, they are just content tags and don't have their own class
