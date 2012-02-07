288edbd8615ab3a7423493d65431bfd1c7437993
Jon Jensen <jon@instructure.com>
2012-02-07 14:33:23 -0700

    fix submission comment migration, clean up data. fixes #7203
    also make view resilient to bad data in the future
    
    test plan:
    
    confirm that the dummy conversation messages go away and that people stop
    getting dashboard errors
    
    Change-Id: I2d59bf400f7b5b92debf4a4e0af2de2204878cf8

---
+++ db/migrate/20120207210631_submission_comment_conversation_fix.rb
@@ -0,0 +1,11 @@
+class SubmissionCommentConversationFix < ActiveRecord::Migration
+  def self.up
+    Submission.find_by_sql("SELECT * FROM submissions WHERE id IN (SELECT asset_id FROM conversation_messages WHERE asset_id IS NOT NULL AND body = '')").each do |submission|
+      submission.create_or_update_conversations!(:destroy) if submission.visible_submission_comments.empty?
+    end
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
