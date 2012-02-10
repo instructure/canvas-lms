eb11fc1fb08d154b500ed163efa9b9aa4fc52494
Jon Jensen <jon@instructure.com>
2012-02-10 10:57:17 -0700

    refine conversation creation for group submissions, fixes #7212
    changes the behavior such that the only submission comments that create/
    update conversations are ones by participants in the conversation.
    previously, a comment on a group submission would update all private
    conversations between the teacher(s) and all group members, rather than
    the one(s) between the teacher(s) and the commenter. group comments by a
    teacher will still go to each of his conversations with group members
    
    test plan:
    
    1. create a group assignment
    2. submit it as a student
    3. make a group comment as that student
    4. ensure the student's conversation with the teacher gets updated if it
       already exists
    5. ensure the teacher's conversation with the student gets updated and
       marked as unread (if not already)
    6. ensure that no conversations between other group members and the
       teacher get created/updated
    7. now make a group comment as the teacher
    8. ensure that the conversations get updated/created between the teacher
       and all group memebrs, i.e.
       1. they should be unread from the student's perspective
       2. for everyone, the teacher's comment should be the reflected in the
          left-hand conversation preview
    
    Change-Id: I1cfac34fc7b78fad17edf93acaa50b8b97461af6
    Reviewed-on: https://gerrit.instructure.com/8644
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>

---
+++ db/migrate/20120210173646_remove_irrelevant_submission_messages.rb
@@ -0,0 +1,24 @@
+class RemoveIrrelevantSubmissionMessages < ActiveRecord::Migration
+  def self.up
+    # destroy any submission messages where none of the commenters are
+    # participants in the conversation. in production, this will remove about
+    # 7k rows
+    ConversationMessage.destroy_all(<<-CONDITIONS)
+      asset_id IS NOT NULL
+      AND id NOT IN (
+        SELECT DISTINCT cm.id
+        FROM conversation_messages cm,
+          conversation_participants cp,
+          submission_comments sc
+        WHERE 
+          cm.asset_id = sc.submission_id
+          AND cp.conversation_id = cm.conversation_id
+          AND sc.author_id = cp.user_id
+      )
+    CONDITIONS
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
