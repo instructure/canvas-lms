c6eef316129f4d967e951db51c876461c6edcc30
Jon Jensen <jon@instructure.com>
2012-02-13 16:29:27 -0700

    do data migrations outside of a transaction
    Change-Id: I4d43559ddff61f90593f8bb5616c2f1c57932717
    Reviewed-on: https://gerrit.instructure.com/8685
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- db/migrate/20120208180341_remove_duplicate_submission_messages.rb
+++ db/migrate/20120208180341_remove_duplicate_submission_messages.rb
@@ -1,5 +1,10 @@
 class RemoveDuplicateSubmissionMessages < ActiveRecord::Migration
   def self.up
+    if supports_ddl_transactions?
+      commit_db_transaction
+      decrement_open_transactions while open_transactions > 0
+    end
+
     # destroy rather than delete so that callbacks happen 
     ConversationMessage.destroy_all(<<-CONDITIONS)
       asset_id IS NOT NULL
@@ -10,6 +15,11 @@ class RemoveDuplicateSubmissionMessages < ActiveRecord::Migration
         GROUP BY conversation_id, asset_id
       )
     CONDITIONS
+
+    if supports_ddl_transactions?
+      increment_open_transactions
+      begin_db_transaction
+    end
   end
 
   def self.down
