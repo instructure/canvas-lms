dadfce7c872ab2f5596d59a568b6f0b51adc2df3
Cody Cutrer <cody@instructure.com>
2012-02-16 10:45:42 -0700

    detect pseudonyms that are in the transition state for trusted
    fixes #7166
    
    test plan:
     * create a user in another account
     * login with that user at the default account
     * should get a warning about needing to log in
       at the correct account
    
    Change-Id: I976d51f0e3f18f0b4a9c7303bf9f71668c8c1f2b
    Reviewed-on: https://gerrit.instructure.com/8745
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>

--- app/controllers/pseudonym_sessions_controller.rb
+++ app/controllers/pseudonym_sessions_controller.rb
@@ -132,6 +132,9 @@ class PseudonymSessionsController < ApplicationController
       if valid_alternatives.map(&:user).uniq.length == 1
         # prefer a pseudonym from Site Admin if possible, otherwise just choose one
         valid_alternative = valid_alternatives.find {|p| p.account_id == Account.site_admin.id } || valid_alternatives.first
+        if (!@domain_root_account.trusted_account_ids.include?(valid_alternative.account_id))
+          flash[:notice] = t 'warnings.wrong_login_spot', "In the future, please login at %{domain}.", :domain => HostUrl.context_host(valid_alternative.account)
+        end
         @pseudonym_session = PseudonymSession.new(valid_alternative, params[:pseudonym_session][:remember_me] == "1")
         @pseudonym_session.save
         found = true
@@ -357,7 +360,7 @@ class PseudonymSessionsController < ApplicationController
 
   def successful_login(user, pseudonym)
     respond_to do |format|
-      flash[:notice] = t 'notices.login_success', "Login successful."
+      flash[:notice] ||= t 'notices.login_success', "Login successful."
       if session[:oauth2]
         # this is where we will verify client authorization and scopes, once implemented
         # .....
