8dc494cf5ea00c86efaed2a6d9f96bf5051a576f
Ryan Shaw <ryan@instructure.com>
2012-02-23 18:04:14 -0700

    don't disable a calendar when you click something in it's cog menu
    fixes: #7111
    
    test plan:
    * in calendar2, click cog menu on one of the courses
      on the sidebar 
    * click either add event or add assignment
    * verify that the calendar does not get turned off.
    
    Change-Id: I87a8c7c5f5a11027385c61a21b00deb487b73adf
    Reviewed-on: https://gerrit.instructure.com/8982
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Jake Sorce <jake@instructure.com>

--- spec/selenium/calendar2_spec.rb
+++ spec/selenium/calendar2_spec.rb
@@ -116,33 +116,29 @@ describe "calendar2" do
         end
 
         it "should create an event through the context list drop down" do
-          pending("Bug 7111 - Course in context list being disabled bug") do
-            event_title = 'new event'
-            get "/calendar2"
-            wait_for_ajaximations
+          event_title = 'new event'
+          get "/calendar2"
+          wait_for_ajaximations
 
-            driver.execute_script(%{$(".context_list_context:nth-child(2)").trigger('mouseenter')})
-            find_with_jquery('ul#context-list li:nth-child(2) button').click
-            driver.find_element(:id, "ui-menu-1-0").click
-            edit_event_dialog = driver.find_element(:id, 'edit_event_tabs')
-            edit_event_dialog.should be_displayed
-            create_calendar_event(event_title)
-          end
+          driver.execute_script(%{$(".context_list_context:nth-child(2)").trigger('mouseenter')})
+          find_with_jquery('ul#context-list li:nth-child(2) button').click
+          driver.find_element(:id, "ui-menu-1-0").click
+          edit_event_dialog = driver.find_element(:id, 'edit_event_tabs')
+          edit_event_dialog.should be_displayed
+          create_calendar_event(event_title)
         end
 
         it "should create an assignment through the context list drop down" do
-          pending("Bug 7111 - Course in context list being disabled bug") do
-            assignment_title = 'new assignment'
-            get "/calendar2"
-            wait_for_ajaximations
+          assignment_title = 'new assignment'
+          get "/calendar2"
+          wait_for_ajaximations
 
-            driver.execute_script(%{$(".context_list_context:nth-child(2)").trigger('mouseenter')})
-            find_with_jquery('ul#context-list li:nth-child(2) button').click
-            driver.find_element(:id, "ui-menu-1-1").click
-            edit_event_dialog = driver.find_element(:id, 'edit_event_tabs')
-            edit_event_dialog.should be_displayed
-            create_assignment_event(assignment_title)
-          end
+          driver.execute_script(%{$(".context_list_context:nth-child(2)").trigger('mouseenter')})
+          find_with_jquery('ul#context-list li:nth-child(2) button').click
+          driver.find_element(:id, "ui-menu-1-1").click
+          edit_event_dialog = driver.find_element(:id, 'edit_event_tabs')
+          edit_event_dialog.should be_displayed
+          create_assignment_event(assignment_title)
         end
 
         it "should toggle event display when context is clicked" do
