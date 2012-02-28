763a858c96f6d3cb663feb683d6bc552ee96637f
Zach Pendleton <zachp@instructure.com>
2012-02-28 16:17:04 -0700

    ensure jquery exists for all global javascripts.
    when a user includes a global javascript include, make sure that
    jquery has been loaded and is available for use.
    
    Change-Id: I4506a28355c4015f2a015faff4d50f699fb4a615
    Reviewed-on: https://gerrit.instructure.com/9071
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Ryan Florence <ryanf@instructure.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -641,7 +641,9 @@ module ApplicationHelper
     end
     if includes.length > 0
       content_tag :script, <<-ENDSCRIPT
-        require([#{includes.join(', ')}]);
+        require(['jquery'], function() {
+          require([#{includes.join(', ')}]);
+        });
       ENDSCRIPT
     end
   end
