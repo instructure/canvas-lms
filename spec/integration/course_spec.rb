8d83c5b5b16bcafaf85f61558acdd8a90c1dcbb1
Cameron Matheson <cameron@instructure.com>
2012-02-29 10:46:22 -0700

    fix todo bug introduced in 17494bce1a745af20c8ec20
    Test plan:
      - go to a course page without logging in
    
    Change-Id: I185f74f41cbe547efdba41064f650b5fe7ed2e5a
    Reviewed-on: https://gerrit.instructure.com/9080
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

---
+++ spec/integration/course_spec.rb
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2012 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "course" do
+
+  # normally this would be a controller test, but there is a some code in the
+  # views that i need to not explode
+  it "should not require authorization for public courses" do
+    course(:active_all => true)
+    @course.update_attribute(:is_public, true)
+    get "/courses/#{@course.id}"
+    response.should be_success
+  end
+
+end
