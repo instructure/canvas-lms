2137fc8c99b7053bc88edbf80cd52b01bf52d03c
Jon Jensen <jon@instructure.com>
2012-04-02 17:28:16 -0600

    fix attachment migration for mysql, make it nicer for all dbs
    * changes temp table usage to be more efficient (also, mysql doesn't let
      you ref the same temp table twice in a query)
    * make it non-transactional to reduce locking
    * break up the big update statements into batches of 1000
    
    test plan:
    * confirm the migration runs in mysql/postgres/sqlite, and the migration
      spec passes for all three as well
    
    Change-Id: Ib15028fa49103230e2cb4957754d2ac19853f90c
    Reviewed-on: https://gerrit.instructure.com/9802
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

--- db/migrate/20120227194305_reassociate_conversation_attachments.rb
+++ db/migrate/20120227194305_reassociate_conversation_attachments.rb
@@ -1,5 +1,6 @@
 class ReassociateConversationAttachments < ActiveRecord::Migration
   tag :postdeploy
+  self.transactional = false
 
   def self.up
     temp_table_options = adapter_name =~ /mysql/i ? 'engine=innodb' : 'AS'
@@ -37,26 +38,55 @@ class ReassociateConversationAttachments < ActiveRecord::Migration
       FROM _conversation_message_attachments
     SQL
 
-    execute <<-SQL
-      UPDATE conversation_messages
-      SET attachment_ids = (
-        SELECT #{connection.func(:group_concat, :attachment_id, ',')}
-        FROM _conversation_message_attachments
-        WHERE conversation_message_id = conversation_messages.id
-      )
-      WHERE id IN (
-        SELECT conversation_message_id FROM _conversation_message_attachments
-      )
-    SQL
-
-    execute <<-SQL
-      UPDATE attachments
-      SET context_type = 'User',
-        context_id = (SELECT author_id FROM _conversation_message_attachments WHERE attachment_id = attachments.id),
-        folder_id = (SELECT f.id FROM folders f, _conversation_message_attachments cma WHERE f.name = 'conversation attachments' AND f.context_type = 'User' AND f.context_id = cma.author_id AND cma.attachment_id = attachments.id LIMIT 1)
-      WHERE context_type = 'ConversationMessage'
-        AND id IN (SELECT attachment_id FROM _conversation_message_attachments)
-    SQL
+    cmas = connection.select_all("SELECT * FROM _conversation_message_attachments")
+    cmas.map{ |r| r['conversation_message_id'] }.uniq.each_slice(1000) do |ids|
+      execute <<-SQL
+        UPDATE conversation_messages
+        SET attachment_ids = (
+          SELECT #{connection.func(:group_concat, :attachment_id, ',')}
+          FROM _conversation_message_attachments
+          WHERE conversation_message_id = conversation_messages.id
+        )
+        WHERE id IN (#{ids.join(', ')})
+      SQL
+      
+    end
+    cmas.map{ |r| r['attachment_id'] }.each_slice(1000) do |ids|
+      case adapter_name
+        when /mysql/i
+          execute <<-SQL
+            UPDATE attachments, _conversation_message_attachments
+            SET context_type = 'User',
+              context_id = _conversation_message_attachments.author_id,
+              folder_id = (SELECT f.id FROM folders f WHERE f.name = 'conversation attachments' AND f.context_type = 'User' AND f.context_id = _conversation_message_attachments.author_id LIMIT 1)
+            WHERE context_type = 'ConversationMessage'
+              AND attachments.id = _conversation_message_attachments.attachment_id
+              AND attachments.id IN (#{ids.join(', ')})
+          SQL
+        when /postgres/i
+          execute <<-SQL
+            UPDATE attachments
+            SET context_type = 'User',
+              context_id = _conversation_message_attachments.author_id,
+              folder_id = (SELECT f.id FROM folders f WHERE f.name = 'conversation attachments' AND f.context_type = 'User' AND f.context_id = _conversation_message_attachments.author_id LIMIT 1)
+            FROM _conversation_message_attachments
+            WHERE context_type = 'ConversationMessage'
+              AND attachments.id = _conversation_message_attachments.attachment_id
+              AND attachments.id IN (#{ids.join(', ')})
+          SQL
+        else
+          execute <<-SQL
+            UPDATE attachments
+            SET context_type = 'User',
+              context_id = (SELECT author_id FROM _conversation_message_attachments WHERE attachment_id = attachments.id),
+              folder_id = (SELECT f.id FROM folders f, _conversation_message_attachments cma WHERE f.name = 'conversation attachments' AND f.context_type = 'User' AND f.context_id = cma.author_id AND cma.attachment_id = attachments.id LIMIT 1)
+            WHERE context_type = 'ConversationMessage'
+              AND id IN (SELECT attachment_id FROM _conversation_message_attachments)
+              AND attachments.id IN (#{ids.join(', ')})
+          SQL
+      end
+    end
+    execute "DROP TABLE _conversation_message_attachments"
   end
 
   def self.down
