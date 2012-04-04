77002e81dd1872fba0676724ffd1d3302e0b4cbc
Brian Palmer <brianp@instructure.com>
2012-04-04 14:12:28 -0600

    put recompute_student_scores in a singleton by course
    Change-Id: Ic40c4eb92669a2f58b7ed9809d89d14ee1226800
    Reviewed-on: https://gerrit.instructure.com/9834
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -648,7 +648,8 @@ class Course < ActiveRecord::Base
   def recompute_student_scores
     Enrollment.recompute_final_score(self.students.map(&:id), self.id)
   end
-  handle_asynchronously_if_production :recompute_student_scores
+  handle_asynchronously_if_production :recompute_student_scores,
+    :singleton => proc { |c| "recompute_student_scores:#{ c.global_id }" }
 
   def home_page
     WikiNamespace.default_for_context(self).wiki.wiki_page
