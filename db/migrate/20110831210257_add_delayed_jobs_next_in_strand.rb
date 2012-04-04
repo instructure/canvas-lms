fad518c08208512eea6f6170b2b8a7883ac54882
Cody Cutrer <cody@instructure.com>
2012-04-04 16:24:53 -0600

    clean up temporary tables in migrations
    since an external connection pool might be in use, and it would mess
    up future migrations
    
    test plan:
     * create two new shards through pgbouncer in the same database in
       quick succession
    
    Change-Id: I2bf582a543a48f9df83888feda54818211931a34
    Reviewed-on: https://gerrit.instructure.com/9842
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Tested-by: Hudson <hudson@instructure.com>

--- db/migrate/20110831210257_add_delayed_jobs_next_in_strand.rb
+++ db/migrate/20110831210257_add_delayed_jobs_next_in_strand.rb
@@ -96,6 +96,7 @@ class AddDelayedJobsNextInStrand < ActiveRecord::Migration
       # use temp tables to work around subselect limitations in mysql
       execute(%{CREATE TEMPORARY TABLE dj_20110831210257 (strand varchar(255), next_job_id bigint) SELECT strand, min(id) as next_job_id FROM delayed_jobs WHERE strand IS NOT NULL GROUP BY strand})
       execute(%{UPDATE delayed_jobs SET next_in_strand = #{Delayed::Job.quote_value(false)} WHERE strand IS NOT NULL AND id <> (SELECT t.next_job_id FROM dj_20110831210257 t WHERE t.strand = delayed_jobs.strand)})
+      execute(%{DROP TABLE dj_20110831210257})
     else
       execute(%{UPDATE delayed_jobs SET next_in_strand = #{Delayed::Job.quote_value(false)} WHERE strand IS NOT NULL AND id <> (SELECT id FROM delayed_jobs j2 WHERE j2.strand = delayed_jobs.strand ORDER BY j2.strand, j2.id ASC LIMIT 1)})
     end
