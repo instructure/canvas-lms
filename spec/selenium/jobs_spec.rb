d36bedf65d027d092b519560a24ef6fbdef4f23a
Efrain Chu-Jon <efrain@instructure.com>
2012-04-09 15:19:46 -0600

    spec: more jobs specs
    Change-Id: I144cc588d00c705a7105657819f57a94705cb424
    Reviewed-on: https://gerrit.instructure.com/10039
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Jake Sorce <jake@instructure.com>

--- spec/selenium/jobs_spec.rb
+++ spec/selenium/jobs_spec.rb
@@ -6,9 +6,11 @@ describe "jobs ui" do
   before do
     site_admin_logged_in
     2.times { "ohai".send_later :reverse }
+    "hello".send_at Time.now + 40.days, :capitalize
   end
 
   describe "actions" do
+
     it "should only action the individual job when it has been searched for" do
       j = Delayed::Job.last(:order => :id)
       get "/jobs?id=#{j.id}"
@@ -17,31 +19,73 @@ describe "jobs ui" do
       j.reload.locked_by.should == 'on hold'
       Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 1
     end
+    context "go to the jobs page" do
+      before do
+        get "/jobs"
+        wait_for_ajax_requests
+      end
 
-    it "should not action if no rows are selected" do
-      get "/jobs"
-      wait_for_ajax_requests
-      f("#hold-jobs").click
-      driver.switch_to.alert.should_not be_nil
-      driver.switch_to.alert.accept
-      Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 0
+      it "should not action if no rows are selected" do
+        f("#hold-jobs").click
+        driver.switch_to.alert.should_not be_nil
+        driver.switch_to.alert.accept
+        Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 0
+      end
+
+      it "should confirm that all rows were selected" do
+        f("#select-all-jobs").click
+        f("#hold-jobs").click
+        driver.switch_to.alert.should_not be_nil
+        driver.switch_to.alert.accept
+        wait_for_ajax_requests
+        Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 2
+      end
+
+      it "should confirm that all rows were selected" do
+        f("#select-all-jobs").click
+        f("#hold-jobs").click
+        driver.switch_to.alert.should_not be_nil
+        driver.switch_to.alert.accept
+        wait_for_ajax_requests
+        Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 2
+      end
+
+      it "should confirm that future jobs were selected" do
+        f("#jobs-flavor option[value='future']").click
+        f("#jobs-refresh").click
+        wait_for_ajax_requests
+        job = Delayed::Job.find_by_tag("String#capitalize")
+        f("#jobs-grid .l0").text.should eql job.id.to_s
+      end
+
+      it "should confirm that failed jobs were selected" do
+        f("#jobs-flavor option[value='failed']").click
+        f("#jobs-refresh").click
+        wait_for_ajax_requests
+      end
+
+      it "should confirm that clicking on delete button should delete jobs" do
+        f("#jobs-flavor option[value='all']").click
+        wait_for_ajax_requests
+        f("#select-all-jobs").click
+        f("#jobs-grid .odd").should be_displayed
+        f("#jobs-grid .even").should be_displayed
+        Delayed::Job.count.should eql 3
+        f("#delete-jobs").click
+        driver.switch_to.alert.should_not be_nil
+        driver.switch_to.alert.accept
+        wait_for_ajax_requests
+        f("#jobs-grid .odd").should be_nil
+        f("#jobs-grid .even").should be_nil
+        Delayed::Job.count.should eql 0
+      end
     end
 
-    it "should confirm that all rows were selected" do
-      get "/jobs"
-      wait_for_ajax_requests
-      f("#select-all-jobs").click
-      f("#hold-jobs").click
-      driver.switch_to.alert.should_not be_nil
-      driver.switch_to.alert.accept
-      wait_for_ajax_requests
-      Delayed::Job.count(:conditions => {:locked_by => 'on hold'}).should == 2
-    end
   end
 
   describe "running jobs" do
     it "should display running jobs in the workers grid" do
-      j = Delayed::Job.last(:order => :id)
+      j = Delayed::Job.first(:order => :id)
       j.lock_exclusively!(100, 'my test worker')
       get "/jobs"
       wait_for_ajax_requests
