4725d3a81b3914d8c1c1a098a42e0585ce89b1f2
Ryan Shaw <ryan@instructure.com>
2012-05-02 14:45:40 -0600

    don't allow students to edit graded discussions, fixes: #7289
    test plan:
    * make a discussion topic that is a graded group assignment
    * make a reply as the teacher
    * view it as a student, verify that you cannot edit the
      teacher's entry.
    
    Change-Id: I2af43dfda854d85b86f7dda0aa652c6c62b1292b
    Reviewed-on: https://gerrit.instructure.com/10501
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- spec/models/discussion_topic_spec.rb
+++ spec/models/discussion_topic_spec.rb
@@ -199,7 +199,7 @@ describe DiscussionTopic do
       @sub_topic = @group.discussion_topics.build(:title => "sub topic", :message => "not ok to be edited", :user => @first_user)
       @sub_topic.root_topic_id = @parent_topic.id
       @sub_topic.save!
-      @group_topic.grants_right?(@second_user, nil, :update).should eql(true)
+      @group_topic.grants_right?(@second_user, nil, :update).should eql(false)
       @sub_topic.grants_right?(@second_user, nil, :update).should eql(false)
     end
   end
