f13ed72c461d4cc372f945af9e054a94ec9b0bfb
Brian Palmer <brianp@instructure.com>
2012-05-17 20:30:42 -0600

    fix class variable warnings in 1.9.3
    Change-Id: If72525d059f3628ad0071f1c8ceffad91c14af67
    Reviewed-on: https://gerrit.instructure.com/10855
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Zach Wily <zach@instructure.com>

--- config/initializers/connection_specification.rb
+++ config/initializers/connection_specification.rb
@@ -1,50 +1,46 @@
 ActiveRecord::Base::ConnectionSpecification.class_eval do
-  @@environment = ENV['RAILS_DATABASE_ENVIRONMENT'].presence.try(:to_sym)
-  @@explicit_user = ENV['RAILS_DATABASE_USER'].presence
 
   cattr_accessor :environment, :explicit_user
-
-  def self.environment
-    @@environment
-  end
+  self.environment = ENV['RAILS_DATABASE_ENVIRONMENT'].presence.try(:to_sym)
+  self.explicit_user = ENV['RAILS_DATABASE_USER'].presence
 
   def config
     @current_config ||= @config
     @current_config_key ||= []
-    return @current_config if @current_config_key == [@@environment, @@explicit_user]
-    @current_config_key = [@@environment, @@explicit_user]
+    return @current_config if @current_config_key == [self.class.environment, self.class.explicit_user]
+    @current_config_key = [self.class.environment, self.class.explicit_user]
     @current_config = @config.dup
-    if @@environment && @config.has_key?(@@environment)
-      @current_config.merge!(@config[@@environment].symbolize_keys)
+    if self.class.environment && @config.has_key?(self.class.environment)
+      @current_config.merge!(@config[self.class.environment].symbolize_keys)
     end
     @current_config[:username] = @current_config[:username].gsub('{schema}', @current_config[:schema_search_path]) if @current_config[:username] && @current_config[:schema_search_path]
-    if @@explicit_user
-      @current_config[:username] = @@explicit_user
+    if self.class.explicit_user
+      @current_config[:username] = self.class.explicit_user
       @current_config.delete(:password)
     end
     @current_config
   end
 
   def self.with_environment(environment)
-    return yield if environment == @@environment
+    return yield if environment == self.environment
     begin
-      old_environment = @@environment
-      @@environment = environment
+      old_environment = self.environment
+      self.environment = environment
       ActiveRecord::Base.connection_handler.clear_all_connections! unless Rails.env.test?
       yield
     ensure
-      @@environment = old_environment
+      self.environment = old_environment
       ActiveRecord::Base.connection_handler.clear_all_connections! unless Rails.env.test?
     end
   end
 
   # for use from script/console ONLY
   def self.switch_user!(user)
-    @@explicit_user = user
+    self.explicit_user = user
     ActiveRecord::Base.connection_handler.clear_all_connections!
   end
   def self.switch_environment!(environment)
-    @@environment = environment
+    self.environment = environment
     ActiveRecord::Base.connection_handler.clear_all_connections!
   end
 end
