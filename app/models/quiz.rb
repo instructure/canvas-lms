06f47cac2369a727fbc228e85633d60df674a2d5
Simon Williams <simon@instructure.com>
2012-05-21 10:34:52 -0600

    handle sorting quiz questions with nil position; fixes #8639
    when retrieving all the quiz questions for a quiz edit page in the correct
    order, we need to be robust to the possibility of the quiz questions position
    attribute being nil.
    
    test plan:
    - in a quiz in a state where you have a quiz question with a nil position
      inside of a question group
    - make sure the edit quiz page loads
    
    Change-Id: I79abb4bdb397cfb1b329a7ae95b7046f03d667f0
    Reviewed-on: https://gerrit.instructure.com/10907
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -328,7 +328,7 @@ class Quiz < ActiveRecord::Base
           data[:assessment_question_bank_id] = e.assessment_question_bank_id
           data[:questions] = []
         else
-          data[:questions] = e.quiz_questions.sort_by{|q| q.position}.map{|q| q.data}
+          data[:questions] = e.quiz_questions.sort_by{|q| q.position || 99999}.map(&:data)
         end
         data[:actual_pick_count] = e.actual_pick_count
         res = data
