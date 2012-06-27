160d37d54bb4f818aae8b0a2360f1172b06a9880
Ryan Shaw <ryan@instructure.com>
2012-06-27 17:49:08 -0600

    re-enable link_data endpoint since we have specs that rely on it
    Change-Id: I7f1db2e4d4a4196a2cd1e245b05ebef62458218a

--- config/routes.rb
+++ config/routes.rb
@@ -918,7 +918,7 @@ ActionController::Routing::Routes.draw do |map|
   # commenting out all collection urls until collections are live
   # map.resources :collection_items, :only => [:new]
   # map.get_bookmarklet 'get_bookmarklet', :controller => 'collection_items', :action => 'get_bookmarklet'
-  # map.collection_item_link_data 'collection_items/link_data', :controller => 'collection_items', :action => 'link_data', :conditions => { :method => :post }
+  map.collection_item_link_data 'collection_items/link_data', :controller => 'collection_items', :action => 'link_data', :conditions => { :method => :post }
   # 
   # map.resources :collections, :only => [:show, :index] do |collection|
   #   collection.resources :collection_items, :only => [:show, :index]
