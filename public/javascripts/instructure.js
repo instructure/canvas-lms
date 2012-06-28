5353a2bc8e588f5b400040799f39d395fc17c5b3
Ryan Shaw <ryan@instructure.com>
2012-06-28 13:04:49 -0600

    only set focus on link if using keyboard nav, fixes: #9211
    Change-Id: Id1bbde5d933ad0d7d6e994f4ed89681d59823b56
    Reviewed-on: https://gerrit.instructure.com/11932
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Ryan Florence <ryanf@instructure.com>

--- public/javascripts/instructure.js
+++ public/javascripts/instructure.js
@@ -982,7 +982,16 @@ define([
     // this is for things like the to-do, recent items and upcoming, it
     // happend a lot so rather than duplicating it everywhere I stuck it here
     $(".more_link").click(function(event) {
-      $(this).closest("li").slideUp().parents("ul").children(":hidden").slideDown().first().find(":tabbable:first").focus();
+      var $children = $(this).closest("li").slideUp().parents("ul").children(":hidden").slideDown();
+
+      // if they are using the keyboard to navigate (they hit enter on the link instead of actually
+      // clicking it) then put focus on the first of the now-visible items--otherwise, since the
+      // .more_link is hidden, focus would be completely lost and leave a blind person stranded.
+      // don't want to set focus if came from a mouse click because then you'd have 2 of the tooltip
+      // bubbles staying visible, see #9211
+      if (event.screenX === 0) {
+        $children.first().find(":tabbable:first").focus();
+      }
       return false;
     });
     $(".to-do-list, #topic_list").delegate('.disable_item_link', 'click', function(event) {
