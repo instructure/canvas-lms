af092ae3ddcacd8b7bbc9a79fa31e60f10ee6337
Ryan Shaw <ryan@instructure.com>
2012-07-11 13:55:44 -0600

    `require` handlebars partials that are used inside a template
    Change-Id: I3f2d0af56fa9ece4dafc33ab7cc707458477eb7e
    Reviewed-on: https://gerrit.instructure.com/12164
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jon Jensen <jon@instructure.com>

--- lib/handlebars/handlebars.rb
+++ lib/handlebars/handlebars.rb
@@ -62,11 +62,21 @@ class Handlebars
         partial_path = id.sub(filename, partial_name)
         partial_registration = "\nHandlebars.registerPartial('#{partial_path}', templates['#{id}']);\n"
       end
-      prepared = prepare_i18n(source, id)
-      template = context.call "Handlebars.precompile", prepared[:content]
+
       dependencies = ['compiled/handlebars_helpers']
+      prepared = prepare_i18n(source, id)
       dependencies << "i18n!#{normalize(id)}" if prepared[:keys].size > 0
 
+      # take care of `require`ing partials
+      partials = context.call("findPartialDeps", prepared[:content]).uniq
+      partials.each do |partial|
+        split = partial.split /\//
+        split[-1] = "_#{split[-1]}"
+        require_path = split.join '/'
+        dependencies << "jst/#{require_path}"
+      end
+
+      template = context.call "Handlebars.precompile", prepared[:content]
       <<-JS
 define('#{plugin ? plugin + "/" : ""}jst/#{id}', #{dependencies.to_json}, function (Handlebars) {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
@@ -104,7 +114,33 @@ JS
     # Compiles and caches the handlebars JavaScript
     def set_context
       handlebars_source = File.read(File.dirname(__FILE__) + '/vendor/handlebars.js')
-      @context = ExecJS.compile handlebars_source
+      find_partial_deps_fn = """
+        function findPartialDeps( source ) {
+          var nodes = Handlebars.parse(source);
+
+          function recursiveNodeSearch( statements, res ) {
+            statements.forEach(function ( statement ) {
+              if ( statement && statement.type === 'partial' ) {
+                  res.push(statement.id.string);
+              }
+              if ( statement && statement.program && statement.program.statements ) {
+                recursiveNodeSearch( statement.program.statements, res );
+              }
+              if ( statement && statement.program && statement.program.inverse && statement.program.inverse.statements ) {
+                recursiveNodeSearch( statement.program.inverse.statements, res );
+              }
+            });
+            return res;
+          }
+
+          var res   = [];
+          if ( nodes && nodes.statements ) {
+            res = recursiveNodeSearch( nodes.statements, [] );
+          }
+          return res;
+        }
+      """
+      @context = ExecJS.compile handlebars_source + find_partial_deps_fn
     end
   end
 end
