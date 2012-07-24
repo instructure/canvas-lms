73f238d418dfc983efd3d6c639a8cce553c6ede8
Jeremy Stanley <jeremy@instructure.com>
2012-07-24 11:39:21 -0600

    fix performance of file notification query; closes #9012
    the index condition and the query condition didn't quite line up,
    forcing it to do table scans
    
    test plan:
     * notifications should work
     * delayed job query that finds files to notify for should not
       do a full table scan
    
    Change-Id: I86930ef6b4ec450bdd325bb96651fc0f88967836
    Reviewed-on: https://gerrit.instructure.com/12451
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

--- db/migrate/20120717140515_add_need_notify_column_to_attachments.rb
+++ db/migrate/20120717140515_add_need_notify_column_to_attachments.rb
@@ -6,14 +6,21 @@ class AddNeedNotifyColumnToAttachments < ActiveRecord::Migration
     add_column :attachments, :need_notify, :boolean
     Attachment.reset_column_information
     if connection.adapter_name =~ /\Apostgresql/i
-      execute('CREATE INDEX CONCURRENTLY "index_attachments_on_need_notify" ON attachments(need_notify) WHERE need_notify IS NOT NULL')
+      # bad index condition; postpone creating this index until FixNeedNotifyIndexCondition
+      #execute('CREATE INDEX CONCURRENTLY "index_attachments_on_need_notify" ON attachments(need_notify) WHERE need_notify IS NOT NULL')
     else
       add_index :attachments, [:need_notify], :name => "index_attachments_on_need_notify"
     end
   end
 
   def self.down
+    if connection.adapter_name =~ /\Apostgresql/i
+      # this may exist if the source gets upated between migrate and rollback
+      execute('DROP INDEX IF EXISTS index_attachments_on_need_notify')
+    else
+      remove_index :attachments, :name => "index_attachments_on_need_notify"
+    end
     remove_column :attachments, :need_notify
-    remove_index :attachments, :name => "index_attachments_on_need_notify"
   end
 end
+
