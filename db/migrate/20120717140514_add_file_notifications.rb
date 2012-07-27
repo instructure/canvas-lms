3a995ab852f174fa86643f1531b76978bc0704c9
Jeremy Stanley <jeremy@instructure.com>
2012-07-27 09:33:33 -0600

    fix duplicate Notifications (and add unique constraint)
    also avoid unnecessary row updates when processing notifications
    
    Change-Id: Ie25b83b72cdf56fa75b6bb415b27f1720ab1bdc1
    Reviewed-on: https://gerrit.instructure.com/12540
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- db/migrate/20120717140514_add_file_notifications.rb
+++ db/migrate/20120717140514_add_file_notifications.rb
@@ -2,11 +2,14 @@ class AddFileNotifications < ActiveRecord::Migration
   tag :predeploy
 
   def self.up
-    Notification.create(:name => "New File Added", :category => "Files")
-    Notification.create(:name => "New Files Added", :category => "Files")
+    # (shard check added later; dupes removed in AddUniqueIndexOnNotifications)
+    return unless Shard.current.default?
+    Notification.create!(:name => "New File Added", :category => "Files")
+    Notification.create!(:name => "New Files Added", :category => "Files")
   end
 
   def self.down
+    # (try on each shard, because there may be duplicates due to the above)
     Notification.find_by_name("New File Added").try(:destroy)
     Notification.find_by_name("New Files Added").try(:destroy)
   end
