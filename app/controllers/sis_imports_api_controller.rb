d6118d24511ce53a271d57ff91c4fce8a4e4401b
rob orton <rob@instructure.com>
2012-08-02 15:10:08 -0600

    fix sis api docs to use https fixes #9733
    Change-Id: I71cf015dbea9d8c6e6d091893e452711aeb8c847
    Reviewed-on: https://gerrit.instructure.com/12699
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>

--- app/controllers/sis_imports_api_controller.rb
+++ app/controllers/sis_imports_api_controller.rb
@@ -50,7 +50,7 @@ class SisImportsApiController < ApplicationController
   #
   #   Examples:
   #     curl -F attachment=@<filename> -H "Authorization: Bearer <token>" \ 
-  #         'http://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
+  #         'https://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
   #
   #   If you decide to do a raw post, you can skip the 'attachment' argument,
   #   but you will then be required to provide a suitable Content-Type header.
@@ -59,19 +59,19 @@ class SisImportsApiController < ApplicationController
   #   Examples:
   #     curl -H 'Content-Type: application/octet-stream' --data-binary @<filename>.zip \ 
   #         -H "Authorization: Bearer <token>" \ 
-  #         'http://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv&extension=zip'
+  #         'https://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv&extension=zip'
   #
   #     curl -H 'Content-Type: application/zip' --data-binary @<filename>.zip \ 
   #         -H "Authorization: Bearer <token>" \ 
-  #         'http://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
+  #         'https://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
   #
   #     curl -H 'Content-Type: text/csv' --data-binary @<filename>.csv \ 
   #         -H "Authorization: Bearer <token>" \ 
-  #         'http://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
+  #         'https://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv'
   #
   #     curl -H 'Content-Type: text/csv' --data-binary @<filename>.csv \ 
   #         -H "Authorization: Bearer <token>" \ 
-  #         'http://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv&batch_mode=1&batch_mode_term_id=15'
+  #         'https://<canvas>/api/v1/accounts/<account_id>/sis_imports.json?import_type=instructure_csv&batch_mode=1&batch_mode_term_id=15'
   #
   # @argument extension Recommended for raw post request style imports. This
   #   field will be used to distinguish between zip, xml, csv, and other file
