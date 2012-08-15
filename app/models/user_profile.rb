0f6eae3cf17cad5172ca3ea9198dfb932df068b4
Cameron Matheson <cameron@instructure.com>
2012-08-15 12:49:55 -0600

    include root_account in section_tabs cache key
    fixes #9879
    
    The profile nav link should only be shown when @domain_root_account has
    profiles enabled.
    
    Test plan:
      * make sure caching is enabled
      * go to to your user settings on an account with profiles enabled
        - you should see a profile link in the sidebar
      * switch to an account that doesn't have profiles
        - you should no longer see the profile tab in the sidebar
    
    Change-Id: If502829ef9d0bfa8117df82e241beb8cd61e03a9
    Reviewed-on: https://gerrit.instructure.com/12935
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Tested-by: Bryan Madsen <bryan@instructure.com>

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -36,7 +36,7 @@ class UserProfile < ActiveRecord::Base
         { :id => TAB_FILES, :label => I18n.t('#tabs.files', "Files"), :css_class => 'files', :href => :dashboard_files_path, :no_args => true },
         { :id => TAB_PROFILE_SETTINGS, :label => I18n.t('#user_profile.tabs.settings', 'Settings'), :css_class => 'profile_settings', :href => :settings_profile_path, :no_args => true },
       ]
-      if user && user.instance_variable_get(:@show_profile_tab)
+      if user && opts[:root_account] && opts[:root_account].enable_profiles?
         @tabs.insert 1, {:id => TAB_PROFILE, :label => I18n.t('#user_profile.tabs.profile', "Profile"), :css_class => 'profile', :href => :user_profile_path, :args => [user.id]}
       end
 
