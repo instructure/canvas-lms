79c6ec2aa6376a642be815249f110240cf59151c
Jeremy Stanley <jeremy@instructure.com>
2012-08-22 16:07:32 -0600

    catch all URI::Errors; fixes #10098
    URI.parse can raise other errors beside InvalidURIError; catch
    these too, so other types of bad URIs don't cause a 500 error
    when converting links for API calls.
    
    refs #10075 (that one was resolved by fixing the link in the DB)
    
    test plan:
     - reply to a discussion topic
     - click the link button
     - enter "mailto:something@example,com"
     - save the comment; there shouldn't be an error
     - reload the discussion page; the reply should appear
    
    Change-Id: Ia7fd35387625a1fb885bf705cfd17c680924c219
    Reviewed-on: https://gerrit.instructure.com/13126
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- lib/api.rb
+++ lib/api.rb
@@ -280,7 +280,7 @@ module Api
                 element[att] = val
               end
             end
-          rescue URI::InvalidURIError => e
+          rescue URI::Error => e
             # leave it as is
           end
         end
