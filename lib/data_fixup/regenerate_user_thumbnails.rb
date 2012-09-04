49a1859aa5c5797d4e1c331c8b9e399bb09d17b9
Cameron Matheson <cameron@instructure.com>
2012-09-04 10:39:30 -0600

    thumbnails migration fix: skip bad images
    Test plan:
      * change the filename of one of your avatar attachments to something
        made up
      * run the migration; make sure nothing fails
    
    Change-Id: I767658a2c533d84ce40daac3a7d44602ebe4f37d
    Reviewed-on: https://gerrit.instructure.com/13430
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- lib/data_fixup/regenerate_user_thumbnails.rb
+++ lib/data_fixup/regenerate_user_thumbnails.rb
@@ -3,10 +3,13 @@ module DataFixup::RegenerateUserThumbnails
     User.find_in_batches(:include => :active_images) do |users|
       attachments = users.map(&:active_images).flatten
       attachments.each do |a|
-        a.thumbnails.destroy_all
-        tmp_file = a.create_temp_file
-        a.create_or_update_thumbnail(tmp_file, :thumb, '128x128')
-        a.save
+        begin
+          a.thumbnails.destroy_all
+          tmp_file = a.create_temp_file
+          a.create_or_update_thumbnail(tmp_file, :thumb, '128x128')
+          a.save
+        rescue
+        end
       end
     end
   end
