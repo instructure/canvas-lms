0e78d58b31d7ad10ba0996f3fe2b3252eaf4b48a
Jeremy Stanley <jeremy@instructure.com>
2012-09-21 08:52:34 -0600

    make enrollment associated_user_id fixup nicer
    check that a row is not null before resetting it to null, to avoid
    a whole bunch of unnecessary updates.
    
    you might think the database would do this on its own,
    but you'd be wrong.
    
    Change-Id: I53635f00af783a6c8843e5a0466bf464a70e5e11
    Reviewed-on: https://gerrit.instructure.com/13870
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- lib/data_fixup/remove_bogus_enrollment_associated_user_ids.rb
+++ lib/data_fixup/remove_bogus_enrollment_associated_user_ids.rb
@@ -2,7 +2,7 @@ module DataFixup::RemoveBogusEnrollmentAssociatedUserIds
   def self.run
     Enrollment.find_ids_in_ranges do |first, last|
       Enrollment.update_all({:associated_user_id => nil},
-        ["type <> 'ObserverEnrollment' AND id >= ? AND id <= ?",
+        ["associated_user_id IS NOT NULL AND type <> 'ObserverEnrollment' AND id >= ? AND id <= ?",
          first, last])
     end
   end
