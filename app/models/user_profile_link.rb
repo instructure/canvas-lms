63130be90afd8b16bb26456c075aeec3198796e2
Jake Sorce <jake@instructure.com>
2012-09-24 14:56:03 -0600

    add data validation to user profile links
    fixes #10876
    
    test plan:
      1. login as a admin and go to the default account
      2. click 'Settings on the left hand nav
      3. scroll down and click 'Enable Profiles' checkbox
      4. scroll up to the top and click on the user name link
      5. click 'Edit Profile'
      6. try to add a new link with a title that is longer than 255 characters
      7. try to add a url that is longer than 255 characters
      8. the title should not let you go over 255 characters and the url
         should be able to go over 255 characters without a DB error
    
    Change-Id: Icc73941e37365df3a64176b93a31cf9c768ffc68
    Reviewed-on: https://gerrit.instructure.com/13910
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- app/models/user_profile_link.rb
+++ app/models/user_profile_link.rb
@@ -19,4 +19,5 @@ class UserProfileLink < ActiveRecord::Base
   attr_accessible :url, :title
 
   belongs_to :user_profile
+  validates_length_of :title, :maximum => maximum_string_length, :allow_nil => true, :allow_blank => true
 end
