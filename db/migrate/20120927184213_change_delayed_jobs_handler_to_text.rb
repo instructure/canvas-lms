7a04fae2314f68a26659de871fb5181213d0cd50
Cody Cutrer <cody@instructure.com>
2012-09-27 12:45:02 -0600

    change delayed_jobs.handler to text
    test plan:
     * make sure delayed jobs still run
    
    Change-Id: Idf2ed293d80124176161b7de801c9b0ae1d80465
    Reviewed-on: https://gerrit.instructure.com/13994
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

---
+++ db/migrate/20120927184213_change_delayed_jobs_handler_to_text.rb
@@ -0,0 +1,15 @@
+class ChangeDelayedJobsHandlerToText < ActiveRecord::Migration
+  tag :predeploy
+
+  def self.connection
+    Delayed::Job.connection
+  end
+
+  def self.up
+    change_column :delayed_jobs, :handler, :text
+  end
+
+  def self.down
+    change_column :delayed_jobs, :handler, :string, :limit => 500.kilobytes
+  end
+end
