af59ab2c3a38dc5e9000c37c42f22c5a0855a573
Jacob Fugal <jacob@instructure.com>
2012-10-10 15:19:56 -0600

    secure facebook integration hole
    Change-Id: I3ee51aefbcaff577d2fc78014bbe5623e29bafc8
    test-plan:
     - requires production (yay), unless you can get your dev environment
       registered with facebook
     - create a user Adam that has facebook integration enabled but not yet
       configured (such that they have a UserService with
       service='facebook', but don't yet have a service_user_id)
     - go to https://apps.facebook.com/instructure while logged into
       facebook with a user that has not integrated canvas with facebook
     - you should not see a message welcoming Adam and encouraging him to
       setup facebook notifications
    Reviewed-on: https://gerrit.instructure.com/14306
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>

---
+++ spec/controllers/facebook_controller_spec.rb
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2012 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe FacebookController do
+  before :all do
+    Canvas::Plugin.register(:facebook, nil, :settings => {:app_id => 1, :secret => 'sekrit'})
+  end
+
+  describe "get_facebook_user" do
+    def signed_request(data={})
+      str = [data.to_json].pack('m').chomp.tr('+/', '-_')
+      sig = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'), Facebook.config['secret'], str)).strip.tr('+/', '-_').sub(/=+$/, '')
+      signed_request = [sig, str].join('.')
+    end
+
+    it "should find a user with a user_id" do
+      @user = user_model
+      UserService.create!(:user => @user, :service => 'facebook', :service_user_id => 'some_facebook_user_id')
+      get 'index', :signed_request => signed_request(:user_id => 'some_facebook_user_id')
+      assigns[:user].should == @user
+    end
+
+    it "should not find a user without a user_id" do
+      @user = user_model
+      UserService.create!(:user => @user, :service => 'facebook', :service_user_id => nil)
+      get 'index', :signed_request => signed_request
+      assigns[:user].should be_nil
+    end
+  end
+end
