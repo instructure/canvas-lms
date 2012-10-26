cb6661ea4b69b5b6a1ca1353205a240523a299d5
Sterling Cobb <sterling@instructure.com>
2012-10-26 14:32:31 -0600

    Clear shared secret when in settings dialog
    fixes #11321
    
    The shared secret field was not behaving consistantly
    so it will now always be cleared when saving, updating
    or clicking cancel in the dialog box.
    
    Test Plan:
      1. Create a course
      2. Go to the settings page
      3. Go to the external tools tab
      4. Click 'Add External Tool'
      5. Enter in bogus data it it's fields but be sure to
         add a shared secret.
      6. Click "Save tool settings" then edit the saved External Tool
         The Shared Secret should not appear in the input field.
      7. Repeat steps 1-5, it should remain clear.
      8. Try any other scenario you can think of and it should still
         be clear!
    
    Change-Id: Ibd7b4353c2989d9ce07a14f7a162dd0ce55472a9
    Reviewed-on: https://gerrit.instructure.com/14779
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>

--- public/javascripts/external_tools.js
+++ public/javascripts/external_tools.js
@@ -9,8 +9,10 @@ define([
 
 $(document).ready(function() {
   var $dialog = $("#external_tools_dialog");
+
   $(".add_tool_link").click(function(event) {
     event.preventDefault();
+
     var formData = {
       domain: "",
       url: "",
@@ -23,74 +25,114 @@ $(document).ready(function() {
       consumer_key: "",
       shared_secret: ""
     }
+
     $dialog.dialog({
       title: I18n.t('titles.edit_external_tool', "Edit External Tool"),
-      width: 600,
-      height: 420
+      width: 600
     });
+
     $dialog.find(".shared_secret_note").hide();
     $dialog.find("form")
       .attr('method', 'POST')
       .attr('action', $dialog.find(".external_tools_url").attr('href'));
     $dialog.fillFormData(formData, {object_name: 'external_tool'});
     $dialog.find(".config_type_option").show();
+
     $("#external_tool_match_by").val('domain').change();
     $("#external_tool_config_type").val('manual').change();
   });
+
+  /* Form Submit Summary */
   $dialog.find("form").formSubmit({
     beforeSubmit: function(data) {
-      $(this).find("button").attr('disabled', true).filter('.save_button').text(I18n.t('messages.saving_tool_settings', "Saving Tool Settings..."));
+      $(this).find("button")
+           .attr('disabled', true)
+           .filter('.save_button')
+           .text(I18n.t('messages.saving_tool_settings', "Saving Tool Settings..."));
     },
     success: function(tool) {
-      $(this).find("button").attr('disabled', false).filter('.save_button').text(I18n.t('buttons.save_tool_settings', "Save Tool Settings"));
-      $dialog.dialog('close');
+      var $this = $(this);
       var $tool = $("#external_tool_" + tool.id);
-      if($tool.length == 0) {
-        $tool = $("#external_tool_blank").clone(true).removeAttr('id');
+
+      $this.find("button")
+             .attr('disabled', false)
+             .filter('.save_button')
+             .text(I18n.t('buttons.save_tool_settings', "Save Tool Settings"));
+
+      $dialog.dialog('close');
+
+      var tool_div_doesnt_exist = $tool.length == 0;
+      if(tool_div_doesnt_exist) {
+        $tool = $("#external_tool_blank").clone(true)
+                                         .removeAttr('id');
         $("#external_tools").append($tool);
       }
+
       $tool.fillTemplateData({
         data: tool,
         dataValues: ['id', 'workflow_state'],
         hrefValues: ['id'],
         id: 'external_tool_' + tool.id
       });
+
+      /* Clear the shared seceret input field because it never gets updated and 
+       * should no longer be visible after you update * 
+       */
+      $('#external_tool_shared_secret').val('');
+
       $tool
         .toggleClass('has_editor_button', tool.has_editor_button)
         .toggleClass('has_resource_selection', tool.has_resource_selection)
         .toggleClass('has_course_navigation', tool.has_course_navigation)
         .toggleClass('has_user_navigation', tool.has_user_navigation)
         .toggleClass('has_account_navigation', tool.has_account_navigation);
-      $tool.find(".tool_url").showIf(tool.url).end()
-        .find(".tool_domain").showIf(tool.domain);
+
+      $tool.find(".tool_url")
+           .showIf(tool.url).end()
+           .find(".tool_domain")
+           .showIf(tool.domain);
+
       $tool.show();
     },
     error: function(data) {
-      $(this).find("button").attr('disabled', false).filter('.save_button').text(I18n.t('errors.save_tool_settings_failed', "Save Tool Settings Failed"));
+      $(this).find("button")
+             .attr('disabled', false)
+             .filter('.save_button')
+             .text(I18n.t('errors.save_tool_settings_failed', "Save Tool Settings Failed"));
     }
   });
+
   $dialog.find(".cancel_button").click(function() {
     $dialog.dialog('close');
+    $('#external_tool_shared_secret').val('');
   });
+
   $("#external_tools").delegate('.edit_tool_link', 'click', function(event) {
     event.preventDefault();
+
     var $tool = $(this).parents(".external_tool");
     var data = $tool.getTemplateData({textValues: ['name', 'description', 'domain', 'url', 'consumer_key', 'custom_fields_string'], dataValues: ['id', 'workflow_state']});
     
     data.privacy_level = data.workflow_state;
+
     $("#external_tool_match_by").val(data.url ? 'url' : 'domain').change();
+
     $dialog.find(".shared_secret_note").show();
+
     $dialog.find("form")
       .attr('method', 'PUT')
       .attr('action', $tool.find(".update_tool_url").attr('rel'));
+
     $dialog.fillFormData(data, {object_name: 'external_tool'});
+
     $dialog.dialog({
       title: I18n.t('titles.edit_external_tool', "Edit External Tool"),
-      width: 600,
-      height: 420
+      width: 600
     });
+
     $dialog.find(".config_type_option").hide();
     $("#external_tool_config_type").val('manual').change();
+
   }).delegate('.delete_tool_link', 'click', function(event) {
     event.preventDefault();
     var $tool = $(this).parents(".external_tool");
@@ -105,6 +147,7 @@ $(document).ready(function() {
       }
     });
   });
+
   $("#external_tool_match_by").change(function() {
     if($(this).val() == 'url') {
       $(this).parents("form").find(".tool_domain").hide().find(":text").val("").end().end()
@@ -114,9 +157,11 @@ $(document).ready(function() {
         .find(".tool_domain").show();
     }
   });
+
   $("#external_tool_config_type").change(function(event) {
     $("#external_tool_form .config_type").hide();
     $("#external_tool_form .config_type." + $(this).val()).show();
   });
+
 });
 });
