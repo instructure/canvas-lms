67954972c6144ed442f369a856e2f9038889ade2
Stanley Stuart <stanley@instructure.com>
2012-11-28 11:37:54 -0700

    speed up views/quizzes/_display_question.html.erb rendering
    Fixes #11973
    
    Previously, QuizzesHelper.answer_type returned an OpenObject, which when
    used in quizzes/_display_question.html.erb and subsequent partials,
    caused a slowdown.
    
    Test Plan:
      - Check out master and start a server.
      - If you don't have a quiz, create one.
      - Try to edit the quiz by using the "Edit this Quiz" button.
      - Watch the render times for display_question.html.erb and record the
        render times.
        - script/server | grep 'display_question.html.erb' is useful.
      - Repeat by refreshing the page a few times so you get a good sample
        size ( 5 or 6 times is good enough )
      - Check out this commit
      - Watch the render times again like you did previously for master
      - Render times on this commit should be faster than master.
      - You should be able to edit the quiz as usual.
    
    Change-Id: I2ab080c80ab233e7b5fe6773f9f2c033e4dfcbad
    Reviewed-on: https://gerrit.instructure.com/15663
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Amber Taniuchi <amber@instructure.com>

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -25,83 +25,113 @@ module QuizzesHelper
     end
   end
 
+  QuestionType = Struct.new(:question_type,
+                          :entry_type,
+                          :display_answers,
+                          :answer_type,
+                          :multiple_sets,
+                          :unsupported
+                         )
+
   def answer_type(question)
-    return OpenObject.new unless question
+    return QuestionType.new unless question
     @answer_types_lookup ||= {
-      "multiple_choice_question" => OpenObject.new({
-        :question_type => "multiple_choice_question",
-        :entry_type => "radio",
-        :display_answers => "multiple",
-        :answer_type => "select_answer"
-      }),
-      "true_false_question" => OpenObject.new({
-        :question_type => "true_false_question",
-        :entry_type => "radio",
-        :display_answers => "multiple",
-        :answer_type => "select_answer"
-      }),
-      "short_answer_question" => OpenObject.new({
-        :question_type => "short_answer_question",
-        :entry_type => "text_box",
-        :display_answers => "single",
-        :answer_type => "select_answer"
-      }),
-      "essay_question" => OpenObject.new({
-        :question_type => "essay_question",
-        :entry_type => "textarea",
-        :display_answers => "single",
-        :answer_type => "text_answer"
-      }),
-      "matching_question" => OpenObject.new({
-        :question_type => "matching_question",
-        :entry_type => "matching",
-        :display_answers => "multiple",
-        :answer_type => "matching_answer"
-      }),
-      "missing_word_question" => OpenObject.new({
-        :question_type => "missing_word_question",
-        :entry_type => "select",
-        :display_answers => "multiple",
-        :answer_type => "select_answer"
-      }),
-      "numerical_question" => OpenObject.new({
-        :question_type => "numerical_question",
-        :entry_type => "numerical_text_box",
-        :display_answers => "single",
-        :answer_type => "numerical_answer"
-      }),
-      "calculated_question" => OpenObject.new({
-        :question_type => "calculated_question",
-        :entry_type => "numerical_text_box",
-        :display_answers => "single",
-        :answer_type => "numerical_answer"
-      }),
-      "multiple_answers_question" => OpenObject.new({
-        :question_type => "multiple_answers_question",
-        :entry_type => "checkbox",
-        :display_answers => "multiple",
-        :answer_type => "select_answer"
-      }),
-      "fill_in_multiple_blanks_question" => OpenObject.new({
-        :question_type => "fill_in_multiple_blanks_question",
-        :entry_type => "text_box",
-        :display_answers => "multiple",
-        :answer_type => "select_answer",
-        :multiple_sets => true
-      }),
-      "multiple_dropdowns_question" => OpenObject.new({
-        :question_type => "multiple_dropdowns_question",
-        :entry_type => "select",
-        :display_answers => "none",
-        :answer_type => "select_answer",
-        :multiple_sets => true
-      }),
-      "other" =>  OpenObject.new({
-        :question_type => "text_only_question",
-        :entry_type => "none",
-        :display_answers => "none",
-        :answer_type => "none"
-      })
+      "multiple_choice_question" => QuestionType.new(
+        "multiple_choice_question",
+        "radio",
+        "multiple",
+        "select_answer",
+        false,
+        false
+      ),
+      "true_false_question" => QuestionType.new(
+        "true_false_question",
+        "radio",
+        "multiple",
+        "select_answer",
+        false,
+        false
+      ),
+      "short_answer_question" => QuestionType.new(
+        "short_answer_question",
+        "text_box",
+        "single",
+        "select_answer",
+        false,
+        false
+      ),
+      "essay_question" => QuestionType.new(
+        "essay_question",
+        "textarea",
+        "single",
+        "text_answer",
+        false,
+        false
+      ),
+      "matching_question" => QuestionType.new(
+        "matching_question",
+        "matching",
+        "multiple",
+        "matching_answer",
+        false,
+        false
+      ),
+      "missing_word_question" => QuestionType.new(
+        "missing_word_question",
+        "select",
+        "multiple",
+        "select_answer",
+        false,
+        false
+      ),
+      "numerical_question" => QuestionType.new(
+        "numerical_question",
+        "numerical_text_box",
+        "single",
+        "numerical_answer",
+        false,
+        false
+      ),
+      "calculated_question" => QuestionType.new(
+        "calculated_question",
+        "numerical_text_box",
+        "single",
+        "numerical_answer",
+        false,
+        false
+      ),
+      "multiple_answers_question" => QuestionType.new(
+        "multiple_answers_question",
+        "checkbox",
+        "multiple",
+        "select_answer",
+        false,
+        false
+      ),
+      "fill_in_multiple_blanks_question" => QuestionType.new(
+        "fill_in_multiple_blanks_question",
+        "text_box",
+        "multiple",
+        "select_answer",
+        true,
+        false
+      ),
+      "multiple_dropdowns_question" => QuestionType.new(
+        "multiple_dropdowns_question",
+        "select",
+        "none",
+        "select_answer",
+        true,
+        false
+      ),
+      "other" =>  QuestionType.new(
+        "text_only_question",
+        "none",
+        "none",
+        "none",
+        false,
+        false
+      )
     }
     res = @answer_types_lookup[question[:question_type]] || @answer_types_lookup["other"]
     if res.question_type == "text_only_question"
