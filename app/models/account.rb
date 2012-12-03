1bc1cfe258115a0143dfbbee5dd154db911350ac
Cody Cutrer <cody@instructure.com>
2012-12-03 14:47:34 -0700

    use the correct permission for developer keys tab fixes #CNVS-1332
    test plan:
     * create a role in site admin without access to developer keys
     * add a user to that role
     * login as that user, and go to site admin
     * the Developer Keys tab should not be visible
     * going to /developer_keys should give you access denied
    
    Change-Id: I7ce3cbab13939067fb6d7e11c38dab3d48918442
    Reviewed-on: https://gerrit.instructure.com/15775
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -930,7 +930,7 @@ class Account < ActiveRecord::Base
       tabs << { :id => TAB_AUTHENTICATION, :label => t('#account.tab_authentication', "Authentication"), :css_class => 'authentication', :href => :account_account_authorization_configs_path } if manage_settings
       tabs << { :id => TAB_PLUGINS, :label => t("#account.tab_plugins", "Plugins"), :css_class => "plugins", :href => :plugins_path, :no_args => true } if self.grants_right?(user, nil, :manage_site_settings)
       tabs << { :id => TAB_JOBS, :label => t("#account.tab_jobs", "Jobs"), :css_class => "jobs", :href => :jobs_path, :no_args => true } if self.grants_right?(user, nil, :manage_jobs)
-      tabs << { :id => TAB_DEVELOPER_KEYS, :label => t("#account.tab_developer_keys", "Developer Keys"), :css_class => "developer_keys", :href => :developer_keys_path, :no_args => true } if self.grants_right?(user, nil, :manage_site_settings)
+      tabs << { :id => TAB_DEVELOPER_KEYS, :label => t("#account.tab_developer_keys", "Developer Keys"), :css_class => "developer_keys", :href => :developer_keys_path, :no_args => true } if self.grants_right?(user, nil, :manage_developer_keys)
     else
       tabs = []
       tabs << { :id => TAB_COURSES, :label => t('#account.tab_courses', "Courses"), :css_class => 'courses', :href => :account_path } if user && self.grants_right?(user, nil, :read_course_list)
