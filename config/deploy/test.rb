af7ae58a470a1d2b7b3bd5e9e3c3adc6d296b23c
Graham Ballantyne <grahamb@sfu.ca>
2012-12-07 14:40:46 -0800

    add test servers to test.rb

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,4 +1,4 @@
-server 'canvas-mt.tier2.sfu.ca', :app, :web, :primary => true
-set :rails_env, "development"
-set :deploy_env, "development"
-set :bundle_without, [:production, :test]
+role :app, "canvas-at1.tier2.sfu.ca", "canvas-at2.tier2.sfu.ca", "canvas-at3.tier2.sfu.ca"
+role :db, "canvas-mt.tier2.sfu.ca", :primary => true
+
+set :rails_env, "production"
