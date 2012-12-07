6bbf7b92fe2e624fb4944b4f9d2ec884e10551cd
Graham Ballantyne <hello@grahamballantyne.com>
2012-12-06 16:06:06 -0800

    Update config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require "bundler/capistrano"
 # require "capistrano/ext/multistage"
 
 set :application,   "canvas"
-set :repository,    "git@github.com:grahamb/canvas-lms.git"
+set :repository,    "git://github.com/grahamb/canvas-lms.git"
 set :scm,           :git
 set :user,          "canvasuser"
 set :branch,        "dev"
