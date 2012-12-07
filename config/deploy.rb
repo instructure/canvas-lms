f7fe5a8d74e8352666c27d23eca746ecfa6e7a4a
Graham Ballantyne <grahamb@sfu.ca>
2012-12-07 14:40:59 -0800

    add deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,9 +15,50 @@ set :deploy_env,    "production"
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa_canvas")]
 
-role :app, "canvas-at1.tier2.sfu.ca", "canvas-at2.tier2.sfu.ca", "canvas-at3.tier2.sfu.ca"
-role :db,  "canvas-mt.tier2.sfu.ca"
-
-task :wtf, :roles => [:db] do
-  puts "#{latest_release}"
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart do; end
 end
+
+namespace :canvas do
+
+    desc "Create symlink for files folder to mount point"
+    task :symlink_canvasdata do
+        target = "mnt/data"
+        run "mkdir -p #{latest_release}/mnt/data && ln -s /mnt/data/canvasfiles #{latest_release}/#{target}/canvasfiles"
+    end
+
+    desc "Compile static assets"
+    task :compile_assets, :on_error => :continue do
+      # On remote: bundle exec rake canvas:compile_assets
+      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} canvas:compile_assets"
+      run "cd #{latest_release} && chown -R canvasuser:canvasuser ."
+    end
+
+    desc "Load new notification types"
+    task :load_notifications, :roles => :db, :only => { :primary => true } do
+      # On remote: RAILS_ENV=production bundle exec rake db:load_notifications
+      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} db:load_notifications --quiet"
+    end
+
+    desc "Restart delayed jobs workers"
+    task :restart_jobs, :on_error => :continue do
+      run "/etc/init.d/canvas_init restart"
+    end
+
+    desc "Post-update commands"
+    task :update_remote do
+      # deploy.migrate
+      # load_notifications
+      restart_jobs
+      puts "\x1b[42m\x1b[1;37m Deploy complete!  \x1b[0m"
+    end
+
+end
+
+after(:deploy, "deploy:cleanup")
+before("deploy:restart", "canvas:files_symlink")
+before("deploy:restart", "canvas:compile_assets")
+
+
