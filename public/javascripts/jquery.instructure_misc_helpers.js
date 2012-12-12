b4b38293c6ddf6157cf12c5a2a9fd5dae8cbc61a
Ryan Shaw <ryan@instructure.com>
2012-12-12 14:43:38 -0700

    factor mimeClass out into it's own module
    has no visible impact, just code organization
    
    Change-Id: I0c7546c5fedb785c23c44c4f5b64cce6c0b3071a
    Reviewed-on: https://gerrit.instructure.com/16034
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Stanley Stuart <stanley+gerrit@instructure.com>
    QA-Review: Clare Hetherington <clare@instructure.com>

--- public/javascripts/jquery.instructure_misc_helpers.js
+++ public/javascripts/jquery.instructure_misc_helpers.js
@@ -41,57 +41,6 @@ define([
     return result;
   };
 
-  $.mimeClass = function(contentType){
-    return {
-      "video/mp4": "video",
-      "application/x-rar-compressed": "zip",
-      "application/vnd.oasis.opendocument.spreadsheet": "xls",
-      "application/x-docx": "doc",
-      "application/x-shockwave-flash": "flash",
-      "audio/x-mpegurl": "audio",
-      "image/png": "image",
-      "text/xml": "code",
-      "video/x-ms-asf": "video",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xls",
-      "text/html": "html",
-      "video/x-msvideo": "video",
-      "audio/x-pn-realaudio": "audio",
-      "application/x-zip-compressed": "zip",
-      "text/css": "code",
-      "video/x-sgi-movie": "video",
-      "audio/x-aiff": "audio",
-      "application/zip": "zip",
-      "application/xml": "code",
-      "application/x-zip": "zip",
-      "text/rtf": "doc",
-      "text": "text",
-      "video/mpeg": "video",
-      "video/quicktime": "video",
-      "audio/3gpp": "audio",
-      "audio/mid": "audio",
-      "application/x-rar": "zip",
-      "image/x-psd": "image",
-      "application/vnd.ms-excel": "xls",
-      "application/msword": "doc",
-      "video/x-la-asf": "video",
-      "image/gif": "image",
-      "application/rtf": "doc",
-      "video/3gpp": "video",
-      "image/pjpeg": "image",
-      "image/jpeg": "image",
-      "application/vnd.oasis.opendocument.text": "doc",
-      "audio/x-wav": "audio",
-      "audio/basic": "audio",
-      "audio/mpeg": "audio",
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation": "ppt",
-      "application/vnd.ms-powerpoint": "ppt",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "doc",
-      "application/pdf": "pdf",
-      "text/plain": "text",
-      "text/x-csharp": "code"
-    }[contentType] || 'file'
-  }
-
   $.encodeToHex = function(str) {
     var hex = "";
     var e = str.length;
