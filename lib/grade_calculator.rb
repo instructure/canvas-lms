34878c564e0d583baaf109725efcce9bfb1338bf
Cameron Matheson <cameron@instructure.com>
2012-12-21 13:31:53 -0700

    fix ruby calculation of grades (grade-dropping)
    fixes #CNVS-1546
    
    Test plan:
      * set up some assignments in an assignment group with grade-dropping
        rules configured
      * use the courses api to check the current and final scores in the
        course, and compare those to the values in the gradebook
        - note that computed_final_score is equivalent to the score in
          gradebook when 'Treat ungraded as 0s' is enabled.
    
    Change-Id: Icdd67d18ab0f00d9b333d523752ebe1f489a9917
    Reviewed-on: https://gerrit.instructure.com/16141
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Amber Taniuchi <amber@instructure.com>

--- lib/grade_calculator.rb
+++ lib/grade_calculator.rb
@@ -51,7 +51,7 @@ class GradeCalculator
     end
   end
 
-  :private
+  private
   
   # The score ignoring unsubmitted assignments
   def calculate_current_score(user_id, submissions)
@@ -67,91 +67,143 @@ class GradeCalculator
     @final_updates << "WHEN user_id=#{user_id} THEN #{score || "NULL"}"
   end
  
-  # Creates a hash for each assignment group with stats and the end score for each group
+  # returns information about assignments groups in the form:
+  # {1=>
+  #   {:tally=>0.818181818181818,
+  #    :submission_count=>2,
+  #    :total_points=>110,
+  #    :user_points=>90,
+  #    :name=>"Assignments",
+  #    :weighted_tally=>0,
+  #    :group_weight=>0},
+  #  5=> {...}}
+  # each group
   def create_group_sums(submissions, ignore_ungraded=true)
+    assignments_by_group_id = @assignments.group_by(&:assignment_group_id)
+    submissions_by_assignment_id = Hash[
+      submissions.map { |s| [s.assignment_id, s] }
+    ]
+
     group_sums = {}
     @groups.each do |group|
-      group_assignments = @assignments.select { |a| a.assignment_group_id == group.id }
-      assignment_submissions = []
-      sums = {:name => group.name,
-              :total_points => 0, 
-              :user_points => 0, 
-              :group_weight => group.group_weight || 0, 
-              :submission_count => 0}
+      assignments = assignments_by_group_id[group.id] || []
       
-      # collect submissions for this user for all the assignments
-      # if an assignment is muted it will be treated as if there is no submission
-      group_assignments.each do |assignment|
-        submission = submissions.detect { |s| s.assignment_id == assignment.id }
-        submission = nil if assignment.muted
-        submission ||= OpenStruct.new(:assignment_id=>assignment.id, :score=>0) unless ignore_ungraded
-        assignment_submissions << {:assignment => assignment, :submission => submission}
+      group_submissions = assignments.map do |a|
+        s = submissions_by_assignment_id[a.id]
+
+        # ignore submissions for muted assignments
+        s = nil if a.muted?
+
+        {
+          :assignment => a,
+          :submission => s,
+          :score => s && s.score,
+          :total => a.points_possible
+        }
       end
-      
-      # Sort the submissions that have a grade by score (to easily drop lowest/highest grades)
-      if ignore_ungraded
-        sorted_assignment_submissions = assignment_submissions.select { |hash| hash[:submission] && hash[:submission].score }
-      else
-        sorted_assignment_submissions = assignment_submissions.select { |hash| hash[:submission] }
-      end
-      sorted_assignment_submissions = sorted_assignment_submissions.sort_by do |hash|
-        val = (((hash[:submission].score || 0)/ hash[:assignment].points_possible) rescue 999999)
-        val.to_f.finite? ? val : 999999
-      end
-      
-      # Mark the assignments that aren't allowed to be dropped
-      if group.rules_hash[:never_drop]
-        sorted_assignment_submissions.each do |hash|
-          never_drop = group.rules_hash[:never_drop].include?(hash[:assignment].id)
-          hash[:never_drop] = true if never_drop
-        end
-      end
-      
-      # Flag the the lowest assignments to be dropped
-      low_drop_count = 0
-      high_drop_count = 0
-      total_scored = sorted_assignment_submissions.length
-      if group.rules_hash[:drop_lowest]
-        drop_total = group.rules_hash[:drop_lowest] || 0
-        sorted_assignment_submissions.each do |hash|
-          if !hash[:drop] && !hash[:never_drop] && low_drop_count < drop_total && (low_drop_count + high_drop_count + 1) < total_scored
-            low_drop_count += 1
-            hash[:drop] = true
-          end
-        end
-      end
-      
-      # Flag the highest assignments to be dropped
-      if group.rules_hash[:drop_highest]
-        drop_total = group.rules_hash[:drop_highest] || 0
-        sorted_assignment_submissions.reverse.each do |hash|
-          if !hash[:drop] && !hash[:never_drop] && high_drop_count < drop_total && (low_drop_count + high_drop_count + 1) < total_scored
-            high_drop_count += 1
-            hash[:drop] = true
-          end
-        end
-      end
-      
-      # If all submissions are marked to be dropped: don't drop the highest because we need at least one
-      if !sorted_assignment_submissions.empty? && sorted_assignment_submissions.all? { |hash| hash[:drop] }
-        sorted_assignment_submissions[-1][:drop] = false
-      end
-      
-      # Count the points from all the non-dropped submissions
-      sorted_assignment_submissions.select { |hash| !hash[:drop] }.each do |hash|
-        sums[:submission_count] += 1
-        sums[:total_points] += hash[:assignment].points_possible || 0
-        sums[:user_points] += (hash[:submission] && hash[:submission].score) || 0
-      end
-      
-      # Calculate the tally for this group
-      sums[:group_weight] = group.group_weight || 0
-      sums[:tally] = sums[:user_points].to_f / sums[:total_points].to_f
-      sums[:tally] = 0.0 unless sums[:tally].finite?
-      sums[:weighted_tally] = sums[:tally] * sums[:group_weight].to_f
-      group_sums[group.id] = sums
+      group_submissions.reject! { |s| s[:score].nil? } if ignore_ungraded
+      group_submissions.reject! { |s| s[:total].to_i.zero? }
+      group_submissions.each { |s| s[:score] ||= 0 }
+
+      kept = drop_assignments(group_submissions, group.rules_hash)
+
+      score, possible = kept.inject([0, 0]) { |(s_sum,p_sum),s|
+        [s_sum + s[:score], p_sum + s[:total]]
+      }
+      grade = score.to_f / possible
+      weight = group.group_weight.to_f
+
+      group_sums[group.id] = {
+        :name             => group.name,
+        :tally            => grade,
+        :submission_count => kept.size,
+        :total_points     => possible,
+        :user_points      => score,
+        :group_weight     => weight,
+        :weighted_tally   => grade * weight,
+      }
     end
     group_sums
+  end
+
+  # see comments for dropAssignments in grade_calculator.coffee
+  def drop_assignments(submissions, rules)
+    drop_lowest    = rules[:drop_lowest] || 0
+    drop_highest   = rules[:drop_highest] || 0
+    never_drop_ids = rules[:never_drop] || []
+    return submissions if drop_lowest.zero? && drop_highest.zero?
+
+    if never_drop_ids.empty?
+      cant_drop = []
+    else
+      cant_drop, submissions = submissions.partition { |s|
+        never_drop_ids.include? s[:assignment].id
+      }
+    end
+
+    # fudge the drop rules if there aren't enough submissions
+    return cant_drop if submissions.empty?
+    drop_lowest = submissions.size - 1 if drop_lowest >= submissions.size
+    drop_highest = 0 if drop_lowest + drop_highest >= submissions.size
+
+    totals = submissions.map { |s| s[:total] }
+    max_total = totals.max
+
+    kept = keep_highest(submissions, submissions.size - drop_lowest, max_total)
+    kept = keep_lowest(kept, kept.size - drop_highest, max_total)
+    kept += cant_drop
+  end
+
+  def keep_highest(submissions, keep, max_total)
+    keep_helper(submissions, keep, max_total) { |*args| big_f_best(*args) }
+  end
+
+  def keep_lowest(submissions, keep, max_total)
+    keep_helper(submissions, keep, max_total) { |*args| big_f_worst(*args) }
+  end
+
+  def keep_helper(submissions, keep, max_total, &big_f_blk)
+    keep = 1 if keep <= 0
+    return submissions if submissions.size <= keep
+
+    grades = submissions.map { |s| s[:score].to_f / s[:total] }.sort
+    q_low  = grades.first
+    q_high = grades.last
+    q_mid  = (q_low + q_high) / 2
+
+    x, kept = big_f_blk.call(q_mid, submissions, keep)
+    threshold = 1 / (2 * keep * max_total**2)
+    until q_high - q_low < threshold
+      x < 0 ?
+        q_high = q_mid :
+        q_low  = q_mid
+      q_mid = (q_low + q_high) / 2
+      x, kept = big_f_blk.call(q_mid, submissions, keep)
+    end
+
+    kept
+  end
+
+  def big_f(q, submissions, keep, &sort_blk)
+    kept = submissions.map { |s|
+      rated_score = s[:score] - q * s[:total]
+      [rated_score, s]
+    }.sort(&sort_blk).first(keep)
+
+    q_kept = kept.reduce(0) { |sum,(rated_score,_)| sum + rated_score }
+    [q_kept, kept.map(&:last)]
+  end
+
+  # determines the best +keep+ assignments from submissions for the given q
+  # (suitable for use with drop_lowest)
+  def big_f_best(q, submissions, keep)
+    big_f(q, submissions, keep) { |(a,_),(b,_)| b <=> a }
+  end
+
+  # determines the worst +keep+ assignments from submissions for the given q
+  # (suitable for use with drop_highest)
+  def big_f_worst(q, submissions, keep)
+    big_f(q, submissions, keep) { |(a,_),(b,_)| a <=> b }
   end
   
   # Calculates the final score from the sums of all the assignment groups
