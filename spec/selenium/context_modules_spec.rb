e352748afe57bf7ecab05e963361aeee665b03fb
James Williams <jamesw@instructure.com>
2013-01-04 15:37:48 -0700

    add specs for using overridden dates on modules page for observers
    turns out the code was already working, just needed some
    specs to make sure for the case of observers
    
    test plan:
    * as an observer:
     * should see one due date, (the latest one)
      when not associated with a student
    
     * when associated with a student, should see the same
      due date that the student does
    
     * when associated with multiple students who have
      different dates applied to them, should see
      "Multiple Due Dates" indicator
    
    closes #CNVS-2012
    
    Change-Id: I358eb8433ff640bc9b7c15ab071b2b790ea2ce85
    Reviewed-on: https://gerrit.instructure.com/16557
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>
    QA-Review: Bracken Mosbacker <bracken@instructure.com>

--- spec/selenium/context_modules_spec.rb
+++ spec/selenium/context_modules_spec.rb
@@ -710,6 +710,93 @@ describe "context_modules" do
   end
 end
 
+  context "as an observer" do
+    before (:each) do
+      @course   = course(:active_all => true)
+      @student  = user(:active_all => true, :active_state => 'active')
+      @observer = user(:active_all => true, :active_state => 'active')
+
+      @student_enrollment = @course.enroll_user(@student, 'StudentEnrollment', :enrollment_state => 'active')
+
+      @assignment = @course.assignments.create!(:title => 'assignment 1', :name => 'assignment 1')
+      @due_at = 3.days.from_now
+      override_for_student(@student, @due_at)
+
+      course_module
+      @module.add_item({:id => @assignment.id, :type => 'assignment'})
+
+      user_session(@observer)
+    end
+
+    def override_for_student(student, due_at)
+      override = assignment_override_model(:assignment => @assignment)
+      override.override_due_at(due_at)
+      override.save!
+      override_student = override.assignment_override_students.build
+      override_student.user = student
+      override_student.save!
+    end
+
+    it "when not associated, and in one section, it should show the section's due date" do
+      section2 = @course.course_sections.create!
+      override = assignment_override_model(:assignment => @assignment)
+      override.set = section2
+      override.override_due_at(@due_at)
+      override.save!
+
+      @observer_enrollment = @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active', :section => section2)
+      get "/courses/#{@course.id}/modules"
+
+      wait_for_ajaximations
+      f(".due_date_display").text.should_not be_blank
+      f(".due_date_display").text.should == @due_at.strftime('%b %-d')
+    end
+
+    it "when not associated, and in multiple sections, it should show the latest due date" do
+      override = assignment_override_model(:assignment => @assignment)
+      override.set = @course.default_section
+      override.override_due_at(@due_at)
+      override.save!
+
+      section2 = @course.course_sections.create!
+      override = assignment_override_model(:assignment => @assignment)
+      override.set = section2
+      override.override_due_at(@due_at - 1.day)
+      override.save!
+
+      @observer_enrollment = @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active')
+      @observer_enrollment = @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active', :allow_multiple_enrollments => true, :section => section2)
+      get "/courses/#{@course.id}/modules"
+
+      wait_for_ajaximations
+      f(".due_date_display").text.should_not be_blank
+      f(".due_date_display").text.should == @due_at.strftime('%b %-d')
+    end
+
+    it "when associated with a student, it should show the student's overridden due date" do
+      @observer_enrollment = @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active', :associated_user_id => @student.id)
+      get "/courses/#{@course.id}/modules"
+
+      wait_for_ajaximations
+      f(".due_date_display").text.should_not be_blank
+      f(".due_date_display").text.should_not == "Multiple Due Dates"
+    end
+
+    it "should indicate multiple due dates for multiple observed students" do
+      student2 = user(:active_all => true, :active_state => 'active')
+      @course.enroll_user(student2, 'StudentEnrollment', :enrollment_state => 'active')
+      override_for_student(student2, @due_at + 1.day)
+
+      @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active', :associated_user_id => @student.id)
+      @course.enroll_user(@observer, 'ObserverEnrollment', :enrollment_state => 'active', :allow_multiple_enrollments => true, :associated_user_id => student2.id)
+
+      get "/courses/#{@course.id}/modules"
+
+      wait_for_ajaximations
+      f(".due_date_display").text.should == "Multiple Due Dates"
+    end
+  end
+
 describe "files" do
   FILE_NAME = 'some test file'
 
