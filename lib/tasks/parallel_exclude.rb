3aa55db6c25929810ec366af742c7d520b00f633
Shawn Meredith <shawn@instructure.com>
2013-01-07 14:36:12 -0700

    specs: removal of files in parallel_exclude.rb
    Change-Id: Ic2796bef0c636973ed369b61c8b07aca39c8c4fc
    Reviewed-on: https://gerrit.instructure.com/16593
    Reviewed-by: Jake Sorce <jake@instructure.com>
    Tested-by: Jake Sorce <jake@instructure.com>
    QA-Review: Jake Sorce <jake@instructure.com>

--- lib/tasks/parallel_exclude.rb
+++ lib/tasks/parallel_exclude.rb
@@ -1,37 +1,5 @@
 module ParallelExclude
-  FILES = [
-      'spec/apis/v1/submissions_api_spec.rb',
-      'vendor/plugins/wiziq/spec_canvas/aglive_com_util_spec.rb',
-      'spec/controllers/files_controller_spec.rb',
-      'vendor/plugins/multiple_root_accounts/spec_canvas/integration/quotas_spec.rb',
-      'vendor/plugins/wiziq/spec_canvas/wiziq_conference_spec.rb',
-      'vendor/plugins/multiple_root_accounts/spec_canvas/lib/shard_importer_spec.rb',
-      'vendor/plugins/respondus_soap_endpoint/spec_canvas/integration/respondus_endpoint_spec.rb',
-      'vendor/plugins/multiple_root_accounts/spec_canvas/models/attachment_spec.rb',
-      'vendor/plugins/webct_scraper/spec_canvas/lib/models/bulk_course_migration_spec.rb',
-      'vendor/plugins/instructure_misc_plugin/spec_canvas/student_access_spec.rb',
-      'spec/apis/api_spec_helper.rb',
-      'spec/apis/general_api_spec.rb',
-      'spec/apis/user_content_spec.rb',
-      'spec/apis/v1/groups_api_spec.rb',
-      'spec/apis/v1/courses_api_spec.rb',
-      'spec/apis/v1/collections_spec.rb',
-      'spec/apis/auth_spec.rb',
-      'spec/integration/files_spec.rb',
-      'spec/integration/security_spec.rb',
-      'spec/lib/acts_as_list.rb',
-      'spec/lib/content_zipper_spec.rb',
-      'spec/lib/turnitin_spec.rb',
-      'spec/models/attachment_spec.rb',
-      'spec/models/course_spec.rb',
-      'spec/models/eportfolio_entry_spec.rb',
-      'spec/models/media_object_spec.rb',
-      'spec/models/zip_file_import_spec.rb',
-      'spec/models/content_migration_spec.rb',
-      'spec/models/collections_spec.rb',
-      'spec/lib/canvas/http_spec.rb',
-      'spec/migrations/count_existing_collection_items_and_followers_spec.rb'
-  ]
+  FILES = []
 
   test_files = FileList['vendor/plugins/*/spec_canvas/**/*_spec.rb'].exclude('vendor/plugins/*/spec_canvas/selenium/*_spec.rb') + FileList['spec/**/*_spec.rb'].exclude('spec/selenium/**/*_spec.rb')
   AVAILABLE_FILES = test_files.select { |file_name| FILES.include?(file_name) }
