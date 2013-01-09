27ebae2e428220c437f42dd68c9c3a9f89618a5d
Joe Tanner <joe@instructure.com>
2013-01-09 09:38:16 -0700

    adding ActiveRecord::Base.count_by_date
    refs #CNVS-2064
    
    Change-Id: Idc122b7550e112abc57ac7ffd810005fd303e493
    Reviewed-on: https://gerrit.instructure.com/16663
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Joe Tanner <joe@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -340,6 +340,43 @@ class ActiveRecord::Base
     end
   end
 
+  def self.count_by_date(options = {})
+    column = options[:column] || "created_at"
+    max_date = (options[:max_date] || Time.zone.now).midnight
+    num_days = options[:num_days] || 20
+    min_date = (options[:min_date] || max_date.advance(:days => -(num_days-1))).midnight
+
+    # if the db can't do (named) timezones, we do the best we can (dates on the
+    # other side of dst will be wrong though)
+    offset = max_date.utc_offset
+
+    expression = case connection.adapter_name.downcase
+    when /mysql/
+      # TODO: detect mysql named timezone support and use it
+      offset = "%s%02d:%02d" % [offset < 0 ? "-" : "+", offset.abs / 3600, offset.abs % 3600]
+      "DATE(CONVERT_TZ(#{column}, '+00:00', '#{offset}'))"
+    when /sqlite/
+      "DATE(STRFTIME('%s', #{column}) + #{offset}, 'unixepoch')"
+    when /postgres/
+      "(#{column} AT TIME ZONE '#{Time.zone.tzinfo.name}')::DATE"
+    end
+
+    result = count(
+      :conditions => [
+        "#{column} >= ? AND #{column} < ?",
+        min_date,
+        max_date.advance(:days => 1)
+      ],
+      :group => expression,
+      :order => expression
+    )
+    # mysql gives us date keys, sqlite/postgres don't 
+    return result if result.keys.first.is_a?(Date)
+    Hash[result.map { |date, count|
+      [Time.zone.parse(date).to_date, count]
+    }]
+  end
+
   class DynamicFinderTypeError < Exception; end
   class << self
     def construct_attributes_from_arguments_with_type_cast(attribute_names, arguments)
