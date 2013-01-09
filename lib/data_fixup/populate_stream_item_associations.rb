ca4f9ce86e21dd1625e98aec81fd30124ce1a136
Cody Cutrer <cody@instructure.com>
2013-01-08 20:47:18 -0700

    fix PopulateStreamItemAssociations data fixup
    fixes gh-258
    
    the dropped columns initializer was hiding the data from the migration,
    so avoid using the model at all
    
    Change-Id: I25fa901a36c17a30587725b482a74eff62da5094
    Reviewed-on: https://gerrit.instructure.com/16658
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- lib/data_fixup/populate_stream_item_associations.rb
+++ lib/data_fixup/populate_stream_item_associations.rb
@@ -1,28 +1,51 @@
 module DataFixup::PopulateStreamItemAssociations
   def self.run
     while true
-      batch = StreamItem.scoped(:conditions => "(context_type IS NULL AND context_code IS NOT NULL) OR (asset_type IS NULL AND item_asset_string IS NOT NULL)").find(:all, :limit => 1000)
+      batch = StreamItem.connection.select_all(<<-SQL)
+        SELECT id, context_type, context_id, context_code, asset_type, asset_id, item_asset_string
+        FROM stream_items
+        WHERE (context_type IS NULL AND context_code IS NOT NULL) OR
+              (asset_type IS NULL AND item_asset_string IS NOT NULL)
+        LIMIT 1000
+SQL
       break if batch.empty?
       batch.each do |si|
-        si.context_type, si.context_id = ActiveRecord::Base.parse_asset_string(si.context_code) if si.context_code.present?
-        si.asset_type, si.asset_id = ActiveRecord::Base.parse_asset_string(si.item_asset_string) if si.item_asset_string.present?
+        updates = {}
+        if si['context_code'].present? && (!si['context_type'].present? || !si['context_id'].present?)
+          context_type, context_id = ActiveRecord::Base.parse_asset_string(si['context_code'])
+          updates[:context_type] = context_type
+          updates[:context_id] = context_id
+        end
+        if si['item_asset_string'].present? && (!si['asset_type'].present? || !si['asset_id'].present?)
+          asset_type, asset_id = ActiveRecord::Base.parse_asset_string(si['item_asset_string'])
+          updates[:asset_type] = asset_type
+          updates[:asset_id] = asset_id
+        end
         begin
-          si.save! if si.changed?
+          StreamItem.update_all(updates, :id => si['id']) unless updates.empty?
         rescue ActiveRecord::Base::UniqueConstraintViolation
           # duplicate!
           # we have no way of knowing which one (or both) has stream item instances,
           # so just let the first one win
-          si.destroy
+          StreamItem.delete_all(:id => si['id'])
         end
       end
     end
 
     while true
-      batch = StreamItemInstance.scoped(:conditions => "context_type IS NULL AND context_code IS NOT NULL").find(:all, :limit => 1000)
+      batch = StreamItemInstance.connection.select_all(<<-SQL)
+        SELECT id, context_type, context_id, context_code
+        FROM stream_items
+        WHERE context_type IS NULL AND context_code IS NOT NULL
+        LIMIT 1000
+SQL
       break if batch.empty?
       batch.each do |sii|
-        sii.context_type, sii.context_id = ActiveRecord::Base.parse_asset_string(sii.context_code) if sii.context_code.present?
-        sii.save! if sii.changed?
+        updates = {}
+        context_type, context_id = ActiveRecord::Base.parse_asset_string(sii['context_code'])
+        updates[:context_type] = context_type
+        updates[:context_id] = context_id
+        StreamItemInstance.update_all(updates, :id => sii['id'])
       end
     end
   end
