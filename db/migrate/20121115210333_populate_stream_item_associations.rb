5db8700b1acbb59a01ca9ccb196d7aefa818ffae
Cody Cutrer <cody@instructure.com>
2013-01-10 15:16:38 -0700

    this migration is non-transactional
    Change-Id: Ia54bd05cc58ee95160b52dd27c918df2bba005eb
    Reviewed-on: https://gerrit.instructure.com/16719
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20121115210333_populate_stream_item_associations.rb
+++ db/migrate/20121115210333_populate_stream_item_associations.rb
@@ -1,5 +1,6 @@
 class PopulateStreamItemAssociations < ActiveRecord::Migration
   tag :postdeploy
+  self.transactional = false
 
   def self.up
     # the drop migration now exists, so this can no longer run async
