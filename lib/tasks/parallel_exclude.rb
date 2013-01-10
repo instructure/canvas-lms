1daead4940a2d8588bd470c6136815f19fa083be
Shawn Meredith <shawn@instructure.com>
2013-01-10 16:38:53 -0700

    spec: added spec/integration/files_spec.rb back to exclude list
    Change-Id: I394f273a0b04e446bad1faf3022472f0e3d7480c
    Reviewed-on: https://gerrit.instructure.com/16722
    QA-Review: Bryan Madsen <bryan@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Tested-by: Bryan Madsen <bryan@instructure.com>

--- lib/tasks/parallel_exclude.rb
+++ lib/tasks/parallel_exclude.rb
@@ -1,5 +1,5 @@
 module ParallelExclude
-  FILES = []
+  FILES = ["spec/integration/files_spec.rb"]
 
   test_files = FileList['vendor/plugins/*/spec_canvas/**/*_spec.rb'].exclude('vendor/plugins/*/spec_canvas/selenium/*_spec.rb') + FileList['spec/**/*_spec.rb'].exclude('spec/selenium/**/*_spec.rb')
   AVAILABLE_FILES = test_files.select { |file_name| FILES.include?(file_name) }
