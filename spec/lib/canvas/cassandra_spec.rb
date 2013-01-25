f75f0626e692b97cb5a69e3d1691552cab9a5966
Brian Palmer <brianp@instructure.com>
2013-01-25 10:40:44 -0700

    cassandra batching, and batch up the page view updates
    Add a batch method that uses cassandra's BEGIN BATCH/APPLY BATCH
    functionality to cut down on the number of network round trips, and
    possibly save a bit of CPU time on the cassandra servers.
    
    Since execute returns a result set, it can't be batched, so we add another
    method update for INSERT/UPDATE CQL statements that don't return a
    value.
    
    Closes CNVS-3526
    
    test plan: No behavioral change. Do a regression test that page view
    information is still send to cassandra properly. You can check the debug
    logs to see that the CQL statements are all sent in one big BEGIN BATCH
    statement.
    
    Change-Id: Ibca4f6fbd84f2644436599c017f1ec8c39783e36
    Reviewed-on: https://gerrit.instructure.com/17156
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Clare Hetherington <clare@instructure.com>

--- spec/lib/canvas/cassandra_spec.rb
+++ spec/lib/canvas/cassandra_spec.rb
@@ -16,53 +16,142 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../../cassandra_spec_helper.rb')
 
 describe "Canvas::Redis::Cassandra" do
+  let(:db) do
+    @cql_db = mock()
+    CassandraCQL::Database.stubs(:new).returns(@cql_db)
+    Canvas::Cassandra::Database.new([], {}, {})
+  end
+
+  describe "#batch" do
+    it "should do nothing for empty batches" do
+      db.expects(:execute).never
+      db.in_batch?.should == false
+      db.batch do
+      db.in_batch?.should == true
+      end
+      db.in_batch?.should == false
+    end
+
+    it "should do update statements in a batch" do
+      db.expects(:execute).with("1")
+      db.batch { db.update("1") }
+
+      db.expects(:execute).with("BEGIN BATCH UPDATE ? ? UPDATE ? ? APPLY BATCH", 1, 2, 3, 4)
+      db.batch { db.update("UPDATE ? ?", 1, 2); db.update("UPDATE ? ?", 3, 4) }
+    end
+
+    it "should not batch up execute statements" do
+      db.expects(:execute).with("SELECT").returns("RETURN")
+      db.expects(:execute).with("BEGIN BATCH 1 2 APPLY BATCH")
+      db.batch do
+        db.update("1")
+        db.execute("SELECT").should == "RETURN"
+        db.update("2")
+      end
+    end
+
+    it "should allow nested batch calls" do
+      db.expects(:execute).with("BEGIN BATCH 1 2 APPLY BATCH")
+      db.batch do
+        db.update("1")
+        db.batch do
+          db.in_batch?.should == true
+          db.update("2")
+        end
+      end
+      db.in_batch?.should == false
+    end
+
+    it "should clean up from exceptions" do
+      db.expects(:execute).never
+      begin
+        db.batch do
+          db.update("1")
+          raise "oh noes"
+        end
+      rescue
+        db.in_batch?.should == false
+      end
+      db.expects(:execute).with("2")
+      db.batch do
+        db.update("2")
+      end
+    end
+  end
+
   describe "#update_record" do
     it "should do nothing if there are no updates or deletes" do
-      statement, args = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, {})
-      statement.should be_nil
+      db.expects(:execute).never
+      db.update_record("test_table", { :id => 5 }, {})
     end
 
     it "should do lone updates" do
-      cql1 = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => "test" })
-      cql2 = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => [nil, "test"] })
-      cql1.should == ["UPDATE test_table SET name = ? WHERE id = ?", ["test", 5]]
-      cql1.should == cql2
+      db.expects(:execute).with("UPDATE test_table SET name = ? WHERE id = ?", "test", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => "test" })
+      db.expects(:execute).with("UPDATE test_table SET name = ? WHERE id = ?", "test", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => [nil, "test"] })
     end
 
     it "should do multi-updates" do
-      cql = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => "test", :nick => ["old", "new"] })
-      cql.should == ["UPDATE test_table SET name = ?, nick = ? WHERE id = ?", ["test", "new", 5]]
+      db.expects(:execute).with("UPDATE test_table SET name = ?, nick = ? WHERE id = ?", "test", "new", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => "test", :nick => ["old", "new"] })
     end
 
     it "should do lone deletes" do
-      cql1 = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => nil })
-      cql2 = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => ["old", nil] })
-      cql1.should == ["DELETE name FROM test_table WHERE id = ?", [5]]
-      cql1.should == cql2
+      db.expects(:execute).with("DELETE name FROM test_table WHERE id = ?", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => nil })
+      db.expects(:execute).with("DELETE name FROM test_table WHERE id = ?", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => ["old", nil] })
     end
 
     it "should do multi-deletes" do
-      cql = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => nil, :nick => ["old", nil] })
-      cql.should == ["DELETE name, nick FROM test_table WHERE id = ?", [5]]
+      db.expects(:execute).with("DELETE name, nick FROM test_table WHERE id = ?", 5)
+      db.update_record("test_table", { :id => 5 }, { :name => nil, :nick => ["old", nil] })
     end
 
     it "should do combined updates and deletes" do
-      cql = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5 }, { :name => "test", :nick => nil })
-      cql.should == ["BEGIN BATCH UPDATE test_table SET name = ? WHERE id = ? DELETE nick FROM test_table WHERE id = ? APPLY BATCH", ["test", 5, 5]]
+      db.expects(:execute).with("BEGIN BATCH UPDATE test_table SET name = ? WHERE id = ? DELETE nick FROM test_table WHERE id = ? APPLY BATCH", "test", 5, 5)
+      db.update_record("test_table", { :id => 5 }, { :name => "test", :nick => nil })
+    end
+
+    it "should work when already in a batch" do
+      db.expects(:execute).with("BEGIN BATCH UPDATE ? UPDATE test_table SET name = ? WHERE id = ? DELETE nick FROM test_table WHERE id = ? UPDATE ? APPLY BATCH", 1, "test", 5, 5, 2)
+      db.batch do
+        db.update("UPDATE ?", 1)
+        db.update_record("test_table", { :id => 5 }, { :name => "test", :nick => nil })
+        db.update("UPDATE ?", 2)
+      end
     end
 
     it "should handle compound primary keys" do
-      cql = Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5, :sub_id => "sub!" }, { :name => "test", :id => 5, :sub_id => [nil, "sub!"] })
-      cql.should == ["UPDATE test_table SET name = ? WHERE id = ? AND sub_id = ?", ["test", 5, "sub!"]]
+      db.expects(:execute).with("UPDATE test_table SET name = ? WHERE id = ? AND sub_id = ?", "test", 5, "sub!")
+      db.update_record("test_table", { :id => 5, :sub_id => "sub!" }, { :name => "test", :id => 5, :sub_id => [nil, "sub!"] })
     end
 
     it "should disallow changing a primary key component" do
       expect {
-        Canvas::Cassandra::Database.build_update_record_cql("test_table", { :id => 5, :sub_id => "sub!" }, { :name => "test", :id => 5, :sub_id => ["old", "sub!"]})
+        db.update_record("test_table", { :id => 5, :sub_id => "sub!" }, { :name => "test", :id => 5, :sub_id => ["old", "sub!"]})
       }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "execute and update" do
+    it_should_behave_like "cassandra page views"
+
+    let(:db) { PageView.cassandra }
+
+    it "should return the result from execute" do
+      db.execute("select count(*) from page_views").fetch['count'].should == 0
+      db.select_value("select count(*) from page_views").should == 0
+      db.execute("insert into page_views (request_id, user_id) values (?, ?)", "test", 0).should == nil
+    end
+
+    it "should return nothing from update" do
+      db.update("select count(*) from page_views").should == nil
+      db.update("insert into page_views (request_id, user_id) values (?, ?)", "test", 0).should == nil
     end
   end
 end
