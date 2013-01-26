a1eb3942845efb93a0777b30357e77902bd2d4ae
Stanley Stuart <stanley@instructure.com>
2013-01-26 09:33:54 -0700

    ensure todos have needed fields during api calls
    This commit fixes a regression from d30084, in which calls to
    Api::V1::assignment_json were not being provided needed fields such as
    'turnitin_settings' when accessed through the todos api. This caused the
    instantiated ActiveRecord objects to throw an exception because the
    field(s) had not been assigned.
    
    test plan:
      - Ensure you have a todo, e.g. an assignment that you haven't
        submitted but is due in the future.
      - Make an authenticated API request for todos
        (e.g. /api/v1/users/self/todo ).
      - The API response should be successful and include todo information.
        The API response should not contain an error message, or have an
        HTTP status indicating errors such as 500.
    
    fixes CNVS-3362
    
    Change-Id: I8af8f17590ac8e37fa2a545c9e49deadf6f3d2b6
    Reviewed-on: https://gerrit.instructure.com/17176
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1519,9 +1519,18 @@ class User < ActiveRecord::Base
 
   def assignments_needing_submitting(opts={})
     ActiveRecord::Base::ConnectionSpecification.with_environment(:slave) do
-      course_codes = opts[:contexts] ? (Array(opts[:contexts]).map(&:asset_string) & current_student_enrollment_course_codes) : current_student_enrollment_course_codes
-      ignored_ids = ignored_items(:submitting).select{|key, val| key.match(/\Aassignment_/) }.map{|key, val| key.sub(/\Aassignment_/, "") }
-      Assignment.for_context_codes(course_codes).active.due_before(1.week.from_now).
+      course_codes = if opts[:contexts]
+        (Array(opts[:contexts]).map(&:asset_string) &
+         current_student_enrollment_course_codes)
+      else
+        current_student_enrollment_course_codes
+      end
+      ignored_ids = ignored_items(:submitting).
+        select{|key, val| key.match(/\Aassignment_/) }.
+        map{|key, val| key.sub(/\Aassignment_/, "") }
+      Assignment.for_context_codes(course_codes).
+        active.
+        due_before(1.week.from_now).
         expecting_submission.due_after(opts[:due_after] || 4.weeks.ago).
         need_submitting_info(id, opts[:limit] || 15, ignored_ids).
         not_locked
@@ -1540,9 +1549,19 @@ class User < ActiveRecord::Base
 
   def assignments_needing_grading(opts={})
     ActiveRecord::Base::ConnectionSpecification.with_environment(:slave) do
-      course_codes = opts[:contexts] ? (Array(opts[:contexts]).map(&:asset_string) & current_admin_enrollment_course_codes) : current_admin_enrollment_course_codes
-      ignored_ids = ignored_items(:grading).select{|key, val| key.match(/\Aassignment_/) }.map{|key, val| key.sub(/\Aassignment_/, "") }
-      Assignment.for_context_codes(course_codes).active.expecting_submission.need_grading_info(opts[:limit] || 15, ignored_ids).reject{|a| a.needs_grading_count_for_user(self) == 0}
+      course_codes = if opts[:contexts]
+        (Array(opts[:contexts]).map(&:asset_string) &
+        current_admin_enrollment_course_codes)
+      else
+        current_admin_enrollment_course_codes
+      end
+      ignored_ids = ignored_items(:grading).
+        select{|key, val| key.match(/\Aassignment_/) }.
+        map{|key, val| key.sub(/\Aassignment_/, "") }
+      Assignment.for_context_codes(course_codes).active.
+        expecting_submission.
+        need_grading_info(opts[:limit] || 15, ignored_ids).
+        reject{|a| a.needs_grading_count_for_user(self) == 0}
     end
   end
   memoize :assignments_needing_grading
