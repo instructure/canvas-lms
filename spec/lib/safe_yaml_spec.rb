d20cfd609ec100fc04636ff5c539c7e10f162f86
Zach Wily <zach@instructure.com>
2013-02-03 03:36:30 -0700

    use safe_yaml
    Includes the safe_yaml gem, which replaces YAML.load and friends with a "safe"
    version by default, that does not create arbitrary objects.
    
    DelayedJobs was modified to use unsafe_load, as it relies on deserializing
    ruby objects.
    
    The biggest impact is with serialized columns - many of those store
    non-simple data types. Most commonly HashWithIndifferentAccess, but
    there are a few others as well. Our version of the safe_yaml gem allows
    for whitelisting certain classes.
    
    The I18nExtraction::SafeYAML class was also removed, as it's no longer
    needed. The extraction task was updated to call YAML.safe_load to be
    explicit.
    
    Currently, Gemfile is pointing to the Instructure fork of the safe_yaml
    gem on github. This needs to be released as a gem.
    
    Closes CNVS-3784
    
    test plan: If any serialized YAML columns contain a class that we missed
    in our whitelist, then that column will fail to deserialize and the
    model will behave incorrectly. It's difficult to say what exactly should
    be tested, as all the classes should be whitelisted. A general
    regression test on migrations, course copy, scribd, and quizzes would
    cover most of it.
    
    Change-Id: I3e1a95e101ada3a1b2366ff1ca70db6d17742cce
    Reviewed-on: https://gerrit.instructure.com/17404
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Clare Hetherington <clare@instructure.com>

---
+++ spec/lib/safe_yaml_spec.rb
@@ -0,0 +1,123 @@
+#
+# Copyright (C) 2013 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+
+describe "safe_yaml" do
+  it "should be used by default" do
+    yaml = <<-YAML
+--- !ruby/object:ActionController::Base 
+real_format: 
+YAML
+    expect { YAML.load yaml }.to raise_error(SafeYAML::UnsafeTagError)
+    result = YAML.unsafe_load yaml
+    result.class.should == ActionController::Base
+  end
+
+  it "should allow some whitelisted classes" do
+    yaml = <<-YAML
+---
+hwia: !map:HashWithIndifferentAccess
+  a: 1
+  b: 2
+float: !float
+  5.1
+os: !ruby/object:OpenStruct
+  modifiable: true
+  table: 
+    :a: 1
+    :b: 2
+    :sub: !ruby/object:OpenStruct
+      modifiable: true
+      table: 
+        :c: 3
+scribd: !ruby/object:Scribd::Document
+str: !str
+  hai
+mime: !ruby/object:Mime::Type
+  string: png
+  symbol: 
+  synonyms: []
+http: !ruby/object:URI::HTTP 
+  fragment: 
+  host: example.com
+  opaque: 
+  parser: 
+  password: 
+  path: /
+  port: 80
+  query: 
+  registry: 
+  scheme: http
+  user: 
+https: !ruby/object:URI::HTTPS 
+  fragment: 
+  host: example.com
+  opaque: 
+  parser: 
+  password: 
+  path: /
+  port: 443
+  query: 
+  registry: 
+  scheme: https
+  user: 
+ab: !ruby/object:Class AcademicBenchmark::Converter
+qt: !ruby/object:Class Qti::Converter
+verbose_symbol: !ruby/symbol blah
+YAML
+    result = YAML.load yaml
+
+    def verify(result, key, klass)
+      obj = result[key]
+      obj.class.should == klass
+      obj
+    end
+
+    hwia = verify(result, 'hwia', HashWithIndifferentAccess)
+    hwia.values_at(:a, :b).should == [1, 2]
+
+    float = verify(result, 'float', Float)
+    float.should == 5.1
+
+    os = verify(result, 'os', OpenStruct)
+    os.a.should == 1
+    os.b.should == 2
+    os.sub.class.should == OpenStruct
+    os.sub.c.should == 3
+
+    scribd = verify(result, 'scribd', Scribd::Document)
+
+    str = verify(result, 'str', String)
+    str.should == "hai"
+
+    mime = verify(result, 'mime', Mime::Type)
+    mime.to_s.should == 'png'
+
+    http = verify(result, 'http', URI::HTTP)
+    http.host.should == 'example.com'
+
+    https = verify(result, 'https', URI::HTTPS)
+    https.host.should == 'example.com'
+
+    result['ab'].should == AcademicBenchmark::Converter
+    result['qt'].should == Qti::Converter
+
+    result['verbose_symbol'].should == :blah
+  end
+end
