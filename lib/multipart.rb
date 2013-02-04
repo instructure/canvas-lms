707e4e981a1c4277473c7c9e63c0e70a7f83403c
Brian Palmer <brianp@instructure.com>
2013-02-04 09:20:19 -0700

    1.9: fix our multipart form building to work with StringIO objects again
    In 1.8 StringIO#path returns nil, but in 1.9 the method doesn't exist,
    so building multipart forms was failing.
    
    lib/multipart.rb could use some larger refactoring but I'm limiting the
    scope of this change since it's getting race tracked.
    
    fixes CNVS-3562
    
    test plan: With an environment running ruby 1.9, do a course copy with a
    course that includes at least 1 video. The copy should succeed and the
    video should work in the new course, as expected.
    
    Change-Id: Ib8a9f1559c639831977f0118874735891b98d02f
    Reviewed-on: https://gerrit.instructure.com/17407
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Clare Hetherington <clare@instructure.com>

--- lib/multipart.rb
+++ lib/multipart.rb
@@ -57,7 +57,8 @@ module Multipart
       def file_param(k, v)
         file_data = v.read rescue nil
         if file_data
-          FileParam.new(k, v.path, file_data)
+          file_path = v.respond_to?(:path) ? v.path : k.to_s
+          FileParam.new(k, file_path, file_data)
         else
           Param.new(k,v)
         end
