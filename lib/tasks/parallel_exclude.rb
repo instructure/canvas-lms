efff7b5146bd79cf6cc96349c6be29b20d5c0398
Bryan Madsen <bryan@instructure.com>
2013-02-13 15:24:39 -0700

    spec: adding 2 spec files that fail intermittently to parallel_exclude
    Change-Id: Ibbdd78cf7bcc7c7c6951f0eea95cf77bd8e1a43d
    Reviewed-on: https://gerrit.instructure.com/17706
    Reviewed-by: Shawn Meredith <shawn@instructure.com>
    QA-Review: Shawn Meredith <shawn@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- lib/tasks/parallel_exclude.rb
+++ lib/tasks/parallel_exclude.rb
@@ -1,7 +1,9 @@
 module ParallelExclude
   FILES = [
     "spec/integration/files_spec.rb",
-    "spec/apis/v1/calendar_events_api_spec.rb" #fails intermittently with ruby 1.9.3
+    "spec/apis/v1/calendar_events_api_spec.rb", #fails intermittently with ruby 1.9.3
+    "spec/models/attachment_spec.rb",
+    "spec/lib/content_zipper_spec.rb"
   ]
 
   test_files = FileList['vendor/plugins/*/spec_canvas/**/*_spec.rb'].exclude('vendor/plugins/*/spec_canvas/selenium/*_spec.rb') + FileList['spec/**/*_spec.rb'].exclude('spec/selenium/**/*_spec.rb')
