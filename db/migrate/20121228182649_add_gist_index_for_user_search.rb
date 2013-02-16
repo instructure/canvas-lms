7e74923f8bd67e28269c82342dbaf45317a1beec
Brian Palmer <brianp@instructure.com>
2013-02-15 22:58:34 -0700

    create gist indexes concurrently
    Change-Id: If4f1d6cfee765843d7cb83d0e5132f4a415933c5
    Reviewed-on: https://gerrit.instructure.com/17794
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>
    Tested-by: Brian Palmer <brianp@instructure.com>

--- db/migrate/20121228182649_add_gist_index_for_user_search.rb
+++ db/migrate/20121228182649_add_gist_index_for_user_search.rb
@@ -7,9 +7,9 @@ class AddGistIndexForUserSearch < ActiveRecord::Migration
       execute('create extension if not exists pg_trgm;') rescue nil
 
       if has_postgres_proc?('show_trgm')
-        execute('create index index_trgm_users_name on users USING gist(lower(name) gist_trgm_ops);')
-        execute('create index index_trgm_pseudonyms_sis_user_id on pseudonyms USING gist(lower(sis_user_id) gist_trgm_ops);')
-        execute('create index index_trgm_communication_channels_path on communication_channels USING gist(lower(path) gist_trgm_ops);')
+        execute('create index concurrently index_trgm_users_name on users USING gist(lower(name) gist_trgm_ops);')
+        execute('create index concurrently index_trgm_pseudonyms_sis_user_id on pseudonyms USING gist(lower(sis_user_id) gist_trgm_ops);')
+        execute('create index concurrently index_trgm_communication_channels_path on communication_channels USING gist(lower(path) gist_trgm_ops);')
       end
     end
   end
