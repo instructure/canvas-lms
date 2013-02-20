d03fd7f1d7f745cc5c30ed1d03de95be61bb29d3
Cameron Matheson <cameron@instructure.com>
2013-02-20 11:29:48 -0700

    fetch gb2 enrollments in chunks of api_max_per_page
    refs CNVS-3927
    
    Hopefully the api_max_per_page size is going to be increased soon, this
    will prevent us from being artificially limited at 100.
    
    Test plan:
      * set your api_max_per_page setting to > 100
      * ensure that the pages fetched by gb2 match your api_max_per_page
        setting
    
    Change-Id: Ia0468f182b83acb911f9d328b97e9f8cbd696364
    Reviewed-on: https://gerrit.instructure.com/17893
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- app/controllers/gradebook2_controller.rb
+++ app/controllers/gradebook2_controller.rb
@@ -7,26 +7,26 @@ class Gradebook2Controller < ApplicationController
     if authorized_action(@context, @current_user, [:manage_grades, :view_all_grades])
       @gradebook_is_editable = @context.grants_right?(@current_user, session, :manage_grades)
 
-        js_env  :GRADEBOOK_OPTIONS => {
-          :chunk_size => Setting.get_cached('gradebook2.submissions_chunk_size', '35').to_i,
-          :assignment_groups_url => api_v1_course_assignment_groups_url(@context, :include => [:assignments]),
-          :sections_url => api_v1_course_sections_url(@context),
-          :students_url => api_v1_course_enrollments_url(@context, :include => [:avatar_url], :type => ['StudentEnrollment', 'StudentViewEnrollment'], :per_page => 100),
-          :students_url_with_concluded_enrollments => api_v1_course_enrollments_url(@context, :include => [:avatar_url], :type => ['StudentEnrollment', 'StudentViewEnrollment'], :state => ['active', 'invited', 'completed'], :per_page => 100),
-          :submissions_url => api_v1_course_student_submissions_url(@context, :grouped => '1'),
-          :change_grade_url => api_v1_course_assignment_submission_url(@context, ":assignment", ":submission"),
-          :context_url => named_context_url(@context, :context_url),
-          :download_assignment_submissions_url => named_context_url(@context, :context_assignment_submissions_url, "{{ assignment_id }}", :zip => 1),
-          :re_upload_submissions_url => named_context_url(@context, :context_gradebook_submissions_upload_url, "{{ assignment_id }}"),
-          :context_id => @context.id,
-          :context_code => @context.asset_string,
-          :group_weighting_scheme => @context.group_weighting_scheme,
-          :grading_standard =>  @context.grading_standard_enabled? && (@context.grading_standard.try(:data) || GradingStandard.default_grading_standard),
-          :course_is_concluded => @context.completed?,
-          :gradebook_is_editable => @gradebook_is_editable,
-          :speed_grader_enabled => @context.allows_speed_grader?,
-        }
-
+      per_page = Setting.get_cached('api_max_per_page', '50').to_i
+      js_env  :GRADEBOOK_OPTIONS => {
+        :chunk_size => Setting.get_cached('gradebook2.submissions_chunk_size', '35').to_i,
+        :assignment_groups_url => api_v1_course_assignment_groups_url(@context, :include => [:assignments]),
+        :sections_url => api_v1_course_sections_url(@context),
+        :students_url => api_v1_course_enrollments_url(@context, :include => [:avatar_url], :type => ['StudentEnrollment', 'StudentViewEnrollment'], :per_page => per_page),
+        :students_url_with_concluded_enrollments => api_v1_course_enrollments_url(@context, :include => [:avatar_url], :type => ['StudentEnrollment', 'StudentViewEnrollment'], :state => ['active', 'invited', 'completed'], :per_page => per_page),
+        :submissions_url => api_v1_course_student_submissions_url(@context, :grouped => '1'),
+        :change_grade_url => api_v1_course_assignment_submission_url(@context, ":assignment", ":submission"),
+        :context_url => named_context_url(@context, :context_url),
+        :download_assignment_submissions_url => named_context_url(@context, :context_assignment_submissions_url, "{{ assignment_id }}", :zip => 1),
+        :re_upload_submissions_url => named_context_url(@context, :context_gradebook_submissions_upload_url, "{{ assignment_id }}"),
+        :context_id => @context.id,
+        :context_code => @context.asset_string,
+        :group_weighting_scheme => @context.group_weighting_scheme,
+        :grading_standard =>  @context.grading_standard_enabled? && (@context.grading_standard.try(:data) || GradingStandard.default_grading_standard),
+        :course_is_concluded => @context.completed?,
+        :gradebook_is_editable => @gradebook_is_editable,
+        :speed_grader_enabled => @context.allows_speed_grader?,
+      }
     end
   end
 end
