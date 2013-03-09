709853de4b97626a19d76874fb2aef95ba71289a
Brian Palmer <brianp@instructure.com>
2013-03-09 14:27:44 -0700

    fix permissions check for UsersController#show
    This needs to check the user, not the account, but we no longer grant
    :view_statistics if a user has :read_reports on a course, effectively
    blocking teachers from viewing the top-level user page by default.
    
    A new user-level :read_reports permission was added so that teachers and
    account admins can still view the teacher/student interaction reports as
    before, even though teachers don't have :view_statistics
    
    fixes CNVS-2964
    
    test plan:
    - a site admin user should be able to view /users/X on any account
    - an account admin user should be able to view /users/X for students in
      thier account, but not in other accounts
    - a teacher should not be able to view /users/X for a student in their
      course Y, but they should be able to view /courses/Y/users/X
    
    Change-Id: Iebc639bd935f50344cb77614f6eeae2bacb421e2
    Reviewed-on: https://gerrit.instructure.com/18473
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- spec/controllers/page_views_controller_spec.rb
+++ spec/controllers/page_views_controller_spec.rb
@@ -41,16 +41,17 @@ describe PageViewsController do
   end
 
   shared_examples_for "GET 'index' as csv" do
-    it "should succeed" do
-      course_with_teacher_logged_in
+    before do
+      user_session(account_admin_user)
       student_in_course
+    end
+
+    it "should succeed" do
       page_view(@user, '/somewhere/in/app', :created_at => 2.days.ago)
       get 'index', :user_id => @user.id, :format => 'csv'
       response.should be_success
     end
     it "should order rows by created_at in DESC order" do
-      course_with_teacher_logged_in
-      student_in_course
       pv2 = page_view(@user, '/somewhere/in/app', :created_at => 2.days.ago)    # 2nd day
       pv1 = page_view(@user, '/somewhere/in/app/1', :created_at => 1.day.ago)  # 1st day
       pv3 = page_view(@user, '/somewhere/in/app/2', :created_at => 3.days.ago)  # 3rd day
