b420729e61f9d84ed0d6186b8cd54b7b7dca653b
Jon Jensen <jon@instructure.com>
2013-03-11 12:19:00 -0600

    quiz item analysis generation, closes CNVS-3421
    report generator to do quiz item analysis
    
    usage:
    
    quiz = Quiz.find(<id>)
    summary = QuizStatistics::ItemAnalysis::Summary.new(quiz)
    summary.each do |item|
      puts item.question_text
      puts "respondents: #{item.num_respondents}"
      puts "correct: #{item.num_respondents(:correct)}"
      puts "standard deviation: #{item.standard_deviation}"
      ...
      puts "----------------"
    end
    
    test plan:
    N/A, see specs
    
    Change-Id: Id36e4ef404af089f526cf9243a14d42eb52064a6
    Reviewed-on: https://gerrit.instructure.com/18494
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Joe Tanner <joe@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

---
+++ app/models/quiz_statistics/item_analysis/summary.rb
@@ -0,0 +1,86 @@
+class QuizStatistics::ItemAnalysis::Summary
+  include Enumerable
+  extend Forwardable
+
+  def_delegators :sorted_items, :size, :length, :each
+
+  def initialize(quiz, options = {})
+    @quiz = quiz
+    @attempts = quiz.quiz_submissions.map { |qs| qs.submitted_versions.first }
+    @options = options
+    @options[:buckets] ||= [
+      [:bottom, 0.27],
+      [:middle, 0.63],
+      [:top,    1]
+    ]
+
+    aggregate_data
+  end
+
+  def aggregate_data
+    @attempts.each do |attempt|
+      add_respondent attempt.user_id, attempt.score
+      attempt.quiz_data.each_with_index do |question, i|
+        add_response question, attempt.submission_data[i], attempt.user_id
+      end
+    end
+  end
+
+  def add_response(question, answer, respondent_id)
+    @items ||= {}
+    @items[question] ||= QuizStatistics::ItemAnalysis::Item.from(self, question) or return
+    @items[question].add_response(answer, respondent_id)
+  end
+
+  def add_respondent(respondent_id, score)
+    @respondent_scores ||= {}
+    @respondent_scores[respondent_id] = score
+  end
+
+  # group the student ids into buckets according to score (e.g. bottom
+  # 27%, middle 46%, top 27%)
+  def buckets
+    @buckets ||= begin
+      buckets = @options[:buckets]
+      ranked_respondent_ids = @respondent_scores.sort_by(&:last).map(&:first)
+      Hash[buckets.each_with_index.map { |(name, cutoff), i|
+        floor = i > 0 ? (buckets[i - 1][1] * ranked_respondent_ids.length).round : 0
+        ceiling = (cutoff * ranked_respondent_ids.length).round
+        [name, ranked_respondent_ids[floor...ceiling]]
+      }]
+    end
+  end
+
+  def mean_score_for(respondent_ids)
+    return nil if respondent_ids.empty?
+    @respondent_scores.slice(*respondent_ids).values.sum * 1.0 / respondent_ids.size
+  end
+
+  def sorted_items
+    @sorted_items ||= @items.values.sort
+  end
+
+  # population variance, not sample variance, since we have all datapoints
+  def variance(respondent_ids = :all)
+    @variance ||= {}
+    @variance[respondent_ids] ||= begin
+      scores = (respondent_ids == :all ? @respondent_scores : @respondent_scores.slice(*respondent_ids)).values
+      SimpleStats.variance(scores)
+    end
+  end
+
+  # population sd, not sample sd, since we have all datapoints
+  def standard_deviation(respondent_ids = :all)
+    @sd ||= {}
+    @sd[respondent_ids] ||= Math.sqrt(variance(respondent_ids))
+  end
+
+  def alpha
+    @alpha ||= begin
+      items = @items.values
+      variance_sum = items.map(&:variance).sum
+      items.size / (items.size - 1.0) * (1 - variance_sum / variance)
+    end
+  end
+end
+
---
+++ spec/models/quiz_statistics/item_analysis/common.rb
@@ -0,0 +1,92 @@
+# like be_within, but also works on arrays/hashes that have reals
+class BeApproximately
+  def initialize(expected, tolerance)
+    @expected = expected
+    @tolerance = tolerance
+  end
+
+  def matches?(target)
+    @target = target
+    approximates?(@target, @expected)
+  end
+
+  def approximates?(target, expected)
+    return true  if target == expected
+    return false unless target.class == expected.class
+    case target
+    when Array; array_approximates(target, expected)
+    when Hash;  hash_approximates(target, expected)
+    when Fixnum,
+         Bignum,
+         Float; real_approximates(target, expected)
+    else        false
+    end
+  end
+
+  def array_approximates(target, expected)
+    target.size == expected.size &&
+    target.map.with_index.all? { |value, index|
+      approximates?(target[index], expected[index])
+    }
+  end
+
+  def hash_approximates(target, expected)
+    target.keys.sort == expected.keys.sort &&
+    target.keys.all? { |key|
+      approximates?(target[key], expected[key])
+    }
+  end
+
+  def real_approximates(target, expected)
+    (target - expected).abs <= @tolerance
+  end
+
+  def failure_message_for_should
+    "expected #{@target.inspect} to be approximately #{@expected}"
+  end
+
+  def failure_message_for_should_not
+    "expected #{@target.inspect} not to be approximately #{@expected}"
+  end
+end
+
+def be_approximately(expected, tolerance = 0.01)
+  BeApproximately.new(expected, tolerance)
+end
+
+# set up a quiz with graded submissions. supports T/F and multiple
+# choice questions
+#
+# * answer_key is an array of correct answers (one of [A-DFT])
+# * each submission is an array of a student's submitted answers
+#
+# question types are inferred from the correct answer, and multiple
+# choice questions always have four possibilities
+#
+# note that you can specify point values for each question by
+# providing an array for each answer (e.g. ["A", 2] instead of just "A")
+def simple_quiz_with_submissions(answer_key, *submissions)
+  questions = answer_key.each_with_index.map { |answer, i|
+    points = 1
+    answer, points = answer if answer.is_a?(Array)
+    true_false = answer == 'T' || answer == 'F'
+    type = true_false ? 'true_false_question' : 'multiple_choice_question'
+    answers = Hash[(true_false ? ['T', 'F'] : 'A'..'D').each_with_index.map { |a, j|
+      ["answer_#{j}", {:answer_text => a, :answer_weight => (a == answer ? 100 : 0), :id => (4 * i + j)}]
+    }]
+    {:question_data => {:name => "question #{i + 1}", :points_possible => points, :question_type => type, :answers => answers}}
+  }
+  assignment_quiz(questions)
+  submissions.each do |data|
+    sub = @quiz.generate_submission(user())
+    sub.mark_completed
+    sub.submission_data = Hash[data.each_with_index.map{ |answer, i|
+      answer = {"T" => "True", "F" => "False"}[answer] || answer
+      matched_answer = @questions[i].question_data[:answers].detect{ |a| a[:text] == answer}
+      ["question_#{@questions[i].id}", matched_answer ? matched_answer[:id].to_s : nil]
+    }]
+    sub.grade_submission
+  end
+  @quiz.reload
+end
+
