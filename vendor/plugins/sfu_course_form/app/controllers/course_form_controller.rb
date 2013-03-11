94cafe5afbf9b98754bf3de685538cf2e798ab12
Ron Santos <ron.santos@gmail.com>
2013-03-11 13:34:28 -0700

    Fixed cross-list section csv import
    Also, did some code cleanup by separating the create method into smaller methods

--- vendor/plugins/sfu_course_form/app/controllers/course_form_controller.rb
+++ vendor/plugins/sfu_course_form/app/controllers/course_form_controller.rb
@@ -15,13 +15,13 @@ class CourseFormController < ApplicationController
 
   def create
     selected_courses = []
-    sections = []
     account_id = Account.find_by_name('Simon Fraser University').id
     teacher_username = params[:username]
     teacher2_username = params[:enroll_me]
     teacher_sis_user_id = sis_user_id(teacher_username, account_id)
     teacher2_sis_user_id = sis_user_id(teacher2_username, account_id) unless teacher2_username.nil?
     cross_list = params[:cross_list]
+
     params.each do |key, value|
       if key.to_s.starts_with? "selected_course"
         selected_courses.push value
@@ -31,94 +31,54 @@ class CourseFormController < ApplicationController
     course_array = ["course_id,short_name,long_name,account_id,term_id,status"]
     section_array = ["section_id,course_id,name,status,start_date,end_date"]
     enrollment_array = ["course_id,user_id,role,section_id,status"]
-    default_section_id = ""
 
     unless cross_list
+
       selected_courses.compact.uniq.each do |course|
-        # 1131:::ensc:::351:::d100:::Real Time and Embedded Systems
         unless course == "sandbox"
           logger.info "[SFU Course Form] Creating single course container : #{course}"
-          course_info = course.split(":::")
-          term = course_info[0]
-          name = course_info[1].to_s
-          number = course_info[2]
-          section_name = course_info[3].to_s
-          title = course_info[4].to_s
-          section_tutorials = course_info[5]
-
-          course_id = "#{term}-#{name}-#{number}-#{section_name}"
-          section_id = "#{term}-#{name}-#{number}-#{section_name}:::section"
-          short_name = "#{name.upcase}#{number} #{section_name.upcase}"
-          long_name =  "#{short_name} #{title}"
-          # Default Section set D100, D200, E300, G800 or if only 1 section (i.e. no section tutorials)
-          default_section_id = section_id if section_name.end_with? "00" || section_tutorials.nil?
-
-          sections.push "#{section_id}:_:#{section_name.upcase}"
-
-          # add section tutorials csv
-          unless section_tutorials.nil?
-            section_tutorials.split(",").each do |tutorial_name|
-              section_id = "#{term}-#{name}-#{number}-#{tutorial_name.downcase}:::section"
-              sections.push "#{section_id}:_:#{tutorial_name.upcase}"
-            end
-          end
+          course_info = course_info(course, account_id, teacher_sis_user_id, teacher2_sis_user_id)
 
           # create course csv
-          course_array.push "#{course_id},#{short_name},#{long_name},#{account_id},#{term},active"
+          course_array.push course_info["course_csv"]
 
           # create section csv
-          sections.compact.uniq.each do  |section|
+          course_info["sections"].compact.uniq.each do  |section|
             section_info = section.split(":_:")
-            section_array.push "#{section_info[0]},#{course_id},#{section_info[1]},active,,,"
+            section_array.push "#{section_info[0]},#{course_info["course_id"]},#{section_info[1]},active,,,"
           end
 
-          # create enrollment csv to default section
-          enrollment_array.push "#{course_id},#{teacher_sis_user_id},teacher,#{default_section_id},active"
-          # enroll other teacher/ta to default section
-          enrollment_array.push "#{course_id},#{teacher2_sis_user_id},teacher,#{default_section_id},active" unless teacher2_username.nil?
+          enrollment_array.push course_info["enrollment_csv_1"]
+          enrollment_array.push course_info["enrollment_csv_2"] unless teacher2_username.nil?
+
         else
           logger.info "[SFU Course Form] Creating sandbox for #{teacher_username}"
-          datestamp = "1"
-          course_id = "sandbox-#{teacher_username}-#{datestamp}"
-          short_long_name = "Sandbox - #{teacher_username}"
+          sandbox = sandbox_info(teacher_username, account_id, teacher_sis_user_id, teacher2_sis_user_id)
 
-          course_array.push "#{course_id},#{short_long_name},#{short_long_name},#{account_id},,active"
-          # create enrollment csv to default section
-          enrollment_array.push "#{course_id},#{teacher_sis_user_id},teacher,#{default_section_id},active"
-          # enroll other teacher/ta to default section
-          enrollment_array.push "#{course_id},#{teacher2_sis_user_id},teacher,#{default_section_id},active" unless teacher2_sis_user_id.nil?
+          course_array.push sandbox["csv"]
+          enrollment_array.push sandbox["enrollment_csv_1"]
+          enrollment_array.push sandbox["enrollment_csv_2"] unless teacher2_sis_user_id.nil?
         end
       end
+
     else
+
       logger.info "[SFU Course Form] Creating cross-list container : #{selected_courses.inspect}"
       course_id = ""
       short_name = ""
       long_name = ""
       term = ""
+      sections = []
 
       selected_courses.each do |course|
-        course_info = course.split(":::")
-        term = course_info[0]
-        name = course_info[1].to_s
-        number = course_info[2]
-        section_name = course_info[3].to_s
-        title = course_info[4].to_s
-        section_tutorials = course_info[5]
+        course_info = course_info(course, account_id, teacher_sis_user_id, teacher2_sis_user_id)
 
-        course_id.concat "#{term}-#{name}-#{number}-#{section_name}:"
-        section_id = "#{term}-#{name}-#{number}-#{section_name}:::section"
-        short_name.concat "#{name.upcase}#{number} #{section_name.upcase} / "
-        long_name.concat  "#{name.upcase}#{number} #{section_name.upcase} #{title} / "
+        course_id.concat "#{course_info["course_id"]}:"
+        short_name.concat "#{course_info["short_name"]} / "
+        long_name.concat  "#{course_info["long_name"]} / "
+        term = course_info["term"]
 
-        sections.push "#{section_id}:_:#{name.upcase}#{number} #{section_name.upcase}"
-
-        # add section tutorials csv
-        unless section_tutorials.nil?
-          section_tutorials.split(",").compact.uniq.each do |tutorial_name|
-            section_id = "#{term}-#{name}-#{number}-#{tutorial_name.downcase}:::section"
-            sections.push "#{section_id}:_:#{tutorial_name.upcase}"
-          end
-        end
+        sections.push course_info["sections"]
       end
 
       # create course csv
@@ -127,14 +87,13 @@ class CourseFormController < ApplicationController
 
       # create section csv
       sections.compact.uniq.each do  |section|
-          section_info = section.split(":_:")
-          section_array.push "#{section_info[0]},#{course_id},#{section_info[1]},active,,,"
+        section_info = section.first.split(":_:")
+        section_array.push "#{section_info[0]},#{course_id},#{section_info[1]},active,,,"
       end
 
       # create enrollment csv to default section
-      enrollment_array.push "#{course_id},#{teacher_sis_user_id},teacher,#{default_section_id},active\n"
-      # enroll other teacher/ta to default section
-      enrollment_array.push "#{course_id},#{teacher2_sis_user_id},teacher,#{default_section_id},active\n" unless teacher2_sis_user_id.nil?
+      enrollment_array.push "#{course_id},#{teacher_sis_user_id},teacher,,active\n"
+      enrollment_array.push "#{course_id},#{teacher2_sis_user_id},teacher,,active\n" unless teacher2_sis_user_id.nil?
 
     end
 
@@ -160,10 +119,63 @@ class CourseFormController < ApplicationController
       flash[:error] = "Course request failed. Please try agin."
       redirect_to "/sfu/course/new"
     end
+
+  end
+
+  def course_info(course_line, account_id, teacher1, teacher2 = nil)
+    # Example; course_line = 1131:::ensc:::351:::d100:::Real Time and Embedded Systems
+    course = {}
+    sections = []
+    course_arr = course_line.split(":::")
+    course["term"] = course_arr[0]
+    course["name"] = course_arr[1].to_s
+    course["number"] = course_arr[2]
+    course["section_name"] = course_arr[3].to_s
+    course["title"] = course_arr[4].to_s
+    course["section_tutorials"] = course_arr[5]
+
+    course["course_id"] = "#{course["term"]}-#{course["name"]}-#{course["number"]}-#{course["section_name"]}"
+    course["section_id"] = "#{course["term"]}-#{course["name"]}-#{course["number"]}-#{course["section_name"]}:::section"
+    course["short_name"] = "#{course["name"].upcase}#{course["number"]} #{course["section_name"].upcase}"
+    course["long_name"] =  "#{course["short_name"]} #{course["title"]}"
+    # Default Section set D100, D200, E300, G800 or if only 1 section (i.e. no section tutorials)
+    course["default_section_id"] = course["section_id"] if course["section_name"].end_with? "00" || course["section_tutorials"].nil?
+
+    course["course_csv"] = "#{course["course_id"]},#{course["short_name"]},#{course["long_name"]},#{account_id},#{course["term"]},active"
+    course["enrollment_csv_1"] = "#{course["course_id"]},#{teacher1},teacher,#{course["default_section_id"]},active"
+    course["enrollment_csv_2"] = "#{course["course_id"]},#{teacher2},teacher,#{course["default_section_id"]},active" unless teacher2.nil?
+
+    sections.push "#{course["section_id"]}:_:#{course["name"].upcase}#{course["number"]} #{course["section_name"].upcase}"
+
+    # add section tutorials csv
+    unless course["section_tutorials"].nil?
+      course["section_tutorials"].split(",").compact.uniq.each do |tutorial_name|
+        section_id = "#{course["term"]}-#{course["name"]}-#{course["number"]}-#{tutorial_name.downcase}:::section"
+        sections.push "#{section_id}:_:#{tutorial_name.upcase}"
+      end
+    end
+
+    course["sections"] = sections
+
+    course
+  end
+
+  def sandbox_info(username, account_id, teacher1, teacher2 = nil)
+    datestamp = "1"
+    sandbox = {}
+    sandbox["course_id"] = "sandbox-#{username}-#{datestamp}"
+    sandbox["short_long_name"] = "Sandbox - #{username}"
+    sandbox["default_section_id"] = ""
+
+    sandbox["csv"] = "#{sandbox["course_id"]},#{sandbox["short_long_name"]},#{sandbox["short_long_name"]},#{account_id},,active"
+    sandbox["enrollment_csv_1"] = "#{sandbox["course_id"]},#{teacher1},teacher,#{sandbox["default_section_id"]},active"
+    sandbox["enrollment_csv_1"] = "#{sandbox["course_id"]},#{teacher2},teacher,#{sandbox["default_section_id"]},active" unless teacher2.nil?
+    sandbox
   end
 
   def sis_user_id(username, account_id)
     user = Pseudonym.find_by_unique_id_and_account_id(username, account_id)
     user.sis_user_id unless user.nil?
   end
+
 end
