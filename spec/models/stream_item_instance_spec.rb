9c6f628bcf0336389c1998738762ffa6a9125719
Brian Palmer <brianp@instructure.com>
2013-03-19 17:07:36 -0600

    spec: mark two specs pending in 1.8
    a bug with mocha stubbing class methods causes these to fail in 1.8
    
    Change-Id: I2f4900f2786ddcbb1e5cb74a1ad143c92df937e1
    Reviewed-on: https://gerrit.instructure.com/18779
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Product-Review: Bryan Madsen <bryan@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>
    Tested-by: Bryan Madsen <bryan@instructure.com>

--- spec/models/stream_item_instance_spec.rb
+++ spec/models/stream_item_instance_spec.rb
@@ -27,6 +27,7 @@ describe StreamItemInstance do
 
   describe ".update_all_with_invalidation" do
      it "invalidates stream item cache keys and runs update_all (the original)" do
+       pending("mocha class method bug") if RUBY_VERSION < "1.9"
        # expect
        StreamItemCache.expects(:invalidate_context_stream_item_key).twice
        updates, conditions, options = ['updates', {:conditions => ''}, {:options => ''}]
