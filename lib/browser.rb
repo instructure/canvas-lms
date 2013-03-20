0d4ccb9d6c4cfe9512318c5119f562391d1980aa
Jon Jensen <jon@instructure.com>
2013-03-20 11:13:09 -0600

    don't warn browsers we don't recognize, fixes CNVS-4754
    treat minimums as a blacklist, and assume that anything we don't recognize
    is supported
    
    test plan:
    1. load canvas in unsupported browsers (e.g. IE 8), confirm you get a
       warning
    2. load canvas in supported browsers (e.g. IE 9), confirm you don't get a
       warning
    3. load canvas in an unknown browser (e.g. Android), confirm you don't get
       a warning
    
    Change-Id: I87ed90e97d031431f6a48c814dfa874b785ea777
    Reviewed-on: https://gerrit.instructure.com/18801
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Cam Theriault <cam@instructure.com>
    Reviewed-by: Joe Tanner <joe@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>

--- lib/browser.rb
+++ lib/browser.rb
@@ -1,15 +1,16 @@
 class Browser < Struct.new(:browser, :version)
   def self.supported?(user_agent)
     user_agent = UserAgent.parse(user_agent)
-    supported_browsers.any?{ |browser| user_agent >= browser }
+    return false if minimum_browsers.any?{ |browser| user_agent < browser }
+    true # if we don't recognize it (e.g. Android), be nice
   end
 
   def self.configuration
     @configuration ||= YAML.load_file(Rails.root.join("config/browsers.yml"))
   end
 
-  def self.supported_browsers
-    @supported_browsers ||= (configuration['supported'] || []).
+  def self.minimum_browsers
+    @minimum_browsers ||= (configuration['minimums'] || []).
       map{ |browser, version| new(browser, version.to_s) }
   end
 end
