6b2da819bb7388dbb768b2b0b422b7f29dce94f2
James Williams <jamesw@instructure.com>
2013-03-26 10:43:40 -0600

    translate user content links to replaced files
    changes the user content html rewriter so links
    to files that were deleted and replaced will still
    be translated correctly (rather than pointing to
    the old deleted file)
    
    test plan:
    * upload a file
    * create an assignment
    * add a link to the file in the assignment description
    * delete the file
    * edit the file locally and re-upload it
    * edit the assignment and save it
    * should still be able to click on the link
     and receive the updated file
    
    fixes #CNVS-4799
    
    Change-Id: I527756f02d7b38dda36dbcc3cfae32a3980a25b3
    Reviewed-on: https://gerrit.instructure.com/18992
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Adam Phillipps <adam@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- lib/api.rb
+++ lib/api.rb
@@ -267,7 +267,13 @@ module Api
 
     rewriter = UserContent::HtmlRewriter.new(context, user)
     rewriter.set_handler('files') do |match|
-      obj = match.obj_id && match.obj_class.find_by_id(match.obj_id)
+      if match.obj_id
+        if match.obj_class == Attachment && context && !context.is_a?(User)
+          obj = context.attachments.find(match.obj_id) rescue nil
+        else
+          obj = match.obj_class.find_by_id(match.obj_id)
+        end
+      end
       next unless obj && rewriter.user_can_view_content?(obj)
       file_download_url(obj.id, :verifier => obj.uuid, :download => '1', :host => host, :protocol => protocol)
     end
