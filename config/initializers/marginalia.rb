6339d1e4bef68b6ca7b07b38aa57429abf95613d
Brian Palmer <brianp@instructure.com>
2013-04-08 14:23:01 -0600

    include information about current request in SQL queries
    This will help immensely when debugging slow or misbehaving queries.
    Thanks to 37signals: https://github.com/37signals/marginalia
    
    closes CNVS-5108
    
    test plan:
    
    cp config/marginalia.yml.example config/marginalia.yml
    then edit config/marginalia.yml to uncomment the production block and
    change "production" to "development".
    
    run Canvas, and see in your log file how SQL statements have some extra,
    useful information in a comment at the end. Run delayed jobs, and you'll
    also see useful info on SQL statements in the log -- including job tag,
    and context_id in this case will be job id.
    
    Change-Id: I7988a9afaf674bd00c3edc6adafccaf51e7ff60f
    Reviewed-on: https://gerrit.instructure.com/16008
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Clare Hetherington <clare@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>

---
+++ config/initializers/marginalia.rb
@@ -0,0 +1,22 @@
+require_dependency 'setting'
+
+config = Setting.from_config('marginalia') || {}
+
+if config[:components].present?
+  require 'marginalia'
+  Marginalia::Railtie.insert
+
+  module Marginalia
+    module Comment
+      def self.context_id
+        RequestContextGenerator.request_id
+      end
+
+      def self.job_tag
+        Delayed::Worker.current_job.try(:tag)
+      end
+    end
+  end
+
+  Marginalia::Comment.components = config[:components].map(&:to_sym)
+end
