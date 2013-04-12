a9a5d94ddec1768701e30c9522c8492b9184ae32
James Williams <jamesw@instructure.com>
2013-04-12 15:47:24 -0600

    fix broken assignments data fixup again
    fixes CNVS-5212
    
    Change-Id: I1045957567b56f7f427b2ba6bbb6c6ba6cfc8d63
    Reviewed-on: https://gerrit.instructure.com/19565
    Reviewed-by: Zach Wily <zach@instructure.com>
    Reviewed-by: Paul Hinze <paulh@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Bryan Madsen <bryan@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>

--- lib/data_fixup/fix_broken_file_links_in_assignments.rb
+++ lib/data_fixup/fix_broken_file_links_in_assignments.rb
@@ -1,8 +1,13 @@
 module DataFixup::FixBrokenFileLinksInAssignments
-  def self.run
+  
+  def self.broken_assignment_scope
     Assignment.where(["context_type = ? AND updated_at > ? AND
       (description LIKE ? OR description LIKE ? OR description LIKE ?)",
-      "Course", "2013-03-08", "%verifier%", "%'/files/", "%\"/files/"]).find_in_batches do |assignments|
+      "Course", "2013-03-08", "%verifier%", "%'/files/%", "%\"/files/%"])
+  end
+  
+  def self.run
+    broken_assignment_scope.find_in_batches do |assignments|
       # This date is the date that g/16823 was deployed to beta and production
       assignments.each do |assignment|
         check_and_fix_assignment_description(assignment)
@@ -13,6 +18,7 @@ module DataFixup::FixBrokenFileLinksInAssignments
   COURSE_FILES_REGEX = %r{\A/courses/\d+/files/(\d+)/download}
   FILES_REGEX = %r{\A/files/(\d+)/download}
 
+  # see spec for examples of how the urls are changed
   def self.check_and_fix_assignment_description(assignment)
     attrs = ['href', 'src']
     changed = false
@@ -32,13 +38,15 @@ module DataFixup::FixBrokenFileLinksInAssignments
 
           if file_id
             if att = Attachment.find_by_id(file_id)
-              att = att.context.attachments.find(att.id)
+              # this find returns the passed in att if nothing found in the context
+              # and sometimes URI.unescape errors so ignore that
+              att = att.context.attachments.find(att.id) rescue att
               if att.context_type == "Course" && att.context_id == assignment.context_id
                 course_id = assignment.context_id
                 file_id = att.id
               elsif att.cloned_item_id && cloned_att = assignment.context.attachments.find_by_cloned_item_id(att.cloned_item_id)
                 course_id = assignment.context_id
-                file_id = assignment.context.attachments.find(cloned_att.id).id
+                file_id = assignment.context.attachments.find(cloned_att.id).id rescue cloned_att.id
               elsif att.context_type == "Course"
                 course_id = att.context_id
               end
@@ -60,7 +68,11 @@ module DataFixup::FixBrokenFileLinksInAssignments
 
     if changed
       assignment.description = doc.at_css('body').inner_html
-      assignment.save_without_broadcasting!
+      if !assignment.save_without_broadcasting
+        Rails.logger.error "FixBrokenFileLinksInAssignments couldn't fix #{assignment.id}"
+      end
     end
+  rescue 
+    Rails.logger.error "FixBrokenFileLinksInAssignments couldn't fix #{assignment.id}"
   end
 end
