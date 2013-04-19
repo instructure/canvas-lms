8e9599757e86a3191594492dbc7d0712ca5e2162
Cody Cutrer <cody@instructure.com>
2013-04-19 16:22:21 -0600

    migration_lint!
    Change-Id: I0c0e12e0aa6401e15cf52915f1fdebe82686a0bf
    Reviewed-on: https://gerrit.instructure.com/19837
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20111020191436_translate_links_on_assessment_questions.rb
+++ db/migrate/20111020191436_translate_links_on_assessment_questions.rb
@@ -1,7 +1,9 @@
 class TranslateLinksOnAssessmentQuestions < ActiveRecord::Migration
   def self.up
     AssessmentQuestion.find_in_batches(:conditions => "question_data LIKE '%/files/%'") do |batch|
-      AssessmentQuestion.send_later_enqueue_args(:translate_links, { :priority => Delayed::LOWER_PRIORITY, :max_attempts => 1, :strand => 'mass_translate_links_migration' }, batch.map(&:id))
+      AssessmentQuestion.send(:with_exclusive_scope) do
+        AssessmentQuestion.send_later_if_production_enqueue_args(:translate_links, { :priority => Delayed::LOWER_PRIORITY, :max_attempts => 1, :strand => 'mass_translate_links_migration' }, batch.map(&:id))
+      end
     end
   end
 
