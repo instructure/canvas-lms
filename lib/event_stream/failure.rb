261e983f61150a9be53f0e0d559a68db5d363b7c
Jacob Fugal <jacob@instructure.com>
2013-04-23 13:29:49 -0600

    capture failed EventStream operations in DB
    Create an event_stream_failures table, and add to it when an EventStream
    operation intended for cassandra fails. intentionally swallows the
    error, so the code inserting/updating the event stream doesn't notice
    and bail on the failure, but we can keep an eye our for new rows and
    also have enough data to replay those failures once fixed.
    
    fixes CNVS-4161
    
    test-plan:
     - enable cassandra page views but intentionally misconfigure so that
       inserts into cassandra fail
     - generate traffic in canvas that should create page views
       - the browsing user should not notice any errors
       - but page views should stay empty
       - event_stream_failures should be getting new rows
    
    Change-Id: I1acabfef785ee0637b241924c6ff254c41d3d1e3
    Reviewed-on: https://gerrit.instructure.com/19975
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>

---
+++ lib/event_stream/failure.rb
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2013 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+class EventStream::Failure < ActiveRecord::Base
+  set_table_name :event_stream_failures
+
+  serialize :payload, Hash
+  serialize :backtrace, Array
+
+  def self.log!(operation, stream, record, exception)
+    create!(:operation => operation.to_s,
+            :event_stream => stream.identifier,
+            :record_id => record.id.to_s,
+            :payload => stream.operation_payload(operation, record),
+            :exception => exception.message.to_s,
+            :backtrace => exception.backtrace)
+  end
+end
