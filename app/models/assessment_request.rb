b1751ef05cc9adc002eaf117b1f9d510364b603a
Zach Pendleton <zachp@instructure.com>
2013-05-01 17:13:54 -0600

    only send reminder notifications for peer reviews.
    fixes CNVS-5532
    
    test plan:
      * create at least two students in a course with immediate
        notification preferences;
      * create a new assignment in their class with
        automatically assigned peer reviews and online
        submission;
      * create a rubric for the assignment;
      * as each of the two students, submit the assignment;
      * wait for the assignment due date to pass, then check
        each students' messages. verify that each has received
        one notification about their peer reviews.
    
    Change-Id: I2ef5be7fedfa775e8ed60aa80545c34e88a53623
    Reviewed-on: https://gerrit.instructure.com/20321
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Mark Ericksen <marke@instructure.com>
    Product-Review: Zach Pendleton <zachp@instructure.com>
    QA-Review: Zach Pendleton <zachp@instructure.com>

--- app/models/assessment_request.rb
+++ app/models/assessment_request.rb
@@ -28,79 +28,66 @@ class AssessmentRequest < ActiveRecord::Base
   has_many :submission_comments
   belongs_to :rubric_assessment
   validates_length_of :comments, :maximum => maximum_text_length, :allow_nil => true, :allow_blank => true
-  
+
   before_save :infer_uuid
   has_a_broadcast_policy
-  
+
   def infer_uuid
     self.uuid ||= AutoHandle.generate_securish_uuid
   end
   protected :infer_uuid
-  
+
   set_broadcast_policy do |p|
     p.dispatch :rubric_assessment_submission_reminder
     p.to { self.user }
-    p.whenever {|record|
-      record.assigned? && (!record.just_created || (@send_reminder && !@send_invitation))
-    }
-
-    p.dispatch :rubric_assessment_invitation
-    p.to { self.assessor }
-    p.whenever {|record|
-      record.assigned? && (record.just_created || @send_invitation)
+    p.whenever { |record|
+      record.assigned? && @send_reminder
     }
   end
-  
+
   scope :incomplete, where(:workflow_state => 'assigned')
   scope :for_assessee, lambda { |user_id| where(:user_id => user_id) }
 
-  def send_invitation!
-    @send_invitation = true
-    self.save!
-    @send_invitation = nil
-    true
-  end
-  
   def send_reminder!
     @send_reminder = true
     self.updated_at = Time.now
     self.save!
+  ensure
     @send_reminder = nil
-    true
   end
 
   def context
     submission.try(:context)
   end
-  
+
   def assessor_name
     self.rubric_assessment.assessor_name rescue ((self.assessor.name rescue nil) || t("#unknown", "Unknown"))
   end
-  
+
   workflow do
     state :assigned do
       event :complete, :transitions_to => :completed
     end
-    
+
     # assessment request now has rubric_assessment
     state :completed
   end
-  
+
   def asset_title
     (self.asset.assignment.title rescue self.asset.title) rescue t("#unknown", "Unknown")
   end
-  
+
   def comment_added(comment)
     self.workflow_state = "completed" unless self.rubric_association && self.rubric_association.rubric
   end
-  
+
   def asset_user_name
     self.asset.user.name rescue t("#unknown", "Unknown")
   end
-  
+
   def asset_context_name
     (self.asset.context.name rescue self.asset.assignment.context.name) rescue t("#unknown", "Unknown")
   end
-  
+
   def self.serialization_excludes; [:uuid]; end
 end
---
+++ spec/models/assessment_request_spec.rb
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2011 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+
+describe AssessmentRequest do
+  describe "workflow" do
+    let(:request) { AssessmentRequest.create! }
+
+    it "defaults to assigned" do
+      request.should be_assigned
+    end
+
+    it "can be completed" do
+      request.complete!
+      request.should be_completed
+    end
+  end
+
+  describe "notifications" do
+    let(:notification_name) { 'Rubric Assessment Submission Reminder' }
+    let(:notification)      { Notification.create!(:name => notification_name, :category => 'Invitation') }
+
+    it "should send submission reminders" do
+      course_with_student(:active_all => true)
+      @student.communication_channels.create!(:path => 'test@example.com').confirm!
+      NotificationPolicy.create!(:notification => notification,
+        :communication_channel => @user.communication_channel, :frequency => 'immediately')
+
+      request = AssessmentRequest.new(:user => @user)
+      request.send_reminder!
+
+      request.messages_sent.keys.should include(notification_name)
+    end
+  end
+end
