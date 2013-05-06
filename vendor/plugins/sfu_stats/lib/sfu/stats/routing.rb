5cbd9ff7fec6a2d9b963d853aa546fee36d1611a
Graham Ballantyne <grahamb@sfu.ca>
2013-05-06 14:09:47 -0700

    add enrollments route

--- vendor/plugins/sfu_stats/lib/sfu/stats/routing.rb
+++ vendor/plugins/sfu_stats/lib/sfu/stats/routing.rb
@@ -7,6 +7,7 @@ module SFU #:nodoc:
           @set.add_route("/sfu/stats", {:controller => "stats", :action => "index"})
           @set.add_route("/sfu/stats/restricted", {:controller => "stats", :action => "restricted"})
           @set.add_route("/sfu/stats/courses/:term_id.:format", {:controller => "stats", :action => "courses"})
+          @set.add_route("/sfu/stats/enrollments/:term_id/:enrollment_type.:format", {:controller => "stats", :action => "enrollments"})
         end
 
        end
