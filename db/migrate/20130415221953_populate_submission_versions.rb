859fae77b3b4bcb118451009a77cda60716fde3d
Brian Palmer <brianp@instructure.com>
2013-05-08 16:40:13 -0600

    tweak job settings on the long-running populate_submission_versions
    Change-Id: Ie9a8186d1430d4d85452facb00e4ed6fca136107
    Reviewed-on: https://gerrit.instructure.com/20503
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

--- db/migrate/20130415221953_populate_submission_versions.rb
+++ db/migrate/20130415221953_populate_submission_versions.rb
@@ -2,6 +2,7 @@ class PopulateSubmissionVersions < ActiveRecord::Migration
   tag :postdeploy
 
   def self.up
-    DataFixup::PopulateSubmissionVersions.send_later_if_production(:run)
+    DataFixup::PopulateSubmissionVersions.send_later_if_production_enqueue_args(:run,
+      :priority => Delayed::LOWER_PRIORITY, :max_attempts => 1, :n_strand => 'long_datafixups')
   end
 end
