b70cc5438f87f4e321e573c453ef9802a5dfd204
Ethan Vizitei <ethan@12spokes.com>
2013-05-29 15:43:34 -0500

    speedgrader nav links
    FIXES CNVS-5990
    FIXES CNVS-5991
    
    This adds the quiz question links to the
    speed grader form and handles the
    scrolling and clicking to make it function
    usefully.  This also persists the 
    active question between students
    
    I also refactored a bit of the quiz_history file.
    
    TEST PLAN:
    
    1) Login as a teacher.
    2) grade a quiz with speed grader.
    3) as you scroll down through questions,
    the nav numbers at the top should highlight
    as you go over the relevant questions.
    4) if you click a number in the nav, it
    should pleasantly scroll to the question you want.
    5) no speed grader functionality should be broken.
    
    Change-Id: I0b0b6e60138f08ce4bfec45a5a1a1fe10f4d1847
    Reviewed-on: https://gerrit.instructure.com/21024
    QA-Review: Myller de Araujo <myller@instructure.com>
    Reviewed-by: Stanley Stuart <stanley@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Ethan Vizitei <ethan@12spokes.com>

--- public/javascripts/quiz_history.js
+++ public/javascripts/quiz_history.js
@@ -6,7 +6,40 @@ define([
   'compiled/behaviors/quiz_selectmenu'
 ], function($) {
 
+  var parentWindow = {
+    exists: function(){
+      return (window.parent && window.parent.INST);
+    },
+
+    respondsTo: function(funcName){
+      return (parentWindow.exists() && $.isFunction(window.parent.INST[funcName]));
+    },
+
+    hasProperty: function(propName){
+      return (parentWindow.exists() && window.parent.INST[propName]);
+    },
+
+    set: function(propName, value){
+      if(parentWindow.exists()){
+        window.parent.INST[propName] = value;
+      }
+    },
+
+    get: function(propName){
+      if(parentWindow.hasProperty(propName)){
+        return window.parent.INST[propName];
+      }
+    }
+
+  };
+  // end parentWindow object
+
+
+
   var data = $("#submission_details").getTemplateData({textValues: ['version_number', 'user_id']});
+
+
+
   var scoringSnapshot = {
     snapshot: {
       user_id: parseInt(data.user_id, 10) || null,
@@ -15,14 +48,21 @@ define([
       question_updates: {},
       fudge_points: 0
     },
-    getSnapshot: function() {
-      return scoringSnapshot.snapshot;
-    },
+
+    $quizBody: null,
+
     jumpToQuestion: function(question_id) {
-      var top = $("#question_" + question_id).offset().top - 10;
-      $("html,body").scrollTo({top: top, left:0});
+      var top = $("#question_" + question_id).offset().top - 180;
+      if(scoringSnapshot.$quizBody === null){
+        scoringSnapshot.$quizBody = $('html,body');
+      }
+      scoringSnapshot.$quizBody.stop();
+      scoringSnapshot.$quizBody.clearQueue();
+      scoringSnapshot.$quizBody.animate({scrollTop: top}, 500);
     },
+
     externallySet: false,
+
     setSnapshot: function(data, cancelIfAlreadyExternallySet) {
       if(data) {
         if(cancelIfAlreadyExternallySet && scoringSnapshot.externallySet) { return; }
@@ -35,66 +75,78 @@ define([
           $question.find(".user_points :text").val(question.points).end()
             .find(".question_neutral_comment .question_comment_text textarea").val(question.comments);
         }
-        if(window.parent && window.parent.INST && window.parent.INST.lastQuestionTouched) {
+        if(parentWindow.hasProperty('lastQuestionTouched')) {
           scoringSnapshot.jumpToQuestion(window.parent.INST.lastQuestionTouched);
         } else if(scoringSnapshot.snapshot.last_question_touched) {
           scoringSnapshot.jumpToQuestion(scoringSnapshot.snapshot.last_question_touched);
         }
       } else if(cancelIfAlreadyExternallySet) {
-        if(window.parent && window.parent.INST && window.parent.INST.lastQuestionTouched) {
+        if(parentWindow.hasProperty('lastQuestionTouched')) {
           scoringSnapshot.jumpToQuestion(window.parent.INST.lastQuestionTouched);
         }
       }
       if(scoringSnapshot.externallySet || cancelIfAlreadyExternallySet) {
         $("#feel_free_to_toggle_message").show();
       }
-      if(window.parent && window.parent.INST && window.parent.INST.refreshQuizSubmissionSnapshot && $.isFunction(window.parent.INST.refreshQuizSubmissionSnapshot)) {
+      if(parentWindow.respondsTo('refreshQuizSubmissionSnapshot')) {
         window.parent.INST.refreshQuizSubmissionSnapshot(scoringSnapshot.snapshot);
       }
+    },
+
+    update: function(question_id, data){
+      scoringSnapshot.snapshot.question_updates[question_id] = data;
+      scoringSnapshot.snapshot.last_question_touched = question_id;
+      scoringSnapshot.setSnapshot();
     }
   }
-  $(document).ready(function() {
-    $(":text").focus(function() {
-      $(this).select();
-    });
-    $(document).fragmentChange(function(event, hash) {
+  //end of scoringSnapshot object
+
+
+
+  var gradingForm = {
+    ensureSelectEventsFire: function(){
+      $("input[type=text]").focus(function() {
+        $(this).select();
+      });
+    },
+
+    scrollToUpdatedQuestion: function(event, hash) {
       if(hash.indexOf("#question") == 0) {
         var id = hash.substring(10);
         scoringSnapshot.jumpToQuestion(id);
       }
-    });
-    if(window.parent && window.parent.INST && window.parent.INST.getQuizSubmissionSnapshot && $.isFunction(window.parent.INST.getQuizSubmissionSnapshot)) {
-      $("#feel_free_to_toggle_message").show();
-      var data = window.parent.INST.getQuizSubmissionSnapshot(scoringSnapshot.snapshot.user_id, scoringSnapshot.snapshot.version_number);
-      if(data) {
-        scoringSnapshot.setSnapshot(data);
-      } else {
-        scoringSnapshot.setSnapshot(null, true);
-      }
-    }
-    $(".question_holder .user_points :text,.question_holder .question_neutral_comment .question_comment_text textarea").change(function() {
-      var $question = $(this).parents(".display_question");
+    },
+
+    updateSnapshotFor: function($question){
       var question_id = parseInt($question.attr('id').substring(9), 10) || null;
       if(question_id) {
         var data = {};
         $question.addClass('modified_but_not_saved');
         data.points = parseFloat($question.find(".user_points :text").val(), 10);
         data.comments = $question.find(".question_neutral_comment .question_comment_text textarea").val() || "";
-        scoringSnapshot.snapshot.question_updates[question_id] = data;
-        scoringSnapshot.snapshot.last_question_touched = question_id;
-        scoringSnapshot.setSnapshot();
+        scoringSnapshot.update(question_id, data);
       }
       $(document).triggerHandler('score_changed');
-    });
-    $("#fudge_points_entry").change(function() {
-      var points = parseFloat($(this).val(), 10);
+    },
+
+    addFudgePoints: function(points){
       if(points || points === 0) {
         scoringSnapshot.snapshot.fudge_points = points;
         scoringSnapshot.setSnapshot();
       }
       $(document).triggerHandler('score_changed');
-    });
-    $(document).bind('score_changed', function() {
+    },
+
+    setInitialSnapshot: function(data){
+      $("#feel_free_to_toggle_message").show();
+      if(data) {
+        scoringSnapshot.setSnapshot(data);
+      } else {
+        scoringSnapshot.setSnapshot(null, true);
+      }
+    },
+
+    onScoreChanged: function(){
       var $total = $("#after_fudge_points_total");
       var total = 0;
       $(".display_question .user_points:visible").each(function() {
@@ -106,18 +158,260 @@ define([
       fudge = Math.round(fudge * 100.0) / 100.0;
       total = total + fudge;
       $total.text(total || "0");
+    },
+
+    questions: function(){
+      return $('.question_holder').map(function(index, el) {
+        return $(el).position().top - 320;
+      }).toArray();
+    },
+
+    onScroll: function(){
+      var qNum = quizNavBar.activateCorrectLink();
+      quizNavBar.toggleDropShadow();
+    }
+
+  };
+  //end of gradingForm object
+
+
+
+  var quizNavBar = {
+    index: 0,
+    windowSize: 10,
+    minWidth: 66,
+    startingLeftPos: 32,
+    navItemWidth: 34,
+
+    initialize: function(){
+      $('.user_points > .question_input').each(function(index){
+        quizNavBar.updateStatusFor($(this));
+        var questionId = parseInt(parentWindow.get('active_question_id'));
+        if(!isNaN(questionId)){
+          scoringSnapshot.jumpToQuestion(questionId);
+        }
+      });
+    },
+
+    size: function(){
+      return $('.question-nav-link').length;
+    },
+
+    tooBig: function(){
+      return quizNavBar.size() > quizNavBar.windowSize;
+    },
+
+    updateWindowSize: function(){
+      var fullWidth = $('.quiz-nav, .quiz-nav-fullpage').width();
+      var minPadding = 10;
+      var maxWidth = fullWidth - (minPadding * 2);
+      var itemCount = Math.floor((maxWidth - quizNavBar.minWidth) / quizNavBar.navItemWidth);
+      quizNavBar.windowSize = itemCount;
+      var actualWidth = (itemCount * quizNavBar.navItemWidth) + quizNavBar.minWidth;
+      $('.quiz-nav .nav, .quiz-nav-fullpage .nav').animate({width: actualWidth + 'px'}, 10);
+    },
+
+    navArrowCache: null,
+
+    $navArrows: function(){
+      if(quizNavBar.navArrowCache === null){
+        quizNavBar.navArrowCache = $('.quiz-nav .nav-arrow, .quiz-nav-fullpage .nav-arrow');
+      }
+      return quizNavBar.navArrowCache;
+    },
+
+    navWrapperCache: null,
+
+    $navWrapper: function(){
+      if(quizNavBar.navWrapperCache === null){
+        quizNavBar.navWrapperCache = $('#quiz-nav-inner-wrapper');
+      }
+      return quizNavBar.navWrapperCache;
+    },
+
+    updateArrows: function(){
+      if(quizNavBar.tooBig()){
+        quizNavBar.$navArrows().show();
+        quizNavBar.$navWrapper().css({position: 'absolute'});
+      } else {
+        quizNavBar.$navArrows().hide();
+        quizNavBar.$navWrapper().css({position: 'relative'});
+      }
+    },
+
+    toggleDropShadow: function(){
+      //Add shadow to top bar
+      $('.quiz-nav').toggleClass('drshadow', ($(document).scrollTop() > 0));
+    },
+
+    updateStatusFor: function($scoreInput){
+      try{
+        var questionId = $scoreInput.attr('name').split('_')[2];
+        var scoreValue = $scoreInput.val();
+        $('#quiz_nav_' + questionId).toggleClass('complete', (!isNaN(parseFloat(scoreValue))));
+      } catch(err) {
+        // do nothing; if there's no status to update, continue with other execution
+      }
+    },
+
+    activateLink: function(index){
+      $('.quiz-nav li').removeClass('active');
+      $('.q' + index).addClass('active');
+    },
+
+    activateCorrectLink: function(){
+      var qNum = 1;
+      var qArray = gradingForm.questions();
+      var docScroll = $(document).scrollTop();
+      var maxScroll = $(document).height() - $('body').height();
+      if(docScroll >= maxScroll){
+        qNum = qArray.length;
+        quizNavBar.activateLink(qNum);
+      }else{
+        for(var t = 0; t <= qArray.length; t++) {
+          if ( (docScroll > qArray[t] && docScroll < qArray[t+1])  || ( t == (qArray.length - 1) && docScroll > qArray[t])) {
+            qNum = t + 1;
+            var questionId = $($('.question')[t]).attr('id');
+            if(questionId !== undefined){
+              questionId.split('_')[1];
+              parentWindow.set('active_question_id', questionId);
+              quizNavBar.activateLink(qNum);
+            }
+          } else {
+            $('.q'+ (t + 1)).removeClass('active');
+          }
+        }
+      }
+      quizNavBar.setScrollWindowPosition(qNum);
+      return qNum;
+    },
+
+    showQuestionsInWindow: function(startingIndex, endingIndex){
+      var $navWrapper = $('#quiz-nav-inner-wrapper');
+      var leftPosition = quizNavBar.startingLeftPos - (startingIndex * quizNavBar.navItemWidth);
+      var newPos = '' + leftPosition + 'px';
+      var currentPos = $navWrapper.css('left');
+      if(newPos !== currentPos){
+        $navWrapper.stop();
+        $navWrapper.clearQueue();
+        $navWrapper.animate({left: leftPosition + 'px'}, 300);
+      }
+    },
+
+    windowScrollLength: function(){
+      return Math.floor(quizNavBar.windowSize/2.0);
+    },
+
+    setScrollWindowPosition: function(currentIndex){
+      if(isNaN(currentIndex)){
+        currentIndex = 0;
+      }
+      quizNavBar.index = currentIndex;
+      quizNavBar.updateArrows();
+      if(quizNavBar.tooBig()){
+        var startingIndex = currentIndex - quizNavBar.windowScrollLength();
+        var maxStartingIndex = quizNavBar.size() - quizNavBar.windowSize;
+
+        if(startingIndex < 0){
+          startingIndex = 0;
+          quizNavBar.index = 0;
+        }else if(startingIndex > maxStartingIndex){
+          startingIndex = maxStartingIndex;
+          quizNavBar.index = maxStartingIndex + quizNavBar.windowScrollLength();
+        }
+
+        endingIndex = startingIndex + quizNavBar.windowSize - 1;
+        quizNavBar.showQuestionsInWindow(startingIndex, endingIndex);
+      }
+    },
+
+    previousQuestionBlock: function(){
+      quizNavBar.setScrollWindowPosition(quizNavBar.index - quizNavBar.windowSize);
+    },
+
+    nextQuestionBlock: function(){
+      quizNavBar.setScrollWindowPosition(quizNavBar.index + quizNavBar.windowSize);
+    }
+  };
+  //End of quizNavBar object
+
+
+  $(document).ready(function() {
+    gradingForm.ensureSelectEventsFire();
+    quizNavBar.initialize();
+    $(document).fragmentChange( gradingForm.scrollToUpdatedQuestion );
+
+    if(parentWindow.respondsTo('getQuizSubmissionSnapshot')) {
+      var data = window.parent.INST.getQuizSubmissionSnapshot(scoringSnapshot.snapshot.user_id, scoringSnapshot.snapshot.version_number);
+      gradingForm.setInitialSnapshot(data);
+    }
+
+    $(".question_holder .user_points :text,.question_holder .question_neutral_comment .question_comment_text textarea").change(function() {
+      var $question = $(this).parents(".display_question");
+      gradingForm.updateSnapshotFor($question);
+      if($(this).hasClass('question_input')){
+        quizNavBar.updateStatusFor($(this));
+      }
     });
+
+    $("#fudge_points_entry").change(function() {
+      var points = parseFloat($(this).val(), 10);
+      gradingForm.addFudgePoints(points);
+    });
+
+    $(document).bind('score_changed', gradingForm.onScoreChanged);
+
+    $('.question-nav-link').click(function(e) {
+      e.preventDefault();
+      var questionId = $(this).attr('data-id');
+      scoringSnapshot.jumpToQuestion(questionId);
+    });
+
+    $('#nav-prev').click(function(e){
+      e.preventDefault();
+      quizNavBar.previousQuestionBlock();
+    });
+
+    $('#nav-next').click(function(e){
+      e.preventDefault();
+      quizNavBar.nextQuestionBlock();
+    });
+
+    $(document).scroll(gradingForm.onScroll);
+
+    //Add padding to the bottom of the last question
+    var winHeight = $(window).innerHeight();
+    var lastHeight = $('div.question_holder:last-child').outerHeight();
+    var paddingHeight = (winHeight - lastHeight - 110);
+    $('#update_history_form .button-container').css('marginBottom', paddingHeight + 'px');
+
+
+    $('.question_holder').click(function() {
+      $('.quiz-nav li').removeClass('active');
+      var qId = $(this).find('.question').attr('id').split('_')[1];
+      parentWindow.set('active_question_id', qId);
+      $('#quiz_nav_' + qId).addClass('active');
+    });
+
+    $(window).resize(function () {
+      quizNavBar.updateWindowSize();
+      quizNavBar.setScrollWindowPosition(quizNavBar.index);
+    });
+
+    quizNavBar.updateWindowSize();
+    quizNavBar.setScrollWindowPosition(0);
   });
 
   if (ENV.SCORE_UPDATED) {
     $(document).ready(function() {
-      if(window.parent && window.parent.INST && window.parent.INST.refreshGrades && $.isFunction(window.parent.INST.refreshGrades)) {
+      if(parentWindow.respondsTo('refreshGrades')) {
         window.parent.INST.refreshGrades();
       }
-      if(window.parent && window.parent.INST && window.parent.INST.clearQuizSubmissionSnapshot && $.isFunction(window.parent.INST.clearQuizSubmissionSnapshot)) {
+      if(parentWindow.respondsTo('clearQuizSubmissionSnapshot')) {
         window.parent.INST.clearQuizSubmissionSnapshot(scoringSnapshot.snapshot);
       }
     });
   }
+
 });
 
