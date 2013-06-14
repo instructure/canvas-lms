f9109c18c9c1432f20bb68aec5e0134615db4197
James Williams <jamesw@instructure.com>
2013-06-14 16:09:58 -0600

    fix imported wiki pages that were set to unpublished
    data fixup for wiki pages that were imported during a
    certain period when they were set to unpublished by default
    but linked to active module content items
    
    test plan:
     (Note: the problem should no longer occur, so we will have
     to manually re-create the problem)
    
    * create a wiki page
    * add the page as a module item
    * identify the item id by observing the module item's url
     on the modules page -
     e.g. (/courses/:course_id/module/items/:item_id)
    
    * open the console and run the following instructions:
    
    id = [INSERT THE ITEM ID HERE]
    
    WikiPage.where(:id => ContentTag.find(id).content_id
    ).update_all(:workflow_state => 'unpublished')
    
    * now, as a student, clicking the link should give an
     'unauthorized' message, as in #CNVS-6177
    
    * run the data fixup by running the following command in
    the console:
    
    DataFixup::FixImportedWikiPageWorkflow.run
    
    * now the page should be accessible by students
    
    closes #CNVS-6323
    
    Change-Id: Iffb74de96cb4d3603ccd8a6ecb55567714d95db4
    Reviewed-on: https://gerrit.instructure.com/21513
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: Clare Strong <clare@instructure.com>

---
+++ lib/data_fixup/fix_imported_wiki_page_workflow.rb
@@ -0,0 +1,14 @@
+module DataFixup::FixImportedWikiPageWorkflow
+  # finds unpublished wiki pages that are linked to active content_tags
+  def self.broken_wiki_page_scope
+    WikiPage.joins("INNER JOIN content_tags ON content_tags.content_id = wiki_pages.id"
+    ).where(["content_tags.content_type = ? AND content_tags.workflow_state = ? AND
+      wiki_pages.workflow_state = ?", "WikiPage", "active", "unpublished"])
+  end
+
+  def self.run
+    self.broken_wiki_page_scope.find_in_batches do |wiki_pages|
+      WikiPage.where(:id => wiki_pages).update_all(:workflow_state => 'active')
+    end
+  end
+end
