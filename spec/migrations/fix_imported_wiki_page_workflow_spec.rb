e6fa4abc4b69cf615299d0b4840020ba53bda458
James Williams <jamesw@instructure.com>
2013-06-14 16:09:58 -0600

    fix imported wiki pages that were set to unpublished
    data fixup for wiki pages that were imported during a
    certain period when they were set to unpublished by default
    but linked to active module content items
    
    test plan:
     (Note: the problem should no longer occur, so we will have
     to manually re-create the problem)
    
    * create a wiki page
    * add the page as a module item
    * identify the item id by observing the module item's url
     on the modules page -
     e.g. (/courses/:course_id/module/items/:item_id)
    
    * open the console and run the following instructions:
    
    id = [INSERT THE ITEM ID HERE]
    
    WikiPage.where(:id => ContentTag.find(id).content_id
    ).update_all(:workflow_state => 'unpublished')
    
    * now, as a student, clicking the link should give an
     'unauthorized' message, as in #CNVS-6177
    
    * run the data fixup by running the following command in
    the console:
    
    DataFixup::FixImportedWikiPageWorkflow.run
    
    * now the page should be accessible by students
    
    closes #CNVS-6323
    
    Change-Id: Iffb74de96cb4d3603ccd8a6ecb55567714d95db4
    Reviewed-on: https://gerrit.instructure.com/21513
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: Clare Strong <clare@instructure.com>

---
+++ spec/migrations/fix_imported_wiki_page_workflow_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+require 'db/migrate/20130617152008_fix_imported_wiki_page_workflow.rb'
+
+describe 'DataFixup::FixImportedWikiPageWorkflow' do
+  before :all do
+    course
+    @wiki_pages = []
+    5.times do |n|
+      mod = @course.context_modules.create!(:name => "module")
+      page = @course.wiki.wiki_pages.create!(:title => "wiki page #{n}", :body => "whatevaa")
+      mod.add_item(:id => page.id, :type => 'wiki_page')
+      @wiki_pages << page
+
+      dummy_page = @course.wiki.wiki_pages.create!(:title => "ignore this wiki page #{n}", :body => "whatevaa")
+      mod.add_item(:id => dummy_page.id, :type => 'wiki_page')
+    end
+    WikiPage.where(:id => @wiki_pages).update_all(:workflow_state => 'unpublished')
+  end
+
+  it "should find unpublished wiki pages" do
+    DataFixup::FixImportedWikiPageWorkflow.broken_wiki_page_scope.map(&:id).sort.should == @wiki_pages.map(&:id).sort
+  end
+
+  it "should publish the broken wiki pages" do
+    FixImportedWikiPageWorkflow.up
+    @wiki_pages.each do |page|
+      page.reload
+      page.workflow_state.should == 'active'
+    end
+    DataFixup::FixImportedWikiPageWorkflow.broken_wiki_page_scope.count.should == 0
+  end
+end
