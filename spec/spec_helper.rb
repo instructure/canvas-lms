cd3b30b8169a4d5cdc4040239c854db40f494261
James Williams <jamesw@instructure.com>
2013-06-19 08:48:01 -0600

    fix media_object caption permissions
    test plan:
    * upload a video file
    * identify the attachment id from the file's download url
     (e.g. '/courses/:course_id/files/:attachment_id/download')
    
    * set the media object's user_id to nil by
     entering the following commands into the console:
    
    id = [[INSERT ATTACHMENT ID HERE]]
    mo = Attachment.find(id).media_object
    mo.user = nil
    mo.save!
    
    * confirm that, as any course admin user, the 'CC'
    button appears when previewing the video
    
    fixes #CNVS-3855
    
    Change-Id: I00b8ec4de50850cffee7be1332d811909d85932a
    Reviewed-on: https://gerrit.instructure.com/21596
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -689,9 +689,10 @@ Spec::Runner.configure do |config|
     mo = MediaObject.new
     mo.media_id = opts[:media_id] || "1234"
     mo.media_type = opts[:media_type] || "video"
-    mo.context = opts[:context] || @user || @course
+    mo.context = opts[:context] || @course
     mo.user = opts[:user] || @user
     mo.save!
+    mo
   end
 
   def message(opts={})
