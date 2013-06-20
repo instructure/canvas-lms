e0e04c0703ff1d63afbc1fa86ba4b0511c4e1523
James Williams <jamesw@instructure.com>
2013-06-20 07:59:45 -0600

    fix FixHiddenWikiFrontPages migration
    Change-Id: I6ce491599c3bbd2ebdd330c468ac0e52ba1a47d4
    Reviewed-on: https://gerrit.instructure.com/21615
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

--- db/migrate/20130516204101_fix_hidden_wiki_front_pages.rb
+++ db/migrate/20130516204101_fix_hidden_wiki_front_pages.rb
@@ -2,7 +2,10 @@ class FixHiddenWikiFrontPages < ActiveRecord::Migration
   tag :postdeploy
 
   def self.up
-    WikiPage.where(:url => 'front-page', :hide_from_students => true).update_all(:hide_from_students => false)
+    WikiPage.find_ids_in_ranges do |first, last|
+      WikiPage.where("url = ? AND hide_from_students = ? AND id >= ? AND id <= ?", 'front-page', true, first, last).
+          update_all(:hide_from_students => false)
+    end
   end
 
   def self.down
