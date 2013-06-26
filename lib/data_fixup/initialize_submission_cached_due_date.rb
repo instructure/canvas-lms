e1b69f4153f47b89b5d4a46382ef5820f513997e
Cody Cutrer <cody@instructure.com>
2013-06-26 10:58:27 -0600

    fix DueDateCacher range creation
    Change-Id: I8ca00d0d96acfeab5f0e9c79231866bed941c2a2
    Reviewed-on: https://gerrit.instructure.com/21756
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/data_fixup/initialize_submission_cached_due_date.rb
+++ lib/data_fixup/initialize_submission_cached_due_date.rb
@@ -1,8 +1,8 @@
 module DataFixup::InitializeSubmissionCachedDueDate
   def self.run
-    Assignment.find_ids_in_ranges do |(min, max)|
+    Assignment.find_ids_in_ranges do |min, max|
       Assignment.send(:with_exclusive_scope) do
-        DueDateCacher.recompute_batch(min..max)
+        DueDateCacher.recompute_batch(min.to_i..max.to_i)
       end
     end
   end
