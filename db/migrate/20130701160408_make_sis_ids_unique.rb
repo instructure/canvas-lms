8690a7ac77f9e06f37ba820b6ef78e05e0759805
Cody Cutrer <cody@instructure.com>
2013-07-01 14:00:50 -0600

    add unique indices to enforce uniqueness of sis id
    refs CNVS-6285
    
    test plan:
     * n/a
    
    Change-Id: I5036e522dd3a2318de4314bda8f80da617c2907e
    Reviewed-on: https://gerrit.instructure.com/21903
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

---
+++ db/migrate/20130701160408_make_sis_ids_unique.rb
@@ -0,0 +1,52 @@
+class MakeSisIdsUnique < ActiveRecord::Migration
+  tag :postdeploy
+  self.transactional = false
+
+  def self.up
+    add_index :accounts, [:sis_source_id, :root_account_id], conditions: "sis_source_id IS NOT NULL", unique: true, concurrently: true
+    add_index :accounts, :root_account_id, concurrently: true
+    add_index :courses, [:sis_source_id, :root_account_id], conditions: "sis_source_id IS NOT NULL", unique: true, concurrently: true
+    add_index :course_sections, [:sis_source_id, :root_account_id], conditions: "sis_source_id IS NOT NULL", unique: true, concurrently: true
+    add_index :enrollment_terms, [:sis_source_id, :root_account_id], conditions: "sis_source_id IS NOT NULL", unique: true, concurrently: true
+    add_index :enrollment_terms, :root_account_id, concurrently: true
+    if connection.adapter_name == 'PostgreSQL'
+      concurrently = " CONCURRENTLY" if connection.open_transactions == 0
+      execute "CREATE UNIQUE INDEX#{concurrently} index_pseudonyms_on_unique_id_and_account_id ON pseudonyms (LOWER(unique_id), account_id) WHERE workflow_state='active'"
+      remove_index :pseudonyms, :unique_id
+    end
+    add_index :pseudonyms, [:sis_user_id, :account_id], conditions: "sis_user_id IS NOT NULL", unique: true, concurrently: true
+    add_index :pseudonyms, :account_id, concurrently: true
+    add_index :groups, [:sis_source_id, :root_account_id], conditions: "sis_source_id IS NOT NULL", unique: true, concurrently: true
+
+    remove_index :accounts, :sis_source_id
+    remove_index :accounts, [:root_account_id, :sis_source_id]
+    remove_index :courses, :sis_source_id
+    remove_index :course_sections, [:root_account_id, :sis_source_id]
+    remove_index :enrollment_terms, :sis_source_id
+    remove_index :enrollment_terms, [:root_account_id, :sis_source_id]
+    remove_index :pseudonyms, :sis_user_id
+  end
+
+  def self.down
+    add_index :accounts, :sis_source_id, concurrently: true
+    add_index :accounts, [:root_account_id, :sis_source_id], concurrently: true
+    add_index :courses, :sis_source_id, concurrently: true
+    add_index :course_sections, [:root_account_id, :sis_source_id], concurrently: true
+    add_index :enrollment_terms, :sis_source_id, concurrently: true
+    add_index :enrollment_terms, [:root_account_id, :sis_source_id], concurrently: true
+    add_index :pseudonyms, :sis_user_id, concurrently: true
+
+    remove_index :accounts, [:sis_source_id, :root_account_id]
+    remove_index :accounts, :root_account_id
+    remove_index :courses, [:sis_source_id, :root_account_id]
+    remove_index :course_sections, [:sis_source_id, :root_account_id]
+    remove_index :enrollment_terms, [:sis_source_id, :root_account_id]
+    remove_index :enrollment_terms, :root_account_id
+    if connection.adapter_name == 'PostgreSQL'
+      #remove_index :pseudonyms, [:unique_id, :account_id]
+    end
+    remove_index :pseudonyms, [:sis_user_id, :account_id]
+    remove_index :pseudonyms, :account_id
+    remove_index :groups, [:sis_source_id, :root_account_id]
+  end
+end
