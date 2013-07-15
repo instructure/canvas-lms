e0e55b53be14d574fbb2036d2b0c9c99330581a5
Bryan Madsen <bryan@instructure.com>
2013-07-15 15:07:34 -0600

    spec: add respodus specs to parallel exclude
    Change-Id: I59bed51cb593c6805a1835aa009ecbcb3820d934
    Reviewed-on: https://gerrit.instructure.com/22334
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Product-Review: Bryan Madsen <bryan@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>

--- lib/tasks/parallel_exclude.rb
+++ lib/tasks/parallel_exclude.rb
@@ -1,9 +1,10 @@
 module ParallelExclude
   FILES = [
     "spec/integration/files_spec.rb",
-    "spec/apis/v1/calendar_events_api_spec.rb", #fails intermittently with ruby 1.9.3
+    "spec/apis/v1/calendar_events_api_spec.rb",
     "spec/models/attachment_spec.rb",
-    "spec/lib/content_zipper_spec.rb"
+    "spec/lib/content_zipper_spec.rb",
+    "vendor/plugins/respondus_lockdown_browser/spec_canvas/integration/respondus_ldb_spec.rb"
   ]
 
   test_files = FileList['vendor/plugins/*/spec_canvas/**/*_spec.rb'].exclude('vendor/plugins/*/spec_canvas/selenium/*_spec.rb') + FileList['spec/**/*_spec.rb'].exclude('spec/selenium/**/*_spec.rb')
