a3cf39ebd5f6d326ed53dea796db11c2dc2b6a9f
Graham Ballantyne <grahamb@sfu.ca>
2013-07-18 17:14:39 -0700

    shenanigans to get the root element out of the json response

--- vendor/plugins/sfu_api/app/controllers/term_controller.rb
+++ vendor/plugins/sfu_api/app/controllers/term_controller.rb
@@ -1,18 +1,59 @@
 class TermController < ApplicationController
 
+
+  # get all terms
+  def all_terms
+    ActiveRecord::Base.include_root_in_json = false
+    t = EnrollmentTerm.find(:all, :select => select_fields, :conditions => ["workflow_state = 'active' AND (root_account_id = 2)"])
+    respond_to do |format|
+      format.json {render :json => t}
+    end
+    ActiveRecord::Base.include_root_in_json = true
+  end
+
+  # get specific term by sis id
+  def term_by_sis_id
+    ActiveRecord::Base.include_root_in_json = false
+    t = EnrollmentTerm.find(:all, :select => select_fields, :conditions => ["workflow_state = 'active' AND (root_account_id = 2) AND (sis_source_id = '#{params[:sis_id]}')"])
+    respond_to do |format|
+      format.json {render :json => t}
+    end
+    ActiveRecord::Base.include_root_in_json = true
+  end
+
   # get current term
   def current_term
-    EnrollmentTerm.find(:all, :conditions => ["workflow_state = 'active' AND (:date BETWEEN start_at AND end_at) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}]).first
+    ActiveRecord::Base.include_root_in_json = false
+    t = EnrollmentTerm.find(:all, :select => select_fields, :conditions => ["workflow_state = 'active' AND (root_account_id = 2) AND (:date BETWEEN start_at AND end_at) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}]).first
+    respond_to do |format|
+      format.json {render :json => t}
+    end
+    ActiveRecord::Base.include_root_in_json = true
   end
 
   # get next n term(s)
-  def next_terms(num_terms=1)
-    EnrollmentTerm.find(:all, :conditions => ["workflow_state = 'active' AND (start_at > :date) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}], :order => "sis_source_id", :limit => num_terms)
+  def next_terms
+    ActiveRecord::Base.include_root_in_json = false
+    t = EnrollmentTerm.find(:all, :select => select_fields, :conditions => ["workflow_state = 'active' AND (start_at > :date) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}], :order => "sis_source_id", :limit => params[:num_terms])
+    respond_to do |format|
+      format.json {render :json => t}
+    end
+    ActiveRecord::Base.include_root_in_json = true
   end
 
   # get prev n term(s)
   def prev_terms(num_terms=1)
-    EnrollmentTerm.find(:all, :conditions => ["workflow_state = 'active' AND (end_at < :date) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}], :order => "sis_source_id DESC", :limit => num_terms)
+    ActiveRecord::Base.include_root_in_json = false
+    t = EnrollmentTerm.find(:all, :select => select_fields, :conditions => ["workflow_state = 'active' AND (end_at < :date) AND (sis_source_id IS NOT NULL)", {:date => DateTime.now}], :order => "sis_source_id DESC", :limit => params[:num_terms])
+    respond_to do |format|
+      format.json {render :json => t}
+    end
+    ActiveRecord::Base.include_root_in_json = true
+  end
+
+  private
+  def select_fields
+    "name, sis_source_id, start_at, end_at"
   end
 
 end
