ca9029980699247772e04d16992bd08aef213ea4
Cody Cutrer <cody@instructure.com>
2013-08-01 09:47:39 -0600

    fix uniqueness validation of login ids
    fixes CNVS-7266
    
    test plan:
     * create an SIS user with a login id, then delete them
     * create a non-SIS user with the same login id
     * use an SIS import to resurrect the original user
     * the SIS import should succeed, but warn that it skipped the user
       cause someone else had his login information
    
    Change-Id: I2102107fefb1d20fae6046ebe3a7e0c03416dcc9
    Reviewed-on: https://gerrit.instructure.com/22867
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/models/pseudonym.rb
+++ app/models/pseudonym.rb
@@ -56,7 +56,7 @@ class Pseudonym < ActiveRecord::Base
     config.validations_scope = [:account_id, :workflow_state]
     config.perishable_token_valid_for = 30.minutes
     config.validates_length_of_login_field_options = {:within => 1..MAX_UNIQUE_ID_LENGTH}
-    config.validates_uniqueness_of_login_field_options = { :case_sensitive => false, :scope => [:account_id, :workflow_state], :if => lambda { |p| p.unique_id_changed? && p.active? } }
+    config.validates_uniqueness_of_login_field_options = { :case_sensitive => false, :scope => [:account_id, :workflow_state], :if => lambda { |p| (p.unique_id_changed? || p.workflow_state_changed?) && p.active? } }
   end
 
   attr_writer :require_password
