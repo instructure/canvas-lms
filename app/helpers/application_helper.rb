455a6dec2d72f32683a2912828303b6a0e36a471
James Williams <jamesw@instructure.com>
2013-08-05 07:30:28 -0600

    prevent select db calls in folder_as_options
    fixes #CNVS-7324
    
    Change-Id: I40e4e56affaa73264c2cb41fce617d9dd0f129cb
    Reviewed-on: https://gerrit.instructure.com/22982
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -605,7 +605,7 @@ module ApplicationHelper
       opts[:options_so_far] << %{<option value="#{folder.id}" #{'selected' if opts[:selected_folder_id] == folder.id}>#{"&nbsp;" * opts[:indent_width] * opts[:depth]}#{"- " if opts[:depth] > 0}#{html_escape folder.name}</option>}
       if opts[:max_depth].nil? || opts[:depth] < opts[:max_depth]
         child_folders = if opts[:all_folders]
-                          opts[:all_folders].select {|f| f.parent_folder_id == folder.id }
+                          opts[:all_folders].to_a.select {|f| f.parent_folder_id == folder.id }
                         else
                           folder.active_sub_folders.by_position
                         end
