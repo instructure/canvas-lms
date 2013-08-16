bb62be72c28bbb6faa1e5d8bf7b1ce76c6b6255b
Cody Cutrer <cody@instructure.com>
2013-08-16 16:35:35 -0600

    fix cassandra page view queue drain migration
    migrations are already run once per shard; no need to explode it
    into n^2
    
    Change-Id: Id994105b7ed77602b51f30f2d34f96a16536399c
    Reviewed-on: https://gerrit.instructure.com/23433
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20130814173425_drain_cassandra_page_views_from_redis_queue.rb
+++ db/migrate/20130814173425_drain_cassandra_page_views_from_redis_queue.rb
@@ -8,14 +8,10 @@ class DrainCassandraPageViewsFromRedisQueue < ActiveRecord::Migration
     # cassandra page views don't use a redis cache intermediary anymore, but they
     # did. pull any remaining page views off the cache and insert them into
     # cassandra
-    Shard.with_each_shard do
-      unless Shard.current.settings[:process_page_view_queue] == false
-        PageView.send_later_if_production_enqueue_args(
-          :process_cache_queue,
-          singleton: "PageView.process_cache_queue:#{Shard.current.id}",
-          max_attempts: 1)
-      end
-    end
+    PageView.send_later_if_production_enqueue_args(
+      :process_cache_queue,
+      singleton: "PageView.process_cache_queue:#{Shard.current.id}",
+      max_attempts: 1)
   end
 
   def self.down
