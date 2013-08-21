86464dc7a389cfa6089d3c0ca0d2f709a683d4c2
Brian Palmer <brianp@instructure.com>
2013-08-21 09:03:42 -0600

    user request throttling
    Define the cost of a request as (user cpu + time spent in db queries),
    then store that using a leaky bucket algorithm in redis. The algorithm
    is slightly modified from a normal leaky bucket, see the comments in the
    code.
    
    The parameters (maximum, hwm, outflow) are configurable Settings.
    Because this code path is hit on every request, I've tried to keep the
    added latency to a minimum.
    
    Uses lua scripting in redis to avoid the latency of an extra round trip
    (two round trips with lua, one at the beginning of the request and one
    at the end, vs at least three without lua).
    
    closes CNVS-7008
    
    test plan:
    * Given the default params, you're not going to ever hit the throttling
      limit without making multiple requests in parallel. Normally local dev
      environments won't process parallel requests, so you'll want to tweak
      the config params.
      * Setting.set('request_throttle.hwm', '2')
      * Setting.set('request_throttle.outflow', '0.1')
    * Make canvas http requests as a logged in user, and verify that after a
      few requests in quick succession, you get a 403 response. After
      waiting a second for your bucket to fall back below the threshold, you
      can make requests again.
    * Do the same using the api and sending your access token in the query
      string or http auth header.
    * This should work correctly with a clustered redis as well.
    * If possible it'd be good to test parallel requests as well. The code
      should handle this correctly and combine the cost of the parallel
      requests, rather than stomping on the values.
    
    Change-Id: I1fdfd4e009d81bd6525bcf45a93437f4c395f129
    Reviewed-on: https://gerrit.instructure.com/24256
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>

--- lib/canvas/request_throttle.rb
+++ lib/canvas/request_throttle.rb
@@ -16,6 +16,18 @@
 #
 require 'set'
 
+# unfortunately this timing data isn't exposed elsewhere,
+# so we override this method to put it in the rack env
+class ActionController::Base
+  def active_record_runtime
+    db_runtime = ActiveRecord::Base.connection.reset_runtime
+    db_runtime += @db_rt_before_render if @db_rt_before_render
+    db_runtime += @db_rt_after_render if @db_rt_after_render
+    request.env['active_record_runtime'] = db_runtime / 1000.0 if request
+    "DB: %.0f" % db_runtime
+  end
+end
+
 module Canvas
 class RequestThrottle
   # this @@last_sample data isn't thread-safe, and if canvas ever becomes
@@ -36,7 +48,9 @@ class RequestThrottle
     # up when using certain servers until request_uri is called once to set env['REQUEST_URI']
     request.request_uri
 
-    result = if !allowed?(request)
+    bucket = bucket(request)
+
+    result = if !allowed?(request, bucket)
       rate_limit_exceeded
     else
       @app.call(env)
@@ -50,17 +64,31 @@ class RequestThrottle
     account = env["canvas.domain_root_account"]
     report_on_stats(account, starting_mem, ending_mem, user_cpu, system_cpu)
 
+    # currently we define cost as the amount of user cpu time plus the amount
+    # of time spent in db queries
+    cost = user_cpu + (env['active_record_runtime'] || 0.0)
+    bucket.increment(cost) if bucket
+
     result
   end
 
-  def allowed?(request)
-    case
-      # note that the whitelist isn't useful until usage-based throttling is
-      # added, since anything not in the blacklist is allowed through anyway
-      when whitelisted?(request)       then true
-      when blacklisted?(request)       then false
-      else
-        true
+  def allowed?(request, bucket)
+    if whitelisted?(request)
+      return true
+    elsif blacklisted?(request)
+      Rails.logger.info("blocking request due to blacklist, client id: #{client_identifier(request)} ip: #{request.remote_ip}")
+      Canvas::Statsd.increment("request_throttling.blacklisted")
+      return false
+    else
+      if bucket
+        bucket.load_data()
+        if bucket.full?
+          Rails.logger.info("blocking request due to throttling, client id: #{client_identifier(request)} bucket: #{bucket.to_json}")
+          Canvas::Statsd.increment("request_throttling.throttled")
+          return false
+        end
+      end
+      return true
     end
   end
 
@@ -82,7 +110,22 @@ class RequestThrottle
   # object won't be caught.
   def client_identifier(request)
     request.env['canvas.request_throttle.user_id'] ||=
-      AuthenticationMethods.access_token(request, :GET) || AuthenticationMethods.user_id(request).to_s
+      (AuthenticationMethods.access_token(request, :GET) ||
+       AuthenticationMethods.user_id(request) ||
+       session_id(request)).to_s.presence
+  end
+
+  def session_id(request)
+    request.env['rack.session.options'].try(:[], :id)
+  end
+
+  def bucket(request)
+    request.env['canvas.request_throttle.bucket'] ||=
+      if Canvas.redis_enabled? && client_id = client_identifier(request)
+        LeakyBucket.new(client_id)
+      else
+        nil
+      end
   end
 
   def self.blacklist
@@ -129,6 +172,84 @@ class RequestThrottle
 
     Rails.logger.info "[STAT] #{mem_stat} #{user_cpu} #{system_cpu}"
     @@last_sample = ending_mem
+  end
+
+  # Our leaky bucket has a separate high water mark (where the bucket is
+  # considered "full") and maximum.
+  # Think of the HWM as a line on the inside of the bucket, where the bucket is
+  # considered full if the bucket gets filled past that line, but the bucket
+  # can continue to hold more up to the maximum (top of the bucket).
+  # The reason we add this to the normal leaky bucket algorithm is if maximum
+  # and hwm were equal, then the bucket would always leak at least a tiny bit
+  # by the beginning of the next request, and thus would never be considered
+  # full.
+  class LeakyBucket < Struct.new(:client_identifier, :count, :last_touched)
+    def initialize(client_identifier, count = 0.0, last_touched = nil)
+      raise(ArgumentError, "client_identifier required") unless client_identifier.present?
+      super
+    end
+
+    def redis
+      @redis ||= Canvas.redis.respond_to?(:node_for) ? Canvas.redis.node_for(cache_key) : Canvas.redis
+    end
+
+    # Cache this on the class, so we don't load the lua script on each request.
+    def self.lua
+      @lua ||= ::Redis::Scripting::Module.new(nil, File.join(File.dirname(__FILE__), "request_throttle"))
+    end
+
+    def cache_key
+      "request_throttling:#{client_identifier}"
+    end
+
+    [[:maximum, 800], [:hwm, 600], [:outflow, 10]].each do |(setting, default)|
+      define_method(setting) do
+        Setting.get_cached("request_throttle.#{setting}", default).to_f
+      end
+    end
+
+    def load_data
+      count, last_touched = redis.hmget(cache_key, "count", "last_touched")
+      self.count = (count || 0.0).to_f
+      self.last_touched = (last_touched || Time.now).to_f
+    end
+
+    def full?(current_time = Time.now)
+      new_count = leak(current_time)
+      new_count >= hwm
+    end
+
+    # This same leak logic is implemented in the lua script.
+    # Note that this leaking applies only locally to the current process, it
+    # isn't saved back to redis. That redis update happens purely in lua.
+    def leak(current_time)
+      new_count = self.count
+      if new_count > 0
+        timespan = current_time - Time.at(self.last_touched)
+        loss = outflow * timespan
+        if loss > 0
+          if loss > new_count
+            loss = new_count
+          end
+          new_count -= loss
+        end
+      end
+      new_count
+    end
+
+    # This is where we both leak and then increment by the given cost amount,
+    # all in lua on the redis server.
+    # We do this all in lua to save on redis calls.
+    # Without lua, we'd have to use a redis optimistic transaction, reading the
+    # old values, and then pushing the new values. That would take at least 2
+    # round trips, and possibly more when we get a transaction conflict.
+    def increment(amount, current_time = Time.now)
+      current_time = current_time.to_f
+      Rails.logger.debug("request throttling increment: #{([amount, current_time] + self.as_json).to_json}")
+      count, last_touched = LeakyBucket.lua.run(:increment_bucket, [cache_key], [amount, current_time, outflow, maximum], redis)
+      self.count = count.to_f
+      self.last_touched = last_touched.to_f
+    end
   end
 end
 end
--- spec/lib/canvas/request_throttle_spec.rb
+++ spec/lib/canvas/request_throttle_spec.rb
@@ -19,10 +19,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
 
 describe 'Canvas::RequestThrottle' do
   let(:base_req) { { 'QUERY_STRING' => '', 'PATH_INFO' => '/' } }
-  let(:request1) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4', 'rack.session' => { user_id: 4 } }) }
-  let(:request2) { base_req.merge({ 'REMOTE_ADDR' => '4.3.2.1', 'rack.session' => { user_id: 5 } }) }
-  let(:request3) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4', 'QUERY_STRING' => 'access_token=xyz' }) }
-  let(:request4) { base_req.merge({ 'REMOTE_ADDR' => '4.3.2.1', 'HTTP_AUTHORIZATION' => 'Bearer abc' }) }
+  let(:request_user_1) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4', 'rack.session' => { user_id: 1 } }) }
+  let(:request_user_2) { base_req.merge({ 'REMOTE_ADDR' => '4.3.2.1', 'rack.session' => { user_id: 2 } }) }
+  let(:request_query_token) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4', 'QUERY_STRING' => 'access_token=xyz' }) }
+  let(:request_header_token) { base_req.merge({ 'REMOTE_ADDR' => '4.3.2.1', 'HTTP_AUTHORIZATION' => 'Bearer abc' }) }
+  let(:request_logged_out) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4', 'rack.session.options' => { id: 'sess1' } }) }
+  let(:request_no_session) { base_req.merge({ 'REMOTE_ADDR' => '1.2.3.4' }) }
 
   let(:response) { [200, {'Content-Type' => 'text/plain'}, ['Hello']] }
   let(:inner_app) { lambda { |env| response } }
@@ -30,6 +32,28 @@ describe 'Canvas::RequestThrottle' do
   let(:rate_limit_exceeded) { throttler.rate_limit_exceeded }
 
   after { Canvas::RequestThrottle.reload! }
+
+  describe "#client_identifier" do
+    def req(hash)
+      ActionController::Request.new(hash).tap { |req| req.request_uri }
+    end
+
+    it "should use access token" do
+      throttler.client_identifier(req request_header_token).should == "abc"
+    end
+
+    it "should use user id" do
+      throttler.client_identifier(req request_user_2).should == "2"
+    end
+
+    it "should use session id" do
+      throttler.client_identifier(req request_logged_out).should == 'sess1'
+    end
+
+    it "should fall back to nil" do
+      throttler.client_identifier(req request_no_session).should == nil
+    end
+  end
 
   describe "#call" do
     def set_blacklist(val)
@@ -40,30 +64,30 @@ describe 'Canvas::RequestThrottle' do
     it "should pass on other requests" do
       throttler.stubs(:whitelisted?).returns(false)
       throttler.stubs(:blacklisted?).returns(false)
-      throttler.call(request1).should == response
+      throttler.call(request_user_1).should == response
     end
 
     it "should blacklist based on ip" do
       set_blacklist('1.2.3.4')
-      throttler.call(request1).should == rate_limit_exceeded
-      throttler.call(request2).should == response
+      throttler.call(request_user_1).should == rate_limit_exceeded
+      throttler.call(request_user_2).should == response
       set_blacklist('1.2.3.4,4.3.2.1')
-      throttler.call(request2).should == rate_limit_exceeded
+      throttler.call(request_user_2).should == rate_limit_exceeded
     end
 
     it "should blacklist based on user id" do
-      set_blacklist('5')
-      throttler.call(request1).should == response
-      throttler.call(request2).should == rate_limit_exceeded
+      set_blacklist('2')
+      throttler.call(request_user_1).should == response
+      throttler.call(request_user_2).should == rate_limit_exceeded
     end
 
     it "should blacklist based on access token" do
       set_blacklist('abc')
-      throttler.call(request3).should == response
-      throttler.call(request4).should == rate_limit_exceeded
+      throttler.call(request_query_token).should == response
+      throttler.call(request_header_token).should == rate_limit_exceeded
       set_blacklist('abc,xyz')
-      throttler.call(request3).should == rate_limit_exceeded
-      throttler.call(request4).should == rate_limit_exceeded
+      throttler.call(request_query_token).should == rate_limit_exceeded
+      throttler.call(request_header_token).should == rate_limit_exceeded
     end
   end
 
@@ -71,6 +95,101 @@ describe 'Canvas::RequestThrottle' do
     it "should split the string and create a set" do
       Setting.set('list_test', 'x,y ,  z ')
       Canvas::RequestThrottle.list_from_setting('list_test').should == Set.new(%w[z y x])
+    end
+  end
+
+  describe "cost throttling" do
+    before do
+      throttler.stubs(:whitelisted?).returns(false)
+      throttler.stubs(:blacklisted?).returns(false)
+    end
+
+    it "should skip without redis enabled" do
+      Canvas.stubs(:redis_enabled?).returns(false)
+      Canvas::RequestThrottle::LeakyBucket.expects(:new).never
+      throttler.call(request_user_1).should == response
+    end
+
+    it "should skip if no client_identifier found" do
+      Canvas::RequestThrottle::LeakyBucket.expects(:new).never
+      throttler.call(request_no_session).should == response
+    end
+
+    it "should throttle if bucket is full" do
+      bucket = mock('Bucket')
+      Canvas::RequestThrottle::LeakyBucket.expects(:new).with("1").returns(bucket)
+      bucket.expects(:load_data)
+      bucket.expects(:full?).returns(true)
+      bucket.expects(:to_json) # in the logger.info line
+      bucket.expects(:increment)
+
+      throttler.call(request_user_1).should == rate_limit_exceeded
+    end
+
+    it "should not throttle, but update, if bucket is not full" do
+      bucket = mock('Bucket')
+      Canvas::RequestThrottle::LeakyBucket.expects(:new).with("1").returns(bucket)
+      bucket.expects(:load_data)
+      bucket.expects(:full?).returns(false)
+      bucket.expects(:increment)
+
+      throttler.call(request_user_1).should == response
+    end
+  end
+
+  if Canvas.redis_enabled?
+    describe Canvas::RequestThrottle::LeakyBucket do
+      before do
+        @outflow = 15.5
+        Setting.set('request_throttle.outflow', @outflow.to_s)
+        @bucket = Canvas::RequestThrottle::LeakyBucket.new("test", 150.0, 15.0)
+        @current_time = 20.2
+        # this magic number is @bucket.count - ((@current_time - @bucket.last_touched) * @outflow)
+        @expected = 69.4
+      end
+
+      describe "#full?" do
+        it "should leak based on time and outflow" do
+          @bucket.leak(Time.at(@current_time)).should be_close(@expected, 0.1)
+          @bucket.leak(Time.at(75)).should == 0.0 # clamps to 0
+        end
+
+        it "should leak then compare to hwm" do
+          Setting.set('request_throttle.hwm', '100')
+          @bucket.full?(Time.at(@current_time)).should == false
+          @bucket.full?(Time.at(@bucket.last_touched)).should == true
+          Setting.set('request_throttle.hwm', @expected - 0.5)
+          @bucket.full?(Time.at(@current_time)).should == true
+        end
+      end
+
+      describe "redis interaction" do
+        it "should use defaults if no redis data" do
+          Timecop.freeze('2012-01-29 12:00:00 UTC') do
+            @bucket.load_data
+            @bucket.count.should == 0
+            @bucket.last_touched.should == Time.now.to_f
+          end
+        end
+
+        it "should load data from redis" do
+          ts = Time.parse('2012-01-29 12:00:00 UTC')
+          @bucket.redis.hmset(@bucket.cache_key, 'count', '15', 'last_touched', ts.to_f)
+          @bucket.load_data
+          @bucket.count.should == 15
+          @bucket.last_touched.should be_close(ts.to_f, 0.1)
+        end
+
+        it "should update redis via the lua script" do
+          @bucket.redis.hmset(@bucket.cache_key, 'count', @bucket.count, 'last_touched', @bucket.last_touched)
+          @cost = 20.5
+          @bucket.increment(@cost, @current_time)
+          @bucket.count.should be_close(@expected + @cost, 0.1)
+          @bucket.last_touched.should be_close(@current_time, 0.1)
+          @bucket.redis.hget(@bucket.cache_key, 'count').to_f.should be_close(@expected + @cost, 0.1)
+          @bucket.redis.hget(@bucket.cache_key, 'last_touched').to_f.should be_close(@current_time, 0.1)
+        end
+      end
     end
   end
 end
