7add5028b5331ed15e8ea73525d5f2b9b95626ce
Bryan Madsen <bryan@instructure.com>
2013-08-23 12:10:29 -0600

    spec: fix fragile outcomes selenium spec
    Change-Id: If0b67db85ce2aef1799b9e4c771cca9d395c72b4
    Reviewed-on: https://gerrit.instructure.com/23672
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bryan Madsen <bryan@instructure.com>
    Product-Review: Bryan Madsen <bryan@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>

--- spec/selenium/helpers/outcome_common.rb
+++ spec/selenium/helpers/outcome_common.rb
@@ -304,12 +304,13 @@ def should_edit_an_outcome_group
   get outcome_url
   wait_for_ajaximations
   fj('.outcomes-sidebar .outcome-level:first li.outcome-group').click
-  f('.edit_button').click
 
-  ## when
-  # edit title
+  keep_trying_until do
+    fj('.edit_button').click
+    fj('.outcomes-content input[name=title]').should be_displayed
+  end
+
   replace_content f('.outcomes-content input[name=title]'), edited_title
-  # submit
   f('.submit_button').click
   wait_for_ajaximations
 
