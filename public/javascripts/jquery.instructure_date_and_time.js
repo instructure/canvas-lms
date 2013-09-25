d982a725a8fdbb359f7a277e926b58a5d54eacb7
Jacob Fugal <jacob@instructure.com>
2013-09-25 16:15:04 -0600

    don't include user_offset in timestamp
    result.timestamp should be the true timestamp of the parsed date, not
    adjusted in any way. only include user_offset (the profile timezone,
    counter to tz_offset which is the browser timezone) in the calculation
    of `time', which is a fudged value
    
    this wasn't a bigger issue before, because the timestamp value was only
    used for sorting. even being off by user_offset, all timestamps were off
    by the same amount, so they still sorted consistently.
    
    test-plan:
     - load a page while logged in, open the javascript console
     - set "d = new Date()"
     - compare "$.parseFromISO(d.toISOString()).timestamp * 1000) == d"
     - should be true (before this code, was off by the profile timezone's
       offset)
     - go to the assignments page for a course with 2 or more assignments
       with distinct due dates
     - add a third assignment with a due date between the others
     - the added assignment should sort into the others appropriately
    
    Change-Id: I30c45ea57d64da5dd82c535987fa728f2f69d147
    Reviewed-on: https://gerrit.instructure.com/31597
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>
    QA-Review: Jacob Fugal <jacob@instructure.com>

--- public/javascripts/jquery.instructure_date_and_time.js
+++ public/javascripts/jquery.instructure_date_and_time.js
@@ -128,7 +128,12 @@ var speakMessage = function ($this, message) {
       var offset = offset_string == 'Z' ? 0 : +offset_string;
       if (isNaN(hour) || isNaN(minute) || isNaN(second) || isNaN(offset)) { throw 'invalid'; }
 
-      var date_offset = offset;
+      // combine them into an accurate unfudged timestamp
+      var date_timestamp = Date.UTC(year, month - 1, day);
+      var time_timestamp = (((hour - offset) * 60 + minute) * 60 + second) * 1000;
+      result.timestamp = (time_timestamp + date_timestamp) / 1000;
+      result.minute_timestamp = result.timestamp - (result.timestamp % 60);
+
       if(result.date.getTimezoneOffset() != today.getTimezoneOffset()) {
         user_offset = user_offset - ((result.date.getTimezoneOffset() - today.getTimezoneOffset()) / 60);
       }
@@ -136,12 +141,8 @@ var speakMessage = function ($this, message) {
       // passed in and may technically be incorrect if there
       // is shifting due to time zones.
       // result.date = $.datepicker.parseDate("yy-mm-dd", iso.substring(0, 10));
-      var date_timestamp = Date.UTC(year, month - 1, day);
-      var time_timestamp = (((hour + user_offset - date_offset) * 60 + minute) * 60 + second) * 1000;
-      result.timestamp = (time_timestamp + date_timestamp) / 1000;
-      result.minute_timestamp = result.timestamp - (result.timestamp % 60);
       var tz_offset = result.date.getTimezoneOffset() * 60000;
-      var time = new Date(date_timestamp + time_timestamp + tz_offset);
+      var time = new Date(date_timestamp + time_timestamp + user_offset * 3600000 + tz_offset);
 
       // format date fields
       result.date_sortable = iso.substring(0, 10);
