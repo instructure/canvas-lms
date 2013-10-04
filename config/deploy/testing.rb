a6916cebb5da5c42ac49440a78a8fc9be45e8889
Graham Ballantyne <grahamb@sfu.ca>
2013-10-04 11:23:24 -0700

    Modify cap scripts to pull number of app nodes from config file
    fix indenting
    
    replace push_app_servers with a cap task to fetch number of nodes from a management node
    
    modify environment deploy files to use canvas:set_app_nodes

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,8 +1,6 @@
-num_app_nodes = 3
-app_node_prefix = "canvas-at"
+set :app_node_prefix, "canvas-at"
 set :canvas_url, 'https://canvas-test.sfu.ca'
 
-push_app_servers(num_app_nodes, app_node_prefix)
 role :db, "canvas-mt.tier2.sfu.ca", :primary => true
 
 set :rails_env, "production"
@@ -11,3 +9,5 @@ set :branch, "sfu-develop"
 if ENV.has_key?('branch')
   set :branch, ENV['branch']
 end
+
+on :start, "canvas:set_app_nodes"
