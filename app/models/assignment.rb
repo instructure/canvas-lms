d97fb1611ab8f64da7b6fce78462244d4f6e6bd0
Liz Abinante <labinante@instructure.com>
2013-10-07 14:19:37 -0500

    validates length of allowed_extensions
    fixes CNVS-6066
    
    test plan
    * using API:
     - make an update call to an existing assignment
     - set assignment[allowed_extenstions] to be a string > 255 chars
     - verify error message is "internal_server_error"
     - API call should not return anything from the database
     - make another update call to an existing assignment
     - set assignment[allowed_extensions] to be a string <= 255 chars
     - verify that the change was saved
    
    * from assignment edit page:
     - check 'Allow file uploads'
     - check 'Restrict upload file types'
     - add some file types in the box and save (<= 255 chars)
     - verify that it saved the file types
     - edit the assignment again
     - verify that you cannot exceed 255 chars in the 'file types' field
    
    Change-Id: Ibf8ce3c77282951d6180894415d23470d4402f63
    Reviewed-on: https://gerrit.instructure.com/25082
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Amber Taniuchi <amber@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -118,6 +118,7 @@ class Assignment < ActiveRecord::Base
   validates_presence_of :context_id, :context_type, :workflow_state
   validates_length_of :title, :maximum => maximum_string_length, :allow_nil => true
   validates_length_of :description, :maximum => maximum_long_text_length, :allow_nil => true, :allow_blank => true
+  validates_length_of :allowed_extensions, :maximum => maximum_long_text_length, :allow_nil => true, :allow_blank => true
   validate :frozen_atts_not_altered, :if => :frozen?, :on => :update
 
   acts_as_list :scope => :assignment_group_id
