9cfd6dc9e143a24ad9de242ac59d6a97c7049f7d
Rob Orton <rob@instructure.com>
2013-10-29 12:35:44 -0600

    allow delete all on inner join with aliased scope
    test plan
     - specs should pass
    
    Change-Id: I37857a560d3159c77706bfc737e71c7ac62c3428
    Reviewed-on: https://gerrit.instructure.com/25771
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -679,7 +679,7 @@ class ActiveRecord::Base
       join_conditions = []
       joins_sql.strip.split('INNER JOIN')[1..-1].each do |join|
         # this could probably be improved
-        raise "PostgreSQL update_all/delete_all only supports INNER JOIN" unless join.strip =~ /([a-zA-Z'"_]+)\s+ON\s+(.*)/
+        raise "PostgreSQL update_all/delete_all only supports INNER JOIN" unless join.strip =~ /([a-zA-Z'"_]+(?:(?: [aA][sS])? [a-zA-Z_])?)\s+ON\s+(.*)/
         tables << $1
         join_conditions << $2
       end
