7cd36ce5fc1e25c26fa5b3fda8d3f7e5c44171b6
Cody Cutrer <cody@instructure.com>
2013-11-18 13:38:44 -0700

    automatically repair data before fixing unique index
    Change-Id: I4d5689fe8872654f8c91233017ad1e1894a7dc2b
    Reviewed-on: https://gerrit.instructure.com/26414
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20131105230615_fix_submission_versions_index.rb
+++ db/migrate/20131105230615_fix_submission_versions_index.rb
@@ -4,7 +4,13 @@ class FixSubmissionVersionsIndex < ActiveRecord::Migration
 
   def self.up
     if connection.adapter_name == 'PostgreSQL' && connection.select_value("SELECT 1 FROM pg_index WHERE indexrelid='index_submission_versions'::regclass AND NOT indisunique")
-      add_index :submission_versions, [:context_id, :version_id, :user_id, :assignment_id],
+      columns = [:context_id, :version_id, :user_id, :assignment_id]
+      SubmissionVersion.select(columns).where(context_type: 'Course').group(columns).having("COUNT(*) > 1").find_each do |sv|
+        scope = SubmissionVersion.where(Hash[columns.map { |c| [c, sv[c]]}]).where(context_type: 'Course')
+        keeper = scope.first
+        scope.where("id<>?", keeper).delete_all
+      end
+      add_index :submission_versions, columns,
                 :name => 'index_submission_versions2',
                 :where => { :context_type => 'Course' },
                 :unique => true
