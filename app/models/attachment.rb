4c960ef93425cfc27384a1ea1407bacbd66b6d2d
Rob Orton <rob@instructure.com>
2013-11-25 12:19:31 -0700

    set scribd user different per context
    closes CNVS-9691
    
    test plan
     - scribd user should be canvas-context-global_id
    
    Change-Id: I2374aa656134b7ad80986be758e9cab3401bceec
    Reviewed-on: https://gerrit.instructure.com/26663
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -369,7 +369,7 @@ class Attachment < ActiveRecord::Base
   end
 
   def scribd_user
-    self.scribd_doc.try(:owner) || "canvas-#{Rails.env}"
+    self.scribd_doc.try(:owner) || (Rails.env.production? ? "canvas-#{context.global_asset_string}" : "canvas-#{Rails.env}")
   end
 
   # This method retrieves a URL to the thumbnail of a document, in a given size, and for any page in that document. Note that docs.getSettings and docs.getList also retrieve thumbnail URLs in default size - this method is really for resizing those. IMPORTANT - it is possible that at some time in the future, Scribd will redesign its image system, invalidating these URLs. So if you cache them, please have an update strategy in place so that you can update them if necessary.
