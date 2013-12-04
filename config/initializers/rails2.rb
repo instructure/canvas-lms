6b02cf91ed1339dc82f029cb27d960defaaa7dae
Cody Cutrer <cody@instructure.com>
2013-12-04 14:57:46 -0700

    fix association callback in ruby 2.0
    Change-Id: I53e6caa46a27edcf117e9583b99daf2f39dc7099
    Reviewed-on: https://gerrit.instructure.com/26987
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/rails2.rb
+++ config/initializers/rails2.rb
@@ -319,4 +319,36 @@ ActiveRecord::Callbacks.class_eval do
   end
 end
 
+# ditto
+ActiveRecord::AutosaveAssociation.class_eval do
+  def save_collection_association(reflection)
+    if association = association_instance_get(reflection.name)
+      autosave = reflection.options[:autosave]
+
+      if records = associated_records_to_validate_or_save(association, @new_record_before_save, autosave)
+        records.each do |record|
+          next if record.destroyed?
+
+          if autosave && record.marked_for_destruction?
+            association.destroy(record)
+          elsif autosave != false && (@new_record_before_save || record.new_record?)
+            if autosave
+              saved = association.send(:insert_record, record, false, false)
+            else
+              association.send(:insert_record, record)
+            end
+          elsif autosave
+            saved = record.save(false)
+          end
+
+          raise ActiveRecord::Rollback if saved == false
+        end
+      end
+
+      # reconstruct the SQL queries now that we know the owner's id
+      association.__send__(:construct_sql) if association.respond_to?(:construct_sql, true)
+    end
+  end
+end
+
 end
