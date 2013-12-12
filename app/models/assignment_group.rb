fb6bd60508c27bbcc84e4e3bca8f4475968a9739
Michael Nomitch <mnomitch@instructure.com>
2013-12-12 12:11:38 -0600

    filters unpublished assignments out of student grades page
    fixes CNVS-9948
    
    calls for active_published_assignments thru assignment group
    rather than just active_assignments
    
    test plan:
    add multiple unpublished assignments as a teacher
    as a student go to the grades page
    unpublished assignments should not be visible
    
    Change-Id: I0d1d1d8f46b9a3447a134b8bd8f40627a0bfb370
    Reviewed-on: https://gerrit.instructure.com/27339
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Amber Taniuchi <amber@instructure.com>

--- app/models/assignment_group.rb
+++ app/models/assignment_group.rb
@@ -28,6 +28,7 @@ class AssignmentGroup < ActiveRecord::Base
 
   has_many :assignments, :order => 'position, due_at, title', :dependent => :destroy
   has_many :active_assignments, :class_name => 'Assignment', :conditions => ['assignments.workflow_state != ?', 'deleted'], :order => 'assignments.position, assignments.due_at, assignments.title'
+  has_many :published_assignments, :class_name => 'Assignment', :conditions => "assignments.workflow_state = 'published'", :order => 'assignments.position, assignments.due_at, assignments.title'
 
   validates_presence_of :context_id, :context_type, :workflow_state
   validates_length_of :rules, :maximum => maximum_text_length, :allow_nil => true, :allow_blank => true
