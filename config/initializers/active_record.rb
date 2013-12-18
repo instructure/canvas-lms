d1c35ae1870414f84a3d516bf6415f8a3f18cfa1
Cody Cutrer <cody@instructure.com>
2013-12-18 15:21:25 -0700

    fix deconstruction of joins with aliases
    Change-Id: I7b8bc73bd546e08723a0dfdca932e9f617ab36dd
    Reviewed-on: https://gerrit.instructure.com/27680
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Anthus Williams <awilliams@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -698,7 +698,7 @@ class ActiveRecord::Base
       join_conditions = []
       joins_sql.strip.split('INNER JOIN')[1..-1].each do |join|
         # this could probably be improved
-        raise "PostgreSQL update_all/delete_all only supports INNER JOIN" unless join.strip =~ /([a-zA-Z'"_]+(?:(?: [aA][sS])? [a-zA-Z_])?)\s+ON\s+(.*)/
+        raise "PostgreSQL update_all/delete_all only supports INNER JOIN" unless join.strip =~ /([a-zA-Z0-9'"_]+(?:(?:\s+[aA][sS])?\s+[a-zA-Z0-9'"_]+)?)\s+ON\s+(.*)/
         tables << $1
         join_conditions << $2
       end
