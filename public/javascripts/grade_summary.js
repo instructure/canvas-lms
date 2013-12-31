75004e5e1c20ead813047afc5bc17e80801cff60
Michael Nomitch <mnomitch@instructure.com>
2013-12-31 10:59:01 -0600

    accessibility for what if score
    fixes CNVS-7088
    
    test plan:
      - Do the following with OSX screenreader and JAWS
        - as a student go to the grade summary page
        - using screenreader controls navigate to the what-if score
        - change the score and revert the score back to what it was
        - navigation should work properly
        - text that is read aloud should make sense
      - note: after changing the score you will refocus on the revert link,
        this is a fix for an OSX screenreader bug (and hopefully helpful too)
    
    Change-Id: I84482d203e5ec73a4bc374bb663a0d451c502d51
    Reviewed-on: https://gerrit.instructure.com/27995
    Reviewed-by: Mike Nomitch <mnomitch@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- public/javascripts/grade_summary.js
+++ public/javascripts/grade_summary.js
@@ -128,7 +128,6 @@ define([
       $(originEl).focus();
       $(this).closest('.rubric_assessments, .comments').hide();
     });
-
     $('.student_assignment.editable .assignment_score').click(function(event) {
       if ($('#grades_summary.editable').length === 0 || $(this).find('#grade_entry').length > 0 || $(event.target).closest('.revert_score_link').length > 0) {
         return;
@@ -136,6 +135,8 @@ define([
       if (!$(this).find('.grade').data('originalValue')){
         $(this).find('.grade').data('originalValue', $(this).find('.grade').html());
       }
+      var screenreader_link_clone = $(this).find('.screenreader-only').clone(true);
+      $(this).find('.grade').data("screenreader_link", screenreader_link_clone);
       $(this).find('.grade').empty().append($("#grade_entry"));
       $(this).find('.score_value').hide();
       var val = $(this).parents('.student_assignment').find('.score').text();
@@ -187,6 +188,7 @@ define([
           .find(".score_teaser").text(I18n.t('titles.hypothetical_score', "This is a What-If score")).end()
           .find(".score_holder").append($("#revert_score_template").clone(true).attr('id', '').show())
           .find(".grade").addClass('changed');
+        setTimeout(function() { $assignment.find(".revert_score_link").focus();}, 0)
       } else {
         var tooltip = $assignment.data('muted') ?
           I18n.t('student_mute_notification', 'Instructor is working on grades') :
@@ -199,6 +201,10 @@ define([
       if (val === 0) { val = '0.0'; }
       if (val === originalVal) { val = originalScore; }
       $assignment.find('.grade').html(val || $assignment.find('.grade').data('originalValue'));
+      if (!isChanged) {
+        var screenreader_link_clone = $assignment.find('.grade').data("screenreader_link");
+        $assignment.find('.grade').prepend(screenreader_link_clone);
+      }
 
       updateScoreForAssignment(assignment_id, val);
       updateStudentGrades();
@@ -230,6 +236,9 @@ define([
       if(!skipEval) {
         updateStudentGrades();
       }
+      var screenreader_link_clone = $assignment.find('.grade').data("screenreader_link");
+      $assignment.find('.grade').prepend(screenreader_link_clone);
+      setTimeout(function() { $assignment.find(".grade").focus();}, 0);
     });
     $("#grades_summary:not(.editable) .assignment_score").css('cursor', 'default');
     $("#grades_summary tr").hover(function() {
