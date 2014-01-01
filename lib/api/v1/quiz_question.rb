37aed89c01fcc53faeaa97d91e949d6701fab958
Ahmad Amireh <ahmad@instructure.com>
2014-01-01 16:28:57 +0200

    Quiz Questions API - Allow student access
    Students can now get a censored version of quiz questions, with enough
    data to build the quizzes they have to take, but not enough to read the
    correct answers and make the entire thing moot.
    
    closes CNVS-10222
    
    Note to code reviewers: the spec file shows too many changes because of
    a re-indentation/rescope .. the original specs are untouched, but it was
    necessary to scope them under a context ("as a teacher") to escape the
    wrath of the global before(:each) block that was installed for all
    specs.
    
    TEST PLAN
    ---- ----
    
    Endpoints:
    
      1. /api/v1/courses/:course_id/quizzes/:quiz_id/questions
      2. /api/v1/courses/:course_id/quizzes/:quiz_id/questions/:question_id
    
    - Create a quiz with a question or two
    - As a student using the API, make a GET request to both endpoints
      above and verify that:
      - you can read the questions
      - you don't get to see the answer weights
    - Add a "?include=assessment_question" to the URL and:
      - verify that the "answers" array entries in the "assessment_question"
        do not list the answer weight either
    
    Change-Id: I5c32030d92a1c46993706c2aa6f0575f858b60c0
    Reviewed-on: https://gerrit.instructure.com/28022
    QA-Review: Myller de Araujo <myller@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Derek DeVries <ddevries@instructure.com>
    Product-Review: Ahmad Amireh <ahmad@instructure.com>

--- lib/api/v1/quiz_question.rb
+++ lib/api/v1/quiz_question.rb
@@ -45,22 +45,44 @@ module Api::V1::QuizQuestion
     matching_answer_incorrect_matches
   )
 
-  def questions_json(questions, user, session, context = nil, includes = [])
+  def questions_json(questions, user, session, context = nil, includes = [], censored = false)
     questions.map do |question|
-      question_json(question, user, session, context, includes)
+      question_json(question, user, session, context, includes, censored)
     end
   end
 
-  def question_json(question, user, session, context = nil, includes = [])
+  def question_json(question, user, session, context = nil, includes = [], censored = false)
     hsh = api_json(question, user, session, API_ALLOWED_QUESTION_OUTPUT_FIELDS) do |json, q|
-      API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.each { |field| json.send("#{field}=", q.question_data[field]) }
+      API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.each do |field|
+        json.send("#{field}=", q.question_data[field])
+      end
     end
 
     if includes.include?(:assessment_question)
       hsh[:assessment_question] = api_json(question.assessment_question, user, session)
+
+      censor(hsh[:assessment_question][:question_data]) if censored
     end
 
+    # Remove the answer weights if we're censoring the data for student access;
+    # the answer weights denote the correct answer !!!!!
+    censor(hsh) if censored
+
     hsh
+  end
+
+  private
+
+  # Delete sensitive question data that students shouldn't get to see.
+  #
+  # @param [Object] question_data
+  #   The question data record to censor. This would either be the serialized
+  #   quiz_question or the question_data field in a serialized assessment
+  #   question.
+  def censor(question_data)
+    question_data[:answers].each do |record|
+      record.delete(:weight)
+    end
   end
 
 end
--- spec/apis/v1/quiz_questions_api_spec.rb
+++ spec/apis/v1/quiz_questions_api_spec.rb
@@ -19,89 +19,162 @@
 require File.expand_path(File.dirname(__FILE__) + '/../api_spec_helper')
 
 describe QuizQuestionsController, :type => :integration do
-  before do
-    @course = course
-    teacher_in_course active_all: true
-    @quiz = @course.quizzes.create!(:title => "A Sample Quiz")
-  end
 
-  describe "GET /courses/:course_id/quizzes/:quiz_id/questions (index)" do
-    it "returns a list of questions" do
-      questions = (1..10).map do |n|
-        @quiz.quiz_questions.create!(:question_data => { :question_name => "Question #{n}" })
-      end
-
-
-      json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
-                      :controller => "quiz_questions", :action => "index", :format => "json",
-                      :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s)
-
-      question_ids = json.collect { |q| q['id'] }
-      question_ids.should == questions.map(&:id)
+  context 'as a teacher' do
+    before do
+      @course = course
+      teacher_in_course active_all: true
+      @quiz = @course.quizzes.create!(:title => "A Sample Quiz")
     end
 
-    it "returns unauthorized if the user doesn't have the proper rights" do
-      student_in_course active_all: true
-      response = raw_api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
-                      :controller => "quiz_questions", :action => "index", :format => "json",
-                      :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s)
+    describe "GET /courses/:course_id/quizzes/:quiz_id/questions (index)" do
+      it "returns a list of questions" do
+        questions = (1..10).map do |n|
+          @quiz.quiz_questions.create!(:question_data => { :question_name => "Question #{n}" })
+        end
+
+        json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
+                        :controller => "quiz_questions", :action => "index", :format => "json",
+                        :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s)
+
+        question_ids = json.collect { |q| q['id'] }
+        question_ids.should == questions.map(&:id)
+      end
+    end
+
+    describe "GET /courses/:course_id/quizzes/:quiz_id/questions/:id (show)" do
+      context "existing question" do
+        before do
+          @question = @quiz.quiz_questions.create!(:question_data => {
+                                                                      "question_name"=>"Example Question", "assessment_question_id"=>"",
+                                                                      "question_type"=>"multiple_choice_question", "points_possible"=>"1",
+                                                                      "correct_comments"=>"", "incorrect_comments"=>"", "neutral_comments"=>"",
+                                                                      "question_text"=>"<p>What's your favorite color?</p>", "position"=>"0",
+                                                                      "text_after_answers"=>"", "matching_answer_incorrect_matches"=>"",
+                                                                      "answers"=>[]
+                                                                      })
+
+          @json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions/#{@question.id}",
+                           :controller => "quiz_questions", :action => "show", :format => "json",
+                           :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s, :id => @question.id.to_s)
 
 
-      response.should == 401
+          @json.symbolize_keys!
+        end
+
+        it "has only the allowed question output fields" do
+          question_fields = Api::V1::QuizQuestion::API_ALLOWED_QUESTION_OUTPUT_FIELDS[:only].map(&:to_sym) +  Api::V1::QuizQuestion::API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.map(&:to_sym)
+          @json.keys.each { |key| question_fields.should include(key) }
+        end
+
+        it "has the question data fields" do
+          Api::V1::QuizQuestion::API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.map(&:to_sym).each do |field|
+            @json.should have_key(field)
+
+            # ugh... due to wonkiness in Question#question_data's treatment of keys,
+            # and the fact that symbolize_keys doesn't recurse, we resort to this.
+            if @json[field].is_a?(Array) && @question.question_data[field].is_a?(Array)
+              @json[field].map(&:symbolize_keys).should == @question.question_data[field].map(&:symbolize_keys)
+            else
+              @json[field].should == @question.question_data.symbolize_keys[field]
+            end
+          end
+        end
+      end
+
+      context "non-existent question" do
+        before do
+          @json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions/9034831",
+                           {:controller => "quiz_questions", :action => "show", :format => "json", :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s, :id => "9034831"},
+                             {}, {}, {:expected_status => 404})
+        end
+
+        it "should return a not found error message" do
+          @json.should contain "not found"
+        end
+      end
     end
   end
 
-  describe "GET /courses/:course_id/quizzes/:quiz_id/questions/:id (show)" do
-    context "existing question" do
-      before do
-        @question = @quiz.quiz_questions.create!(:question_data => {
-                                                                    "question_name"=>"Example Question", "assessment_question_id"=>"",
-                                                                    "question_type"=>"multiple_choice_question", "points_possible"=>"1",
-                                                                    "correct_comments"=>"", "incorrect_comments"=>"", "neutral_comments"=>"",
-                                                                    "question_text"=>"<p>What's your favorite color?</p>", "position"=>"0",
-                                                                    "text_after_answers"=>"", "matching_answer_incorrect_matches"=>"",
-                                                                    "answers"=>[]
-                                                                    })
+  context 'as a student' do
+    before do
+      course_with_student_logged_in :active_all => true
 
-        @json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions/#{@question.id}",
-                         :controller => "quiz_questions", :action => "show", :format => "json",
-                         :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s, :id => @question.id.to_s)
+      @quiz = @course.quizzes.create!(:title => 'quiz')
+      @quiz.published_at = Time.now
+      @quiz.workflow_state = 'available'
+      @quiz.save!
+    end
 
+    describe 'GET /courses/:course_id/quizzes/:quiz_id/questions (index)' do
+      it 'returns a censored version of the questions' do
+        @quiz.quiz_questions.create!(:question_data => multiple_choice_question_data)
 
-        @json.symbolize_keys!
-      end
+        json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
+                        :controller => "quiz_questions", :action => "index", :format => "json",
+                        :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s)
 
-      it "has only the allowed question output fields" do
-        question_fields = Api::V1::QuizQuestion::API_ALLOWED_QUESTION_OUTPUT_FIELDS[:only].map(&:to_sym) +  Api::V1::QuizQuestion::API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.map(&:to_sym)
-        @json.keys.each { |key| question_fields.should include(key) }
-      end
-
-      it "has the question data fields" do
-        Api::V1::QuizQuestion::API_ALLOWED_QUESTION_DATA_OUTPUT_FIELDS.map(&:to_sym).each do |field|
-          @json.should have_key(field)
-
-          # ugh... due to wonkiness in Question#question_data's treatment of keys,
-          # and the fact that symbolize_keys doesn't recurse, we resort to this.
-          if @json[field].is_a?(Array) && @question.question_data[field].is_a?(Array)
-            @json[field].map(&:symbolize_keys).should == @question.question_data[field].map(&:symbolize_keys)
-          else
-            @json[field].should == @question.question_data.symbolize_keys[field]
+        json.length.should == 1
+        json.each do |question|
+          question.symbolize_keys!
+          question[:answers].class.name.should == 'Array'
+          question[:answers].each do |answer|
+            answer.has_key?(:weight).should be_false
           end
         end
       end
     end
 
-    context "non-existent question" do
-      before do
-        @json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions/9034831",
-                         {:controller => "quiz_questions", :action => "show", :format => "json", :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s, :id => "9034831"},
-                           {}, {}, {:expected_status => 404})
+    describe 'GET /courses/:course_id/quizzes/:quiz_id/questions/:id (show)' do
+      it 'returns a censored version of the question' do
+        @quiz.quiz_questions.create!(:question_data => multiple_choice_question_data)
+
+        json = api_call(:get, "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
+                        :controller => "quiz_questions", :action => "index", :format => "json",
+                        :course_id => @course.id.to_s, :quiz_id => @quiz.id.to_s)
+
+        json.length.should == 1
+        json.each do |question|
+          question.symbolize_keys!
+          question[:answers].class.name.should == 'Array'
+          question[:answers].each do |answer|
+            answer.has_key?(:weight).should be_false
+          end
+        end
       end
 
-      it "should return a not found error message" do
-        @json.should contain "not found"
-      end
+      it 'censors both the question and its assessment question' do
+        @quiz.quiz_questions.create!(:question_data => multiple_choice_question_data)
 
+        json = api_call(:get,
+          "/api/v1/courses/#{@course.id}/quizzes/#{@quiz.id}/questions",
+          {
+            :controller => "quiz_questions",
+            :action => "index",
+            :format => "json",
+            :course_id => @course.id.to_s,
+            :quiz_id => @quiz.id.to_s
+          }, {
+            :include => [ :assessment_question ]
+          })
+
+        json.length.should == 1
+
+        question = json[0].with_indifferent_access
+        question[:answers].class.name.should == 'Array'
+        question[:answers].each do |answer|
+          answer.has_key?(:weight).should be_false
+        end
+
+        assessment_question = question[:assessment_question]
+        assessment_question.should be_present
+        assessment_question[:question_data].should be_present
+        assessment_question[:question_data].should be_present
+        assessment_question[:question_data][:answers].class.name.should == 'Array'
+        assessment_question[:question_data][:answers].each do |answer|
+          answer.has_key?(:weight).should be_false
+        end
+      end
     end
   end
 end
