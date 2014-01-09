d55cfcb5365f110b712ee862fac405fc3590a1b0
Braden Anderson <banderson@instructure.com>
2014-01-08 17:51:42 -0700

    calendar: upgrade color-slicer
    fixes CNVS-10351
    
    test plan:
      * open calendar2
      * verify that calendar colors look more distinct than in
          http://screencast.com/t/PpKgnpPZ
    
    Change-Id: I488acb1e16af4e51728aa11c3dd667025b11fbc4
    Reviewed-on: https://gerrit.instructure.com/28261
    Reviewed-by: Braden Anderson <banderson@instructure.com>
    QA-Review: Braden Anderson <banderson@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Braden Anderson <banderson@instructure.com>

--- public/javascripts/bower/color-slicer/dist/color-slicer.js
+++ public/javascripts/bower/color-slicer/dist/color-slicer.js
@@ -9,13 +9,38 @@ var fairSlicer = require('./lib/fair-slicer');
 var converter = require("color-convert");
 
 module.exports = {
-  hueToRgb: function(h) {
-    h = h / 360 * 2 * Math.PI;
-    // legible lightness for small text on a white background
-    var l = 40;
-    // chroma
-    var c = 80;
-    var lab = [l, c * Math.cos(h), c * Math.sin(h)];
+  lchToLab: function(lch) {
+    var l = lch[0];
+    var c = lch[1];
+    var h = lch[2] / 360 * 2 * Math.PI;
+    return [l, c * Math.cos(h), c * Math.sin(h)];
+  },
+
+  hueToLch: function(options, h) {
+    var l, c;
+    if (options.l) {
+      l = options.l;
+      c = options.c;
+    } else if (options.bright) {
+      l = 73;
+      c = 42;
+    } else {
+      l = 50;
+      c = 32;
+      // vary chroma to roughly match boundary of RGB-expressible colors
+      var delta = 18;
+      var most_constrained_hue = 210;
+      var hr = (h - most_constrained_hue) / 360 * 2 * Math.PI;
+      c += delta - Math.round(delta * Math.cos(hr));
+    }
+    return [l, c, h]
+  },
+
+  lchToCss: function(lch) {
+    return this.rgbToCss(this.labToRgb(this.lchToLab(lch)));
+  },
+
+  labToRgb: function(lab) {
     var xyz = converter.lab2xyz.apply(converter, lab);
     var rgb = converter.xyz2rgb.apply(converter, xyz);
     return rgb;
@@ -25,17 +50,35 @@ module.exports = {
     return "rgb("+rgb.join(',')+")";
   },
 
-  getRawColors: function(limit, startX) {
+  getLchColors: function(limit, startX, options) {
     if (startX === undefined) {
       startX = 330;
     }
+    if (!options) {
+      options = {};
+    }
+
+    var hueToLch = function(h) {
+      return this.hueToLch(options, h);
+    }.bind(this);
+
     var slices = fairSlicer(limit, 0, 360, startX);
-    return slices.map(this.hueToRgb.bind(this));
+    return slices.map(hueToLch);
   },
 
-  getColors: function(limit, startX) {
-    var rawColors = this.getRawColors(limit, startX);
-    return rawColors.map(this.rgbToCss);
+  getLabColors: function(limit, startX, options) {
+    var lchColors = this.getLchColors(limit, startX, options);
+    return lchColors.map(this.lchToLab);
+  },
+
+  getRgbColors: function(limit, startX, options) {
+    var labColors = this.getLabColors(limit, startX, options);
+    return labColors.map(this.labToRgb);
+  },
+
+  getColors: function(limit, startX, options) {
+    var rgbColors = this.getRgbColors(limit, startX, options);
+    return rgbColors.map(this.rgbToCss);
   }
 };
 
