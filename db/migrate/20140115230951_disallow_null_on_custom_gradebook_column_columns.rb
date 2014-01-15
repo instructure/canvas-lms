59a7930b3b2287accca4eb67457e79b64737e247
Cameron Matheson <cameron@instructure.com>
2014-01-15 16:20:35 -0700

    disallow NULL on custom gradebook column columns
    Change-Id: Ib6de3f7168a59d2be1d8663bb9901bcd102558a0
    Reviewed-on: https://gerrit.instructure.com/28667
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

---
+++ db/migrate/20140115230951_disallow_null_on_custom_gradebook_column_columns.rb
@@ -0,0 +1,21 @@
+class DisallowNullOnCustomGradebookColumnColumns < ActiveRecord::Migration
+  tag :predeploy
+
+  def self.allow_null(frd)
+    %w(position workflow_state course_id).each { |col|
+      change_column_null :custom_gradebook_columns, col, frd
+    }
+
+    %w(content user_id custom_gradebook_column_id).each { |col|
+      change_column_null :custom_gradebook_column_data, col, frd
+    }
+  end
+
+  def self.up
+    allow_null(false)
+  end
+
+  def self.down
+    allow_null(true)
+  end
+end
