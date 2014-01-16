9e576a9a76eea108045cb8d7d7cb2079f3a0fc01
Cody Cutrer <cody@instructure.com>
2014-01-16 13:19:52 -0700

    mark models returned from old versions as not new records
    rails 3 optimizes to not do certain operations on new records,
    breaking things
    
    Change-Id: I9dea310f042cc1713e1693b353bf8c35ffe3c840
    Reviewed-on: https://gerrit.instructure.com/28721
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: James Williams  <jamesw@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- vendor/plugins/simply_versioned/lib/version.rb
+++ vendor/plugins/simply_versioned/lib/version.rb
@@ -23,8 +23,9 @@ class Version < ActiveRecord::Base #:nodoc:
     obj = versionable_type.constantize.new
     YAML::load( self.yaml ).each do |var_name,var_value|
       # INSTRUCTURE:  added if... so that if a column is removed in a migration after this was versioned it doesen't die with NoMethodError: undefined method `some_column_name=' for ...
-      obj.__send__( "#{var_name}=", var_value ) if obj.respond_to?("#{var_name}=")
+       obj.write_attribute(var_name, var_value) if obj.class.columns_hash[var_name]
     end
+    obj.instance_variable_set(:@new_record, false)
     obj.simply_versioned_options[:on_load].try(:call, obj, self)
     # INSTRUCTURE: Added to allow model instances pulled out
     # of versions to still know their version number
