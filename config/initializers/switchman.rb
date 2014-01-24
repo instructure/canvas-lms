dfebdc049c192842d234fe22330ff23357cb7672
James Williams <jamesw@instructure.com>
2014-01-24 11:00:11 -0700

    implement encryption_key settings for rails 3 switchman shard
    Change-Id: I9637715c285890404d22d1f78c07def3ca7930e1
    Reviewed-on: https://gerrit.instructure.com/29271
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -7,10 +7,39 @@ unless CANVAS_RAILS2
     self.primary_key = "id"
     serialize :settings, Hash
 
+    before_save :encrypt_settings
+
     def settings
-      v = super
-      v = v.unserialize unless v.is_a?(Hash)
-      v || {}
+      s = super
+      if s.nil?
+        self.settings = s = {}
+      elsif !s.is_a?(Hash)
+        s = s.unserialize
+      end
+
+      salt = s.delete(:encryption_key_salt)
+      secret = s.delete(:encryption_key_enc)
+      if secret || salt
+        if secret && salt
+          s[:encryption_key] = Canvas::Security.decrypt_password(secret, salt, 'shard_encryption_key')
+        end
+        self.settings = s
+      end
+
+      s
+    end
+
+    def encrypt_settings
+      s = self.settings.dup
+      if encryption_key = s.delete(:encryption_key)
+        secret, salt = Canvas::Security.encrypt_password(encryption_key, 'shard_encryption_key')
+        s[:encryption_key_enc] = secret
+        s[:encryption_key_salt] = salt
+      end
+      if s != self.settings
+        self.settings = s
+      end
+      s
     end
   end
 
