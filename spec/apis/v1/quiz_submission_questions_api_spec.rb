c552184a9be7e3d3ffd7b91764717dda7564b60a
Cody Cutrer <cody@instructure.com>
2014-01-24 13:11:54 -0700

    spec: s/integration/request/
    Change-Id: I9158248a217d67a3184caed07744a3b8c6de469e
    Reviewed-on: https://gerrit.instructure.com/29285
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Cody Cutrer <cody@instructure.com>

--- spec/apis/v1/quiz_submission_questions_api_spec.rb
+++ spec/apis/v1/quiz_submission_questions_api_spec.rb
@@ -18,7 +18,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../api_spec_helper')
 
-describe QuizSubmissionQuestionsController, :type => :integration do
+describe QuizSubmissionQuestionsController, :type => :request do
   module Helpers
     def create_question(type, factory_options = {}, quiz=@quiz)
       factory = method(:"#{type}_question_data")
