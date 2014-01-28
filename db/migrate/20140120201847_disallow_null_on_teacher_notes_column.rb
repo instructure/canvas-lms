5c34efc44588883ca3ad17fcacbf958d426cfe3f
Cody Cutrer <cody@instructure.com>
2014-01-28 13:50:07 -0700

    do some minor datafixups before adding schema constraints
    Change-Id: Ic4e887b46a8035daafb0bc0491ead1f0fc52557c
    Reviewed-on: https://gerrit.instructure.com/29402
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20140120201847_disallow_null_on_teacher_notes_column.rb
+++ db/migrate/20140120201847_disallow_null_on_teacher_notes_column.rb
@@ -2,6 +2,7 @@ class DisallowNullOnTeacherNotesColumn < ActiveRecord::Migration
   tag :predeploy
 
   def self.up
+    CustomGradebookColumn.where(teacher_notes: nil).update_all(teacher_notes: false)
     change_column_null :custom_gradebook_columns, :teacher_notes, false
   end
 
