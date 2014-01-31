a9f131e55b0666d8db5d34220ff8b42c604ab65c
Raphael Weiner <rweiner@pivotallabs.com>
2014-01-31 08:50:37 -0700

    convert adheres to policy plugin into proper gem
    * vendoring gems to allow the gems that we own to reference a locked
    * down version in their gemspecs. Specifically, with the old version of
    * ActiveSupport that we were getting from the LTS Rails repo, there is
    * no way to tell bundler to install a gem that is a subdirectory of a
    * git repo.
    
    Change-Id: I32d9d0fae15da1fcf27f11e5cd8fca26af18d383
    Signed-off-by: David Julia <djulia@pivotallabs.com>
    Reviewed-on: https://gerrit.instructure.com/29438
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Stephan Hagemann <stephan@pivotallabs.com>
    Product-Review: Stephan Hagemann <stephan@pivotallabs.com>
    QA-Review: Stephan Hagemann <stephan@pivotallabs.com>

---
+++ gems/adheres_to_policy/adheres_to_policy.gemspec
@@ -0,0 +1,22 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "adheres_to_policy"
+  spec.version       = '0.0.1'
+  spec.authors       = ["Raphael Weiner", "Stephan Hagemann"]
+  spec.email         = ["rweiner@pivotallabs.com", "stephan@pivotallabs.com"]
+  spec.summary       = %q{The canvas adheres to policy gem}
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_dependency "rails"
+
+  spec.add_development_dependency "bundler", "~> 1.5"
+  spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
+end
---
+++ gems/adheres_to_policy/lib/adheres_to_policy/class_methods.rb
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2011 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module AdheresToPolicy #:nodoc:
+  module ClassMethods
+    # This stores the policy or permissions for a class.  It works like a
+    # macro.  The policy block will be stored in @policy_block.  Then, an
+    # instance will use that to instantiate a Policy object.
+    def set_policy(&block)
+      include InstanceMethods if @_policy_blocks.nil? || @_policy_blocks.empty?
+      @_policy = nil
+      @_policy_blocks ||= []
+      @_policy_blocks << block
+    end
+
+    alias :set_permissions :set_policy
+
+    def policy
+      return superclass.policy if @_policy_blocks.nil? || @_policy_blocks.empty?
+      return @_policy if @_policy
+      @_policy = Policy.new(*@_policy_blocks)
+    end
+  end
+end
