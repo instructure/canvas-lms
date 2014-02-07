3d52c37671395e1ad644e321c1c25b9789131084
Derek DeVries <ddevries@instructure.com>
2014-02-07 13:25:08 -0700

    fix rails3 spec that checks missing referer value
    Change-Id: I6c36fd2b5be9cad941f60f852b0124ca8720dd46
    Reviewed-on: https://gerrit.instructure.com/29807
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Ryan Florence <ryanf@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Derek DeVries <ddevries@instructure.com>
    QA-Review: Derek DeVries <ddevries@instructure.com>

--- spec/controllers/quiz_submissions_controller_spec.rb
+++ spec/controllers/quiz_submissions_controller_spec.rb
@@ -160,6 +160,7 @@ describe QuizSubmissionsController do
     end
 
     it "should redirect back to quiz after login if unauthorized" do
+      controller.request.env['HTTP_REFERER'] = 'http://test.host/'
       post 'record_answer', :quiz_id => @quiz.id, :course_id => @course.id, :id => @qsub.id, :a => 'test'
       assert_unauthorized
       session[:return_to].should_not be_nil
