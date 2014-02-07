0fef09857719aac1d32a3d6a165dffe8547f3280
Cody Cutrer <cody@instructure.com>
2014-02-06 17:29:28 -0700

    fix most assignments controller specs in rails 3
    Change-Id: Ib96144c0cdd4be1c9599dd722cfaa7d9fcd1cdcf
    Reviewed-on: https://gerrit.instructure.com/29778
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Ryan Florence <ryanf@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -30,7 +30,7 @@ describe AssignmentsController do
 
   describe "GET 'index'" do
     it "should throw 404 error without a valid context id" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       get 'index'
       assert_status(404)
@@ -127,7 +127,7 @@ describe AssignmentsController do
   
   describe "GET 'show'" do
     it "should return 404 on non-existant assignment" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       course_with_student_logged_in(:active_all => true)
 
@@ -202,7 +202,7 @@ describe AssignmentsController do
       assigns[:locked].should be_true
       # make sure that the show.html.erb template is rendered, because
       # in normal cases we redirect to the assignment's external_tool_tag.
-      response.rendered[:template].should eql 'assignments/show.html.erb'
+      response.should render_template('assignments/show')
     end
 
     it "should require login for external tools in a public course" do
@@ -230,7 +230,7 @@ describe AssignmentsController do
   describe "GET 'syllabus'" do
     it "should require authorization" do
       course_with_student
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       get 'syllabus', :course_id => @course.id
       assert_unauthorized
@@ -256,7 +256,7 @@ describe AssignmentsController do
 
   describe "GET 'new'" do
     it "should require authorization" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       course_with_student(:active_all => true)
       get 'new', :course_id => @course.id
@@ -276,7 +276,7 @@ describe AssignmentsController do
   
   describe "POST 'create'" do
     it "should require authorization" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       course_with_student(:active_all => true)
       post 'create', :course_id => @course.id
@@ -325,7 +325,7 @@ describe AssignmentsController do
   
   describe "GET 'edit'" do
     it "should require authorization" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       course_with_student(:active_all => true)
       course_assignment
@@ -357,7 +357,7 @@ describe AssignmentsController do
 
   describe "PUT 'update'" do
     it "should require authorization" do
-      rescue_action_in_public!
+      rescue_action_in_public! if CANVAS_RAILS2
       #controller.use_rails_error_handling!
       course_with_student(:active_all => true)
       course_assignment
