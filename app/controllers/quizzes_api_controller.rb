0d0631533396997e4198227f656d373437930944
Rob Orton <rob@instructure.com>
2014-02-11 16:23:38 -0700

    converting @object notation to @model part 9/11
    converting for:
    profile controller
    progress controller
    quiz questions controller
    quiz reports controller
    quizzes api controller
    
    Contributed by github.com/mcgachey
    
    Fixes SIS-146
    
    Currently, some API objects are documented using the @object tag, and
    others are documented using @model. The @model tag is preferred because
    it provides more information in the machine-readable Swagger documents
    generated by the API documentation build step. For example, the @object
    notation does not give a way to specify that a model property contains
    an instance of another type. This complete information is necessary in
    order to build an SDK generator.
    
    This change converts the @object notations into @model, adding missing
    type information and correcting inaccurate data (for example, dates were
    typed as "string" in the existing JSON documentation). Embedded types are
    also separated out so that their format can also be documented - this
    information was previously lost.
    
    The change also makes two minor fixes to the HTML and JSON documentation
    generators. The HTML generator is fixed so that a missing documentation
    string is ignored rather than being shown as an empty comment, and the
    JSON generator is modified so that the Model-level documentation and
    required attributes are properly provided.
    
    Note that the JSON generated here is not fully-complient to the Swagger
    format. Swagger does not support a Map structure, but the Canvas API
    uses (semi-)arbitrary key/value dictionaries in a few places. In those
    instances the properties are typed as "map", but this is not a part of the
    Swagger spec. Any cleaner alternative approaches here would be welcome.
    
    Change-Id: I0604516bb3d6cad94e99c98309db3ff6d0b2c19c
    Reviewed-on: https://gerrit.instructure.com/30003
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- app/controllers/quizzes_api_controller.rb
+++ app/controllers/quizzes_api_controller.rb
@@ -18,120 +18,175 @@
 
 # @API Quizzes
 #
-# @object Quiz
+# @model Quiz
 #     {
-#       // the ID of the quiz
-#       "id": 5,
-#
-#       // the title of the quiz
-#       "title": "Hamlet Act 3 Quiz",
-#
-#       // the HTTP/HTTPS URL to the quiz
-#       "html_url": "http://canvas.example.edu/courses/1/quizzes/2",
-#
-#       // a url suitable for loading the quiz in a mobile webview.  it will
-#       // persiste the headless session and, for quizzes in public courses, will
-#       // force the user to login
-#       "mobile_url": "http://canvas.example.edu/courses/1/quizzes/2?persist_healdess=1&force_user=1",
-#
-#       // the description of the quiz
-#       "description": "This is a quiz on Act 3 of Hamlet",
-#
-#       // type of quiz
-#       // possible values: "practice_quiz", "assignment", "graded_survey", "survey"
-#       "quiz_type": "assignment",
-#
-#       // the ID of the quiz's assignment group:
-#       "assignment_group_id": 3,
-#
-#       // quiz time limit in minutes
-#       "time_limit": 5,
-#
-#       // shuffle answers for students?
-#       "shuffle_answers": false,
-#
-#       // let students see their quiz responses?
-#       // possible values: null, "always", "until_after_last_attempt"
-#       "hide_results": "always",
-#
-#       // show which answers were correct when results are shown?
-#       // only valid if hide_results=null
-#       "show_correct_answers": true,
-#
-#       // when should the correct answers be visible by students?
-#       //
-#       // only valid if show_correct_answers=true
-#       "show_correct_answers_at": "2013-01-23T23:59:00-07:00",
-#
-#       // prevent the students from seeing correct answers after the specified
-#       // date has passed.
-#       //
-#       // only valid if show_correct_answers=true
-#       "hide_correct_answers_at": "2013-01-23T23:59:00-07:00",
-#
-#       // which quiz score to keep (only if allowed_attempts != 1)
-#       // possible values: "keep_highest", "keep_latest"
-#       "scoring_policy": "keep_highest",
-#
-#       // how many times a student can take the quiz
-#       // -1 = unlimited attempts
-#       "allowed_attempts": 3,
-#
-#       // show one question at a time?
-#       "one_question_at_a_time": false,
-#
-#       // the number of questions in the quiz
-#       "question_count": 12,
-# 
-#       // The total point value given to the quiz
-#       "points_possible": 20,
-#
-#       // lock questions after answering?
-#       // only valid if one_question_at_a_time=true
-#       "cant_go_back": false,
-#
-#       // access code to restrict quiz access
-#       "access_code": "2beornot2be",
-#
-#       // IP address or range that quiz access is limited to
-#       "ip_filter": "123.123.123.123",
-#
-#       // when the quiz is due
-#       "due_at": "2013-01-23T23:59:00-07:00",
-#
-#       // when to lock the quiz
-#       "lock_at": null,
-#
-#       // when to unlock the quiz
-#       "unlock_at": "2013-01-21T23:59:00-07:00",
-#
-#       // whether the quiz has a published or unpublished draft state.
-#       "published": true,
-#
-#       // Whether the assignment's "published" state can be changed to false.
-#       // Will be false if there are student submissions for the quiz.
-#       "unpublishable": true,
-#
-#       // Whether or not this is locked for the user.
-#       "locked_for_user": false,
-#
-#       // (Optional) Information for the user about the lock. Present when locked_for_user is true.
-#       "lock_info": {
-#         // Asset string for the object causing the lock
-#         "asset_string": "quiz_5",
-#
-#         // (Optional) Time at which this was/will be unlocked.
-#         "unlock_at": "2013-01-01T00:00:00-06:00",
-#
-#         // (Optional) Time at which this was/will be locked.
-#         "lock_at": "2013-02-01T00:00:00-06:00",
-#
-#         // (Optional) Context module causing the lock.
-#         "context_module": {}
-#       },
-#
-#       // (Optional) An explanation of why this is locked for the user. Present when locked_for_user is true.
-#       "lock_explanation": "This quiz is locked until September 1 at 12:00am"
+#       "id": "Quiz",
+#       "description": "",
+#       "properties": {
+#         "id": {
+#           "description": "the ID of the quiz",
+#           "example": 5,
+#           "type": "integer"
+#         },
+#         "title": {
+#           "description": "the title of the quiz",
+#           "example": "Hamlet Act 3 Quiz",
+#           "type": "string"
+#         },
+#         "html_url": {
+#           "description": "the HTTP/HTTPS URL to the quiz",
+#           "example": "http://canvas.example.edu/courses/1/quizzes/2",
+#           "type": "string"
+#         },
+#         "mobile_url": {
+#           "description": "a url suitable for loading the quiz in a mobile webview.  it will persiste the headless session and, for quizzes in public courses, will force the user to login",
+#           "example": "http://canvas.example.edu/courses/1/quizzes/2?persist_healdess=1&force_user=1",
+#           "type": "string"
+#         },
+#         "description": {
+#           "description": "the description of the quiz",
+#           "example": "This is a quiz on Act 3 of Hamlet",
+#           "type": "string"
+#         },
+#         "quiz_type": {
+#           "description": "type of quiz possible values: 'practice_quiz', 'assignment', 'graded_survey', 'survey'",
+#           "example": "assignment",
+#           "type": "string",
+#           "allowableValues": {
+#             "values": [
+#               "practice_quiz",
+#               "assignment",
+#               "graded_survey",
+#               "survey"
+#             ]
+#           }
+#         },
+#         "assignment_group_id": {
+#           "description": "the ID of the quiz's assignment group:",
+#           "example": 3,
+#           "type": "integer"
+#         },
+#         "time_limit": {
+#           "description": "quiz time limit in minutes",
+#           "example": 5,
+#           "type": "integer"
+#         },
+#         "shuffle_answers": {
+#           "description": "shuffle answers for students?",
+#           "example": false,
+#           "type": "boolean"
+#         },
+#         "hide_results": {
+#           "description": "let students see their quiz responses? possible values: null, 'always', 'until_after_last_attempt'",
+#           "example": "always",
+#           "type": "string",
+#           "allowableValues": {
+#             "values": [
+#               "always",
+#               "until_after_last_attempt"
+#             ]
+#           }
+#         },
+#         "show_correct_answers": {
+#           "description": "show which answers were correct when results are shown? only valid if hide_results=null",
+#           "example": true,
+#           "type": "boolean"
+#         },
+#         "show_correct_answers_at": {
+#           "description": "when should the correct answers be visible by students? only valid if show_correct_answers=true",
+#           "example": "2013-01-23T23:59:00-07:00",
+#           "type": "datetime"
+#         },
+#         "hide_correct_answers_at": {
+#           "description": "prevent the students from seeing correct answers after the specified date has passed. only valid if show_correct_answers=true",
+#           "example": "2013-01-23T23:59:00-07:00",
+#           "type": "datetime"
+#         },
+#         "scoring_policy": {
+#           "description": "which quiz score to keep (only if allowed_attempts != 1) possible values: 'keep_highest', 'keep_latest'",
+#           "example": "keep_highest",
+#           "type": "string",
+#           "allowableValues": {
+#             "values": [
+#               "keep_highest",
+#               "keep_latest"
+#             ]
+#           }
+#         },
+#         "allowed_attempts": {
+#           "description": "how many times a student can take the quiz -1 = unlimited attempts",
+#           "example": 3,
+#           "type": "integer"
+#         },
+#         "one_question_at_a_time": {
+#           "description": "show one question at a time?",
+#           "example": false,
+#           "type": "boolean"
+#         },
+#         "question_count": {
+#           "description": "the number of questions in the quiz",
+#           "example": 12,
+#           "type": "integer"
+#         },
+#         "points_possible": {
+#           "description": "The total point value given to the quiz",
+#           "example": 20,
+#           "type": "integer"
+#         },
+#         "cant_go_back": {
+#           "description": "lock questions after answering? only valid if one_question_at_a_time=true",
+#           "example": false,
+#           "type": "boolean"
+#         },
+#         "access_code": {
+#           "description": "access code to restrict quiz access",
+#           "example": "2beornot2be",
+#           "type": "string"
+#         },
+#         "ip_filter": {
+#           "description": "IP address or range that quiz access is limited to",
+#           "example": "123.123.123.123",
+#           "type": "string"
+#         },
+#         "due_at": {
+#           "description": "when the quiz is due",
+#           "example": "2013-01-23T23:59:00-07:00",
+#           "type": "datetime"
+#         },
+#         "lock_at": {
+#           "description": "when to lock the quiz",
+#           "type": "datetime"
+#         },
+#         "unlock_at": {
+#           "description": "when to unlock the quiz",
+#           "example": "2013-01-21T23:59:00-07:00",
+#           "type": "datetime"
+#         },
+#         "published": {
+#           "description": "whether the quiz has a published or unpublished draft state.",
+#           "example": true,
+#           "type": "boolean"
+#         },
+#         "unpublishable": {
+#           "description": "Whether the assignment's 'published' state can be changed to false. Will be false if there are student submissions for the quiz.",
+#           "example": true,
+#           "type": "boolean"
+#         },
+#         "locked_for_user": {
+#           "description": "Whether or not this is locked for the user.",
+#           "example": false,
+#           "type": "boolean"
+#         },
+#         "lock_info": {
+#           "description": "(Optional) Information for the user about the lock. Present when locked_for_user is true.",
+#           "$ref": "LockInfo"
+#         },
+#         "lock_explanation": {
+#           "description": "(Optional) An explanation of why this is locked for the user. Present when locked_for_user is true.",
+#           "example": "This quiz is locked until September 1 at 12:00am",
+#           "type": "string"
+#         }
+#       }
 #     }
 #
 class QuizzesApiController < ApplicationController
