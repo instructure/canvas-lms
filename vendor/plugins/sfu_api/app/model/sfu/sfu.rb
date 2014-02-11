f1e286d3b4d8ef0932a3b0214e9b7141d0e7f475
Graham Ballantyne <grahamb@sfu.ca>
2014-02-11 11:17:45 -0800

    Modify the test cluster plugin to allow whitelisted users to bypass the student lockout
    add a REST.text method to handle making non-JSON calls to AOBRestServer
    
    Add the maillist_membership URL to the SFU API
    
    add a SFU::User::maillist method to the SFU API
    
    Modify test cluster plugin to allow whitelisted users to bypass the student lockout

--- vendor/plugins/sfu_api/app/model/sfu/sfu.rb
+++ vendor/plugins/sfu_api/app/model/sfu/sfu.rb
@@ -79,6 +79,12 @@ module SFU
       def info(sfuid)
         REST.json(REST.account_url, "&username=#{sfuid}")
       end
+
+      def belongs_to_maillist?(username, maillist)
+        membership = REST.text(REST.maillist_membership_url, "&address=#{username}&listname=#{maillist}")
+        !(membership == '""')
+      end
+
     end
   end
 
