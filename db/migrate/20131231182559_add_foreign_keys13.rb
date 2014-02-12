78c7ff42a1c8f226123ba728a76628a3bf61d7eb
Cody Cutrer <cody@instructure.com>
2014-02-12 09:56:40 -0700

    more datafixup before installing FK
    Change-Id: If3c294a54d931498d811f171cbadbe9f8d3b2bf4
    Reviewed-on: https://gerrit.instructure.com/30035
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20131231182559_add_foreign_keys13.rb
+++ db/migrate/20131231182559_add_foreign_keys13.rb
@@ -14,6 +14,7 @@ class AddForeignKeys13 < ActiveRecord::Migration
     add_foreign_key_if_not_exists :content_migrations, :attachments, column: :exported_attachment_id, delay_validation: true
     add_foreign_key_if_not_exists :content_migrations, :attachments, column: :overview_attachment_id, delay_validation: true
     add_foreign_key_if_not_exists :context_module_progressions, :context_modules, delay_validation: true
+    DiscussionEntry.where("attachment_id IS NOT NULL AND NOT EXISTS (SELECT 1 FROM attachments WHERE attachment_id=attachments.id)").update_all(attachment_id: nil)
     add_foreign_key_if_not_exists :discussion_entries, :attachments, delay_validation: true
     add_foreign_key_if_not_exists :discussion_entries, :discussion_topics, delay_validation: true
     add_foreign_key_if_not_exists :discussion_entry_participants, :discussion_entries, delay_validation: true
@@ -24,9 +25,9 @@ class AddForeignKeys13 < ActiveRecord::Migration
     remove_foreign_key_if_exists :notification_policies, :communication_channels
     remove_foreign_key_if_exists :web_conference_participants, :web_conferences
     remove_foreign_key_if_exists :pseudonyms, :accounts
-    remove_foreign_key_if_exists :assessment_requests, :submissions, column: :asset_id
-    remove_foreign_key_if_exists :content_migrations, :attachments, column: :exported_attachment_id
-    remove_foreign_key_if_exists :content_migrations, :attachments, column: :overview_attachment_id
+    remove_foreign_key_if_exists :assessment_requests, column: :asset_id
+    remove_foreign_key_if_exists :content_migrations, column: :exported_attachment_id
+    remove_foreign_key_if_exists :content_migrations, column: :overview_attachment_id
     remove_foreign_key_if_exists :context_module_progressions, :context_modules
     remove_foreign_key_if_exists :discussion_entries, :attachments
     remove_foreign_key_if_exists :discussion_entries, :discussion_topics
