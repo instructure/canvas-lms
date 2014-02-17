1d936656f7e644f602b8b8e90d99d71bc356b5a9
Jacob Fugal <jacob@instructure.com>
2014-02-17 15:51:57 -0700

    make the codecogs equation base url a setting
    fixes CNVS-10049
    
    test-plan:
    
     * clear codecogs.equation_image_link setting
     * visit /equation_images/%5Csqrt%5B2%5D%7B900%7D%3Dx
     * should be redirected to http://latex.codecogs.com/gif.latex?%5Csqrt%5B2%5D%7B900%7D%3Dx
     * set codecogs.equation_image_link setting to "http://example.com/"
     * visit /equation_images/%5Csqrt%5B2%5D%7B900%7D%3Dx
     * should be redirected to http://example.com/?%5Csqrt%5B2%5D%7B900%7D%3Dx
    
    Change-Id: Ic683111ec4109bacdd5c06c633c70129ab688fcc
    Reviewed-on: https://gerrit.instructure.com/30212
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Jacob Fugal <jacob@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Tested-by: Cody Cutrer <cody@instructure.com>

--- app/controllers/equation_images_controller.rb
+++ app/controllers/equation_images_controller.rb
@@ -5,7 +5,8 @@ class EquationImagesController < ApplicationController
     # TODO: escape id here, and stop double escaping it in
     # public/javascripts/tinymce/jscripts/tiny_mce/plugins/instructure_equation/editor_plugin.js
     # this will require a corresponding data migration to fix
-    redirect_to 'http://latex.codecogs.com/gif.latex?' + params[:id]
+    base_url = Setting.get('codecogs.equation_image_link', 'http://latex.codecogs.com/gif.latex')
+    redirect_to base_url + '?' + params[:id]
   end
 
 end
