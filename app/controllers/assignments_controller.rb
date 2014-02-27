dfef9f1f64d7d5f0ed066f92cf32041332fb8e81
Michael Nomitch <mnomitch@instructure.com>
2014-02-27 14:30:50 -0600

    corrected breadcrumbs on draft state index
    fixes CNVS-11325
    
    test plan:
      - turn draft state on and go to the assignment index
      - click the breadcrumb for assignments
        - it should take you to the same page
    
    Change-Id: I35750202b1e0fe549b12ee9981d947987ce33240
    Reviewed-on: https://gerrit.instructure.com/31007
    Reviewed-by: Liz Abinante <labinante@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Mike Nomitch <mnomitch@instructure.com>

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -36,6 +36,7 @@ class AssignmentsController < ApplicationController
 
     if authorized_action(@context, @current_user, :read)
       return unless tab_enabled?(@context.class::TAB_ASSIGNMENTS)
+      add_crumb(t('#crumbs.assignments', "Assignments"), named_context_url(@context, :context_assignments_url))
 
       # It'd be nice to do this as an after_create, but it's not that simple
       # because of course import/copy.
