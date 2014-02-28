22ba4762e247ee7e3cd8d350ee8b09eaa620f09a
Derek DeVries <ddevries@instructure.com>
2014-02-28 13:05:44 -0700

    rails3: fix issues with polymorphic type overrides
    fixes:
    read_attribute works slightly different in rails3, which surfaced a bug
    with how polymorphic mappings swapped out the namespaced objects such as
    QuizSubmission being replaced by Quizzes::QuizSubmission.
    
    Change-Id: I91b6137e74d6015fd49bafa9f5c66a8f6e664ced
    Reviewed-on: https://gerrit.instructure.com/31096
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Josh Simpson <jsimpson@instructure.com>
    Product-Review: Derek DeVries <ddevries@instructure.com>
    QA-Review: Derek DeVries <ddevries@instructure.com>

--- app/models/polymorphic_type_override.rb
+++ app/models/polymorphic_type_override.rb
@@ -10,11 +10,13 @@ module PolymorphicTypeOverride
       @@polymorphic_type_mappings ||= {}
       @@polymorphic_type_mappings[self.name] = OverrideMapper.new(polymorphic_type_mappings)
 
-      @@polymorphic_type_mappings[self.name].each_with_mapping do |overridden, old_class, new_class|
+      @@polymorphic_type_mappings[self.name].each_with_mapping do |overridden, mapping|
         # define attr_reader for mapping[:type] e.g., ContentTag#content_type
         define_method overridden do
-          if self.instance_variable_get(:@attributes)[overridden.to_s] == old_class
-            return new_class
+          current_type = self.instance_variable_get(:@attributes)[overridden.to_s]
+
+          if mapping.keys.include? current_type
+            return mapping[current_type]
           end
 
           super()
@@ -24,7 +26,6 @@ module PolymorphicTypeOverride
       # define read_attribute
       define_method :read_attribute do |attr|
         mapper = @@polymorphic_type_mappings[self.class.name]
-
         if mapper.overrides?(attr)
           overriding = mapper.override_from_attributes(attr, self.instance_variable_get(:@attributes))
           return overriding if overriding
@@ -35,16 +36,18 @@ module PolymorphicTypeOverride
     end
 
     # override .shard_category_code_for_reflection here
-    def shard_category_code_for_reflection(reflection)
-      mapper = @@polymorphic_type_mappings[self.name]
+    if CANVAS_RAILS2
+      def shard_category_code_for_reflection(reflection)
+        mapper = @@polymorphic_type_mappings[self.name]
 
-      if mapper && reflection && reflection.options && reflection.options[:foreign_type]
-        if mapper.overrides?(reflection.options[:foreign_type])
-          const_hash = mapper.constants_mapping(reflection.options[:foreign_type])
-          return "#{const_hash}[self[:#{reflection.options[:foreign_type]}]].try(:constantize).try(:shard_category) || :default"
+        if mapper && reflection && reflection.options && reflection.options[:foreign_type]
+          if mapper.overrides?(reflection.options[:foreign_type])
+            const_hash = mapper.constants_mapping(reflection.options[:foreign_type])
+            return "#{const_hash}[self[:#{reflection.options[:foreign_type]}]].try(:constantize).try(:shard_category) || :default"
+          end
         end
+        super reflection
       end
-      super reflection
     end
 
     private
@@ -53,19 +56,19 @@ module PolymorphicTypeOverride
       attr_reader :mappings
 
       def initialize(mappings)
-        @mappings = mappings.symbolize_keys
+        @mappings = mappings.with_indifferent_access
       end
 
       def overrides?(type)
-        mappings.key?(type.to_sym)
+        mappings.key?(type)
       end
 
       def constants_mapping(type)
-        mappings[type.to_sym]
+        mappings[type]
       end
 
       def override_from_attributes(attr, attributes)
-        mapping = mappings[attr.to_sym]
+        mapping = mappings[attr]
         attr = attr.to_s
 
         mapping.each do |original_class, new_class|
@@ -79,9 +82,7 @@ module PolymorphicTypeOverride
 
       def each_with_mapping
         mappings.each do |attr, mapping|
-          mapping.each do |original_class, new_class|
-            yield attr, original_class, new_class
-          end
+          yield attr, mapping
         end
 
       end
