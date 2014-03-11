24680d9925a802ee7bbb48555abfb02b7dd62f4b
Joseph Rodriguez <jrodriguez@pivotallabs.com>
2014-03-11 10:57:00 -0600

    extract JSONToken gem
    fixes: CNVS-11785
    
    Test Plan:
    Automated tests will check most of it.  Make sure Canvas is not broken.
    
    Change-Id: I797a71b492a043fc7d14b88457842bbde4295bb2
    Reviewed-on: https://gerrit.instructure.com/31719
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>

--- gems/json_token/lib/json_token.rb
+++ gems/json_token/lib/json_token.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 Instructure, Inc.
+# Copyright (C) 2014 Instructure, Inc.
 #
 # This file is part of Canvas.
 #
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+
+require 'json'
+require 'base64'
 
 # Convenience methods for encoding and decoding a slug of data into base64
 # encoded JSON, e.g. to use in a URL
