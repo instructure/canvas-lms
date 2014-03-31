4498b890bdf50b6702b8fc54eadd0c7573638298
Shawn Meredith <shawn@instructure.com>
2014-03-31 08:38:35 +0000

    Revert "spec: selenium 2.40 firefox 2.70 upgrades"
    This reverts commit de462a82d2682f8d70cce4f54f72e88523c9e461.
    
    Change-Id: I70f6ae29a4e8955b84d2be03672b3a7c9c1a6bd3
    Reviewed-on: https://gerrit.instructure.com/32575
    Reviewed-by: Shawn Meredith <shawn@instructure.com>
    Product-Review: Shawn Meredith <shawn@instructure.com>
    QA-Review: Shawn Meredith <shawn@instructure.com>
    Tested-by: Shawn Meredith <shawn@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -17,8 +17,8 @@ group :test do
     gem 'rspec-rails', '2.14.1'
   end
   gem 'sequel', '4.5.0', :require => false
-  gem 'selenium-webdriver', '2.40.0'
-  gem 'childprocess', '0.5.0'
+  gem 'selenium-webdriver', '2.39.0'
+  gem 'childprocess', '0.4.0'
   gem 'websocket', '1.0.7'
   gem 'webmock', '1.16.1', :require => false
   gem 'addressable', '2.3.5'
