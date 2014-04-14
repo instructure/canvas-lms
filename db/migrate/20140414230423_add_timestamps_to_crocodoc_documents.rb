8ae4e0dfcb7bf6c74631743ef3f2cf444f23215b
Cameron Matheson <cameron@instructure.com>
2014-04-14 17:09:54 -0600

    add timestamps to crocodoc_documents
    Change-Id: Ib1226b0aed4be89456b5c8f2ca250318ec254b13
    Reviewed-on: https://gerrit.instructure.com/33317
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

---
+++ db/migrate/20140414230423_add_timestamps_to_crocodoc_documents.rb
@@ -0,0 +1,12 @@
+class AddTimestampsToCrocodocDocuments < ActiveRecord::Migration
+  tag :predeploy
+
+  def self.up
+    add_timestamps(:crocodoc_documents)
+  end
+
+  def self.down
+    remove_column :crocodoc_documents, :created_at
+    remove_column :crocodoc_documents, :updated_at
+  end
+end
