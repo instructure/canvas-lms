a2c548114652c10aa5c1d3f1e9b02b2b8edc274d
Jacob Fugal <jacob@instructure.com>
2014-04-16 15:19:20 -0600

    don't localized datetimes formatted for datepicker
    refs CNVS-12398
    
    the eventual complete solution will involve teaching the date picker to
    parse the localized datetime, but for now, just format the time in
    english if the target is a datepicker's field value.
    
    test-plan:
     - set locale to portuguese
     - datepickers (e.g. assignment due date when editing) populated on page
       load should contain the date time in english (e.g. "<date> at 3:00pm"
       vs. "<date> em 3:00")
     - using the date picker should produce a time in english in the field
     - saving the english time should work
     - non-datepicker field values with the same formatting rules (e.g.
       assignment due date when *not* editing) should display localized
    
    Change-Id: Ibc69d2066518b4875c1f42b794df784c7e70c2c7
    Reviewed-on: https://gerrit.instructure.com/33444
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Reviewed-by: Anthus Williams <awilliams@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1539,8 +1539,8 @@ class CoursesController < ApplicationController
       authorized_action(@context, @current_user, :read_as_admin) &&
       authorized_action(@domain_root_account.manually_created_courses_account, @current_user, [:create_courses, :manage_courses])
     # For prepopulating the date fields
-    js_env(:OLD_START_DATE => datetime_string(@context.start_at, :verbose, nil, true))
-    js_env(:OLD_END_DATE => datetime_string(@context.conclude_at, :verbose, nil, true))
+    js_env(:OLD_START_DATE => unlocalized_datetime_string(@context.start_at, :verbose))
+    js_env(:OLD_END_DATE => unlocalized_datetime_string(@context.conclude_at, :verbose))
   end
 
   def copy_course
