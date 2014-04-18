e6b6058080db381547eeb394689c15b586a10eb4
Mark Severson <markse@instructure.com>
2014-04-18 15:05:55 -0600

    fix lock_version migration for module progressions
    test plan:
     * delayed job gets triggered by database migrations
    
    fixes CNVS-12544
    
    Change-Id: Ia281dc152689124150977d6cce35cc1062c3e88a
    Reviewed-on: https://gerrit.instructure.com/33611
    QA-Review: Clare Strong <clare@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- db/migrate/20140418210000_populate_lock_version_on_context_module_progressions.rb
+++ db/migrate/20140418210000_populate_lock_version_on_context_module_progressions.rb
@@ -1,7 +1,7 @@
 class PopulateLockVersionOnContextModuleProgressions < ActiveRecord::Migration
   tag :postdeploy
 
-  def up
+  def self.up
     DataFixup::PopulateLockVersionOnContextModuleProgressions.send_later_if_production_enqueue_args(:run, :priority => Delayed::LOW_PRIORITY, :max_attempts => 1)
   end
 end
