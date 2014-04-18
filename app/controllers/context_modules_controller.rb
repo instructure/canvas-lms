72cf1beff55bef3adb8c04112e3ea7438ce353db
James Williams <jamesw@instructure.com>
2014-04-18 07:15:26 -0600

    fix caching issue when moving module item into an empty module
    if you drag a module item into an empty module, the module is
    currently not being touched after the module item is saved,
    leaving the cached html content for the module showing no
    module items
    
    fixes #CNVS-12460
    
    Change-Id: I4820018fd52e3862a9b891fa2bbe2d15b4905df1
    Reviewed-on: https://gerrit.instructure.com/33554
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>

--- app/controllers/context_modules_controller.rb
+++ app/controllers/context_modules_controller.rb
@@ -255,7 +255,7 @@ class ContextModulesController < ApplicationController
     if authorized_action(@module, @current_user, :update)
       order = params[:order].split(",").map{|id| id.to_i}
       tags = @context.context_module_tags.not_deleted.find_all_by_id(order).compact
-      affected_module_ids = tags.map(&:context_module_id).uniq.compact
+      affected_module_ids = (tags.map(&:context_module_id) + [@module.id]).uniq.compact
       affected_items = []
       items = order.map{|id| tags.detect{|t| t.id == id.to_i } }.compact.uniq
       items.each_with_index do |item, idx|
