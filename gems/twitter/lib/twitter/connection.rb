302f2bd6b49649dbd773ab598e942995e02336ee
Joseph Rodriguez <jrodriguez@pivotallabs.com>
2014-04-25 14:14:00 -0600

    fixed code indentation
    fixes: CNVS-12683
    
    test plan:
    - code formatting only, no qa needed
    
    Change-Id: I9e52687c886c6ca3ff2e1f92ae3c9f0733375dd1
    Reviewed-on: https://gerrit.instructure.com/33869
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- gems/twitter/lib/twitter/connection.rb
+++ gems/twitter/lib/twitter/connection.rb
@@ -17,107 +17,107 @@
 #
 
 module Twitter
-class Connection
-  def initialize(oauth_access_token, oauth_access_token_secret)
-    @oauth_access_token = oauth_access_token
-    @oauth_access_token_secret = oauth_access_token_secret
-  end
-
-  def retrieve_access_token
-    consumer = self.class.twitter_consumer
-    return nil unless consumer
-    @access_token ||= OAuth::AccessToken.new(consumer, @oauth_access_token, @oauth_access_token_secret)
-  end
-
-  def get_service_user(access_token)
-    credentials = JSON.parse(access_token.get('/1.1/account/verify_credentials.json').body)
-    service_user_id = credentials["id"]
-    service_user_name = credentials["screen_name"]
-    return service_user_id, service_user_name
-  end
-
-
-  def get_access_token(token, secret, oauth_verifier) #  oauth_request, oauth_verifier, session)
-    request_token = OAuth::RequestToken.new(self.class.twitter_consumer, token, secret)
-    request_token.get_access_token(:oauth_verifier => oauth_verifier)
-  end
-
-  def self.request_token(success_url)
-    consumer = twitter_consumer
-    consumer.get_request_token(:oauth_callback => success_url)
-  end
-
-  def twitter_send(message, access_token=nil)
-    access_token ||= retrieve_access_token
-    response = access_token.post("/1.1/statuses/update.json", {:status => message})
-    res = JSON.parse(response.body)
-    res
-  end
-
-  def send_direct_message(user_name, user_id, message)
-    access_token = retrieve_access_token
-    response = access_token.post("/1.1/direct_messages/new.json", {:screen_name => user_name, :user_id => user_id, :text => message})
-    res = JSON.parse(response.body)
-    res
-  end
-
-  def list(access_token=nil, since_id=nil)
-    access_token ||= twitter_retrieve_access_token
-    url = "/1.1/statuses/user_timeline.json"
-    url += "?since_id=#{since_id}" if since_id
-    response = access_token.get(url)
-    case response
-      when Net::HTTPSuccess
-        return JSON.parse(response.body) rescue []
-      else
-        data = JSON.parse(response.body) rescue nil
-        if data && data['error'] && data['error'].match(/requires authentication/)
-          @service.destroy if @service
-          @twitter_service.destroy if @twitter_service
-        end
-        ErrorReport.log_error(:processing, {
-          :backtrace => "Retrieving twitter list for #{@twitter_service.inspect}",
-          :response => response.inspect,
-          :body => response.body,
-          :message => response['X-RateLimit-Reset'],
-          :url => url
-        })
-        retry_after = (response['X-RateLimit-Reset'].to_i - Time.now.utc.to_i) rescue 0 #response['Retry-After'].to_i rescue 0
-        raise "Retry After #{retry_after}"
+  class Connection
+    def initialize(oauth_access_token, oauth_access_token_secret)
+      @oauth_access_token = oauth_access_token
+      @oauth_access_token_secret = oauth_access_token_secret
     end
-    res
-  end
 
-  def self.twitter_consumer(key=nil, secret=nil)
-    require 'oauth'
-    require 'oauth/consumer'
-    twitter_config = Twitter::Connection.config
-    key ||= twitter_config['api_key']
-    secret ||= twitter_config['secret_key']
-    OAuth::Consumer.new(key, secret, {
-      :site => "https://api.twitter.com",
-      :request_token_path => "/oauth/request_token",
-      :access_token_path => "/oauth/access_token",
-      :authorize_path => "/oauth/authorize",
-      :signature_method => "HMAC-SHA1"
-    })
-  end
-
-  def self.config_check(settings)
-    consumer = self.twitter_consumer(settings[:api_key], settings[:secret_key])
-    token = consumer.get_request_token rescue nil
-    token ? nil : "Configuration check failed, please check your settings"
-  end
-
-  def self.config=(config)
-    if !config.is_a?(Proc)
-      raise "Config must be a Proc"
+    def retrieve_access_token
+      consumer = self.class.twitter_consumer
+      return nil unless consumer
+      @access_token ||= OAuth::AccessToken.new(consumer, @oauth_access_token, @oauth_access_token_secret)
     end
-    @config = config
-  end
 
-  def self.config
-    @config.call()
+    def get_service_user(access_token)
+      credentials = JSON.parse(access_token.get('/1.1/account/verify_credentials.json').body)
+      service_user_id = credentials["id"]
+      service_user_name = credentials["screen_name"]
+      return service_user_id, service_user_name
+    end
+
+
+    def get_access_token(token, secret, oauth_verifier) #  oauth_request, oauth_verifier, session)
+      request_token = OAuth::RequestToken.new(self.class.twitter_consumer, token, secret)
+      request_token.get_access_token(:oauth_verifier => oauth_verifier)
+    end
+
+    def self.request_token(success_url)
+      consumer = twitter_consumer
+      consumer.get_request_token(:oauth_callback => success_url)
+    end
+
+    def twitter_send(message, access_token=nil)
+      access_token ||= retrieve_access_token
+      response = access_token.post("/1.1/statuses/update.json", {:status => message})
+      res = JSON.parse(response.body)
+      res
+    end
+
+    def send_direct_message(user_name, user_id, message)
+      access_token = retrieve_access_token
+      response = access_token.post("/1.1/direct_messages/new.json", {:screen_name => user_name, :user_id => user_id, :text => message})
+      res = JSON.parse(response.body)
+      res
+    end
+
+    def list(access_token=nil, since_id=nil)
+      access_token ||= twitter_retrieve_access_token
+      url = "/1.1/statuses/user_timeline.json"
+      url += "?since_id=#{since_id}" if since_id
+      response = access_token.get(url)
+      case response
+        when Net::HTTPSuccess
+          return JSON.parse(response.body) rescue []
+        else
+          data = JSON.parse(response.body) rescue nil
+          if data && data['error'] && data['error'].match(/requires authentication/)
+            @service.destroy if @service
+            @twitter_service.destroy if @twitter_service
+          end
+          ErrorReport.log_error(:processing, {
+            :backtrace => "Retrieving twitter list for #{@twitter_service.inspect}",
+            :response => response.inspect,
+            :body => response.body,
+            :message => response['X-RateLimit-Reset'],
+            :url => url
+          })
+          retry_after = (response['X-RateLimit-Reset'].to_i - Time.now.utc.to_i) rescue 0 #response['Retry-After'].to_i rescue 0
+          raise "Retry After #{retry_after}"
+      end
+      res
+    end
+
+    def self.twitter_consumer(key=nil, secret=nil)
+      require 'oauth'
+      require 'oauth/consumer'
+      twitter_config = Twitter::Connection.config
+      key ||= twitter_config['api_key']
+      secret ||= twitter_config['secret_key']
+      OAuth::Consumer.new(key, secret, {
+        :site => "https://api.twitter.com",
+        :request_token_path => "/oauth/request_token",
+        :access_token_path => "/oauth/access_token",
+        :authorize_path => "/oauth/authorize",
+        :signature_method => "HMAC-SHA1"
+      })
+    end
+
+    def self.config_check(settings)
+      consumer = self.twitter_consumer(settings[:api_key], settings[:secret_key])
+      token = consumer.get_request_token rescue nil
+      token ? nil : "Configuration check failed, please check your settings"
+    end
+
+    def self.config=(config)
+      if !config.is_a?(Proc)
+        raise "Config must be a Proc"
+      end
+      @config = config
+    end
+
+    def self.config
+      @config.call()
+    end
   end
-end
 end
--- gems/twitter/lib/twitter/messenger.rb
+++ gems/twitter/lib/twitter/messenger.rb
@@ -1,33 +1,33 @@
 module Twitter
-class Messenger
-  attr_reader :message
-  attr_reader :host
-  attr_reader :id
+  class Messenger
+    attr_reader :message
+    attr_reader :host
+    attr_reader :id
 
-  def initialize(message, twitter_service, host, id)
-    @message = message
-    @twitter_service = twitter_service
-    @host = host
-    @id = id
-  end
+    def initialize(message, twitter_service, host, id)
+      @message = message
+      @twitter_service = twitter_service
+      @host = host
+      @id = id
+    end
 
-  def deliver
-    return unless @twitter_service
-    twitter_connection = Twitter::Connection.new(@twitter_service.token, @twitter_service.secret)
-    twitter_connection.send_direct_message(@twitter_service.service_user_name, @twitter_service.service_user_id, "#{body}")
-  end
+    def deliver
+      return unless @twitter_service
+      twitter_connection = Twitter::Connection.new(@twitter_service.token, @twitter_service.secret)
+      twitter_connection.send_direct_message(@twitter_service.service_user_name, @twitter_service.service_user_id, "#{body}")
+    end
 
-  def url
-    message.main_link || message.url || "http://#{host}/mr/#{id}"
-  end
+    def url
+      message.main_link || message.url || "http://#{host}/mr/#{id}"
+    end
 
-  def body
-    truncated_body = HtmlTextHelper.strip_and_truncate(message.body, :max_length => (139 - url.length))
-    "#{truncated_body} #{url}"
-  end
+    def body
+      truncated_body = HtmlTextHelper.strip_and_truncate(message.body, :max_length => (139 - url.length))
+      "#{truncated_body} #{url}"
+    end
 
-  def asset_context
-    @message.asset_context
+    def asset_context
+      @message.asset_context
+    end
   end
-end
 end
