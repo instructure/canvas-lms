3b6fb6c5e66d271d7473bb8ef03408df0931a380
James Williams <jamesw@instructure.com>
2014-04-30 09:51:53 -0600

    rename outcome_gradebook feature flag
    fixes #CNVS-11653
    
    Change-Id: I8037521bd6995228271f8bcb66692923d5a779a8
    Reviewed-on: https://gerrit.instructure.com/34048
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>

--- lib/feature.rb
+++ lib/feature.rb
@@ -129,9 +129,9 @@ END
     },
     'outcome_gradebook' =>
     {
-      display_name: -> { I18n.t('features.outcome_gradebook', 'Outcome Gradebook') },
-      description:  -> { I18n.t('outcome_gradebook_description', <<-END) },
-Outcome Gradebook provides a way for teachers to quickly view student and course
+      display_name: -> { I18n.t('features.learning_mastery_gradebook', 'Learning Mastery Gradebook') },
+      description:  -> { I18n.t('learning_mastery_gradebook_description', <<-END) },
+Learning Mastery Gradebook provides a way for teachers to quickly view student and course
 progress on course learning outcomes. Outcomes are presented in a Gradebook-like
 format and student progress is displayed both as a numerical score and as mastered/near
 mastery/remedial.
