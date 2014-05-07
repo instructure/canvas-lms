d1a60d2cea67ec4a32dd8a9aedd3b08d2ad1e00d
Rob Orton <rob@instructure.com>
2014-05-07 13:58:15 -0600

    validate length of user_notes.title
    fixes CNVS-7111
    
    test plan
     - UserNote title should not
       throw database error when over 255 characters
    
    Change-Id: I18366c1ec5a9e2434d2c6cc6638ea74803e05970
    Reviewed-on: https://gerrit.instructure.com/34494
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- app/models/user_note.rb
+++ app/models/user_note.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 - 2013 Instructure, Inc.
+# Copyright (C) 2011 - 2014 Instructure, Inc.
 #
 # This file is part of Canvas.
 #
@@ -27,6 +27,7 @@ class UserNote < ActiveRecord::Base
 
   validates_presence_of :user_id, :created_by_id, :workflow_state
   validates_length_of :note, :maximum => maximum_text_length, :allow_nil => true, :allow_blank => true
+  validates_length_of :title, :maximum => maximum_string_length, :allow_nil => true, :allow_blank => true
   after_save :update_last_user_note
 
   sanitize_field :note, CanvasSanitize::SANITIZE
