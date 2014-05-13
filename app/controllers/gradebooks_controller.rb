8eb1df70405d4998596c09fad8b8c250e453a13f
Tyler Pickett <tpickett+gerrit@instructure.com>
2014-05-13 12:24:39 -0500

    update student grade listing icons for submissions
    fixes CNVS-12336
    
    Test Plan:
    
    - As a teacher create a course with an assignment of each online
      submission type
    - Log in as a student and submit each assignment
    - Select the student grade book
      - The icons should be updated to match the style guide
    - As a teacher grade some of the assignments
    - As the student refresh the grades page (this requires a server
      restart if done locally because of CNVS-12995)
      - The grades should still be displayed correctly (we're checking
        this because there were significant refactors of this display)
    
    Change-Id: I7aa61643eae86a98b735a18414d9f3a58eddde62
    Reviewed-on: https://gerrit.instructure.com/34817
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Amber Taniuchi <amber@instructure.com>
    Reviewed-by: Liz Abinante <labinante@instructure.com>
    Product-Review: Hilary Scharton <hilary@instructure.com>

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 Instructure, Inc.
+# Copyright (C) 2012 - 2014 Instructure, Inc.
 #
 # This file is part of Canvas.
 #
--- spec/helpers/gradebooks_helper_spec.rb
+++ spec/helpers/gradebooks_helper_spec.rb
@@ -91,4 +91,103 @@ describe GradebooksHelper do
       end
     end
   end
+
+  describe '#student_score_display_for(submission, can_manage_grades)' do
+    FakeSubmission = Struct.new(:assignment, :score, :grade, :submission_type)
+    FakeAssignment = Struct.new(:grading_type)
+
+    let(:submission) { FakeSubmission.new(assignment) }
+    let(:assignment) { FakeAssignment.new }
+
+    let(:score_display) { helper.student_score_display_for(submission) }
+    let(:parsed_display) { Nokogiri::HTML.parse(score_display) }
+    let(:score_icon) { parsed_display.at_css('i') }
+    let(:score_screenreader_text) { parsed_display.at_css('.screenreader-only').text }
+
+    context 'when the supplied submission is nil' do
+      it 'must return a dash' do
+        score = helper.student_score_display_for(nil)
+        score.should == '-'
+      end
+    end
+
+    context 'when the submission has been graded' do
+      before do
+        submission.score = 1
+        submission.grade = 1
+      end
+
+      context 'and the assignment is graded pass-fail' do
+        before do
+          assignment.grading_type = 'pass_fail'
+        end
+
+        context 'with a passing grade' do
+          before do
+            submission.score = 1
+          end
+
+          it 'must give us a check icon' do
+            score_icon['class'].should include 'icon-check'
+          end
+
+          it 'must indicate the assignment is complete via alt text' do
+            score_screenreader_text.should include 'Complete'
+          end
+        end
+
+        context 'with a faililng grade' do
+          before do
+            submission.grade = 'incomplete'
+            submission.score = nil
+          end
+
+          it 'must give us a check icon' do
+            score_icon['class'].should include 'icon-x'
+          end
+
+          it 'must indicate the assignment is complete via alt text' do
+            score_screenreader_text.should include 'Incomplete'
+          end
+        end
+      end
+
+      context 'and the assignment is a percentage grade' do
+        it 'must output the percentage' do
+          assignment.grading_type = 'percent'
+          submission.grade = '42.5%'
+          score_display.should == '42.5%'
+        end
+      end
+
+      context 'and the grade field matches the score field' do
+        it 'must output the grade rounded to two decimal points' do
+          submission.grade = '42.3542'
+          submission.score = 42.3542
+          score_display.should == 42.35
+        end
+      end
+    end
+
+    context 'when the submission is ungraded' do
+      before do
+        submission.score = nil
+        submission.grade = nil
+      end
+
+      context 'and the submission is an online submission type' do
+        it 'must output an appropriate icon' do
+          submission.submission_type = 'online_quiz'
+          score_icon['class'].should include 'submission_icon'
+        end
+      end
+
+      context 'and the submission is some unknown type' do
+        it 'must output a dash' do
+          submission.submission_type = 'bogus_type'
+          score_display.should == '-'
+        end
+      end
+    end
+  end
 end
