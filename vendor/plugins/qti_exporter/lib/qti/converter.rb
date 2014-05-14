1d7f77d4fcd42eed1a9b095c8a0c1195eec30b70
James Williams <jamesw@instructure.com>
2014-05-14 11:31:29 -0600

    export canvas meta data in qti exports
    test plan:
    * create a quiz with a description
    * export the quiz in a qti export
    * import into another course
    * should retain the description
    
    fixes #CNVS-8852
    
    Change-Id: I454db916692b6bc58b908322301d5956c99d9901
    Reviewed-on: https://gerrit.instructure.com/34886
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>

--- vendor/plugins/qti_exporter/lib/qti/converter.rb
+++ vendor/plugins/qti_exporter/lib/qti/converter.rb
@@ -3,6 +3,8 @@ require_dependency 'qti/respondus_settings'
 module Qti
 class Converter < Canvas::Migration::Migrator
 
+  include CC::Importer::Canvas::QuizMetadataConverter
+
   MANIFEST_FILE = "imsmanifest.xml"
   QTI_2_1_URL = 'http://www.imsglobal.org/xsd/imsqti_v2p1'
   QTI_2_0_URL = 'http://www.imsglobal.org/xsd/imsqti_v2p0'
@@ -39,6 +41,13 @@ class Converter < Canvas::Migration::Migrator
     path_map = @course[:file_map].values.inject({}){|h, v| h[v[:path_name]] = v[:migration_id]; h }
     @course[:assessment_questions] = convert_questions(:file_path_map => path_map, :flavor => @flavor)
     @course[:assessments] = convert_assessments(@course[:assessment_questions][:assessment_questions])
+
+    original_manifest_path = File.join(@unzipped_file_path, MANIFEST_FILE)
+    if File.exists?(original_manifest_path)
+      @manifest = Nokogiri::XML(File.open(original_manifest_path))
+      post_process_assessments # bring in canvas metadata if available
+    end
+
     @course[:files_import_root_path] = unique_quiz_dir
 
     if settings[:apply_respondus_settings_file]
