8c70c50fc48087921665eb9f8cb4b964d16f9500
Braden Anderson <banderson@instructure.com>
2014-05-23 12:21:30 -0600

    fix and rerun discussion_topics.group_category population
    test plan:
      * run migrations
    
    Change-Id: Idfb81d3c8524002a7222245896c196651dac07a6
    Reviewed-on: https://gerrit.instructure.com/35405
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Trevor deHaan <tdehaan@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Product-Review: Braden Anderson <banderson@instructure.com>

--- db/migrate/20140523164418_populate_group_category_on_discussion_topics.rb
+++ db/migrate/20140523164418_populate_group_category_on_discussion_topics.rb
@@ -1,7 +1,7 @@
 class PopulateGroupCategoryOnDiscussionTopics < ActiveRecord::Migration
   tag :postdeploy
 
-  def up
+  def self.up
     DataFixup::PopulateGroupCategoryOnDiscussionTopics.send_later_if_production_enqueue_args(:run, :priority => Delayed::LOW_PRIORITY, :max_attempts => 1)
   end
 end
