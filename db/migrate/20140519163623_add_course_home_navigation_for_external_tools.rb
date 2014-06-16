56aee2cfc1db79c4bc7512e55a7f09bc56230163
Jon Willesen <jonw@instructure.com>
2014-06-16 16:16:37 -0600

    add concurrently to recent add_index migration
    Change-Id: If3cd851f83315a7bc7484fbd9948aebcd356eb2a
    Reviewed-on: https://gerrit.instructure.com/36518
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20140519163623_add_course_home_navigation_for_external_tools.rb
+++ db/migrate/20140519163623_add_course_home_navigation_for_external_tools.rb
@@ -1,8 +1,10 @@
 class AddCourseHomeNavigationForExternalTools < ActiveRecord::Migration
   tag :predeploy
+  disable_ddl_transaction!
+
   def self.up
     add_column :context_external_tools, :has_course_home_sub_navigation, :boolean
-    add_index :context_external_tools, [:context_id, :context_type, :has_course_home_sub_navigation], :name => "external_tools_course_home_sub_navigation"
+    add_index :context_external_tools, [:context_id, :context_type, :has_course_home_sub_navigation], :name => "external_tools_course_home_sub_navigation", :algorithm => :concurrently
   end
 
   def self.down
