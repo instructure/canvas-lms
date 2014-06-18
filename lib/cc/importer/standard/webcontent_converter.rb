e68c2484ee7a6f70e7be35230c6083d52549086a
James Williams <jamesw@instructure.com>
2014-06-18 07:02:47 -0600

    fix webcontent converter zip file error
    test plan:
    * import the package referenced in the ticket
    * should not fail
    
    fixes #CNVS-13425
    
    Change-Id: I2663d4479824b90e80d7421f2e4ff6975f6731a6
    Reviewed-on: https://gerrit.instructure.com/36588
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>
    Product-Review: Bracken Mosbacker <bracken@instructure.com>
    QA-Review: Nathan Rogowski <nathan@instructure.com>

--- lib/cc/importer/standard/webcontent_converter.rb
+++ lib/cc/importer/standard/webcontent_converter.rb
@@ -58,13 +58,15 @@ module CC::Importer::Standard
 
       Zip::File.open(zip_file, 'w') do |zipfile|
         file_map.each_value do |val|
+          next if zipfile.entries.include?(val[:path_name])
+
           file_path = File.join(@unzipped_file_path, val[:path_name])
           if File.exists?(file_path)
-            zipfile.add(val[:path_name], file_path)
+            zipfile.add(val[:path_name], file_path) if !File.directory?(file_path)
           else
             web_file_path = File.join(@unzipped_file_path, WEB_RESOURCES_FOLDER, val[:path_name])
             if File.exists?(web_file_path)
-              zipfile.add(val[:path_name], web_file_path)
+              zipfile.add(val[:path_name], web_file_path) if !File.directory?(web_file_path)
             else
               val[:errored] = true
             end
