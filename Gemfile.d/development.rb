3ab3603c0f48a8c0a1272e0b04916540e9ae232e
Cody Cutrer <cody@instructure.com>
2014-06-23 15:05:10 -0600

    update byebug
    Change-Id: I556531bc8a959586161768a1fa8723b171bbedce
    Reviewed-on: https://gerrit.instructure.com/36753
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Nick Cloward <ncloward@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/development.rb
+++ Gemfile.d/development.rb
@@ -15,7 +15,7 @@ group :development do
   # The ruby debug gems conflict with the IDE-based debugger gem.
   # Set this option in your dev environment to disable.
   unless ENV['DISABLE_RUBY_DEBUGGING']
-    gem 'byebug', '2.4.1', :platforms => [:ruby_20, :ruby_21]
+    gem 'byebug', '3.1.2', :platforms => [:ruby_20, :ruby_21]
     gem 'debugger', '1.6.6', :platforms => :ruby_19
   end
 end
