93dcb5da85f08bd5d7994735102b3d250eb5bce0
Cody Cutrer <cody@instructure.com>
2014-06-26 16:17:05 -0600

    fix switchman initializer for class reloading
    Change-Id: I39477220c0f1751ae245d504a80bfcc986493fa2
    Reviewed-on: https://gerrit.instructure.com/36957
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -1,109 +1,116 @@
-unless CANVAS_RAILS2  
-  Switchman::Shard.class_eval do
-    class << self
-      alias :birth :default
+unless CANVAS_RAILS2
+  Rails.application.config.to_prepare do
+    Switchman::Shard.class_eval do
+      class << self
+        alias :birth :default
 
-      def current_with_delayed_jobs(category=:default)
-        if category == :delayed_jobs
-          active_shards[category] || current_without_delayed_jobs(:default).delayed_jobs_shard
-        else
-          current_without_delayed_jobs(category)
+        def current_with_delayed_jobs(category=:default)
+          if category == :delayed_jobs
+            active_shards[category] || current_without_delayed_jobs(:default).delayed_jobs_shard
+          else
+            current_without_delayed_jobs(category)
+          end
+        end
+        alias_method_chain :current, :delayed_jobs
+
+        def activate_with_delayed_jobs!(categories)
+          if !categories[:delayed_jobs] && categories[:default]
+            categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
+          end
+          activate_without_delayed_jobs!(categories)
         end
       end
-      alias_method_chain :current, :delayed_jobs
 
-      def activate_with_delayed_jobs!(categories)
-        if !categories[:delayed_jobs] && categories[:default]
-          categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
+      self.primary_key = "id"
+      reset_column_information # make sure that the id column object knows it is the primary key
+
+      # make sure settings attribute is loaded, so that on class reload we don't get into a state
+      # that it thinks it's serialized, but the data isn't set up right
+      default.is_a?(self) && default.settings
+      serialize :settings, Hash
+      # the default shard was already loaded, but didn't deserialize it
+      if default.is_a?(self)
+        settings = ActiveRecord::AttributeMethods::Serialization::Attribute.new(serialized_attributes['settings'],
+                                                                     default.read_attribute('settings'),
+                                                                     :serialized).unserialize
+        default.settings = settings
+      end
+
+      before_save :encrypt_settings
+
+      def settings
+        s = super
+        unless s.is_a?(Hash) || s.nil?
+          s = s.unserialize
         end
-        activate_without_delayed_jobs!(categories)
-      end
-    end
-
-    self.primary_key = "id"
-    reset_column_information # make sure that the id column object knows it is the primary key
-
-    serialize :settings, Hash
-    # the default shard was already loaded, but didn't deserialize it
-    if default.is_a?(self)
-      settings = ActiveRecord::AttributeMethods::Serialization::Attribute.new(serialized_attributes['settings'],
-                                                                   default.read_attribute('settings'),
-                                                                   :serialized).unserialize
-      default.settings = settings
-    end
-
-    before_save :encrypt_settings
-
-    def settings
-      s = super
-      unless s.is_a?(Hash) || s.nil?
-        s = s.unserialize
-      end
-      if s.nil?
-        self.settings = s = {}
-      end
-
-      salt = s.delete(:encryption_key_salt)
-      secret = s.delete(:encryption_key_enc)
-      if secret || salt
-        if secret && salt
-          s[:encryption_key] = Canvas::Security.decrypt_password(secret, salt, 'shard_encryption_key')
+        if s.nil?
+          self.settings = s = {}
         end
-        self.settings = s
+
+        salt = s.delete(:encryption_key_salt)
+        secret = s.delete(:encryption_key_enc)
+        if secret || salt
+          if secret && salt
+            s[:encryption_key] = Canvas::Security.decrypt_password(secret, salt, 'shard_encryption_key')
+          end
+          self.settings = s
+        end
+
+        s
       end
 
-      s
-    end
-
-    def encrypt_settings
-      s = self.settings.dup
-      if encryption_key = s.delete(:encryption_key)
-        secret, salt = Canvas::Security.encrypt_password(encryption_key, 'shard_encryption_key')
-        s[:encryption_key_enc] = secret
-        s[:encryption_key_salt] = salt
+      def encrypt_settings
+        s = self.settings.dup
+        if encryption_key = s.delete(:encryption_key)
+          secret, salt = Canvas::Security.encrypt_password(encryption_key, 'shard_encryption_key')
+          s[:encryption_key_enc] = secret
+          s[:encryption_key_salt] = salt
+        end
+        if s != self.settings
+          self.settings = s
+        end
+        s
       end
-      if s != self.settings
-        self.settings = s
+
+      def delayed_jobs_shard
+        shard = Shard.lookup(self.delayed_jobs_shard_id) if self.read_attribute(:delayed_jobs_shard_id)
+        shard || self.database_server.delayed_jobs_shard(self)
       end
-      s
     end
 
-    def delayed_jobs_shard
-      shard = Shard.lookup(self.delayed_jobs_shard_id) if self.read_attribute(:delayed_jobs_shard_id)
-      shard || self.database_server.delayed_jobs_shard(self)
+    Switchman::DatabaseServer.class_eval do
+      def delayed_jobs_shard(shard = nil)
+        return shard if self.config[:delayed_jobs_shard] == 'self'
+        dj_shard = self.config[:delayed_jobs_shard] &&
+          Shard.lookup(self.config[:delayed_jobs_shard])
+        # have to avoid recursion for the default shard asking for the default shard's dj shard
+        dj_shard ||= shard if shard.default?
+        dj_shard ||= Shard.default.delayed_jobs_shard
+        dj_shard
+      end
     end
+
+    Object.send(:remove_const, :Shard) if defined?(::Shard)
+    Object.send(:remove_const, :DatabaseServer) if defined?(::DatabaseServer)
+    ::Shard = Switchman::Shard
+    ::DatabaseServer = Switchman::DatabaseServer
+
+    Switchman::DefaultShard.class_eval do
+      attr_writer :settings
+
+      def settings
+        {}
+      end
+
+      def delayed_jobs_shard
+        self
+      end
+    end
+
+    Delayed::Backend::ActiveRecord::Job.class_eval do
+      self.shard_category = :delayed_jobs
+    end
+
+    Shard.default.delayed_jobs_shard.activate!(:delayed_jobs)
   end
-
-  Switchman::DatabaseServer.class_eval do
-    def delayed_jobs_shard(shard = nil)
-      return shard if self.config[:delayed_jobs_shard] == 'self'
-      dj_shard = self.config[:delayed_jobs_shard] &&
-        Shard.lookup(self.config[:delayed_jobs_shard])
-      # have to avoid recursion for the default shard asking for the default shard's dj shard
-      dj_shard ||= shard if shard.default?
-      dj_shard ||= Shard.default.delayed_jobs_shard
-      dj_shard
-    end
-  end
-
-  ::Shard = Switchman::Shard
-  ::DatabaseServer = Switchman::DatabaseServer
-
-  Switchman::DefaultShard.class_eval do
-    attr_writer :settings
-
-    def settings
-      {}
-    end
-
-    def delayed_jobs_shard
-      self
-    end
-  end
-
-  Delayed::Backend::ActiveRecord::Job.class_eval do
-    self.shard_category = :delayed_jobs
-  end
-
-  Shard.default.delayed_jobs_shard.activate!(:delayed_jobs)
 end
