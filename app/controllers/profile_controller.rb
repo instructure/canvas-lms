745c7fae78b8d0cf2745afc7df7b177be8fd4804
Jake Sorce <jake@instructure.com>
2014-06-30 15:25:50 -0600

    update user profile api to return user locale
    refs PS-1725
    
    test plan:
      1. make the user profile api call
      2. validate the new locale key and values are returned
      3. go to your settings and change your locale
      4. make sure the new locale is returned via api
    Change-Id: I0a0ebae6c227b74e1f8c149dcd638aa735edbea8
    Reviewed-on: https://gerrit.instructure.com/37099
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jake Sorce <jake@instructure.com>
    QA-Review: Jake Sorce <jake@instructure.com>
    Tested-by: Jake Sorce <jake@instructure.com>

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -81,6 +81,10 @@
 #           "description": "Optional: This field is only returned in certain API calls, and will return the IANA time zone name of the user's preferred timezone.",
 #           "example": "America/Denver",
 #           "type": "string"
+#         },
+#         "locale": {
+#           "description": "The users locale.",
+#           "type": "string"
 #         }
 #       }
 #     }
@@ -250,7 +254,7 @@ class ProfileController < ApplicationController
   # @API List avatar options
   # Retrieve the possible user avatar options that can be set with the user update endpoint. The response will be an array of avatar records. If the 'type' field is 'attachment', the record will include all the normal attachment json fields; otherwise it will include only the 'url' and 'display_name' fields. Additionally, all records will include a 'type' field and a 'token' field. The following explains each field in more detail
   # type:: ["gravatar"|"attachment"|"no_pic"] The type of avatar record, for categorization purposes.
-  # url:: The url of the avatar 
+  # url:: The url of the avatar
   # token:: A unique representation of the avatar record which can be used to set the avatar with the user update endpoint. Note: this is an internal representation and is subject to change without notice. It should be consumed with this api endpoint and used in the user update endpoint, and should not be constructed by the client.
   # display_name:: A textual description of the avatar record
   # id:: ['attachment' type only] the internal id of the attachment
@@ -296,7 +300,7 @@ class ProfileController < ApplicationController
       render :json => avatars_json_for_user(@user)
     end
   end
-  
+
   def update
     @user = @current_user
 
