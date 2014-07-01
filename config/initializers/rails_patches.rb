2e6f30617224ba2e71ccc5f8e3be038eabf58766
Brian Palmer <brianp@instructure.com>
2014-07-01 17:05:59 -0600

    backport rails4's nullifaction of association scopes on new records
    fixes CNVS-13824
    
    test plan:
    - with rails3, you should be able to create new course modules without error
    
    Change-Id: I02c38f6d50eff575fc4c7f3371735eaf5d33f332
    Reviewed-on: https://gerrit.instructure.com/37165
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>

--- config/initializers/rails_patches.rb
+++ config/initializers/rails_patches.rb
@@ -250,4 +250,24 @@ else
     alias_method_chain :value, :untaint
   end
 
+  # Fix the behavior of association scope chaining off of a new record.
+  # Without this fix, rails3 will happily generate "WHERE fk IS NULL" queries such as:
+  #
+  # ContextModule.new.content_tags.not_deleted.count
+  # => SELECT COUNT(*) FROM "content_tags" WHERE "content_tags"."context_module_id" IS NULL AND (content_tags.workflow_state<>'deleted')
+  #
+  # (This is fixed in rails4, see https://github.com/rails/rails/commit/aae4f357b5dae389b91129258f9d6d3043e7631e)
+  if Rails.version < '4'
+    ActiveRecord::Associations::CollectionAssociation.class_eval do
+      def target_scope
+        scope = super
+        scope = scope.none if null_scope?
+        scope
+      end
+
+      def null_scope?
+        owner.new_record? && !foreign_key_present?
+      end
+    end
+  end
 end
