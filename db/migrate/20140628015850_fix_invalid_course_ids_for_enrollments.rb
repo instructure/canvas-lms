17d5bd42a6802852bfc184f89dab52db498cc540
James Williams <jamesw@instructure.com>
2014-07-07 07:36:07 -0600

    fix db:migrate in non-production environment
    Change-Id: Ibb4465348162219b4f1da6f1a842234a7cecd78e
    Reviewed-on: https://gerrit.instructure.com/37316
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- db/migrate/20140628015850_fix_invalid_course_ids_for_enrollments.rb
+++ db/migrate/20140628015850_fix_invalid_course_ids_for_enrollments.rb
@@ -3,6 +3,7 @@ class FixInvalidCourseIdsForEnrollments < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def self.up
+    Enrollment.reset_column_information if !Rails.env.production?
     DataFixup::FixInvalidCourseIdsOnEnrollments.send_later_if_production(:run)
   end
 
