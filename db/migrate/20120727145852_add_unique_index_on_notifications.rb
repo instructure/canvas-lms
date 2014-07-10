9b1e8c735065a0611495795d33ed9940e614d9e0
Cody Cutrer <cody@instructure.com>
2014-07-10 10:24:23 -0600

    more fk indexes
    also fix an old migration that should have ran on all shards
    
    Change-Id: Ib4305c4f1b84ba0cfd525d0f552cfc21d365dc24
    Reviewed-on: https://gerrit.instructure.com/37492
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20120727145852_add_unique_index_on_notifications.rb
+++ db/migrate/20120727145852_add_unique_index_on_notifications.rb
@@ -2,14 +2,12 @@ class AddUniqueIndexOnNotifications < ActiveRecord::Migration
   tag :postdeploy
 
   def self.up
-    return unless Shard.current.default?
     # the excess subquery is necessary to avoid error 1093 on mysql
     Notification.where("id NOT IN (SELECT * FROM (SELECT MIN(id) FROM notifications GROUP BY name) x)").delete_all
     add_index :notifications, [:name], :unique => true, :name => "index_notifications_unique_on_name"
   end
 
   def self.down
-    return unless Shard.current.default?
     remove_index :notifications, :name => "index_notifications_unique_on_name"
   end
 end
