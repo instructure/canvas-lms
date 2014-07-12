f280b91a53b9abe24c6457ccfc68abbf687b1ac6
Cameron Matheson <cameron@instructure.com>
2014-07-11 18:14:09 -0600

    fix canvadoc locked-files issue
    fixes CNVS-14192
    
    Test plan:
      * Make a course with a canvadocable file
      * Add the file to a wiki page
      * Lock the file
      * Students viewing the wiki page should not be able to preview the file
    
    Change-Id: Ic706085492b07db24dfd48469bcb3e76e1e7e609
    Reviewed-on: https://gerrit.instructure.com/37602
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Hannah Bottalla <hannah@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>
    
    Conflicts:
    	app/controllers/files_controller.rb

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -514,7 +514,8 @@ class FilesController < ApplicationController
       end
       if request.format == :json
         options = {:permissions => {:user => @current_user}}
-        if attachment.grants_right?(@current_user, session, :download)
+        can_download = attachment.grants_right?(@current_user, session, :download)
+        if can_download
           # Right now we assume if they ask for json data on the attachment
           # which includes the scribd doc data, then that means they have 
           # viewed or are about to view the file in some form.
@@ -533,8 +534,10 @@ class FilesController < ApplicationController
       end
       format.json {
         render :json => attachment.as_json(options).tap { |json|
-          canvadoc_url = attachment.canvadoc_url(@current_user)
-          json['attachment']['canvadoc_session_url'] = canvadoc_url
+          if can_download
+            canvadoc_url = attachment.canvadoc_url(@current_user)
+            json['attachment']['canvadoc_session_url'] = canvadoc_url
+          end
         }
       }
     end
