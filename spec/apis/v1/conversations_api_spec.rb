2f8880d2e209a496fada0d8a1a96fd760c15a186
Joel Hough <joel@instructure.com>
2014-08-01 11:14:58 -0600

    Revert "conversations: fix last_authored_message_at"
    fixes CNVS-14333
    
    This reverts commit 4e83960eb4b5ba2f5bfa61d33ddbc3d3d896a745.
    
    Change-Id: I4631088293abc5f2bcb281d60165f9710c7b6111
    Reviewed-on: https://gerrit.instructure.com/38622
    QA-Review: Steven Shepherd <sshepherd@instructure.com>
    Reviewed-by: Matthew Wheeler <mwheeler@instructure.com>
    Product-Review: Matthew Wheeler <mwheeler@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- spec/apis/v1/conversations_api_spec.rb
+++ spec/apis/v1/conversations_api_spec.rb
@@ -1207,6 +1207,7 @@ describe ConversationsController, type: :request do
               { :controller => 'conversations', :action => 'add_message', :id => conversation.conversation_id.to_s, :format => 'json' },
               { :body => "partially hydrogenated context oils", :recipients => [@billy.id], :included_messages => [message.id]})
       conversation.reload
+      json.delete("last_authored_message_at") # This is sometimes not updated. It's a known bug.
       json.delete("avatar_url")
       json["participants"].each{ |p|
         p.delete("avatar_url")
@@ -1220,7 +1221,6 @@ describe ConversationsController, type: :request do
         "last_message" => "partially hydrogenated context oils",
         "last_message_at" => conversation.last_message_at.to_json[1, 20],
         "last_authored_message" => "partially hydrogenated context oils",
-        "last_authored_message_at" => conversation.last_authored_at.to_json[1, 20],
         "message_count" => 3,
         "subscribed" => true,
         "private" => false,
