7866fc3ab6b158a1796f2de6b3b7a83510c75d90
James Williams <jamesw@instructure.com>
2014-08-07 20:59:42 -0600

    fix automatic activation of DJ category
    refs CNVS-14629
    
    test plan:
     * have three shards
     * set the shard 2's delayed_job_shard_id to shard 3
     * Shard.find(2).activate { Shard.current(:delayed_jobs) }
       should return shard 3, not the default shard
    
    Change-Id: I296885c331b31a59214314a72a74f5e4b1198d0b
    Reviewed-on: https://gerrit.instructure.com/38895
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -13,10 +13,21 @@ Rails.application.config.to_prepare do
       alias_method_chain :current, :delayed_jobs
 
       def activate_with_delayed_jobs!(categories)
-        if !categories[:delayed_jobs] && categories[:default]
-          categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
+        if !categories[:delayed_jobs] && categories[:default] && !@skip_delayed_job_auto_activation
+          skip_delayed_job_auto_activation do
+            categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
+          end
         end
         activate_without_delayed_jobs!(categories)
+      end
+      alias_method_chain :activate!, :delayed_jobs
+
+      def skip_delayed_job_auto_activation
+        was = @skip_delayed_job_auto_activation
+        @skip_delayed_job_auto_activation = true
+        yield
+      ensure
+        @skip_delayed_job_auto_activation = was
       end
     end
 
