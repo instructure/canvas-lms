a3d0699dd7fc5a60d8566d646aa51f732e826eb2
Paul Hinze <paulh@instructure.com>
2014-08-18 12:26:56 -0500

    refactor: extract methods from kaltura add_media_files
     * build_partner_data handles the logic to build up the metadata JSON
        - switch to Hash#to_json in favor of string mangling
        - fan out the postfix conditionals to make the many
          conditionals in this logic more apparent
     * send_sis_data_to_kaltura? simplifies the conditional check on config
     * handle_bulk_upload_response does the optional polling for completion
       and builds media objects if the bulk upload completes
     * refresh_later DRYs up the follow on job scheduling
    
    the too-long argument lists for these helper methods highlight the
    complexity and the need for further refactoring.
    
    so we're definitely not there yet, but this gets us another step in the
    right direction.
    
    Change-Id: I329876e64210b3a63afee3c16dd93b0f1ea96675
    Reviewed-on: https://gerrit.instructure.com/39464
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Zach Pendleton <zachp@instructure.com>
    Product-Review: Paul Hinze <paulh@instructure.com>
    QA-Review: Paul Hinze <paulh@instructure.com>

--- spec/models/kaltura_media_file_handler_spec.rb
+++ spec/models/kaltura_media_file_handler_spec.rb
@@ -27,6 +27,7 @@ describe KalturaMediaFileHandler do
     let(:attachment_context) { uploading_user }
     let(:attachment) { attachment_obj_with_context(attachment_context, user: uploading_user) }
     let(:wait_for_completion) { false }
+    let(:bulk_upload_add_response) {{ id: "someBulkUploadId", ready: false }}
 
     before do
       CanvasKaltura::ClientV3.stubs(:config).returns(kaltura_config)
@@ -34,11 +35,52 @@ describe KalturaMediaFileHandler do
       kaltura_client.stubs(:startSession)
       kaltura_client.stubs(:bulkUploadAdd).with do |files|
         files_sent_to_kaltura.concat(files)
-      end.returns({})
+      end.returns(bulk_upload_add_response)
     end
 
     it "should work for user context" do
       KalturaMediaFileHandler.new.add_media_files(attachment, wait_for_completion)
+    end
+
+    it "queues a job to check on the bulk upload later" do
+      MediaObject.expects(:send_later_enqueue_args).with do |method, config, *args|
+        method.should == :refresh_media_files
+        args.should == ['someBulkUploadId', [attachment.id], attachment.root_account_id]
+      end
+
+      KalturaMediaFileHandler.new.add_media_files(attachment, wait_for_completion)
+    end
+
+    context "when wait_for_completion is true" do
+      let(:wait_for_completion) { true }
+
+      it "polls until the bulk upload completes, then calls build_media_objects with the result" do
+        media_file_handler = KalturaMediaFileHandler.new
+        unfinished_bulk_upload_get = { ready: false }
+        successful_bulk_upload_get = { ready: true, entries: [:some_details] }
+
+        media_file_handler.expects(:sleep).with(60).twice
+        kaltura_client.expects(:bulkUploadGet).with("someBulkUploadId").twice
+          .returns(unfinished_bulk_upload_get).then
+          .returns(successful_bulk_upload_get)
+
+        MediaObject.expects(:build_media_objects).with(successful_bulk_upload_get, attachment.root_account_id)
+
+        media_file_handler.add_media_files(attachment, wait_for_completion)
+      end
+
+      it "times out after media_bulk_upload_timeout, queuing a job to check in later" do
+        media_file_handler = KalturaMediaFileHandler.new
+
+        Setting.set('media_bulk_upload_timeout', 0)
+
+        MediaObject.expects(:send_later_enqueue_args).with do |method, config, *args|
+          method.should == :refresh_media_files
+          args.should == ['someBulkUploadId', [attachment.id], attachment.root_account_id]
+        end
+
+        media_file_handler.add_media_files(attachment, wait_for_completion)
+      end
     end
 
     context "partner_data" do
