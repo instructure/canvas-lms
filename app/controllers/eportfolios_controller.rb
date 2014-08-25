93f9accc8231b008035ba0bd64b2de2b40a61ec1
Jacob Fugal <jacob@instructure.com>
2014-08-24 21:54:05 -0600

    smarter linking of public portfolio owner's name
    fixes CNVS-5352
    
    only link to a user page if one is visible to the person viewing the
    page. prefer the /about/:id page if the account has profiles enabled.
    otherwise, prefer the /users/:id/profile page when viewing my own
    portfolio and the /users/:id page otherwise (and only if permitted).
    
    test-plan:
      - have a target user with a student enrollment
      - create a public portfolio for the target user
    
      - enable profiles on the account
        * log out and view the portfolio
          - the portfolio owner's name should not be linked
    
        * log in as portolio owner and view the portfolio
          - the portfolio owner's name should be linked to /about/...
          - following the link should be authorized
    
        * log in as an account admin and view the portfolio
          - the portfolio owner's name should be linked to /about/...
          - following the link should be authorized
    
        * log in as a teacher in the student's class and view the portfolio
          - the portfolio owner's name should be linked to /about/...
          - following the link should be authorized
    
        * log in as an unrelated non-admin user (i.e. student from some
          other class) and view the portfolio
          - the portfolio owner's name should not be linked
    
      - disable profiles on the account
        * log out and view the portfolio
          - the portfolio owner's name should not be linked
    
        * log in as portolio owner and view the portfolio
          - the portfolio owner's name should be linked to
            /profile
          - following the link should be authorized
    
        * log in as an account admin and view the portfolio
          - the portfolio owner's name should be linked to /users/...
          - following the link should be authorized
    
        * log in as a teacher in the student's class and view the portfolio
          - the portfolio owner's name should not be linked
    
        * log in as an unrelated non-admin user (i.e. student from some
          other class) and view the portfolio
          - the portfolio owner's name should not be linked
    
    Change-Id: I812076d10bd3c56b87938627c9f095a1793128e6
    Reviewed-on: https://gerrit.instructure.com/39863
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>

--- app/controllers/eportfolios_controller.rb
+++ app/controllers/eportfolios_controller.rb
@@ -59,7 +59,7 @@ class EportfoliosController < ApplicationController
       session[:eportfolio_ids] << @portfolio.id
       session[:permissions_key] = CanvasUUID.generate
     end
-    if authorized_action(@portfolio, @current_user, :read)      
+    if authorized_action(@portfolio, @current_user, :read)
       @portfolio.ensure_defaults
       @category = @portfolio.eportfolio_categories.first
       @page = @category.eportfolio_entries.first
@@ -77,6 +77,18 @@ class EportfoliosController < ApplicationController
       @show_left_side = true
       eportfolio_page_attributes
       if @current_user
+        # if profiles are enabled and I can message the portfolio's owner, link
+        # to their profile
+        @owner_url = user_profile_url(@portfolio.user) if @domain_root_account.enable_profiles? && @current_user.load_messageable_user(@portfolio.user)
+
+        # otherwise, if I'm the portfolio's owner (implying I can message
+        # myself, so therefore profiles just aren't enabled), link to my
+        # profile
+        @owner_url ||= profile_url if @current_user == @portfolio.user
+
+        # otherwise, if  I can otherwise view the user, link directly to them
+        @owner_url ||= user_url(@portfolio.user) if @portfolio.user.grants_right?(@current_user, :view_statistics)
+
         js_env :folder_id => Folder.unfiled_folder(@current_user).id,
                :context_code => @current_user.asset_string
       end
