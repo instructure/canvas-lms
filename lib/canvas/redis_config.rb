c007afcf1b6749b64c106284eb80fc6e01ffaf68
Brian Palmer <brianp@instructure.com>
2014-08-25 11:45:30 -0600

    support redis timeout option for cache and data conns
    test plan:
    
    - add a `timeout: 1` option to redis.yml
    - confirm that rails cache and redis operations complete as normal
    - to test timeout, you can set up a dummy tcp server that listens but
      never accepts, add it to the redis config, and verify that the (first)
      redis operation times out as expected. after the first, it'll be
      blacklisted for 5m as usual.
    
    Change-Id: I2494b4b319afffcf1ea6ab87eb1c3d7d3baec58b
    Reviewed-on: https://gerrit.instructure.com/39895
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

--- lib/canvas/redis_config.rb
+++ lib/canvas/redis_config.rb
@@ -2,15 +2,16 @@ module Canvas
   class RedisConfig
     attr_reader :redis
 
-    def initialize(servers, database=nil)
-      @redis = RedisConfig.from_servers(servers)
+    def initialize(servers, database=nil, options=nil)
+      @redis = RedisConfig.from_servers(servers, options)
       @redis.select database if database.present?
     end
 
     def self.from_settings(settings)
       RedisConfig.new(
         settings[:servers],
-        settings[:database]
+        settings[:database],
+        settings.except(:servers, :database).symbolize_keys
       )
     end
 
@@ -19,11 +20,11 @@ module Canvas
       ::Redis::Factory
     end
 
-    def self.from_servers(servers)
+    def self.from_servers(servers, options)
       factory.create(servers.map { |s|
         # convert string addresses to options hash, and disable redis-cache's
         # built-in marshalling code
-        factory.convert_to_redis_client_options(s).merge(:marshalling => false)
+        factory.convert_to_redis_client_options(s).merge(:marshalling => false).merge(options || {})
       })
     end
 
