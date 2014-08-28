3d54cf7943e47bae3aa94d5431b168241542324f
Graham Ballantyne <grahamb@sfu.ca>
2014-08-28 10:53:16 -0700

    add capistrano-canvas to the sfu gemfile

--- Gemfile.d/sfu_gems.rb
+++ Gemfile.d/sfu_gems.rb
@@ -2,5 +2,6 @@ group :development do
   gem 'capistrano', '~> 3.0', require: false
   gem 'capistrano-rails',   '~> 1.1', require: false
   gem 'capistrano-bundler', '~> 1.1', require: false
+  gem 'capistrano-canvas', :git => 'https://github.com/grahamb/capistrano-canvas.git', require: false
   gem 'capistrano-scm-copy', :git => 'https://github.com/grahamb/capistrano-scm-copy.git', require: false
 end
