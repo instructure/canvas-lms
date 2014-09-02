8554c9fe8dd645066dd29b61404563a25d2f68db
Graham Ballantyne <hello@grahamballantyne.com>
2014-09-01 18:00:04 -0700

    add release_dir to JS ENV hash

--- vendor/plugins/aaa_sfu_misc/lib/jsenv.rb
+++ vendor/plugins/aaa_sfu_misc/lib/jsenv.rb
@@ -2,6 +2,7 @@ ApplicationController.class_eval do
   before_filter :add_node_name_to_jsenv
   def add_node_name_to_jsenv
     appnode = Socket.gethostname().split('.')[0]
-    js_env(:APP_NODE => appnode)
+    release_dir = File.dirname(__FILE__)
+    js_env(:APP_NODE => appnode, :RELEASE => release_dir)
   end
 end
