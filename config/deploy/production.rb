9d35118f3e7c2f0427dad5d3cab07b1aab121651
Graham Ballantyne <grahamb@sfu.ca>
2014-09-05 11:44:54 -0700

    fix production deploy file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,9 @@
 set :stage, :production
 role :db,  %w{canvas-mp1.tier2.sfu.ca canvas-mp2.tier2.sfu.ca}
 
-on :start, 'canvas:set_app_nodes'
+namespace :deploy do
+  before :started, 'canvas:set_app_nodes'
+end
 
 set :default_env, {
   'PATH' => '/usr/pgsql-9.1/bin:$PATH'
