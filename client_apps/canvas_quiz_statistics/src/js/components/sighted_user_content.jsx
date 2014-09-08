f3d3c5ec0297364d39587777b93fa94913e6706d
Ahmad Amireh <ahmad@instructure.com>
2014-09-08 14:55:32 +0300

    CQS: accessible summary stats table
    Closes CNVS-15310
    
    TEST PLAN
    ---- ----
    
      - go to statistics cqs page
      - turn on screen-reader
      - locate the table and verify that it reads in a sensible manner
      - verify you hear a short description of what the table is when you
        get to it
      - verify that no column header is abbreviated anymore, so "Std.
        Deviation" now reads "Standard Deviation" and "Avg. Time" now reads
        "Average Time"
      - verify the last column for the Average Time reads a
        human-understandable duration, like 10 seconds or 5 minutes and 30
        seconds instead of zero-zero-colon-one-zero (00:10)
      - sighted users should still see the duration in timestamp-form, e.g,
        00:10 and 05:30
    
    Change-Id: Ia15877f654a4538e939758fcf89ad45f36e52df2
    Reviewed-on: https://gerrit.instructure.com/40686
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Trevor deHaan <tdehaan@instructure.com>
    Reviewed-by: Josh Simpson <jsimpson@instructure.com>
    Product-Review: Derek DeVries <ddevries@instructure.com>

---
+++ client_apps/canvas_quiz_statistics/src/js/components/sighted_user_content.jsx
@@ -0,0 +1,45 @@
+/** @jsx React.DOM */
+define(function(require) {
+  var React = require('react');
+
+  /**
+   * @class Components.SightedUserContent
+   *
+   * A component that *tries* to hide itself from screen-readers, absolutely
+   * expecting that you're providing a more accessible version of the resource
+   * using something like a ScreenReaderContent component.
+   *
+   * Be warned that this does not totally prevent all screen-readers from
+   * seeing this content in all modes. For example, VoiceOver in OS X will
+   * still see this element when running in the "Say-All" mode and read it
+   * along with the accessible version you're providing.
+   *
+   * > **Warning**
+   * >
+   * > Use of this component is discouraged unless there's no alternative!!!
+   * >
+   * > The only one case that justifies its use is when design provides a
+   * > totally inaccessible version of a resource, and you're trying to
+   * > accommodate the design (for sighted users,) and provide a genuine layer
+   * > of accessibility (for others.)
+   */
+  var SightedUserContent = React.createClass({
+    getDefaultProps: function() {
+      return {
+        tagName: 'span'
+      };
+    },
+
+    render: function() {
+      var tagFactory = React.DOM[this.props.tagName];
+
+      return this.transferPropsTo(tagFactory({
+        'aria-hidden': true,
+        'role': 'presentation',
+        'aria-role': 'presentation'
+      }, this.props.children));
+    }
+  });
+
+  return SightedUserContent;
+});
