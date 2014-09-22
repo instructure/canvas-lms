990d41bb816a5a30d690fc13cc62ee5c6bf1f00f
Matt Wheeler <matt.wheeler@12spokes.com>
2014-09-22 17:40:30 -0500

    Split collaborations selenium specs into 2 files
    Refs CNVS-15688
    
    One file for the form specific specs, one file for the remainder.
    
    Also deleted an unused method.
    
    Test Plan:
    Verify that both files run in under two minutes each, on Jenkins.
    
    Change-Id: I0977a64a1bf85c411a1b7b9893755d889e015fc4
    Reviewed-on: https://gerrit.instructure.com/41589
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brad Horrocks <bhorrocks@instructure.com>
    Product-Review: Matthew Wheeler <mwheeler@instructure.com>
    QA-Review: Matthew Wheeler <mwheeler@instructure.com>

--- spec/selenium/collaborations_form_spec.rb
+++ spec/selenium/collaborations_form_spec.rb
@@ -99,113 +99,62 @@ describe "collaborations" do
           end
         end
 
-        it 'should display the new collaboration form if there are no existing collaborations' do
-          PluginSetting.create!(:name => type, :settings => {})
-          validate_collaborations
-        end
-
-        it 'should be editable' do
+        it 'should not display the new collaboration form if other collaborations exist' do
           create_collaboration!(type, title)
           validate_collaborations(%W{/courses/#{@course.id}/collaborations}, false)
-
-          new_title = 'Edited collaboration'
-          f('.edit_collaboration_link').click
-          replace_content(fj('input[name="collaboration[title]"]:visible'), new_title)
-          expect_new_page_load do
-            submit_form('.edit_collaboration')
-          end
-
-          f('.collaboration .title').text.should == new_title
-          Collaboration.order("id DESC").last.title.should == new_title
         end
 
-        it 'should be delete-able' do
+        describe '#add_collaboration fragment' do
+          it 'should display the new collaboration form if no collaborations exist' do
+            PluginSetting.create!(:name => type, :settings => {})
+            validate_collaborations(%W{/courses/#{@course.id}/collaborations
+              /courses/#{@course.id}/collaborations#add_collaboration}, true)
+          end
+
+          it 'should hide the new collaboration form if collaborations exist' do
+            create_collaboration!(type, title)
+            validate_collaborations(%W{/courses/#{@course.id}/collaborations
+              /courses/#{@course.id}/collaborations#add_collaboration}, false)
+          end
+        end
+
+        it 'should open the new collaboration form if the last collaboration is deleted' do
           create_collaboration!(type, title)
-          validate_collaborations(%W{/courses/#{@course.id}/collaborations}, false)
-
-          f('.delete_collaboration_link').click
-
-          if type == 'google_docs'
-            f('#delete_collaboration_dialog .delete_button').click
-          else
-            driver.switch_to.alert.accept
-          end
-          wait_for_ajaximations
-
-          f('#no_collaborations_message').should be_displayed
-          Collaboration.order("id DESC").last.should be_deleted
+          validate_collaborations("/courses/#{@course.id}/collaborations/", false, true)
+          delete_collaboration(@collaboration, type)
+          form_visible?.should be_true
         end
 
-        it 'should display available collaborators' do
+        it 'should not display the new collaboration form when the penultimate collaboration is deleted' do
           PluginSetting.create!(:name => type, :settings => {})
 
-          student_in_course(:course => @course)
-          @student.update_attribute(:name, 'Don Draper')
+          @collaboration1 = Collaboration.typed_collaboration_instance(title)
+          @collaboration1.context = @course
+          @collaboration1.attributes = {:title => "My Collab 1"}
+          @collaboration1.user = @user
+          @collaboration1.save!
+          @collaboration2 = Collaboration.typed_collaboration_instance(title)
+          @collaboration2.context = @course
+          @collaboration2.attributes = {:title => "My Collab 2"}
+          @collaboration2.user = @user
+          @collaboration2.save!
 
-          get "/courses/#{@course.id}/collaborations"
-
-          wait_for_ajaximations
-
-          keep_trying_until {
-            ffj('.available-users:visible li').length.should == 1
-          }
+          validate_collaborations("/courses/#{@course.id}/collaborations/", false, true)
+          delete_collaboration(@collaboration1, type)
+          form_visible?.should be_false
+          delete_collaboration(@collaboration2, type)
+          form_visible?.should be_true
         end
 
-        it 'should select collaborators' do
-          PluginSetting.create!(:name => type, :settings => {})
-
-          student_in_course(:course => @course)
-          @student.update_attribute(:name, 'Don Draper')
-
-          get "/courses/#{@course.id}/collaborations"
-
-          wait_for_ajaximations
-          fj('.available-users:visible a').click
-          keep_trying_until {
-            ffj('.members-list li').length.should == 1
-          }
-        end
-
-        it 'should deselect collaborators' do
-          PluginSetting.create!(:name => type, :settings => {})
-
-          student_in_course(:course => @course)
-          @student.update_attribute(:name, 'Don Draper')
-
-          get "/courses/#{@course.id}/collaborations"
-
-          wait_for_ajaximations
-
-          fj('.available-users:visible a').click
-          fj('.members-list a').click
-          ffj('.members-list li').length.should == 0
+        it 'should leave the new collaboration form open when the last collaboration is deleted' do
+          create_collaboration!(type, title)
+          validate_collaborations(%W{/courses/#{@course.id}/collaborations
+                                     /courses/#{@course.id}/collaborations#add_collaboration}, false, true)
+          f('.add_collaboration_link').click
+          delete_collaboration(@collaboration, type)
+          form_visible?.should be_true
         end
       end
-    end
-  end
-
-  context "a student's etherpad collaboration" do
-    before(:each) do
-      course_with_teacher(:active_all => true, :name => 'teacher@example.com')
-      student_in_course(:course => @course, :name => 'Don Draper')
-    end
-
-    it 'should be visible to the student' do
-      PluginSetting.create!(:name => 'etherpad', :settings => {})
-
-      @collaboration = Collaboration.typed_collaboration_instance('EtherPad')
-      @collaboration.context = @course
-      @collaboration.attributes = { :title => 'My collaboration',
-                                    :user  => @teacher }
-      @collaboration.update_members([@student])
-      @collaboration.save!
-
-      user_session(@student)
-      get "/courses/#{@course.id}/collaborations"
-
-      wait_for_ajaximations
-
-      ff('#collaborations .collaboration').length == 1
     end
   end
 end
