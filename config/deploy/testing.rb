971fefe68644918171e22930cc232e181f28bd79
Graham Ballantyne <grahamb@sfu.ca>
2014-09-22 11:29:04 -0700

    move old testing.rb file and replace with buildbox file

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,8 +1,11 @@
-set :stage, :testing
+require 'capistrano-scm-copy'
+set :scm, :copy
+set :copy_local_tar, "/usr/local/bin/gtar" if `uname` =~ /Darwin/
+
+set :stage, :testing_buildbox
 set :app_node_prefix, "canvas-at"
 set :canvas_url, 'https://canvas-test.sfu.ca'
 
-role :app, %w{canvas-at1.tier2.sfu.ca canvas-at2.tier2.sfu.ca canvas-at3.tier2.sfu.ca}
 role :db,  %w{canvas-mt.tier2.sfu.ca}
 set :branch, ENV['branch'] || 'sfu-develop'
 
