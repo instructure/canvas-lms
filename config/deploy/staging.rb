58869f9b9f7e5e71a2d778fb2b85364d225109a4
Graham Ballantyne <grahamb@sfu.ca>
2014-09-22 10:38:26 -0700

    fix staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ role :app, "canvas-as1.tier2.sfu.ca"
 role :app, "canvas-as2.tier2.sfu.ca"
 role :app, "canvas-as3.tier2.sfu.ca"
 role :db, "canvas-ms.tier2.sfu.ca", :primary => true
-set :branch, ENV['branch'] || 'sfu-develop'
+set :branch, ENV['branch'] || 'sfu-deploy'
 
 namespace :deploy do
   before :started, 'canvas:set_app_nodes'
