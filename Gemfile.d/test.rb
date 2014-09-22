934209a6f33bbfa292a8baf2d8035c56526e170f
Shawn Meredith <shawn@instructure.com>
2014-09-22 10:39:36 -0600

    spec: testbot rspec tagging
    Change-Id: I20a5069fee3a5b75cc0208f312ea4135fd525bc4
    Reviewed-on: https://gerrit.instructure.com/41519
    Reviewed-by: Kyle Rosenbaum <kyler@instructure.com>
    Product-Review: Shawn Meredith <shawn@instructure.com>
    QA-Review: Shawn Meredith <shawn@instructure.com>
    Tested-by: Shawn Meredith <shawn@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -1,5 +1,5 @@
 group :test do
-  gem 'testbot', :github => 'smeredith0506/testbot'
+  gem 'testbot', :github => 'smeredith0506/testbot', :branch => 'master', :ref => '47fbf057ab40f8a6e24b1ae780c3f1a176621892'
   gem 'simplecov', '0.8.2', :require => false
     gem 'docile', '1.1.3'
   gem 'simplecov-rcov', '0.2.3', :require => false
