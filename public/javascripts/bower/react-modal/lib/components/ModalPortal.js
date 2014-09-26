95c383492a92e12f1d845e35215cefad6f76f8e0
Clay Diffrient <cdiffrient@instructure.com>
2014-09-26 10:31:04 -0600

    Adds react-modal to Canvas
    Test Plan:
      - Nothing really testable, since it just adds a library
    
    Change-Id: Ib5977abeffdc759a57c5373e193a9b113af54a49
    Reviewed-on: https://gerrit.instructure.com/41835
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jason Madsen <jmadsen@instructure.com>
    Product-Review: Jason Madsen <jmadsen@instructure.com>
    QA-Review: Jason Madsen <jmadsen@instructure.com>

---
+++ public/javascripts/bower/react-modal/lib/components/ModalPortal.js
@@ -0,0 +1,155 @@
+var React = require('react');
+var div = React.DOM.div;
+var focusManager = require('../helpers/focusManager');
+var scopeTab = require('../helpers/scopeTab');
+
+// so that our CSS is statically analyzable
+var CLASS_NAMES = {
+  overlay: {
+    base: 'ReactModal__Overlay',
+    afterOpen: 'ReactModal__Overlay--after-open',
+    beforeClose: 'ReactModal__Overlay--before-close',
+  },
+  content: {
+    base: 'ReactModal__Content',
+    afterOpen: 'ReactModal__Content--after-open',
+    beforeClose: 'ReactModal__Content--before-close',
+  }
+};
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+var ModalPortal = module.exports = React.createClass({
+
+  displayName: 'ModalPortal',
+
+  getInitialState: function() {
+    return {
+      afterOpen: false,
+      beforeClose: false
+    };
+  },
+
+  componentDidMount: function() {
+    this.handleProps(this.props);
+    this.maybeFocus();
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    this.handleProps(newProps);
+  },
+
+  handleProps: function(props) {
+    if (props.isOpen === true)
+      this.open();
+    else if (props.isOpen === false)
+      this.close();
+  },
+
+  open: function() {
+    focusManager.setupScopedFocus(this.getDOMNode());
+    focusManager.markForFocusLater();
+    this.setState({isOpen: true}, function() {
+      this.setState({afterOpen: true});
+    }.bind(this));
+  },
+
+  close: function() {
+    if (!this.ownerHandlesClose())
+      return;
+    if (this.props.closeTimeoutMS > 0)
+      this.closeWithTimeout();
+    else
+      this.closeWithoutTimeout();
+  },
+
+  componentDidUpdate: function() {
+    this.maybeFocus();
+  },
+
+  maybeFocus: function() {
+    if (this.props.isOpen)
+      this.focusContent();
+  },
+
+  focusContent: function() {
+    this.refs.content.getDOMNode().focus();
+  },
+
+  closeWithTimeout: function() {
+    this.setState({beforeClose: true}, function() {
+      setTimeout(this.closeWithoutTimeout, this.props.closeTimeoutMS);
+    }.bind(this));
+  },
+
+  closeWithoutTimeout: function() {
+    this.setState({
+      afterOpen: false,
+      beforeClose: false
+    }, this.afterClose);
+  },
+
+  afterClose: function() {
+    focusManager.returnFocus();
+    focusManager.teardownScopedFocus();
+  },
+
+  handleKeyDown: function(event) {
+    if (event.key == 9 /*tab*/) scopeTab(this.getDOMNode(), event);
+    if (event.key == 27 /*esc*/) this.requestClose();
+  },
+
+  handleOverlayClick: function() {
+    if (this.ownerHandlesClose())
+      this.requestClose();
+    else
+      this.focusContent();
+  },
+
+  requestClose: function() {
+    if (this.ownerHandlesClose)
+      this.props.onRequestClose();
+  },
+
+  ownerHandlesClose: function() {
+    return this.props.onRequestClose;
+  },
+
+  shouldBeClosed: function() {
+    return !this.props.isOpen && !this.state.beforeClose;
+  },
+
+  overlayStyles: { position: 'fixed', left: 0, right: 0, top: 0, bottom: 0 },
+
+  buildClassName: function(which) {
+    var className = CLASS_NAMES[which].base;
+    if (this.state.afterOpen)
+      className += ' '+CLASS_NAMES[which].afterOpen;
+    if (this.state.beforeClose)
+      className += ' '+CLASS_NAMES[which].beforeClose;
+    return className;
+  },
+
+  render: function() {
+    return this.shouldBeClosed() ? div() : (
+      div({
+        className: this.buildClassName('overlay'),
+        style: this.overlayStyles,
+        onClick: this.handleOverlayClick
+      },
+        div({
+          ref: "content",
+          className: this.buildClassName('content'),
+          tabIndex: "-1",
+          onClick: stopPropagation,
+          onKeyDown: this.handleKeyDown
+        },
+          this.props.children
+        )
+      )
+    );
+  }
+});
+
---
+++ public/javascripts/bower/react-modal/lib/helpers/focusManager.js
@@ -0,0 +1,52 @@
+var findTabbable = require('../helpers/tabbable');
+var modalElement = null;
+var focusLaterElement = null;
+var needToFocus = false;
+
+function handleBlur(event) {
+  needToFocus = true;
+}
+
+function handleFocus(event) {
+  if (needToFocus) {
+    needToFocus = false;
+    // need to see how jQuery shims document.on('focusin') so we don't need the
+    // setTimeout, firefox doesn't support focusin, if it did, we could focus
+    // the the element outisde of a setTimeout. Side-effect of this
+    // implementation is that the document.body gets focus, and then we focus
+    // our element right after, seems fine.
+    setTimeout(function() {
+      if (modalElement.contains(document.activeElement))
+        return;
+      var el = (findTabbable(modalElement)[0] || modalElement);
+      el.focus();
+    }, 0);
+  }
+}
+
+exports.markForFocusLater = function() {
+  focusLaterElement = document.activeElement;
+};
+
+exports.returnFocus = function() {
+  try {
+    focusLaterElement.focus();
+  }
+  catch (e) {
+    console.warn('You tried to return focus to '+focusLaterElement+' but it is not in the DOM anymore');
+  }
+  focusLaterElement = null;
+};
+
+exports.setupScopedFocus = function(element) {
+  modalElement = element;
+  window.addEventListener('blur', handleBlur, false);
+  document.addEventListener('focus', handleFocus, true);
+};
+
+exports.teardownScopedFocus = function() {
+  modalElement = null;
+  window.removeEventListener('blur', handleBlur);
+  document.removeEventListener('focus', handleFocus);
+};
+
