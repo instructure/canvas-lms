b8145b1e325f2ed7714db43f2d7fdd7f452ec52d
Jeremy Stanley <jeremy@instructure.com>
2014-10-02 15:38:26 -0600

    prevent LTI logout service token reuse
    test plan:
     0. configure an external tool that uses $Canvas.logoutService.url
     1. use that URL to register a logout callback (using OAuth 1.0
        signing with the tool's consumer key and shared secret, and
        adding a callback parameter set to the URL to hit). you should
        get a status 200 response if the auth is correct.
     2. repeat step 1. this time you should get status 401 with a
        message saying the logout service token has already been used.
     3. log out and confirm that the callback registered in step 1
        is hit.
    
    fixes CNVS-16009
    
    Change-Id: Ic4aaa8fe928b791a63a9cce61648dc48aeba4f9d
    Reviewed-on: https://gerrit.instructure.com/42187
    Reviewed-by: Brad Humphrey <brad@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Bracken Mosbacker <bracken@instructure.com>

--- app/models/lti/logout_service.rb
+++ app/models/lti/logout_service.rb
@@ -6,21 +6,25 @@ module Lti
     TOKEN_EXPIRATION = 600
 
     def self.cache_key(pseudonym)
-      ['logout_service_callbacks', pseudonym.id].cache_key
+      ['logout_service_callbacks_2', pseudonym.id].cache_key
     end
 
     def self.get_logout_callbacks(pseudonym)
-      Rails.cache.read(cache_key(pseudonym)) || []
+      Rails.cache.read(cache_key(pseudonym)) || {}
     end
 
     def self.clear_logout_callbacks(pseudonym)
       Rails.cache.delete(cache_key(pseudonym))
     end
 
-    class Token < Struct.new(:tool, :pseudonym, :timestamp)
+    class Token < Struct.new(:tool, :pseudonym, :timestamp, :nonce)
+      def self.create(tool, pseudonym)
+        Token.new(tool, pseudonym, Time.now, SecureRandom.hex(8))
+      end
+
       def serialize
         key = tool.shard.settings[:encryption_key]
-        payload = [tool.id, pseudonym.id, timestamp.to_i].join('-')
+        payload = [tool.id, pseudonym.id, timestamp.to_i, nonce].join('-')
         "#{payload}-#{Canvas::Security.hmac_sha1(payload, key)}"
       end
 
@@ -28,21 +32,22 @@ module Lti
         parts = serialized_token.split('-')
         tool = ContextExternalTool.find(parts[0].to_i)
         key = tool.shard.settings[:encryption_key]
-        unless parts.size == 4 && Canvas::Security.hmac_sha1(parts[0..-2].join('-'), key) == parts[-1]
+        unless parts.size == 5 && Canvas::Security.hmac_sha1(parts[0..-2].join('-'), key) == parts[-1]
           raise BasicLTI::BasicOutcomes::Unauthorized, "Invalid logout service token"
         end
         pseudonym = Pseudonym.find(parts[1].to_i)
         timestamp = parts[2].to_i
+        nonce = parts[3]
         unless Time.now.to_i - timestamp < Lti::LogoutService::TOKEN_EXPIRATION
           raise BasicLTI::BasicOutcomes::Unauthorized, "Logout service token has expired"
         end
-        Token.new(tool, pseudonym, timestamp)
+        Token.new(tool, pseudonym, timestamp, nonce)
       end
     end
 
     class Runner < Struct.new(:callbacks)
       def perform
-        callbacks.each do |callback|
+        callbacks.each do |tool_id, callback|
           begin
             Net::HTTP::get(URI.parse(callback))
           rescue => e
@@ -53,14 +58,15 @@ module Lti
     end
 
     def self.create_token(tool, pseudonym)
-      Token.new(tool, pseudonym, Time.now).serialize
+      Token.create(tool, pseudonym).serialize
     end
 
-    def self.register_logout_callback(pseudonym, callback)
-      return unless pseudonym && pseudonym.id
-      callbacks = get_logout_callbacks(pseudonym)
-      callbacks << callback
-      Rails.cache.write(cache_key(pseudonym), callbacks, :expires_in => 1.day)
+    def self.register_logout_callback(token, callback)
+      return unless token.pseudonym && token.pseudonym.id && callback.present?
+      callbacks = get_logout_callbacks(token.pseudonym)
+      raise BasicLTI::BasicOutcomes::Unauthorized, 'Logout service token has already been used' if callbacks.has_key?(token.nonce)
+      callbacks[token.nonce] = callback
+      Rails.cache.write(cache_key(token.pseudonym), callbacks, :expires_in => 1.day)
     end
 
     def self.queue_callbacks(pseudonym)
