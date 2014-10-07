1cb77685c37b8cd16cb85fad02bd85e03727f234
Jeremy Stanley <jeremy@instructure.com>
2014-10-07 12:23:50 -0600

    fix /files/preflight route
    test plan: you should be able to upload files to the files page
    with Better File Browsing disabled, and Flash enabled in the
    browser.
    
    fixes CNVS-16126
    
    Change-Id: Ieafb0030d9228eee846ff0cf326dcf83ceb5b8b2
    Reviewed-on: https://gerrit.instructure.com/42347
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: James Williams  <jamesw@instructure.com>
    QA-Review: Anna Koalenz <akoalenz@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- config/routes.rb
+++ config/routes.rb
@@ -713,7 +713,7 @@ CanvasRails::Application.routes.draw do
   get "files/search", controller: 'files', action: 'ember_app', format: false
   get 'files/s3_success/:id' => 'files#s3_success', as: :s3_success
   get 'files/:id/public_url' => 'files#public_url', as: :public_url
-  post 'files/preflight' => 'files#preflight', as: :file_preflight
+  get 'files/preflight' => 'files#preflight', as: :file_preflight
   post 'files/pending' => 'files#create_pending', as: :file_create_pending
   resources :assignments, only: :index do
     resources :files, only: [] do
