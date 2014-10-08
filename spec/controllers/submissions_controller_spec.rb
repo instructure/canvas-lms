4c5c48ce347cffe8db98072db68655e20559c353
Benjamin Porter <bporter@instructure.com>
2014-10-07 18:17:48 -0600

    Verify we are getting the correct error message from the SubmissionsController
    This test is to verify that the SubmissionsController properly
    rejects assignment submissions that have files attached with illegal
    file extensions, based on what is whitelisted by the instructor.
    
    Previously the test was not setting the context of the attachment,
    which caused the attachment to not be properly associated with the
    user.  Because of that, the submission was properly being rejected
    but it was being rejected because there was no file attached,
    not because of the file extension.  We also weren't checking the
    returned error message, so we thought everything was good when it
    wasn't.  This patch set fixes those issues.
    
    Fixes CNVS-16135
    
    Test Plan:
    
    None, this patch set only changes a unit test and doesn't actually
    touch application code at all.
    
    Change-Id: Ic263a5fd990db1b600e128fbe78134ed21f039a1
    Reviewed-on: https://gerrit.instructure.com/42386
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    QA-Review: Sean Lewis <slewis@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Benjamin Porter <bporter@instructure.com>

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -42,11 +42,12 @@ describe SubmissionsController do
     it "should reject illegal file extensions from submission" do
       course_with_student_logged_in(:active_all => true)
       @assignment = @course.assignments.create!(:title => "an additional assignment", :submission_types => "online_upload", :allowed_extensions => ['txt'])
-      att = attachment_model(:uploaded_data => stub_file_data('test.m4v', 'asdf', 'video/mp4'))
-      post 'create', :course_id => @course.id, :assignment_id => @assignment.id, :submission => {:submission_type => "online_upload", :attachment_ids => att.id}, :attachments => {}
+      att = attachment_model(:context => @student, :uploaded_data => stub_file_data('test.m4v', 'asdf', 'video/mp4'))
+      post 'create', :course_id => @course.id, :assignment_id => @assignment.id, :submission => {:submission_type => "online_upload", :attachment_ids => att.id}, :attachments => { "0" => { :uploaded_data => "" }, "-1" => { :uploaded_data => "" } }
       response.should be_redirect
       assigns[:submission].should be_nil
       flash[:error].should_not be_nil
+      flash[:error].should match(/Invalid file type/)
     end
 
     it "should use the appropriate group based on the assignment's category and the current user" do
