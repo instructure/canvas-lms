68756d03ccd26d44beac63d0ffba6e6afce7676e
Nathan Mills <nathanm@instructure.com>
2014-10-09 14:20:12 -0600

    update ims-lti gem to fix specs
    fixes PLAT-676
    
    test-plan:
    specs should pass
    
    Change-Id: I39118e706ab6b6b8ed83f4914407ac2388cfb886
    Reviewed-on: https://gerrit.instructure.com/42521
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brad Humphrey <brad@instructure.com>
    Product-Review: Nathan Mills <nathanm@instructure.com>
    QA-Review: Nathan Mills <nathanm@instructure.com>

--- Gemfile.d/other_stuff.rb
+++ Gemfile.d/other_stuff.rb
@@ -46,7 +46,7 @@ gem 'i18n', '0.6.9'
 gem 'i18nema', '0.0.8', :platforms => [:mri_20, :mri_21]
 gem 'i18nema19', '0.0.8', :platform => :mri_19
 gem 'icalendar', '1.5.4'
-gem 'ims-lti', '2.0.0.beta.7'
+gem 'ims-lti', '2.0.0.beta.10'
 gem 'jammit', :github => 'documentcloud/jammit', :ref => '98b50a67029c2860717485a72a2ff0ae8ec37840'
   gem 'cssmin', '1.0.3'
   gem 'jsmin', '1.0.1'
