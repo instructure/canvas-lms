9fbea5bd0904b40a63a97410360431076afab0d6
Jon Jensen <jon@instructure.com>
2014-10-17 01:45:54 -0600

    i18nliner interlude: separate out i18n.js hacks
    to facilitate i18nliner.js runtime integration and an i18n.js
    upgrade, separate all the tweaks we've made to the latter in
    the last 3 1/2 years
    
    vendor/i18n.js is the one we originally started with, i.e.
    https://github.com/fnando/i18n-js/blob/61c0396/source/i18n.js
    
    test plan:
    1. specs should pass and canvas should not be broken
    
    Change-Id: I40f1118e960ec529a89a4ed0eaae2280f038c946
    Reviewed-on: https://gerrit.instructure.com/42893
    Reviewed-by: Jennifer Stern <jstern@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Matt Fairbourn <mfairbourn@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- public/javascripts/i18nObj.js
+++ public/javascripts/i18nObj.js
@@ -1,20 +1,15 @@
 define([
+  'vendor/i18n',
   'jquery',
   'str/htmlEscape',
   'str/pluralize',
   'str/escapeRegex',
   'compiled/str/i18nLolcalize',
   'vendor/date' /* Date.parse, Date.UTC */
-], function($, htmlEscape, pluralize, escapeRegex, i18nLolcalize) {
+], function(I18n, $, htmlEscape, pluralize, escapeRegex, i18nLolcalize) {
 
-// Instantiate the object, export globally for tinymce/specs
-var I18n = window.I18n = {};
-
-// Set default locale to english
-I18n.defaultLocale = "en";
-
-// Set default separator
-I18n.defaultSeparator = ".";
+// Export globally for tinymce/specs
+window.I18n = I18n;
 
 I18n.locale = document.documentElement.getAttribute('lang');
 
@@ -64,34 +59,6 @@ I18n.lookup = function(scope, options) {
   }
 
   return messages;
-};
-
-// Merge serveral hash options, checking if value is set before
-// overwriting any value. The precedence is from left to right.
-//
-//   I18n.prepareOptions({name: "John Doe"}, {name: "Mary Doe", role: "user"});
-//   #=> {name: "John Doe", role: "user"}
-//
-I18n.prepareOptions = function() {
-  var options = {};
-  var opts;
-  var count = arguments.length;
-
-  for (var i = 0; i < count; i++) {
-    opts = arguments[i];
-
-    if (!opts) {
-      continue;
-    }
-
-    for (var key in opts) {
-      if (!this.isValidNode(options, key)) {
-        options[key] = opts[key];
-      }
-    }
-  }
-
-  return options;
 };
 
 I18n.interpolate = function(message, options) {
@@ -163,88 +130,13 @@ I18n.applyWrappers = function(string, wrappers) {
   return string;
 };
 
-I18n.translate = function(scope, options) {
-  options = this.prepareOptions(options);
-  var translation = this.lookup(scope, options);
-
-  try {
-    if (typeof(translation) == "object") {
-      if (typeof(options.count) == "number") {
-        return this.pluralize(options.count, scope, options);
-      } else {
-        return translation;
-      }
-    } else {
-      return this.interpolate(translation, options);
-    }
-  } catch(err) {
-    return this.missingTranslation(scope);
-  }
-};
-
+var _localize = I18n.localize;
 I18n.localize = function(scope, value) {
-  switch (scope) {
-    case "currency":
-      return this.toCurrency(value);
-    case "number":
-      scope = this.lookup("number.format");
-      return this.toNumber(value, scope);
-    case "percentage":
-      return this.toPercentage(value);
-    default:
-      if (scope.match(/^(date|time)/)) {
-        return this.toTime(scope, value).replace(/\s{2,}/, ' ');
-      } else {
-        return value.toString();
-      }
-  }
-};
-
-I18n.parseDate = function(d) {
-  var matches, date;
-  matches = d.toString().match(/(\d{4})-(\d{2})-(\d{2})(?:[ |T](\d{2}):(\d{2}):(\d{2}))?(Z)?/);
-
-  if (matches) {
-    // date/time strings: yyyy-mm-dd hh:mm:ss or yyyy-mm-dd or yyyy-mm-ddThh:mm:ssZ
-    for (var i = 1; i <= 6; i++) {
-      matches[i] = parseInt(matches[i], 10) || 0;
-    }
-
-    // month starts on 0
-    matches[2] -= 1;
-
-    if (matches[7]) {
-      date = new Date(Date.UTC(matches[1], matches[2], matches[3], matches[4], matches[5], matches[6]));
-    } else {
-      date = new Date(matches[1], matches[2], matches[3], matches[4], matches[5], matches[6]);
-    }
-  } else if (typeof(d) == "number") {
-    // UNIX timestamp
-    date = new Date();
-    date.setTime(d);
-  } else {
-    // an arbitrary javascript string
-    date = new Date();
-    date.setTime(Date.parse(d));
-  }
-
-  return date;
-};
-
-I18n.toTime = function(scope, d) {
-  var date = this.parseDate(d);
-  var format = this.lookup(scope);
-
-  if (date.toString().match(/invalid/i)) {
-    return date.toString();
-  }
-
-  if (!format) {
-    return date.toString();
-  }
-
-  return this.strftime(date, format);
-};
+  var result = _localize.call(this, scope, value);
+  if (scope.match(/^(date|time)/))
+    result = result.replace(/\s{2,}/, ' ');
+  return result;
+}
 
 I18n.strftime = function(date, format) {
   var options = this.lookup("date");
@@ -369,166 +261,6 @@ I18n.strftime = function(date, format) {
   return f;
 };
 
-I18n.toNumber = function(number, options) {
-  options = this.prepareOptions(
-    options,
-    this.lookup("number.format"),
-    {precision: 3, separator: ".", delimiter: ",", strip_insignificant_zeros: false}
-  );
-
-  var negative = number < 0;
-  var string = Math.abs(number).toFixed(options.precision).toString();
-  var parts = string.split(".");
-
-  number = parts[0];
-  var precision = parts[1];
-
-  var n = [];
-
-  while (number.length > 0) {
-    n.unshift(number.substr(Math.max(0, number.length - 3), 3));
-    number = number.substr(0, number.length -3);
-  }
-
-  var formattedNumber = n.join(options.delimiter);
-
-  if (options.precision > 0) {
-    formattedNumber += options.separator + parts[1];
-  }
-
-  if (negative) {
-    formattedNumber = "-" + formattedNumber;
-  }
-
-  if (options.strip_insignificant_zeros) {
-    var regex = {
-        separator: new RegExp(options.separator.replace(/\./, "\\.") + "$")
-      , zeros: /0+$/
-    };
-
-    formattedNumber = formattedNumber
-      .replace(regex.zeros, "")
-      .replace(regex.separator, "");
-  }
-
-  return formattedNumber;
-};
-
-I18n.toCurrency = function(number, options) {
-  options = this.prepareOptions(
-    options,
-    this.lookup("number.currency.format"),
-    this.lookup("number.format"),
-    {unit: "$", precision: 2, format: "%u%n", delimiter: ",", separator: "."}
-  );
-
-  number = this.toNumber(number, options);
-  number = options.format
-    .replace("%u", options.unit)
-    .replace("%n", number);
-
-  return number;
-};
-
-I18n.toHumanSize = function(number, options) {
-  var kb = 1024
-    , size = number
-    , iterations = 0
-    , unit
-    , precision
-  ;
-
-  while (size >= kb && iterations < 4) {
-    size = size / kb;
-    iterations += 1;
-  }
-
-  if (iterations === 0) {
-    unit = this.t("number.human.storage_units.units.byte", {count: size});
-    precision = 0;
-  } else {
-    unit = this.t("number.human.storage_units.units." + [null, "kb", "mb", "gb", "tb"][iterations]);
-    precision = (size - Math.floor(size) === 0) ? 0 : 1;
-  }
-
-  options = this.prepareOptions(
-    options,
-    {precision: precision, format: "%n%u", delimiter: ""}
-  );
-
-  number = this.toNumber(size, options);
-  number = options.format
-    .replace("%u", unit)
-    .replace("%n", number);
-
-  return number;
-};
-
-I18n.toPercentage = function(number, options) {
-  options = this.prepareOptions(
-    options,
-    this.lookup("number.percentage.format"),
-    this.lookup("number.format"),
-    {precision: 3, separator: ".", delimiter: ""}
-  );
-
-  number = this.toNumber(number, options);
-  return number + "%";
-};
-
-I18n.pluralize = function(count, scope, options) {
-  var translation;
-
-  try {
-    translation = this.lookup(scope, options);
-  } catch (error) {}
-
-  if (!translation) {
-    return this.missingTranslation(scope);
-  }
-
-  var message;
-  options = this.prepareOptions(options);
-  options.count = count.toString();
-
-  switch(Math.abs(count)) {
-    case 0:
-      message = this.isValidNode(translation, "zero") ? translation.zero :
-                this.isValidNode(translation, "none") ? translation.none :
-                this.isValidNode(translation, "other") ? translation.other :
-                this.missingTranslation(scope, "zero");
-      break;
-    case 1:
-      message = this.isValidNode(translation, "one") ? translation.one : this.missingTranslation(scope, "one");
-      break;
-    default:
-      message = this.isValidNode(translation, "other") ? translation.other : this.missingTranslation(scope, "other");
-  }
-
-  return this.interpolate(message, options);
-};
-
-I18n.missingTranslation = function() {
-  var message = '[missing "' + this.currentLocale();
-  var count = arguments.length;
-
-  for (var i = 0; i < count; i++) {
-    message += "." + arguments[i];
-  }
-
-  message += '" translation]';
-
-  return message;
-};
-
-I18n.currentLocale = function() {
-  return (I18n.locale || I18n.defaultLocale);
-};
-
-// shortcuts
-I18n.t = I18n.translate;
-I18n.l = I18n.localize;
-I18n.p = I18n.pluralize;
 
 
 var normalizeDefault = function(str) { return str };
@@ -574,24 +306,14 @@ I18n.scope.prototype = {
   beforeLabel: function(text) {
     return this.t("#before_label_wrapper", "%{text}:", {'text': text});
   },
-  toTime: function(scope, d) {
-    return I18n.toTime(scope, d);
-  },
-  toNumber: function(number, options) {
-    return I18n.toNumber(number, options);
-  },
-  toCurrency: function(number, options) {
-    return I18n.toCurrency(number, options);
-  },
-  toHumanSize: function(number, options) {
-    return I18n.toHumanSize(number, options);
-  },
-  toPercentage: function(number, options) {
-    return I18n.toPercentage(number, options);
-  },
   lookup: function(scope, options) {
     return I18n.lookup(this.resolveScope(scope), options);
-  }
+  },
+  toTime:       I18n.toTime.bind(I18n),
+  toNumber:     I18n.toNumber.bind(I18n),
+  toCurrency:   I18n.toCurrency.bind(I18n),
+  toHumanSize:  I18n.toHumanSize.bind(I18n),
+  toPercentage: I18n.toPercentage.bind(I18n)
 };
 I18n.scope.prototype.t = I18n.scope.prototype.translate;
 I18n.scope.prototype.l = I18n.scope.prototype.localize;
