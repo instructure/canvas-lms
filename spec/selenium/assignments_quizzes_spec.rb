72f4bfacc984d1907895b07027a44ca6d8dae449
Sean Lewis <slewis@instructure.com>
2014-10-17 14:31:18 -0600

    spec: refactor graded discussion/quiz spec
    rewrote helper methods for building and editing assignments in draft state.
    rewrote all tests in assignments_discussion_spec and asssignments_discussion_spec
    to use new helper method.
    
    Change-Id: If2b619723cfa7c04510d4e233f912ecea482dae1
    Reviewed-on: https://gerrit.instructure.com/42931
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Product-Review: Kyle Rosenbaum <kyler@instructure.com>
    QA-Review: Kyle Rosenbaum <kyler@instructure.com>

--- spec/selenium/assignments_quizzes_spec.rb
+++ spec/selenium/assignments_quizzes_spec.rb
@@ -5,24 +5,26 @@ describe "quizzes assignments" do
 
 
   before (:each) do
+    @domain_root_account = Account.default
+    @domain_root_account.enable_feature!(:draft_state)
     course_with_teacher_logged_in
   end
 
   context "created on the index page" do
     it "should redirect to the quiz" do
+      ag = @course.assignment_groups.create!(:name => "Quiz group")
       get "/courses/#{@course.id}/assignments"
-      build_assignment_with_type("Quiz", :submit => true)
-      expect_new_page_load { f(".assignment_list .group_assignment .assignment_title a").click }
+      build_assignment_with_type("Quiz", :assignment_group_id => ag.id, :name => "New Quiz", :submit => true)
+      expect_new_page_load { f("#assignment_group_#{ag.id}_assignments .ig-title").click }
       expect(driver.current_url).to match %r{/courses/\d+/quizzes/\d+}
     end
   end
 
   context "created with 'more options'" do
     it "should redirect to the quiz new page and maintain parameters" do
+      ag = @course.assignment_groups.create!(:name => "Quiz group")
       get "/courses/#{@course.id}/assignments"
-      build_assignment_with_type("Quiz", :name => "Testy!")
-      expect_new_page_load { f('.more_options_link').click }
-      wait_for_ajaximations
+      expect_new_page_load { build_assignment_with_type("Quiz", :assignment_group_id => ag.id, :name => "Testy!", :more_options => true) }
       expect(fj('input[name="quiz[title]"]').attribute(:value)).to eq "Testy!"
     end
   end
@@ -31,9 +33,7 @@ describe "quizzes assignments" do
     it "should update quiz when updated" do
       assign = @course.assignments.create!(:name => "Testy!", :submission_types => "online_quiz")
       get "/courses/#{@course.id}/assignments"
-      driver.execute_script %{$('#assignment_#{assign.id} .edit_assignment_link:first').addClass('focus');}
-      f("#assignment_#{assign.id} .edit_assignment_link").click
-      edit_assignment(:name => "Retest!", :submit => true)
+      edit_assignment(assign.id, :name => "Retest!", :submit => true)
       expect(Quizzes::Quiz.find_by_assignment_id(assign.id).title).to eq "Retest!"
     end
   end
@@ -42,11 +42,7 @@ describe "quizzes assignments" do
     it "should redirect to the quiz edit page and maintain parameters" do
       assign = @course.assignments.create!(:name => "Testy!", :submission_types => "online_quiz")
       get "/courses/#{@course.id}/assignments"
-      driver.execute_script %{$('#assignment_#{assign.id} .edit_assignment_link:first').addClass('focus');}
-      f("#assignment_#{assign.id} .edit_assignment_link").click
-      edit_assignment(:name => "Retest!")
-      expect_new_page_load { f('.more_options_link').click }
-      wait_for_ajaximations
+      expect_new_page_load { edit_assignment(assign.id, :name => "Retest!", :more_options => true)}
       expect(fj('input[name="quiz[title]"]').attribute(:value)).to eq "Retest!"
     end
   end
--- spec/selenium/helpers/assignments_common.rb
+++ spec/selenium/helpers/assignments_common.rb
@@ -3,30 +3,61 @@ require File.expand_path(File.dirname(__FILE__) + '/../common')
 
 
   def build_assignment_with_type(type, opts={})
-    driver.execute_script %{$('.header_content .add_assignment_link:first').addClass('focus');}
-    f(".header_content .add_assignment_link").click
-    wait_for_ajaximations
-
-    edit_assignment({:type => type}.merge(opts))
-  end
-
-  def edit_assignment(opts={})
-    if opts[:type]
-      click_option(".assignment_submission_types", opts[:type])
+    if opts[:assignment_group_id]
+      assignment_group_id = opts[:assignment_group_id]
+    else
+      assignment_group = @course.assignment_groups.first!
+      assignment_group_id = assignment_group.id
     end
+
+    f("#assignment_group_#{assignment_group_id} .add_assignment").click
+    click_option(f("#ag_#{assignment_group_id}_assignment_type"), type)
 
     if opts[:name]
-      f('#assignment_title').clear
-      f('#assignment_title').send_keys opts[:name]
+      f("#ag_#{assignment_group_id}_assignment_name").clear
+      f("#ag_#{assignment_group_id}_assignment_name").send_keys opts[:name]
     end
-
     if opts[:points]
-      f('#assignment_points_possible').clear
-      f('#assignment_points_possible').send_keys opts[:points]
+      f("#ag_#{assignment_group_id}_assignment_points").clear
+      f("#ag_#{assignment_group_id}_assignment_points").send_keys opts[:points]
     end
-
+    if opts[:due_at]
+      f("#ag_#{assignment_group_id}_assignment_due_at").clear
+      f("#ag_#{assignment_group_id}_assignment_due_at").send_keys opts[:due_at]
+    end
     if opts[:submit]
-      submit_form("#add_assignment_form")
+      fj(".create_assignment:visible").click
+      wait_for_ajaximations
+    end
+    if opts[:more_options]
+      fj('.more_options:visible').click
       wait_for_ajaximations
     end
   end
+
+  def edit_assignment(assignment_id, opts={})
+    f("#assignment_#{assignment_id} .al-trigger").click
+    f("#assignment_#{assignment_id} .edit_assignment").click
+
+    if opts[:name]
+      f("#assign_#{assignment_id}_assignment_name").clear
+      f("#assign_#{assignment_id}_assignment_name").send_keys opts[:name]
+    end
+    if opts[:points]
+      f("#assign_#{assignment_id}_assignment_points").clear
+      f("#assign_#{assignment_id}_assignment_points").send_keys opts[:points]
+    end
+    if opts[:due_at]
+      f("#assign_#{assignment_id}_assignment_due_at").clear
+      f("#assign_#{assignment_id}_assignment_due_at").send_keys opts[:due_at]
+    end
+    if opts[:submit]
+      fj(".create_assignment:visible").click
+      wait_for_ajaximations
+    end
+    if opts[:more_options]
+      fj('.more_options:visible').click
+      wait_for_ajaximations
+    end
+  end
+
