8bb977155be944d6ff1c399e2d871ec6db712927
tdehaan <tdehaan@instructure.com>
2014-10-21 16:15:47 -0600

    spec: adds student quizzes with draft state spec
    TEST PLAN
    - run specs
    
    Change-Id: Iab1fea8aef419e074012f94426531bc35013be79
    Reviewed-on: https://gerrit.instructure.com/43124
    Reviewed-by: Ryan Taylor <rtaylor@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Finney <bfinney@instructure.com>
    Product-Review: Bryan Madsen <bryan@instructure.com>
    QA-Review: Bryan Madsen <bryan@instructure.com>

---
+++ spec/selenium/student_quizzes_with_draft_state_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path(File.dirname(__FILE__) + '/helpers/quizzes_common')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/assignment_overrides.rb')
+
+describe "quizzes with draft state" do
+  before :once do
+    Account.default.enable_feature!(:draft_state)
+  end
+
+  include AssignmentOverridesSeleniumHelper
+  include_examples "quizzes selenium tests"
+
+  before(:each) do
+    course_with_student_logged_in
+    @course.update_attributes(:name => 'teacher course')
+    @course.save!
+    @course.reload
+  end
+
+
+
+  context "with an unpublished quiz" do
+
+    it "should show an error to students" do
+      @context = @course
+      q = quiz_model
+      q.unpublish!
+      get "/courses/#{@course.id}/quizzes/#{q.id}"
+      wait_for_ajaximations
+      expect(f("#unauthorized_holder")).to be_displayed
+    end
+
+    it "should not be able to take unpublished quiz" do
+      @context = @course
+      q = quiz_model
+      q.unpublish!
+      get "/courses/#{@course.id}/quizzes/#{q.id}/take"
+      wait_for_ajaximations
+      expect(f("#unauthorized_holder")).to be_displayed
+    end
+  end
+end
+
