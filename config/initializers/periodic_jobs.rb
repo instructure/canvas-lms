fdd11e64432a6f1a340afe7d50ac0963451428cf
Cody Cutrer <cody@instructure.com>
2014-11-04 11:04:22 -0700

    use a per-db-server strand and parallelize all multi-shard periodic jobs
    fixes CNVS-15238
    
    Change-Id: I4a53ec8c79eaf25112fddacf46311eab6252b1a1
    Reviewed-on: https://gerrit.instructure.com/43851
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Nick Cloward <ncloward@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/periodic_jobs.rb
+++ config/initializers/periodic_jobs.rb
@@ -24,14 +24,14 @@ Rails.configuration.after_initialize do
   persistence_token_expire_after = (ConfigFile.load("session_store") || {})[:expire_remember_me_after]
   persistence_token_expire_after ||= 1.month
   Delayed::Periodic.cron 'SessionPersistenceToken.delete_all', '35 11 * * *' do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      SessionPersistenceToken.delete_all(['updated_at < ?', persistence_token_expire_after.ago])
+    Shard.with_each_shard do
+      SessionPersistenceToken.send_later_enqueue_args(:delete_all, { strand: "SessionPersistenceToken.delete_all:#{Shard.current.database_server.id}", max_attempts: 1 }, ['updated_at < ?', persistence_token_expire_after.ago])
     end
   end
 
   Delayed::Periodic.cron 'ExternalFeedAggregator.process', '*/30 * * * *' do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      ExternalFeedAggregator.process
+    Shard.with_each_shard do
+      ExternalFeedAggregator.send_later_enqueue_args(:process, strand: "ExternalFeedAggregator.process:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
@@ -43,8 +43,8 @@ Rails.configuration.after_initialize do
 
   Delayed::Periodic.cron 'CrocodocDocument.update_process_states', '*/5 * * * *' do
     if Canvas::Crocodoc.config
-      Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-        CrocodocDocument.update_process_states
+      Shard.with_each_shard do
+        CrocodocDocument.send_later_enqueue_args(:update_process_states, strand: "CrocodocDocument.update_process_sets:#{Shard.current.database_server.id}", max_attempts: 1)
       end
     end
   end
@@ -54,8 +54,8 @@ Rails.configuration.after_initialize do
   end
 
   Delayed::Periodic.cron 'StreamItem.destroy_stream_items', '45 11 * * *' do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      StreamItem.destroy_stream_items_using_setting
+    Shard.with_each_shard do
+      StreamItem.send_later_enqueue_args(:destroy_stream_items_using_setting, strand: "StreamItem.destroy_stream_items:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
@@ -68,39 +68,39 @@ Rails.configuration.after_initialize do
   Delayed::Periodic.cron 'ErrorReport.destroy_error_reports', '35 */1 * * *' do
     cutoff = Setting.get('error_reports_retain_for', 3.months.to_s).to_i
     if cutoff > 0
-      Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-        ErrorReport.destroy_error_reports(cutoff.ago)
+      Shard.with_each_shard do
+        ErrorReport.send_later_enqueue_args(:destroy_error_reports, { strand: "ErrorReport.destroy_error_reports:#{Shard.current.database_server.id}", max_attempts: 1 }, cutoff.ago)
       end
     end
   end
 
   Delayed::Periodic.cron 'Alerts::DelayedAlertSender.process', '30 11 * * *', :priority => Delayed::LOW_PRIORITY do
     Shard.with_each_shard do
-      Alerts::DelayedAlertSender.process
+      Alerts::DelayedAlertSender.send_later_enqueue_args(:process, strand: "Alerts::DelayedAlertSender.process:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
   Delayed::Periodic.cron 'Attachment.do_notifications', '*/10 * * * *', :priority => Delayed::LOW_PRIORITY do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      Attachment.do_notifications
+    Shard.with_each_shard do
+      Attachment.send_later_enqueue_args(:do_notifications, strand: "Attachment.do_notifications:#{Shard.current.database_server.id}")
     end
   end
 
   Delayed::Periodic.cron 'Ignore.cleanup', '45 23 * * *' do
     Shard.with_each_shard do
-      Ignore.send_later_enqueue_args(:cleanup, :singleton => "Ignore.cleanup:#{Shard.current.id}")
+      Ignore.send_later_enqueue_args(:cleanup, strand: "Ignore.cleanup:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
   Delayed::Periodic.cron 'MessageScrubber.scrub_all', '0 0 * * *' do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      MessageScrubber.new.scrub
+    Shard.with_each_shard do
+      MessageScrubber.send_later_enqueue_args(:scrub, strand: "MessageScrubber.scrub_all:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
   Delayed::Periodic.cron 'DelayedMessageScrubber.scrub_all', '0 1 * * *' do
-    Shard.with_each_shard(exception: -> { ErrorReport.log_exception(:periodic_job, $!) }) do
-      DelayedMessageScrubber.new.scrub
+    Shard.with_each_shard do
+      DelayedMessageScrubber.send_later_enqueue_args(:scrub, strand: "DelayedMessageScrubber.scrub_all:#{Shard.current.database_server.id}", max_attempts: 1)
     end
   end
 
