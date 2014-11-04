54f34c67caf3df566cced3b5ce9ace40ab82649f
Mark Severson <markse@instructure.com>
2014-11-04 10:42:56 -0700

    allow lti tool to set a beforeunload message
    refs CNVS-16272
    
    test plan (in supported browsers):
     * post this message from an lti tool:
        parent.postMessage(JSON.stringify({
          subject: 'lti.setUnloadMessage',
          message: 'Interrupting navigation'
        }), '*')
     * attempt to navigate away from the page
     * a navigation confirmation dialog should be shown
     * post this message from an lti tool:
        parent.postMessage(JSON.stringify({
          subject: 'lti.removeUnloadMessage'
        }), '*')
     * attempt to navigate away from the page
     * the navigation should be successful without a confirmation
    
    Change-Id: I9b41d18b79e7830145445137a75c01aeb61f748c
    Reviewed-on: https://gerrit.instructure.com/43844
    Reviewed-by: Nathan Mills <nathanm@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Steven Shepherd <sshepherd@instructure.com>
    Product-Review: Bracken Mosbacker <bracken@instructure.com>

--- public/javascripts/tool_inline.js
+++ public/javascripts/tool_inline.js
@@ -112,13 +112,23 @@ $(function() {
 window.addEventListener('message', function(e) {
   try {
     var message = JSON.parse(e.data);
-    if (message.subject === 'lti.frameResize') {
-      var height = message.height;
-      if (height >= 5000) height = 5000;
-      if (height <= 0) height = 1;
+    switch (message.subject) {
+      case 'lti.frameResize':
+        var height = message.height;
+        if (height >= 5000) height = 5000;
+        if (height <= 0) height = 1;
 
-      tool_content_wrapper().data('height_overridden', true);
-      resize_tool_content_wrapper(height);
+        tool_content_wrapper().data('height_overridden', true);
+        resize_tool_content_wrapper(height);
+        break;
+
+      case 'lti.setUnloadMessage':
+        setUnloadMessage(message.message);
+        break;
+
+      case 'lti.removeUnloadMessage':
+        removeUnloadMessage();
+        break;
     }
   } catch(err) {
     (console.error || console.log)('invalid message received from ', e.origin);
@@ -134,5 +144,21 @@ window.addEventListener('message', function(e) {
 
 });
 
+var beforeUnloadHandler;
+function setUnloadMessage(msg) {
+  removeUnloadMessage();
+
+  beforeUnloadHandler = function(e) {
+    return (e.returnValue = msg || "");
+  }
+  window.addEventListener('beforeunload', beforeUnloadHandler);
+}
+
+function removeUnloadMessage() {
+  if (beforeUnloadHandler) {
+    window.removeEventListener('beforeunload', beforeUnloadHandler);
+    beforeUnloadHandler = null;
+  }
+}
 
 });
