772ef5ab321aa1c47da37291987c26122422f93d
Jacob Fugal <jacob@instructure.com>
2014-11-14 09:45:04 -0700

    use SSL-only flag on CSRF cookie
    fixes CNVS-16884
    
    test-plan:
     - in HTTP-session environment (`secure: false` or unset in
       config/session_store.yml), CSRF cookie should be sent without secure
       flag
    
     - in HTTPS-session environment (`secure: true` in
       config/session_store.yml), CSRF cookie should be sent with secure
       flag.
    
     - on a non-files host, javascript should be able to read the CSRF
       cookie: value of "document.cookie" in javascript console should
       include "_csrf_token"
    
     - on a files host, javascript should not be able to read the CSRF
       cookie: value of "document.cookie" in javascript console should not
       include "_csrf_token"
    
    Change-Id: Ifd57c973478a6d07497a404dcf1a9b9caa9014af
    Reviewed-on: https://gerrit.instructure.com/44451
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/canvas_breach_mitigation/spec/masking_secrets_spec.rb
+++ gems/canvas_breach_mitigation/spec/masking_secrets_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe CanvasBreachMitigation::MaskingSecrets do
   before do
-    Rails = double("Rails").as_null_object unless defined? Rails
+    Rails = mock("Rails") unless defined? Rails
   end
 
   let(:masking_secrets) { CanvasBreachMitigation::MaskingSecrets }
@@ -11,59 +11,85 @@ describe CanvasBreachMitigation::MaskingSecrets do
     it "puts :_csrf_token into the supplied object" do
       hash = {}
       masking_secrets.masked_authenticity_token(hash)
-      hash['_csrf_token'].should_not be_nil
+      expect(hash['_csrf_token']).not_to be nil
     end
 
     it "returns a byte string" do
-      masking_secrets.masked_authenticity_token({}).should_not be nil
+      expect(masking_secrets.masked_authenticity_token({})).not_to be nil
     end
   end
 
   describe ".valid_authenticity_token?" do
-    let(:cookies) do
-      # Seed a "cookie jar" with a :_csrf_token
-      Hash.new.tap do |cookies|
-        masking_secrets.masked_authenticity_token(cookies)
-      end
-    end
-
-    let(:session) do
-      Hash.new
-    end
-
-    it "returns true for a valid unmasked token" do
-      valid_unmasked = cookies['_csrf_token']
-      masking_secrets.valid_authenticity_token?(session, cookies, valid_unmasked).should == true
-    end
-
-    it "returns false for an invalid unmasked token" do
-      masking_secrets.valid_authenticity_token?(session, cookies, SecureRandom.base64(32)).should == false
-    end
+    let(:unmasked_token) { SecureRandom.base64(32) }
+    let(:cookies) { {'_csrf_token' => masking_secrets.masked_token(unmasked_token)} }
+    let(:session) { {} }
 
     it "returns true for a valid masked token" do
-      valid_masked = masking_secrets.masked_authenticity_token(cookies)
-      masking_secrets.valid_authenticity_token?(session, cookies, valid_masked).should == true
+      valid_masked = masking_secrets.masked_token(unmasked_token)
+      expect(masking_secrets.valid_authenticity_token?(session, cookies, valid_masked)).to be true
     end
 
     it "returns false for an invalid masked token" do
-      masking_secrets.valid_authenticity_token?(session, cookies, SecureRandom.base64(64)).should == false
+      expect(masking_secrets.valid_authenticity_token?(session, cookies, SecureRandom.base64(64))).to be false
     end
 
     it "returns false for a token of the wrong length" do
-      masking_secrets.valid_authenticity_token?(session, cookies, SecureRandom.base64(2)).should == false
+      expect(masking_secrets.valid_authenticity_token?(session, cookies, SecureRandom.base64(2))).to be false
     end
 
     it "returns true if the session is still set instead of the cookies" do
       cookies.delete('_csrf_token')
-
       session[:_csrf_token] = SecureRandom.base64(32)
-      one_time_pad = SecureRandom.random_bytes(32)
+      encoded_masked_token = masking_secrets.masked_token(Base64.strict_decode64(session[:_csrf_token]))
+      expect(masking_secrets.valid_authenticity_token?(session, cookies, encoded_masked_token)).to be true
+    end
+  end
 
-      encrypted_csrf_token = masking_secrets.send(:xor_byte_strings, one_time_pad, Base64.strict_decode64(session[:_csrf_token]))
-      masked_token = one_time_pad + encrypted_csrf_token
-      encoded_masked_token = Base64.strict_encode64(masked_token)
+  describe ".masked_authenticity_token" do
+    let(:cookies) { {} }
 
-      masking_secrets.valid_authenticity_token?(session, cookies, encoded_masked_token).should == true
+    it "initializes the _csrf_token cookie" do
+      token = masking_secrets.masked_authenticity_token(cookies)
+      expect(cookies['_csrf_token'][:value]).to eq token
+    end
+
+    it "initializes the _csrf_token cookie without explicit domain by default" do
+      masking_secrets.masked_authenticity_token(cookies)
+      expect(cookies['_csrf_token']).not_to be_has_key(:domain)
+    end
+
+    it "initializes the _csrf_token cookie without explicit httponly by default" do
+      masking_secrets.masked_authenticity_token(cookies)
+      expect(cookies['_csrf_token']).not_to be_has_key(:httponly)
+    end
+
+    it "initializes the _csrf_token cookie without explicit secure by default" do
+      masking_secrets.masked_authenticity_token(cookies)
+      expect(cookies['_csrf_token']).not_to be_has_key(:secure)
+    end
+
+    it "copies domain option to _csrf_token cookie" do
+      masking_secrets.masked_authenticity_token(cookies, domain: "domain")
+      expect(cookies['_csrf_token'][:domain]).to eq "domain"
+    end
+
+    it "copies httponly option to _csrf_token cookie" do
+      masking_secrets.masked_authenticity_token(cookies, httponly: true)
+      expect(cookies['_csrf_token'][:httponly]).to be true
+    end
+
+    it "copies secure option to _csrf_token cookie" do
+      masking_secrets.masked_authenticity_token(cookies, secure: true)
+      expect(cookies['_csrf_token'][:secure]).to be true
+    end
+
+    it "remasks an existing _csrf_token cookie" do
+      original_token = masking_secrets.masked_token(SecureRandom.base64(32))
+      cookies['_csrf_token'] = original_token
+      token = masking_secrets.masked_authenticity_token(cookies)
+      expect(token).not_to eq original_token
+      expect(cookies['_csrf_token'][:value]).to eq token
+      expect(masking_secrets.valid_authenticity_token?({}, {'_csrf_token' => token}, original_token)).to be true
     end
   end
 end
