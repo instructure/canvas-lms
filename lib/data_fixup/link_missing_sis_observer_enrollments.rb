8c9fe2c690b9db4fee40be4aadcba921c968e661
James Williams <jamesw@instructure.com>
2014-11-24 14:28:00 -0700

    don't send notifications for added observer enrollments in datafix
    refs #CNVS-16391
    
    Change-Id: Ib2d0044b96fe51cc5f232655b6b30ba1f45bd2ca
    Reviewed-on: https://gerrit.instructure.com/44844
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- lib/data_fixup/link_missing_sis_observer_enrollments.rb
+++ lib/data_fixup/link_missing_sis_observer_enrollments.rb
@@ -5,7 +5,16 @@ module DataFixup::LinkMissingSisObserverEnrollments
         if enrollment.linked_enrollment_for(uo.observer).nil? && uo.observer.can_be_enrolled_in_course?(enrollment.course)
           new_enrollment = uo.observer.observer_enrollments.build
           new_enrollment.associated_user_id = enrollment.user_id
-          new_enrollment.update_from(enrollment)
+
+          new_enrollment.course_id = enrollment.course_id
+          new_enrollment.workflow_state = enrollment.workflow_state
+          new_enrollment.start_at = enrollment.start_at
+          new_enrollment.end_at = enrollment.end_at
+          new_enrollment.course_section_id = enrollment.course_section_id
+          new_enrollment.root_account_id = enrollment.root_account_id
+          new_enrollment.save_without_broadcasting!
+
+          uo.observer.touch
         end
       end
     end
