f9952d0f92611a4f296ee5a80f06c74a475e0f79
Brian Sadler <brian.sadler@beyondz.org>
2014-12-04 10:49:42 -0800

    Update Capistrano deploy configuration to do the first real deploy (e.g. setup releases, current, shared dirs)

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,21 @@ lock '3.2.1'
 
 set :application, 'canvas'
 set :repo_url, 'git@github.com:beyond-z/canvas-lms.git'
+set :rails_env,     'production'
+set :user,          'canvasuser'
+set :group,         'canvasadmin'
+
+# These settings were pulled from here: https://github.com/sfu/canvas-lms/blob/sfu-develop/config/deploy.rb
+# The reason is that canvas:compile_assets requires development gems (parallel-0.5.16) and by default
+# the capistrano/bundle lib runs this: 
+#     /usr/bin/env bundle install --binstubs /var/canvas/shared/bin --path /var/canvas/shared/bundle --without development test --deployment
+# which doesn't install parallel-0.5.16 in the shared/bundle directory.
+# 
+# This essentially makes Capistrano run the "bundle install" command found in the Canvas Production Start guide
+# which doesn't specify the "--without development test" options.
+set :bundle_path, "vendor/bundle"
+set :bundle_without, nil
+set :bundle_flags,  ""
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -23,10 +38,14 @@ set :deploy_to, '/var/canvas'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+#set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# Oh nelly, DO NOT add the "config" dir in here.  Symlinked config files don't work
+# for core ruby config, like boot.rb when it sets RAILS_ROOT.  You'll get Rack loading
+# errors when it can't find files (e.g. lib/canvas_logger).  This is the reason that
+# configs are copied over in the copy_configs task.
+set :linked_dirs, %w{bin log tmp/pids public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,33 +53,124 @@ set :deploy_to, '/var/canvas'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+# Canvas uses it's own precompile assets defined below.
+Rake::Task["deploy:compile_assets"].clear_actions
+
+# Disable for now until we get the basic Cap deploy and rollback going for code and can really test this.
+Rake::Task["deploy:migrate"].clear_actions
+
 namespace :deploy do
 
-  desc 'Update application code'
-  task :update do
-    on roles(:app) do |host|
-      within fetch(:deploy_to) do
-        execute :git, 'pull', 'origin', fetch(:branch)
-        execute :sudo, 'apachectl', 'graceful'
+  # TODO: change this to tag the git branch. 
+  #desc 'Update application code'
+  #task :update do
+  #  on roles(:app) do |host|
+  #    within fetch(:deploy_to) do
+  #      execute :git, 'pull', 'origin', fetch(:branch)
+  #    end
+  #  end
+  #end
+
+  desc "Copy config files from <deploy_to>/config to the release directory"
+  task :copy_config do
+    on roles(:app) do
+      execute :sudo, 'cp -rp', "#{fetch(:deploy_to)}/config/*", "#{release_path}/config"
+    end
+  end
+
+  before :updated, :copy_config
+
+  desc "Clone QTIMigrationTool so that course import and export works"
+  task :clone_qtimigrationtool do
+    on roles(:app) do
+      within release_path do
+        execute :git, 'clone', 'https://github.com/instructure/QTIMigrationTool.git', 'vendor/QTIMigrationTool'
+        execute :sudo, 'chmod', '+x vendor/QTIMigrationTool/migrate.py'
       end
     end
   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :sudo, 'apachectl', 'graceful'
+  before :updated, :clone_qtimigrationtool
+
+  desc "Migrate database"
+  task :migrate do
+    # TODO: need to get this working, but for now we're just focusing on getting a code deploy and rollback flow going
+  end
+
+  desc "Install node dependencies"
+  task :npm_install do
+    on roles(:app) do
+      within release_path do
+        execute :npm, 'install', '--silent'
+      end
     end
   end
 
-  after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  # TODO: This takes forever, see if we can copy the assets from the last deploy if nothing has changed.
+  # Try this: https://coderwall.com/p/aridag/only-precompile-assets-when-necessary-rails-4-capistrano-3
+  desc "Compile static assets"
+  task :compile_assets => :npm_install do
+   on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          # Note: this took me forever to get going because the "deploy" user that it runs as needs rwx permissions on many
+          # files in the config directory, however, we setup those files to only be accessible from canvasuser.
+          # The way it works now is that /var/canvas/config has the master copies of the files owned by "canvasadmin:canvasadmin"
+          # with their permissions set loosely enough on the group so that compile_assets will work since "deploy" is in the 
+          # "canvasadmin" group.
+          #execute :rake, 'canvas:compile_assets --trace'
+          execute :rake, 'canvas:compile_assets'
+        end
+      end
+    end
+  end
+
+  desc "Fix ownership on Canvas install directory"
+  task :fix_owner do
+    on roles(:app) do
+      user = fetch :user
+      group = fetch :group
+      execute :sudo, 'chown -R', "#{group}:#{group}", "#{release_path}"
+      within release_path do
+        execute :sudo, 'chown', "#{user}", "config/*", "Gemfile.lock", "config.ru"
+        execute :sudo, 'chmod 400', "config/*.yml"
+      end
+    end
+  end
+
+  after :compile_assets, :fix_owner
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app) do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :published, :restart
+
+  #after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+  #  end
+  #end
+
+  # Note: see this for a way to run start, stop, or restart: https://github.com/grahamb/capistrano-canvas/blob/master/lib/capistrano/tasks/canvas.rake
+  namespace :delayed_jobs do
+    %w[start stop restart].each do |command|
+      desc "#{command} the delayed_jobs processor"
+      task command do
+        on roles(:db) do
+          execute :sudo, '/etc/init.d/canvas_init', "#{command}"
+        end
+      end
     end
   end
+
+  after :published, 'deploy:delayed_jobs:restart'
+
 end
