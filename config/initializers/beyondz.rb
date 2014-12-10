d57ac81e4beccc3ba5f78bf820b9d785ca99367d
Brian Sadler <brian.sadler@beyondz.org>
2014-12-10 15:27:53 -0800

    Fix the Google Analytics code to load config properly using the old Rails way

--- config/initializers/beyondz.rb
+++ config/initializers/beyondz.rb
@@ -1,9 +1,17 @@
 class BeyondZConfiguration
   @config = (ConfigFile.load("beyondz") || {}).symbolize_keys
 
+  # Returns the URL of the configured public facing beyondz.org server with the
+  # specified path_symbol concatenated.
   def self.url(path_symbol)
     path = @config[path_symbol]
     path = path[1 .. -1] if path.starts_with?('/')
     "#{@config[:base_url]}#{path}"
   end
+
+  # Returns the Google Analytics token if configured or nil if not.  Example token: UA-XXXXXXXX-1 
+  def self.google_analytics_account
+    @config[:google_analytics_account]
+  end
+
 end
