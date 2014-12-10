d01e60377f2a2b20730cc34285d4af92c954c186
Addison Higham <ahigham@instructure.com>
2014-12-10 15:36:41 -0700

    Make Guard coffeescript compilation include line numbers when the build fails
    Change-Id: If06afc8db7d13c9ecd7737e452dfef7eba32e4e2
    Reviewed-on: https://gerrit.instructure.com/45581
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- Gemfile.d/development_and_test.rb
+++ Gemfile.d/development_and_test.rb
@@ -1,5 +1,5 @@
 group :development, :test do
-  gem 'coffee-script', '2.2.0'
+  gem 'coffee-script', '2.3.0'
   gem 'coffee-script-source', '1.6.2' #pinned so everyone's compiled output matches
   gem 'execjs', '1.4.0'
   gem 'hashdiff', '0.2.0'
--- lib/canvas/coffee_script.rb
+++ lib/canvas/coffee_script.rb
@@ -3,6 +3,9 @@ module Canvas
 class CoffeeScript
   def self.coffee_script_binary_is_available?
     return @is_available if instance_variable_defined?(:@is_available)
+    # patch up the path to make sure we get node_modules bin, which should ensure the proper
+    # pinned coffee-script
+    ENV['PATH'] = 'node_modules/.bin:' + ENV['PATH']
     coffee_is_installed = `which coffee` && $?.success?
     if coffee_is_installed
       coffee_version = `coffee -v`.strip
