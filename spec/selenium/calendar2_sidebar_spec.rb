ccc108e7a8102640aa0348be9dba01d6a19ce7b2
Derek Hansen <dhansen@instructure.com>
2014-12-23 14:52:54 -0700

    Split apart spec/selenium/calendar2_spec.rb
    Fixes CNVS-16488
    
    Test Plan:
    * Make sure jenkins passes
    
    Change-Id: I5be6bea11991955ed1b493e4a202596f542c5352
    Reviewed-on: https://gerrit.instructure.com/46279
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Matthew Wheeler <mwheeler@instructure.com>
    Product-Review: Matthew Wheeler <mwheeler@instructure.com>
    QA-Review: Derek Hansen <dhansen@instructure.com>

---
+++ spec/selenium/calendar2_sidebar_spec.rb
@@ -0,0 +1,102 @@
+require File.expand_path(File.dirname(__FILE__) + '/common')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/calendar2_common')
+
+describe "calendar2" do
+  include_examples "in-process server selenium tests"
+
+  before (:each) do
+    Account.default.tap do |a|
+      a.settings[:show_scheduler]   = true
+      a.save!
+    end
+  end
+
+  context "as a teacher" do
+    before (:each) do
+      course_with_teacher_logged_in
+    end
+
+    describe "sidebar" do
+      describe "mini calendar" do
+        it "should add the event class to days with events" do
+          c = make_event
+          get "/calendar2"
+          wait_for_ajax_requests
+
+          events = ff("#minical .event")
+          expect(events.size).to eq 1
+          expect(events.first.text.strip).to eq c.start_at.day.to_s
+        end
+
+        it "should change the main calendars month on click" do
+          title_selector = ".navigation_title"
+          get "/calendar2"
+
+          orig_titles = ff(title_selector).map(&:text)
+          f("#minical .fc-other-month").click
+
+          expect(orig_titles).not_to eq ff(title_selector).map(&:text)
+        end
+      end
+
+      describe "contexts list" do
+        it "should toggle event display when context is clicked" do
+          make_event :context => @course, :start => Time.now
+          get "/calendar2"
+
+          f('.context_list_context').click
+          context_course_item = fj('.context_list_context:nth-child(2)')
+          expect(context_course_item).to have_class('checked')
+          expect(f('.fc-event')).to be_displayed
+
+          context_course_item.click
+          expect(context_course_item).to have_class('not-checked')
+          expect(element_exists('.fc_event')).to be_falsey
+        end
+
+        it "should constrain context selection to 10" do
+          30.times do |x|
+            course_with_teacher(:course_name => "Course #{x + 1}", :user => @user, :active_all => true).course
+          end
+
+          get "/calendar2"
+          ff('.context_list_context').each(&:click)
+          expect(ff('.context_list_context.checked').count).to eq 10
+        end
+
+        it "should validate calendar feed display" do
+          get "/calendar2"
+
+          f('#calendar-feed a').click
+          expect(f('#calendar_feed_box')).to be_displayed
+        end
+      end
+
+      describe "undated calendar items" do
+        it "should show undated events after clicking link" do
+          e = make_event :start => nil, :title => "pizza party"
+          get "/calendar2"
+
+          f("#undated-events-button").click
+          f("#undated-events-button").click
+          wait_for_ajaximations
+          undated_events = ff("#undated-events > ul > li")
+          expect(undated_events.size).to eq 1
+          expect(undated_events.first.text).to match /#{e.title}/
+        end
+
+        it "should truncate very long undated event titles" do
+          make_event :start => nil, :title => "asdfjkasldfjklasdjfklasdjfklasjfkljasdklfjasklfjkalsdjsadkfljasdfkljfsdalkjsfdlksadjklsadjsadklasdf"
+          get "/calendar2"
+
+          f("#undated-events-button").click
+          f("#undated-events-button").click
+          wait_for_ajaximations
+          undated_events = ff("#undated-events > ul > li")
+          expect(undated_events.size).to eq 1
+          expect(undated_events.first.text).to eq "asdfjkasldfjklasdjfklasdjfklasjf..."
+        end
+      end
+    end
+  end
+end
--- spec/selenium/helpers/calendar2_common.rb
+++ spec/selenium/helpers/calendar2_common.rb
@@ -34,3 +34,101 @@ require File.expand_path(File.dirname(__FILE__) + '/../common')
     driver.execute_script("$('.edit_event_link').trigger('click')")
     wait_for_ajaximations
   end
+
+def make_event(params = {})
+  opts = {
+      :context => @user,
+      :start => Time.now,
+      :description => "Test event"
+  }.with_indifferent_access.merge(params)
+  c = CalendarEvent.new :description => opts[:description],
+                        :start_at => opts[:start],
+                        :title => opts[:title]
+  c.context = opts[:context]
+  c.save!
+  c
+end
+
+def find_middle_day
+  f('.calendar .fc-week:nth-child(1) .fc-wed')
+end
+
+def change_calendar(direction = :next)
+  css_selector = case direction
+                   when :next then
+                     '.navigate_next'
+                   when :prev then
+                     '.navigate_prev'
+                   when :today then
+                     '.navigate_today'
+                   else
+                     raise "unrecognized direction #{direction}"
+                 end
+
+  f('.calendar_header ' + css_selector).click
+  wait_for_ajax_requests
+end
+
+def quick_jump_to_date(text)
+  f('.navigation_title').click
+  dateInput = keep_trying_until { f('.date_field') }
+  dateInput.send_keys(text + "\n")
+  wait_for_ajaximations
+end
+
+def add_date(middle_number)
+  fj('.ui-datepicker-trigger:visible').click
+  datepicker_current(middle_number)
+end
+
+def create_assignment_event(assignment_title, should_add_date = false, publish = false)
+  middle_number = find_middle_day.find_element(:css, '.fc-day-number').text
+  find_middle_day.click
+  edit_event_dialog = f('#edit_event_tabs')
+  expect(edit_event_dialog).to be_displayed
+  edit_event_dialog.find_element(:css, '.edit_assignment_option').click
+  edit_assignment_form = edit_event_dialog.find_element(:id, 'edit_assignment_form')
+  title = edit_assignment_form.find_element(:id, 'assignment_title')
+  keep_trying_until { title.displayed? }
+  replace_content(title, assignment_title)
+  add_date(middle_number) if should_add_date
+  edit_assignment_form.find_element(:id, 'assignment_published').click if publish
+  submit_form(edit_assignment_form)
+  keep_trying_until { expect(f('.fc-view-month .fc-event-title')).to include_text(assignment_title) }
+end
+
+def create_calendar_event(event_title, should_add_date = false, should_add_location = false)
+  middle_number = find_middle_day.find_element(:css, '.fc-day-number').text
+  find_middle_day.click
+  edit_event_dialog = f('#edit_event_tabs')
+  expect(edit_event_dialog).to be_displayed
+  edit_event_form = edit_event_dialog.find_element(:id, 'edit_calendar_event_form')
+  title = edit_event_form.find_element(:id, 'calendar_event_title')
+  keep_trying_until { title.displayed? }
+  replace_content(title, event_title)
+  add_date(middle_number) if should_add_date
+  replace_content(f('#calendar_event_location_name'), 'location title') if should_add_location
+  submit_form(edit_event_form)
+  wait_for_ajax_requests
+  keep_trying_until { expect(f('.fc-view-month .fc-event-title')).to include_text(event_title) }
+end
+
+def get_header_text
+  header = f('.calendar_header .navigation_title')
+  header.text
+end
+
+def create_middle_day_event(name = 'new event', with_date = false, with_location = false)
+  get "/calendar2"
+  create_calendar_event(name, with_date, with_location)
+end
+
+def create_middle_day_assignment(name = 'new assignment')
+  get "/calendar2"
+  create_assignment_event(name)
+end
+
+def create_published_middle_day_assignment
+  get "/calendar2"
+  create_assignment_event(name = 'new assignment', false, true)
+end
