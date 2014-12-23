4cdd5b895503a8d4846a43fe90801b286c588eb7
Jeremy Stanley <jeremy@instructure.com>
2014-12-22 21:08:04 -0700

    fix ChangeEnrollmentsRoleIdNull
    test plan: fixes migrations with lingering StudentViewEnrollments
    with null role_id
    
    fixes CNVS-17644
    
    Change-Id: Ic75dee9a73447afdd21aabb0ef9732f9dcd74d30
    Reviewed-on: https://gerrit.instructure.com/46239
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>

--- db/migrate/20141212134557_change_enrollments_role_id_null.rb
+++ db/migrate/20141212134557_change_enrollments_role_id_null.rb
@@ -3,9 +3,34 @@ class ChangeEnrollmentsRoleIdNull < ActiveRecord::Migration
   tag :postdeploy
 
   def up
-    role = Role.get_built_in_role("ObserverEnrollment")
-    while Enrollment.where("role_id IS NULL AND type = ?", role.name).limit(1000).update_all(:role_id => role.id) > 0; end
+    cleanup_observer_enrollments
+    cleanup_student_view_enrollments
     change_column_null_with_less_locking :enrollments, :role_id
+  end
+
+  def cleanup_observer_enrollments
+    role = Role.get_built_in_role("ObserverEnrollment")
+    Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
+      Enrollment.where(id: start_id..end_id, role_id: nil, type: role.name).update_all(:role_id => role.id)
+    end
+  end
+
+  def cleanup_student_view_enrollments
+    student_role = Role.get_built_in_role("StudentEnrollment")
+    # set the proper role_id where this has not already happened manually
+    Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
+      Enrollment.where(<<-SQL, start_id, end_id, student_role.id).update_all(:role_id => student_role.id)
+        (id BETWEEN ? AND ?) AND role_id IS NULL AND type='StudentViewEnrollment' AND NOT EXISTS
+        (SELECT 1 FROM enrollments AS e2 WHERE e2.user_id=enrollments.user_id AND
+          e2.type='StudentViewEnrollment' AND e2.role_id=? AND
+          e2.course_section_id=enrollments.course_section_id AND
+          e2.associated_user_id IS NULL)
+      SQL
+    end
+    # delete remaining (obsolete) StudentViewEnrollments with no role_id
+    Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
+      Enrollment.where(id: start_id..end_id, type: 'StudentViewEnrollment', role_id: nil).delete_all
+    end
   end
 
   def down
