d61372d717a4af39c5002409ba0c0fab52489556
Derek Hansen <dhansen@instructure.com>
2014-12-26 11:32:26 -0700

    spec has 3 sections that should be split to better match testrails structure.
    fixes: CNVS-17672
    
    Test Plan:
    * Jenkins should pass specs
    
    Change-Id: If01a5abb4c4b8658aeb796d338860ffafbe9150c
    Reviewed-on: https://gerrit.instructure.com/46314
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Matthew Wheeler <mwheeler@instructure.com>
    Product-Review: Matthew Wheeler <mwheeler@instructure.com>
    QA-Review: Derek Hansen <dhansen@instructure.com>

---
+++ spec/selenium/conversations_replying_spec.rb
@@ -0,0 +1,71 @@
+require File.expand_path(File.dirname(__FILE__) + '/helpers/conversations_common')
+
+describe "conversations new" do
+  include_examples "in-process server selenium tests"
+
+  before do
+    conversation_setup
+    @s1 = user(name: "first student")
+    @s2 = user(name: "second student")
+    [@s1, @s2].each { |s| @course.enroll_student(s).update_attribute(:workflow_state, 'active') }
+    cat = @course.group_categories.create(:name => "the groups")
+    @group = cat.groups.create(:name => "the group", :context => @course)
+    @group.users = [@s1, @s2]
+  end
+
+  describe "replying" do
+    before do
+      cp = conversation(@s1, @teacher, @s2, workflow_state: 'unread')
+      @convo = cp.conversation
+      @convo.update_attribute(:subject, 'homework')
+      @convo.add_message(@s1, "What's this week's homework?")
+      @convo.add_message(@s2, "I need the homework too.")
+    end
+
+    it "should maintain context and subject" do
+      get_conversations
+      conversation_elements[0].click
+      wait_for_ajaximations
+      fj('#reply-btn').click
+      expect(fj('#compose-message-course')).to have_attribute(:disabled, 'true')
+      expect(fj('#compose-message-course')).to have_value(@course.id.to_s)
+      expect(fj('#compose-message-subject')).to have_attribute(:disabled, 'true')
+      expect(fj('#compose-message-subject')).not_to be_displayed
+      expect(fj('#compose-message-subject')).to have_value(@convo.subject)
+      expect(fj('.message_subject_ro')).to be_displayed
+      expect(fj('.message_subject_ro').text).to eq @convo.subject
+    end
+
+    it "should address replies to the most recent author by default" do
+      get_conversations
+      conversation_elements[0].click
+      wait_for_ajaximations
+      fj('#reply-btn').click
+      expect(ffj('input[name="recipients[]"]').length).to eq 1
+      expect(fj('input[name="recipients[]"]')).to have_value(@s2.id.to_s)
+    end
+
+    it "should add new messages to the conversation" do
+      get_conversations
+      initial_message_count = @convo.conversation_messages.length
+      conversation_elements[0].click
+      wait_for_ajaximations
+      fj('#reply-btn').click
+      set_message_body('Read chapters five and six.')
+      click_send
+      wait_for_ajaximations
+      expect(ffj('.message-item-view').length).to eq initial_message_count + 1
+      @convo.reload
+      expect(@convo.conversation_messages.length).to eq initial_message_count + 1
+    end
+
+    it "should not allow adding recipients to private messages" do
+      @convo.update_attribute(:private_hash, '12345')
+      get_conversations
+      conversation_elements[0].click
+      wait_for_ajaximations
+      fj('#reply-btn').click
+      expect(fj('.compose_form .ac-input-box.disabled')).not_to be_nil
+    end
+  end
+end
