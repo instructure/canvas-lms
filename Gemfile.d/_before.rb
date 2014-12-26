6f260591a8bc3bcc75f2687f840a94bfe54ef304
Cody Cutrer <cody@instructure.com>
2014-12-26 16:29:15 -0700

    bump permitted bundler
    Change-Id: I289b943d141ed53a742606364a573ba620ec4b2d
    Reviewed-on: https://gerrit.instructure.com/46318
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/_before.rb
+++ Gemfile.d/_before.rb
@@ -17,7 +17,7 @@
 #
 
 # # enforce the version of bundler itself, to avoid any surprises
-required_bundler_version = '1.6.0'..'1.7.6'
+required_bundler_version = '1.6.0'..'1.7.9'
 gem 'bundler', [">=#{required_bundler_version.first}", "<=#{required_bundler_version.last}"]
 
 unless required_bundler_version.include?(Bundler::VERSION)
