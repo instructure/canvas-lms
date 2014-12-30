9f5c5ee25da480515767f7208d20c93c5e482de7
James Williams <jamesw@instructure.com>
2014-12-30 10:45:38 -0700

    fix ChangeEnrollmentsRoleIdNull again
    test plan:
    * should be able to run migrations with duplicate
    ObserverEnrollments as well
    
    Change-Id: I15d811a31081177fc2c4c7b9b356858ab79db307
    Reviewed-on: https://gerrit.instructure.com/46390
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- db/migrate/20141212134557_change_enrollments_role_id_null.rb
+++ db/migrate/20141212134557_change_enrollments_role_id_null.rb
@@ -3,33 +3,29 @@ class ChangeEnrollmentsRoleIdNull < ActiveRecord::Migration
   tag :postdeploy
 
   def up
-    cleanup_observer_enrollments
-    cleanup_student_view_enrollments
+    cleanup_enrollments(Role.get_built_in_role("ObserverEnrollment"), "ObserverEnrollment")
+    cleanup_enrollments(Role.get_built_in_role("StudentEnrollment"), "StudentViewEnrollment")
     change_column_null_with_less_locking :enrollments, :role_id
   end
 
-  def cleanup_observer_enrollments
-    role = Role.get_built_in_role("ObserverEnrollment")
+  def cleanup_enrollments(role, type)
     Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
-      Enrollment.where(id: start_id..end_id, role_id: nil, type: role.name).update_all(:role_id => role.id)
-    end
-  end
-
-  def cleanup_student_view_enrollments
-    student_role = Role.get_built_in_role("StudentEnrollment")
-    # set the proper role_id where this has not already happened manually
-    Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
-      Enrollment.where(<<-SQL, start_id, end_id, student_role.id).update_all(:role_id => student_role.id)
-        (id BETWEEN ? AND ?) AND role_id IS NULL AND type='StudentViewEnrollment' AND NOT EXISTS
-        (SELECT 1 FROM enrollments AS e2 WHERE e2.user_id=enrollments.user_id AND
-          e2.type='StudentViewEnrollment' AND e2.role_id=? AND
+      Enrollment.where(<<-SQL, start_id, end_id, type, role.id).update_all(:role_id => role.id)
+        (id BETWEEN ? AND ?) AND role_id IS NULL AND type=? AND NOT EXISTS
+        (SELECT 1 FROM enrollments AS e2 WHERE
+          e2.role_id=? AND
+          e2.user_id=enrollments.user_id AND
+          e2.type=enrollments.type AND
           e2.course_section_id=enrollments.course_section_id AND
-          e2.associated_user_id IS NULL)
+          (e2.associated_user_id=enrollments.associated_user_id OR
+            (e2.associated_user_id IS NULL AND enrollments.associated_user_id IS NULL)
+          ))
       SQL
     end
-    # delete remaining (obsolete) StudentViewEnrollments with no role_id
+
+    # delete remaining duplicate enrollments
     Enrollment.find_ids_in_ranges(batch_size: 10000) do |start_id, end_id|
-      Enrollment.where(id: start_id..end_id, type: 'StudentViewEnrollment', role_id: nil).delete_all
+      Enrollment.where(id: start_id..end_id, type: type, role_id: nil).delete_all
     end
   end
 
