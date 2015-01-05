f2af1a322efb914d8546c4ebeaf63b8e1855971d
Michael Nomitch <mnomitch@instructure.com>
2015-01-05 12:12:42 -0600

    DA - remove filter from assignment_stats
    fixes CNVS-17748
    
    test plan:
      - with DA on and off
        - go to the grade summary page as a teacher
          - it should load assignment max/min/ave
        - go to it as a student
          - it should load assignment max/min/ave
    
    Change-Id: I8996495eaf9c1427be6a3bbf3883b5c78e9ee571
    Reviewed-on: https://gerrit.instructure.com/46521
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Adam Stone <astone@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    
    Conflicts:
    	spec/presenters/grade_summary_presenter_spec.rb

--- app/presenters/grade_summary_presenter.rb
+++ app/presenters/grade_summary_presenter.rb
@@ -168,12 +168,9 @@ class GradeSummaryPresenter
   def assignment_stats
     @stats ||= begin
       chain = @context.assignments.active.except(:order)
-      if @context.feature_enabled?(:differentiated_assignments)
-        # if DA is on, further restrict the assignments to those visible to each student
-        # note: adding the context_id avoids a seq scan
-        chain = chain.joins(:assignment_student_visibilities).
-          where("assignment_student_visibilities.course_id = ?", @context.id)
-      end
+      # note: because a score is needed for max/min/ave we are not filtering
+      # by assignment_student_visibilities, if a stat is added that doesn't
+      # require score then add a filter when the DA feature is on
       chain.joins(:submissions)
         .where("submissions.user_id in (?)", real_and_active_student_ids)
         .group("assignments.id")
