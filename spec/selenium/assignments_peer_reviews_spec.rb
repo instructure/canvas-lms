542c755ba071a57665252736626b1176f7b8e004
Simon Williams <simon@instructure.com>
2015-01-07 13:47:33 -0700

    restore assignments_peer_reviews.coffee
    this file is still needed in canvas
    
    fixes CNVS-17796
    
    test plan:
    - create a peer review assignment
    - go to the assignment peer reviews page
    - you should not see a javascript error in the console
    - the page should work (let you assign/remove reviews)
    
    Change-Id: I419aa0a7190ef7b1d91ec4e1cdcfba7f956fc763
    Reviewed-on: https://gerrit.instructure.com/46714
    Reviewed-by: James Williams  <jamesw@instructure.com>
    QA-Review: Adam Stone <astone@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    Product-Review: Matt Fairbourn <mfairbourn@instructure.com>

---
+++ spec/selenium/assignments_peer_reviews_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/common')
+
+describe "assignments" do
+  include_examples "in-process server selenium tests"
+
+  context "peer reviews" do
+
+    it "allows deleting a peer review" do
+      course_with_teacher_logged_in
+      @student1 = student_in_course.user
+      @student2 = student_in_course.user
+
+      @assignment = assignment_model({
+        course: @course,
+        peer_reviews: true,
+        automatic_peer_reviews: false,
+      })
+
+      @assignment.assign_peer_review(@student1, @student2)
+      @assignment.assign_peer_review(@student2, @student1)
+
+      get "/courses/#{@course.id}/assignments/#{@assignment.id}/peer_reviews"
+
+      hover_and_click('.student_reviews:first .delete_review_link')
+      accept_alert
+      wait_for_ajaximations
+
+      expect(ffj('.student_reviews:first .peer_reviews li:visible')).to be_empty
+      expect(@assignment.submissions.map(&:assessment_requests).flatten.length).to eq 1
+    end
+  end
+end
