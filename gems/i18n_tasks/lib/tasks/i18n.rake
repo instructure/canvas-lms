instructure/canvas-lms

commit d005e2d10e04734fcd106990d7cc2ebe1025fb00

don't require :environment for i18n:check
refs https://trello.com/c/Ls5yeXt4/40

reduces wall time ~20 sec (when not using spring)

test plan:
1. change your database.yml to have a bogus db
2. `rake i18n:check` should not give you a db error
3. `rake i18n:generate` should produce the same yml as before
4. they both should start up about 20 sec faster \o/

Change-Id: I1f13b350f3ccc4d9b572514b5cdaee2f35c02ce4
Reviewed-on: https://gerrit.instructure.com/43887
Product-Review: Jennifer Stern <jstern@instructure.com>
Reviewed-by: Jennifer Stern <jstern@instructure.com>
QA-Review: Jennifer Stern <jstern@instructure.com>
Tested-by: Jenkins <jenkins@instructure.com>

gems/i18n_tasks/lib/tasks/i18n.rake
@@ -3,10 +3,8 @@ require 'i18n_extraction'
 
 namespace :i18n do
   desc "Verifies all translation calls"
-  task :check => :environment do
+  task :check => :i18n_environment do
     Hash.send(:include, I18nTasks::HashExtensions) unless Hash.new.kind_of?(I18nTasks::HashExtensions)
-
-    I18n.available_locales
 
     def I18nliner.manual_translations
       I18n.backend.direct_lookup('en')
@@ -48,8 +46,9 @@ namespace :i18n do
     print "Wrote new #{yaml_file}\n\n"
   end
 
-  desc "Generates JS bundle i18n files (non-en) and adds them to assets.yml"
-  task :generate_js do
+  # like the top-level :environment, but just the i18n-y stuff we need.
+  # also it's faster and doesn't require a db \o/
+  task :i18n_environment do
     # This is intentionally requiring things individually rather than just
     # loading the rails+canvas environment, because that environment isn't
     # available during the deploy process. Don't change this out for a call to
@@ -64,6 +63,7 @@ namespace :i18n do
     # set up rails i18n paths ... normally rails env does this for us :-/
     require 'action_controller'
     require 'active_record'
+    require 'will_paginate'
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
 
     require 'i18nema'
@@ -71,7 +71,10 @@ namespace :i18n do
     I18n.backend = I18nema::Backend.new
     I18nema::Backend.send(:include, I18n::Backend::Fallbacks)
     I18n.backend.init_translations
+  end
 
+  desc "Generates JS bundle i18n files (non-en) and adds them to assets.yml"
+  task :generate_js => :i18n_environment do
     Hash.send(:include, I18nTasks::HashExtensions) unless Hash.new.kind_of?(I18nTasks::HashExtensions)
 
     locales = I18n.available_locales - [:en]

commit 444733336f1080d6cea7afffb5f5f79d0081f248

ensure pagination links get translated, fixes CNVS-17180
this appears to have broken in d005e2d1 even though the test plan should
have caught it :-/ ... the will_paginate strings were previously
translated, but started disappearing in subsequent transifex round trips
since i18n:generate stopped including them ... e.g. see commits fa8419c2
and bffbe3db

note:
the translations won't magically reappear, we'll need to wait for the
transifex round trip (unless someone manually loads them in there sooner)

test plan:
1. run rake i18n:generate before and after this commit
2. the only difference should be the addition of the will_paginate strings

Change-Id: Iad73ceb56d17129111dcabb2d78e41b0313e81f6
Reviewed-on: https://gerrit.instructure.com/47541
Tested-by: Jenkins
Reviewed-by: Nick Cloward <ncloward@instructure.com>
QA-Review: August Thornton <august@instructure.com>
Product-Review: Jon Jensen <jon@instructure.com>

gems/i18n_tasks/lib/tasks/i18n.rake
@@ -64,6 +64,7 @@ namespace :i18n do
     require 'action_controller'
     require 'active_record'
     require 'will_paginate'
+    I18n.load_path.unshift(*WillPaginate::I18n.load_path)
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
 
     require 'i18nema'