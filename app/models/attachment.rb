6de899a8283e29374e14ee4835e3a7bf95238e59
James Williams <jamesw@instructure.com>
2015-01-27 11:13:08 -0700

    fix file submissions for cross-shard users
    test plan:
    * create a course
    * create an assignment with a file upload requirement
    * publish the assignment and course
    
    * create a user on another shard
    * invite that user into the course as a student
    * as that user, upload a file submission
    * it should show the uploaded file in the submission
     details
    * it should also show the file in the list when
     the student resubmits and clicks on "click here
     to find a file you've already uploaded"
    
    closes #CNVS-16785
    
    Change-Id: I9bfa02082db9ea7b59e248e04018cd2b5e3099ac
    Reviewed-on: https://gerrit.instructure.com/47870
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -600,16 +600,18 @@ class Attachment < ActiveRecord::Base
     method = method.to_sym
     deleted_attachments = []
     if method == :rename
-      self.save unless self.id
+      self.save! unless self.id
 
       valid_name = false
-      while !valid_name
-        existing_names = self.folder.active_file_attachments.where("id <> ?", self.id).pluck(:display_name)
-        self.display_name = Attachment.make_unique_filename(self.display_name, existing_names)
+      self.shard.activate do
+        while !valid_name
+          existing_names = self.folder.active_file_attachments.where("id <> ?", self.id).pluck(:display_name)
+          self.display_name = Attachment.make_unique_filename(self.display_name, existing_names)
 
-        if Attachment.where("id = ? AND NOT EXISTS (SELECT 1 FROM attachments WHERE id <> ? AND display_name = ? AND folder_id = ? AND file_state <> ?)",
-                            self.id, self.id, self.display_name, self.folder_id, 'deleted').limit(1).update_all(:display_name => self.display_name) > 0
-          valid_name = true
+          if Attachment.where("id = ? AND NOT EXISTS (SELECT 1 FROM attachments WHERE id <> ? AND display_name = ? AND folder_id = ? AND file_state <> ?)",
+                              self.id, self.id, self.display_name, self.folder_id, 'deleted').limit(1).update_all(:display_name => self.display_name) > 0
+            valid_name = true
+          end
         end
       end
     elsif method == :overwrite
