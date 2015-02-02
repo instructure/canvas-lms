c6dc1d188e619c94ceb21b51b8769dcc04d4ff50
Cody Cutrer <cody@instructure.com>
2015-02-02 13:03:04 -0700

    spread feature flag cache around a ring
    Change-Id: I4831cc45d8646f6f86a34ade6546644c4dacdd49
    Reviewed-on: https://gerrit.instructure.com/48065
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- spec/lib/feature_flags_spec.rb
+++ spec/lib/feature_flags_spec.rb
@@ -286,7 +286,7 @@ describe FeatureFlags do
   end
 
   describe "caching" do
-    let(:t_cache_key) { t_root_account.feature_flag_cache_key('course_feature') }
+    let(:t_cache_key) { "#{t_root_account.feature_flag_cache_key('course_feature')}:0" }
     before do
       t_root_account.feature_flags.create! feature: 'course_feature', state: 'allowed'
     end
@@ -301,7 +301,7 @@ describe FeatureFlags do
     it "should cache a nil result" do
       enable_cache do
         t_root_account.feature_flag('course_feature2')
-        expect(Rails.cache).to be_exist(t_root_account.feature_flag_cache_key('course_feature2'))
+        expect(Rails.cache).to be_exist("#{t_root_account.feature_flag_cache_key('course_feature2')}:0")
         t_root_account.expects(:feature_flags).never
         t_root_account.feature_flag('course_feature2')
       end
