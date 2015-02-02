b1abbc453bbcd3322b4d3f09b303480ab37d02ab
Cody Cutrer <cody@instructure.com>
2015-02-02 13:03:04 -0700

    spread feature flag cache around a ring
    Change-Id: I4831cc45d8646f6f86a34ade6546644c4dacdd49
    Reviewed-on: https://gerrit.instructure.com/48065
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -53,7 +53,7 @@ class FeatureFlag < ActiveRecord::Base
   end
 
   def clear_cache
-    connection.after_transaction_commit { Rails.cache.delete(self.context.feature_flag_cache_key(feature)) } if self.context
+    connection.after_transaction_commit { MultiCache.delete(self.context.feature_flag_cache_key(feature), copies: MultiCache.copies('feature_flags')) } if self.context
   end
 
 private
