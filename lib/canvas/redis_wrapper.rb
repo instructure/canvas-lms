486b03af76cb79c1e35c1f88847b909b01e7c5b3
Cody Cutrer <cody@instructure.com>
2015-02-03 09:36:42 -0700

    share redis cache and redis data connections if possible
    had to change the level that marshalling is disabled for the data
    redis so that marshalling can be enabled for cache
    
    test plan:
     * configure redis.yml
     * configure cache_store.yml for development/production to be
       cache_store: redis_store and nothing else
     * open console
     * Canvas.redis.set('a', 1); Canvas.redis.get('a') should return
       "1" (as a string)
     * Rails.cache.write('a', 1); Rails.cache.read('a') should return
       1 (as a Fixnum)
     * Canvas.redis.__getobj__.object_id should be the same as
       Rails.cache.instance_variable_get(:@data).object_id
     * netstat -an should show only one connection from your console
       process to the redis server
     * with an already populated cache (having used Canvas before
       applying this commit), click around Canvas, and make sure
       nothing breaks
    
    Change-Id: I507b95af3ec35f65501bb61e428a3e8479b8f895
    Reviewed-on: https://gerrit.instructure.com/48134
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/canvas/redis_wrapper.rb
+++ lib/canvas/redis_wrapper.rb
@@ -64,5 +64,31 @@ module Canvas
         raise(UnsupportedRedisMethod, "Redis method `#{method}` is not supported by Twemproxy, and so shouldn't be used in Canvas")
       }
     end
+
+    # We don't marshal for the data wrapper
+    def set(key, value, options = nil)
+      options ||= {}
+      super(key, value, options.merge(raw: true))
+    end
+
+    def setnx(key, value, options = nil)
+      options ||= {}
+      super(key, value, options.merge(raw: true))
+    end
+
+    def setex(key, expiry, value, options = nil)
+      options ||= {}
+      super(key, expiry, value, options.merge(raw: true))
+    end
+
+    def get(key, options = nil)
+      options ||= {}
+      super(key, options.merge(raw: true))
+    end
+
+    def mget(*keys)
+      options keys.extract_options!
+      super(*keys, options.merge(raw: true))
+    end
   end
 end
