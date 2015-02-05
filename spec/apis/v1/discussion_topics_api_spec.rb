f16f75754d627a5466b2e4dbb84dffa4468e9881
James Williams <jamesw@instructure.com>
2015-02-05 08:44:37 -0700

    require context read access for discussion topics
    test plan:
    * create a course with a discussion topic
    * keep it unpublished but add a student
    * as a student, should not be able to
     view the discussion topic through a direct link
    
    closes #CNVS-6384
    
    Change-Id: I0c0bc66e377d870de6c97555e25e82a8226264f4
    Reviewed-on: https://gerrit.instructure.com/48278
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Adam Stone <astone@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- spec/apis/v1/discussion_topics_api_spec.rb
+++ spec/apis/v1/discussion_topics_api_spec.rb
@@ -431,6 +431,13 @@ describe DiscussionTopicsController, type: :request do
         expect(json).to eq @response_json.merge("subscribed" => @topic.subscribed?(@user))
       end
 
+      it "should require course to be published for students" do
+        @course.claim
+        json = api_call(:get, "/api/v1/courses/#{@course.id}/discussion_topics/#{@topic.id}",
+                        {:controller => 'discussion_topics_api', :action => 'show', :format => 'json', :course_id => @course.id.to_s, :topic_id => @topic.id.to_s},
+                        {}, :expected_status => 401)
+      end
+
       it "should properly translate a video media comment in the discussion topic's message" do
         @topic.update_attributes(
           message: '<p><a id="media_comment_m-spHRwKY5ATHvPQAMKdZV_g" class="instructure_inline_media_comment video_comment" href="/media_objects/m-spHRwKY5ATHvPQAMKdZV_g">this is a media comment</a></p>'
@@ -1701,6 +1708,7 @@ describe DiscussionTopicsController, type: :request do
       course_with_teacher
       create_topic(@course, :title => "topic", :message => "topic")
       course_with_observer_logged_in(:course => @course)
+      @course.offer
       json = api_call(:get, "/api/v1/courses/#{@course.id}/discussion_topics.json",
                       { :controller => 'discussion_topics', :action => 'index', :format => 'json',
                         :course_id => @course.id.to_s })
