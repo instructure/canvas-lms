6581a5cdb0511744b23245f5b9bc821aaf2df1a4
August Thornton <august@instructure.com>
2015-02-13 12:49:16 -0700

    use more appropriate method to handle nil case
    fixes CNVS-18497
    
    test plan:
      - verify the optional include[]=term parameter succeeds and includes
        the term hash for each course object using the following endpoints:
            GET /api/v1/accounts/:account_id/courses
            GET /api/v1/courses
    
    Change-Id: I2e39f449ba71d58f4ec7a3a60298b4da066ebddf
    Reviewed-on: https://gerrit.instructure.com/48813
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

--- lib/api/v1/enrollment_term.rb
+++ lib/api/v1/enrollment_term.rb
@@ -4,7 +4,7 @@ module Api::V1::EnrollmentTerm
   def enrollment_term_json(enrollment_term, user, session, enrollments=[], includes=[])
     api_json(enrollment_term, user, session, :only => %w(id name start_at end_at workflow_state)).tap do |hash|
       hash['sis_term_id'] = enrollment_term.sis_source_id if enrollment_term.root_account.grants_any_right?(user, :read_sis, :manage_sis)
-      hash['start_at'], hash['end_at'] = enrollment_term.overridden_term_dates(enrollments) if enrollments.any?
+      hash['start_at'], hash['end_at'] = enrollment_term.overridden_term_dates(enrollments) if enrollments.present?
     end
   end
 
