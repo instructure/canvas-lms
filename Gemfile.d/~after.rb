618eacf181eebcc88974f2b78b9b97ce91ff04a4
Cody Cutrer <cody@instructure.com>
2015-02-18 08:51:09 -0700

    use eval_gemfile for bundler goodness
    Change-Id: I1ce8c5135cb5b15f4e1fdd07cb890f5f823f11a4
    Reviewed-on: https://gerrit.instructure.com/48952
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/~after.rb
+++ Gemfile.d/~after.rb
@@ -4,11 +4,11 @@ Dir[File.join(File.dirname(__FILE__), '../vendor/plugins/*/Gemfile')].each do |g
   unless Dir[File.join(File.dirname(g), "*.gemspec")].empty?
     raise "#{File.dirname(g)} has a gemspec, and probably needs to be moved to gems/plugins"
   end
-  eval(File.read(g))
+  eval_gemfile(g)
 end
 
 # Non-standard Canvas extension to Bundler behavior -- load the Gemfiles from
 # plugins.
 Dir[File.join(File.dirname(__FILE__), '../gems/plugins/*/Gemfile.d/*')].each do |g|
-  eval(File.read(g))
+  eval_gemfile(g)
 end
