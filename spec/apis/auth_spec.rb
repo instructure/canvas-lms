4b9e607ab5f39f4d89b4dca3609b4ba532e6a153
Cody Cutrer <cody@instructure.com>
2015-02-27 09:21:49 -0700

    don't allow GETs for saml_consume
    fixes CNVS-18890
    
    we use HTTP-POST binding, so they're not supposed to be used anyway,
    and it protects the endpoint against forced logout attacks
    
    Change-Id: I2f8b09490b8d036fe78847690d9ad01fd1ab49f3
    Reviewed-on: https://gerrit.instructure.com/49591
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- spec/apis/auth_spec.rb
+++ spec/apis/auth_spec.rb
@@ -264,7 +264,7 @@ describe "API Authentication", type: :request do
           Onelogin::Saml::Response.any_instance.stubs(:name_qualifier).returns(nil)
           Onelogin::Saml::Response.any_instance.stubs(:session_index).returns(nil)
 
-          get 'saml_consume', :SAMLResponse => "foo"
+          post 'saml_consume', :SAMLResponse => "foo"
         end
       end
 
