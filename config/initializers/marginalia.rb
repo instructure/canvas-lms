b76c15bb30656186b35de5d6f818f01b100f7209
Cody Cutrer <cody@instructure.com>
2015-03-09 11:18:37 -0600

    add rake task to marginalia comment
    test plan:
     * add rake_task to your marginalia.yml
     * run a rake task (like db:migrate)
     * confirm in your log that sql queries are tagged with the task
       name
    
    Change-Id: I2e7549da3216e46904bac482540e5442dfe24018
    Reviewed-on: https://gerrit.instructure.com/50047
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/marginalia.rb
+++ config/initializers/marginalia.rb
@@ -8,15 +8,30 @@ if config[:components].present?
 
   module Marginalia
     module Comment
-      def self.context_id
-        RequestContextGenerator.request_id
-      end
+      class << self
+        attr_accessor :rake_task
 
-      def self.job_tag
-        Delayed::Worker.current_job.try(:tag)
+        def context_id
+          RequestContextGenerator.request_id
+        end
+
+        def job_tag
+          Delayed::Worker.current_job.try(:tag)
+        end
       end
     end
   end
 
   Marginalia::Comment.components = config[:components].map(&:to_sym)
+
+  module Marginalia::RakeTask
+    def execute(args = nil)
+      previous, Marginalia::Comment.rake_task = Marginalia::Comment.rake_task, self.name
+      super
+    ensure
+      Marginalia::Comment.rake_task = previous
+    end
+  end
+
+  Rake::Task.prepend(Marginalia::RakeTask)
 end
