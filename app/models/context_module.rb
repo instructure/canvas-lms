eb93c534d801f497fd714a2c14b01550316ef60a
James Williams <jamesw@instructure.com>
2015-03-17 10:24:19 -0600

    add strand to evaluate_all_progressions job options
    should ease the db strain a little when re-saving all the
     context modules in a course (such as during a reorder)
    
    Change-Id: Iba900a88a92c943948b92ef7cbe424c488b3a6c5
    Reviewed-on: https://gerrit.instructure.com/50472
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/models/context_module.rb
+++ app/models/context_module.rb
@@ -87,7 +87,7 @@ class ContextModule < ActiveRecord::Base
   def invalidate_progressions
     connection.after_transaction_commit do
       context_module_progressions.update_all(current: false)
-      send_later_if_production(:evaluate_all_progressions)
+      send_later_if_production_enqueue_args(:evaluate_all_progressions, {:strand => "module_reeval_#{self.global_context_id}"})
     end
   end
 
