851d092dc1beca4f996a03c0239197f91c0ed9ce
Strand McCutchen <smccutchen@instructure.com>
2015-03-25 17:12:18 -0700

    make focus accessible for srgb set default grade modal
    fixes CNVS-19218
    
    When a user clicks "set default grade" in the individual gradebook
    view the focus is set on the close modal button, and when they
    click on "set default grade" in that modal, the focus returns to
    the "set default grade" button on the page. When they click the
    close button in that modal, focus returns to the "set default grade"
    button on the page.
    
    Test Plan:
    
    1. In individual view gradebook, select an assignment.
    2. Click "set default grade" and confirm focus is on the close
        button.
    3. Click "set default grade" in that modal and confirm that focus
        returns to the "set default grade" on the page.
    4. Click "set default grade" and then the close button and confirm
        that focus returns to the "set default grade" on the page.
    
    Change-Id: I6cd16c581c7c6a1d428be5a4ef91d85b4d777a83
    Reviewed-on: https://gerrit.instructure.com/51051
    Tested-by: Jenkins
    Reviewed-by: Jonathan Featherstone <jfeatherstone@instructure.com>
    QA-Review: Nathan Rogowski <nathan@instructure.com>
    Product-Review: Strand McCutchen <smccutchen@instructure.com>

--- spec/selenium/gradebook2_srgb_spec.rb
+++ spec/selenium/gradebook2_srgb_spec.rb
@@ -31,6 +31,30 @@ describe "screenreader gradebook" do
         expect(ui_options.include? section[:name]).to be_truthy
       end
     end
+
+    it "should focus on accessible elements when setting default grades" do
+      get "/courses/#{@course.id}/gradebook"
+      f("#change_gradebook_version_link_holder .ellipsible").click
+      refresh_page
+      Selenium::WebDriver::Support::Select.new(f("#assignment_select"))
+                                          .select_by(:text, 'second assignment')
+
+      # When the modal opens the close button should have focus
+      f("#set_default_grade").click
+      focused_classes = driver.execute_script('return document.activeElement.classList')
+      expect(focused_classes).to include("ui-dialog-titlebar-close")
+
+      # When the modal closes
+      # by setting a grade the "set default grade" button should have focus
+      f(".button_type_submit").click
+      driver.switch_to.alert.accept
+      check_element_has_focus(f "#set_default_grade")
+
+      # by the close button the "set default grade" button should have focus
+      f("#set_default_grade").click
+      fj('.ui-icon-closethick:visible').click
+      check_element_has_focus(f "#set_default_grade")
+    end
   end
 end
 
