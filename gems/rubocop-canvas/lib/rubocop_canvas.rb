913e89b0d69772f21fa3b61817ee7023513d09aa
Ethan Vizitei <evizitei@instructure.com>
2015-04-13 10:16:46 -0600

    make rlint handle Time.now.utc correctly
    Change-Id: If2facabedd06c8f16466eebd045988a401834ea1
    Reviewed-on: https://gerrit.instructure.com/52036
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- gems/rubocop-canvas/lib/rubocop_canvas.rb
+++ gems/rubocop-canvas/lib/rubocop_canvas.rb
@@ -8,6 +8,7 @@ require 'rubocop_canvas/helpers/comments'
 require 'rubocop_canvas/helpers/diff_parser'
 
 require 'rubocop_canvas/cops/lint/freeze_constants'
+require 'rubocop_canvas/cops/rails/smart_time_zone'
 require 'rubocop_canvas/cops/datafixup/find_ids'
 require 'rubocop_canvas/cops/migration/concurrent_index'
 require 'rubocop_canvas/cops/migration/primary_key'
