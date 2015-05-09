6b88b6eb1cd457ca7c0de07dc7936c8bb98b6582
Ethan Vizitei <evizitei@instructure.com>
2015-05-09 13:36:05 -0600

    dont blow up if there's no database server in environments like beta
    Change-Id: I8963a583254160f408f0963acaa55d2211058e4e
    Reviewed-on: https://gerrit.instructure.com/53824
    Reviewed-by: Simon Williams <simon@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -87,7 +87,7 @@ Rails.application.config.to_prepare do
 
     def delayed_jobs_shard
       shard = Shard.lookup(self.delayed_jobs_shard_id) if self.read_attribute(:delayed_jobs_shard_id)
-      shard || self.database_server.delayed_jobs_shard(self)
+      shard || self.database_server.try(:delayed_jobs_shard, self)
     end
 
     delegate :in_current_region?, to: :database_server
