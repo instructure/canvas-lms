7075ef009a105e0cda091bdf2580aa7262155b3d
Cody Cutrer <cody@instructure.com>
2015-05-26 14:43:13 -0600

    ... and a trailing quote to fix the now double-botched conflict resolution

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -19,6 +19,6 @@ if settings.present?
 
   Canvas::Errors.register!(:sentry_notification) do |exception, data|
     setting = Setting.get("sentry_error_logging_enabled", 'true')
-    SentryProxy.capture(exception, data) if setting == 'true
+    SentryProxy.capture(exception, data) if setting == 'true'
   end
 end
