f4ad574b9e2aa6d362dea17184afa8d9aeab4c1e
cguanzon <cguanzon@instructure.com>
2015-05-27 14:09:17 -0600

    selenium: adds cn-image to TEST_FILE_UUIDS
    - this image is needed to unpend a canvasnet_registration
       selenium spec
    
    Change-Id: Ibe07de4c0d12ecf26348a95d02aec784444d71d7
    Reviewed-on: https://gerrit.instructure.com/55200
    Tested-by: Jenkins
    Reviewed-by: Ben Bolton <bbolton@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>

--- spec/selenium/test_setup/common_helper_methods/other_helper_methods.rb
+++ spec/selenium/test_setup/common_helper_methods/other_helper_methods.rb
@@ -124,7 +124,8 @@ module OtherHelperMethods
       "b_file.txt" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/b_file.txt')),
       "c_file.txt" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/c_file.txt')),
       "amazing_file.txt" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/amazing_file.txt')),
-      "Dog_file.txt" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/Dog_file.txt'))
+      "Dog_file.txt" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/Dog_file.txt')),
+      "cn-image.jpg" => File.read(File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/files/cn_image.jpg'))
   }.freeze
 
   def get_file(filename, data = nil)
