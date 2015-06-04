821974add89d6d9889aa02ef3474e96dd9f11008
Cody Cutrer <cody@instructure.com>
2015-06-04 14:18:34 -0600

    freeze!
    Change-Id: If8498630d6f08fc1695b94e2f5d4ca7e1a3e4c0c
    Reviewed-on: https://gerrit.instructure.com/55748
    Tested-by: Jenkins
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/dropped_columns.rb
+++ config/initializers/dropped_columns.rb
@@ -18,43 +18,98 @@
 
 class ActiveRecord::Base
   DROPPED_COLUMNS = {
-    'abstract_courses' => %w(sis_name sis_course_code),
-    'accounts' => %w(type sis_name account_code authentication_type ldap_host ldap_domain),
-    'account_authorization_configs' => %w(auth_uid login_handle_name change_password_url unknown_user_url),
-    'account_notification_roles' => %w(role_type),
-    'account_users' => %w(membership_type),
-    'access_tokens' => %w(token),
-    'asset_user_accesses' => %w(asset_access_stat_id interaction_seconds progress count),
-    'assignments' => %w(sequence_position minimum_required_blog_posts minimum_required_blog_comments reminders_created_for_due_at publishing_reminder_sent previously_published before_quiz_submission_types),
-    'attachments' => %w(enrollment_id cached_s3_url s3_url_cached_at scribd_account_id scribd_user scribd_mime_type_id submitted_to_scribd_at scribd_doc scribd_attempts cached_scribd_thumbnail last_inline_view),
-    'calendar_events' => %w(calendar_event_repeat_id for_repeat_on),
-    'communication_channels' => %w(access_token_id internal_path),
-    'content_exports' => %w(course_id),
-    'content_tags' => %w(sequence_position context_module_association_id),
-    'context_modules' => %w(downstream_modules),
-    'conversation_messages' => %w(context_message_id),
-    'course_sections' => %w(sis_cross_listed_section_id sis_cross_listed_section_sis_batch_id sticky_xlist sis_name students_can_participate_before_start_at section_organization_name long_section_code account_id section_code),
-    'courses' => %w(section hidden_tabs sis_name sis_course_code hashtag allow_student_assignment_edits publish_grades_immediately),
-    'discussion_topics' => %w(authorization_list_id),
-    'enrollment_terms' => %w(sis_data sis_name ignore_term_date_restrictions),
-    'enrollments' => %w(invitation_email can_participate_before_start_at limit_priveleges_to_course_sections role_name),
-    'external_feeds' => %w(body_match feed_type feed_purpose),
-    'failed_jobs' => %w(original_id),
-    'gradebook_uploads' => %w(context_type context_id),
-    'grading_periods' => %w(course_id account_id),
-    'groups' => %w(sis_name type groupable_id groupable_type),
-    'messages' => %w(cc bcc),
-    'notification_policies' => %w(user_id broadcast),
-    'page_views' => %w(contributed),
-    'pseudonyms' => %w(sis_update_data deleted_unique_id sis_source_id crypted_webdav_access_code type),
-    'role_overrides' => %w(context_code enrollment_type),
-    'users' => %w(type creation_unique_id creation_sis_batch_id creation_email sis_name bio),
-    'quizzes' => %w(root_quiz_id),
-    'sis_batches' => %w{batch_id},
-    'stream_items' => %w{context_code item_asset_string},
-    'stream_item_instances' => %w(context_code),
-    'submissions' => %w(changed_since_publish late),
-    'wiki_pages' => %w(hide_from_students)
+    'abstract_courses' => %w(sis_name sis_course_code).freeze,
+    'accounts' => %w(type
+                     sis_name
+                     account_code
+                     authentication_type
+                     ldap_host
+                     ldap_domain).freeze,
+    'account_authorization_configs' => %w(auth_uid
+                                          login_handle_name
+                                          change_password_url
+                                          unknown_user_url).freeze,
+    'account_notification_roles' => %w(role_type).freeze,
+    'account_users' => %w(membership_type).freeze,
+    'access_tokens' => %w(token).freeze,
+    'asset_user_accesses' => %w(asset_access_stat_id
+                                interaction_seconds
+                                progress
+                                count).freeze,
+    'assignments' => %w(sequence_position
+                        minimum_required_blog_posts
+                        minimum_required_blog_comments
+                        reminders_created_for_due_at
+                        publishing_reminder_sent
+                        previously_published
+                        before_quiz_submission_types).freeze,
+    'attachments' => %w(enrollment_id
+                        cached_s3_url
+                        s3_url_cached_at
+                        scribd_account_id
+                        scribd_user
+                        scribd_mime_type_id
+                        submitted_to_scribd_at
+                        scribd_doc
+                        scribd_attempts
+                        cached_scribd_thumbnail
+                        last_inline_view).freeze,
+    'calendar_events' => %w(calendar_event_repeat_id for_repeat_on).freeze,
+    'communication_channels' => %w(access_token_id internal_path).freeze,
+    'content_exports' => %w(course_id).freeze,
+    'content_tags' => %w(sequence_position context_module_association_id).freeze,
+    'context_modules' => %w(downstream_modules).freeze,
+    'conversation_messages' => %w(context_message_id).freeze,
+    'course_sections' => %w(sis_cross_listed_section_id
+                            sis_cross_listed_section_sis_batch_id
+                            sticky_xlist
+                            sis_name
+                            students_can_participate_before_start_at
+                            section_organization_name
+                            long_section_code
+                            account_id
+                            section_code).freeze,
+    'courses' => %w(section
+                    hidden_tabs
+                    sis_name
+                    sis_course_code
+                    hashtag
+                    allow_student_assignment_edits
+                    publish_grades_immediately).freeze,
+    'discussion_topics' => %w(authorization_list_id).freeze,
+    'enrollment_terms' => %w(sis_data
+                             sis_name
+                             ignore_term_date_restrictions).freeze,
+    'enrollments' => %w(invitation_email
+                        can_participate_before_start_at
+                        limit_priveleges_to_course_sections
+                        role_name).freeze,
+    'external_feeds' => %w(body_match feed_type feed_purpose).freeze,
+    'failed_jobs' => %w(original_id).freeze,
+    'gradebook_uploads' => %w(context_type context_id).freeze,
+    'grading_periods' => %w(course_id account_id).freeze,
+    'groups' => %w(sis_name type groupable_id groupable_type).freeze,
+    'messages' => %w(cc bcc).freeze,
+    'notification_policies' => %w(user_id broadcast).freeze,
+    'page_views' => %w(contributed).freeze,
+    'pseudonyms' => %w(sis_update_data
+                       deleted_unique_id
+                       sis_source_id
+                       crypted_webdav_access_code
+                       type).freeze,
+    'role_overrides' => %w(context_code enrollment_type).freeze,
+    'users' => %w(type
+                  creation_unique_id
+                  creation_sis_batch_id
+                  creation_email
+                  sis_name
+                  bio).freeze,
+    'quizzes' => %w(root_quiz_id).freeze,
+    'sis_batches' => %w(batch_id).freeze,
+    'stream_items' => %w(context_code item_asset_string).freeze,
+    'stream_item_instances' => %w(context_code).freeze,
+    'submissions' => %w(changed_since_publish late).freeze,
+    'wiki_pages' => %w(hide_from_students).freeze
   }.freeze
 
   def self.columns_with_remove_dropped_columns
