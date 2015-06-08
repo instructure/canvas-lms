f6edb777026f1b5433d0fbbf32a9765e33d46c14
Ryan Shaw <ryan@instructure.com>
2015-06-07 23:36:10 -0600

    s/jammit_css/css_bundle
    with the new css stuff, we don't actually use
    jammit so jammit_css has been renamed to css_bundle.
    
    we left an aliased jammit_css and marked it deprecated
    so these all still worked but spit out a deprecation warning.
    
    Change-Id: Ib74037711f559f64851d9379644479587c1f83f2
    Reviewed-on: https://gerrit.instructure.com/55888
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -364,7 +364,7 @@ class FilesController < ApplicationController
       @page_title = t('files_page_title', 'Files')
       @body_classes << 'full-width padless-content'
       js_bundle :react_files
-      jammit_css :react_files
+      css_bundle :react_files
       js_env({
         :FILES_CONTEXTS => files_contexts,
         :NEW_FOLDER_TREE => @context.feature_enabled?(:use_new_tree)
