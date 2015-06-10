8153cdad8b823c928d7c7e6d8fc453a4fb5d432e
Brian Palmer <brianp@instructure.com>
2015-06-10 12:16:52 -0600

    fix cache store instrumentation (logging) under Passenger
    It turns out this setting is a thread-local variable, I'm guessing somebody
    originally added it to debug specific requests.
    
    test plan: This isn't really testable without running canvas inside of
    Passenger. Basically, the "CacheStore" log lines should appear even when running
    Canvas in Passenger.
    
    Change-Id: I526514eeb1be15c70fecfaaf8acd6383e3c261b2
    Reviewed-on: https://gerrit.instructure.com/56122
    Reviewed-by: Kevin Blackham <kblackham@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Brian Palmer <brianp@instructure.com>

--- config/initializers/cache_store_instrumentation.rb
+++ config/initializers/cache_store_instrumentation.rb
@@ -1,6 +1,16 @@
+# In rails4, this is always true, and in rails5, this setter is removed.
+# In other words, just remove this whole block of code when we drop rails3 support.
 if CANVAS_RAILS3
-  # In rails4, this is always true, and in rails5, this setter is removed
   ActiveSupport::Cache::Store.instrument = true
+  if defined?(PhusionPassenger)
+    # For whatever reason this is a thread-local setting, so under Passenger we
+    # need to set it on each handler thread. Note that even in process mode,
+    # Passenger spins up a separate thread in each process for the actual rack
+    # handler.
+    PhusionPassenger.on_event(:starting_request_handler_thread) do
+      ActiveSupport::Cache::Store.instrument = true
+    end
+  end
 end
 
 %w[read write delete exist? generate].each do |method|
