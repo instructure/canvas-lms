4c7d4f27474ce6999124d8a9d98baf0e5f7840e4
Jason Madsen <jmadsen@instructure.com>
2015-06-18 10:10:44 -0600

    enforce 2 decimal precision for statsd logs
    bump the canvas_statsd gem to get 2 decimal precision in log values
    
    test plan:
    
    - tail your environment log
      - repeat for several page visits
        - visit a page
          - log should have an entry with [STATSD]
            - values on that line will have 2 decimal precision
    
    Change-Id: I142f7bcde83c59b05f6a64e7bc19f3cd1f213a5b
    Reviewed-on: https://gerrit.instructure.com/56767
    Tested-by: Jenkins
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Jason Madsen <jmadsen@instructure.com>

--- Gemfile.d/other_stuff.rb
+++ Gemfile.d/other_stuff.rb
@@ -99,7 +99,7 @@ gem 'foreigner', '0.9.2'
 gem 'crocodoc-ruby', '0.0.1', require: false
 gem 'hey', '1.3.0', require: false
 gem 'sentry-raven', '0.13.2', require: false
-gem 'canvas_statsd', '1.0.2'
+gem 'canvas_statsd', '1.0.3'
 
 gem 'active_polymorph', path: 'gems/active_polymorph'
 gem 'activesupport-suspend_callbacks', path: 'gems/activesupport-suspend_callbacks'
