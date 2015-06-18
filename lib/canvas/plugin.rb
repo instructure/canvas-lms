d7444ed4374138aa419b52d9ec4f9fe629525125
Cody Cutrer <cody@instructure.com>
2015-06-17 19:58:56 -0600

    accept 1/0 as true/false in API calls
    Change-Id: I5db3edca3182119e32b36064d1854a736b03b741
    Reviewed-on: https://gerrit.instructure.com/56738
    Tested-by: Jenkins
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/canvas/plugin.rb
+++ lib/canvas/plugin.rb
@@ -178,8 +178,8 @@ module Canvas
 
     def self.value_to_boolean(value)
       if value.is_a?(String) || value.is_a?(Symbol)
-        return true if ["yes", "true", "on"].include?(value.to_s.downcase)
-        return false if ["no", "false", "off"].include?(value.to_s.downcase)
+        return true if ["yes", "true", "on", "1"].include?(value.to_s.downcase)
+        return false if ["no", "false", "off", "0"].include?(value.to_s.downcase)
       end
       return value if [true, false].include?(value)
       return value.to_i != 0
