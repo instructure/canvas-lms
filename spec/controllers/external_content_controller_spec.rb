db7ffa76a837353261f21b8c7de7afd3b141e2c7
Brad Horrocks <bhorrocks@instructure.com>
2015-06-26 12:46:04 -0600

    Test js_env for content item selection callback
    Adds a spec to test js_env is set correctly for external content
    controller
    
    Fixes PLAT-1123
    
    Test Plan:
    run the spec
    
    Change-Id: Ia041095089ea79192db6224b706441d2e9cc26ab
    Reviewed-on: https://gerrit.instructure.com/57289
    Reviewed-by: Nathan Mills <nathanm@instructure.com>
    Tested-by: Jenkins
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Brad Horrocks <bhorrocks@instructure.com>

--- spec/controllers/external_content_controller_spec.rb
+++ spec/controllers/external_content_controller_spec.rb
@@ -13,7 +13,38 @@ describe ExternalContentController do
       get :success, service: 'external_tool_dialog', course_id:c.id
       expect(assigns[:context]).to_not be_nil
     end
-
   end
 
+  describe "POST success/external_tool_dialog" do
+    it "js env is set correctly" do
+
+      c = course
+      post(:success, service: 'external_tool_dialog', course_id:c.id, lti_message_type: 'ContentItemSelection',
+           lti_version: 'LTI-1p0',
+           data: '',
+           content_items: '{"@context":"http://purl.imsglobal.org/ctx/lti/v1/ContentItem","@graph":[{"@type":"LtiLink","@id":"http://lti-tool-provider-example.dev/messages/blti","url":"http://lti-tool-provider-example.dev/messages/blti","title":"Its like sexy for your computer","text":"Arch Linux","mediaType":"application/vnd.ims.lti.v1.ltilink","windowTarget":"","placementAdvice":{"displayWidth":800,"presentationDocumentTarget":"iframe","displayHeight":600},"thumbnail":{"@id":"http://www.runeaudio.com/assets/img/banner-archlinux.png","height":128,"width":128}}]}',
+           lti_msg: '',
+           lti_log: '',
+           lti_errormsg: '',
+           lti_errorlog: '')
+
+      expect(controller.js_env[:retrieved_data]).to_not be_nil
+      expect(controller.js_env[:retrieved_data].first.instance_of?(IMS::LTI::Models::ContentItems::ContentItem)).to be_truthy
+
+      expect(controller.js_env[:retrieved_data].first.id).to eq("http://lti-tool-provider-example.dev/messages/blti")
+      expect(controller.js_env[:retrieved_data].first.url).to eq("http://lti-tool-provider-example.dev/messages/blti")
+      expect(controller.js_env[:retrieved_data].first.text).to eq("Arch Linux")
+      expect(controller.js_env[:retrieved_data].first.title).to eq("Its like sexy for your computer")
+      expect(controller.js_env[:retrieved_data].first.placement_advice.presentation_document_target).to eq("iframe")
+      expect(controller.js_env[:retrieved_data].first.placement_advice.display_height).to eq(600)
+      expect(controller.js_env[:retrieved_data].first.placement_advice.display_width).to eq(800)
+      expect(controller.js_env[:retrieved_data].first.media_type).to eq("application/vnd.ims.lti.v1.ltilink")
+      expect(controller.js_env[:retrieved_data].first.type).to eq("LtiLink")
+      expect(controller.js_env[:retrieved_data].first.thumbnail.height).to eq(128)
+      expect(controller.js_env[:retrieved_data].first.thumbnail.width).to eq(128)
+      expect(controller.js_env[:retrieved_data].first.thumbnail.id).to eq("http://www.runeaudio.com/assets/img/banner-archlinux.png")
+      expect(controller.js_env[:retrieved_data].first.canvas_url).to end_with("external_tools/retrieve?display=borderless&url=http%3A%2F%2Flti-tool-provider-example.dev%2Fmessages%2Fblti")
+
+    end
+  end
 end
