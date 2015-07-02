72f54a5071e939f9656f7287c438a40b8b45b858
James Williams <jamesw@instructure.com>
2015-07-02 13:28:18 -0600

    rails4: change deprecated default scope
    refs #CNVS-21596
    
    Change-Id: I12191f7b335ca7672618cebe362d66a8eff60c7a
    Reviewed-on: https://gerrit.instructure.com/57637
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -73,7 +73,7 @@ class Profile < ActiveRecord::Base
     context_type = klass.name.sub(/Profile\z/, '')
     klass.class_eval { alias_method context_type.downcase.underscore, :context }
     klass.instance_eval { def table_name; "profiles"; end }
-    klass.default_scope :conditions => ["context_type = ?", context_type]
+    klass.default_scope -> { where(:context_type => context_type) }
   end
 
   def self.columns_hash
