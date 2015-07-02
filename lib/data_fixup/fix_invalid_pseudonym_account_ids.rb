71e065a35a4769c6b1368fbe64ff43623112e663
Rob Orton <rob@instructure.com>
2015-07-02 16:22:58 -0600

    validate that pseudonyms belong to root_accounts
    fixes CNVS-21700
    
    test plan
     - in rails console run
    
    a = Account.default
    u1 = User.create(name: 'test user1')
    p1 = Pseudonym.create!(account: a, user: u1,unique_id: 'so_unique')
    a2 = Account.create!(name: 'sub_account1')
    u2 = User.create!(name: 'test user2')
    p2 = Pseudonym.create!(account: a2, user: u2,unique_id: 'so_unique')
    u3 = User.create(name: 'test user3')
    p3 = Pseudonym.create!(account: a2, user: u3,unique_id: 'more_unique')
    a2.parent_account_id = a2.root_account_id = a.id
    a2.save!
    
    p1.workflow_state # should = 'active'
    u1.reload.workflow_state # should be 'pre_registered' or 'registered'
    p2.reload #should be ActiveRecord::RecordNotFound: Couldn't find Pseudonym with id=x
    u2.reload.workflow_state # should be 'deleted'
    p3.reload.account_id # should be Account.default.id
    u3.reload.workflow_state # should be 'pre_registered' or 'registered'
    
    Change-Id: I298fb662716fae7d0ba806db6878959e38cec322
    Reviewed-on: https://gerrit.instructure.com/57677
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

---
+++ lib/data_fixup/fix_invalid_pseudonym_account_ids.rb
@@ -0,0 +1,28 @@
+module DataFixup::FixInvalidPseudonymAccountIds
+  def self.run
+    Pseudonym.where("NOT EXISTS (SELECT 1 FROM accounts WHERE account_id=accounts.id AND root_account_id IS NULL)").
+      includes(:account, :user).find_each do |p|
+      if p.workflow_state == 'deleted'
+        destroy_pseudonym(p)
+      elsif Pseudonym.where(account_id: p.root_account_id, sis_user_id: p.sis_user_id).
+        order(:workflow_state).where("sis_user_id IS NOT NULL").first
+
+        destroy_pseudonym(p)
+      elsif (p2 = Pseudonym.by_unique_id(p.unique_id).active.
+        where(account_id: p.root_account_id).order(:workflow_state).first)
+
+        UserMerge.from(p.user).into(p2.user)
+        destroy_pseudonym(p)
+      else
+        p.account_id = p.root_account_id
+        p.save!
+      end
+    end
+  end
+
+  def self.destroy_pseudonym(p)
+    p.session_persistence_tokens.scoped.delete_all
+    p.destroy!
+  end
+
+end
