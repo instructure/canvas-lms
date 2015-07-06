3470ba073bdda351c4498086572c334eb6b0f4e1
Cody Cutrer <cody@instructure.com>
2015-07-06 08:31:35 -0600

    bump rspec-rails
    Change-Id: Ibe94e273fd3df1088db01c115581658f1300d25e
    Reviewed-on: https://gerrit.instructure.com/57730
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -14,7 +14,7 @@ group :test do
     gem 'eventmachine', '1.0.4', require: false
 
   gem 'rspec', '3.2.0'
-  gem 'rspec-rails', '3.2.0'
+  gem 'rspec-rails', '3.2.3'
   gem 'rspec-legacy_formatters', '1.0.0'
   gem 'rspec-collection_matchers', '1.1.2'
 
