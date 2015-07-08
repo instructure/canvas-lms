7376ce89e599eff9796105d911b2b9cae823d34c
James Williams <jamesw@instructure.com>
2015-07-08 09:46:41 -0600

    rails4: fix lti specs missing ims/lti dependency
    refs #CNVS-21596
    
    Change-Id: I8c11fdfc52566e9a5286d4ac53c66a6ac4ced32c
    Reviewed-on: https://gerrit.instructure.com/57950
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- lib/lti/content_item_converter.rb
+++ lib/lti/content_item_converter.rb
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+require 'ims/lti'
+
 module Lti
   class ContentItemConverter
 
