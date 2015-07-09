6678e147c24cc6a85f9a979be000fb0f61ecd851
Colleen Palmer <colleen@instructure.com>
2015-07-09 16:59:43 -0600

    Adds new icons for global nav when k12 flag is on
    refs: CNVS-21746
    
    This makes it so when the k12 flag is on, we show a few different icons
    in the global navigation for:
    
    - Dashboard
    - Courses
    - Grades
    - Inbox
    - Calendar
    
    Note: focus is on icons - background color may change based on your settings
    
    Test Plan:
    - Turn on New UI and you should see these icons:
    http://cl.ly/image/0K1D031M0Y43
    
    - Turn on k12 flag and you should see these:
    http://cl.ly/image/1l232g0K2O1I
    
    Change-Id: Ic79cdecb3b8dc83066e58fcd1c45f12422c56f17
    Reviewed-on: https://gerrit.instructure.com/58135
    Reviewed-by: Jennifer Stern <jstern@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Tested-by: Jenkins

--- app/helpers/global_navigation_helper.rb
+++ app/helpers/global_navigation_helper.rb
@@ -25,4 +25,21 @@ module GlobalNavigationHelper
           GLOBAL_NAV_MENU_ITEMS: @mapped_tools
     })
   end
+
+  # When k12 flag is on, replaces global navigation icon with a different one
+  def svg_icon(icon)
+    base = k12? ? 'k12/' : ''
+    begin
+      render_icon_partial(base, icon)
+    rescue ActionView::MissingTemplate => e
+      logger.warn "Global nav icon does not exist: #{e}"
+      render_icon_partial('', icon) rescue nil # worst case we don't render anything
+    end
+  end
+
+  private
+
+  def render_icon_partial(base, icon)
+    render "shared/svg/#{base}svg_icon_#{icon}.svg"
+  end
 end
