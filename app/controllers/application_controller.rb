1e91b8860bca96a6ba8f00b01e82e5dded896f0b
Ryan Shaw <ryan@instructure.com>
2015-07-15 22:34:47 -0600

    fix css loading inside tinyMCE editors
    fixes: CNVS-21910
    
    test plan:
    
    * load a page with a tinymce editor
    * make sure the content inside of it is styled like
      it is currently in production
    
    Change-Id: I7640575a21929ea6b3f80e09b5036c38d54f9b59
    Reviewed-on: https://gerrit.instructure.com/58585
    Tested-by: Jenkins
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Myller de Araujo <myller@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -107,8 +107,10 @@ class ApplicationController < ActionController::Base
     return {} unless request.format.html?
     # set some defaults
     unless @js_env
+      editor_css = view_context.stylesheet_path(css_url_for('what_gets_loaded_inside_the_tinymce_editor'))
       @js_env = {
         active_brand_config: active_brand_config.try(:md5),
+        url_to_what_gets_loaded_inside_the_tinymce_editor_css: editor_css,
         current_user_id: @current_user.try(:id),
         current_user: user_display_json(@current_user, :profile),
         current_user_roles: @current_user.try(:roles, @domain_root_account),
