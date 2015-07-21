a2723773a6b1d82ad6fa7f450a54872ea9f8075b
Ryan Shaw <ryan@instructure.com>
2015-07-21 13:48:27 -0600

    Theme Editor: Correctly handle resetting images
    closes: CNVS-21591
    
    * Upload a brand image (e.g. nav logo) in the theme editor
    * Preview and Apply changes
    * change a color, hit preview
    * you should still see your logo uploaded in step 1
    * Click 'Use Default' under the image in the theme editor
    * Click 'Preview Your Changes' button.
    * you should see whatever the canvas default for that was
    
    Change-Id: I7f9c1aea0992509945914416bb31ad3e822ebb5a
    Reviewed-on: https://gerrit.instructure.com/58944
    Tested-by: Jenkins
    Reviewed-by: Jennifer Stern <jstern@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Adrian Foong <afoong@instructure.com>

--- app/jsx/theme_editor/ThemeEditorImageRow.jsx
+++ app/jsx/theme_editor/ThemeEditorImageRow.jsx
@@ -6,6 +6,11 @@ define([
   'i18n!theme_editor'
 ], (React, customTypes, I18n) => {
 
+  // consider anything other than null or undefined (including '') as "set"
+  function isSet(val) {
+    return val === null || val === undefined
+  }
+
   return React.createClass({
 
     displayName: 'ThemeEditorImageRow',
@@ -19,31 +24,28 @@ define([
     },
 
     getDefaultProps(){
-      return ({
+      return {
         userInput: {}
-      })
+      }
     },
 
-    setValue(inputElement) {
-      var chosenValue = inputElement
+    // valid input: null, '', or an HTMLInputElement
+    setValue(inputElementOrNewValue) {
+      var chosenValue = inputElementOrNewValue
 
-      if (!chosenValue) {
-        // if they hit the "remove" button, we want to also clear out the value of the <input type=file>
+      if (!inputElementOrNewValue) { //if it's null or ''
+        // if they hit the "Undo" or "Use Default" button,
+        // we want to also clear out the value of the <input type=file>
         // but we don't want to mess with its value otherwise
         this.refs.fileInput.getDOMNode().value = ''
       } else {
-        chosenValue = window.URL.createObjectURL(inputElement.files[0])
+        chosenValue = window.URL.createObjectURL(inputElementOrNewValue.files[0])
       }
       this.props.onChange(chosenValue)
     },
 
-    inputName(){
-      // dont insert name & send params if default value
-      var valueIsDefault = !this.props.userInput.val && !this.props.currentValue
-      return valueIsDefault ? '' : 'brand_config[variables]['+ this.props.varDef.variable_name +']'
-    },
-
     render() {
+      var inputName = 'brand_config[variables]['+ this.props.varDef.variable_name +']'
       return (
         <section className="Theme__editor-accordion_element Theme__editor-upload">
           <div className="te-Flex">
@@ -67,30 +69,30 @@ define([
                   />
                 </div>
               </div>
-              <input
-                type="hidden"
-                name={this.inputName()}
-                value={this.props.currentValue}
-              />
               <div className="Theme__editor-image_upload">
                 <label className="Theme__editor-image_upload-label">
-                  <span className="Theme__editor-button_upload Button Button--link">Upload Image</span>
+                  <span className="Theme__editor-button_upload Button Button--link">{I18n.t('Select Image')}</span>
+                  <input
+                    type="hidden"
+                    name={!this.props.userInput.val && inputName}
+                    value={(this.props.userInput.val === '') ? '' : this.props.currentValue}
+                  />
                   <input
                     type="file"
                     className="Theme__editor-input_upload"
-                    name={this.props.userInput.val ? 'brand_config[variables]['+ this.props.varDef.variable_name +']' : ''}
+                    name={this.props.userInput.val && inputName}
                     accept={this.props.varDef.accept}
                     onChange={event => this.setValue(event.target)}
                     ref="fileInput"
                   />
                 </label>
-                {this.props.userInput.val || (this.props.currentValue && this.props.userInput.val !== '') ? (
+                {this.props.userInput.val || this.props.currentValue ? (
                   <button
                     type="button"
                     className="Button Button--link"
-                    onClick={() => this.setValue(this.props.userInput.val ? null : '')}
+                    onClick={() => this.setValue(isSet(this.props.userInput.val) ? '' : null)}
                   >
-                    {this.props.userInput.val && this.props.currentValue ? I18n.t('Undo') : I18n.t('Reset')}
+                    {isSet(this.props.userInput.val) ? I18n.t('Use Default') : I18n.t('Undo')}
                   </button>
                 ) : (
                   null
