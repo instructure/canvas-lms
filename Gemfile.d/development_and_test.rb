5e11241a9e45d30798c6d448c6be5d9d6782af7e
Derek Bender <dbender@instructure.com>
2015-07-24 09:59:31 -0600

    add backported log colorizer
    Change-Id: I0dde9eb720b019edd8b2bb03ecbe52cde69e65f8
    Reviewed-on: https://gerrit.instructure.com/59219
    Tested-by: Jenkins
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>
    Product-Review: Derek Bender <dbender@instructure.com>

--- Gemfile.d/development_and_test.rb
+++ Gemfile.d/development_and_test.rb
@@ -3,5 +3,6 @@ group :development, :test do
   gem 'coffee-script-source', '1.6.2' #pinned so everyone's compiled output matches
   gem 'execjs', '1.4.0'
   gem 'hashdiff', '0.2.0'
+  gem 'activerecord-colored_log_subscriber', '0.1.0'
 end
 
