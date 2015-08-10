06e6e74dfe76254939b081a4276408859a4ce9cc
Cody Cutrer <cody@instructure.com>
2015-08-10 16:07:02 -0600

    improve client_app_symlinks and plugin_symlinks
     * fix not working from rails root for both
     * don't require write permissions to the fs if nothing needs to be done
       for client_app_symlinks
    
    Change-Id: I5bf9d194f33e432ca581a69b7e4b03b5be29296c
    Reviewed-on: https://gerrit.instructure.com/60452
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/client_app_symlinks.rb
+++ config/initializers/client_app_symlinks.rb
@@ -1,13 +1,13 @@
 require 'fileutils'
 require 'pathname'
+require 'set'
 
 # stolen and adapted from ./plugin_symlinks.rb
 def maintain_client_app_symlinks
   output_dir = Pathname.new "public/javascripts/client_apps"
-  # remove anything that was there first
-  output_dir.rmtree if output_dir.exist?
 
-  # create new ones
+  links = []
+  # make a list of what should exist
   Pathname.glob("client_apps/*").select(&:directory?).each do |app_dir|
     app = app_dir.basename
     dist = app_dir.join('dist')
@@ -16,16 +16,46 @@ def maintain_client_app_symlinks
      [Pathname.new("#{app}.js")] + Pathname.glob("#{app}/**/*").reject(&:directory?)
     end
 
-    files.each do |asset|
+    links.concat(files.map do |asset|
       original = dist.join(asset)
       target = output_dir.join(asset)
       FileUtils.mkdir_p(target.dirname)
-      File.symlink(original.relative_path_from(target.dirname), target)
+      [original.relative_path_from(target.dirname), target]
+    end)
+  end
+
+  valid_files = links.map(&:last).to_set
+  valid_dirs = Set.new
+  valid_files.each do |file|
+    dir = file.dirname
+    while dir != output_dir
+      valid_dirs << dir
+      dir = dir.dirname
+    end
+  end
+
+  # remove any unnecessary links
+  Pathname.glob("public/javascripts/client_apps/**/*").each do |file|
+    if file.directory?
+      file.rmtree unless valid_dirs.include?(file)
+    else
+      File.unlink(file) unless valid_files.include?(file)
+    end
+  end
+
+  # create links
+  links.each do |(target, source)|
+    unless File.symlink?(source) && File.readlink(source) == target
+      File.unlink(source) if File.exist?(source)
+      File.symlink(target, source)
     end
   end
 end
 
 File.open(__FILE__) do |f|
   f.flock(File::LOCK_EX)
-  maintain_client_app_symlinks
+
+  Dir.chdir(Rails.root) do
+    maintain_client_app_symlinks
+  end
 end
