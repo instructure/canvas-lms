dea1d0104bf2379bcc99cff31664f19c8dfc4159
Steven Shepherd <sshepherd@instructure.com>
2015-08-12 08:26:31 -0600

    Add specs for testing group discussions
    sshepherd,panda,7
    
    Change-Id: I34120d9ee94a7a09a814216ba635e06a9d201b03
    Reviewed-on: https://gerrit.instructure.com/60585
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>

--- spec/selenium/groups_pages_spec.rb
+++ spec/selenium/groups_pages_spec.rb
@@ -1,9 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/common')
 require File.expand_path(File.dirname(__FILE__) + '/helpers/groups_common')
 require File.expand_path(File.dirname(__FILE__) + '/helpers/announcements_common')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/discussions_common')
 
 describe "groups" do
   include_context "in-process server selenium tests"
+
+  let(:url) {"/groups/#{@testgroup.first.id}"}
+  let(:announcements_page) {url + '/announcements'}
+  let(:people_page) {url + '/users'}
+  let(:discussions_page) {url + '/discussion_topics'}
 
   context "as a student" do
     before do
@@ -12,10 +18,6 @@ describe "groups" do
       # adds all students to the group
       add_users_to_group(@students + [@user],@testgroup.first)
     end
-
-    let(:url) {"/groups/#{@testgroup.first.id}"}
-    let(:announcements_page) {url + '/announcements'}
-    let(:people_page) {url + '/users'}
 
     describe "announcement page" do
       it "should center the add announcement button if no announcements are present", priority: "1", test_id: 273606 do
@@ -82,6 +84,82 @@ describe "groups" do
         # Checks that all students and teachers created in setup are listed on page
         expect(ff('.student_roster .user_name').size).to eq 5
         expect(ff('.teacher_roster .user_name').size).to eq 1
+      end
+    end
+
+    describe "discussions page" do
+      it "should allow discussions to be created within a group", priority: "1", test_id: 273615 do
+        get discussions_page
+        expect_new_page_load { f('#new-discussion-btn').click }
+        # This creates the discussion and also tests its creation
+        edit_topic('from a student', 'tell me a story')
+      end
+
+      it "should allow group members to access a discussion", priority: "1", test_id: 273616 do
+        dt = DiscussionTopic.create!(context: @testgroup.first, user: @teacher,
+                                     title: 'Discussion Topic', message: 'hi dudes')
+        get discussions_page
+        # Verifies group member can access the teacher's group discussion & that it's the correct discussion
+        expect_new_page_load { f('.discussion-title').click }
+        expect(f('.message.user_content')).to include_text(dt.message)
+      end
+
+      it "should have two options when creating a discussion", priority: "1", test_id: 273617 do
+        get discussions_page
+        expect_new_page_load { f('#new-discussion-btn').click }
+        expect(f('#threaded')).to be_displayed
+        expect(f('#allow_rating')).to be_displayed
+        # Shouldn't be Enable Podcast Feed option
+        expect(f('#podcast_enabled')).to be_nil
+      end
+
+      it "should only list in-group discussions in the content right pane", priority: "1", test_id: 273622 do
+        # create group and course announcements
+        group_dt = DiscussionTopic.create!(context: @testgroup.first, user: @teacher,
+                                           title: 'Group Discussion', message: 'Group')
+        course_dt = DiscussionTopic.create!(context: @course, user: @teacher,
+                                            title: 'Course Discussion', message: 'Course')
+
+        get discussions_page
+        expect_new_page_load { f('.btn-primary').click }
+        fj(".ui-accordion-header a:contains('Discussions')").click
+        expect(fln("#{group_dt.title}")).to be_displayed
+        expect(fln("#{course_dt.title}")).to be_nil
+      end
+    end
+  end
+
+  context "as a teacher" do
+    before do
+      course_with_teacher_logged_in(active_all: true)
+      group_test_setup(4,1,1)
+      # adds all students to the group
+      add_users_to_group(@students,@testgroup.first)
+    end
+
+    describe "discussions page" do
+      it "should allow teachers to create discussions within a group", priority: "1", test_id: 285586 do
+        get discussions_page
+        expect_new_page_load { f('#new-discussion-btn').click }
+        # This creates the discussion and also tests its creation
+        edit_topic('from a teacher', 'tell me a story')
+      end
+
+      it "should have three options when creating a discussion", priority: "1", test_id: 285584 do
+        get discussions_page
+        expect_new_page_load { f('#new-discussion-btn').click }
+        expect(f('#threaded')).to be_displayed
+        expect(f('#allow_rating')).to be_displayed
+        expect(f('#podcast_enabled')).to be_displayed
+      end
+
+      it "should allow teachers to access a discussion", priority: "1", test_id: 285585 do
+        dt = DiscussionTopic.create!(context: @testgroup.first, user: @students.first,
+                                     title: 'Discussion Topic', message: 'hi dudes')
+        get discussions_page
+        # Verifies group member can access the teacher's group discussion & that it's the correct discussion
+        expect_new_page_load { f('.discussion-title').click }
+        expect(f('.message.user_content')).to include_text(dt.message)
       end
     end
   end
