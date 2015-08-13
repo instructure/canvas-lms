4004c8c972ff32797ff7b2d00f4f03d4917a39ac
Davis McClellan <dmcclellan@instructure.com>
2015-08-13 14:06:10 -0600

    Let OQAAT quizzes be previewed past due
    fixes CNVS-22048
    
    test plan:
    - Create OQAAT Quiz with the Availability Date in the Past
    - Preview.
    - Attempt to proceed to the next question.
    - Succeeds if able to proceed.
    
    Change-Id: Iffed1c6357f84bfedb01723d3c91ddb263029103
    Reviewed-on: https://gerrit.instructure.com/60825
    Tested-by: Jenkins
    Reviewed-by: John Corrigan <jcorrigan@instructure.com>
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Davis McClellan <dmcclellan@instructure.com>

---
+++ spec/selenium/quizzes_teacher_oqaat_spec.rb
@@ -0,0 +1,57 @@
+require_relative 'helpers/quiz_questions_common'
+
+describe 'One Question at a Time Quizzes' do
+  include_examples 'quiz question selenium tests'
+
+  context 'with a teacher' do
+
+    before(:each) do
+      create_oqaat_quiz
+      user_session(@teacher)
+    end
+
+    context 'when taking an OQAAT quiz' do
+
+      it 'saves answers and grades the quiz', priority: "1", test_id: 209372 do
+        preview_the_quiz
+        answers_flow
+      end
+
+      it 'displays one question at a time', priority: "1", test_id: 209373 do
+        preview_the_quiz
+        back_and_forth_flow
+      end
+    end
+
+    context 'when taking a sequential OQAAT quiz' do
+
+      before(:each) do
+        @quiz.update_attribute(:cant_go_back, true)
+      end
+
+      it 'displays one question at a time and prevents going back', priority: "1", test_id: 209374 do
+        preview_the_quiz
+        check_if_cant_go_back
+      end
+
+      it 'saves answers and grades the quiz', priority: "1", test_id: 209375 do
+        preview_the_quiz
+        answers_flow
+      end
+    end
+
+    context 'when taking an OQAAT quiz with a past due date' do
+
+      before(:each) do
+        @quiz.update_attribute(:lock_at, Date.new(2014,2,3))
+        @quiz.update_attribute(:unlock_at, Date.new(2014,2,4))
+        @quiz.update_attribute(:due_at, Date.new(2014,2,4))
+      end
+
+      it 'displays one question at a time with a past due date', priority: "1", test_id: 209376 do
+        preview_the_quiz
+        back_and_forth_flow
+      end
+    end
+  end
+end
