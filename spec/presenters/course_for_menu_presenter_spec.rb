94528721613a736562860272eefebe05e29a6236
James Williams <jamesw@instructure.com>
2015-08-25 07:08:46 -0600

    show correct roles for cross-shard enrollments in menu
    Change-Id: I8f299dde7b6980d735e362e0537793e3deb53870
    Reviewed-on: https://gerrit.instructure.com/61737
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- spec/presenters/course_for_menu_presenter_spec.rb
+++ spec/presenters/course_for_menu_presenter_spec.rb
@@ -16,7 +16,7 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../sharding_spec_helper')
 
 describe CourseForMenuPresenter do
   let_once(:course) { course_model }
@@ -48,6 +48,23 @@ describe CourseForMenuPresenter do
 
     it 'should include available_section_tabs as link element of hash' do
       expect(presenter.to_h[:links].length).to eq presenter.available_section_tabs.length
+    end
+  end
+
+  describe '#role' do
+    specs_require_sharding
+    it "should retrieve the correct role for cross-shard enrollments" do
+      @shard1.activate do
+        account = Account.create
+        @role = account.roles.create :name => "1337 Student"
+        @role.base_role_type = 'StudentEnrollment'
+        @role.save!
+
+        @cs_course = account.courses.create!
+        @cs_course.primary_enrollment_role_id = @role.local_id
+      end
+      cs_presenter = CourseForMenuPresenter.new(@cs_course, available_section_tabs)
+      expect(cs_presenter.send(:role)).to eq @role
     end
   end
 end
