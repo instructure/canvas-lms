d443bd2851d4856f5b5b506c830686b48dfea29d
Nick Houle <nhoule@instructure.com>
2015-09-03 14:46:01 -0600

    Allow submission types to be sent for GBP with sis_assignment API
    Change-Id: Iae69c83cd0603bb412403512e5d813a83e3b306f
    Fixes: SIS-1405
    Reviewed-on: https://gerrit.instructure.com/62556
    Reviewed-by: Tyler Pickett <tpickett@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Brad Humphrey <brad@instructure.com>

--- app/controllers/sis_api_controller.rb
+++ app/controllers/sis_api_controller.rb
@@ -50,6 +50,7 @@ class SisApiController < ApplicationController
   #       "due_at": "2015-01-01T17:00:00Z",
   #       "points_possible": 100,
   #       "integration_id": "IA-100",
+  #       "submission_types": "["online_quiz"]",
   #       "integration_data": {
   #         "other_data": "values"
   #       },
