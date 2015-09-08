e2fcdd81ad61517f10390888689889ad86506009
Matthew Berns <mberns@instructure.com>
2015-09-08 14:07:51 -0500

    fix submit assignment button not appearing for admins enrolled as students
    fixes CNVS-22087
    
    test plan:
    - enroll an admin in a course as a student
    - log in as the admin and go to an assignment in the course
    - the submit assignment button should appear near the pubish and edit buttons
    - the submit assignment button should only appear for admins that also have
      a student enrollment. other admins should not see it.
    - the admin should be able to submit without issues
    
    Change-Id: I91fb68a8afec68c2ba2018704027f5eaa95e01cf
    Reviewed-on: https://gerrit.instructure.com/62778
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Tested-by: Jenkins
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Jason Sparks <jsparks@instructure.com>

--- spec/selenium/admin/assignments_admin_spec.rb
+++ spec/selenium/admin/assignments_admin_spec.rb
@@ -7,12 +7,12 @@ describe "assignments" do
     before do
       @student = user_with_pseudonym(:active_user => true)
       course_with_student(:active_all => true, :user => @student)
+      assignment_model(:course => @course, :submission_types => 'online_upload', :title => 'Assignment 1')
       site_admin_logged_in
     end
 
     it "should not show google docs tab for masquerading admin" do
       PluginSetting.create!(:name => 'google_docs', :settings => {})
-      assignment_model(:course => @course, :submission_types => 'online_upload', :title => 'Assignment 1')
       get "/users/#{@student.id}/masquerade"
       expect_new_page_load { f('.masquerade_button').click }
 
@@ -27,6 +27,19 @@ describe "assignments" do
       f('#section-tabs .home').click
       driver.switch_to.alert.accept
       driver.switch_to.default_content
+    end
+
+    it "should show the submit button if admin is enrolled as student" do
+      get "/courses/#{@course.id}/assignments/#{@assignment.id}"
+      wait_for_ajaximations
+
+      expect(f('.submit_assignment_link')).to be_nil
+
+      @course.enroll_student(@admin, :enrollment_state => 'active')
+
+      refresh_page
+      wait_for_ajaximations
+      expect(f('.submit_assignment_link')).to be_displayed
     end
   end
 end
