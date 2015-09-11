d30c3855ef85c6d3fdd7202dfff662f00f3ab2d2
Clay Diffrient <cdiffrient@instructure.com>
2015-09-11 10:49:48 -0600

    Adds + Reviewer button to the moderated assignment header
    This refactors the header to use the appropriate styles from the
    style guide as well.  The + Reviewer button needs to get the appropriate
    action once the store/api underneath it get reworked.
    
    refs CNVS-22002
    
    Test Plan:
      - Enable moderated grading
      - Go to an assignment that can be moderated's moderation page
      - You should see a + Reviewer button.
      - Clicking it should cause an alert to pop up.
    
    Change-Id: I33be45dd4cbbdf9e49f55934d9a871684b0903f5
    Reviewed-on: https://gerrit.instructure.com/63095
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- app/jsx/assignments/Header.jsx
+++ app/jsx/assignments/Header.jsx
@@ -25,30 +25,42 @@ define([
     },
 
     handlePublishClick () {
-      // Make a better looking confirm one day
+      // TODO: Make a better looking confirm one day
       var confirmMessage = I18n.t('Are you sure you want to do this? It cannot be undone and will override existing grades in the gradebook.')
       if (window.confirm(confirmMessage)) {
         this.props.store.dispatch(this.props.actions.publishGrades());
       }
     },
 
+    handleReviewerClick () {
+      window.alert('TODO: Fix once the API for this calms down.');
+    },
+
     render () {
       return (
-        <div className='ModeratedGrading__Header content-box'>
-          <div className='grid-row'>
-            <div className='ModeratedGrading__Header-Instructions col-xs-11'>
+        <div className='ModeratedGrading__Header ic-Action-header'>
+          <div className='ic-Action-header__Primary'>
+            <div className='ic-Action-header__Heading ModeratedGrading__Header-Instructions'>
               {I18n.t('Select assignments for review')}
             </div>
-            <div className='ModeratedGrading__Header-Buttons col-xs-1'>
-              <button
-                type='button'
-                className='ModeratedGrading__Header-PublishBtn Button Button--primary'
-                onClick={this.handlePublishClick}
-                disabled={this.state.published}
-              >
-                {I18n.t('Publish')}
-              </button>
-            </div>
+          </div>
+          <div className='ic-Action-header__Secondary ModeratedGrading__Header-Buttons '>
+            <button
+              type='button'
+              className='ModeratedGrading__Header-AddReviewerBtn Button'
+              onClick={this.handleReviewerClick}
+            >
+              <i className='icon-plus' />
+              {I18n.t(' Reviewer')}
+            </button>
+            <button
+              type='button'
+              className='ModeratedGrading__Header-PublishBtn Button Button--primary'
+              onClick={this.handlePublishClick}
+              disabled={this.state.published}
+            >
+              {I18n.t('Publish')}
+            </button>
           </div>
         </div>
       );
