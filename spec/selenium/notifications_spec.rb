0f2e2c13d6e58179603d4ff937c78b02e13da519
Steven Shepherd <sshepherd@instructure.com>
2015-09-16 14:31:53 -0600

    Add specs for testing appointment group  notifications
    Test Plan:
      - Run specs
    
    Change-Id: I3df01166308c7ce30fa6f690ecb209e936144b22
    Reviewed-on: https://gerrit.instructure.com/63439
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: Steven Shepherd <sshepherd@instructure.com>
    QA-Review: Steven Shepherd <sshepherd@instructure.com>

---
+++ spec/selenium/notifications_spec.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/common')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/notifications_common')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/calendar2_common')
+
+describe "Notifications" do
+  include_context "in-process server selenium tests"
+
+  context "admin" do
+    before(:all) do
+      course_with_student(active_all: true)
+      site_admin_logged_in
+      setup_comm_channel(@student)
+    end
+
+    it "should send a notification to users that appointment groups are available", priority: "1", test_id: 186566 do
+      note_name = 'Appointment Group Published'
+      setup_notification(@student, name: note_name)
+      create_appointment_group
+
+      get "/users/#{@student.id}/messages"
+
+      # Checks that the notification is there and has the correct "Notification Name" field
+      fj('.ui-tabs-anchor:contains("Meta Data")').click
+      expect(ff('.table-condensed.grid td').last).to include_text(note_name)
+    end
+  end
+end
