062c6ab097cc3ec7034fbe0fe315ee1ad03ee5df
Cody Poll <cpoll@instructure.com>
2015-09-28 15:27:17 -0600

    Discussion groups are ordered by ID
    fixes CNVS-23671
    
    On 'courses/:course_id/discussion_topics/:topic_id', groups were ordered
    randomly. They are now ordered by ID, which for many cases is also alphabetical
    order.
    
    Test Plan:
      1. Create a course with multiple discussion groups
      2. Fill the groups with students
      3. Go to the discussion group's page (/courses/:course_id/discussion_topics/:id
      4. Verify that the order of the groups stays consistent across page refreshes
    
    Change-Id: Ibfc9879b30f7206a44c4ab8764705a3adde6996d
    Reviewed-on: https://gerrit.instructure.com/64172
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Tested-by: Jenkins
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Jason Sparks <jsparks@instructure.com>

--- app/controllers/discussion_topics_controller.rb
+++ app/controllers/discussion_topics_controller.rb
@@ -464,7 +464,7 @@ class DiscussionTopicsController < ApplicationController
       @unlock_at = @topic.available_from_for(@current_user)
       @topic.change_read_state('read', @current_user) unless @locked
       if @topic.for_group_discussion?
-        @groups = @topic.group_category.groups.active.select{ |g| g.grants_right?(@current_user, session, :post_to_forum) }
+        @groups = @topic.group_category.groups.active.select{ |g| g.grants_right?(@current_user, session, :post_to_forum) }.sort! {|a, b| a.id <=> b.id}
         topics = @topic.child_topics.to_a
         topics = topics.select{|t| @groups.include?(t.context) } unless @topic.grants_right?(@current_user, session, :update)
         @group_topics = @groups.map do |group|
