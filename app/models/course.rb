8cedf7634e00b6dfbf0e5fe0c1c0920b26b7e646
James Williams <jamesw@instructure.com>
2015-09-29 09:31:58 -0600

    use role overrides for :view_all_grades for concluded enrollments
    test plan:
    * create a account with the "View all grades" permission
     disabled for a TA role
    * as a TA in a course in that account, should not be able
     to view the gradebook when the course is concluded
    
    closes #CNVS-23677
    
    Change-Id: Ie0ba4d7fa4a45446e10d8caab584168088ed7547
    Reviewed-on: https://gerrit.instructure.com/64211
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -1130,15 +1130,18 @@ class Course < ActiveRecord::Base
     end
     can [:read, :read_as_admin, :read_roster, :read_prior_roster, :read_forum, :use_student_view, :read_outcomes, :view_unpublished_items]
 
-    given do |user|
-      !self.deleted? && user &&
-        (prior_enrollments.for_user(user).any?{|e| e.instructor? } ||
-          user.cached_not_ended_enrollments.any? do |e|
-            e.course_id == self.id && e.instructor? && e.completed?
-          end
-        )
+    # overrideable permissions for concluded admins
+    [:read_question_banks, :view_all_grades].each do |permission|
+      given do |user|
+        !self.deleted? && user &&
+          (prior_enrollments.for_user(user).any?{|e| e.admin? && e.has_permission_to?(permission)} ||
+            user.cached_not_ended_enrollments.any? do |e|
+              e.course_id == self.id && e.admin? && e.completed? && e.has_permission_to?(permission)
+            end
+          )
+      end
+      can permission
     end
-    can :view_all_grades
 
     # Teacher or Designer of a concluded course
     given do |user|
