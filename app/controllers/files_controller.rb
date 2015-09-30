16b49a9d43cfc146745773d1e783ed93439362e8
Jeremy Stanley <jeremy@instructure.com>
2015-09-30 16:55:32 -0600

    allow users to view course files with :manage_files alone
    test plan:
     - create an account role that grants "manage files" but not
       "read course content"
     - set up a user in this role
     - the user should be able to view the course files tab
       (also a limited subset of the Settings tab, which is
        not new for this commit)
    
    fixes CNVS-22913
    
    Change-Id: I0ee87fb13746ceb3226d2693d5584d92312ba334
    Reviewed-on: https://gerrit.instructure.com/64377
    Tested-by: Jenkins
    Reviewed-by: James Williams  <jamesw@instructure.com>
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -321,7 +321,7 @@ class FilesController < ApplicationController
   end
 
   def react_files
-    if authorized_action(@context, @current_user, :read) && tab_enabled?(@context.class::TAB_FILES)
+    if authorized_action(@context, @current_user, [:read, :manage_files]) && tab_enabled?(@context.class::TAB_FILES)
       @contexts = [@context]
       get_all_pertinent_contexts(include_groups: true) if @context == @current_user
       files_contexts = @contexts.map do |context|
