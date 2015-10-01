fa2ccc3970b19d56561185a1b0591cd9f2036f28
James Williams <jamesw@instructure.com>
2015-10-01 07:06:07 -0600

    improve moderated grading needs_grading_count with null scores
    test plan:
    * as a provisional grader for a moderated assignment,
     merely adding a comment (and not giving a grade)
     should not decrement the
     "needs grading" count on the course dashboard sidebar
    
     closes #CNVS-23758
    
    Change-Id: Ie6fa807f468a58d5429f0c93c360dc4095cd759d
    Reviewed-on: https://gerrit.instructure.com/64389
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Clare Strong <clare@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- app/models/assignments/needs_grading_count_query.rb
+++ app/models/assignments/needs_grading_count_query.rb
@@ -64,13 +64,16 @@ module Assignments
       # ignore submissions this user has graded
       graded_sub_ids = assignment.submissions.joins(:provisional_grades).
         where("moderated_grading_provisional_grades.final = ?", false).
-        where("moderated_grading_provisional_grades.scorer_id = ?", user.id).pluck(:id)
+        where("moderated_grading_provisional_grades.scorer_id = ?", user.id).
+        where("moderated_grading_provisional_grades.score IS NOT NULL").pluck(:id)
 
       moderation_set_student_ids = assignment.moderated_grading_selections.pluck(:student_id)
 
-      # ignore submissions that are fully graded
+      # ignore submissions that don't need any more provisional grades
       pg_scope = assignment.submissions.joins(:provisional_grades).
-        where("moderated_grading_provisional_grades.final = ?", false).group("submissions.id", "submissions.user_id")
+        where("moderated_grading_provisional_grades.final = ?", false).
+        where("moderated_grading_provisional_grades.scorer_id <> ?", user.id).
+        group("submissions.id", "submissions.user_id")
       pg_scope = pg_scope.where("submissions.id NOT IN (?)", graded_sub_ids) if graded_sub_ids.any?
       pg_scope.count.each do |key, count|
         sub_id, user_id = key
