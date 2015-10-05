6148c935a41398a567579e879685794c1e0c1cf4
Jon Willesen <jonw@instructure.com>
2015-10-05 14:50:36 -0600

    remove containment for sortable modules
    this allows tall modules to be dragged to the bottom of the list
    
    fixes CNVS-22525
    
    test plan:
     - make a few modules
     - add a few module items to one of the modules to make it
       taller
     - with the tall module open, drag it to the bottom of the
       module list to put it at the bottom
    
    Change-Id: I96047e68a044fd1b48b62a4d45a1787f200cbf1e
    Reviewed-on: https://gerrit.instructure.com/64599
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Jon Willesen <jonw@instructure.com>

--- public/javascripts/context_modules.js
+++ public/javascripts/context_modules.js
@@ -1311,7 +1311,6 @@ define([
       $("#context_modules").sortable({
         handle: '.reorder_module_link',
         helper: 'clone',
-        containment: '#context_modules_sortable_container',
         axis: 'y',
         update: modules.updateModulePositions
       });
