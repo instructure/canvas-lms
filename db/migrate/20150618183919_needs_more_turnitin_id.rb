8bd92920a245ff6a741ea34a82e7ef0a05d68aa4
Cameron Matheson <cameron@instructure.com>
2015-10-12 16:27:47 -0600

    make unique index on turnitin columns
    Change-Id: I8b5b105023b492905ee878f6db09490d4caff98c
    Reviewed-on: https://gerrit.instructure.com/65076
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20150618183919_needs_more_turnitin_id.rb
+++ db/migrate/20150618183919_needs_more_turnitin_id.rb
@@ -2,7 +2,7 @@ class NeedsMoreTurnitinId < ActiveRecord::Migration
   tag :predeploy
 
   def change
-    add_column :assignments, :turnitin_id, :integer, limit: 8, unique: :true
-    add_column :users, :turnitin_id, :integer, limit: 8, unique: :true
+    add_column :assignments, :turnitin_id, :integer, limit: 8
+    add_column :users, :turnitin_id, :integer, limit: 8
   end
 end
