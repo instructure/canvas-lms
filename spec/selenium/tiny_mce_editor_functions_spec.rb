95019c5b4d40ffe56cec004ef5bc5216608e29a2
Jermey Putnam <jeremyp@instructure.com>
2015-10-16 09:35:09 -0600

    spec: add tiny-mce italic button check
    Change-Id: I901246674e3b1b95aecbdaf01a28c147b3fe8d97
    Reviewed-on: https://gerrit.instructure.com/65277
    Tested-by: Jenkins
    Product-Review: Jeremy Putnam <jeremyp@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>

--- spec/selenium/tiny_mce_editor_functions_spec.rb
+++ spec/selenium/tiny_mce_editor_functions_spec.rb
@@ -43,5 +43,39 @@ describe "Tiny MCE editor functions" do
       # To avoid fragility of extra formatting characters being added, I just want the first 31 characters
       expect(p.body[0..30]).to eq "<p>this should get unbolded</p>"
     end
+
+    it "should italicize text on wiki description", priority: "1", test_id: 285129 do
+      p = create_wiki_page("test_page", false, "public")
+      get "/courses/#{@course.id}/pages/#{p.title}/edit"
+      wait_for_ajaximations
+
+      f("div[aria-label='Italic'] button").send_keys :shift # no content but gives the italic button focus
+      f("div[aria-label='Italic'] button").click
+      type_in_tiny('textarea.body', "this should be typed in italics")
+      f("form.edit-form button.submit").click
+      wait_for_ajaximations
+
+      p.reload
+      # To avoid fragility of extra formatting characters being added, I just want the first 50 characters
+      expect(p.body[0..46]).to eq "<p><em>this should be typed in italics</em></p>"
+    end
+
+    it "should unbold text on wiki description", priority: "1", test_id: 417603 do
+      p = create_wiki_page("test_page", false, "public")
+      p.body = "<p><em>this should get unitalicized</em></p>"
+      p.save!
+      get "/courses/#{@course.id}/pages/#{p.title}/edit"
+      wait_for_ajaximations
+
+      select_all_wiki
+      f("div[aria-label='Italic'] button").send_keys :shift # no content but gives the italic button focus
+      f("div[aria-label='Italic'] button").click
+      f("form.edit-form button.submit").click
+      wait_for_ajaximations
+
+      p.reload
+      # To avoid fragility of extra formatting characters being added, I just want the first 31 characters
+      expect(p.body[0..34]).to eq "<p>this should get unitalicized</p>"
+    end
   end
 end
