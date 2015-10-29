b858cba8ca3d25ca5cbad3a65b81e557b935728a
kinezu <dhansen@instructure.com>
2015-10-29 15:16:47 -0600

    Taking appium mobile specs out of the canvas-lms project
    They now live in their own private repository.
    
    Change-Id: Id4e9e3f745c1311fc75f99bb62333af44b90f145
    Reviewed-on: https://gerrit.instructure.com/66125
    Tested-by: Jenkins
    Reviewed-by: Taylor Wilson <twilson@instructure.com>
    Product-Review: Derek Hansen <dhansen@instructure.com>
    QA-Review: Derek Hansen <dhansen@instructure.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -229,9 +229,6 @@ def truncate_table(model)
       begin
         old_proc = model.connection.raw_connection.set_notice_processor {}
         model.connection.execute("TRUNCATE TABLE #{model.connection.quote_table_name(model.table_name)} CASCADE")
-
-        # mobile verify expects specific id seq for developer key, this forces the sequence to always start at 101
-        model.connection.execute("SELECT setval('developer_keys_id_seq', 100);") if model == DeveloperKey
       ensure
         model.connection.raw_connection.set_notice_processor(&old_proc)
       end
