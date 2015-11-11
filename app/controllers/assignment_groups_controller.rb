a6849f84d71fb8b5754fcdeaefec1b5c17a540fb
John Corrigan <jcorrigan@instructure.com>
2015-11-11 17:11:38 -0600

    assignment group API includes submission information
    fixes CNVS-21989
    
    - Moves submissions_hash from AssignmentsApiController to
      Api::V1::Assignment module.
    - Adds `submission` flag to index & show assignment groups api
      controller actions.
    
    test plan:
    - Have an assignment group including assignments that have submissions
      for a given user, and those that do not.
    - Hit the index & show actions without any flags to make sure endpoints
      still behave as expected.
    - Hit the index & show actions with the `assignments` flag to make sure
      assignmetns without the submission flag are still returned & behave as
      expected.
    - Hit the index & show actions with the `assignments` & `submission`
      flag and observe that a submission object is returned for assignments
      that have a submission for the given user, and that submission objects
      are not returned for assignments without submissions for the given user.
    - Hit the index & show actions with only the `submission` flag and make
      sure that neither assignments non submissions are returned (the
      `submission` flag is a noop without also providing the `assignments`
      flag).
    
    Change-Id: I5d216a7caa65c69e0a3c219abcbe9132b6a567aa
    Reviewed-on: https://gerrit.instructure.com/67010
    Tested-by: Jenkins
    Reviewed-by: Matt Berns <mberns@instructure.com>
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: Brandon Pluim <bpluim@instructure.com>

--- app/controllers/assignment_groups_controller.rb
+++ app/controllers/assignment_groups_controller.rb
@@ -91,9 +91,9 @@ class AssignmentGroupsController < ApplicationController
   # Returns the list of assignment groups for the current context. The returned
   # groups are sorted by their position field.
   #
-  # @argument include[] [String, "assignments"|"discussion_topic"|"all_dates"|"assignment_visibility"|"overrides"]
+  # @argument include[] [String, "assignments"|"discussion_topic"|"all_dates"|"assignment_visibility"|"overrides"|"submission"]
   #  Associations to include with the group. "discussion_topic", "all_dates"
-  #  "assignment_visibility" are only valid are only valid if "assignments" is also included.
+  #  "assignment_visibility" & "submission" are only valid are only valid if "assignments" is also included.
   #  The "assignment_visibility" option additionally requires that the Differentiated Assignments course feature be turned on.
   #
   # @argument override_assignment_dates [Boolean]
@@ -114,9 +114,13 @@ class AssignmentGroupsController < ApplicationController
         []
       end
 
+      if assignments.any? && include_params.include?('submission')
+        submissions = submissions_hash(['submission'], assignments)
+      end
+
       respond_to do |format|
         format.json do
-          render json: index_groups_json(@context, @current_user, groups, assignments)
+          render json: index_groups_json(@context, @current_user, groups, assignments, submissions)
         end
       end
     end
@@ -270,7 +274,7 @@ class AssignmentGroupsController < ApplicationController
     @context.feature_enabled?(:differentiated_assignments)
   end
 
-  def index_groups_json(context, current_user, groups, assignments)
+  def index_groups_json(context, current_user, groups, assignments, submissions = [])
     include_overrides = include_params.include?('overrides')
 
     assignments_by_group = assignments.group_by(&:assignment_group_id)
@@ -298,7 +302,8 @@ class AssignmentGroupsController < ApplicationController
           assignment_visibilities: assignment_visibilities(context, assignments),
           differentiated_assignments_enabled: differentiated_assignments?,
           exclude_descriptions: !!params[:exclude_descriptions],
-          overrides: group_overrides
+          overrides: group_overrides,
+          submissions: submissions
         }
       )
     end
@@ -328,7 +333,7 @@ class AssignmentGroupsController < ApplicationController
       context,
       groups,
       assignment_includes
-    ).with_student_submission_count
+    ).with_student_submission_count.all
 
     if params[:grading_period_id].present? && multiple_grading_periods?
       grading_period = GradingPeriod.context_find(
