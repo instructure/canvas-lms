b2f36fd6ab95334263a3caac10787d835a2373d6
Spencer Miller <smiller@instructure.com>
2015-11-16 12:10:32 -0700

    Typo fix; "porfolio" -> "portfolio"
    fixes CNVS-24782
    
    test plan:
    - ensure message "Portfolio successfully updated/created" is spelled
        correctly when updating or creating an ePortfolio
    
    Change-Id: I33ce74b46fc7a292b1df79e1372049e2fe102138
    Reviewed-on: https://gerrit.instructure.com/67212
    Tested-by: Jenkins
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Jason Carter <jcarter@instructure.com>
    Product-Review: Christi Wruck

--- app/controllers/eportfolios_controller.rb
+++ app/controllers/eportfolios_controller.rb
@@ -44,7 +44,7 @@ class EportfoliosController < ApplicationController
       respond_to do |format|
         if @portfolio.save
           @portfolio.ensure_defaults
-          flash[:notice] = t('notices.created', "Porfolio successfully created")
+          flash[:notice] = t('notices.created', "ePortfolio successfully created")
           format.html { redirect_to eportfolio_url(@portfolio) }
           format.json { render :json => @portfolio.as_json(:permissions => {:user => @current_user, :session => session}) }
         else
@@ -104,7 +104,7 @@ class EportfoliosController < ApplicationController
       respond_to do |format|
         if @portfolio.update_attributes(params[:eportfolio])
           @portfolio.ensure_defaults
-          flash[:notice] = t('notices.updated', "Porfolio successfully updated")
+          flash[:notice] = t('notices.updated', "ePortfolio successfully updated")
           format.html { redirect_to eportfolio_url(@portfolio) }
           format.json { render :json => @portfolio.as_json(:permissions => {:user => @current_user, :session => session}) }
         else
@@ -120,7 +120,7 @@ class EportfoliosController < ApplicationController
     if authorized_action(@portfolio, @current_user, :delete)
       respond_to do |format|
         if @portfolio.destroy
-          flash[:notice] = t('notices.deleted', "Portfolio successfully deleted")
+          flash[:notice] = t('notices.deleted', "ePortfolio successfully deleted")
           format.html { redirect_to user_profile_url(@current_user) }
           format.json { render :json => @portfolio }
         else
