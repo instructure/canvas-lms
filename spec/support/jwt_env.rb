4e08344ad86d1705ec57e43af8e6b5266b9c790b
Jeremy Stanley <jeremy@instructure.com>
2015-11-17 12:46:37 -0700

    spec: don't leave tests trapped in March 2013
    hopefully fixes CNVS-25101
    
    Change-Id: I52350f0a1bd6a64bf1e353f4e94208d64b1d910b
    Reviewed-on: https://gerrit.instructure.com/67323
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>

--- spec/support/jwt_env.rb
+++ spec/support/jwt_env.rb
@@ -3,7 +3,6 @@ RSpec.shared_context "JWT setup" do
   let(:encryption_secret){ "jkl;jkl;jkl;jkl;jkl;jkl;jkl;jkl;" }
 
   before do
-    Timecop.freeze(Time.utc(2013,3,13,9,12))
     @preexisting_signing_secret = ENV['ECOSYSTEM_SECRET']
     @preexisting_encryption_secret = ENV['ECOSYSTEM_KEY']
     ENV['ECOSYSTEM_SECRET'] = signing_secret
@@ -13,5 +12,9 @@ RSpec.shared_context "JWT setup" do
   after do
     ENV['ECOSYSTEM_SECRET'] = @preexisting_signing_secret
     ENV['ECOSYSTEM_KEY'] = @preexisting_encryption_secret
+  end
+
+  around do |example|
+    Timecop.freeze(Time.utc(2013,3,13,9,12), &example)
   end
 end
