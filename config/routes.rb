25feb6fcefe1c9d344186e035035dde950506c76
Josh Orr <jgorr@instructure.com>
2015-11-24 10:42:41 -0600

    Add api call to retrieve assignments for specified user
    New API Endpoint:
    get 'users/:user_id/courses/:course_id/assignments'
    
    Test Plan:
    Create 'student' user
    Create 'parent' user on a different shard
    Create observer relationship between student and parent using api/token
    Enroll student in course
    Create assignment for student
    Verify the assignment can be retrieved using student id and course id in
    api call to parent's shard with the parent's token
    
    Needs unit tests
    
    Fixes MBL-5160
    
    Change-Id: I05bef55c3cf2e40df324505e056ea3487775cf0b
    Reviewed-on: https://gerrit.instructure.com/67791
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: John Corrigan <jcorrigan@instructure.com>
    Product-Review: Ben Kirato <benk@instructure.com>
    QA-Review: Ben Kirato <benk@instructure.com>

--- config/routes.rb
+++ config/routes.rb
@@ -945,6 +945,7 @@ CanvasRails::Application.routes.draw do
 
     scope(controller: :assignments_api) do
       get 'courses/:course_id/assignments', action: :index, as: 'course_assignments'
+      get 'users/:user_id/courses/:course_id/assignments', action: :user_index, as: 'user_course_assignments'
       get 'courses/:course_id/assignments/:id', action: :show, as: 'course_assignment'
       post 'courses/:course_id/assignments', action: :create
       put 'courses/:course_id/assignments/:id', action: :update
