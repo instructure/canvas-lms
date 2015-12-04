ba682914dcdd55c771f6096cb9e5df6e22f2fe69
Rob Orton <rob@instructure.com>
2015-12-03 22:04:55 -0700

    don't touch courses on sis imports
    fixes CNVS-23014
    
    test plan
     - run enrollment sis import
     - it should work like before
    
    Change-Id: I16f6f822ddbcbf1403268b399e68dca7c1f05be8
    Reviewed-on: https://gerrit.instructure.com/68198
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- spec/lib/sis/enrollment_importer_spec.rb
+++ spec/lib/sis/enrollment_importer_spec.rb
@@ -19,5 +19,28 @@ module SIS
       expect(messages).not_to be_empty
     end
 
+    it 'should skip touching courses' do
+      Timecop.freeze(2.days.ago) do
+        @c = course_model(sis_source_id: 'C001')
+        u = user_with_managed_pseudonym(sis_user_id: 'U001')
+        @e = @c.enroll_user(u)
+        @time = @c.updated_at
+      end
+
+      # this is the new way that the callback is being suspended
+      Enrollment.suspend_callbacks("(belongs_to_touch_after_save_or_destroy_for_course)") do
+        @e.save!
+      end
+      @c.reload
+      expect(@c.updated_at).to eq @time
+
+      # this is the old way that the callback was being suspended
+      Enrollment.suspend_callbacks(:belongs_to_touch_after_save_or_destroy_for_course) do
+        @e.save!
+      end
+      @c.reload
+      expect(@c.updated_at).not_to eq @time
+    end
+
   end
 end
