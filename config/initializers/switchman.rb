95fa871de8b8b01ddb6be5dc49652ec20ec73bd8
Cody Cutrer <cody@instructure.com>
2015-12-11 15:42:21 -0700

    bump switchman
    and fix extensions to Switchman::Shard to deal with it _not_ being
    reloaded
    
    Change-Id: Ia9c504594cf80c163eef3aa05b4580eb8c3a2013
    Reviewed-on: https://gerrit.instructure.com/68811
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -3,102 +3,104 @@ Rails.application.config.to_prepare do
 
   module Canvas
     module Shard
+      module ClassMethods
+        def current(category=:default)
+          if category == :delayed_jobs
+            active_shards[category] || super(:default).delayed_jobs_shard
+          else
+            super
+          end
+        end
+
+        def activate!(categories)
+          if !categories[:delayed_jobs] && categories[:default] && !@skip_delayed_job_auto_activation
+            skip_delayed_job_auto_activation do
+              categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
+            end
+          end
+          super
+        end
+
+        def skip_delayed_job_auto_activation
+          was = @skip_delayed_job_auto_activation
+          @skip_delayed_job_auto_activation = true
+          yield
+        ensure
+          @skip_delayed_job_auto_activation = was
+        end
+      end
+
+      module IncludedClassMethods
+        def birth
+          default
+        end
+      end
+
       def clear_cache
         self.class.connection.after_transaction_commit { super }
+      end
+
+      def settings
+        return {} unless self.class.columns_hash.key?('settings')
+        s = super
+        unless s.is_a?(Hash) || s.nil?
+          s = CANVAS_RAILS3 ? s.unserialize : s.unserialize(s.value)
+        end
+        if s.nil?
+          self.settings = s = {}
+        end
+
+        salt = s.delete(:encryption_key_salt)
+        secret = s.delete(:encryption_key_enc)
+        if secret || salt
+          if secret && salt
+            s[:encryption_key] = Canvas::Security.decrypt_password(secret, salt, 'shard_encryption_key')
+          end
+          self.settings = s
+        end
+
+        s
+      end
+
+      def delayed_jobs_shard
+        shard = Switchman::Shard.lookup(self.delayed_jobs_shard_id) if self.read_attribute(:delayed_jobs_shard_id)
+        shard || self.database_server.try(:delayed_jobs_shard, self)
+      end
+
+      def encrypt_settings
+        s = self.settings.dup
+        if encryption_key = s.delete(:encryption_key)
+          secret, salt = Canvas::Security.encrypt_password(encryption_key, 'shard_encryption_key')
+          s[:encryption_key_enc] = secret
+          s[:encryption_key_salt] = salt
+        end
+        if s != self.settings
+          self.settings = s
+        end
+        s
       end
     end
   end
 
   Switchman::Shard.prepend(Canvas::Shard)
+  Switchman::Shard.singleton_class.prepend(Canvas::Shard::ClassMethods)
+  Switchman::Shard.singleton_class.include(Canvas::Shard::IncludedClassMethods)
 
   Switchman::Shard.class_eval do
-    class << self
-      alias :birth :default unless instance_methods.include?(:birth)
-
-      def current_with_delayed_jobs(category=:default)
-        if category == :delayed_jobs
-          active_shards[category] || current_without_delayed_jobs(:default).delayed_jobs_shard
-        else
-          current_without_delayed_jobs(category)
-        end
-      end
-      alias_method_chain :current, :delayed_jobs
-
-      def activate_with_delayed_jobs!(categories)
-        if !categories[:delayed_jobs] && categories[:default] && !@skip_delayed_job_auto_activation
-          skip_delayed_job_auto_activation do
-            categories[:delayed_jobs] = categories[:default].delayed_jobs_shard
-          end
-        end
-        activate_without_delayed_jobs!(categories)
-      end
-      alias_method_chain :activate!, :delayed_jobs
-
-      def skip_delayed_job_auto_activation
-        was = @skip_delayed_job_auto_activation
-        @skip_delayed_job_auto_activation = true
-        yield
-      ensure
-        @skip_delayed_job_auto_activation = was
-      end
-    end
-
     self.primary_key = "id"
     reset_column_information # make sure that the id column object knows it is the primary key
 
-    # make sure settings attribute is loaded, so that on class reload we don't get into a state
-    # that it thinks it's serialized, but the data isn't set up right
-    default.is_a?(self) && default.settings
     serialize :settings, Hash
+
     # the default shard was already loaded, but didn't deserialize it
-    if default.is_a?(self)
+    if default.is_a?(self) && default.instance_variable_get(:@attributes)['settings'].is_a?(String)
       settings = ActiveRecord::AttributeMethods::Serialization::Attribute.new(serialized_attributes['settings'],
-                                                                   default.read_attribute('settings'),
+                                                                   default.instance_variable_get(:@attributes)['settings'],
                                                                    :serialized).unserialized_value
       default.settings = settings
     end
 
     before_save :encrypt_settings
-
-    def settings
-      return {} unless self.class.columns_hash.key?('settings')
-      s = super
-      unless s.is_a?(Hash) || s.nil?
-        s = CANVAS_RAILS3 ? s.unserialize : s.unserialize(s.value)
-      end
-      if s.nil?
-        self.settings = s = {}
-      end
-
-      salt = s.delete(:encryption_key_salt)
-      secret = s.delete(:encryption_key_enc)
-      if secret || salt
-        if secret && salt
-          s[:encryption_key] = Canvas::Security.decrypt_password(secret, salt, 'shard_encryption_key')
-        end
-        self.settings = s
-      end
-
-      s
-    end
-
-    def encrypt_settings
-      s = self.settings.dup
-      if encryption_key = s.delete(:encryption_key)
-        secret, salt = Canvas::Security.encrypt_password(encryption_key, 'shard_encryption_key')
-        s[:encryption_key_enc] = secret
-        s[:encryption_key_salt] = salt
-      end
-      if s != self.settings
-        self.settings = s
-      end
-      s
-    end
-
-    def delayed_jobs_shard
-      shard = Shard.lookup(self.delayed_jobs_shard_id) if self.read_attribute(:delayed_jobs_shard_id)
-      shard || self.database_server.try(:delayed_jobs_shard, self)
-    end
 
     delegate :in_current_region?, to: :database_server
 
