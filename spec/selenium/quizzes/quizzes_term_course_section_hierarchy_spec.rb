ce96d6227cf244b84d077285eec98a6b4c9418fb
Ethan Vizitei <ethan.vizitei@gmail.com>
2015-12-18 16:00:04 -0600

    spec: stabilize a selenium spec
    refs CNVS-25916
    
    the "click" function was working (in the browser)
    but was returning some value that caused the
    "keep trying" loop to go again, which meant
    that it would keep trying to find and click
    the link after the action was complete.
    
    This makes the workflow stable and passes reliably.
    
    Change-Id: I3a4018549dee201f5d434856897e3295f031a0d5
    Reviewed-on: https://gerrit.instructure.com/69233
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- spec/selenium/quizzes/quizzes_term_course_section_hierarchy_spec.rb
+++ spec/selenium/quizzes/quizzes_term_course_section_hierarchy_spec.rb
@@ -57,19 +57,23 @@ describe "quizzes section hierarchy" do
 
       it "should allow the teacher to preview the quiz", priority: "1", test_id: 282838 do
         get "/courses/#{@course.id}/quizzes"
-        keep_trying_until { fln('Test Quiz').click }
+        keep_trying_until do
+          link = fln('Test Quiz')
+          link.click if link.present?
+          link.present?
+        end
         expect_new_page_load{f('#preview_quiz_button').click}
         keep_trying_until do
          expect(f(' .quiz-header').text).to include('This is a preview of the published version of the quiz')
         end
       end
 
-    it "should work with lock and unlock dates set up", priority: "1", test_id: 323086 do
-      @override.unlock_at = Time.zone.now.advance(days:-1)
-      @override.lock_at = Time.zone.now.advance(days:4)
-      user_session(@student)
-      take_hierarchy_quiz
-    end
+      it "should work with lock and unlock dates set up", priority: "1", test_id: 323086 do
+        @override.unlock_at = Time.zone.now.advance(days:-1)
+        @override.lock_at = Time.zone.now.advance(days:4)
+        user_session(@student)
+        take_hierarchy_quiz
+      end
     end
 
     context "term end date in past" do
