f6b4b6e0e1a909f9e08a1ffbdcd8fee53a27f58e
Ethan Vizitei <ethan.vizitei@gmail.com>
2015-12-21 16:05:27 -0600

    glob for web pack bundles
    refs CNVS-25916
    
    newly added bundles are being missed.
    Look for bundles in "app/coffeescripts/bundles"
    and reject those which are not entry points.
    
    TEST PLAN:
      1) specs should pass
      2) web pack selenium build should have less failures
    
    Change-Id: I7562e4f6124a78b62cdd59151067ce31ac65e164
    Reviewed-on: https://gerrit.instructure.com/69311
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- frontend_build/bundles.js
+++ frontend_build/bundles.js
@@ -1,176 +1,35 @@
-var bundleNames = [
-  'account_admin_tools',
-  'account_authorization_configs',
-  'account_search',
-  'account_settings',
-  'account_show',
-  'account_statistics',
-  'add_assignment_group',
-  'add_course_or_user',
-  'alerts',
-  'all_courses',
-  'announcements_index',
-  'assignments',
-  'assignment_edit',
-  'assignment_index',
-  'assignment_moderation',
-  'assignment_show',
-  'attendance',
-  'calendar2',
-  'change_password',
-  'collaborations',
-  'conferences',
-  'confirm_email',
-  'content_exports',
-  'content_migration',
-  'context_modules',
-  'context_module_progressions',
-  'conversations_new',
-  'copy_course',
-  'course',
-  'course_list',
-  'course_settings',
-  'course_statistics',
-  'course_wizard',
-  'dashboard',
-  'dashboard_card',
-  'datagrid',
-  'developer_keys',
-  'discussion',
-  'discussion_topics_edit',
-  'discussion_topics_index',
-  'edit_calendar_event',
-  'edit_rubric',
-  'eportfolio',
-  'epub_exports',
-  'external_apps',
-  'external_tool_redirect',
-  'file_inline',
-  'file_not_found_bundle',
-  'file_preview',
-  'file_show',
-  'grade_summary',
-  'gradebook_history',
-  'gradebook_uploads',
-  'gradebook2',
-  'grading_standards',
-  'group_settings',
-  'groups',
-  'jobs',
-  'ldap_settings_test',
-  'learning_outcomes',
-  'legacy/add_group_category',
-  'legacy/assignments_peer_reviews',
-  'legacy/calendars_wizard_box',
-  'legacy/collaborations_forms',
-  'legacy/content_imports_files',
-  'legacy/context_media_comment',
-  'legacy/context_media_object_inline',
-  'legacy/context_roster_usage',
-  'legacy/context_roster_user',
-  'legacy/context_roster_user_services',
-  'legacy/context_undelete_item',
-  'legacy/courses_show',
-  'legacy/edit_rubric_user_index',
-  'legacy/eportfolios_wizard_box',
-  'legacy/error_form',
-  'legacy/external_content_cancel',
-  'legacy/external_content_success',
-  'legacy/external_tools_tool_show',
-  'legacy/gradebooks_grade_summary',
-  'legacy/gradebooks_student_attendance',
-  'legacy/inlined_preview',
-  'legacy/plugins_error_reporting_settings',
-  'legacy/quiz_submission',
-  'legacy/rubrics_index',
-  'legacy/rubrics_show',
-  'legacy/submissions_show_preview_media',
-  'legacy/submissions_show_preview_text',
-  'legacy/submissions_show_preview_upload',
-  'legacy/terms_index',
-  'legacy/user_outcome_results',
-  'legacy/users_admin_merge',
-  'legacy/users_index',
-  'legacy/users_registered',
-  'link_enrollment',
-  'locale',
-  'login',
-  'manage_avatars',
-  'manage_groups',
-  'manage_groups2',
-  'messages',
-  'moderate_quiz',
-  'module_sequence_footer',
-  'navigation_header',
-  'notification_preferences',
-  'oauth2_confirm',
-  'otp_login',
-  'page_views',
-  'plugins',
-  'prerequisites_lookup',
-  'profile',
-  'profile_show',
-  'question_bank',
-  'question_banks',
-  'quiz_history',
-  'quiz_show',
-  'quiz_statistics',
-  'quiz_statistics_cqs',
-  'quiz_submission_events',
-  'quizzes_bundle',
-  'quizzes_index',
-  'react_files',
-  'react_gradebook',
-  'react_groups',
-  'react_outcome_alignment',
-  'registration',
-  'registration_confirmation',
-  'roles',
-  'roster',
-  'rubric_assessment',
-  'section',
-  'select_content_dialog',
-  'self_enrollment',
-  'sis_import',
-  'smartbanner',
-  'speed_grader',
-  'student_group_dialog',
-  'styleguide',
-  'sub_accounts',
-  'submission_download',
-  'submissions',
-  'submit_assignment',
-  'syllabus',
-  'take_quiz',
-  'teacher_activity_report',
-  'theme_editor',
-  'theme_preview',
-  'user',
-  'user_lists',
-  'user_logins',
-  'user_name',
-  'user_notes',
-  'user_observees',
-  'wiki_page_edit',
-  'wiki_page_index',
-  'wiki_page_revisions',
-  'wiki_page_show',
-  'zip_file_imports'
-];
+var glob = require("glob");
 
 var entries = {};
 
-bundleNames.forEach(function(entry){
-  entries[entry] = "./app/coffeescripts/bundles/" + entry + ".coffee";
+var bundlesPattern = __dirname + "/../app/coffeescripts/bundles/**/*.coffee";
+var pluginBundlesPattern = __dirname + "/../gems/plugins/*/app/coffeescripts/bundles/*.coffee";
+var bundleNameRegexp = /\/coffeescripts\/bundles\/(.*).coffee/;
+var fileNameRegexp = /\/([^/]+)\.coffee/;
+var pluginNameRegexp = /plugins\/([^/]+)\/app/;
+
+var appBundles = glob.sync(bundlesPattern, []);
+var pluginBundles = glob.sync(pluginBundlesPattern, []);
+
+// these are bundles that are dependencies, and therefore should not be compiled
+//  as entry points (webpack won't allow that).
+// TODO: Ultimately we should move them to other directories.
+var nonEntryPoints = ['common', 'jquery_ui_menu', 'modules/account_quota_settings'];
+
+appBundles.forEach(function(entryFilepath){
+  var entryBundlePath = entryFilepath.replace(/^.*app\/coffeescripts\/bundles/, "./app/coffeescripts/bundles")
+  var entryName = bundleNameRegexp.exec(entryBundlePath)[1];
+  if(nonEntryPoints.indexOf(entryName) < 0){
+    entries[entryName] = entryBundlePath;
+  }
 });
 
+
+
+// TODO: Include this from source rather than after the ember app compilation step.
+//      This whole "compiled" folder should eventually go away
 entries['screenreader_gradebook'] = "./public/javascripts/compiled/bundles/screenreader_gradebook.js";
 
-var glob = require("glob");
-var pluginBundlesPattern = __dirname + "/../gems/plugins/*/app/coffeescripts/bundles/*.coffee";
-var pluginBundles = glob.sync(pluginBundlesPattern, []);
-var pluginNameRegexp = /plugins\/([^/]+)\/app/;
-var fileNameRegexp = /\/([^/]+)\.coffee/;
 pluginBundles.forEach(function(entryFilepath){
   var pluginName = pluginNameRegexp.exec(entryFilepath)[1];
   var fileName = fileNameRegexp.exec(entryFilepath)[1];
