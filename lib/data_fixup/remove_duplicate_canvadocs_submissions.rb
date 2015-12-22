987b6c2bb1900b40931965516161568a5963d3e4
Cameron Matheson <cameron@instructure.com>
2015-12-21 18:22:56 -0700

    make canvadocs_submissions entries unique
    closes CNVS-26089
    
    Test plan:
      * in a rails console, call
        DataFixup::CreateCanvadocsSubmissionsRecords.run
        a few times
      * run migrations
      * there should be no duplicate entries in canvadocs_submissions
      * you should not be able to create duplicate entries in
        canvadocs_submissions
    
    Change-Id: I38cd3c990c13434d5daa016f3b4d192bdc877e16
    Reviewed-on: https://gerrit.instructure.com/69327
    Tested-by: Jenkins
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Amber Taniuchi <amber@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

---
+++ lib/data_fixup/remove_duplicate_canvadocs_submissions.rb
@@ -0,0 +1,21 @@
+module DataFixup::RemoveDuplicateCanvadocsSubmissions
+  def self.run
+    %w[crocodoc_document_id canvadoc_id].each do |column|
+      duplicates = CanvadocsSubmission.
+        select("#{column}, submission_id").
+        group("#{column}, submission_id").
+        having("count(*) > 1")
+
+      duplicates.find_each do |dup|
+        scope = CanvadocsSubmission.where(
+          column => dup[column],
+          submission_id: dup.submission_id
+        )
+        keeper = scope.first
+        scope.where("id <> ?", keeper.id).delete_all
+      end
+    end
+  end
+
+  class CanvadocsSubmission < ActiveRecord::Base; end
+end
