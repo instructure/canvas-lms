d0c68246bc42027f7751a2c3852a71a1131d819c
August Thornton <august@instructure.com>
2015-12-31 12:53:15 -0700

    create admin level LTI report
    closes PLAT-701
    
    As an admin I would like to be able to tell what tools are installed
    in my Canvas instance. I want to see if there are tools we are using a
    lot or if there are tools we do not want our instructors to use.
    Ideally, I would see tool information, course information and privacy
    level (including if variable substitution is used).
    
    test plan:
      * run 'Lti Report' as an admin
        and make sure it works
      * should run in root and sub accounts
      * should be scoped to only run on current root
        account and associated sub-accounts
    
    Change-Id: I24fd1cb4c542f70d967ef34cfd46df881d239ae8
    Reviewed-on: https://gerrit.instructure.com/69622
    Tested-by: Jenkins
    Reviewed-by: Brad Horrocks <bhorrocks@instructure.com>
    Product-Review: Karl Lloyd <karl@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>

---
+++ gems/plugins/account_reports/lib/account_reports/lti_reports.rb
@@ -0,0 +1,81 @@
+#
+# Copyright (C) 2013 - 2016 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'account_reports/report_helper'
+
+module AccountReports
+
+  class LtiReports
+    include ReportHelper
+
+    def initialize(account_report)
+      @account_report = account_report
+      @account_report.parameters ||= {}
+    end
+
+    def lti_report
+      file = AccountReports.generate_file(@account_report)
+      CSV.open(file, "w") do |csv|
+
+        headers = [ 'context_type', 'context_id', 'account_name', 'course_name', 'tool_type_name',
+                    'tool_type_id', 'tool_created_at', 'privacy_level', 'launch_url', 'custom_fields' ]
+
+        csv << headers
+
+        tools = ContextExternalTool.active.
+          where("context_type = 'Account' OR context_type = 'Course'").
+          joins(
+            "LEFT OUTER JOIN #{Course.quoted_table_name} ON context_id=courses.id AND context_type='Course'",
+            "LEFT OUTER JOIN #{Account.quoted_table_name} ON context_id=accounts.id AND context_type='Account'").
+          select("context_external_tools.*, courses.name AS course_name, accounts.name AS account_name")
+          if account.root_account?
+            tools.where!("courses.root_account_id= :root OR
+                accounts.root_account_id = :root OR accounts.id = :root", {root: root_account})
+          else
+            tools.where("EXISTS (?) OR EXISTS
+              (
+                WITH RECURSIVE t AS (
+                  SELECT * FROM #{Account.quoted_table_name} WHERE id=?
+                  UNION
+                  SELECT accounts.* FROM #{Account.quoted_table_name} INNER JOIN t ON accounts.parent_account_id=t.id
+                )
+              SELECT * FROM t)",
+              CourseAccountAssociation.where("course_id=courses.id").where(account_id: account), account)
+          end
+
+        Shackles.activate(:slave) do
+          tools.find_each do |t|
+            row = []
+            row << t.context_type
+            row << t.context_id
+            row << t.account_name
+            row << t.course_name
+            row << t.name
+            row << t.tool_id
+            row << t.created_at
+            row << t.privacy_level
+            row << t.url
+            row << t.custom_fields
+            csv << row
+          end
+        end
+      end
+      send_report(file)
+    end
+  end
+end
---
+++ gems/plugins/account_reports/spec_canvas/lti_reports_spec.rb
@@ -0,0 +1,88 @@
+#
+# Copyright (C) 2013 - 2016 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require_relative 'report_spec_helper'
+
+describe 'lti report' do
+  include ReportSpecHelper
+
+  before(:each) do
+    @type = 'lti_report_csv'
+    @account = Account.create(name: 'New Account', default_time_zone: 'UTC')
+    @sub_account = Account.create(parent_account: @account, name: 'Sub Account')
+    @course = Course.create(name: 'New Course', account: @sub_account)
+
+    @t1 = ContextExternalTool.new.tap do |t|
+      t.id = 1
+      t.context_id = @account.id
+      t.context_type = 'Account'
+      t.name = 'Account Tool'
+      t.consumer_key = 'key'
+      t.shared_secret = 'secret'
+      t.tool_id = 'Vimeo'
+      t.url = 'https://launch_url.test'
+      t.save
+    end
+
+    @t2 = ContextExternalTool.new.tap do |t|
+      t.id = 2
+      t.context_id = @course.id
+      t.context_type = 'Course'
+      t.name = 'Course Tool'
+      t.consumer_key = 'key'
+      t.shared_secret = 'secret'
+      t.tool_id = 'Youtube'
+      t.url = 'https://launch_url.test'
+      t.save
+    end
+  end
+
+  it 'should run on a root account' do
+    parsed = read_report(@type, {order: 1})
+    expect(parsed.length).to eq 2
+    expect(parsed[0]).to eq([
+      @t1.context_type,
+      @t1.context_id.to_s,
+      @account.name,
+      nil,
+      @t1.name,
+      @t1.tool_id,
+      @t1.created_at.strftime("%Y-%m-%d %H:%M:%S UTC"),
+      @t1.privacy_level,
+      @t1.url,
+      nil
+    ])
+  end
+
+  it 'should run on a sub account' do
+    parsed = read_report(@type, {account: @sub_account})
+    expect(parsed.length).to eq 2
+    expect(parsed[1]).to eq([
+      @t2.context_type,
+      @t2.context_id.to_s,
+      nil,
+      @course.name,
+      @t2.name,
+      @t2.tool_id,
+      @t2.created_at.strftime("%Y-%m-%d %H:%M:%S UTC"),
+      @t2.privacy_level,
+      @t2.url,
+      nil
+    ])
+  end
+end
