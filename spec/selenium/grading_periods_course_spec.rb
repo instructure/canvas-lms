7edcc213a29791c04032900fe48b8a4ee1de03f5
Derek Bender <djbender@instructure.com>
2016-01-04 11:22:31 -0700

    spec: 🕘 💣 use relative dates for grading periods
    This fixes a problem where the hard coded values in this spec
    would eventually overlap with the relatively coded grading period
    factories. This patchset uses relative values that are outside
    of the factories.
    
    Change-Id: Id916790c0c65b33d30c01514f9ec6b2bc5e62128
    Reviewed-on: https://gerrit.instructure.com/69657
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Jon Jensen <jon@instructure.com>

--- spec/selenium/grading_periods_course_spec.rb
+++ spec/selenium/grading_periods_course_spec.rb
@@ -143,8 +143,8 @@ describe 'Course Grading Periods Inheritance' do
   include_examples 'in-process server selenium tests'
 
   let(:title) {'hi'}
-  let(:start_date) {'Feb 1'}
-  let(:end_date)  {'Feb 28'}
+  let(:start_date) { 3.months.from_now.strftime('%b %-d') }
+  let(:end_date) { (4.months.from_now - 1.day).strftime('%b %-d') }
   let(:date_time_format)  {'%b %-d, %Y at %-l:%M%P'}   # e.g. May 28, 2015 at 8:58pm
 
   before(:each) do
