8fcd732b21e3d2cf89a1adfe8f4a4bb041add308
Mark Severson <markse@instructure.com>
2016-01-04 18:47:46 -0700

    sis assignments handle only visible to overrides
    some assignments are only visible to overrides, not to an entire course,
    these must be handled specially when applying assignment overrides
    
    Added a basic spec for Api::V1::SisAssignment.  Then added a failing
    spec for 'sis assignments handle only visible to overrides', then added
    the bit of code to get the spec to pass.
    
    test plan:
     * in a course with multiple sections
     * create an assignment
     * create another assignment that is only applicable to a single section
     * query the sis assignments api:
       - /api/sis/courses/:course_id/assignments
     * the first assignment should return all sections in the course
     * the second assignment should only return the section for which it is
       applicable
    
    Fixes: SIS-1623
    
    Change-Id: Iaa34295b98ea9615df400287ffbea0d762cc48bc
    Reviewed-on: https://gerrit.instructure.com/69702
    Tested-by: Jenkins
    Reviewed-by: Brad Humphrey <brad@instructure.com>
    QA-Review: Steven Shepherd <sshepherd@instructure.com>
    Product-Review: Ken Romney <kromney@instructure.com>

---
+++ spec/lib/api/v1/sis_assignment_spec.rb
@@ -0,0 +1,99 @@
+require_relative '../../../spec_helper.rb'
+
+class SisAssignmentHarness
+  include Api::V1::SisAssignment
+end
+
+describe Api::V1::SisAssignment do
+  subject { SisAssignmentHarness.new }
+
+  context "#sis_assignments_json" do
+    let(:course_1) { course }
+    let(:assignment_1) { assignment_model(course: course) }
+    let(:assignment_with_context) { Assignment.new }
+
+    let(:assignment_override_1) do
+      assignment_override_model(assignment: assignment_1)
+    end
+
+    let(:assignment_override_2) do
+      assignment_override_model(assignment: assignment_1)
+    end
+
+    let(:assignment_overrides) do
+      assignment_override_1.stubs(:set_type).returns("CourseSection")
+      assignment_override_1.stubs(:set_id).returns(1)
+
+      assignment_override_2.stubs(:set_type).returns("CourseSection")
+      assignment_override_2.stubs(:set_id).returns(2)
+
+      [
+        assignment_override_1,
+        assignment_override_2
+      ]
+    end
+
+
+    let(:course_section_1) { CourseSection.new }
+    let(:course_section_2) { CourseSection.new }
+    let(:course_section_3) { CourseSection.new }
+
+    let(:course_sections) do
+      [
+        course_section_1,
+        course_section_2,
+        course_section_3
+      ]
+    end
+
+    before do
+      assignment_1.stubs(:locked_for?).returns(false)
+
+      assignment_with_context.stubs(:locked_for?).returns(false)
+      assignment_with_context.stubs(:context).returns(course_sections)
+      assignment_with_context.stubs(:association).with(:context).returns(course_sections)
+      assignment_with_context.stubs(:association).with(:assignment_group).returns(
+        OpenStruct.new(:loaded? => false))
+      assignment_with_context.stubs(:association).with(:active_assignment_overrides).returns(
+        OpenStruct.new(:loaded? => true))
+      assignment_with_context.stubs(:active_assignment_overrides).returns(assignment_overrides)
+      assignment_with_context.stubs(:only_visible_to_overrides).returns(true)
+
+      assignment_overrides.stubs(:loaded?).returns(true)
+
+      course_section_1.stubs(:id).returns(1)
+      course_section_2.stubs(:id).returns(2)
+      course_section_3.stubs(:id).returns(3)
+
+      course_sections.each do |course_section|
+        course_section.stubs(:nonxlist_course).returns(course_1)
+        course_section.stubs(:crosslisted?).returns(false)
+      end
+
+      course_sections.stubs(:loaded?).returns(true)
+      course_sections.stubs(:active_course_sections).returns(course_sections)
+      course_sections.stubs(:association).returns(OpenStruct.new(:loaded? => true))
+    end
+
+    it "returns an empty hash for 0 assignments" do
+      assignments = []
+      expect(subject.sis_assignments_json(assignments)).to eq([])
+    end
+
+    context "returns hash when there are no courses" do
+      let(:assignments) { [assignment_1] }
+      let(:results) { subject.sis_assignments_json(assignments) }
+      it { expect(results.size).to eq(1) }
+      it { expect(results.first["id"]).to eq(assignment_1.id) }
+    end
+
+    it "sis assignments handle only visible to overrides" do
+      assignments = [assignment_with_context]
+      result = subject.sis_assignments_json(assignments)
+      expect(result[0]['sections'].size).to eq(2)
+      expect(result[0]['sections'][0]["id"]).to eq(1)
+      expect(result[0]['sections'][1]["id"]).to eq(2)
+    end
+  end
+end
+
