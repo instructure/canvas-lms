6f4891930aa845511683d6be615dd7a15c43f02b
Sterling Cobb <sterling@instructure.com>
2016-01-07 02:52:20 -0700

    remove old sort code
    fixes CNVS-26294
    
    Removes old sorting code.
    
    Test Plan
    
    Given you have enabled the new Admin people/course search feature
    When you got to the new people/course search page
    And you click on the "Courses" tab
    Then the columns should NOT be sortable
    And when you click on the "People" tab
    Then the columns should NOT be sortable
    
    Change-Id: I6950269d7c73ca391f10599c243e3af10dc570db
    Reviewed-on: https://gerrit.instructure.com/69849
    Tested-by: Jenkins
    Reviewed-by: Jon Willesen <jonw@instructure.com>
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Sterling Cobb <sterling@instructure.com>

--- app/jsx/account_course_user_search/CoursesList.jsx
+++ app/jsx/account_course_user_search/CoursesList.jsx
@@ -7,63 +7,12 @@ define([
 
   var { number, string, func, shape, arrayOf } = React.PropTypes;
 
-  let defaultDirection = 'ASC';
   var CoursesList = React.createClass({
-    getInitialState () {
-      return {
-        sort: {
-          direction: defaultDirection,
-          property: 'name'
-        }
-      }
-    },
     propTypes: {
       courses: arrayOf(shape(CoursesListRow.propTypes)).isRequired
     },
-    sortArrow (column) {
-      let className = this.state.sort.direction == defaultDirection ? 'icon-mini-arrow-down' : 'icon-mini-arrow-up';
-
-      // Defaults to sort the name
-      if (column == this.state.sort.property) {
-        return <i className={className} />;
-      }
-    },
-    sortCourses ({property, direction}) {
-      let courses = _.sortBy(this.props.courses, (course) => {
-        return property === 'display_name' ? course.teachers.display_name : course[property];
-      });
-
-      if (direction == 'DESC') {
-        courses.reverse()
-      }
-
-      return courses;
-    },
-    toggleDirection (direction) {
-      if (direction == 'DESC') {
-        return 'ASC';
-      } else {
-        return 'DESC';
-      }
-    },
-    sort (theProperty) {
-      let {property, direction} = this.state.sort;
-
-      if (theProperty == property) { // toggle the direction? Or use a new default
-        direction = this.toggleDirection(direction);
-      } else {
-        direction = defaultDirection;
-      }
-
-      this.setState({
-        sort: {
-          direction: direction,
-          property: theProperty
-        }
-      });
-    },
     render() {
-      let courses = this.sortCourses(this.state.sort);
+      let courses = this.props.courses;
 
       return (
 
@@ -74,20 +23,18 @@ define([
                 <div className="col-xs-2">
                 </div>
                 <div className="col-xs-10" role='columnheader'>
-                  <strong>
-                    <a href="#" className="coursesHeaderLink" onClick={this.sort.bind(this, 'name')}><small>{I18n.t("Course")}</small>{this.sortArrow('name')}</a>
-                  </strong>
+                  <strong><small>{I18n.t("Course")}</small></strong>
                 </div>
               </div>
             </div>
             <div role='columnheader' className="col-xs-1">
-              <strong><a href="#" className="courseSIS" onClick={this.sort.bind(this, 'sis_course_id')}><small>{I18n.t("SIS ID")}</small>{this.sortArrow('sis_course_id')}</a></strong>
+              <strong><small>{I18n.t("SIS ID")}</small></strong>
             </div>
             <div role='columnheader' className="col-md-3">
-              <strong><a href="#" className="sortTeacherName" onClick={this.sort.bind(this, 'display_name')}><small>{I18n.t("Teacher")}</small>{this.sortArrow('display_name')}</a></strong>
+              <strong><small>{I18n.t("Teacher")}</small></strong>
             </div>
             <div role='columnheader' className="col-md-3">
-              <strong><a href="#" className="sortEnrollments" onClick={this.sort.bind(this, 'total_students')}><small>{I18n.t("Enrollments")}</small>{this.sortArrow('total_students')}</a></strong>
+              <strong><small>{I18n.t("Enrollments")}</small></strong>
             </div>
             <div role='columnheader' className="col-md-2">
               <span className='screenreader-only'>{I18n.t("Course option links")}</span>
