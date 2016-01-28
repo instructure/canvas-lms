17d79595ffa776da3da2d6fbba0d3741562a2963
Ethan Vizitei <ethan.vizitei@gmail.com>
2016-01-28 10:56:24 -0700

    glob webpack unit tests
    maintaining a list of specs manually is crummy.  This loads them
    dynamically.
    
    also solves some run-order fragileness between screenreader
    gradebook and shared ember component tests.
    
    TEST PLAN:
     1) webpack aux build should pass
    
    Change-Id: I120b0bafc3b48523e758f576babea7f4af244673
    Reviewed-on: https://gerrit.instructure.com/71082
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- spec/javascripts/webpack_ember_spec_index.js
+++ spec/javascripts/webpack_ember_spec_index.js
@@ -6,20 +6,9 @@ fixturesDiv.id = 'fixtures';
 document.body.appendChild(fixturesDiv);
 if(!window.ENV) window.ENV = {};
 
-require(__dirname + "/../../app/coffeescripts/ember/shared/tests/xhr/fetch_all_pages.spec")
-require(__dirname + "/../../app/coffeescripts/ember/shared/tests/components/ic_submission_download_dialog.spec")
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
 
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/components/assignment_group_grades.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/components/custom_column_cell.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/components/grading_cell.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/components/muter.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/controllers/screenreader_gradebook.spec")
-
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/grading_cell.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/item_selection.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/muter_component.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/settings.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/sorting.spec")
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/integration/ungraded.spec")
-
-require(__dirname + "/../../app/coffeescripts/ember/screenreader_gradebook/tests/app.spec")
+var testsContext = require.context(__dirname + "/../../app/coffeescripts", true, /\.spec$/);
+requireAll(testsContext);
