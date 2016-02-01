ab0f9be30c823d97087a227776cf786c8563d007
James Williams <jamesw@instructure.com>
2016-02-01 07:48:23 -0700

    rails 4.2: turn xss_safe mode on for json
    refs #CNVS-26056
    
    Change-Id: I1c5c8e0568d619dd7cadc20f6a3a34cab46b12fa
    Reviewed-on: https://gerrit.instructure.com/71173
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- config/initializers/json.rb
+++ config/initializers/json.rb
@@ -4,6 +4,7 @@ if CANVAS_RAILS4_0
 else
   require 'json/jwt'
   require 'oj_mimic_json' # have to load after json/jwt or else the oj_mimic_json will make it never load
+  Oj.default_options = {:escape_mode => :xss_safe}
 
   ActiveSupport::JSON::Encoding.time_precision = 0
 end
