347d8f6021fd5f764aeb2c250c5333777655de4b
Jon Jensen <jon@instructure.com>
2016-02-05 10:09:22 -0700

    spec: remove brittle/unnecessary spec
    this spec fails all the time, and selenium is overkill for it... if it's
    even worth testing, it should be done as a JS test
    
    Change-Id: I2feea6484b4bf8576bd1c772a51d82ac1d670b75
    Reviewed-on: https://gerrit.instructure.com/71593
    Tested-by: Jenkins
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- spec/selenium/quizzes/quizzes_unpublish_quiz_teacher_spec.rb
+++ spec/selenium/quizzes/quizzes_unpublish_quiz_teacher_spec.rb
@@ -5,10 +5,10 @@ describe 'unpublishing a quiz on the quiz show page' do
   include_context "in-process server selenium tests"
   include QuizzesCommon
 
-  def unpublish_quiz_via_ui(opts={})
+  def unpublish_quiz_via_ui
     get "/courses/#{@course.id}/quizzes/#{@quiz.id}"
     f('#quiz-publish-link').click
-    wait_for_ajaximations if opts.fetch(:wait_for_ajaximations, true)
+    wait_for_ajaximations
     wait_for_quiz_publish_button_to_populate
   end
 
@@ -18,51 +18,42 @@ describe 'unpublishing a quiz on the quiz show page' do
       create_quiz_with_due_date
     end
 
-    context 'before the ajax calls finish' do
-      it 'temporarily changes the button text to |Unpublishing...|', priority: "1", test_id: 401339 do
-        unpublish_quiz_via_ui(wait_for_ajaximations: false)
-        expect(fj('.publish-text', '#quiz-publish-link').text).to include_text 'Unpublishing...'
-      end
-    end
+    it 'performs all expected changes on the page', priority: "1", test_id: 401338 do
+      unpublish_quiz_via_ui
 
-    context 'after the ajax calls finish' do
-      it 'performs all expected changes on the page', priority: "1", test_id: 401338 do
-        unpublish_quiz_via_ui
+      # changes the button's text to |Unpublished|
+      driver.mouse.move_to f('#footer')
+      expect(f('#quiz-publish-link').text.strip!.split("\n")[1].split('.')[0]).to eq 'Unpublished'
 
-        # changes the button's text to |Unpublished|
-        driver.mouse.move_to f('#footer')
-        expect(f('#quiz-publish-link').text.strip!.split("\n")[1].split('.')[0]).to eq 'Unpublished'
+      # changes the button text on hover to |Publish|
+      expect(f('#quiz-publish-link').text.strip!.split("\n")[0]).to eq 'Publish'
 
-        # changes the button text on hover to |Publish|
-        expect(f('#quiz-publish-link').text.strip!.split("\n")[0]).to eq 'Publish'
+      # displays the 'This quiz is unpublished' message
+      expect(fj('.unpublished_warning', '.alert')).to be_displayed
 
-        # displays the 'This quiz is unpublished' message
-        expect(fj('.unpublished_warning', '.alert')).to be_displayed
+      # removes links from the right sidebar
+      links_text = []
+      ffj('li', 'ul.page-action-list').each { |link| links_text << link.text }
+      expect(links_text.size).to eq 0
 
-        # removes links from the right sidebar
-        links_text = []
-        ffj('li', 'ul.page-action-list').each { |link| links_text << link.text }
-        expect(links_text.size).to eq 0
+      # removes the |Take the Quiz| button
+      expect(f('#take_quiz_link')).to be_nil
 
-        # removes the |Take the Quiz| button
-        expect(f('#take_quiz_link')).to be_nil
+      # shows pre-published options when clicking the cog menu tool
+      fj('a.al-trigger', '.header-group-right').click
+      wait_for_ajaximations
 
-        # shows pre-published options when clicking the cog menu tool
-        fj('a.al-trigger', '.header-group-right').click
-        wait_for_ajaximations
+      items = ffj('li.ui-menu-item', 'ul#toolbar-1')
+      items_text = []
+      items.each { |i| items_text << i.text.split("\n")[0] }
 
-        items = ffj('li.ui-menu-item', 'ul#toolbar-1')
-        items_text = []
-        items.each { |i| items_text << i.text.split("\n")[0] }
+      expect(items_text).to include 'Show Rubric'
+      expect(items_text).to include 'Lock this Quiz Now'
+      expect(items_text).to include 'Delete'
 
-        expect(items_text).to include 'Show Rubric'
-        expect(items_text).to include 'Lock this Quiz Now'
-        expect(items_text).to include 'Delete'
-
-        expect(items_text).to_not include 'Preview'
-        expect(items_text).to_not include 'Show Student Quiz Results'
-        expect(items_text).to_not include 'Message Students Who...'
-      end
+      expect(items_text).to_not include 'Preview'
+      expect(items_text).to_not include 'Show Student Quiz Results'
+      expect(items_text).to_not include 'Message Students Who...'
     end
   end
 end
