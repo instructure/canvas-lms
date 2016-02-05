0b3eb59b7418bab01b89812dd425b2fdede3a791
Nick Houle <nhoule@instructure.com>
2016-02-05 11:23:54 -0700

    Allow assignment override validation to validate properly
    Fixes: SIS-1505
    
    Test plan:
    - Create a valid PowerSchool Agent locally in sis_app
    - Connect the agent to your local Canvas instance via the agents' token
    - Run a sync job on the agent to provision data into Canvas
    - In a course create multiple scenarios of differentiated assignments
    - Go to the gradebook of the course and confirm the different scenarios
    of the differentiated assignments are validating correctly.
    - Post grades from the gradebook and confirm the assignments and grades
    have successfully been passed back via the PowerTeacher gradebook for
    that specific course.
    
    Change-Id: I020e3948ef0507fc3729805c400ff097ebbf8e10
    Reviewed-on: https://gerrit.instructure.com/71608
    Tested-by: Jenkins
    QA-Review: Steven Shepherd <sshepherd@instructure.com>
    Reviewed-by: Ken Romney <kromney@instructure.com>
    Product-Review: Ken Romney <kromney@instructure.com>

--- app/jsx/gradebook/SISGradePassback/PostGradesStore.jsx
+++ app/jsx/gradebook/SISGradePassback/PostGradesStore.jsx
@@ -34,6 +34,25 @@ define([
           null;
       },
 
+      allOverrideIds(a) {
+        var overrides = []
+        _.each(a.overrides, (o) => {
+          overrides.push(o.course_section_id)
+        })
+        return overrides
+      },
+
+      overrideForEveryone(a) {
+        var overrides = this.allOverrideIds(a)
+        var sections = _.keys(this.getState().sections)
+        var section_ids_with_no_overrides = $(sections).not(overrides).get();
+
+        var section_for_everyone = _.find(section_ids_with_no_overrides, (o) => {
+          return state.selected.id == o
+        });
+        return section_for_everyone
+      },
+
       setGradeBookAssignments (gradebookAssignments) {
         var assignments = []
         for (var id in gradebookAssignments) {
@@ -57,14 +76,47 @@ define([
         this.setSelectedSection( this.getState().sectionToShow )
       },
 
+      validCheck(a) {
+        if(a.overrideForThisSection != undefined && a.currentlySelected.type == 'course' && a.currentlySelected.id.toString() == a.overrideForThisSection.course_section_id){
+          return a.due_at != null ? true : false
+        }
+        else if(a.overrideForThisSection != undefined && a.currentlySelected.type == 'section' && a.currentlySelected.id.toString() == a.overrideForThisSection.course_section_id){
+          return a.overrideForThisSection.due_at != null ? true : false
+        }
+        else{
+          return true
+        }
+      },
+
       getAssignments() {
         var assignments = this.getState().assignments
-        if (this.getState().selected.type == "section") {
+        var state = this.getState()
+        if (state.selected.type == "section") {
           _.each(assignments, (a) => {
             a.recentlyUpdated = false
+            a.currentlySelected = state.selected
+            a.sectionCount = _.keys(state.sections).length
             a.overrideForThisSection = _.find(a.overrides, (override) => {
-              return override.course_section_id == this.getState().selected.id;
+              return override.course_section_id == state.selected.id;
             });
+
+            //Handle assignment with overrides and the 'Everyone Else' scenario with a section that does not have any overrides
+            //cleanup overrideForThisSection logic
+            if(a.overrideForThisSection == undefined){ a.selectedSectionForEveryone = this.overrideForEveryone(a) }
+          });
+        } else {
+          _.each(assignments, (a) => {
+            a.recentlyUpdated = false
+            a.currentlySelected = state.selected
+            a.sectionCount = _.keys(state.sections).length
+
+            //Course is currentlySlected with sections that have overrides AND are invalid
+            a.overrideForThisSection = _.find(a.overrides, (override) => {
+              return override.due_at == null || typeof(override.due_at) == 'object';
+            });
+
+            //Handle assignment with overrides and the 'Everyone Else' scenario with the course currentlySelected
+            if(a.overrideForThisSection == undefined){ a.selectedSectionForEveryone = this.overrideForEveryone(a) }
           });
         }
         return assignments;
@@ -104,22 +156,41 @@ define([
       },
 
       updateAssignmentDate(assignment_id, date){
-        var assignment = this.getAssignment(assignment_id)
-        if(assignment.overrideForThisSection != undefined) {
+        var assignments = this.getState().assignments
+        var assignment = _.find(assignments, (a) => a.id == assignment_id)
+        //the assignment has an override and the override being updated is for the section that is currentlySelected update it
+        if(assignment.overrideForThisSection != undefined && assignment.currentlySelected.id.toString() == assignment.overrideForThisSection.course_section_id) {
           assignment.overrideForThisSection.due_at = date
           assignment.please_ignore = false
-          assignment.recentlyUpdated = true
-          this.updateAssignment(assignment_id, assignment)
+          assignment.hadOriginalErrors = true
+
+          this.setState({assignments: assignments})
         }
+
+        //the section override being set from the course level of the sction dropdown
+        else if(assignment.overrideForThisSection != undefined && assignment.currentlySelected.id.toString() != assignment.overrideForThisSection.course_section_id){
+          assignment.overrideForThisSection.due_at = date
+          assignment.please_ignore = false
+          assignment.hadOriginalErrors = true
+
+          this.setState({assignments: assignments})
+        }
+
+        //update normal assingment and the 'Everyone Else' scenario if the course is currentlySelected
         else {
-          this.updateAssignment(assignment_id, {due_at: date, please_ignore: false})
+          this.updateAssignment(assignment_id, {due_at: date, please_ignore: false, hadOriginalErrors: true})
         }
+      },
+
+      assignmentOverrideOrigianlErrorCheck(a) {
+        a.hadOriginalErrors = a.hadOriginalErrors == true
       },
 
       saveAssignments () {
         var assignments = assignmentUtils.withOriginalErrorsNotIgnored(this.getAssignments())
         var course_id = this.getState().course.id
         _.each(assignments, (a) => {
+          this.assignmentOverrideOrigianlErrorCheck(a)
           assignmentUtils.saveAssignmentToCanvas(course_id, a)
         });
       },
