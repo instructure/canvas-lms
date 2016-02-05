de98b8896edd3fa790af0b06c2c87e7404dd2ac7
Cody Cutrer <cody@instructure.com>
2016-02-05 09:31:41 -0700

    add comment reminder to cache clearing migration
    Change-Id: Ibd9ad824bbf64223651b75768b026bd9e033019f
    Reviewed-on: https://gerrit.instructure.com/71585
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20151228174629_clear_rails_cache.rb
+++ db/migrate/20151228174629_clear_rails_cache.rb
@@ -1,6 +1,10 @@
 class ClearRailsCache < ActiveRecord::Migration
   tag :predeploy, :postdeploy
 
+  # note that if you have any environments that are "split" somehow -
+  # sharing a database, or created from a snapshot of a database,
+  # or have non-connected cache servers - you'll need to manually
+  # clear the cache in each of them.
   def up
     Rails.cache.clear if Shard.current.default?
   end
