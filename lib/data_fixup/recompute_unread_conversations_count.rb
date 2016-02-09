da57843b6cdf479b76756b3abf7cc83b1f8accbf
Cody Cutrer <cody@instructure.com>
2016-02-09 11:30:58 -0700

    fix unread conversations count data fixup for qualified table names
    refs CNVS-26480
    
    Change-Id: I8567f11ef6eae2bd88f1138d1c2eea640dd678ef
    Reviewed-on: https://gerrit.instructure.com/71836
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/data_fixup/recompute_unread_conversations_count.rb
+++ lib/data_fixup/recompute_unread_conversations_count.rb
@@ -3,9 +3,9 @@ module DataFixup::RecomputeUnreadConversationsCount
     # Include "last_message_at IS NOT NULL" to prevent it from counting unread deleted messages.
     User.find_ids_in_batches do |ids|
       User.connection.execute(User.send(:sanitize_sql_array, [<<-SQL, ids]))
-        UPDATE users u SET unread_conversations_count = (
+        UPDATE #{User.quoted_table_name} u SET unread_conversations_count = (
           SELECT COUNT(*)
-          FROM conversation_participants p
+          FROM #{ConversationParticipant.quoted_table_name} p
           WHERE p.workflow_state = 'unread'
             AND p.user_id = u.id
             AND p.last_message_at IS NOT NULL
