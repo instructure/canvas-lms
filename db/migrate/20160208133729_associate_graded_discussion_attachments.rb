d6118fa76b83debc6a115e19d1cdd692860e26ef
James Williams <jamesw@instructure.com>
2016-02-16 14:14:26 -0700

    add n_strand to graded discussion attachment datafixup
    refs #CNVS-27114
    
    Change-Id: I77ab25440570d079f7887851ecda99c38363140b
    Reviewed-on: https://gerrit.instructure.com/72277
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Rob Orton <rob@instructure.com>

--- db/migrate/20160208133729_associate_graded_discussion_attachments.rb
+++ db/migrate/20160208133729_associate_graded_discussion_attachments.rb
@@ -2,7 +2,8 @@ class AssociateGradedDiscussionAttachments < ActiveRecord::Migration
   tag :postdeploy
 
   def up
-    DataFixup::AssociateGradedDiscussionAttachments.send_later_if_production(:run)
+    DataFixup::AssociateGradedDiscussionAttachments.send_later_if_production_enqueue_args(:run,
+      priority: Delayed::LOW_PRIORITY, n_strand: 'long_datafixups')
   end
 
   def down
