f5176675719d2641b2e87b3eca260dbb602e264b
James Williams <jamesw@instructure.com>
2016-02-19 07:45:15 -0700

    fix to-do list caching over soft-conclusion dates
    i think this problem is really that it can cache
     a user's active enrollments past the course's
     conclusion dates
    
    closes #CNVS-25957
    
    Change-Id: Iedb2d7dc2d235ca82b4f3580a2334e5063eb7f81
    Reviewed-on: https://gerrit.instructure.com/72564
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1781,7 +1781,7 @@ class User < ActiveRecord::Base
 
   def participating_enrollments
     @participating_enrollments ||= self.shard.activate do
-      Rails.cache.fetch([self, 'participating_enrollments'].cache_key) do
+      Rails.cache.fetch([self, 'participating_enrollments', :expires_in => 1.hour].cache_key) do
         self.cached_current_enrollments(:preload_dates => true).select(&:participating?)
       end
     end
