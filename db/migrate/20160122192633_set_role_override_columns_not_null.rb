6a5de092b358206760df6149f50ad42fbcc71351
Cody Cutrer <cody@instructure.com>
2016-02-22 10:33:05 -0700

    fix role override nullness migration
    have to leave back-compats in for rows that still have enabled=null, because
    it accidentally meant to inherit (instead of always being interpreted as false)
    
    Change-Id: I8877fbac208d638582b975497e66fe8ffa3db420
    Reviewed-on: https://gerrit.instructure.com/72696
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20160122192633_set_role_override_columns_not_null.rb
+++ db/migrate/20160122192633_set_role_override_columns_not_null.rb
@@ -2,8 +2,15 @@ class SetRoleOverrideColumnsNotNull < ActiveRecord::Migration
   tag :postdeploy
 
   def self.up
-    RoleOverride.where(enabled: nil).update_all(enabled: false)
+    RoleOverride.where(enabled: nil).preload(:context, :role).find_each do |ro|
+      # can't simply set to false, since it was conflated with being for inheritance
+      # in RoleOverride.permission_for
+      ro.enabled = RoleOverride.enabled_for?(ro.context, ro.permission.to_sym, ro.role).include?(:self)
+      ro.save!
+    end
+
     RoleOverride.where(locked: nil).update_all(locked: false)
+
     change_column :role_overrides, :enabled, :bool, default: true, null: false
     change_column :role_overrides, :locked, :bool, default: false, null: false
   end
