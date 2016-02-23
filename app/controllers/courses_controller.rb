5619c49c9f9749dc106050ed2f057a0c7d2da6d0
James Williams <jamesw@instructure.com>
2016-02-23 07:18:30 -0700

    don't show inactive students as available for collaborations
    test plan:
    * enable collaborations
    * create a course with an active user and an inactive one
    * in the course collaborations page, add a collaboration
    * in the "Collaborate With" section, the inactive user
     should not be listed
    
    closes #CNVS-27440
    
    Change-Id: Ie58344d851e348fd790d6d6e7b6452e53cd0d547
    Reviewed-on: https://gerrit.instructure.com/72782
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
    Product-Review: Matt Goodwin <mattg@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -802,7 +802,8 @@ class CoursesController < ApplicationController
       params[:per_page] ||= params[:limit]
 
       search_params = params.slice(:search_term, :enrollment_role, :enrollment_role_id, :enrollment_type, :enrollment_state)
-      include_inactive = @context.grants_right?(@current_user, session, :read_as_admin)
+      include_inactive = @context.grants_right?(@current_user, session, :read_as_admin) && (!params.has_key?(:include_inactive) || value_to_boolean(params[:include_inactive]))
+
       search_params[:include_inactive_enrollments] = true if include_inactive
       search_term = search_params[:search_term].presence
 
