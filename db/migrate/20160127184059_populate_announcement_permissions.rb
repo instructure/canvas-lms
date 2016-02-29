7d753f502a4312803c6b5ae3e5e8d67a030ec3a2
James Williams <jamesw@instructure.com>
2016-02-29 08:32:07 -0700

    fix :read_announcement role override population
    closes #CNVS-27475
    
    Change-Id: I68bbab410a0c2dbb867edceb424496155319f290
    Reviewed-on: https://gerrit.instructure.com/73227
    Reviewed-by: Alex Boyd <aboyd@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- db/migrate/20160127184059_populate_announcement_permissions.rb
+++ db/migrate/20160127184059_populate_announcement_permissions.rb
@@ -2,7 +2,7 @@ class PopulateAnnouncementPermissions < ActiveRecord::Migration
   tag :postdeploy
 
   def up
-    DataFixup::AddRoleOverridesForNewPermission.send_later_if_production(:run, :view_forum, :view_announcements)
+    DataFixup::AddRoleOverridesForNewPermission.send_later_if_production(:run, :read_forum, :read_announcements)
   end
 
   def down
