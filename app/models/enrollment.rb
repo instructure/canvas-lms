3bf51b246ef0926eea864dfd7ef0db3fe96eaed3
James Williams <jamesw@instructure.com>
2016-03-02 09:18:20 -0700

    cancel future appointments when enrollment is deleted
    test plan:
    * as a teacher, create a scheduler appointment group
    * as a student, sign up for one of the slots
    * remove the student from the course
    * the slot should be free
    
    closes #CNVS-27376
    
    Change-Id: I098a8c8ad7b534d86a4f7be390e49d203ccbd4e1
    Reviewed-on: https://gerrit.instructure.com/73483
    QA-Review: Heath Hales <hhales@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -450,7 +450,7 @@ class Enrollment < ActiveRecord::Base
   end
 
   def cancel_future_appointments
-    if workflow_state_changed? && completed?
+    if workflow_state_changed? && %w{completed deleted}.include?(workflow_state)
       course.appointment_participants.active.current.for_context_codes(user.asset_string).update_all(:workflow_state => 'deleted')
     end
   end
