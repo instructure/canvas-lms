64bb692a70c59e39e3098e2654db52364d001b7c
James Williams <jamesw@instructure.com>
2016-03-03 08:46:23 -0700

    don't record enrollment activity for activity_stream_summary
    calls from new ui dashboard shouldn't count
    
    closes #CNVS-27723
    
    Change-Id: I904fcdff9728f0ba89b02821e897fed0990563ba
    Reviewed-on: https://gerrit.instructure.com/73629
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -292,7 +292,7 @@ class CoursesController < ApplicationController
   before_filter :require_user, :only => [:index]
   before_filter :require_user_or_observer, :only=>[:user_index]
   before_filter :require_context, :only => [:roster, :locks, :create_file, :ping]
-  skip_after_filter :update_enrollment_last_activity_at, only: [:enrollment_invitation]
+  skip_after_filter :update_enrollment_last_activity_at, only: [:enrollment_invitation, :activity_stream_summary]
 
   include Api::V1::Course
   include Api::V1::Progress
