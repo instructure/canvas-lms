30531fd658a15a80fd5b52eb4fe26c7276c5289f
Jon Jensen <jon@instructure.com>
2016-03-09 13:48:06 -0700

    spec: fix slow/brittle spec
    this spec fails at least once on ~40% of builds
    
    * don't call `add_announcement`, use a factory instead (cuz we already test
      it in the other spec)
    * just replace content so we don't deal w/ timing / caret position issues
      that cause the new text to go at the beginning
    * delete an unused file and other cruft
    
    Change-Id: I2f226ea63903a15d4ab5a10d613daf4862a9d9b4
    Reviewed-on: https://gerrit.instructure.com/74156
    Reviewed-by: Tyson Brown <tbrown@instructure.com>
    Tested-by: Jenkins
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- spec/selenium/admin/admin_settings_announcements_spec.rb
+++ spec/selenium/admin/admin_settings_announcements_spec.rb
@@ -1,60 +1,57 @@
-require File.expand_path(File.dirname(__FILE__) + '/../common')
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/external_tools_common')
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/basic/settings_specs')
+require_relative '../common'
 
 describe "settings tabs" do
-  def add_announcement
-    f("#tab-announcements-link").click
-    fj(".element_toggler:visible").click
-    subject = "This is a date change"
-    f("#account_notification_subject").send_keys(subject)
-    f("#account_notification_icon .calendar").click
-
-    ff("#add_notification_form .ui-datepicker-trigger")[0].click
-    f(".ui-datepicker-next").click
-    fln("1").click
-    ff("#add_notification_form .ui-datepicker-trigger")[1].click
-    f(".ui-datepicker-next").click
-    fln("15").click
-
-    type_in_tiny "textarea", "this is a message"
-    yield if block_given?
-    submit_form("#add_notification_form")
-    wait_for_ajax_requests
-    notification = AccountNotification.first
-    expect(notification.message).to include_text("this is a message")
-    expect(notification.subject).to include_text(subject)
-    expect(notification.start_at.day).to eq 1
-    expect(notification.end_at.day).to eq 15
-    login_text = f("#header .user_name").text
-    expect(f("#tab-announcements .announcement-details").text).to include_text(login_text)
-    expect(f("#tab-announcements .notification_subject").text).to eq subject
-    expect(f("#tab-announcements .notification_message").text).to eq "this is a message"
-  end
-
-  def edit_announcement
-    notification = AccountNotification.first
-    f("#notification_edit_#{notification.id}").click
-    new_text = "...edited"
-    f("#account_notification_subject_#{notification.id}").send_keys(new_text)
-    f("#account_notification_icon .warning").click
-    type_in_tiny("textarea", new_text)
-    f(".account_notification_role_cbx").click
-    ff(".edit_notification_form .ui-datepicker-trigger")[0].click
-    fln("2").click
-    ff(".edit_notification_form .ui-datepicker-trigger")[1].click
-    fln("16").click
-    f("#edit_notification_form_#{notification.id}").submit
-  end
-
   context "announcements tab" do
-    include_examples "external tools tests"
-    before(:each) do
+    include_context "in-process server selenium tests"
+
+    def add_announcement
+      f("#tab-announcements-link").click
+      fj(".element_toggler:visible").click
+      subject = "This is a date change"
+      f("#account_notification_subject").send_keys(subject)
+      f("#account_notification_icon .calendar").click
+
+      ff("#add_notification_form .ui-datepicker-trigger")[0].click
+      f(".ui-datepicker-next").click
+      fln("1").click
+      ff("#add_notification_form .ui-datepicker-trigger")[1].click
+      f(".ui-datepicker-next").click
+      fln("15").click
+
+      type_in_tiny "textarea", "this is a message"
+      yield if block_given?
+      submit_form("#add_notification_form")
+      wait_for_ajax_requests
+      notification = AccountNotification.first
+      expect(notification.message).to include_text("this is a message")
+      expect(notification.subject).to include_text(subject)
+      expect(notification.start_at.day).to eq 1
+      expect(notification.end_at.day).to eq 15
+      login_text = f("#header .user_name").text
+      expect(f("#tab-announcements .announcement-details").text).to include_text(login_text)
+      expect(f("#tab-announcements .notification_subject").text).to eq subject
+      expect(f("#tab-announcements .notification_message").text).to eq "this is a message"
+    end
+
+    def edit_announcement(notification)
+      f("#notification_edit_#{notification.id}").click
+      replace_content f("#account_notification_subject_#{notification.id}"), "edited subject"
+      f("#account_notification_icon .warning").click
+      type_in_tiny("textarea", "edited message", clear: true)
+      f(".account_notification_role_cbx").click
+      ff(".edit_notification_form .ui-datepicker-trigger")[0].click
+      fln("2").click
+      ff(".edit_notification_form .ui-datepicker-trigger")[1].click
+      fln("16").click
+      f("#edit_notification_form_#{notification.id}").submit
+    end
+
+    before do
       course_with_admin_logged_in
-      get "/accounts/#{Account.default.id}/settings"
     end
 
     it "should add and delete an announcement" do
+      get "/accounts/#{Account.default.id}/settings"
       add_announcement
       f(".delete_notification_link").click
       accept_alert
@@ -63,11 +60,13 @@ describe "settings tabs" do
     end
 
     it "should edit an announcement" do
-      add_announcement
-      edit_announcement
-      notification = AccountNotification.first
-      expect(notification.subject).to eq "This is a date change...edited"
-      expect(notification.message).to eq "<p>this is a message...edited</p>"
+      notification = account_notification(user: @user)
+      get "/accounts/#{Account.default.id}/settings"
+      f("#tab-announcements-link").click
+      edit_announcement(notification)
+      notification.reload
+      expect(notification.subject).to eq "edited subject"
+      expect(notification.message).to eq "<p>edited message</p>"
       expect(notification.icon).to eq "warning"
       expect(notification.account_notification_roles.count).to eq 1
       expect(notification.start_at.day).to eq 2
