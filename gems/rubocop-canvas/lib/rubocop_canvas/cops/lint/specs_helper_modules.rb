adcbe37d5081306668c30a0964cf0f20b050941f
Landon Wilkins <lwilkins@instructure.com>
2016-03-09 14:24:10 -0700

    linter: fix helper module linter
    Change-Id: I8beee4ccb1bce9d05383327841b5c512233c70da
    Reviewed-on: https://gerrit.instructure.com/74162
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>
    Tested-by: Jenkins

--- gems/rubocop-canvas/lib/rubocop_canvas/cops/lint/specs_helper_modules.rb
+++ gems/rubocop-canvas/lib/rubocop_canvas/cops/lint/specs_helper_modules.rb
@@ -6,6 +6,7 @@ module RuboCop
 
         MSG = "Define all helper and factory methods within modules (or `shared_context`). Otherwise they will live on Object and potentially wreak havoc on other specs."
         SPEC_DIR_REGEX = /\/spec\//
+        WHITELISTED_BLOCKS = %i[context describe shared_context shared_examples]
 
         def on_def(node)
           return unless top_level_spec_def?(node)
@@ -17,7 +18,11 @@ module RuboCop
         def top_level_spec_def?(node)
           return false unless file_path =~ SPEC_DIR_REGEX
           return false unless node.def_type?
-          return false if node.ancestors.any?(&:module_type?)
+          return false if node.ancestors.any? do |ancestor|
+            ancestor.module_type? || ancestor.class_type? ||
+              ancestor.type == :block &&
+                WHITELISTED_BLOCKS.include?(ancestor.method_name)
+          end
           true
         end
       end
