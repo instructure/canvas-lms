13380ac8fbccb07c1f4ce0795ccc4c1f8f95c539
Ethan Vizitei <ethan.vizitei@gmail.com>
2016-03-14 17:11:57 -0600

    fix multi-textarea bug in global annoucements
    fixes CNVS-28006
    
    TEST PLAN
     1) load global announcements with RCE flag enabled
     2) rce editor should load
     3) should not break functionality of Global Announcements
    
    Change-Id: I3ae8989719646fab4cf3359bdba452f900ef80a3
    Reviewed-on: https://gerrit.instructure.com/74476
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- public/javascripts/account_settings.js
+++ public/javascripts/account_settings.js
@@ -1,8 +1,8 @@
 define([
   'i18n!account_settings',
   'jquery', // $
-  'jsx/shared/rce/RichContentEditor',
   'tinymce.config',
+  'global_announcements',
   'jquery.ajaxJSON', // ajaxJSON
   'jquery.instructure_date_and_time', // date_field, time_field, datetime_field, /\$\.datetime/
   'jquery.instructure_forms', // formSubmit, getFormData, validateForm
@@ -15,12 +15,7 @@ define([
   'vendor/date', // Date.parse
   'vendor/jquery.scrollTo', // /\.scrollTo/
   'jqueryui/tabs' // /\.tabs/
-], function(I18n, $, RichContentEditor, EditorConfig) {
-
-  // optimization so user isn't waiting on RCS to
-  // respond when they hit announcements
-  richContentEditor = new RichContentEditor({riskLevel: "basic"})
-  richContentEditor.preloadRemoteModule()
+], function(I18n, $, EditorConfig, globalAnnouncements) {
 
   EditorConfig.prototype.balanceButtonsOverride = function(instructure_buttons) {
     var instBtnGroup = "table,instructure_links,unlink" + instructure_buttons;
@@ -85,73 +80,9 @@ define([
     });
     $(".datetime_field").datetime_field();
 
-    $(".add_notification_toggle_focus").click(function() {
-      var aria_expanded = $('add_notification_form').attr('aria-expanded') === "true";
-      if(!aria_expanded) {
-        setTimeout(function() {$('#account_notification_subject').focus()}, 100);
-      }
-    });
+    globalAnnouncements.augmentView()
+    globalAnnouncements.bindDomEvents()
 
-    $(".edit_notification_toggle_focus").click(function() {
-      var id = $(this).attr('data-edit-toggle-id');
-      var form_id = '#edit_notification_form_' + id;
-      var aria_expanded = $(form_id).attr('aria-expanded') === "true";
-      if(!aria_expanded) {
-        setTimeout(function() {$('#account_notification_subject_' + id).focus()}, 100);
-      }
-    });
-
-    $(".add_notification_cancel_focus").click(function() {
-      $("#add_announcement_button").focus();
-    });
-
-    $(".edit_cancel_focus").click(function() {
-      var id = $(this).attr('data-cancel-focus-id');
-      $("#notification_edit_" + id).focus();
-    });
-
-    $("#add_notification_form textarea").width('100%');
-    richContentEditor.loadNewEditor($("textarea.edit_notification_form, #add_notification_form textarea"))
-
-    $("#add_notification_form, .edit_notification_form").submit(function(event) {
-      var $this = $(this);
-      var $confirmation = $this.find('#confirm_global_announcement:visible:not(:checked)');
-      if ($confirmation.length > 0) {
-        $confirmation.errorBox(I18n.t('confirms.global_announcement', "You must confirm the global announcement"));
-        return false;
-      }
-      var validations = {
-        object_name: 'account_notification',
-        required: ['start_at', 'end_at', 'subject', 'message'],
-        date_fields: ['start_at', 'end_at'],
-        numbers: []
-      };
-      if ($this[0].id == 'add_notification_form' && $('#account_notification_months_in_display_cycle').length > 0) {
-        validations.numbers.push('months_in_display_cycle');
-      }
-      var result = $this.validateForm(validations);
-      if(!result) {
-        return false;
-      }
-    });
-    $("#account_notification_required_account_service").click(function(event) {
-      $this = $(this);
-      $("#confirm_global_announcement_field").showIf(!$this.is(":checked"));
-      $("#account_notification_months_in_display_cycle").prop("disabled", !$this.is(":checked"));
-    });
-    $(".delete_notification_link").click(function(event) {
-      event.preventDefault();
-      var $link = $(this);
-      $link.parents("li").confirmDelete({
-        url: $link.attr('rel'),
-        message: I18n.t('confirms.delete_announcement', "Are you sure you want to delete this announcement?"),
-        success: function() {
-          $(this).slideUp(function() {
-            $(this).remove();
-          });
-        }
-      });
-    });
     $("#account_settings_tabs").tabs().show();
     $(".add_ip_filter_link").click(function(event) {
       event.preventDefault();
---
+++ spec/coffeescripts/globalAnnouncementsSpec.coffee
@@ -0,0 +1,26 @@
+define [
+  'jquery'
+  'helpers/fakeENV'
+  'global_announcements'
+  'jsx/shared/rce/serviceRCELoader'
+], ($, fakeENV, globalAnnouncements, serviceRCELoader)->
+
+  module "GlobalAnnouncements",
+    setup: ->
+      fakeENV.setup()
+      ENV.RICH_CONTENT_SERVICE_ENABLED = true
+      @loadOnTargetStub = sinon.stub(serviceRCELoader, "loadOnTarget")
+      globalAnnouncements.refreshEditor()
+
+    teardown: ->
+      serviceRCELoader.loadOnTarget.restore()
+      $("#fixtures").empty()
+      fakeENV.teardown()
+
+  test "loads an editor for every matching node", ->
+    html = "<textarea id='a1' class='edit_notification_form'>Announcement 1</textarea>" +
+           "<textarea id='a2' class='edit_notification_form'>Announcement 2</textarea>" +
+           "<form id='add_notification_form'><textarea id='a3'></textarea></form>"
+    $(html).appendTo("#fixtures")
+    globalAnnouncements.augmentView()
+    ok(@loadOnTargetStub.calledThrice)
