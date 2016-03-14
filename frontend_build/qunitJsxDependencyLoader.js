e1a19ff4873ce612e4363c13e02ad59678164999
Clay Diffrient <cdiffrient@instructure.com>
2016-03-14 11:07:36 -0600

    Fix qunit jsx dependency loader regex
    This makes it so that multi-line define statements work with
    the qunit dep loader for jsx.
    
    Prior to this commit
    ```
      define([
        'jsx/account_course_user_search/reducers/rootReducer'
      ], (reducer) => {
    ```
    
    is not properly transformed, however
    
    ```
    define(['jsx/account_course_user_search/reducers/rootReducer'], (reducer) => {
    ```
    
    does get transformed properly.
    
    Effectively, this replaces the `.` regex operator which doesn't transend
    newlines with `[\s\S]` which effectively does the same thing
    including newlines as well though.
    
    Change-Id: Ife96438695997050fbdf866b7c319fa1b075875a
    Reviewed-on: https://gerrit.instructure.com/74419
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Tested-by: Jenkins
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>

--- frontend_build/qunitJsxDependencyLoader.js
+++ frontend_build/qunitJsxDependencyLoader.js
@@ -21,14 +21,14 @@ module.exports = function(source){
   });
 
   // add a qunit reference in the AMD callback to capture the qunit dependency
-  newSource = newSource.replace(/define.*\],\s*\(.*[a-zA-Z].*\)\s*=>/, function(match){
+  newSource = newSource.replace(/define[\s\S]*\],\s*\(.*[a-zA-Z].*\)\s*=>/, function(match){
     return match.replace(/\],\s*\(/, function(innerMatch){
       return innerMatch + "qunit,";
     });
   });
 
   // don't want the comma if the list is empty
-  newSource = newSource.replace(/define.*\],\s*\(\s*\)\s*=>/, function(match){
+  newSource = newSource.replace(/define[\s\S]*\],\s*\(\s*\)\s*=>/, function(match){
     return match.replace(/\],\s*\(/, function(innerMatch){
       return innerMatch + "qunit";
     });
