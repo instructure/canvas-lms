b391a163e022ffc852c2c46c926a15de650e189c
MFairbourn <mfairbourn@instructure.com>
2016-03-14 20:00:05 -0600

    spec: fix map strip issue in discussion specs
    test plan:
    - specs pass
    - code review
    
    Change-Id: I2c1c259168137cc2d0c5664f7c64f78ff12e80dd
    Reviewed-on: https://gerrit.instructure.com/74484
    Tested-by: Jenkins
    Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
    Product-Review: Landon Wilkins <lwilkins@instructure.com>
    QA-Review: Landon Wilkins <lwilkins@instructure.com>

--- spec/selenium/discussion_group_submit_spec.rb
+++ spec/selenium/discussion_group_submit_spec.rb
@@ -27,7 +27,7 @@ describe "discussion assignments" do
       f("#discussion-title").send_keys("New Discussion Title")
       type_in_tiny('textarea[name=message]', 'Discussion topic message body')
       f("#has_group_category").click
-      drop_down = get_options('#assignment_group_category_id').map(&:text)
+      drop_down = get_options('#assignment_group_category_id').map(&:text).map(&:strip)
       expect(drop_down).to include('category 1')
       click_option('#assignment_group_category_id', 'category 1')
     end
