7486682573936832fed4da42f01746f314548698
MFairbourn <mfairbourn@instructure.com>
2016-03-15 23:12:10 -0600

    spec: refactor cal2 specs to run on chromedriver
    Test Plan:
    - Tests Should pass
    - Code review
    
    Change-Id: Iaf50ea04a9adbc737c5701bada2667eace53cb03
    Reviewed-on: https://gerrit.instructure.com/74614
    Tested-by: Jenkins
    Reviewed-by: Pedro Fajardo <pfajardo@instructure.com>
    Product-Review: Matt Fairbourn <mfairbourn@instructure.com>
    QA-Review: Matt Fairbourn <mfairbourn@instructure.com>

--- spec/selenium/calendar/calendar2_sidebar_spec.rb
+++ spec/selenium/calendar/calendar2_sidebar_spec.rb
@@ -40,7 +40,9 @@ describe "calendar2" do
           end
 
           orig_titles = ff(title_selector).map(&:text)
-          f("#minical .fc-other-month").click
+
+          other_month = f("#minical .fc-other-month")
+          driver.action.move_to(other_month).click.perform
 
           expect(orig_titles).not_to eq ff(title_selector).map(&:text)
         end
