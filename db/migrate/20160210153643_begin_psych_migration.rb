ab394d372bdab57e7235a75d271e95c43b22a911
James Williams <jamesw@instructure.com>
2016-03-31 09:10:54 -0600

    pend psych migration in production for gradual rollout
    closes #CNVS-28366
    
    Change-Id: I4c4a559d31e82e4444950402e14972c05bab5456
    Reviewed-on: https://gerrit.instructure.com/75862
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- db/migrate/20160210153643_begin_psych_migration.rb
+++ db/migrate/20160210153643_begin_psych_migration.rb
@@ -1,6 +1,14 @@
 class BeginPsychMigration < ActiveRecord::Migration
   tag :postdeploy
 
+  def self.runnable? # TODO: Remove when we're ready to run this everywhere
+    if ApplicationController.respond_to?(:test_cluster?)
+      ApplicationController.test_cluster?
+    else
+      true
+    end
+  end
+
   def up
     DataFixup::PsychMigration.run if CANVAS_RAILS4_0 || !Rails.env.test?
   end
