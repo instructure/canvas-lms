c24896df637979fc1abdd174765ff126b1dc99fe
James Williams <jamesw@instructure.com>
2016-04-04 14:08:49 -0600

    keep track of psych migration time
    also make it runnable with ENV var
    
    refs #CNVS-28417
    
    Change-Id: Ic2c3cea224167dfb3b8bcfee4999b0d76540c1da
    Reviewed-on: https://gerrit.instructure.com/76168
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- db/migrate/20160210153643_begin_psych_migration.rb
+++ db/migrate/20160210153643_begin_psych_migration.rb
@@ -2,7 +2,9 @@ class BeginPsychMigration < ActiveRecord::Migration
   tag :postdeploy
 
   def self.runnable? # TODO: Remove when we're ready to run this everywhere
-    if ApplicationController.respond_to?(:test_cluster?)
+    if ENV['RUN_PSYCH_MIGRATION']
+      true
+    elsif ApplicationController.respond_to?(:test_cluster?)
       ApplicationController.test_cluster?
     else
       true
