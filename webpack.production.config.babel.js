aeac107657fe6e7fab7a109caac9a39eeb536252
Ethan Vizitei <ethan.vizitei@gmail.com>
2016-04-11 10:13:27 -0600

    fix webpack caching babel issue for build
    fixes CNVS-27514
    
    TEST PLAN:
     1) warmfix
     2) webpack should work in beta
    
    Change-Id: I31fcea9571d3ff9761d96feb4a8a1d9136886a5a
    Reviewed-on: https://gerrit.instructure.com/76766
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- webpack.production.config.babel.js
+++ webpack.production.config.babel.js
@@ -3,26 +3,37 @@ var baseWebpackConfig = require("./frontend_build/baseWebpackConfig");
 var productionWebpackConfig = baseWebpackConfig;
 
 var publicPath = "/webpack-dist-optimized/"
-if(process.env.NODE_ENV == 'production'){
-  var yaml = require('js-yaml');
-  var fs   = require('fs');
-  try {
-    var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/config/canvas_cdn.yml', 'utf8'));
-    var cdnHost = doc.production.host
-    if(cdnHost !== undefined && cdnHost !== null){
-      publicPath = cdnHost + "/dist" + publicPath;
-      console.log(publicPath);
-    }else{
-      console.log("NO CDN HOST, USING LOCAL ASSETS");
-    }
-  } catch (e) {
-    console.log("NO CDN FILE, USING LOCAL ASSETS");
+
+var yaml = require('js-yaml');
+var fs   = require('fs');
+try {
+  var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/config/canvas_cdn.yml', 'utf8'));
+  var cdnHost = doc.production.host
+  if(cdnHost !== undefined && cdnHost !== null){
+    publicPath = cdnHost + "/dist" + publicPath;
+    console.log(publicPath);
+  }else{
+    console.log("NO CDN HOST, USING LOCAL ASSETS");
   }
+} catch (e) {
+  console.log("NO CDN FILE, USING LOCAL ASSETS");
 }
 
 productionWebpackConfig.devtool = undefined;
 productionWebpackConfig.output.path = __dirname + '/public/webpack-dist-optimized';
 productionWebpackConfig.output.publicPath = publicPath;
+
+// make sure we don't try to cache JSX assets when building for production
+productionWebpackConfig.module.loaders.forEach(function(loader){
+  if(loader.loaders){
+    var babelCacheIndex = loader.loaders.indexOf("babel?cacheDirectory=tmp")
+    if(babelCacheIndex > -1){
+      loader.loaders[babelCacheIndex] = "babel"
+    }
+  }else if(loader.loader == "babel?cacheDirectory=tmp"){
+    loader.loader = "babel"
+  }
+})
 
 if (!process.env.JS_BUILD_NO_UGLIFY) {
   productionWebpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
