e017e4a46a37797c6564e6b21823f3ca75753b9c
Jeremy Neander <jneander@instructure.com>
2016-04-18 09:57:56 -0500

    record excused/un-excused submissions in grade change log
    Previously, excused and un-excused submissions were not being recorded by
    the grade change log. As a result, those logs were causing invalid states
    to be displayed within Admin Tools. It now correctly tracks the excusing
    and un-excusing of submissions.
    
    test plan:
     1. Create an account, course, student, and assignment
     2. Ensure the assignment is assigned to the student
     3. Visit the Gradebook Individual View
     4. Select the given student and assignment
     5. Make the following grade changes:
        a. Grade the assignment with 10
        b. Grade the assignment with -
        c. Excuse the assignment
        d. Grade the assignment with -
        e. Excuse the assignment
        f. Grade the assignment with 1
        g. Excuse the assignment
     6. Visit the Admin Tools for the account
     7. Click the Logging tab
     8. Select the 'Grade Change Activity' log type
     9. Search for the assignment
    10. Verify the following logged grade changes, top-to-bottom:
        a. before: 1;    after: 'EX'
        b. before: 'EX'; after: 1
        c. before: '-';  after: 'EX'
        d. before: 'EX'; after: '-'
        e. before: '-';  after: 'EX'
        f. before: 10;   after: '-'
        g. before: '-';  after: 10
    11. Visit the SpeedGrader™ for the given assignment and student
    12. Verify that the student has a grade of 'EX'
    13. Verify the presence of a checkmark beside the student's name
    
    closes CNVS-28313
    
    Change-Id: I0bac427a4b4bfe282a78cbf04b74f828c5048dc0
    Reviewed-on: https://gerrit.instructure.com/77280
    Tested-by: Jenkins
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Reviewed-by: Shahbaz Javeed <sjaveed@instructure.com>
    QA-Review: KC Naegle <knaegle@instructure.com>
    Product-Review: Christi Wruck

--- lib/api/v1/grade_change_event.rb
+++ lib/api/v1/grade_change_event.rb
@@ -39,6 +39,8 @@ module Api::V1::GradeChangeEvent
       :event_type => event.event_type,
       :grade_before => event.grade_before,
       :grade_after => event.grade_after,
+      :excused_before => event.excused_before,
+      :excused_after => event.excused_after,
       :graded_anonymously => event.graded_anonymously,
       :version_number => event.version_number,
       :links => links
