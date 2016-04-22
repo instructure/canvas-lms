01e3ff1476d8b714458f7ebf6ae2264f8a741233
Jon Jensen <jon@instructure.com>
2016-04-22 15:53:07 -0600

    spec: add delay to track down spec failure
    Change-Id: I2abd6f509df927edafafeaaddbab18b678bbbcb8
    Reviewed-on: https://gerrit.instructure.com/77763
    Tested-by: Jenkins
    Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
    Product-Review: Landon Wilkins <lwilkins@instructure.com>
    QA-Review: Landon Wilkins <lwilkins@instructure.com>

--- spec/selenium/profile_spec.rb
+++ spec/selenium/profile_spec.rb
@@ -181,6 +181,7 @@ describe "profile" do
       register_form.find_element(:css, '.sms_number').send_keys(test_cell_number)
       click_option('select.user_selected.carrier', 'AT&T')
       submit_form(register_form)
+      sleep 1 # wait a moment before we close any dialogs, since this spec fails intermittently ... this way we can catch any formError thingies in the video capture
       wait_for_ajaximations
       close_visible_dialog
       keep_trying_until { expect(f('.other_channels .path')).to include_text(test_cell_number) }
