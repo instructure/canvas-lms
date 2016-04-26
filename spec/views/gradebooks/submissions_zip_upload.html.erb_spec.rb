f302b5a6250482276ff94a4a3f824a974a79e718
Shahbaz Javeed <sjaveed@instructure.com>
2016-04-26 18:56:30 -0400

    link back properly to gradebook from the submissions upload page
    closes CNVS-27819
    
    test plan:
    1. Create a course with an assignment that requires an upload
    2. Enroll two students in the course and upload two submissions,
       one as each of them, for that assignment
    3. Go to the gradebook and click on the assignment column header
       and "Download all assignments" as a zip file
    4. Once downloaded, click on the assignment column header again
       and "Re-upload assignments" and re-upload the same zip file
       that you downloaded
    5. The submission upload page you see once the upload is complete
       should have a link named "back to Gradebook" which should not
       have any anchor tags in it (#assignment_id=xxx)
    6. Click the link named "back to Gradebook" and you should see
       Gradebook loading properly.
    
    Change-Id: Idc4d2d38ba77d2b9891ded1ed094a30b3db7fe6a
    Reviewed-on: https://gerrit.instructure.com/78068
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    Tested-by: Jenkins
    QA-Review: KC Naegle <knaegle@instructure.com>
    Product-Review: Christi Wruck

---
+++ spec/views/gradebooks/submissions_zip_upload.html.erb_spec.rb
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2016 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'spec_helper'
+require 'spec/views/views_helper'
+
+describe "/gradebooks/submissions_zip_upload", type: :view do
+  before do
+    course_with_student
+    view_context
+    assigns[:students] = [@user]
+    assigns[:assignment] = @course.assignments.create!(:title => "some assignment")
+    assigns[:comments] = []
+    assigns[:failures] = []
+  end
+
+  it "should render" do
+    render 'gradebooks/submissions_zip_upload'
+
+    expect(rendered).to be_present
+  end
+
+  it "includes a link back to the gradebook (gradebook2 by default)" do
+    render 'gradebooks/submissions_zip_upload'
+
+    expect(view.content_for(:right_side)).to match(/a.+?href="\/courses\/#{@course.id}\/gradebook"/)
+  end
+end
