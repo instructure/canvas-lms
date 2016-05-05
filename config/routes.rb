055329bc41c51ae47b86ae8e3c01e1b0ec16f511
Simon Gaeremynck <gaeremyncks@gmail.com>
2016-05-04 17:15:20 -0700

    Ally endpoints
    This adds three endpoints that allow the Ally plugin to function within
    Instructure Canvas.
    
     * /api/v1/ally - Whether the Ally plugin has been enabled
     * /api/v1/courses/:course_id/ally/proxy - Proxy data to/from the Ally REST API
     * /api/v1/courses/:course_id/ally/sign - Generate a signature allowing the Ally UI to communicate with the Ally REST API
    
    More tests to follow

--- config/routes.rb
+++ config/routes.rb
@@ -1757,6 +1757,10 @@ CanvasRails::Application.routes.draw do
     get '/crocodoc_session', controller: 'crocodoc_sessions', action: 'show', as: :crocodoc_session
     get '/canvadoc_session', controller: 'canvadoc_sessions', action: 'show', as: :canvadoc_session
 
+    get '/ally', controller: 'ally', action: 'enabled'
+    get '/courses/:course_id/ally/sign', controller: 'ally', action: 'sign'
+    get '/courses/:course_id/ally/proxy', controller: 'ally', action: 'proxy'
+
     scope(controller: :grading_periods) do
       %w(course account).each do |context|
         content_prefix = "#{context.pluralize}/:#{context}_id"
