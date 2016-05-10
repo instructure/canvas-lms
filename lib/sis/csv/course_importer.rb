2acdf131310c56cb7087c6a869cb4280d5a94786
Rob Orton <rob@instructure.com>
2016-05-10 09:24:28 -0600

    remove is_ from importer methods
    refs CNVS-29218
    
    test plan
     - specs should pass
    
    Change-Id: I44731a0fb2b41b0f2f5390ce2e9d9030a7a9be05
    Reviewed-on: https://gerrit.instructure.com/79191
    Tested-by: Jenkins
    Reviewed-by: John Corrigan <jcorrigan@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- lib/sis/csv/course_importer.rb
+++ lib/sis/csv/course_importer.rb
@@ -20,7 +20,7 @@ module SIS
   module CSV
     class CourseImporter < CSVBaseImporter
 
-      def self.is_course_csv?(row)
+      def self.course_csv?(row)
         row.include?('course_id') && row.include?('short_name')
       end
 
