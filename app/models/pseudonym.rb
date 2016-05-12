3d5bd3a2848c2c3ad7143921f1723610a20d5edd
Cody Cutrer <cody@instructure.com>
2016-05-12 11:27:33 -0600

    actually find explicitly-linked Canvas and LDAP logins during login
    refs CNVS-28356
    
    test plan:
     * add a login that's explicitly linked to Canvas auth
     * you should be able to actually log in with it
    
    Change-Id: I79574a5da8b5fddaa2c12cecf1cc0cbdf811b89b
    Reviewed-on: https://gerrit.instructure.com/79442
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/models/pseudonym.rb
+++ app/models/pseudonym.rb
@@ -142,7 +142,9 @@ class Pseudonym < ActiveRecord::Base
   end
 
   def self.custom_find_by_unique_id(unique_id)
-    self.for_auth_configuration(unique_id, nil) if unique_id
+    return unless unique_id
+    active.by_unique_id(unique_id).where("authentication_provider_id IS NULL OR EXISTS (?)",
+      AccountAuthorizationConfig.active.where(auth_type: ['canvas', 'ldap']).where("authentication_provider_id=account_authorization_configs.id")).first
   end
 
   def self.for_auth_configuration(unique_id, aac)
