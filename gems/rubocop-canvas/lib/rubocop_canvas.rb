6cb3339a727d7435b8bc9d66b0bfcd499163a99c
Landon Wilkins <lwilkins@instructure.com>
2016-05-13 10:39:23 -0600

    port ModelBehavior cop over from bridge, fixes SD-1119
    test plan:
    * see test commit
    
    Change-Id: Ibf3a663c0e04cc463595a4f0bd6dae81e572eff3
    Reviewed-on: https://gerrit.instructure.com/79564
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Landon Wilkins <lwilkins@instructure.com>
    QA-Review: Landon Wilkins <lwilkins@instructure.com>

--- gems/rubocop-canvas/lib/rubocop_canvas.rb
+++ gems/rubocop-canvas/lib/rubocop_canvas.rb
@@ -21,6 +21,7 @@ require 'rubocop_canvas/cops/lint/no_file_utils_rm_rf'
 require 'rubocop_canvas/cops/lint/no_sleep'
 ## migration
 require 'rubocop_canvas/cops/migration/concurrent_index'
+require 'rubocop_canvas/cops/migration/model_behavior'
 require 'rubocop_canvas/cops/migration/primary_key'
 require 'rubocop_canvas/cops/migration/remove_column'
 require 'rubocop_canvas/cops/migration/send_later'
