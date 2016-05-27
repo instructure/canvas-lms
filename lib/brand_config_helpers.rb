0bab00414e177104dd56d1f0faf69cc74afbbc03
Simon Williams <simon@instructure.com>
2016-05-27 15:22:25 -0600

    theme editor for siteadmin
    fixes CNVS-28289
    
    this enables theme editor to work as a virtual "parent" account for root
    account themes, but only root accounts on the same shard as the site
    admin account.  this is a limitation we hope to remove in the future,
    but replicating the site admin theme to all shards is going to be
    significantly more complicated.
    
    test plan:
    - create a mixture of root accounts on the same shard as site admin and
    different shards
    - regression test theme editor in siteadmin.  it should act as a parent
    for same-shard accounts and not interact with different-shard accounts
    
    Change-Id: Ia7e3e1a8adbb7bc2e49b2d9b5ef3b4d30cf4c581
    Reviewed-on: https://gerrit.instructure.com/82000
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Tested-by: Jenkins
    QA-Review: Benjamin Christian Nelson <bcnelson@instructure.com>
    Product-Review: Colleen Palmer <colleen@instructure.com>

--- lib/brand_config_helpers.rb
+++ lib/brand_config_helpers.rb
@@ -4,18 +4,24 @@ module BrandConfigHelpers
   end
 
   def effective_brand_config
-    return brand_config if root_account? || (branding_allowed? && brand_config)
-    parent_account.effective_brand_config
+    first_config_in_chain(
+      brand_config_chain(include_self: true).select(&:branding_allowed?)
+    )
   end
 
   def first_parent_brand_config
-    @first_parent_brand_config ||= begin
-      _, *chain = self.account_chain
-      chain.find do |a|
-        config = a.brand_config
-        break config if config
-      end
-    end
+    first_config_in_chain(brand_config_chain(include_self: false))
   end
 
+  def brand_config_chain(include_self:)
+    chain = self.account_chain(include_site_admin: true)
+    chain.shift unless include_self
+    chain.select{ |a| a.shard == self.shard }
+  end
+  private :brand_config_chain
+
+  def first_config_in_chain(chain)
+    chain.find(&:brand_config_md5).try(:brand_config)
+  end
+  private :first_config_in_chain
 end
