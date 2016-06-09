d611168314a7a5e30766fd528fca50377fad46a0
Neil Gupta <ngupta@instructure.com>
2016-06-09 17:40:35 -0500

    Don't make containerized phantomjs karma tests single-run
    Change-Id: I6c3162b5e414a4ce9e814fb847153db8ddd87ab4
    Reviewed-on: https://gerrit.instructure.com/82166
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Tested-by: Jenkins
    Product-Review: Neil Gupta <ngupta@instructure.com>
    QA-Review: Neil Gupta <ngupta@instructure.com>

--- docker-compose/phantomjs-karma/entry_point.sh
+++ docker-compose/phantomjs-karma/entry_point.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./node_modules/karma/bin/karma start --browsers PhantomJS --single-run --reporters progress 2>&1
+./node_modules/karma/bin/karma start --browsers PhantomJS --reporters progress 2>&1
