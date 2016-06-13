b74748d1794e59fdb07d8e7a3c49b12fb0f6b02e
Simon Williams <simon@instructure.com>
2016-06-12 20:34:45 -0600

    render sso buttons on login error
    fixes CNVS-29906
    
    test plan:
    - enable an oauth sso provider, like google
    - go to the login page, notice that the 'login with google' option is
    there
    - enter an incorrect user / pass in the canvas login form, submit
    - 'login with google' should still be an option
    
    Change-Id: I645a04019774d88386ed201ce0d4105ba2353694
    Reviewed-on: https://gerrit.instructure.com/82331
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- app/controllers/login/canvas_controller.rb
+++ app/controllers/login/canvas_controller.rb
@@ -28,7 +28,6 @@ class Login::CanvasController < ApplicationController
   def new
     @pseudonym_session = PseudonymSession.new
     @headers = false
-    @aacs_with_buttons = @domain_root_account.authentication_providers.active.select { |aac| aac.class.login_button? }
     flash.now[:error] = params[:message] if params[:message]
 
     maybe_render_mobile_login
@@ -141,6 +140,7 @@ class Login::CanvasController < ApplicationController
       )
       render :mobile_login, layout: 'mobile_auth', status: status
     else
+      @aacs_with_buttons = @domain_root_account.authentication_providers.active.select { |aac| aac.class.login_button? }
       render :new, status: status
     end
   end
