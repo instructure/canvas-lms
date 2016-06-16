47cda00abb496a331a193331e5da2d9ff7588c25
Andrew Butterfield <abutterfield@instructure.com>
2016-06-16 11:30:41 -0600

    Add include to collaboration members api endpoint
    fixes PLAT-1607
    
    Test plan:
    * In a course with some collaborations hit the following endpoint
      /api/v1/collaborations/:id/members?include[]=collaborator_lti_id&include[]=avatar_image_url
    * Ensure that the collaborator_lti_id is included for both user and
      group members
    * Ensure that the avatar_image_url is included for members of type user
      who have an avatar_image_url set
    
    Change-Id: Id253242587d73b57f9caf203db2091706a347623
    Reviewed-on: https://gerrit.instructure.com/82741
    Tested-by: Jenkins
    Reviewed-by: Brad Humphrey <brad@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Andrew Butterfield <abutterfield@instructure.com>

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -253,6 +253,12 @@ class CollaborationsController < ApplicationController
   #
   # List the collaborators of a given collaboration
   #
+  # @argument include[] [String, "collaborator_lti_id"|"avatar_image_url"]
+  #   - "collaborator_lti_id": Optional information to include with each member.
+  #     Represents an identifier to be used for the member in an LTI context.
+  #   - "avatar_image_url": Optional information to include with each member.
+  #     The url for the avatar of a collaborator with type 'user'.
+  #
   # @example_request
   #
   #   curl https://<canvas>/api/v1/courses/1/collaborations/1/members
@@ -260,12 +266,13 @@ class CollaborationsController < ApplicationController
   # @returns [Collaborator]
   def members
     return unless authorized_action(@collaboration, @current_user, :read)
+    options = {:include => params[:include]}
     collaborators = @collaboration.collaborators.preload(:group, :user)
     collaborators = Api.paginate(collaborators,
                                  self,
                                  api_v1_collaboration_members_url)
 
-    render :json => collaborators.map { |c| collaborator_json(c, @current_user, session) }
+    render :json => collaborators.map { |c| collaborator_json(c, @current_user, session, options) }
   end
 
   private
