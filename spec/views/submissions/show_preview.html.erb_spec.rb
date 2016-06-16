8089364cf7920a600cadc1301baca7f39996a94e
Matthew Berns <mberns@instructure.com>
2016-06-16 16:42:23 -0500

    add LTI launch capability to submission show
    closes CNVS-29712
    
    test plan:
    - Add an external tool assignment
    - Passback a URL type score which matches the launch URL of the tool
    - Confirm that the submission show page launches into the tool
      for both students and teachers
    
    Change-Id: I8747c236310f6fb3054f2183714eddda997f48a8
    Reviewed-on: https://gerrit.instructure.com/82814
    Tested-by: Jenkins
    Reviewed-by: Ryan Taylor <rtaylor@instructure.com>
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Matt Berns <mberns@instructure.com>

--- spec/views/submissions/show_preview.html.erb_spec.rb
+++ spec/views/submissions/show_preview.html.erb_spec.rb
@@ -30,6 +30,16 @@ describe "/submissions/show_preview" do
     expect(response).not_to be_nil
   end
 
+  it "should load an lti launch" do
+    course_with_student
+    view_context
+    a = @course.assignments.create!(:title => "external assignment", :submission_types => 'basic_lti_launch')
+    assigns[:assignment] = a
+    assigns[:submission] = a.submit_homework @user, submission_type: 'basic_lti_launch', url: 'http://www.example.com'
+    render "submissions/show_preview"
+    expect(response.body).to match /(?=.*external_tools\/retrieve?)(?=.*www\.example\.com).*/
+  end
+
   it "should give a user-friendly explaination why there's no preview" do
     course_with_student
     view_context
