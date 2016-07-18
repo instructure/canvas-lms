e57651b7f20ec4a4ccf1317a4f0b345420e50cda
James Williams <jamesw@instructure.com>
2016-07-18 11:05:37 -0600

    speed up submission comment draft field population
    Change-Id: Ia3769f6b97f924f8740ce87f8437cd478c9660e8
    Reviewed-on: https://gerrit.instructure.com/85587
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- lib/data_fixup/populate_submission_comment_draft_field.rb
+++ lib/data_fixup/populate_submission_comment_draft_field.rb
@@ -2,6 +2,8 @@ module DataFixup::PopulateSubmissionCommentDraftField
   def self.run
     relevant_comments = SubmissionComment.where(draft: nil)
 
-    while relevant_comments.limit(1000).update_all(draft: false) > 0; end
+    SubmissionComment.find_ids_in_ranges do |min_id, max_id|
+      relevant_comments.where(:id => min_id..max_id).update_all(draft: false)
+    end
   end
 end
