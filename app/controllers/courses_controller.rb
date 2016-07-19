6593b8dc9c0251706a1525498f90fab508dc8094
James Williams <jamesw@instructure.com>
2016-07-19 09:25:57 -0600

    rails4.2: fix course home sub navigation tools for non-admins
    fixes #CNVS-30505
    
    Change-Id: I733f8876b0b48e35c5abe6542f4e5bfbfde4d436
    Reviewed-on: https://gerrit.instructure.com/85691
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1610,7 +1610,7 @@ class CoursesController < ApplicationController
         @recent_feedback = (@current_user && @current_user.recent_feedback(:contexts => @contexts)) || []
       end
 
-      @course_home_sub_navigation_tools = ContextExternalTool.all_tools_for(@context, :placements => :course_home_sub_navigation, :root_account => @domain_root_account, :current_user => @current_user)
+      @course_home_sub_navigation_tools = ContextExternalTool.all_tools_for(@context, :placements => :course_home_sub_navigation, :root_account => @domain_root_account, :current_user => @current_user).to_a
       unless @context.grants_right?(@current_user, session, :manage_content)
         @course_home_sub_navigation_tools.reject! { |tool| tool.course_home_sub_navigation(:visibility) == 'admins' }
       end
