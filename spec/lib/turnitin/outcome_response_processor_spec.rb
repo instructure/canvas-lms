cd6249d60aabe939722745233d6db90303d09ecd
Matthew Wheeler <mwheeler@instructure.com>
2016-08-04 11:21:59 -0600

    Add retry to TII submission fetching
    Fixes PLAT-1548
    
    Test Plan: Regression test Turn it in integration
    
    Change-Id: I46e85439b37f8eba536c77dcf460fc57d20edc06
    Reviewed-on: https://gerrit.instructure.com/86908
    Reviewed-by: Nathan Mills <nathanm@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Tested-by: Jenkins
    Product-Review: Matthew Wheeler <mwheeler@instructure.com>

--- spec/lib/turnitin/outcome_response_processor_spec.rb
+++ spec/lib/turnitin/outcome_response_processor_spec.rb
@@ -75,42 +75,65 @@ module Turnitin
     end
 
     describe "#process with request errors" do
-      it 'creates an attachment for "Faraday::TimeoutError"' do
-        TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Faraday::TimeoutError, 'Net::ReadTimeout')
-        expect { subject.process_without_send_later }.to raise_error
-        attachment = lti_assignment.attachments.first
-        expect(lti_assignment.attachments.count).to eq 1
-        expect(attachment.display_name).to eq "Failed turnitin submission"
+      context 'when it is not the last attempt' do
+        it 'does not create an error attachment' do
+          subject.class.any_instance.stubs(:attempt_number).returns(subject.class.max_attempts-1)
+          TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Faraday::TimeoutError, 'Net::ReadTimeout')
+          expect { subject.process_without_send_later }.to raise_error(Faraday::TimeoutError)
+          expect(lti_assignment.attachments.count).to eq 0
+        end
       end
 
-      it 'creates an attachment for "Errno::ETIMEDOUT"' do
-        TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Errno::ETIMEDOUT, 'Connection timed out - connect(2) for "api.turnitin.com" port 443')
-        expect { subject.process_without_send_later }.to raise_error
-        attachment = lti_assignment.attachments.first
-        expect(lti_assignment.attachments.count).to eq 1
-        expect(attachment.display_name).to eq "Failed turnitin submission"
-      end
+      context 'when it is the last attempt' do
+        it 'creates an attachment for "Faraday::TimeoutError"' do
+          subject.class.stubs(:max_attempts).returns(1)
+          TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Faraday::TimeoutError, 'Net::ReadTimeout')
+          expect { subject.process_without_send_later }.to raise_error(Faraday::TimeoutError)
+          attachment = lti_assignment.attachments.first
+          expect(lti_assignment.attachments.count).to eq 1
+          expect(attachment.display_name).to eq "Failed turnitin submission"
+        end
 
-      it 'creates an attachment for "Faraday::ConnectionFailed"' do
-        TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Faraday::ConnectionFailed, 'Connection reset by peer')
-        expect { subject.process_without_send_later }.to raise_error
-        attachment = lti_assignment.attachments.first
-        expect(lti_assignment.attachments.count).to eq 1
-        expect(attachment.display_name).to eq "Failed turnitin submission"
+        it 'creates an attachment for "Errno::ETIMEDOUT"' do
+          subject.class.stubs(:max_attempts).returns(1)
+          TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Errno::ETIMEDOUT, 'Connection timed out - connect(2) for "api.turnitin.com" port 443')
+          expect { subject.process_without_send_later }.to raise_error(Errno::ETIMEDOUT)
+          attachment = lti_assignment.attachments.first
+          expect(lti_assignment.attachments.count).to eq 1
+          expect(attachment.display_name).to eq "Failed turnitin submission"
+        end
+
+        it 'creates an attachment for "Faraday::ConnectionFailed"' do
+          subject.class.stubs(:max_attempts).returns(1)
+          TurnitinApi::OutcomesResponseTransformer.any_instance.expects(:original_submission).raises(Faraday::ConnectionFailed, 'Connection reset by peer')
+          expect { subject.process_without_send_later }.to raise_error(Faraday::ConnectionFailed)
+          attachment = lti_assignment.attachments.first
+          expect(lti_assignment.attachments.count).to eq 1
+          expect(attachment.display_name).to eq "Failed turnitin submission"
+        end
       end
     end
 
     describe "#update_originality_data" do
-      it 'sets an error message if max attempts are exceeded' do
+      it 'raises an error if max attempts are not exceeded' do
+        subject.class.any_instance.stubs(:attempt_number).returns(subject.class.max_attempts-1)
         mock_turnitin_client = mock('turnitin_client')
         mock_turnitin_client.stubs(:scored?).returns(false)
-        subject.instance_variable_set(:@turnitin_client, mock_turnitin_client)
+        subject.stubs(:build_turnitin_client).returns(mock_turnitin_client)
         submission = lti_assignment.submit_homework(lti_student, attachments:[attachment], submission_type: 'online_upload')
-        subject.update_originality_data(submission, attachment.asset_string, (OutcomeResponseProcessor::MAX_ATTEMPTS+1))
+        expect { subject.update_originality_data(submission, attachment.asset_string) }.to raise_error Turnitin::SubmissionNotScoredError
+      end
+
+      it 'sets an error message if max attempts are exceeded' do
+        subject.class.any_instance.stubs(:attempt_number).returns(subject.class.max_attempts)
+        mock_turnitin_client = mock('turnitin_client')
+        mock_turnitin_client.stubs(:scored?).returns(false)
+        subject.stubs(:build_turnitin_client).returns(mock_turnitin_client)
+        submission = lti_assignment.submit_homework(lti_student, attachments:[attachment], submission_type: 'online_upload')
+        subject.update_originality_data(submission, attachment.asset_string)
         expect(submission.turnitin_data[attachment.asset_string][:status]).to eq 'error'
         expect(submission.turnitin_data[attachment.asset_string][:public_error_message]).to start_with "Turnitin has not"
       end
     end
-
   end
 end
