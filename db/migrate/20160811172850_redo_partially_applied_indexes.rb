300dd5f8d9204feea899b1138c1916e24433e903
Simon Williams <simon@instructure.com>
2016-08-11 12:14:36 -0600

    recheck a partially applied index removal
    refs CNVS-30214
    
    test plan: migrations should run
    
    Change-Id: I88be99b8f5f44f3ffea78530fd2960ee665401a7
    Reviewed-on: https://gerrit.instructure.com/87530
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

---
+++ db/migrate/20160811172850_redo_partially_applied_indexes.rb
@@ -0,0 +1,9 @@
+class RedoPartiallyAppliedIndexes < ActiveRecord::Migration
+  tag :postdeploy
+
+  def up
+    if index_exists?(:attachments, [:folder_id, :file_state, :display_name], name: "index_attachments_on_folder_id_and_file_state_and_display_name2")
+      remove_index "attachments", name: "index_attachments_on_folder_id_and_file_state_and_display_name2"
+    end
+  end
+end
