801612b775cbe5bef8612e330d88132e8ccde481
Jeremy Stanley <jeremy@instructure.com>
2016-08-12 15:50:54 -0600

    don't show past appointment slots to students for reservation
    test plan:
     - as a student, you should not see already-ended scheduler slots
       available for reservation on the Scheduler page
    
    closes CNVS-20980
    
    Change-Id: Ic0e813ebede2afa697a57e923f81ba52ddd20845
    Reviewed-on: https://gerrit.instructure.com/87717
    Tested-by: Jenkins
    Reviewed-by: Joel Hough <joel@instructure.com>
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- lib/api/v1/calendar_event.rb
+++ lib/api/v1/calendar_event.rb
@@ -193,12 +193,14 @@ module Api::V1::CalendarEvent
       hash['new_appointments'] = group.new_appointments.map{ |event| calendar_event_json(event, user, session, :skip_details => true, :appointment_group_id => group.id) }
     end
     if include.include?('appointments')
+      appointments_scope = group.appointments
+      appointments_scope = appointments_scope.where('end_at IS NULL OR end_at>?', Time.now.utc) unless options[:include_past_appointments]
       if include.include?('child_events')
-        all_child_events = group.appointments.map(&:child_events).flatten
+        all_child_events = appointments_scope.map(&:child_events).flatten
         ActiveRecord::Associations::Preloader.new.preload(all_child_events, :context)
         user_json_preloads(all_child_events.map(&:context)) if !all_child_events.empty? && all_child_events.first.context.is_a?(User) && user_json_is_admin?(@context, user)
       end
-      hash['appointments'] = group.appointments.map { |event| calendar_event_json(event, user, session,
+      hash['appointments'] = appointments_scope.map { |event| calendar_event_json(event, user, session,
                                                                                  :context => group,
                                                                                  :appointment_group => group,
                                                                                  :appointment_group_id => group.id,
