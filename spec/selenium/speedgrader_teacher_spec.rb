b19c7cc4f3f283d0aecb4baeac6d286ce62f64ff
Jon Jensen <jon@instructure.com>
2016-08-16 16:13:37 -0600

    spec: fix brittle spec
    section order here is not deterministic (just whatever the db decides), and
    this spec has started failing w/ the new SHARED_DB stuff
    
    Change-Id: I46e7f750762408b543128a318394a2beeb279f4f
    Reviewed-on: https://gerrit.instructure.com/87980
    Tested-by: Jenkins
    Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
    Product-Review: Landon Wilkins <lwilkins@instructure.com>
    QA-Review: Landon Wilkins <lwilkins@instructure.com>

--- spec/selenium/speedgrader_teacher_spec.rb
+++ spec/selenium/speedgrader_teacher_spec.rb
@@ -167,7 +167,8 @@ describe "speed grader" do
       wait_for_ajaximations
 
       sections = @course.course_sections
-      expect(ff("#section-menu ul li a")[1]).to have_attribute("text", @course_section.name)
+      section_options_text = f("#section-menu ul")[:textContent] # hidden
+      expect(section_options_text).to include(@course_section.name)
       goto_section(sections[0].id)
       expect(ff("#students_selectmenu option").length).to eq 1
       goto_section(sections[1].id)
