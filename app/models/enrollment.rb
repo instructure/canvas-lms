06993375a043240086cb5251ca41e9826db41df1
Ethan Vizitei <ethan.vizitei@gmail.com>
2016-08-22 12:51:01 -0600

    fix enrollment_importer deadlocks
    closes CNVS-31154
    
    don't touch users within transaction, that way
    we don't deadlock when this is running at the
    same time as another importer that tries to
    touch_all for some users in a different course
    and overlaps.
    
    Change-Id: I1ccca396d7bcf1e984c5854108c5acb5a4875d63
    Reviewed-on: https://gerrit.instructure.com/88410
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -74,7 +74,7 @@ class Enrollment < ActiveRecord::Base
   after_save :recalculate_enrollment_state
   after_destroy :update_assignment_overrides_if_needed
 
-  attr_accessor :already_enrolled, :need_touch_user
+  attr_accessor :already_enrolled, :need_touch_user, :skip_touch_user
   attr_accessible :user, :course, :workflow_state, :course_section, :limit_privileges_to_course_section, :already_enrolled, :start_at, :end_at
 
   scope :current, -> { joins(:course).where(QueryBuilder.new(:active).conditions).readonly(false) }
@@ -682,6 +682,7 @@ class Enrollment < ActiveRecord::Base
       self.enrollment_state.state_is_current = false
       self.enrollment_state.is_direct_recalculation = true
     end
+    self.enrollment_state.skip_touch_user ||= self.skip_touch_user
     self.enrollment_state.ensure_current_state
   end
 
