7fed15d627befcf82e42d267ead13141d90eeba9
James Williams <jamesw@instructure.com>
2016-09-07 10:32:51 -0600

    move user touching into after transaction
    Change-Id: Ic833256a7a94a6fec0e6adc1c50464701abf24aa
    Reviewed-on: https://gerrit.instructure.com/89792
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -209,13 +209,8 @@ class ActiveRecord::Base
 
   def touch_user
     if self.respond_to?(:user_id) && self.user_id
-      shard = self.user.shard
-      User.where(:id => self.user_id).update_all(:updated_at => Time.now.utc)
       User.connection.after_transaction_commit do
-        shard.activate do
-          User.where(:id => self.user_id).update_all(:updated_at => Time.now.utc)
-        end if shard != Shard.current
-        User.invalidate_cache(self.user_id)
+        User.where(:id => self.user_id).update_all(:updated_at => Time.now.utc)
       end
     end
     true
