704c6ca9cfdfb6900b5f36c0fbce7962b781c252
Rob Orton <rob@instructure.com>
2016-09-15 18:58:05 -0600

    handle communication channels on user merge
    refs CNVS-29149
    
    test plan
     - when merging users
     - communication channels should be recorded
    
    Change-Id: I24709e85ced40e4a5c62d62042c464c669e77738
    Reviewed-on: https://gerrit.instructure.com/89883
    Tested-by: Jenkins
    QA-Review: August Thornton <august@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/models/user_merge_data_record.rb
+++ app/models/user_merge_data_record.rb
@@ -18,7 +18,8 @@
 class UserMergeDataRecord < ActiveRecord::Base
   belongs_to :previous_user, class_name: 'User'
   belongs_to :user_merge_data
-  belongs_to :context, polymorphic: [:account_user, :enrollment, :pseudonym, :user_observer, :attachment]
+  belongs_to :context, polymorphic: [:account_user, :enrollment, :pseudonym,:user_observer,
+                                     :attachment, :communication_channel, :user_service]
 
   strong_params
 end
