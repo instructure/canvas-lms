67a3da3fcaa480849f574c289abe263cfd8f3ed9
Cody Cutrer <cody@instructure.com>
2016-09-20 15:45:16 -0600

    just assume switchman is present for specs
    fixes gh-892
    
    Change-Id: Ib4ed6565343be7569282a5e243d0026f2b95f6a9
    Reviewed-on: https://gerrit.instructure.com/90903
    Tested-by: Jenkins
    Reviewed-by: Neil Gupta <ngupta@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- spec/sharding_spec_helper.rb
+++ spec/sharding_spec_helper.rb
@@ -16,39 +16,17 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/support/onceler/sharding')
+require 'switchman/r_spec_helper'
+require_relative 'spec_helper'
+require_relative 'support/onceler/sharding'
 
-unless defined?(Switchman::RSpecHelper)
-  module ShardRSpecHelper
-    def self.included(klass)
-      klass.before do
-        skip "needs a sharding implementation"
-      end
-      require File.expand_path(File.dirname(__FILE__) + '/support/onceler/noop') unless defined?(Onceler::Noop)
-      klass.send(:include, Onceler::Noop)
-    end
-  end
+def specs_require_sharding
+  include Switchman::RSpecHelper
+  include Onceler::Sharding
 
-  RSpec.configure do |config|
-    config.before :all do
-      Shard.default.destroy if Shard.default.is_a?(Shard)
-      Shard.default(true)
-    end
-  end
-
-  def specs_require_sharding
-    include ShardRSpecHelper
-  end
-else
-  def specs_require_sharding
-    include Switchman::RSpecHelper
-    include Onceler::Sharding
-
-    before :all do
-      Shard.with_each_shard do
-        Role.ensure_built_in_roles!
-      end
+  before :all do
+    Shard.with_each_shard do
+      Role.ensure_built_in_roles!
     end
   end
 end
