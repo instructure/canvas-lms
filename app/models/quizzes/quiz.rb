8c978047f7cf7d61ef23c83e4d9d690f6c06cf1e
James Williams <jamesw@instructure.com>
2016-09-22 08:55:28 -0600

    create assignments when importing graded quizzes
    test plan:
    * import the package referenced in the ticket
    * it should create assignments for the graded quizzes instead
     of waiting until you publish them
    
    closes #CNVS-32049
    
    Change-Id: I02b5156fb617cc178c6a5b53ce98e640c1ea67da
    Reviewed-on: https://gerrit.instructure.com/91033
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- app/models/quizzes/quiz.rb
+++ app/models/quizzes/quiz.rb
@@ -183,8 +183,8 @@ class Quizzes::Quiz < ActiveRecord::Base
     end
   end
 
-  def build_assignment
-    if !self.assignment_id && self.graded? && ![:assignment, :clone, :migration].include?(@saved_by)
+  def build_assignment(force: false)
+    if !self.assignment_id && self.graded? && (force || ![:assignment, :clone, :migration].include?(@saved_by))
       assignment = self.assignment
       assignment ||= self.context.assignments.build(:title => self.title, :due_at => self.due_at, :submission_types => 'online_quiz')
       assignment.assignment_group_id = self.assignment_group_id
