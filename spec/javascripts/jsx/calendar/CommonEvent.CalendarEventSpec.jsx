484025dee6bf987beda7bb1969586b0b500f05a5
Clay Diffrient <cdiffrient@instructure.com>
2016-09-26 12:35:18 -0600

    Show reserved names in agenda view with better scheduler
    closes CNVS-32183
    
    Test Plan:
      - Set up an appointment group and limit the signups to 6 students
      - Add students to the appointment group
      - As a teacher, look at the agenda view
      - It should show all the students like:
         "Banner, Bruce; Lang, Scott; Parker, Peter; Rogers, Steve; Stark, Tony"
        when there are 5 or fewer people signed up
      - When there are more than 5 is should look like:
         "Banner, Bruce; Lang, Scott; Parker, Peter; Rogers, Steve; Stark, Tony; and more..."
      - Please note that it should also convey the status of the appointment group as a
        whole prior to listing attendees.  For instance, X more available should show
        whenever there are still slots remaining or Filled when all the slots are
        taken
         - If no one has reserved any slots it should show "X Available"
           rather than "X more available"
    Change-Id: I00e85bf0886a9105abdb1d7bb8d08fe60e0b9f62
    Reviewed-on: https://gerrit.instructure.com/91303
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Heath Hales <hhales@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- spec/javascripts/jsx/calendar/CommonEvent.CalendarEventSpec.jsx
+++ spec/javascripts/jsx/calendar/CommonEvent.CalendarEventSpec.jsx
@@ -4,21 +4,63 @@ define([
 
   let calendarEvent;
 
+  const getFakeChildEvents = () => {
+    return [
+      { user: {sortable_name: 'Stark, Tony'}},
+      { user: {sortable_name: 'Parker, Peter'}},
+      { user: {sortable_name: 'Rogers, Steve'}}
+    ];
+  }
+
   module('CommonEvent.CalendarEvent', {
     setup () {
-      const data = {}
-      const contexts = ['course_1']
-      calendarEvent = new CalendarEvent(data, contexts)
+      const data = {
+        child_events: []
+      };
+      const contexts = ['course_1'];
+      calendarEvent = new CalendarEvent(data, contexts);
     },
     teardown () {
       calendarEvent = null;
     }
   });
 
-  test('calculateAppointmentGroupEventStatus returns number of available slots string when more than 0', () => {
+  test('Constructor sets reservedUsers from child_events limited to 5 and joined by "; "', () => {
+    const data = {
+      child_events: getFakeChildEvents().concat([
+        { user: {sortable_name: 'Banner, Bruce'}},
+        { user: {sortable_name: 'Lang, Scott'}}
+      ])
+    };
+    calendarEvent = new CalendarEvent(data, ['course_1']);
+    const expected = "Banner, Bruce; Lang, Scott; Parker, Peter; Rogers, Steve; Stark, Tony";
+    equal(calendarEvent.reservedUsers, expected);
+  });
+
+  test('Constructor sets reservedUsers from child_events limited to 5 and joined by "; " with "and more..." if more than 5', () => {
+    const data = {
+      child_events: getFakeChildEvents().concat([
+        { user: {sortable_name: 'Banner, Bruce'}},
+        { user: {sortable_name: 'Lang, Scott'}},
+        { user: {sortable_name: 'Barton, Clint'}}
+
+      ])
+    };
+    calendarEvent = new CalendarEvent(data, ['course_1']);
+    const expected = "Banner, Bruce; Barton, Clint; Lang, Scott; Parker, Peter; Rogers, Steve; and more...";
+    equal(calendarEvent.reservedUsers, expected);
+  });
+
+  test('calculateAppointmentGroupEventStatus returns number of available slots string when more than 0 and none are reserved', () => {
     calendarEvent.calendarEvent.available_slots = 20;
     equal(calendarEvent.calculateAppointmentGroupEventStatus(), '20 Available');
   });
+
+  test('calculateAppointmentGroupEventStatus returns "X more available" when there are some reserved and some open', () => {
+    calendarEvent.calendarEvent.child_events = getFakeChildEvents();
+    calendarEvent.calendarEvent.available_slots = 17;
+    equal(calendarEvent.calculateAppointmentGroupEventStatus(), '17 more available');
+  })
 
   test('calculateAppointmentGroupEventStatus gives "Filled" string when 0 available_slots', () => {
     calendarEvent.calendarEvent.available_slots = 0;
@@ -36,6 +78,18 @@ define([
     calendarEvent.calendarEvent.parent_event_id = 30;
     equal(calendarEvent.calculateAppointmentGroupEventStatus(), 'Reserved');
   });
+
+  test('consideredReserved returns true when reserve should be shown', () => {
+    calendarEvent.calendarEvent.reserved = true;
+    ok(calendarEvent.consideredReserved(), 'true when reserved');
+
+    calendarEvent.calendarEvent.reserved = false;
+    calendarEvent.calendarEvent.appointment_group_url = 1;
+    calendarEvent.calendarEvent.parent_event_id = 30;
+    ok(calendarEvent.consideredReserved(), 'true with appointment_group_url and parent_event_id');
+  });
+
+
 
 
 })
