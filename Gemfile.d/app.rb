c4852e4255cb9c66654dae5e709d1a7078c480d8
Cody Cutrer <cody@instructure.com>
2016-09-29 13:00:34 -0600

    bump rake and hoe to latest
    Change-Id: Ic3eb5d2882681c7d0fa4c9b0948c968fdb9e3103
    Reviewed-on: https://gerrit.instructure.com/91655
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -48,7 +48,7 @@ gem 'hairtrigger', '0.2.15'
   gem 'ruby_parser', '3.7.0', require: false
 gem 'hashery', '1.3.0', require: false
 gem 'highline', '1.6.1', require: false
-gem 'hoe', '3.8.1'
+gem 'hoe', '3.15.1'
 gem 'httparty', '0.13.7'
 gem 'i18n', '0.7.0'
 gem 'i18nema', '0.0.8'
@@ -79,7 +79,7 @@ gem 'parallel', '1.4.1', require: false
 gem 'rack-mini-profiler', '0.9.2', require: false
 gem 'flamegraph', '0.1.0', require: false
   gem 'stackprof', '0.2.7', require: false
-gem 'rake', '10.5.0'
+gem 'rake', '11.3.0'
 gem 'rdoc', '3.12'
 gem 'ratom-nokogiri', '0.10.4', require: false
 gem 'rdiscount', '1.6.8', require: false
