39264969b5e00b0d8c92ceb09618abbaacf0bb16
Cody Cutrer <cody@instructure.com>
2016-10-05 17:09:26 -0600

    optimize speedgrader json for quizzes
    fixes CNVS-32276
    
     * use a single query to check too many versions threshold
     * avoid instantiating AR objects for each of the quiz submission
       versions
     * avoid cloning the same quiz over and over again when we only
       need 1 field
    
    Change-Id: I921e0bc7a2153a6eb7627a88bb8519beefeaa87e
    Reviewed-on: https://gerrit.instructure.com/92123
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/assignment_override_applicator.rb
+++ lib/assignment_override_applicator.rb
@@ -20,12 +20,16 @@ module AssignmentOverrideApplicator
   # top-level method intended for consumption. given an assignment or quiz(of specific
   # version) and user, determine the list of overrides, apply them to the
   # assignment or quiz, and return the overridden stand-in.
-  def self.assignment_overridden_for(assignment_or_quiz, user)
+  # pass skip_clone if you don't really care about the override attributes, and
+  # it's okay to get back the passed in object - that you promise not to modify -
+  # if there are no overrides
+  def self.assignment_overridden_for(assignment_or_quiz, user, skip_clone: false)
     return assignment_or_quiz if assignment_or_quiz.overridden_for?(user)
 
     # this is a cheap hack to avoid unnecessary work (especially stupid
     # simply_versioned queries)
     if assignment_or_quiz.has_no_overrides
+      return assignment_or_quiz if skip_clone
       return setup_overridden_clone(assignment_or_quiz)
     end
 
