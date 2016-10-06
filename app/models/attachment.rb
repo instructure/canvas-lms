2a7a4d0c72d82f771d1cdd79c0cf02600475c5ed
James Williams <jamesw@instructure.com>
2016-10-06 09:56:19 -0600

    add a singleton to attachment thumbnail processing job
    should help avoid duplicate thumbnail insertion race conditions
    
    closes #CNVS-3065
    
    Change-Id: If38ff0103ec24d6c20dcfeb5ac8d30ddec6d8aff
    Reviewed-on: https://gerrit.instructure.com/92156
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -211,7 +211,9 @@ class Attachment < ActiveRecord::Base
     # try an infer encoding if it would be useful to do so
     send_later(:infer_encoding) if self.encoding.nil? && self.content_type =~ /text/ && self.context_type != 'SisBatch'
     if respond_to?(:process_attachment_with_processing, true) && thumbnailable? && !attachment_options[:thumbnails].blank? && parent_id.nil?
-      self.class.attachment_options[:thumbnails].each { |suffix, size| send_later_if_production(:create_thumbnail_size, suffix) }
+      self.class.attachment_options[:thumbnails].each do |suffix, size|
+        send_later_if_production_enqueue_args(:create_thumbnail_size, {:singleton => "attachment_thumbnail_#{self.global_id}_#{suffix}"}, suffix)
+      end
     end
   end
 
