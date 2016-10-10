8a192a35203e1f53cfa40efd872dcd47ee67c8c4
Cody Cutrer <cody@instructure.com>
2016-10-10 12:51:41 -0600

    bump redis
    Change-Id: I2ab0de44c6ab4d470e3066da16aab6bf5b16b9ef
    Reviewed-on: https://gerrit.instructure.com/92434
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/redis.rb
+++ Gemfile.d/redis.rb
@@ -1,6 +1,6 @@
 group :redis do
   gem 'redis-store', '1.1.4', github: 'ccutrer/redis-store', ref: '72db36c56c6563fc65f213dcf8a1b77ddd22d1bb'
-  gem 'redis', '3.1.0'
+  gem 'redis', '3.3.1'
   gem 'redis-rails', '4.0.0'
   gem 'redis-scripting', '1.0.1'
 end
