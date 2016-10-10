40e25a0f9be77962058485a18913d9472a40e3bb
Cody Cutrer <cody@instructure.com>
2016-10-10 13:06:03 -0600

    bump stackprof
    Change-Id: I70d33e320abe2f0d5bb83874193ea85645f9343a
    Reviewed-on: https://gerrit.instructure.com/92400
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -77,7 +77,7 @@ gem 'parallel', '1.6.2', require: false
   gem 'ruby-progressbar', '1.7.5', require: false #used to show progress of S3Uploader
 gem 'rack-mini-profiler', '0.9.2', require: false
 gem 'flamegraph', '0.1.0', require: false
-  gem 'stackprof', '0.2.7', require: false
+  gem 'stackprof', '0.2.9', require: false
 gem 'rake', '11.3.0'
 gem 'ratom-nokogiri', '0.10.4', require: false
 gem 'rdiscount', '1.6.8', require: false
