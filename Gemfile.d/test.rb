f2fb5e1a8de517b905e3a7c537b7999f8a09c8ab
Jon Jensen <jon@instructure.com>
2016-10-10 13:38:41 -0600

    Revert "bump rspec"
    this turned test-queue + rspec into a no-op
    
    This reverts commit 9b01cb3140d230dc673713c1d891a3499b7af410.

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -13,8 +13,8 @@ group :test do
   gem 'thin', '1.7.0'
     gem 'eventmachine', '1.2.0.1', require: false
 
-  gem 'rspec', '3.5.0'
-  gem 'rspec-rails', '3.5.2'
+  gem 'rspec', '3.4.0'
+  gem 'rspec-rails', '3.4.1'
   gem 'rspec-collection_matchers', '1.1.2'
   gem 'shoulda-matchers', '2.8.0'
 
