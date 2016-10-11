61a2134bef1ee089b39d2e408d6c0c2e5095772a
Cody Cutrer <cody@instructure.com>
2016-10-11 10:17:52 -0600

    bump dress_code dependencies
    Change-Id: Ie7eb666c675c857511c213ea868b8daa4d4300b9
    Reviewed-on: https://gerrit.instructure.com/92495
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/assets.rb
+++ Gemfile.d/assets.rb
@@ -2,8 +2,8 @@ group :assets do
   gem 'dress_code', '1.0.2', github: "colleenpalmer/dress_code"
     gem 'colored', '1.2', require: false
     gem 'mustache', '1.0.3', require: false
-    gem 'pygments.rb', '0.5.4', require: false
-      gem 'posix-spawn', '0.3.9', require: false
-      gem 'yajl-ruby', '1.1.0', require: false
+    gem 'pygments.rb', '0.6.3', require: false
+      gem 'posix-spawn', '0.3.11', require: false
+      gem 'yajl-ruby', '1.2.1', require: false
   gem 'fontcustom', '~> 1.3.3', require: false
 end
