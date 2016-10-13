8a854ad6f5b144bafe3d36eb01fe0a628d2ab871
Rob Orton <rob@instructure.com>
2016-10-13 17:35:40 -0600

    make migration work with local storage
    fixes CNVS-32654
    
    test plan
     - with local storage
     - run migrations
     - it should work 
    
    Change-Id: I923f7f1c9eb034c9b8745e16a5e24b50bcb39aba
    Reviewed-on: https://gerrit.instructure.com/92829
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- lib/data_fixup/ensure_root_attachment_filename.rb
+++ lib/data_fixup/ensure_root_attachment_filename.rb
@@ -4,8 +4,14 @@ module DataFixup::EnsureRootAttachmentFilename
       Attachment.where(root_attachment_id: nil).
         joins("INNER JOIN #{Attachment.quoted_table_name} child ON attachments.id = child.root_attachment_id AND attachments.filename <> child.filename").
         where.not(workflow_state: 'broken').find_each do |root|
-        unless root.s3object.exists?
-          try_children root
+        if Attachment.s3_storage?
+          unless root.s3object.exists?
+            try_s3_children root
+          end
+        else
+          unless File.exist? root.full_filename
+            try_local_children root
+          end
         end
       end
       Attachment.where.not(root_attachment_id: nil, filename: nil, workflow_state: 'broken').find_in_batches do |batch|
@@ -16,14 +22,27 @@ module DataFixup::EnsureRootAttachmentFilename
     end
   end
 
-  def self.try_children(root)
+  def self.try_s3_children(root)
     root.children.where.not(filename: nil).where.not(filename: root.filename).each do |child|
       if child.s3object.exists?
-        Shackles.activate(:master) do
-          root.update_attribute(:filename, child.filename)
-        end
+        update_root_from_child(child, root)
         return true
       end
+    end
+  end
+
+  def self.try_local_children(root)
+    root.children.where.not(filename: nil).where.not(filename: root.filename).each do |child|
+      if File.exist? child.full_filename
+        update_root_from_child(child, root)
+        return true
+      end
+    end
+  end
+
+  def self.update_root_from_child(child, root)
+    Shackles.activate(:master) do
+      root.update_attribute(:filename, child.filename)
     end
   end
 end
