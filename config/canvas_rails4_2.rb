b4320585760a071d3a45aa5243b653d8224b34c8
James Williams <jamesw@instructure.com>
2016-10-14 13:14:40 -0600

    make rails 4.2 the default
    can explicitly switch back with CANVAS_RAILS4_2=0
    
    Change-Id: I5dc5b0146028bafc9c152cb9909b287494d0ed2e
    Reviewed-on: https://gerrit.instructure.com/92882
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- config/canvas_rails4_2.rb
+++ config/canvas_rails4_2.rb
@@ -1,9 +1,9 @@
-# You can enable the Rails 4.2 support by either defining a
-# CANVAS_RAILS4_2=1 env var, or create an empty RAILS4_2 file in the canvas config dir
+# You can disable the Rails 4.2 support by defining a
+# CANVAS_RAILS4_2=0 env var
 if !defined?(CANVAS_RAILS4_0)
   if ENV['CANVAS_RAILS4_2']
-    CANVAS_RAILS4_0 = ENV['CANVAS_RAILS4_2'] != '1'
+    CANVAS_RAILS4_0 = ENV['CANVAS_RAILS4_2'] == '0'
   else
-    CANVAS_RAILS4_0 = !File.exist?(File.expand_path("../RAILS4_2", __FILE__))
+    CANVAS_RAILS4_0 = false
   end
 end
