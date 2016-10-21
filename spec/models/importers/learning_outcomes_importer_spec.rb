915f2138ee75646cce2fa9f3387a1e2adbecc720
Matthew Berns <mberns@instructure.com>
2016-10-21 15:33:05 -0500

    fix re-importing of state outcomes
    fixes OUT-427
    
    test plan:
    - set up Academic Benchmark importing
    - import state standards
    - once the first import is complete,
      attempt to import again
    - the import should happen successfully,
      check error logs to ensure there's
      no alerts for invalid calculation method
      or int
    
    Change-Id: I9946d556e661e7a3116b894829bb03fb09870f52
    Reviewed-on: https://gerrit.instructure.com/93442
    Tested-by: Jenkins
    Reviewed-by: Michael Brewer-Davis <mbd@instructure.com>
    QA-Review: Cemal Aktas <caktas@instructure.com>
    Product-Review: Josh Simpson <jsimpson@instructure.com>

--- spec/models/importers/learning_outcomes_importer_spec.rb
+++ spec/models/importers/learning_outcomes_importer_spec.rb
@@ -19,24 +19,33 @@
 require File.expand_path(File.dirname(__FILE__) + '../../../import_helper')
 
 describe "Importing Learning Outcomes" do
+  before :once do
+    @context = course_model
+    @migration = ContentMigration.create!(:context => @context)
+    @migration.migration_ids_to_import = {:copy=>{}}
+    @data = get_import_data [], 'outcomes'
+    @data = {'learning_outcomes'=>@data}
+
+    Importers::LearningOutcomeImporter.process_migration(@data, @migration)
+  end
+
   it "should import" do
-    context = course_model
-    migration = ContentMigration.create!(:context => context)
-    migration.migration_ids_to_import = {:copy=>{}}
-    data = get_import_data [], 'outcomes'
-    data = {'learning_outcomes'=>data}
-
-    Importers::LearningOutcomeImporter.process_migration(data, migration)
-    Importers::LearningOutcomeImporter.process_migration(data, migration)
-
-    expect(context.learning_outcomes.count).to eq 2
     lo1 = LearningOutcome.where(migration_id: "bdf6dc13-5d8f-43a8-b426-03380c9b6781").first
     expect(lo1.description).to eq "Outcome 1: Read stuff"
     lo2 = LearningOutcome.where(migration_id: "fa67b467-37c7-4fb9-aef4-21a33a06d0be").first
     expect(lo2.description).to eq "Outcome 2: follow directions"
+    log = @context.root_outcome_group
 
-    log = context.root_outcome_group
+    expect(@context.learning_outcomes.count).to eq 2
     expect(log.child_outcome_links.detect{ |link| link.content == lo1 }).not_to be_nil
     expect(log.child_outcome_links.detect{ |link| link.content == lo2 }).not_to be_nil
+  end
+
+  it "should not fail when passing an outcome that already exists" do
+    existing_outcome = LearningOutcome.where(migration_id: "bdf6dc13-5d8f-43a8-b426-03380c9b6781").first
+    identifier = existing_outcome.migration_id
+    lo_data = @data["learning_outcomes"].find{|lo| lo["migration_id"] == identifier }
+    Importers::LearningOutcomeImporter.import_from_migration(lo_data, @migration, existing_outcome)
+    expect(@context.learning_outcomes.count).to eq 2
   end
 end
