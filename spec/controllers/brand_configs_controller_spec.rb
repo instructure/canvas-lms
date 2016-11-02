3616d2a0a48f76ec03544b410bff4c1126a43e23
Simon Williams <simon@instructure.com>
2016-11-01 20:58:14 -0600

    add tests for brand_configs_controller
    test plan: specs should pass
    
    Change-Id: I433371f421ac755a20ed28f3db316835ae53c2e1
    Reviewed-on: https://gerrit.instructure.com/94200
    Tested-by: Jenkins
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- spec/controllers/brand_configs_controller_spec.rb
+++ spec/controllers/brand_configs_controller_spec.rb
@@ -24,18 +24,34 @@ describe BrandConfigsController do
     @bc = BrandConfig.create(variables: {"ic-brand-primary" => "#321"})
   end
 
-  describe '#new' do
-    it "should allow authorized admin to create" do
+  describe '#index' do
+    it "should allow authorized admin to view" do
       admin = account_admin_user(account: @account)
       user_session(admin)
-      post 'new', {brand_config: @bc, account_id: @account.id}
+      get 'index', account_id: @account.id
+      assert_status(200)
+    end
+
+    it 'should not allow non admin access' do
+      user = user_with_pseudonym(active_all: true)
+      user_session(user)
+      get 'index', account_id: @account.id
+      assert_status(401)
+    end
+  end
+
+  describe '#new' do
+    it "should allow authorized admin to see create" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      get 'new', {brand_config: @bc, account_id: @account.id}
       assert_status(200)
     end
 
     it "should not allow non admin access" do
       user = user_with_pseudonym(active_all: true)
       user_session(user)
-      post 'new', {brand_config: @bc, account_id: @account.id}
+      get 'new', {brand_config: @bc, account_id: @account.id}
       assert_status(401)
     end
 
@@ -68,6 +84,102 @@ describe BrandConfigsController do
 
       primary = vars.detect{|v| v["variable_name"] == "ic-brand-primary"}
       expect(primary["default"]).to eq "#321"
+    end
+  end
+
+  describe '#create' do
+    let (:bcin) { { variables: { "ic-brand-primary" => "#000000" } } }
+
+    it "should allow authorized admin to create" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      post 'create', account_id: @account.id, brand_config: bcin
+      assert_status(200)
+      json = JSON.parse(response.body)
+      expect(json['brand_config']['variables']['ic-brand-primary']).to eq "#000000"
+    end
+
+    it 'should not allow non admin access' do
+      user = user_with_pseudonym(active_all: true)
+      user_session(user)
+      post 'create', account_id: @account.id, brand_config: bcin
+      assert_status(401)
+    end
+
+    it 'should return an existing brand config' do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      post 'create', account_id: @account.id, brand_config: { 
+        variables: {
+          "ic-brand-primary" => "#321"
+        }
+      }
+      assert_status(200)
+      json = JSON.parse(response.body)
+      expect(json['brand_config']['md5']).to eq @bc.md5
+    end
+  end
+
+  describe '#destroy' do
+    it "should allow authorized admin to create" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      session[:brand_config_md5] = @bc.md5
+      delete 'destroy', account_id: @account.id
+      assert_status(302)
+      expect(session[:brand_config_md5]).to be_nil
+      expect { @bc.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'should not allow non admin access' do
+      user = user_with_pseudonym(active_all: true)
+      user_session(user)
+      delete 'destroy', account_id: @account.id
+      assert_status(401)
+    end
+  end
+
+  describe '#save_to_account' do
+    it "should allow authorized admin to create" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      post 'save_to_account', account_id: @account.id
+      assert_status(200)
+    end
+
+    it 'should not allow non admin access' do
+      user = user_with_pseudonym(active_all: true)
+      user_session(user)
+      post 'save_to_account', account_id: @account.id
+      assert_status(401)
+    end
+  end
+
+  describe '#save_to_user_session' do
+    it "should allow authorized admin to create" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      post 'save_to_user_session', account_id: @account.id, brand_config_md5: @bc.md5
+      assert_status(302)
+      expect(session[:brand_config_md5]).to eq @bc.md5
+    end
+
+    it "should allow authorized admin to remove" do
+      admin = account_admin_user(account: @account)
+      user_session(admin)
+      session[:brand_config_md5] = @bc.md5
+      post 'save_to_user_session', account_id: @account.id, brand_config_md5: ''
+      assert_status(302)
+      expect(session[:brand_config_md5]).to eq false
+      expect { @bc.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'should not allow non admin access' do
+      user = user_with_pseudonym(active_all: true)
+      user_session(user)
+      post 'save_to_user_session', account_id: @account.id, brand_config_md5: @bc.md5
+      assert_status(401)
+      expect(session[:brand_config_md5]).to be_nil
     end
   end
 end
