fdab70ab84c8d9dd5af463e400b95bd3fb3d0eda
Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
2016-11-08 12:24:20 -0700

    fix appointment slot student name + available status
    fixes CNVS-33168 CNVS-33167
    
    test plan:
    - create an appointment group with a few slots and limit the number
     of students per slot to 1
    - sign a unique student up for each slot
    - as the teacher, go to the appointment group's edit page
    - notice each appointment slot shows the correct student name
    - notice none of full appointment slots show 'Available'
    
    Change-Id: I1a484a3150e4814441dba8b6755f2721c84b8568
    Reviewed-on: https://gerrit.instructure.com/94711
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Heath Hales <hhales@instructure.com>
    Product-Review: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>

--- app/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupList.jsx
+++ app/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupList.jsx
@@ -1,65 +1,68 @@
 define([
   'react',
+  'classnames',
   'i18n!appointment_groups',
   'jsx/shared/FriendlyDatetime',
+  'compiled/util/natcompare',
   'jquery',
   'jquery.instructure_date_and_time'
-], (React, I18n, FriendlyDatetime, $) => {
-
-  class AppointmentGroupList extends React.Component {
-
+], (React, classnames, I18n, FriendlyDatetime, natcompare, $) => {
+  return class AppointmentGroupList extends React.Component {
     static propTypes = {
       appointmentGroup: React.PropTypes.object,
     }
 
-    renderEvent () {
-      if (this.props.appointmentGroup.appointments_count) {
-        return this.props.appointmentGroup.appointments.map((c, index) => {
-          if (!c.reserved) {
-            if (c.child_events.length) {
-              const names = this.props.appointmentGroup.appointments[0].child_events.map((child_event) => child_event.user.sortable_name)
-              const sorted = names.sort((a, b) => natcompare.strings(a, b))
+    renderAppointment (appointment, statusLabel) {
+      const timeLabel = I18n.t('%{start_time} to %{end_time}', { start_time: $.timeString(appointment.start_at), end_time: $.timeString(appointment.end_at) })
+      const label = statusLabel ? `${timeLabel} - ${statusLabel}` : timeLabel
+
+      const reservedClass = classnames({
+        AppointmentGroupList__unreserved: !appointment.reserved,
+        AppointmentGroupList__reserved: appointment.reserved,
+      })
+
+      return (
+        <div key={appointment.id} className="AppointmentGroupList__Appointment">
+          <div className={reservedClass}>
+            <i className="icon-calendar-month" />
+            <span className="pad-box-micro AppointmentGroupList__Appointment-label">{label}</span>
+          </div>
+        </div>
+      )
+    }
+
+    renderAppointmentList () {
+      const { appointmentGroup } = this.props
+      return (appointmentGroup.appointments || []).map((appointment) => {
+        let statusLabel = null
+
+        if (!appointment.reserved) {
+          if (appointment.child_events.length) {
+            const names = appointment.child_events.map(event => event.user.sortable_name)
+            const sorted = names.sort((a, b) => natcompare.strings(a, b))
+            const maxParticipants = appointmentGroup.participants_per_appointment
+
+            // if there's no limit or we are below the limit, show Available
+            if (!maxParticipants || maxParticipants > appointment.child_events_count) {
               sorted.push(I18n.t('Available'))
-              return (
-                <div key={c.id} className='AppointmentGroupList__Appointment'>
-                  <div className='AppointmentGroupList__unreserved'>
-                    <i className="icon-calendar-month"></i>
-                    {I18n.t('%{start_time} to %{end_time}', {start_time: $.timeString(c.start_at), end_time: $.timeString(c.end_at)})} - {sorted.join('; ')}
-                  </div>
-                </div>
-              )
-            } else {
-              return (
-                <div key={c.id} className='AppointmentGroupList__Appointment'>
-                  <div className='AppointmentGroupList__unreserved'>
-                    <i className="icon-calendar-month"></i>
-                    {I18n.t('%{start_time} to %{end_time}', {start_time: $.timeString(c.start_at), end_time: $.timeString(c.end_at)})} - {I18n.t('Available')}
-                  </div>
-                </div>
-              )
             }
+
+            statusLabel = sorted.join('; ')
           } else {
-            return (
-              <div key={c.id} className='AppointmentGroupList__Appointment'>
-                <div className='AppointmentGroupList__reserved'>
-                  <i className="icon-calendar-month"></i>
-                  {I18n.t('%{start_time} to %{end_time}', {start_time: $.timeString(c.start_at), end_time: $.timeString(c.end_at)})}
-                </div>
-              </div>
-            )
+            statusLabel = I18n.t('Available')
           }
-        })
-      }
+        }
+
+        return this.renderAppointment(appointment, statusLabel)
+      })
     }
 
     render () {
       return (
-        <div className='AppointmentGroupList__List'>
-          {this.renderEvent()}
+        <div className="AppointmentGroupList__List">
+          {this.renderAppointmentList()}
         </div>
       )
     }
   }
-
-  return AppointmentGroupList
 })
--- spec/javascripts/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupListSpec.jsx
+++ spec/javascripts/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupListSpec.jsx
@@ -38,4 +38,14 @@ define([
     const appointmentGroupReserved = TestUtils.findRenderedDOMComponentWithClass(component, 'AppointmentGroupList__reserved')
     ok(appointmentGroupReserved)
   })
+
+  test('renders correct user names', () => {
+    const appointmentGroup = {"appointments": [{"child_events": [{"user": {"sortable_name": "test1"}}], "start_at": "2016-10-18T19:00:00Z", "end_at": "2016-10-18T110:00:00Z"}, {"child_events": [{"user": {"sortable_name": "test2"}}], "start_at": "2016-10-18T16:00:00Z", "end_at": "2016-10-18T17:00:00Z"}], "appointments_count": 2, participants_per_appointment: 1}
+
+    const component = TestUtils.renderIntoDocument(<AppointmentGroupList appointmentGroup={appointmentGroup}/>)
+    const appointmentGroupNames = TestUtils.scryRenderedDOMComponentsWithClass(component, 'AppointmentGroupList__Appointment-label').map(nameComp => ReactDOM.findDOMNode(nameComp))
+    equal(appointmentGroupNames.length, 2)
+    equal(appointmentGroupNames[0].textContent.split(' - ')[1], 'test1')
+    equal(appointmentGroupNames[1].textContent.split(' - ')[1], 'test2')
+  })
 })
