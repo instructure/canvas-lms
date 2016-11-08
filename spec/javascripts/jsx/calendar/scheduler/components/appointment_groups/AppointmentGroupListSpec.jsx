3095cd6ff00324b91803bb14e3b26ae8a05b6c23
Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
2016-11-08 12:24:20 -0700

    fix appointment slot student name + available status
    fixes CNVS-33168 CNVS-33167
    
    test plan:
    - create an appointment group with a few slots and limit the number
     of students per slot to 1
    - sign a unique student up for each slot
    - as the teacher, go to the appointment group's edit page
    - notice each appointment slot shows the correct student name
    - notice none of full appointment slots show 'Available'
    
    Change-Id: I1a484a3150e4814441dba8b6755f2721c84b8568
    Reviewed-on: https://gerrit.instructure.com/94711
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Heath Hales <hhales@instructure.com>
    Product-Review: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>

--- spec/javascripts/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupListSpec.jsx
+++ spec/javascripts/jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupListSpec.jsx
@@ -1,8 +1,9 @@
 define([
   'react',
+  'react-dom',
   'react-addons-test-utils',
   'jsx/calendar/scheduler/components/appointment_groups/AppointmentGroupList',
-], (React, TestUtils, AppointmentGroupList) => {
+], (React, ReactDOM, TestUtils, AppointmentGroupList) => {
   module('AppointmentGroupList')
 
   test('renders the AppointmentGroupList component', () => {
@@ -36,5 +37,15 @@ define([
     const component = TestUtils.renderIntoDocument(<AppointmentGroupList appointmentGroup={appointmentGroup}/>)
     const appointmentGroupReserved = TestUtils.findRenderedDOMComponentWithClass(component, 'AppointmentGroupList__reserved')
     ok(appointmentGroupReserved)
+  })
+
+  test('renders correct user names', () => {
+    const appointmentGroup = {"appointments": [{"child_events": [{"user": {"sortable_name": "test1"}}], "start_at": "2016-10-18T19:00:00Z", "end_at": "2016-10-18T110:00:00Z"}, {"child_events": [{"user": {"sortable_name": "test2"}}], "start_at": "2016-10-18T16:00:00Z", "end_at": "2016-10-18T17:00:00Z"}], "appointments_count": 2, participants_per_appointment: 1}
+
+    const component = TestUtils.renderIntoDocument(<AppointmentGroupList appointmentGroup={appointmentGroup}/>)
+    const appointmentGroupNames = TestUtils.scryRenderedDOMComponentsWithClass(component, 'AppointmentGroupList__Appointment-label').map(nameComp => ReactDOM.findDOMNode(nameComp))
+    equal(appointmentGroupNames.length, 2)
+    equal(appointmentGroupNames[0].textContent.split(' - ')[1], 'test1')
+    equal(appointmentGroupNames[1].textContent.split(' - ')[1], 'test2')
   })
 })
