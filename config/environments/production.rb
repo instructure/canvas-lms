9cff199c189dadba8cce0a3b1a58171dda088fd9
Cody Cutrer <cody@instructure.com>
2016-11-11 13:58:05 -0700

    rename renamed rails option
    Change-Id: I1d28d271a1ce824c0aecd4432f381b2a39e0fa22
    Reviewed-on: https://gerrit.instructure.com/95063
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ environment_configuration(defined?(config) && config) do |config|
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
