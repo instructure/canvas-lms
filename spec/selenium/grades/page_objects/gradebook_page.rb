4a9521965386ba63d5ef1a56211233e6690488c8
KC Naegle <knaegle@instructure.com>
2016-11-12 14:09:07 -0700

    spec: add srgb final grade spec
    add C615710 and partial refactor for srgb_page
    
    Change-Id: If863977f9197e29021fffda73689711ec354594a
    Reviewed-on: https://gerrit.instructure.com/95873
    Tested-by: Jenkins
    Reviewed-by: Shahbaz Javeed <sjaveed@instructure.com>
    Reviewed-by: Alex Morris <amorris@instructure.com>
    Product-Review: Gentry Beckmann <gbeckmann@instructure.com>
    QA-Review: Gentry Beckmann <gbeckmann@instructure.com>

--- spec/selenium/grades/page_objects/gradebook_page.rb
+++ spec/selenium/grades/page_objects/gradebook_page.rb
@@ -58,7 +58,7 @@ module Gradebook
       end
 
       def visit_gradebook(course)
-        get "/courses/#{course.id}/gradebook2"
+        get "/courses/#{course.id}/gradebook/change_gradebook_version?version=2"
       end
 
       def select_grading_period(grading_period_id)
@@ -78,7 +78,7 @@ module Gradebook
 
       def cell_graded?(grade, x_coordinate, y_coordinate)
         cell = grading_cell(x_coordinate, y_coordinate)
-        if (cell.text == grade)
+        if cell.text == grade
           return true
         else
           return false
--- spec/selenium/grades/srgb/srgb_global_checkboxes_spec.rb
+++ spec/selenium/grades/srgb/srgb_global_checkboxes_spec.rb
@@ -1,61 +1,51 @@
 require_relative '../../helpers/gradebook2_common'
-require_relative '../../helpers/gradebook2_srgb_common'
+require_relative '../page_objects/srgb_page'
 
 describe "Screenreader Gradebook" do
   include_context 'in-process server selenium tests'
   include_context 'gradebook_components'
-  include_context 'srgb_components'
   include_context 'reusable_course'
   include Gradebook2Common
-  include Gradebook2SRGBCommon
+
+  let(:srgb_page) { SRGB }
 
   let(:course_setup) do
     enroll_teacher_and_students
     assignment_1
     assignment_2
     student_submission
-  end
-  let(:login_to_srgb) do
-    user_session(teacher)
-    get "/courses/#{test_course.id}/gradebook/change_gradebook_version?version=srgb"
-    select_student(student)
+    assignment_1.grade_student(student, grade: 10, grader: teacher)
   end
 
   before(:each) do
     course_setup
+    user_session(teacher)
+    srgb_page.visit(test_course.id)
+    srgb_page.select_student(student)
   end
 
   it 'toggles ungraded as 0 with correct grades', priority: "2", test_id: 615672 do
-    assignment_1.grade_student(student, grade: 10)
+    srgb_page.select_assignment(assignment_1)
+    srgb_page.ungraded_as_zero.click
+    expect(srgb_page.final_grade).to include_text('50%')
 
-    login_to_srgb
-    select_assignment(assignment_1)
-    ungraded_as_zero.click
-    expect(final_grade).to include_text('50%')
-
-    ungraded_as_zero.click
-    expect(final_grade).to include_text('100%')
+    srgb_page.ungraded_as_zero.click
+    expect(srgb_page.final_grade).to include_text('100%')
   end
 
   it 'hides student names', priority: "2", test_id: 615673 do
-    login_to_srgb
-    hide_student_names.click
-
-    expect(secondary_id_label).to include_text('hidden')
+    srgb_page.hide_student_names.click
+    expect(srgb_page.secondary_id_label).to include_text('hidden')
   end
 
   it 'shows conluded enrollments', priority: "2", test_id: 615674 do
-    login_to_srgb
-    concluded_enrollments.click
+    srgb_page.concluded_enrollments.click
     wait_for_ajaximations
-
-    expect(student_dropdown).to include_text('Stewie Griffin')
+    expect(srgb_page.student_dropdown).to include_text('Stewie Griffin')
   end
 
   it 'shows notes in student info', priority: "2", test_id: 615675 do
-    login_to_srgb
-    show_notes_option.click
-
-    expect(notes_field).to be_present
+    srgb_page.show_notes_option.click
+    expect(srgb_page.notes_field).to be_present
   end
 end
