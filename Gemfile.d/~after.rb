5fc6c0a62ee598078cab85a34ed8752cdb6b0199
Cody Cutrer <cody@instructure.com>
2016-11-16 19:34:57 -0700

    fix Gemfile.d for plugins
    Change-Id: I96b8f208c2daff56657bf08f76b14d18d6108eb3
    Reviewed-on: https://gerrit.instructure.com/95471
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/~after.rb
+++ Gemfile.d/~after.rb
@@ -1,5 +1,5 @@
 # Non-standard Canvas extension to Bundler behavior -- load the Gemfiles from
 # plugins.
-Dir[File.join(File.dirname(__FILE__), 'gems/plugins/*/Gemfile.d/*')].each do |g|
+Dir[File.join(File.dirname(__FILE__), '../gems/plugins/*/Gemfile.d/*')].each do |g|
   eval(File.read(g), nil, g)
 end
