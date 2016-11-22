4b512655eb53393c8872842ef5c1974bed48d1a4
KC Naegle <knaegle@instructure.com>
2016-11-21 17:05:47 -0700

    spec: Add speedgrader_page
    Change-Id: I2787dbea33899545452a154224393ebb82b0338c
    Reviewed-on: https://gerrit.instructure.com/95885
    Reviewed-by: Gentry Beckmann <gbeckmann@instructure.com>
    Product-Review: Gentry Beckmann <gbeckmann@instructure.com>
    QA-Review: Gentry Beckmann <gbeckmann@instructure.com>
    Tested-by: Jenkins

---
+++ spec/selenium/grades/page_objects/speedgrader_page.rb
@@ -0,0 +1,24 @@
+class Speedgrader
+  class << self
+    include SeleniumDriverSetup
+    include OtherHelperMethods
+    include CustomSeleniumActions
+    include CustomAlertActions
+    include CustomPageLoaders
+    include CustomScreenActions
+    include CustomValidators
+    include CustomWaitMethods
+    include CustomDateHelpers
+    include LoginAndSessionMethods
+    include SeleniumErrorRecovery
+
+    def grade_value() f('#grade_container input[type=text]').attribute('value') end
+
+    def fraction_graded() f("#x_of_x_graded") end
+
+    def visit(course, assignment)
+      get "/courses/#{course.id}/gradebook/speed_grader?assignment_id=#{assignment.id}"
+    end
+  end
+end
+
