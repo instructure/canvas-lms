86939d90da9b7b798daaaec9e6378910c5f80337
KC Naegle <knaegle@instructure.com>
2016-11-22 17:12:42 -0700

    spec: add speedgrader displayes average grade spec
    Change-Id: I4d5a4ece386343c769f048f6744b789f0aa1b931
    Reviewed-on: https://gerrit.instructure.com/95987
    Tested-by: Jenkins
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    Reviewed-by: David Tan <dtan@instructure.com>
    Product-Review: Gentry Beckmann <gbeckmann@instructure.com>
    QA-Review: Gentry Beckmann <gbeckmann@instructure.com>

--- spec/selenium/grades/page_objects/speedgrader_page.rb
+++ spec/selenium/grades/page_objects/speedgrader_page.rb
@@ -12,9 +12,17 @@ class Speedgrader
     include LoginAndSessionMethods
     include SeleniumErrorRecovery
 
-    def grade_value() f('#grade_container input[type=text]').attribute('value') end
+    def grade_value
+      f('#grade_container input[type=text]').attribute('value')
+    end
 
-    def fraction_graded() f("#x_of_x_graded") end
+    def fraction_graded
+      f("#x_of_x_graded")
+    end
+
+    def average_grade
+      f("#average_score")
+    end
 
     def visit(course, assignment)
       get "/courses/#{course.id}/gradebook/speed_grader?assignment_id=#{assignment.id}"
--- spec/selenium/grades/speedgrader/speedgrader_grade_display_spec.rb
+++ spec/selenium/grades/speedgrader/speedgrader_grade_display_spec.rb
@@ -6,26 +6,28 @@ describe "speed grader - grade display" do
   include_context "in-process server selenium tests"
   include SpeedGraderCommon
 
+  POINTS = 10.0
+  GRADE = 3.0
+
   before(:each) do
     course_with_teacher_logged_in
-    @assignment = @course.assignments.create(name: 'assignment', points_possible: 10)
+    create_and_enroll_students(2)
+    @assignment = @course.assignments.create(name: 'assignment', points_possible: POINTS)
+    @assignment.submit_homework(@students[0])
+    @assignment.grade_student(@students[0], grade: GRADE, grader: @teacher)
+    Speedgrader.visit(@course, @assignment)
   end
 
   it "displays the score on the sidebar", priority: "1", test_id: 283993 do
-    create_and_enroll_students(1)
-    submit_and_grade_homework(@students[0], 3)
-
-    Speedgrader.visit(@course, @assignment)
-    expect(Speedgrader.grade_value).to eq "3"
+    expect(Speedgrader.grade_value).to eq GRADE.to_int.to_s
   end
 
   it "displays total number of graded assignments to students", priority: "1", test_id: 283994 do
-    create_and_enroll_students(2)
-    submit_and_grade_homework(@students[0], 3)
-
-    Speedgrader.visit(@course, @assignment)
     expect(Speedgrader.fraction_graded).to include_text("1/2")
   end
 
-  it "displays average submission grade for total assignment submissions", priority: "1", test_id: 283995
+  it "displays average submission grade for total assignment submissions", priority: "1", test_id: 283995 do
+    average = (GRADE / POINTS * 100).to_int
+    expect(Speedgrader.average_grade).to include_text("#{GRADE.to_int} / #{POINTS.to_int} (#{average}%)")
+  end
 end
