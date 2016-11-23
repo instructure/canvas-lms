b550841176b962769ea976a5f601dfd5acc09edc
Clay Diffrient <cdiffrient@instructure.com>
2016-11-23 08:32:36 -0700

    Make axios request stringified ids
    closes CNVS-33522
    
    Test Plan:
      - Automated tests pass
      - Requesting an id from the API with axios should
        return a stringified ID.
    
    Change-Id: I10a9cd0a7359612c7686f22a9e2adbedc2d77ac7
    Reviewed-on: https://gerrit.instructure.com/96012
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>

---
+++ spec/javascripts/jsx/axiosSpec.jsx
@@ -0,0 +1,26 @@
+define(['axios', 'moxios'], (axios, moxios) => {
+  module('Custom Axios Tests', {
+    setup () {
+      moxios.install();
+    },
+    teardown () {
+      moxios.uninstall();
+    }
+  });
+
+  test('Accept headers request stringified ids', (assert) => {
+    const done = assert.async();
+
+    moxios.stubRequest('/some/url', {
+      status: 200,
+      responseText: 'hello'
+    });
+
+    axios.get('/some/url').then((response) => {
+      ok(response.config.headers.Accept.includes('application/json+canvas-string-ids'));
+      done();
+    });
+
+    moxios.wait();
+  });
+});
