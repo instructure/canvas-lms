29cb7a65a4c916a59d00685b52eeb26eff328e91
Cemal Aktas <caktas@instructure.com>
2016-11-30 15:33:44 -0700

    spec: Move outcomes related tests in the outcomes test folder
    Test Plan:
     - Make sure you get +1 from jenkins
    
    Change-Id: I225d9e4fb4d1ec4b2acbef8ec099aea69fc93c7f
    Reviewed-on: https://gerrit.instructure.com/96430
    Tested-by: Jenkins
    Reviewed-by: Alex Ortiz-Rosado <aortiz@instructure.com>
    Product-Review: Alex Ortiz-Rosado <aortiz@instructure.com>
    QA-Review: Alex Ortiz-Rosado <aortiz@instructure.com>
    Tested-by: Alex Ortiz-Rosado <aortiz@instructure.com>

--- spec/selenium/outcomes/sub_account_outcomes_spec.rb
+++ spec/selenium/outcomes/sub_account_outcomes_spec.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../common')
-require File.expand_path(File.dirname(__FILE__) + '/../../helpers/outcome_common')
+require File.expand_path(File.dirname(__FILE__) + '/../common')
+require File.expand_path(File.dirname(__FILE__) + '/../helpers/outcome_common')
 
 describe "sub account outcomes" do
   include_context "in-process server selenium tests"
