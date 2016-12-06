43e64d7c5211e527d4b2b7e28b14ebbaf6adce9c
John Corrigan <jcorrigan@instructure.com>
2016-12-06 12:03:06 -0600

    add Rating component to StudentContextTray
    fixes FALCOR-48
    
    test plan:
    - Make sure that StudentContextTray is turned on.
    - Trigger Tray for a student as a teacher.
    - Observe that the Rating component shows up twice at the bottom of the
      Tray, once for participation level, once for page views level.
    
    Change-Id: I2158b6203a6a6fd1c9b4c8c7faa1a97cb279ef04
    Reviewed-on: https://gerrit.instructure.com/96852
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Tested-by: Jenkins
    Product-Review: John Corrigan <jcorrigan@instructure.com>

---
+++ spec/javascripts/jsx/context_cards/RatingSpec.jsx
@@ -0,0 +1,58 @@
+define([
+  'react',
+  'react-addons-test-utils',
+  'jsx/context_cards/Rating',
+  'instructure-ui/Rating'
+], (React, TestUtils, Rating, { default: InstUIRating }) => {
+
+  module('StudentContextTray/Rating', () => {
+    let subject
+    const participationsLevel = 2
+
+    module('valueNow', () => {
+      test('returns value associated with metricName', () => {
+        subject = TestUtils.renderIntoDocument(
+          <Rating
+            label='Participation'
+            metricName='participations_level'
+            analytics={{
+              participations_level: participationsLevel
+            }}
+          />
+        )
+
+        equal(subject.valueNow, participationsLevel)
+      })
+    })
+
+    module('formatValueText', () => {
+      subject = TestUtils.renderIntoDocument(
+        <Rating />
+      )
+      const valueText = [
+        'None', 'Low', 'Moderate', 'High'
+      ]
+      valueText.forEach((v, i) => {
+        test(`returns value ${v} for rating ${i}`, () => {
+          equal(subject.formatValueText(i, 3), v)
+        })
+      })
+    })
+
+    module('render', () => {
+      test('delegates to InstUIRating', () => {
+        subject = TestUtils.renderIntoDocument(
+          <Rating
+            label='Participation'
+            metricName='participations_level'
+            analytics={{
+              participations_level: participationsLevel
+            }}
+          />
+        )
+        const instUIRating = TestUtils.findRenderedComponentWithType(subject, InstUIRating)
+        equal(instUIRating.props.label, subject.props.label)
+      })
+    })
+  })
+})
