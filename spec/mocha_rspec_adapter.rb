87d6f6984a52ddd0337008db9d4136e271c4b691
Simon Williams <simon@instructure.com>
2016-12-06 21:40:15 -0700

    spec: support rspec-mocks syntax
    inspired by https://github.com/endeepak/rspec-multi-mock
    
    gotchas: rspec and mocha both define these methods, so you need to
    prefix rspec_ to get the rspec version for now:
    - anything (rspec_anything)
    - kind_of (rspec_kind_of)
    - instance_of (rspec_instance_of)
    
    closes CNVS-33712
    
    test plan: specs should pass
    
    Change-Id: I06b50e899681898f94a7ae8fe15f7dbb3eb40279
    Reviewed-on: https://gerrit.instructure.com/96934
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Benjamin Christian Nelson <bcnelson@instructure.com>
    Tested-by: Jenkins
    Product-Review: Simon Williams <simon@instructure.com>

--- spec/mocha_rspec_adapter.rb
+++ spec/mocha_rspec_adapter.rb
@@ -2,16 +2,50 @@ require 'mocha/api'
 
 module MochaRspecAdapter
   include Mocha::API
+  include ::RSpec::Mocks::ExampleMethods
+
   def setup_mocks_for_rspec
     mocha_setup
+    ::RSpec::Mocks.setup
   end
 
   def verify_mocks_for_rspec
     mocha_verify
+    ::RSpec::Mocks.verify
   end
 
   def teardown_mocks_for_rspec
     mocha_teardown
+    ::RSpec::Mocks.teardown
+  end
+
+  # mocha and rspec both define:
+  # - anything
+  # - kind_of
+  # - instance_of
+
+  def anything
+    ::Mocha::ParameterMatchers::Anything.new
+  end
+
+  def rspec_anything
+    ::RSpec::Mocks::ArgumentMatchers::AnyArgMatcher::INSTANCE
+  end
+
+  def kind_of(klass)
+    ::Mocha::ParameterMatchers::KindOf.new(klass)
+  end
+
+  def rspec_kind_of(klass)
+    ::RSpec::Mocks::ArgumentMatchers::KindOf.new(klass)
+  end
+
+  def instance_of(klass)
+    ::Mocha::ParameterMatchers::InstanceOf.new(klass)
+  end
+
+  def rspec_instance_of(klass)
+    ::RSpec::Mocks::ArgumentMatchers::InstanceOf.new(klass)
   end
 end
 
