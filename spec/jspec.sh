d2e0d92daa407d75e956ea9d3cd6a355b5a31a98
Dan Minkevitch <dan@instructure.com>
2016-12-13 11:01:46 -0800

    Add "Requires Webpack" messaging to jspec
    Closes CNVS-33839
    
    Test Plan:
    * Make sure Webpack isn't configured by removing
      config/WEBPACK (if it exists) and removing the
      USE_WEBPACK environment variable
    * Try using jspec a la:
      `npm run jspec spec/javascripts/some_spec`
    * It should throw an error and exit, stating that
      webpack needs to be enabled to work
    * Run `touch config/WEBPACK`
    * Retry the `jspec` command from above
    * It should run the spec without throwing an error
    
    Change-Id: Ia6260c442c847a8bed966f8bacc8a70fa22442d7
    Reviewed-on: https://gerrit.instructure.com/97487
    Tested-by: Jenkins
    Reviewed-by: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
    Product-Review: Dan Minkevitch <dan@instructure.com>
    QA-Review: Dan Minkevitch <dan@instructure.com>

--- spec/jspec.sh
+++ spec/jspec.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+NC="\033[0m" # No Color
+
+# Fail fast if Webpack not configured
+if [ ! -f config/WEBPACK ] && [[ ! ${USE_WEBPACK+x} ]]; then
+    echo -e "${RED}Webpack must be enabled to use jspec${NC}"
+    echo -e "${YELLOW}Run 'touch config/WEBPACK' to enable Webpack."
+    echo -e "For more information, see the following documentation:"
+    echo -e "  doc/working_with_webpack.md"
+    echo -e "  doc/testing_javascript.md${NC}"
+    exit
+fi
 
 export JSPEC_WD=$(pwd)
 
