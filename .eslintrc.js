77501e2688323c2ba4fa50e0ad454b29b68491eb
Clay Diffrient <cdiffrient@instructure.com>
2016-12-16 16:08:07 -0700

    [eslint] Improve setup for different JS levels
    This makes it so eslint handles situations where we don't have
    ES2015+ available, while still allowing us to have our own common
    rules.
    
    Change-Id: I86b49a32433970afbcc48ef0f6fcdd7301795b63
    Reviewed-on: https://gerrit.instructure.com/97872
    Tested-by: Jenkins
    Reviewed-by: Dan Minkevitch <dan@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "amd": true,
+    "browser": true
+  },
+  "extends": "airbnb",
+  "globals": {
+    "ENV": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "parser": "babel-eslint",
+  "rules": require('./eslintrc.common.js').rules
+};
