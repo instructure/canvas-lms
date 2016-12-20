5255d49a1b8275b6f4b0b7a0dc8e3e45fcffcd27
Jon Willesen <jonw@instructure.com>
2016-12-20 13:13:54 -0700

    fix linting in public/javascripts
    Change-Id: If58a756f28f040c0778cd759d36ac1aba9ff3253
    Reviewed-on: https://gerrit.instructure.com/98185
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Tested-by: Jenkins
    Product-Review: Jon Willesen <jonw+gerrit@instructure.com>
    QA-Review: Jon Willesen <jonw+gerrit@instructure.com>

--- public/javascripts/.eslintrc.js
+++ public/javascripts/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true, // Makes sure we don't get additional settings (such as ES2015+ specifics)
   extends: 'airbnb-base/legacy', // Only ES5 and below
-  rules: require('../../eslintrc.common.js').rules // Pull in our common rules
+  rules: require('../../.eslintrc.common.js').rules // Pull in our common rules
 };
