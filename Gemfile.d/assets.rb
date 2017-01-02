c1a5e29fde8d23f8b55306070dd7b3d6853e5057
Simon Williams <simon@instructure.com>
2017-01-02 14:40:51 -0700

    move gems dealing with asset generation to Gemfile.d/assets
    also remove github-markdown, it was unused
    
    refs CNVS-34084
    
    test plan: specs should pass
    
    Change-Id: I6747e42e151cf04b093056886c3277a1f3f81786
    Reviewed-on: https://gerrit.instructure.com/98685
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- Gemfile.d/assets.rb
+++ Gemfile.d/assets.rb
@@ -3,7 +3,10 @@ group :development, :test do
     gem 'colored', '1.2', require: false
     gem 'mustache', '1.0.3', require: false
     gem 'pygments.rb', '1.1.1', require: false
+  gem 'bluecloth', '2.2.0' # for generating api docs
   gem 'coffee-script', '2.4.1'
     gem 'execjs', '2.7.0', require: false
     gem 'coffee-script-source', '1.6.2' #pinned so everyone's compiled output matches
+  gem 'yard', '0.9.5'
+  gem 'yard-appendix', '0.1.8'
 end
--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -14,10 +14,8 @@ group :test do
   gem 'simplecov', '0.9.2', require: false
     gem 'docile', '1.1.5', require: false
   gem 'simplecov-rcov', '0.2.3', require: false
-  gem 'bluecloth', '2.2.0' # for generating api docs
+  gem 'bullet_instructure', '4.14.8', require: 'bullet'
     gem 'redcarpet', '3.3.4', require: false
-    gem 'github-markdown', '0.6.9', require: false
-    gem 'bullet_instructure', '4.14.8', require: 'bullet'
   gem 'mocha', github: 'maneframe/mocha', ref: 'bb8813fbb4cc589d7c58073d93983722d61b6919', require: false
     gem 'metaclass', '0.0.4', require: false
   gem 'thin', '1.7.0'
@@ -46,8 +44,6 @@ group :test do
   gem 'webmock', '1.22.3', require: false
     gem 'addressable', '2.3.8', require: false
     gem 'crack', '0.4.3', require: false
-  gem 'yard', '0.9.5'
-  gem 'yard-appendix', '0.1.8'
   gem 'timecop', '0.8.1'
   gem 'jira_ref_parser', '1.0.1'
   gem 'headless', '2.3.1', require: false
