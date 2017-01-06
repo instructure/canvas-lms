506e00d00aeadf8cbf8d72536c59a850bc604d32
Derek Bender <djbender@instructure.com>
2017-01-05 18:14:41 -0700

    remove non-input grading els from KO nav
    fixes: CNVS-33198
    
    test plan:
     - given a grading scheme
     - the name of the scheme should not be focusable when tabbing through
       elements or using any other keyboard-only nav inputs.
     - given a grading period
     - the names and dates are not focusable when tabbing through input
       elements
    
    Change-Id: Iea2166f26d855e3b060a35b7b5fe2915534164ac
    Reviewed-on: https://gerrit.instructure.com/98945
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: KC Naegle <knaegle@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

--- app/jsx/grading/tabContainer.jsx
+++ app/jsx/grading/tabContainer.jsx
@@ -22,25 +22,23 @@ function(React, ReactDOM, GradingStandardCollection, GradingPeriodCollection, $,
     render: function () {
       if(this.props.multipleGradingPeriodsEnabled) {
         return (
-          <div>
-            <div className="ui-tabs-minimal">
-              <ul>
-                <li><a href="#grading-periods-tab" className="grading_periods_tab"> {I18n.t('Grading Periods')}</a></li>
-                <li><a href="#grading-standards-tab" className="grading_standards_tab"> {I18n.t('Grading Schemes')}</a></li>
-              </ul>
-              <div ref="gradingPeriods" id="grading-periods-tab">
-                <GradingPeriodCollection/>
-              </div>
-              <div ref="gradingStandards" id="grading-standards-tab">
-                <GradingStandardCollection/>
-              </div>
+          <div className="ui-tabs-minimal">
+            <ul>
+              <li><a href="#grading-periods-tab" className="grading_periods_tab"> {I18n.t('Grading Periods')}</a></li>
+              <li><a href="#grading-standards-tab" className="grading_standards_tab"> {I18n.t('Grading Schemes')}</a></li>
+            </ul>
+            <div ref="gradingPeriods" id="grading-periods-tab">
+              <GradingPeriodCollection/>
+            </div>
+            <div ref="gradingStandards" id="grading-standards-tab">
+              <GradingStandardCollection/>
             </div>
           </div>
         );
       } else{
         return (
           <div ref="gradingStandards">
-            <h1 tabIndex="0">{I18n.t("Grading Schemes")}</h1>
+            <h1>{I18n.t("Grading Schemes")}</h1>
             <GradingStandardCollection/>
           </div>
         );
