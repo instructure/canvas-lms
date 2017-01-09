73abcb8ba35fb53bade3b779da7edd9746134275
Ryan Shaw <ryankshaw@gmail.com>
2017-01-09 14:47:15 -0700

    get webpack to work if using symlinks
    fixes: CNVS-34235
    
    test plan:
    * automated tests should pass
    * make sure that you see that it picks up all 
      the plugin stuff in the jenkins webpack build
    * mv gems/plugins ./moved_plugins
    * ln -s ./moved_plugins gems/plugins
    * run webpack with those plugins in the symlinked
      directory
    * it should work
    
    Change-Id: I97024d29d9dbf153147781af05c205fe766c0bd3
    Reviewed-on: https://gerrit.instructure.com/99159
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- frontend_build/bundles.js
+++ frontend_build/bundles.js
@@ -1,21 +1,17 @@
+const path = require('path')
 const glob = require('glob')
 
 const entries = {}
 
-const bundlesPattern = __dirname + '/../app/coffeescripts/bundles/**/*.coffee'
-const pluginBundlesPattern = __dirname + '/../gems/plugins/*/app/coffeescripts/bundles/*.coffee'
-const bundleNameRegexp = /\/coffeescripts\/bundles\/(.*).coffee/
-const fileNameRegexp = /\/([^/]+)\.coffee/
-const pluginNameRegexp = /plugins\/([^/]+)\/app/
-
-const appBundles = glob.sync(bundlesPattern, [])
-const pluginBundles = glob.sync(pluginBundlesPattern, [])
+const appBundles    = glob.sync(path.join(__dirname, '/../app/coffeescripts/bundles/**/*.coffee'))
+const pluginBundles = glob.sync(path.join(__dirname, '/../gems/plugins/*/app/coffeescripts/bundles/*.coffee'))
 
 // these are bundles that are dependencies, and therefore should not be compiled
 //  as entry points (webpack won't allow that).
 // TODO: Ultimately we should move them to other directories.
 const nonEntryPoints = ['modules/account_quota_settings', 'modules/content_migration_setup']
 
+const bundleNameRegexp = /\/coffeescripts\/bundles\/(.*).coffee/
 appBundles.forEach((entryFilepath) => {
   const entryBundlePath = entryFilepath.replace(/^.*app\/coffeescripts\/bundles/, './app/coffeescripts/bundles')
   const entryName = bundleNameRegexp.exec(entryBundlePath)[1]
@@ -28,6 +24,8 @@ appBundles.forEach((entryFilepath) => {
 //      This whole "compiled" folder should eventually go away
 entries.screenreader_gradebook = './public/javascripts/compiled/bundles/screenreader_gradebook.js'
 
+const fileNameRegexp = /\/([^/]+)\.coffee/
+const pluginNameRegexp = /plugins\/([^/]+)\/app/
 pluginBundles.forEach((entryFilepath) => {
   const pluginName = pluginNameRegexp.exec(entryFilepath)[1]
   const fileName = fileNameRegexp.exec(entryFilepath)[1]
