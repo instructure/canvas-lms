8692b78c76f1ae80a6258d17d817ec5f01cb25b6
Jeremy Stanley <jeremy@instructure.com>
2017-01-13 11:04:31 -0700

    add date override support to enrollment terms API
    the API could actually set date overrides previously, but the
    means to do so was not documented, and it could not retrieve them.
    this commit fixes both.
    
    test plan:
     - render documentation
     - use the enrollment terms API to create a term with dates
       overridden for specific enrollment types
       - ensure the only accepted enrollment types are those
         in the UI and the documentation and that other strings
         result in a 400 Bad Request error
     - use the enrollment terms API to edit override dates for
       existing terms (with and without changing the term name)
     - use the list enrollment terms endpoint with include[]=overrides
       and ensure override dates are returned properly
    
    fixes CNVS-34227
    fixes CNVS-34358
    
    Change-Id: I012224b0cf5c7bdcf4cd0ae1a1e8d9d7965d42c1
    Reviewed-on: https://gerrit.instructure.com/99563
    Tested-by: Jenkins
    Reviewed-by: Dan Minkevitch <dan@instructure.com>
    QA-Review: Heath Hales <hhales@instructure.com>
    Product-Review: Chris Ward <cward@instructure.com>

--- lib/api/v1/enrollment_term.rb
+++ lib/api/v1/enrollment_term.rb
@@ -5,10 +5,21 @@ module Api::V1::EnrollmentTerm
     api_json(enrollment_term, user, session, :only => %w(id name start_at end_at workflow_state grading_period_group_id created_at)).tap do |hash|
       hash['sis_term_id'] = enrollment_term.sis_source_id if enrollment_term.root_account.grants_any_right?(user, :read_sis, :manage_sis)
       hash['start_at'], hash['end_at'] = enrollment_term.overridden_term_dates(enrollments) if enrollments.present?
+      hash['overrides'] = date_overrides_json(enrollment_term) if includes.include?('overrides')
     end
   end
 
   def enrollment_terms_json(enrollment_terms, user, session, enrollments=[], includes=[])
+    if includes.include?('overrides')
+      ActiveRecord::Associations::Preloader.new.preload(enrollment_terms, :enrollment_dates_overrides)
+    end
     enrollment_terms.map{ |t| enrollment_term_json(t, user, session, enrollments, includes) }
+  end
+
+  protected def date_overrides_json(term)
+    term.enrollment_dates_overrides.select { |o| o.start_at || o.end_at }.inject({}) do |json, override|
+      json[override.enrollment_type] = override.attributes.slice('start_at', 'end_at')
+      json
+    end
   end
 end
