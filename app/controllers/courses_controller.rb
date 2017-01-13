9aa1233d0620930a6b9e5f739c761c0a78df0623
Jon Willesen <jonw@instructure.com>
2017-01-13 11:45:19 -0700

    fix saving offline web export course setting
    fixes OFFW-77
    
    test plan:
    * make sure the course setting for the offline web export save correctly
    
    Change-Id: If60d35a9388c9b4f0c40200ae684b61fd9f1d0c0
    Reviewed-on: https://gerrit.instructure.com/99573
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Product-Review: Cameron Sutter <csutter@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jon Willesen <jonw+gerrit@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2792,6 +2792,6 @@ class CoursesController < ApplicationController
       :abstract_course, :storage_quota, :storage_quota_mb, :restrict_enrollments_to_course_dates,
       :restrict_student_past_view, :restrict_student_future_view, :grading_standard, :grading_standard_enabled,
       :locale, :integration_id, :hide_final_grades, :hide_distribution_graphs, :lock_all_announcements, :public_syllabus,
-      :public_syllabus_to_auth, :course_format, :time_zone, :organize_epub_by_content_type)
+      :public_syllabus_to_auth, :course_format, :time_zone, :organize_epub_by_content_type, :enable_offline_web_export)
   end
 end
