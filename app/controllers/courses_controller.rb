09f75dabbdc652db6c20e9ecf2401b328e476a86
Matthew Sessions <msessions@instructure.com>
2017-01-16 10:58:04 -0700

    Add course image to show/list courses endpoint
    Fixes MBL-7126
    
    Test Plan:
     - Enable the course image feature flags for a course
       (must first allow or enable on the account feature
       flags)
     - Add a course image to a course
     - Hit /api/v1/courses/<course_id>
       with include[]=course_image
     - It should return a field on the course called image_download_url
       which will be a url to either the flicker image or user uploaded
       image that they provided when adding their course image
     - Remove the include and it shouldn't return the
       image
     - Turn the feature flag off but provide the include
       parameter and it shouldn't return the image
     - This should all also work on the list courses endpoint
       the same include value
    
    Change-Id: I6774201235dcd6806e870a4cddab6d63e06b8c42
    Reviewed-on: https://gerrit.instructure.com/99664
    Reviewed-by: Dan Minkevitch <dan@instructure.com>
    Tested-by: Jenkins
    QA-Review: David Tan <dtan@instructure.com>
    Product-Review: Matthew Sessions <msessions@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -334,7 +334,7 @@ class CoursesController < ApplicationController
   #   When set, only return courses where the user has an enrollment with the given state.
   #   This will respect section/course/term date overrides.
   #
-  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"]
+  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"|"course_image"]
   #   - "needs_grading_count": Optional information to include with each Course.
   #     When needs_grading_count is given, and the current user has grading
   #     rights, the total number of submissions needing grading for all
@@ -398,6 +398,8 @@ class CoursesController < ApplicationController
   #   - "tabs": Optional information to include with each Course.
   #     Will include the list of tabs configured for each course.  See the
   #     {api:TabsController#index List available tabs API} for more information.
+  #   - "course_image": Optional course image data for when there is a course image
+  #     and the course image feature flag has been enabled
   #
   # @argument state[] [String, "unpublished"|"available"|"completed"|"deleted"]
   #   If set, only return courses that are in the given state(s).
@@ -447,7 +449,7 @@ class CoursesController < ApplicationController
   # @API List courses for a user
   # Returns a list of active courses for this user. To view the course list for a user other than yourself, you must be either an observer of that user or an administrator.
   #
-  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"]
+  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"|"course_image"]
   #   - "needs_grading_count": Optional information to include with each Course.
   #     When needs_grading_count is given, and the current user has grading
   #     rights, the total number of submissions needing grading for all
@@ -511,6 +513,8 @@ class CoursesController < ApplicationController
   #   - "tabs": Optional information to include with each Course.
   #     Will include the list of tabs configured for each course.  See the
   #     {api:TabsController#index List available tabs API} for more information.
+  #   - "course_image": Optional course image data for when there is a course image
+  #     and the course image feature flag has been enabled
   #
   # @argument state[] [String, "unpublished"|"available"|"completed"|"deleted"]
   #   If set, only return courses that are in the given state(s).
@@ -1529,11 +1533,13 @@ class CoursesController < ApplicationController
   #
   # Accepts the same include[] parameters as the list action plus:
   #
-  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"|"all_courses"|"permissions"|"observed_users"]
+  # @argument include[] [String, "needs_grading_count"|"syllabus_body"|"public_description"|"total_scores"|"current_grading_period_scores"|"term"|"course_progress"|"sections"|"storage_quota_used_mb"|"total_students"|"passback_status"|"favorites"|"teachers"|"observed_users"|"all_courses"|"permissions"|"observed_users"|"course_image"]
   #   - "all_courses": Also search recently deleted courses.
   #   - "permissions": Include permissions the current user has
   #     for the course.
   #   - "observed_users": include observed users in the enrollments
+  #   - "course_image": Optional course image data for when there is a course image
+  #     and the course image feature flag has been enabled
   #
   # @returns Course
   def show
