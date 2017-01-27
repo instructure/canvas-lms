65e255f635eafb3ad84264f8c05a2d777f297509
James Williams <jamesw@instructure.com>
2017-01-27 08:52:28 -0700

    fix setting of new announcement home page course settings
    test plan:
    * changing the "Show recent announcements"
     and "Number of announcements shown" course settings
     should work
    
    closes #CNVS-34595
    
    Change-Id: Ib7822208c96ef45eaa4e4158ba8b1cf3ec9adb74
    Reviewed-on: https://gerrit.instructure.com/100657
    Tested-by: Jenkins
    Reviewed-by: Dan Minkevitch <dan@instructure.com>
    QA-Review: Heath Hales <hhales@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2810,6 +2810,8 @@ class CoursesController < ApplicationController
       :abstract_course, :storage_quota, :storage_quota_mb, :restrict_enrollments_to_course_dates,
       :restrict_student_past_view, :restrict_student_future_view, :grading_standard, :grading_standard_enabled,
       :locale, :integration_id, :hide_final_grades, :hide_distribution_graphs, :lock_all_announcements, :public_syllabus,
-      :public_syllabus_to_auth, :course_format, :time_zone, :organize_epub_by_content_type, :enable_offline_web_export)
+      :public_syllabus_to_auth, :course_format, :time_zone, :organize_epub_by_content_type, :enable_offline_web_export,
+      :show_announcements_on_home_page, :home_page_announcement_limit
+    )
   end
 end
