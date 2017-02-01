6421eca9273e2f1ee281d633d597ee4d96f7f43c
Steven Burnett <sburnett@instructure.com>
2017-02-01 10:05:46 -0700

    fix warnings
    Test Plan:
    - Jenkins Passes
    
    Change-Id: Ie71062c87f8b114e146251190556fa082c097823
    Reviewed-on: https://gerrit.instructure.com/100878
    Tested-by: Jenkins
    Reviewed-by: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>

--- spec/javascripts/jsx/outcomes/OutcomeAlignmentDeleteLink.jsx
+++ spec/javascripts/jsx/outcomes/OutcomeAlignmentDeleteLink.jsx
@@ -9,13 +9,13 @@ define([
   module('OutcomeAlignmentDeleteLink');
 
   test('should render span if hasRubricAssociation', () => {
-    const wrapper = mount(<OutcomeAlignmentDeleteLink has_rubric_association />);
+    const wrapper = mount(<OutcomeAlignmentDeleteLink url="http://hellow" has_rubric_association="" />);
     const span = wrapper.find('span');
     ok(span.exists());
   });
 
   test('should render a link if !hasRubricAssociation', () => {
-    const wrapper = mount(<OutcomeAlignmentDeleteLink />);
+    const wrapper = mount(<OutcomeAlignmentDeleteLink url="http://hellow" />);
     const span = wrapper.find('a');
     ok(span.exists());
   });
