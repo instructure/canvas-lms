ae3568d04a578373e5ba20f1aa918346bb36c697
Cody Cutrer <cody@instructure.com>
2017-02-01 09:11:36 -0700

    Revert "remove functionaltiy to move courses between root accounts"
    This reverts commit 3fb74991d6d9798b95de76a15230943203360e1e.
    
    also hide it behind a setting
    
    Change-Id: Iec49940426c73632e191af2f99f36e6e3c4b6f0d
    Reviewed-on: https://gerrit.instructure.com/100872
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- public/javascripts/course_settings.js
+++ public/javascripts/course_settings.js
@@ -260,6 +260,16 @@ define([
         $("#course_account_id").val(ui.item.id);
       }
     });
+    $(".move_course_link").click(function(event) {
+      event.preventDefault();
+      $("#move_course_dialog").dialog({
+        title: I18n.t('titles.move_course', "Move Course"),
+        width: 500
+      }).fixDialogButtons();
+    });
+    $("#move_course_dialog").delegate('.cancel_button', 'click', function() {
+      $("#move_course_dialog").dialog('close');
+    });
     $course_form.find(".grading_standard_checkbox").change(function() {
       $course_form.find(".grading_standard_link").showIf($(this).attr('checked'));
     }).change();
