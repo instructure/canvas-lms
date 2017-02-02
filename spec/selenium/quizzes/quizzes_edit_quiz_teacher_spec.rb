26542a340102773ea379d79c8508483264b0d897
Jon Jensen <jon@instructure.com>
2017-02-02 11:48:29 -0700

    spec: speed up quiz specs
    group: 1:56 -> 1:05
    slowest example: 0:20 -> 0:08
    
    Change-Id: I8a9a861fa13c9788a8423c43737d643339b39481
    Reviewed-on: https://gerrit.instructure.com/100962
    Tested-by: Jenkins
    Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
    Product-Review: Landon Wilkins <lwilkins@instructure.com>
    QA-Review: Landon Wilkins <lwilkins@instructure.com>

--- spec/selenium/quizzes/quizzes_edit_quiz_teacher_spec.rb
+++ spec/selenium/quizzes/quizzes_edit_quiz_teacher_spec.rb
@@ -17,50 +17,53 @@ describe 'editing a quiz' do
   end
 
   context 'as a teacher' do
-    before(:each) do
-      course_with_teacher_logged_in
+    before(:once) do
+      course_with_teacher(active_all: true)
       create_quiz_with_due_date
-      get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
+    end
+
+    before(:each) do
+      user_session(@teacher)
     end
 
     context 'when the quiz is published' do
       it 'indicates the quiz is published', priority: "1", test_id: 351924 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         expect(f('#quiz-draft-state').text.strip).to match accessible_variant_of 'Published'
       end
 
       it 'hides the |Save and Publish| button', priority: "1", test_id: 255478 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         expect(f("#content")).not_to contain_css('.save_and_publish')
       end
 
       it 'shows the speedgrader link', priority: "1", test_id: 351926 do
-        expect_new_page_load do
-          click_save_settings_button
-        end
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}"
         expect(f('.icon-speed-grader')).to be_displayed
       end
 
       it 'remains published after saving changes', priority: "1", test_id: 210059 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         type_in_tiny('#quiz_description', 'changed description')
         click_save_settings_button
         expect(f('#quiz-publish-link')).to include_text 'Published'
       end
 
       it 'deletes the quiz', priority: "1", test_id: 351921 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         delete_quiz
       end
 
       it 'saves question changes with the |Save it now| button', priority: "1", test_id: 140647 do
-        course_with_student(course: @course, active_enrollment: true)
-
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         # add new question without saving changes
         click_questions_tab
         click_new_question_button
-        question_description = 'New Test Question'
-        create_multiple_choice_question(description: question_description)
+        create_file_upload_question
         cancel_quiz_edit
 
         # verify alert
-        alert_box = fj('.unpublished_warning', '.alert')
+        alert_box = f('.alert .unpublished_warning')
         expect(alert_box.text).to \
           eq "You have made changes to the questions in this quiz.\nThese "\
             "changes will not appear for students until you save the quiz."
@@ -71,55 +74,48 @@ describe 'editing a quiz' do
 
         # verify the alert disappears after clicking the button
         save_it_now_button.click
-        wait_for_ajaximations
-        expect(fj('.unpublished_warning', '.alert')).not_to be_displayed
+        expect(f('.alert .unpublished_warning')).not_to be_displayed
 
-        # verify the student sees the changes
-        user_session(@student)
-        take_quiz do
-          expect(f('.display_question.question.multiple_choice_question')).to \
-            include_text question_description
-        end
+        expect { @quiz.quiz_questions.count }.to become(1)
       end
     end
 
     context 'when the quiz isn\'t published' do
-      before(:each) do
+      before(:once) do
         @quiz.workflow_state = 'unavailable'
         @quiz.save!
-        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
       end
 
       it 'indicates the quiz is unpublished', priority: "1", test_id: 351925 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         expect(f('#quiz-draft-state').text.strip).to match accessible_variant_of 'Not Published'
       end
 
       it 'hides the speedgrader link', priority: "1", test_id: 351927 do
-        expect_new_page_load do
-          click_save_settings_button
-        end
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}"
         expect(f("#content")).not_to contain_css('.icon-speed-grader')
       end
 
       it 'shows the |Save and Publish| button', priority: "1", test_id: 255479 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         expect(f('.save_and_publish')).to be_displayed
       end
 
       it 'deletes the quiz', priority: "1", test_id: 351922 do
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
         delete_quiz
       end
 
       it 'publishes the quiz', priority: "1", test_id: 351928 do
         skip_if_chrome('research')
-        expect_new_page_load do
-          click_save_settings_button
-        end
+        get "/courses/#{@course.id}/quizzes/#{@quiz.id}"
         f('#quiz-publish-link.btn-publish').click
         expect(f('#quiz-publish-link')).to include_text 'Unpublish'
       end
     end
 
     it 'hides question form when cancelling edit', priority: "1", test_id: 209956 do
+      get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
       wait_for_tiny f('#quiz_description')
       click_questions_tab
       click_new_question_button
@@ -128,20 +124,13 @@ describe 'editing a quiz' do
     end
 
     it 'changes the quiz\'s description', priority: "1", test_id: 210057 do
-      wait_for_ajaximations
-      expect(f('#quiz_description_ifr')).to be_displayed
+      get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
 
       test_text = 'changed description'
-      type_in_tiny '#quiz_description', test_text
-      in_frame 'quiz_description_ifr' do
-        expect(f('#tinymce')).to include_text(test_text)
-      end
+      type_in_tiny '#quiz_description', test_text, clear: true
 
       click_save_settings_button
-      wait_for_ajaximations
-
-      get "/courses/#{@course.id}/quizzes/#{@quiz.id}"
-      expect(f('#main .description')).to include_text(test_text)
+      expect { @quiz.reload.description }.to become("<p>#{test_text}</p>")
     end
 
     it 'loads existing due date', priority: "1", test_id: 209961 do
@@ -149,41 +138,12 @@ describe 'editing a quiz' do
       compare_assignment_times(@quiz.reload)
     end
 
-    it 'overrides quiz details', priority: "2", test_id: 210074 do
-      skip('This spec is frail')
-      default_section = @course.course_sections.first
-      other_section = @course.course_sections.create!(name: 'other section')
-      default_section_due = Time.zone.now + 1.day
-      other_section_due = Time.zone.now + 2.days
-      get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
-      wait_for_ajaximations
-      select_first_override_section(default_section.name)
-      first_due_at_element.clear
-      first_due_at_element.
-          send_keys(format_date_for_view(default_section_due))
-
-      add_override
-
-      select_last_override_section(other_section.name)
-      last_due_at_element.
-          send_keys(format_date_for_view(other_section_due))
-      expect_new_page_load do
-        click_save_settings_button
-        wait_for_ajax_requests
-      end
-      overrides = @quiz.reload.assignment_overrides
-      expect(overrides.size).to eq 2
-      default_override = overrides.detect { |o| o.set_id == default_section.id }
-      expect(default_override.due_at.to_date).
-          to eq default_section_due.to_date
-      other_override = overrides.detect { |o| o.set_id == other_section.id }
-      expect(other_override.due_at.to_date).
-          to eq other_section_due.to_date
-    end
-
     context 'when the quiz has a submission' do
-      before(:each) do
+      before(:once) do
         quiz_with_submission
+      end
+
+      before(:each) do
         get "/courses/#{@course.id}/quizzes/#{@quiz.id}/edit"
       end
 
