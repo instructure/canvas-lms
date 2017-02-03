df6872ff11d285658d892302a6636a1562624cb4
Jon Jensen <jon@instructure.com>
2017-02-03 10:49:04 -0700

    spec: speed up scheduler specs
    total: 1:40 -> 0:59
    slowest example: 0:17 -> 0:05
    
    Change-Id: Ic109fe329e87d9e088618dab64a4abc4ed9de740
    Reviewed-on: https://gerrit.instructure.com/101056
    Tested-by: Jenkins
    Reviewed-by: Shawn Meredith <shawn@instructure.com>
    Product-Review: Shawn Meredith <shawn@instructure.com>
    QA-Review: Shawn Meredith <shawn@instructure.com>

--- spec/selenium/calendar/calendar2_scheduler_teacher_spec.rb
+++ spec/selenium/calendar/calendar2_scheduler_teacher_spec.rb
@@ -15,17 +15,16 @@ describe "scheduler" do
         a.settings[:agenda_view]      = true
         a.save!
       end
+      course_with_teacher(active_all: true)
     end
 
     before(:each) do
-      course_with_teacher_logged_in
+      user_session(@teacher)
       make_full_screen
     end
 
     it "should bring up 'edit appointment group' modal when clicking 'create an appointment group' scheduler button" do
-      get "/calendar2"
-      click_scheduler_link
-      wait_for_ajaximations
+      get "/calendar2#view_name=scheduler"
       f('#right-side .create_link').click
       expect(f('#edit_event')).not_to be_nil
     end
@@ -33,9 +32,7 @@ describe "scheduler" do
     it "should not enforce minimum event lengths when loading appointment times into the edit group form" do
       date = Date.today.to_s
       create_appointment_group(:new_appointments => [[date + ' 12:00:00', date + ' 12:13:00']])
-      get "/calendar2"
-      click_scheduler_link
-      wait_for_ajaximations
+      get "/calendar2#view_name=scheduler"
       click_appointment_link
       click_al_option('.edit_link')
       expect(f('.end_time')['value']).to include(":13")
@@ -44,11 +41,8 @@ describe "scheduler" do
     it "should split time slots", priority: "1", test_id: 140190 do
       start_time_text = '02'
       end_time_text = '06'
-      local_start_time = '01'
-      local_end_time = '05'
 
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
       f('.create_link').click
       fj('.ui-datepicker-trigger:visible').click
       datepicker_next
@@ -67,19 +61,14 @@ describe "scheduler" do
       f("#option_course_#{@course.id}").click
       f('.ag_contexts_done').click
       submit_appointment_group_form
-      get "/calendar2"
       last_group = AppointmentGroup.last
 
-      start_time_correct = true if last_group.end_at.strftime("%I") == end_time_text || local_end_time
-      end_time_correct = true if last_group.start_at.strftime("%I") == start_time_text || local_start_time
-
-      expect(start_time_correct).to eq true
-      expect(end_time_correct).to eq true
+      expect(last_group.end_at.strftime("%I")).to eq end_time_text
+      expect(last_group.start_at.strftime("%I")).to eq start_time_text
     end
 
     it "should require at least one time slot" do
-      get '/calendar2'
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
       f('.create_link').click
       fj('.ui-datepicker-trigger:visible').click
       datepicker_next
@@ -95,8 +84,7 @@ describe "scheduler" do
     end
 
     it "should allow checkboxes in the options section to be edited" do
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
       create_appointment_group_manual :checkable_options => {
           :per_slot_option => true,
           :participant_visibility => true,
@@ -122,50 +110,28 @@ describe "scheduler" do
     end
 
     it "should send messages to appropriate participants", priority: "1", test_id: 140192 do
-      gc = group_category
-      ug1 = @course.groups.create!(:group_category => gc)
-      ug1.users << student1 = student_in_course(:course => @course, :active_all => true).user
-      ug1.users << student2 = student_in_course(:course => @course, :active_all => true).user
+      student1 = student_in_course(:course => @course, :active_all => true).user
+      student2 = student_in_course(:course => @course, :active_all => true).user
 
-      ug2 = @course.groups.create!(:group_category => gc)
-      ug2.users << student3 = student_in_course(:course => @course, :active_all => true).user
-
-      student4 = student_in_course(:course => @course, :active_all => true).user
-
-      other_section = @course.course_sections.create!
-      @course.enroll_user(student5 = user_factory(active_all: true), 'StudentEnrollment', :section => other_section).accept!
-
-      # create some appointment groups and sign up a participant in each one
+      # create an appointment groups and sign up a participant
       appointment_participant_model(:course => @course, :participant => student1)
-      appointment_participant_model(:course => @course, :participant => ug1)
-      appointment_participant_model(:course => @course, :sub_context => @course.default_section, :participant => student1)
 
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
 
-      appointment_groups = ffj('.appointment-group-item')
-      appointment_groups.each_with_index do |ag, i|
-        driver.execute_script("$('.appointment-group-item:index(#{i}').addClass('ui-state-hover')")
-        %w(all registered unregistered).each do |registration_status|
-          click_al_option('.message_link', i)
-          form = f('#message_participants_form')
-          expect(form).to be_displayed
-          wait_for_ajaximations
+      appointment_group = f('.appointment-group-item')
+      hover(appointment_group)
+      click_al_option('.message_link', 0)
+      form = f('#message_participants_form')
+      expect(form).to be_displayed
+      set_value(form.find('.message_groups'), 'registered')
+      wait_for_ajax_requests
 
-          set_value(form.find('.message_groups'), registration_status)
-          wait_for_ajaximations
-
-          expect(form.find_all('.participant_list li')).not_to be_empty
-          set_value(form.find('#body'), 'hello')
-          submit_dialog(fj('.ui-dialog:visible'), '.ui-button')
-          expect(f("body")).not_to contain_css('#message_participants_form')
-        end
-      end
-      expect(student1.conversations.first.messages.size).to eq 6 # registered/all * 3
-      expect(student2.conversations.first.messages.size).to eq 6 # unregistered/all * 2 + registered/all (ug1)
-      expect(student3.conversations.first.messages.size).to eq 6 # unregistered/all * 3
-      expect(student4.conversations.first.messages.size).to eq 4 # unregistered/all * 2 (not in any group)
-      expect(student5.conversations.first.messages.size).to eq 2 # unregistered/all * 1 (doesn't meet any sub_context criteria)
+      expect(form.find_all('.participant_list li')).not_to be_empty
+      set_value(form.find('#body'), 'hello')
+      submit_dialog(fj('.ui-dialog:visible'), '.ui-button')
+      wait_for_ajax_requests
+      expect(student1.conversations.first.messages.size).to eq 1
+      expect(student2.conversations.size).to eq 0
     end
 
     it "should use the correct context for messages sent through scheduler" do
@@ -179,24 +145,21 @@ describe "scheduler" do
       other_course.offer
       other_course.enroll_student(student1).accept!
 
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
 
-      ag = f('.appointment-group-item')
-      driver.execute_script("$('.appointment-group-item').addClass('ui-state-hover')")
+      hover f('.appointment-group-item')
 
       click_al_option('.message_link')
       form = f('#message_participants_form')
       expect(form).to be_displayed
-      wait_for_ajaximations
 
       set_value(form.find('.message_groups'), "all")
-      wait_for_ajaximations
+      wait_for_ajax_requests
 
       expect(form.find_all('.participant_list li')).not_to be_empty
       set_value(form.find('#body'), 'hello')
       submit_dialog(fj('.ui-dialog:visible'), '.ui-button')
-      expect(f("body")).not_to contain_css('#message_participants_form')
+      wait_for_ajax_requests
 
       part1 = student1.conversations.first
       expect(part1.tags).to eq ["course_#{first_course.id}"]
@@ -209,11 +172,8 @@ describe "scheduler" do
       create_appointment_group(:new_appointments => [
         [date + ' 12:00:00', date + ' 13:00:00'],
       ])
-      get "/calendar2"
-      click_scheduler_link
-      wait_for_ajaximations
+      get "/calendar2#view_name=scheduler"
       click_appointment_link
-      wait_for_ajaximations
       expect(f('.agenda-event__item .agenda-event__item-container')).to be_present
     end
 
@@ -234,8 +194,7 @@ describe "scheduler" do
     end
 
     it "should not allow limiting the max appointments per participant to less than 1", priority: "1", test_id: 140194 do
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
       fill_out_appointment_group_form('max appointments')
 
       # invalid max_appointments
@@ -252,14 +211,10 @@ describe "scheduler" do
       student_in_course(:course => @course, :active_all => true)
       ag.appointments.first.reserve_for(@user, @user, comments: 'this is important')
 
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
       f(".appointment-group-item:nth-child(1) .view_calendar_link").click
-      wait_for_ajaximations
 
-      fj('.agenda-event__item .agenda-event__item-container').click
-
-      wait_for_ajaximations
+      f('.agenda-event__item .agenda-event__item-container').click
 
       expect(ff('#attendees li')).to have_size(1)
 
@@ -275,9 +230,7 @@ describe "scheduler" do
         ag.appointments.first.reserve_for(@user, @user, comments: 'this is important')
       end
 
-      get "/calendar2"
-      click_scheduler_link
-
+      get "/calendar2#view_name=scheduler"
 
       f(".appointment-group-item:nth-child(1) .view_calendar_link").click
 
@@ -287,15 +240,12 @@ describe "scheduler" do
 
       # delete the first appointment
       fj('.cancel_appointment_link:eq(1)').click
-      wait_for_ajaximations
       f('.ui-dialog-buttonset .btn-primary').click
-      wait_for_ajaximations
       expect(ff('#attendees li')).to have_size(1)
 
       f('.agenda-event__item .agenda-event__item-container').click
 
       expect(ff('#attendees li')).to have_size(1)
-      f('.scheduler_done_button').click
     end
 
     it "should allow removing individual appointment groups" do
@@ -314,25 +264,19 @@ describe "scheduler" do
         ag.appointments.first.reserve_for(group, @user)
       end
 
-      get "/calendar2"
-      click_scheduler_link
+      get "/calendar2#view_name=scheduler"
 
       f(".appointment-group-item:nth-child(1) .view_calendar_link").click
-      wait_for_ajaximations
       fj('.agenda-event__item .agenda-event__item-container').click
-      wait_for_ajaximations
-      expect(ffj('#attendees li').size).to eq 2
+      expect(ff('#attendees li')).to have_size 2
 
       # delete the first appointment
-      driver.execute_script("$('.cancel_appointment_link:eq(1)').trigger('click')")
-      wait_for_ajaximations
-      driver.execute_script("$('.ui-dialog-buttonset .btn-primary').trigger('click')")
-      wait_for_ajaximations
-      expect(ff('#attendees li').size).to eq 1
+      fj('.cancel_appointment_link:eq(1)').click
+      f('.ui-dialog-buttonset .btn-primary').click
+      expect(ff('#attendees li')).to have_size 1
 
-      fj('.agenda-event__item .agenda-event__item-container').click
-      expect(ff('#attendees li').size).to eq 1
-      f('.scheduler_done_button').click
+      f('.agenda-event__item .agenda-event__item-container').click
+      expect(ff('#attendees li')).to have_size 1
     end
 
     # TODO reimplement per CNVS-29591, but make sure we're testing at the right level
@@ -340,9 +284,7 @@ describe "scheduler" do
 
     it "should allow me to override the participant limit on a slot-by-slot basis" do
       create_appointment_group :participants_per_appointment => 2
-      get "/calendar2"
-      click_scheduler_link
-      wait_for_ajaximations
+      get "/calendar2#view_name=scheduler"
       click_appointment_link
 
       open_edit_appointment_group_event_dialog
