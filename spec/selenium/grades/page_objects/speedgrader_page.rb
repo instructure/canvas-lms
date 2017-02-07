bf350cfb22c0091ae98d5949ff2ea17bae85ae26
KC Naegle <knaegle@instructure.com>
2017-02-07 11:03:24 -0700

    spec: speedgrader comments diplay attachments properly
    Test case id: C3058055
    
    Change-Id: Ie414b60e9a4e963c439a9e16bdf606d45f63beb0
    Reviewed-on: https://gerrit.instructure.com/101276
    Tested-by: Jenkins
    Reviewed-by: Anju Reddy <areddy@instructure.com>
    Reviewed-by: David Tan <dtan@instructure.com>
    Product-Review: KC Naegle <knaegle@instructure.com>
    QA-Review: KC Naegle <knaegle@instructure.com>

--- spec/selenium/grades/page_objects/speedgrader_page.rb
+++ spec/selenium/grades/page_objects/speedgrader_page.rb
@@ -83,6 +83,18 @@ class Speedgrader
       f('#submission_to_view')
     end
 
+    def attachment_button
+      f('#add_attachment')
+    end
+
+    def attachment_input
+      f('#comment_attachments input')
+    end
+
+    def attachment_link
+      f('.display_name')
+    end
+
     # action
     def visit(course_id, assignment_id)
       get "/courses/#{course_id}/gradebook/speed_grader?assignment_id=#{assignment_id}"
@@ -125,6 +137,11 @@ class Speedgrader
     def add_comment_and_submit(comment)
       replace_content(comment_text_area, comment)
       comment_submit_button.click
+    end
+
+    def add_comment_attachment(file_path)
+      attachment_button.click
+      attachment_input.send_keys(file_path)
     end
 
     def click_submissions_to_view
