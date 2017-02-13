instructure/canvas-lms

commit e087da776f2a8037886510ab1677773244ee5653

OriginalityReport: enable in gradebook
Fixes PLAT-1999

Test Plan
- Create an assignment with a valid submission,
  attachment, and originality report. The originality
  report should not have the 'origanality_report_url'
  field set.
- Navigate to gradebook and verify the 'tunitin' speech
  bubble icon appers next to the submission. Click on the
  submission to open the details modal and verify that
  an icon appears displaying the originality score.
  Verify that the icon does not link to any page when
  clicked.
- Set the 'originality_report_url' of the existing
  OriginalityReport to a valid URL and save it.
- Navigate back to speedgrader and verify the same
  'turnitin' speech bubble appears next to the submission.
  Open the submission details modal and verify the same
  icon appears displaying the originality score. Verify
  that clicking on the icon navigates to the url set on
  the OriginalityReport
- Create an assignment with speedgrader enabled. Create
  a submission and attachment with valid turnitin_data.
- Navigate to gradebook and verify that the turnitin
  submission behaves as it did before.

Change-Id: I9b3577ea55fe802753ffcd0b46bd4c5a4a2ea244
Reviewed-on: https://gerrit.instructure.com/97019
Tested-by: Jenkins
Reviewed-by: Nathan Mills <nathanm@instructure.com>
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
QA-Review: August Thornton <august@instructure.com>
Product-Review: Weston Dransfield <wdransfield@instructure.com>

app/models/submission.rb
@@ -537,7 +537,9 @@ class Submission < ActiveRecord::Base
         status: originality_report.workflow_state
       }
     end
-    turnitin_data.merge(data)
+    ret_val = turnitin_data.merge(data)
+    ret_val.delete(:provider)
+    ret_val
   end
 
   def turnitin_assets

lib/api/v1/submission.rb
@@ -131,13 +131,19 @@ module Api::V1::Submission
       hash['media_comment'] = media_comment_json(:media_id => attempt.media_comment_id, :media_type => attempt.media_comment_type)
     end
 
-    if attempt.turnitin_data.present? && attempt.grants_right?(@current_user, :view_turnitin_report)
-      turnitin_hash = attempt.turnitin_data.dup
+    if attempt.originality_reports.present?
+       hash['has_originality_report'] = true
+    end
+
+    if attempt.originality_data.present? && attempt.grants_right?(@current_user, :view_turnitin_report)
+      turnitin_hash = attempt.originality_data.dup
       turnitin_hash.delete(:last_processed_attempt)
       hash['turnitin_data'] = turnitin_hash
     end
 
-    if attempt.vericite_data(false).present? && attempt.can_view_plagiarism_report('vericite', @current_user, session)
+    if attempt.vericite_data(false).present? &&
+      attempt.can_view_plagiarism_report('vericite', @current_user, session) &&
+      attempt.assignment.vericite_enabled?
       vericite_hash = attempt.vericite_data(false).dup
       vericite_hash.delete(:last_processed_attempt)
       hash['vericite_data'] = vericite_hash

app/coffeescripts/gradebook2/Turnitin.coffee
@@ -31,9 +31,11 @@ define [
     extractDataForTurnitin: (submission, key, urlPrefix) ->
       data = submission?.turnitin_data
       type = "turnitin"
-      if !data? || data.provider == 'vericite'
+      if !data? || (submission?.vericite_data && submission?.vericite_data.provider == 'vericite')
         data = submission?.vericite_data
         type = "vericite"
+      if submission?.has_originality_report
+        type = "originality_report"
       return {} unless data and data[key] and data[key].similarity_score?
       data = data[key]
       data.state = "#{data.state || 'no'}_score"

app/controllers/submissions_api_controller.rb
@@ -201,7 +201,7 @@ class SubmissionsApiController < ApplicationController
                                                            @current_user, section_ids)
                         .pluck(:user_id)
                     end
-      submissions = @assignment.submissions.where(user_id: student_ids)
+      submissions = @assignment.submissions.where(user_id: student_ids).preload(:originality_reports)
 
       if includes.include?("visibility")
         json = bulk_process_submissions_for_visibility(submissions, includes)
@@ -354,11 +354,11 @@ class SubmissionsApiController < ApplicationController
           eager_load(:user => :pseudonyms).
           where("users.id" => student_ids)
 
-      submissions = if requested_assignment_ids.present?
+      submissions_scope = if requested_assignment_ids.present?
                       Submission.where(
                         :user_id => student_ids,
                         :assignment_id => assignments
-                      ).to_a
+                      )
                     else
                       Submission.joins(:assignment).where(
                         :user_id => student_ids,
@@ -366,8 +366,9 @@ class SubmissionsApiController < ApplicationController
                         "assignments.context_id" => @context.id
                       ).where(
                         "assignments.workflow_state != 'deleted'"
-                      ).to_a
+                      )
                     end
+      submissions = submissions_scope.preload(:originality_reports).to_a
       bulk_load_attachments_and_previews(submissions)
       submissions_for_user = submissions.group_by(&:user_id)
 
@@ -418,7 +419,7 @@ class SubmissionsApiController < ApplicationController
       order = "#{order_by} #{order_direction}"
       submissions = @context.submissions.except(:order).where(:user_id => student_ids).order(order)
       submissions = submissions.where(:assignment_id => assignments) unless assignments.empty?
-      submissions = submissions.preload(:user)
+      submissions = submissions.preload(:user, :originality_reports)
 
       submissions = Api.paginate(submissions, self, polymorphic_url([:api_v1, @section || @context, :student_submissions]))
       Submission.bulk_load_versioned_attachments(submissions)

spec/apis/v1/submissions_api_spec.rb
@@ -1196,11 +1196,45 @@ describe 'Submissions API', type: :request do
     expect(json.first['integration_id']).to eq 'xyz'
   end
 
-  it "returns turnitin data if present" do
+  it "returns vericite data if present and vericite is enabled for the assignment" do
     student = user(:active_all => true)
     course_with_teacher(:active_all => true)
     @course.enroll_student(student).accept!
-    a1 = @course.assignments.create!(:title => 'assignment1', :grading_type => 'letter_grade', :points_possible => 15)
+    a1 = @course.assignments.create!(:title => 'assignment1', :grading_type => 'letter_grade', :points_possible => 15, vericite_enabled: true)
+    a1.turnitin_settings = {:originality_report_visibility => 'after_grading'}
+    a1.save!
+    submission = submit_homework(a1, student)
+    sample_vericite_data = {
+      :last_processed_attempt=>1,
+      "attachment_504177"=> {
+        :web_overlap=>73,
+        :publication_overlap=>0,
+        :error=>true,
+        :student_overlap=>100,
+        :state=>"failure",
+        :similarity_score=>100,
+        :object_id=>"123345"
+      },
+      provider: 'vericite'
+    }
+    submission.turnitin_data = sample_vericite_data
+    submission.save!
+
+    json = api_call(:get,
+          "/api/v1/courses/#{@course.id}/assignments/#{a1.id}/submissions/#{student.id}.json",
+          { :controller => 'submissions_api', :action => 'show',
+            :format => 'json', :course_id => @course.id.to_s,
+            :assignment_id => a1.id.to_s, :user_id => student.id.to_s })
+    expect(json).to have_key 'vericite_data'
+    sample_vericite_data.delete :last_processed_attempt
+    expect(json['vericite_data']).to eq sample_vericite_data.with_indifferent_access
+  end
+
+  it "returns turnitin data if present and turnitin is enabled for the assignment" do
+    student = user(:active_all => true)
+    course_with_teacher(:active_all => true)
+    @course.enroll_student(student).accept!
+    a1 = @course.assignments.create!(:title => 'assignment1', :grading_type => 'letter_grade', :points_possible => 15, turnitin_enabled: true)
     a1.turnitin_settings = {:originality_report_visibility => 'after_grading'}
     a1.save!
     submission = submit_homework(a1, student)
@@ -1346,6 +1380,56 @@ describe 'Submissions API', type: :request do
             { student_ids: [@student1.to_param],
               order: "graded_at", order_direction: "descending" })
       expect(json.map { |a| a["assignment_id"] }).to eq [@a2.id, @a1.id, @a3.id]
+    end
+
+    context 'OriginalityReport' do
+      it 'includes has_originality_report if the submission has an originality_report' do
+        attachment_model
+        course active_all: true
+        student_in_course active_all: true
+        teacher_in_course active_all: true
+        @assignment = @course.assignments.create!(title: "some assignment",
+                                                assignment_group: @group,
+                                                points_possible: 12,
+                                                tool_settings_tool: @tool)
+        @attachment.context = @student
+        @attachment.save!
+        submission = @assignment.submit_homework(@student, attachments: [@attachment])
+        OriginalityReport.create!(attachment: @attachment,
+                                  submission: submission,
+                                  workflow_state: 'pending',
+                                  originality_score: 0.1)
+
+        user_session(@teacher)
+        json = api_call(:get,
+                "/api/v1/courses/#{@course.id}/students/submissions.json",
+                { :controller => 'submissions_api', :action => 'for_students',
+                  :format => 'json', :course_id => @course.to_param },
+                { :student_ids => [@student.to_param] })
+        expect(json.first).to have_key 'has_originality_report'
+      end
+
+      it 'does not include has_originality_report if the submission has no originality_report' do
+        attachment_model
+        course active_all: true
+        student_in_course active_all: true
+        teacher_in_course active_all: true
+        @assignment = @course.assignments.create!(title: "some assignment",
+                                                assignment_group: @group,
+                                                points_possible: 12,
+                                                tool_settings_tool: @tool)
+        @attachment.context = @student
+        @attachment.save!
+        @assignment.submit_homework(@student, attachments: [@attachment])
+
+        user_session(@teacher)
+        json = api_call(:get,
+                "/api/v1/courses/#{@course.id}/students/submissions.json",
+                { :controller => 'submissions_api', :action => 'for_students',
+                  :format => 'json', :course_id => @course.to_param },
+                { :student_ids => [@student.to_param] })
+        expect(json.first).not_to have_key 'has_originality_report'
+      end
     end
   end
 

spec/coffeescripts/TurnitinSpec.coffee
@@ -1,0 +1,73 @@
+define ['compiled/gradebook2/Turnitin'], (Turnitin) ->
+
+  submissionWithReport = null
+
+  module "Turnitin",
+    setup: ->
+      submissionWithReport =
+        'id': '7'
+        'body': null
+        'url': null
+        'grade': null
+        'score': null
+        'submitted_at': '2016-11-29T22:29:44Z'
+        'assignment_id': '52'
+        'user_id': '2'
+        'submission_type': 'online_upload'
+        'workflow_state': 'submitted'
+        'grade_matches_current_submission': true
+        'graded_at': null
+        'grader_id': null
+        'attempt': 1
+        'excused': null
+        'late': false
+        'preview_url': 'http://canvas.docker/courses/2/assignments/52/submissions/2?preview=1&version=1'
+        'turnitin_data': 'attachment_103':
+          'similarity_score': 0.8
+          'state': 'acceptable'
+          'report_url': 'http://www.instructure.com'
+          'status': 'pending'
+        'has_originality_report': true
+        'attachments': [ {
+          'id': '103'
+          'folder_id': '9'
+          'display_name': 'Untitled-2.rtf'
+          'filename': '1480456390_119__Untitled.rtf'
+          'content-type': 'text/rtf'
+          'url': 'http://canvas.docker/files/103/download?download_frd=1&verifier=kRS6CMQUNlpF1sobUbALPa0AxE2J70vxPAX7GQqo'
+          'size': null
+          'created_at': '2016-11-29T22:29:43Z'
+          'updated_at': '2016-11-29T22:29:43Z'
+          'unlock_at': null
+          'locked': false
+          'hidden': false
+          'lock_at': null
+          'hidden_for_user': false
+          'thumbnail_url': null
+          'modified_at': '2016-11-29T22:29:43Z'
+          'mime_class': 'doc'
+          'media_entry_id': null
+          'locked_for_user': false
+          'preview_url': null
+        } ]
+        'turnitin': {}
+
+  test 'uses originality_report type in url if submission has an OriginalityReport', () ->
+    tii_data = Turnitin.extractDataForTurnitin(submissionWithReport, 'attachment_103', '/courses/2')
+    equal tii_data.reportUrl, '/courses/2/assignments/52/submissions/2/originality_report/attachment_103'
+
+  test 'uses turnitin or vericite type if no OriginalityReport is present for the submission', () ->
+    submissionWithoutReport = submissionWithReport
+    submissionWithoutReport.has_originality_report = null
+    tii_data = Turnitin.extractDataForTurnitin(submissionWithoutReport, 'attachment_103', '/courses/2')
+    equal tii_data.reportUrl, '/courses/2/assignments/52/submissions/2/turnitin/attachment_103'
+
+  test 'it uses vericite type if vericite data is present', () ->
+    submissionWithReport.vericite_data = submissionWithReport.turnitin_data
+    submissionWithReport.vericite_data.provider = 'vericite'
+    delete submissionWithReport.turnitin_data
+    delete submissionWithReport.has_originality_report
+    tii_data = Turnitin.extractDataForTurnitin(submissionWithReport, 'attachment_103', '/courses/2')
+    equal tii_data.reportUrl, '/courses/2/assignments/52/submissions/2/vericite/attachment_103'
+
+

spec/models/submission_spec.rb
@@ -854,6 +854,20 @@ describe Submission do
         expect(submission.originality_data[attachment.asset_string][:similarity_score]).to eq(2.95)
       end
 
+      it "filters out :provider key and value" do
+         originality_report.originality_report_url = 'http://example.com'
+        originality_report.save!
+        tii_data = {
+          provider: 'vericite',
+          similarity_score: 10,
+          state: 'acceptable',
+          report_url: 'http://example.com/tii',
+          status: 'pending'
+        }
+        submission.turnitin_data[attachment.asset_string] = tii_data
+        expect(submission.originality_data).not_to include :vericite
+      end
+
     end
 
     describe '#originality_report_url' do

spec/selenium/grades/gradebook/gradebook_turnitin_spec.rb
@@ -8,6 +8,7 @@ describe "gradebook2 - turnitin" do
   before(:each) { user_session(@teacher) }
 
   it "should show turnitin data" do
+    @first_assignment.update_attribute(:turnitin_enabled, true)
     s1 = @first_assignment.submit_homework(@student_1, :submission_type => 'online_text_entry', :body => 'asdf')
     s1.update_attribute :turnitin_data, {
       "submission_#{s1.id}" => {

commit 6a9541cf4015cc4c4baf03ced1c612c675f6c20c

VeriCite report links do not work when a user clicks to view the report This seems to be a regression from https://github.com/instructure/canvas-lms/commit/e087da776f2a8037886510ab1677773244ee5653 where it deletes the provider identifier for the originality_data, which is used to determine whether the link is a VeriCite or TII link
Test Plan:
1) enable VeriCite plugin
2) create an assignment with VeriCite enabled
3) submit to the assignment and wait for report to be ready
4) click the link to view the report
5) make sure report works and doesn't give an error saying report is missing

app/models/submission.rb
@@ -561,7 +561,6 @@ class Submission < ActiveRecord::Base
       }
     end
     ret_val = turnitin_data.merge(data)
-    ret_val.delete(:provider)
     ret_val
   end
 