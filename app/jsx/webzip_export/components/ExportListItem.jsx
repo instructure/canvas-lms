1c9362b6b052e6f9df71ea75365e040369b2d76c
Mysti Sadler <mysti@instructure.com>
2017-02-13 16:32:36 -0700

    Stop filtering failed exports from web zip export list
    fixes OFFW-100
    
    Test plan
    - Turn off your local offline web zip set up in offline_web.yml
      so that your export will fail
    - Cause a couple of failures
    - Ensure that the failures all show up in the list when you view
      the offline_web_exports course URL
    - Previously the last one would show up, but it should be all of
      them now.
    
    Change-Id: I785f63d0bab1e86acfbedbf7b6e06c3fc9a25c90
    Reviewed-on: https://gerrit.instructure.com/101985
    Tested-by: Jenkins
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    QA-Review: Nathan Rogowski <nathan@instructure.com>
    Product-Review: Mysti Sadler <mysti@instructure.com>

--- app/jsx/webzip_export/components/ExportListItem.jsx
+++ app/jsx/webzip_export/components/ExportListItem.jsx
@@ -15,11 +15,11 @@ define([
     render () {
       let text = <span>{I18n.t('Package export from')}</span>
       let body = <a href={this.props.link}>{$.datetimeString(this.props.date)}</a>
-      if (this.props.newExport && this.props.workflowState === 'generated') {
-        text = <span>{I18n.t('Most recent export')}</span>
-      } else if (this.props.newExport && this.props.workflowState === 'failed') {
+      if (this.props.workflowState === 'failed') {
         text = <span className="text-error">{I18n.t('Export failed')}</span>
         body = $.datetimeString(this.props.date)
+      } else if (this.props.newExport) {
+        text = <span>{I18n.t('Most recent export')}</span>
       }
       return (
         <li className="webzipexport__list__item">
