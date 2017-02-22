8ea2bb006a1e28a96b2d0c63aa5d271c45f5fa79
Clay Diffrient <cdiffrient@instructure.com>
2017-02-22 13:53:02 -0700

    Make frontend utilize api for collapsed state
    closes FALCOR-108
    
    Test Plan:
      - Enable New User Tutorial feature flag
      - Go to /courses/XX/
      - Collapse the tray, refresh the page
      - Tray should remain collapsed
      - Expand the tray, refresh the page
      - Tray should remain expanded
    
    Change-Id: Ic802cd3434264817610c41f809b954db696e3429
    Reviewed-on: https://gerrit.instructure.com/102953
    Tested-by: Jenkins
    Reviewed-by: Cameron Matheson <cameron@instructure.com>
    QA-Review: Dan Sasaki
    Product-Review: Matt Goodwin <mattg@instructure.com>

--- app/jsx/new_user_tutorial/utils/getProperTray.jsx
+++ app/jsx/new_user_tutorial/utils/getProperTray.jsx
@@ -3,16 +3,17 @@ define([
   '../trays/HomeTray',
   '../trays/ModulesTray'
 ], (I18n, HomeTray, ModulesTray) => {
-  const generateObject = (component, label) => ({
+  const generateObject = (component, label, pageName) => ({
     component,
-    label
+    label,
+    pageName
   });
 
   const getProperTray = (path = window.location.pathname) => {
     if (path.includes('modules')) {
-      return generateObject(ModulesTray, I18n.t('Modules Tutorial Tray'));
+      return generateObject(ModulesTray, I18n.t('Modules Tutorial Tray'), 'modules');
     }
-    return generateObject(HomeTray, I18n.t('Home Tutorial Tray'));
+    return generateObject(HomeTray, I18n.t('Home Tutorial Tray'), 'home');
   }
 
   return getProperTray;
