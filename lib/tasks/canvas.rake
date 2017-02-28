instructure/canvas-lms

commit 02d13b4e173a227deba6b5584fab4813482cb7e0

compile_assets improvements, refs SD-2097
speed up require_js and webpack builds. here's how:

* bump i18nliner
  * it uses babel
  * it is much faster
* tweak canvas_i18nliner to not need to precompile anything
* run i18n:generate_js without having to precompile anything
* webpack can avoid running js:generate altogether

test plan:
* tests pass
* i18n works as per usual under requirejs
* i18n works as per usual under webpack
* `rake i18n:generate`:
   * builds identical yml before/after
   * runs faster now
   * works even if you nuke compiled coffee/jsx
* `rake i18n:generate_js`:
   * builds identical translation files before/after
   * runs faster now
   * works even if you nuke compiled coffee/jsx

Change-Id: I2682e68ff87168d9f76e20deb93713aa9a50477a
Reviewed-on: https://gerrit.instructure.com/103285
Tested-by: Jenkins
Reviewed-by: Ryan Shaw <ryan@instructure.com>
Product-Review: Jon Jensen <jon@instructure.com>
QA-Review: Jon Jensen <jon@instructure.com>

lib/tasks/canvas.rake
@@ -51,8 +51,13 @@ namespace :canvas do
     build_js = ENV["COMPILE_ASSETS_BUILD_JS"] != "0"
     build_api_docs = ENV["COMPILE_ASSETS_API_DOCS"] != "0"
 
-    # opt in
-    webpack_and_rjs = ENV["COMPILE_ASSETS_WEBPACK_RJS_FALLBACK"] == "1"
+    compile_js = !CANVAS_WEBPACK
+    # normally one or the other
+    build_requirejs = build_js && !CANVAS_WEBPACK
+    build_webpack = build_js && CANVAS_WEBPACK
+
+    # unless you opt-in to both
+    build_requirejs = true if ENV["COMPILE_ASSETS_WEBPACK_RJS_FALLBACK"] == "1"
 
     if npm_install
       log_time('Making sure node_modules are up to date') {
@@ -76,32 +81,31 @@ namespace :canvas do
       }
     end
 
-    # TODO: Once webpack is the only way, remove js:build
-    if CANVAS_WEBPACK
-      build_js_msg = build_js ? ", js 18n, run webpack" : ""
-      msg = webpack_and_rjs ? ", r.js optimizer" : ""
-      tasks["compile coffee/jsx#{build_js_msg}#{msg}"] = -> {
-        log_time('js:generate') { Rake::Task['js:generate'].invoke }
-        if build_js
-          log_time('i18n:generate_js') { Rake::Task['i18n:generate_js'].invoke }
-          ptasks = ['js:webpack']
-          ptasks << 'js:build' if webpack_and_rjs
-          # webpack and js:build can run concurrently
-          Parallel.each(ptasks, in_threads: parallel_processes) do |name|
-            log_time(name) { Rake::Task[name].invoke }
-          end
+    # do this up front, since concurrent `i18n:generate_js` + `js:clean` = sadness
+    Rake::Task['js:clean'].invoke
+    Rake::Task['js:build_client_apps'].invoke
+
+    generate_tasks = []
+    generate_tasks << 'js:generate' if compile_js
+    generate_tasks << 'i18n:generate_js' if build_requirejs || build_webpack
+    build_tasks = []
+    build_tasks << 'js:webpack' if build_webpack
+    build_tasks << 'js:build' if build_requirejs
+
+    msg = "run " + (generate_tasks + build_tasks).join(", ")
+    tasks[msg] = -> {
+      if generate_tasks.any?
+        Parallel.each(generate_tasks, in_processes: parallel_processes) do |name|
+          log_time(name) { Rake::Task[name].invoke }
         end
-      }
-    else
-      msg = build_js ? ", js i18n, r.js optimizer" : ""
-      tasks["compile coffee/jsx#{msg}"] = -> {
-        log_time('js:generate') { Rake::Task['js:generate'].invoke }
-        if build_js
-          log_time('i18n:generate_js') { Rake::Task['i18n:generate_js'].invoke }
-          log_time('js:build') { Rake::Task['js:build'].invoke }
+      end
+
+      if build_tasks.any?
+        Parallel.each(build_tasks, in_threads: parallel_processes) do |name|
+          log_time(name) { Rake::Task[name].invoke }
         end
-      }
-    end
+      end
+    }
 
     if build_api_docs
       tasks["Generate documentation [yardoc]"] = -> {

.eslintignore
@@ -1,4 +1,5 @@
 public/javascripts/vendor/**/*
 public/javascripts/enzyme_requireJS.js
 
+gems/canvas_i18nliner
 doc/

app/coffeescripts/.i18nignore
@@ -1,0 +1,1 @@
+/handlebars_helpers.coffee

app/jsx/calendar/scheduler/components/FindAppointment.jsx
@@ -6,7 +6,7 @@ define([
 ], (React, Modal, I18n, Actions) => {
   class FindAppointment extends React.Component {
 
-    static propTypes: {
+    static propTypes = {
       courses: React.PropTypes.array.isRequired,
       store: React.PropTypes.object.isRequired,
     }

frontend_build/emberHandlebars.js
@@ -7,7 +7,7 @@
 const Handlebars = require('handlebars')
 const EmberHandlebars = require('ember-template-compiler').EmberHandlebars
 const ScopedHbsExtractor = require(__dirname + '/../gems/canvas_i18nliner/js/scoped_hbs_extractor')
-const PreProcessor = require(__dirname + '/../gems/canvas_i18nliner/node_modules/i18nliner-handlebars/dist/lib/pre_processor')
+const PreProcessor = require(__dirname + '/../gems/canvas_i18nliner/node_modules/i18nliner-handlebars/dist/lib/pre_processor').default
 
 function compileHandlebars (data) {
   const {path, source} = data

frontend_build/i18nLinerHandlebars.js
@@ -9,7 +9,7 @@ const {EmberHandlebars} = require('ember-template-compiler')
 const ScopedHbsExtractor = require('./../gems/canvas_i18nliner/js/scoped_hbs_extractor')
 require('babel-polyfill')
 const {allFingerprintsFor} = require('brandable_css/lib/main')
-const PreProcessor = require('./../gems/canvas_i18nliner/node_modules/i18nliner-handlebars/dist/lib/pre_processor')
+const PreProcessor = require('./../gems/canvas_i18nliner/node_modules/i18nliner-handlebars/dist/lib/pre_processor').default
 require('./../gems/canvas_i18nliner/js/scoped_hbs_pre_processor')
 
 // In this main file, we do a bunch of stuff to monkey-patch the default behavior of

gems/canvas_i18nliner/bin/prepare_hbs
@@ -4,7 +4,7 @@ var readline = require('readline');
 var Handlebars = require('handlebars');
 var EmberHandlebars = require('ember-template-compiler').EmberHandlebars;
 var ScopedHbsExtractor = require('../js/scoped_hbs_extractor');
-var PreProcessor = require('i18nliner-handlebars/dist/lib/pre_processor');
+var PreProcessor = require('i18nliner-handlebars/dist/lib/pre_processor').default;
 
 // make sure necessary overrides are set up (e.g. HbsPreProcessor.normalizeInterpolationKey)
 require("../js/main");

gems/canvas_i18nliner/js/generate_js.js
@@ -1,4 +1,4 @@
-var I18nliner = require("i18nliner");
+var I18nliner = require("i18nliner").default;
 var Commands = I18nliner.Commands;
 var Check = Commands.Check;
 var mkdirp = require("mkdirp");

commit 31c0b81cd6b415f868c69af847584ce703385dc8

ensure you can still do requirejs and webpack simultaneously
regression in 02d13b4e

test plan:
* `USE_WEBPACK=1 COMPILE_ASSETS_WEBPACK_RJS_FALLBACK=1 \
   rake canvas:compile_assets`
* it should work

Change-Id: Ie9f51367e11b97315fe2fc4b0fd6ac254802cfd7
Reviewed-on: https://gerrit.instructure.com/103404
Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
Product-Review: Landon Wilkins <lwilkins@instructure.com>
QA-Review: Landon Wilkins <lwilkins@instructure.com>
Tested-by: Jenkins

lib/tasks/canvas.rake
@@ -51,7 +51,7 @@ namespace :canvas do
     build_js = ENV["COMPILE_ASSETS_BUILD_JS"] != "0"
     build_api_docs = ENV["COMPILE_ASSETS_API_DOCS"] != "0"
 
-    compile_js = !CANVAS_WEBPACK
+    compile_js = !CANVAS_WEBPACK || ENV["COMPILE_ASSETS_WEBPACK_RJS_FALLBACK"] == "1"
     # normally one or the other
     build_requirejs = build_js && !CANVAS_WEBPACK
     build_webpack = build_js && CANVAS_WEBPACK