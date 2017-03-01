e1bbcdf0e67d1e3563146f7aecb96700fc8138cc
Clay Diffrient <cdiffrient@instructure.com>
2017-03-01 08:41:56 -0700

    Add modules tutorial tray
    closes FALCOR-109
    
    Test Plan:
      - Enable new user tutorials
      - Go to the modules page for a course
      - The tutorial tray should show the content like this
        mockup https://cl.ly/0G1w473T1o3b
    
    Change-Id: Iada14ffcd929742f5ae7172acdae93b3714f0379
    Reviewed-on: https://gerrit.instructure.com/103571
    Tested-by: Jenkins
    QA-Review: Dan Sasaki
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Christi Wruck

--- app/jsx/new_user_tutorial/trays/ModulesTray.jsx
+++ app/jsx/new_user_tutorial/trays/ModulesTray.jsx
@@ -2,12 +2,26 @@ define([
   'react',
   'i18n!new_user_tutorial',
   'instructure-ui',
-], (React, I18n, { Heading }) => {
-  const HomeTray = () => (
+  'jsx/shared/SVGWrapper'
+], (React, I18n, { Heading, Typography }, SVGWrapper) => {
+  const ModulesTray = () => (
     <div>
       <Heading as="h2" level="h1" >{I18n.t('Modules')}</Heading>
+      <Typography size="large" as="p">
+        {I18n.t('Organize your course content')}
+      </Typography>
+      <Typography as="p">
+        {
+            I18n.t(`Organize and segment your course by topic, unit, chapter,
+                    or week. Sequence select modules by defining criteria and
+                    prerequisites.`)
+          }
+      </Typography>
+      <div className="ModulesTutorialTray__ImageContainer">
+        <SVGWrapper url="/images/module_tutorial.svg" />
+      </div>
     </div>
   );
 
-  return HomeTray;
+  return ModulesTray;
 });
