d2d7b81fee31d933144ef3edb78c1a34f4606618
Cody Cutrer <cody@instructure.com>
2017-03-02 13:47:55 -0700

    ruby 2.4: bump cassandra-cql
    Change-Id: I5e576b9351bc378a5f3ce3df0f0a91b8b8ffa45a
    Reviewed-on: https://gerrit.instructure.com/103807
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/cassandra.rb
+++ Gemfile.d/cassandra.rb
@@ -1,5 +1,5 @@
 group :cassandra do
-  gem 'cassandra-cql', '1.2.2', github: 'kreynolds/cassandra-cql', ref: 'beed72e249d02cebc850a4b92b468c8b64b12257' #dependency of canvas_cassandra
+  gem 'cassandra-cql', '1.2.2', github: 'kreynolds/cassandra-cql', ref: 'fa9e4253ec35e1066f76418b1cd6ee03019ecb82' #dependency of canvas_cassandra
     gem 'simple_uuid', '0.4.0', require: false
     gem 'thrift', '0.8.0', require: false
     gem 'thrift_client', '0.8.4', require: false
