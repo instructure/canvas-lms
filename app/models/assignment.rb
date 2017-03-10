instructure/canvas-lms

commit ffe268b829934efa684139fc21cfe58cbe864433

add default originality setting to turn it in accounts settings
fixes: PFS-4378

**Test Plan
1. Enable TII in root account
2. set default originality, and save
3. create new assignment, make sure it inherits default originality
4. repeat 1-3 for a subaccount, make sure correct originality
setting is inherited.

Change-Id: I66dd964b8faac7c69f2f37ce920a35e025f2ca66
Reviewed-on: https://gerrit.instructure.com/80213
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
Tested-by: Jenkins
QA-Review: KC Naegle <knaegle@instructure.com>
Product-Review: Christi Wruck

app/models/assignment.rb
@@ -366,7 +366,14 @@ class Assignment < ActiveRecord::Base
   end
 
   def turnitin_settings
-    super.empty? ? Turnitin::Client.default_assignment_turnitin_settings : super
+    if super.empty?
+      settings = Turnitin::Client.default_assignment_turnitin_settings
+      default_originality = course.turnitin_originality if course
+      settings[:originality_report_visibility] = default_originality if default_originality
+      settings
+    else
+      super
+    end
   end
 
   def turnitin_settings=(settings)

app/helpers/accounts_helper.rb
@@ -20,12 +20,12 @@ module AccountsHelper
   def show_last_batch
     @last_batch && !(@current_batch && @current_batch.importing?)
   end
-    
+
   def print_messages(batch)
     return '' unless batch
     render :partial => 'accounts/sis_batch_messages', :object => batch
   end
-  
+
   def print_counts(batch)
     return '' unless batch.data && batch.data[:counts]
     render :partial => 'accounts/sis_batch_counts', :object => batch
@@ -35,5 +35,14 @@ module AccountsHelper
     show_term = course.enrollment_term && !course.enrollment_term.default_term?
     show_code = course.course_code != course.name
     "#{course.course_code if show_code}#{', ' if show_term && show_code}#{course.enrollment_term.name if show_term}"
+  end
+
+  def turnitin_originality_options
+    [
+      [I18n.t('#turnitin_settings.originality_report_visible_immediately', "Immediately"), 'immediate'],
+      [I18n.t('#turnitin_settings.originality_report_visible_after_grading', "After the assignment is graded"), 'after_grading'],
+      [I18n.t('#turnitin_settings.originality_report_visible_after_due_date', "After the Due Date"), 'after_due_date'],
+      [I18n.t('#turnitin_settings.originality_report_never', "Never"), 'never']
+    ]
   end
 end

app/models/account.rb
@@ -21,7 +21,7 @@ require 'atom'
 class Account < ActiveRecord::Base
   include Context
   attr_accessible :name, :turnitin_account_id, :turnitin_shared_secret,
-    :turnitin_host, :turnitin_comments, :turnitin_pledge,
+    :turnitin_host, :turnitin_comments, :turnitin_pledge, :turnitin_originality,
     :default_time_zone, :parent_account, :settings, :default_storage_quota,
     :default_storage_quota_mb, :storage_quota, :ip_filters, :default_locale,
     :default_user_storage_quota_mb, :default_group_storage_quota_mb, :integration_id, :brand_config_md5
@@ -1244,6 +1244,14 @@ class Account < ActiveRecord::Base
       self.turnitin_comments
     else
       self.parent_account.try(:closest_turnitin_comments)
+    end
+  end
+
+  def closest_turnitin_originality
+    if self.turnitin_originality && !self.turnitin_originality.empty?
+      self.turnitin_originality
+    else
+      self.parent_account.try(:turnitin_originality)
     end
   end
 

app/models/course.rb
@@ -1790,6 +1790,10 @@ class Course < ActiveRecord::Base
     self.account.closest_turnitin_pledge
   end
 
+  def turnitin_originality
+    self.account.closest_turnitin_originality
+  end
+
   def all_turnitin_comments
     comments = self.account.closest_turnitin_comments || ""
     if self.turnitin_comments && !self.turnitin_comments.empty?

app/views/accounts/settings.html.erb
@@ -605,6 +605,12 @@ TEXT
                   <%= f.text_area :turnitin_pledge, :style => "width: 90%; height: 50px;" %>
                 </td>
               </tr>
+              <tr>
+                <td><%= f.blabel :originality_report_visibility, :en => "Students can see the originality report" %></td>
+                <td>
+                  <%= f.select :turnitin_originality, options_for_select(turnitin_originality_options, @account.turnitin_originality)%>
+                </td>
+              </tr>
             </table>
           </fieldset>
         <% end %>

db/migrate/20160520035902_add_turnitin_originality_to_accounts.rb
@@ -1,0 +1,7 @@
+class AddTurnitinOriginalityToAccounts < ActiveRecord::Migration
+  tag :predeploy
+
+  def change
+    add_column :accounts, :turnitin_originality, :string
+  end
+end

spec/lib/notification_message_creator_spec.rb
@@ -134,10 +134,11 @@ describe NotificationMessageCreator do
     end
 
     it "should not send dispatch messages for pre-registered users" do
+      course
       notification_model
       u1 = user_model(:name => "user 2")
       u1.communication_channels.create(:path => "user2@example.com").confirm!
-      @a = Assignment.create
+      @a = @course.assignments.create
       messages = NotificationMessageCreator.new(@notification, @a, :to_list => u1).create_message
       expect(messages).to be_empty
     end
@@ -160,10 +161,11 @@ describe NotificationMessageCreator do
     end
 
     it "should send dashboard and dispatch messages for registered users based on default policies" do
+      course
       notification_model(:category => 'TestImmediately')
       u1 = user_model(:name => "user 1", :workflow_state => "registered")
       u1.communication_channels.create(:path => "user1@example.com").confirm!
-      @a = Assignment.create
+      @a = @course.assignments.create
       messages = NotificationMessageCreator.new(@notification, @a, :to_list => u1).create_message
       expect(messages).not_to be_empty
       expect(messages.length).to eql(2)
@@ -235,11 +237,12 @@ describe NotificationMessageCreator do
     end
 
     it "should send dashboard (but not dispatch messages) for registered users based on default policies" do
+      course
       notification_model(:category => 'TestNever', :name => "Show In Feed")
       expect(@notification.default_frequency).to eql("never")
       u1 = user_model(:name => "user 1", :workflow_state => "registered")
       u1.communication_channels.create(:path => "user1@example.com").confirm!
-      @a = Assignment.create
+      @a = @course.assignments.create()
       messages = NotificationMessageCreator.new(@notification, @a, :to_list => u1).create_message
       expect(messages).not_to be_empty
       expect(messages.length).to eql(1)
@@ -247,11 +250,12 @@ describe NotificationMessageCreator do
     end
 
     it "should not send dashboard messages for non-feed or non-dashboard messages" do
+      course
       notification_model(:category => 'TestNever', :name => "Don't Show In Feed")
       expect(@notification.default_frequency).to eql("never")
       u1 = user_model(:name => "user 1", :workflow_state => "registered")
       u1.communication_channels.create(:path => "user1@example.com").confirm!
-      @a = Assignment.create
+      @a = @course.assignments.create
       messages = NotificationMessageCreator.new(@notification, @a, :to_list => u1).create_message
       expect(messages).to be_empty
       @notification.name = "Show In Feed"

spec/models/account_spec.rb
@@ -361,6 +361,29 @@ describe Account do
     end
   end
 
+  context "closest_turnitin_originality" do
+    before :each do
+        @root_account = Account.create!(:turnitin_pledge => "root")
+        @root_account.turnitin_originality = 'after_grading'
+        @root_account.save!
+    end
+
+    it "should find closest_turnitin_originality from root account" do
+      expect(@root_account.closest_turnitin_originality).to eq('after_grading')
+    end
+
+    it "should find closest_turnitin_originality from sub account" do
+      sub_account = Account.create(:name => 'sub', :parent_account => @root_account)
+      sub_account.turnitin_originality = 'never'
+      expect(sub_account.closest_turnitin_originality).to eq('never')
+    end
+
+    it "should find closest_turnitin_originality from sub account when set on root account" do
+      sub_account = Account.create(:name => 'sub', :parent_account => @root_account)
+      expect(sub_account.closest_turnitin_originality).to eq('after_grading')
+    end
+  end
+
   context "closest_turnitin_pledge" do
     it "should work for custom sub, custom root" do
       root_account = Account.create!(:turnitin_pledge => "root")

commit 0a0c05ffe00a2f58b03e2b29d49d86c94d43fb65

VeriCite Advanced Settings in the Assignment tool get overridden by Turnitin settings after the first submission to the assignment This seems to be a regression introduced from https://github.com/instructure/canvas-lms/commit/ffe268b829934efa684139fc21cfe58cbe864433 When a VeriCite assignment is first saved, it will have the correct data saved in the turnitin_data structure. However, once a user submits, the assignment is resaved with the default Turnitin settings and the VeriCite settings get overridden
Test Plan:
1) Make sure that the test plan in https://github.com/instructure/canvas-lms/commit/ffe268b829934efa684139fc21cfe58cbe864433 works
2) Create a VeriCite assignment with the advanced settings all set to true/checked (make sure only VeriCite is enabled and not other plagiarism service)
3) Edit the assignment and verify that the advanced settings for VeriCite are still all set to true (no need to resave)
4) Submit as a student to the assignment
5) Wait a few mins (cron job takes about 1-2 mins)
6) Edit the assignment and verify that the advanced settings for VeriCite are still all set to true

app/models/assignment.rb
@@ -429,7 +429,11 @@ class Assignment < ActiveRecord::Base
   end
 
   def turnitin_settings=(settings)
-    settings = Turnitin::Client.normalize_assignment_turnitin_settings(settings)
+    if vericite_enabled?
+      settings = VeriCite::Client.normalize_assignment_vericite_settings(settings)
+    else
+      settings = Turnitin::Client.normalize_assignment_turnitin_settings(settings)
+    end
     unless settings.blank?
       [:created, :error].each do |key|
         settings[key] = self.turnitin_settings[key] if self.turnitin_settings[key]