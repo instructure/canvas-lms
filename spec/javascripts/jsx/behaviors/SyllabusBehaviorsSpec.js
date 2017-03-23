b114036d39aeff0d36d9d9f2b5b24b716d6a86b3
Clay Diffrient <cdiffrient@instructure.com>
2017-03-23 13:09:04 -0600

    spec: convert remainder of syllabus behaviors spec to js
    Test Plan:
      - Automated tests pass
    
    Change-Id: Ib51f6e44eb29096bd1025535e722c67c52bae218
    Reviewed-on: https://gerrit.instructure.com/106131
    Tested-by: Jenkins
    Reviewed-by: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>

--- spec/javascripts/jsx/behaviors/SyllabusBehaviorsSpec.js
+++ spec/javascripts/jsx/behaviors/SyllabusBehaviorsSpec.js
@@ -3,6 +3,7 @@ import Sidebar from 'jsx/shared/rce/Sidebar';
 import editorUtils from 'helpers/editorUtils';
 import fixtures from 'helpers/fixtures';
 import $ from 'jquery';
+import RichContentEditor from 'jsx/shared/rce/RichContentEditor';
 
 QUnit.module('SyllabusBehaviors.bindToEditSyllabus', {
   setup () {
@@ -31,4 +32,36 @@ test('sets focus to the edit button when hide_edit occurs', function () {
   SyllabusBehaviors.bindToEditSyllabus();
   $('#edit_course_syllabus_form').trigger('hide_edit');
   equal(document.activeElement, $('.edit_syllabus_link')[0]);
+});
+
+test('initializes sidebar when edit link present', function () {
+  fixtures.create('<a href="#" class="edit_syllabus_link">Edit Link</a>');
+  SyllabusBehaviors.bindToEditSyllabus();
+  ok(Sidebar.init.called, 'foo');
+});
+
+
+test('skips initializing sidebar when edit link absent', function () {
+  equal(fixtures.find('.edit_syllabus_link').length, 0);
+  SyllabusBehaviors.bindToEditSyllabus();
+  ok(Sidebar.init.notCalled, 'bar');
+});
+
+
+test('sets syllabus_body data value on fresh node when showing edit form', function () {
+  const fresh = { val: sinon.spy() };
+  this.stub(RichContentEditor, 'freshNode').returns(fresh);
+  this.stub(RichContentEditor, 'loadNewEditor');
+  fixtures.create('<div id="course_syllabus"></div>');
+  fixtures.create('<a href="#" class="edit_syllabus_link">Edit Link</a>');
+  fixtures.create('<form id="edit_course_syllabus_form"></form>');
+  fixtures.create('<textarea id="course_syllabus_body"></textarea>');
+  const text = 'foo';
+  $('#course_syllabus').data('syllabus_body', text);
+  const $form = SyllabusBehaviors.bindToEditSyllabus();
+  $form.triggerHandler('edit');
+  ok(RichContentEditor.freshNode.called);
+  const body = document.getElementById('course_syllabus_body');
+  equal(RichContentEditor.freshNode.firstCall.args[0][0], body);
+  ok(fresh.val.calledWith(text));
 });
