a708ae75ac9774564625aae9a7f82d6d92a3fed8
Ryan Shaw <ryan@instructure.com>
2017-03-24 17:22:18 -0600

    fix bug on ModulesHomePage
    fixes: CNVS-35909
    
    ModulesHomePage would sometimes try to 
    render into a DOM node that doesn’t exist
    
    I noticed when trying to run spec/selenium/context_modules_student_spec.rb
    It would keep throwing a javascript error about:
    “_registerComponent(...): Target container is not a DOM element.”
    and the root cause was because this file was trying to render
    Into a Dom element that doesn’t exist.
    
    test plan:
    * go to the modules home page when it is blank. you should see the
      new stuff that falcor added to that page
    * create a module
    * go to that page again, it should not throw a javascript error in the
      web console.
    
    Change-Id: Icf5dd46586fc86915135dec3624c602028168384
    Reviewed-on: https://gerrit.instructure.com/106385
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Reviewed-by: Chris Hart <chart@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/bundles/context_modules.js
+++ app/jsx/bundles/context_modules.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom'
 import ModulesHomePage from 'jsx/courses/ModulesHomePage'
 import modules from 'context_modules'
 
-ReactDOM.render(
-  <ModulesHomePage onCreateButtonClick={modules.addModule} />, $('#modules_homepage_user_create')[0]
-)
+
+const container = document.getElementById('modules_homepage_user_create')
+if (container) {
+  ReactDOM.render(<ModulesHomePage onCreateButtonClick={modules.addModule} />, container)
+}
 
 if (ENV.NO_MODULE_PROGRESSIONS) {
   $('.module_progressions_link').remove()
