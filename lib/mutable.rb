56653b907253555be5951ec7234ec21742ea941b
James Williams <jamesw@instructure.com>
2017-04-11 07:37:10 -0600

    update submission_comments_count after unmuting assignment
    test plan:
    * have a muted assignment with a submission
    * as a teacher, make a submission comment
    * unmute the assignment
    * the submission comments should show on the
     student's conversations inbox page
    
    closes #CNVS-18916
    
    Change-Id: I05fcfdfb6c06ea71f4626d22e32bed32ee966dfa
    Reviewed-on: https://gerrit.instructure.com/108166
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- lib/mutable.rb
+++ lib/mutable.rb
@@ -24,6 +24,7 @@ module Mutable
     self.update_attribute(:muted, true)
     clear_sent_messages
     hide_stream_items
+    true
   end
 
   def unmute!
@@ -31,6 +32,7 @@ module Mutable
     self.update_attribute(:muted, false)
     broadcast_unmute_event
     show_stream_items
+    true
   end
 
   def broadcast_unmute_event
@@ -62,9 +64,9 @@ module Mutable
 
   def show_stream_items
     if self.respond_to? :submissions
-      submissions        = self.submissions.preload(hidden_submission_comments: :author)
+      submission_ids = self.submissions.pluck(:id)
       stream_items = StreamItem.select([:id, :context_type, :context_id]).
-          where(:asset_type => 'Submission', :asset_id => submissions).
+          where(:asset_type => 'Submission', :asset_id => submission_ids).
           preload(:context).to_a
       stream_item_contexts = stream_items.map { |si| [si.context_type, si.context_id] }
       associated_shards = stream_items.inject([]) { |result, si| result | si.associated_shards }
@@ -73,12 +75,14 @@ module Mutable
             update_all_with_invalidation(stream_item_contexts, :hidden => false)
       end
 
-      outstanding = submissions.map{ |submission|
-        comments = submission.hidden_submission_comments.to_a
-        next if comments.empty?
-        [submission, comments.map(&:author_id).uniq.size == 1 ? [comments.last.author] : []]
-      }.compact
-      SubmissionComment.where(:hidden => true, :submission_id => submissions).update_all(:hidden => false)
+      hidden_comment_sub_ids = SubmissionComment.where(:hidden => true, :submission_id => submission_ids).pluck(:submission_id)
+      if hidden_comment_sub_ids.any?
+        SubmissionComment.where(:hidden => true, :submission_id => hidden_comment_sub_ids).update_all(:hidden => false)
+        Submission.where(:id => hidden_comment_sub_ids).
+          update_all(["submission_comments_count = (SELECT COUNT(*) FROM #{SubmissionComment.quoted_table_name} WHERE
+            submissions.id = submission_comments.submission_id AND submission_comments.hidden = ? AND
+            submission_comments.draft IS NOT TRUE AND submission_comments.provisional_grade_id IS NULL)", false])
+      end
     end
   end
 end
