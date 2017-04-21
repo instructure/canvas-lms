fdb8c5f7500177eb6b6cbaae2a4e6abae567acc6
Brad Humphrey <brad@instructure.com>
2017-04-21 12:45:15 -0600

    create a class for ContentItemSelectionRequest generation
    test plan: specs pass
    
    Change-Id: I6c3fd4026b643329af438bcf0a033ed3b4d5f444
    Fixes: PLAT-2534
    Reviewed-on: https://gerrit.instructure.com/109386
    Tested-by: Jenkins
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Brad Humphrey <brad@instructure.com>
    QA-Review: Brad Humphrey <brad@instructure.com>

---
+++ spec/lib/lti/content_item_selection_request_spec.rb
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2017 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
+
+describe Lti::ContentItemSelectionRequest do
+  subject(:lti_request) { described_class.new }
+  context '#generate_lti_launch' do
+    it 'generates an Lti::Launch' do
+      expect(lti_request.generate_lti_launch).to be_a Lti::Launch
+    end
+  end
+end
