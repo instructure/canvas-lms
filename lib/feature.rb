5720e59f84980e768aaa4cdfa016f5231dc76c63
James Williams <jamesw@instructure.com>
2017-04-21 19:41:34 +0000

    Revert "Hide Enable new SIS integration settings in prod"
    This reverts commit 38254bc3409fa7e8ac53cea35ec5398f341858f4.
    
    Change-Id: I5aae121822208636bb11907f252cb24190f8f301
    Reviewed-on: https://gerrit.instructure.com/109393
    Tested-by: Jenkins
    QA-Review: Pedro Fajardo <pfajardo@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- lib/feature.rb
+++ lib/feature.rb
@@ -322,8 +322,7 @@ END
       applies_to: 'Account',
       state: 'hidden',
       root_opt_in: true,
-      beta: true,
-      development: true
+      beta: true
     },
     'bulk_sis_grade_export' =>
       {
