388e6f474ffcedcf8616aba230b6d6eb0f54705e
Landon Wilkins <lwilkins@instructure.com>
2017-04-27 14:02:40 -0600

    da licença part 28
    add consistent license headers to all source files
    (ruby, coffeescript, javascript)
    
    except for vendor files
    
    Change-Id: I394b225a1a6d43da86d0986b0ef303544bc9ddfc
    Reviewed-on: https://gerrit.instructure.com/110052
    Tested-by: Jenkins
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- lib/data_fixup/copy_role_overrides.rb
+++ lib/data_fixup/copy_role_overrides.rb
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2013 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 module DataFixup::CopyRoleOverrides
   def self.run(old_permission, new_permission)
     RoleOverride.where(:permission => old_permission.to_s).find_in_batches do |old_role_overrides|
