58e4df24946c3a4b0c690f94a5c27c89dac07ec9
Landon Wilkins <lwilkins@instructure.com>
2017-04-27 14:17:40 -0600

    da licença part 37
    add consistent license headers to all source files
    (ruby, coffeescript, javascript)
    
    except for vendor files
    
    Change-Id: Ibc881431ca51b7da3b0d3182033e95ebf7d6e524
    Reviewed-on: https://gerrit.instructure.com/110067
    Tested-by: Jenkins
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- db/migrate/20111017124400_group_categories_cleanup_migration.rb
+++ db/migrate/20111017124400_group_categories_cleanup_migration.rb
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2011 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 # an older version of 20111007115901_group_categories_data_migration.rb was
 # broken and did not update assignments correctly. the current version is
 # fixed, but if you ran the broken one, this will clean it up. if you ran the
