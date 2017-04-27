ba2daaf15873d42fa951e93a9964eeb3ed95c5e1
Landon Wilkins <lwilkins@instructure.com>
2017-04-27 13:46:17 -0600

    da licença part 13
    add consistent license headers to all source files
    (ruby, coffeescript, javascript)
    
    except for vendor files
    
    Change-Id: I120ddb74d7f2a34153334b01d3ce2fb77000a631
    Reviewed-on: https://gerrit.instructure.com/110034
    Tested-by: Jenkins
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- app/models/master_courses/master_template.rb
+++ app/models/master_courses/master_template.rb
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2016 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 class MasterCourses::MasterTemplate < ActiveRecord::Base
   # NOTE: at some point we can use this model if we decide to allow collections of objects within a course to be pushed out
   # instead of the entire course, but for now that's what we'll roll with
