c5b5cc786d42171b750f11bcb1ae5ac7b341eec7
Landon Wilkins <lwilkins@instructure.com>
2017-04-29 10:25:41 -0600

    da licença part 53
    add consistent license headers to all source files
    
    Change-Id: I405d2def01ad68c76533b9254983497485cd7073
    Reviewed-on: https://gerrit.instructure.com/110160
    Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Jon Jensen <jon@instructure.com>
    Product-Review: Jon Jensen <jon@instructure.com>
    QA-Review: Jon Jensen <jon@instructure.com>

--- gems/csv_diff/lib/csv_diff/diff.rb
+++ gems/csv_diff/lib/csv_diff/diff.rb
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2015 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 module CsvDiff
   # We need to know which key(s) comprise the id, to properly detect deletes
   # vs updates. Other than that, we don't need to care what the data is, just
--- gems/event_stream/spec/event_stream/attr_config_spec.rb
+++ gems/event_stream/spec/event_stream/attr_config_spec.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 Instructure, Inc.
+# Copyright (C) 2014 - present Instructure, Inc.
 #
 # This file is part of Canvas.
 #
@@ -12,7 +12,7 @@
 # A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
 #
-# You have received a copy of the GNU Affero General Public License along
+# You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
