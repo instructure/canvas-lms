b15619ae396f59d14103d1f8d6d6f2ead63cc212
Dan Minkevitch <dan@instructure.com>
2017-05-03 17:38:15 -0700

    Fix datetime input display issue with datepicker
    Fixes CNVS-36705
    
    Test Plan:
    * using new scheduler, navigate to the calendar page
    * click on a day to open the create event modal
    * for each of the three tabs, do the following:
      * ensure the date picker value contains the correct date
        corresponding with the date you clicked and time you selected
      * ensure the date picker value contains day of week, date, and time
        if specified
      * use the date picker to pick a new date
      * ensure the new date picker value also contains the new day of the week,
        date, and time
    
    Change-Id: Ia67ca130fe024403cf4b0c419fa9ab33e83244a5
    Reviewed-on: https://gerrit.instructure.com/110786
    Tested-by: Jenkins
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: David Tan <dtan@instructure.com>
    Product-Review: Dan Minkevitch <dan@instructure.com>

--- app/jsx/shared/helpers/datePickerFormat.js
+++ app/jsx/shared/helpers/datePickerFormat.js
@@ -16,8 +16,20 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// translate a strftime style format string (guaranteed to only use %d, %-d,
-// %b, and %Y, though in dynamic order) into a datepicker style format string
+// translate a strftime style format string into a datepicker style format string
+// Examples:
+// %a %b %-d, %Y %-l:%M%P
+// %a %b %-d, %Y %-k:%M
+// %b %-d, %Y
+import tz from 'timezone'
+
 export default function datePickerFormat (format) {
-  return format.replace(/%Y/, 'yy').replace(/%b/, 'M').replace(/%-?d/, 'd').replace(/%a/, 'D')
+  return tz.adjustFormat(format)
+    .replace(/%Y/, 'yy') // Year (eg. 2017)
+    .replace(/%b/, 'M') // Month (eg. May)
+    .replace(/%-?d/, 'd') // Day of Month (eg. 3)
+    .replace(/%a/, 'D') // Day of week (eg. Wed)
+    .replace(/%-?[lk]|:|%M|%P/g, '') // Remove time info*
+    .trim()
+  // *Time info removed because it's already added by the datetime picker
 }
