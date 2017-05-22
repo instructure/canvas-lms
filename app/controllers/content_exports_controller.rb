f20a840f70b4e541fbf471fca59ae3358d99bda8
Simon Williams <simon@instructure.com>
2017-05-22 15:56:50 -0600

    mark content exports older than 30 days as expired
    closes CNVS-37071
    
    test plan:
    - export course content, user content, and a folder
    - tweak their created_at dates
    - try different values of Setting.get('content_exports_expire_after_days', '30')
    - the UI should show expired and api should not return an attachment url
    
    Change-Id: I91923666752bc1e5d11f1e5319a83e040a363d41
    Reviewed-on: https://gerrit.instructure.com/112720
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- app/controllers/content_exports_controller.rb
+++ app/controllers/content_exports_controller.rb
@@ -94,7 +94,7 @@ class ContentExportsController < ApplicationController
 
   def render_export(export)
     json = export.as_json(:only => [:id, :progress, :workflow_state],:methods => [:error_message])
-    json['content_export']['download_url'] = verified_file_download_url(export.attachment, export) if export.attachment
+    json['content_export']['download_url'] = verified_file_download_url(export.attachment, export) if export.attachment && !export.expired?
     render :json => json
   end
 end
