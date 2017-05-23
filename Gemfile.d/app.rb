7a83ab65ecb8576fb1e980808761ebf1e24fcfdf
Cody Cutrer <cody@instructure.com>
2017-05-23 13:27:09 -0600

    bump after_transaction_commit
    fixes CNVS-37008
    
    test plan:
     * in rails 5
     * in production mode, and with a jobs shard that's not on the same
       shard as your account
     * enable open registration on the account
     * create a new user by enrolling them via e-mail address in the
       course
     * run jobs
     * look at messages on the user; it should have sent them an
       invitation
    
    Change-Id: I1f5231c8fcc480e37377bedd41dd71d08e7b11ac
    Reviewed-on: https://gerrit.instructure.com/112831
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -47,7 +47,7 @@ gem 'folio-pagination', '0.0.12', require: 'folio/rails'
   gem 'will_paginate', '3.1.5', require: false
 
 gem 'addressable', '2.5.0', require: false
-gem "after_transaction_commit", '1.1.1'
+gem "after_transaction_commit", '1.1.2'
 gem "aws-sdk", '2.6.7', require: false
 gem 'barby', '0.6.5', require: false
   gem 'rqrcode', '0.10.1', require: false
