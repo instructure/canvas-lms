0d995c6b6865154b4760ae7810175fbbd943b913
Ryan Shaw <ryan@instructure.com>
2017-05-25 12:25:03 -0600

    amdtoes6 app/jsx/grading/GradeSummary.js
    Refs: CNVS-35466
    
    Test plan:
    Grade summary page should still work with no js errors
    
    Change-Id: I647e95cde058a5832ca87cc30be4a17c0b5fae2f
    Reviewed-on: https://gerrit.instructure.com/113227
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/gradebook/EffectiveDueDates.js
+++ app/jsx/gradebook/EffectiveDueDates.js
@@ -18,12 +18,12 @@
 
 import _ from 'underscore'
 
-  export function scopeToUser (dueDateData, userId) {
-    const scopedData = {};
-    _.forEach(dueDateData, (dueDateDataByUserId, assignmentId) => {
-      if (dueDateDataByUserId[userId]) {
-        scopedData[assignmentId] = dueDateDataByUserId[userId];
-      }
-    });
-    return scopedData;
-  }
+export function scopeToUser (dueDateData, userId) {
+  const scopedData = {};
+  _.forEach(dueDateData, (dueDateDataByUserId, assignmentId) => {
+    if (dueDateDataByUserId[userId]) {
+      scopedData[assignmentId] = dueDateDataByUserId[userId];
+    }
+  });
+  return scopedData;
+}
