b308b24a29061173052a680a4f4cf6ef1b7b635e
Ryan Shaw <ryan@instructure.com>
2017-06-07 12:58:36 -0600

    Convert some more files from .coffee to .js
    closes: CNVS-37358
    
    most of this conversion was automated by 2 tools,
    the `amd-to-es6` and `bulk-decaffeinate` npm packages.
    but see the test plan for the manual changes that were 
    required.
    
    Test plan, aka things to check for in code review:
    
    * look for errors in the conversion of the AMD
      `define` block and the es6 `import` statements.
      * the `amd-to-es6` tool choked on anything that
        did destructuring so those had to be done manually,
        eg: make sure `defile([‘underscore’], function({reduce}) {…`
        gets converted to `import {reduce} from ‘underscore’
        (and not the erroneous `import ‘underscore’)
    
    * I had to manually convert the cs `for foo, bar in baz` loops
      because decaffeinate would try to convert them to `for … of` 
      iterator loops which we don’t support.
      * Keep a close eye on those and where I converted things to 
        .map .forEach or Object.values(…).forEach
    
    * I manually removed some `return` statements, especially where
      Coffee script was building up an array (like when a method ended in 
      a `for foo in bar` loop, which it would return an array with the
      built up values even though it wasn’t being used. If in doubt, I tried
      not to remove any `return`s I wasn’t absolutely sure we didn’t need
      but keep an eye out for those
    
    Change-Id: I840691698f1cdc5542737600678c94a7bdc14b35
    Reviewed-on: https://gerrit.instructure.com/114594
    Tested-by: Jenkins
    Reviewed-by: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
    Product-Review: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
    QA-Review: Felix Milea-Ciobanu <fmileaciobanu@instructure.com>

---
+++ app/coffeescripts/SubmissionDetailsDialog.js
@@ -0,0 +1,138 @@
+import $ from 'jquery'
+import submissionDetailsDialog from 'jst/SubmissionDetailsDialog'
+import I18n from 'i18n!submission_details_dialog'
+import numberHelper from 'jsx/shared/helpers/numberHelper'
+import GradeFormatHelper from 'jsx/gradebook/shared/helpers/GradeFormatHelper'
+import GradebookHelpers from 'compiled/gradebook/GradebookHelpers'
+import {extractDataForTurnitin} from 'compiled/gradebook/Turnitin'
+import OutlierScoreHelper from 'jsx/grading/helpers/OutlierScoreHelper'
+import 'jst/_submission_detail' // a partial needed by the SubmissionDetailsDialog template
+import 'jst/_turnitinScore' // a partial needed by the submission_detail partial
+import 'jquery.ajaxJSON'
+import 'jquery.disableWhileLoading'
+import 'jquery.instructure_forms'
+import 'jqueryui/dialog'
+import 'jquery.instructure_misc_plugins'
+import 'vendor/jquery.scrollTo'
+import 'vendor/jquery.ba-tinypubsub'
+
+
+export default class SubmissionDetailsDialog {
+  constructor (assignment, student, options) {
+    this.assignment = assignment
+    this.student = student
+    this.options = options
+    const speedGraderUrl = this.options.speed_grader_enabled
+      ? `${this.options.context_url}/gradebook/speed_grader?assignment_id=${this.assignment.id}#%7B%22student_id%22%3A${this.student.id}%7D`
+      : null
+
+    this.url = this.options.change_grade_url.replace(':assignment', this.assignment.id).replace(':submission', this.student.id)
+    const submission = this.student[`assignment_${this.assignment.id}`]
+    this.submission = $.extend({}, submission, {
+      label: `student_grading_${this.assignment.id}`,
+      inputName: 'submission[posted_grade]',
+      assignment: this.assignment,
+      speedGraderUrl,
+      loading: true,
+      showPointsPossible:
+        (this.assignment.points_possible || this.assignment.points_possible === '0') &&
+        this.assignment.grading_type !== 'gpa_scale',
+      formattedPointsPossible: I18n.n(this.assignment.points_possible),
+      shouldShowExcusedOption: true,
+      isInPastGradingPeriodAndNotAdmin: submission.gradeLocked
+    })
+    this.submission[`assignment_grading_type_is_${this.assignment.grading_type}`] = true
+    if (this.submission.excused) this.submission.grade = 'EX'
+    this.$el = $('<div class="use-css-transitions-for-show-hide" style="padding:0;"/>')
+    this.$el.html(submissionDetailsDialog(this.submission))
+
+    this.dialog = this.$el.dialog({
+      title: this.student.name,
+      width: 600,
+      resizable: false
+    })
+
+    this.dialog.on('dialogclose', this.options.onClose)
+    this.dialog
+      .delegate('select', 'change', event => this.dialog.find('.submission_detail').each(function (index) {
+        $(this).showIf(index === event.currentTarget.selectedIndex)
+      }))
+      .delegate('.submission_details_grade_form', 'submit', (event) => {
+        event.preventDefault()
+        let formData = $(event.currentTarget).getFormData()
+        const rawGrade = formData['submission[posted_grade]']
+        if (rawGrade.toUpperCase() === 'EX') {
+          formData = {'submission[excuse]': true}
+        } else {
+          formData['submission[posted_grade]'] = GradeFormatHelper.delocalizeGrade(rawGrade)
+        }
+        $(event.currentTarget.form).disableWhileLoading(
+          $.ajaxJSON(this.url, 'PUT', formData, (data) => {
+            this.update(data)
+            if (!data.excused) {
+              const outlierScoreHelper = new OutlierScoreHelper(this.submission.score, this.submission.assignment.points_possible)
+              if (outlierScoreHelper.hasWarning()) {
+                $.flashWarning(outlierScoreHelper.warningMessage())
+              }
+            }
+            $.publish('submissions_updated', [this.submission.all_submissions])
+            setTimeout(() => this.dialog.dialog('close'), 500)
+          })
+        )
+      })
+      .delegate('.submission_details_add_comment_form', 'submit', (event) => {
+        event.preventDefault()
+        $(event.currentTarget).disableWhileLoading(
+          $.ajaxJSON(this.url, 'PUT', $(event.currentTarget).getFormData(), (data) => {
+            this.update(data)
+            setTimeout(() => this.dialog.dialog('close'), 500)
+          })
+        )
+      })
+
+    const url = `${this.url}&include[]=submission_history&include[]=submission_comments&include[]=rubric_assessment`
+    const deferred = $.ajaxJSON(url, 'GET', {}, this.update)
+    this.dialog.find('.submission_details_comments').disableWhileLoading(deferred)
+  }
+
+  open = () => {
+    this.dialog.dialog('open')
+    this.scrollCommentsToBottom()
+    $('.ui-dialog-titlebar-close').focus()
+  }
+
+  scrollCommentsToBottom = () =>
+    this.dialog.find('.submission_details_comments').scrollTop(999999)
+
+
+  update = (newData) => {
+    $.extend(this.submission, newData)
+    this.submission.moreThanOneSubmission = this.submission.submission_history.length > 1
+    this.submission.loading = false
+    this.submission.submission_history.forEach((submission) => {
+      submission.submission_comments && submission.submission_comments.forEach((comment) => {
+        comment.url = `${this.options.context_url}/users/${comment.author_id}`
+        const urlPrefix = `${location.protocol}//${location.host}`
+        comment.image_url = `${urlPrefix}/images/users/${comment.author_id}`
+      })
+      submission.turnitin = extractDataForTurnitin(submission, `submission_${submission.id}`, this.options.context_url)
+      submission.attachments && submission.attachments.forEach((attachment) => {
+        attachment.turnitin = extractDataForTurnitin(submission, `attachment_${attachment.id}`, this.options.context_url)
+      })
+    })
+
+
+    if (this.submission.excused) {
+      this.submission.grade = 'EX'
+    } else if (['points', 'percent'].includes(this.assignment.grading_type)) {
+      this.submission.grade = GradeFormatHelper.formatGrade(this.submission.grade)
+    }
+    this.dialog.html(submissionDetailsDialog(this.submission))
+    this.dialog.find('select').trigger('change')
+    return this.scrollCommentsToBottom()
+  }
+
+  static open (assignment, student, options) {
+    return new SubmissionDetailsDialog(assignment, student, options, ENV).open()
+  }
+}
