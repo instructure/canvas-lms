7e57452799d48a8810dd79e8e41099821da4dbda
Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
2017-06-10 00:25:13 +0530

    spec: Add specs for sync history modal
    Test Plan:
      Passes Jenkins
    
    Change-Id: I5a6c42c29e98191a17b311242c3772b941b81954
    Reviewed-on: https://gerrit.instructure.com/114926
    Tested-by: Jenkins
    Reviewed-by: Heath Hales <hhales@instructure.com>
    Reviewed-by: David Tan <dtan@instructure.com>
    Product-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>

--- spec/selenium/helpers/blueprint_common.rb
+++ spec/selenium/helpers/blueprint_common.rb
@@ -24,4 +24,96 @@ module BlueprintCourseCommon
         template.update(default_restrictions: master_blueprint_settings)
         course.reload
     end
+
+    def run_master_course_migration(master)
+      template = master.master_course_templates.first
+      master_teacher = master.teachers.first
+      @migration = MasterCourses::MasterMigration.start_new_migration!(template, master_teacher)
+      run_jobs
+      @migration.reload
+    end
+
+    def create_and_migrate_master_assignments(master)
+      template = master.master_course_templates.first
+      @assignment1 = master.assignments.create!(title: "Assignment 1", grading_type: "points", points_possible: 10.0)
+      template.create_content_tag_for!(@assignment1)
+      @assignment2 = master.assignments.create!(title: "Assignment 2", grading_type: "points", points_possible: 10.0)
+      tag1 = template.create_content_tag_for!(@assignment2)
+      tag1.update(restrictions: {points: true})
+      run_master_course_migration(master)
+    end
+
+    def update_child_assignment(minion)
+      child_assignment = minion.assignments.where(title: @assignment1.title).first
+      child_assignment.update(points_possible: 15.0)
+    end
+
+    def update_master_assignment_and_migrate(master)
+      @assignment1.update(points_possible: 8.0)
+      @assignment2.update(points_possible: 8.0)
+      run_master_course_migration(master)
+    end
+
+    def create_and_migrate_master_discussions(master)
+      template = master.master_course_templates.first
+      @discussion1 = master.discussion_topics.create!(title: "Discussion 1", delayed_post_at: Time.zone.now+2.days)
+      template.create_content_tag_for!(@discussion1)
+      @discussion2 = master.discussion_topics.create!(title: "Discussion 2", delayed_post_at: Time.zone.now+3.days)
+      tag1 = template.create_content_tag_for!(@discussion2)
+      tag1.update(restrictions: {availability_dates: true})
+      run_master_course_migration(master)
+    end
+
+    def update_child_discussion(minion)
+      child_discussion = minion.discussion_topics.where(title: @discussion1.title).first
+      child_discussion.update(delayed_post_at: Time.zone.now+3.days)
+    end
+
+    def update_master_discussion_and_migrate(master)
+      @discussion1.update(delayed_post_at: Time.zone.now+1.day)
+      @discussion2.update(delayed_post_at: Time.zone.now+1.day)
+      run_master_course_migration(master)
+    end
+
+    def create_and_migrate_master_pages(master)
+      template = master.master_course_templates.first
+      @page1 = master.wiki.wiki_pages.create!(title: "Page 1", body: "Wiki Page 1")
+      template.create_content_tag_for!(@page1)
+      @page2 = master.wiki.wiki_pages.create!(title: "Page 2", body: "Wiki Page 2")
+      tag1 = template.create_content_tag_for!(@page2)
+      tag1.update(restrictions: {content: true})
+      run_master_course_migration(master)
+    end
+
+    def update_child_page(minion)
+      child_page = minion.wiki.wiki_pages.where(title: @page1.title).first
+      child_page.update(body: "Child Wiki Page updated")
+    end
+
+    def update_master_page_and_migrate(master)
+      @page1.update(body: "Wiki Page 1 updated")
+      @page2.update(body: "Wiki Page 2 updated")
+      run_master_course_migration(master)
+    end
+
+    def create_and_migrate_master_quizzes(master)
+      template = master.master_course_templates.first
+      @quiz1 = master.quizzes.create!(title: "Quiz 1", due_at: Time.zone.now+2.days)
+      template.create_content_tag_for!(@quiz1)
+      @quiz2 = master.quizzes.create!(title: "Quiz 2", due_at: Time.zone.now+2.days)
+      tag1 = template.create_content_tag_for!(@quiz2)
+      tag1.update(restrictions: {due_dates: true})
+      run_master_course_migration(master)
+    end
+
+    def update_child_quiz(minion)
+      child_quiz = minion.quizzes.where(title: @quiz1.title).first
+      child_quiz.update(due_at: Time.zone.now + 1.day)
+    end
+
+    def update_master_quiz_and_migrate(master)
+      @quiz1.update(due_at: Time.zone.now + 3.days)
+      @quiz2.update(due_at: Time.zone.now + 3.days)
+      run_master_course_migration(master)
+    end
 end
