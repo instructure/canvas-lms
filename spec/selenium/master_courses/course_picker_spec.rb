070f6435313bc56045807246a159971551ce22e6
Robert Lamb <rlamb@instructure.com>
2017-06-22 15:58:23 -0600

    Change test_id numbers for course picker specs
    Change-Id: Icf05f59c300d726d154c578124e7c0746ab072f3
    Reviewed-on: https://gerrit.instructure.com/116670
    Tested-by: Jenkins
    Reviewed-by: David Mirabile <dmirabile-c@instructure.com>
    Product-Review: Robert Lamb <rlamb@instructure.com>
    QA-Review: Robert Lamb <rlamb@instructure.com>

--- spec/selenium/master_courses/course_picker_spec.rb
+++ spec/selenium/master_courses/course_picker_spec.rb
@@ -76,7 +76,7 @@ describe "master courses - course picker" do
     available_courses
   end
 
-  it "should show all courses by default", priority: "1", test_id: "3072438" do
+  it "should show all courses by default", priority: "1", test_id: "3077485" do
     get "/courses/#{@master.id}"
     open_associations
     open_courses_list
@@ -84,17 +84,17 @@ describe "master courses - course picker" do
     expect(available_courses().length).to eq(5)
   end
 
-  it "should filter the course list by name", priority: "1", test_id: "3072438" do
+  it "should filter the course list by name", priority: "1", test_id: "3265699" do
     matches = test_filter('Alpha')
     expect(matches.length).to eq(3)
   end
 
-  it "should filter the course list by short name", priority: "1", test_id: "3072438" do
+  it "should filter the course list by short name", priority: "1", test_id: "3265700" do
     matches = test_filter('CCC')
     expect(matches.length).to eq(3)
   end
 
-  it "should filter the course list by SIS ID", priority: "1", test_id: "3072438" do
+  it "should filter the course list by SIS ID", priority: "1", test_id: "3265701" do
     matches = test_filter('SIS_B')
     expect(matches.length).to eq(2)
   end
