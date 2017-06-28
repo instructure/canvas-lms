b94fd7a7b34b9bc99ee8559c2411aa0183834436
David Mirabile <dmirabile@instructure.com>
2017-06-28 14:35:27 -0600

    spec: stage 3 of blueprint courses refactor
    update: All hail Gergich
    update: Please the linter
    
    Change-Id: I03b8e2dc8ae2a92de7f630c237a04ab7ca52e497
    Reviewed-on: https://gerrit.instructure.com/117355
    Tested-by: Jenkins
    Reviewed-by: Robert Lamb <rlamb@instructure.com>
    Product-Review: Robert Lamb <rlamb@instructure.com>
    QA-Review: Robert Lamb <rlamb@instructure.com>

--- spec/selenium/master_courses/child_course_assignments_spec.rb
+++ spec/selenium/master_courses/child_course_assignments_spec.rb
@@ -20,103 +20,133 @@ require_relative '../common'
 describe "master courses - child courses - assignment locking" do
   include_context "in-process server selenium tests"
 
-  before :once do
-    Account.default.enable_feature!(:master_courses)
+  context "in the child course" do
+    before :once do
+      Account.default.enable_feature!(:master_courses)
 
-    due_date = format_date_for_view(Time.zone.now - 1.month)
-    @copy_from = course_factory(:active_all => true)
-    @template = MasterCourses::MasterTemplate.set_as_master_course(@copy_from)
-    @original_assmt = @copy_from.assignments.create!(
-      :title => "blah", :description => "bloo", :points_possible => 27, :due_at => due_date
-    )
-    @tag = @template.create_content_tag_for!(@original_assmt)
+      due_date = format_date_for_view(Time.zone.now - 1.month)
+      @copy_from = course_factory(:active_all => true)
+      @template = MasterCourses::MasterTemplate.set_as_master_course(@copy_from)
+      @original_assmt = @copy_from.assignments.create!(
+        :title => "blah", :description => "bloo", :points_possible => 27, :due_at => due_date
+      )
+      @tag = @template.create_content_tag_for!(@original_assmt)
 
-    course_with_teacher(:active_all => true)
-    @copy_to = @course
-    @template.add_child_course!(@copy_to)
-    # just create a copy directly instead of doing a real migration
-    @assmt_copy = @copy_to.assignments.new(
-      :title => "blah", :description => "bloo", :points_possible => 27, :due_at => due_date
-    )
-    @assmt_copy.migration_id = @tag.migration_id
-    @assmt_copy.save!
+      course_with_teacher(:active_all => true)
+      @copy_to = @course
+      @template.add_child_course!(@copy_to)
+      # just create a copy directly instead of doing a real migration
+      @assmt_copy = @copy_to.assignments.new(
+        :title => "blah", :description => "bloo", :points_possible => 27, :due_at => due_date
+      )
+      @assmt_copy.migration_id = @tag.migration_id
+      @assmt_copy.save!
+    end
+
+    before :each do
+      user_session(@teacher)
+    end
+
+    it "should contain the delete cog-menu option on the index when unlocked" do
+      get "/courses/#{@copy_to.id}/assignments"
+
+      expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint')
+
+      f('.al-trigger').click
+      expect(f('.assignment')).to contain_css('a.delete_assignment')
+    end
+
+    it "should not contain the delete cog-menu option on the index when locked" do
+      @tag.update(restrictions: {content: true})
+
+      get "/courses/#{@copy_to.id}/assignments"
+
+      expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint-lock')
+
+      f('.al-trigger').click
+      expect(f('.assignment')).not_to contain_css('a.delete_assignment')
+    end
+
+    it "should show the delete cog-menu option on the index when not locked" do
+      get "/courses/#{@copy_to.id}/assignments"
+
+      expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint')
+
+      f('.al-trigger').click
+      expect(f('.assignment')).not_to contain_css('a.delete_assignment.disabled')
+      expect(f('.assignment')).to contain_css('a.delete_assignment')
+    end
+
+    it "should not allow the delete options on the edit page when locked" do
+      @tag.update(restrictions: {content: true})
+
+      get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
+
+      # when locked, the whole menu is removed
+      expect(f('#edit_assignment_header')).not_to contain_css('.al-trigger')
+    end
+
+    it "should show the delete cog-menu options on the edit when not locked" do
+      get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
+
+      f('.al-trigger').click
+      expect(f('#edit_assignment_header')).not_to contain_css('a.delete_assignment_link.disabled')
+      expect(f('#edit_assignment_header')).to contain_css('a.delete_assignment_link')
+    end
+
+    it "should not allow editing of restricted items" do
+      # restrict everything
+      @tag.update(restrictions: {content: true, points: true, due_dates: true, availability_dates: true})
+
+      get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
+
+      expect(f("#assignment_name").tag_name).to eq 'h1'
+      expect(f("#assignment_description").tag_name).to eq 'div'
+      expect(f("#assignment_points_possible").attribute("readonly")).to eq "true"
+      expect(f("#due_at").attribute("readonly")).to eq "true"
+      expect(f("#unlock_at").attribute("readonly")).to eq "true"
+      expect(f("#lock_at").attribute("readonly")).to eq "true"
+    end
+
+    it "should not allow popup editing of restricted items" do
+      # restrict everything
+      @tag.update(restrictions: {content: true, points: true, due_dates: true, availability_dates: true})
+
+      get "/courses/#{@copy_to.id}/assignments"
+
+      hover_and_click(".edit_assignment")
+      expect(f('.ui-dialog-titlebar .ui-dialog-title').text).to eq "Edit Assignment"
+      expect(f("#assign_#{@assmt_copy.id}_assignment_name").tag_name).to eq "h3"
+      expect(f("#assign_#{@assmt_copy.id}_assignment_due_at").attribute("readonly")).to eq "true"
+      expect(f("#assign_#{@assmt_copy.id}_assignment_points").attribute("readonly")).to eq "true"
+    end
   end
 
-  before :each do
-    user_session(@teacher)
-  end
+  context "in the master course" do
+    before :once do
+      Account.default.enable_feature!(:master_courses)
 
-  it "should contain the delete cog-menu option on the index when unlocked" do
-    get "/courses/#{@copy_to.id}/assignments"
+      @course = course_factory(active_all: true)
+      @template = MasterCourses::MasterTemplate.set_as_master_course(@course)
+      @assignment = @course.assignments.create!(title: "blah", description: "bloo")
+      @tag = @template.create_content_tag_for!(@assignment)
+    end
 
-    expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint')
+    before :each do
+      user_session(@teacher)
+    end
 
-    f('.al-trigger').click
-    expect(f('.assignment')).to contain_css('a.delete_assignment')
-  end
+    it "should show unlocked button on index page for unlocked assignment" do
+      get "/courses/#{@course.id}/assignments"
+      expect(f('[data-view="lock-icon"] i.icon-blueprint')).to be_displayed
+    end
 
-  it "should not contain the delete cog-menu option on the index when locked" do
-    @tag.update_attribute(:restrictions, {:content => true})
+    it "should show locked button on index page for locked assignment" do
+      # restrict something
+      @tag.update(restrictions: {content: true})
+      get "/courses/#{@course.id}/assignments"
+      expect(f('[data-view="lock-icon"] i.icon-blueprint-lock')).to be_displayed
+    end
 
-    get "/courses/#{@copy_to.id}/assignments"
-
-    expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint-lock')
-
-    f('.al-trigger').click
-    expect(f('.assignment')).not_to contain_css('a.delete_assignment')
-  end
-
-  it "should show the delete cog-menu option on the index when not locked" do
-    get "/courses/#{@copy_to.id}/assignments"
-
-    expect(f("#assignment_#{@assmt_copy.id}")).to contain_css('.icon-blueprint')
-
-    f('.al-trigger').click
-    expect(f('.assignment')).not_to contain_css('a.delete_assignment.disabled')
-    expect(f('.assignment')).to contain_css('a.delete_assignment')
-  end
-
-  it "should not allow the delete options on the edit page when locked" do
-    @tag.update_attribute(:restrictions, {:content => true})
-
-    get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
-
-    # when locked, the whole menu is removed
-    expect(f('#edit_assignment_header')).not_to contain_css('.al-trigger')
-  end
-
-  it "should show the delete cog-menu options on the edit when not locked" do
-    get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
-
-    f('.al-trigger').click
-    expect(f('#edit_assignment_header')).not_to contain_css('a.delete_assignment_link.disabled')
-    expect(f('#edit_assignment_header')).to contain_css('a.delete_assignment_link')
-  end
-
-  it "should not allow editing of restricted items" do
-    # restrict everything
-    @tag.update_attribute(:restrictions, {:content => true, :points => true, :due_dates => true, :availability_dates => true})
-
-    get "/courses/#{@copy_to.id}/assignments/#{@assmt_copy.id}/edit"
-
-    expect(f("#assignment_name").tag_name).to eq 'h1'
-    expect(f("#assignment_description").tag_name).to eq 'div'
-    expect(f("#assignment_points_possible").attribute("readonly")).to eq "true"
-    expect(f("#due_at").attribute("readonly")).to eq "true"
-    expect(f("#unlock_at").attribute("readonly")).to eq "true"
-    expect(f("#lock_at").attribute("readonly")).to eq "true"
-  end
-
-  it "should not allow popup editing of restricted items" do
-    # restrict everything
-    @tag.update_attribute(:restrictions, {:content => true, :points => true, :due_dates => true, :availability_dates => true})
-
-    get "/courses/#{@copy_to.id}/assignments"
-
-    hover_and_click(".edit_assignment")
-    expect(f('.ui-dialog-titlebar .ui-dialog-title').text).to eq "Edit Assignment"
-    expect(f("#assign_#{@assmt_copy.id}_assignment_name").tag_name).to eq "h3"
-    expect(f("#assign_#{@assmt_copy.id}_assignment_due_at").attribute("readonly")).to eq "true"
-    expect(f("#assign_#{@assmt_copy.id}_assignment_points").attribute("readonly")).to eq "true"
   end
 end
--- spec/selenium/master_courses/child_course_modules_spec.rb
+++ spec/selenium/master_courses/child_course_modules_spec.rb
@@ -21,84 +21,122 @@ describe "master courses - child courses - module item locking" do
   include_context "in-process server selenium tests"
   include ContextModulesCommon
 
-  before :once do
-    Account.default.enable_feature!(:master_courses)
+  context "in the child course" do
+    before :once do
+      Account.default.enable_feature!(:master_courses)
 
-    @copy_from = course_factory(:active_all => true)
-    @template = MasterCourses::MasterTemplate.set_as_master_course(@copy_from)
-    @original_page = @copy_from.wiki.wiki_pages.create!(:title => "blah", :body => "bloo")
-    @page_mc_tag = @template.create_content_tag_for!(@original_page, :restrictions => {:content => true})
+      @copy_from = course_factory(active_all: true)
+      @template = MasterCourses::MasterTemplate.set_as_master_course(@copy_from)
+      @original_page = @copy_from.wiki.wiki_pages.create!(title: "blah", body: "bloo")
+      @page_mc_tag = @template.create_content_tag_for!(@original_page, restrictions: {content: true})
 
-    @original_topic = @copy_from.discussion_topics.create!(:title => "blah", :message => "bloo")
-    @topic_mc_tag = @template.create_content_tag_for!(@original_topic)
+      @original_topic = @copy_from.discussion_topics.create!(title: "blah", message: "bloo")
+      @topic_mc_tag = @template.create_content_tag_for!(@original_topic)
 
-    course_with_teacher(:active_all => true)
-    @copy_to = @course
-    @sub = @template.add_child_course!(@copy_to)
+      course_with_teacher(active_all: true)
+      @copy_to = @course
+      @sub = @template.add_child_course!(@copy_to)
 
-    @page_copy = @copy_to.wiki.wiki_pages.create!(:title => "locked page", :migration_id => @page_mc_tag.migration_id)
-    @topic_copy = @copy_to.discussion_topics.create!(:title => "unlocked topic", :migration_id => @topic_mc_tag.migration_id)
-    [@page_copy, @topic_copy].each{|obj| @sub.create_content_tag_for!(obj)}
-    @assmt = @copy_to.assignments.create!(:title => "normal assignment")
+      @page_copy = @copy_to.wiki.wiki_pages.create!(title: "locked page", migration_id: @page_mc_tag.migration_id)
+      @topic_copy = @copy_to.discussion_topics.create!(title: "unlocked topic", migration_id: @topic_mc_tag.migration_id)
+      [@page_copy, @topic_copy].each{|obj| @sub.create_content_tag_for!(obj)}
+      @assmt = @copy_to.assignments.create!(title: "normal assignment")
 
-    @mod = @copy_to.context_modules.create!(:name => "modle")
-    @locked_tag = @mod.add_item(:id => @page_copy.id, :type => "wiki_page")
-    @unlocked_tag = @mod.add_item(:id => @topic_copy.id, :type => "discussion_topic")
-    @normal_tag = @mod.add_item(:id => @assmt.id, :type => "assignment")
+      @mod = @copy_to.context_modules.create!(name: "modle")
+      @locked_tag = @mod.add_item(id: @page_copy.id, type: "wiki_page")
+      @unlocked_tag = @mod.add_item(id: @topic_copy.id, type: "discussion_topic")
+      @normal_tag = @mod.add_item(id: @assmt.id, type: "assignment")
+    end
+
+    before :each do
+      user_session(@teacher)
+    end
+
+    it "should show all the icons on the modules index" do
+      get "/courses/#{@copy_to.id}/modules"
+
+      # objects inherited from master show the lock
+      expect(f("#context_module_item_#{@locked_tag.id} .lock-icon")).to contain_css('.icon-blueprint-lock')
+      expect(f("#context_module_item_#{@unlocked_tag.id} .lock-icon")).to contain_css('.icon-blueprint')
+      # objects aded to the child do not
+      expect(f("#context_module_item_#{@normal_tag.id} .lock-icon")).not_to contain_css('.icon-blueprint-lock')
+      expect(f("#context_module_item_#{@normal_tag.id} .lock-icon")).not_to contain_css('.icon-blueprint')
+    end
+
+    it "should disable the title edit input for locked items" do
+      get "/courses/#{@copy_to.id}/modules"
+
+      f("#context_module_item_#{@locked_tag.id} .al-trigger").click
+      f("#context_module_item_#{@locked_tag.id} .al-options .edit_link").click
+      expect(f("#content_tag_title")).to be_disabled
+    end
+
+    it "should not disable the title edit input for unlocked items" do
+      get "/courses/#{@copy_to.id}/modules"
+
+      f("#context_module_item_#{@unlocked_tag.id} .al-trigger").click
+      f("#context_module_item_#{@unlocked_tag.id} .al-options .edit_link").click
+      expect(f("#content_tag_title")).not_to be_disabled
+    end
+
+    it "loads new restriction info as needed when adding an item" do
+      title = "new quiz"
+      original_quiz = @copy_from.quizzes.create!(title: title)
+      quiz_mc_tag = @template.create_content_tag_for!(original_quiz, restrictions: {content: true})
+
+      quiz_copy = @copy_to.quizzes.create!(title: title, migration_id: quiz_mc_tag.migration_id)
+      @sub.create_content_tag_for!(quiz_copy)
+
+      get "/courses/#{@copy_to.id}/modules"
+
+      f("#context_module_#{@mod.id} .add_module_item_link").click
+      wait_for_ajaximations
+      click_option('#add_module_item_select', "Quiz")
+      click_option('#quizs_select .module_item_select', title)
+      f('.add_item_button.ui-button').click
+
+      wait_for_ajaximations
+      new_tag = ContentTag.last
+      expect(new_tag.content).to eq quiz_copy
+
+      # does another fetch to get restrictions for the new tag
+      expect(f("#context_module_item_#{new_tag.id} .lock-icon")).to contain_css('.icon-blueprint-lock')
+    end
   end
 
-  before :each do
-    user_session(@teacher)
-  end
+  context "in the master course" do
+    before :once do
+      Account.default.enable_feature!(:master_courses)
 
-  it "should show all the icons on the modules index" do
-    get "/courses/#{@copy_to.id}/modules"
+      @course = course_factory(active_all: true)
+      @template = MasterCourses::MasterTemplate.set_as_master_course(@course)
 
-    # objects inherited from master show the lock
-    expect(f("#context_module_item_#{@locked_tag.id} .lock-icon")).to contain_css('.icon-blueprint-lock')
-    expect(f("#context_module_item_#{@unlocked_tag.id} .lock-icon")).to contain_css('.icon-blueprint')
-    # objects aded to the child do not
-    expect(f("#context_module_item_#{@normal_tag.id} .lock-icon")).not_to contain_css('.icon-blueprint-lock')
-    expect(f("#context_module_item_#{@normal_tag.id} .lock-icon")).not_to contain_css('.icon-blueprint')
-  end
+      @assmt = @course.assignments.create!(title: "assmt blah", description: "bloo")
+      @assmt_tag = @template.create_content_tag_for!(@assmt)
 
-  it "should disable the title edit input for locked items" do
-    get "/courses/#{@copy_to.id}/modules"
+      @page = @course.wiki.wiki_pages.create!(title: "page blah", body: "bloo")
+      @page_tag = @template.create_content_tag_for!(@page, restrictions: {all: true})
 
-    f("#context_module_item_#{@locked_tag.id} .al-trigger").click
-    f("#context_module_item_#{@locked_tag.id} .al-options .edit_link").click
-    expect(f("#content_tag_title")).to be_disabled
-  end
+      @topic = @course.discussion_topics.create!(title: "topic blah", message: "bloo")
+      # note the lack of a content tag
 
-  it "should not disable the title edit input for unlocked items" do
-    get "/courses/#{@copy_to.id}/modules"
+      @mod = @course.context_modules.create!(name: "modle")
+      @assmt_mod_tag = @mod.add_item(id: @assmt.id, type: "assignment")
+      @page_mod_tag  = @mod.add_item(id: @page.id, type: "wiki_page")
+      @topic_mod_tag = @mod.add_item(id: @topic.id, type: "discussion_topic")
+    end
 
-    f("#context_module_item_#{@unlocked_tag.id} .al-trigger").click
-    f("#context_module_item_#{@unlocked_tag.id} .al-options .edit_link").click
-    expect(f("#content_tag_title")).not_to be_disabled
-  end
+    before :each do
+      user_session(@teacher)
+    end
 
-  it "loads new restriction info as needed when adding an item" do
-    title = "new quiz"
-    original_quiz = @copy_from.quizzes.create!(:title => title)
-    quiz_mc_tag = @template.create_content_tag_for!(original_quiz, :restrictions => {:content => true})
+    it "should show all the icons on the modules index" do
+      get "/courses/#{@course.id}/modules"
 
-    quiz_copy = @copy_to.quizzes.create!(:title => title, :migration_id => quiz_mc_tag.migration_id)
-    @sub.create_content_tag_for!(quiz_copy)
-
-    get "/courses/#{@copy_to.id}/modules"
-
-    f("#context_module_#{@mod.id} .add_module_item_link").click
-    wait_for_ajaximations
-    click_option('#add_module_item_select', "Quiz")
-    click_option('#quizs_select .module_item_select', title)
-    fj('.add_item_button.ui-button').click
-
-    wait_for_ajaximations
-    new_tag = ContentTag.last
-    expect(new_tag.content).to eq quiz_copy
-
-    # does another fetch to get restrictions for the new tag
-    expect(f("#context_module_item_#{new_tag.id} .lock-icon")).to contain_css('.icon-blueprint-lock')
+      expect(f("#context_module_item_#{@assmt_mod_tag.id} .lock-icon")).to contain_css('.icon-blueprint')
+      expect(f("#context_module_item_#{@page_mod_tag.id} .lock-icon")).to contain_css('.icon-blueprint-lock')
+      # should still have icon even without tag
+      expect(f("#context_module_item_#{@topic_mod_tag.id} .lock-icon")).to contain_css('.icon-blueprint')
+    end
   end
 end
