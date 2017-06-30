277178f7d703e008798dc534ff5bf607f2e60c8b
Robert Lamb <rlamb@instructure.com>
2017-06-30 10:38:33 -0600

    spec: add associated course filter by term spec - 3075534
    refactor term filter finder
    refactor gergich suggestions
    
    Change-Id: I84b7337509c28b8d3202ba18fb502b5db0b7cd52
    Reviewed-on: https://gerrit.instructure.com/117648
    Tested-by: Jenkins
    Reviewed-by: David Tan <dtan@instructure.com>
    Product-Review: Robert Lamb <rlamb@instructure.com>
    QA-Review: Robert Lamb <rlamb@instructure.com>

--- spec/selenium/master_courses/course_picker_spec.rb
+++ spec/selenium/master_courses/course_picker_spec.rb
@@ -28,25 +28,33 @@ describe "master courses - course picker" do
     @master = course_factory(active_all: true)
     @template = MasterCourses::MasterTemplate.set_as_master_course(@master)
 
+    term1 = Account.default.enrollment_terms.create!(name: "fall term")
+    term2 = Account.default.enrollment_terms.create!(name: "spring term")
+
     # create some courses
     c = Course.create!(
-      :account => @account, :name => "AlphaDog", :course_code => "CCC1", :sis_source_id => "SIS_A1"
+      :account => @account, :name => "AlphaDog", :course_code => "CCC1", :sis_source_id => "SIS_A1",
+      :enrollment_term_id => term1.id
     )
     c.offer!
     c = Course.create!(
-      :account => @account, :name => "AlphaMale", :course_code => "CCC2", :sis_source_id => "SIS_A2"
+      :account => @account, :name => "AlphaMale", :course_code => "CCC2", :sis_source_id => "SIS_A2",
+      :enrollment_term_id => term1.id
     )
     c.offer!
     c = Course.create!(
-      :account => @account, :name => "Alphabet", :course_code => "CCC3", :sis_source_id => "SIS_A3"
+      :account => @account, :name => "Alphabet", :course_code => "CCC3", :sis_source_id => "SIS_A3",
+      :enrollment_term_id => term1.id
     )
     c.offer!
     c = Course.create!(
-      :account => @account, :name => "BetaCarotine", :course_code => "DDD4", :sis_source_id => "SIS_B4"
+      :account => @account, :name => "BetaCarotine", :course_code => "DDD4", :sis_source_id => "SIS_B4",
+      :enrollment_term_id => term1.id
     )
     c.offer!
     c = Course.create!(
-      :account => @account, :name => "BetaGetOuttaHere", :course_code => "DDD5", :sis_source_id => "SIS_B5"
+      :account => @account, :name => "BetaGetOuttaHere", :course_code => "DDD5", :sis_source_id => "SIS_B5",
+      :enrollment_term_id => term2.id
     )
     c.offer!
     account_admin_user(active_all: true)
@@ -59,6 +67,12 @@ describe "master courses - course picker" do
   let(:course_search_input) {'.bca-course-filter input[type="search"]'}
   let(:filter_output) {'.bca-course-details__wrapper'}
   let(:loading) {'.bca-course-picker__loading'}
+  let(:term_filter) {'.bca-course-filter select:contains("Any Term")'}
+
+  def wait_for_spinner
+    expect(f(filter_output)).to contain_css(loading)      # the loading spinner appears
+    expect(f(filter_output)).not_to contain_css(loading)  # and disappears
+  end
 
   # enter search term into the filter text box and wait for the response
   # this is complicated b the fact that the search api query doesn't happen until
@@ -70,9 +84,8 @@ describe "master courses - course picker" do
     open_courses_list
     filter = f(course_search_input)
     filter.click
-    filter.send_keys(search_term)                         # type into the filter text box
-    expect(f(filter_output)).to contain_css(loading)      # the loading spinner appears
-    expect(f(filter_output)).not_to contain_css(loading)  # and disappears
+    filter.send_keys(search_term) # type into the filter text box
+    wait_for_spinner
     available_courses
   end
 
@@ -97,5 +110,14 @@ describe "master courses - course picker" do
   it "should filter the course list by SIS ID", priority: "1", test_id: "3265701" do
     matches = test_filter('SIS_B')
     expect(matches.length).to eq(2)
+  end
+
+  it "should filter the course list by term", priority: "1", test_id: "3075534" do
+    get "/courses/#{@master.id}"
+    open_associations
+    open_courses_list
+    click_option(term_filter, 'fall term')
+    wait_for_spinner
+    expect(available_courses().length).to eq(4)
   end
 end
