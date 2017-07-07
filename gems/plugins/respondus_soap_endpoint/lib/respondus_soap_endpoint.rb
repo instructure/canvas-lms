d22f9174d784a8a275815e937da2215849d36a52
Bryan Petty <bpetty@instructure.com>
2017-07-07 11:53:55 -0600

    use Windows-compatible filenames in Respondus
    closes gh-1070
    
    Test Plan:
    1. Respondus SOAP endpoint specs pass.
    
    Change-Id: Ibebf03b0a41ea6520a21971b02cb005fcd50afca
    Reviewed-on: https://gerrit.instructure.com/118191
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Bryan Petty <bpetty@instructure.com>

--- gems/plugins/respondus_soap_endpoint/lib/respondus_soap_endpoint.rb
+++ gems/plugins/respondus_soap_endpoint/lib/respondus_soap_endpoint.rb
@@ -20,8 +20,8 @@ require 'soap4r-middleware'
 require 'respondus_soap_endpoint/engine'
 require 'respondus_soap_endpoint/middleware'
 require 'respondus_soap_endpoint/api_port'
-require 'respondus_soap_endpoint/urn:RespondusAPIMappingRegistry.rb'
-require 'respondus_soap_endpoint/urn:RespondusAPIServant.rb'
+require 'respondus_soap_endpoint/urn_RespondusAPIMappingRegistry.rb'
+require 'respondus_soap_endpoint/urn_RespondusAPIServant.rb'
 
 module RespondusSoapEndpoint
 end
