5d96efdc97777cdc8397c09dfea1148397430d56
James Williams <jamesw@instructure.com>
2017-07-10 14:33:08 -0600

    fix course_and_group wiki page scope
    doing a left join like it was meant we were
    loading every wiki page in the entire db
    
    i can't guarantee this the only db destroying issue
    
    refs #FALCOR-455
    
    Change-Id: If0358557cd9a13fc365706bc570a4315193a6ee8
    Reviewed-on: https://gerrit.instructure.com/118376
    Reviewed-by: Dan Minkevitch <dan@instructure.com>
    Tested-by: Jenkins
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -72,10 +72,12 @@ class WikiPage < ActiveRecord::Base
   end
   scope :todo_date_between, -> (starting, ending) { where(todo_date: starting...ending) }
   scope :for_courses_and_groups, -> (course_ids, group_ids) do
-    joins("LEFT JOIN #{Course.quoted_table_name} on wiki_pages.wiki_id = courses.wiki_id
-           LEFT JOIN #{Group.quoted_table_name} on wiki_pages.wiki_id = groups.wiki_id").
-      where("courses.id IN (?) OR groups.id IN (?)", course_ids, group_ids)
+    wiki_ids = []
+    wiki_ids += Course.where(:id => course_ids).pluck(:wiki_id) if course_ids.any?
+    wiki_ids += Group.where(:id => group_ids).pluck(:wiki_id) if group_ids.any?
+    where(:wiki_id => wiki_ids)
   end
+
   scope :visible_to_user, -> (user_id) do
     joins("LEFT JOIN #{AssignmentStudentVisibility.quoted_table_name} as asv on wiki_pages.assignment_id = asv.assignment_id").
       where("wiki_pages.assignment_id IS NULL OR asv.user_id = ?", user_id)
