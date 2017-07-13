227724c770811d806ef9e8ab7862b48b3a0d267d
Jon Jensen <jon@instructure.com>
2017-07-13 14:26:30 -0600

    spec: wait for textarea to appear
    Change-Id: I7b6490f17b86983f4ad5eb3dabe2d2956381cb75
    Reviewed-on: https://gerrit.instructure.com/118942
    Tested-by: Jenkins
    Reviewed-by: Zach Warrell <zwarrell@instructure.com>
    Product-Review: Zach Warrell <zwarrell@instructure.com>
    QA-Review: Zach Warrell <zwarrell@instructure.com>
    Tested-by: Zach Warrell <zwarrell@instructure.com>

--- spec/selenium/discussions/discussions_show_page_any_user_spec.rb
+++ spec/selenium/discussions/discussions_show_page_any_user_spec.rb
@@ -258,10 +258,8 @@ describe "discussions" do
       it "should not show keyboard shortcut modal during html editing", priority: "2", test_id: 846539 do
         get url
         f('.discussion-reply-action').click
-        wait_for_ajaximations
         fln('HTML Editor').click
-        wait_for_ajaximations
-        f('.reply-textarea').send_keys("< , > , ?, /")
+        fj('.reply-textarea:visible').send_keys("< , > , ?, /")
         expect(f('.ui-dialog')).not_to be_displayed
       end
 
