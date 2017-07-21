95795fa65fd7767c0d73793e16bed64e1c525d76
Han Yan <hyan@instructure.com>
2017-07-21 08:58:14 -0500

    Instructor assigns rubric to whole quiz
    closes QUIZ-394
    
    test plan:
    - create a QUIZ2 quiz
    - edit the quiz
    - at the bottom of editing page, a user can append rubric
    - this rubrics editing should not be available for traditional quiz
    
    Change-Id: I28d18a5bae1a91a25f04df3e970b07aaaf109dfc
    Reviewed-on: https://gerrit.instructure.com/120033
    Tested-by: Jenkins
    Reviewed-by: Omar Khan <okhan@instructure.com>
    QA-Review: Omar Khan <okhan@instructure.com>
    Product-Review: Kevin Dougherty <jdougherty@instructure.com>

---
+++ spec/views/assignments/edit.html.erb_spec.rb
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2011 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../views_helper')
+
+describe "/assignments/edit" do
+  before do
+    course_with_teacher(active_all: true)
+    view_context(@course, @user)
+    group = @course.assignment_groups.create!(name: "some group")
+    @assignment = @course.assignments.create!(
+      title: "some assignment",
+      submission_types: 'external_tool'
+    )
+    @assignment.assignment_group_id = group.id
+    @assignment.save!
+    assign(:assignment, @assignment)
+    assign(:assignment_groups, [group])
+    assign(:current_user_rubrics, [])
+  end
+
+  it "should render" do
+    render 'assignments/edit'
+    expect(response).not_to be_nil # have_tag()
+  end
+
+  it "should render rubrics" do
+    allow(@assignment).to receive(:quiz_lti?).and_return(true)
+    render 'assignments/edit'
+    expect(response).to render_template(partial: '_rubrics_component')
+  end
+end
+
