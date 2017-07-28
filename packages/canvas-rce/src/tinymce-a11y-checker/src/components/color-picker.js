2c2eed71f1b6cb5bd3748077e6bbcf2671bddfd3
Brent Burgoyne <bburgoyne@instructure.com>
2017-07-28 14:57:29 -0600

    improve color picker pointer contrast

--- packages/canvas-rce/src/tinymce-a11y-checker/src/components/color-picker.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/components/color-picker.js
@@ -1,12 +1,21 @@
 const React = require('react')
 const { ColorWrap, Saturation, Hue, Alpha } = require('react-color/lib/components/common')
+const Pointer = require('./pointer')
+const PointerCircle = require('react-color/lib/components/photoshop/PhotoshopPointerCircle').default
+console.log(Pointer, PointerCircle)
 
 class ColorPicker extends React.Component {
   render () {
     return <div>
-      <div style={{ position: 'relative', height: 150, marginTop: 10 }}><Saturation {...this.props} /></div>
-      <div style={{ position: 'relative', height: 10, marginTop: 10 }}><Hue {...this.props} /></div>
-      <div style={{ position: 'relative', height: 10, marginTop: 10 }}><Alpha {...this.props} /></div>
+      <div style={{ position: 'relative', height: 150, marginTop: 10 }}>
+        <Saturation {...this.props} pointer={PointerCircle} />
+      </div>
+      <div style={{ position: 'relative', height: 10, marginTop: 10 }}>
+        <Hue {...this.props} pointer={Pointer} />
+      </div>
+      <div style={{ position: 'relative', height: 10, marginTop: 10 }}>
+        <Alpha {...this.props} pointer={Pointer} />
+      </div>
     </div>
   }
 }
