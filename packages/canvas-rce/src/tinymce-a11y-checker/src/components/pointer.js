f5210cae5d783736f4577ca92ae87c7bf3d9f6ec
Brent Burgoyne <bburgoyne@instructure.com>
2017-07-28 14:57:29 -0600

    improve color picker pointer contrast

---
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/components/pointer.js
@@ -0,0 +1,19 @@
+const React = require('react')
+
+class Pointer extends React.PureComponent {
+  render () {
+    const styles = {
+      width: '10px',
+      height: '10px',
+      border: '1px solid rgba(0, 0, 0, 0.6)',
+      borderRadius: '6px',
+      transform: 'translate(-6px, -1px)',
+      backgroundColor: 'rgb(248, 248, 248)',
+      boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.37)'
+    }
+
+    return <div style={styles} />
+  }  
+}
+
+module.exports = Pointer
