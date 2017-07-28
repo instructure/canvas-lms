df063c820e6540a0efc1f3d4bd696ddf21af678d
Brent Burgoyne <bburgoyne@instructure.com>
2017-07-28 10:36:28 -0600

    add rule for ensuring tables have headers

--- packages/canvas-rce/src/tinymce-a11y-checker/src/rules/index.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/rules/index.js
@@ -1,11 +1,13 @@
 const imgAlt = require('./img-alt')
 const tableCaption = require('./table-caption')
+const tableHeader = require('./table-header')
 const smallTextContrast = require('./small-text-contrast')
 const largeTextContrast = require('./large-text-contrast')
 
 module.exports = [
   imgAlt,
   tableCaption,
+  tableHeader,
   smallTextContrast,
   largeTextContrast
 ]
