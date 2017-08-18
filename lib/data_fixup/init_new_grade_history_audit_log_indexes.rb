0da39062b99f1e64f09d0c9e7ee4cb0f4a27f9a3
Derek Bender <djbender@instructure.com>
2017-08-18 16:06:17 -0500

    Track last seen id for when timeouts occur
    This is a variant of 31b372eeab where the old migration has been deleted
    and split into two migrations. The first migration creates the table and
    the second migration runs the data fixup.
    
    When this job times out it will now resume from the last
    id seen instead of from the very beginning.
    
    This also makes the read/write batch amount configurable via Setting.
    
    closes: GRADE-207
    
    test plan:
    - migrations work
    
    Optional hard mode test plan:
    - manual checking of records in CQLSH
    
    Change-Id: I60b4b2b9a5fff3101eb307134b93d4b463090cbc
    Reviewed-on: https://gerrit.instructure.com/123317
    Tested-by: Jenkins
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Keith T. Garner <kgarner@instructure.com>
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>
    Reviewed-by: Shahbaz Javeed <sjaveed@instructure.com>

--- lib/data_fixup/init_new_grade_history_audit_log_indexes.rb
+++ lib/data_fixup/init_new_grade_history_audit_log_indexes.rb
@@ -23,13 +23,13 @@ module DataFixup
       new.build_indexes
     end
 
+    LAST_BATCH_TABLE = 'grade_changes_index_last_batch'.freeze
     SEARCH_CQL = %{
       SELECT id, created_at, context_id, assignment_id, grader_id, student_id
       FROM grade_changes
       WHERE token(id) > token(?)
       LIMIT ?
     }.freeze
-
     INDEX_METHODS = [
       :add_course_assignment_index,
       :add_course_assignment_grader_index,
@@ -40,24 +40,30 @@ module DataFixup
       :add_course_student_index
     ].freeze
 
-    READ_BATCH_SIZE = 1000
-    WRITE_BATCH_SIZE = 400
+    def read_batch_size
+      @read_batch_size ||=
+        Setting.get('init_new_grade_history_audit_log_indexes_read_batch_size', 1000).to_i
+    end
+
+    def write_batch_size
+      @write_batch_size ||=
+        Setting.get('init_new_grade_history_audit_log_indexes_write_batch_size', 200).to_i
+    end
 
     def build_indexes
       new_index_entries = []
-      last_seen_id = nil
+      last_seen_id = fetch_last_id
       done = false
       until done
         done, last_seen_id = read_and_process_batch(last_seen_id, new_index_entries)
       end
-      write_batch(new_index_entries)
     end
 
     private
 
     def read_and_process_batch(starting_key, index_entries)
       last_id = nil
-      result = database.execute(SEARCH_CQL, starting_key, READ_BATCH_SIZE)
+      result = database.execute(SEARCH_CQL, starting_key, read_batch_size)
       return true, nil if result.rows == 0
       result.fetch do |row|
         last_id = row['id']
@@ -67,14 +73,13 @@ module DataFixup
         end
       end
       write_in_batches(index_entries)
+      save_last_id(last_id)
       return false, last_id
     end
 
-    ResultStruct = Struct.new(:index, :record, :key)
-
     def write_in_batches(batch)
-      while batch.size >= WRITE_BATCH_SIZE
-        write_batch(batch.shift(WRITE_BATCH_SIZE))
+      while batch.size > 0
+        write_batch(batch.shift(write_batch_size))
       end
     end
 
@@ -86,6 +91,8 @@ module DataFixup
     def database
       @database ||= Canvas::Cassandra::DatabaseBuilder.from_config(:auditors)
     end
+
+    ResultStruct = Struct.new(:index, :record, :key)
 
     def add_course_assignment_index(row)
       index = Auditors::GradeChange::Stream.course_assignment_index
@@ -135,6 +142,22 @@ module DataFixup
       index = Auditors::GradeChange::Stream.course_student_index
       key = [row['context_id'], row['student_id']]
       ResultStruct.new(index, OpenStruct.new(row.to_hash), key)
+    end
+
+    def fetch_last_id
+      database.execute("SELECT last_id FROM #{LAST_BATCH_TABLE}").fetch do |row|
+        return row.to_hash['last_id']
+      end
+
+      nil
+    end
+
+    def save_last_id(last_id)
+      database.execute("INSERT INTO #{LAST_BATCH_TABLE} (id, last_id) VALUES (1, ?) ", last_id)
+    end
+
+    def log_message(message)
+      Rails.logger.debug("InitNewGradeHistoryAuditLogIndexes: #{message}")
     end
   end
 end
