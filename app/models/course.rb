ee3461b4e2ae2009a568a52a70c6ee3b298bdda2
Jon Willesen <jonw@instructure.com>
2017-08-24 15:37:18 -0600

    Revert "unify SR course nav labels"
    This reverts commit 639e3899d2e0bbd3c4eecfcadff241e8cf95da20.
    
    The above commit removed the screenreader field that's being used by
    the dashcards interface. See CNVS-37921.
    
    fixes CNVS-38600
    
    test plan:
    * make sure the actions hover text and screenreader text show and read
      the proper strings.
    
    Change-Id: If97203a21274563e8894c3593a66ae4d3960a395
    Reviewed-on: https://gerrit.instructure.com/123982
    Tested-by: Jenkins
    Reviewed-by: Aaron Kc Hsu <ahsu@instructure.com>
    QA-Review: Dan Sasaki <dsasaki@instructure.com>
    Product-Review: Jon Willesen <jonw+gerrit@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -2418,24 +2418,28 @@ class Course < ActiveRecord::Base
         :label => t('#tabs.announcements', "Announcements"),
         :css_class => 'announcements',
         :href => :course_announcements_path,
+        :screenreader => t("Course Announcements"),
         :icon => 'icon-announcement'
       }, {
         :id => TAB_ASSIGNMENTS,
         :label => t('#tabs.assignments', "Assignments"),
         :css_class => 'assignments',
         :href => :course_assignments_path,
+        :screenreader => t('#tabs.course_assignments', "Course Assignments"),
         :icon => 'icon-assignment'
       }, {
         :id => TAB_DISCUSSIONS,
         :label => t('#tabs.discussions', "Discussions"),
         :css_class => 'discussions',
         :href => :course_discussion_topics_path,
+        :screenreader => t("Course Discussions"),
         :icon => 'icon-discussion'
       }, {
         :id => TAB_GRADES,
         :label => t('#tabs.grades', "Grades"),
         :css_class => 'grades',
-        :href => :course_grades_path
+        :href => :course_grades_path,
+        :screenreader => t('#tabs.course_grades', "Course Grades")
       }, {
         :id => TAB_PEOPLE,
         :label => t('#tabs.people', "People"),
@@ -2451,6 +2455,7 @@ class Course < ActiveRecord::Base
         :label => t('#tabs.files', "Files"),
         :css_class => 'files',
         :href => :course_files_path,
+        :screenreader => t("Course Files"),
         :icon => 'icon-folder'
       }, {
         :id => TAB_SYLLABUS,
@@ -2491,7 +2496,8 @@ class Course < ActiveRecord::Base
         :id => TAB_SETTINGS,
         :label => t('#tabs.settings', "Settings"),
         :css_class => 'settings',
-        :href => :course_settings_path
+        :href => :course_settings_path,
+        :screenreader => t('#tabs.course_settings', "Course Settings")
       }
     ]
   end
