fd49bcc436fe1b11bd862157d0090452ed68acac
Mysti Sadler <mysti@instructure.com>
2017-08-30 17:20:03 -0600

    Remove "Courses" from navigation link SR text
    closes CNVS-37919
    closes GRADE-219
    
    Test plan
    - In a course, navigate through the course navigation tabs
    - Ensure their link text matches the screenreader text
    - On the dashboard, using the dashboard card view, ensure
      the screenreader reads the appropriate text for the
      announcements/discussions/files icon links
    - Ensure the mouseover text for the links shows the same
    
    Change-Id: I66a0c88c218ebc186ff0f8d6ca698c2471754fec
    Reviewed-on: https://gerrit.instructure.com/124584
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: David Tan <dtan@instructure.com>
    Product-Review: Nathan Rogowski <nathan@instructure.com>
    Tested-by: Jenkins

--- app/models/course.rb
+++ app/models/course.rb
@@ -2410,97 +2410,90 @@ class Course < ActiveRecord::Base
 
   def self.default_tabs
     [{
-        :id => TAB_HOME,
-        :label => t('#tabs.home', "Home"),
-        :css_class => 'home',
-        :href => :course_path
-      }, {
-        :id => TAB_ANNOUNCEMENTS,
-        :label => t('#tabs.announcements', "Announcements"),
-        :css_class => 'announcements',
-        :href => :course_announcements_path,
-        :screenreader => t("Course Announcements"),
-        :icon => 'icon-announcement'
-      }, {
-        :id => TAB_ASSIGNMENTS,
-        :label => t('#tabs.assignments', "Assignments"),
-        :css_class => 'assignments',
-        :href => :course_assignments_path,
-        :screenreader => t('#tabs.course_assignments', "Course Assignments"),
-        :icon => 'icon-assignment'
-      }, {
-        :id => TAB_DISCUSSIONS,
-        :label => t('#tabs.discussions', "Discussions"),
-        :css_class => 'discussions',
-        :href => :course_discussion_topics_path,
-        :screenreader => t("Course Discussions"),
-        :icon => 'icon-discussion'
-      }, {
-        :id => TAB_GRADES,
-        :label => t('#tabs.grades', "Grades"),
-        :css_class => 'grades',
-        :href => :course_grades_path,
-        :screenreader => t('#tabs.course_grades', "Course Grades")
-      }, {
-        :id => TAB_PEOPLE,
-        :label => t('#tabs.people', "People"),
-        :css_class => 'people',
-        :href => :course_users_path
-      }, {
-        :id => TAB_PAGES,
-        :label => t('#tabs.pages', "Pages"),
-        :css_class => 'pages',
-        :href => :course_wiki_path
-      }, {
-        :id => TAB_FILES,
-        :label => t('#tabs.files', "Files"),
-        :css_class => 'files',
-        :href => :course_files_path,
-        :screenreader => t("Course Files"),
-        :icon => 'icon-folder'
-      }, {
-        :id => TAB_SYLLABUS,
-        :label => t('#tabs.syllabus', "Syllabus"),
-        :css_class => 'syllabus',
-        :href => :syllabus_course_assignments_path
-      }, {
-        :id => TAB_OUTCOMES,
-        :label => t('#tabs.outcomes', "Outcomes"),
-        :css_class => 'outcomes',
-        :href => :course_outcomes_path
-      }, {
-        :id => TAB_QUIZZES,
-        :label => t('#tabs.quizzes', "Quizzes"),
-        :css_class => 'quizzes',
-        :href => :course_quizzes_path
-      }, {
-        :id => TAB_MODULES,
-        :label => t('#tabs.modules', "Modules"),
-        :css_class => 'modules',
-        :href => :course_context_modules_path
-      }, {
-        :id => TAB_CONFERENCES,
-        :label => t('#tabs.conferences', "Conferences"),
-        :css_class => 'conferences',
-        :href => :course_conferences_path
-      }, {
-        :id => TAB_COLLABORATIONS,
-        :label => t('#tabs.collaborations', "Collaborations"),
-        :css_class => 'collaborations',
-        :href => :course_collaborations_path
-      }, {
-        :id => TAB_COLLABORATIONS_NEW,
-        :label => t('#tabs.collaborations', "Collaborations"),
-        :css_class => 'collaborations',
-        :href => :course_lti_collaborations_path
-      }, {
-        :id => TAB_SETTINGS,
-        :label => t('#tabs.settings', "Settings"),
-        :css_class => 'settings',
-        :href => :course_settings_path,
-        :screenreader => t('#tabs.course_settings', "Course Settings")
-      }
-    ]
+      :id => TAB_HOME,
+      :label => t('#tabs.home', "Home"),
+      :css_class => 'home',
+      :href => :course_path
+    }, {
+      :id => TAB_ANNOUNCEMENTS,
+      :label => t('#tabs.announcements', "Announcements"),
+      :css_class => 'announcements',
+      :href => :course_announcements_path,
+      :icon => 'icon-announcement'
+    }, {
+      :id => TAB_ASSIGNMENTS,
+      :label => t('#tabs.assignments', "Assignments"),
+      :css_class => 'assignments',
+      :href => :course_assignments_path,
+      :icon => 'icon-assignment'
+    }, {
+      :id => TAB_DISCUSSIONS,
+      :label => t('#tabs.discussions', "Discussions"),
+      :css_class => 'discussions',
+      :href => :course_discussion_topics_path,
+      :icon => 'icon-discussion'
+    }, {
+      :id => TAB_GRADES,
+      :label => t('#tabs.grades', "Grades"),
+      :css_class => 'grades',
+      :href => :course_grades_path,
+    }, {
+      :id => TAB_PEOPLE,
+      :label => t('#tabs.people', "People"),
+      :css_class => 'people',
+      :href => :course_users_path
+    }, {
+      :id => TAB_PAGES,
+      :label => t('#tabs.pages', "Pages"),
+      :css_class => 'pages',
+      :href => :course_wiki_path
+    }, {
+      :id => TAB_FILES,
+      :label => t('#tabs.files', "Files"),
+      :css_class => 'files',
+      :href => :course_files_path,
+      :icon => 'icon-folder'
+    }, {
+      :id => TAB_SYLLABUS,
+      :label => t('#tabs.syllabus', "Syllabus"),
+      :css_class => 'syllabus',
+      :href => :syllabus_course_assignments_path
+    }, {
+      :id => TAB_OUTCOMES,
+      :label => t('#tabs.outcomes', "Outcomes"),
+      :css_class => 'outcomes',
+      :href => :course_outcomes_path
+    }, {
+      :id => TAB_QUIZZES,
+      :label => t('#tabs.quizzes', "Quizzes"),
+      :css_class => 'quizzes',
+      :href => :course_quizzes_path
+    }, {
+      :id => TAB_MODULES,
+      :label => t('#tabs.modules', "Modules"),
+      :css_class => 'modules',
+      :href => :course_context_modules_path
+    }, {
+      :id => TAB_CONFERENCES,
+      :label => t('#tabs.conferences', "Conferences"),
+      :css_class => 'conferences',
+      :href => :course_conferences_path
+    }, {
+      :id => TAB_COLLABORATIONS,
+      :label => t('#tabs.collaborations', "Collaborations"),
+      :css_class => 'collaborations',
+      :href => :course_collaborations_path
+    }, {
+      :id => TAB_COLLABORATIONS_NEW,
+      :label => t('#tabs.collaborations', "Collaborations"),
+      :css_class => 'collaborations',
+      :href => :course_lti_collaborations_path
+    }, {
+      :id => TAB_SETTINGS,
+      :label => t('#tabs.settings', "Settings"),
+      :css_class => 'settings',
+      :href => :course_settings_path,
+    }]
   end
 
   def tab_hidden?(id)
@@ -2545,13 +2538,9 @@ class Course < ActiveRecord::Base
           tab[:external] = default_tab[:external]
           tab[:icon] = default_tab[:icon]
           tab[:target] = default_tab[:target] if default_tab[:target]
-          tab[:screenreader] = default_tab[:screenreader]
           default_tabs.delete_if {|t| t[:id] == tab[:id] }
           external_tabs.delete_if {|t| t[:id] == tab[:id] }
           tab
-        else
-          # Remove any tabs we don't know about in default_tabs (in case we removed them or something, like Groups)
-          nil
         end
       end
       tabs.compact!
