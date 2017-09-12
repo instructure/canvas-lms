254cccb54484a6a03b4979230bba3d8a326a69da
Dan Minkevitch <dan@instructure.com>
2017-09-12 09:13:41 -0700

    Correct bounced comm. channels endpoint naming
    refs CNVS-24295
    
    Change-Id: I5ecf04ecf3091bf639247174d2792b8a31a4d344
    Reviewed-on: https://gerrit.instructure.com/125713
    Tested-by: Jenkins
    Reviewed-by: Venk Natarajan <vnatarajan@instructure.com>
    Product-Review: Dan Minkevitch <dan@instructure.com>
    QA-Review: Dan Minkevitch <dan@instructure.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1380,8 +1380,8 @@ CanvasRails::Application.routes.draw do
       get 'users/:user_id/communication_channels', action: :index, as: 'communication_channels'
       post 'users/:user_id/communication_channels', action: :create
       post 'users/:user_id/communication_channels/:id', action: :reset_bounce_count, as: 'reset_bounce_count'
-      get 'accounts/:account_id/bouncing_communication_channels.csv', action: :bouncing_channel_report
-      post 'accounts/:account_id/bouncing_communication_channels/reset', action: :bulk_reset_bounce_counts
+      get 'accounts/:account_id/bounced_communication_channels.csv', action: :bouncing_channel_report
+      post 'accounts/:account_id/bounced_communication_channels/reset', action: :bulk_reset_bounce_counts
       get 'accounts/:account_id/unconfirmed_communication_channels.csv', action: :unconfirmed_channel_report
       post 'accounts/:account_id/unconfirmed_communication_channels/confirm', action: :bulk_confirm
       delete 'users/:user_id/communication_channels/:id', action: :destroy
