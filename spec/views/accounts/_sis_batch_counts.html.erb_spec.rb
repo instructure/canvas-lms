966c1c6477b37e763a18d0853cda801213573f6f
Rob Orton <rob@instructure.com>
2017-09-20 20:43:40 -0600

    add user_observers + change_sis_ids to sis counts
    fixes CNVS-39322
    
    test plan
     - counts should have user_observers and change_sis_ids
    
    Change-Id: I861595cd6312eb6f7a68ec9fbe43c8cf39680e39
    Reviewed-on: https://gerrit.instructure.com/126855
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- spec/views/accounts/_sis_batch_counts.html.erb_spec.rb
+++ spec/views/accounts/_sis_batch_counts.html.erb_spec.rb
@@ -24,16 +24,17 @@ describe "accounts/_sis_batch_counts.html.erb" do
   it "should render sis count data" do
     data = {counts: {xlists: 2, enrollments: 3, courses: 5, users: 6, terms: 6,
                      group_memberships: 7, groups: 8, sections: 9, accounts: 10,
-                     admins: 1, }}
+                     admins: 1, user_observers: 3, change_sis_id: 0}}
     report = double()
     expect(report).to receive(:data).and_return(data)
     render :partial => 'accounts/sis_batch_counts', :object => report
 
-    map = {:xlists => "Crosslists", :group_memberships => "Group Enrollments"}
+    map = {xlists: "Crosslists", group_memberships: "Group Enrollments",
+           user_observers: "User Observers", change_sis_id: "Change SIS IDs"}
 
     data[:counts].each_pair do |type, count|
       name = map[type] || type.to_s.capitalize
-      expect(response.body).to match /#{name}: #{count}/
+      expect(response.body).to match(/#{name}: #{count}/)
     end
   end
 end
