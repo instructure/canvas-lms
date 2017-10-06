3d080a7c2b8589012c1d8fcd1522aeb369f5611b
brian kirkby <bkirkby@instructure.com>
2017-10-06 12:00:05 -0600

    implement the image-alt-length rules
    fixes CNVS-39827
    
    test plan:
    - run demo
    - create alt text greater than 120 chars
    - open a11y-checker
    - see if the issue is detected and if you can fix it
    
    Change-Id: I1f3677e14a0dd7142bcea5cc764eac9497ba8cba
    Reviewed-on: https://gerrit.instructure.com/128847
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: brian kirkby <bkirkby@instructure.com>
    Reviewed-by: brian kirkby <bkirkby@instructure.com>
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>

---
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/rules/__tests__/img-alt-length.js
@@ -0,0 +1,49 @@
+const rule = require("../img-alt-length")
+
+let el
+
+beforeEach(() => {
+  el = document.createElement("img")
+})
+
+describe("test", () => {
+  test("returns true if alt text is shorter than max length", () => {
+    el.setAttribute("alt", "some text")
+    expect(rule.test(el)).toBeTruthy()
+  })
+
+  test("returns true if not image tag", () => {
+    const div = document.createElement("div")
+    expect(rule.test(div)).toBeTruthy()
+  })
+
+  test("returns false if alt is longer than max length", () => {
+    const moreThanMaxString = Array(rule["max-alt-length"] + 2).join("x")
+    el.setAttribute("alt", moreThanMaxString)
+    expect(rule.test(el)).toBeFalsy()
+  })
+})
+
+describe("data", () => {
+  test("returns alt text", () => {
+    el.setAttribute("alt", "some text")
+    expect(rule.data(el).alt).toBe("some text")
+  })
+
+  test("returns empty alt text if no alt attribute", () => {
+    expect(rule.data(el).alt).toBe("")
+  })
+})
+
+describe("form", () => {})
+
+describe("update", () => {
+  test("returns same element", () => {
+    expect(rule.update(el, {})).toBe(el)
+  })
+
+  test("set alt text to value", () => {
+    const text = "some text"
+    expect(rule.update(el, { alt: text }).getAttribute("alt")).toBe(text)
+  })
+})
