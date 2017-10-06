12d63c670b6a5932c794119ccf00310dfc1f3cc6
Ryan Shaw <ryan@instructure.com>
2017-10-06 09:51:55 -0600

    fix styling of header in account User search page’
    Fixes: CNVS-39559
    
    I had previously already fixed the styling of
    the courses tab, this also fixes it for the
    “People” tab
    
    Test plan:
    * Turn on “course/user search” feature flag
    * go to “courses & people” in an account
    * click the “people” tab
    * the headers should be styled correctly
    * click them to make sure that the arrows show up and sort correctly
    
    Change-Id: I84374d3c7354e5d51c80afb76e4eeecdb4f556c7
    Reviewed-on: https://gerrit.instructure.com/128818
    Tested-by: Jenkins
    Reviewed-by: brian kirkby <bkirkby@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/account_course_user_search/UsersList.js
+++ app/jsx/account_course_user_search/UsersList.js
@@ -17,10 +17,11 @@
  */
 
 import preventDefault from 'compiled/fn/preventDefault'
-import IconArrowUpSolid from 'instructure-icons/lib/Solid/IconArrowUpSolid'
-import IconArrowDownSolid from 'instructure-icons/lib/Solid/IconArrowDownSolid'
+import IconMiniArrowUpSolid from 'instructure-icons/lib/Solid/IconMiniArrowUpSolid'
+import IconMiniArrowDownSolid from 'instructure-icons/lib/Solid/IconMiniArrowDownSolid'
 import Typography from 'instructure-ui/lib/components/Typography'
 import Tooltip from 'instructure-ui/lib/components/Tooltip'
+import Link from 'instructure-ui/lib/components/Link'
 import React from 'react'
 import PropTypes from 'prop-types'
 import I18n from 'i18n!account_course_user_search'
@@ -28,7 +29,7 @@ import UsersListRow from './UsersListRow'
 
 const { string, object, func } = PropTypes
 
-class UsersList extends React.Component {
+export default class UsersList extends React.Component {
   static propTypes = {
     accountId: string.isRequired,
     users: PropTypes.arrayOf(object).isRequired,
@@ -60,149 +61,77 @@ class UsersList extends React.Component {
     this.props.onApplyFilters()
   }
 
+  renderHeader ({id, label, tipDesc, tipAsc}) {
+    const {sort, order} = this.props.userList.searchFilter
+    return (
+      <Tooltip
+        as={Link}
+        tip={(sort === id && order === 'asc') ? tipAsc : tipDesc}
+        onClick={preventDefault(() => this.updateOrder(id))}
+      >
+        {label}
+        {sort === id ?
+          (order === 'asc' ? <IconMiniArrowDownSolid /> : <IconMiniArrowUpSolid />) :
+          ''
+        }
+      </Tooltip>
+    )
+  }
+
   render () {
-    const { users, timezones, accountId } = this.props;
-
-    const sort = this.props.userList.searchFilter.sort
-    const order = this.props.userList.searchFilter.order
-
-    const nameLabel = I18n.t('Name')
-    const lastLoginLabel = I18n.t('Last Login')
-    const emailLabel = I18n.t('Email')
-    const idLabel = I18n.t('SIS ID')
-
-    let nameTip
-    let lastLoginTip
-    let emailTip
-    let idTip
-
-    let nameArrow = ''
-    let lastLoginArrow = ''
-    let emailArrow = ''
-    let idArrow = ''
-
-    if (sort === 'username' || !sort) {
-      emailTip = I18n.t('Click to order by email ascending')
-      lastLoginTip = I18n.t('Click to order by last login ascending')
-      idTip = I18n.t('Click to order by SIS ID ascending')
-      if (order === 'asc' || !order) {
-        nameTip = I18n.t('Click to sort by name descending')
-        nameArrow = <IconArrowDownSolid />
-      } else {
-        nameTip = I18n.t('Click to sort by name ascending')
-        nameArrow = <IconArrowUpSolid />
-      }
-    } else if (sort === 'last_login') {
-      nameTip = I18n.t('Click to sort by name ascending')
-      emailTip = I18n.t('Click to order by email ascending')
-      idTip = I18n.t('Click to order by SIS ID ascending')
-      if (order === 'asc' || !order) {
-        lastLoginTip = I18n.t('Click to sort by last login descending')
-        lastLoginArrow = <IconArrowDownSolid />
-      } else {
-        lastLoginTip = I18n.t('Click to sort by last login ascending')
-        lastLoginArrow = <IconArrowUpSolid />
-      }
-    } else if (sort === 'email') {
-      nameTip = I18n.t('Click to sort by name ascending')
-      idTip = I18n.t('Click to order by SIS ID ascending')
-      lastLoginTip = I18n.t('Click to order by last login ascending')
-      if (order === 'asc' || !order) {
-        emailTip = I18n.t('Click to sort by email descending')
-        emailArrow = <IconArrowDownSolid />
-      } else {
-        emailTip = I18n.t('Click to sort by email ascending')
-        emailArrow = <IconArrowUpSolid />
-      }
-    } else if (sort === 'sis_id') {
-      nameTip = I18n.t('Click to sort by name ascending')
-      emailTip = I18n.t('Click to order by email ascending')
-      lastLoginTip = I18n.t('Click to order by last login ascending')
-      if (order === 'asc' || !order) {
-        idTip = I18n.t('Click to sort by SIS ID descending')
-        idArrow = <IconArrowDownSolid />
-      } else {
-        idTip = I18n.t('Click to sort by SIS ID ascending')
-        idArrow = <IconArrowUpSolid />
-      }
-    }
-
+    const { users, timezones, accountId } = this.props
     return (
       <div className="content-box" role="grid">
         <div role="row" className="grid-row border border-b pad-box-mini">
           <div role="columnheader" className="col-xs-3">
-            <a
-              role="button"
-              href="#"
-              className="courses-user-list-header"
-              onClick={preventDefault(() => this.updateOrder('username'))}
-            >
-              <Tooltip as={Typography} tip={nameTip}>
-                {nameLabel}
-                {nameArrow}
-              </Tooltip>
-            </a>
+            {this.renderHeader({
+              id: 'username',
+              label: I18n.t('Name'),
+              tipDesc: I18n.t('Click to sort by name ascending'),
+              tipAsc: I18n.t('Click to sort by name descending')
+            })}
           </div>
           <div role="columnheader" className="col-xs-3">
-            <a
-              role="button"
-              href="#"
-              className="courses-user-list-header"
-              onClick={preventDefault(() => this.updateOrder('email'))}
-            >
-              <Tooltip as={Typography} tip={emailTip}>
-                {emailLabel}
-                {emailArrow}
-              </Tooltip>
-            </a>
+            {this.renderHeader({
+              id: 'email',
+              label: I18n.t('Email'),
+              tipDesc: I18n.t('Click to sort by email ascending'),
+              tipAsc: I18n.t('Click to sort by email descending')
+            })}
           </div>
           <div role="columnheader" className="col-xs-1">
-            <a
-              role="button"
-              href="#"
-              className="courses-user-list-header"
-              onClick={preventDefault(() => this.updateOrder('sis_id'))}
-            >
-              <Tooltip as={Typography} tip={idTip}>
-                {idLabel}
-                {idArrow}
-              </Tooltip>
-            </a>
+            {this.renderHeader({
+              id: 'sis_id',
+              label: I18n.t('SIS ID'),
+              tipDesc: I18n.t('Click to sort by SIS ID ascending'),
+              tipAsc: I18n.t('Click to sort by SIS ID descending')
+            })}
           </div>
           <div role="columnheader" className="col-xs-2">
-            <a
-              role="button"
-              href="#"
-              className="courses-user-list-header"
-              onClick={preventDefault(() => this.updateOrder('last_login'))}
-            >
-              <Tooltip as={Typography} tip={lastLoginTip}>
-                {lastLoginLabel}
-                {lastLoginArrow}
-              </Tooltip>
-            </a>
+            {this.renderHeader({
+              id: 'last_login',
+              label: I18n.t('Last Login'),
+              tipDesc: I18n.t('Click to sort by last login ascending'),
+              tipAsc: I18n.t('Click to sort by last login descending')
+            })}
           </div>
           <div role="columnheader" className="col-xs-2">
             <span className="screenreader-only">{I18n.t('User option links')}</span>
           </div>
         </div>
         <div className="users-list" role="rowgroup">
-          {
-              users.map(user => (
-                <UsersListRow
-                  handlers={this.props.handlers}
-                  key={user.id}
-                  timezones={timezones}
-                  accountId={accountId}
-                  user={user}
-                  permissions={this.props.permissions}
-                />
-                ))
-            }
+          {users.map(user =>
+            <UsersListRow
+              handlers={this.props.handlers}
+              key={user.id}
+              timezones={timezones}
+              accountId={accountId}
+              user={user}
+              permissions={this.props.permissions}
+            />
+          )}
         </div>
       </div>
     )
   }
-  }
-
-export default UsersList
+}
