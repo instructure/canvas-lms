fc2c66a7e5d5d916f553e89ee0f0f653b82bf7f4
brian kirkby <bkirkby@instructure.com>
2017-10-06 14:07:17 -0600

    fix/write unit test for img-alt-filename rule
    fixes CNVS-39831
    
    test plan:
    - run the unit tests and make sure they pass
    - cr the unit test ot make sure they test the right rule
    
    Change-Id: Ia04975b2dc8419f4d67296c97fc86365af7928f9
    Reviewed-on: https://gerrit.instructure.com/128872
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Product-Review: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: Brent Burgoyne <bburgoyne@instructure.com>

--- packages/canvas-rce/src/tinymce-a11y-checker/src/rules/__tests__/img-alt-filename.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/rules/__tests__/img-alt-filename.js
@@ -1,28 +1,29 @@
-const rule = require("../img-alt")
+const rule = require("../img-alt-filename")
 
 let el
 
 beforeEach(() => {
   el = document.createElement("img")
+  el.setAttribute("src", "/file.txt")
 })
 
 describe("test", () => {
-  test("returns true if alt text is not empty", () => {
+  test("returns true if alt text is empty", () => {
+    expect(rule.test(el)).toBeTruthy()
+  })
+
+  test("returns true if not img tag", () => {
+    const div = document.createElement("div")
+    expect(rule.test(div)).toBeTruthy()
+  })
+
+  test("returns true if alt text is not filename", () => {
     el.setAttribute("alt", "some text")
     expect(rule.test(el)).toBeTruthy()
   })
 
-  test("returns true if decorative", () => {
-    el.setAttribute("data-decorative", "")
-    expect(rule.test(el)).toBeTruthy()
-  })
-
-  test("returns false if no alt attribute", () => {
-    expect(rule.test(el)).toBeFalsy()
-  })
-
-  test("returns false if alt is empty and not decorative", () => {
-    el.setAttribute("alt", "")
+  test("returns false if alt text is filename", () => {
+    el.setAttribute("alt", "file.txt")
     expect(rule.test(el)).toBeFalsy()
   })
 })
@@ -36,54 +37,18 @@ describe("data", () => {
   test("returns empty alt text if no alt attribute", () => {
     expect(rule.data(el).alt).toBe("")
   })
-
-  test("returns decorative true if el has data-decorative", () => {
-    el.setAttribute("data-decorative", "")
-    expect(rule.data(el).decorative).toBeTruthy()
-  })
-
-  test("returns decorative false if el has alt text and data-decorative", () => {
-    el.setAttribute("data-decorative", "")
-    el.setAttribute("alt", "some text")
-    expect(rule.data(el).decorative).toBeFalsy()
-  })
-
-  test("returns decorative false if el does not have data-decorative", () => {
-    expect(rule.data(el).decorative).toBeFalsy()
-  })
 })
 
-describe("form", () => {
-  test("alt field is disabled if decorative", () => {
-    const altField = rule.form().find(f => f.dataKey === "alt")
-    expect(altField.disabledIf({ decorative: true })).toBeTruthy()
-  })
-
-  test("alt field is not disabled if not decorative", () => {
-    const altField = rule.form().find(f => f.dataKey === "alt")
-    expect(altField.disabledIf({ decorative: false })).toBeFalsy()
-  })
-})
+describe("form", () => {})
 
 describe("update", () => {
   test("returns same element", () => {
     expect(rule.update(el, {})).toBe(el)
   })
 
-  test("sets alt text to empty and data-decorative if decorative", () => {
-    rule.update(el, { decorative: true })
-    expect(el.hasAttribute("data-decorative")).toBeTruthy()
-    expect(el.getAttribute("alt")).toBe("")
-  })
-
-  test("sets alt text if not decorative", () => {
-    rule.update(el, { decorative: false, alt: "some text" })
-    expect(el.getAttribute("alt")).toBe("some text")
-  })
-
-  test("removes data-decorative if not decorative", () => {
-    el.setAttribute("data-decorative", "")
-    rule.update(el, { decorative: false })
-    expect(el.hasAttribute("data-decorative")).toBeFalsy()
+  test("changes alt text if requested", () => {
+    const text = "this is my text"
+    el.setAttribute("alt", "thisismy.txt")
+    expect(rule.update(el, { alt: text }).getAttribute("alt")).toBe(text)
   })
 })
