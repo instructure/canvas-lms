3e3aa78523e295df0a7561c8f62094450b3a9198
Ryan Shaw <ryan@instructure.com>
2017-10-11 12:09:40 -0600

    Make it work with the version of React Canvas uses
    The version of react that canvas uses (“0.14.9”) does not have
    React.PureComponent. so this will throw an error.
    
    The reason why this was not caught was because the initial commit
    That added this to canvas had changes that it did not commit to
    yarn.lock and so in the review, no one noticed that it was making
    it add a new copy of react to yarn.lock to use just for this. And that
    New copy of react (15.x) did have `PureComponent`
    
    Test plan:
    * when this is used in canvas, it should work
    
    Change-Id: Ie56e7c5bc47923b4144be61396ec73ef20563d94
    Reviewed-on: https://gerrit.instructure.com/129323
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- packages/canvas-rce/src/tinymce-a11y-checker/src/components/pointer.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/components/pointer.js
@@ -1,21 +1,15 @@
 import React from "react"
 
-export default class Pointer extends React.PureComponent {
-  static get displayName() {
-    return "Pointer"
+export default function Pointer() {
+  const styles = {
+    width: "10px",
+    height: "10px",
+    border: "1px solid rgba(0, 0, 0, 0.6)",
+    borderRadius: "6px",
+    transform: "translate(-6px, -1px)",
+    backgroundColor: "rgb(248, 248, 248)",
+    boxShadow: "0 1px 4px 0 rgba(0, 0, 0, 0.37)"
   }
 
-  render() {
-    const styles = {
-      width: "10px",
-      height: "10px",
-      border: "1px solid rgba(0, 0, 0, 0.6)",
-      borderRadius: "6px",
-      transform: "translate(-6px, -1px)",
-      backgroundColor: "rgb(248, 248, 248)",
-      boxShadow: "0 1px 4px 0 rgba(0, 0, 0, 0.37)"
-    }
-
-    return <div style={styles} />
-  }
+  return <div style={styles} />
 }
