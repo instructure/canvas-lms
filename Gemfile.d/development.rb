dc51ebdb6bc4b6421ee83f65ec2a7d1e65886d85
Yuri <yuri@sparkroad.com>
2017-10-12 17:06:00 -0700

    working but pending internal email auth and creation of courseware@strongmind

--- Gemfile.d/development.rb
+++ Gemfile.d/development.rb
@@ -20,11 +20,12 @@ group :development do
   gem 'letter_opener', '1.4.1'
   gem 'spring', '2.0.2'
   gem 'spring-commands-rspec', '1.0.4'
-
+  gem 'awesome_print'
   # Option to DISABLE_RUBY_DEBUGGING is helpful IDE-based debugging.
   # The ruby debug gems conflict with the IDE-based debugger gem.
   # Set this option in your dev environment to disable.
   unless ENV['DISABLE_RUBY_DEBUGGING']
     gem 'byebug', '9.0.6', platform: :mri
+    
   end
 end
