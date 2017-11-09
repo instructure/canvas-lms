585e7fc48ccdba2802221a5dbac0fed3a9498ccd
Jacob Fugal <jacob@instructure.com>
2017-11-09 14:53:34 -0700

    remove redundant alt text from adjacent links
    fixes CORE-376
    
    when combining adjacent links to the same target for which one contains
    a solitary image whose alt text is redundant with the text contents of
    the other, remove the redundant alt text and flag the image as
    'decorative'.
    
    see https://www.w3.org/TR/WCAG20-TECHS/H2.html
    
    Change-Id: Ic305396a1d1c49fbefd6eedd20604608a312d73d
    Reviewed-on: https://gerrit.instructure.com/132400
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>

--- packages/canvas-rce/src/tinymce-a11y-checker/src/rules/__tests__/adjacent-links.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/rules/__tests__/adjacent-links.js
@@ -54,4 +54,107 @@ describe("update", () => {
     expect(newA.textContent).toBe(content + content)
     expect(newA.tagName).toBe("A")
   })
+
+  // https://www.w3.org/TR/WCAG20-TECHS/H2.html
+  describe("adjacent image and text links for the same resource", () => {
+    const content = "home"
+    let img, text
+    beforeEach(() => {
+      img = document.createElement("img")
+      img.setAttribute("alt", content)
+      text = document.createTextNode(content)
+    })
+
+    test("removes redundant image alt text", () => {
+      a1.appendChild(img)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe("")
+    })
+
+    test("leaves unique alt text alone", () => {
+      const altText = "unique alt text"
+      img.setAttribute("alt", altText)
+      a1.appendChild(img)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe(altText)
+    })
+
+    test("finds solitary images with nesting between them and link", () => {
+      const div = document.createElement("div")
+      div.appendChild(img)
+      a1.appendChild(div)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.tagName).toBe("DIV")
+      expect(newA.firstElementChild.firstElementChild.tagName).toBe("IMG")
+      expect(newA.firstElementChild.firstElementChild.getAttribute("alt")).toBe(
+        ""
+      )
+    })
+
+    test("ignores non-solitary images", () => {
+      const otherText = document.createTextNode("other text")
+      a1.appendChild(img)
+      a1.appendChild(otherText)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe(content)
+    })
+
+    test("doesn't treat surrounding whitespace as non-solitary", () => {
+      const otherText = document.createTextNode(" ")
+      a1.appendChild(img)
+      a1.appendChild(otherText)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe("")
+    })
+
+    test("checks redundancy against all text content from the non-image link", () => {
+      const part1 = "many"
+      const part2 = "words"
+      img.setAttribute("alt", part1 + " " + part2)
+      a1.appendChild(img)
+
+      const span1 = document.createElement("span")
+      const span2 = document.createElement("span")
+      span1.appendChild(document.createTextNode(part1))
+      span2.appendChild(document.createTextNode(part2))
+      a2.appendChild(span1)
+      a2.appendChild(document.createTextNode(" "))
+      a2.appendChild(span2)
+
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe("")
+    })
+
+    test("checks redundancy with whitespace normalization", () => {
+      const variant1 = " some  whitespace"
+      const variant2 = "some whitespace  "
+      img.setAttribute("alt", variant1)
+      a1.appendChild(img)
+      a2.appendChild(document.createTextNode(variant2))
+
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe("")
+    })
+
+    test("sets data-decorative when removing redundant image alt text", () => {
+      a1.appendChild(img)
+      a2.appendChild(text)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("data-decorative")).toBe(
+        "true"
+      )
+    })
+
+    test("works with image on right as well", () => {
+      a1.appendChild(text)
+      a2.appendChild(img)
+      const newA = rule.update(a1, { combine: true })
+      expect(newA.firstElementChild.getAttribute("alt")).toBe("")
+    })
+  })
 })
--- packages/canvas-rce/src/tinymce-a11y-checker/src/rules/adjacent-links.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/rules/adjacent-links.js
@@ -7,6 +7,61 @@ const shouldMergeAnchors = (elem1, elem2) => {
   return elem1.getAttribute("href") === elem2.getAttribute("href")
 }
 
+const notWhitespace = node => {
+  return node.nodeType !== Node.TEXT_NODE || node.textContent.match(/\S/)
+}
+
+const onlyChild = parent => {
+  const child = parent.firstElementChild
+  if (!child) {
+    return null
+  }
+  if ([...parent.childNodes].filter(notWhitespace).length > 1) {
+    return null
+  }
+  return child
+}
+
+const solitaryDescendantImage = link => {
+  let parent = link
+  let child = onlyChild(parent)
+  while (child) {
+    if (child.tagName === "IMG") {
+      return child
+    }
+    parent = child
+    child = onlyChild(parent)
+  }
+  return null
+}
+
+const normalizeText = text => {
+  // normalize whitespace and trim leading and trailing whitespace
+  return text.replace(/\s+/g, " ").trim()
+}
+
+const descendantImageWithRedundantAltText = (left, right) => {
+  let leftImage = solitaryDescendantImage(left)
+  let rightImage = solitaryDescendantImage(right)
+  if (
+    leftImage &&
+    !rightImage &&
+    normalizeText(leftImage.getAttribute("alt")) ===
+      normalizeText(right.textContent)
+  ) {
+    return leftImage
+  } else if (
+    rightImage &&
+    !leftImage &&
+    normalizeText(rightImage.getAttribute("alt")) ===
+      normalizeText(left.textContent)
+  ) {
+    return rightImage
+  } else {
+    return null
+  }
+}
+
 export default {
   test: function(elem) {
     if (elem.tagName != "A") {
@@ -33,6 +88,14 @@ export default {
     const rootElem = elem.parentNode
     if (data.combine) {
       const next = elem.nextElementSibling
+
+      // https://www.w3.org/TR/WCAG20-TECHS/H2.html
+      const image = descendantImageWithRedundantAltText(elem, next)
+      if (image) {
+        image.setAttribute("data-decorative", "true")
+        image.setAttribute("alt", "")
+      }
+
       rootElem.removeChild(next)
       elem.innerHTML += next.innerHTML
     }
