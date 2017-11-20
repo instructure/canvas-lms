b032951ae43919a82e48d5d9a9d2e1e1b25c8b07
Andrew Butterfield <abutterfield@instructure.com>
2017-11-20 12:24:06 -0800

    Make ampersand regex less greedy
    fixes PLAT-2019
    
    Test plan:
    * Using the redirect tool attempt to install using the problematic url
    * Ensure that tool installs and that the redirect works
    
    Change-Id: I7f409c764faea90389e5172df6e988f5a6808468
    Reviewed-on: https://gerrit.instructure.com/133364
    Tested-by: Jenkins
    Reviewed-by: James Williams  <jamesw@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Andrew Butterfield <abutterfield@instructure.com>

--- spec/lib/basic_lti_spec.rb
+++ spec/lib/basic_lti_spec.rb
@@ -97,6 +97,30 @@ describe BasicLTI do
     expect {lti.convert_blti_xml(xml)}.to raise_error(CC::Importer::BLTIConverter::CCImportError)
   end
 
+  it "xml converter should handle urls with semicolons" do
+    url = 'http://example.com/other_url;ID=0;STATUS=1;OTHERSTUFFS=%20Dude%20where%20is%20my%20car'
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <cartridge_basiclti_link xmlns="http://www.imsglobal.org/xsd/imslticc_v1p0"
+          xmlns:blti = "http://www.imsglobal.org/xsd/imsbasiclti_v1p0"
+          xmlns:lticm ="http://www.imsglobal.org/xsd/imslticm_v1p0"
+          xmlns:lticp ="http://www.imsglobal.org/xsd/imslticp_v1p0"
+          xmlns:xsi = "http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation = "http://www.imsglobal.org/xsd/imslticc_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticc_v1p0.xsd
+          http://www.imsglobal.org/xsd/imsbasiclti_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imsbasiclti_v1p0.xsd
+          http://www.imsglobal.org/xsd/imslticm_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticm_v1p0.xsd
+          http://www.imsglobal.org/xsd/imslticp_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticp_v1p0.xsd">
+          <blti:title>Other Name</blti:title>
+          <blti:description>Description</blti:description>
+          <blti:launch_url>#{url}</blti:launch_url>
+          <cartridge_bundle identifierref="BLTI001_Bundle"/>
+          <cartridge_icon identifierref="BLTI001_Icon"/>
+      </cartridge_basiclti_link>
+    XML
+    lti = CC::Importer::BLTIConverter.new
+    expect(lti.convert_blti_xml(xml)[:url]).to eq(url)
+  end
+
   context 'whitespace stripping' do
     let(:config_xml) do
      <<-XML
