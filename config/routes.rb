638e32444227bc7247e7e42e304319c2027039f1
James Williams <jamesw@instructure.com>
2017-11-22 08:01:59 -0700

    use api endpoint for enrollment deletion
    avoids domain redirection
    
    test plan:
    * should still be able to remove enrollments
     on a user's account details page
     (/accounts/X/users/Y)
    
    closes #CNVS-40422
    
    Change-Id: I1d14c4bc79bbe93597c5a7f093beec3d1d89e64c
    Reviewed-on: https://gerrit.instructure.com/133573
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Dan Minkevitch <dan@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- config/routes.rb
+++ config/routes.rb
@@ -989,7 +989,7 @@ CanvasRails::Application.routes.draw do
 
       put 'courses/:course_id/enrollments/:id/reactivate', :action => :reactivate, :as => 'reactivate_enrollment'
 
-      delete 'courses/:course_id/enrollments/:id', action: :destroy
+      delete 'courses/:course_id/enrollments/:id', action: :destroy, :as => "destroy_enrollment"
     end
 
     scope(controller: :terms_api) do
