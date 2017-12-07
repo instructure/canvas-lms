01e189fc15d2ab207ed56924fd3af627987650c2
Clay Diffrient <cdiffrient@instructure.com>
2017-12-07 11:31:15 -0700

    Add cypress for e2e tests
    closes CORE-723
    
    Change-Id: I2d81241edb058259274f681d0615c705dd73f863
    Reviewed-on: https://gerrit.instructure.com/134937
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- packages/canvas-rce/src/tinymce-a11y-checker/build.sh
+++ packages/canvas-rce/src/tinymce-a11y-checker/build.sh
@@ -5,6 +5,10 @@ docker run tinymce-a11y-checker npm test
 unit_status=$?
 ((unit_status)) && echo "[!] Failed unit tests."
 
+docker run tinymce-a11y-checker npm run cypress:run
+cypress_status=$?
+((cypress_status)) && echo "[!] Failed Cypress E2E tests."
+
 docker run tinymce-a11y-checker npm run fmt-check
 fmt_status=$?
 ((fmt_status)) && echo "[!] Failed format check. Be sure to run: npm run fmt"
---
+++ packages/canvas-rce/src/tinymce-a11y-checker/cypress/run.js
@@ -0,0 +1,17 @@
+const cypress = require("cypress")
+const Webpack = require("webpack")
+const WebpackDevServer = require("webpack-dev-server/lib/Server")
+const webpackConfig = require("../webpack.config")
+
+const compiler = Webpack(webpackConfig)
+
+const server = new WebpackDevServer(compiler, webpackConfig.devServer)
+
+server.listen(8080, "127.0.0.1", () => {
+  const config = {
+    videoRecording: false
+  }
+  cypress.run({ config }).then(results => {
+    return server.close()
+  })
+})
