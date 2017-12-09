c3f2d6888fda8b6f7afe474e4702668939e4e45c
Cody Cutrer <cody@instructure.com>
2017-12-09 08:58:04 -0700

    use a UNION, not an OR for wiki page content tag restriction
    Change-Id: I2e9a67f6ff73d57f29171d1ed49b7ee981062a1b
    Reviewed-on: https://gerrit.instructure.com/135160
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -332,8 +332,9 @@ class Course < ActiveRecord::Base
       where("assignments.id is null or submissions.user_id = ?", user.id).
       select(:id)
 
-    tags.where("content_tags.content_type <> 'WikiPage' or
-      content_tags.content_id in (?)", path_visible_pages)
+    tags = tags.where.not(content_type: 'WikiPage')
+    tags = tags.union(ContentTag.where(content_type: 'WikiPage', content_id: path_visible_pages))
+    tags
   end
 
   def sequential_module_item_ids
