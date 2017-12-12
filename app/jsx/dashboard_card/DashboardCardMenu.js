2d9e021d90848d22210c7828288e7ff4c05e6888
Ryan Shaw <ryan@instructure.com>
2017-12-11 23:41:47 -0700

    [instui 3->4] upgrade dashboard_card
    Closes: CORE-749
    
    this is the result of running these code mods:
    http://instructure.github.io/instructure-ui/#ui-codemods
    against these files to upgrade them from instui 3 to 4.
    
    Test plan:
    * if this passes Jenkins, you can probably merge this as-is
    * but first have the devs on your team give it a code-review
    * and have QA manually click around and make sure these
      pages/components still work
    * if it doesn’t pass Jenkins. You, or someone on the team that “owns”
      this part of the code needs to check out this commit and push new
      patchsets until it does pass. Please do so ASAP
    
    Change-Id: Ib25f5f20e036d1ce0f66d9edc6419831f71e9d8a
    Reviewed-on: https://gerrit.instructure.com/135353
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/dashboard_card/DashboardCardMenu.js
+++ app/jsx/dashboard_card/DashboardCardMenu.js
@@ -19,8 +19,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import I18n from 'i18n!dashcards'
-import Popover, {PopoverTrigger, PopoverContent} from 'instructure-ui/lib/components/Popover'
-import TabList, {TabPanel} from 'instructure-ui/lib/components/TabList'
+import Popover, {PopoverTrigger, PopoverContent} from '@instructure/ui-core/lib/components/Popover'
+import TabList, {TabPanel} from '@instructure/ui-core/lib/components/TabList'
 import ColorPicker from 'jsx/shared/ColorPicker'
 import DashboardCardMovementMenu from './DashboardCardMovementMenu'
 
