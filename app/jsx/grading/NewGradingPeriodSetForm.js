05f3d6ea0aa1b2561ea09268cb2c3b4ed3aaa978
Jeremy Neander <jneander@instructure.com>
2017-12-15 12:00:39 -0600

    [instui 3->4] upgrade grading
    closes CORE-756
    
    test plan:
     * QA as part of GRADE-726
    
    Change-Id: Ief7d14a3aa7ab52d092a53c8eee669c74a41ee40
    Reviewed-on: https://gerrit.instructure.com/135837
    Reviewed-by: Shahbaz Javeed <sjaveed@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Tested-by: Jenkins
    QA-Review: Indira Pai <ipai@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

--- app/jsx/grading/NewGradingPeriodSetForm.js
+++ app/jsx/grading/NewGradingPeriodSetForm.js
@@ -18,8 +18,8 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import Button from 'instructure-ui/lib/components/Button'
-import Checkbox from 'instructure-ui/lib/components/Checkbox'
+import Button from '@instructure/ui-core/lib/components/Button'
+import Checkbox from '@instructure/ui-core/lib/components/Checkbox'
 import I18n from 'i18n!grading_periods'
 import setsApi from 'compiled/api/gradingPeriodSetsApi'
 import EnrollmentTermInput from '../grading/EnrollmentTermInput'
