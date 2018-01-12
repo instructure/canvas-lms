a33231cf0abef49f3befd9706c2aae3370c8b616
Ryan Shaw <ryan@instructure.com>
2018-01-12 10:25:40 -0700

    let jest know about some of our aliases
    …and prevent a giant error from being logged from ui-themable
    
    
    test plan:
    * the “Add Ability To Add External RSS Feeds…” commit that is based
      on top of this should pass jest
    * you should not see an errors like this when jest runs:
    console.error node_modules/@instructure/ui-themeable/lib/registry.js:342
          TypeError: Cannot read property 'porcelain' of undefined
    
    
    Change-Id: I8a9583a3d16394a919a223cedfd577aef7f70ec1
    Reviewed-on: https://gerrit.instructure.com/137778
    Product-Review: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   moduleNameMapper: {
     '^i18n!(.*$)': '<rootDir>/jest/i18nTransformer.js',
-    '^compiled/(.*)$': '<rootDir>/app/coffeescripts/$1'
+    '^compiled/(.*)$': '<rootDir>/app/coffeescripts/$1',
+    '^jsx/(.*)$': '<rootDir>/app/jsx/$1'
   },
   roots: ['app/jsx'],
-  moduleDirectories: ['node_modules', 'public/javascripts'],
+  moduleDirectories: [
+    'node_modules',
+    'public/javascripts',
+    'public/javascripts/vendor'
+  ],
+  setupFiles: [
+    'jest-localstorage-mock',
+    '<rootDir>/jest/jest-setup.js'
+  ],
   transform: {
     '^i18n': '<rootDir>/jest/i18nTransformer.js',
     '^.+\\.jsx?$': 'babel-jest'
-  },
-  setupFiles: ['jest-localstorage-mock']
+  }
 }
