dd7b7d71bea9fe626c03c76ed970a9eaf3156d0b
Michael Brewer-Davis <mbd@instructure.com>
2018-01-17 16:59:48 -0600

    add fields to outcomes reports
    closes OUT-1573
    
    adds
    - section name/id/sis id
    - assignment url
    - outcome friendly name/points possible/mastery score/mastered
    - account id/name
    to Student Competency report and Outcome Results report
    
    Test plan:
    Create assignments in courses
    and verify that the generated report is correct, order is correct,
    and the report includes the values above.
    
    In particular, test
    - assignments with aligned rubrics
    - quizzes with aligned question banks (each question should appear
      on the results report; nothing will appear in the competency report)
    - assignments that have not been submitted (should appear on competency
      report, but not on results report)
    - assignments aligned to multiple outcomes
    - students in multiple sections
    - students with multiple logins
    - courses in multiple accounts (records should appear for
      courses in an account and its subaccounts)
    
    Change-Id: I6bbbe9f9d6d02b305079ea143c302e0fd8dfd359
    Reviewed-on: https://gerrit.instructure.com/138578
    Reviewed-by: Matt Berns <mberns@instructure.com>
    QA-Review: Augusto Callejas <acallejas@instructure.com>
    Tested-by: Jenkins
    QA-Review: Andrew Porter <hporter-c@instructure.com>
    Product-Review: Sidharth Oberoi <soberoi@instructure.com>

--- gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
+++ gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
@@ -30,24 +30,8 @@ module AccountReports
       include_deleted_objects
     end
 
-    # retrieve the list of students for all active courses
-    # for each student, iterate through all applicable assignments
-    # for each assignment, find the submission, then iterate through all
-    #   outcome alignments and find the outcome result
-    # for each student-assignment-outcome pairing, generate a row
-    #   based on the found outcome result
-    # each row should include:
-    # - student name
-    # - student id
-    # - assignment title
-    # - assignment id
-    # - submission date
-    # - assignment score
-    # - learning outcome name
-    # - learning outcome id
-    # - outcome result score
-    def student_assignment_outcome_map
-      headers = {
+    def self.student_assignment_outcome_headers
+       {
         'student name' => I18n.t('student name'),
         'student id' => I18n.t('student id'),
         'student sis id' => I18n.t('student sis id'),
@@ -65,14 +49,27 @@ module AccountReports
         'section name' => I18n.t('section name'),
         'section id' => I18n.t('section id'),
         'section sis id' => I18n.t('section sis id'),
-        'assignment url' => I18n.t('assignment url')
+        'assignment url' => I18n.t('assignment url'),
+        'learning outcome friendly name' => I18n.t('learning outcome friendly name'),
+        'learning outcome points possible' => I18n.t('learning outcome points possible'),
+        'learning outcome mastery score' => I18n.t('learning outcome mastery score'),
+        'learning outcome mastered' => I18n.t('learning outcome mastered'),
+        'learning outcome rating' => I18n.t('learning outcome rating'),
+        'learning outcome rating points' => I18n.t('learning outcome rating points'),
+        'account id' => I18n.t('account id'),
+        'account name' => I18n.t('account name'),
+        'enrollment state' => I18n.t('enrollment state')
       }
-
-      write_outcomes_report(headers, student_assignment_outcome_map_scope)
     end
 
-    def outcome_results
-      headers = {
+    # returns rows for each assignment that is linked to an outcome,
+    # whether or not it has been submitted or graded
+    def student_assignment_outcome_map
+      write_outcomes_report(self.class.student_assignment_outcome_headers, student_assignment_outcome_map_scope)
+    end
+
+    def self.outcome_result_headers
+      {
         'student name' => I18n.t('student name'),
         'student id' => I18n.t('student id'),
         'student sis id' => I18n.t('student sis id'),
@@ -89,15 +86,31 @@ module AccountReports
         'assessment question id' => I18n.t('assessment question id'),
         'course name' => I18n.t('course name'),
         'course id' => I18n.t('course id'),
-        'course sis id' => I18n.t('course sis id')
+        'course sis id' => I18n.t('course sis id'),
+        'section name' => I18n.t('section name'),
+        'section id' => I18n.t('section id'),
+        'section sis id' => I18n.t('section sis id'),
+        'assignment url' => I18n.t('assignment url'),
+        'learning outcome friendly name' => I18n.t('learning outcome friendly name'),
+        'learning outcome points possible' => I18n.t('learning outcome points possible'),
+        'learning outcome mastery score' => I18n.t('learning outcome mastery score'),
+        'learning outcome mastered' => I18n.t('learning outcome mastered'),
+        'learning outcome rating' => I18n.t('learning outcome rating'),
+        'learning outcome rating points' => I18n.t('learning outcome rating points'),
+        'account id' => I18n.t('account id'),
+        'account name' => I18n.t('account name'),
+        'enrollment state' => I18n.t('enrollment state')
       }
-      write_outcomes_report(headers, outcome_results_scope)
+    end
+
+    # returns rows for each assessed outcome result (or question result)
+    def outcome_results
+      write_outcomes_report(self.class.outcome_result_headers, outcome_results_scope)
     end
 
     private
 
     def student_assignment_outcome_map_scope
-
       parameters = {
         :account_id => account.id,
         :root_account_id => root_account.id
@@ -114,28 +127,36 @@ module AccountReports
           sub.score              AS "submission score",
           lo.short_description   AS "learning outcome name",
           lo.id                  AS "learning outcome id",
+          lo.display_name        AS "learning outcome friendly name",
+          lo.data                AS "learning outcome data",
           r.attempt              AS "attempt",
           r.score                AS "outcome score",
+          r.possible             AS "learning outcome points possible",
+          r.mastery              AS "learning outcome mastered",
           c.name                 AS "course name",
           c.id                   AS "course id",
           c.sis_source_id        AS "course sis id",
           s.name                 AS "section name",
           s.id                   AS "section id",
           s.sis_source_id        AS "section sis id",
+          e.workflow_state       AS "enrollment state",
           lo.context_id          AS "outcome context id",
-          lo.context_type        AS "outcome context type"}).
+          lo.context_type        AS "outcome context type",
+          acct.id                AS "account id",
+          acct.name              AS "account name"}).
         joins(Pseudonym.send(:sanitize_sql, ["
           INNER JOIN #{User.quoted_table_name} u ON pseudonyms.user_id = u.id
           INNER JOIN (
-            SELECT user_id, course_id, course_section_id
+            SELECT user_id, course_id, course_section_id, workflow_state
             FROM #{Enrollment.quoted_table_name}
             WHERE type = 'StudentEnrollment'
             AND root_account_id = :root_account_id
-           " + (@include_deleted ? "" : "AND workflow_state = 'active'") + "
-            GROUP BY user_id, course_id, course_section_id
+           " + (@include_deleted ? "" : "AND workflow_state <> 'deleted'") + "
+            GROUP BY user_id, course_id, course_section_id, workflow_state
           ) e ON pseudonyms.user_id = e.user_id
           INNER JOIN #{Course.quoted_table_name} c ON c.id = e.course_id
             AND c.root_account_id = :root_account_id
+          INNER JOIN #{Account.quoted_table_name} acct ON acct.id = c.account_id
           INNER JOIN #{CourseSection.quoted_table_name} s ON s.id = e.course_section_id
           INNER JOIN #{Assignment.quoted_table_name} a ON (a.context_id = c.id
                                        AND a.context_type = 'Course')
@@ -150,9 +171,10 @@ module AccountReports
             LEFT JOIN #{LearningOutcomeResult.quoted_table_name} r ON (r.user_id=pseudonyms.user_id
               AND r.content_tag_id = ct.id)
             LEFT JOIN #{Submission.quoted_table_name} sub ON sub.assignment_id = a.id
-            AND sub.user_id = pseudonyms.user_id AND sub.workflow_state <> 'deleted'", parameters])).
-              where("ct.tag_type = 'learning_outcome' AND ct.workflow_state <> 'deleted'
-                AND (r.id IS NULL OR (r.artifact_type IS NOT NULL AND r.artifact_type <> 'Submission'))")
+              AND sub.user_id = pseudonyms.user_id AND sub.workflow_state <> 'deleted'
+              AND sub.workflow_state <> 'unsubmitted'", parameters])).
+          where("ct.tag_type = 'learning_outcome' AND ct.workflow_state <> 'deleted'
+            AND (r.id IS NULL OR (r.artifact_type IS NOT NULL AND r.artifact_type <> 'Submission'))")
 
       unless @include_deleted
         students = students.where("pseudonyms.workflow_state<>'deleted' AND c.workflow_state='available'")
@@ -165,9 +187,11 @@ module AccountReports
     def outcome_results_scope
       students = account.learning_outcome_links.active.
         select(<<~SELECT).
+          distinct on (#{outcome_order}, p.id, s.id, r.id, qr.id, q.id, a.id, subs.id, qs.id, aq.id)
           u.sortable_name                             AS "student name",
           p.user_id                                   AS "student id",
           p.sis_user_id                               AS "student sis id",
+          a.id                                        AS "assignment id",
           COALESCE(q.title, a.title)                  AS "assessment title",
           COALESCE(q.id, a.id)                        AS "assessment id",
           COALESCE(qs.finished_at, subs.submitted_at) AS "submission date",
@@ -176,6 +200,10 @@ module AccountReports
           aq.id                                       AS "assessment question id",
           learning_outcomes.short_description         AS "learning outcome name",
           learning_outcomes.id                        AS "learning outcome id",
+          learning_outcomes.display_name              AS "learning outcome friendly name",
+          COALESCE(qr.possible, r.possible)           AS "learning outcome points possible",
+          COALESCE(qr.mastery, r.mastery)             AS "learning outcome mastered",
+          learning_outcomes.data                      AS "learning outcome data",
           COALESCE(qr.attempt, r.attempt)             AS "attempt",
           COALESCE(qr.score, r.score)                 AS "outcome score",
           c.name                                      AS "course name",
@@ -183,7 +211,13 @@ module AccountReports
           c.sis_source_id                             AS "course sis id",
           CASE WHEN r.association_type IN ('Quiz', 'Quizzes::Quiz') THEN 'quiz'
                WHEN ct.content_type = 'Assignment' THEN 'assignment'
-          END                                         AS "assessment type"
+          END                                         AS "assessment type",
+          s.name                                      AS "section name",
+          s.id                                        AS "section id",
+          s.sis_source_id                             AS "section sis id",
+          e.workflow_state                            AS "enrollment state",
+          acct.id                                     AS "account id",
+          acct.name                                   AS "account name"
         SELECT
         joins(<<~JOINS).
           INNER JOIN #{LearningOutcome.quoted_table_name} ON content_tags.content_id = learning_outcomes.id
@@ -193,13 +227,18 @@ module AccountReports
           INNER JOIN #{User.quoted_table_name} u ON u.id = r.user_id
           INNER JOIN #{Pseudonym.quoted_table_name} p on p.user_id = r.user_id
           INNER JOIN #{Course.quoted_table_name} c ON r.context_id = c.id
+          INNER JOIN #{Account.quoted_table_name} acct ON acct.id = c.account_id
+          INNER JOIN #{Enrollment.quoted_table_name} e ON e.type = 'StudentEnrollment' and e.root_account_id = #{account.root_account.id}
+            AND e.user_id = p.user_id AND e.course_id = c.id
+            #{@include_deleted ? '' : "AND e.workflow_state <> 'deleted'"}
+          INNER JOIN #{CourseSection.quoted_table_name} s ON e.course_section_id = s.id
           LEFT OUTER JOIN #{LearningOutcomeQuestionResult.quoted_table_name} qr on qr.learning_outcome_result_id = r.id
           LEFT OUTER JOIN #{Quizzes::Quiz.quoted_table_name} q ON q.id = r.association_id
            AND r.association_type IN ('Quiz', 'Quizzes::Quiz')
-          LEFT OUTER JOIN #{Assignment.quoted_table_name} a ON a.id = ct.content_id
-           AND ct.content_type = 'Assignment'
+          LEFT OUTER JOIN #{Assignment.quoted_table_name} a ON (a.id = ct.content_id
+           AND ct.content_type = 'Assignment') OR a.id = q.assignment_id
           LEFT OUTER JOIN #{Submission.quoted_table_name} subs ON subs.assignment_id = a.id
-           AND subs.user_id = u.id AND subs.workflow_state <> 'deleted'
+           AND subs.user_id = u.id AND subs.workflow_state <> 'deleted' AND subs.workflow_state <> 'unsubmitted'
           LEFT OUTER JOIN #{Quizzes::QuizSubmission.quoted_table_name} qs ON r.artifact_id = qs.id
            AND r.artifact_type IN ('QuizSubmission', 'Quizzes::QuizSubmission')
           LEFT OUTER JOIN #{AssessmentQuestion.quoted_table_name} aq ON aq.id = qr.associated_asset_id
@@ -211,7 +250,7 @@ module AccountReports
        students = students.where("p.workflow_state<>'deleted' AND c.workflow_state='available'")
       end
 
-      students = add_course_sub_account_scope(students, 'c')
+      students = join_course_sub_account_scope(account, students, 'c')
       students = add_term_scope(students, 'c')
       students.order(outcome_order)
     end
@@ -230,9 +269,22 @@ module AccountReports
         add_extra_text(I18n.t('account_reports.outcomes.order',
                               "Order: %{order}", order: select.first))
       else
-        order = ('u.id, learning_outcomes.id, c.id')
+        order = 'u.id, learning_outcomes.id, c.id'
       end
       order
+    end
+
+    def join_course_sub_account_scope(account, scope, table = 'courses')
+      if account != account.root_account
+        scope.joins(<<~JOINS)
+          join #{CourseAccountAssociation.quoted_table_name} caa
+            ON caa.account_id = #{account.id}
+            AND caa.course_id = #{table}.id
+            AND caa.course_section_id IS NULL
+        JOINS
+      else
+        scope
+      end
     end
 
     def write_outcomes_report(headers, scope)
@@ -245,14 +297,35 @@ module AccountReports
         Shackles.activate(:master) { AccountReport.where(id: @account_report.id).update_all(total_lines: total) }
         scope.each do |row|
           row = row.attributes.dup
+
           row['assignment url'] = "https://#{host}"
           row['assignment url'] << "/courses/#{row['course id']}"
           row['assignment url'] << "/assignments/#{row['assignment id']}"
           row['submission date'] = default_timezone_format(row['submission date'])
-
+          add_outcomes_data(row)
           csv << header_keys.map { |h| row[h] }
         end
         csv << ['No outcomes found'] if total == 0
+      end
+    end
+
+    def add_outcomes_data(row)
+      row['learning outcome mastered'] = unless row['learning outcome mastered'].nil?
+                                           row['learning outcome mastered'] ? 1 : 0
+                                         end
+      outcome_data = if row['learning outcome data'].present?
+                       YAML.safe_load(row['learning outcome data'])[:rubric_criterion]
+                     else
+                       {}
+                     end
+      row['learning outcome mastery score'] = outcome_data[:mastery_points]
+
+      score = row['outcome score']
+      if score.present? && row['assessment_type'] != 'quiz'
+        ratings = outcome_data[:ratings]&.sort_by { |r| r[:points] }&.reverse || []
+        rating = ratings.detect { |r| r[:points] <= score } || {}
+        row['learning outcome rating'] = rating[:description]
+        row['learning outcome rating points'] = rating[:points]
       end
     end
   end
