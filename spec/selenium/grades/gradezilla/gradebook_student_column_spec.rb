528c5712861bf30124a49e15f572e78193ac1831
Anju Reddy <areddy@instructure.com>
2018-01-19 12:18:19 -0700

    spec: flaky spec; database query did not guarantee ordering
    Change-Id: Iec1f8fddecb11f2b071ac5697bfd0d461d05860c
    Reviewed-on: https://gerrit.instructure.com/138486
    Tested-by: Jenkins
    Reviewed-by: KC Naegle <knaegle@instructure.com>
    Product-Review: Anju Reddy <areddy@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>

--- spec/selenium/grades/gradezilla/gradebook_student_column_spec.rb
+++ spec/selenium/grades/gradezilla/gradebook_student_column_spec.rb
@@ -43,17 +43,18 @@ describe "Student column header options" do
   context "Display as" do
     before(:each) do
       Gradezilla.visit(@course)
+      @students = @course.students.sort_by{|x| x[:id]}
     end
 
     it "displays student names as First Last", priority: "1", test_id: 3253319 do
       Gradezilla.click_student_menu_display_as('First,Last')
-      expect(Gradezilla.fetch_student_names[0]).to eq(@course.students[0].name)
+      expect(Gradezilla.fetch_student_names[0]).to eq(@students[0].name)
     end
 
     it "displays student names as Last,First", priority: "2", test_id: 3253320 do
       Gradezilla.click_student_menu_display_as('Last,First')
 
-      student_name = @course.students[0].last_name + ", " + @course.students[0].first_name
+      student_name = @students[0].last_name + ", " + @students[0].first_name
       expect(Gradezilla.fetch_student_names[0]).to eq(student_name)
     end
 
@@ -61,7 +62,7 @@ describe "Student column header options" do
       Gradezilla.click_student_menu_display_as('Last,First')
       Gradezilla.visit(@course)
 
-      student_name = @course.students[0].last_name + ", " + @course.students[0].first_name
+      student_name = @students[0].last_name + ", " + @students[0].first_name
       expect(Gradezilla.fetch_student_names[0]).to eq(student_name)
     end
   end
