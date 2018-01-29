eaee7fb3fb1106c935fe354e11c6197eb7dbbc40
Ryan Hurst <captainhurst@gmail.com>
2018-01-29 10:00:37 -0700

    WIP - implementing strongmind gradebook in canvas

--- app/jsx/bundles/strongmind_gradebook.js
+++ app/jsx/bundles/strongmind_gradebook.js
@@ -1,4 +1,4 @@
-import '../strongmind_gradebook/build/static/js/main.d13f13c5.js'
-import '../strongmind_gradebook/build/static/css/main.76673efb.css'
+import '../strongmind_gradebook/build/static/js/main.ff9429e0.js'
+import '../strongmind_gradebook/build/static/css/main.bbdbacfe.css'
 
 console.log("Hello Strongmind!")
