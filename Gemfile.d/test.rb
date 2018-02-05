cc20e55f9c5a42009171cbc23541aecf5f770c62
Simon Williams <simon@instructure.com>
2018-02-05 09:46:18 -0700

    bump some test gems
    Change-Id: I0c80a1de350031996a4de4f52d1b874d2cd4651d
    Reviewed-on: https://gerrit.instructure.com/139862
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -26,7 +26,7 @@ group :test do
   gem 'simplecov', '0.15.1', require: false
     gem 'docile', '1.1.5', require: false
   gem 'simplecov-rcov', '0.2.3', require: false
-  gem 'puma', '3.11.0'
+  gem 'puma', '3.11.2'
 
   gem 'rspec', '3.7.0'
   gem 'rspec_around_all', '0.2.0'
@@ -37,7 +37,7 @@ group :test do
   gem 'rubocop-canvas', require: false, path: 'gems/rubocop-canvas'
     gem 'rubocop', '0.52.1', require: false
       gem 'rainbow', '3.0.0', require: false
-  gem 'rubocop-rspec', '1.21.0', require: false
+  gem 'rubocop-rspec', '1.22.2', require: false
 
   gem 'once-ler', '0.1.2'
 
@@ -50,7 +50,7 @@ group :test do
   gem 'test-queue', github: 'instructure/test-queue', ref: '1b92ebbca70705599c78a1bad5b16d6a37f741f2', require: false
   gem 'testrailtagging', '0.3.8.3', require: false
 
-  gem 'webmock', '3.1.1', require: false
+  gem 'webmock', '3.3.0', require: false
     gem 'crack', '0.4.3', require: false
   gem 'timecop', '0.9.1'
   gem 'jira_ref_parser', '1.0.1'
