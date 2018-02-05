2125349e5350fb8b8bc0985d4842dc002061e141
Simon Williams <simon@instructure.com>
2018-02-05 09:37:32 -0700

    bump chunky_png
    Change-Id: Ie4b96d5cc439458b423ce4e6da961bfe3f8bcd7e
    Reviewed-on: https://gerrit.instructure.com/139861
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -53,7 +53,7 @@ gem "aws-sdk-sns", '1.1.0', require: false
 gem "aws-sdk-sqs", '1.3.0', require: false
 gem 'barby', '0.6.5', require: false
   gem 'rqrcode', '0.10.1', require: false
-  gem 'chunky_png', '1.3.8', require: false
+  gem 'chunky_png', '1.3.10', require: false
 gem 'bcrypt', '3.1.11'
 gem 'canvas_connect', '0.3.11'
   gem 'adobe_connect', '1.0.6', require: false
