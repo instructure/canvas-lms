1de24c57dfe2bf877262d66614414fe374f67a6f
Clay Diffrient <cdiffrient@instructure.com>
2018-02-05 15:54:03 -0700

    Make a lock icon appear for locked files
    When a locked file gets requested with an image accept header
    we send back an image of a lock.  Technically we should probably
    send back a 401, but this preserves existing behavior and makes
    things line up with the /preview endpoint.
    
    closes CORE-582
    
    Test Plan:
      - Lock an image file
      - Use the RCS sidebar to add it to a page in a course
      - Look at that page as a student
      - You should see a lock icon
      - Unlock the file
      - Look at that page as student again and you should see the image
    
    Change-Id: Ia1a45b70bc429c276fd432e78a1693d30c0168bd
    Reviewed-on: https://gerrit.instructure.com/139940
    Tested-by: Jenkins
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- spec/javascripts/jsx/behaviors/brokenImagesSpec.js
+++ spec/javascripts/jsx/behaviors/brokenImagesSpec.js
@@ -14,21 +14,58 @@
 //
 // You should have received a copy of the GNU Affero General Public License along
 // with this program. If not, see <http://www.gnu.org/licenses/>.
-import $ from 'jquery';
-import attachErrorHandler from 'compiled/behaviors/broken-images';
+import $ from 'jquery'
+import attachErrorHandler from 'compiled/behaviors/broken-images'
+import {setTimeout} from 'timers'
 
+let server
 QUnit.module('Broken Images Behavior', {
-  setup () {
-    $('#fixtures').html('<img id="borked" src="broken_image.jpg">');
+  setup() {
+    $('#fixtures').html('<img id="borked" src="broken_image.jpg" alt="broken">')
   },
-  teardown () {
-    $('#fixtures').empty();
+  teardown() {
+    $('#fixtures').empty()
   }
-});
+})
 
-test('on error handler attaches the proper class to images when called', () => {
-  const $imgElement = $('#borked');
-  attachErrorHandler($imgElement);
-  $imgElement.triggerHandler('error');
-  ok($imgElement.hasClass('broken-image'));
+test('on error handler attaches the proper class to images when they are broken and not locked', assert => {
+  const done = assert.async()
+  const $imgElement = $('#borked')
+  attachErrorHandler($imgElement)
+  $imgElement.triggerHandler('error')
+  setTimeout(() => {
+    ok($imgElement.hasClass('broken-image'))
+    done()
+  }, 100)
+})
+
+test('on error handler changes src when the image is locked', assert => {
+  const done = assert.async()
+  server = sinon.fakeServer.create()
+  server.respondWith('GET', '/broken_image.jpg', [403, {}, 'Forbidden'])
+  const $imgElement = $('#borked')
+  attachErrorHandler($imgElement)
+  $imgElement.triggerHandler('error')
+
+  setTimeout(() => {
+    server.respond()
+    equal($imgElement.attr('src'), '/images/svg-icons/icon_lock.svg')
+    server.restore()
+    done()
+  }, 100)
+})
+
+test('on error handler sets appropriate alt text indicating the image is locked', assert => {
+  const done = assert.async()
+  server = sinon.fakeServer.create()
+  server.respondWith('GET', '/broken_image.jpg', [403, {}, 'Forbidden'])
+  const $imgElement = $('#borked')
+  attachErrorHandler($imgElement)
+  $imgElement.triggerHandler('error')
+  setTimeout(() => {
+    server.respond()
+    equal($imgElement.attr('alt'), 'Locked image')
+    server.restore()
+    done()
+  }, 100)
 })
