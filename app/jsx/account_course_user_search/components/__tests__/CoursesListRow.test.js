38bf009c88293a93f4bd287d74fc0df242ef06e2
Ryan Shaw <ryan@instructure.com>
2018-02-07 14:44:36 -0700

    Indicate if a course is a blueprint course
    Closes: CORE-983
    
    Test plan:
    * enable “blueprint courses” feature
    * go to /accounts/self
    * you should see a ‘Blueprint course” tooltip if a course is a blueprint
      course on the table row next to the name
    
    Change-Id: I855d85340e7bf039250482a9e3d9f50ac6c05734
    Reviewed-on: https://gerrit.instructure.com/140365
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

---
+++ app/jsx/account_course_user_search/components/__tests__/CoursesListRow.test.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {shallow} from 'enzyme'
+import CoursesListRow from '../CoursesListRow'
+
+const props = {
+  id: '1',
+  name: 'A',
+  sis_course_id: 'SIS 1',
+  workflow_state: 'alive',
+  total_students: 6,
+  teachers: [
+    {
+      id: '1',
+      display_name: 'Testing Teacher'
+    }
+  ],
+  term: {
+    name: 'A Term'
+  }
+}
+
+it('indicates if a course is a blueprint course', () => {
+  const tooltip = 'Tooltip[tip="This is a blueprint course"] IconBlueprintLine'
+
+  expect(
+    shallow(<CoursesListRow {...props} />)
+      .find(tooltip)
+      .exists()
+  ).toBeFalsy()
+
+  expect(
+    shallow(<CoursesListRow {...props} blueprint />)
+      .find(tooltip)
+      .exists()
+  ).toBeTruthy()
+})
