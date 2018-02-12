debfae0249b79479de81ef750f55dcdec130829d
Derek Bender <djbender@instructure.com>
2018-02-12 15:25:59 -0600

    add not null constraint to metadata workflow_state
    make backfill nulls on score_metadata synchronous
    
    closes: GRADE-845, GRADE-839
    
    test plan for synchronous backfill: migrations work
    
    test plan for not null constraint:
    1. in a rails console create a score object:
    
        course = Course.create!
        user = User.create!
        enrollment = StudentEnrollment.create!(course: course, user: user)
        score = enrollment.scores.create!
    
    2. creating a metadata with a null workflow_state persists 'active'
    instead:
    
        metadata = score.create_score_metadata!(
          calculation_details: {foo: :bar},
          workflow_state: nil
        )
        metadata.workflow_state # => 'active'
    
    3. using a direct write method with a null workflow_state throws an
    error: `ActiveRecord::StatementInvalid: PG::NotNullViolation: ERROR:  null
    value in column "workflow_state" violates not-null constraint`:
    
        metadata.update_attribute(:workflow_state, nil)
    
    Change-Id: Ie6c75e074c860a0b2c928c0cbb627dfe6b389f16
    Reviewed-on: https://gerrit.instructure.com/140683
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>
    Tested-by: Jenkins

--- db/migrate/20180207061345_add_workflow_state_null_contraint_and_index_to_score_metadata.rb
+++ db/migrate/20180207061345_add_workflow_state_null_contraint_and_index_to_score_metadata.rb
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2018 - present Instructure, Inc.
 #
 # This file is part of Canvas.
@@ -14,12 +13,16 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
-class BackfillWorkflowStateNullsOnScoreMetadata < ActiveRecord::Migration[5.0]
+class AddWorkflowStateNullContraintAndIndexToScoreMetadata < ActiveRecord::Migration[5.0]
   tag :postdeploy
   disable_ddl_transaction!
 
-  def up
-    DataFixup::BackfillNulls.run ScoreMetadata, :workflow_state, default_value: :active
+  def change
+    # prime the database cache so that the entire table is
+    # in memory by using  the index on id
+    ScoreMetadata.count
+    change_column_null :score_metadata, :workflow_state, false
   end
 end
