9920baa791646fc2b130dc0e49b01cf314212eda
KC Naegle <knaegle@instructure.com>
2018-02-13 14:21:03 -0700

    spec: discussion pom
    Change-Id: Ide656485b68e25ead845b9f41c4aedd5793d6b43
    Reviewed-on: https://gerrit.instructure.com/140864
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: KC Naegle <knaegle@instructure.com>
    QA-Review: KC Naegle <knaegle@instructure.com>

---
+++ spec/selenium/discussions/pages/discussions_new_edit_page.rb
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2017 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../../common'
+
+class DiscussionNewEdit
+  class << self
+    include SeleniumDependencies
+
+    # ---------------------- Page ----------------------
+    def visit(course)
+      get("/courses/#{course.id}/discussion_topics/new")
+    end
+
+    def set_section_specific_discussion_flag(course, state)
+      course.account.set_feature_flag! :section_specific_discussions, state
+    end
+
+    def new_discussion_url
+      '/discussion_topics/new'
+    end
+
+    def individual_discussion_url(discussion)
+      "/discussion_topics/#{discussion.id}"
+    end
+
+    # ---------------------- Controls ----------------------
+    def section_autocomplete_css
+      "#sections_autocomplete_root input[type='text']"
+    end
+
+    def submit_discussion_form
+      submit_form('.form-actions')
+    end
+
+    def add_message(message)
+      type_in_tiny('textarea[name=message]', message)
+    end
+
+    def add_title(title)
+      replace_content(f('input[name=title]'), title)
+    end
+
+    def section_error
+      f('#sections_autocomplete_root').text
+    end
+
+    def select_a_section(section_name)
+      fj(section_autocomplete_css).click
+      if !section_name.empty?
+        set_value(fj(section_autocomplete_css), section_name)
+        driver.action.send_keys(:enter).perform
+      else
+        driver.action.send_keys(:backspace).perform
+      end
+      wait_for_ajax_requests
+    end
+  end
+end
