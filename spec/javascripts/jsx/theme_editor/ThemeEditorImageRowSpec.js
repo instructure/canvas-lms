7495cccc81f91f27d5d42f93b59289af37d1bf3f
Clay Diffrient <cdiffrient@instructure.com>
2018-02-16 14:17:48 -0700

    Convert ThemeEditorImageRow to ES class syntax
    closes CORE-1022
    
    Test Plan:
      - Theme editor works without the refactor feature flag
      - Automated tests pass
    
    Change-Id: If9a91ace9a6d5290affe96f86f23e46841eae63f
    Reviewed-on: https://gerrit.instructure.com/141264
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Tested-by: Jenkins
    QA-Review: Brent Burgoyne <bburgoyne@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- spec/javascripts/jsx/theme_editor/ThemeEditorImageRowSpec.js
+++ spec/javascripts/jsx/theme_editor/ThemeEditorImageRowSpec.js
@@ -16,91 +16,86 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'react-dom',
-  'jsx/theme_editor/ThemeEditorImageRow'
-], (React, ReactDOM, ThemeEditorImageRow) => {
+define(
+  ['react', 'react-dom', 'jsx/theme_editor/ThemeEditorImageRow'],
+  (React, ReactDOM, ThemeEditorImageRow) => {
+    let elem, props
 
-  let elem, props
-
-  QUnit.module('ThemeEditorImageRow Component', {
-    setup () {
-      elem = document.createElement('div')
-      props = {
-        varDef: {},
-        onChange: sinon.spy()
+    QUnit.module('ThemeEditorImageRow Component', {
+      setup() {
+        elem = document.createElement('div')
+        props = {
+          varDef: {},
+          onChange: sinon.spy()
+        }
       }
-    }
-  })
+    })
 
-  test('renders with human name heading', () => {
-    const expected = 'Human'
-    props.varDef.human_name = expected
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    const subject = elem.getElementsByTagName('h4')[0]
-    equal(subject.textContent, expected, 'renders human name')
-  })
+    test('renders with human name heading', () => {
+      const expected = 'Human'
+      props.varDef.human_name = expected
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      const subject = elem.getElementsByTagName('h4')[0]
+      equal(subject.textContent, expected, 'renders human name')
+    })
 
-  test('renders with helper text', () => {
-    const expected = 'Halp!'
-    props.varDef.helper_text = expected
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    const subject = elem.getElementsByClassName('Theme__editor-upload_restrictions')[0]
-    equal(subject.textContent, expected, 'renders helper text')
-  })
+    test('renders with helper text', () => {
+      const expected = 'Halp!'
+      props.varDef.helper_text = expected
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      const subject = elem.getElementsByClassName('Theme__editor-upload_restrictions')[0]
+      equal(subject.textContent, expected, 'renders helper text')
+    })
 
-  test('renders image with placeholder', () => {
-    const expected = 'image.png'
-    props.placeholder = expected
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    const subject = elem.getElementsByTagName('img')[0]
-    equal(subject.src.split('/').pop(), expected, 'has placeholder for src')
-  })
+    test('renders image with placeholder', () => {
+      const expected = 'image.png'
+      props.placeholder = expected
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      const subject = elem.getElementsByTagName('img')[0]
+      equal(subject.src.split('/').pop(), expected, 'has placeholder for src')
+    })
 
-  test('renders image with user input val', () => {
-    const expected = 'image.png'
-    props.placeholder = 'other.png'
-    props.userInput = {val: expected}
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    const subject = elem.getElementsByTagName('img')[0]
-    equal(subject.src.split('/').pop(), expected, 'has userInput.val for src')
-  })
+    test('renders image with user input val', () => {
+      const expected = 'image.png'
+      props.placeholder = 'other.png'
+      props.userInput = {val: expected}
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      const subject = elem.getElementsByTagName('img')[0]
+      equal(subject.src.split('/').pop(), expected, 'has userInput.val for src')
+    })
 
-  test('setValue clears file input and calls onChange when arg is null', () => {
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    var subject = component.refs.fileInput.getDOMNode()
-    subject.setAttribute('type', 'text')
-    subject.value = 'foo'
-    component.setValue(null)
-    equal(subject.value, '', 'file upload field is empty')
-    ok(props.onChange.calledWith(null), 'onChange called with null')
-  })
+    test('setValue clears file input and calls onChange when arg is null', () => {
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      var subject = component.fileInput
+      subject.setAttribute('type', 'text')
+      subject.value = 'foo'
+      component.setValue(null)
+      equal(subject.value, '', 'file upload field is empty')
+      ok(props.onChange.calledWith(null), 'onChange called with null')
+    })
 
-  test('setValue clears file input and calls onChange when arg is empty string', () => {
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    var subject = component.refs.fileInput.getDOMNode()
-    subject.setAttribute('type', 'text')
-    subject.value = 'foo'
-    component.setValue('')
-    equal(subject.value, '', 'file upload field is empty')
-    ok(props.onChange.calledWith(''), 'onChange called with empty string')
-  })
+    test('setValue clears file input and calls onChange when arg is empty string', () => {
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      var subject = component.fileInput
+      subject.setAttribute('type', 'text')
+      subject.value = 'foo'
+      component.setValue('')
+      equal(subject.value, '', 'file upload field is empty')
+      ok(props.onChange.calledWith(''), 'onChange called with empty string')
+    })
 
-  test('setValue calls onChange with blob url of input file', () => {
-    const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
-    const blob = new Blob(['foo'], {type: 'text/plain'})
-    // URL.createObjectURL returns different URLS when called twice with the
-    // same blob.
-    const originalCreateObjectURL = window.URL.createObjectURL
-    const expected = {}
-    sinon.stub(window.URL, 'createObjectURL').returns(expected)
-    component.setValue({files: [blob]})
-    ok(props.onChange.calledWith(expected), 'onChange called with blob url')
-    ok(
-      window.URL.createObjectURL.calledWith(blob),
-      'object url created for file'
-    )
-    window.URL.createObjectURL.restore()
-  })
-})
+    test('setValue calls onChange with blob url of input file', () => {
+      const component = ReactDOM.render(<ThemeEditorImageRow {...props} />, elem)
+      const blob = new Blob(['foo'], {type: 'text/plain'})
+      // URL.createObjectURL returns different URLS when called twice with the
+      // same blob.
+      const originalCreateObjectURL = window.URL.createObjectURL
+      const expected = {}
+      sinon.stub(window.URL, 'createObjectURL').returns(expected)
+      component.setValue({files: [blob]})
+      ok(props.onChange.calledWith(expected), 'onChange called with blob url')
+      ok(window.URL.createObjectURL.calledWith(blob), 'object url created for file')
+      window.URL.createObjectURL.restore()
+    })
+  }
+)
