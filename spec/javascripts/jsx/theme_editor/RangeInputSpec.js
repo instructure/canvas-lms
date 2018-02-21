8b85f80b558af430f7012232bca6c775b0277c22
Clay Diffrient <cdiffrient@instructure.com>
2018-02-21 11:47:56 -0700

    Convert RangeInput to ES class syntax
    closes CORE-1030
    
    Test Plan:
      - Range inputs on the theme editor work without the refactor
        feature flag being enabled
    
    Change-Id: I2001b90ff7600ce0e067deb95d9f346068eb4bf3
    Reviewed-on: https://gerrit.instructure.com/141520
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Tested-by: Jenkins
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- spec/javascripts/jsx/theme_editor/RangeInputSpec.js
+++ spec/javascripts/jsx/theme_editor/RangeInputSpec.js
@@ -16,65 +16,55 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'react-dom',
-  'jsx/theme_editor/RangeInput',
-], (React, ReactDOM, RangeInput) => {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RangeInput from 'jsx/theme_editor/RangeInput'
 
-  let elem, props
+let elem, props
 
-  QUnit.module('RangeInput Component', {
-    setup () {
-      elem = document.createElement('div')
-      props = {
-        min: 1,
-        max: 10,
-        defaultValue: 5,
-        labelText: 'Input Label',
-        name: 'input_name',
-        formatValue: sinon.stub(),
-        onChange: sinon.stub()
-      }
+QUnit.module('RangeInput Component', {
+  setup() {
+    elem = document.createElement('div')
+    props = {
+      min: 1,
+      max: 10,
+      defaultValue: 5,
+      labelText: 'Input Label',
+      name: 'input_name',
+      formatValue: sinon.stub(),
+      onChange: sinon.stub()
     }
-  })
+  }
+})
 
-  test('renders range input', () => {
-    const component = ReactDOM.render(<RangeInput {...props} />, elem)
-    const input = component.refs.rangeInput.getDOMNode()
-    equal(input.type, 'range', 'renders range input')
-    equal(input.value, props.defaultValue, 'renders default value')
-    equal(input.name, props.name, 'renders with name from props')
-  })
+test('renders range input', () => {
+  const component = ReactDOM.render(<RangeInput {...props} />, elem)
+  const input = component.rangeInput
+  equal(input.type, 'range', 'renders range input')
+  equal(input.value, props.defaultValue, 'renders default value')
+  equal(input.name, props.name, 'renders with name from props')
+})
 
-  test('renders formatted output', assert => {
-    const start = assert.async()
-    const component = ReactDOM.render(<RangeInput {...props} />, elem)
-    const expected = 47
-    const expectedFormatted = '47%'
-    props.formatValue.returns(expectedFormatted)
-    component.setState({value: 47}, () => {
-      const output = component.getDOMNode().querySelector('output')
-      ok(output, 'renders the output element')
-      ok(props.formatValue.calledWith(expected), 'formats the value')
-      equal(output.textContent, expectedFormatted, 'outputs value')
-      start()
-    })
-  })
-
-  test('handleChange', () => {
-    const component = ReactDOM.render(<RangeInput {...props} />, elem)
-    sinon.spy(component, 'setState')
-    const event = {target: {value: 8}}
-    component.handleChange(event)
-    ok(
-      component.setState.calledWithMatch({value: event.target.value}),
-      'updates value in state'
-    )
-    ok(
-      props.onChange.calledWith(event.target.value),
-      'calls onChange with the new value'
-    )
+test('renders formatted output', assert => {
+  const start = assert.async()
+  const component = ReactDOM.render(<RangeInput {...props} />, elem)
+  const expected = 47
+  const expectedFormatted = '47%'
+  props.formatValue.returns(expectedFormatted)
+  component.setState({value: 47}, () => {
+    const output = component.outputElement
+    ok(output, 'renders the output element')
+    ok(props.formatValue.calledWith(expected), 'formats the value')
+    equal(output.textContent, expectedFormatted, 'outputs value')
+    start()
   })
 })
 
+test('handleChange', () => {
+  const component = ReactDOM.render(<RangeInput {...props} />, elem)
+  sinon.spy(component, 'setState')
+  const event = {target: {value: 8}}
+  component.handleChange(event)
+  ok(component.setState.calledWithMatch({value: event.target.value}), 'updates value in state')
+  ok(props.onChange.calledWith(event.target.value), 'calls onChange with the new value')
+})
