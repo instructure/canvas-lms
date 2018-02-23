583f549caf74bb6eef511f2711bda3fca1eccbf6
Clay Diffrient <cdiffrient@instructure.com>
2018-02-23 09:48:44 -0700

    Convert ThemeEditorFileUpload to ES class syntax
    closes CORE-1048
    
    Test Plan:
      - Enable custom css/js
      - Turn the refactor feature flag OFF
      - Go to the Upload tab in theme editor
      - Uploading files works
    
    Change-Id: Ib8b3ee756eb45fae921fdb87ec5817f2e8bf8472
    Reviewed-on: https://gerrit.instructure.com/141740
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- app/jsx/theme_editor/ThemeEditorFileUpload.js
+++ app/jsx/theme_editor/ThemeEditorFileUpload.js
@@ -16,146 +16,137 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import customTypes from './PropTypes'
 import I18n from 'i18n!theme_editor'
+import customTypes from './PropTypes'
 
-export default React.createClass({
+export default class ThemeEditorFileUpload extends Component {
+  static propTypes = {
+    label: PropTypes.string,
+    accept: PropTypes.string,
+    name: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    currentValue: PropTypes.string,
+    userInput: customTypes.userVariableInput
+  }
 
-    displayName: 'ThemeEditorFileUpload',
+  static defaultProps = {
+    userInput: {}
+  }
 
-    propTypes: {
-      label: PropTypes.string,
-      accept: PropTypes.string,
-      name: PropTypes.string,
-      onChange: PropTypes.func.isRequired,
-      currentValue: PropTypes.string,
-      userInput: customTypes.userVariableInput
-    },
+  state = {
+    selectedFileName: ''
+  }
 
-    getDefaultProps(){
-      return ({
-        userInput: {}
-      })
-    },
+  hasSomethingToReset = () => {
+    return this.props.userInput.val || this.props.userInput.val === '' || this.props.currentValue
+  }
 
-    getInitialState() {
-      return {
-        selectedFileName: ''
-      }
-    },
+  hasUserInput = () => {
+    // null means the userInput has been reverted so ignore it
+    return this.props.userInput.val !== null && this.props.userInput.val !== undefined
+  }
 
-    hasSomethingToReset() {
-      return this.props.userInput.val || this.props.userInput.val === '' || this.props.currentValue;
-    },
+  handleFileChanged = event => {
+    var file = event.target.files[0]
+    this.setState({selectedFileName: file.name})
+    this.props.onChange(window.URL.createObjectURL(file))
+  }
 
-    hasUserInput() {
-      // null means the userInput has been reverted so ignore it
-      return this.props.userInput.val !== null && this.props.userInput.val !== undefined
-    },
+  handleResetClicked() {
+    // if they hit the "Reset" button,
+    // we want to also clear out the value of the <input type=file>
+    // but we don't want to mess with its value otherwise
+    this.fileInput.value = ''
+    this.setState({selectedFileName: ''})
+    this.props.onChange(!this.hasUserInput() ? '' : null)
+  }
 
-    handleFileChanged(event) {
-      var file = event.target.files[0]
-      this.setState({ selectedFileName: file.name })
-      this.props.onChange(window.URL.createObjectURL(file))
-    },
-
-    handleResetClicked() {
-      // if they hit the "Reset" button,
-      // we want to also clear out the value of the <input type=file>
-      // but we don't want to mess with its value otherwise
-      this.refs.fileInput.getDOMNode().value = ''
-      this.setState({ selectedFileName: '' })
-      this.props.onChange(!this.hasUserInput() ? '' : null)
-    },
-
-    displayValue() {
-      if (!this.hasUserInput() && this.props.currentValue) {
-        return this.props.currentValue
-      } else if (this.props.userInput.val) {
-        return this.state.selectedFileName
-      } else { // no saved value and no unsaved value
-        return ''
-      }
-    },
-
-    resetButtonLabel() {
-      if (this.props.userInput.val || this.props.userInput.val === '') {
-        return (
-          <span>
-            <i className="icon-reset" aria-hidden="true" />
-            <span className="screenreader-only">
-              { I18n.t('Undo') }
-            </span>
-          </span>
-        )
-      } else if (this.props.currentValue) {
-        return (
-          <span>
-            <i className="icon-x" aria-hidden="true" />
-            <span className="screenreader-only">
-              { I18n.t('Clear') }
-            </span>
-          </span>
-        )
-      } else {
-        return (
-          <span className="screenreader-only">
-            { I18n.t('Reset') }
-          </span>
-        )
-      }
-    },
-
-   viewFileLink() {
-      return (!this.hasUserInput() && this.props.currentValue) ? (
-        <a href={this.props.currentValue} target="_blank" className="ThemeEditorFileUpload__view-file">
-          { I18n.t('View File') }
-        </a>
-      ) : null;
-    },
-
-    render() {
-      return (
-        <div className="ThemeEditorFileUpload">
-          <div className="ThemeEditorFileUpload__label ic-Label">
-            {this.props.label} {this.viewFileLink()}
-          </div>
-          <div className="ic-Input-group">
-            <button
-              disabled={!this.hasSomethingToReset()}
-              type="button"
-              className="ic-Input-group__add-on Button ThemeEditorFileUpload__reset-button"
-              onClick={this.handleResetClicked}
-            >
-              { this.resetButtonLabel() }
-            </button>
-            <input
-              type="hidden"
-              name={!this.props.userInput.val && this.props.name}
-              value={(this.props.userInput.val === '') ? '' : this.props.currentValue}
-            />
-            <label className="ThemeEditorFileUpload__file-chooser">
-              <div className="screenreader-only">
-                { this.props.label }
-              </div>
-              <input
-                type="file"
-                name={this.props.userInput.val && this.props.name}
-                accept={this.props.accept}
-                onChange={this.handleFileChanged}
-                ref="fileInput"
-              />
-              <div className="ThemeEditorFileUpload__fake-input uneditable-input ic-Input" aria-hidden="true">
-                { this.displayValue() }
-              </div>
-              <div className="Button" aria-hidden="true">
-                { I18n.t('Select') }
-              </div>
-            </label>
-          </div>
-        </div>
-      )
+  displayValue() {
+    if (!this.hasUserInput() && this.props.currentValue) {
+      return this.props.currentValue
+    } else if (this.props.userInput.val) {
+      return this.state.selectedFileName
+    } else {
+      // no saved value and no unsaved value
+      return ''
     }
-  })
+  }
+
+  resetButtonLabel() {
+    if (this.props.userInput.val || this.props.userInput.val === '') {
+      return (
+        <span>
+          <i className="icon-reset" aria-hidden="true" />
+          <span className="screenreader-only">{I18n.t('Undo')}</span>
+        </span>
+      )
+    } else if (this.props.currentValue) {
+      return (
+        <span>
+          <i className="icon-x" aria-hidden="true" />
+          <span className="screenreader-only">{I18n.t('Clear')}</span>
+        </span>
+      )
+    } else {
+      return <span className="screenreader-only">{I18n.t('Reset')}</span>
+    }
+  }
+
+  viewFileLink() {
+    return !this.hasUserInput() && this.props.currentValue ? (
+      <a
+        href={this.props.currentValue}
+        target="_blank"
+        className="ThemeEditorFileUpload__view-file"
+      >
+        {I18n.t('View File')}
+      </a>
+    ) : null
+  }
+
+  render() {
+    return (
+      <div className="ThemeEditorFileUpload">
+        <div className="ThemeEditorFileUpload__label ic-Label">
+          {this.props.label} {this.viewFileLink()}
+        </div>
+        <div className="ic-Input-group">
+          <button
+            disabled={!this.hasSomethingToReset()}
+            type="button"
+            className="ic-Input-group__add-on Button ThemeEditorFileUpload__reset-button"
+            onClick={this.handleResetClicked}
+          >
+            {this.resetButtonLabel()}
+          </button>
+          <input
+            type="hidden"
+            name={!this.props.userInput.val && this.props.name}
+            value={this.props.userInput.val === '' ? '' : this.props.currentValue}
+          />
+          <label className="ThemeEditorFileUpload__file-chooser">
+            <div className="screenreader-only">{this.props.label}</div>
+            <input
+              type="file"
+              name={this.props.userInput.val && this.props.name}
+              accept={this.props.accept}
+              onChange={this.handleFileChanged}
+              ref={c => (this.fileInput = c)}
+            />
+            <div
+              className="ThemeEditorFileUpload__fake-input uneditable-input ic-Input"
+              aria-hidden="true"
+            >
+              {this.displayValue()}
+            </div>
+            <div className="Button" aria-hidden="true">
+              {I18n.t('Select')}
+            </div>
+          </label>
+        </div>
+      </div>
+    )
+  }
+}
