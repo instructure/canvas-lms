79889c66c508a7b417c8cf87a39bd12b7e625eb4
Derek Bender <djbender@instructure.com>
2018-03-01 16:37:09 -0600

    add anonymous moderated marking feature flag
    this should not be visible available in prod
    
    closes: GRADE-874
    
    test plan:
    - go to the feature flags for an account
    - the feature Anonymous Moderated Marking should be present
    - it should be marked as in development
    - it should be marked as in beta
    
    Change-Id: Iafa329d9f28e0588f718cac68251a314b9bdc645
    Reviewed-on: https://gerrit.instructure.com/142383
    Reviewed-by: Shahbaz Javeed <sjaveed@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Gentry Beckmann <gbeckmann@instructure.com>
    QA-Review: Gentry Beckmann <gbeckmann@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

---
+++ lib/features/anonymous_moderated_marking.rb
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+Feature.register(anonymous_moderated_marking: {
+  display_name: -> { I18n.t('Anonymous Moderated Marking') },
+  description:  -> {
+    I18n.t <<~DESCRIPTION
+      Anonymous Moderated Marking allows teachers to select moderators to anonymously grade assignments. After which a moderator can select or supply a custom grade.
+    DESCRIPTION
+  },
+  applies_to: 'Account',
+  state: 'hidden',
+  root_opt_in: true,
+  beta: true,
+  development: true
+})
