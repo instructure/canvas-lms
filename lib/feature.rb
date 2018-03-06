11a46b8fb3436a03ece60363757edec2dac57006
Cameron Matheson <cameron@instructure.com>
2018-03-06 13:41:28 -0700

    [ci selinimum capture] Revert "re-add and disable graphql feature flag"
    This reverts commit 02cb94cdc1595b9209700a44c6c623b0addfd635.
    
    Change-Id: Ifcb20d6036822f4ef8c562a4036c40b89a2af5bc
    Reviewed-on: https://gerrit.instructure.com/142760
    Tested-by: Jenkins
    Reviewed-by: Jonathan Featherstone <jfeatherstone@instructure.com>
    QA-Review: Collin Parrish <cparrish@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

--- lib/feature.rb
+++ lib/feature.rb
@@ -590,14 +590,6 @@ END
       beta: true,
       development: false
     },
-    'graphql' =>
-    {
-      display_name: -> { I18n.t("GraphQL API") },
-      description: -> { I18n.t("EXPERIMENTAL GraphQL API.") },
-      applies_to: "RootAccount",
-      state: "hidden_in_prod",
-      beta: true,
-    },
     'rubric_criterion_range' =>
     {
       display_name: -> { I18n.t('Rubric Criterion Range') },
