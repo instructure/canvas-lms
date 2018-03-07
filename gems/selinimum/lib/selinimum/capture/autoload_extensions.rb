01cdb978e890503fe54b7640f75902e972bbd0e9
Cameron Matheson <cameron@instructure.com>
2018-03-06 23:53:47 -0700

    "properly" "handle" graphql types in selinimum
    closes ???
    
    Change-Id: I0f249e4fd38099615d2a2affdbf791cc2365cf3a
    Reviewed-on: https://gerrit.instructure.com/142829
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jonathan Featherstone <jfeatherstone@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

--- gems/selinimum/lib/selinimum/capture/autoload_extensions.rb
+++ gems/selinimum/lib/selinimum/capture/autoload_extensions.rb
@@ -31,6 +31,12 @@ module Selinimum
         # irrelevant to selinimization
         return super unless relative_path
 
+        # autoloaded files from graphql contain a GraphQL::ObjectType instead
+        # of a ruby class/module which really confuses selinimum so we'll skip
+        # those
+        # NOTE: remove this after we switch to the class-based api
+        return super if relative_path =~ %r|app/graphql|
+
         # typically we'll have a const_path (e.g. `FooBar`), unless we
         # got here from a call to `require_dependency`
         const_names = const_path ? [const_path] : loadable_constants_for_path(file_path)
