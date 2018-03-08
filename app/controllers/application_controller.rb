c86911acf8823977449924d4f81008c78ebbe5ab
Rob Orton <rob@instructure.com>
2018-03-07 17:10:39 -0700

    allow creating users when they have permission to
    fixes CORE-861
    
    test plan
     - as an admin with manage logins on root_account
       and not see user list on sub_account it should
       show the + sign to create a user
    
    Change-Id: I6bc34b9b3d2602642513951cafd79e71b0450fdc
    Reviewed-on: https://gerrit.instructure.com/142927
    Tested-by: Jenkins
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    QA-Review: Tucker McKnight <tmcknight@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2356,7 +2356,7 @@ class ApplicationController < ActionController::Base
         can_read_course_list: can_read_course_list,
         can_read_roster: can_read_roster,
         can_create_courses: @account.grants_right?(@current_user, session, :manage_courses),
-        can_create_users: @account.root_account? && @account.grants_right?(@current_user, session, :manage_user_logins),
+        can_create_users: @account.root_account.grants_right?(@current_user, session, :manage_user_logins),
         analytics: @account.service_enabled?(:analytics),
         can_masquerade: @account.grants_right?(@current_user, session, :become_user),
         can_message_users: @account.grants_right?(@current_user, session, :send_messages),
