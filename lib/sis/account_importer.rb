3e611c87dee7625d01c6673ab9834e437a7c597a
Rob Orton <rob@instructure.com>
2018-03-10 18:02:11 -0700

    don’t allow deleting dirty sub_accounts
    fixes CORE-1128
    
    test plan
     - it should raise an error if sub_account has
       active accounts or courses
    
    Change-Id: I77d4a0d45adb3b7f230c9ea0f320fba2bc11f48e
    Reviewed-on: https://gerrit.instructure.com/143230
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- lib/sis/account_importer.rb
+++ lib/sis/account_importer.rb
@@ -86,6 +86,8 @@ module SIS
           if status =~ /active/i
             account.workflow_state = 'active'
           elsif status =~ /deleted/i
+            raise ImportError, "Cannot delete the sub_account with ID: #{account_id} because it has active sub accounts." if account.sub_accounts.active.exists?
+            raise ImportError, "Cannot delete the sub_account with ID: #{account_id} because it has active courses." if account.courses.active.exists?
             account.workflow_state = 'deleted'
           end
         end
