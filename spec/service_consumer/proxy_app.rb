a9a89c39936cb47c7bea356bcf7349096a02d4f8
Robert Lamb <rlamb@instructure.com>
2018-03-12 13:24:45 -0600

    spec: WIP - add pact provider files
    Add pact provider files in Canvas base project following
    Pact search conventions where pact_helper.rb is located in
    canvas-lms/spec/service-consumer/
    
    Add copyright to files
    
    proxy.app doesn't work yet - unable to create token
    
    Change-Id: I505a6165650ba1d89fcf054441e172e584a39305
    Reviewed-on: https://gerrit.instructure.com/143296
    Tested-by: Jenkins
    Reviewed-by: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: Robert Lamb <rlamb@instructure.com>
    QA-Review: Robert Lamb <rlamb@instructure.com>

---
+++ spec/service_consumer/proxy_app.rb
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ENV["RAILS_ENV"] = 'test'
+
+class ProxyApp
+  def initialize
+    require ::File.expand_path('../../../config/environment', __FILE__)
+    @real_provider_app = Rails.application
+  end
+
+  def call env
+    # modify request (env) here
+    # See http://www.rubydoc.info/github/rack/rack/file/SPEC for contents of the ENV
+    root_user = Account.all.select{|a| a.root_account? }.first.users.first
+    api_key = User.find(1).access_tokens.create!
+    env["HTTP_AUTHORIZATION"] = "Bearer #{api_key.full_token}" if env.include?("HTTP_AUTHORIZATION")
+    response = @real_provider_app.call(env)
+    response
+  end
+end
