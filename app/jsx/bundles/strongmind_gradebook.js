82babd4d390e4d4a45bafd7e0abb2ebb908f3c15
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2018-03-20 09:46:36 -0700

    update gradebook app

--- app/jsx/bundles/strongmind_gradebook.js
+++ app/jsx/bundles/strongmind_gradebook.js
@@ -1,2 +1,2 @@
-import '../strongmind_gradebook/main.aeeee24b.js'
-import '../strongmind_gradebook/main.58e48290.css'
+import '../strongmind_gradebook/main.03b2a855.js'
+import '../strongmind_gradebook/main.51c2dbf2.css'
