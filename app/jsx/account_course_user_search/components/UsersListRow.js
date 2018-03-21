a885ec251800d2f1ee93b0f0edb39e1a9dff1207
Ryan Shaw <ryan@instructure.com>
2018-03-20 23:45:41 -0600

    Fix Act As and Send Message icons not centered
    … in focus indication
    
    Fixes: CORE-1110	
    
    Test plan: 
    Should look like this: https://cl.ly/220O2s3c0621
    
    Change-Id: I9af950d71d1816582da95b5d6afc35f589a6a8ef
    Reviewed-on: https://gerrit.instructure.com/144303
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/account_course_user_search/components/UsersListRow.js
+++ app/jsx/account_course_user_search/components/UsersListRow.js
@@ -46,7 +46,7 @@ export default function UsersListRow({accountId, user, permissions, handlers}) {
         {permissions.can_masquerade && (
           <Tooltip tip={I18n.t('Act as %{name}', {name: user.name})}>
             <Button variant="icon" size="small" href={`/users/${user.id}/masquerade`}>
-              <IconMasqueradeLine title={I18n.t('Act as %{name}', {name: user.name})} />
+              <IconMasqueradeLine height="1.5em" title={I18n.t('Act as %{name}', {name: user.name})} />
             </Button>
           </Tooltip>
         )}
@@ -57,7 +57,7 @@ export default function UsersListRow({accountId, user, permissions, handlers}) {
               size="small"
               href={`/conversations?user_name=${user.name}&user_id=${user.id}`}
             >
-              <IconMessageLine title={I18n.t('Send message to %{name}', {name: user.name})} />
+              <IconMessageLine height="1.5em" title={I18n.t('Send message to %{name}', {name: user.name})} />
             </Button>
           </Tooltip>
         )}
