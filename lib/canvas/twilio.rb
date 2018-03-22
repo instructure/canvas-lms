f359f871b12115ca65de06a67afdc2715fa4973d
Simon Williams <simon@instructure.com>
2018-03-22 09:44:08 -0600

    bump twilio-ruby
    Change-Id: I96025376f71751bef2ce4bfc10b2f05ddb5979da
    Reviewed-on: https://gerrit.instructure.com/144472
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins
    Product-Review: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>

--- lib/canvas/twilio.rb
+++ lib/canvas/twilio.rb
@@ -16,6 +16,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 # Utilities to send text messages via Twilio.
+require 'twilio-ruby'
+
 module Canvas::Twilio
   DEFAULT_COUNTRY = 'US'
 
