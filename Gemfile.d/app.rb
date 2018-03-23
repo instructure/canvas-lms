a1c0fb33c8d1a05a3b43d938dc572f383d7546ba
Cody Cutrer <cody@instructure.com>
2018-03-23 09:56:45 -0600

    bump imperium
    Change-Id: If7167f9f102cdd8d2c5b3e205004f0f79b04bf09
    Reviewed-on: https://gerrit.instructure.com/144619
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -116,7 +116,7 @@ gem 'canvas_statsd', '2.0.4'
   gem 'statsd-ruby', '1.4.0', require: false
   gem 'aroi', '0.0.5', require: false
 gem 'gepub', '0.7.0beta4'
-gem 'imperium', '0.2.4', require: false
+gem 'imperium', '0.3.0', require: false
 gem 'academic_benchmarks', '0.0.10', require: false
 
 gem 'graphql', '1.7.12'
