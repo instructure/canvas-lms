cec890474919f3cfd54264945676dae0a565a2b1
Rob Orton <rob@instructure.com>
2018-03-26 14:54:16 -0600

    fix group_category importer on refactored sis
    refs CORE-929
    
    test plan
     - an import with group_categories should not fail
    
    Change-Id: If341872e3cdb4feb39275f75862d013a793fe95e
    Reviewed-on: https://gerrit.instructure.com/144878
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- lib/sis/csv/group_category_importer.rb
+++ lib/sis/csv/group_category_importer.rb
@@ -30,9 +30,9 @@ module SIS
 
       # expected columns
       # group_category_id, account_id, name, status
-      def process(csv)
+      def process(csv, index=nil, count=nil)
         count = SIS::GroupCategoryImporter.new(@root_account, importer_opts).process do |importer|
-          csv_rows(csv) do |row|
+          csv_rows(csv, index, count) do |row|
             update_progress
             begin
               importer.add_group_category(row['group_category_id'], row['account_id'],
