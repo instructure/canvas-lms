03c89408cb5e1e9aee9d2496653e9db128aaae07
jfederico <jesus@123it.ca>
2018-04-02 19:54:22 +0000

    BBB enable Arc integration
    Summary:
    Extension for BigBluebutton recordings ready post-processing
    that enables the integration with Arc.
    
    Description:
    The only update is to the big_blue_button model for adding a new meta
    parameter that includes the user email in RFC2822 format. This parameter
    is required in order to match the user accounts between Canvas and Arc.
    
    Test Plan:
    As everything happens in the BigBlueButton recording server, the only
    thing to test here is that the parameter is included and correctly sent.
    
    Development ID:
    blindsidenetworks#22
    
    Internally ID:
    Closes COMMS-604
    
    Change-Id: I330746525013b846cbd79ee1c32626e6cc40408d
    Reviewed-on: https://gerrit.instructure.com/146018
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    QA-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Product-Review: Matt Goodwin <mattg@instructure.com>

--- spec/models/big_blue_button_conference_spec.rb
+++ spec/models/big_blue_button_conference_spec.rb
@@ -30,11 +30,13 @@ describe BigBlueButtonConference do
           :secret_dec => "secret",
         })
       ])
+      @course = course_factory
       user_with_communication_channel
+      @course.enroll_teacher(@user).accept
       @conference = BigBlueButtonConference.create!(
         :title => "my conference",
         :user => @user,
-        :context => course_factory
+        :context => @course
       )
     end
 
@@ -80,6 +82,10 @@ describe BigBlueButtonConference do
       @conference.initiate_conference
       expect(@conference).to be_active
       expect(@conference.craft_url(@user)).to match(/\Ahttps:\/\/bbb\.instructure\.com\/bigbluebutton\/api\/join/)
+    end
+
+    it "should have a well formed user string as for recording_user_ready" do
+      expect(@conference.recording_ready_user).to eq "#{@user['name']} <#{@user.email}>"
     end
 
     it "return nil if a request times out" do
