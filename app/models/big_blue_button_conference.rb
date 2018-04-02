90bf8dcef1c12d99edf761953c63112c377cb650
jfederico <jesus@123it.ca>
2018-04-02 19:54:22 +0000

    BBB enable Arc integration
    Summary:
    Extension for BigBluebutton recordings ready post-processing
    that enables the integration with Arc.
    
    Description:
    The only update is to the big_blue_button model for adding a new meta
    parameter that includes the user email in RFC2822 format. This parameter
    is required in order to match the user accounts between Canvas and Arc.
    
    Test Plan:
    As everything happens in the BigBlueButton recording server, the only
    thing to test here is that the parameter is included and correctly sent.
    
    Development ID:
    blindsidenetworks#22
    
    Internally ID:
    Closes COMMS-604
    
    Change-Id: I330746525013b846cbd79ee1c32626e6cc40408d
    Reviewed-on: https://gerrit.instructure.com/146018
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    QA-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Product-Review: Matt Goodwin <mattg@instructure.com>

--- app/models/big_blue_button_conference.rb
+++ app/models/big_blue_button_conference.rb
@@ -55,11 +55,18 @@ class BigBlueButtonConference < WebConference
       :logoutURL => (settings[:default_return_url] || "http://www.instructure.com"),
       :record => settings[:record] ? "true" : "false",
       :welcome => settings[:record] ? t("This conference may be recorded.") : "",
+      "meta_canvas-recording-ready-user" => recording_ready_user,
       "meta_canvas-recording-ready-url" => recording_ready_url(current_host)
     }) or return nil
     @conference_active = true
     save
     conference_key
+  end
+
+  def recording_ready_user
+    if self.course.instructor_enrollments.active_or_pending.where(user_id: self.user).exists?
+      "#{self.user['name']} <#{self.user.email}>"
+    end
   end
 
   def recording_ready_url(current_host = nil)
