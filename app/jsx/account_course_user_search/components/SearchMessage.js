c55ff773d7f181dd779603e74ae38571abaf61e7
Clay Diffrient <cdiffrient@instructure.com>
2018-04-02 15:43:57 -0600

    Make screenreaders alert when search results are loaded
    closes CORE-1116
    closes CORE-1117
    
    Test Plan:
      - Enable a screenreader
      - Go to the new account level user/course search
      - Search for something
      - The screenreader should indicate to you when results have finished loading
      - This should work for Users and Courses
    
    Change-Id: I4e81a86fb9e9340d3acbdab3e8f28fcbfb42d2cd
    Reviewed-on: https://gerrit.instructure.com/145634
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- app/jsx/account_course_user_search/components/SearchMessage.js
+++ app/jsx/account_course_user_search/components/SearchMessage.js
@@ -19,8 +19,9 @@
 import React, { Component } from 'react'
 import Pagination, {PaginationButton} from '@instructure/ui-core/lib/components/Pagination'
 import Spinner from '@instructure/ui-core/lib/components/Spinner'
-import { array, func, string, shape } from 'prop-types'
+import { array, func, string, shape, oneOf } from 'prop-types'
 import I18n from 'i18n!account_course_user_search'
+import Alert from '@instructure/ui-alerts/lib/components/Alert'
 
 const linkPropType = shape({
   url: string.isRequired,
@@ -39,7 +40,15 @@ export default class SearchMessage extends Component {
       })
     }).isRequired,
     setPage: func.isRequired,
-    noneFoundMessage: string.isRequired
+    noneFoundMessage: string.isRequired,
+    getLiveAlertRegion: func,
+    dataType: oneOf(['Course', 'User']).isRequired
+  }
+
+  static defaultProps = {
+    getLiveAlertRegion () {
+      return document.getElementById('flash_screenreader_holder')
+    }
   }
 
   constructor (props) {
@@ -85,57 +94,89 @@ export default class SearchMessage extends Component {
     });
   }
 
+  renderSearchDoneAlert = (isLoading, message) => {
+    if (isLoading) {
+      return null;
+    }
+    return (
+      <Alert screenReaderOnly liveRegion={this.props.getLiveAlertRegion}>
+        {message}
+      </Alert>
+    );
+  };
+
 
   render () {
     const { collection, noneFoundMessage } = this.props
+    let resultsFoundMessage = ''
+    switch (this.props.dataType) {
+      case 'User':
+        resultsFoundMessage = I18n.t('User results updated.')
+        break;
+      case 'Course':
+        resultsFoundMessage = I18n.t('Course results updated.');
+        break;
+      default:
+        break;
+    }
+    const errorLoadingMessage = I18n.t('There was an error with your query; please try a different search')
 
     if (collection.error) {
       return (
         <div className="text-center pad-box">
           <div className="alert alert-error">
-            {I18n.t('There was an error with your query; please try a different search')}
+            {this.renderSearchDoneAlert(collection.loading, errorLoadingMessage)}
+            {errorLoadingMessage}
           </div>
         </div>
       )
     } else if (!collection.data.length) {
       return (
         <div className="text-center pad-box">
+          {this.renderSearchDoneAlert(collection.loading, noneFoundMessage)}
           <div className="alert alert-info">{noneFoundMessage}</div>
         </div>
       )
     } else if (collection.links) {
       return (
-        <Pagination
-          variant="compact"
-          labelNext={I18n.t('Next Page')}
-          labelPrev={I18n.t('Previous Page')}
-        >
-          {this.state.pageNumbers.map((v, i) => {
-            const pageNumber = i + 1
-            const isCurrent = (this.state.pageBecomingCurrent) ?
-                            pageNumber === this.state.pageBecomingCurrent :
-                            pageNumber === this.state.currentPage;
-            return (
-              <PaginationButton
-                key={pageNumber}
-                onClick={() => this.handleSetPage(pageNumber)}
-                current={isCurrent}
-              >
-                  { isCurrent && this.state.pageBecomingCurrent ? (
-                    <Spinner size="x-small" title={I18n.t('Loading...')} />
-                  ) : (
-                    I18n.n(pageNumber)
-                  )}
-              </PaginationButton>
-            )
-          }).concat(this.state.lastUnknown
-            ? <span key="page-count-is-unknown-indicator" aria-hidden>...</span>
-            : []
-          )}
-        </Pagination>
+        <div>
+          {this.renderSearchDoneAlert(collection.loading, resultsFoundMessage)}
+          <Pagination
+            variant="compact"
+            labelNext={I18n.t('Next Page')}
+            labelPrev={I18n.t('Previous Page')}
+          >
+            {this.state.pageNumbers.map((v, i) => {
+              const pageNumber = i + 1
+              const isCurrent = (this.state.pageBecomingCurrent) ?
+                              pageNumber === this.state.pageBecomingCurrent :
+                              pageNumber === this.state.currentPage;
+              return (
+                <PaginationButton
+                  key={pageNumber}
+                  onClick={() => this.handleSetPage(pageNumber)}
+                  current={isCurrent}
+                >
+                    { isCurrent && this.state.pageBecomingCurrent ? (
+                      <Spinner size="x-small" title={I18n.t('Loading...')} />
+                    ) : (
+                      I18n.n(pageNumber)
+                    )}
+                </PaginationButton>
+              )
+            }).concat(this.state.lastUnknown
+              ? <span key="page-count-is-unknown-indicator" aria-hidden>...</span>
+              : []
+            )}
+          </Pagination>
+        </div>
       )
     } else {
-      return <noscript />
+      return (
+        <div>
+          {this.renderSearchDoneAlert(collection.loading, resultsFoundMessage)}
+        </div>
+      )
     }
   }
 }
--- app/jsx/account_course_user_search/components/__tests__/SearchMessage.test.js
+++ app/jsx/account_course_user_search/components/__tests__/SearchMessage.test.js
@@ -16,34 +16,99 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
-import {mount} from 'enzyme'
-import SearchMessage from '../SearchMessage'
+import React from "react";
+import { mount } from "enzyme";
+import SearchMessage from "../SearchMessage";
 
-describe('Pagination Handling', () => {
-  it('shows the loading spinner on the page that is becoming current', () => {
-    const props = {
-      collection: {
-        data: [1,2,3],
-        links: {
-          current: {
-            url: 'abc',
-            page: '5'
-          },
-          last: {
-            url: 'abc10',
-            page: '10'
-          }
-        }
+const getProps = () => ({
+  collection: {
+    data: [1, 2, 3],
+    links: {
+      current: {
+        url: "abc",
+        page: "5"
       },
-      setPage: jest.fn(),
-      noneFoundMessage: 'None Found!'
+      last: {
+        url: "abc10",
+        page: "10"
+      }
     }
-    const wrapper = mount(<SearchMessage {...props} />);
-    wrapper.setProps({}) // Make sure it triggers componentWillReceiveProps
-    wrapper.instance().handleSetPage(6)
-    const buttons = wrapper.find('PaginationButton').map(x => x.text())
-    expect(buttons).toEqual(['1', '5', 'Loading...', '7', '8', '9', '10'])
-  });
-})
+  },
+  setPage: jest.fn(),
+  noneFoundMessage: "None Found!",
+  dataType: "Course"
+});
 
+let flashElements;
+beforeEach(() => {
+  flashElements = document.createElement("div");
+  flashElements.setAttribute("id", "flash_screenreader_holder");
+  flashElements.setAttribute("role", "alert");
+  document.body.appendChild(flashElements);
+});
+
+afterEach(() => {
+  document.body.removeChild(flashElements);
+});
+
+describe("Pagination Handling", () => {
+  it("shows the loading spinner on the page that is becoming current", () => {
+    const props = getProps();
+    const wrapper = mount(<SearchMessage {...props} />);
+    wrapper.setProps({}); // Make sure it triggers componentWillReceiveProps
+    wrapper.instance().handleSetPage(6);
+    const buttons = wrapper.find("PaginationButton").map(x => x.text());
+    expect(buttons).toEqual(["1", "5", "Loading...", "7", "8", "9", "10"]);
+  });
+});
+
+describe("Screenreader Alerting", () => {
+  it("renders a screenreader only alert indicating when results are updated", () => {
+    const props = getProps();
+    props.collection.loading = true;
+    const wrapper = mount(<SearchMessage {...props} />);
+    const alert = wrapper.find("Alert");
+    expect(alert.exists()).toBe(false);
+  });
+  it("renders a screenreader only alert when there are errors", () => {
+    const props = getProps();
+    props.collection.error = true;
+    const wrapper = mount(<SearchMessage {...props} />);
+    const alert = wrapper.find("Alert");
+    expect(alert.exists()).toBe(true);
+    expect(
+      document.getElementById("flash_screenreader_holder").textContent
+    ).toBe("There was an error with your query; please try a different search");
+  });
+  it("renders a screenreader only result when there are no additional pages of results", () => {
+    const props = getProps();
+    props.collection.links = undefined;
+    const wrapper = mount(<SearchMessage {...props} />);
+    const alert = wrapper.find("Alert");
+    expect(alert.exists()).toBe(true);
+    expect(
+      document.getElementById("flash_screenreader_holder").textContent
+    ).toBe("Course results updated.");
+  });
+  it("renders a screenreader only message when there are no results", () => {
+    const props = getProps();
+    props.collection.data = [];
+    const wrapper = mount(<SearchMessage {...props} />);
+    const alert = wrapper.find("Alert");
+    expect(alert.exists()).toBe(true);
+    expect(
+      document.getElementById("flash_screenreader_holder").textContent
+    ).toBe("None Found!");
+  });
+
+  it('renders the proper message when the dataType prop is "User"', () => {
+    const props = getProps();
+    props.dataType = "User";
+    const wrapper = mount(<SearchMessage {...props} />);
+    const alert = wrapper.find("Alert");
+    expect(alert.exists()).toBe(true);
+    expect(
+      document.getElementById("flash_screenreader_holder").textContent
+    ).toBe("User results updated.");
+  });
+});
