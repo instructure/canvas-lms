213cd4fac94f79c75c2d85e20189d579f9ea2b9d
Rob Orton <rob@instructure.com>
2018-04-04 15:28:17 -0600

    fix migrations names
    Change-Id: I07124c782b1e63196bd1ff5e9ebcd76067eeabe8
    Reviewed-on: https://gerrit.instructure.com/145937
    Tested-by: Jenkins
    Reviewed-by: Sean Mikkelsen <smikkelsen@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>

--- db/migrate/20180327092620_create_account_report_rows.rb
+++ db/migrate/20180327092620_create_account_report_rows.rb
@@ -16,19 +16,21 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-class CreateAccountReportRunners < ActiveRecord::Migration[5.1]
+class CreateAccountReportRows < ActiveRecord::Migration[5.1]
   tag :predeploy
 
   def change
-    create_table :account_report_runners do |t|
+    return if ActiveRecord::SchemaMigration.where(version: '20180501192616').exists?
+    create_table :account_report_rows do |t|
       t.integer :account_report_id, null: false, limit: 8
-      t.string :workflow_state, null: false, default: 'created', limit: 255
-      t.string :batch_items, array: true, default: []
-      t.timestamps
-      t.datetime :started_at
-      t.datetime :ended_at
+      t.integer :account_report_runner_id, null: false, limit: 8
+      t.integer :row_number
+      t.string :row, array: true, default: []
+      t.datetime :created_at, null: false
     end
-    add_foreign_key :account_report_runners, :account_reports
-    add_index :account_report_runners, :account_report_id
+    add_foreign_key :account_report_rows, :account_reports
+    add_foreign_key :account_report_rows, :account_report_runners
+    add_index :account_report_rows, :account_report_id
+    add_index :account_report_rows, :account_report_runner_id
   end
 end
