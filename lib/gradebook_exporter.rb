instructure/canvas-lms

commit c2532cb9b137cd41517640cdcd1c33085b610601

oops wrong column

lib/gradebook_exporter.rb
@@ -148,9 +148,11 @@ class GradebookExporter
             end
           end
           pseudonym = SisPseudonym.for(student, @course, include_root_account)
-          row = [student_name(student), student.id, student.email.blank? ? pseudonym.try(:sis_user_id) : student.email]
-          row << pseudonym.try(:sis_user_id) if include_sis_id
+          sis_user_id = nil
+          sis_user_id = pseudonym.try(:sis_user_id) if include_sis_id
           pseudonym ||= student.find_pseudonym_for_account(@course.root_account, include_root_account)
+          row = [student_name(student), student.id, student.email.blank? ? pseudonym.try(:unique_id) : student.email]
+          row << sis_user_id
           row << pseudonym.try(:unique_id)
           row << (pseudonym && HostUrl.context_host(pseudonym.account)) if include_sis_id && include_root_account
           row << student_sections

commit 317e5205fbba6cf05c8f15f1cff54c5f65dbd2ec

fix column offset bug introduced by c2532cb9b137cd41517640cdcd1c33085b610601

lib/gradebook_exporter.rb
@@ -152,7 +152,7 @@ class GradebookExporter
           sis_user_id = pseudonym.try(:sis_user_id) if include_sis_id
           pseudonym ||= student.find_pseudonym_for_account(@course.root_account, include_root_account)
           row = [student_name(student), student.id, student.email.blank? ? pseudonym.try(:unique_id) : student.email]
-          row << sis_user_id
+          row << sis_user_id if include_sis_id
           row << pseudonym.try(:unique_id)
           row << (pseudonym && HostUrl.context_host(pseudonym.account)) if include_sis_id && include_root_account
           row << student_sections