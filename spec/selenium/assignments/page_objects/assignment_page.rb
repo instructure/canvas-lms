fd7a940128d3204e7741e8a69ecabb8ac8907ab3
Anju Reddy <areddy@instructure.com>
2018-04-06 10:33:19 -0600

    spec: add spec to pick moderator for assignment
    Refs GRADE-973
    Fixes GRADE-1047
    
    Change-Id: I032ac609589d489260b60d6f087d0d71e86f0966
    Reviewed-on: https://gerrit.instructure.com/146160
    Tested-by: Jenkins
    Reviewed-by: Indira Pai <ipai@instructure.com>
    Product-Review: Anju Reddy <areddy@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>

--- spec/selenium/assignments/page_objects/assignment_page.rb
+++ spec/selenium/assignments/page_objects/assignment_page.rb
@@ -15,15 +15,40 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-class Assignment
-  include SeleniumDependencies
+class AssignmentPage
+  class << self
+    include SeleniumDependencies
 
-  def visit_as_student(course, assignment)
-    get "/courses/#{course}/assignments/#{assignment}"
+    def visit_as_student(course, assignment)
+      get "/courses/#{course}/assignments/#{assignment}"
+    end
+
+    def visit_assignment_edit_page(course, assignment)
+      get "/courses/#{course}/assignments/#{assignment}/edit"
+    end
+
+    def submission_detail_link
+      fj("a:contains('Submission Details')")
+    end
+
+    def select_grader_dropdown
+      f("select[name='grader-dropdown']")
+    end
+
+    def moderate_checkbox
+      f("input[type=checkbox][name='moderated_grading']")
+    end
+
+    def filter_grader(grader_name)
+      fj("option:contains(\"#{grader_name}\")")
+    end
+
+    def select_moderate_checkbox
+      moderate_checkbox.click
+    end
+
+    def select_grader_from_dropdown(grader_name)
+      filter_grader(grader_name).click
+    end
   end
-
-  def submission_detail_link
-    fj("a:contains('Submission Details')")
-  end
-
 end
