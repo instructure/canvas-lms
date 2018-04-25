667c7dee19f0749352dc76f6f10e7cde7295471b
Cody Cutrer <cody@instructure.com>
2018-04-25 09:29:41 -0600

    bump saml2
    fixes schema being fetched remotely, which also fixes a spec depending on
    that that is timing out
    
    Change-Id: I4fb0143eb59bff0ca89ebd52a3ff507b75e572a1
    Reviewed-on: https://gerrit.instructure.com/148074
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -104,7 +104,7 @@ gem 'rotp', '3.3.1', require: false
 gem 'net-ldap', '0.16.1', require: false
 gem 'ruby-duration', '3.2.3', require: false
 gem 'ruby-saml-mod', '0.3.8'
-gem 'saml2', '2.2.0'
+gem 'saml2', '2.2.3'
   gem 'nokogiri-xmlsec-instructure', '0.9.5', require: false
 gem 'rubycas-client', '2.3.9', require: false
 gem 'rubyzip', '1.2.1', require: 'zip'
