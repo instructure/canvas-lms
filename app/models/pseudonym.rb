30c2b1b3c3f44eaffbaf890f3c6ccaaef691c84c
Artem Kolesnikov <sirruf@me.com>
2018-04-27 16:45:11 +0300

    integration_id validation added.
    If we try to create a user with the same id as an existing or remote user, we get the following error:
    
    PG::UniqueViolation: ERROR:  duplicate key value violates unique constraint "index_pseudonyms_on_integration_id"
    DETAIL:  Key (integration_id, account_id)=(0001, 1) already exists.
    
    Adding this validation fixes this problem.
    
    Closes gh-1285
    
    Change-Id: I91621c22fb07bb991c1f62add282732f9fa3087f
    Reviewed-on: https://gerrit.instructure.com/148729
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Omar Khan <okhan@instructure.com>

--- app/models/pseudonym.rb
+++ app/models/pseudonym.rb
@@ -44,7 +44,7 @@ class Pseudonym < ActiveRecord::Base
   before_destroy :retire_channels
 
   before_save :set_password_changed
-  before_validation :infer_defaults, :verify_unique_sis_user_id
+  before_validation :infer_defaults, :verify_unique_sis_user_id, :verify_unique_integration_id
   after_save :update_account_associations_if_account_changed
   has_a_broadcast_policy
 
@@ -232,11 +232,17 @@ class Pseudonym < ActiveRecord::Base
 
   def verify_unique_sis_user_id
     return true unless self.sis_user_id
-    existing_pseudo = Pseudonym.where(account_id: self.account_id, sis_user_id: self.sis_user_id.to_s).first
-    return true if !existing_pseudo || existing_pseudo.id == self.id
+    return true unless Pseudonym.where.not(id: id).where(account_id: self.account_id, sis_user_id: self.sis_user_id).exists?
     self.errors.add(:sis_user_id, :taken,
-      message: t('#errors.sis_id_in_use', "SIS ID \"%{sis_id}\" is already in use", :sis_id => self.sis_user_id)
-    )
+      message: t('#errors.sis_id_in_use', "SIS ID \"%{sis_id}\" is already in use", sis_id: self.sis_user_id))
+    throw :abort
+  end
+
+  def verify_unique_integration_id
+    return true unless self.integration_id
+    return true unless Pseudonym.where.not(id: id).where(account_id: self.account_id, integration_id: self.integration_id).exists?
+    self.errors.add(:integration_id, :taken,
+      message: t("Integration ID \"%{integration_id}\" is already in use", integration_id: self.integration_id))
     throw :abort
   end
 
