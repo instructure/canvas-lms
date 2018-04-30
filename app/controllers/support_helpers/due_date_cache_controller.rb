10dbd7f01e9508a1b686bb262ed18312e5f1a2a1
Keith Garner <kgarner@instructure.com>
2018-04-30 17:38:31 -0500

    add support helper to run due date cacher for a course
    To help support close out issues, add a support helper that runs the
    DueDateCacher and then recalculates grades for a course.
    
    closes GRADE-1120
    
    test plan:
     - access /api/v1/support_helpers/due_date_cache/course?course_id=6
       to make sure it kicks off a new DueDateCacher run. It should send
       you a success email.
     - make sure that endpoint is only accessible when you are logged in as
       a site admin user
    
    Change-Id: Ida5f7db0484cfd4e33e1094244a3b8c00b523c6a
    Reviewed-on: https://gerrit.instructure.com/148694
    Tested-by: Jenkins
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    QA-Review: Shahbaz Javeed <sjaveed@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

---
+++ app/controllers/support_helpers/due_date_cache_controller.rb
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+module SupportHelpers
+  class DueDateCacheController < ApplicationController
+    include SupportHelpers::ControllerHelpers
+
+    before_action :require_site_admin
+
+    protect_from_forgery with: :exception
+
+    def course
+      if params[:course_id]
+        run_fixer(SupportHelpers::DueDateCache::CourseFixer, params[:course_id].to_i)
+      else
+        render plain: "Missing course id parameter", status: 400
+      end
+    end
+  end
+end
