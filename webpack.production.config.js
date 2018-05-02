4393e1b7e695593ca734fa3fd5c8e4712a355b91
Ryan Shaw <ryan@instructure.com>
2018-05-02 14:49:15 -0600

    a couple uglifyJS settings to make prod debugging easier
    Test plan:
    * run compile_assets in prod mode
    * go to a page
    * load a script in firefox/chorme devtools, 
    * it shouldn’t kill the browser forever while it syntax highlights
      And each statement should be on its own line
    * setting a breakpoint on any statement should be possible since they
      Are not all combined by commas
    
    Change-Id: I978d93985c210afd8ab5a397af04a14e92847c5d
    Reviewed-on: https://gerrit.instructure.com/149002
    Reviewed-by: Simon Williams <simon@instructure.com>
    Tested-by: Jenkins
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -9,9 +9,13 @@ if (!process.env.JS_BUILD_NO_UGLIFY) {
       sourceMap: true,
       parallel: true,
       uglifyOptions: {
+        compress: {
+          sequences: false // prevents it from combining a bunch of statments with ","s so it is easier to set breakpoints
+        },
         ecma: 5,
         output: {
           comments: false,
+          semicolons: false, // not because of the holy war but because it prevents everything being on one line and is easer to read in browser
         },
         safari10: true,
       }
