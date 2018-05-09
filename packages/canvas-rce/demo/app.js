d90f7b14cee419905eb9f2eed05bafa3a0443962
Jared Crystal <jcrystal@instructure.com>
2018-05-09 15:44:20 -0600

    fix i18n for react CanvasRce
    refs QUIZ-4451
    
    test plan:
     - comment out the non-react examples for the demo
       (if you don't comment them out, they will pull in the locale files)
     - change the language for the demo
     - the rce should reflect the change
    
    Change-Id: I516f917fc466dc2d081c4e18fb34b909940711cc
    Reviewed-on: https://gerrit.instructure.com/149760
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Product-Review: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: Brent Burgoyne <bburgoyne@instructure.com>

--- packages/canvas-rce/demo/app.js
+++ packages/canvas-rce/demo/app.js
@@ -69,7 +69,7 @@ function renderDemos(
     getProps("textarea2", language)
   );
   ReactDOM.render(
-    <CanvasRce rceProps={getProps("textarea3")} />,
+    <CanvasRce rceProps={getProps("textarea3", language)} />,
     document.getElementById("editor3")
   );
 
