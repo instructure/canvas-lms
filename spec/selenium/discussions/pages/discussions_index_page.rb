29aae6278dc635a492e9cc771520cd0c6a41c088
Aaron Kc Hsu <ahsu@instructure.com>
2018-05-10 15:28:10 -0600

    spec: finish ssd selenium test cases
    fixes COMMS-1094
    
    Test Plan
    * Let jenkins pass
    
    Change-Id: I03f09f3e75edb93e3cc9dc83489415489d6eae54
    Reviewed-on: https://gerrit.instructure.com/149914
    Tested-by: Jenkins
    Reviewed-by: Landon Gilbert-Bland <lbland@instructure.com>
    Product-Review: Aaron Kc Hsu <ahsu@instructure.com>
    QA-Review: Aaron Kc Hsu <ahsu@instructure.com>

--- spec/selenium/discussions/pages/discussions_index_page.rb
+++ spec/selenium/discussions/pages/discussions_index_page.rb
@@ -107,6 +107,10 @@ class DiscussionsIndex
       f('.subscribe-button', discussion(title))
     end
 
+    def discussion_availability(title)
+      f('.discussion-availability', discussion(title))
+    end
+
     # ---------------------- Discussion Menu ----------------------
     def discussion_menu(title)
       f('.discussions-index-manage-menu button', discussion(title))
