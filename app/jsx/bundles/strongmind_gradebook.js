cadba12db2cbfbc096642420e75eb0ca51f122f8
Rawya Husain <rawya.husain@strongmind.com>
2018-05-18 15:21:12 -0700

    Play 480 updated css in student list view

--- app/jsx/bundles/strongmind_gradebook.js
+++ app/jsx/bundles/strongmind_gradebook.js
@@ -1,2 +1,2 @@
-import '../strongmind_gradebook/main.68a43d59.js'
-import '../strongmind_gradebook/main.bc1248b6.css'
+import '../strongmind_gradebook/main.4812865b.js'
+import '../strongmind_gradebook/main.996da06d.css'
