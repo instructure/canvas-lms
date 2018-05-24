adbfbebb13d5e895784f0b739e7d883584c1786c
James Butters <jbutters@instructure.com>
2018-05-24 10:48:27 -0600

    Spec: student view course grade without grading periods
    refs: QA-341
    
    Test Plan:
      -- Passes Jenkins
    
    Change-Id: I7dce30020c077f1ae1ef54e440266b46b0f1b339
    Reviewed-on: https://gerrit.instructure.com/151362
    Tested-by: Jenkins
    Reviewed-by: Anju Reddy <areddy@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>

--- spec/selenium/grades/student_grades_summary/global_grades_spec.rb
+++ spec/selenium/grades/student_grades_summary/global_grades_spec.rb
@@ -28,23 +28,35 @@ describe 'Global Grades' do
   SCORE2 = 76.0
   SCORE3 = 9.0
   SCORE4 = 8.0
+  SCORE5 = 47
 
   before(:once) do
 
     now = Time.zone.now
 
-    @ungraded_course = course_factory(active_all: true, course_name: "Course 1")
+    # create a second term
+    Account.default.enrollment_terms.create! name: "another term",
+                                             start_at: 2.months.ago(now),
+                                             end_at: 2.months.from_now(now)
+    second_term = Account.default.enrollment_terms.second
+
+    # create first course
+    @course_no_gp = course_factory(active_all: true, course_name: "Course 1")
     @student = user_factory(active_all: true)
-    @ungraded_course.enroll_student(@student, enrollment_state: 'active')
+    @course_no_gp.enroll_student(@student, enrollment_state: 'active')
 
-    # create a second course and enroll student
-    @graded_course = course_factory(course_name: "Course 2", active_course: true)
-    @graded_course.enroll_teacher(@teacher, enrollment_state: 'active')
-    @graded_course.enroll_student(@student, allow_multiple_enrollments: true, enrollment_state: 'active')
 
-    # create grading periods
+    # create a second course, associate with second term created above, and enroll student
+    @course_with_gp = course_factory(course_name: "Course 2", active_course: true)
+    @course_with_gp.enrollment_term_id = second_term.id
+    @course_with_gp.save!
+    @course_with_gp.reload
+    @course_with_gp.enroll_teacher(@teacher, enrollment_state: 'active')
+    @course_with_gp.enroll_student(@student, allow_multiple_enrollments: true, enrollment_state: 'active')
+
+    # create grading periods associated with second term
     gpg = GradingPeriodGroup.new
-    gpg.account_id = @graded_course.root_account
+    gpg.account_id = @course_with_gp.root_account
     gpg.save!
     gpg.grading_periods.create! start_date: 6.months.ago,
                                 end_date: 3.months.ago,
@@ -54,36 +66,43 @@ describe 'Global Grades' do
                                 end_date:   2.months.from_now(now),
                                 close_date: 3.months.from_now(now),
                                 title: "current grading period"
-    term = @graded_course.enrollment_term
+    term = @course_with_gp.enrollment_term
     term.update_attribute :grading_period_group, gpg
 
     # create 3 assignments
-    @assignment1 = @graded_course.assignments.create!(
+    @assignment1 = @course_with_gp.assignments.create!(
       title: 'assignment one',
       grading_type: 'points',
       points_possible: 100,
       due_at: now,
       submission_types: 'online_text_entry'
     )
-    @assignment2 = @graded_course.assignments.create!(
+    @assignment2 = @course_with_gp.assignments.create!(
       title: 'assignment two',
       grading_type: 'points',
       points_possible: 100,
       due_at: now,
       submission_types: 'online_text_entry'
     )
-    @assignment3 = @graded_course.assignments.create!(
+    @assignment3 = @course_with_gp.assignments.create!(
       title: 'assignment three',
       grading_type: 'points',
       points_possible: 10,
       due_at: now,
       submission_types: 'online_text_entry'
     )
-    @assignment4 = @graded_course.assignments.create!(
+    @assignment4 = @course_with_gp.assignments.create!(
       title: 'assignment 4',
       grading_type: 'points',
       points_possible: 10,
       due_at: 4.months.ago(now),
+      submission_types: 'online_text_entry'
+    )
+    @assignment5 = @course_no_gp.assignments.create!(
+      title: 'assignment 5',
+      grading_type: 'points',
+      points_possible: 50,
+      due_at: 3.days.ago(now),
       submission_types: 'online_text_entry'
     )
 
@@ -92,12 +111,14 @@ describe 'Global Grades' do
     @assignment2.grade_student(@student, grade: SCORE2, grader: @teacher)
     @assignment3.grade_student(@student, grade: SCORE3, grader: @teacher)
     @assignment4.grade_student(@student, grade: SCORE4, grader: @teacher)
+    @assignment5.grade_student(@student, grade: SCORE5, grader: @teacher)
 
     GRADE_CURRENT_GP = ((SCORE1 + SCORE2 + SCORE3)/(@assignment1.points_possible + @assignment2.points_possible +
       @assignment3.points_possible))*100
     GRADE_OLD_GP = ((SCORE4/@assignment4.points_possible)*100)
-    GRADE_TOTAL = ((SCORE1 + SCORE2 + SCORE3 + SCORE4)/(@assignment1.points_possible + @assignment2.points_possible +
+    GRADE_TOTAL_GP = ((SCORE1 + SCORE2 + SCORE3 + SCORE4)/(@assignment1.points_possible + @assignment2.points_possible +
       @assignment3.points_possible + @assignment4.points_possible))*100
+    GRADE_TOTAL_NO_GP = (SCORE5/@assignment5.points_possible)*100
   end
 
   context 'as student' do
@@ -110,20 +131,24 @@ describe 'Global Grades' do
 
     it 'goes to student grades page', priority: "1", test_id: 3491485 do
       # grab score to compare
-      course_score = GlobalGrades.get_score_for_course(@graded_course)
+      course_score = GlobalGrades.get_score_for_course(@course_with_gp)
       # find link for Second Course and click
-      wait_for_new_page_load(GlobalGrades.click_course_link(@graded_course))
+      wait_for_new_page_load(GlobalGrades.click_course_link(@course_with_gp))
 
       # verify url has correct course id
-      expect(driver.current_url).to eq app_url + "/courses/#{@graded_course.id}/grades/#{@student.id}"
+      expect(driver.current_url).to eq app_url + "/courses/#{@course_with_gp.id}/grades/#{@student.id}"
       # verify assignment score is correct
       expect(StudentGradesPage.final_grade.text).to eq(course_score)
     end
 
-    it 'show score for grading period' do
-      GlobalGrades.select_grading_period(@graded_course, "old grading period")
+    it 'show score for grading period', priority: "1", test_id: 3501070 do
+      GlobalGrades.select_grading_period(@course_with_gp, "old grading period")
       # verify course grade
-      expect(GlobalGrades.get_score_for_course_no_percent(@graded_course)).to eq(GRADE_OLD_GP.round(2))
+      expect(GlobalGrades.get_score_for_course_no_percent(@course_with_gp)).to eq(GRADE_OLD_GP.round(2))
+    end
+
+    it 'show score for course without grading periods', priority: "1", test_id: 3501470 do
+      expect(GlobalGrades.get_score_for_course_no_percent(@course_no_gp)).to eq(GRADE_TOTAL_NO_GP.round(2))
     end
   end
 
@@ -136,37 +161,37 @@ describe 'Global Grades' do
     end
 
     it 'has grades table with courses', priority: "1", test_id: 3500053 do
-      expect(GlobalGrades.course_details).to include_text(@ungraded_course.name)
-      expect(GlobalGrades.course_details).to include_text(@graded_course.name)
+      expect(GlobalGrades.course_details).to include_text(@course_no_gp.name)
+      expect(GlobalGrades.course_details).to include_text(@course_with_gp.name)
     end
 
     it 'has grades table with student average' do # test id 350053
-      expect(GlobalGrades.score(@graded_course)).to include_text("average for 1 student")
+      expect(GlobalGrades.score(@course_with_gp)).to include_text("average for 1 student")
       # calculate expected grade average
 
-      expect(GlobalGrades.get_score_for_course_no_percent(@graded_course)).to eq GRADE_TOTAL.round(2)
+      expect(GlobalGrades.get_score_for_course_no_percent(@course_with_gp)).to eq GRADE_TOTAL_GP.round(2)
     end
 
     it 'has grades table with interactions report' do # test id 350053
-      expect(GlobalGrades.report(@graded_course)).to contain_link("Student Interactions Report")
+      expect(GlobalGrades.report(@course_with_gp)).to contain_link("Student Interactions Report")
     end
 
     it 'goes to gradebook page', priority: "1", test_id: 3494790 do
       # find link for Second Course and click
-      wait_for_new_page_load(GlobalGrades.click_course_link(@graded_course))
+      wait_for_new_page_load(GlobalGrades.click_course_link(@course_with_gp))
 
       # verify url has correct course id
-      expect(driver.current_url).to eq app_url + "/courses/#{@graded_course.id}/gradebook"
+      expect(driver.current_url).to eq app_url + "/courses/#{@course_with_gp.id}/gradebook"
       # verify assignment score is correct
       expect(Gradebook::MultipleGradingPeriods.student_total_grade(@student)).to eq("#{GRADE_CURRENT_GP.round(2)}%")
     end
 
     it 'goes to student interactions report', priority: "1", test_id: 3500433 do
-      GlobalGrades.click_report_link(@graded_course)
+      GlobalGrades.click_report_link(@course_with_gp)
 
       expect(StudentInteractionsReport.report).to be_displayed
       # verify current score
-      expect(StudentInteractionsReport.current_score(@student.name)).to eq("#{GRADE_TOTAL.round(1)}%")
+      expect(StudentInteractionsReport.current_score(@student.name)).to eq("#{GRADE_TOTAL_GP.round(1)}%")
     end
   end
 end
