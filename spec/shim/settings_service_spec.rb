88f8b1cbc51420135bf6cb45bc5ebd67a46d2294
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-05-30 15:52:08 -0700

    play-412: finished integration test and fixed bad call to dynamodb

---
+++ spec/shim/settings_service_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "The SettingsService" do
+  describe 'toggling sequence control' do
+    it 'updates enrollment settings' do
+      expect(SettingsService::Repository).to receive(:put).with('true')
+      SettingsService::Enrollment.canvas_domain = 'test_suite'
+      SettingsService.update_enrollment_setting(
+        id: 1,
+        setting: 'sequence_control',
+        value: 'true'
+      )
+    end
+  end
+end
