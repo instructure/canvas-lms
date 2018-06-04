ed043d831353def1aa4077e938a94edd3855590d
Rob Orton <rob@instructure.com>
2018-06-04 10:20:09 -0600

    fix api documentation for sis restore states
    test plan
     - run rake doc:api
     - sis import page should look normal
    
    Change-Id: I18d108596d6150f5b80bc79f5924b4420cfc0385
    Reviewed-on: https://gerrit.instructure.com/152374
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins

--- app/controllers/sis_imports_api_controller.rb
+++ app/controllers/sis_imports_api_controller.rb
@@ -518,15 +518,15 @@ class SisImportsApiController < ApplicationController
 
   # @API Restore workflow_states of SIS imported items
   #
-  #  This will restore the the workflow_state for all the items that changed
-  #  their workflow_state during the import being restored.
-  #  This will restore states for items imported with the following importers:
-  #  accounts.csv terms.csv courses.csv sections.csv group_categories.csv
-  #  groups.csv users.csv admins.csv
-  #  This also restores states for other items that changed during the import.
-  #  An example would be if an enrollment was deleted from a sis import and the
-  #  group_membership was also deleted as a result of the enrollment deletion,
-  #  both items would be restored when the sis batch is restored.
+  # This will restore the the workflow_state for all the items that changed
+  # their workflow_state during the import being restored.
+  # This will restore states for items imported with the following importers:
+  # accounts.csv terms.csv courses.csv sections.csv group_categories.csv
+  # groups.csv users.csv admins.csv
+  # This also restores states for other items that changed during the import.
+  # An example would be if an enrollment was deleted from a sis import and the
+  # group_membership was also deleted as a result of the enrollment deletion,
+  # both items would be restored when the sis batch is restored.
   #
   # @argument batch_mode [Boolean]
   #   If set, will only restore items that were deleted from batch_mode.
