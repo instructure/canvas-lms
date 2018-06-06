1a7c68dcc96121e76796aef2a1c7c90eebfdb41b
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-06-06 14:11:05 -0700

    fix tests

--- spec/shim/pipeline/event_spec.rb
+++ spec/shim/pipeline/event_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe 'pipeline service' do
   let(:endpoint_instance) { double('endpoint instance', call: nil) }
--- spec/shim/settings_service_spec.rb
+++ spec/shim/settings_service_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "The SettingsService" do
   describe 'toggling sequence control' do
     it 'updates enrollment settings' do
-      expect(SettingsService::Repository).to receive(:put).with('true')
+      allow(SettingsService::Repository).to receive(:create_table)
+      expect(SettingsService::Repository).to receive(:put).with(:table_name=>"test_suite-enrollment_settings", :id=>1, :setting=>"sequence_control", :value=>"true")
       SettingsService::Enrollment.canvas_domain = 'test_suite'
       SettingsService.update_enrollment_setting(
         id:      1,
