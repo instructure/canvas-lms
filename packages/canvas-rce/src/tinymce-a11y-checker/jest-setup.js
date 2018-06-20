c55bcdb81ea177e9c259eb71ec1ab9ad8281a74e
Ryan Shaw <ryankshaw@gmail.com>
2018-06-20 10:31:05 -0600

    upgrade to instUI 5 (#14)
    * upgrade to instUI 5
    
    Closes: CORE-1436
    
    Test plan:
    * run `npm start`
    * go through the demo and make sure all the components still work
      the same as they used to

--- packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
@@ -3,3 +3,5 @@ const Adapter = require("enzyme-adapter-react-15")
 require("@instructure/ui-themes")
 
 Enzyme.configure({ adapter: new Adapter() })
+
+document.documentElement.setAttribute("dir", "ltr")
--- packages/canvas-rce/src/tinymce-a11y-checker/webpack.config.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/webpack.config.js
@@ -1,20 +1,19 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
   entry: {
-    demo: './src/demo.js'
+    demo: "./src/demo.js"
   },
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
-    ]
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }]
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './demo'
+    contentBase: "./demo"
   },
+  mode: "development",
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'demo')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "demo")
   }
 }
