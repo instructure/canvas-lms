ac3fb3b2f03f0903d68c2377479d52e4a9c0179e
Jeremy Neander <jneander@instructure.com>
2018-06-25 09:05:23 -0500

    spec: update grading period form specs
    Change-Id: Ia175488f5d60687daf3c749a8d35bdea8ebeb28b
    Reviewed-on: https://gerrit.instructure.com/156947
    Tested-by: Jenkins
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Product-Review: Jeremy Neander <jneander@instructure.com>
    QA-Review: Jeremy Neander <jneander@instructure.com>

--- spec/javascripts/jsx/grading/GradingPeriodFormSpec.js
+++ spec/javascripts/jsx/grading/GradingPeriodFormSpec.js
@@ -16,171 +16,369 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'react-dom',
-  'react-addons-test-utils',
-  'underscore',
-  'jsx/grading/GradingPeriodForm',
-  'timezone/America/Chicago',
-  'timezone',
-  'helpers/fakeENV'
-], (React, ReactDOM, {Simulate}, _, GradingPeriodForm, chicago, tz,
-  fakeENV) => {
-  const wrapper = document.getElementById('fixtures');
+import React from 'react'
+import $ from 'jquery'
+import {mount} from 'enzyme'
+import GradingPeriodForm from 'jsx/grading/GradingPeriodForm'
+import chicago from 'timezone/America/Chicago'
+import tz from 'timezone'
+import fakeENV from 'helpers/fakeENV'
 
-  const examplePeriod = {
-    id: '1',
-    title: 'Q1',
-    weight: 30,
-    startDate: new Date("2015-11-01T12:00:00Z"),
-    endDate: new Date("2015-12-31T12:00:00Z"),
-    closeDate: new Date("2016-01-07T12:00:00Z")
-  };
+QUnit.module('GradingPeriodForm', suiteHooks => {
+  let gradingPeriod
+  let props
+  let wrapper
 
-  QUnit.module('GradingPeriodForm', {
-    renderComponent: function(opts={}) {
-      const defaults = {
-        period: examplePeriod,
-        weighted: true,
-        disabled: false,
-        onSave:   () => {},
-        onCancel: () => {}
-      };
-      const element = React.createElement(GradingPeriodForm, _.defaults(opts, defaults));
-      return ReactDOM.render(element, wrapper);
-    },
+  suiteHooks.beforeEach(() => {
+    fakeENV.setup({CONTEXT_TIMEZONE: 'Etc/GMT+0', TIMEZONE: 'Etc/GMT+0'})
 
-    teardown: function() {
-      ReactDOM.unmountComponentAtNode(wrapper);
-    }
-  });
-
-  test("sets form values from 'period' props", function() {
-    let form = this.renderComponent();
-    equal(form.refs.title.value, 'Q1');
-    equal(form.refs.startDate.refs.dateInput.value, 'Nov 1, 2015 12pm');
-    equal(form.refs.endDate.refs.dateInput.value, 'Dec 31, 2015 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Jan 7, 2016 12pm');
-    equal(form.weightInput.value, '30');
-  });
-
-  test("does not render weighted input if weighted is false", function() {
-    const form = this.renderComponent({weighted: false});
-    equal(form.weightInput, null);
-  });
-
-  test('renders with the save button enabled', function() {
-    let form = this.renderComponent();
-    equal(form.refs.saveButton.props.disabled, false);
-  });
-
-  test('renders with the cancel button enabled', function() {
-    let form = this.renderComponent();
-    equal(form.refs.cancelButton.props.disabled, false);
-  });
-
-  test('optionally renders with the save and cancel buttons disabled', function() {
-    let form = this.renderComponent({disabled: true});
-    equal(form.refs.saveButton.props.disabled, true);
-    equal(form.refs.cancelButton.props.disabled, true);
-  });
-
-  test("auto-updates 'closeDate' when not already set and 'endDate' changes", function() {
-    let incompletePeriod = _.extend({}, examplePeriod, { closeDate: null });
-    let form = this.renderComponent({period: incompletePeriod});
-    let endDateInput = ReactDOM.findDOMNode(form.refs.endDate.refs.dateInput);
-    endDateInput.value = 'Dec 31, 2015 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    equal(form.refs.endDate.refs.dateInput.value, 'Dec 31, 2015 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Dec 31, 2015 12pm');
-  });
-
-  test("auto-updates 'closeDate' when set equal to 'endDate' and 'endDate' changes", function() {
-    let consistentPeriod = _.extend({}, examplePeriod, { closeDate: examplePeriod.endDate });
-    let form = this.renderComponent({period: consistentPeriod});
-    let endDateInput = ReactDOM.findDOMNode(form.refs.endDate.refs.dateInput);
-    endDateInput.value = 'Dec 30, 2015 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    equal(form.refs.endDate.refs.dateInput.value, 'Dec 30, 2015 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Dec 30, 2015 12pm');
-  });
-
-  test("preserves 'closeDate' when not set equal to 'endDate' and 'endDate' changes", function() {
-    let form = this.renderComponent();
-    let endDateInput = ReactDOM.findDOMNode(form.refs.endDate.refs.dateInput);
-    endDateInput.value = 'Dec 30, 2015 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    equal(form.refs.endDate.refs.dateInput.value, 'Dec 30, 2015 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Jan 7, 2016 12pm');
-  });
-
-  test("preserves 'closeDate' when already set and 'endDate' changes to match, then changes again", function() {
-    let form = this.renderComponent();
-    let endDateInput = ReactDOM.findDOMNode(form.refs.endDate.refs.dateInput);
-    endDateInput.value = 'Jan 7 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    endDateInput.value = 'Dec 30, 2015 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    equal(form.refs.endDate.refs.dateInput.value, 'Dec 30, 2015 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Jan 7, 2016 12pm');
-  });
-
-  test("auto-updates 'closeDate' when cleared and 'endDate' changes", function() {
-    let form = this.renderComponent();
-    let closeDateInput = ReactDOM.findDOMNode(form.refs.closeDate.refs.dateInput);
-    closeDateInput.value = '';
-    closeDateInput.dispatchEvent(new Event("change"));
-    let endDateInput = ReactDOM.findDOMNode(form.refs.endDate.refs.dateInput);
-    endDateInput.value = 'Jan 7 12pm';
-    endDateInput.dispatchEvent(new Event("change"));
-    equal(form.refs.endDate.refs.dateInput.value, 'Jan 7 12pm');
-    equal(form.refs.closeDate.refs.dateInput.value, 'Jan 7 12pm');
-  });
-
-  test("given a different content timezone, local and server time are shown", function() {
-    tz.preload('America/Chicago', chicago);
-    fakeENV.setup({TIMEZONE: 'America/Denver', CONTEXT_TIMEZONE: 'America/Chicago'});
-    const form = this.renderComponent();
-    const datetimeSuggestList = ReactDOM.findDOMNode(form).querySelectorAll('.datetime_suggest');
-    equal(datetimeSuggestList.length, 6);
-    fakeENV.teardown();
-  });
-
-  test("given the same timezone, local and server time are not shown", function() {
-    const form = this.renderComponent();
-    const datetimeSuggestList = ReactDOM.findDOMNode(form).querySelectorAll('.datetime_suggest');
-    equal(datetimeSuggestList.length, 0);
-  });
-
-  test("calls the 'onSave' callback when the save button is clicked", function() {
-    let spy = sinon.spy();
-    let form = this.renderComponent({onSave: spy});
-    let saveButton = ReactDOM.findDOMNode(form.refs.saveButton);
-    Simulate.click(saveButton);
-    ok(spy.calledOnce);
-  });
-
-  test("sends form values in 'onSave'", function() {
-    let spy = sinon.spy();
-    let form = this.renderComponent({onSave: spy});
-    let saveButton = ReactDOM.findDOMNode(form.refs.saveButton);
-    Simulate.click(saveButton);
-    deepEqual(spy.args[0][0], {
-      id: '1',
+    gradingPeriod = {
+      closeDate: new Date('2016-01-07T12:00:00Z'),
+      endDate: new Date('2015-12-31T12:00:00Z'),
+      id: '1401',
+      startDate: new Date('2015-11-01T12:00:00Z'),
       title: 'Q1',
-      startDate: new Date("2015-11-01T12:00:00Z"),
-      endDate: new Date("2015-12-31T12:00:00Z"),
-      closeDate: new Date("2016-01-07T12:00:00Z"),
       weight: 30
-    });
-  });
+    }
 
-  test("calls the 'onCancel' callback when the cancel button is clicked", function() {
-    let spy = sinon.spy();
-    let form = this.renderComponent({onCancel: spy});
-    let cancelButton = ReactDOM.findDOMNode(form.refs.cancelButton);
-    Simulate.click(cancelButton);
-    ok(spy.calledOnce);
-  });
-});
+    props = {
+      disabled: false,
+      onCancel: sinon.spy(),
+      onSave: sinon.spy(),
+      period: gradingPeriod,
+      weighted: true
+    }
+  })
+
+  suiteHooks.afterEach(() => {
+    wrapper.unmount()
+    $('#ui-datepicker-div').datepicker('destroy')
+    $('#ui-datepicker-div').remove()
+    fakeENV.teardown()
+  })
+
+  function mountComponent() {
+    wrapper = mount(<GradingPeriodForm {...props} />)
+  }
+
+  function getButton(label) {
+    const buttons = wrapper.find('button')
+    return buttons.filterWhere(button => button.text() === label)
+  }
+
+  function getDateInput(inputLabel) {
+    const labels = wrapper.find('label')
+    const $label = labels.filterWhere(label => label.text() === inputLabel).get(0)
+    return wrapper.find(`input[aria-labelledby="${$label.id}"]`).get(0)
+  }
+
+  function getDateTimeSuggestions(inputLabel) {
+    const $input = getDateInput(inputLabel)
+    let $parent = $input.parentElement
+    while ($parent && !$parent.classList.contains('ic-Form-control')) {
+      $parent = $parent.parentElement
+    }
+    return Array.from($parent.querySelectorAll('.datetime_suggest'))
+  }
+
+  function getInput(id) {
+    return wrapper.find(`input#${id}`).get(0)
+  }
+
+  function setDateInputValue(label, value) {
+    const $input = getDateInput(label)
+    $input.value = value
+    $input.dispatchEvent(new Event('change', {target: $input}))
+  }
+
+  function setInputValue(id, value) {
+    // Using Enzyme here is a workaround for React 14 swallowing input events.
+    const input = wrapper.find(`input#${id}`)
+    input.get(0).value = value
+    input.simulate('change', {target: {value}})
+  }
+
+  QUnit.module('"Title" input', () => {
+    test('value is set to the grading period title for an existing grading period', () => {
+      mountComponent()
+      equal(getInput('title').value, 'Q1')
+    })
+  })
+
+  QUnit.module('"Start Date" input', () => {
+    test('value is set to the grading period start date', () => {
+      mountComponent()
+      equal(getDateInput('Start Date').value, 'Nov 1, 2015 12pm')
+    })
+
+    QUnit.module('when local and server time are different', hooks => {
+      hooks.beforeEach(() => {
+        Object.assign(ENV, {CONTEXT_TIMEZONE: 'America/Chicago'})
+        tz.preload('America/Chicago', chicago)
+        mountComponent()
+      })
+
+      test('shows both local and context time suggestions for start date', () => {
+        strictEqual(getDateTimeSuggestions('Start Date').length, 2)
+      })
+
+      test('formats the start date for the local timezone', () => {
+        const $suggestions = getDateTimeSuggestions('Start Date')
+        // Local is GMT
+        strictEqual($suggestions[0].textContent, 'Local: Sun Nov 1, 2015 12:00pm')
+      })
+
+      test('formats the start date for the context timezone', () => {
+        const $suggestions = getDateTimeSuggestions('Start Date')
+        // Course is in Chicago
+        strictEqual($suggestions[1].textContent, 'Account: Sun Nov 1, 2015 6:00am')
+      })
+    })
+
+    test('does not show local and server time for start date when they are the same', () => {
+      mountComponent()
+      strictEqual(getDateTimeSuggestions('Start Date').length, 0)
+    })
+  })
+
+  QUnit.module('"End Date" input', () => {
+    test('value is set to the grading period end date', () => {
+      mountComponent()
+      equal(getDateInput('End Date').value, 'Dec 31, 2015 12pm')
+    })
+
+    /* eslint-disable-next-line qunit/no-identical-names */
+    QUnit.module('when local and server time are different', hooks => {
+      hooks.beforeEach(() => {
+        Object.assign(ENV, {CONTEXT_TIMEZONE: 'America/Chicago'})
+        tz.preload('America/Chicago', chicago)
+        mountComponent()
+      })
+
+      test('shows both local and context time suggestions for end date', () => {
+        strictEqual(getDateTimeSuggestions('End Date').length, 2)
+      })
+
+      test('formats the end date for the local timezone', () => {
+        const $suggestions = getDateTimeSuggestions('End Date')
+        // Local is GMT
+        strictEqual($suggestions[0].textContent, 'Local: Thu Dec 31, 2015 12:00pm')
+      })
+
+      test('formats the end date for the context timezone', () => {
+        const $suggestions = getDateTimeSuggestions('End Date')
+        // Course is in Chicago
+        strictEqual($suggestions[1].textContent, 'Account: Thu Dec 31, 2015 6:00am')
+      })
+    })
+
+    test('does not show local and server time for end date when they are the same', () => {
+      mountComponent()
+      strictEqual(getDateTimeSuggestions('End Date').length, 0)
+    })
+  })
+
+  QUnit.module('"Close Date" input', () => {
+    test('value is set to the grading period close date for an existing grading period', () => {
+      mountComponent()
+      equal(getDateInput('Close Date').value, 'Jan 7, 2016 12pm')
+    })
+
+    /* eslint-disable-next-line qunit/no-identical-names */
+    QUnit.module('when local and server time are different', hooks => {
+      hooks.beforeEach(() => {
+        Object.assign(ENV, {CONTEXT_TIMEZONE: 'America/Chicago'})
+        tz.preload('America/Chicago', chicago)
+        mountComponent()
+      })
+
+      test('shows both local and context time suggestions for close date', () => {
+        strictEqual(getDateTimeSuggestions('Close Date').length, 2)
+      })
+
+      test('formats the close date for the local timezone', () => {
+        const $suggestions = getDateTimeSuggestions('Close Date')
+        // Local is GMT
+        strictEqual($suggestions[0].textContent, 'Local: Thu Jan 7, 2016 12:00pm')
+      })
+
+      test('formats the close date for the context timezone', () => {
+        const $suggestions = getDateTimeSuggestions('Close Date')
+        // Course is in Chicago
+        strictEqual($suggestions[1].textContent, 'Account: Thu Jan 7, 2016 6:00am')
+      })
+    })
+
+    test('updates to match "End Date" when not previously set and "End Date" changes', () => {
+      props.period = null
+      mountComponent()
+      setDateInputValue('End Date', 'Dec 31, 2015 12pm')
+      equal(getDateInput('Close Date').value, 'Dec 31, 2015 12pm')
+    })
+
+    test('updates to match "End Date" when currently matching "End Date" and "End Date" changes', () => {
+      props.period.closeDate = props.period.endDate
+      mountComponent()
+      setDateInputValue('End Date', 'Dec 31, 2015 12pm')
+      equal(getDateInput('Close Date').value, 'Dec 31, 2015 12pm')
+    })
+
+    test('does not update when not set equal to "End Date" and "End Date" changes', () => {
+      mountComponent()
+      setDateInputValue('End Date', 'Dec 31, 2015 12pm')
+      equal(getDateInput('Close Date').value, 'Jan 7, 2016 12pm')
+    })
+
+    test('does not update when "End Date" changes to match and changes again', () => {
+      mountComponent()
+      setDateInputValue('End Date', 'Jan 7, 2016 12pm')
+      setDateInputValue('End Date', 'Dec 31, 2015 12pm')
+      equal(getDateInput('Close Date').value, 'Jan 7, 2016 12pm')
+    })
+
+    test('updates to match "End Date" after being cleared and "End Date" changes', () => {
+      mountComponent()
+      setDateInputValue('Close Date', '')
+      setDateInputValue('End Date', 'Dec 31, 2015 12pm')
+      equal(getDateInput('Close Date').value, 'Dec 31, 2015 12pm')
+    })
+  })
+
+  QUnit.module('"Weight" input', () => {
+    test('is present when the grading period set is weighted', () => {
+      mountComponent()
+      strictEqual(wrapper.find('input#weight').length, 1)
+    })
+
+    test('is absent when the grading period set is not weighted', () => {
+      props.weighted = false
+      mountComponent()
+      strictEqual(wrapper.find('input#weight').length, 0)
+    })
+
+    test('value is set to the grading period weight for an existing grading period', () => {
+      mountComponent()
+      strictEqual(getInput('weight').value, '30')
+    })
+  })
+
+  QUnit.module('"Save" button', () => {
+    function getSavedGradingPeriod() {
+      return props.onSave.lastCall.args[0]
+    }
+
+    test('calls the onSave callback when clicked', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      strictEqual(props.onSave.callCount, 1)
+    })
+
+    test('includes the grading period id when updating an existing grading period', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      strictEqual(getSavedGradingPeriod().id, '1401')
+    })
+
+    test('excludes the grading period id when creating a new grading period', () => {
+      delete props.period.id
+      mountComponent()
+      getButton('Save').simulate('click')
+      strictEqual(typeof getSavedGradingPeriod().id, 'undefined')
+    })
+
+    test('includes the grading period title', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      equal(getSavedGradingPeriod().title, 'Q1')
+    })
+
+    test('includes updates to the grading period title', () => {
+      mountComponent()
+      setInputValue('title', 'Quarter 1')
+      getButton('Save').simulate('click')
+      equal(getSavedGradingPeriod().title, 'Quarter 1')
+    })
+
+    test('includes the grading period start date', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().startDate, new Date('2015-11-01T12:00:00Z'))
+    })
+
+    test('includes updates to the grading period start date', () => {
+      mountComponent()
+      setDateInputValue('Start Date', 'Nov 2, 2015 12pm')
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().startDate, new Date('2015-11-02T12:00:00Z'))
+    })
+
+    test('includes the grading period end date', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().endDate, new Date('2015-12-31T12:00:00Z'))
+    })
+
+    test('includes updates to the grading period end date', () => {
+      mountComponent()
+      setDateInputValue('End Date', 'Dec 30, 2015 12pm')
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().endDate, new Date('2015-12-30T12:00:00Z'))
+    })
+
+    test('includes the grading period close date', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().closeDate, new Date('2016-01-07T12:00:00Z'))
+    })
+
+    test('includes updates to the grading period close date', () => {
+      mountComponent()
+      setDateInputValue('Close Date', 'Dec 31, 2015 12pm')
+      getButton('Save').simulate('click')
+      deepEqual(getSavedGradingPeriod().closeDate, new Date('2015-12-31T12:00:00Z'))
+    })
+
+    test('includes the grading period weight', () => {
+      mountComponent()
+      getButton('Save').simulate('click')
+      strictEqual(getSavedGradingPeriod().weight, 30)
+    })
+
+    test('includes updates to the grading period weight', () => {
+      mountComponent()
+      setInputValue('weight', '25')
+      getButton('Save').simulate('click')
+      strictEqual(getSavedGradingPeriod().weight, 25)
+    })
+
+    test('is disabled when the form is disabled', () => {
+      props.disabled = true
+      mountComponent()
+      strictEqual(getButton('Save').prop('disabled'), true)
+    })
+
+    test('is not disabled when the form is not disabled', () => {
+      mountComponent()
+      notEqual(getButton('Save').prop('disabled'), true)
+    })
+  })
+
+  QUnit.module('"Cancel" button', () => {
+    test('calls the onCancel callback when clicked', () => {
+      mountComponent()
+      getButton('Cancel').simulate('click')
+      strictEqual(props.onCancel.callCount, 1)
+    })
+
+    test('is disabled when the form is disabled', () => {
+      props.disabled = true
+      mountComponent()
+      strictEqual(getButton('Cancel').prop('disabled'), true)
+    })
+
+    test('is not disabled when the form is not disabled', () => {
+      mountComponent()
+      notEqual(getButton('Cancel').prop('disabled'), true)
+    })
+  })
+})
