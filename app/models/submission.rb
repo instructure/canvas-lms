fcf030f632556e9554e419d875a43c66620118e8
Nate Armstrong <narmstrong@instructure.com>
2018-06-25 14:42:55 -0600

    Base assignment grade alerts on percentage
    Refs: MBL-10731
    
    Test plan:
    * Create an assignment with a points possible that isn't 100 (like 10)
    * Create assignment_grade_high and assignment_grade_low thresholds
    * Verify that alerts are created when the score percentage crosses the
    thresholds
    
    Change-Id: I49bac5115089d2ecf7fcaafb68c6df98fb544d6b
    Reviewed-on: https://gerrit.instructure.com/155125
    Tested-by: Jenkins
    Reviewed-by: Matthew Sessions <msessions@instructure.com>
    Product-Review: Matthew Sessions <msessions@instructure.com>
    QA-Review: Matthew Sessions <msessions@instructure.com>

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -548,13 +548,17 @@ class Submission < ActiveRecord::Base
   end
 
   def create_alert
-    return unless saved_change_to_score? && self.grader_id && !self.autograded?
+    return unless saved_change_to_score? && self.grader_id && !self.autograded? &&
+      self.assignment.points_possible && self.assignment.points_possible > 0
 
     thresholds = ObserverAlertThreshold.active.where(student: self.user,
       alert_type: ['assignment_grade_high', 'assignment_grade_low'])
 
     thresholds.each do |threshold|
-      next unless threshold.did_pass_threshold(saved_changes['score'][0], self.score)
+      prev_score = saved_changes['score'][0]
+      prev_percentage = prev_score.present? ? prev_score.to_f / self.assignment.points_possible * 100 : nil
+      percentage = self.score.present? ? self.score.to_f / self.assignment.points_possible * 100 : nil
+      next unless threshold.did_pass_threshold(prev_percentage, percentage)
 
       ObserverAlert.create!(observer: threshold.observer, student: self.user,
                             observer_alert_threshold: threshold,
