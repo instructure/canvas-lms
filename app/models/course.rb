353f663866a34870666945f7151613f725d1df8f
Cody Cutrer <cody@instructure.com>
2018-06-25 13:42:46 -0600

    rails 5.2: fix some order(Arel.sql()) warnings
    Change-Id: Iced0d33f841f09a499b1ed914ae7efc4a390fdbd
    Reviewed-on: https://gerrit.instructure.com/155127
    Tested-by: Jenkins
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -629,7 +629,7 @@ class Course < ActiveRecord::Base
   scope :recently_ended, -> { where(:conclude_at => 1.month.ago..Time.zone.now).order("start_at DESC").limit(10) }
   scope :recently_created, -> { where("created_at>?", 1.month.ago).order("created_at DESC").limit(50).preload(:teachers) }
   scope :for_term, lambda {|term| term ? where(:enrollment_term_id => term) : all }
-  scope :active_first, -> { order("CASE WHEN courses.workflow_state='available' THEN 0 ELSE 1 END, #{best_unicode_collation_key('name')}") }
+  scope :active_first, -> { order(Arel.sql("CASE WHEN courses.workflow_state='available' THEN 0 ELSE 1 END, #{best_unicode_collation_key('name')}")) }
   scope :name_like, lambda {|name| where(coalesced_wildcard('courses.name', 'courses.sis_source_id', 'courses.course_code', name)) }
   scope :needs_account, lambda { |account, limit| where(:account_id => nil, :root_account_id => account).limit(limit) }
   scope :active, -> { where("courses.workflow_state<>'deleted'") }
