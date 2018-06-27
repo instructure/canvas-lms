c2402f2880de1bdf86c1b5e797cda69b3c92776d
Jeremy Neander <jneander@instructure.com>
2018-06-27 12:06:07 -0500

    account for floating point grade calculations
    closes GRADE-1288
    
    test plan:
     * Yeah, no.
    
    Change-Id: Icf0ba7d85216f8e9592484e471bc624df03e4db3
    Reviewed-on: https://gerrit.instructure.com/155967
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Keith T. Garner <kgarner@instructure.com>

--- app/jsx/gradebook/shared/helpers/GradeCalculationHelper.js
+++ app/jsx/gradebook/shared/helpers/GradeCalculationHelper.js
@@ -19,21 +19,29 @@
 import _ from 'underscore'
 import Big from 'big.js'
 
-function addWithPrecision(a, b) {
+export function add(a, b) {
   return new Big(a || 0).plus(b || 0)
 }
 
-function divideWithPrecision(a, b) {
+export function divide(a, b) {
   return new Big(a || 0).div(b || 0)
 }
 
-function multiplyWithPrecision(a, b) {
+export function multiply(a, b) {
   return new Big(a || 0).times(b || 0)
 }
 
+export function toNumber(big) {
+  return Number.parseFloat(big, 10)
+}
+
+export function bigSum(values) {
+  return values.reduce((total, value) => total.plus(value || 0), Big(0))
+}
+
 export function sum(collection) {
-  const bigValue = _.reduce(collection, addWithPrecision, 0)
-  return parseFloat(bigValue, 10)
+  const bigValue = _.reduce(collection, add, 0)
+  return toNumber(bigValue)
 }
 
 export function sumBy(collection, attr) {
@@ -47,5 +55,9 @@ export function scoreToPercentage(score, pointsPossible) {
     return floatingPointResult
   }
 
-  return Number.parseFloat(multiplyWithPrecision(divideWithPrecision(score, pointsPossible), 100))
+  return toNumber(multiply(divide(score, pointsPossible), 100))
+}
+
+export function weightedPercent({score, possible, weight}) {
+  return (score && weight) ? Big(score).div(possible).times(weight) : Big(0)
 }
