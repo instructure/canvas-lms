692c5334f064f4d243d077177012b921271c7d23
James Butters <jbutters@instructure.com>
2018-06-28 08:39:34 -0600

    spec: refactor gradebook amm spec for performance
    Improve readability of the spec, save an average of
    1.5 seconds runtime.
    Before refactor average runtime: 19.666667
    After refactor average runtime: 18.113333
    
    Test Plan:
    	Jenkins Passes
    
    Change-Id: I78931360e7f2607c22d28c4db6728bca7abc620c
    Reviewed-on: https://gerrit.instructure.com/155579
    Tested-by: Jenkins
    Reviewed-by: Anju Reddy <areddy@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Tested-by: James Butters <jbutters@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_anonymous_moderated_marking_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_anonymous_moderated_marking_spec.rb
@@ -21,18 +21,16 @@ require_relative '../pages/gradebook_page'
 describe 'Original Gradebook' do
   include_context "in-process server selenium tests"
 
-  before(:each) do
+  before(:once) do
     # create a course with a teacher
-    course_with_teacher(course_name: 'Course1', active_all: true)
+    @teacher1 = course_with_teacher(course_name: 'Course1', active_all: true).user
+
+    # enroll a second teacher
+    @teacher2 = course_with_teacher(course: @course, name: 'Teacher2', active_all: true).user
 
     # enroll two students
-    @student1 = User.create!(name: 'Student1')
-    @student1.register!
-    @course.enroll_student(@student1, enrollment_state: 'active')
-
-    @student2 = User.create!(name: 'Student2')
-    @student2.register!
-    @course.enroll_student(@student2, enrollment_state: 'active')
+    @student1 = course_with_student(course: @course, name: 'Student1', active_all: true).user
+    @student2 = course_with_student(course: @course, name: 'Student2', active_all: true).user
   end
 
   context 'with an anonymous assignment' do
@@ -49,13 +47,14 @@ describe 'Original Gradebook' do
 
       # submit homework and give a grade
       @student1_submission = @anonymous_assignment.submit_homework(@student1, body: 'student 1 submission moderated assignment')
-      @student1_submission = @anonymous_assignment.grade_student(@student1, grade: 13, grader: @teacher)
+      @student1_submission = @anonymous_assignment.grade_student(@student1, grade: 13, grader: @teacher1)
+
+      user_session(@teacher1)
     end
 
     context 'in submission detail' do
       before(:each) do
         @anonymous_assignment.unmute!
-        user_session(@teacher)
         Gradebook::MultipleGradingPeriods.visit_gradebook(@course)
         Gradebook::MultipleGradingPeriods.open_comment_dialog(0,1)
       end
@@ -72,7 +71,6 @@ describe 'Original Gradebook' do
 
     context 'grade cells', priority: '1', test_id: 3496299 do
       before(:each) do
-        user_session(@teacher)
         Gradebook::MultipleGradingPeriods.visit_gradebook(@course)
       end
 
@@ -87,26 +85,16 @@ describe 'Original Gradebook' do
 
   context 'with a moderated assignment' do
     before(:each) do
-      # enroll a second teacher
-      @teacher2 = User.create!(name: 'Teacher2')
-      @teacher2.register!
-      @course.enroll_teacher(@teacher2, enrollment_state: 'active')
-
       # create moderated assignment
       @moderated_assignment = @course.assignments.create!(
         title: 'Moderated Assignment1',
         grader_count: 2,
-        final_grader_id: @teacher.id,
+        final_grader_id: @teacher1.id,
         grading_type: 'points',
         points_possible: 15,
         submission_types: 'online_text_entry',
         moderated_grading: true
       )
-
-      # enroll a student
-      @student1 = User.create!(name: 'Student1')
-      @student1.register!
-      @course.enroll_student(@student1, enrollment_state: 'active')
 
       # switch session to non-final-grader
       user_session(@teacher2)
