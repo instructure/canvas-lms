dd68d00b44b22a6c4c4c2df4097899035540e6d2
James Butters <jbutters@instructure.com>
2018-06-28 11:20:16 -0600

    spec: refactor gradezilla amm spec
    improve performance and readability
    Before refactor average runtime: 36.98
    After refactor average runtime: 29.11666667
    
    Test Plan:
    	Jenkins Passes
    
    Change-Id: Ib94a07b21d75ad54b5478a96b4a824d8172ba2dd
    Reviewed-on: https://gerrit.instructure.com/155618
    Tested-by: Jenkins
    Reviewed-by: Anju Reddy <areddy@instructure.com>
    Product-Review: Anju Reddy <areddy@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>

--- spec/selenium/grades/pages/gradezilla_page.rb
+++ spec/selenium/grades/pages/gradezilla_page.rb
@@ -220,8 +220,6 @@ class Gradezilla
     def visit(course)
       Account.default.enable_feature!(:new_gradebook)
       get "/courses/#{course.id}/gradebook/change_gradebook_version?version=default"
-      # the pop over menus is too lengthy so make screen bigger
-      make_full_screen
     end
 
     def visit_upload(course)
