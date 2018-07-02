6fd8dcec703e6f0ca904c016d506715198ae5e15
Jeremy Neander <jneander@instructure.com>
2018-07-02 14:23:26 -0500

    Revert "increase minimum width for assignment columns"
    This reverts commit 9fa9ae56ccfc253f411158698088516a0d4fd32e.
    
    closes GRADE-1298
    
    test plan:
     1. Visit New Gradebook
     2. Verify assignment columns can be reduced in width
        down to 10 pixels
    
    Change-Id: Ided7eef200957d233ff7ccb7c0f6f7687a2fcfc5
    Reviewed-on: https://gerrit.instructure.com/155971
    Tested-by: Jenkins
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

--- spec/javascripts/jsx/gradezilla/default_gradebook/GradebookColumnWidthSpec.js
+++ spec/javascripts/jsx/gradezilla/default_gradebook/GradebookColumnWidthSpec.js
@@ -205,7 +205,7 @@ QUnit.module('Gradebook Grid Column Widths', (suiteHooks) => {
   QUnit.module('when initializing the grid', (hooks) => {
     hooks.beforeEach(() => {
       gradebook = createGradebook();
-      gradebook.gradebookColumnSizeSettings = { assignment_2302: 10, assignment_2303: 254 };
+      gradebook.gradebookColumnSizeSettings = { assignment_2302: 10, assignment_2303: 54 };
       gradebook.initialize();
       addGridData();
       gridSpecHelper = new SlickGridSpecHelper(gradebook.gradebookGrid);
@@ -218,7 +218,18 @@ QUnit.module('Gradebook Grid Column Widths', (suiteHooks) => {
 
     test('uses a stored width for assignment column headers', () => {
       const columnNode = gridSpecHelper.getColumnHeaderNode('assignment_2303');
-      strictEqual(columnNode.offsetWidth, 254);
+      strictEqual(columnNode.offsetWidth, 54);
+    });
+
+    test('hides assignment column header content when the column is minimized', () => {
+      const columnNode = gridSpecHelper.getColumnHeaderNode('assignment_2302');
+      ok(columnNode.classList.contains('minimized'));
+    });
+
+    test('hides assignment column cell content when the column is minimized', () => {
+      const columnIndex =  gridSpecHelper.listColumnIds().indexOf('assignment_2302');
+      const cellNode = gradebook.gradebookGrid.grid.getCellNode(0, columnIndex);
+      ok(cellNode.classList.contains('minimized'));
     });
   });
 
@@ -238,6 +249,34 @@ QUnit.module('Gradebook Grid Column Widths', (suiteHooks) => {
       const originalWidth = gridSpecHelper.getColumn('assignment_2304').width;
       resizeColumn('assignment_2304', -20);
       strictEqual(gradebook.gradebookGrid.gridData.columns.definitions.assignment_2304.width, originalWidth - 20);
+    });
+
+    test('hides assignment column header content when the column is minimized', () => {
+      resizeColumn('assignment_2304', -100);
+      const columnNode = gridSpecHelper.getColumnHeaderNode('assignment_2304');
+      ok(columnNode.classList.contains('minimized'));
+    });
+
+    test('hides assignment column cell content when the column is minimized', () => {
+      resizeColumn('assignment_2304', -100);
+      const columnIndex =  gridSpecHelper.listColumnIds().indexOf('assignment_2304');
+      const cellNode = gradebook.gradebookGrid.grid.getCellNode(0, columnIndex);
+      ok(cellNode.classList.contains('minimized'));
+    });
+
+    test('unhides assignment column header content when the column is unminimized', () => {
+      resizeColumn('assignment_2304', -100);
+      resizeColumn('assignment_2304', 1);
+      const columnNode = gridSpecHelper.getColumnHeaderNode('assignment_2304');
+      notOk(columnNode.classList.contains('minimized'));
+    });
+
+    test('unhides assignment column cell content when the column is unminimized', () => {
+      resizeColumn('assignment_2304', -100);
+      resizeColumn('assignment_2304', 1);
+      const columnIndex =  gridSpecHelper.listColumnIds().indexOf('assignment_2304');
+      const cellNode = gradebook.gradebookGrid.grid.getCellNode(0, columnIndex);
+      notOk(cellNode.classList.contains('minimized'));
     });
   });
 });
