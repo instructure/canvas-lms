19ca613ab5a940694ee34c82775632e3a7c058cb
Cody Cutrer <cody@instructure.com>
2018-08-08 10:36:15 -0600

    bump inst-jobs
    Change-Id: Idb6312b3a2f317321dfea3b6ab985341ed625d21
    Reviewed-on: https://gerrit.instructure.com/160095
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -61,10 +61,10 @@ gem 'bcrypt', '3.1.11'
 gem 'canvas_connect', '0.3.11'
   gem 'adobe_connect', '1.0.6', require: false
 gem 'canvas_webex', '0.17'
-gem 'inst-jobs', '0.14.8'
+gem 'inst-jobs', '0.15.1'
   gem 'rufus-scheduler', '3.4.2', require: false
     gem 'et-orbi', '1.0.8', require: false
-gem 'switchman-inst-jobs', '1.1.7'
+gem 'switchman-inst-jobs', '1.2.1'
 gem 'inst-jobs-autoscaling', '1.0.4'
   gem 'aws-sdk-autoscaling', '1.5.0', require: false
 # ffi 1.9.2x seem to have issues on RHEL & CentOS causing segfaults
