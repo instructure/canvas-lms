4c3d0fc3bebb45df7446f76f5638381dc0f64f4c
Nate Armstrong <narmstrong@instructure.com>
2018-08-09 11:29:51 -0600

    Allow sub account admins to generate pairing codes
    Refs MBL-10998
    
    Test plan:
    * Create a sub account
    * Add an admin to the sub account
    * Create a course for the sub account
    * Enroll a student in the course
    * Verify that you can generate pairing codes as the admin in the sub
    account
    
    Change-Id: I7a67fc6c5c9335bbd7cd8716720b66bf9fe31f42
    Reviewed-on: https://gerrit.instructure.com/160286
    Tested-by: Jenkins
    Reviewed-by: Matthew Sessions <msessions@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: Matthew Sessions <msessions@instructure.com>
    QA-Review: Matthew Sessions <msessions@instructure.com>

--- spec/apis/v1/observer_pairing_codes_api_controller_spec.rb
+++ spec/apis/v1/observer_pairing_codes_api_controller_spec.rb
@@ -63,5 +63,32 @@ describe ObserverPairingCodesApiController, type: :request do
       api_call_as_user(user_model, :post, @path, @params)
       expect(response.code).to eq "401"
     end
+
+    describe 'sub_accounts' do
+      before :once do
+        @sub_account = Account.create! root_account: Account.default
+        @student = course_with_student(account: @sub_account, active_all: true).user
+        @sub_admin = account_admin_user(account: @sub_account)
+        @path = "/api/v1/users/#{@student.id}/observer_pairing_codes"
+        @params = {user_id: @student.to_param,
+          controller: 'observer_pairing_codes_api', action: 'create', format: 'json'}
+      end
+
+      it 'sub_account admin can generate code' do
+        json = api_call_as_user(@sub_admin, :post, @path, @params)
+        expect(response.code).to eq "200"
+        expect(json['code']).not_to be nil
+      end
+
+      it "sub_account admin cant generate code for students in other sub accounts" do
+        other_sub_account = Account.create! root_account: Account.default
+        other_student = course_with_student(account: other_sub_account, active_all: true).user
+        path = "/api/v1/users/#{other_student.id}/observer_pairing_codes"
+        params = {user_id: other_student.to_param,
+          controller: 'observer_pairing_codes_api', action: 'create', format: 'json'}
+        api_call_as_user(@sub_admin, :post, path, params)
+        expect(response.code).to eq "401"
+      end
+    end
   end
 end
