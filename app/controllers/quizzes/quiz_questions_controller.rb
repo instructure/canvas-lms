c3d8cf0431e99ec586059b83cf0714c2fde2a401
Matthew Berns <mberns@instructure.com>
2018-08-13 11:18:32 -0500

    fix outcomes not adding to course with bank
    fixes OUT-2281
    
    test plan:
    - create two account level outcomes
    - create an account level bank with at least
      one question
    - attach the new outcomes to the bank
    - create or edit a quiz in a course
    - go to "Find Questions" and add the question(s)
      from the account level bank
    - ensure the account level outcomes from the
      bank now appear in the course's Outcomes page
    
    Change-Id: I64278f5ee4d17eabca2bc40d6c3d4f84ae9b144b
    Reviewed-on: https://gerrit.instructure.com/160565
    Reviewed-by: Neil Gupta <ngupta@instructure.com>
    Reviewed-by: Frank Murphy <fmurphy@instructure.com>
    Tested-by: Jenkins
    QA-Review: Dariusz Dzien <ddzien@instructure.com>
    Product-Review: Sidharth Oberoi <soberoi@instructure.com>

--- app/controllers/quizzes/quiz_questions_controller.rb
+++ app/controllers/quizzes/quiz_questions_controller.rb
@@ -298,7 +298,8 @@ class Quizzes::QuizQuestionsController < ApplicationController
       @assessment_questions = @bank.assessment_questions.active.where(id: params[:assessment_questions_ids].split(",")).to_a
       @group = @quiz.quiz_groups.where(id: params[:quiz_group_id]).first if params[:quiz_group_id].to_i > 0
       @questions = @quiz.add_assessment_questions(@assessment_questions, @group)
-
+      bank_outcome_ids = @bank.learning_outcome_alignments.select(:learning_outcome_id)
+      LearningOutcome.ensure_presence_in_context(bank_outcome_ids, @context)
       render json: questions_json(@questions, @current_user, session, [:assessment_question])
     end
   end
