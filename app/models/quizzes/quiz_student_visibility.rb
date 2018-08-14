6ca41a7b4b7c8de1c3da4908910aa5d987fa5b59
Cody Cutrer <cody@instructure.com>
2018-08-14 08:40:36 -0600

    query item visibilities from the slave
    Change-Id: I034f1f528172e249506024299cabee37d0f6730f
    Reviewed-on: https://gerrit.instructure.com/160698
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- app/models/quizzes/quiz_student_visibility.rb
+++ app/models/quizzes/quiz_student_visibility.rb
@@ -29,10 +29,6 @@ class Quizzes::QuizStudentVisibility < ActiveRecord::Base
     visible_object_ids_in_course_by_user(:quiz_id, opts)
   end
 
-  def self.users_with_visibility_by_quiz(opts)
-    users_with_visibility_by_object_id(:quiz_id, opts)
-  end
-
   # readonly? is not checked in destroy though
   before_destroy { |record| raise ActiveRecord::ReadOnlyRecord }
 end
