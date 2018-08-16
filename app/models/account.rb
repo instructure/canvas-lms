d4b2e888c58fff27e7b29b2bb61469dcac4c0c27
Cody Cutrer <cody@instructure.com>
2018-08-16 08:41:37 -0600

    slave the other incarnation of account_chain
    Change-Id: I6c3c9e095eebec11b9ee95bacbe5959aa68d7925
    Reviewed-on: https://gerrit.instructure.com/161024
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -729,7 +729,7 @@ class Account < ActiveRecord::Base
     end
 
     if starting_account_id
-      if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
+      Shackles.activate(:slave) do
         chain.concat(Shard.shard_for(starting_account_id).activate do
           Account.find_by_sql(<<-SQL)
                 WITH RECURSIVE t AS (
@@ -740,13 +740,6 @@ class Account < ActiveRecord::Base
                 SELECT * FROM t
           SQL
         end)
-      else
-        account = Account.find(starting_account_id)
-        chain << account
-        while account.parent_account
-          account = account.parent_account
-          chain << account
-        end
       end
     end
     chain
