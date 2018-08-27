3a5c655e0c529ece2088dde7f1eadb5c20856de0
James Butters <jbutters@instructure.com>
2018-08-27 08:23:38 -0600

    spec: fix flaky gradebook_grade_detail_spec
    Test Plan:
    	Jenkins Passes
    
    Change-Id: I54c9f85d6aca4732a3c469895668612926648583
    Reviewed-on: https://gerrit.instructure.com/162156
    Tested-by: Jenkins
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>

--- spec/selenium/grades/pages/gradezilla_cells_page.rb
+++ spec/selenium/grades/pages/gradezilla_cells_page.rb
@@ -47,6 +47,10 @@ class Gradezilla
         f("#{grading_cell_selector(student, assignment)} .Grid__AssignmentRowCell__#{menu_selector}Menu button")
       end
 
+      def grade_tray_button
+        f('.Grid__AssignmentRowCell__Options button')
+      end
+
       def get_grade(student, assignment)
         grading_cell(student, assignment).text
       end
@@ -91,10 +95,9 @@ class Gradezilla
       end
 
       def open_tray(student, assignment)
-        cell = grading_cell(student, assignment)
-        cell.click
-        driver.action.send_keys(:escape).perform
-        driver.action.send_keys('c').perform
+        grading_cell(student, assignment).click
+        grade_tray_button.click
+        Gradezilla::GradeDetailTray.submission_tray_full_content
         wait_for_ajaximations
       end
 
