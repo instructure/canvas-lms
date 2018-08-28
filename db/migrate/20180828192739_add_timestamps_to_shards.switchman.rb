7b7e72f7acffa1294474ff68d0ac358df71cde1c
Cody Cutrer <cody@instructure.com>
2018-08-28 15:25:32 -0600

    only datafix default shard
    Change-Id: Ieb56eca158e45e235da5dc8d260e74501236301e
    Reviewed-on: https://gerrit.instructure.com/162439
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- db/migrate/20180828192739_add_timestamps_to_shards.switchman.rb
+++ db/migrate/20180828192739_add_timestamps_to_shards.switchman.rb
@@ -5,7 +5,7 @@ class AddTimestampsToShards < ActiveRecord::Migration[4.2]
   def change
     add_timestamps :switchman_shards, null: true
     now = Time.now.utc
-    Switchman::Shard.update_all(updated_at: now, created_at: now)
+    Switchman::Shard.update_all(updated_at: now, created_at: now) if Switchman::Shard.current.default?
     change_column_null :switchman_shards, :updated_at, false
     change_column_null :switchman_shards, :created_at, false
 
