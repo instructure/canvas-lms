a12fb419f75be63050235f2a5a65d1bc66b13628
Cody Cutrer <cody@instructure.com>
2018-08-28 10:59:14 -0600

    don't allow configuring saml_max_message_size
    fixes CORE-1784
    
    we've never adjusted it, and it's causing a thundering herd of connections
    on HUP and restart
    
    Change-Id: I2d479be48cb19de6389f03cf25e41154bb12bef6
    Reviewed-on: https://gerrit.instructure.com/162333
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -19,11 +19,6 @@ Rails.configuration.to_prepare do
   require 'saml2'
   require 'onelogin/saml'
 
-  block = -> do
-    Onelogin::Saml.config[:max_message_size] =
-      SAML2.config[:max_message_size] =
-        Setting.get('saml_max_message_size', 1.megabyte).to_i
-  end
-  block.call
-  Canvas::Reloader.on_reload(&block)
+  Onelogin::Saml.config[:max_message_size] =
+    SAML2.config[:max_message_size] = 1.megabyte
 end
