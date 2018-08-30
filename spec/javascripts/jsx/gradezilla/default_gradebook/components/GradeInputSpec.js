08c9e15bcf0c0f1bd8450fcda37d8cd6d443e039
Jeremy Neander <jneander@instructure.com>
2018-08-30 13:27:42 -0500

    spec: update enzyme in GradeInputSpec
    closes GRADE-1554
    
    Change-Id: I22ca2d0845b6779d1d817ad7a847610c2ae4b3f5
    Reviewed-on: https://gerrit.instructure.com/162795
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Tested-by: Jenkins
    Product-Review: Jeremy Neander <jneander@instructure.com>
    QA-Review: Jeremy Neander <jneander@instructure.com>

--- spec/javascripts/jsx/gradezilla/default_gradebook/components/GradeInputSpec.js
+++ spec/javascripts/jsx/gradezilla/default_gradebook/components/GradeInputSpec.js
@@ -17,7 +17,7 @@
  */
 
 import React from 'react'
-import {mount} from 'old-enzyme-2.x-you-need-to-upgrade-this-spec-to-enzyme-3.x-by-importing-just-enzyme'
+import {mount} from 'enzyme'
 import GradeInput from 'jsx/gradezilla/default_gradebook/components/GradeInput'
 
 /* eslint qunit/no-identical-names: 0 */
@@ -29,6 +29,14 @@ QUnit.module('GradeInput', suiteHooks => {
   let pendingGradeInfo
   let props
   let wrapper
+
+  function getInputValue() {
+    return wrapper.find('input').prop('value')
+  }
+
+  function getSelectValue() {
+    return wrapper.find('select').prop('value')
+  }
 
   suiteHooks.beforeEach(() => {
     assignment = {
@@ -84,43 +92,37 @@ QUnit.module('GradeInput', suiteHooks => {
 
     test('sets the formatted entered score of the submission as the input value', () => {
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '7.8')
+      strictEqual(getInputValue(), '7.8')
     })
 
     test('rounds the formatted entered score to two decimal places', () => {
       submission.enteredScore = 7.816
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '7.82')
+      strictEqual(getInputValue(), '7.82')
     })
 
     test('strips insignificant zeros', () => {
       submission.enteredScore = 8.0
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '8')
+      strictEqual(getInputValue(), '8')
     })
 
     test('is blank when the submission is not graded', () => {
       submission.enteredScore = null
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '–')
+      strictEqual(getInputValue(), '–')
     })
 
     test('is blank when the assignment has anonymized students', () => {
       props.assignment.anonymizeStudents = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '–')
+      strictEqual(getInputValue(), '–')
     })
 
     test('displays "Excused" as the input value when the submission is excused', () => {
       submission.excused = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('disables the input when the submission is excused', () => {
@@ -279,7 +281,7 @@ QUnit.module('GradeInput', suiteHooks => {
       const input = wrapper.find('input')
       input.simulate('change', {target: {value: 'EX'}})
       input.simulate('blur')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('trims whitespace from the input value when blurring', () => {
@@ -287,23 +289,21 @@ QUnit.module('GradeInput', suiteHooks => {
       const input = wrapper.find('input')
       input.simulate('change', {target: {value: ' EX '}})
       input.simulate('blur')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('does not update the input value when the submission begins updating', () => {
       mountComponent()
       const updatedSubmission = {...submission, enteredGrade: '8.9'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: true})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '7.8')
+      strictEqual(getInputValue(), '7.8')
     })
 
     test('updates the input value when the submission is replaced', () => {
       mountComponent()
       const nextSubmission = {excused: false, enteredScore: 8.9, enteredGrade: '8.9', id: '2502'}
       wrapper.setProps({submission: nextSubmission, submissionUpdating: true})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '8.9')
+      strictEqual(getInputValue(), '8.9')
     })
 
     test('updates the input value when the submission has updated', () => {
@@ -311,8 +311,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       const updatedSubmission = {...submission, enteredScore: 8.9, enteredGrade: '8.9'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '8.9')
+      strictEqual(getInputValue(), '8.9')
     })
 
     test('rounds the formatted entered score of the updated submission to two decimal places', () => {
@@ -322,8 +321,7 @@ QUnit.module('GradeInput', suiteHooks => {
         submission: {...submission, enteredScore: 7.816, enteredGrade: '7.816'},
         submissionUpdating: false
       })
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '7.82')
+      strictEqual(getInputValue(), '7.82')
     })
 
     test('strips insignificant zeros on the updated grade', () => {
@@ -333,16 +331,14 @@ QUnit.module('GradeInput', suiteHooks => {
         submission: {...submission, enteredScore: 8.0, enteredGrade: '8.00'},
         submissionUpdating: false
       })
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '8')
+      strictEqual(getInputValue(), '8')
     })
 
     test('is blank when the updated submission is not graded', () => {
       props.submissionUpdating = true
       mountComponent()
       wrapper.setProps({submission: {...submission, enteredGrade: null}, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '')
+      strictEqual(getInputValue(), '')
     })
 
     test('does not call the onSubmissionUpdate prop a submission update and the input has not changed', () => {
@@ -374,8 +370,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       wrapper.find('input').simulate('change', {target: {value: '8.9'}})
       wrapper.setProps({submission: {...submission}})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '8.9')
+      strictEqual(getInputValue(), '8.9')
     })
   })
 
@@ -400,43 +395,37 @@ QUnit.module('GradeInput', suiteHooks => {
 
     test('sets the formatted entered score of the submission as the input value', () => {
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '78%')
+      strictEqual(getInputValue(), '78%')
     })
 
     test('rounds the formatted entered score to two decimal places', () => {
       submission.enteredScore = 7.8916
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '78.92%')
+      strictEqual(getInputValue(), '78.92%')
     })
 
     test('strips insignificant zeros', () => {
       submission.enteredScore = 8
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '80%')
+      strictEqual(getInputValue(), '80%')
     })
 
     test('is blank when the submission is not graded', () => {
       submission.enteredGrade = null
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '')
+      strictEqual(getInputValue(), '')
     })
 
     test('displays "Excused" as the input value when the submission is excused', () => {
       submission.excused = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('is blank when the assignment has anonymized students', () => {
       props.assignment.anonymizeStudents = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '–')
+      strictEqual(getInputValue(), '–')
     })
 
     test('disables the input when the submission is excused', () => {
@@ -576,15 +565,14 @@ QUnit.module('GradeInput', suiteHooks => {
       const input = wrapper.find('input')
       input.simulate('change', {target: {value: 'EX'}})
       input.simulate('blur')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('does not update the input value when the submission begins updating', () => {
       mountComponent()
       const updatedSubmission = {...submission, enteredGrade: '89%'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: true})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '78%')
+      strictEqual(getInputValue(), '78%')
     })
 
     test('updates the input value when the submission has updated', () => {
@@ -592,8 +580,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       const updatedSubmission = {...submission, enteredScore: 8.9, enteredGrade: '89%'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '89%')
+      strictEqual(getInputValue(), '89%')
     })
 
     test('rounds the formatted entered score of the updated submission to two decimal places', () => {
@@ -603,8 +590,7 @@ QUnit.module('GradeInput', suiteHooks => {
         submission: {...submission, enteredScore: 7.8916, enteredGrade: '78.916%'},
         submissionUpdating: false
       })
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '78.92%')
+      strictEqual(getInputValue(), '78.92%')
     })
 
     test('strips insignificant zeros on the updated grade', () => {
@@ -614,16 +600,14 @@ QUnit.module('GradeInput', suiteHooks => {
         submission: {...submission, enteredScore: 8.9, enteredGrade: '89.00%'},
         submissionUpdating: false
       })
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '89%')
+      strictEqual(getInputValue(), '89%')
     })
 
     test('is blank when the updated submission is not graded', () => {
       props.submissionUpdating = true
       mountComponent()
       wrapper.setProps({submission: {...submission, enteredGrade: null}, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '')
+      strictEqual(getInputValue(), '')
     })
 
     test('does not call the onSubmissionUpdate prop a submission update and the input has not changed', () => {
@@ -655,8 +639,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       wrapper.find('input').simulate('change', {target: {value: '89%'}})
       wrapper.setProps({submission: {...submission}})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '89%')
+      strictEqual(getInputValue(), '89%')
     })
   })
 
@@ -682,29 +665,25 @@ QUnit.module('GradeInput', suiteHooks => {
 
     test('sets as the input value the grade corresponding to the entered score', () => {
       mountComponent()
-      const input = wrapper.find('input')
-      equal(input.prop('value'), 'C')
+      equal(getInputValue(), 'C')
     })
 
     test('is blank when the submission is not graded', () => {
       submission.enteredGrade = null
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '')
+      strictEqual(getInputValue(), '')
     })
 
     test('displays "Excused" as the input value when the submission is excused', () => {
       submission.excused = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('is blank when the assignment has anonymized students', () => {
       props.assignment.anonymizeStudents = true
       mountComponent()
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '–')
+      strictEqual(getInputValue(), '–')
     })
 
     test('disables the input when the submission is excused', () => {
@@ -853,15 +832,14 @@ QUnit.module('GradeInput', suiteHooks => {
       const input = wrapper.find('input')
       input.simulate('change', {target: {value: 'EX'}})
       input.simulate('blur')
-      strictEqual(input.prop('value'), 'Excused')
+      strictEqual(getInputValue(), 'Excused')
     })
 
     test('does not update the input value when the submission begins updating', () => {
       mountComponent()
       const updatedSubmission = {...submission, enteredScore: 10, enteredGrade: 'A'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: true})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'C')
+      strictEqual(getInputValue(), 'C')
     })
 
     test('updates the input value when the submission has updated', () => {
@@ -869,16 +847,14 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       const updatedSubmission = {...submission, enteredScore: 10, enteredGrade: 'A'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'A')
+      strictEqual(getInputValue(), 'A')
     })
 
     test('is blank when the updated submission is not graded', () => {
       props.submissionUpdating = true
       mountComponent()
       wrapper.setProps({submission: {...submission, enteredGrade: null}, submissionUpdating: false})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), '')
+      strictEqual(getInputValue(), '')
     })
 
     test('does not call the onSubmissionUpdate prop a submission update and the input has not changed', () => {
@@ -910,8 +886,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       wrapper.find('input').simulate('change', {target: {value: 'A'}})
       wrapper.setProps({submission: {...submission}})
-      const input = wrapper.find('input')
-      strictEqual(input.prop('value'), 'A')
+      strictEqual(getInputValue(), 'A')
     })
   })
 
@@ -969,30 +944,26 @@ QUnit.module('GradeInput', suiteHooks => {
     test('shows empty string if the assignment has anonymized students', () => {
       props.assignment.anonymizeStudents = true
       mountComponent()
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), '');
+      strictEqual(getSelectValue(), '');
     })
 
     test('sets the select value to "Ungraded" when the submission is not graded', () => {
       submission.enteredGrade = null
       mountComponent()
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), '', 'empty string is the value for "Ungraded"')
+      strictEqual(getSelectValue(), '', 'empty string is the value for "Ungraded"')
     })
 
     test('sets the select value to "Complete" when the submission is complete', () => {
       submission.enteredScore = 10
       submission.enteredGrade = 'complete'
       mountComponent()
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), 'complete')
+      strictEqual(getSelectValue(), 'complete')
     })
 
     test('sets the select value to "Incomplete" when the submission is incomplete', () => {
       submission.enteredGrade = 'incomplete'
       mountComponent()
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), 'incomplete')
+      strictEqual(getSelectValue(), 'incomplete')
     })
 
     test('disables the input when disabled is true', () => {
@@ -1057,8 +1028,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       const updatedSubmission = {...submission, enteredGrade: 'complete'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: true})
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), 'incomplete')
+      strictEqual(getSelectValue(), 'incomplete')
     })
 
     test('updates the input value when the submission has updated', () => {
@@ -1066,8 +1036,7 @@ QUnit.module('GradeInput', suiteHooks => {
       mountComponent()
       const updatedSubmission = {...submission, enteredGrade: 'complete'}
       wrapper.setProps({submission: updatedSubmission, submissionUpdating: false})
-      const input = wrapper.find('select')
-      strictEqual(input.prop('value'), 'complete')
+      strictEqual(getSelectValue(), 'complete')
     })
   })
 
@@ -1098,24 +1067,21 @@ QUnit.module('GradeInput', suiteHooks => {
       props.pendingGradeInfo = {grade: '1234', valid: true, excused: false}
       mountComponent()
 
-      const input = wrapper.find('input[type="text"]')
-      strictEqual(input.prop('value'), '1234')
+      strictEqual(getInputValue(), '1234')
     })
 
     test('updates the text input with the value of the pending grade when invalid', () => {
       props.pendingGradeInfo = {grade: '1234', valid: false, excused: false}
       mountComponent()
 
-      const input = wrapper.find('input[type="text"]')
-      equal(input.prop('value'), '1234')
+      equal(getInputValue(), '1234')
     })
 
     test('sets the text input to "Excused" when the pending grade is marked as excused', () => {
       props.pendingGradeInfo = {grade: '111', valid: false, excused: true}
       mountComponent()
 
-      const input = wrapper.find('input[type="text"]')
-      equal(input.prop('value'), 'Excused')
+      equal(getInputValue(), 'Excused')
     })
   })
 
