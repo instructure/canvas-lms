1486d5990c0246f81dd73f8083f2f603a6da5b8a
James Williams <jamesw@instructure.com>
2018-09-04 06:58:14 -0600

    fix precalculated read_as_admin permission for concluded teachers
    [ci no-cached-dist]
    
    test plan:
    * teachers in courses with term dates for teachers
     set in the past should still be able to view all the tabs
     in their past courses
    
    closes #CORE-1792
    
    Change-Id: I6d2204e691ee0693d8c6aa99abcdfd91ec45d76b
    Reviewed-on: https://gerrit.instructure.com/163046
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>

--- spec/lib/permissions_helper_spec.rb
+++ spec/lib/permissions_helper_spec.rb
@@ -367,6 +367,17 @@ describe PermissionsHelper do
       })
     end
 
+    it "should still let concluded term teachers read_as_admin" do
+      concluded_teacher_term = Account.default.enrollment_terms.create!(:name => "concluded")
+      concluded_teacher_term.set_overrides(Account.default, 'TeacherEnrollment' => { start_at: '2014-12-01', end_at: '2014-12-31' })
+      concluded_teacher_enrollment = course_with_teacher(user: @user, active_all: true)
+      @course.update_attributes(:enrollment_term => concluded_teacher_term)
+
+      expect(@user.precalculate_permissions_for_courses([@course], [:manage_calendar])).to eq({
+        concluded_teacher_enrollment.global_course_id => {:manage_calendar => false, :read => true, :read_as_admin => true}
+      })
+    end
+
     it 'should return true for enrollments that have permission from a direct account override' do
       student_enrollment = course_with_student(active_all: true)
       teacher_enrollment = course_with_teacher(user: @user, active_all: true)
