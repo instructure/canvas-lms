0f9b0502c01cc58344934476f5f8ee3a47d7e024
Spencer Olson <solson@instructure.com>
2018-09-04 09:40:09 -0500

    change default value for payload from string to hash literal
    closes GRADE-1568
    
    Test Plan:
    1. Before running migrations, enter a rails console and check the
       default value for the payload for an AnonymousOrModerationEvent.
       Notice that it is "{}"
    
       AnonymousOrModerationEvent.new.payload
       => "{}"
    
    2. Run migrations. Enter a rails console and check the default value
       for the payload for an AnonymousOrModerationEvent. Notice that it is
       {}.
    
       AnonymousOrModerationEvent.new.payload
       => {}
    
    Change-Id: I96e2ef5cbf9f11134d9235e437cb41d4a97a4865
    Reviewed-on: https://gerrit.instructure.com/163051
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Neander <jneander@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

---
+++ db/migrate/20180830195158_change_default_value_for_payload.rb
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+class ChangeDefaultValueForPayload < ActiveRecord::Migration[5.1]
+  tag :predeploy
+
+  def change
+    change_column_default :anonymous_or_moderation_events, :payload, from: '{}', to: {}
+  end
+end
