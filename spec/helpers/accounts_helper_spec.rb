9252e18966f05d3c781d608282d3e8f19fd48be8
wdransfield <wdransfield@instructure.com>
2018-09-05 08:38:16 -0600

    Indicate when visibility setting is delegated to parent account
    Closes PLAT-3704
    
    Test Plan:
    - Navigate to a sub account's settings page and verify the
      'Students can see originality score' select box has defaulted
      to 'Parent account setting'
    - In the root account change the value of the same setting to
      'Never'
    - In the sub account create an assignent with a plagiarism platform
      tool and verify the visibility option is 'Never'
    - In the sub account settings change the same setting to 'After
      the Due Date'
    - In the sub account create an assignment with a plagiaris platform
      tool and verify the visibility option is 'After the Due Date'
    
    Change-Id: I3d9456d90eba7d43e7355da6459c78563f9fdecd
    Reviewed-on: https://gerrit.instructure.com/163214
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Tested-by: Jenkins
    Product-Review: Jesse Poulos <jpoulos@instructure.com>

---
+++ spec/helpers/accounts_helper_spec.rb
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe AccountsHelper do
+  include AccountsHelper
+
+  describe 'turnitin_originality_options' do
+    it 'contains the expected options' do
+      expect(turnitin_originality_options.map(&:first)).to match_array [
+        'Use parent account setting',
+        'Immediately',
+        'After the assignment is graded',
+        'After the Due Date',
+        'Never'
+      ]
+    end
+  end
+end
