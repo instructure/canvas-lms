693632cf807acc780ac70f40bb25d6e45d007e17
Ryan Shaw <ryan@instructure.com>
2018-09-06 13:08:13 -0600

    Remove .getDOMNode() from InfiniteScroll.js
    Change-Id: I17ff64b459ff1a605fbc982de44159548df08e66
    Reviewed-on: https://gerrit.instructure.com/163427
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/external_apps/components/InfiniteScroll.js
+++ app/jsx/external_apps/components/InfiniteScroll.js
@@ -18,71 +18,63 @@
 
 import React from 'react'
 
-  function topPosition(domElt) {
-    if (!domElt) {
-      return 0;
-    }
-    return domElt.offsetTop + topPosition(domElt.offsetParent);
+function topPosition(domElt) {
+  if (!domElt) return 0
+  return domElt.offsetTop + topPosition(domElt.offsetParent)
+}
+
+export default class InfiniteScroll extends React.Component {
+  static defaultProps = {
+    pageStart: 0,
+    hasMore: false,
+    loadMore() {},
+    threshold: 250
   }
 
-export default React.createClass({
-    displayName: 'InfiniteScroll',
+  componentDidMount() {
+    this.pageLoaded = this.props.pageStart
+    this.attachScrollListener()
+  }
 
-    getDefaultProps() {
-      return {
-        pageStart: 0,
-        hasMore: false,
-        loadMore: function () {},
-        threshold: 250
-      };
-    },
+  componentDidUpdate() {
+    this.attachScrollListener()
+  }
 
-    componentDidMount() {
-      this.pageLoaded = this.props.pageStart;
-      this.attachScrollListener();
-    },
+  render() {
+    return (
+      <div ref={el => (this.node = el)}>
+        {this.props.children}
+        {this.props.hasMore ? this.props.loader : null}
+      </div>
+    )
+  }
 
-    componentDidUpdate() {
-      this.attachScrollListener();
-    },
-
-    render() {
-      var props = this.props;
-      return (
-        <div>
-          {this.props.children}
-          {this.props.hasMore ? this.props.loader : null}
-        </div>
-      );
-    },
-
-    handleWindowScroll() {
-      var el = this.getDOMNode();
-      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-      if (topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight < Number(this.props.threshold)) {
-        this.detachScrollListener();
-        // call loadMore after detachScrollListener to allow
-        // for non-async loadMore functions
-        this.props.loadMore(this.pageLoaded += 1);
-      }
-    },
-
-    attachScrollListener() {
-      if (!this.props.hasMore) {
-        return;
-      }
-      window.addEventListener('scroll', this.handleWindowScroll);
-      window.addEventListener('resize', this.handleWindowScroll);
-      this.handleWindowScroll();
-    },
-
-    detachScrollListener() {
-      window.removeEventListener('scroll', this.handleWindowScroll);
-      window.removeEventListener('resize', this.handleWindowScroll);
-    },
-
-    componentWillUnmount() {
-      this.detachScrollListener();
+  handleWindowScroll = () => {
+    const scrollTop =
+      window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : (document.documentElement || document.body.parentNode || document.body).scrollTop
+    if (topPosition(this.node) + this.node.offsetHeight - scrollTop - window.innerHeight < Number(this.props.threshold)) {
+      this.detachScrollListener()
+      // call loadMore after detachScrollListener to allow
+      // for non-async loadMore functions
+      this.props.loadMore((this.pageLoaded += 1))
     }
+  }
 
-  });
+  attachScrollListener() {
+    if (!this.props.hasMore) return
+    window.addEventListener('scroll', this.handleWindowScroll)
+    window.addEventListener('resize', this.handleWindowScroll)
+    this.handleWindowScroll()
+  }
+
+  detachScrollListener() {
+    window.removeEventListener('scroll', this.handleWindowScroll)
+    window.removeEventListener('resize', this.handleWindowScroll)
+  }
+
+  componentWillUnmount() {
+    this.detachScrollListener()
+  }
+}
