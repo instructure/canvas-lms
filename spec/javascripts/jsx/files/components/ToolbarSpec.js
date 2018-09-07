c6c00aee99f573cddb1c610f02ad5fa2592862ef
Ryan Shaw <ryan@instructure.com>
2018-09-07 11:01:18 -0600

    Consolidate Toolbar into one file & make work in react 15
    Change-Id: I5419149f0498f57bddb2e8ef0d0183833e301e4a
    Reviewed-on: https://gerrit.instructure.com/163521
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- spec/javascripts/jsx/files/components/ToolbarSpec.js
+++ spec/javascripts/jsx/files/components/ToolbarSpec.js
@@ -16,100 +16,135 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'react-addons-test-utils',
-  'jquery',
-  'jsx/files/Toolbar'
-], (React, TestUtils, $, Toolbar) => {
+import React from 'react'
+import {mount} from 'enzyme'
+import Toolbar from 'jsx/files/Toolbar'
+import Folder from 'compiled/models/Folder'
+import File from 'compiled/models/File'
 
-  const Folder = require('compiled/models/Folder');
-  const File = require('compiled/models/File');
+let file = null
+let courseFolder = null
+let userFolder = null
 
-  let file = null;
-  let courseFolder = null;
-  let userFolder = null;
-  let toolbarFactory = null;
-
-  const buttonEnabled = (button) => {
-    if(button.length == 1){
-      const el = button[0];
-      if(el.nodeName === 'A'){
-        return !el.disabled && el.tabIndex !== -1;
-      } else if(el.nodeName === 'BUTTON'){
-        return !el.disabled;
-      }
+const buttonEnabled = button => {
+  if (button.length == 1) {
+    const el = button.instance()
+    if (el.nodeName === 'A') {
+      return !el.disabled && el.tabIndex !== -1
+    } else if (el.nodeName === 'BUTTON') {
+      return !el.disabled
     }
-    return false;
-  };
+  }
+  return false
+}
 
-  const buttonsEnabled = (toolbar, config) => {
-    for(const prop in config){
-      const button = toolbar.find(prop);
-      if((config[prop] && buttonEnabled(button)) || (!config[prop] && !buttonEnabled(button))){
-        continue;
-      } else {
-        return false;
-      }
+const buttonsEnabled = (toolbar, config) => {
+  for (const prop in config) {
+    const button = toolbar.find(prop)
+    if ((config[prop] && buttonEnabled(button)) || (!config[prop] && !buttonEnabled(button))) {
+      continue
+    } else {
+      return false
     }
-    return true;
-  };
+  }
+  return true
+}
 
-  QUnit.module('Toolbar', {
-    setup() {
-      file = new File({id: 1});
-      courseFolder = new Folder({context_type: 'Course', context_id: 1});
-      userFolder = new Folder({context_type: 'User', context_id: 2});
-      toolbarFactory = React.createFactory(Toolbar);
-    }
-  });
+QUnit.module('Toolbar', {
+  setup() {
+    file = new File({id: 1})
+    courseFolder = new Folder({context_type: 'Course', context_id: 1})
+    userFolder = new Folder({context_type: 'User', context_id: 2})
+  }
+})
 
+test('renders multi select action items when there is more than one item selected', () => {
+  const toolbar = mount(
+    <Toolbar
+      params="foo"
+      query=""
+      selectedItems={[file]}
+      contextId="1"
+      contextType="courses"
+    />
+  )
+  ok(toolbar.find('.ui-buttonset .ui-button').exists(), 'shows multiple select action items')
+})
 
-  test('renders multi select action items when there is more than one item selected', () => {
-    const toolbar = TestUtils.renderIntoDocument(toolbarFactory({params: 'foo', query:'', selectedItems: [file], contextId: '1', contextType: 'courses'}));
-    ok($(toolbar.getDOMNode()).find('.ui-buttonset .ui-button').length, 'shows multiple select action items');
-  });
+test('renders only view and download buttons for limited users', () => {
+  const toolbar = mount(
+    <Toolbar
+      params="foo"
+      query=""
+      selectedItems={[file]}
+      currentFolder={userFolder}
+      contextId="2"
+      contextType="users"
+      userCanManageFilesForContext={false}
+    />
+  )
+  const config = {
+    '.btn-view': true,
+    '.btn-download': true,
+    '.btn-move': false,
+    '.btn-restrict': false,
+    '.btn-delete': false,
+    '.btn-add-folder': false,
+    '.btn-upload': false
+  }
+  ok(buttonsEnabled(toolbar, config), 'only view and download buttons are shown')
+})
 
-  test('renders only view and download buttons for limited users', () => {
-    const toolbar = TestUtils.renderIntoDocument(toolbarFactory({params: 'foo', query:'', selectedItems: [file], currentFolder: userFolder, contextId: '2', contextType: 'users', userCanManageFilesForContext: false}));
-    const config = {
-      '.btn-view': true,
-      '.btn-download': true,
-      '.btn-move': false,
-      '.btn-restrict': false,
-      '.btn-delete': false,
-      '.btn-add-folder': false,
-      '.btn-upload': false
-    };
-    ok(buttonsEnabled($(toolbar.getDOMNode()), config), 'only view and download buttons are shown');
-  });
+test('renders all buttons for users with manage_files permissions', () => {
+  const toolbar = mount(
+    <Toolbar
+      params="foo"
+      query=""
+      selectedItems={[file]}
+      currentFolder={courseFolder}
+      contextId="1"
+      contextType="courses"
+      userCanManageFilesForContext
+      userCanRestrictFilesForContext
+    />
+  )
 
-  test('renders all buttons for users with manage_files permissions', ()  => {
-    const toolbar = TestUtils.renderIntoDocument(toolbarFactory({params: 'foo', query:'', selectedItems: [file], currentFolder: courseFolder, contextId: '1', contextType: 'courses', userCanManageFilesForContext: true, userCanRestrictFilesForContext: true}));
-    const config = {
-      '.btn-view': true,
-      '.btn-download': true,
-      '.btn-move': true,
-      '.btn-restrict': true,
-      '.btn-delete': true,
-      '.btn-add-folder': true,
-      '.btn-upload': true
-    };
-    ok(buttonsEnabled($(toolbar.getDOMNode()), config), 'move, restrict access, delete, add folder, and upload file buttons are additionally shown for users with manage_files permissions');
-  });
+  const config = {
+    '.btn-view': true,
+    '.btn-download': true,
+    '.btn-move': true,
+    '.btn-restrict': true,
+    '.btn-delete': true,
+    '.btn-add-folder': true,
+    '.btn-upload': true
+  }
+  ok(
+    buttonsEnabled(toolbar, config),
+    'move, restrict access, delete, add folder, and upload file buttons are additionally shown for users with manage_files permissions'
+  )
+})
 
-  test('disables preview button on folder', () => {
-    const toolbar = TestUtils.renderIntoDocument(toolbarFactory({params: 'foo', query:'', selectedItems: [userFolder], currentFolder: courseFolder, contextId: '1', contextType: 'courses', userCanManageFilesForContext: true, userCanRestrictFilesForContext: true}));
-    const config = {
-      '.btn-view': false,
-      '.btn-download': true,
-      '.btn-move': true,
-      '.btn-restrict': true,
-      '.btn-delete': true,
-      '.btn-add-folder': true,
-      '.btn-upload': true
-    };
-    ok(buttonsEnabled($(toolbar.getDOMNode()), config), 'view button hidden when folder selected');
-  });
-
-});
+test('disables preview button on folder', () => {
+  const toolbar = mount(
+    <Toolbar
+      params="foo"
+      query=""
+      selectedItems={[userFolder]}
+      currentFolder={courseFolder}
+      contextId="1"
+      contextType="courses"
+      userCanManageFilesForContext
+      userCanRestrictFilesForContext
+    />
+  )
+  const config = {
+    '.btn-view': false,
+    '.btn-download': true,
+    '.btn-move': true,
+    '.btn-restrict': true,
+    '.btn-delete': true,
+    '.btn-add-folder': true,
+    '.btn-upload': true
+  }
+  ok(buttonsEnabled(toolbar, config), 'view button hidden when folder selected')
+})
