85d9e19eb827ddb5a7cc2c7c6ed939c777d6abd9
Ryan Shaw <ryan@instructure.com>
2018-09-11 14:40:23 -0600

    add packages/*/node_modules to nuke_node script
    Change-Id: I218c77b289ae8f179db74edfabd2d7bb5c5e3ba7
    Reviewed-on: https://gerrit.instructure.com/164045
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- script/nuke_node.sh
+++ script/nuke_node.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "removing node_modules and any cached/generated artifacts of any JS related files..."
-git clean client_apps packages public spec/javascripts node_modules coverage-js gems/*/node_modules app/coffeescripts -Xfd
+git clean client_apps packages public spec/javascripts node_modules coverage-js gems/*/node_modules app/coffeescripts packages/*/node_modules -Xfd
 rm -rfv gems/plugins/*/node_modules
 
 echo "yarn installing..."
