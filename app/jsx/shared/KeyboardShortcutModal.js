1a93fb643c762484265dfd1cc30032948cdaf7fa
Ryan Shaw <ryan@instructure.com>
2018-09-11 11:32:13 -0600

    Get KeyboardShortcutModal to work w/ react 16
    Change-Id: I98de04e46585cb9e16c3b870194ce64b53eac6cb
    Reviewed-on: https://gerrit.instructure.com/164013
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/shared/KeyboardShortcutModal.js
+++ app/jsx/shared/KeyboardShortcutModal.js
@@ -20,67 +20,72 @@ import React from 'react'
 import Modal from './modal'
 import ModalContent from './modal-content'
 import I18n from 'i18n!react_files'
-  var KeyboardShortcutModal = React.createClass({
-    getInitialState() {
-      return {
-        isOpen: false
-      }
-    },
-    componentDidMount() {
-      document.addEventListener("keydown", this.handleKeydown);
-    },
-    componentWillUnmount() {
-      document.removeEventListener("keydown", this.handleKeydown);
-    },
-    closeModal() {
-      this.setState({isOpen: false});
-    },
-    handleKeydown(e) {
-      // 188 is comma and 191 is forward slash
-      var keyComboPressed = e.which === 188 || (e.which === 191 && e.shiftKey);
-      if (keyComboPressed && e.target.nodeName !== "INPUT" && e.target.nodeName !== "TEXTAREA") {
-        e.preventDefault();
-        this.setState({isOpen: !this.state.isOpen});
-      }
-    },
-    shortcuts() {
-      if (this.props.shortcuts) {
-        return this.props.shortcuts.map(function(shortcut) {
-          return (
-            <li key={shortcut.keycode}>
-              <span className="keycode">{shortcut.keycode}</span>
-              <span className="colon">:</span>
-              <span className="description">{shortcut.description}</span>
-            </li>
-          );
-        })
-      }
-    },
-    render() {
-      var { title, className, styles, ...other } = this.props;
-      return (
-        <Modal isOpen={this.state.isOpen}
-               title={I18n.t("Keyboard Shortcuts")}
-               className="ReactModal__Content--canvas ReactModal__Content--mini-modal"
-               overlayClassName="ReactModal__Overlay--canvas"
-               onRequestClose={this.closeModal}
-               {...other}>
-          <ModalContent>
-            <div className="keyboard_navigation">
-              <span className="screenreader-only">
-                {I18n.t("Users of screen readers may need to turn off the virtual cursor in order to use these keyboard shortcuts")}
-              </span>
-              <ul className="navigation_list">
-                {this.shortcuts()}
-              </ul>
-              <span className="screenreader-only">
-                {I18n.t("Press the esc key to close this modal")}
-              </span>
-            </div>
-          </ModalContent>
-        </Modal>
-      );
+
+class KeyboardShortcutModal extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeydown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeydown)
+  }
+
+  closeModal = () => {
+    this.setState({isOpen: false})
+  }
+
+  handleKeydown = e => {
+    // 188 is comma and 191 is forward slash
+    const keyComboPressed = e.which === 188 || (e.which === 191 && e.shiftKey)
+    if (keyComboPressed && e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
+      e.preventDefault()
+      this.setState({isOpen: !this.state.isOpen})
     }
-  });
+  }
+
+  shortcuts = () => {
+    if (this.props.shortcuts) {
+      return this.props.shortcuts.map(shortcut => (
+        <li key={shortcut.keycode}>
+          <span className="keycode">{shortcut.keycode}</span>
+          <span className="colon">:</span>
+          <span className="description">{shortcut.description}</span>
+        </li>
+      ))
+    }
+  }
+
+  render() {
+    const {title, className, styles, ...other} = this.props
+    return (
+      <Modal
+        isOpen={this.state.isOpen}
+        title={I18n.t('Keyboard Shortcuts')}
+        className="ReactModal__Content--canvas ReactModal__Content--mini-modal"
+        overlayClassName="ReactModal__Overlay--canvas"
+        onRequestClose={this.closeModal}
+        {...other}
+      >
+        <ModalContent>
+          <div className="keyboard_navigation">
+            <span className="screenreader-only">
+              {I18n.t(
+                'Users of screen readers may need to turn off the virtual cursor in order to use these keyboard shortcuts'
+              )}
+            </span>
+            <ul className="navigation_list">{this.shortcuts()}</ul>
+            <span className="screenreader-only">
+              {I18n.t('Press the esc key to close this modal')}
+            </span>
+          </div>
+        </ModalContent>
+      </Modal>
+    )
+  }
+}
 
 export default KeyboardShortcutModal
