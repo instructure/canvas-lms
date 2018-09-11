ecec53543234b27fed679a6c8574c38966732ff5
Ryan Shaw <ryan@instructure.com>
2018-09-10 23:34:14 -0600

    react 16: convert some react.createClass’es to es6 classes
    Change-Id: I6bfd069f64ebeacae6eb983282e1db6d1b8911f9
    Reviewed-on: https://gerrit.instructure.com/163917
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/assignments/IndexMenu.js
+++ app/jsx/assignments/IndexMenu.js
@@ -19,15 +19,13 @@
 import _ from 'underscore'
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
 import I18n from 'i18n!assignment_index_menu'
 import ExternalToolModalLauncher from '../shared/ExternalToolModalLauncher'
 import Actions from './actions/IndexMenuActions'
 
-export default React.createClass({
-  displayName: 'IndexMenu',
+export default class IndexMenu extends React.Component {
 
-  propTypes: {
+  static propTypes = {
     store: PropTypes.object.isRequired,
     contextType: PropTypes.string.isRequired,
     contextId: PropTypes.number.isRequired,
@@ -37,21 +35,19 @@ export default React.createClass({
     disableSyncToSis: PropTypes.func.isRequired,
     sisName: PropTypes.string.isRequired,
     postToSisDefault: PropTypes.bool.isRequired,
-    hasAssignments: PropTypes.bool.isRequired,
-  },
+    hasAssignments: PropTypes.bool.isRequired
+  }
 
-  getInitialState () {
-    return this.props.store.getState();
-  },
+  state = this.props.store.getState()
 
   componentWillMount () {
-    this.setState(this.getInitialState());
-  },
+    this.setState(this.props.store.getState())
+  }
 
-  componentDidMount () {
+  componentDidMount() {
     this.unsubscribe = this.props.store.subscribe(() => {
-      this.setState(this.props.store.getState());
-    });
+      this.setState(this.props.store.getState())
+    })
 
     const toolsUrl = [
       '/api/v1/',
@@ -59,80 +55,84 @@ export default React.createClass({
       's/',
       this.props.contextId,
       '/lti_apps/launch_definitions?placements[]=course_assignments_menu'
-    ].join('');
+    ].join('')
 
-    this.props.store.dispatch(Actions.apiGetLaunches(null, toolsUrl));
-    this.props.setTrigger(this.refs.trigger);
-    this.props.setDisableTrigger(this.disableTrigger);
-    this.props.registerWeightToggle('weightedToggle', this.onWeightedToggle, this);
-  },
+    this.props.store.dispatch(Actions.apiGetLaunches(null, toolsUrl))
+    this.props.setTrigger(this.refs.trigger)
+    this.props.setDisableTrigger(this.disableTrigger)
+    this.props.registerWeightToggle('weightedToggle', this.onWeightedToggle, this)
+  }
 
-  componentWillUnmount () {
-    this.unsubscribe();
-  },
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
 
-  onWeightedToggle (value) {
-    this.props.store.dispatch(Actions.setWeighted(value));
-  },
+  onWeightedToggle = value => {
+    this.props.store.dispatch(Actions.setWeighted(value))
+  }
 
-  onLaunchTool (tool) {
-    return (e) => {
-      e.preventDefault();
-      this.props.store.dispatch(Actions.launchTool(tool));
-    };
-  },
-
-  closeModal () {
-    this.props.store.dispatch(Actions.setModalOpen(false));
-  },
-
-  renderWeightIcon () {
-    if (this.state && this.state.weighted) {
-      return <i className="icon-check" />;
+  onLaunchTool = tool => {
+    return e => {
+      e.preventDefault()
+      this.props.store.dispatch(Actions.launchTool(tool))
     }
-    return <i className="icon-blank" />;
-  },
+  }
 
-  renderDisablePostToSis () {
+  closeModal = () => {
+    this.props.store.dispatch(Actions.setModalOpen(false))
+  }
+
+  renderWeightIcon = () => {
+    if (this.state && this.state.weighted) {
+      return <i className="icon-check" />
+    }
+    return <i className="icon-blank" />
+  }
+
+  renderDisablePostToSis = () => {
     if (this.props.hasAssignments && this.props.postToSisDefault) {
       return (
         <li role="menuitem">
           <a
-            ref={(node) => { this.disableTrigger = node; }}
-            href="#" role="button"
+            ref={node => {
+              this.disableTrigger = node
+            }}
+            href="#"
+            role="button"
             id="assignmentDisableSyncCog"
             title={I18n.t('Disable Sync to %{name}', {name: this.props.sisName})}
             aria-label={I18n.t('Disable Sync to %{name}', {name: this.props.sisName})}
             data-focus-returns-to="course_assignment_settings_link"
-            onClick={() =>
-                  {
-                    this.props.setDisableTrigger(this.disableTrigger);
-                    this.props.disableSyncToSis();
-                  }}
+            onClick={() => {
+              this.props.setDisableTrigger(this.disableTrigger)
+              this.props.disableSyncToSis()
+            }}
           >
-            { I18n.t('Disable Sync to %{name}', {name: this.props.sisName}) }
+            {I18n.t('Disable Sync to %{name}', {name: this.props.sisName})}
           </a>
         </li>
-      );
+      )
     }
-  },
+  }
 
-  renderTools () {
-    return this.state.externalTools.map(tool =>
+  renderTools = () => {
+    return this.state.externalTools.map(tool => (
       <li key={tool.definition_id} role="menuitem">
         <a aria-label={tool.name} href="#" onClick={this.onLaunchTool(tool)}>
-          <i className="icon-import"></i>
-          { tool.name }
+          <i className="icon-import" />
+          {tool.name}
         </a>
       </li>
-    );
-  },
+    ))
+  }
 
-  render () {
+  render() {
     return (
       <div
         className="inline-block"
-        ref={(node) => { this.node = node; }}
+        ref={node => {
+          this.node = node
+        }}
       >
         <a
           className="al-trigger btn"
@@ -142,23 +142,22 @@ export default React.createClass({
           title={I18n.t('Assignments Settings')}
           aria-label={I18n.t('Assignments Settings')}
         >
-          <i className="icon-more" aria-hidden="true"/>
+          <i className="icon-more" aria-hidden="true" />
           <span className="screenreader-only">{I18n.t('Assignment Options')}</span>
         </a>
-        <ul
-          className="al-options"
-          role="menu"
-        >
+        <ul className="al-options" role="menu">
           <li role="menuitem">
             <a
               ref="trigger"
-              href="#" id="assignmentSettingsCog" role="button"
+              href="#"
+              id="assignmentSettingsCog"
+              role="button"
               title={I18n.t('Assignment Groups Weight')}
               data-focus-returns-to="course_assignment_settings_link"
               aria-label={I18n.t('Assignment Groups Weight')}
             >
-              { this.renderWeightIcon() }
-              { I18n.t('Assignment Groups Weight')}
+              {this.renderWeightIcon()}
+              {I18n.t('Assignment Groups Weight')}
             </a>
           </li>
           {this.renderDisablePostToSis()}
@@ -171,9 +170,12 @@ export default React.createClass({
           contextType={this.props.contextType}
           contextId={this.props.contextId}
           launchType="course_assignments_menu"
-          title={this.state.selectedTool && this.state.selectedTool.placements.course_assignments_menu.title}
+          title={
+            this.state.selectedTool &&
+            this.state.selectedTool.placements.course_assignments_menu.title
+          }
         />
       </div>
-    );
+    )
   }
-});
+}
