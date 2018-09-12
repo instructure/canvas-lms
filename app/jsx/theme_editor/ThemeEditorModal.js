040bed24ee097a271ac1e28e1b0310ed2dee001d
Ryan Shaw <ryan@instructure.com>
2018-09-12 11:56:45 -0600

    convert themeEditorModal to instUI modal
    Closes: CORE-1880
    
    It currently used react-modal which doesn’t work with react 16
    
    Test plan:
    * the modal that appears when you hit “preview” or “apply” should work
      the same as it used to
    
    Change-Id: Iedf471ebd4c16f083bbd72c8c4e191c44b6c045f
    Reviewed-on: https://gerrit.instructure.com/164214
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/theme_editor/ThemeEditorModal.js
+++ app/jsx/theme_editor/ThemeEditorModal.js
@@ -19,26 +19,9 @@
 import I18n from 'i18n!theme_editor'
 import React from 'react'
 import PropTypes from 'prop-types'
-import Modal from 'react-modal'
+import Modal, {ModalHeader, ModalBody} from '@instructure/ui-overlays/lib/components/Modal'
+import Heading from '@instructure/ui-elements/lib/components/Heading'
 import ProgressBar from '../shared/ProgressBar'
-
-Modal.setAppElement(document.body)
-
-const modalOverrides = {
-  overlay: {
-    backgroundColor: 'rgba(0,0,0,0.5)'
-  },
-  content: {
-    position: 'static',
-    top: '0',
-    left: '0',
-    right: 'auto',
-    bottom: 'auto',
-    borderRadius: '0',
-    border: 'none',
-    padding: '0'
-  }
-}
 
 export default class ThemeEditorModal extends React.Component {
   static propTypes = {
@@ -50,52 +33,12 @@ export default class ThemeEditorModal extends React.Component {
 
   modalOpen = () => this.props.showProgressModal || this.props.showSubAccountProgress
 
-  modalContent = () => {
-    if (this.props.showProgressModal) {
-      return this.previewGenerationModalContent()
-    } else if (this.props.showSubAccountProgress) {
-      return this.subAccountModalContent()
-    }
-  }
-
-  previewGenerationModalContent = () => (
-    <div className="ReactModal__Layout">
-      <header className="ReactModal__Header">
-        <div className="ReactModal__Header-Title">
-          <h3>{I18n.t('Generating preview...')}</h3>
-        </div>
-      </header>
-      <div className="ReactModal__Body">
-        <ProgressBar
-          progress={this.props.progress}
-          title={I18n.t('%{percent} complete', {
-            percent: I18n.toPercentage(this.props.progress, {precision: 0})
-          })}
-        />
-      </div>
-    </div>
-  )
-
-  subAccountModalContent = () => (
-    <div className="ReactModal__Layout">
-      <header className="ReactModal__Header">
-        <div className="ReactModal__Header-Title">
-          <h3>{I18n.t('Applying new styles to subaccounts')}</h3>
-        </div>
-      </header>
-      <div className="ReactModal__Body">
-        <p>{I18n.t('Changes will still apply if you leave this page.')}</p>
-        <ul className="unstyled_list">
-          {this.props.activeSubAccountProgresses.map(this.subAccountProgressBar)}
-        </ul>
-      </div>
-    </div>
-  )
-
   subAccountProgressBar = progress => (
     <li className="Theme-editor-progress-list-item">
       <div className="Theme-editor-progress-list-item__title">
-        {I18n.t('%{account_name}', {account_name: this.messageToName(progress.message)})}
+        {I18n.t('%{account_name}', {
+          account_name: this.messageToName(progress.message)
+        })}
       </div>
       <div className="Theme-editor-progress-list-item__bar">
         <ProgressBar
@@ -109,22 +52,39 @@ export default class ThemeEditorModal extends React.Component {
   )
 
   messageToName = message =>
-    message.indexOf('Syncing for') > -1
+    message.includes('Syncing for')
       ? message.replace('Syncing for ', '')
       : I18n.t('Unknown Account')
 
   render() {
     return (
-      <Modal
-        isOpen={this.modalOpen()}
-        className={
-          this.props.showProgressModal
-            ? 'ReactModal__Content--canvas ReactModal__Content--mini-modal'
-            : 'ReactModal__Content--canvas'
-        }
-        style={modalOverrides}
-      >
-        {this.modalContent()}
+      <Modal open={this.modalOpen()} size={this.props.showProgressModal ? 'small' : 'medium'}>
+        <ModalHeader>
+          <Heading>
+            {this.props.showProgressModal
+              ? I18n.t('Generating preview...')
+              : I18n.t('Applying new styles to subaccounts')}{' '}
+          </Heading>
+        </ModalHeader>
+        <ModalBody>
+          {this.props.showProgressModal ? (
+            <ProgressBar
+              progress={this.props.progress}
+              title={I18n.t('%{percent} complete', {
+                percent: I18n.toPercentage(this.props.progress, {
+                  precision: 0
+                })
+              })}
+            />
+          ) : (
+            <div>
+              <p>{I18n.t('Changes will still apply if you leave this page.')}</p>
+              <ul className="unstyled_list">
+                {this.props.activeSubAccountProgresses.map(this.subAccountProgressBar)}
+              </ul>
+            </div>
+          )}
+        </ModalBody>
       </Modal>
     )
   }
--- spec/javascripts/jsx/theme_editor/ThemeEditorModalSpec.js
+++ spec/javascripts/jsx/theme_editor/ThemeEditorModalSpec.js
@@ -16,65 +16,30 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'react-modal',
-  'jsx/theme_editor/ThemeEditorModal'
-], (React, Modal, ThemeEditorModal) => {
+import React from 'react';
+import {shallow} from 'enzyme'
+import ThemeEditorModal from 'jsx/theme_editor/ThemeEditorModal';
 
-  let elem, props
+const defaultProps = {
+  showProgressModal: false,
+  showSubAccountProgress: false,
+  progress: 0.5,
+  activeSubAccountProgresses: []
+}
 
-  QUnit.module('ThemeEditorModal Component', {
-    setup () {
-      elem = document.createElement('div')
-      props = {
-        showProgressModal: false,
-        showSubAccountProgress: false,
-        progress: 0.5,
-        activeSubAccountProgresses: []
-      }
-    }
-  })
+QUnit.module('ThemeEditorModal Component')
 
-  test('modalOpen', () => {
-    const c = new ThemeEditorModal(props)
+test('modalOpen', () => {
+  notOk(shallow(<ThemeEditorModal {...defaultProps} />).prop('open'), 'modal is closed')
 
-    notOk(c.modalOpen(), 'modal is closed')
+  ok(shallow(<ThemeEditorModal {...defaultProps} showProgressModal />).prop('open'), 'modal is open')
+  ok(shallow(<ThemeEditorModal {...defaultProps} showSubAccountProgress />).prop('open'), 'modal is open')
+})
 
-    c.props.showProgressModal = true
-    ok(c.modalOpen(), 'modal is open')
+test('modalContent', () => {
+  let wrapper = shallow(<ThemeEditorModal {...defaultProps} showProgressModal />)
+  equal(wrapper.find('ProgressBar').prop('title'), '1% complete')
 
-    c.props.showProgressModal = false
-    c.props.showSubAccountProgress = true
-    ok(c.modalOpen(), 'modal is open')
-  })
-
-  test('modalContent', () => {
-    const c = new ThemeEditorModal(props)
-    const content = {name: 'content'}
-    const subContent = {name: 'subContent'}
-    sinon.stub(c, 'previewGenerationModalContent').returns(content)
-    sinon.stub(c, 'subAccountModalContent').returns(subContent)
-
-    equal(c.modalContent(), undefined, 'no modal content')
-
-    c.props.showProgressModal = true
-    equal(c.modalContent(), content, 'returns previewGenerationModalContent')
-
-    c.props.showProgressModal = false
-    c.props.showSubAccountProgress = true
-    equal(c.modalContent(), subContent, 'returns subAccountModalContent')
-  })
-
-  test('renders a Modal', () => {
-    const c = new ThemeEditorModal(props)
-    const modalOpen = {}
-    sinon.stub(c, 'modalOpen').returns(modalOpen)
-    const modalContent = {}
-    sinon.stub(c, 'modalContent').returns(modalContent)
-    const vdom = c.render()
-    equal(vdom.type, Modal, 'renders a Modal component')
-    equal(vdom.props.isOpen, modalOpen, 'passes isOpen as prop')
-    equal(vdom.props.children, modalContent, 'passes modelContent as child')
-  })
+  wrapper = shallow(<ThemeEditorModal {...defaultProps} showSubAccountProgress />)
+  equal(wrapper.find('p').text(), 'Changes will still apply if you leave this page.')
 })
