ba7510ff4f44a781aaffcace16296abc1406b88f
Ryan Shaw <ryan@instructure.com>
2018-09-11 22:43:10 -0600

    get UsageRightsIndicator to work w/ react 16
    Change-Id: Ib7114fabe1fbca5e2b5a8156f0f90047e544a4c7
    Reviewed-on: https://gerrit.instructure.com/164130
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/files/UsageRightsIndicator.js
+++ app/jsx/files/UsageRightsIndicator.js
@@ -25,95 +25,89 @@ import Folder from 'compiled/models/Folder'
 import filesEnv from 'compiled/react_files/modules/filesEnv'
 import UsageRightsDialog from '../files/UsageRightsDialog'
 
-  var UsageRightsIndicator = React.createClass({
-    displayName: 'UsageRightsIndicator',
+export default class UsageRightsIndicator extends React.Component {
+  warningMessage = I18n.t('Before publishing this file, you must specify usage rights.')
 
-    warningMessage: I18n.t('Before publishing this file, you must specify usage rights.'),
+  static propTypes = {
+    model: customPropTypes.filesystemObject.isRequired,
+    userCanManageFilesForContext: PropTypes.bool.isRequired,
+    userCanRestrictFilesForContext: PropTypes.bool.isRequired,
+    usageRightsRequiredForContext: PropTypes.bool.isRequired,
+    modalOptions: PropTypes.object.isRequired
+  }
 
-    propTypes: {
-      model: customPropTypes.filesystemObject.isRequired,
-      userCanManageFilesForContext: PropTypes.bool.isRequired,
-      userCanRestrictFilesForContext: PropTypes.bool.isRequired,
-      usageRightsRequiredForContext: PropTypes.bool.isRequired,
-      modalOptions: PropTypes.object.isRequired
-    },
+  handleClick = event => {
+    event.preventDefault()
+    const contents = (
+      <UsageRightsDialog
+        closeModal={this.props.modalOptions.closeModal}
+        itemsToManage={[this.props.model]}
+        userCanRestrictFilesForContext={this.props.userCanRestrictFilesForContext}
+      />
+    )
+    this.props.modalOptions.openModal(contents, () => {
+      ReactDOM.findDOMNode(this).focus()
+    })
+  }
 
-    handleClick (event) {
-      event.preventDefault();
-      var contents = (
-        <UsageRightsDialog
-          closeModal={this.props.modalOptions.closeModal}
-          itemsToManage={[this.props.model]}
-          userCanRestrictFilesForContext={this.props.userCanRestrictFilesForContext}
-        />
-      );
-      this.props.modalOptions.openModal(contents, () => {
-        ReactDOM.findDOMNode(this).focus();
-      });
-    },
+  getIconData = useJustification => {
+    switch (useJustification) {
+      case 'own_copyright':
+        return {iconClass: 'icon-files-copyright', text: I18n.t('Own Copyright')}
+      case 'public_domain':
+        return {iconClass: 'icon-files-public-domain', text: I18n.t('Public Domain')}
+      case 'used_by_permission':
+        return {iconClass: 'icon-files-obtained-permission', text: I18n.t('Used by Permission')}
+      case 'fair_use':
+        return {iconClass: 'icon-files-fair-use', text: I18n.t('Fair Use')}
+      case 'creative_commons':
+        return {iconClass: 'icon-files-creative-commons', text: I18n.t('Creative Commons')}
+    }
+  }
 
-    getIconData (useJustification) {
-      switch (useJustification) {
-        case 'own_copyright':
-          return {iconClass: 'icon-files-copyright', text: I18n.t('Own Copyright')};
-        case 'public_domain':
-          return {iconClass: 'icon-files-public-domain', text: I18n.t('Public Domain')};
-        case 'used_by_permission':
-          return {iconClass: 'icon-files-obtained-permission', text: I18n.t('Used by Permission')};
-        case 'fair_use':
-          return {iconClass: 'icon-files-fair-use', text: I18n.t('Fair Use')};
-        case 'creative_commons':
-          return {iconClass: 'icon-files-creative-commons', text: I18n.t('Creative Commons')};
-      }
-    },
-
-    render () {
-      if ((this.props.model instanceof Folder) || (!this.props.usageRightsRequiredForContext && !this.props.model.get('usage_rights'))) {
-        return null;
-      } else if (this.props.usageRightsRequiredForContext && !this.props.model.get('usage_rights')) {
-        if (this.props.userCanManageFilesForContext) {
-          return (
-            <button
-              className='UsageRightsIndicator__openModal btn-link'
-              onClick={this.handleClick}
-              title={this.warningMessage}
-              data-tooltip='top'
-            >
-              <span className='screenreader-only'>
-                {this.warningMessage}
-              </span>
-              <i className='UsageRightsIndicator__warning icon-warning' />
-            </button>
-          );
-        } else {
-          return null;
-        }
-      } else {
-        var useJustification = this.props.model.get('usage_rights').use_justification;
-        var iconData = this.getIconData(useJustification);
-
+  render() {
+    if (
+      this.props.model instanceof Folder ||
+      (!this.props.usageRightsRequiredForContext && !this.props.model.get('usage_rights'))
+    ) {
+      return null
+    } else if (this.props.usageRightsRequiredForContext && !this.props.model.get('usage_rights')) {
+      if (this.props.userCanManageFilesForContext) {
         return (
           <button
-            className='UsageRightsIndicator__openModal btn-link'
+            className="UsageRightsIndicator__openModal btn-link"
             onClick={this.handleClick}
-            disabled={!this.props.userCanManageFilesForContext}
-            title={this.props.model.get('usage_rights').license_name}
-            data-tooltip='top'
+            title={this.warningMessage}
+            data-tooltip="top"
           >
-            <span
-              ref='screenreaderText'
-              className='screenreader-only'
-            >
-              {iconData.text}
-            </span>
-            <span className='screenreader-only'>
-              {this.props.model.get('usage_rights').license_name}
-            </span>
-            <i ref='icon' className={iconData.iconClass} />
+            <span className="screenreader-only">{this.warningMessage}</span>
+            <i className="UsageRightsIndicator__warning icon-warning" />
           </button>
-        );
+        )
+      } else {
+        return null
       }
-    }
-  });
+    } else {
+      const useJustification = this.props.model.get('usage_rights').use_justification
+      const iconData = this.getIconData(useJustification)
 
-export default UsageRightsIndicator
+      return (
+        <button
+          className="UsageRightsIndicator__openModal btn-link"
+          onClick={this.handleClick}
+          disabled={!this.props.userCanManageFilesForContext}
+          title={this.props.model.get('usage_rights').license_name}
+          data-tooltip="top"
+        >
+          <span ref="screenreaderText" className="screenreader-only">
+            {iconData.text}
+          </span>
+          <span className="screenreader-only">
+            {this.props.model.get('usage_rights').license_name}
+          </span>
+          <i ref="icon" className={iconData.iconClass} />
+        </button>
+      )
+    }
+  }
+}
