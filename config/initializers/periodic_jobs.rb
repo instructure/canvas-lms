911fcc78e42ded793244387496db0175228b6326
Cody Cutrer <cody@instructure.com>
2018-09-12 09:37:29 -0600

    faux singleton per-shard periodic jobs
    it's still on a strand per-db, but don't enqueue multiple per-shard
    
    Change-Id: I32185d6fddd6007ef1dbc507e88aa10556ec4995
    Reviewed-on: https://gerrit.instructure.com/164172
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/periodic_jobs.rb
+++ config/initializers/periodic_jobs.rb
@@ -29,8 +29,11 @@
 class PeriodicJobs
   def self.with_each_shard_by_database_in_region(klass, method, *args)
     Shard.with_each_shard(Shard.in_current_region) do
+      strand = "#{klass}.#{method}:#{Shard.current.database_server.id}"
+      # TODO: allow this to work with redis jobs
+      next if Delayed::Job == Delayed::Backend::ActiveRecord && Delayed::Job.where(strand: strand, shard_id: Shard.current.id, locked_by: nil).exists?
       klass.send_later_enqueue_args(method, {
-          strand: "#{klass}.#{method}:#{Shard.current.database_server.id}",
+          strand: strand,
           max_attempts: 1,
           priority: 40
       }, *args)
