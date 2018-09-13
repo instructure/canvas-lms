76162384409a436710ad5acb4ca6fe9f5ec11c05
Ryan Shaw <ryankshaw@gmail.com>
2018-09-13 10:00:38 -0600

    upgrade to canvas-rce 4.0.1 & allow react 16 (#35)

--- packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
@@ -1,5 +1,5 @@
 const Enzyme = require("enzyme")
-const Adapter = require("enzyme-adapter-react-15")
+const Adapter = require("enzyme-adapter-react-16")
 require("@instructure/ui-themes")
 
 Enzyme.configure({ adapter: new Adapter() })
