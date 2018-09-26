063497aaa7c6e87fd2062fa13cb81560b2cbf7a1
Ryan Shaw <ryan@instructure.com>
2018-09-26 14:17:16 -0600

    Fix “Assigning directly to this.state” warning in react 16
    Test plan:
    *Go to course settings page
    * click the link to upload a new course image:
    * search Flickr
    * it should work and not log:
    Warning: FlickrSearch.componentWillMount(): Assigning directly to 
    this.state is  deprecated (except inside a component's constructor). 
    Use setState instead.
    
    Change-Id: Ide331d7ea20b60c079fe914d345aca5bc07e1032
    Reviewed-on: https://gerrit.instructure.com/165946
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/shared/FlickrSearch.js
+++ app/jsx/shared/FlickrSearch.js
@@ -24,116 +24,120 @@ import FlickrImage from './FlickrImage'
 import SVGWrapper from './SVGWrapper'
 import Spinner from '@instructure/ui-elements/lib/components/Spinner'
 
-  class FlickrSearch extends React.Component {
+export default class FlickrSearch extends React.Component {
+  state = FlickrStore.getState()
 
-    constructor () {
-      super();
+  componentWillMount() {
+    this.unsubscribe = FlickrStore.subscribe(() => this.handleChange())
+  }
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleInput = this.handleInput.bind(this);
-      this.incrementPageCount = this.incrementPageCount.bind(this);
-      this.decrementPageCount = this.decrementPageCount.bind(this);
-    }
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
 
-    componentWillMount() {
-      this.state = FlickrStore.getState();
-      this.unsubscribe = FlickrStore.subscribe(this.handleChange);
-    }
+  handleChange() {
+    this.setState(FlickrStore.getState())
+  }
 
-    componentWillUnmount() {
-      this.unsubscribe();
-    }
+  handleInput = event => {
+    event.preventDefault()
+    const value = event.target.value
 
-    handleChange() {
-      this.setState(FlickrStore.getState());
-    }
-
-    handleInput (event) {
-      event.preventDefault();
-      var value = event.target.value;
-
-      if (value === '') {
-        this.clearFlickrResults();
-      }
-      else {
-        this.searchFlickr(value, 1);
-      }
-    }
-
-    searchFlickr (value, page) {
-      FlickrStore.dispatch(FlickrActions.searchFlickr(value, page));
-    }
-
-    clearFlickrResults () {
-      FlickrStore.dispatch(FlickrActions.clearFlickrSearch());
-    }
-
-    incrementPageCount () {
-      this.searchFlickr(this.state.searchTerm, this.state.page + 1);
-    }
-
-    decrementPageCount() {
-      this.searchFlickr(this.state.searchTerm, this.state.page - 1);
-    }
-
-    render () {
-      var photos = this.state.searchResults.photos;
-
-      return (
-        <div>
-          <div className="FlickrSearch__logo">
-            <SVGWrapper url="/images/flickr_logo.svg" />
-          </div>
-          <div className="ic-Input-group">
-            <div className="ic-Input-group__add-on" role="presentation" aria-hidden="true" tabIndex="-1">
-              <i className="icon-search"></i>
-            </div>
-            <input className="ic-Input"
-                   placeholder={I18n.t('Search flickr')}
-                   aria-label="Search widgets"
-                   value={this.state.searchTerm}
-                   type="search"
-                   onChange={this.handleInput} />
-          </div>
-
-          {!this.state.searching ?
-            <div className="FlickrSearch__images">
-              {photos ? photos.photo.map( (photo) => {
-                return <FlickrImage key={photo.id}
-                                    url={photo.url_m}
-                                    searchTerm={this.state.searchTerm}
-                                    selectImage={this.props.selectImage} />
-              }) : null }
-            </div>
-            :
-            <div className="FlickrSearch__loading">
-              <Spinner title="Loading"/>
-            </div>
-          }
-
-          {photos ?
-            <span className="FlickrSearch__pageNavigation">
-              {(this.state.page > 1 && !this.state.searching) ?
-                <a className="FlickrSearch__control" ref="flickrSearchControlPrev" href="#" onClick={this.decrementPageCount}>
-                  <i className="icon-arrow-open-left"/> {I18n.t('Previous')}
-                </a>
-                :
-                null
-              }
-              {(this.state.page < photos.pages && !this.state.searching) ?
-                <a className="FlickrSearch__control" ref="flickrSearchControlNext" href="#" onClick={this.incrementPageCount}>
-                  {I18n.t('Next')} <i className="icon-arrow-open-right"/>
-                </a>
-                :
-                null
-              }
-            </span>
-            :
-            null
-          }
-        </div>
-      );
+    if (value === '') {
+      this.clearFlickrResults()
+    } else {
+      this.searchFlickr(value, 1)
     }
   }
 
-export default FlickrSearch
+  searchFlickr(value, page) {
+    FlickrStore.dispatch(FlickrActions.searchFlickr(value, page))
+  }
+
+  clearFlickrResults() {
+    FlickrStore.dispatch(FlickrActions.clearFlickrSearch())
+  }
+
+  incrementPageCount = () => {
+    this.searchFlickr(this.state.searchTerm, this.state.page + 1)
+  }
+
+  decrementPageCount = () => {
+    this.searchFlickr(this.state.searchTerm, this.state.page - 1)
+  }
+
+  render() {
+    const photos = this.state.searchResults.photos
+
+    return (
+      <div>
+        <div className="FlickrSearch__logo">
+          <SVGWrapper url="/images/flickr_logo.svg" />
+        </div>
+        <div className="ic-Input-group">
+          <div
+            className="ic-Input-group__add-on"
+            role="presentation"
+            aria-hidden="true"
+            tabIndex="-1"
+          >
+            <i className="icon-search" />
+          </div>
+          <input
+            className="ic-Input"
+            placeholder={I18n.t('Search flickr')}
+            aria-label="Search widgets"
+            value={this.state.searchTerm}
+            type="search"
+            onChange={this.handleInput}
+          />
+        </div>
+
+        {!this.state.searching ? (
+          <div className="FlickrSearch__images">
+            {photos &&
+              photos.photo.map(photo => (
+                <FlickrImage
+                  key={photo.id}
+                  url={photo.url_m}
+                  searchTerm={this.state.searchTerm}
+                  selectImage={this.props.selectImage}
+                />
+              ))}
+          </div>
+        ) : (
+          <div className="FlickrSearch__loading">
+            <Spinner title="Loading" />
+          </div>
+        )}
+
+        {photos && (
+          <span className="FlickrSearch__pageNavigation">
+            {this.state.page > 1 &&
+              !this.state.searching && (
+                <a
+                  className="FlickrSearch__control"
+                  ref="flickrSearchControlPrev"
+                  href="#"
+                  onClick={this.decrementPageCount}
+                >
+                  <i className="icon-arrow-open-left" /> {I18n.t('Previous')}
+                </a>
+              )}
+            {this.state.page < photos.pages &&
+              !this.state.searching && (
+                <a
+                  className="FlickrSearch__control"
+                  ref="flickrSearchControlNext"
+                  href="#"
+                  onClick={this.incrementPageCount}
+                >
+                  {I18n.t('Next')} <i className="icon-arrow-open-right" />
+                </a>
+              )}
+          </span>
+        )}
+      </div>
+    )
+  }
+}
