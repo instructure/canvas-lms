faf38b6095fb0e44db4323b4db5beef6405e1559
Robin Kuss <rkuss@instructure.com>
2018-10-02 15:23:20 -0500

    update quizzes next contract for started at value
    Fixes: CNVS-43910
    
    Test Plan:
      - all quiz_lti lives events test run successfully locally and in jenkins
    
    Change-Id: I5a3a0ba9034269ebed3557b3dcfee98e0c7515e4
    Reviewed-on: https://gerrit.instructure.com/166682
    Tested-by: Jenkins
    Reviewed-by: Michael Hargiss <mhargiss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/contracts/service_consumers/live_events/quiz_lti/quizzes_next_quiz_duplicated_spec.rb
+++ spec/contracts/service_consumers/live_events/quiz_lti/quizzes_next_quiz_duplicated_spec.rb
@@ -28,8 +28,7 @@ RSpec.describe 'Canvas LMS Live Events', :pact_live_events do
       )
     end
 
-    xit 'keeps the contract' do
-      pending('this test fails because ContentMigraiton.new needs to be persisted CNVS-43910')
+    it 'keeps the contract' do
       live_event.emit_with do
         # arrange
         params = {
@@ -58,8 +57,16 @@ RSpec.describe 'Canvas LMS Live Events', :pact_live_events do
         new_course.enable_feature!(:quizzes_next)
 
         # act
+
+        migration = ContentMigration.create!(
+          context: Account.default,
+          user: @teacher
+        )
+        migration.started_at=Time.now.utc - 1.hour
+        migration.save!
+
         exported_content = QuizzesNext::ExportService.begin_export(old_course, {})
-        QuizzesNext::ExportService.send_imported_content(new_course, ContentMigration.new, exported_content)
+        QuizzesNext::ExportService.send_imported_content(new_course, migration, exported_content)
       end
 
       # assert
