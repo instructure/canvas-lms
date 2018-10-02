88b63f48ae72364b8e560c3fd159846b3da364df
Nate Collings <ncollings@instructure.com>
2018-10-02 13:59:13 -0600

    add a full_width_in_context external tool layout
    This layout will give you access to the full screen
    width, while still including the regular Canvas
    navigation and breadcrumbs. This differs from
    the full_width layout, which gives you the full
    width of the screen, but removes the breadcrumbs
    and Canvas sidebar.
    
    Test Plan:
    
    - Create an external tool through the API and
      set the display_type to "full_width_in_context"
    - Launch the tool and validate that the iframe has
      access to the full screen width (minus the Canvas
      navigation on the left)
    
    refs SKUNK-76
    
    Change-Id: Ie019ccc111d5688e5b7eaa4528386f350e7ab669
    Reviewed-on: https://gerrit.instructure.com/166671
    Tested-by: Jenkins
    Reviewed-by: Mark Valentine <mvalentine@instructure.com>
    Reviewed-by: Nathan Mills <nathanm@instructure.com>
    Product-Review: Karl Lloyd <karl@instructure.com>
    QA-Review: Marisa Jense <mjense@instructure.com>

--- app/controllers/external_tools_controller.rb
+++ app/controllers/external_tools_controller.rb
@@ -652,7 +652,7 @@ class ExternalToolsController < ApplicationController
   #
   # @argument account_navigation[display_type] [String]
   #   The layout type to use when launching the tool. Must be
-  #   "full_width", "borderless", or "default"
+  #   "full_width", "full_width_in_context", "borderless", or "default"
   #
   # @argument user_navigation[url] [String]
   #   The url of the external tool for user navigation
@@ -700,7 +700,7 @@ class ExternalToolsController < ApplicationController
   #
   # @argument course_navigation[display_type] [String]
   #   The layout type to use when launching the tool. Must be
-  #   "full_width", "borderless", or "default"
+  #   "full_width", "full_width_in_context", "borderless", or "default"
   #
   # @argument editor_button[url] [String]
   #   The url of the external tool
