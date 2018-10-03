e547154967555e9a8748107a0e04a85009e156a9
Gary Mei <gmei@instructure.com>
2018-10-03 16:14:53 -0500

    anonymize students in email notifications
    fixes GRADE-1630
    
    When an assignment is anonymous and still muted, students submitting
    late will not have their names exposed in the email notification.
    
    Test Plan
    - Check your teacher's notification settings for late assignment
      submissions and verify that they are set to send immediately.
    - Create a plain assignment, due yesterday.
    - Create an anonymous assignment, due yesterday.
    
    - For each assignment:
    - As a student, submit to that assignment. Have the submission
      uniquely identify the student somehow.
    - As another student, repeat the previous step.
    - As the teacher, check your email for the notifications generated by
      the submissions.
    
    - The plain assignment email notification should show the name of
      the student who submitted late. There should be a link to the
      submission details page. Verify this for both html and plain text.
    - The anonymous assignment email notification should _not_ show the
      name of the student who submitted late. There should be a link to
      the speed grader page of that submission. Verify this for both html
      and plain text.
    
    Change-Id: I3e0f07f392dc653dcc67a3cff7da409960d6a96a
    Reviewed-on: https://gerrit.instructure.com/168528
    Tested-by: Jenkins
    QA-Review: Gary Mei <gmei@instructure.com>
    Product-Review: Sidharth Oberoi <soberoi@instructure.com>
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Reviewed-by: Keith Garner <kgarner@instructure.com>

---
+++ spec/messages/assignment_submitted_late.email.html.erb_spec.rb
@@ -0,0 +1,62 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require "spec_helper"
+
+describe "assignment_submitted_late.email.html" do
+  include Rails.application.routes.url_helpers
+
+  def default_url_options
+    { host: 'localhost' }
+  end
+
+  let(:course) { Course.create!(name: "Inst101") }
+  let(:student) { course_with_user("StudentEnrollment", course: course, name: "Tom", active_all: true).user }
+  let(:assignment) { course.assignments.create!(name: "Assignment#1", due_at: 1.day.ago) }
+  let(:submission) { assignment.submit_homework(student) }
+  let(:message) { generate_message(:assignment_submitted_late, :email, submission, {}) }
+
+  include_examples "assignment submitted late email"
+
+  it "html body includes the student name" do
+    expect(
+      message.html_body
+    ).to include(
+      "Tom has just turned in a late submission for Assignment#1 in the course Inst101"
+    )
+  end
+
+  it "html body does not include the student name if assignment is anonymous and muted" do
+    assignment.update!(anonymous_grading: true)
+    expect(
+      message.html_body
+    ).to include(
+      "A student has just turned in a late submission for Assignment#1 in the course Inst101"
+    )
+  end
+
+  it "html body includes the student name if assignment is anonymous and unmuted" do
+    assignment.update!(anonymous_grading: true, muted: false)
+    assignment.unmute!
+    expect(
+      message.html_body
+    ).to include(
+      "Tom has just turned in a late submission for Assignment#1 in the course Inst101"
+    )
+  end
+end
