37a7877b74cdf7a020e9b251febd898dc447ec71
Ray Villaraza <3093207+rahkee@users.noreply.github.com>
2018-10-04 15:44:52 -0700

    Hamburger menu (#133)
    Moved menu to side bar and animated it

--- public/javascripts/tool_inline.js
+++ public/javascripts/tool_inline.js
@@ -87,7 +87,7 @@ $.trackEvent(messageType, toolName, toolPath);
 
 //Iframe resize handler
 var $tool_content_wrapper;
-var min_tool_height, canvas_chrome_height;
+var min_tool_height;
 
 $(function() {
   var $window = $(window);
@@ -108,20 +108,6 @@ $(function() {
     $(this).addClass('screenreader-only');
   })
 
-  if ( !$('body').hasClass('ic-full-screen-lti-tool') ) {
-    canvas_chrome_height = $tool_content_wrapper.offset().top + $('#footer').outerHeight(true);
-  }
-
-  // Only calculate height on resize if body does not have
-  // .ic-full-screen-lti-tool class
-  if ( $tool_content_wrapper.length && !$('body').hasClass('ic-full-screen-lti-tool') ) {
-    $window.resize(function () {
-      if (!$tool_content_wrapper.data('height_overridden')) {
-        // toolResizer.resize_tool_content_wrapper($window.height() - canvas_chrome_height - $('#sequence_footer').outerHeight(true));
-      }
-    }).triggerHandler('resize');
-  }
-
   if (ENV.LTI != null && ENV.LTI.SEQUENCE != null) {
     $('#module_sequence_footer').moduleSequenceFooter({
       assetType: 'Lti',
@@ -129,7 +115,6 @@ $(function() {
       courseID: ENV.LTI.SEQUENCE.COURSE_ID
     });
   }
-
 });
 
 window.addEventListener('message', function(e) {
@@ -142,7 +127,6 @@ window.addEventListener('message', function(e) {
         if (height <= 0) height = 1;
 
         const container = toolResizer.tool_content_wrapper(message.token || e.origin).data('height_overridden', true);
-        // toolResizer.resize_tool_content_wrapper(height, container);
         break;
 
       case 'lti.showModuleNavigation':
