b0e38c3f5acb6ab82598a875370e452a9bbd756f
Simon Williams <simon@instructure.com>
2018-10-05 11:19:16 -0600

    spec: increase selinimum lookback temporarily
    Change-Id: I50ba0c7db3241325918c25355205b5f2e7f2fb10
    Reviewed-on: https://gerrit.instructure.com/167245
    Reviewed-by: Mysti Sadler <mysti@instructure.com>
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- gems/selinimum/lib/selinimum/git.rb
+++ gems/selinimum/lib/selinimum/git.rb
@@ -27,7 +27,7 @@ module Selinimum
       end
 
       def recent_shas
-        `git log --oneline --first-parent --pretty=format:'%H'|head -n 100`.split(/\n/)
+        `git log --oneline --first-parent --pretty=format:'%H'|head -n 500`.split(/\n/)
       end
 
       def head
