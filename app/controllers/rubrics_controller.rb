f3ae09158c714c9ad8f6f2db81801767b7eb85eb
Simon Williams <simon@instructure.com>
2018-10-11 16:40:11 -0600

    api: explain non-standard rubric return value
    Change-Id: Ie883048a09d91b6a6acaaa2d1870eae1b30141d7
    Reviewed-on: https://gerrit.instructure.com/167980
    Reviewed-by: Matthew Edwards <medwards@instructure.com>
    Tested-by: Jenkins
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- app/controllers/rubrics_controller.rb
+++ app/controllers/rubrics_controller.rb
@@ -51,7 +51,16 @@ class RubricsController < ApplicationController
   end
 
   # @API Create a single rubric
+  #
   # Returns the rubric with the given id.
+  #
+  # Unfortuantely this endpoint does not return a standard Rubric object,
+  # instead it returns a hash that looks like
+  #   { 'rubric': Rubric, 'rubric_association': RubricAssociation }
+  #
+  # This may eventually be deprecated in favor of a more standardized return
+  # value, but that is not currently planned.
+  #
   # @argument id [Integer]
   #   The id of the rubric
   # @argument rubric_association_id [Integer]
@@ -67,8 +76,6 @@ class RubricsController < ApplicationController
   # @argument rubric_association[purpose] [String]
   # @argument rubric[criteria] [Hash]
   #   An indexed Hash of RubricCriteria objects where the keys are integer ids and the values are the RubricCriteria objects
-  # @returns Rubric
-
   def create
     update
   end
@@ -82,7 +89,16 @@ class RubricsController < ApplicationController
 
 
   # @API Update a single rubric
+  #
   # Returns the rubric with the given id.
+  #
+  # Unfortuantely this endpoint does not return a standard Rubric object,
+  # instead it returns a hash that looks like
+  #   { 'rubric': Rubric, 'rubric_association': RubricAssociation }
+  #
+  # This may eventually be deprecated in favor of a more standardized return
+  # value, but that is not currently planned.
+  #
   # @argument id [Integer]
   #   The id of the rubric
   # @argument rubric_association_id [Integer]
@@ -100,8 +116,6 @@ class RubricsController < ApplicationController
   # @argument rubric_association[purpose] [String]
   # @argument rubric[criteria] [Hash]
   #   An indexed Hash of RubricCriteria objects where the keys are integer ids and the values are the RubricCriteria objects
-  # @returns Rubric
-
   def update
     association_params = params[:rubric_association] ?
       params[:rubric_association].permit(:use_for_grading, :title, :purpose, :url, :hide_score_total, :hide_points, :hide_outcome_results, :bookmarked) : {}
