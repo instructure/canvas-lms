7f77c1ec937099138f96273e55035f3d1cf3171a
Rob Orton <rob@instructure.com>
2018-10-27 14:00:00 +0000

    Revert "Add Progress widget"
    This reverts commit de24e65b85b7222978019d57e94defd4c4e3d63f.
    
    Change-Id: I0d13c23684dbc2a80aa1ba2212f99107c176f4c2
    Reviewed-on: https://gerrit.instructure.com/170014
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- spec/javascripts/jsx/speed_graderSpec.js
+++ spec/javascripts/jsx/speed_graderSpec.js
@@ -680,7 +680,6 @@ QUnit.module('SpeedGrader#handleGradeSubmit', {
     sandbox.spy($.fn, 'append');
     this.originalWindowJSONData = window.jsonData;
     setupFixtures(`
-      <div id="iframe_holder"></div>
       <div id="multiple_submissions"></div>
       <a class="update_submission_grade_url" href="my_url.com" title="POST"></a>
     `);
@@ -1115,8 +1114,6 @@ QUnit.module('handleSubmissionSelectionChange', (hooks) => {
     submissions = `/submissions/{{submissionId}}`;
     params = `?download={{attachmentId}}`;
     setupFixtures(`
-      <div id="iframe_holder"></div>
-      <div id="react_pill_container"></div>
       <div id='grade_container'>
         <input type='text' id='grading-box-extended' />
       </div>
@@ -2428,7 +2425,6 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
 
         setupFixtures(`
           <div id="combo_box_container"></div>
-          <div id="react_pill_container"></div>
           <div class="comment" id="comment_fixture" style="display: none;">
             <button class="submit_comment_button"/></button>
           </div>
@@ -2659,10 +2655,6 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
           show_help_menu_item: false,
           RUBRIC_ASSESSMENT: {}
         })
-
-        setupFixtures(`
-          <div id="react_pill_container"></div>
-        `);
 
         SpeedGrader.setup()
         window.jsonData = windowJsonData
@@ -3110,7 +3102,6 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
         submissions = `/anonymous_submissions/{{anonymousId}}`;
         params = `?download={{attachmentId}}`;
         setupFixtures(`
-          <div id="react_pill_container"></div>
           <div id="full_width_container"></div>
           <div id="submission_file_hidden">
             <a
