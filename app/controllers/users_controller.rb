752dc32b9d6ae4b1994a967233102bc9f089d4a2
Mysti Sadler <mysti@instructure.com>
2018-10-27 09:41:46 -0600

    Allow users with/without due dates to get todos
    fixes ADMIN-1575
    
    Test plan
    - Set up a teacher user who also has a studet
      enrollment
    - In the teacher course, set up an assignment
      without a due date
    - Have a student in that course submit to the
      assignment
    - In the student course, set up an assignment
      with a current due date that needs to be
      submitted
    - Load /api/v1/users/todo and verify no errors
    
    Change-Id: I5367f4c148a358ddef9c223b7a56b72c316aad94
    Reviewed-on: https://gerrit.instructure.com/170015
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Tested-by: Jenkins
    Product-Review: Mysti Sadler <mysti@instructure.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -849,7 +849,7 @@ class UsersController < ApplicationController
   def todo_items
     return render_unauthorized_action unless @current_user
 
-    bookmark = Plannable::Bookmarker.new(Assignment, false, :due_at, :id)
+    bookmark = Plannable::Bookmarker.new(Assignment, false, [:due_at, :created_at], :id)
     grading_scope = @current_user.assignments_needing_grading(scope_only: true).
       reorder(:due_at, :id).preload(:external_tool_tag, :rubric_association, :rubric, :discussion_topic, :quiz).eager_load(:duplicate_of)
     submitting_scope = @current_user.
@@ -872,7 +872,7 @@ class UsersController < ApplicationController
     ]
 
     if Array(params[:include]).include? 'ungraded_quizzes'
-      quizzes_bookmark = Plannable::Bookmarker.new(Quizzes::Quiz, false, :due_at, :id)
+      quizzes_bookmark = Plannable::Bookmarker.new(Quizzes::Quiz, false, [:due_at, :created_at], :id)
       quizzes_scope = @current_user.
         ungraded_quizzes(
           :needing_submitting => true,
