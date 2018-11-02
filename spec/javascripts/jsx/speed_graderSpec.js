6fe71b81ad77b4ce66abafa1ff07ef1fb54f7eda
Nick Houle <nhoule@instructure.com>
2018-11-02 18:37:22 +0000

    Add Progress widget
    Fixes: GOOF-443, GOOF-436, GOOF-628
    
    This reverts commit 1f7dccc1a13f6c7554e68ec90af868e5b7eddde7.
    
    Test plan:
    - Compile assets for canvas
    - Boot up the docker container
    - Navigate to a course with the google lti installed
    - As a student submit an assignment from google drive
    - On the assignment detials, submission details and grade summary
      pages (found on the invision links in the ticket) confirm you
      see the progress widgets and the files correct upload status
    - On the submission details page in the grade book confirm you
      can see the icons
    - In speedgrader confirm you can see the images for the correct
      states in the iframe and see the correct icons on the right
      hand sidebar AND confirm they change accrodignly as you change
      the submission selected from the dropdown if there are multiple
      submissions
    
    Change-Id: I5e44483b07d0fde462db93838864437dd631e36d
    Reviewed-on: https://gerrit.instructure.com/170883
    Reviewed-by: Jeremy Slade <jslade@instructure.com>
    Reviewed-by: Michael Guymon <mguymon@instructure.com>
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Tested-by: Jenkins
    QA-Review: Deepeeca Soundarrajan <dsoundarrajan@instructure.com>
    Product-Review: Jesse Poulos <jpoulos@instructure.com>

--- spec/javascripts/jsx/speed_graderSpec.js
+++ spec/javascripts/jsx/speed_graderSpec.js
@@ -658,6 +658,7 @@ QUnit.module('SpeedGrader#handleGradeSubmit', {
     sandbox.spy($.fn, 'append');
     this.originalWindowJSONData = window.jsonData;
     setupFixtures(`
+      <div id="iframe_holder"></div>
       <div id="multiple_submissions"></div>
       <a class="update_submission_grade_url" href="my_url.com" title="POST"></a>
     `);
@@ -1092,6 +1093,8 @@ QUnit.module('handleSubmissionSelectionChange', (hooks) => {
     submissions = `/submissions/{{submissionId}}`;
     params = `?download={{attachmentId}}`;
     setupFixtures(`
+      <div id="iframe_holder"></div>
+      <div id="react_pill_container"></div>
       <div id='grade_container'>
         <input type='text' id='grading-box-extended' />
       </div>
@@ -2403,6 +2406,7 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
 
         setupFixtures(`
           <div id="combo_box_container"></div>
+          <div id="react_pill_container"></div>
           <div class="comment" id="comment_fixture" style="display: none;">
             <button class="submit_comment_button"/></button>
           </div>
@@ -2633,6 +2637,10 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
           show_help_menu_item: false,
           RUBRIC_ASSESSMENT: {}
         })
+
+        setupFixtures(`
+          <div id="react_pill_container"></div>
+        `);
 
         SpeedGrader.setup()
         window.jsonData = windowJsonData
@@ -3119,6 +3127,7 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
         submissions = `/anonymous_submissions/{{anonymousId}}`;
         params = `?download={{attachmentId}}`;
         setupFixtures(`
+          <div id="react_pill_container"></div>
           <div id="full_width_container"></div>
           <div id="submission_file_hidden">
             <a
