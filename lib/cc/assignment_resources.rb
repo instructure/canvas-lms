d2f1e22fbc0048777e83d506afc070e1d831fced
johnvbk <31219912+johnvbk@users.noreply.github.com>
2018-11-08 13:28:24 -0700

    Export max attempts to cartridge and copies of courses (#146)
    * Replace configuration mechanism with env var.
    
    * get and set max attempts
    
    * Put this in the right place, clean up debug stuff.

--- lib/cc/assignment_resources.rb
+++ lib/cc/assignment_resources.rb
@@ -164,6 +164,10 @@ module CC
       node.due_at CCHelper::ims_datetime(assignment.due_at, nil)
       node.lock_at CCHelper::ims_datetime(assignment.lock_at, nil)
       node.unlock_at CCHelper::ims_datetime(assignment.unlock_at, nil)
+      max_attempts = SettingsService.get_settings(object: 'assignment', id: assignment.migration_id)['max_attempts'].to_i || false
+      if max_attempts
+        node.max_attempts max_attempts
+      end
       if manifest && manifest.try(:user).present?
         node.module_locked assignment.locked_by_module_item?(manifest.user, deep_check_if_needed: true).present?
       end
