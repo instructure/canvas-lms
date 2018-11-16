3580dd2e48d11a640c45bd9eb1152b0d944788b7
Frank Murphy <fmurphy@instructure.com>
2018-11-16 16:47:23 -0500

    React-ify criterion info button.
    Fixes OUT-2849
    Fixes OUT-2857
    
    Test Plan:
    - On the course outcomes page, create a new outcome.
    - Verify the info button is a button, has screen reader text,
      and opens a modal dialog with information.
    - Edit the same outcome after creation and verify the button still
      works as outlined above.
    
    Change-Id: I34d0918c7b70f2e1d67b606d5296f3c25ab9be79
    Reviewed-on: https://gerrit.instructure.com/172767
    Tested-by: Jenkins
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>
    QA-Review: Brian Watson <bwatson@instructure.com>
    Product-Review: Neil Gupta <ngupta@instructure.com>

---
+++ app/jsx/outcomes/__tests__/CriterionInfo.test.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react'
+import { shallow } from 'enzyme'
+import CriterionInfo from '../CriterionInfo'
+
+describe('The CriterionInfo component', () => {
+  it('renders the root component as expected', () => {
+    const component = shallow(<CriterionInfo />)
+    expect(component).toMatchSnapshot()
+
+    component.find('Button').prop('onClick')()
+    component.update()
+    expect(component).toMatchSnapshot()
+  })
+})
