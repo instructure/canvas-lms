1506e683c7e064749ea0ff4e47f298b0f626ed2b
Cameron Matheson <cameron@instructure.com>
2018-11-19 13:17:17 -0700

    use apollo client instead of apollo boost
    This prepares us for using apollo's instrospection fragment matcher
    (which isn't supported with -boost).
    
    Test plan:
      pages that use graphql still work
    
    refs GQL-17
    
    Change-Id: Ib6af304855e6c0e23b598344ac1d30f4d4aa03a5
    Reviewed-on: https://gerrit.instructure.com/172936
    Tested-by: Jenkins
    Reviewed-by: Carl Kibler <ckibler@instructure.com>
    QA-Review: Cameron Matheson <cameron@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

--- app/jsx/assignments_2/teacherIndex.js
+++ app/jsx/assignments_2/teacherIndex.js
@@ -20,27 +20,13 @@ import $ from 'jquery'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ApolloClient from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
+import {ApolloProvider, client} from '../canvas-apollo'
 
 import TeacherView from './teacher/components/TeacherView'
 
-const apolloClient = new ApolloClient({
-  uri: '/api/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'GraphQL-Metrics': true,
-        'X-CSRF-Token': $.cookie('_csrf_token')
-      }
-    })
-  }
-})
-
 export default function renderAssignmentsApp(env, elt) {
   ReactDOM.render(
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <TeacherView assignmentLid={ENV.ASSIGNMENT_ID.toString()} />
     </ApolloProvider>,
     elt
--- app/jsx/canvas-apollo.js
+++ app/jsx/canvas-apollo.js
@@ -18,21 +18,39 @@
 
 import $ from 'jquery'
 import gql from 'graphql-tag'
-import ApolloClient from 'apollo-boost'
+import {ApolloClient} from 'apollo-client'
 import {InMemoryCache} from 'apollo-cache-inmemory'
+import {HttpLink} from 'apollo-link-http'
+import {onError} from 'apollo-link-error'
+import {ApolloLink} from 'apollo-link'
 import {ApolloProvider, Query} from 'react-apollo'
 
 const client = new ApolloClient({
-  uri: '/api/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'GraphQL-Metrics': true,
-        'X-CSRF-Token': $.cookie('_csrf_token')
-      }
+  link: ApolloLink.from([
+    onError(({graphQLErrors, networkError}) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({message, locations, path}) =>
+          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+        )
+      if (networkError) console.log(`[Network error]: ${networkError}`)
+    }),
+
+    new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'GraphQL-Metrics': true,
+          'X-CSRF-Token': $.cookie('_csrf_token')
+        }
+      })
+      return forward(operation)
+    }),
+
+    new HttpLink({
+      uri: '/api/graphql',
+      credentials: 'same-origin'
     })
-  },
+  ]),
   cache: new InMemoryCache({
     addTypename: true,
     dataIdFromObject: object => object.id || null
