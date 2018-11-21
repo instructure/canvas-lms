6372677929d93e213fddecabfa9bd0e495b6f710
Chris Young <beesucker@gmail.com>
2018-11-20 17:04:53 -0700

    only run if setting is on (#150)

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -292,6 +292,7 @@ class Submission < ActiveRecord::Base
   after_save :send_unit_grades_to_pipeline
 
   def send_unit_grades_to_pipeline
+    return unless SettingsService.get_settings(object: :school, id: 1)['enable_unit_grade_calculations'] == true
     PipelineService.publish(PipelineService::Nouns::UnitGrades.new(self))
   end
 
