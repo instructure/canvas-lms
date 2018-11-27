39d831f6b5808c0aa96df7a26defefa4d230becc
Cameron Matheson <cameron@instructure.com>
2018-11-27 11:44:58 -0700

    graphql: ensure nodes field is always enabled
    The graphql-ruby gem defaults to not including the nodes field for
    connections defined with the legacy api
    
    Test plan:
      All connections should now have a `nodes` field.
    
    closes GQL-19
    
    Change-Id: I5981b68cdbde73cac2ca06ca4ac90448ab348008
    Reviewed-on: https://gerrit.instructure.com/173412
    Tested-by: Jenkins
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Cameron Matheson <cameron@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

---
+++ config/initializers/graphql_connection_nodes.rb
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+# NOTE: this initializer is only necessary while some object types are still
+# using the legacy graphql api.  This should be removed when we all objects
+# have been migrated to the graphql-ruby-1.8-style api.
+
+GraphQL::Relay::ConnectionType.default_nodes_field = true
