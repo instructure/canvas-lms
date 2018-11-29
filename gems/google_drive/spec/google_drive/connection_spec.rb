06879d29a35426bed2f1b71454a67d13ba25e710
Michael Guymon <mguymon@instructure.com>
2018-11-29 16:37:01 -0500

    Custom retries for Google Drive access.
    The Google API keeps getting hit with throttle responses, which are not
    covered by the built in retries in the GoogleDrive::Client. Have to
    manually check to retry on 4xx and 5xx status codes.
    
    fixes GOOF-680
    
    Test Plan:
    * Access non-lti Google Drive
    * Mock response to Google API to send a 429
    * Verify retry attempts with backoff to access the API
    * Mock response to Google API to send a 503
    * Verify retry attempts with backoff to access the API
    
    Change-Id: I0ab095ffe1717439d06444dc37bebb9a403729c1
    Reviewed-on: https://gerrit.instructure.com/173756
    Tested-by: Jenkins
    Reviewed-by: Nick Houle <nhoule@instructure.com>
    QA-Review: Nick Houle <nhoule@instructure.com>
    Product-Review: Nick Houle <nhoule@instructure.com>

--- gems/google_drive/spec/google_drive/connection_spec.rb
+++ gems/google_drive/spec/google_drive/connection_spec.rb
@@ -20,93 +20,80 @@ require 'spec_helper'
 
 describe GoogleDrive::Connection do
 
+  let(:connection) { GoogleDrive::Connection.new(token, secret, retries: 1) }
   let(:token) { "token" }
   let(:secret) { "secret" }
-
-  before do
-    config = {
+  let(:config) do
+    {
       "api_key" => "key",
       "secret_key" => "secret",
     }
-    GoogleDrive::Connection.config = proc do
-      config
-    end
+  end
+
+  before do
+    GoogleDrive::Connection.config = proc { config }
   end
 
   describe "#file_extension from headers" do
     it "should pull the file extension from the response header" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
       headers = {
         'content-disposition' => 'attachment;filename="Testing.docx"'
       }
 
       entry = double('Entry', extension: "not")
-      file_extension = google_docs.send(:file_extension_from_header, headers, entry)
+      file_extension = connection.send(:file_extension_from_header, headers, entry)
 
       expect(file_extension).to eq("docx")
     end
 
     it "should pull the file extension from the entry if its not in the response header" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
       headers = {
         'content-disposition' => 'attachment"'
       }
 
       entry = double('Entry', extension: "not")
-      file_extension = google_docs.send(:file_extension_from_header, headers, entry)
+      file_extension = connection.send(:file_extension_from_header, headers, entry)
       expect(file_extension).to eq("not")
     end
 
     it "should use unknown as a last resort file extension" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
       headers = {
         'content-disposition' => 'attachment"'
       }
 
       entry = double('Entry', extension: "")
-      file_extension = google_docs.send(:file_extension_from_header, headers, entry)
+      file_extension = connection.send(:file_extension_from_header, headers, entry)
       expect(file_extension).to eq("unknown")
     end
 
     it "should use unknown as file extension when extension is nil" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
       headers = {}
       entry = double('Entry', extension: nil)
 
-      file_extension = google_docs.send(:file_extension_from_header, headers, entry)
+      file_extension = connection.send(:file_extension_from_header, headers, entry)
       expect(file_extension).to eq("unknown")
     end
   end
 
   describe "#normalize_document_id" do
     it "should remove prefixes" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
-      spreadsheet_id = google_docs.send(:normalize_document_id, "spreadsheet:awesome-spreadsheet-id")
+      spreadsheet_id = connection.send(:normalize_document_id, "spreadsheet:awesome-spreadsheet-id")
       expect(spreadsheet_id).to eq("awesome-spreadsheet-id")
 
-      doc_id = google_docs.send(:normalize_document_id, "document:awesome-document-id")
+      doc_id = connection.send(:normalize_document_id, "document:awesome-document-id")
       expect(doc_id).to eq("awesome-document-id")
     end
 
     it "shouldnt do anything to normalized ids" do
-      google_docs = GoogleDrive::Connection.new(token, secret)
-
-      spreadsheet_id = google_docs.send(:normalize_document_id, "awesome-spreadsheet-id")
+      spreadsheet_id = connection.send(:normalize_document_id, "awesome-spreadsheet-id")
       expect(spreadsheet_id).to eq("awesome-spreadsheet-id")
 
-      doc_id = google_docs.send(:normalize_document_id, "awesome-document-id")
+      doc_id = connection.send(:normalize_document_id, "awesome-document-id")
       expect(doc_id).to eq("awesome-document-id")
     end
   end
 
   describe "API interaction" do
-    let(:connection){ GoogleDrive::Connection.new(token, secret) }
-
     before do
       stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/drive/v2/rest").
         to_return(
@@ -117,66 +104,120 @@ describe GoogleDrive::Connection do
     end
 
     describe "#list_with_extension_filter" do
-      before(:each) do
-        stub_request(
-          :get, "https://www.googleapis.com/drive/v2/files?maxResults=0&q=trashed=false"
-        ).to_return(
-          :status => 200, :body => load_fixture('list.json'), :headers => {'Content-Type' => 'application/json'}
-        )
+      let(:http_status) { 200 }
+      let(:body) { load_fixture('list.json') }
+      let(:url) { 'https://www.googleapis.com/drive/v2/files?maxResults=0&q=trashed=false' }
+      let(:request) do
+        stub_request(:get, url).
+          to_return(
+            status: http_status,
+            body: body,
+            headers: {'Content-Type' => 'application/json'}
+          )
       end
 
-      it "should submit `trashed = false` parameter" do
-        connection.list_with_extension_filter('txt')
-        expect(WebMock).to have_requested(:get,
-                                          "https://www.googleapis.com/drive/v2/files?maxResults=0&q=trashed=false")
+      before { request }
+
+      context 'with 200 success response' do
+        it "should submit `trashed = false` parameter" do
+          connection.list_with_extension_filter('txt')
+          expect(WebMock).to have_requested(:get, url)
+        end
+
+        it "should return allowed extension" do
+          returned_list = connection.list_with_extension_filter('png')
+          expect(returned_list.files.select { |a| a.entry["fileExtension"] == 'png' }).not_to be_empty
+        end
+
+        it "should not return other extension" do
+          returned_list = connection.list_with_extension_filter('txt')
+          expect(returned_list.files).to be_empty
+        end
+
+        it "should return all extensions if no extension filter provided" do
+          returned_list = connection.list_with_extension_filter('')
+          expect(returned_list.files.select { |a| a.entry["fileExtension"] == 'png' }).not_to be_empty
+        end
       end
 
-      it "should return allowed extension" do
-        returned_list = connection.list_with_extension_filter('png')
-        expect(returned_list.files.select { |a| a.entry["fileExtension"] == 'png' }).not_to be_empty
+      context 'with 429 throttling response' do
+        let(:http_status) { 429 }
+        let(:body) { '' }
+
+        it 'will retry on ClientError' do
+          expect { connection.list_with_extension_filter('txt') }.to raise_error Google::APIClient::ClientError
+
+          expect(WebMock).to(have_requested(:get, url).times(2))
+        end
       end
 
-      it "should not return other extension" do
-        returned_list = connection.list_with_extension_filter('txt')
-        expect(returned_list.files).to be_empty
-      end
+      context 'with 503 gateway error response' do
+        let(:http_status) { 503 }
+        let(:body) { '' }
 
-      it "should return all extensions if no extension filter provided" do
-        returned_list = connection.list_with_extension_filter('')
-        expect(returned_list.files.select { |a| a.entry["fileExtension"] == 'png' }).not_to be_empty
+        it 'will retry on ServerError' do
+          expect { connection.list_with_extension_filter('txt') }.to raise_error Google::APIClient::ServerError
+
+          expect(WebMock).to(have_requested(:get, url).times(2))
+        end
       end
     end
 
     describe "#download" do
-      it "requests a download from the api client" do
-        stub_request(
-          :get, "https://www.googleapis.com/drive/v2/files/42"
-        ).to_return(
-          :status => 200, :body => load_fixture('file_data.json'), :headers => {'Content-Type' => 'application/json'}
+      let(:export_url) { 'https://docs.google.com/feeds/download/documents/export/Export?exportFormat=docx&id=**file_id**' }
+      let(:files_url) { 'https://www.googleapis.com/drive/v2/files/42' }
+      let(:http_status) { 200 }
+
+      before do
+        stub_request(:get, files_url).to_return(
+          :status => http_status,
+          body: load_fixture('file_data.json'),
+          headers: {'Content-Type' => 'application/json'}
         )
 
-        stub_request(
-          :get, "https://docs.google.com/feeds/download/documents/export/Export?exportFormat=docx&id=**file_id**"
-        ).to_return(
-          :status => 200, :body => "",
-          :headers => {
+        stub_request(:get, export_url).to_return(
+          status: 200,
+          body: '',
+          headers: {
             'Content-Type' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
           }
         )
+      end
 
+      it "requests a download from the api client" do
         output = connection.download("42", nil)
         expect(output[1]).to eq("Biology 100 Collaboration.docx")
         expect(output[2]).to eq("docx")
       end
 
       it "wraps a timeout in a drive connection exception" do
-
         allow(Timeout).to receive(:timeout).and_raise(Timeout::Error)
         expect{ connection.download("42", nil) }.to(
           raise_error(GoogleDrive::ConnectionException) do |e|
             expect(e.message).to eq("Google Drive connection timed out")
           end
         )
+      end
+
+      context 'with 429 throttling response' do
+        let(:http_status) { 429 }
+
+        it 'will retry on ClientError' do
+          expect { connection.download("42", nil) }.to raise_error Google::APIClient::ClientError
+
+          expect(WebMock).to(have_requested(:get, files_url).times(2))
+        end
+      end
+
+      context 'with 503 gateway error response' do
+        let(:http_status) { 503 }
+        let(:body) { '' }
+
+        it 'will retry on ServerError' do
+          expect { connection.download("42", nil) }.to raise_error Google::APIClient::ServerError
+
+          expect(WebMock).to(have_requested(:get, files_url).times(2))
+        end
       end
     end
 
@@ -194,7 +235,6 @@ describe GoogleDrive::Connection do
 
     describe "#authorized?" do
       it "returns false when there ConnectionException" do
-
         GoogleDrive::Connection.config = GoogleDrive::Connection.config = proc do
           nil
         end
