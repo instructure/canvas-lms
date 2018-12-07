5bb90ae2af90a14aa7821723ffd6601e734f73b3
Ed Schiebel <eschiebel@instructure.com>
2018-12-07 10:39:18 -0500

    Improve account course list table accessibility
    closes ADMIN-2212
    
    test plan
      - go to /accounts/1 (should be the table of courses)
      > expect the first column to have a "Published" heading
      > expect rows of published courses to include the published icon
      > expect screen readers to read "yes" or "no" when reading the
        published column
    
    Change-Id: I8e6a40fe1a50fd48f4edce10372481ee86266845
    Reviewed-on: https://gerrit.instructure.com/174671
    Tested-by: Jenkins
    Reviewed-by: Mysti Sadler <mysti@instructure.com>
    QA-Review: Mysti Sadler <mysti@instructure.com>
    Product-Review: Daniel Sasaki <dsasaki@instructure.com>

--- app/jsx/account_course_user_search/components/CoursesListRow.js
+++ app/jsx/account_course_user_search/components/CoursesListRow.js
@@ -25,6 +25,7 @@ import IconBlueprintLine from '@instructure/ui-icons/lib/Line/IconBlueprint'
 import IconPlusLine from '@instructure/ui-icons/lib/Line/IconPlus'
 import IconSettingsLine from '@instructure/ui-icons/lib/Line/IconSettings'
 import IconStatsLine from '@instructure/ui-icons/lib/Line/IconStats'
+import IconPublish from '@instructure/ui-icons/lib/Line/IconPublish'
 import axios from 'axios'
 import {uniqBy} from 'lodash'
 import $ from 'compiled/jquery.rails_flash_notifications'
@@ -157,13 +158,16 @@ export default class CoursesListRow extends React.Component {
 
     return (
       <tr>
-        <th scope="row">
-          {isPublished && (
-            <Tooltip tip={I18n.t('Published')}>
-              <span className="published-status published">
-                <i className="icon-publish" />
-              </span>
-            </Tooltip>
+        <th scope="row" style={{textAlign: 'center'}}>
+          {isPublished ? (
+            <span className="published-status published">
+              <IconPublish size="x-smll" />
+              <ScreenReaderContent>{I18n.t('yes')}</ScreenReaderContent>
+            </span>
+          ) : (
+            <span className="published-status unpublished">
+              <ScreenReaderContent>{I18n.t('no')}</ScreenReaderContent>
+            </span>
           )}
         </th>
         <td>
@@ -192,12 +196,11 @@ export default class CoursesListRow extends React.Component {
               />
             </div>
           ))}
-          {teachers.length > 2 &&
-            teachersToShow.length === 2 && (
-              <Button variant="link" size="small" onClick={this.showMoreTeachers}>
-                {I18n.t('Show More')}
-              </Button>
-            )}
+          {teachers.length > 2 && teachersToShow.length === 2 && (
+            <Button variant="link" size="small" onClick={this.showMoreTeachers}>
+              {I18n.t('Show More')}
+            </Button>
+          )}
         </td>
         <td>{subaccount_name}</td>
         <td>{I18n.n(total_students + newlyEnrolledStudents)}</td>
