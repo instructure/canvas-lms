bf92f2e1db609f3ab4cd0283d80cfd423ee3c14f
James Williams <jamesw@instructure.com>
2018-12-19 10:28:45 -0700

    fix broken quiz yaml with misplaced escape slash
    need to move it after the url before the quote
    
    closes #CORE-2289
    
    Change-Id: Ia1e13d79ceab35b4d31e1c87e03c313fd8797575
    Reviewed-on: https://gerrit.instructure.com/176026
    Tested-by: Jenkins
    Reviewed-by: Brent Burgoyne <bburgoyne@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>

--- spec/migrations/reinsert_assessment_question_file_verifiers_spec.rb
+++ spec/migrations/reinsert_assessment_question_file_verifiers_spec.rb
@@ -56,4 +56,50 @@ describe 'DataFixup::ReinsertAssessmentQuestionFileVerifiers' do
     expect(@quiz.reload.quiz_data.first["question_text"]).to eq original_text
     expect(linked_question.reload.question_data["question_text"]).to eq original_text
   end
+
+  it "should be able to fix the broken yaml with another fixup" do
+    course_factory
+    @att1 = attachment_with_context(@course)
+    bank = @course.assessment_question_banks.create!(:title=>'Test Bank')
+    aq = bank.assessment_questions.create!(:question_data => {
+      :question_type => 'essay_question',
+      :question_text =>
+        "File refs:
+        <img src=\"/courses/#{@course.id}/files/#{@att1.id}/download\">
+        <img src=\"/courses/#{@course.id}/files/#{@att1.id}/download?some_query=1\">
+        Here's a whole lot of weird ol' yaml with a whole lot of text\n
+        that for some reason causes it to do something different but i can't figure\n
+        out what exactly about it does that but it somehow involves unicode\n
+        hey look here's some random unicode there 𝐶𝐶=3.5𝑝𝑝\n
+        i hope it doesn't ruin everything
+        "})
+
+    aq_att = aq.attachments.first
+
+    original_text = aq.reload.question_data['question_text']
+    # the fixup will append the verifier so just rearrange them now
+    original_text.sub!("verifier=#{aq_att.uuid}&some_query=1", "some_query=1&verifier=#{aq_att.uuid}")
+
+    stripped_text = original_text.dup
+    stripped_text.gsub!("&verifier=#{aq_att.uuid}", "").gsub!("verifier=#{aq_att.uuid}", "")
+
+    expect(stripped_text).to_not include("verifier=")
+    expect(stripped_text).to include("assessment_questions/#{aq.id}/files")
+    aq.question_data["question_text"] = stripped_text
+    aq.save!
+
+    @quiz = @course.quizzes.create!
+    linked_question = @quiz.quiz_questions.create!(:question_data => aq.question_data, :assessment_question_id => aq.id)
+    @quiz.reload.publish!
+    expect(@quiz.reload.quiz_data.first["question_text"]).to eq stripped_text
+
+    DataFixup::ReinsertAssessmentQuestionFileVerifiers.run
+
+    expect { @quiz.reload.quiz_data }.to raise_error(Psych::SyntaxError)
+    expect { linked_question.reload.question_data }.to raise_error(Psych::SyntaxError)
+
+    DataFixup::FixReinsertedVerifierQuestionYaml.run # fix the broken yaml
+    expect(@quiz.reload.quiz_data.first["question_text"]).to eq original_text
+    expect(linked_question.reload.question_data["question_text"]).to eq original_text
+  end
 end
