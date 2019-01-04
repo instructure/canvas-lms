14b57bbbb5686018f7670d3d32224bc37ef5ebd2
Clay Diffrient <cdiffrient@instructure.com>
2019-01-04 09:28:38 -0700

    Make links for more information underlined
    This also updates using InstUI to 5.37
    
    closes CORE-2097
    
    Test Plan:
      - yarn start
      - Run the a11y checker
      - Click the (?)
      - The "Learn more about..." link should be underlined

--- packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/jest-setup.js
@@ -5,3 +5,10 @@ require("@instructure/ui-themes")
 Enzyme.configure({ adapter: new Adapter() })
 
 document.documentElement.setAttribute("dir", "ltr")
+
+// set up mocks for native APIs
+if (!("MutationObserver" in window)) {
+  Object.defineProperty(window, "MutationObserver", {
+    value: require("mutation-observer")
+  })
+}
