30648267092e13f97170c4f557562f753895bd69
Cameron Matheson <cameron@instructure.com>
2019-01-21 16:08:58 -0700

    mock testing for vanilla apollo client
    react-apollo can be tested with MockProvider.  This commit introduces
    similar functionality for code that deals with apollo client directly.
    
    Test plan:
      * specs pass
    
    closes GQL-36
    
    Change-Id: Ib9c6598d051d0b623c3f24de9fb673e6cfc5d124
    Reviewed-on: https://gerrit.instructure.com/178763
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    QA-Review: Cameron Matheson <cameron@instructure.com>
    Product-Review: Cameron Matheson <cameron@instructure.com>

---
+++ spec/javascripts/apollo_without_react_spec.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MockCanvasClient from './support/MockCanvasClient'
+import {createClient, gql} from 'jsx/canvas-apollo'
+
+QUnit.module('Apollo without React demo spec', hooks => {
+  const COURSE_INFO_QUERY = gql`
+    query courseInfo($id: ID!) {
+      course(id: $id) {
+        id
+        name
+      }
+    }
+  `
+
+  const getCourseInfo = courseId =>
+    createClient()
+      .query({
+        query: COURSE_INFO_QUERY,
+        variables: {id: courseId}
+      })
+      .then(({data}) => {
+        const {id, name} = data.course
+        return `${id} - ${name}`
+      })
+      .catch(_err => 'Something went wrong')
+
+  hooks.beforeEach(() => {
+    MockCanvasClient.install([
+      {
+        request: {
+          query: COURSE_INFO_QUERY,
+          variables: {id: 'GOOD'}
+        },
+        result: {
+          data: {
+            course: {
+              id: 'asdf',
+              name: 'First Course',
+              __typename: 'Course'
+            }
+          }
+        }
+      },
+      {
+        request: {
+          query: COURSE_INFO_QUERY,
+          variables: {id: 'ERR1'}
+        },
+        result: {
+          errors: [{message: 'uh oh graphql response error'}]
+        }
+      },
+      {
+        request: {
+          query: COURSE_INFO_QUERY,
+          variables: {id: 'ERR2'}
+        },
+        error: new Error('uh oh transport or other kind of error')
+      }
+    ])
+  })
+
+  hooks.afterEach(() => {
+    MockCanvasClient.uninstall()
+  })
+
+  test('works', async () => {
+    const response = await getCourseInfo('GOOD')
+    equal(response, 'asdf - First Course')
+  })
+
+  test('works with query response errors', async () => {
+    const response = await getCourseInfo('ERR1')
+    equal(response, 'Something went wrong')
+  })
+
+  test('works when something goes wrong', async () => {
+    const response = await getCourseInfo('ERR2')
+    equal(response, 'Something went wrong')
+  })
+})
---
+++ spec/javascripts/support/MockCanvasClient.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This apolloClient is meant to be used in old pre-React Canvas (for testing
+ * React components that use Apollo, see the `MockProvider` in the Apollo
+ * docs).
+ *
+ * see apollo_without_react_spec.js for examples of this module.
+ */
+
+import {createClient} from 'jsx/canvas-apollo'
+import {MockLink} from 'react-apollo/test-links'
+
+/*
+ * React components should use Apollo's MockProvider instead of MockCanvasClient.
+ *
+ * Make sure your mocked query/variables exactly match the query/variables your
+ * script will issue.
+ */
+
+const MockCanvasClient = {
+  install: mocks => {
+    createClient.mockLink = new MockLink(mocks, true)
+  },
+
+  uninstall: () => {
+    delete createClient.mockLink
+  }
+}
+
+export default MockCanvasClient
