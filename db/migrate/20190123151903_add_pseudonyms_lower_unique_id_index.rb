07816edf38f041f000492a58c4dbc3d0ec2f1b01
James Williams <jamesw@instructure.com>
2019-01-23 08:44:39 -0700

    add unique_id and account_id index to pseudonyms
    closes #CORE-2386
    
    Change-Id: I1061e68427b66bf99a866a9853b356a9275f1c9a
    Reviewed-on: https://gerrit.instructure.com/179016
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

---
+++ db/migrate/20190123151903_add_pseudonyms_lower_unique_id_index.rb
@@ -0,0 +1,8 @@
+class AddPseudonymsLowerUniqueIdIndex < ActiveRecord::Migration[5.1]
+  tag :postdeploy
+  disable_ddl_transaction!
+
+  def change
+    add_index :pseudonyms, "LOWER(unique_id), account_id", name: "index_pseudonyms_on_unique_id_and_account_id", algorithm: :concurrently
+  end
+end
