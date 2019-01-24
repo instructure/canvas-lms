9920f9fb151934f4f0babd9752834c10659b21a9
wdransfield <wdransfield@instructure.com>
2019-01-24 08:28:03 -0700

    Add AssignmentGroup updated and created events
    Closes PLAT-4070
    
    Test Plan:
    - Create an assignment group
    - Verify an assignment_group_created event is triggered
    - Edit the assignment group
    - Verify an assignment_group_updated event is triggered
    
    Change-Id: I68a90c7dc733394c9a4a8853d6803dec569c3834
    Reviewed-on: https://gerrit.instructure.com/179194
    Tested-by: Jenkins
    Reviewed-by: Marc Phillips <mphillips@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

--- app/observers/live_events_observer.rb
+++ app/observers/live_events_observer.rb
@@ -28,6 +28,7 @@ class LiveEventsObserver < ActiveRecord::Observer
           :group_membership,
           :wiki_page,
           :assignment,
+          :assignment_group,
           :submission,
           :attachment,
           :user,
@@ -81,6 +82,8 @@ class LiveEventsObserver < ActiveRecord::Observer
       end
     when Assignment
       Canvas::LiveEvents.assignment_updated(obj)
+    when AssignmentGroup
+      Canvas::LiveEvents.assignment_group_updated(obj)
     when Attachment
       if attachment_eligible?(obj)
         if changes["display_name"]
@@ -135,6 +138,8 @@ class LiveEventsObserver < ActiveRecord::Observer
       Canvas::LiveEvents.wiki_page_created(obj)
     when Assignment
       Canvas::LiveEvents.assignment_created(obj)
+    when AssignmentGroup
+      Canvas::LiveEvents.assignment_group_created(obj)
     when Submission
       Canvas::LiveEvents.submission_created(obj)
     when UserAccountAssociation
