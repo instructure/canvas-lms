92415a75d4401692a10dbae38c296e2b7ad042df
Jeremy Putnam <jeremyp@instructure.com>
2019-01-24 14:15:07 -0700

    spec: add missing groups_pages_teacher_spec tests back to RCS p1
    Change-Id: I6abfd7a1ccc08d4b52ca695ec21aa716ef093a41
    closes: CORE-2408
    Reviewed-on: https://gerrit.instructure.com/179280
    Tested-by: Jenkins
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Jeremy Putnam <jeremyp@instructure.com>

--- spec/selenium/rcs/groups_pages_teacher_spec.rb
+++ spec/selenium/rcs/groups_pages_teacher_spec.rb
@@ -16,6 +16,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 require_relative '../common'
+require_relative '../announcements/pages/announcement_index_page'
+require_relative '../announcements/pages/announcement_new_edit_page'
 require_relative '../helpers/groups_common'
 require_relative '../helpers/legacy_announcements_common'
 require_relative '../helpers/discussions_common'
@@ -50,6 +52,156 @@ describe "groups" do
     before :each do
       user_session(@teacher)
       stub_rcs_config
+    end
+
+    #-------------------------------------------------------------------------------------------------------------------
+    describe "home page" do
+      it_behaves_like 'home_page', :teacher
+    end
+
+    describe "announcements page v2" do
+      # will fix the below shared tests in a separate commit
+      # it_behaves_like 'announcements_page_v2', :teacher
+
+      it "should allow teachers to see announcements", ignore_js_errors: true do
+        @announcement = @testgroup.first.announcements.create!(
+          title: 'Group Announcement',
+          message: 'Group',
+          user: @students.first
+        )
+        AnnouncementIndex.visit_groups_index(@testgroup.first)
+        expect(ff('.ic-announcement-row').size).to eq 1
+      end
+
+      it "should allow teachers to create an announcement", ignore_js_errors: true do
+        # Checks that initial user can create an announcement
+        AnnouncementNewEdit.create_group_announcement(@testgroup.first,
+                                                      "Announcement by #{@teacher.name}", 'sup')
+        get announcements_page
+        expect(ff('.ic-announcement-row').size).to eq 1
+      end
+
+      it "should allow teachers to delete their own group announcements", ignore_js_errors: true do
+        skip_if_safari(:alert)
+        @testgroup.first.announcements.create!(
+          title: 'Student Announcement',
+          message: 'test message',
+          user: @teacher
+        )
+
+        get announcements_page
+        expect(ff('.ic-announcement-row').size).to eq 1
+        AnnouncementIndex.delete_announcement_manually("Student Announcement")
+        expect(f(".announcements-v2__wrapper")).not_to contain_css('.ic-announcement-row')
+      end
+
+      it "should allow teachers to delete group member announcements", ignore_js_errors: true do
+        skip_if_safari(:alert)
+        @testgroup.first.announcements.create!(
+          title: 'Student Announcement',
+          message: 'test message', user:
+            @students.first
+        )
+
+        get announcements_page
+        expect(ff('.ic-announcement-row').size).to eq 1
+        AnnouncementIndex.delete_announcement_manually("Student Announcement")
+        expect(f(".announcements-v2__wrapper")).not_to contain_css('.ic-announcement-row')
+      end
+
+      it "should let teachers see announcement details", ignore_js_errors: true do
+        announcement = @testgroup.first.announcements.create!(
+          title: 'Test Announcement',
+          message: 'test message',
+          user: @teacher
+        )
+        get announcements_page
+        expect_new_page_load { AnnouncementIndex.click_on_announcement(announcement.title) }
+        expect(f('.discussion-title').text).to eq 'Test Announcement'
+        expect(f('.message').text).to eq 'test message'
+      end
+
+      it "edit button from announcement details works on teachers announcement" do
+        announcement = @testgroup.first.announcements.create!(
+          title: 'Test Announcement',
+          message: 'test message',
+          user: @teacher
+        )
+        url_base = AnnouncementNewEdit.full_individual_announcement_url(
+          @testgroup.first,
+          announcement
+        )
+        get url_base
+        expect_new_page_load { f('.edit-btn').click }
+        expect(driver.current_url).to include "#{url_base}/edit"
+        expect(f('#content-wrapper')).not_to contain_css('#sections_autocomplete_root input')
+      end
+
+      it "edit page should succeed for their own announcements" do
+        announcement = @testgroup.first.announcements.create!(
+          title: "Announcement by #{@user.name}",
+          message: 'The Force Awakens',
+          user: @teacher
+        )
+        AnnouncementNewEdit.edit_group_announcement(@testgroup.first, announcement,
+                                                    "Canvas will be rewritten in chicken")
+        announcement.reload
+        # Editing *appends* to existing message, and the resulting announcement's
+        # message is wrapped in paragraph tags
+        expect(announcement.message).to eq("<p>The Force AwakensCanvas will be rewritten in chicken</p>")
+      end
+
+      it "should let teachers edit group member announcements" do
+        announcement = @testgroup.first.announcements.create!(
+          title: 'Your Announcement',
+          message: 'test message',
+          user: @students.first
+        )
+        url_base = AnnouncementNewEdit.full_individual_announcement_url(
+          @testgroup.first,
+          announcement
+        )
+        get url_base
+        expect_new_page_load { f('.edit-btn').click }
+        expect(driver.current_url).to include "#{url_base}/edit"
+        expect(f('#content-wrapper')).not_to contain_css('#sections_autocomplete_root input')
+      end
+
+      it "edit page should succeed for group member announcements" do
+        announcement = @testgroup.first.announcements.create!(
+          title: "Announcement by #{@user.name}",
+          message: 'The Force Awakens',
+          user: @students.first
+        )
+        AnnouncementNewEdit.edit_group_announcement(@testgroup.first, announcement,
+                                                    "Canvas will be rewritten in chicken")
+        announcement.reload
+        # Editing *appends* to existing message, and the resulting announcement's
+        # message is wrapped in paragraph tags
+        expect(announcement.message).to eq("<p>The Force AwakensCanvas will be rewritten in chicken</p>")
+      end
+    end
+
+    #-------------------------------------------------------------------------------------------------------------------
+    describe "people page" do
+      it_behaves_like 'people_page', :teacher
+
+      it "should display and show a list of group members", priority: "2", test_id: 324929 do
+        get people_page
+        # Checks that all students and teachers created in setup are listed on page
+        expect(ff('.student_roster .user_name').size).to eq 4
+        expect(ff('.teacher_roster .user_name').size).to eq 2
+      end
+
+      it "shows both active and inactive members in groups to teachers", priority: "2", test_id: 2771091 do
+        get people_page
+        expect(ff('.student_roster .user_name').size).to eq 4
+        student_enrollment = StudentEnrollment.last
+        student_enrollment.workflow_state = "inactive"
+        student_enrollment.save!
+        refresh_page
+        expect(ff('.student_roster .user_name').size).to eq 4
+      end
     end
   end
 end
