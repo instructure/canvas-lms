30d5d514ec62260067232ba787564600b6b13224
Rob Orton <rob@instructure.com>
2019-02-05 14:12:09 -0700

    only allow resetting password every 5 minutes
    fixes CORE-2483
    
    test plan
     - reset password for canvas auth
     - do it again
     - you should only get one reset through
    
    Change-Id: I8bf06380c003723541ad932a13405d2414a90e9c
    Reviewed-on: https://gerrit.instructure.com/180626
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/models/communication_channel.rb
+++ app/models/communication_channel.rb
@@ -271,7 +271,9 @@ class CommunicationChannel < ActiveRecord::Base
   end
 
   def forgot_password!
+    return if Rails.cache.read(['recent_password_reset', self.global_id].cache_key) == true
     @request_password = true
+    Rails.cache.write(['recent_password_reset', self.global_id].cache_key, true, expires_in: Setting.get('resend_password_reset_time', 5).to_f.minutes)
     set_confirmation_code(true, Setting.get('password_reset_token_expiration_minutes', '120').to_i.minutes.from_now)
     self.save!
     @request_password = false
