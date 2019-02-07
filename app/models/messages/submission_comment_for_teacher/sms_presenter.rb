5152b4e1be7a1f96395bdafae5ebf79390956bff
Jeremy Neander <jneander@instructure.com>
2019-02-07 13:12:11 -0600

    anonymize submission comment for teacher notifications
    fixes GRADE-1988
    
    When an assignment is anonymous and still muted, comments from students
    will not have their names exposed in notifications to the teacher.
    
    test plan:
     * Submission Comment Steps
        1. Sign in or act as the first student
        2. Submit to the anonymous assignment
        3. Submit to the non-anonymous assignment
        4. Sign in or act as either student
        5. Comment on the anonymous submission
        6. Comment on the non-anonymous submission
    
     * Notification Preferences are found at
        /profile/communication
    
     A. Setup
        1. Enroll two students
        2. Create an anonymous assignment, due in the future
           * allow resubmissions
        3. Create a non-anonymous assignment, due in the future
           * allow resubmissions
    
     B. Test Plan for Immediate Email
        1. Set your "All Submissions" notification preferences for immediate
           email
        2. Perform the Submission Comment Steps above
        3. As the teacher, find the resubmission emails
    
        4. Verify the student names are present for the non-anonymous email
        5. Verify the student names are absent for the anonymous email
    
     C. Test Plan for Daily/Weekly Email
        1. Set your "All Submissions" notification preferences for daily/weekly
           email
        2. Perform the Submission Comment Steps above
        3. Wait a day, or a week, ... or whatever
    
        4. As the teacher, find the resubmission emails
        5. Verify the student names are present for the non-anonymous email
        6. Verify the student names are absent for the anonymous email
    
     D. Test Plan for Twitter
        1. Set your "All Submissions" notification preferences for Twitter
        2. Perform the Submission Comment Steps above
        3. As the teacher, check your Twitter
    
        4. Verify the student names are present for the non-anonymous message
        5. Verify the student names are absent for the anonymous message
    
     E. Test Plan for SMS
        1. Set your "All Submissions" notification preferences for Twitter
        2. Perform the Submission Comment Steps above
        3. As the teacher, fumble with your phone while using the bathroom
           If you drop it in the toilet, you MUST repeat all steps above.
    
        4. Verify the student names are present for the non-anonymous text
        5. Verify the student names are absent for the anonymous text
    
    Change-Id: I9d08df409e5bb925cf5c479ddd822b66987a2ccf
    Reviewed-on: https://gerrit.instructure.com/181208
    Tested-by: Jenkins
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: Keith Garner <kgarner@instructure.com>

---
+++ app/models/messages/submission_comment_for_teacher/sms_presenter.rb
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2019 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Messages::SubmissionCommentForTeacher
+  class SMSPresenter < Presenter
+    def subject
+      if anonymous?
+        if anonymous_author_id.present?
+          I18n.t(
+            "New anonymous comment by Student (%{author_id}) for %{assignment_title}, Student (%{user_id}), %{course_name}.",
+            assignment_title: assignment.title,
+            author_id: anonymous_author_id,
+            course_name: course.name,
+            user_id: submission.anonymous_id
+          )
+        else
+          I18n.t(
+            "New anonymous comment for %{assignment_title}, Student (%{user_id}), %{course_name}.",
+            assignment_title: assignment.title,
+            course_name: course.name,
+            user_id: submission.anonymous_id
+          )
+        end
+      else
+        I18n.t(
+          "New comment by %{author_name} for %{assignment_title}, %{user_name}, %{course_name}.",
+          assignment_title: assignment.title,
+          author_name: submission_comment.author.name,
+          course_name: course.name,
+          user_name: submission.user.name
+        )
+      end
+    end
+
+    def body
+      I18n.t(
+        "More info at %{web_address}",
+        web_address: HostUrl.context_host(assignment.context)
+      )
+    end
+  end
+end
