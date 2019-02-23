96315b8626b1f458446c84cf082e9665d7bca4ea
Brent Burgoyne <bburgoyne@instructure.com>
2019-02-22 23:41:33 -0700

    support testing coffeescript with jest
    Change-Id: I540ad44c033e489ff49c251256263d547ca258cd
    Reviewed-on: https://gerrit.instructure.com/182600
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Brent Burgoyne <bburgoyne@instructure.com>

---
+++ jest/coffeeTransformer.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const {compile} = require('coffee-script')
+const {transform} = require('babel-core')
+
+exports.process = coffee => {
+  const amd = compile(coffee, {bare: true})
+  const cjs = transform(amd, {plugins: ['transform-amd-to-commonjs']}).code
+  return cjs
+}
