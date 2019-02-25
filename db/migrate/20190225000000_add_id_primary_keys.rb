a68fbf0528ef91c58c93f9ea245c85c0e52aeaf6
Harry Li <hcli.consulting@gmail.com>
2019-02-25 16:41:22 -0500

    bring the primary back
    just adding some primary key id columns to tables that were missing it

---
+++ db/migrate/20190225000000_add_id_primary_keys.rb
@@ -0,0 +1,10 @@
+class AddPrimaryIdColumns < ActiveRecord::Migration[5.2]
+  def change
+    tables_to_modify = %i(
+      assignment_student_visibilities
+      quiz_student_visibilities
+    )
+
+    tables_to_modify.each { |t| add_column t, :id, :primary_key }
+  end
+end
