419df5132ed6334bd504f6968930c04793d4c0bc
James Butters <jbutters@instructure.com>
2019-02-28 10:41:19 -0700

    spec: better locator for submit and wait after submitting reply
    Change-Id: I5a3e4eb224f7fda01168bd489cc323b809205ffa
    Reviewed-on: https://gerrit.instructure.com/183272
    Tested-by: Jenkins
    Reviewed-by: KC Naegle <knaegle@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- spec/selenium/helpers/discussions_common.rb
+++ spec/selenium/helpers/discussions_common.rb
@@ -88,8 +88,9 @@ module DiscussionsCommon
       @last_entry.find_element(:css, '.discussion-reply-attachments input').send_keys(fullpath)
     end
 
-    scroll_to_submit_button_and_click(@last_entry.find_element(:css, ".discussion-reply-form"))
-    wait_for_ajaximations
+    fj('button:contains("Post Reply")').click
+    wait_for(method: nil, timeout: 5) { f('#discussion_subentries .discussion_entry').displayed? }
+
     id = DiscussionEntry.last.id
     @last_entry = f "#entry-#{id}"
   end
