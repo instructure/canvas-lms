0fa3dc36eb72fd69768e3f0a916054e9e57a7fa1
Adrian Packel <apackel@instructure.com>
2019-03-18 16:00:11 -0500

    Add posted_at to speedgrader json
    closes GRADE-62
    
    Test plan:
    - Enable post policies
    - Set the posted_at date for a submission
    - Open SpeedGrader
      - Observe that the field is included in submission JSON, e.g.:
    
        > window.jsonData.studentsWithSubmissions[0].submission.posted_at
    
      - (it should be the date you set, or nil for unposted submissions)
    
    Change-Id: I4ad286c8038be4a469ce1496a80a206969151035
    Reviewed-on: https://gerrit.instructure.com/186195
    Tested-by: Jenkins
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Reviewed-by: Gary Mei <gmei@instructure.com>
    QA-Review: Adrian Packel <apackel@instructure.com>
    Product-Review: Keith Garner <kgarner@instructure.com>

--- app/models/speed_grader/assignment.rb
+++ app/models/speed_grader/assignment.rb
@@ -42,6 +42,7 @@ module SpeedGrader
                                   grading_period_id excused updated_at)
 
       submission_json_fields << (anonymous_students?(current_user: @current_user, assignment: @assignment) ? :anonymous_id : :user_id)
+      submission_json_fields << :posted_at if @course.feature_enabled?(:post_policies)
 
       attachment_json_fields = %i(id comment_id content_type context_id context_type display_name
                                   filename mime_class size submitter_id workflow_state)
