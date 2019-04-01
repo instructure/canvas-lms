31dd43b9b915222a5eebce90a9f36db5ac388fdf
Cody Cutrer <cody@instructure.com>
2019-03-31 18:35:58 -0600

    prevent a named scope from getting modified and shared
    Change-Id: I08b1e8b994f70ed154ac0a873d3b8c237d65ff2b
    Reviewed-on: https://gerrit.instructure.com/187723
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- config/initializers/switchman.rb
+++ config/initializers/switchman.rb
@@ -109,16 +109,15 @@ Rails.application.config.after_initialize do
     end
 
     scope :in_current_region, -> do
-      @current_region_scope ||=
-        if !default.is_a?(Switchman::Shard)
-          # sharding isn't set up? maybe we're in tests, or a somehow degraded environment
-          # either way there's only one shard, and we always want to see it
-          [default]
-        elsif !ApplicationController.region || DatabaseServer.all.all? { |db| !db.config[:region] }
-          all
-        else
-          in_region(ApplicationController.region)
-        end
+      if !default.is_a?(Switchman::Shard)
+        # sharding isn't set up? maybe we're in tests, or a somehow degraded environment
+        # either way there's only one shard, and we always want to see it
+        [default]
+      elsif !ApplicationController.region || DatabaseServer.all.all? { |db| !db.config[:region] }
+        all
+      else
+        in_region(ApplicationController.region)
+      end
     end
   end
 
