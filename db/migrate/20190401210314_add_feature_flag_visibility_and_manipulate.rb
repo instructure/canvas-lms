04ca6ab6dd50f3b7a65b0987ae2ec15fd194759d
Rex Fleischer <rfleischer@instructure.com>
2019-04-01 12:08:36 -0700

    separate feature flag definitions from code
    fixes: ADMIN-2539
    
    this moves and loads feature flags from yaml files. there should be
    absolutely no changes to functionality of anything in the system.
    
    Test-Plan:
    change things with feature flags and different feature options, and make
    sure they work the same way as before.
    
    Change-Id: Id096e6c9975f86224c1ee07304cef79a0d4c7755
    Reviewed-on: https://gerrit.instructure.com/187804
    Reviewed-by: Carl Kibler <ckibler@instructure.com>
    Tested-by: Jenkins
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Rex Fleischer <rfleischer@instructure.com>

--- db/migrate/20190401210314_add_feature_flag_visibility_and_manipulate.rb
+++ db/migrate/20190401210314_add_feature_flag_visibility_and_manipulate.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 - present Instructure, Inc.
+# Copyright (C) 2019 - present Instructure, Inc.
 #
 # This file is part of Canvas.
 #
@@ -14,18 +14,12 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
-#
 
-module Features
-  module DifferentiatedAssignments
-    class ModuleEvaluator < Struct.new(:course_id)
-      def perform
-        ContextModuleProgression.joins(:context_module).readonly(false).
-          where(:context_modules => { :context_type => 'Course', :context_id => course_id}).
-          find_each do |prog|
-            prog.mark_as_outdated!
-          end
-      end
-    end
+class AddFeatureFlagVisibilityAndManipulate < ActiveRecord::Migration[5.1]
+  tag :predeploy
+
+  def change
+    add_column :feature_flags, :visibility, :string, limit: 255
+    add_column :feature_flags, :manipulate, :string, limit: 255
   end
 end
---
+++ spec/lib/feature_flag_definitions_spec.rb
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2013 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require "spec_helper"
+
+describe "feature_flag_definition_spec" do
+  Feature.definitions.each_key do |feature_name|
+    it "#{feature_name} should have a display_name and description lambdas" do
+      feature = Feature.definitions[feature_name]
+      expect(feature).to_not be_nil
+      expect(feature.display_name.call).to_not be_nil
+      expect(feature.description.call).to_not be_nil
+    end
+  end
+
+  FeatureFlags::Loader.load_yaml_files.each do |name, definition|
+    [:custom_transition_proc, :after_state_change_proc, :visible_on].each do |hook|
+      if definition[hook]
+        hook_name = definition[hook]
+        it "#{name} hook for #{hook} (#{hook_name}) should exist in FeatureFlags::Hooks as a static method" do
+          expect(FeatureFlags::Hooks.respond_to?(hook_name)).to be true
+        end
+      end
+    end
+  end
+end
