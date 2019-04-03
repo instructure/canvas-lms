5b0157f8b3343f748eb7ff28b2362994ae28f10f
Ryan Shaw <ryan@instructure.com>
2019-04-03 10:47:21 -0600

    Upgrade to babel 7 & ui-babel-preset

--- packages/canvas-rce/src/tinymce-a11y-checker/src/components/color-picker.js
+++ packages/canvas-rce/src/tinymce-a11y-checker/src/components/color-picker.js
@@ -8,26 +8,20 @@ import {
 import Pointer from "./pointer"
 import PointerCircle from "react-color/lib/components/photoshop/PhotoshopPointerCircle"
 
-class ColorPicker extends React.Component {
-  static get displayName() {
-    return "ColorPicker"
-  }
-
-  render() {
-    return (
-      <div>
-        <div style={{ position: "relative", height: 150, marginTop: 10 }}>
-          <Saturation {...this.props} pointer={PointerCircle} />
-        </div>
-        <div style={{ position: "relative", height: 10, marginTop: 10 }}>
-          <Hue {...this.props} pointer={Pointer} />
-        </div>
-        <div style={{ position: "relative", height: 10, marginTop: 10 }}>
-          <Alpha {...this.props} pointer={Pointer} />
-        </div>
+function ColorPicker(props) {
+  return (
+    <div>
+      <div style={{ position: "relative", height: 150, marginTop: 10 }}>
+        <Saturation {...props} pointer={PointerCircle} />
       </div>
-    )
-  }
+      <div style={{ position: "relative", height: 10, marginTop: 10 }}>
+        <Hue {...props} pointer={Pointer} />
+      </div>
+      <div style={{ position: "relative", height: 10, marginTop: 10 }}>
+        <Alpha {...props} pointer={Pointer} />
+      </div>
+    </div>
+  )
 }
 
 export default ColorWrap(ColorPicker)
