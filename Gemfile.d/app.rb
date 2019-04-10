7a72707c8ceda41af454d5fc0718c9e2219e13f8
Cody Cutrer <cody@instructure.com>
2019-04-10 11:16:42 -0600

    bump switchman-inst-jobs
    Change-Id: Id9c4ed99985775f4c3a84a9c9dbbe6fd5167e42b
    Reviewed-on: https://gerrit.instructure.com/188980
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    Tested-by: Jenkins

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -63,7 +63,7 @@ gem 'canvas_webex', '0.17'
 gem 'inst-jobs', '0.15.5'
   gem 'rufus-scheduler', '3.4.2', require: false
     gem 'et-orbi', '1.0.8', require: false
-gem 'switchman-inst-jobs', '1.3.1'
+gem 'switchman-inst-jobs', '1.3.2'
 gem 'inst-jobs-autoscaling', '1.0.5'
   gem 'aws-sdk-autoscaling', '1.5.0', require: false
 # ffi 1.9.2x seem to have issues on RHEL & CentOS causing segfaults
