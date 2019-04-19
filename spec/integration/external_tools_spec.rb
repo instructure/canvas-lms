7e4d3683fb7af8c3b9333688942110c1edb1b171
James Williams <jamesw@instructure.com>
2019-04-19 07:37:25 -0600

    optionally lock content publishing for unconfirmed users
    test plan:
    * configure the root account in rails console with:
    
    Account.default.tap do |a|
      a.settings[:require_confirmed_email] = true
      a.save!
    end
    
    * create a new user and add them to a course but don't
     confirm their email - just register them in console:
    
    User.find(X).register
    
    * confirm that the new user cannot do the following
     (and receives a message to finish registering):
    
     - launch LTI tools
     - publish a course
     - access eportfolios
    
    closes #CORE-2774 #CORE-2775 #CORE-2776
    
    Change-Id: I97ad90e714f7d191e84674924ceb4c9a8b87a00b
    Reviewed-on: https://gerrit.instructure.com/189946
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- spec/integration/external_tools_spec.rb
+++ spec/integration/external_tools_spec.rb
@@ -154,6 +154,20 @@ describe "External Tools" do
     expect(tab['class'].split).to include("active")
   end
 
+  it "should prevent access for unverified users if account requires it" do
+    course_with_teacher_logged_in(:active_all => true)
+
+    @tool = @course.context_external_tools.create!(:shared_secret => 'test_secret', :consumer_key => 'test_key', :name => 'my grade passback test tool', :domain => 'example.com')
+    @tool.course_navigation = {:url => "http://www.example.com", :text => "Example URL"}
+    @tool.save!
+
+    Account.default.tap{|a| a.settings[:require_confirmed_email] = true; a.save!}
+    get "/courses/#{@course.id}/external_tools/#{@tool.id}"
+    expect(response).to be_redirect
+    expect(response.location).to eq root_url
+    expect(flash[:warning]).to include("Complete registration")
+  end
+
   context 'global navigation' do
     before :once do
       Account.default.enable_feature!(:lor_for_account)
