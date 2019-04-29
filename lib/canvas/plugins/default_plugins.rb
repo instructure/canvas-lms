97d38c52006cc2e42715e56fdafba22fbc533872
Cody Cutrer <cody@instructure.com>
2019-04-29 16:04:58 -0600

    remove duplicate plugin registration
    Change-Id: I2bf41ef8c76189de23b9b9156d9dce58980fb6b8
    Reviewed-on: https://gerrit.instructure.com/191342
    Reviewed-by: James Williams <jamesw@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>
    Tested-by: Jenkins

--- lib/canvas/plugins/default_plugins.rb
+++ lib/canvas/plugins/default_plugins.rb
@@ -389,23 +389,6 @@ Canvas::Plugin.register('live_events', nil, {
   :settings_partial => 'plugins/live_events_settings',
   :validator => 'LiveEventsValidator'
 })
-Canvas::Plugin.register('live_events', nil, {
-  :name => lambda{ t :name, 'Live Events' },
-  :description => lambda{ t :description, 'Service for real-time events.' },
-  :author => 'Instructure',
-  :author_website => 'http://www.instructure.com',
-  :version => '1.0.0',
-  :settings => {
-    :kinesis_stream_name => nil,
-    :aws_access_key_id => nil,
-    :aws_secret_access_key => nil,
-    :aws_region => 'us-east-1',
-    :aws_endpoint => nil,
-  },
-  :encrypted_settings => [ :aws_secret_access_key ],
-  :settings_partial => 'plugins/live_events_settings',
-  :validator => 'LiveEventsValidator'
-})
 Canvas::Plugin.register('inst_fs', nil, {
   :name =>lambda{ t :name, 'Inst-FS' },
   :description => lambda{ t :description, 'File service that proxies for S3.' },
