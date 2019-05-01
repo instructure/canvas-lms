60290c1eaa5a0425615b0817e8ba93d5938fd50b
Ryan Shaw <ryan@instructure.com>
2019-05-01 13:44:05 -0600

    AMD -> ES modules
    This commit just converts AMD imports to ES module import/exports
    
    Change-Id: I7bc912fd9ccb87f7477d13d58d52675fcfa1f5e2
    Reviewed-on: https://gerrit.instructure.com/191731
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- spec/javascripts/jsx/conditional_release_stats/helpers/actionsSpec.js
+++ spec/javascripts/jsx/conditional_release_stats/helpers/actionsSpec.js
@@ -16,26 +16,25 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'jsx/conditional_release_stats/helpers/actions',
-], ({ createAction, createActions }) => {
-  QUnit.module('Conditional Release Stats action helpers')
+import { createAction, createActions } from 'jsx/conditional_release_stats/helpers/actions'
 
-  test('creates a new action', () => {
-    const actionCreator = createAction('ACTION_ONE')
-    const action = actionCreator('payload')
+QUnit.module('Conditional Release Stats action helpers')
 
-    equal(action.type, 'ACTION_ONE', 'action type match')
-    equal(action.payload, 'payload', 'action payload match')
-  })
+test('creates a new action', () => {
+  const actionCreator = createAction('ACTION_ONE')
+  const action = actionCreator('payload')
 
-  test('creates multiple actions', () => {
-    const actionDefs = ['ACTION_ONE', 'ANOTHER_MORE_COMPLEX_ACTION_NAME']
-    const { actionTypes, actions } = createActions(actionDefs)
-
-    equal(actions.actionOne.type, 'ACTION_ONE')
-    equal(actionTypes.ACTION_ONE, 'ACTION_ONE')
-    equal(actions.anotherMoreComplexActionName.type, 'ANOTHER_MORE_COMPLEX_ACTION_NAME')
-    equal(actionTypes.ANOTHER_MORE_COMPLEX_ACTION_NAME, 'ANOTHER_MORE_COMPLEX_ACTION_NAME')
-  })
+  equal(action.type, 'ACTION_ONE', 'action type match')
+  equal(action.payload, 'payload', 'action payload match')
 })
+
+test('creates multiple actions', () => {
+  const actionDefs = ['ACTION_ONE', 'ANOTHER_MORE_COMPLEX_ACTION_NAME']
+  const { actionTypes, actions } = createActions(actionDefs)
+
+  equal(actions.actionOne.type, 'ACTION_ONE')
+  equal(actionTypes.ACTION_ONE, 'ACTION_ONE')
+  equal(actions.anotherMoreComplexActionName.type, 'ANOTHER_MORE_COMPLEX_ACTION_NAME')
+  equal(actionTypes.ANOTHER_MORE_COMPLEX_ACTION_NAME, 'ANOTHER_MORE_COMPLEX_ACTION_NAME')
+})
+
--- spec/javascripts/jsx/gradezilla/individual-gradebook/components/GradebookSelectorSpec.js
+++ spec/javascripts/jsx/gradezilla/individual-gradebook/components/GradebookSelectorSpec.js
@@ -16,152 +16,150 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define([
-  'react',
-  'prop-types',
-  'react-dom',
-  'enzyme',
-  'jsx/gradezilla/individual-gradebook/components/GradebookSelector'
-], (React, PropTypes, ReactDOM, {mount}, GradebookSelector) => {
-  QUnit.module('GradebookSelector', {
-    setup() {
-      this.setLocationStub = sandbox.stub(GradebookSelector.prototype, 'setLocation')
-      this.wrapper = mount(
-        <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
-      )
-    },
-    teardown() {
-      this.wrapper.unmount()
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import {mount} from 'enzyme'
+import GradebookSelector from 'jsx/gradezilla/individual-gradebook/components/GradebookSelector'
+
+QUnit.module('GradebookSelector', {
+  setup() {
+    this.setLocationStub = sandbox.stub(GradebookSelector.prototype, 'setLocation')
+    this.wrapper = mount(
+      <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
+    )
+  },
+  teardown() {
+    this.wrapper.unmount()
+  }
+})
+
+test('#selectDefaultGradebook calls setLocation', function() {
+  this.wrapper.find('select').simulate('change', {target: {value: 'default-gradebook'}})
+  const url = `${
+    this.wrapper.props().courseUrl
+  }/gradebook/change_gradebook_version?version=default`
+  ok(this.setLocationStub.withArgs(url).calledOnce)
+})
+
+test('#selectGradebookHistory calls setLocation', function() {
+  this.wrapper.find('select').simulate('change', {target: {value: 'gradebook-history'}})
+  const url = `${this.wrapper.props().courseUrl}/gradebook/history`
+  ok(this.setLocationStub.withArgs(url).calledOnce)
+})
+
+QUnit.module('Switching between ic-tabs', {
+  setup() {
+    const ICTabs = props => (
+      <ic-tabs>
+        <ic-tab onClick={props.firstOnClick} />
+        <ic-tab onClick={props.secondOnClick} />
+      </ic-tabs>
+    )
+
+    ICTabs.propTypes = {
+      firstOnClick: PropTypes.func.isRequired,
+      secondOnClick: PropTypes.func.isRequired
     }
-  })
 
-  test('#selectDefaultGradebook calls setLocation', function() {
-    this.wrapper.find('select').simulate('change', {target: {value: 'default-gradebook'}})
-    const url = `${
-      this.wrapper.props().courseUrl
-    }/gradebook/change_gradebook_version?version=default`
-    ok(this.setLocationStub.withArgs(url).calledOnce)
-  })
-
-  test('#selectGradebookHistory calls setLocation', function() {
-    this.wrapper.find('select').simulate('change', {target: {value: 'gradebook-history'}})
-    const url = `${this.wrapper.props().courseUrl}/gradebook/history`
-    ok(this.setLocationStub.withArgs(url).calledOnce)
-  })
-
-  QUnit.module('Switching between ic-tabs', {
-    setup() {
-      const ICTabs = props => (
-        <ic-tabs>
-          <ic-tab onClick={props.firstOnClick} />
-          <ic-tab onClick={props.secondOnClick} />
-        </ic-tabs>
-      )
-
-      ICTabs.propTypes = {
-        firstOnClick: PropTypes.func.isRequired,
-        secondOnClick: PropTypes.func.isRequired
-      }
-
-      this.firstOnClickStub = sinon.stub()
-      this.secondOnClickStub = sinon.stub()
-      const ICTabsProps = {
-        firstOnClick: this.firstOnClickStub,
-        secondOnClick: this.secondOnClickStub
-      }
-      const element = React.createElement(ICTabs, ICTabsProps)
-      this.fixtures = document.getElementById('fixtures')
-      ReactDOM.render(element, this.fixtures)
-      this.wrapper = mount(
-        <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
-      )
-    },
-
-    teardown() {
-      ReactDOM.unmountComponentAtNode(this.fixtures)
-      this.wrapper.unmount()
+    this.firstOnClickStub = sinon.stub()
+    this.secondOnClickStub = sinon.stub()
+    const ICTabsProps = {
+      firstOnClick: this.firstOnClickStub,
+      secondOnClick: this.secondOnClickStub
     }
-  })
+    const element = React.createElement(ICTabs, ICTabsProps)
+    this.fixtures = document.getElementById('fixtures')
+    ReactDOM.render(element, this.fixtures)
+    this.wrapper = mount(
+      <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
+    )
+  },
 
-  test('#selectIndividualGradebook calls click on the first ic-tab', function() {
-    this.wrapper.find('select').simulate('change', {target: {value: 'individual-gradebook'}})
-    ok(this.firstOnClickStub.calledOnce)
-  })
+  teardown() {
+    ReactDOM.unmountComponentAtNode(this.fixtures)
+    this.wrapper.unmount()
+  }
+})
 
-  test('#selectLearningMastery calls click on the second ic-tab', function() {
-    this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
-    ok(this.secondOnClickStub.calledOnce)
-  })
+test('#selectIndividualGradebook calls click on the first ic-tab', function() {
+  this.wrapper.find('select').simulate('change', {target: {value: 'individual-gradebook'}})
+  ok(this.firstOnClickStub.calledOnce)
+})
 
-  test('defaults to Individual View', function() {
-    equal(this.wrapper.find('select').instance().value, 'individual-gradebook')
-  })
+test('#selectLearningMastery calls click on the second ic-tab', function() {
+  this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
+  ok(this.secondOnClickStub.calledOnce)
+})
 
-  test('clicking on learning mastery changes the selected value to learning mastery', function() {
-    this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
-    equal(this.wrapper.find('select').instance().value, 'learning-mastery')
-  })
+test('defaults to Individual View', function() {
+  equal(this.wrapper.find('select').instance().value, 'individual-gradebook')
+})
 
-  test('clicking on individual view changes the selected value to individual view', function() {
-    // by default individual-gradebook is selected
-    this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
-    this.wrapper.find('select').simulate('change', {target: {value: 'individual-gradebook'}})
-    equal(this.wrapper.find('select').instance().value, 'individual-gradebook')
-  })
+test('clicking on learning mastery changes the selected value to learning mastery', function() {
+  this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
+  equal(this.wrapper.find('select').instance().value, 'learning-mastery')
+})
 
-  QUnit.module('Menu Items Rendered with Learning Mastery Enabled', {
-    setup() {
-      this.wrapper = mount(
-        <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
-      )
-      this.menuItems = this.wrapper.find('option').map(option => option.instance())
-    },
-    teardown() {
-      this.wrapper.unmount()
-    }
-  })
+test('clicking on individual view changes the selected value to individual view', function() {
+  // by default individual-gradebook is selected
+  this.wrapper.find('select').simulate('change', {target: {value: 'learning-mastery'}})
+  this.wrapper.find('select').simulate('change', {target: {value: 'individual-gradebook'}})
+  equal(this.wrapper.find('select').instance().value, 'individual-gradebook')
+})
 
-  test('Individual View is first', function() {
-    equal(this.menuItems[0].textContent, 'Individual View')
-  })
+QUnit.module('Menu Items Rendered with Learning Mastery Enabled', {
+  setup() {
+    this.wrapper = mount(
+      <GradebookSelector courseUrl="http://someUrl/" learningMasteryEnabled navigate={() => {}} />
+    )
+    this.menuItems = this.wrapper.find('option').map(option => option.instance())
+  },
+  teardown() {
+    this.wrapper.unmount()
+  }
+})
 
-  test('Learning Mastery is second', function() {
-    equal(this.menuItems[1].textContent, 'Learning Mastery…')
-  })
+test('Individual View is first', function() {
+  equal(this.menuItems[0].textContent, 'Individual View')
+})
 
-  test('Gradebook is third', function() {
-    equal(this.menuItems[2].textContent, 'Gradebook…')
-  })
+test('Learning Mastery is second', function() {
+  equal(this.menuItems[1].textContent, 'Learning Mastery…')
+})
 
-  test('Gradebook History is fourth', function() {
-    equal(this.menuItems[3].textContent, 'Gradebook History…')
-  })
+test('Gradebook is third', function() {
+  equal(this.menuItems[2].textContent, 'Gradebook…')
+})
 
-  QUnit.module('Menu Items Rendered with Learning Mastery Disabled', {
-    setup() {
-      this.wrapper = mount(
-        <GradebookSelector
-          courseUrl="http://someUrl/"
-          learningMasteryEnabled={false}
-          navigate={() => {}}
-        />
-      )
-      this.menuItems = this.wrapper.find('option').map(option => option.instance())
-    },
-    teardown() {
-      this.wrapper.unmount()
-    }
-  })
+test('Gradebook History is fourth', function() {
+  equal(this.menuItems[3].textContent, 'Gradebook History…')
+})
 
-  test('Individual Gradebook is first', function() {
-    equal(this.menuItems[0].textContent, 'Individual View')
-  })
+QUnit.module('Menu Items Rendered with Learning Mastery Disabled', {
+  setup() {
+    this.wrapper = mount(
+      <GradebookSelector
+        courseUrl="http://someUrl/"
+        learningMasteryEnabled={false}
+        navigate={() => {}}
+      />
+    )
+    this.menuItems = this.wrapper.find('option').map(option => option.instance())
+  },
+  teardown() {
+    this.wrapper.unmount()
+  }
+})
 
-  test('Gradebook is second', function() {
-    equal(this.menuItems[1].textContent, 'Gradebook…')
-  })
+test('Individual Gradebook is first', function() {
+  equal(this.menuItems[0].textContent, 'Individual View')
+})
 
-  test('Gradebook History Menu Item is third in the Menu', function() {
-    equal(this.menuItems[2].textContent, 'Gradebook History…')
-  })
+test('Gradebook is second', function() {
+  equal(this.menuItems[1].textContent, 'Gradebook…')
+})
+
+test('Gradebook History Menu Item is third in the Menu', function() {
+  equal(this.menuItems[2].textContent, 'Gradebook History…')
 })
