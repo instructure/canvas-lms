3d7d728f9df07535606f9d5a2742788e8e1a5d64
Marc Phillips <mphillips@instructure.com>
2019-05-10 13:45:31 -0600

    Fix ClientCredentials for local development
    Was hardcoding the aud to be https, when we
    are requiring http endpoints.
    
    refs PLAT-4474
    
    Test Plan:
     - Use of the 1.3 tool to do services should work
    
    Change-Id: Id8bd6ce92dcefecb6d046316704cf7db937fdebc
    Reviewed-on: https://gerrit.instructure.com/193079
    Tested-by: Jenkins
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Marc Phillips <mphillips@instructure.com>

--- lib/canvas/oauth/client_credentials_provider.rb
+++ lib/canvas/oauth/client_credentials_provider.rb
@@ -18,14 +18,12 @@
 
 module Canvas::Oauth
   class ClientCredentialsProvider < Provider
-    AUD_PROTOCOL = 'https://'.freeze
-
-    def initialize(jwt, host, scopes = nil)
+    def initialize(jwt, host, scopes = nil, protocol = 'http://')
       @client_id = JSON::JWT.decode(jwt, :skip_verification)[:sub]
       @scopes = scopes || []
       @expected_aud = Rails.application.routes.url_helpers.oauth2_token_url(
         host: host,
-        protocol: AUD_PROTOCOL
+        protocol: protocol
       )
       if key.nil? || key.public_jwk.nil?
         @invalid_key = true
