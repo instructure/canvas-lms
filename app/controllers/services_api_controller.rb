57235221d1d69d63fe0624224f203c2b6f802f8f
Steven Burnett <sburnett@instructure.com>
2019-05-17 15:28:28 -0600

    update kaltura session to return config
    refs CORE-2661
    
    Test Plan:
    - Make a post request to http://localhost:3001/api/v1/services/kaltura_session
    - notice a new top level key called "kaltura_settings"
    
    Change-Id: I3f60cfcfdbdfc29705a50080a5630030038a7d62
    Reviewed-on: https://gerrit.instructure.com/194235
    Reviewed-by: James Williams <jamesw@instructure.com>
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Reviewed-by: Landon Gilbert-Bland <lbland@instructure.com>
    Tested-by: Jenkins
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>

--- app/controllers/services_api_controller.rb
+++ app/controllers/services_api_controller.rb
@@ -46,14 +46,12 @@ class ServicesApiController < ApplicationController
     if @current_user
       @kal = CanvasKaltura::ClientV3.config
       response = { 'enabled' => !@kal.nil? }
-      
       if @kal
         response['domain'] = @kal['domain']
         response['resource_domain'] = @kal['resource_domain']
         response['rtmp_domain'] = @kal['rtmp_domain']
         response['partner_id'] = @kal['partner_id']
       end
-    
       render :json => response
     else
       render_unauthorized_action
@@ -80,14 +78,31 @@ class ServicesApiController < ApplicationController
     client = CanvasKaltura::ClientV3.new
     uid = "#{@user.id}_#{@domain_root_account.id}"
     res = client.startSession(CanvasKaltura::SessionType::USER, uid)
-    raise "Kaltura session failed to generate" if res.match(/START_SESSION_ERROR/)
-    render :json => {
+    raise "Kaltura session failed to generate" if res =~ /START_SESSION_ERROR/
+
+    hash = {
       :ks => res,
       :subp_id => CanvasKaltura::ClientV3.config['subpartner_id'],
       :partner_id => CanvasKaltura::ClientV3.config['partner_id'],
       :uid => uid,
-      :serverTime => Time.now.to_i
+      :serverTime => Time.zone.now.to_i
     }
+    if value_to_boolean(params[:include_upload_config])
+      pseudonym = @context ? SisPseudonym.for(@current_user, @context) : @current_user.primary_pseudonym
+      hash[:kaltura_setting] = CanvasKaltura::ClientV3.config.try(:slice,
+                                    'domain', 'resource_domain', 'rtmp_domain',
+                                    'partner_id', 'subpartner_id', 'player_ui_conf',
+                                    'player_cache_st', 'kcw_ui_conf', 'upload_ui_conf',
+                                    'max_file_size_bytes', 'do_analytics', 'hide_rte_button', 'js_uploader')
+      hash[:kaltura_setting][:uploadUrl] ="#{request.protocol}#{hash[:kaltura_setting][:domain]}/index.php/partnerservices2/upload"
+      hash[:kaltura_setting][:entryUrl] ="#{request.protocol}#{hash[:kaltura_setting][:domain]}/index.php/partnerservices2/addEntry"
+      hash[:kaltura_setting][:uiconfUrl] ="#{request.protocol}#{hash[:kaltura_setting][:domain]}/index.php/partnerservices2/getuiconf"
+      hash[:kaltura_setting][:partner_data] = {
+        :root_account_id => @domain_root_account.id,
+        :sis_user_id => pseudonym&.sis_user_id,
+        :sis_source_id  => @context&.sis_source_id
+      }
+    end
+    render json: hash
   end
-  
 end
