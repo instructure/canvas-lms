3b7aa2c1aecfeafa4035f66b26b13c44f96ce7db
Ryan Shaw <ryan@instructure.com>
2019-05-20 11:20:03 -0600

    decaffeinate models/PageView
    Test plan:
    * No changes, automated builds should pass
    
    Change-Id: I74965852edef28c994074eb47cbffad9a6d18d79
    Reviewed-on: https://gerrit.instructure.com/194379
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins

--- app/coffeescripts/models/PageView.js
+++ app/coffeescripts/models/PageView.js
@@ -1,44 +1,54 @@
-#
-# Copyright (C) 2012 - present Instructure, Inc.
-#
-# This file is part of Canvas.
-#
-# Canvas is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, version 3 of the License.
-#
-# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Affero General Public License along
-# with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+/*
+ * Copyright (C) 2012 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import $ from 'jquery'
 import Backbone from 'Backbone'
 import TextHelper from '../str/TextHelper'
-import 'jquery.instructure_misc_helpers' # $.parseUserAgentString
+import 'jquery.instructure_misc_helpers' // $.parseUserAgentString
 
-export default class PageView extends Backbone.Model
+export default class PageView extends Backbone.Model {
+  isLinkable() {
+    const method = this.get('http_method')
+    if (method == null) return true
+    return method === 'get'
+  }
 
-  computedAttributes: ['summarizedUserAgent', 'readableInteractionTime', 'truncatedURL', 'isLinkable']
+  summarizedUserAgent() {
+    return this.get('app_name') || $.parseUserAgentString(this.get('user_agent'))
+  }
 
-  isLinkable: ->
-    method = @get 'http_method'
-    return true unless method?
-    method is 'get'
+  readableInteractionTime() {
+    const seconds = this.get('interaction_seconds')
+    if (seconds > 5) {
+      return Math.round(seconds)
+    } else {
+      return '--'
+    }
+  }
 
-  summarizedUserAgent: ->
-    @get('app_name') || $.parseUserAgentString(@get 'user_agent')
+  truncatedURL() {
+    return TextHelper.truncateText(this.get('url'), {max: 90})
+  }
+}
 
-  readableInteractionTime: ->
-    seconds = @get 'interaction_seconds'
-    if seconds > 5
-      Math.round(seconds)
-    else
-      '--'
-
-  truncatedURL: ->
-    TextHelper.truncateText @get('url'), max: 90
+PageView.prototype.computedAttributes = [
+  'summarizedUserAgent',
+  'readableInteractionTime',
+  'truncatedURL',
+  'isLinkable'
+]
