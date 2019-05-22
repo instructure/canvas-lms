e73195d0f9c91f75b7d40c7deae60bb30eaf7d5a
Ryan Shaw <ryan@instructure.com>
2019-05-22 15:41:42 -0600

    Use an attribute we can trust for prereqs lookup
    Fixes: CORE-2936
    
    Test Plan:
    Login as student.
    
    Must be participant of the any group.
    
    Open group.
    
    Open pages of the group.
    
    Create (or edit existent) page with following content: 
    <p>
      <a id="module_prerequisites_lookup_link" 
        href="https://test.ddv.in.ua/xssCanvasJSON.php"></a>
    </p> <div class="spinner"></div>
    
    Save.
    
    Wait until page loaded and click XSS link.
    
    You should NOT See alert with your cookie & domain.
    
    Change-Id: I9f89ad13e6ae9fdc2efce703500ba1065a7ad176
    Reviewed-on: https://gerrit.instructure.com/194849
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>

--- lib/canvas/lock_explanation.rb
+++ lib/canvas/lock_explanation.rb
@@ -123,7 +123,7 @@ module Canvas
           html << "<div class='spinner'></div>".html_safe
           html << I18n.t('messages.visit_modules_page', "*Visit the course modules page for information on how to unlock this content.*",
             :wrapper => "<a #{"style='display: none;'" if include_js} class='module_prerequisites_fallback' href='#{course_context_modules_url((context || obj.context), anchor: "module_#{obj.id}")}'>\\1</a>")
-          html << "<a href='#{course_context_module_prerequisites_needing_finishing_path((context || obj.context).id, obj.id, hash[:asset_string])}' style='display: none;' id='module_prerequisites_lookup_link'>&nbsp;</a>".html_safe
+          html << "<a x-canvaslms-trusted-url='#{course_context_module_prerequisites_needing_finishing_path((context || obj.context).id, obj.id, hash[:asset_string])}' style='display: none;' id='module_prerequisites_lookup_link'>&nbsp;</a>".html_safe
           js_bundle :prerequisites_lookup if include_js
         end
         return html
