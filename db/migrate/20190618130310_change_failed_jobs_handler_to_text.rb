bb3f8a0c0469055a5f5f3b14be6bbf8bd5444bc6
James Williams <jamesw@instructure.com>
2019-06-18 07:07:18 -0600

    make failed_jobs.handler a text column
    just like delayed_jobs.handler
    
    test plan: existing specs
    
    closes #CORE-3091
    
    Change-Id: I8c073cd269e4b52acd1431924970cec18634356c
    Reviewed-on: https://gerrit.instructure.com/198033
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Jenkins

---
+++ db/migrate/20190618130310_change_failed_jobs_handler_to_text.rb
@@ -0,0 +1,15 @@
+class ChangeFailedJobsHandlerToText < ActiveRecord::Migration[5.1]
+  tag :predeploy
+
+  def connection
+    Delayed::Backend::ActiveRecord::Job.connection
+  end
+
+  def up
+    change_column :failed_jobs, :handler, :text
+  end
+
+  def down
+    change_column :failed_jobs, :handler, :string, :limit => 500.kilobytes
+  end
+end
