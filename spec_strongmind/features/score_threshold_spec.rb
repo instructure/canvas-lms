cb08ec8ea515dc60e87e69ab9f653689023384a0
Tesla Team <tesla@strongmind.com>
2019-06-26 14:54:41 -0700

    Play 1108

--- spec_strongmind/features/score_threshold_spec.rb
+++ spec_strongmind/features/score_threshold_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Score threshold feature', type: :feature, js: true do
 
   context "with score_threshold set" do
     before do
-      allow_any_instance_of(ContextModule).to receive(:score_threshold).and_return(75.0)
+      allow_any_instance_of(RequirementsService::Commands::ApplyMinimumScores).to receive(:score_threshold).and_return(75.0)
 
       account_admin_user
       user_session(@admin)
