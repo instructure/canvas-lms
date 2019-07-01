9312726174256ee3f0ffd32859f5a67d4e03c7fc
Ethan Knapp <eknapp@instructure.com>
2019-07-01 12:21:09 -0600

    add live events for assignment overrides
    refs PFS-12035, PFS-12662
    
    Change-Id: Ic6dd9d58ecf456256b62212d4f8d7ec37312b449
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/199749
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Tanner <ctanner@instructure.com>
    Reviewed-by: Clint Furse <cfurse@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Ethan Knapp <eknapp@instructure.com>

--- spec/lib/canvas/live_events_spec.rb
+++ spec/lib/canvas/live_events_spec.rb
@@ -800,6 +800,64 @@ describe Canvas::LiveEvents do
     end
   end
 
+  describe 'assignment_override_updated' do
+    def base_override_hash(override)
+      {
+        assignment_override_id: override.id.to_s,
+        assignment_id: override.assignment.id.to_s,
+        due_at: override.due_at,
+        all_day: override.all_day,
+        all_day_date: override.all_day_date,
+        unlock_at: override.unlock_at,
+        lock_at: override.lock_at,
+        type: override.set_type,
+        workflow_state: override.workflow_state,
+      }.compact!
+    end
+
+    it 'triggers a live event with ADHOC assignment override details' do
+      course_with_student_submissions
+      assignment = @course.assignments.first
+      override = create_adhoc_override_for_assignment(assignment, @student)
+
+      expect_event('assignment_override_updated',
+        hash_including(base_override_hash(override).merge({
+          type: 'ADHOC',
+        }))).once
+
+      Canvas::LiveEvents.assignment_override_updated(override)
+    end
+
+    it 'triggers a live event with CourseSection assignment override details' do
+      course_with_student_submissions
+      assignment = @course.assignments.first
+      override = create_section_override_for_assignment(assignment)
+      section = override.set
+
+      expect_event('assignment_override_updated',
+        hash_including(base_override_hash(override).merge({
+          type: 'CourseSection',
+          course_section_id: section.id.to_s,
+        }))).once
+
+      Canvas::LiveEvents.assignment_override_updated(override)
+    end
+
+    it 'triggers a live event with Group assignment override details' do
+      course_with_student
+      assignment = group_assignment_discussion(course: @course).assignment
+      override = create_group_override_for_assignment(assignment, group: @group)
+
+      expect_event('assignment_override_updated',
+        hash_including(base_override_hash(override).merge({
+          type: 'Group',
+          group_id: override.set.id.to_s,
+        }))).once
+
+      Canvas::LiveEvents.assignment_override_updated(override)
+    end
+  end
+
   describe '.quiz_export_complete' do
     class FakeExport
       attr_accessor :context
