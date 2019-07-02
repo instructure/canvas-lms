1b17249ae21688d9f7c32483746568e8adb34cd0
Tucker McKnight <tmcknight@instructure.com>
2019-07-02 17:16:45 -0600

    spec: Use upstream Pact gem
    We do not use any of the features that the fork was intended for.
    
    Change-Id: I19c8e69ac77ecb8aef586bcda15fc26723de7253
    Reviewed-on: https://gerrit.instructure.com/200050
    Reviewed-by: Jeremy Putnam <jeremyp@instructure.com>
    Tested-by: Jenkins
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -58,8 +58,7 @@ group :test do
   gem 'escape_code', '0.2', require: false
   gem 'hashdiff', '0.3.7'
   gem 'luminosity_contrast', '0.2.1'
-  gem 'pact-support', github: 'tucker-m/pact-support', tag: 'pr1.2'
-  gem 'pact', github: 'tucker-m/pact-ruby', tag: 'pr1.2'
+  gem 'pact', '1.24.0'
   gem 'pact-messages', '0.2.0'
   gem 'pact_broker-client'
   gem 'database_cleaner', '~> 1.5', '>= 1.5.3'
