ebd86dab7556c3ff02e37940b5882ebbc8ab43b3
Ryan Norton <rnorton@instructure.com>
2019-07-03 15:40:50 -0600

    fix blank display when media capture is disabled
    if media capture is blocked by a purpose such as a browser
    protocol it renders a component as an empty div instead of
    giving any meaningful messaging
    
    Test Plan:
    * find a computer without a mic / camera
    * enable the new RCE feature flag
    * navigate to a discussion topic
    * click the Record/Upload Media button in the modal
    * click on the Record tab in the modal
    * confirm that you see an error message instead of a blank box
    
    fixes COMMS-2201
    
    Change-Id: Id51caf11b44daa9723ff102ca4deb95b955fa96c
    Reviewed-on: https://gerrit.instructure.com/200209
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>

--- packages/canvas-rce/src/rce/plugins/instructure_record/UploadMedia/MediaRecorder.js
+++ packages/canvas-rce/src/rce/plugins/instructure_record/UploadMedia/MediaRecorder.js
@@ -16,7 +16,9 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {MediaCapture, canUseMediaCapture} from '@instructure/media-capture'
+import {Alert} from '@instructure/ui-alerts'
+import {canUseMediaCapture, MediaCapture} from '@instructure/media-capture'
+import formatMessage from '../../../../format-message'
 import {MediaCaptureStrings} from './MediaCaptureStrings'
 import {object, func} from 'prop-types'
 import React from 'react'
@@ -29,12 +31,19 @@ export default class MediaRecorder extends React.Component {
   render() {
     return (
       <div>
-        {canUseMediaCapture() && (
+        {canUseMediaCapture() ? (
           <MediaCapture
             translations={MediaCaptureStrings}
             onCompleted={this.saveFile}
           />
-        )}
+        ) :
+        (<Alert
+          variant="error"
+          margin="small"
+        >
+          {formatMessage('Error uploading video/audio recording')}
+        </Alert>)
+      }
       </div>
     )
   }
