54a587793a2cb41d32096b9bcb64c800307827c6
Simon Williams <simon@instructure.com>
2019-07-08 15:23:30 -0500

    spec: update rails-controller-testing to 1.0.4
    https://github.com/rails/rails-controller-testing/compare/v1.0.2..v1.0.4
    
    Change-Id: I6026e968a0d90851b7cb9e9fffafbe11dcbd933c
    Reviewed-on: https://gerrit.instructure.com/200499
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -17,7 +17,7 @@
 
 group :test do
   gem 'rails-dom-testing', '2.0.3'
-  gem 'rails-controller-testing', '1.0.2'
+  gem 'rails-controller-testing', '1.0.4'
 
   gem 'gergich', '0.2.2', require: false
   gem 'dotenv', '2.7.4', require: false
