58a199e50f590e58cd39bacaf447370b36fd1360
Cody Cutrer <cody@instructure.com>
2019-07-23 13:27:08 -0600

    split cache_quiz_lti migration into add and backfill
    Change-Id: Idf0241c52d83b2ea70c8ca5df448a3c8fc47e5b3
    Reviewed-on: https://gerrit.instructure.com/202325
    Reviewed-by: James Williams <jamesw@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>
    Tested-by: Jenkins

--- db/migrate/20190614230625_backfill_cached_quiz_lti_on_submissions.rb
+++ db/migrate/20190614230625_backfill_cached_quiz_lti_on_submissions.rb
@@ -16,16 +16,20 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-class AddCachedQuizLtiToSubmissions < ActiveRecord::Migration[5.1]
+class BackfillCachedQuizLtiOnSubmissions < ActiveRecord::Migration[5.1]
   tag :predeploy
   disable_ddl_transaction!
 
   def up
-    add_column :submissions, :cached_quiz_lti, :boolean
-    change_column_default(:submissions, :cached_quiz_lti, false)
+    DataFixup::BackfillNulls.run(Submission, :cached_quiz_lti, default_value: false)
+    Shard.current.database_server.unshackle do
+      connection = Submission.connection
+      connection.execute("VACUUM ANALYZE #{Submission.quoted_table_name}") if connection.open_transactions == 0
+    end
+    change_column_null(:submissions, :cached_quiz_lti, false)
   end
 
   def down
-    remove_column :submissions, :cached_quiz_lti
+    change_column_null(:submissions, :cached_quiz_lti, true)
   end
 end
