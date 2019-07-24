58578a2d04e8095688fbd3c304241197d725af8c
Ryan Norton <rnorton@instructure.com>
2019-07-24 14:44:30 -0600

    handle Select deprecation in StudentGroupFilter
    as part of the instUI upgrade we are removing the Select
    component in favor of native select
    
    Test Plan:
    * in account settings, enable the 'New Gradebook' feature
    * in course settings, enable the 'Launch SpeedGrader Filtered by
      Student Group' option
    * as a teacher, navigate to the Assignment page of the course in
      the assignments 1 view
    * in the top right of the page, above the SpeedGrader link, you
      should see a select menu with which to select a group
    * verify that the select drop down lists all groups for selection
    
    fixes COMMS-2238
    
    Change-Id: I99e2a473cebbe8b6f98044fb1801677b65b4c222
    Reviewed-on: https://gerrit.instructure.com/202547
    Tested-by: Jenkins
    Reviewed-by: Landon Gilbert-Bland <lbland@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>

--- app/jsx/shared/StudentGroupFilter.js
+++ app/jsx/shared/StudentGroupFilter.js
@@ -16,13 +16,10 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
 import {arrayOf, func, shape, string} from 'prop-types'
-import Select from '@instructure/ui-core/lib/components/Select'
+import FormField from '@instructure/ui-form-field/lib/components/FormField'
 import I18n from 'i18n!assignment'
-
-// When Canvas updates Instructure-UI to version 6, change <option /> to
-// <Select.Option /> and <optgroup /> to <Select.Group />
+import React from 'react'
 
 function renderGroup(group) {
   return (
@@ -42,20 +39,25 @@ function renderCategoryAndChildren(category) {
 
 function StudentGroupFilter(props) {
   return (
-    <Select
-      label={props.label}
-      onChange={(event) => {props.onChange(event.target.value)}}
-      value={props.value || "0"}
-    >
-      <option aria-disabled="true" disabled="disabled" key="0" value="0">
-        {I18n.t('Select One')}
-      </option>
-      {
-        props.categories.map(category => (
-          renderCategoryAndChildren(category)
-        ))
-      }
-    </Select>
+    <FormField id="student-group-filter" label={props.label}>
+      <select
+        onChange={(event) => {props.onChange(event.target.value)}}
+        style={{
+          margin: '0',
+          width: '100%'
+        }}
+        value={props.value || "0"}
+      >
+        <option aria-disabled="true" disabled="disabled" key="0" value="0">
+          {I18n.t('Select One')}
+        </option>
+        {
+          props.categories.map(category => (
+            renderCategoryAndChildren(category)
+          ))
+        }
+      </select>
+    </FormField>
   )
 }
 
