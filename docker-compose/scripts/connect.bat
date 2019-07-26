43c7154728adb86e40074683660379930fe4c0e2
Brian Sadler <brian.sadler@beyondz.org>
2019-07-26 10:16:25 -0400

    Add docker scripts for the local development environment.

--- docker-compose/scripts/connect.bat
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker-compose run --rm canvasweb /bin/bash
--- docker-compose/scripts/docker_compose_run.sh
+++ docker-compose/scripts/docker_compose_run.sh
@@ -9,14 +9,15 @@ fi
 echo "Checking that bundle install doesn't need to run"
 bundle check &> /dev/null
 if [ $? -ne 0 ]; then
-  bundle install
+  echo "Bundle check FAILED / ERROR. Make sure the bundle install <ARGS> worked in the Docker container build"
+  exit 1
 else
   echo "Ok!"
 fi
 
 echo "Checking if npm is installed"
 if ! npm -v &> /dev/null; then
-  npm install
+  echo "npm -v FAILED / ERROR. Make sure the npm install worked in the Docker container build."
 else
   echo "Ok!"
 fi
@@ -27,11 +28,15 @@ cp -a /app/docker-compose/config/* /app/config/
 # an error saying i have to run this.  So just do it.
 bundle exec rake db:reset_encryption_key_hash
 
+#TODO: check if everything is setup and if not run this automatically. This stuff takes awhile and we only want to do it once.
+# Esp compile_assets.
 #echo ""
 #echo "Note: If this is the first time you're starting this container, you may have to run the following:"
 #echo ""
 #echo "    bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:initial_setup"
 #echo "    bundle exec rake canvas:compile_assets"
 
-echo "Starting the rails app"
-bundle exec bin/rails s -p 3000 -b '0.0.0.0'
+echo "Starting the rails app using puma"
+#bundle exec bin/rails s -p 3000 -b '0.0.0.0'
+# Use puma to run rails instead of running it directly so that our dev env matches prod.
+bundle exec puma -C config/puma.rb
