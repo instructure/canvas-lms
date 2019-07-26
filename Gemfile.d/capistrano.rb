f1cabc25d87770d4bfab0ab6153875bab2497d4b
Brian Sadler <brian.sadler@beyondz.org>
2019-07-26 07:26:24 -0400

    Fix byebug, net-ssh, and spring gem versions. Regenerate Gemfile.lock.

--- Gemfile.d/capistrano.rb
+++ Gemfile.d/capistrano.rb
@@ -1,4 +1,6 @@
 gem 'capistrano', '3.5.0'
 gem 'capistrano-rails', '~> 1.1'
+# Newer versions of net-ssh require newer versions of Ruby. Need an old one that will run with Ruby 2.1
+gem 'net-ssh', '3.2.0'
 gem 'capistrano-passenger'
 gem 'capistrano-nvm', require: false
