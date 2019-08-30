3f2d4131d2e3cbd237f301ce1fd7ee86af6a807b
Cody Cutrer <cody@instructure.com>
2019-08-30 10:28:08 -0600

    revisit DumpHelper
    model it more closely after marshal.c in ruby
     * avoid triggering ActiveRecord::Relation from loading data (and then asploding
       as it tries to cache that data on a hash we're already iterating)
     * directly identify singleton dump failures, which are otherwise impossible
       to blame the correct object in a circular hierarchy
    
    Change-Id: Ibb35240f62a5d9333a4c1d3cd13de079b06e1753
    Reviewed-on: https://gerrit.instructure.com/207658
    Tested-by: Jenkins
    Reviewed-by: James Williams <jamesw@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>

--- lib/dump_helper.rb
+++ lib/dump_helper.rb
@@ -3,42 +3,55 @@ require 'set'
 module DumpHelper
   class << self
     def find_dump_error(val, key = "<toplevel>", prefix = "", __visited_dump_vars = Set.new)
-      return true if __visited_dump_vars.include?(val)
+      return if __visited_dump_vars.include?(val)
       __visited_dump_vars << val
 
       Marshal.dump(val)
-      false
     rescue TypeError
 
-      # see if anything inside val can't be dumped...
-      sub_prefix = "#{prefix}#{key} (#<#{val.class}>) => "
-
-      if val.respond_to?(:marshal_dump)
-        result = find_dump_error(val.marshal_dump, "marshal_dump", sub_prefix, __visited_dump_vars)
-        return result if result
-      else
-        results = []
-        # instance var?
-        results << val.instance_variables.map do |k|
-          v = val.instance_variable_get(k)
-          find_dump_error(v, k, sub_prefix, __visited_dump_vars)
-        end.any?
-
-        # hash key/value?
-        val.each_pair do |k, v|
-          results << find_dump_error(k, "hash key #{k}", sub_prefix, __visited_dump_vars)
-          results << find_dump_error(v, "[#{k.inspect}]", sub_prefix, __visited_dump_vars)
-        end if val.respond_to?(:each_pair)
-
-        # array element?
-        val.each_with_index do |v, i|
-          results << find_dump_error(v, "[#{i}]", sub_prefix, __visited_dump_vars)
-        end if val.respond_to?(:each_with_index)
-        return true if results.any?
+      if $!.message == "singleton can't be dumped" && !val.singleton_methods.empty?
+        raise TypeError, "Unable to dump #{prefix}#{key} (#<#{val.class}>): #{$!}"
       end
 
-      # guess it's val proper
-      raise TypeError.new("Unable to dump #{prefix}#{key} (#<#{val.class}>): #{$!}")
+      # see if anything inside val can't be dumped...
+      sub_prefix = "  #{prefix}#{key} (#<#{val.class}>) => "
+
+      if val.respond_to?(:marshal_dump, true)
+        return find_dump_error(val.marshal_dump, "marshal_dump", sub_prefix, __visited_dump_vars)
+      elsif val.respond_to?(:_dump, true)
+        stringval = val._dump(-1)
+        unless stringvals.instance_variables.empty?
+          dump_ivars(stringval, "#{sub_prefix}_dump => ", __visited_dump_vars)
+        else
+          dump_ivars(val, sub_prefix, __visited_dump_vars)
+        end
+        return
+      else
+        dump_ivars(val, sub_prefix, __visited_dump_vars)
+
+        if val.is_a?(Hash) || val.is_a?(Struct)
+          val.each_pair do |k, v|
+            find_dump_error(k, "hash key #{k}", sub_prefix, __visited_dump_vars)
+            find_dump_error(v, "[#{k.inspect}]", sub_prefix, __visited_dump_vars)
+          end
+        elsif val.is_a?(Array)
+          val.each_with_index do |v, i|
+            find_dump_error(v, "[#{i}]", sub_prefix, __visited_dump_vars)
+          end if val.is_a?(Array)
+        else
+          # guess it's val proper
+          raise TypeError.new("Unable to dump #{prefix}#{key} (#<#{val.class}>): #{$!}")
+        end
+      end
+    end
+
+    private
+
+    def dump_ivars(val, sub_prefix, __visited_dump_vars)
+      val.instance_variables.map do |k|
+        v = val.instance_variable_get(k)
+        find_dump_error(v, k, sub_prefix, __visited_dump_vars)
+      end
     end
   end
 end
