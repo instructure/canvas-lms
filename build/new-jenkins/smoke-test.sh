459e0ed8a99b6e085605506fb77beff75bfb61a4
James Butters <jbutters@instructure.com>
2019-09-05 10:21:00 -0600

    spec: generate html report
    closes: CORE-3234
    
    Change-Id: I8a85ed899e1097e5c91276d97c8339931cda2e1a
    Reviewed-on: https://gerrit.instructure.com/208309
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/smoke-test.sh
+++ build/new-jenkins/smoke-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose exec -T web bundle exec rspec spec/selenium/login_logout_spec.rb -f doc --format html --out results.html
+# -O spec/spec.opts runs rspec with our formatters for failure reports
+docker-compose exec -T web bundle exec rspec -O spec/spec.opts spec/selenium/login_logout_spec.rb
