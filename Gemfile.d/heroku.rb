36c9663bfda87a385a88631460e4f66222586f35
rshipp <github@rshipp.com>
2019-09-05 15:22:31 -0500

    Add NewRelic
    Task: https://app.asana.com/0/0/1137434504174314/f
    
    Summary:
    
    * Add NewRelic gem
    * Rebuild from inside the docker container to update Gemfile.lock
    * Add newrelic.yml config file

--- Gemfile.d/heroku.rb
+++ Gemfile.d/heroku.rb
@@ -1,3 +1,6 @@
 # Help with logging on Heroku. Also helps with static assets, but I'm focused on logging right now.
 # Commented out until we actually cutover to Heroku since this disables the current logging on our AWS EC2 instance
 #gem 'rails_12factor', group: :production
+
+# New Relic tracking.
+gem 'newrelic_rpm'
