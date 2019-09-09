f28c4deb14f572e879aa09cff6b53e4555d01d26
Clay Diffrient <cdiffrient@instructure.com>
2019-09-09 09:33:37 -0600

    Pass COVERAGE variable into containers running tests
    Test Plan:
      - Merge commits after this should generate coverage
    
    refs COREFE-256
    
    flag = none
    
    Change-Id: Iab459a3ec393d899cb78a52293b5456d0b14bf4c
    Reviewed-on: https://gerrit.instructure.com/208641
    Tested-by: Jenkins
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- build/new-jenkins/frontend/tests-karma.sh
+++ build/new-jenkins/frontend/tests-karma.sh
@@ -4,4 +4,4 @@ export COMPOSE_FILE="docker-compose.new-jenkins-web.yml:docker-compose.new-jenki
 
 NAME='tests-karma-'$JSPEC_GROUP
 
-docker-compose run --name $NAME karma yarn test:karma:headless
+docker-compose run --name $NAME -e COVERAGE karma yarn test:karma:headless
--- build/new-jenkins/frontend/tests-packages.sh
+++ build/new-jenkins/frontend/tests-packages.sh
@@ -2,4 +2,4 @@
 
 export COMPOSE_FILE=./docker-compose.new-jenkins-web.yml
 
-docker-compose run --name tests-packages web yarn test:packages
+docker-compose run --name tests-packages -e COVERAGE web yarn test:packages
