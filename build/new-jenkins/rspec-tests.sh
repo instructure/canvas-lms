56a487811be69a2ad7f11989e53567b577fe20c0
James Williams <jamesw@instructure.com>
2019-09-11 09:49:35 -0600

    spec: knapsack for selenium
    Change-Id: I30c0c0006de8779dea1743d6c626022756f05d5a
    Reviewed-on: https://gerrit.instructure.com/210564
    Tested-by: Jenkins
    Reviewed-by: James Williams <jamesw@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/rspec-tests.sh
+++ build/new-jenkins/rspec-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 ] && [ $1 = 'only-failures' ]; then
-  docker-compose exec -T web bundle exec rake 'knapsack:rspec[-O spec/spec.opts --only-failures]'
+  docker-compose exec -T web bundle exec rspec -O spec/spec.opts --only-failures
 else
   docker-compose exec -T web bundle exec rake 'knapsack:rspec[-O spec/spec.opts]'
 fi
