67ef0eb89ad77e2dc33e9cab470522b49ef2b3ac
Ryan Shaw <ryan@instructure.com>
2019-09-11 15:38:28 -0600

    Transform instUI imports so jest loads less code
    Test plan:
    This should make jest run faster
    
    Change-Id: I69b25931e754cd4dc5c32a4c3b0cf39c72a3f208
    Reviewed-on: https://gerrit.instructure.com/209117
    Tested-by: Jenkins
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- babel.config.js
+++ babel.config.js
@@ -16,17 +16,18 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+const USE_ES_MODULES =
+  'USE_ES_MODULES' in process.env
+    ? process.env.USE_ES_MODULES !== 'false'
+    : !process.env.JEST_WORKER_ID
+
 module.exports = {
   presets: [
     [
       '@instructure/ui-babel-preset',
       {
-        esModules:
-          'USE_ES_MODULES' in process.env
-            ? process.env.USE_ES_MODULES !== 'false'
-            : !process.env.JEST_WORKER_ID,
-        node: !!process.env.JEST_WORKER_ID,
-        transformImports: false
+        esModules: USE_ES_MODULES,
+        node: !!process.env.JEST_WORKER_ID
       }
     ]
   ],
@@ -39,4 +40,28 @@ module.exports = {
       ]
     }
   }
+}
+
+// we can't just use the transformImports option of @instructure/ui-babel-preset because
+// @instructure/ui-media-player uses the old pattern of putting things in /components like InstUI used to do
+if (!USE_ES_MODULES) {
+  module.exports.plugins = [
+    [
+      '@instructure/babel-plugin-transform-imports',
+      {
+        '(@instructure/ui-[^/]+)$': {
+          transform: (importName, matches) => {
+            if (
+              !matches ||
+              !matches[1] ||
+              matches[1] === '@instructure/ui-test-utils' ||
+              matches[1].startsWith('@instructure/ui-media')
+            )
+              return
+            return `${matches[1]}/lib/${importName}`
+          }
+        }
+      }
+    ]
+  ]
 }
