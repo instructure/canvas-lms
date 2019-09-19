9a10036d36a27d373db44a0e3c774e50ef61954b
Rob Orton <rob@instructure.com>
2019-09-19 11:55:36 -0600

    run grade book export report in parallel
    Fixes CORE-3350
    flag: none
    
    test plan
     - run report with more than 1000 courses
     - it should work
    
    Change-Id: I79acd562b664b436596375731d0a5a89e2921bca
    Reviewed-on: https://gerrit.instructure.com/210244
    Tested-by: Jenkins
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/plugins/account_reports/lib/account_reports/default.rb
+++ gems/plugins/account_reports/lib/account_reports/default.rb
@@ -38,6 +38,10 @@ module AccountReports
       GradeReports.new(account_report).grade_export
     end
 
+    def self.parallel_grade_export_csv(account_report, runner)
+      GradeReports.new(account_report, runner).grade_export_runner(runner)
+    end
+
     def self.mgp_grade_export_csv(account_report)
       GradeReports.new(account_report).mgp_grade_export
     end
