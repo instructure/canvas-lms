4e9c60be26557ba38a885faf3c78b4151863b13d
James Williams <jamesw@instructure.com>
2019-09-19 12:47:59 -0600

    don't increment root discussion participants for group users
    test plan:
    * create a course with a group with a student
    * create a course discussion assigned to the
     group category (so students only see the copy of the root
     that's assigned to their group)
    * as the student, view the discussion from the course index
     (to ensure the participant record is created)
    * as the teacher, post to the root discussion topic
     (disregarding the message that students can't see it)
    * it should not create a unclearable unread counter
     for the student
    
    closes #COMMS-2425
    
    Change-Id: I78fd8f147798ffb82c424bbdac76670e18e1178c
    Reviewed-on: https://gerrit.instructure.com/210254
    Tested-by: Jenkins
    Reviewed-by: Landon Gilbert-Bland <lbland@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>

--- spec/models/discussion_entry_spec.rb
+++ spec/models/discussion_entry_spec.rb
@@ -405,6 +405,24 @@ describe DiscussionEntry do
       expect(DiscussionEntry).to receive(:unique_constraint_retry).once
       @entry.change_read_state("read", @student)
     end
+
+    it "should not increment unread count for students in group topics when posting to the root" do
+      course_with_student(:active_all => true)
+      teacher1 = @teacher
+      teacher2 = teacher_in_course(:course => @course, :active_all => true).user
+
+      group_category = @course.group_categories.create(:name => "new category")
+      @group = @course.groups.create(:name => "group", :group_category => group_category)
+      @group.add_user(@student)
+
+      root_topic = @course.discussion_topics.create!(:title => "parent topic", :message => "msg", :group_category => group_category)
+      student_participant = root_topic.discussion_topic_participants.create!(user: @student)
+      teacher_participant = root_topic.discussion_topic_participants.create!(user: teacher2)
+      root_topic.discussion_entries.create!(message: "message", user: teacher1)
+
+      expect(student_participant.reload.unread_entry_count).to eq 0
+      expect(teacher_participant.reload.unread_entry_count).to eq 1 # still count as unread for the user that can read it
+    end
   end
 
   context "rating" do
