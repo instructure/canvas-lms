87890caf2ba9e135d0cb030c3c0fa9bef585c442
Ryan Shaw <ryan@instructure.com>
2019-09-26 16:33:59 -0600

    Add back ability to dismiss browser not supported msg
    Closes: COREFE-319
    
    this used to exist but looking at the code it has not worked for at
    Least a year
    
    test plan:
    - use canvas in an unsupported browser (e.g. Firefox 67,
      or set your user agent to an unsupported one)
    - click the minimum browser warning at the top of the page
    - the warning should be dismissed
    - when visiting other canvas pages the warning should still be dismissed
    - restart the browser
    - the warning should be visible again
    
    Change-Id: I34aacdb8cee25dce6f4e778bce05c39c7bdc7c32
    Reviewed-on: https://gerrit.instructure.com/211213
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Tested-by: Jenkins
    QA-Review: Ryan Shaw <ryan@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/coffeescripts/jquery.rails_flash_notifications.js
+++ app/coffeescripts/jquery.rails_flash_notifications.js
@@ -60,7 +60,7 @@ $.initFlashContainer = () => initFlashContainer()
 function renderServerNotifications () {
   if ((typeof ENV !== 'undefined') && ENV && ENV.notices) {
     ENV.notices.forEach(notice => {
-      helper.createNode(notice.type, notice.content)
+      helper.createNode(notice.type, notice.content, undefined, undefined, notice.classes)
       createScreenreaderNodeWithDelay(notice.content, false)
     })
   }
@@ -70,6 +70,6 @@ function createScreenreaderNodeWithDelay (content, closable = true) {
   setTimeout(() => helper.createScreenreaderNode(content, closable), 100)
 }
 
-$(() => setTimeout(renderServerNotifications, 500))
+$(() => setTimeout(renderServerNotifications, 100))
 
 export default $
