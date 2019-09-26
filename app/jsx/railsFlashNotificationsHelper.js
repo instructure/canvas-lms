b0764f116dc39d51dd038b499ee77db06c9525a7
Ryan Shaw <ryan@instructure.com>
2019-09-26 16:33:59 -0600

    Add back ability to dismiss browser not supported msg
    Closes: COREFE-319
    
    this used to exist but looking at the code it has not worked for at
    Least a year
    
    test plan:
    - use canvas in an unsupported browser (e.g. Firefox 67,
      or set your user agent to an unsupported one)
    - click the minimum browser warning at the top of the page
    - the warning should be dismissed
    - when visiting other canvas pages the warning should still be dismissed
    - restart the browser
    - the warning should be visible again
    
    Change-Id: I34aacdb8cee25dce6f4e778bce05c39c7bdc7c32
    Reviewed-on: https://gerrit.instructure.com/211213
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Tested-by: Jenkins
    QA-Review: Ryan Shaw <ryan@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/railsFlashNotificationsHelper.js
+++ app/jsx/railsFlashNotificationsHelper.js
@@ -55,7 +55,7 @@ class RailsFlashNotificationsHelper {
         }
 
         if ($(event.currentTarget).hasClass('unsupported_browser')) {
-          $.cookie('unsupported_browser_dismissed')
+          $.cookie('unsupported_browser_dismissed', true, {path: '/'})
         }
 
         $(event.currentTarget)
@@ -69,11 +69,12 @@ class RailsFlashNotificationsHelper {
     return this.holder != null
   }
 
-  createNode(type, content, timeout, cssOptions = {}) {
+  createNode(type, content, timeout, cssOptions = {}, classes = '') {
     if (this.holderReady()) {
       const node = this.generateNodeHTML(type, content)
 
       $(node)
+        .addClass(classes)
         .appendTo($(this.holder))
         .css({zIndex: 2, ...cssOptions})
         .show('fast')
