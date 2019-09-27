61c75bae54eab270c9e32183d269a6811faa35cd
Rex Fleischer <rfleischer@instructure.com>
2019-09-27 12:52:36 -0700

    update submission attachment copy to always create new attachment
    fixes: ADMIN-2855
    flag=none
    
    Test-Plan:
    * create 2 courses and enrole the same student into both
    * as student, submit a file for an assignment on first course
    * as teacher, make annotations in speedgrader for first assignment
    * as student, submit the file from already submitted files
    * at teacher, go into speedgrader for second assignment and ensure
      annotations from first are not there
    
    Change-Id: I32c9825c0649c4479300fb3fadf6e77bb20c46df
    Reviewed-on: https://gerrit.instructure.com/211336
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Rex Fleischer <rfleischer@instructure.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1962,6 +1962,17 @@ class Attachment < ActiveRecord::Base
     false
   end
 
+  def self.copy_attachments_to_submissions_folder(assignment_context, attachments)
+    attachments.map do |attachment|
+      if attachment.context.respond_to?(:submissions_folder)
+        # if its already in the submission folder, then we need to make a copy of it.
+        attachment.copy_to_folder!(attachment.context.submissions_folder(assignment_context))
+      else
+        attachment # in a weird context; leave it alone
+      end
+    end
+  end
+
   def set_publish_state_for_usage_rights
     if self.context &&
        (!self.folder || !self.folder.for_submissions?) &&
