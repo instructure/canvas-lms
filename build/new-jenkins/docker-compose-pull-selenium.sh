ea88c7087ec2acb1c5c966764110d94900e7858f
James Butters <jbutters@instructure.com>
2019-10-02 11:57:48 -0600

    spec: downgrade docker chromedriver
    there are some bugs in the newest version causing spec failures.
    
    Change-Id: I5b2864815f552831d273b74843e129228b581f55
    Reviewed-on: https://gerrit.instructure.com/211810
    Tested-by: Jenkins
    Reviewed-by: James Williams <jamesw@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/docker-compose-pull-selenium.sh
+++ build/new-jenkins/docker-compose-pull-selenium.sh
@@ -3,6 +3,6 @@
 # pull docker images (or build them if missing)
 REGISTRY_BASE=starlord.inscloudgate.net/jenkins
 
-docker pull $REGISTRY_BASE/selenium-chrome:3.141.59-vanadium || \
-  (docker build -t $REGISTRY_BASE/selenium-chrome:3.141.59-vanadium docker-compose/selenium-chrome && \
-  docker push $REGISTRY_BASE/selenium-chrome:3.141.59-vanadium)
+docker pull $REGISTRY_BASE/selenium-chrome:3.141.59-selenium || \
+  (docker build -t $REGISTRY_BASE/selenium-chrome:3.141.59-selenium docker-compose/selenium-chrome && \
+  docker push $REGISTRY_BASE/selenium-chrome:3.141.59-selenium)
