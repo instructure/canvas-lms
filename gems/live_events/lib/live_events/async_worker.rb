e1e0dde546d70db6224be4590cd3ae072997fcd6
Marc Phillips <mphillips@instructure.com>
2019-10-04 15:55:39 -0600

    Enrich live_events put_records errors
    Better tag metrics in datadog to allow aggregations
    across all event_types. Also allow for request_id
    to be sent to datadog when a failure is made so we
    can easily track down the problem request in logs.
    
    closes PLAT-5026
    
    Test Plan:
     - force a live event to fail putting to kinesis (you can
       do this by updating the stubbed client to add error code)
     - note that the enrichment is now present
     - see that the dropped event is also pushed to sentry and
       the logs
    
    Change-Id: I6c82081c69ced40b32294bdd0e7f21849b19494e
    Reviewed-on: https://gerrit.instructure.com/212192
    Tested-by: Jenkins
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Marc Phillips <mphillips@instructure.com>

--- gems/live_events/lib/live_events/async_worker.rb
+++ gems/live_events/lib/live_events/async_worker.rb
@@ -59,8 +59,10 @@ module LiveEvents
       @queue << {
         data: event_json,
         partition_key: partition_key,
-        statsd_prefix: "live_events.events.#{event[:attributes][:event_name]}",
-        total_bytes: total_bytes
+        statsd_prefix: "live_events.events",
+        tags: { event: event[:attributes][:event_name] },
+        total_bytes: total_bytes,
+        request_id: event[:attributes][:request_id]
       }
       true
     end
@@ -146,18 +148,28 @@ module LiveEvents
 
     def process_results(res, records)
       res.records.each_with_index do |r, i|
+        rec = records[i]
         if r.error_code.present?
-          log_unprocessed(records[i])
+          log_unprocessed(rec, r.error_code, r.error_message)
         else
-          LiveEvents&.statsd&.increment("#{records[i][:statsd_prefix]}.sends")
+          LiveEvents&.statsd&.increment("#{rec[:statsd_prefix]}.sends", tags: rec[:tags])
           nil
         end
       end.compact
     end
 
-    def log_unprocessed(record)
-      logger.error("Error posting event #{record[:statsd_prefix]} with partition key #{record[:partition_key]}: #{record[:data]}")
-      LiveEvents&.statsd&.increment("#{record[:statsd_prefix]}.send_errors")
+    def log_unprocessed(record, error_code, error_message)
+      logger.error(
+        "Error posting event #{record.dig(:tags, :event)} with partition key #{record[:partition_key]}. Error Code: #{error_code} | Error Message: #{error_message}"
+      )
+      logger.debug(
+        "Failed event data: #{record[:data]}"
+      )
+      LiveEvents&.error_reporter&.capture(:dropped_live_event, message: { request_id: record[:request_id], error_code: error_code, error_message: error_message }.to_json)
+      LiveEvents&.statsd&.increment(
+        "#{record[:statsd_prefix]}.send_errors",
+        tags: rec[:tags].merge(error_code: error_code)
+      )
     end
   end
 end
