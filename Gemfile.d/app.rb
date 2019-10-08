1cd46475315e0de23f9cbe243266a0f2bce68c8e
James Butters <jbutters@instructure.com>
2019-10-08 15:37:50 -0600

    lock version of sprockets to 3.7.2
    Change-Id: Ie8794b227656fd1ce30c39a616af24baaef01276
    Reviewed-on: https://gerrit.instructure.com/212524
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    Tested-by: Jenkins
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -129,6 +129,7 @@ gem 'inst-jobs-statsd', '1.2.3'
 gem 'gepub', '1.0.1'
 gem 'imperium', '0.5.0', require: false
 gem 'academic_benchmarks', '0.0.11', require: false
+  gem 'sprockets', '3.7.2'
 
 gem 'graphql', '1.9.11'
 gem 'graphql-batch', '0.4.1'
