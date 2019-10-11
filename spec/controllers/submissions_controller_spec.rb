90f5ffe5a335f19120a785956c09227a80b573f8
Mysti Lilla <mysti@instructure.com>
2019-10-11 19:51:36 +0000

    Revert "update submission attachment copy to always create new attachment"
    This reverts commit 61c75bae54eab270c9e32183d269a6811faa35cd.
    
    fixes ADMIN-2981
    
    Change-Id: Ic782bcc79741272555706823f788b9510a0ed98f
    Reviewed-on: https://gerrit.instructure.com/212933
    Tested-by: Jenkins
    Reviewed-by: Carl Kibler <ckibler@instructure.com>
    QA-Review: Carl Kibler <ckibler@instructure.com>
    Product-Review: Mysti Lilla <mysti@instructure.com>

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -1107,4 +1107,42 @@ describe SubmissionsController do
       end
     end
   end
+
+  describe "copy_attachments_to_submissions_folder" do
+    before(:once) do
+      course_with_student
+      @course.account.enable_service(:avatars)
+      attachment_model(context: @student)
+    end
+
+    it "copies a user attachment into the user's submissions folder" do
+      atts = SubmissionsController.copy_attachments_to_submissions_folder(@course, [@attachment])
+      expect(atts.length).to eq 1
+      expect(atts[0]).not_to eq @attachment
+      expect(atts[0].folder).to eq @student.submissions_folder(@course)
+    end
+
+    it "leaves files already in submissions folders alone" do
+      @attachment.folder = @student.submissions_folder(@course)
+      @attachment.save!
+      atts = SubmissionsController.copy_attachments_to_submissions_folder(@course, [@attachment])
+      expect(atts).to eq [@attachment]
+    end
+
+    it "copies a group attachment into the group submission folder" do
+      group_model(context: @course)
+      attachment_model(context: @group)
+      atts = SubmissionsController.copy_attachments_to_submissions_folder(@course, [@attachment])
+      expect(atts.length).to eq 1
+      expect(atts[0]).not_to eq @attachment
+      expect(atts[0].folder).to eq @group.submissions_folder
+    end
+
+    it "leaves files in non user/group context alone" do
+      assignment_model(context: @course)
+      weird_file = @assignment.attachments.create! display_name: 'blah', uploaded_data: default_uploaded_data
+      atts = SubmissionsController.copy_attachments_to_submissions_folder(@course, [weird_file])
+      expect(atts).to eq [weird_file]
+    end
+  end
 end
