757edc66fe31cb7dd8b5c3226182713311b3650a
Simon Williams <simon@instructure.com>
2019-10-11 15:10:31 -0500

    standardize on json-jwt, and bump to 1.10.2
    https://github.com/nov/json-jwt/compare/v1.9.4...v1.10.2
    
    We already used json-jwt in all but one place (canvas_panda_pub), so
    move that gem to use it as well. The plain `jwt` gem is still pulled in
    by a couple other dependencies, but not used anywhere in our code.
    
    Change-Id: I023ef76e5b1dc0e7603cd6fed28a7f53266f8db9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/213379
    Tested-by: Jenkins
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -84,8 +84,7 @@ gem 'simple_oauth', '0.3.1', require: false
 gem 'json', '2.2.0'
 gem 'link_header', '0.0.8'
 gem 'oj', '3.3.9'
-gem 'jwt', '1.5.6', require: false
-gem 'json-jwt', '1.9.4', require: false
+gem 'json-jwt', '1.10.2', require: false
 gem 'twilio-ruby', '5.27.1', require: false
 
 gem 'mail', '2.7.1', require: false
