ad1e20d35684951a690939855af8b3e0b49324e0
Landon Gilbert-Bland <lbland@instructure.com>
2019-10-25 09:37:15 -0600

    Fix sharding error with unread submission comments count
    Test Plan:
      - unread submission comments returns correct data in a sharded env
    
    refs COMMS-2481
    
    flag = none
    
    Change-Id: Ibd3371b03e69c8317b1f8fda0ce995bd1e286bbe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/214694
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Tested-by: Jenkins
    Product-Review: Landon Gilbert-Bland <lbland@instructure.com>

--- app/graphql/interfaces/submission_interface.rb
+++ app/graphql/interfaces/submission_interface.rb
@@ -45,13 +45,15 @@ class UnreadCommentCountLoader < GraphQL::Batch::Loader
       count
 
     submission_ids_and_attempts.each do |submission_id, attempt|
+      relative_submission_id = Shard.relative_id_for(submission_id, Shard.current, Shard.current)
+
       # Group attempts nil, zero, and one together as one set of unread counts
       count = if (attempt || 0) <= 1
-        (unread_count_hash[[submission_id, nil]] || 0) +
-        (unread_count_hash[[submission_id, 0]] || 0) +
-        (unread_count_hash[[submission_id, 1]] || 0)
+        (unread_count_hash[[relative_submission_id, nil]] || 0) +
+        (unread_count_hash[[relative_submission_id, 0]] || 0) +
+        (unread_count_hash[[relative_submission_id, 1]] || 0)
       else
-        unread_count_hash[[submission_id, attempt]] || 0
+        unread_count_hash[[relative_submission_id, attempt]] || 0
       end
 
       fulfill([submission_id, attempt], count)
