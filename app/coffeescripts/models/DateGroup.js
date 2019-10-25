56619c785efa5854f0d8f85bd2cff644804fdf65
Ryan Shaw <ryan@instructure.com>
2019-10-24 20:40:11 -0600

    Decaffeinate assignmentKeyBindingsMixin
    I'm just doing the rename and syntax changes as one commit but I 
    did them in 2 patchsets so you can see the diff easier than just
    seeing a deleted file and a new file
    
    diff against patchset 1 to see the actual syntax differences:
    https://gerrit.instructure.com/#/c/canvas-lms/+/214668/1..4
    
    
    Change-Id: Id16242a521e5ebb6161c27957eaf0683470e943f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/214668
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

---
+++ app/coffeescripts/models/DateGroup.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2013 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Backbone from 'Backbone'
+import I18n from 'i18n!models_DateGroup'
+import tz from 'timezone'
+
+export default class DateGroup extends Backbone.Model {
+  dueAt() {
+    const dueAt = this.get('due_at')
+    if (dueAt) {
+      return tz.parse(dueAt)
+    } else {
+      return null
+    }
+  }
+
+  unlockAt() {
+    const unlockAt = this.get('unlock_at')
+    if (unlockAt) {
+      return tz.parse(unlockAt)
+    } else {
+      return null
+    }
+  }
+
+  lockAt() {
+    const lockAt = this.get('lock_at')
+    if (lockAt) {
+      return tz.parse(lockAt)
+    } else {
+      return null
+    }
+  }
+
+  now() {
+    const now = this.get('now')
+    if (now) {
+      return tz.parse(now)
+    } else {
+      return new Date()
+    }
+  }
+
+  // no lock/unlock dates
+  alwaysAvailable() {
+    return !this.unlockAt() && !this.lockAt()
+  }
+
+  // not unlocked yet
+  pending() {
+    const unlockAt = this.unlockAt()
+    return unlockAt && unlockAt > this.now()
+  }
+
+  // available and won't ever lock
+  available() {
+    return this.alwaysAvailable() || (!this.lockAt() && this.unlockAt() < this.now())
+  }
+
+  // available, but will lock at some point
+  open() {
+    return this.lockAt() && !this.pending() && !this.closed()
+  }
+
+  // locked
+  closed() {
+    const lockAt = this.lockAt()
+    return lockAt && lockAt < this.now()
+  }
+
+  toJSON() {
+    return {
+      dueFor: this.get('title'),
+      dueAt: this.dueAt(),
+      unlockAt: this.unlockAt(),
+      lockAt: this.lockAt(),
+      available: this.available(),
+      pending: this.pending(),
+      open: this.open(),
+      closed: this.closed()
+    }
+  }
+}
+DateGroup.prototype.defaults = {
+  title: I18n.t('everyone_else', 'Everyone else'),
+  due_at: null,
+  unlock_at: null,
+  lock_at: null
+}
