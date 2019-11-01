a0a269ca9dee6f96f020793f4be7cfb48541f86d
Brian Sadler <sadler.brian@gmail.com>
2019-10-31 23:05:59 -0400

    More stuff to revert to troubleshoot.

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-
-# TODO: remove me. Still trying to figure out what's going on in heroku!
-pp "Running boot.rb! LOGGER_TYPE=#{ENV['LOGGER_TYPE']}"
