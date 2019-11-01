9aca945a0e9da33e63947cba5c41ce38dfb518b5
Brian Sadler <sadler.brian@gmail.com>
2019-11-01 13:11:37 -0400

    Revert "More tinkering with heroku.yml for troubleshooting."
    This reverts commit 7c1db0923e5b10473ed5887342524725b2288a22.

--- heroku_troubleshooting_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Build is done, deploying container!"
-echo "Testing enc vars: This is in heroku config but now in heroku.yml. What's the value? BZ_BASE_URL=$BZ_BASE_URL" 
-echo "This is in heroku.yml and heroky config. Value? LOGGER_TYPE=$LOGGER_TYPE" 
