7a01b170189dae39f353a5f6f3d2903214fd399e
jonw <jonw@instructure.com>
2019-11-04 10:16:14 -0700

    remove direct_share menu items from modules
    reverts 003f28e1f41c445f36abfac4a0ff775059ba224e
    
    we aren't going to support direct share with modules or module items on
    initial release
    
    closes LA-12
    flag=direct_share
    
    test plan:
    - with direct share feature flag on, the kabob menu on modules and
      module items should not have the "copy to" or "send to" menu items
    
    Change-Id: I5bccae079070606e94ec7a28c7eb9b8f1bcbe113
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/215792
    Tested-by: Jenkins
    Reviewed-by: Carl Kibler <ckibler@instructure.com>
    QA-Review: Carl Kibler <ckibler@instructure.com>
    Product-Review: Jon Willesen <jonw+gerrit@instructure.com>

--- public/javascripts/context_modules.js
+++ public/javascripts/context_modules.js
@@ -1857,26 +1857,6 @@ modules.initModuleManagement = function() {
       .catch(showFlashError('Error duplicating item'))
   })
 
-  $('.module_copy_to').live('click', event => {
-    event.preventDefault()
-    console.log('copy module to course')
-  })
-
-  $('.module_send_to').live('click', event => {
-    event.preventDefault()
-    console.log('send module to user')
-  })
-
-  $('.module_item_copy_to').live('click', event => {
-    event.preventDefault()
-    console.log('copy module item to course')
-  })
-
-  $('.module_item_send_to').live('click', event => {
-    event.preventDefault()
-    console.log('send module item to user')
-  })
-
   $('#add_module_prerequisite_dialog .cancel_button').click(() => {
     $('#add_module_prerequisite_dialog').dialog('close')
   })
