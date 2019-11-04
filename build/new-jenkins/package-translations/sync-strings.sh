5b3543ebae2a767e9a51b5842c882e0481229067
Clay Diffrient <cdiffrient@instructure.com>
2019-11-04 09:50:48 -0700

    Create shared translation infrastructure for packages
    Test Plan:
      - Translations get built and put into
        packages/translations/lib/
    
    closes COREFE-258
    
    flag = none
    
    Change-Id: I05889f9c122aaf1263a6803daad8abd977b32e44
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/213813
    Tested-by: Jenkins
    Reviewed-by: Landon Gilbert-Bland <lbland@instructure.com>
    QA-Review: Steve Shepherd <sshepherd@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

---
+++ build/new-jenkins/package-translations/sync-strings.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+##
+# This script takes an en.json file for package translations, and sends it
+# through the translation process.  It will then push up any changes in translations
+# to the repo.
+##
+
+export AWS_ROLE_ARN="arn:aws:iam::307761260553:role/translations-jenkins"
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -l '$SSH_USERNAME'"
+
+"$(yarn bin)/sync-translations" --ignore-jira --config ./package-translations/sync-config.json
+
+"$(yarn bin)/sync-translations" --ignore-jira --config ./package-translations/sync-config-crowd.json
+
+# Remove empty/missing strings from catalogs.
+for file in packages/translations/lib/*.json; do
+  if [[ "$file" == 'packages/translations/lib/en.json' ]]; then continue; fi
+  jq --indent 4 'with_entries(select(.value.message != ""))' "$file" > tmp.json && mv tmp.json "$file"
+done
+
+# If there are no changes to commit, bail out
+git diff-index --quiet HEAD -- packages/translations/lib
+if [ $? -eq 0 ]
+then
+  echo "No new translations to commit"
+  exit 0
+fi
+
+# Split out locales to their respective package folders
+# This allows consuming packages to pull in only the translations they care about
+# which reduces our overall webpack bundle size later.
+pushd packages/translations
+yarn split
+yarn lint
+popd
+
+git config --global user.name "Jenkins"
+git config --global user.email "hudson@instructure.com"
+
+gitdir=$(git rev-parse --git-dir); scp -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -p -P 29418 "${SSH_USERNAME}@gerrit.instructure.com:hooks/commit-msg" "${gitdir}/hooks/"
+# Commit any changes into a temp branch then push to gerrit
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git checkout -B sync-translations-tmp && \
+  git add -A packages/translations/lib && \
+  git commit -m "[i18n] Update package translations" && \
+  git push origin sync-translations-tmp:refs/for/master%submit,l=Verified+1 && \
+  git checkout "$CURRENT_BRANCH"
