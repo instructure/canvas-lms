9924687ebbc0d7c3af32f4e57458e72432ed0226
Steven Burnett <sburnett@instructure.com>
2019-11-16 15:53:59 -0700

    add pronouns to group manage page
    fixes KNO-114
    
    Test Plan:
    - With pronouns turned on and with a student
      who has pronouns configured go to http://localhost:3000/courses/2/groups#tab-1
    - notice pronouns are showing on the drag items
    
    Change-Id: I06a34c48d09499a6577b679cc7b5c9e55f5c0e86
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/217562
    Tested-by: Jenkins
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Steven Burnett <sburnett@instructure.com>

--- lib/api/v1/user.rb
+++ lib/api/v1/user.rb
@@ -65,9 +65,6 @@ module Api::V1::User
                       :integration_id => pseudonym&.integration_id
         end
 
-        if user.pronouns
-          json[:pronouns] = user.pronouns
-        end
 
         if !excludes.include?('pseudonym') && user_json_is_admin?(context, current_user)
           json[:sis_import_id] = pseudonym&.sis_batch_id if @domain_root_account.grants_right?(current_user, session, :manage_sis)
@@ -77,6 +74,10 @@ module Api::V1::User
             json[:login_id] = pseudonym.unique_id
           end
         end
+      end
+
+      if user.pronouns
+        json[:pronouns] = user.pronouns
       end
 
       if includes.include?('avatar_url') && user.account.service_enabled?(:avatars)
