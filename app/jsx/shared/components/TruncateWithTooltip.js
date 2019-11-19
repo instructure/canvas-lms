238b27aab8bed42e07f79221d01c61479944bfe4
Michael Brewer-Davis <mbrewerdavis@instructure.com>
2019-11-19 12:51:37 -0600

    truncate outcome titles in individ results
    closes OUT-3381
    
    Test plan:
    - create outcomes and outcome groups with long
      and short titles
    - align assignments to the created outcomes
    - assess a student for some of those assignments
    - visit the student's page in the learning mastery
      gradebook
      (go to learning mastery gradebook and click on
      student's name)
    - verify that the long groups and outcomes are
      truncated and have tooltips with their complete names
    - verify that short groups and outcomes do not
      have tooltips
    
    Change-Id: Ibc3ae5a3234188219f1ffe56995344d117c7cb19
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/217843
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Pat Renner <prenner@instructure.com>
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>
    QA-Review: Pat Renner <prenner@instructure.com>
    Product-Review: Jody Sailor

---
+++ app/jsx/shared/components/TruncateWithTooltip.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+import {TruncateText} from '@instructure/ui-elements'
+import {Tooltip} from '@instructure/ui-overlays'
+import {pickProps} from '@instructure/ui-react-utils'
+
+export default class TruncateWithTooltip extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  }
+
+  state = {
+    isTruncated: false
+  }
+
+  onTruncationUpdate = isTruncated => {
+    if (isTruncated !== this.state.isTruncated) {
+      this.setState({isTruncated})
+    }
+  }
+
+  render() {
+    const {children: text, ...remainingProps} = this.props
+    const tooltipProps = pickProps(remainingProps, Tooltip.propTypes)
+    const truncateProps = pickProps(remainingProps, TruncateText.propTypes)
+
+    const truncatedText = (
+      <TruncateText onUpdate={this.onTruncationUpdate} {...truncateProps}>
+        {text}
+      </TruncateText>
+    )
+    if (this.state.isTruncated) {
+      return (
+        <Tooltip {...tooltipProps} tip={text}>
+          <span>{truncatedText}</span>
+        </Tooltip>
+      )
+    } else {
+      return truncatedText
+    }
+  }
+}
