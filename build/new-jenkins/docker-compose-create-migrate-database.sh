67960857c4d4ec362d17864a619a2bde70ec5a5e
Derek Bender <djbender@instructure.com>
2019-11-20 14:56:09 -0600

    Fix cassandra build step for cached image
    Closes: CCI-107
    
    test plan:
    - build passes when using the cached cassandra image
      (i.e. the keyspace already exists)
    
    Change-Id: I0df0a1ff141acd4a30091f544b1058b24163dfba
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/218041
    Reviewed-by: Rex Fleischer <rfleischer@instructure.com>
    Reviewed-by: S. Jacob Powell <spowell@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: Jenkins
    QA-Review: Derek Bender <djbender@instructure.com>
    Product-Review: Derek Bender <djbender@instructure.com>

--- build/new-jenkins/docker-compose-create-migrate-database.sh
+++ build/new-jenkins/docker-compose-create-migrate-database.sh
@@ -3,7 +3,7 @@
 docker-compose exec -T cassandra ./wait-for-it
 docker-compose exec -T dynamodb ./wait-for-it
 for keyspace in auditors global_lookups page_views; do
-  docker-compose exec -T cassandra cqlsh -e "CREATE KEYSPACE ${keyspace} WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+  docker-compose exec -T cassandra cqlsh -e "CREATE KEYSPACE IF NOT EXISTS ${keyspace} WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
 done
 
 (docker-compose exec -T web bundle exec rails db:create db:migrate > /tmp/possible_migration_error.txt 2>&1; cat /tmp/possible_migration_error.txt) && rm /tmp/possible_migration_error.txt
