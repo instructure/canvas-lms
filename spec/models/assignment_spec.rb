076d66b9bb9709d614f0ea0921ea2f987964d762
Brian Sadler <sadler.brian@gmail.com>
2019-11-23 15:10:52 -0500

    Fix a couple specs that didn't require the sharding_spec_helper.

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -17,7 +17,7 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../sharding_spec_helper.rb')
 
 describe Assignment do
   before :once do
