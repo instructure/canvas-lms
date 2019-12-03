7bc28bf881e5b27e8bc5c68ab4c2ed9068cef5da
Robert Lamb <rlamb@instructure.com>
2019-12-03 15:24:30 -0700

    spec: remove COMPOSE_FILE from JS shell scripts
    refs: CCI-131
    
    test plan: The JS tests still pass.
    
    Change-Id: I382aeed29388da45f611b526c9485209c74fb6f1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/219341
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: James Butters <jbutters@instructure.com>
    Reviewed-by: Rex Fleischer <rfleischer@instructure.com>
    QA-Review: Robert Lamb <rlamb@instructure.com>
    Product-Review: Robert Lamb <rlamb@instructure.com>

--- build/new-jenkins/js/tests-karma.sh
+++ build/new-jenkins/js/tests-karma.sh
@@ -2,8 +2,6 @@
 
 set -x -o errexit -o errtrace -o nounset -o pipefail
 
-export COMPOSE_FILE="docker-compose.new-jenkins-web.yml:docker-compose.new-jenkins-karma.yml"
-
 NAME='tests-karma-'$JSPEC_GROUP
 
 docker-compose run --name $NAME -e COVERAGE karma yarn test:karma:headless
--- build/new-jenkins/js/tests-packages.sh
+++ build/new-jenkins/js/tests-packages.sh
@@ -2,6 +2,4 @@
 
 set -x -o errexit -o errtrace -o nounset -o pipefail
 
-export COMPOSE_FILE=./docker-compose.new-jenkins-web.yml
-
 docker-compose run --name tests-packages -e COVERAGE web yarn test:packages
