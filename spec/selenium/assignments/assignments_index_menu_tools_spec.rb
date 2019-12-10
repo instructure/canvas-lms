d7ab3df21fb503a5d5deadc7015d16e6aeb7de16
Anju Reddy <areddy@instructure.com>
2019-12-10 11:15:48 -0700

    spec: spec for lti menu overflows
    flag=Direct_Share
    flag=Commons Favorites
    
    Change-Id: I8417211f5dfda6ad26a70b2374d0235f9b995465
    REFS: LA-351
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/220216
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Anju Reddy <areddy@instructure.com>

--- spec/selenium/assignments/assignments_index_menu_tools_spec.rb
+++ spec/selenium/assignments/assignments_index_menu_tools_spec.rb
@@ -15,21 +15,32 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 require_relative '../common'
 require_relative '../../spec_helper'
+require_relative 'page_objects/assignments_index_page'
 
 describe 'assignments index menu tool placement' do
   include_context 'in-process server selenium tests'
+  include AssignmentsIndexPage
 
   before do
     course_with_teacher_logged_in
 
-    @tool = Account.default.context_external_tools.new(:name => "a", :domain => "google.com", :consumer_key => '12345', :shared_secret => 'secret')
-    @tool.assignment_index_menu = {:url => "http://www.example.com", :text => "Import Stuff"}
-    @tool.assignment_group_menu = {:url => "http://www.example.com", :text => "Import Stuff Here"}
-    @tool.save!
+    @tool1 = Account.default.context_external_tools.new(:name => "a", :domain => "google.com", :consumer_key => '12345', :shared_secret => 'secret')
+    # tool1 is on index and group menus
+    @tool1.assignment_index_menu = {:url => "http://www.example.com", :text => "Import Stuff"}
+    @tool1.assignment_group_menu = {:url => "http://www.example.com", :text => "Import Stuff Here"}
+    @tool1.save!
+    @tool2 = Account.default.context_external_tools.new(:name => "b", :domain => "google.com", :consumer_key => '12345', :shared_secret => 'secret')
+    # tool2 is on assignment menu
+    @tool2.assignment_menu = {:url => "http://www.example.com", :text => "Second Tool"}
+    @tool2.save!
+    # assignments groups
     @agroup1 = @course.assignment_groups.create!(:name => "assignments group1")
+    @course.assignments.create(name: "test assignment", assignment_group: @agroup1)
+    @assignment1 = @course.assignments.first
     @agroup2 = @course.assignment_groups.create!(:name => "assignments group2")
 
     Account.default.enable_feature!(:commons_favorites)
+    Account.default.enable_feature!(:direct_share)
   end
 
   it "should be able to launch the index menu tool via the tray", custom_timeout: 60 do
@@ -45,7 +56,7 @@ describe 'assignments index menu tool placement' do
     tray = f("[role='dialog']")
     expect(tray['aria-label']).to eq "Import Stuff"
     iframe = tray.find_element(:css, "iframe")
-    expect(iframe['src']).to include("/courses/#{@course.id}/external_tools/#{@tool.id}")
+    expect(iframe['src']).to include("/courses/#{@course.id}/external_tools/#{@tool1.id}")
     query_params = Rack::Utils.parse_nested_query(URI.parse(iframe['src']).query)
     expect(query_params["launch_type"]).to eq "assignment_index_menu"
     expect(query_params["com_instructure_course_allow_canvas_resource_selection"]).to eq "true"
@@ -68,7 +79,7 @@ describe 'assignments index menu tool placement' do
     tray = f("[role='dialog']")
     expect(tray['aria-label']).to eq "Import Stuff Here"
     iframe = tray.find_element(:css, "iframe")
-    expect(iframe['src']).to include("/courses/#{@course.id}/external_tools/#{@tool.id}")
+    expect(iframe['src']).to include("/courses/#{@course.id}/external_tools/#{@tool1.id}")
     query_params = Rack::Utils.parse_nested_query(URI.parse(iframe['src']).query)
     expect(query_params["launch_type"]).to eq "assignment_group_menu"
     expect(query_params["com_instructure_course_allow_canvas_resource_selection"]).to eq "false"
@@ -76,5 +87,15 @@ describe 'assignments index menu tool placement' do
     expect(query_params["com_instructure_course_accept_canvas_resource_types"]).to eq ["assignment"]
     group_data = [@agroup2].map{|ag| {"id" => ag.id.to_s, "name" => ag.name}} # just the selected group
     expect(query_params["com_instructure_course_available_canvas_resources"].values).to match_array(group_data)
+  end
+
+  it "should handle having more than one tool in the menu" do
+    user_session(@teacher)
+    visit_assignments_index_page(@course.id)
+
+    expect(assignment_row(@assignment1.id).text).not_to include 'Second Tool'
+
+    manage_assignment_menu(@assignment1.id).click
+    expect(assignment_settings_menu(@assignment1.id).text).to include 'Second Tool'
   end
 end
