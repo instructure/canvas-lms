a7ca52619f18f19744258ff86ddfb65dc64b1c46
Anju Reddy <areddy@instructure.com>
2019-12-10 17:20:01 -0700

    spec: skip flaky js test
    Change-Id: I77f0db9f5c4d08ca6dd9bad384f26443effb4a07
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/220312
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: Jenkins
    Reviewed-by: Jeremy Putnam <jeremyp@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Anju Reddy <areddy@instructure.com>

--- spec/coffeescripts/views/assignments/AssignmentListItemViewSpec.js
+++ spec/coffeescripts/views/assignments/AssignmentListItemViewSpec.js
@@ -309,15 +309,16 @@ test('opens and closes the direct share send to user dialog', async function() {
   await waitForElementToBeRemoved(() => queryByText(document.body, 'Send to:'))
 })
 
-test('opens and closes the direct share copy to course tray', async function() {
-  const view = createView(this.model, {directShareEnabled: true})
-  $('#fixtures').append('<div id="copy-to-mount-point" />')
-  view.$('.copy_assignment_to').click()
-  fetchMock.mock('/users/self/manageable_courses', [])
-  ok(await findByText(document.body, 'Select a Course'))
-  getByText(document.body, 'Close').click()
-  await waitForElementToBeRemoved(() => queryByText(document.body, 'Select a Course'))
-})
+// skip('will be fixed in LA-354')
+// test('opens and closes the direct share copy to course tray', async function() {
+//   const view = createView(this.model, {directShareEnabled: true})
+//   $('#fixtures').append('<div id="copy-to-mount-point" />')
+//   view.$('.copy_assignment_to').click()
+//   fetchMock.mock('/users/self/manageable_courses', [])
+//   ok(await findByText(document.body, 'Select a Course'))
+//   getByText(document.body, 'Close').click()
+//   await waitForElementToBeRemoved(() => queryByText(document.body, 'Select a Course'))
+// })
 
 test('does not show sharing and copying menu items if not DIRECT_SHARE_ENABLED', function() {
   const view = createView(this.model, {
