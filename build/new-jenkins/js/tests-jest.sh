f45ab202a31099eebc770c27de252f35e22deae2
Landon Gilbert-Bland <lbland@instructure.com>
2020-01-06 08:04:11 -0700

    Generate schema.graphql on demand
    This will fix some issues with jenkins builds failing if the
    patchset is behind some newer graphql changes, as well as allowing
    us to include content defined in our plugins in the graphql.schema
    file.
    
    Test Plan:
      - Jenkins passes
    
    fixes USERS-210
    
    flag = none
    
    Change-Id: Ic44849ee57955e0b90395661a66f6d5840a38e87
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/222029
    Reviewed-by: Rex Fleischer <rfleischer@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Steve Shepherd <sshepherd@instructure.com>
    Product-Review: Landon Gilbert-Bland <lbland@instructure.com>

--- build/new-jenkins/js/tests-jest.sh
+++ build/new-jenkins/js/tests-jest.sh
@@ -2,4 +2,5 @@
 
 set -x -o errexit -o errtrace -o nounset -o pipefail
 
-docker-compose run --name tests-jest -e COVERAGE web yarn test:jest --runInBand
+docker-compose run --name tests-jest -e COVERAGE -e RAILS_ENV=test web \
+    bash -c "bundle exec rails graphql:schema && yarn test:jest --runInBand"
