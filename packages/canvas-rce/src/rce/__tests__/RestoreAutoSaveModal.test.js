05ada029a9071f8181827975ec752114a293fe09
Ed Schiebel <eschiebel@instructure.com>
2020-01-07 17:16:25 -0500

    Autosave RCE content
    Autosaves what you type into the RCE in local storage
    keyed by "rceautosave:{your url here}:{textarea.id}"
    
    Only enabled for pages where there is one RCE.
    
    closes LA-219, LA-469
    flag=rce_auto_save
    
    rce_enhancements flag must also be on
    
    test plan:
      - create a new announcement or assignment
      - type some stuff in the rce
      - refresh the page
      > expect to be prompted to restore saved content
      - save the thing
      - edit the thing and make some changes
      - cancel
      - re-edit
      > expect to be prompted to restore saved content
      - edit again, make changes, save, re-edit
      > expect not to be prompted
      - do more stuff like that
    
      test max-age timeout
      - in the rails console, run
        Setting.set('rce_auto_save_max_age_ms', 10000)
      - create a new announcement, put some text in and
        wait 10 seconds
      - create a new assignment
        (jump to /courses/:id/assignments/new)
      > expect local storage to include the entry for
        the announcement's url is gone (you can do this
        with devtools)
    
      test exceeding storage quota
      - this is more easily tested in firefox
      - in FF go to about:config and search for
        dom.storage.default_quota
      - change the value to 1 (k)
      - create a new announcement and past almost-a-k's
        worth of text. this works:
    
    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
    Duis rhoncus erat nunc, ut euismod odio tristique volutpat.
    Vivamus porttitor, nulla vel convallis ultrices, leo leo bibendum
    ligula, at egestas augue velit eget est. Vivamus tincidunt,
    metus non consectetur vulputate, ex neque sagittis neque,
    aliquet luctus ex felis sed neque. Nullam vel feugiat tellus,
    in sagittis lorem. Nullam facilisis turpis vel justo sollicitudin gravida.
    Sed volutpat vel velit a lobortis. Donec vel condimentum massa.
    Maecenas feugiat cursus condimentum. Nam iaculis ex vitae nibh interdum
    pretium. Duis tincidunt diam sit amet fringilla finibus. Aenean magna
    velit, accumsan eget scelerisque ut, mattis in erat.
    
      - w/o saving, create create a new assignment,
        (/courses/:id/assignments/new)
        add ^that lorem ipsum, and refresh
      > expect localStorage to have deleted the announcement's auto-saved text
        and saved the assignment's text
    
    Change-Id: Ia5f8161cc28213d2361054ddad1b189a3bad06d9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/222311
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jon Willesen <jonw+gerrit@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Lauren Williams <lcwilliams@instructure.com>

---
+++ packages/canvas-rce/src/rce/__tests__/RestoreAutoSaveModal.test.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import RestoreAutoSaveModal from '../RestoreAutoSaveModal'
+
+describe('RestoreAutoSaveModal', () => {
+  it('renders all its content', () => {
+    const onNo = jest.fn()
+    const {getByText} = render(
+      <RestoreAutoSaveModal savedContent="<p>hello world</p>" open onNo={onNo} onYes={() => {}} />
+    )
+
+    expect(getByText('Found auto-saved content')).toBeInTheDocument()
+    expect(
+      getByText(
+        'There is auto-saved content that differs from the saved content. Would you like to load it instead?'
+      )
+    ).toBeInTheDocument()
+    expect(getByText('Preview')).toBeInTheDocument()
+    expect(getByText('Click to show preview')).toBeInTheDocument()
+    expect(getByText('No')).toBeInTheDocument()
+    expect(getByText('Yes')).toBeInTheDocument()
+    const closeButton = getByText('Close')
+    expect(closeButton).toBeInTheDocument()
+    fireEvent.click(closeButton.closest('button'))
+    expect(onNo).toHaveBeenCalled()
+  })
+
+  it('responds to clicking "yes"', () => {
+    const onNo = jest.fn()
+    const onYes = jest.fn()
+    const {getByText} = render(
+      <RestoreAutoSaveModal savedContent="<p>hello world</p>" open onNo={onNo} onYes={onYes} />
+    )
+
+    const yesButton = getByText('Yes').closest('button')
+    fireEvent.click(yesButton)
+    expect(onYes).toHaveBeenCalled()
+    expect(onNo).not.toHaveBeenCalled()
+  })
+
+  it('responds to clicking "no"', () => {
+    const onNo = jest.fn()
+    const onYes = jest.fn()
+    const {getByText} = render(
+      <RestoreAutoSaveModal savedContent="<p>hello world</p>" open onNo={onNo} onYes={onYes} />
+    )
+
+    const noButton = getByText('No').closest('button')
+    fireEvent.click(noButton)
+    expect(onYes).not.toHaveBeenCalled()
+    expect(onNo).toHaveBeenCalled()
+  })
+
+  it('displays the auto saved content preview', () => {
+    const {getByText} = render(
+      <RestoreAutoSaveModal
+        savedContent="<p>hello world</p>"
+        open
+        onNo={() => {}}
+        onYes={() => {}}
+      />
+    )
+
+    const previewButton = getByText('Click to show preview')
+    expect(previewButton).toBeInTheDocument()
+    fireEvent.click(previewButton.closest('button'))
+    expect(getByText('hello world')).toBeInTheDocument()
+    expect(getByText('Click to hide preview')).toBeInTheDocument()
+  })
+})
