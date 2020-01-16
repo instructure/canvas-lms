38a0561bd33eaf8d3174a2eee513b1b829120548
sshepherd <sshepherd@instructure.com>
2020-01-16 10:14:27 -0700

    spec: fix flaky selenium spec search_ap_spec.rb
    closes: USERS-197
    
    It seems to be passing fine every single time I run this.
    I am unskipping it for now, and we will see if it fails
    again up on jenkins on my commit. After retriggering a few times
    and seeing it succeed, I will merge. otherwise I will look deeper
    and see what the issue is.
    
    Change-Id: Ieb8b1f50e26fb0e7cdcd6f6bb6d43ddabe6d4df0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/223468
    Reviewed-by: Charley Kline <ckline@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Steve Shepherd <sshepherd@instructure.com>
    Product-Review: Steve Shepherd <sshepherd@instructure.com>

--- spec/apis/v1/search_api_spec.rb
+++ spec/apis/v1/search_api_spec.rb
@@ -510,7 +510,7 @@ describe SearchController, type: :request do
       specs_require_cache(:redis_cache_store)
 
       it "should show new groups in existing categories" do
-        skip('LA-382')
+
         json = api_call(:get, "/api/v1/search/recipients.json?context=course_#{@course.id}_groups&synthetic_contexts=1",
           {:controller => 'search', :action => 'recipients', :format => 'json', :context => "course_#{@course.id}_groups", :synthetic_contexts => "1"})
         expect(json.map{|r| r["id"]}).to eq ["group_#{@group.id}"]
