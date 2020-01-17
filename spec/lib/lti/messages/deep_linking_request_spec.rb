faf6aaf842ca45caf0c8a80b023305aa066945c9
Xander Moffatt <xmoffatt@instructure.com>
2020-01-17 13:33:39 -0700

    accept multiple deep linking items in link selection
    closes PLAT-5260
    flag=none
    
    test plan:
    * install the LTI 1.3 test tool with a placement of link_selection
    * create a module in a course
    * add an item to that module and select
    External Tool from the dropdown
    * open the dev tools to the Network tab
    * select your test tool installation
    * in the Network tab, look for the request to
    the test tool, to the `/launch` url
    * open that and look in the params for the id_token
    * copy that and go to jwt.io to decode it
    * the value for accept_multiple should be true
    
    Change-Id: Ie5d87381cf66e9a14676ee7024802396bf866c7c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/223720
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Marc Phillips <mphillips@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>

--- spec/lib/lti/messages/deep_linking_request_spec.rb
+++ spec/lib/lti/messages/deep_linking_request_spec.rb
@@ -42,10 +42,6 @@ describe Lti::Messages::DeepLinkingRequest do
   describe '#generate_post_payload_message' do
     subject { jws['https://purl.imsglobal.org/spec/lti-dl/claim/deep_linking_settings'] }
 
-    it 'sets "accept_multiple" to false' do
-      expect(subject['accept_multiple']).to eq true
-    end
-
     it 'sets the "deep_link_return_url"' do
       expect(subject['deep_link_return_url']).to eq deep_linking_return_url
     end
@@ -76,8 +72,8 @@ describe Lti::Messages::DeepLinkingRequest do
         expect(subject['auto_create']).to eq false
       end
 
-      it 'sets the correct "accept_multiple"' do
-        expect(subject['accept_multiple']).to eq false
+      it 'sets "accept_multiple to true"' do
+        expect(subject['accept_multiple']).to eq true
       end
     end
 
@@ -189,6 +185,10 @@ describe Lti::Messages::DeepLinkingRequest do
 
       it 'sets the correct "auto_create"' do
         expect(subject['auto_create']).to eq false
+      end
+
+      it 'sets "accept_multiple" to true ' do
+        expect(subject['accept_multiple']).to eq true
       end
     end
   end
