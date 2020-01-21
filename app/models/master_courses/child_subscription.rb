755b33749ee8091ec85cba0cee86a186ebbcccaf
James Williams <jamesw@instructure.com>
2020-01-21 09:23:04 -0700

    add stranding to blueprint subscription deletion job
    and make migration_id changing run outside of transaction
    in smaller batches
    
    also add some indexes to help the queries with
    pattern matching on migration_id
    
    test plan:
    * specs pass
    
    closes #LA-502
    
    Change-Id: I364fa9aa3f375a6910683bba9b54b1433b6cd143
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/223982
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/models/master_courses/child_subscription.rb
+++ app/models/master_courses/child_subscription.rb
@@ -62,12 +62,16 @@ class MasterCourses::ChildSubscription < ActiveRecord::Base
     # mess up the migration ids so restrictions no longer get applied
     if workflow_state_changed?
       if deleted? && workflow_state_was == 'active'
-        self.unlink_syllabus!
-        self.add_deactivation_prefix!
+        self.class.connection.after_transaction_commit do
+          self.unlink_syllabus!
+          self.add_deactivation_prefix!
+        end
       elsif active? && workflow_state_was == 'deleted'
         self.use_selective_copy = false # require a full import next time
-        self.link_syllabus!
-        self.remove_deactivation_prefix!
+        self.class.connection.after_transaction_commit do
+          self.link_syllabus!
+          self.remove_deactivation_prefix!
+        end
       end
     end
   end
@@ -88,7 +92,7 @@ class MasterCourses::ChildSubscription < ActiveRecord::Base
   end
 
   def add_deactivation_prefix!
-    where_clause = ["migration_id LIKE ?", "#{MasterCourses::MasterTemplate.migration_id_prefix(self.shard.id, self.master_template_id)}%"]
+    where_clause = ["migration_id IS NOT NULL AND migration_id LIKE ?", "#{MasterCourses::MasterTemplate.migration_id_prefix(self.shard.id, self.master_template_id)}%"]
     update_query = ["migration_id = concat(?, migration_id)", self.deactivation_prefix]
     update_content_in_child_course(where_clause, update_query)
   end
@@ -102,7 +106,9 @@ class MasterCourses::ChildSubscription < ActiveRecord::Base
   def update_content_in_child_course(where_clause, update_query)
     if self.child_content_tags.where(where_clause).update_all(update_query) > 0 # don't run all the rest of it if there's no reason to
       self.content_scopes_for_deactivation.each do |scope|
-        scope.where(where_clause).update_all(update_query)
+        scope.where(where_clause).find_ids_in_batches do |ids|
+          scope.where(where_clause).where(scope.klass.primary_key => ids).update_all(update_query)
+        end
       end
     end
   end
