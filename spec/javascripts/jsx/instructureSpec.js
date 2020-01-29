58dbef2a81dcbe5dbcf0547d245c7cdc634fe569
Adrian Packel <apackel@instructure.com>
2020-01-29 14:20:28 -0600

    Disable user content enhancement in ePortfolios
    flag = none
    fixes TALLY-468
    
    Test plan:
    - Create a new ePortfolio
      - Take note of its ID
    - Add a new section (call it, e.g., "MySection")
    - Add a page with an HTML component (call it, e.g., "MyPage")
    - Add HTML content like so:
      <div class="user_content unenhanced">
        <div class="tabs enhanceable_content">
          <ul>
            <li><a href="https://somesite.com">some site</a></li>
          </ul>
        </div>
      </div>
    
    (The destination and inner text of the link may be whatever you like,
    but the rest of the content needs to have the structure shown above.)
    
    - Save the page
    
    - Examine your new page from the following vantage points:
      - The "preview" view of the main ePortfolio
        - http://<host>/eportfolios/<id>?view=preview
      - The view of the section
        - http://<host>/eportfolios/<id>/MySection
      - The view of the specific page you added
        - http://<host>/eportfolios/<id>/MySection/MyPage
    
    - In all cases, the content you entered above should continue to appear
      as a list containing links, and *not* be automagically transformed
      into jQuery tabs that then attempt to load said links
    
    Change-Id: I98cab671bfb37f8be2a92ba6caa5a7e58ec8047f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/224877
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Spencer Olson <solson@instructure.com>

--- spec/javascripts/jsx/instructureSpec.js
+++ spec/javascripts/jsx/instructureSpec.js
@@ -18,58 +18,78 @@
 
 import $ from 'jquery'
 import {enhanceUserContent} from 'instructure'
+import fakeENV from 'helpers/fakeENV'
 
-let elem
-QUnit.module('Enhance User Content', {
-  setup() {
+QUnit.module('Enhance User Content', hooks => {
+  let elem
+
+  hooks.beforeEach(() => {
     elem = document.createElement('div')
     document.body.appendChild(elem)
-  },
+  })
 
-  teardown() {
+  hooks.afterEach(() => {
     document.body.removeChild(elem)
-  }
-})
+  })
 
-test('youtube preview gets alt text from link data-preview-alt', () => {
-  const alt = 'test alt string'
-  elem.innerHTML = `
-    <div class="user_content">
-      <a href="#" class="instructure_video_link" data-preview-alt="${alt}">
-        Link
-      </a>
-    </div>
-  `
-  sandbox.stub($, 'youTubeID').returns(47)
-  enhanceUserContent()
-  equal(elem.querySelector('.media_comment_thumbnail').alt, alt)
-})
+  test('youtube preview gets alt text from link data-preview-alt', () => {
+    const alt = 'test alt string'
+    elem.innerHTML = `
+      <div class="user_content">
+        <a href="#" class="instructure_video_link" data-preview-alt="${alt}">
+          Link
+        </a>
+      </div>
+    `
+    sandbox.stub($, 'youTubeID').returns(47)
+    enhanceUserContent()
+    equal(elem.querySelector('.media_comment_thumbnail').alt, alt)
+  })
 
-test('youtube preview ignores missing alt', () => {
-  elem.innerHTML = `
-    <div class="user_content">
-      <a href="#" class="instructure_video_link">
-        Link
-      </a>
-    </div>
-  `
-  sandbox.stub($, 'youTubeID').returns(47)
-  enhanceUserContent()
-  ok(elem.querySelector('.media_comment_thumbnail').outerHTML.match('alt=""'))
-})
+  test('youtube preview ignores missing alt', () => {
+    elem.innerHTML = `
+      <div class="user_content">
+        <a href="#" class="instructure_video_link">
+          Link
+        </a>
+      </div>
+    `
+    sandbox.stub($, 'youTubeID').returns(47)
+    enhanceUserContent()
+    ok(elem.querySelector('.media_comment_thumbnail').outerHTML.match('alt=""'))
+  })
 
-test("enhance '.instructure_inline_media_comment' in questions", () => {
-  const mediaCommentThumbnailSpy = sandbox.spy($.fn, 'mediaCommentThumbnail')
-  elem.innerHTML = `
-    <div class="user_content"></div>
-    <div class="answers">
-      <a href="#" class="instructure_inline_media_comment instructure_video_link">
-        link
-      </a>
-    </div>
-  `
-  enhanceUserContent()
-  equal(mediaCommentThumbnailSpy.thisValues[0].length, 1) // for .instructure_inline_media_comment
-  equal(mediaCommentThumbnailSpy.thisValues[1].length, 1) // for .instructure_video_link
-  $.fn.mediaCommentThumbnail.restore()
+  test("enhance '.instructure_inline_media_comment' in questions", () => {
+    const mediaCommentThumbnailSpy = sandbox.spy($.fn, 'mediaCommentThumbnail')
+    elem.innerHTML = `
+      <div class="user_content"></div>
+      <div class="answers">
+        <a href="#" class="instructure_inline_media_comment instructure_video_link">
+          link
+        </a>
+      </div>
+    `
+    enhanceUserContent()
+    equal(mediaCommentThumbnailSpy.thisValues[0].length, 1) // for .instructure_inline_media_comment
+    equal(mediaCommentThumbnailSpy.thisValues[1].length, 1) // for .instructure_video_link
+    $.fn.mediaCommentThumbnail.restore()
+  })
+
+  test('does not enhance content if ENV.SKIP_ENHANCING_USER_CONTENT is set to true', () => {
+    fakeENV.setup({SKIP_ENHANCING_USER_CONTENT: true})
+
+    const mediaCommentThumbnailSpy = sandbox.spy($.fn, 'mediaCommentThumbnail')
+    elem.innerHTML = `
+      <div class="user_content"></div>
+      <div class="answers">
+        <a href="#" class="instructure_inline_media_comment instructure_video_link">
+          link
+        </a>
+      </div>
+    `
+    enhanceUserContent()
+    strictEqual(mediaCommentThumbnailSpy.callCount, 0)
+
+    fakeENV.teardown()
+  })
 })
