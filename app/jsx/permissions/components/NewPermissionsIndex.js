05a232fbce1697da0695402e25a6fd73d4916c47
Charley Kline <ckline@instructure.com>
2020-02-02 16:10:50 -0600

    Add CanvasMultiSelect and use in permissions index
    Closes USERS-250
    Closes USERS-286
    flag=none
    
    Adds a "CanvasMultiSelect" component, for general use where the
    "multi" mode of the old InstUI <Select> component was.
    
    Temporarily points the PermissionsIndex at this new CanvasMultiSelect
    via a NewPermissionsIndex component.
    
    If UI and a11y looks good, a subsequent commit will promote the "new"
    PermissionsIndex and delete the old one.
    
    Test plan:
    * You can switch back and forth between the old <Select> and the new
      <CanvasMultiSelect> by changing the import source in permissions/
      index.js
    * I can't promise they work exactly identically, but all the same
      functionality should be there, and the UI and a11y should be
      acceptable in the multiple selection part of the Roles selector
      at the top of the table.
    
    Change-Id: I1951947d531377b91a981023e5d871c0aaed0faf
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/226141
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

---
+++ app/jsx/permissions/components/NewPermissionsIndex.js
@@ -0,0 +1,250 @@
+/*
+ * Copyright (C) 2018 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import I18n from 'i18n!permissions_v2'
+import React, {useRef, useState} from 'react'
+import useStateWithCallback from 'jsx/shared/hooks/useStateWithCallback'
+import {func, arrayOf} from 'prop-types'
+import {connect} from 'react-redux'
+import {debounce, isEqual} from 'lodash'
+import $ from 'jquery'
+import 'compiled/jquery.rails_flash_notifications'
+
+import {ScreenReaderContent} from '@instructure/ui-a11y'
+import {Button} from '@instructure/ui-buttons'
+import {Grid} from '@instructure/ui-grid'
+import {View} from '@instructure/ui-view'
+import {IconSearchLine} from '@instructure/ui-icons'
+import {Tabs} from '@instructure/ui-tabs'
+import CanvasMultiSelect from 'jsx/shared/components/CanvasMultiSelect'
+import {TextInput} from '@instructure/ui-text-input'
+import {Heading} from '@instructure/ui-heading'
+
+import actions from '../actions'
+import propTypes, {COURSE, ACCOUNT, ALL_ROLES_VALUE, ALL_ROLES_LABEL} from '../propTypes'
+
+import {ConnectedPermissionsTable} from './PermissionsTable'
+import {ConnectedPermissionTray} from './PermissionTray'
+import {ConnectedRoleTray} from './RoleTray'
+import {ConnectedAddTray} from './AddTray'
+
+function setDiff(minuend, subtrahend) {
+  const difference = new Set(minuend)
+  for (const elt of subtrahend) {
+    difference.delete(elt)
+  }
+  return difference
+}
+
+const SEARCH_DELAY = 350
+
+export default function PermissionsIndex(props) {
+  const setPermissionSearchString = useStateWithCallback('')[1]
+  const [contextType, setContextType] = useStateWithCallback(COURSE)
+  const [selectedTabId, setSelectedTabId] = useState('course')
+
+  function filterPermissions(searchString) {
+    props.searchPermissions({
+      contextType,
+      permissionSearchString: searchString
+    })
+  }
+
+  const debouncedFilterPermissions = useRef(
+    debounce(filterPermissions, SEARCH_DELAY, {
+      leading: false,
+      trailing: true
+    })
+  )
+
+  function onRoleFilterChange(newIdList) {
+    let resultSet = new Set(newIdList)
+    const oldSet = new Set(props.selectedRoles.map(r => r.id || r.value))
+    if (oldSet.size === 0) oldSet.add(ALL_ROLES_VALUE)
+    if (resultSet.size === 0) resultSet.add(ALL_ROLES_VALUE)
+
+    if (!oldSet.has(ALL_ROLES_VALUE) && resultSet.has(ALL_ROLES_VALUE)) {
+      resultSet = new Set(ALL_ROLES_VALUE)
+    } else {
+      const added = setDiff(resultSet, oldSet)
+      const removed = setDiff(oldSet, resultSet)
+      if (added.size > 0) {
+        const addedRole = props.roles.find(r => added.has(r.id || r.value))
+        $.screenReaderFlashMessage(I18n.t('%{value} Added', {value: addedRole.label}))
+      }
+      if (removed.size > 0) {
+        const removedRole = props.roles.find(r => removed.has(r.id || r.value))
+        $.screenReaderFlashMessage(I18n.t('%{value} Removed', {value: removedRole.label}))
+      }
+    }
+
+    if (resultSet.size > 1) resultSet.delete(ALL_ROLES_VALUE)
+    if (isEqual(oldSet, resultSet)) return
+    const sel = props.roles.filter(r => resultSet.has(r.id))
+    props.filterRoles({
+      selectedRoles: sel,
+      contextType
+    })
+  }
+
+  function onSearchStringChange(e) {
+    setPermissionSearchString(e.target.value, debouncedFilterPermissions.current)
+  }
+
+  function onTabChanged(_e, {id}) {
+    if (id === selectedTabId) return
+    const newContextType = id === 'course' ? COURSE : ACCOUNT
+    props.filterRoles({
+      selectedRoles: [{value: ALL_ROLES_VALUE, label: ALL_ROLES_LABEL}],
+      contextType
+    })
+    setPermissionSearchString('')
+    setContextType(newContextType, props.tabChanged)
+    setSelectedTabId(id)
+  }
+
+  function optionsToRender() {
+    const options = props.roles
+      .filter(role => role.contextType === contextType)
+      .map(role => (
+        <CanvasMultiSelect.Option
+          key={role.id}
+          id={role.id || role.value}
+          value={role.value || role.id}
+        >
+          {role.label}
+        </CanvasMultiSelect.Option>
+      ))
+
+    options.push(
+      <CanvasMultiSelect.Option key={ALL_ROLES_VALUE} id={ALL_ROLES_VALUE} value={ALL_ROLES_VALUE}>
+        {ALL_ROLES_LABEL}
+      </CanvasMultiSelect.Option>
+    )
+    return options
+  }
+
+  function renderHeader() {
+    const selectedIds = props.selectedRoles.map(r => r.id || r.value)
+    if (selectedIds.length === 0) selectedIds.push(ALL_ROLES_VALUE)
+    return (
+      <div className="permissions-v2__header_container">
+        <View display="block">
+          <Grid>
+            <Grid.Row vAlign="middle">
+              <Grid.Col width={3}>
+                <TextInput
+                  label={<ScreenReaderContent>{I18n.t('Search Permissions')}</ScreenReaderContent>}
+                  placeholder={I18n.t('Search Permissions')}
+                  icon={() => (
+                    <span disabled>
+                      <IconSearchLine focusable={false} />
+                    </span>
+                  )}
+                  onChange={onSearchStringChange}
+                  name="permission_search"
+                />
+              </Grid.Col>
+              <Grid.Col width={8}>
+                <CanvasMultiSelect
+                  id="permissions-role-filter"
+                  assistiveText={I18n.t(
+                    'Filter Roles. Type or use arrow keys to navigate. Multiple selections are allowed.'
+                  )}
+                  onChange={onRoleFilterChange}
+                  selectedOptionIds={selectedIds}
+                >
+                  {optionsToRender()}
+                </CanvasMultiSelect>
+              </Grid.Col>
+              <Grid.Col width={2}>
+                <Button
+                  id="add_role"
+                  variant="primary"
+                  margin="0 x-small 0 0"
+                  onClick={props.setAndOpenAddTray}
+                >
+                  {I18n.t('Add Role')}
+                </Button>
+              </Grid.Col>
+            </Grid.Row>
+          </Grid>
+        </View>
+      </div>
+    )
+  }
+
+  return (
+    <div className="permissions-v2__wrapper">
+      <ScreenReaderContent>
+        <Heading level="h1">{I18n.t('Permissions')}</Heading>
+      </ScreenReaderContent>
+      <ConnectedRoleTray />
+      <ConnectedAddTray />
+      <ConnectedPermissionTray
+        tab={props.roles.length ? props.roles.find(role => !!role.displayed).contextType : COURSE}
+      />
+      <Tabs onRequestTabChange={onTabChanged}>
+        <Tabs.Panel
+          id="course"
+          renderTitle={I18n.t('Course Roles')}
+          isSelected={selectedTabId === 'course'}
+        >
+          {renderHeader()}
+          <ConnectedPermissionsTable />
+        </Tabs.Panel>
+        <Tabs.Panel
+          id="account"
+          renderTitle={I18n.t('Account Roles')}
+          isSelected={selectedTabId === 'account'}
+        >
+          {renderHeader()}
+          <ConnectedPermissionsTable />
+        </Tabs.Panel>
+      </Tabs>
+    </div>
+  )
+}
+
+PermissionsIndex.propTypes = {
+  filterRoles: func.isRequired,
+  roles: arrayOf(propTypes.role).isRequired,
+  searchPermissions: func.isRequired,
+  tabChanged: func.isRequired,
+  selectedRoles: arrayOf(propTypes.filteredRole).isRequired
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    ...ownProps,
+    roles: state.roles,
+    selectedRoles: state.selectedRoles
+  }
+}
+
+const mapDispatchToProps = {
+  filterRoles: actions.filterRoles,
+  searchPermissions: actions.searchPermissions,
+  setAndOpenAddTray: actions.setAndOpenAddTray,
+  tabChanged: actions.tabChanged
+}
+
+export const ConnectedPermissionsIndex = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PermissionsIndex)
---
+++ app/jsx/shared/components/CanvasMultiSelect.js
@@ -0,0 +1,278 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useState, useRef, useMemo} from 'react'
+import I18n from 'i18n!app_shared_components'
+import keycode from 'keycode'
+import {Select} from '@instructure/ui-select'
+import {Tag} from '@instructure/ui-tag'
+import {func, string, node, arrayOf, oneOfType} from 'prop-types'
+import {matchComponentTypes} from '@instructure/ui-react-utils'
+import {compact, uniqueId} from 'lodash'
+import {Alert} from '@instructure/ui-alerts'
+
+const CanvasMultiSelectOption = () => <div />
+
+CanvasMultiSelectOption.propTypes = {
+  id: string.isRequired, // eslint-disable-line react/no-unused-prop-types
+  value: string.isRequired // eslint-disable-line react/no-unused-prop-types
+}
+
+function alwaysArray(scalarOrArray) {
+  if (!scalarOrArray) return null
+  return Array.isArray(scalarOrArray) ? scalarOrArray : [scalarOrArray]
+}
+
+function liveRegion() {
+  return document.getElementById('flash_screenreader_holder')
+}
+
+const NO_OPTIONS_OPTION_ID = '__no_option-'
+
+function CanvasMultiSelect(props) {
+  const {
+    id: selectId,
+    label: renderLabel,
+    onChange,
+    children,
+    selectedOptionIds,
+    noOptionsLabel,
+    ...otherProps
+  } = props
+
+  const [inputValue, setInputValue] = useState('')
+  const [isShowingOptions, setIsShowingOptions] = useState(false)
+  const [highlightedOptionId, setHighlightedOptionId] = useState(null)
+  const [announcement, setAnnouncement] = useState(null)
+  const inputRef = useRef(null)
+  const noOptionId = useRef(uniqueId(NO_OPTIONS_OPTION_ID))
+
+  const childProps = useMemo(
+    () =>
+      React.Children.map(children, n => ({
+        id: n.props.id,
+        value: n.props.value,
+        label: n.props.children
+      })),
+    [children]
+  )
+
+  const allChildIds = childProps.map(x => x.id)
+  const [filteredOptionIds, setFilteredOptionIds] = useState(allChildIds)
+
+  function getChildById(id) {
+    return childProps.find(c => c.id === id)
+  }
+
+  function renderChildren() {
+    function renderOption(child) {
+      // eslint-disable-next-line no-shadow
+      const {id, children, ...optionProps} = child.props
+      return (
+        <Select.Option
+          id={id}
+          key={child.key || id || uniqueId('multi-select-')}
+          isHighlighted={id === highlightedOptionId}
+          {...optionProps}
+        >
+          {children}
+        </Select.Option>
+      )
+    }
+
+    function renderNoOptionsOption() {
+      return (
+        <Select.Option id={noOptionId.current} isHighlighted={false} isSelected={false}>
+          {noOptionsLabel}
+        </Select.Option>
+      )
+    }
+
+    const filteredChildren = compact(
+      alwaysArray(children).map(child => {
+        if (
+          matchComponentTypes(child, [CanvasMultiSelectOption]) &&
+          filteredOptionIds.includes(child.props.id) &&
+          !selectedOptionIds.includes(child.props.id)
+        ) {
+          return renderOption(child)
+        }
+        return null
+      })
+    )
+
+    return filteredChildren.length === 0 ? renderNoOptionsOption() : filteredChildren
+  }
+
+  function dismissTag(e, id) {
+    e.stopPropagation()
+    e.preventDefault()
+    onChange(selectedOptionIds.filter(x => x !== id))
+  }
+
+  function renderTags() {
+    const options = alwaysArray(children)
+    if (!options) return null
+
+    return compact(
+      selectedOptionIds.map(id => {
+        const opt = options.find(c => c.props.id === id)
+        if (!opt) return null
+        const tagText = opt.props.children || opt.props.label
+        return (
+          <Tag
+            dismissible
+            key={opt.key}
+            text={tagText}
+            title={I18n.t('Remove %{label}', {label: tagText})}
+            margin="0 xxx-small"
+            onClick={e => dismissTag(e, id)}
+          />
+        )
+      })
+    )
+  }
+
+  function onInputChange(e) {
+    const {value} = e.target
+    const matcher = new RegExp('^' + value.trim(), 'i')
+    const filtered = childProps.filter(x => x.label.match(matcher))
+    let message =
+      // if number of options has changed, announce the new total.
+      filtered.length !== filteredOptionIds.length
+        ? I18n.t(
+            {
+              one: 'One option available.',
+              other: '%{count} options available.'
+            },
+            {count: filtered.length}
+          )
+        : null
+    if (message && filtered.length > 0 && highlightedOptionId !== filtered[0].id) {
+      message = getChildById(filtered[0].id).label + '. ' + message
+    }
+    setInputValue(value)
+    setFilteredOptionIds(filtered.map(f => f.id))
+    if (filtered.length > 0) setHighlightedOptionId(filtered[0].id)
+    setIsShowingOptions(true)
+    setAnnouncement(message)
+  }
+
+  function onRequestShowOptions() {
+    setIsShowingOptions(true)
+  }
+
+  function onRequestHideOptions() {
+    setIsShowingOptions(false)
+    if (!highlightedOptionId) return
+    setInputValue('')
+    if (filteredOptionIds.length === 1) {
+      const option = getChildById(filteredOptionIds[0])
+      setAnnouncement(I18n.t('%{label} selected. List collapsed.', {label: option.label}))
+      onChange([...selectedOptionIds, filteredOptionIds[0]])
+    }
+    setFilteredOptionIds(allChildIds)
+  }
+
+  function onRequestHighlightOption(e, {id}) {
+    e.persist()
+    const option = getChildById(id)
+    if (typeof option === 'undefined') return
+    if (e.type === 'keydown') setInputValue(option.label)
+    setHighlightedOptionId(id)
+    setAnnouncement(option.label)
+  }
+
+  function onRequestSelectOption(_e, {id}) {
+    const option = getChildById(id)
+    setInputValue('')
+    setFilteredOptionIds(allChildIds)
+    setIsShowingOptions(false)
+    if (typeof option === 'undefined') return
+    setAnnouncement(I18n.t('%{label} selected. List collapsed.', {label: option.label}))
+    onChange([...selectedOptionIds, id])
+  }
+
+  // if backspace is pressed and there's no input to backspace over, remove the
+  // last selected option if there is one
+  function onKeyDown(e) {
+    if (
+      e.keyCode === keycode.codes.backspace &&
+      inputValue.length === 0 &&
+      selectedOptionIds.length > 0
+    ) {
+      const option = getChildById(selectedOptionIds.slice(-1)[0])
+      setAnnouncement(I18n.t('%{label} removed.', {label: option.label}))
+      onChange(selectedOptionIds.slice(0, -1))
+    }
+  }
+
+  function onBlur() {
+    setHighlightedOptionId(null)
+  }
+
+  const selectProps = {
+    id: selectId,
+    renderLabel,
+    inputValue,
+    inputRef: ref => {
+      inputRef.current = ref
+    },
+    onInputChange,
+    onRequestShowOptions,
+    onRequestHideOptions,
+    isShowingOptions,
+    onRequestHighlightOption,
+    onRequestSelectOption,
+    onKeyDown,
+    onBlur,
+    assistiveText: I18n.t('Type or use arrow keys to navigate. Multiple selections are allowed.'),
+    renderBeforeInput: selectedOptionIds.length > 0 ? renderTags() : null
+  }
+
+  return (
+    <>
+      <Select {...selectProps} {...otherProps}>
+        {renderChildren()}
+      </Select>
+      {announcement && (
+        <Alert liveRegion={liveRegion} liveRegionPoliteness="assertive" screenReaderOnly>
+          {announcement}
+        </Alert>
+      )}
+    </>
+  )
+}
+
+CanvasMultiSelect.propTypes = {
+  id: string,
+  label: oneOfType([node, func]).isRequired,
+  onChange: func.isRequired,
+  children: node,
+  selectedOptionIds: arrayOf(string),
+  noOptionsLabel: string
+}
+
+CanvasMultiSelect.defaultProps = {
+  noOptionsLabel: '---',
+  selectedOptionIds: []
+}
+
+CanvasMultiSelect.Option = CanvasMultiSelectOption
+
+export default CanvasMultiSelect
