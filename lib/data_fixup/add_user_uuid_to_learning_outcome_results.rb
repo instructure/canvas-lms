df4bf2d94f179ef98c423e21b4e1cd7280395a2e
Frank Murphy <fmurphy@instructure.com>
2020-02-10 18:49:34 -0500

    Add user_uuid to LearningOutcomeResult
    Fixes OUT-3542
    
    Test Plan
    - set up cdc with the outcomes service
    - follow test plan from
    
      https://gerrit.instructure.com/c/outcomes-service/+/225550
    
    - verify the result in the service has the proper user_uuid set.
    
    Change-Id: I0f1594645acd6852863726b604311d3952f17abe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/226177
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Brewer-Davis <mbd@instructure.com>
    Reviewed-by: Pat Renner <prenner@instructure.com>
    QA-Review: Pat Renner <prenner@instructure.com>
    Product-Review: Frank Murphy <fmurphy@instructure.com>

---
+++ lib/data_fixup/add_user_uuid_to_learning_outcome_results.rb
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+module DataFixup::AddUserUuidToLearningOutcomeResults
+  def self.run
+    batch = 1000
+    final = LearningOutcomeResult.maximum(:id) || 0
+    (0..final).step(batch).each do |ix|
+      LearningOutcomeResult.connection.exec_update(
+        update_sql(ix, ix + batch)
+      )
+    end
+  end
+
+  def self.update_sql(start_id, end_id)
+    <<-SQL
+      UPDATE #{LearningOutcomeResult.quoted_table_name} AS lor
+      SET user_uuid = users.uuid
+      FROM (SELECT id, uuid FROM #{User.quoted_table_name}) AS users(id, uuid)
+      WHERE lor.user_id = users.id AND lor.id >= #{start_id} AND lor.id < #{end_id}
+    SQL
+  end
+end
