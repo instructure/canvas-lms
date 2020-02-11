1073d9df925d6102396ef3eadaf89714cd956b10
Cody Cutrer <cody@instructure.com>
2020-02-11 13:00:04 -0700

    fix requiring of FallbackMemoryCache
    always use require_dependency for reloadable classes
    
    Change-Id: I3acc6c3296d79ff15cdb5a5f28b61337b06c3d44
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/226285
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/local_cache.rb
+++ config/initializers/local_cache.rb
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require 'fallback_memory_cache'
+require_dependency 'fallback_memory_cache'
