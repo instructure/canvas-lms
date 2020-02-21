0220b00550192c46deebc5a50840aa3513cb7bc5
Simon Williams <simon@instructure.com>
2020-02-21 14:11:06 -0600

    bump icalendar to 2.6.1
    https://github.com/icalendar/icalendar/blob/master/History.txt
    
    Change-Id: If4795bf639f2de26846756a73e38e4bc1ac8cfc6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/227594
    Reviewed-by: James Williams <jamesw@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -66,7 +66,7 @@ gem 'i18n', '1.0.0'
 gem 'i18nliner', '0.1.1'
   gem 'ruby2ruby', '2.4.4', require: false
   gem 'ruby_parser', '3.14.0', require: false
-gem 'icalendar', '2.5.3', require: false
+gem 'icalendar', '2.6.1', require: false
 gem 'imperium', '0.5.2', require: false
 gem 'ims-lti', '2.3.0', require: 'ims'
 gem 'inst_statsd', '2.1.6'
