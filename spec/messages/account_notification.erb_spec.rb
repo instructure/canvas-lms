24f653a46add56509bdab8883623f21a7b2b6a3f
Ben Nelson <bnelson@instructure.com>
2020-02-21 17:52:41 -0700

    Use custom account name for global announcements
    When sending a notification for a global announcement we should be
    respecting the "Custom 'From' Name" account setting if it is set.
    
    Test Plan:
     - Ensure that the feature flag "Immediate notifications for global
         announcements" is turned on
     - Ensure that you have a user with a confirmed email address who has
         the "Global Announcements" notification preference set to ASAP
     - Navigate to the account settings page
     - Click on the Notifications tab
     - Set a Custom From Name and save
     - Click the Announcements tab
     - Create a new announcement with
     -- Start date in the past
     -- End date in the future
     -- Check the box to send notifications directly to users
     - Check a user's users/<user_id>/messages page for the announcement
     - Verify that the "From Name" field is the custom name you set
    
    fixes KNO-291
    flag=none
    
    Change-Id: I5d48f1568718b548001ca8aef7cde49295089032
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/227654
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Davis Hyer <dhyer@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Ben Nelson <bnelson@instructure.com>

---
+++ spec/messages/account_notification.erb_spec.rb
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/messages_helper')
+
+describe 'account_notification' do
+  before :once do
+    account = Account.create!(:name => "some account", :settings => {:outgoing_email_default_name => "Custom From"})
+    @announcement = account_notification(account: account)
+  end
+
+  let(:notification_name) { :account_notification }
+  let(:asset) { @announcement }
+
+  include_examples "a message"
+
+  context ".email" do
+    let(:path_type) { :email }
+
+    it "should use the custom From: setting" do
+      msg = generate_message(notification_name, path_type, asset)
+      msg.save
+      expect(msg.from_name).to eq "Custom From"
+    end
+  end
+end
