0f7ed7270beb002eb1ea01a2da876485e4fc11c1
Rex Fleischer <rfleischer@instructure.com>
2020-02-24 10:50:35 -0800

    make the docker clean more concise
    fixes: CCI-249
    flag = none
    
    Test-Plan:
    read though the logs for the clean script and ensure
    that it is removing all the stuff except for db and master canvas.
    
    Change-Id: I282a072d95ef19b32be98faee2992ae14a7d9c9d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/227748
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Rex Fleischer <rfleischer@instructure.com>
    Product-Review: Rex Fleischer <rfleischer@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    Reviewed-by: Derek Bender <djbender@instructure.com>

--- build/new-jenkins/docker-cleanup.sh
+++ build/new-jenkins/docker-cleanup.sh
@@ -4,9 +4,11 @@ function show_usage_and_exit {
     echo ">> a tool for cleaning up local docker daemon
 
 usage:
-$(basename "$0") [--allow-failure]
+$(basename "$0") [--allow-failure] [--all] [--whitelist <image> ...]
 where:
-    --allow-failure   allows the docker-compose commands to fail
+    --allow-failure   allows the docker commands to fail
+    --all             clean everything from docker
+    --whitelist       add an image to be whitelisted
     --help            prints this
 "
     exit 1
@@ -14,7 +16,16 @@ where:
 
 set -e
 
+whitelist=()
+whitelist+=('instructure/ruby-passenger')
+whitelist+=('starlord.inscloudgate.net/jenkins/canvas-lms:master')
+whitelist+=('starlord.inscloudgate.net/jenkins/dynamodb-local')
+whitelist+=('starlord.inscloudgate.net/jenkins/redis')
+whitelist+=('starlord.inscloudgate.net/jenkins/cassandra')
+whitelist+=('starlord.inscloudgate.net/jenkins/postgres')
+
 ALLOW_FAILURE="0"
+CLEAN_ALL="0"
 
 while [[ $# -gt 0 ]]
 do
@@ -22,6 +33,15 @@ key="$1"
 case $key in
     --allow-failure)
     ALLOW_FAILURE="1"
+    shift
+    ;;
+    --all)
+    CLEAN_ALL="1"
+    shift
+    ;;
+    --whitelist)
+    whitelist+=("$2")
+    shift
     shift
     ;;
     --help)
@@ -34,49 +54,54 @@ case $key in
 esac
 done
 
-if [[ $ALLOW_FAILURE == "1" ]]; then
-  echo "WARNING: allowing failure during cleanup"
-fi
-
 # this is here so we dont get a bunch of junk to get printed when parsing args
 set -x -o errexit -o errtrace -o nounset -o pipefail
 
+if [[ $ALLOW_FAILURE == "1" ]]; then
+  echo "WARNING: allowing failure during cleanup"
+  set +e
+fi
+
 function docker_env_status {
+  echo "============================================"
   echo "running docker images"
-  docker ps -a || true
+  docker ps -a
   echo "images locally"
-  docker images -a || true
+  docker images -a
   echo "volumes left over"
-  docker volume ls || true
+  docker volume ls
   echo "networks left over"
-  docker network ls || true
+  docker network ls
+  echo "HD space info"
+  docker system df
+  echo "============================================"
 }
 
 echo "== status before clean"
 docker_env_status
 
-# sometimes things don't cleanup so run this a few times
-if [[ $ALLOW_FAILURE == "1" ]]; then
-  docker-compose stop || true
-  docker-compose down --volumes --remove-orphans --rmi all || true
-  docker-compose stop || true
-  docker-compose down --volumes --remove-orphans --rmi all || true
-  docker-compose stop || true
-  docker-compose down --volumes --remove-orphans --rmi all || true
-else
-  docker-compose stop && docker-compose down --volumes --remove-orphans --rmi all
-  docker-compose stop && docker-compose down --volumes --remove-orphans --rmi all
-  docker-compose stop && docker-compose down --volumes --remove-orphans --rmi all
+# kill all running containers
+containers="$(docker ps --quiet)"
+if [[ $containers != "" ]]; then
+  docker kill $containers
 fi
 
-# remove all containers
-docker rm --force --volumes $(docker ps --all --quiet) || true
-
-# delete all images
-docker rmi -f $(docker images --all --quiet) || true
-
-# remove any extra networks (errors saying unable to remove is ok)
-docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }') || true
+if [[ $CLEAN_ALL == "1" ]]; then
+  # delete all the things!
+  docker system prune --all --force --volumes
+else
+  # delete most the things!
+  regex=()
+  regex+=(--regexp="REPOSITORY:TAG")
+  for allowed in "${whitelist[@]}"; do
+    regex+=("--regexp=$allowed")
+  done
+  image_ids=$(docker images --all | awk '{ print $1 ":" $2 " " $3 }' | grep --fixed-strings --invert-match ${regex[@]} | awk '{ print $2 }' || echo '')
+  if [[ $image_ids != "" ]]; then
+    docker rmi --force $image_ids
+  fi
+  docker system prune --force --volumes
+fi
 
 echo "== status after clean"
 docker_env_status
