47f5de97a3ac97853bd93e1c765e4eb0e97ec6f9
James Butters <jbutters@instructure.com>
2020-02-28 06:29:47 -0700

    spec: upgrade chromedriver for chrome version 80 local and docker
    flag = none
    closes: CCI-263
    
    Test Plan:
    Jenkins selenium builds pass.
    
    Change-Id: Ib7970d5b5b9c9a6d07051b1a22758ef055bb1bcc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/228326
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Reviewed-by: Jacob Powell <spowell@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/docker-compose-pull-selenium.sh
+++ build/new-jenkins/docker-compose-pull-selenium.sh
@@ -5,6 +5,6 @@ set -x -o errexit -o errtrace -o nounset -o pipefail
 # pull docker images (or build them if missing)
 REGISTRY_BASE=starlord.inscloudgate.net/jenkins
 
-docker pull $REGISTRY_BASE/selenium-chrome:3.141.59-xenon || \
-  (docker build -t $REGISTRY_BASE/selenium-chrome:3.141.59-xenon docker-compose/selenium-chrome && \
-  docker push $REGISTRY_BASE/selenium-chrome:3.141.59-xenon)
+docker pull $REGISTRY_BASE/selenium-chrome:3.141.59-zirconium || \
+  (docker build -t $REGISTRY_BASE/selenium-chrome:3.141.59-zirconium docker-compose/selenium-chrome && \
+  docker push $REGISTRY_BASE/selenium-chrome:3.141.59-zirconium)
