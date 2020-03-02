f8b9904a60ccd9c21990cc4f3a9ef7b00ea2b33b
Matthew Lemon <mlemon@instructure.com>
2020-03-02 14:49:53 -0700

    allow teachers to send messages in unpublished courses
    We are allowing Teachers, TAs, and Designers to be able to send messages
    in unpublished courses. Additionally we are allowing them to also be
    able to continue a conversation after a course has concluded. They will
    not be able to create new conversations using the concluded course as a
    context but they will be able to continue whatever conversation they
    were already having. Note that students will still not be able to send
    messages or reply to existing conversations after the course has
    concluded.
    
    / ---- ---- \
    | Test Plan |
    \ ---- ---- /
    
    - Create an unpublished course
    - Enroll at least 2 teachers
    - As one of the teachers attempt to send a message to the other
    - Note that the message should send successfully
    - Conclude the course
    - As the teacher note that you are no longer able to create new messages
      using the concluded course as context
    - As the teacher note that you are able to continue existing
      conversations
    
    fixes KNO-290
    flag=none
    
    Change-Id: I8abe105948802c6b6096b652fa7dc058a8c5be77
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/228638
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -387,13 +387,13 @@ class ConversationsController < ApplicationController
         return render_error('context_code', 'invalid')
       end
 
+      if context.is_a?(Course) && context.workflow_state == 'completed' && !context.grants_right?(@current_user, session, :read_as_admin)
+        return render_error('Course concluded', 'Unable to send messages')
+      end
+
       shard = context.shard
       context_type = context.class.name
       context_id = context.id
-    end
-
-    if context.is_a?(Course) && context.workflow_state != 'available'
-      return render_error('Course concluded', 'Unable to send messages')
     end
 
     params[:recipients].each do |recipient|
@@ -881,7 +881,8 @@ class ConversationsController < ApplicationController
   def add_message
     get_conversation(true)
 
-    if @conversation.conversation.context.is_a?(Course) && @conversation.conversation.context.workflow_state == 'completed'
+    context = @conversation.conversation.context
+    if context.is_a?(Course) && context.workflow_state == 'completed' && !context.grants_right?(@current_user, session, :read_as_admin)
       return render json: {message: "Unable to send messages in a concluded course"}, status: :unauthorized
     end
 
