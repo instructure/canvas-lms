e8d2165ee92e34d8e6d3dc43b9a4c5c045330c15
Chris Hart <chart@instructure.com>
2020-03-18 09:43:24 -0400

    Update new user tutorial tray content
    flag=none
    closes UXS-8
    
    Adds new content (copy and links to Canvas Guides), as well as
    updated images for each of the existing new user tutorial trays.
    
    Test Plan:
    Verify that the trays work as follows:
    - All links should correctly resolve to the Canvas Guides page
      they name
    - All trays should feature a Panda image
    - All tray should open and close, and should be able to be
      dismissed permanently (no regressions from existing functionality)
    
    Check the following index pages for trays: Announcements, Assignments,
    Collaborations, Conferences, Discussions, Grades, Course Home Page,
    Course Import page (under Settings), Modules, Pages, People, Quizzes,
    Settings, Syllabus. There is a tray for Files, but it seems to be removed
    from the Files index page on page load (not a result of this commit).
    
    Change-Id: I873fdf9ceb6097d987f3e7da38e5fd615c1d98ea
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230372
    QA-Review: David Tan <dtan@instructure.com>
    Reviewed-by: Pam Hiett <phiett@instructure.com>
    Product-Review: Pam Hiett <phiett@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/jsx/new_user_tutorial/trays/AssignmentsTray.js
+++ app/jsx/new_user_tutorial/trays/AssignmentsTray.js
@@ -18,20 +18,44 @@
 
 import React from 'react'
 import I18n from 'i18n!new_user_tutorial'
-import {Text} from '@instructure/ui-elements'
 import TutorialTrayContent from './TutorialTrayContent'
 
 const AssignmentsTray = () => (
   <TutorialTrayContent
     heading={I18n.t('Assignments')}
-    subheading={I18n.t('Create content for your course')}
-    image="/images/tutorial-tray-images/assignments.svg"
+    subheading={I18n.t('Reinforce student understanding')}
+    image="/images/tutorial-tray-images/Panda_Assignments.svg"
+    imageWidth="11rem"
+    seeAllLink={{
+      label: I18n.t('See more in Canvas Guides'),
+      href: `https://community.canvaslms.com/docs/DOC-10460-canvas-
+      instructor-guide-table-of-contents#jive_content_id_Assignments`
+    }}
+    links={[
+      {
+        label: I18n.t('How do I create an assignment?'),
+        href: 'https://community.canvaslms.com/docs/DOC-9873-415267003'
+      },
+      {
+        label: I18n.t('How do I publish or unpublish an assignment as an instructor?'),
+        href: 'https://community.canvaslms.com/docs/DOC-10101-4152180493'
+      },
+      {
+        label: I18n.t('What assignment types can I create in a course?'),
+        href: 'https://community.canvaslms.com/docs/DOC-10092-415254365'
+      },
+      {
+        label: I18n.t('How do I add or edit details in an assignment?'),
+        href: 'https://community.canvaslms.com/docs/DOC-10113-415241285'
+      }
+    ]}
   >
-    <Text as="p">
-      {I18n.t(`Create assignments on the Assignments page. Organize assignments
-                into groups like Homework, In-class Work, Essays, Discussions
-                and Quizzes. Assignment groups can be weighted.`)}
-    </Text>
+    {I18n.t(`Assignments include quizzes, graded discussions, and many types
+      of online submissions (files, images, text, URLs, and media). Assign
+      them to everyone in a course, or assign different due dates for specific
+      sections or users. Create assignment groups to organize your assignments
+      and to weight groups by percentage. Enable Peer Review so students can
+      review each other's work.`)}
   </TutorialTrayContent>
 )
 
