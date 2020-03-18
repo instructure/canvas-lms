bd932b22467cca1b60e2d911c06b9e6e8a301a9b
James Williams <jamesw@instructure.com>
2020-03-18 11:06:08 -0600

    add index for submissions on cached due date
    test plan:
    * migrations run
    
    closes #LA-839
    
    Change-Id: I363ea70401dce45221a487946fbe1a06b60846e8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230464
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: KC Naegle <knaegle@instructure.com>
    QA-Review: James Williams <jamesw@instructure.com>

---
+++ db/migrate/20200318170259_add_cached_due_date_index_on_submissions.rb
@@ -0,0 +1,8 @@
+class AddCachedDueDateIndexOnSubmissions < ActiveRecord::Migration[5.2]
+  tag :postdeploy
+  disable_ddl_transaction!
+
+  def change
+    add_index :submissions, [:user_id, :cached_due_date], algorithm: :concurrently
+  end
+end
