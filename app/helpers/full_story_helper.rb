81244da0b8ab026b0395a030e9fa400d63b392e8
Ed Schiebel <eschiebel@instructure.com>
2020-03-18 15:39:02 -0400

    Enable FullStory, with throttling
    closes UX-31
    flag=enable_fullstory
    
    test plan:
      1. LOG OUT BETWEEN TESTS
      2. to determine if fullstory was enabled, you can:
         a. use devtools to look for the fullstory script
            in the HEAD. It will be after the
            <!-- fullstory snippet --> comment, making it easy to find
         b. lok for a network request for edge.fullstory.com/s/fs.js
      3. to your dynamic_settings.yml, add
         config:
           canvas:
             fullstory:
               sampling_rate: 1.0
               app_key: 'anything'
    
      - do not enable the flag
      - log in
      > expect fullstory not to be enabled
    
      - enable the flag
      - log in
      > expect fullstory to be enabled
    
      - change sampling_rate to 0.0
      - log in
      > expect fullstory not to be enabled
    
    Change-Id: I27885ebafea3066a8996a45f990118584f2bf56c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230591
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Reviewed-by: Gary Mei <gmei@instructure.com>
    QA-Review: Anju Reddy <areddy@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

---
+++ app/helpers/full_story_helper.rb
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module FullStoryHelper
+  # the feature determines if fullstory is turned on
+  # then we enabled it only for every nth login
+  def fullstory_init(account, session)
+    fullstory_enabled = account.feature_enabled?(:enable_fullstory) rescue false
+    return unless fullstory_enabled
+
+    # this session is already hooked up
+    return if session.key?(:fullstory_enabled)
+
+    fsconfig = Canvas::DynamicSettings.find('fullstory', tree: 'config', service: 'canvas')
+    rate = fsconfig[:sampling_rate].to_f
+    sample = rand()
+    session[:fullstory_enabled] = rate >= 0.0 && rate <= 1.0 && sample < rate
+  end
+
+  def fullstory_app_key
+    Canvas::DynamicSettings.find('fullstory', tree: 'config', service: 'canvas')[:app_key] rescue nil
+  end
+
+  def fullstory_enabled_for_session?(session)
+    !!session[:fullstory_enabled]
+  end
+end
