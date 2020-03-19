a3c2e318d6690096c09f5b8afdd659652764a240
Ethan Vizitei <evizitei@instructure.com>
2020-03-19 10:01:31 -0500

    enable instrumentation of APM for jobs
    closes APM-16, APM-20
    flag = none
    
    also adds context id and user id
    to request annotations for APM
    
    configures host-level sampling
    
    TEST PLAN:
      * enable apm collection on datadog agent on single test cluster
      * push consul config to same cluster for enabling apm sampling
      * push consul config depressing host sampling rate to 5%
      * delayed job telemetry should show up in ddog
      * trace count from active clusters should drop by an order of magnitude
    
    Change-Id: I94d97b299ed14403e8b141629740a1627310b259
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230592
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Reviewed-by: David Warkentin <dwarkentin@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

---
+++ lib/canvas/apm/inst_jobs/plugin.rb
@@ -0,0 +1,77 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+require 'inst-jobs'
+
+module Canvas
+  module Apm
+    module InstJobs
+      # Much of this is taken from ddtrace-rb
+      # lib/ddtrace/contrib/delayed_job/plugin.rb
+      # because inst jobs is sufficiently distinct that just
+      # using that instrumentation as-is failed because it has version
+      # requirements unmet by the way we load inst-jobs.
+      # As this grows we might look at patching their
+      # delayed_job integration to be ok with the shape of
+      # inst-jobs by writing our own version of
+      # lib/ddtrace/contrib/delayed_job/integration.rb that
+      # recognizes inst-jobs as valid and just uses the plugin
+      # defined by ddtrace originally for delayed_job
+      class Plugin < ::Delayed::Plugin
+        class << self
+          attr_writer :tracer
+
+          def reset!
+            @tracer = nil
+          end
+
+          def instrument(job)
+            return yield job unless tracer&.enabled
+            job_name = job.name
+            tracer.trace("inst_jobs", service: "canvas_jobs", resource: job_name) do |span|
+              span.set_tag("inst_jobs.id", job.id)
+              span.set_tag("inst_jobs.queue", job.queue) if job.queue
+              span.set_tag("inst_jobs.priority", job.priority)
+              span.set_tag("inst_jobs.attempts", job.attempts)
+              span.set_tag("inst_jobs.strand", job.strand) if job.strand.present?
+              span.set_tag('shard', job.shard_id.to_s) if job.shard_id.present?
+              act_global_id = job.account_id
+              span.set_tag('root_account', act_global_id.to_s) if act_global_id.present?
+              span.span_type = "worker"
+              yield job
+            end
+          end
+
+          def flush(worker)
+            yield worker
+            tracer.shutdown! if tracer&.enabled
+          end
+
+          def tracer
+            return @tracer if @tracer.present?
+            Datadog.tracer
+          end
+        end
+
+        callbacks do |lifecycle|
+          lifecycle.around(:invoke_job, &method(:instrument))
+          lifecycle.around(:execute, &method(:flush))
+        end
+      end
+    end
+  end
+end
