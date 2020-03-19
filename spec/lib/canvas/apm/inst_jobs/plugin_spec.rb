192544a46089c7bc56830c0833bc66e10ededd14
Ethan Vizitei <evizitei@instructure.com>
2020-03-19 10:01:31 -0500

    enable instrumentation of APM for jobs
    closes APM-16, APM-20
    flag = none
    
    also adds context id and user id
    to request annotations for APM
    
    configures host-level sampling
    
    TEST PLAN:
      * enable apm collection on datadog agent on single test cluster
      * push consul config to same cluster for enabling apm sampling
      * push consul config depressing host sampling rate to 5%
      * delayed job telemetry should show up in ddog
      * trace count from active clusters should drop by an order of magnitude
    
    Change-Id: I94d97b299ed14403e8b141629740a1627310b259
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230592
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    Reviewed-by: David Warkentin <dwarkentin@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

---
+++ spec/lib/canvas/apm/inst_jobs/plugin_spec.rb
@@ -0,0 +1,98 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+require 'spec_helper'
+require 'delayed/testing'
+require_dependency "canvas/apm/inst_jobs/plugin"
+
+describe Canvas::Apm::InstJobs::Plugin do
+  class FakeSpan
+    attr_reader :tags
+    attr_accessor :resource, :span_type
+    def initialize
+      self.reset!
+    end
+
+    def reset!
+      @tags = {}
+    end
+
+    def set_tag(key, val)
+      @tags[key] = val
+    end
+  end
+
+  class FakeTracer
+    attr_reader :span
+    def initialize
+      @span = FakeSpan.new
+    end
+
+    def trace(_name, opts = {})
+      span.resource = opts.fetch(:resource, nil)
+      yield span
+    end
+
+    def enabled
+      true
+    end
+  end
+
+  let(:tracer){ FakeTracer.new }
+  let(:span){ tracer.span }
+
+
+  around do |example|
+    Canvas::Apm::InstJobs::Plugin.tracer = tracer
+    Delayed::Worker.plugins << Canvas::Apm::InstJobs::Plugin
+    example.run
+    Delayed::Worker.plugins.delete(Canvas::Apm::InstJobs::Plugin)
+    Delayed::Worker.lifecycle.reset!
+    span.reset!
+    Canvas::Apm::InstJobs::Plugin.reset!
+  end
+
+  describe 'instrumenting worker execution' do
+    let(:worker) { double(:worker, name: 'worker') }
+
+    it 'execution callback yields control' do
+      expect { |b| Delayed::Worker.lifecycle.run_callbacks(:execute, worker, &b) }.to yield_with_args(worker)
+    end
+  end
+
+  describe 'instrumented job invocation' do
+    let(:sample_job_object) do
+      stub_const('SampleJob', Class.new do
+        def perform; end
+      end)
+    end
+
+    it 'has resource name equal to job name' do
+      expect(Canvas::Apm::InstJobs::Plugin.tracer).to eq(tracer)
+      job = Delayed::Job.enqueue(sample_job_object.new, {})
+      Delayed::Testing.run_job(job)
+      expect(span.resource).to eq('SampleJob')
+      expect(span.tags["inst_jobs.id"] > 0).to be_truthy
+      expect(span.tags["inst_jobs.queue"]).to eq("canvas_queue")
+      expect(span.tags["inst_jobs.priority"] > 0).to be_truthy
+      expect(span.tags["inst_jobs.attempts"]).to eq(0)
+      expect(span.tags["inst_jobs.strand"]).to be_nil
+      expect(span.tags["shard"]).to eq(job.shard_id)
+      expect(span.tags["root_account"]).to eq(job.account_id)
+    end
+  end
+end
