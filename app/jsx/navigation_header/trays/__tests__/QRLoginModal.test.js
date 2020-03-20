8cb1a615ecec6fdafb0d13f77971243299bdf66d
Charley Kline <ckline@instructure.com>
2020-03-20 13:12:30 -0500

    Make mobile QR codes regenerate themselves in the modal
    Closes USERS-439
    flag=mobile_qr_login
    
    A potential sad user path through the mobile QR code flow
    would be if the code on the screen expired before they had
    a chance to scan it. So let's make it automatically refresh
    before it expires, and also give the user some indication
    that they are life-limited (in case they try to print their
    screen or something dumb like that).
    
    Nothing was changed here in the QR code generation or
    display itself, so it shouldn't be necessary to heavily QA
    the functionality of the displayed codes themselves.
    
    Test plan:
    * Be sure you're set up to generate mobile login QR codes
      (see test plan for g/230318)
    * Bring up a mobile QR code
    * See that the modal appears with a code, and says it
      expires in 10 minutes
    * Wait one minute (-ish)
    * Now it should say it expires in 9 minutes
    * Keep waiting or go do something else for 7-8 minutes
    * Eventually the message will say "expires in a few seconds"
    * Not long after that (9 minutes 45 seconds after the code
      was generated to be precise) the QR code should change to
      a new one, and the message should reset to say that the
      new code once again expires in 10 minutes
    * You can verify that a new code was fetched by looking in
      the network inspector (or if you have eagle eyes by seeing
      the changes in the QR data bits themselves 😊)
    
    Change-Id: I40ce1ab1f5e210dea12d4158bb8d11fa20fa4f42
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/230956
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Reviewed-by: Jon Willesen <jonw+gerrit@instructure.com>
    QA-Review: Keith Garner <kgarner@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Hilary Scharton <hilary@instructure.com>

--- app/jsx/navigation_header/trays/__tests__/QRLoginModal.test.js
+++ app/jsx/navigation_header/trays/__tests__/QRLoginModal.test.js
@@ -17,15 +17,23 @@
 
 import React from 'react'
 import fetchMock from 'fetch-mock'
+import MockDate from 'mockdate'
+import moment from 'moment'
 import {showQRLoginModal, QRLoginModal, killQRLoginModal} from '../QRLoginModal'
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, act} from '@testing-library/react'
 import {getByText as domGetByText} from '@testing-library/dom'
 
-const loginImageJson = {
-  png: 'R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
-}
+// a fake QR code image, and then a another one after generating a new code
+const loginImageJsons = [
+  {png: 'R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='},
+  {png: 'R0lGODlhAQABZZZZZCH5BAEKAAEALZZZZZABAAEAAAICTAEAOn=='}
+]
 
+const route = '/canvas/login.png'
 const QRModalStub = () => <span>hi there</span>
+
+// used for when we want fetchMock not to ever respond
+const doNotRespond = Function.prototype
 
 describe('Navigation Header > Trays', () => {
   describe('showQRLoginModal function', () => {
@@ -52,41 +60,112 @@ describe('Navigation Header > Trays', () => {
   })
 
   describe('QRLoginModal component', () => {
-    const handleDismiss = jest.fn()
+    describe('before the API call responds', () => {
+      const handleDismiss = jest.fn()
 
-    beforeEach(handleDismiss.mockClear)
+      beforeEach(() => {
+        fetchMock.post(route, doNotRespond, {overwriteRoutes: true})
+      })
 
-    afterEach(fetchMock.restore)
+      afterEach(() => {
+        handleDismiss.mockClear()
+        fetchMock.restore()
+      })
 
-    it('renders the dialog', () => {
-      const {getByText} = render(<QRLoginModal onDismiss={handleDismiss} />)
-      expect(getByText(/Scan this QR code/)).toBeInTheDocument()
+      it('renders the modal', () => {
+        const {getByText} = render(<QRLoginModal onDismiss={handleDismiss} />)
+        expect(getByText(/Scan this QR code/)).toBeInTheDocument()
+      })
+
+      it('renders a spinner', () => {
+        const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
+        expect(getByTestId('qr-code-spinner')).toBeInTheDocument()
+      })
+
+      it('dismisses the modal when "Done" button is clicked', () => {
+        const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
+        const closeButton = getByTestId('qr-close-button')
+        fireEvent.click(closeButton)
+        expect(handleDismiss).toHaveBeenCalled()
+      })
+
+      it('dismisses the modal when the modal dismiss X is clicked', () => {
+        const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
+        const closeButton = getByTestId('instui-modal-close').querySelector('button')
+        fireEvent.click(closeButton)
+        expect(handleDismiss).toHaveBeenCalled()
+      })
     })
 
-    it('renders a spinner before the API call has completed', () => {
-      const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
-      expect(getByTestId('qr-code-spinner')).toBeInTheDocument()
-    })
+    describe('after the API call responds', () => {
+      beforeEach(() => {
+        jest.useFakeTimers()
+        fetchMock
+          .postOnce(route, loginImageJsons[0], {overwriteRoutes: true})
+          .postOnce(route, loginImageJsons[1], {overwriteRoutes: false})
+      })
 
-    it('renders the image returned by the API call', async () => {
-      fetchMock.post('/canvas/login.png', loginImageJson)
-      const {findByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
-      const image = await findByTestId('qr-code-image')
-      expect(image.src).toBe(`data:image/png;base64, ${loginImageJson.png}`)
-    })
+      afterEach(() => {
+        fetchMock.restore()
+        MockDate.reset()
+      })
 
-    it('kills the modal off when "Done" button is clicked', () => {
-      const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
-      const closeButton = getByTestId('qr-close-button')
-      fireEvent.click(closeButton)
-      expect(handleDismiss).toHaveBeenCalled()
-    })
+      // advances both global time and the jest timers by the given time duration
+      function advance(...args) {
+        const delay = moment.duration(...args).asMilliseconds()
+        act(() => {
+          const now = Date.now()
+          MockDate.set(now + delay)
+          jest.advanceTimersByTime(delay)
+        })
+      }
 
-    it('kills the modal off when the modal dismiss X is clicked', () => {
-      const {getByTestId} = render(<QRLoginModal onDismiss={handleDismiss} />)
-      const closeButton = getByTestId('instui-modal-close').querySelector('button')
-      fireEvent.click(closeButton)
-      expect(handleDismiss).toHaveBeenCalled()
+      it('renders the image in the response, and the right expiration time', async () => {
+        const {findByTestId, getByText} = render(<QRLoginModal onDismiss={Function.prototype} />)
+        const image = await findByTestId('qr-code-image')
+        expect(image.src).toBe(`data:image/png;base64, ${loginImageJsons[0].png}`)
+        expect(getByText(/expires in 10 minutes/i)).toBeInTheDocument()
+      })
+
+      it('updates the expiration as time elapses', async () => {
+        const {findByText} = render(<QRLoginModal onDismiss={Function.prototype} />)
+        await findByText(/expires in 10 minutes/i)
+        advance(1, 'minute')
+        const expiresIn = await findByText(/expires in 9 minutes/i)
+        expect(expiresIn).toBeInTheDocument()
+      })
+
+      it('shows the right thing when the token has expired', async () => {
+        const refreshInterval = moment.duration(15, 'minutes')
+        const pollInterval = moment.duration(3, 'minutes')
+        const {findByText} = render(
+          <QRLoginModal
+            onDismiss={Function.prototype}
+            refreshInterval={refreshInterval}
+            pollInterval={pollInterval}
+          />
+        )
+        await findByText(/expires in 10 minutes/)
+        advance(11, 'minutes') // code is only good for 10
+        const expiresIn = await findByText(/code has expired/i)
+        expect(expiresIn).toBeInTheDocument()
+      })
+
+      it('refreshes the code at the right time', async () => {
+        const refreshInterval = moment.duration(2, 'minutes')
+        const {findByText, findByTestId} = render(
+          <QRLoginModal onDismiss={Function.prototype} refreshInterval={refreshInterval} />
+        )
+        const image = await findByTestId('qr-code-image')
+        expect(image.src).toBe(`data:image/png;base64, ${loginImageJsons[0].png}`)
+        expect(fetchMock.calls(route)).toHaveLength(1)
+        advance(1, 'minute')
+        await findByText(/expires in 9 minutes/)
+        advance(1, 'minute')
+        await findByText(/expires in 10 minutes/)
+        expect(fetchMock.calls(route)).toHaveLength(2)
+        expect(image.src).toBe(`data:image/png;base64, ${loginImageJsons[1].png}`)
+      })
     })
   })
 })
