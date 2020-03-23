4d35f6ca8e293a10dcc0f41a36b69ca380d3f0d0
Ethan Vizitei <evizitei@instructure.com>
2020-03-23 11:58:22 -0500

    spec: use sharding for DJ apm spec
    FIXES APM-25
    flag = none
    
    also properly implments plugin
    reset so that we can enable/disable
    without rebooting the process
    
    test plan:
      specs pass flaky spec catcher
    Change-Id: I96e8137e351639559d68c8f1e9242e1c315cb26d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/231006
    Reviewed-by: David Warkentin <dwarkentin@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- spec/lib/canvas/apm_spec.rb
+++ spec/lib/canvas/apm_spec.rb
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require 'spec_helper'
+require_relative '../../sharding_spec_helper'
 
 describe Canvas::Apm do
   after(:each) do
