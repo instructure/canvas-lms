f4edd33b299b46e16d054143c62fa12ed232f120
Aaron Ogata <aogata@instructure.com>
2020-03-26 11:54:11 -0700

    update iframe allowances for FF 74+, refs ARC-6511
    FF 74 has enabled Feature-Policy by default, so for this
    and all future versions, we need to specify * as the
    allowed-origin
    
    Test Plan:
    1. Using FF 74, navigate to the Studio LTI Tool
    2. Click on the "Record" menu in the top-right
    3. Click "Webcam Capture"
    4. It should prompt the user to accept webcam / microphone
    permissions
    
    flag = none
    
    Change-Id: If7464ed8fbc60d7ad02b417d3756fdf8bc90a6ad
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/231717
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    QA-Review: Alex Slaughter <aslaughter@instructure.com>

--- app/models/lti/launch.rb
+++ app/models/lti/launch.rb
@@ -25,7 +25,9 @@ module Lti
     attr_accessor :link_text, :resource_url, :params, :launch_type, :tool_dimensions, :base_string
 
     def self.iframe_allowances(user_agent = nil)
-      if user_agent.blank? || Browser.new(user_agent).chrome?
+      browser = Browser.new(user_agent)
+
+      if user_agent.blank? || browser.chrome? || browser.firefox?(">= 74")
         return FRAME_ALLOWANCES.map { |s| "#{s} *" }
       end
       FRAME_ALLOWANCES
