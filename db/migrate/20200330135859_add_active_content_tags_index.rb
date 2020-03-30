94f740b10e2733686d6d514c304d3a2e6d194492
James Williams <jamesw@instructure.com>
2020-03-30 09:01:37 -0600

    add indexes to content tags and enrollments
    Change-Id: Id3edd501c7f71dfd7b8240279451b700c095f8cc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/232096
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: James Williams <jamesw@instructure.com>

---
+++ db/migrate/20200330135859_add_active_content_tags_index.rb
@@ -0,0 +1,9 @@
+class AddActiveContentTagsIndex < ActiveRecord::Migration[5.2]
+  tag :postdeploy
+  disable_ddl_transaction!
+
+  def change
+    add_index :content_tags, [:context_id, :context_type, :content_type], where: "workflow_state = 'active'",
+      name: "index_content_tags_on_context_when_active", algorithm: :concurrently
+  end
+end
