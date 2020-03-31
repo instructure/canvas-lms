3f152b2cdcd6c6d7e36a72f008ecedec69a19f0d
johnvbk <31219912+johnvbk@users.noreply.github.com>
2020-03-31 14:54:42 -0700

    No summaries (#316)
    * update shim
    
    * no more Summaries
    
    * fix comment

--- lib/canvas/message_helper.rb
+++ lib/canvas/message_helper.rb
@@ -69,6 +69,10 @@ module Canvas::MessageHelper
     values = args.extract_options!
     using = { :delay_for => 0 }.with_indifferent_access.merge(values)
     using[:category] ||= args[1] # type
+
+    #bypass broken summaries and save 15% for spending 15 minutes 
+    return if using[:category] == 'Summaries'
+
     using[:delay_for] ||= args[2] # delay
     # 'txt' is the legacy message body. Pull name from first line.
     if args[4].present?
