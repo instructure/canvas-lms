66db44b7c20d966c5e110bde58b809a5fdd18bf9
Cody Cutrer <cody@instructure.com>
2020-03-31 06:48:41 -0600

    bump switchman
    Change-Id: I47d3d40ad4fb0eb069afacf7031f7c4bc9b56a35
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/232244
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -118,7 +118,7 @@ gem 'sanitize', '2.1.1', require: false
 gem 'sentry-raven', '2.13.0', require: false
 gem 'shackles', '1.4.2'
 gem 'simple_oauth', '0.3.1', require: false
-gem 'switchman', '1.14.9'
+gem 'switchman', '1.14.10'
   gem 'open4', '1.3.4', require: false
 gem 'switchman-inst-jobs', '1.3.7'
 gem 'twilio-ruby', '5.31.1', require: false
