6893896ddf960ff37d08a2357934c2fa2f0c0d00
Ed Schiebel <eschiebel@instructure.com>
2020-03-31 15:34:23 -0400

    Refactor UploadButton
    This change refactors UploadButton into UploadForm, which is
    just the hidden form + processing the files selected by the
    user and UploadButton which is the visible button the user clicks
    plus UploadForm.  UploadButton provides the functionality it
    always has, while UploadForm can be used where we don't want
    the button.
    
    refs UXS-54
    flag=none
    
    no flag because this is a refactor of existing code. If it's
    determined we need to flag this, I can make that happen, but it
    will likely mean having all the original and new files in place
    and switch between the two. As it currently is though, there
    will be a better git history.
    
    test plan:
      - the Upload button on the files page should work
      - you should be able to upload multiple files
      - if you upload the same file(s) twice, you should be
        prompted whether you should replace or rename
      - if you upload a zip, you should be prompted
        to upload or expand
    
    Change-Id: Iaee03da4f7e7da0e0284724ab33ce209c5dd9bef
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/232360
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>
    Reviewed-by: Mark McDermott <mmcdermott@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- app/jsx/files/UploadButton.js
+++ app/jsx/files/UploadButton.js
@@ -17,57 +17,43 @@
  */
 
 import I18n from 'i18n!upload_button'
-import React from 'react'
-import createReactClass from 'create-react-class'
-import FileRenameForm from './FileRenameForm'
-import ZipFileOptionsForm from './ZipFileOptionsForm'
-import UploadButton from 'compiled/react_files/components/UploadButton'
+import React, {useRef} from 'react'
+import {bool} from 'prop-types'
+import classnames from 'classnames'
+import UploadForm, {UploadFormPropTypes} from './UploadForm'
 
-UploadButton.buildPotentialModal = function() {
-  if (this.state.zipOptions.length) {
-    return (
-      <ZipFileOptionsForm
-        fileOptions={this.state.zipOptions[0]}
-        onZipOptionsResolved={this.onZipOptionsResolved}
-        onClose={this.onClose}
-      />
-    )
-  } else if (this.state.nameCollisions.length) {
-    return (
-      <FileRenameForm
-        fileOptions={this.state.nameCollisions[0]}
-        onNameConflictResolved={this.onNameConflictResolved}
-        onClose={this.onClose}
-        allowSkip={window?.ENV?.FEATURES?.files_dnd}
-      />
-    )
-  }
-}
-
-UploadButton.hiddenPhoneClassname = function() {
-  if (this.props.showingButtons) {
-    return 'hidden-phone'
-  }
-}
-
-UploadButton.render = function() {
+const UploadButton = function(props) {
+  const formRef = useRef(null)
   return (
-    <span>
-      <form ref="form" className="hidden">
-        <input type="file" ref="addFileInput" onChange={this.handleFilesInputChange} multiple />
-      </form>
+    <>
+      <UploadForm
+        ref={formRef}
+        currentFolder={props.currentFolder}
+        contextId={props.contextId}
+        contextType={props.contextType}
+      />
       <button
         type="button"
         className="btn btn-primary btn-upload"
-        onClick={this.handleAddFilesClick}
+        onClick={() => formRef.current.addFiles()}
       >
         <i className="icon-upload" aria-hidden />
         &nbsp;
-        <span className={this.hiddenPhoneClassname()}>{I18n.t('Upload')}</span>
+        <span className={classnames({'hidden-phone': props.showingButtons})}>
+          {I18n.t('Upload')}
+        </span>
       </button>
-      {this.buildPotentialModal()}
-    </span>
+    </>
   )
 }
 
-export default createReactClass(UploadButton)
+UploadButton.propTypes = {
+  ...UploadFormPropTypes,
+  showingButtons: bool
+}
+
+UploadButton.defaultProps = {
+  showingButtons: false
+}
+
+export default UploadButton
