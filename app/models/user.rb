ebc59ffb719c20cd326ee62c5ce5f54b57764ac0
James Williams <jamesw@instructure.com>
2020-04-02 13:10:41 -0600

    move courses_with_primary_enrollment to slave
    closes #LA-885
    
    Change-Id: I99880ba1b19aa3769dae3031e8c6ae1e89c01bb6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/232678
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    (cherry picked from commit ac409e0df1354f47fe4a10df3b4c0173346b7470)
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/232935
    Product-Review: Keith Garner <kgarner@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1702,9 +1702,11 @@ class User < ActiveRecord::Base
               where("enrollment_states.state IN ('active', 'invited', 'pending_invited', 'pending_active')")
           end
 
-          scope.select("courses.*, enrollments.id AS primary_enrollment_id, enrollments.type AS primary_enrollment_type, enrollments.role_id AS primary_enrollment_role_id, #{Enrollment.type_rank_sql} AS primary_enrollment_rank, enrollments.workflow_state AS primary_enrollment_state, enrollments.created_at AS primary_enrollment_date").
-              order(Arel.sql("courses.id, #{Enrollment.type_rank_sql}, #{Enrollment.state_rank_sql}")).
-              distinct_on(:id).shard(shards).to_a
+          Shackles.activate(:slave) do
+            scope.select("courses.*, enrollments.id AS primary_enrollment_id, enrollments.type AS primary_enrollment_type, enrollments.role_id AS primary_enrollment_role_id, #{Enrollment.type_rank_sql} AS primary_enrollment_rank, enrollments.workflow_state AS primary_enrollment_state, enrollments.created_at AS primary_enrollment_date").
+                order(Arel.sql("courses.id, #{Enrollment.type_rank_sql}, #{Enrollment.state_rank_sql}")).
+                distinct_on(:id).shard(shards).to_a
+          end
         end
         result.dup
       end
