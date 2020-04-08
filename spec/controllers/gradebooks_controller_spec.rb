4b4affc6a16d9f826a5873e866b241e8ca93a9a9
Jeremy Neander <jneander@instructure.com>
2020-04-08 11:05:09 -0500

    remove outcome proficiency from gradebook env
    `outcome_proficiency` is used only by Default Learning Mastery and by
    Grade Summary, both of which have their own ENV assignments. Default
    Gradebook and Individual Gradebook have no need of it. Individual
    Learning Mastery loads outcome information via the API. Because this
    view is the overlap of the existing Gradebook ENV and Learning Mastery,
    it deserves a quick smoke test.
    
    refs TALLY-756
    flag = none
    
    test plan:
     * Smoke test individual learning mastery
    
    Change-Id: I35a23ffecc1f5507f3446515b3d3ed002a9061a0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/233370
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Spencer Olson <solson@instructure.com>

--- spec/controllers/gradebooks_controller_spec.rb
+++ spec/controllers/gradebooks_controller_spec.rb
@@ -1061,16 +1061,9 @@ describe GradebooksController do
     end
 
     context 'includes data needed by the Gradebook Action menu in ENV' do
-      let(:create_proficiency) { false }
-      let(:enable_non_scoring_rubrics) { false }
-
       before do
         user_session(@teacher)
-        @proficiency = outcome_proficiency_model(@course.account) if create_proficiency
-        @course.root_account.enable_feature! :non_scoring_rubrics if enable_non_scoring_rubrics
-
         get 'show', params: {course_id: @course.id}
-
         @gradebook_env = assigns[:js_env][:GRADEBOOK_OPTIONS]
       end
 
@@ -1090,41 +1083,6 @@ describe GradebooksController do
 
       it "includes the context_modules_url in the ENV" do
         expect(@gradebook_env[:context_modules_url]).to eq(api_v1_course_context_modules_url(@course))
-      end
-
-      shared_examples_for 'returns no outcome proficiency' do
-        it 'returns nil for outcome proficiency' do
-          expect(@gradebook_env[:outcome_proficiency]).to be_nil
-        end
-      end
-
-      context 'non-scoring rubrics feature flag disabled' do
-        context 'no outcome proficiency on account' do
-          include_examples 'returns no outcome proficiency'
-        end
-
-        skip 'outcome proficiency on account' do
-          skip('NSRs are not being disabled properly even with the disable_feature! method')
-          let(:create_proficiency) { true }
-
-          include_examples 'returns no outcome proficiency'
-        end
-      end
-
-      context 'non-scoring rubrics feature flag enabled' do
-        let(:enable_non_scoring_rubrics) { true }
-
-        context 'no outcome proficiency on account' do
-          include_examples 'returns no outcome proficiency'
-        end
-
-        context 'outcome proficiency on account' do
-          let(:create_proficiency) { true }
-
-          it 'returns an outcome proficiency' do
-            expect(@gradebook_env[:outcome_proficiency]).to eq(@proficiency.as_json)
-          end
-        end
       end
     end
 
