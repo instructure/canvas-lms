50ec05ef345f8d21b6a6f67e8675d4c205339b17
Cody Cutrer <cody@instructure.com>
2020-04-09 08:48:20 -0600

    bump switchman-inst-jobs
    Change-Id: I5be35eac4049ea84e426e638a469b08fa5cd046f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/233488
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -120,7 +120,7 @@ gem 'shackles', '1.4.2'
 gem 'simple_oauth', '0.3.1', require: false
 gem 'switchman', '1.14.10'
   gem 'open4', '1.3.4', require: false
-gem 'switchman-inst-jobs', '1.3.7'
+gem 'switchman-inst-jobs', '1.4.1'
 gem 'twilio-ruby', '5.31.1', require: false
 gem 'tzinfo', '1.2.5'
 gem 'vault', '0.13.0', require: false
