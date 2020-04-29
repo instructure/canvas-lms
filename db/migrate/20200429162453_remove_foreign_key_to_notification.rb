fcd3ba0d832a8e3af7e1121f9a051491dfbdaaa3
Rob Orton <rob@instructure.com>
2020-04-29 10:33:18 -0600

    remove FK to notification
    these objects don't live in the same schema
    
    test plan
     - migrations should run
    
    fixes KNO-438
    
    Change-Id: I47a4811616cfef078cfa84da8988ae5a10e8254b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/235653
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

---
+++ db/migrate/20200429162453_remove_foreign_key_to_notification.rb
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify
+# the terms of the GNU Affero General Public License as publishe
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but
+# WARRANTY; without even the implied warranty of MERCHANTABILITY
+# A PARTICULAR PURPOSE. See the GNU Affero General Public Licens
+# details.
+#
+# You should have received a copy of the GNU Affero General Publ
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+class RemoveForeignKeyToNotification < ActiveRecord::Migration[5.2]
+  tag :predeploy
+
+  def up
+    remove_foreign_key_if_exists :notification_policy_override, column: :notification_id
+  end
+end
