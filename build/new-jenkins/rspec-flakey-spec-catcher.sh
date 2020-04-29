0bae9c44dffec007f2bd86cdc19b73c8f3843d6c
Rex Fleischer <rfleischer@instructure.com>
2020-04-29 11:19:53 -0700

    add force failure for fsc
    [skip-docker-build]
    
    fixes: CCI-350
    flag = none
    
    Test-Plan:
    add force failure to commit message and ensure fsc
    actually gets forced to fail.
    
    Change-Id: I3825dd389c9643ed8abb911cb0cd04b7101c6f9e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/235673
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Powell <spowell@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Rex Fleischer <rfleischer@instructure.com>
    Product-Review: Rex Fleischer <rfleischer@instructure.com>

--- build/new-jenkins/rspec-flakey-spec-catcher.sh
+++ build/new-jenkins/rspec-flakey-spec-catcher.sh
@@ -2,8 +2,16 @@
 
 set -o errexit -o errtrace -o nounset -o pipefail -o xtrace
 
-docker-compose exec -T -e FSC_IGNORE_FILES -e FSC_NODE_TOTAL -e FSC_NODE_INDEX web \
-    bundle exec flakey_spec_catcher \
-    --repeat=$FSC_REPEAT_FACTOR \
-    --output=/usr/src/app/tmp/fsc.out \
-    --use-parent
+
+if [ "${FORCE_FAILURE:-}" == '1' ]; then
+  docker-compose exec -T -e FORCE_FAILURE=1 web bundle exec flakey_spec_catcher \
+      --repeat=$FSC_REPEAT_FACTOR \
+      --output=/usr/src/app/tmp/fsc.out \
+      --test=spec/force_failure_spec.rb
+else
+  docker-compose exec -T -e FSC_IGNORE_FILES -e FSC_NODE_TOTAL -e FSC_NODE_INDEX web \
+      bundle exec flakey_spec_catcher \
+      --repeat=$FSC_REPEAT_FACTOR \
+      --output=/usr/src/app/tmp/fsc.out \
+      --use-parent
+fi
