18862fdf0ef94b973d48e4d62beef622da794ffc
James Butters <jbutters@instructure.com>
2020-05-15 09:58:11 -0600

    spec: revert pact scripts in bin/ to use web
    pact scripts in bin/ are for local runs of pact and need to
    use web container, new files that are in build/new-jenkins
    don't need the check if Jenkins.
    
    flag = none
    
    Change-Id: I878b157b9856f1c25e160d7608b226f7185974a3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/237520
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Rex Fleischer <rfleischer@instructure.com>
    Reviewed-by: Derek Bender <djbender@instructure.com>
    Reviewed-by: Jacob Powell <spowell@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/pact/contracts-verify-live-events.sh
+++ build/new-jenkins/pact/contracts-verify-live-events.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -o errexit -o errtrace -o pipefail -o xtrace
 
-if [[ -v JENKINS ]]; then # canvas is running in CI
-  docker-compose run --rm --no-deps \
-    -e RAILS_ENV=test \
-    -e PACT_BROKER_HOST=inst-pact-broker.inseng.net \
-    -e PACT_BROKER_USERNAME="${PACT_BROKER_USERNAME}" \
-    -e PACT_BROKER_PASSWORD="${PACT_BROKER_PASSWORD}" \
-    canvas bundle exec rspec spec/contracts/service_consumers/live_events \
-      --tag pact_live_events --format doc
-fi
+docker-compose run --rm --no-deps \
+  -e RAILS_ENV=test \
+  -e PACT_BROKER_HOST=inst-pact-broker.inseng.net \
+  -e PACT_BROKER_USERNAME="${PACT_BROKER_USERNAME}" \
+  -e PACT_BROKER_PASSWORD="${PACT_BROKER_PASSWORD}" \
+  canvas bundle exec rspec spec/contracts/service_consumers/live_events \
+    --tag pact_live_events --format doc
