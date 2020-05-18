6237a9426f21e4fe0483dddb9b82f8239eebc6ba
Matthew Lemon <mlemon@instructure.com>
2020-05-18 17:10:36 -0600

    only return active communication channels from gql
    If a user deleted a communication channel we would still display it on
    the course notification preferences page because we weren't filtering
    the channels based on them being active.
    
    fixes KNO-496
    flag=notification_granular_course_preferences
    
    / ---- ---- \
    | Test Plan |
    \ ---- ---- /
    
    - Create a course and add a user to it
    - Add a communication channel to the user and confirm it
    - Navigate to the course notification preferences page
    - Note that the new communication channel should show up
    - Delete the notification channel from the user
    - Navigate back to the course notification preferences page
    - Note that the channel no longer shows up
    
    Change-Id: I5fd88bb0e7e4fcb40652f3a41391ff249971a5e6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/237822
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- spec/graphql/types/course_type_spec.rb
+++ spec/graphql/types/course_type_spec.rb
@@ -510,12 +510,28 @@ describe Types::CourseType do
   describe 'notificationPreferences' do
     it 'returns the students notification preferences' do
       Notification.delete_all
-      communication_channel = @student.communication_channels.create!(path: 'test@test.com')
-      notification = notification_model(:name => 'test', :category => 'Announcement')
+      @student.communication_channels.create!(path: 'test@test.com').confirm!
+      notification_model(:name => 'test', :category => 'Announcement')
 
       expect(
         course_type.resolve('notificationPreferences { channels { notificationPolicies(contextType: Course) { notification { name } } } }')[0][0]
       ).to eq 'test'
+    end
+
+    it 'only returns active communication channels' do
+      Notification.delete_all
+      communication_channel = @student.communication_channels.create!(path: 'test@test.com')
+      communication_channel.confirm!
+      notification_model(:name => 'test', :category => 'Announcement')
+
+      expect(
+        course_type.resolve('notificationPreferences { channels { notificationPolicies(contextType: Course) { notification { name } } } }')[0][0]
+      ).to eq 'test'
+
+      communication_channel.destroy
+      expect(
+        course_type.resolve('notificationPreferences { channels { notificationPolicies(contextType: Course) { notification { name } } } }').count
+      ).to eq 0
     end
   end
 end
