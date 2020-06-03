e60716629c2c77a3ea7e914dbdd32c9dfec6c7c0
Ethan Vizitei <evizitei@instructure.com>
2020-06-03 10:48:50 -0500

    index foreign keys for test student reset
    Change-Id: Ib0454b8ea2fec96e822d12b1535efdb83e9f3ddd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/239131
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

---
+++ db/migrate/20200603153351_cascade_fk_idxs_to_auditor_courses.rb
@@ -0,0 +1,19 @@
+class CascadeFkIdxsToAuditorCourses < CanvasPartman::Migration
+  disable_ddl_transaction!
+  tag :postdeploy
+  self.base_class = Auditors::ActiveRecord::CourseRecord
+
+  def up
+    with_each_partition do |partition|
+      add_index partition, :sis_batch_id, algorithm: :concurrently
+      add_index partition, :user_id, algorithm: :concurrently
+    end
+  end
+
+  def down
+    with_each_partition do |partition|
+      remove_index partition, :sis_batch_id, algorithm: :concurrently
+      remove_index partition, :user_id, algorithm: :concurrently
+    end
+  end
+end
