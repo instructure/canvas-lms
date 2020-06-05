99c5a44fec5b0ea66109fa2b56c3d0fdb55e570f
James Williams <jamesw@instructure.com>
2020-06-05 09:10:05 -0600

    drop conditional release template models
    despite all initial appearances it turns out these are
    functionally unused and probably won't change any time soon
    
    test plan:
    * specs
    
    closes #LA-1108
    
    Change-Id: Ia5411d355659471465971e3f154292230841a709
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/239380
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

---
+++ db/migrate/20200605144535_drop_conditional_release_templates.rb
@@ -0,0 +1,16 @@
+class DropConditionalReleaseTemplates < ActiveRecord::Migration[5.2]
+  tag :predeploy
+
+  def up
+    if table_exists?(:conditional_release_scoring_range_templates)
+      drop_table :conditional_release_scoring_range_templates
+    end
+    if table_exists?(:conditional_release_rule_templates)
+      drop_table :conditional_release_rule_templates
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
