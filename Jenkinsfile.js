16c8df703a23d155abced39fc3e4e196694b2448
Aaron Ogata <aogata@instructure.com>
2020-06-26 11:19:48 -0700

    only pull necessary docker images for JS build
    flag = none
    
    Change-Id: I65b9ae8e72884681fff32b926b380417ee1eac60
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/241280
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Norton <rnorton@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -43,7 +43,7 @@ pipeline {
         cleanAndSetup()
         timeout(time: 10) {
           sh 'rm -vrf ./tmp/*'
-          sh 'build/new-jenkins/docker-compose-pull.sh'
+          sh 'docker pull $PATCHSET_TAG'
           sh 'docker-compose build'
         }
       }
