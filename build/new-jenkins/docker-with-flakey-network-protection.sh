62d60d5d653774e671554c2cc670c8ed96be8b93
Aaron Ogata <aogata@instructure.com>
2020-07-02 09:27:02 -0700

    add another case for docker retry
    refs DE-25
    
    Test Plan:
    It’s hard to get Jenkins to intentionally fail with this error, but to test the retry logic we can use the following alternative:
    1. Modify the shell script to look for “requested access to the resource is denied” or some easily reproducible error.
    2. Run “./docker-with-flakey-network-protection.sh pull invalid_image”
    3. Verify that the retry logic works correctly
    4. Verify that the happy path works correctly
    
    flag = none
    
    Change-Id: Icb0119894864ec5f58e4666c2f67dd908b67fdac
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/241788
    Reviewed-by: James Butters <jbutters@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- build/new-jenkins/docker-with-flakey-network-protection.sh
+++ build/new-jenkins/docker-with-flakey-network-protection.sh
@@ -5,11 +5,7 @@ set -o errexit -o errtrace -o nounset -o pipefail -o xtrace
 EXIT_CODE=0
 PULL_RESULT=$(docker $1 $2 2>&1) || EXIT_CODE=$?
 
-if echo $PULL_RESULT | grep -q "net/http: TLS handshake timeout"; then
-  sleep 10
-
-  PULL_RESULT=$(docker $1 $2 2>&1) || EXIT_CODE=$?
-elif echo $PULL_RESULT | grep -q "unknown blob"; then
+if [[ $PULL_RESULT =~ (TLS handshake timeout|unknown blob|i/o timeout) ]]; then
   sleep 10
 
   PULL_RESULT=$(docker $1 $2 2>&1) || EXIT_CODE=$?
