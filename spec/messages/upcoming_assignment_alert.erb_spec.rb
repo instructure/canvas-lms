0882d8fc3b754f7ca13cbad2f9b93174b20929d7
Ben Nelson <bnelson@instructure.com>
2020-07-07 14:20:40 -0600

    create upcoming_assignment_alert message template
    Test Plan:
     - Trigger an upcoming_assingment_alert notification
        (see test plan on g/240397)
     - Verify that the message templates are rendered properly
    
    closes VICE-465
    flag=smart_alerts
    
    Change-Id: I61f66ee74e87acaeb7b7d67b724f6599f90b42df
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/242126
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Matthew Lemon <mlemon@instructure.com>

---
+++ spec/messages/upcoming_assignment_alert.erb_spec.rb
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/messages_helper')
+
+describe 'upcoming_assignment_alert' do
+  before :once do
+    assignment_model
+  end
+
+  let(:asset) { @assignment }
+  let(:notification_name) { :upcoming_assignment_alert }
+
+  include_examples "a message"
+end
