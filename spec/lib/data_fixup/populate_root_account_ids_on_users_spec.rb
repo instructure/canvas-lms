0c454429ea2d6fcf83bb443246468a934079c758
wdransfield <wdransfield@instructure.com>
2020-07-07 16:01:57 -0600

    backfill root account ids on users
    closes INTEROP-5923
    flag = none
    
    Test Plan:
    Testing this PS will require MRA
    
    - Create a user in shard 1
    - Associate the user with a root account on
      shard 1
    - Associate that user with an account on shard 2
    - run the populate_root_account_id_on_models
      fixup
    - Check to make sure the user record has two
      root account IDs- one from shard 1, the
      other from shard 2
    
    Change-Id: I1f65c2873341adcabcd3f6641fee6d602f132914
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/242139
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>

---
+++ spec/lib/data_fixup/populate_root_account_ids_on_users_spec.rb
@@ -0,0 +1,76 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+
+describe DataFixup::PopulateRootAccountIdsOnUsers do
+  it 'populates from user account associations' do
+    user = User.create!
+    a1 = Account.create!
+    a2 = Account.create!
+    p1 = a1.pseudonyms.create!(user: user, unique_id: 'user')
+    p2 = a2.pseudonyms.create!(user: user, unique_id: 'user')
+    expect(p1.root_account_id).to eq a1.id
+    expect(p2.root_account_id).to eq a2.id
+    expect(user.reload.root_account_ids).to eq(nil)
+    DataFixup::PopulateRootAccountIdsOnUsers.populate(user.id, user.id)
+    expect(user.reload.root_account_ids).to eq([a1.id, a2.id])
+  end
+
+  it 'appends to an existing array' do
+    user = User.create!
+    a1 = Account.create!
+    a2 = Account.create!
+    p1 = a1.pseudonyms.create!(user: user, unique_id: 'user')
+    expect(p1.root_account_id).to eq a1.id
+    expect(user.reload.root_account_ids).to eq(nil)
+
+    DataFixup::PopulateRootAccountIdsOnUsers.populate(user.id, user.id)
+    expect(user.reload.root_account_ids).to eq([a1.id])
+
+    p2 = a2.pseudonyms.create!(user: user, unique_id: 'user')
+    expect(p2.root_account_id).to eq a2.id
+    DataFixup::PopulateRootAccountIdsOnUsers.populate(user.id, user.id)
+    expect(user.reload.root_account_ids).to eq([a1.id, a2.id])
+  end
+
+  context "sharding" do
+    specs_require_sharding
+
+    it 'populates root account ids from a different shard' do
+      user1 = User.create!
+      a1 =nil
+      user2 = @shard2.activate { User.create! }
+      @shard1.activate do
+        a1 = Account.create!
+        # users from 2 different foreign shards are associated with this shard
+        # this ensures we exercise the part where each foreign shard is addressed
+        # separately
+        p1 = a1.pseudonyms.create!(user: user1, unique_id: 'user1')
+        p2 = a1.pseudonyms.create!(user: user2, unique_id: 'user2')
+        expect(p1.root_account_id).to eq a1.id
+        expect(p2.root_account_id).to eq a1.id
+        expect(user1.reload.root_account_ids).to eq(nil)
+        expect(user2.reload.root_account_ids).to eq(nil)
+
+        DataFixup::PopulateRootAccountIdsOnUsers.populate_table
+      end
+      expect(user1.reload.root_account_ids).to eq([a1.global_id])
+      expect(user2.reload.root_account_ids).to eq([a1.global_id])
+    end
+  end
+end
