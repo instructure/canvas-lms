5c797e468613cbecd781efd514068ec7b9adcfce
Aaron Ogata <aogata@instructure.com>
2020-07-08 13:35:44 -0700

    exit with success code if retry passes
    flag = none
    
    Change-Id: I73d14d942427189e7bd9ab0318da9a0f56232ee1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/242193
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Reviewed-by: Ryan Norton <rnorton@instructure.com>

--- build/new-jenkins/docker-with-flakey-network-protection.sh
+++ build/new-jenkins/docker-with-flakey-network-protection.sh
@@ -8,6 +8,7 @@ PULL_RESULT=$(docker $1 $2 2>&1) || EXIT_CODE=$?
 if [[ $PULL_RESULT =~ (TLS handshake timeout|unknown blob|i/o timeout|Internal Server Error) ]]; then
   sleep 10
 
+  EXIT_CODE=0
   PULL_RESULT=$(docker $1 $2 2>&1) || EXIT_CODE=$?
 fi
 
