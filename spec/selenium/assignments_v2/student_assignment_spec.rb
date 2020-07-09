7dd5a009522277bd1a72f2a55c8f2bafc5728652
Jackson Howe <jackson.howe@instructure.com>
2020-07-09 14:20:27 -0600

    Add more descriptive SR label to module nav buttons
    The Previous and Next buttons at the bottom of a module item now
    have an aria-label giving more context about the button's purpose
    to SR users.
    
    Fixes LS-1189
    flag = none
    
    Test plan:
     - Go to any module item
     - Using a SR, read the next and prev buttons at the bottom of
       page. Should say 'Previous Module Item' and 'Next Module
       Item,' respectively
     - Buttons should still have tooltip for both sighted and SR users
    
    Change-Id: I007e1fc95a0fa06b39841f2d54c1a95501af7e97
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/242310
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

--- spec/selenium/assignments_v2/student_assignment_spec.rb
+++ spec/selenium/assignments_v2/student_assignment_spec.rb
@@ -153,7 +153,7 @@ describe 'as a student' do
       end
 
       it "shows the module sequence footer" do
-        expect(f('.module-sequence-footer-button--previous')).to have_attribute("href", "/courses/#{@course.id}/modules/items/#{@item_before.id}")
+        expect(f('.module-sequence-footer-button--previous a')).to have_attribute("href", "/courses/#{@course.id}/modules/items/#{@item_before.id}")
         expect(f('.module-sequence-footer-button--next a')).to have_attribute("href", "/courses/#{@course.id}/modules/items/#{@item_after.id}")
       end
     end
