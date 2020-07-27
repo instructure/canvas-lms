c2d4376cbc82efe1ae80401da6ce7e07c6a4e787
James Butters <jbutters@instructure.com>
2020-07-27 09:52:09 -0600

    FSC actually run for plugin changes
    currently plugin changes with 'require spec_helper' will fail
    to run due to spec_canvas not being in the load_path for rspec.
    Add spec_canvas to the load_path to allow these spec files
    to run on FSC. Also adding BUNDLE_GEMFILE=../../../Gemfile
    since we are trying to run these spec from their plugin directory
    we need to specify where the gems exist to actually run.
    
    flag = none
    closes: DE-152
    
    Test Plan:
    -Jenkins builds pass
    -post some examples of plugin FSC builds with this change
    
    Change-Id: I6f5063d20412450cff52de59840586e99264b390
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/243459
    QA-Review: James Butters <jbutters@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/rspec-flakey-spec-catcher.sh
+++ build/new-jenkins/rspec-flakey-spec-catcher.sh
@@ -7,7 +7,17 @@ if [ "${FORCE_FAILURE:-}" == '1' ]; then
       --repeat=$FSC_REPEAT_FACTOR \
       --output=/usr/src/app/tmp/fsc.out \
       --test=spec/force_failure_spec.rb
+elif [ "${IS_PLUGIN}" == "1" ]; then
+   docker-compose --project-name canvas-lms0 exec -T -e FSC_IGNORE_FILES -e FSC_NODE_TOTAL -e FSC_NODE_INDEX canvas \
+         bash -c "cd $DOCKER_WORKDIR && BUNDLE_GEMFILE=../../../Gemfile bundle exec flakey_spec_catcher \
+         --repeat=$FSC_REPEAT_FACTOR \
+         --output=/usr/src/app/tmp/fsc.out \
+         --use-parent \
+         --rspec-options '-I spec_canvas'"
 else
   docker-compose --project-name canvas-lms0 exec -T -e FSC_IGNORE_FILES -e FSC_NODE_TOTAL -e FSC_NODE_INDEX canvas \
-      bash -c "cd $DOCKER_WORKDIR && flakey_spec_catcher --repeat=$FSC_REPEAT_FACTOR --output=/usr/src/app/tmp/fsc.out --use-parent"      
+      bash -c "cd $DOCKER_WORKDIR && bundle exec flakey_spec_catcher \
+      --repeat=$FSC_REPEAT_FACTOR \
+      --output=/usr/src/app/tmp/fsc.out \
+      --use-parent"
 fi
