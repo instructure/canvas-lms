ec73c84f11b0a462f675e582eb52c9cba5470597
Caleb Guanzon <cguanzon@instructure.com>
2020-08-05 14:56:51 -0600

    spec: add /profile/communication new ui
    test plan:
    - specs pass
    
    Change-Id: I9f96e6a2c4cc28daf5c3dc183c95f71b307750ed
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/244390
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ben Nelson <bnelson@instructure.com>
    QA-Review: Ben Nelson <bnelson@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>

---
+++ spec/selenium/profile_communications_new_spec.rb
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2012 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+require File.expand_path(File.dirname(__FILE__) + '/common')
+
+describe "profile communication settings new ui" do
+  include_context "in-process server selenium tests"
+
+  before :each do
+    Account.site_admin.enable_feature!(:notification_update_account_ui)
+
+    Notification.create(:name => "Conversation Message", :category => "DiscussionEntry")
+    Notification.create(:name => "Added To Conversation", :category => "Discussion")
+    Notification.create(:name => "GradingStuff1", :category => "Grading")
+    @sub_comment = Notification.create(:name => "Submission Comment1", :category => "Submission Comment")
+
+    course_with_teacher_logged_in
+  end
+
+  it "should render" do
+    get "/profile/communication"
+    expect(f("h1").text).to eq "Account Notification Settings"
+    expect(fj("div:contains('Account-level notifications apply to all courses.')")).to be
+  end
+end
