ebcb12c309c998315089ac4c6b0a1421a20a5779
Caleb Guanzon <cguanzon@instructure.com>
2020-08-07 10:50:07 -0600

    spec: finish new /profile/communication tests
    test plan:
    - passes jenkins
    
    Change-Id: If2ad2de7068593bc814b1e960492d3873dbed071
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/244522
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- spec/selenium/profile_communications_new_spec.rb
+++ spec/selenium/profile_communications_new_spec.rb
@@ -20,34 +20,59 @@ require File.expand_path(File.dirname(__FILE__) + '/common')
 describe "profile communication settings new ui" do
   include_context "in-process server selenium tests"
 
-  before :each do
+  before :once do
     Account.site_admin.enable_feature!(:notification_update_account_ui)
-
     Notification.create(:name => "Conversation Message", :category => "DiscussionEntry")
     Notification.create(:name => "Added To Conversation", :category => "Discussion")
     Notification.create(:name => "GradingStuff1", :category => "Grading")
     @sub_comment = Notification.create(:name => "Submission Comment1", :category => "Submission Comment")
+  end
+  
+  context "as teacher" do
+    before :each do
+      course_with_teacher_logged_in
+    end
+    
+    it "should render" do
+      get "/profile/communication"
+      expect(f('#breadcrumbs')).to include_text('Account Notification Settings')
+      expect(f("h1").text).to eq "Account Notification Settings"
+      expect(fj("div:contains('Account-level notifications apply to all courses.')")).to be
+    end
 
-    course_with_teacher_logged_in
+    it "should display the users email address as channel" do
+      get "/profile/communication"
+      expect(fj("th[scope='col'] span:contains('email')")).to be
+      expect(fj("th[scope='col'] span:contains('nobody@example.com')")).to be
+    end
+
+    it "should display an SMS number as channel" do
+      communication_channel(@user, {username: '8011235555@vtext.com', path_type: 'sms', active_cc: true})
+      get "/profile/communication"
+      expect(fj("span:contains('sms')")).to be
+      expect(fxpath("//span[contains(text(),'8011235555@vtext')]")).to be
+    end
+
+    it "should save a user-pref checkbox change" do
+      Account.default.settings[:allow_sending_scores_in_emails] = true
+      Account.default.save!
+      # set the user's initial user preference and verify checked or unchecked
+      @user.preferences[:send_scores_in_emails] = false
+      @user.save!
+
+      get "/profile/communication"
+      f("tr[data-testid='grading'] label").click
+      wait_for_ajaximations
+      # test data stored
+      @user.reload
+      expect(@user.preferences[:send_scores_in_emails]).to eq true
+    end
   end
 
-  it "should render" do
-    get "/profile/communication"
-    expect(f('#breadcrumbs')).to include_text('Account Notification Settings')
-    expect(f("h1").text).to eq "Account Notification Settings"
-    expect(fj("div:contains('Account-level notifications apply to all courses.')")).to be
-  end
-
-  it "should display the users email address as channel" do
+  it "should render for a user with no enrollments" do
+    user_logged_in(:username => 'somebody@example.com')
     get "/profile/communication"
     expect(fj("th[scope='col'] span:contains('email')")).to be
-    expect(fj("th[scope='col'] span:contains('nobody@example.com')")).to be
-  end
-
-  it "should display an SMS number as channel" do
-    communication_channel(@user, {username: '8011235555@vtext.com', path_type: 'sms', active_cc: true})
-    get "/profile/communication"
-    expect(fj("span:contains('sms')")).to be
-    expect(fxpath("//span[contains(text(),'8011235555@vtext')]")).to be
+    expect(fj("th[scope='col'] span:contains('somebody@example.com')")).to be
   end
 end
