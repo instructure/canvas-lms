cc2174c56a86b50e11a7eaff9030904009db33c6
Gary Mei <gmei@instructure.com>
2020-08-11 13:39:35 -0500

    fix grade imports with spaces in grades
    fixes EVAL-1035
    flag=none
    
    Test Plan
    - Export the gradebook.
    - In the csv, assign 1 grade to have a leading space, 1 to have a
    trailing space, and 1 with neither.
    - Import the gradebook.
    - Verify that the changes are present without issue.
    
    Change-Id: I62a6696bd6dbbb2b08e1ff4e3133c1b4522e2c2f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/244781
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Syed Hussain <shussain@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- spec/lib/gradebook_importer_spec.rb
+++ spec/lib/gradebook_importer_spec.rb
@@ -419,6 +419,19 @@ describe GradebookImporter do
     end
   end
 
+  it "strips leading and trailing spaces from grades" do
+    rows = [
+      "Student;ID;Section;Aufgabe 1;Aufgabe 2;Final Score",
+      "Points Possible;;;10000,54;100,00;",
+      "'Merkel, Angela';1;Mein Kurs; 123,4;57,4%;",
+      "'Einstein, Albert';2;Mein Kurs;1234,5 ;4.200,3%;"
+    ]
+
+    importer = importer_with_rows(*rows)
+    grades = importer.upload.gradebook.fetch("students").map { |s| s.fetch("submissions").first.fetch("grade") }
+    expect(grades).to match_array ["123,4", "1234,5"]
+  end
+
   it "parses new and existing assignments" do
     course_model
     @assignment1 = @course.assignments.create!(:name => 'Assignment 1')
