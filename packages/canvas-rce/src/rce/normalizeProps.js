37fbec32ecd811efa5bf423e91783d2533451190
Jackson Howe <jackson.howe@instructure.com>
2020-08-14 14:56:47 -0600

    Hide canvas media option from menubar when setting is enabled
    LS-1219 hid media options from toolbar, this commit also
    hides media options from menubar when the setting is enabled.
    
    Fixes LS-1325
    flag=rce_enhancements
    
    Test plan:
     - Go to an RCE, make sure the record/upload media options are
       available in toolbar and in menubar (under insert) and work
       as expected
     - In account settings, enable the 'disable uploads to canvas
       media' setting
     - Back in RCE, expect that those two buttons in toolbar and
       menubar are gone
     - Open tray by clicking documents or images button - should still
       be able to see any previously recorded media under user
       media files
    
    Change-Id: I8557bf5cbb5cc26a547be85893add7970c699002
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/245137
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Reviewed-by: Alex Anderson <raanderson@instructure.com>
    Product-Review: Peyton Craighill <pcraighill@instructure.com>

--- packages/canvas-rce/src/rce/normalizeProps.js
+++ packages/canvas-rce/src/rce/normalizeProps.js
@@ -74,14 +74,22 @@ export default function(props, tinymce, MutationObserver) {
 }
 
 function configureMenus(editorOptions) {
+  const insertMenuItems = [
+    ['instructure_links', 'instructure_image', 'instructure_document'],
+    ['instructure_equation', 'inserttable'],
+    ['hr']
+  ]
+  if (!ENV.RICH_CONTENT_INST_RECORD_TAB_DISABLED) {
+    insertMenuItems[0].splice(2, 0, 'instructure_media')
+  }
+
   editorOptions.menubar = 'edit insert format tools table'
   editorOptions.menu = {
     // default menu options listed at https://www.tiny.cloud/docs/configure/editor-appearance/#menu
     // default edit menu is fine
     insert: {
       title: formatMessage('Insert'),
-      items:
-        'instructure_links instructure_image instructure_media instructure_document | instructure_equation inserttable | hr'
+      items: insertMenuItems.map(item => item.join(' ')).join(' | ')
     },
     format: {
       title: formatMessage('Format'),
