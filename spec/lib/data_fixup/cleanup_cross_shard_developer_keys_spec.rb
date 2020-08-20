c5a457943fb6da7dd795f14425dfd1acfa61e51e
wdransfield <wdransfield@instructure.com>
2020-08-20 16:18:38 -0600

    spec: add specs to ensure dev keys are not deleted in cleanup
    refs INTEROP-6080
    flag=none
    
    Test Plan:
    specs pass
    
    Change-Id: Ic274d602a38a2842acdc04aafca58c5e9e69e977
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/245649
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

--- spec/lib/data_fixup/cleanup_cross_shard_developer_keys_spec.rb
+++ spec/lib/data_fixup/cleanup_cross_shard_developer_keys_spec.rb
@@ -66,4 +66,51 @@ describe DataFixup::CleanupCrossShardDeveloperKeys do
     expect{cet.reload}.to raise_error(ActiveRecord::RecordNotFound)
     expect{cetp.reload}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  context 'with site admin keys' do
+    let(:user) { @shard1.activate { user_model } }
+
+    let(:site_admin_key) do
+      Account.site_admin.shard.activate do
+        DeveloperKey.create!
+      end
+    end
+
+    let(:site_admin_access_token) do
+      user.shard.activate do
+        AccessToken.create!(developer_key: site_admin_key, user: user)
+      end
+    end
+
+    before do
+      site_admin_access_token.shard.activate { DataFixup::CleanupCrossShardDeveloperKeys.run }
+      site_admin_key.shard.activate { DataFixup::CleanupCrossShardDeveloperKeys.run }
+    end
+
+    it 'does not delete site admin keys' do
+      expect(site_admin_key.reload).to be_active
+    end
+
+    it 'does not delete site admin access tokens' do
+      expect(site_admin_access_token.reload).to be_active
+    end
+  end
+
+  context 'with local keys' do
+    let!(:local_access_token) { AccessToken.create!(developer_key: local_key, user: user) }
+    let!(:local_key) { DeveloperKey.create!(account: root_account) }
+
+    let(:root_account) { Account.root_accounts.first }
+    let(:user) { user_model }
+
+    before { DataFixup::CleanupCrossShardDeveloperKeys.run }
+
+    it 'does not delete local keys' do
+      expect(local_key.reload).to be_active
+    end
+
+    it 'does not delete local access tokens' do
+      expect(local_access_token.reload).to be_active
+    end
+  end
 end
