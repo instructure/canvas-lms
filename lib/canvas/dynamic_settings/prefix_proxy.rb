69f57a553c718fa62a8ed3f905850e8deda7c06c
Ethan Vizitei <evizitei@instructure.com>
2020-09-17 14:24:21 -0500

    consul logging and http error handling
    refs FOO-965
    
    raise a real error if we get a response
    other than a 200 or 404, only 404 is a good
    reason for putting a nil value into the cache.
    
    TEST PLAN:
      1) override imperium to return error responses
      2) run dynamic settings lookup
      3) nil is not in cache, error reported
    
    Change-Id: Iba384c122e674864fbcaf49c3159ab8aca3f0833
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/247948
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- lib/canvas/dynamic_settings/prefix_proxy.rb
+++ lib/canvas/dynamic_settings/prefix_proxy.rb
@@ -15,6 +15,7 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 module Canvas
   class DynamicSettings
+    class UnexpectedConsulResponse < StandardError; end
     # A class for reading values from Consul
     #
     # @attr prefix [String] The prefix to be prepended to keys for querying.
@@ -23,6 +24,7 @@ module Canvas
       # The TTL for cached values if none is specified in the constructor
 
       attr_reader :prefix, :tree, :service, :environment, :cluster
+      attr_accessor :query_logging
 
       # Build a new prefix proxy
       #
@@ -35,6 +37,8 @@ module Canvas
       #   values when not specified to the fetch methods.
       # @param kv_client [Imperium::KV] The client to use for connecting to
       #   Consul, defaults to Imperium::KV.default_client
+      # @param data_center [String] Which regional datacenter to address for queries
+      # @param query_logging [Boolean] when enabled (true), will output query logs and timing for each request
       def initialize(prefix = nil,
                      tree: :config,
                      service: :canvas,
@@ -42,7 +46,8 @@ module Canvas
                      cluster: nil,
                      default_ttl: DEFAULT_TTL,
                      kv_client: Imperium::KV.default_client,
-                     data_center: nil)
+                     data_center: nil,
+                     query_logging: true)
         @prefix = prefix
         @tree = tree
         @service = service
@@ -51,6 +56,7 @@ module Canvas
         @default_ttl = default_ttl
         @kv_client = kv_client
         @data_center = data_center
+        @query_logging = query_logging
       end
 
       # Fetch the value at the requested key using the prefix passed to the
@@ -87,10 +93,19 @@ module Canvas
         # okay now pre-cache an entire tree
         tree_key = [tree, service, environment].compact.join("/")
         LocalCache.fetch(CACHE_KEY_PREFIX + tree_key + '/', expires_in: ttl) do
-          result = @kv_client.get(tree_key, :recurse, :stale)
+          result = kv_fetch(tree_key, :recurse, :stale)
+          # result should always be a KVGetResponse, nil is a problem
+          # because it SHOULD be a 404 with a nil
           if result&.status == 200
             populate_cache(tree_key, result.values, ttl) # only populate recursively when we missed
             true # we don't actually need to save the values in the cache anymore if we're not using them
+          elsif result&.status == 404
+            # this just means the value doesn't exist in the consul store
+            nil
+          else
+            # we don't want to store "nil" in the cache if something bad happened
+            # when talking to consul
+            raise(UnexpectedConsulResponse, "Unexpected consul result: #{result}-#{result&.status}")
           end
         end
 
@@ -103,14 +118,21 @@ module Canvas
 
         fallback_keys.each do |full_key|
           result = LocalCache.fetch(CACHE_KEY_PREFIX + full_key, expires_in: ttl) do
-            result = @kv_client.get(full_key, :stale)
+            result = kv_fetch(full_key, :stale)
             result.values if result&.status == 200
+            if result&.status == 200 || result&.status == 404
+              result.values
+            else
+              # we don't want to store "nil" in the cache if something bad happened
+              # when talking to consul
+              raise(UnexpectedConsulResponse, "Unexpected consul result: #{result}-#{result&.status}")
+            end
           end
           return result if result
         end
         Rails.logger.warn("[DYNAMIC_SETTINGS] config requested which was found no-where (#{key})")
         nil
-      rescue Imperium::TimeoutError => exception
+      rescue Imperium::TimeoutError, UnexpectedConsulResponse => exception
         LocalCache.fetch_without_expiration(CACHE_KEY_PREFIX + keys.first).tap do |val|
           if val
             Canvas::Errors.capture_exception(:consul, exception)
@@ -156,6 +178,19 @@ module Canvas
       end
 
       private
+      # bit of helper indirection
+      # so that we can log actual
+      # QUERIES (vs things fetched from the cache)
+      # in one place, and process error states as actual
+      # errors
+      def kv_fetch(full_key, *options)
+        result = nil
+        ms = 1000 * Benchmark.realtime do
+          result = @kv_client.get(full_key, *options)
+        end
+        Rails.logger.debug("  #{"CONSUL (%.2fms)" % [ms]} get (#{full_key}) -> status:#{result&.status}") if @query_logging
+        result
+      end
 
       # Returns the full key
       #
