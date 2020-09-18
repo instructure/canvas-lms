35d6eb7f3db52ed09316df43ca8fe1df44ea447f
Cody Cutrer <cody@instructure.com>
2020-09-18 14:00:01 -0600

    bump nokogiri-xmlsec for xcode 12
    Change-Id: I3be11f3f251f1b48608afea0875e3fccd8ee5b02
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248026
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Davis Hyer <dhyer@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -112,7 +112,7 @@ gem 'rubycas-client', '2.3.9', require: false
 gem 'rubyzip', '2.2.0', require: 'zip'
 gem 'safe_yaml', '1.0.5', require: false
 gem 'saml2', '3.0.9'
-  gem 'nokogiri-xmlsec-instructure', '0.9.6', require: false
+  gem 'nokogiri-xmlsec-instructure', '0.9.7', require: false
 gem 'sanitize', '2.1.1', require: false
 gem 'sentry-raven', '2.13.0', require: false
 gem 'shackles', '1.4.2'
