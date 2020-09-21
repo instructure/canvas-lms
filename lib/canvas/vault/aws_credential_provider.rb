97c91a2f3102bada74b1021da9b7e3c196d45418
Ethan Vizitei <evizitei@instructure.com>
2020-09-21 12:08:48 -0500

    proper error on vault read failure
    Change-Id: I84c02bf8b3f6a239b2efc83cc34adc23721c7576
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248086
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- lib/canvas/vault/aws_credential_provider.rb
+++ lib/canvas/vault/aws_credential_provider.rb
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 module Canvas::Vault
+  class VaultConfigError < StandardError; end
   class AwsCredentialProvider
     include ::Aws::CredentialProvider
 
@@ -29,6 +30,7 @@ module Canvas::Vault
     # out of the local redis cache.
     def credentials
       cred_hash = ::Canvas::Vault.read(@_path)
+      raise(VaultConfigError, "failed to read config for #{@_path}") if cred_hash.blank?
       ::Aws::Credentials.new(cred_hash[:access_key], cred_hash[:secret_key], cred_hash[:security_token])
     end
   end
