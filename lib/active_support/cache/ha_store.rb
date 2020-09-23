c4083f2c98eac7171b995913d315ef842819d3a9
Cody Cutrer <cody@instructure.com>
2020-09-23 13:21:30 -0600

    add a periodic job that detects if HACache invalidations are working
    and report to statsd (datadog)
    
    Change-Id: Ie34dd8bbcd948c68087d54626c20dbf8d5503102
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248332
    QA-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/active_support/cache/ha_store.rb
+++ lib/active_support/cache/ha_store.rb
@@ -41,11 +41,29 @@ class ActiveSupport::Cache::HaStore < ActiveSupport::Cache::RedisCacheStore
     end
   end
 
+  def validate_consul_event
+    key = SecureRandom.uuid
+    patience = Setting.get("ha_store_validate_consul_event_patience", 15).to_f
+    write(key, 1, expires_in: patience * 2)
+    delete(key, skip_local: true)
+    # yes, really, a sleep. we need to run on the same node because we only wrote
+    # to this node. which is why we skip_local'ed above, to ensure the delete
+    # actually went through the consul event stream
+    sleep patience
+    if read(key).nil?
+      InstStatsd::Statsd.gauge("ha_store_event_validate_consul_event", 1)
+      true
+    else
+      InstStatsd::Statsd.gauge("ha_store_event_validate_consul_event", 0)
+      false
+    end
+  end
+
   protected
 
   def delete_entry(key, options)
     # do it locally
-    result = super
+    result = super unless options[:skip_local]
     # then if so configured, trigger consul
     if options[:consul_event]
       datacenters = Array.wrap(options[:consul_datacenters]).presence || [nil]
--- lib/multi_cache.rb
+++ lib/multi_cache.rb
@@ -18,7 +18,7 @@
 
 class MultiCache
   class << self
-    delegate :fetch, :delete, to: :cache
+    delegate :fetch, :delete, :validate_consul_event, to: :cache
 
     def cache
       unless defined?(@multi_cache)
