7100c4d2aa82a06ab7d8ac8641aa9659f61db3bf
Ethan Vizitei <evizitei@instructure.com>
2020-09-24 16:41:39 -0500

    move AUA log compaction state to local table
    closes FOO-1003
    
    TEST PLAN:
      1) aua log compaction continues to work
      2) state on plugin setting and in metadata
         table is in sync
    
    Change-Id: Ie161fcb079f80a4072ddd99f3a561c97d33026fd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248501
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

---
+++ spec/models/canvas_metadatum_spec.rb
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'spec_helper'
+
+describe CanvasMetadatum do
+
+  describe "getting" do
+
+    it 'should get the default value as a hash' do
+      expect(CanvasMetadatum.get('some_key', {state: 23})[:state]).to eq 23
+    end
+
+    it 'will not accept other forms of argument' do
+      expect{ CanvasMetadatum.get('some_key', 'some value') }.to raise_error(CanvasMetadatum::MetadataArgumentError)
+    end
+
+    it 'should return set values' do
+      CanvasMetadatum.set('some_key', {int_val: 23, string_val: "asdf", array_val: [2,4,8,16], hash_val: {nested: "string_value"}})
+      payload = CanvasMetadatum.get('some_key')
+      expect(payload[:int_val]).to eq(23)
+      expect(payload[:string_val]).to eq("asdf")
+      expect(payload[:array_val]).to eq([2,4,8,16])
+      expect(payload[:hash_val][:nested]).to eq("string_value")
+    end
+  end
+
+end
