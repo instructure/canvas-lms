985b6a23eb9efcf7cb4ead43807c2c876159a1d7
James Butters <jbutters@instructure.com>
2020-09-28 17:39:55 -0600

    improve docker-dev-changes.sh
    the current grep is including files we don't actually want, this
    improvement will help narrow down to the file changes we actually
    want to trigger the docker-dev build.
    
    flag = none
    
    Change-Id: I2d689ba9d00dd0955e4ea333f5f5fbd92c2f1f28
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248817
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    Reviewed-by: Ryan Norton <rnorton@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/docker-dev-changes.sh
+++ build/new-jenkins/docker-dev-changes.sh
@@ -4,15 +4,16 @@ set -o nounset -o errexit -o errtrace -o pipefail -o xtrace
 function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
 
 fileArr=(
-  'docker-compose/*'
-  'build/common_docker_build_steps.sh'
-  'script/canvas_update'
-  'docker-compose.yml'
-  'Dockerfile'
+  '^docker-compose/'
+  '^build/common_docker_build_steps.sh'
+  '^script/canvas_update'
+  '^docker-compose.yml'
+  '^Dockerfile'
+  '^lib/tasks/'
 )
 
-files=$(join_by '\|' "${fileArr[@]}")
+files=$(join_by '|' "${fileArr[@]}")
 
-changed="$(git show --pretty="" --name-only HEAD^..HEAD | grep "${files}")"
+changed="$(git show --pretty="" --name-only HEAD^..HEAD | grep -E "${files}")"
 
 [[ -n "$changed" ]]; exit $?
