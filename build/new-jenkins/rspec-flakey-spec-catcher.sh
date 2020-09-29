4196cdb7c4734f3ed820a415ff1fbe45a51dab57
Aaron Ogata <aogata@instructure.com>
2020-09-29 12:39:21 -0700

    clean-up FSC plugin path
    refs DE-298
    
    Test Plan:
    1. Ensure FSC plugin build passes
    2. Ensure FSC main build passes
    3. Ensure FSC distributes selenium tests correctly
    
    Change-Id: Ic60d34adb16e114700c69ec67834d56155dee2e5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/248876
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>

--- build/new-jenkins/rspec-flakey-spec-catcher.sh
+++ build/new-jenkins/rspec-flakey-spec-catcher.sh
@@ -2,19 +2,9 @@
 
 set -o errexit -o errtrace -o nounset -o pipefail -o xtrace
 
-if [ "${IS_PLUGIN}" == "1" ]; then
-   docker-compose --project-name "canvas-lms$1" exec -T -e FORCE_FAILURE=$FORCE_FAILURE canvas \
-         bash -c "cd $DOCKER_WORKDIR && BUNDLE_GEMFILE=../../../Gemfile bundle exec flakey_spec_catcher \
-         --repeat=$FSC_REPEAT_FACTOR \
-         --output=/usr/src/app/tmp/fsc.out \
-         --use-parent \
-         --test=$FSC_TESTS \
-         --rspec-options '-I spec_canvas'"
-else
-  docker-compose --project-name "canvas-lms$1" exec -T -e FORCE_FAILURE=$FORCE_FAILURE canvas \
-      bash -c "cd $DOCKER_WORKDIR && bundle exec flakey_spec_catcher \
-      --repeat=$FSC_REPEAT_FACTOR \
-      --output=/usr/src/app/tmp/fsc.out \
-      --test=$FSC_TESTS \
-      --use-parent"
-fi
+docker-compose --project-name "canvas-lms$1" exec -T -e FORCE_FAILURE=$FORCE_FAILURE canvas \
+    bash -c "cd /usr/src/app && bundle exec flakey_spec_catcher \
+    --repeat=$FSC_REPEAT_FACTOR \
+    --output=/usr/src/app/tmp/fsc.out \
+    --test=$FSC_TESTS \
+    --use-parent"
