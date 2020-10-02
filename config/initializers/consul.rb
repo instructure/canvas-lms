ec5a4fc05017a922e9851d0fdf1da9cfd126f818
Ethan Vizitei <evizitei@instructure.com>
2020-10-02 10:57:44 -0500

    resume cache clearing on SIGHUP
    Change-Id: I7b514f3bf149c111990ac235057e65ac3af747d2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/249158
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/consul.rb
+++ config/initializers/consul.rb
@@ -16,11 +16,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 handle_fallbacks = -> do
-  # TODO: restore once we figure out how to do this
-        # in a safe and synchronized way. Doing it at any time
-        # can cause read failures because of the way we check the tree and then everything
-        # else below it
-  # Canvas::DynamicSettings.reset_cache!
+  # dumps the whole cache, even if it's a shared local redis
+  Canvas::DynamicSettings.reset_cache!
 end
 handle_fallbacks.call
 Canvas::Reloader.on_reload(&handle_fallbacks)
