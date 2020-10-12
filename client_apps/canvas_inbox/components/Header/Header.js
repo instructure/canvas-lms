8122b4dab63ddd6b3a30f0af2372cdab00abdcaf
Jeffrey Johnson <jeffrey.johnson@instructure.com>
2020-10-12 10:42:08 -0700

    Initializing Storybook for Canvas_Inbox refs VICE-874
    Test Plan
    1. Pull down code
    2. Navigate to client_apps/canvas_inbox
    3. Run `yarn`
    4. Run `yarn storybook` to open Storybook at port 6006
    
    Note:
    - There are examples in this commit to help the team get started with storybook
    - Examples can be easily deleted and should be once a few components are started
    
    Change-Id: I5a8cb17053d7cf7ca1a69537f068dccf9be80bb7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/249896
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Matthew Lemon <mlemon@instructure.com>

---
+++ client_apps/canvas_inbox/components/Header/Header.js
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {Button} from '../Button/Button'
+import './header.css'
+
+export const Header = ({user, onLogin, onLogout, onCreateAccount}) => (
+  <header>
+    <div className="wrapper">
+      <div>
+        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fillRule="evenodd">
+            <path
+              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+              fill="#FFF"
+            />
+            <path
+              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+              fill="#555AB9"
+            />
+            <path
+              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+              fill="#91BAF8"
+            />
+          </g>
+        </svg>
+        <h1>Acme</h1>
+      </div>
+      <div>
+        {user ? (
+          <Button size="small" onClick={onLogout} label="Log out" />
+        ) : (
+          <>
+            <Button size="small" onClick={onLogin} label="Log in" />
+            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+          </>
+        )}
+      </div>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  user: PropTypes.shape({}),
+  onLogin: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  onCreateAccount: PropTypes.func.isRequired
+}
+
+Header.defaultProps = {
+  user: null
+}
