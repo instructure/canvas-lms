cebb568b827e805fb8b4af355215e881d078bdcd
Cody Cutrer <cody@instructure.com>
2020-10-13 14:39:48 -0600

    bump switchman for ruby 2.7 deprecation fixes
    Change-Id: I4b53f06cd49a1dbb7b0fdfae559995b1cf9ea031
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/250043
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -117,7 +117,7 @@ gem 'sanitize', '2.1.1', require: false
 gem 'sentry-raven', '2.13.0', require: false
 gem 'guardrail', '2.0.0'
 gem 'simple_oauth', '0.3.1', require: false
-gem 'switchman', '2.0.0'
+gem 'switchman', '2.0.1'
   gem 'open4', '1.3.4', require: false
 gem 'switchman-inst-jobs', '2.0.0'
 gem 'twilio-ruby', '5.36.0', require: false
