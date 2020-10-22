b7a5ce6e6ec101d743e1e84c98502d6f860810d6
Adrian Packel <apackel@instructure.com>
2020-10-22 18:02:02 -0500

    Fall back to scores for override grade changes
    fixes EVAL-1276
    flag=final_grade_override_in_gradebook_history
    
    Test plan:
    - Have a course with a grading scheme enabled (C1) and one without a
      grading scheme (C2)
      - For both courses, add some students, allow final grade override in
        Gradebook, and enter some override grades for students
    - Open Gradebook History for each course and check the
      before/after/current columns for the override grade events
      - For C1, the columns should show the appropriate letter grades
      - For C2, the columns should show the score displayed as a percent
        (e.g., "89%")
      - In both cases, the lack of a value should still be indicated by a
        dash
    - Do some general testing with switching a grading scheme on/off,
      doing some grading, then switching back (e.g., for C1, disable the
      scheme, enter some override grades, the re-enable it)
      - For override grade changes:
        - The before/after columns should reflect the grading scheme (or
          lack thereof) *at the time of the grade change*
        - The current column should always reflect the current grading
          scheme (or lack thereof)
      - Grade changes for assignments should not be affected
    
    Change-Id: Id6011a78a19f69fc39090e8bc7ce56f9aeec0ae3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/250911
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Gary Mei <gmei@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Spencer Olson <solson@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- lib/api/v1/grade_change_event.rb
+++ lib/api/v1/grade_change_event.rb
@@ -37,8 +37,8 @@ module Api::V1::GradeChangeEvent
       id: event.id,
       created_at: event.created_at.in_time_zone,
       event_type: event.event_type,
-      grade_before: event.grade_before,
-      grade_after: event.grade_after,
+      grade_before: display_grade(event: event, grade: event.grade_before, score: event.score_before),
+      grade_after: display_grade(event: event, grade: event.grade_after, score: event.score_after),
       excused_before: event.excused_before,
       excused_after: event.excused_after,
       graded_anonymously: event.graded_anonymously,
@@ -104,5 +104,14 @@ module Api::V1::GradeChangeEvent
       courses: courses_json(courses, user, session, [], []),
       users: users_json(users, user, session, [], @domain_root_account)
     }
+  end
+
+  def display_grade(event:, grade:, score:)
+    # Unlike for individual assignments, override grades get saved with blank
+    # grade_before/grade_after values if the course has no grading scheme.  If
+    # we have a score but no grade, fall back to showing the score.
+    return grade unless event.override_grade? && grade.blank?
+
+    score.present? ? I18n.n(score, percentage: true) : nil
   end
 end
