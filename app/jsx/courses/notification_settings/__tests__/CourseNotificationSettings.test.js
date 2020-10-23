eaba7ee401c61e62301b4a79468e23c015e5fb08
Ben Nelson <bnelson@instructure.com>
2020-10-23 17:36:04 -0600

    add test coverage for course NP updates
    Adding a test to cover actually firing the mutation to update course
    level notification preferences. This test would have caught VICE-954
    
    Test Plan:
     - specs pass
    
    closes VICE-957
    flag=N/A
    
    Change-Id: I53d60cf0bd8ba89ca535d471489871cd7166e7d4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/251068
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/jsx/courses/notification_settings/__tests__/CourseNotificationSettings.test.js
+++ app/jsx/courses/notification_settings/__tests__/CourseNotificationSettings.test.js
@@ -15,16 +15,18 @@
  * You should have received a copy of the GNU Affero General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import AlertManager from '../../../shared/components/AlertManager'
+import AlertManager, {AlertManagerContext} from '../../../shared/components/AlertManager'
 import CourseNotificationSettingsQuery from '../CourseNotificationSettingsQuery'
+import CourseNotificationSettingsManager from '../CourseNotificationSettingsManager'
 import {COURSE_NOTIFICATIONS_QUERY} from '../graphqlData/Queries'
+import {UPDATE_COURSE_NOTIFICATION_PREFERENCES} from '../graphqlData/Mutations'
 import {createCache} from 'jsx/canvas-apollo'
-import {render} from '@testing-library/react'
+import {fireEvent, render, wait} from '@testing-library/react'
 import {MockedProvider} from '@apollo/react-testing'
 import mockGraphqlQuery from '../../../shared/graphql_query_mock'
 import React from 'react'
 
-async function createGraphqlMocks(queryOverrides) {
+async function createQueryMocks(queryOverrides) {
   if (!Array.isArray(queryOverrides)) {
     queryOverrides = [queryOverrides]
   }
@@ -47,9 +49,74 @@ async function createGraphqlMocks(queryOverrides) {
   ]
 }
 
+async function createMutationMocks(mutationOverrides) {
+  if (!Array.isArray(mutationOverrides)) {
+    mutationOverrides = [mutationOverrides]
+  }
+
+  const mutationResult = await mockGraphqlQuery(
+    UPDATE_COURSE_NOTIFICATION_PREFERENCES,
+    mutationOverrides,
+    {
+      courseId: '1',
+      channelId: '1',
+      category: 'Grading',
+      frequency: 'never'
+    }
+  )
+
+  return [
+    {
+      request: {
+        query: UPDATE_COURSE_NOTIFICATION_PREFERENCES,
+        variables: {
+          courseId: '1',
+          channelId: '1',
+          category: 'Grading',
+          frequency: 'never'
+        }
+      },
+      result: mutationResult
+    }
+  ]
+}
+
+const mockedPrefs = {
+  sendScoresInEmails: false,
+  channels: [
+    {
+      _id: '1',
+      path: 'test@example.com',
+      pathType: 'email',
+      categories: {
+        courseActivities: {
+          Grading: {
+            communicationChannelId: '1',
+            frequency: 'immediately'
+          }
+        }
+      },
+      notificationPolicies: [
+        {
+          communicationChannelId: '1',
+          frequency: 'immediately',
+          notification: {
+            _id: '6',
+            category: 'Grading',
+            categoryDescription: 'Description Text',
+            categoryDisplayName: 'Display Name Text',
+            name: 'Assignment Graded'
+          }
+        }
+      ],
+      notificationPolicyOverrides: []
+    }
+  ]
+}
+
 describe('Course Notification Settings', () => {
   it('displays the correct messaging for enabled notification settings', async () => {
-    const mocks = await createGraphqlMocks({Node: {__typename: 'User'}})
+    const mocks = await createQueryMocks({Node: {__typename: 'User'}})
     mocks[0].result.data.legacyNode.notificationPreferencesEnabled = true
     const {findByText, findByTestId} = render(
       <MockedProvider mocks={mocks} cache={createCache()}>
@@ -68,7 +135,7 @@ describe('Course Notification Settings', () => {
   })
 
   it('displays the correct messaging for disabled notification settings', async () => {
-    const mocks = await createGraphqlMocks({Node: {__typename: 'User'}})
+    const mocks = await createQueryMocks({Node: {__typename: 'User'}})
     mocks[0].result.data.legacyNode.notificationPreferencesEnabled = false
     const {findByText} = render(
       <MockedProvider mocks={mocks} cache={createCache()}>
@@ -83,5 +150,45 @@ describe('Course Notification Settings', () => {
         'You will not receive any course notifications at this time. To enable course notifications, use the toggle above.'
       )
     ).toBeInTheDocument()
+  })
+
+  it('successfully calls the mutation when updating', async () => {
+    ENV = {
+      NOTIFICATION_PREFERENCES_OPTIONS: {
+        send_scores_in_emails_text: {
+          label: ''
+        },
+        granular_course_preferences_enabled: true
+      }
+    }
+    const mocks = await createMutationMocks([
+      {UpdateNotificationPreferencesPayload: {errors: null}}
+    ])
+    const mockedSetOnSuccess = jest.fn().mockResolvedValue({})
+
+    const {getByText, getAllByRole} = render(
+      <AlertManagerContext.Provider
+        value={{
+          setOnSuccess: mockedSetOnSuccess
+        }}
+      >
+        <MockedProvider mocks={mocks}>
+          <CourseNotificationSettingsManager
+            courseId="1"
+            courseName="Course"
+            enabled
+            notificationPreferences={mockedPrefs}
+          />
+        </MockedProvider>
+      </AlertManagerContext.Provider>
+    )
+
+    const button = getAllByRole('button').pop()
+    // Sanity check that we grabbed the correct button
+    expect(button).toHaveTextContent('Notify immediately')
+    fireEvent.click(button)
+    fireEvent.click(getByText('Notifications off'))
+
+    await wait(() => expect(mockedSetOnSuccess).toHaveBeenCalled())
   })
 })
