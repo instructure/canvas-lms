c8833063f95f5c470835d333d72f88271ebc9246
Ben Nelson <bnelson@instructure.com>
2020-10-23 13:09:10 -0600

    remove unused variable from Course NP mutation
    Removes a variable that is no longer used from the mutation, which was
    causing the mutation to throw an error.
    
    Test Plan:
     - Go to the Course Notification Preferences page in a course
     - Attempt to change any of the notification preferences
     - Verify that the change was made successfully
    
    fixes VICE-954
    
    Change-Id: Ibdc3faa9b90f1e2b8c8e7e74f2a7c9bbc4993f1f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/251031
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/jsx/courses/notification_settings/CourseNotificationSettingsQuery.js
+++ app/jsx/courses/notification_settings/CourseNotificationSettingsQuery.js
@@ -46,7 +46,6 @@ export default function CourseNotificationSettingsQuery(props) {
 
   return (
     <CourseNotificationSettingsManager
-      userId={props.userId}
       courseId={props.courseId}
       courseName={props.courseName}
       enabled={data?.legacyNode?.notificationPreferencesEnabled}
