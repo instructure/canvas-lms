b5e444215a880f20ea9c5c08a90e1c80e295d1e1
Cody Cutrer <cody@instructure.com>
2020-10-26 11:14:57 -0600

    frozen_string_literals: fix plugin settings transform
    Change-Id: I9e66c24a068859031620373d2e838fca19363e7b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/251160
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/models/plugin_setting.rb
+++ app/models/plugin_setting.rb
@@ -46,7 +46,7 @@ class PluginSetting < ActiveRecord::Base
   def validate_posted_settings
     if @posted_settings
       plugin = Canvas::Plugin.find(name.to_s)
-      @posted_settings.transform_values{|v| v.strip! if v.is_a?(String)}
+      @posted_settings.transform_values! {|v| v.is_a?(String) ? v.strip : v }
       result = plugin.validate_settings(self, @posted_settings)
       throw :abort if result == false
     end
