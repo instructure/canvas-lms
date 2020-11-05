4ad1524db37736566d8923f687d87c3a11097465
Ed Schiebel <eschiebel@instructure.com>
2020-11-05 11:06:25 -0500

    Apply MathJax to equations in Discussion replies
    closes LS-1595
    flag=new_math_equation_handling
    
    Most RCE content is processed by UserContent.escape in the
    canvas backend on its way to the browser, and that is where
    most equation images are replaced with their LaTex for MathJax
    processing.  The flow is different for Discussions whose replies
    follow a different path. This change replaces the image with LaTex
    in the same place in the browser as where the hidden mathml was
    previously injected into the DOM.
    
    test plan:
      - in a discusion, add a reply
      - use the Equation toolbar button to insert some math
      > expect the equation image
      - save by clicking Done
      > expect the image to be replaced with MaxJax typeset content
        (right click and you'll get the mathjax menu)
      - edit the reply
      > expect the equation image in the RCE
      - edit and add some inline math (e.g. $$y = x^2$$)
      - save
      > expect both the original image and the inline equation to
        be typeset by MathJax
    
    Change-Id: Icedcbbf3b2f084b1a9c254ec2a070b31c35ffed9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/251979
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- app/coffeescripts/str/apiUserContent.js
+++ app/coffeescripts/str/apiUserContent.js
@@ -25,16 +25,27 @@ const apiUserContent = {
   xsslint safeString.identifier mathml
   */
   translateMathmlForScreenreaders($equationImage) {
-    // note, it is safe to treat the x-canvaslms-safe-mathml as html because it
-    // only ever gets put there by us (in Api::Html::Content::apply_mathml).
-    // Any user content that gets sent to the server will have the
-    // x-canvaslms-safe-mathml attribute stripped out.
-    const mathml = $('<div/>')
-      .html($equationImage.attr('x-canvaslms-safe-mathml'))
-      .html()
-    const mathmlSpan = $('<span class="hidden-readable"></span>')
-    mathmlSpan.html(mathml)
-    return mathmlSpan
+    if (ENV?.FEATURES?.new_math_equation_handling) {
+      // in most cases, the image will already have been replaced
+      // by UserContent.escape on the backend, but not for
+      // Discussion replies. Catch them here.
+      const equation_text = $equationImage.attr('data-equation-content')
+      const mathtex = document.createElement('span')
+      mathtex.setAttribute('class', 'math_equation_latex')
+      mathtex.textContent = `\\(${equation_text}\\)`
+      $equationImage.replaceWith(mathtex)
+    } else {
+      // note, it is safe to treat the x-canvaslms-safe-mathml as html because it
+      // only ever gets put there by us (in Api::Html::Content::apply_mathml).
+      // Any user content that gets sent to the server will have the
+      // x-canvaslms-safe-mathml attribute stripped out.
+      const mathml = $('<div/>')
+        .html($equationImage.attr('x-canvaslms-safe-mathml'))
+        .html()
+      const mathmlSpan = $('<span class="hidden-readable"></span>')
+      mathmlSpan.html(mathml)
+      return mathmlSpan
+    }
   },
 
   toMediaCommentLink(node) {
--- spec/coffeescripts/str/apiUserContentSpec.js
+++ spec/coffeescripts/str/apiUserContentSpec.js
@@ -19,21 +19,32 @@
 import apiUserContent from 'compiled/str/apiUserContent'
 
 let mathml_html
+let FEATURES
 
 QUnit.module('apiUserContent.convert', {
   setup() {
+    FEATURES = ENV.FEATURES
     mathml_html =
       '<div><ul>\n' +
       "<li><img class=\"equation_image\" x-canvaslms-safe-mathml=\"&lt;math xmlns=&quot;http://www.w3.org/1998/Math/MathML&quot; display=&quot;inline&quot;&gt;&lt;mi&gt;i&lt;/mi&gt;&lt;mi&gt;n&lt;/mi&gt;&lt;mi&gt;t&lt;/mi&gt;&lt;mi&gt;f&lt;/mi&gt;&lt;mo stretchy='false'&gt;(&lt;/mo&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;mo stretchy='false'&gt;)&lt;/mo&gt;&lt;mo&gt;/&lt;/mo&gt;&lt;mi&gt;g&lt;/mi&gt;&lt;mo stretchy='false'&gt;(&lt;/mo&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;mo stretchy='false'&gt;)&lt;/mo&gt;&lt;/math&gt;\"></li>\n" +
       '<li><img class="equation_image" x-canvaslms-safe-mathml=\'&lt;math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"&gt;&lt;mo lspace="thinmathspace" rspace="thinmathspace"&gt;&amp;Sum;&lt;/mo&gt;&lt;mn&gt;1&lt;/mn&gt;&lt;mo&gt;.&lt;/mo&gt;&lt;mo&gt;.&lt;/mo&gt;&lt;mi&gt;n&lt;/mi&gt;&lt;/math&gt;\'></li>\n' +
       '<li><img class="nothing_special"></li>\n' +
       '</ul></div>'
+  },
+  teardown() {
+    ENV.FEATURES = FEATURES
   }
 })
 
 test('moves mathml into a screenreader element', () => {
   const output = apiUserContent.convert(mathml_html)
   ok(output.includes('<span class="hidden-readable"><math '))
+})
+
+test('replaced math equation with LaTex if new_math_equation_handling flag is on', () => {
+  ENV.FEATURES = {new_math_equation_handling: true}
+  const output = apiUserContent.convert(mathml_html)
+  ok(output.includes('<span class="math_equation_latex">\\('))
 })
 
 test('mathml need not be screenreadered if editing content (this would start an update loop)', () => {
