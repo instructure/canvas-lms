f01193f3f9c9e31fb85e17468218ffcc1983c4fc
Aaron Ogata <aogata@instructure.com>
2020-11-09 06:00:11 -0800

    retag instructure/ruby-passenger in case it has changed
    Change-Id: Ic53a5c60b551ea84ad4755ec10049bdc42ab6691
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/252243
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- build/new-jenkins/docker-build.sh
+++ build/new-jenkins/docker-build.sh
@@ -10,6 +10,7 @@ DOCKER_BUILDKIT=1 docker build --file Dockerfile.jenkins-cache --tag "local/cach
 
 # shellcheck disable=SC2086
 docker pull $CACHE_TAG || true
+docker pull instructure/ruby-passenger:$RUBY
 docker build \
   --build-arg COMPILE_ADDITIONAL_ASSETS="$COMPILE_ADDITIONAL_ASSETS" \
   --build-arg CANVAS_RAILS6_0=${CANVAS_RAILS6_0:-0} \
