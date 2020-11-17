35b872f33039184f966b7ca9627279ad1dedf49d
Aaron Ogata <aogata@instructure.com>
2020-11-17 08:34:15 -0800

    restructure Linters jobs for ruby-runner change
    refs DE-376
    
    With the ruby-runner change that is coming, the final image will no longer include the node_modules/ assets, so we have to checkout the WEBPACK_BUILDER separately and copy in the source from the final PATCHSET_TAG.
    
    Also, this change will make Linters job the bottleneck so parallelize the job into 3 different parts, cutting the runtime by 50%.
    
    Test Plan:
    1. Ensure that Gergich posts to the PS for message failures.
    2. Ensure that webpack asset too large failures fail the build.
    3. Ensure that yarn.lock needs updating produces a -2.
    4. Ensure that failures can be clearly found in the build log.
    5. Ensure that linter errors post to the PS
    
    Change-Id: I24772c8386e7e5ebc51884450a7e2fb0daa91433
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/252982
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>

--- build/new-jenkins/linters/run-gergich.sh
+++ build/new-jenkins/linters/run-gergich.sh
@@ -7,6 +7,7 @@ GIT_SSH_COMMAND='ssh -i "$SSH_KEY_PATH" -l "$SSH_USERNAME"' \
 
 inputs=()
 inputs+=("--volume $(pwd)/.git:/usr/src/app/.git")
+inputs+=("--env GERGICH_DB_PATH=/home/docker/gergich")
 inputs+=("--env GERGICH_PUBLISH=$GERGICH_PUBLISH")
 inputs+=("--env GERGICH_KEY=$GERGICH_KEY")
 inputs+=("--env GERRIT_HOST=$GERRIT_HOST")
@@ -22,56 +23,89 @@ inputs+=("--env GERRIT_CHANGE_NUMBER=$GERRIT_CHANGE_NUMBER")
 # send things to gergich
 inputs+=("--env GERRIT_REFSPEC=$GERRIT_REFSPEC")
 
-cat <<EOF | docker run --interactive ${inputs[@]} "$PATCHSET_TAG" /bin/bash -
+# Sometimes Docker doesn't clean up the volume completely and
+# errors when trying to create the backing folder. Make it
+# unique to avoid this.
+GERGICH_VOLUME="gergich-results-$(date +%s)"
+docker volume create $GERGICH_VOLUME
+
+cat <<EOF | DOCKER_BUILDKIT=1 PROGRESS_NO_TRUNC=1 docker build \
+  --build-arg PATCHSET_TAG="$PATCHSET_TAG" \
+  --build-arg WEBPACK_BUILDER_TAG="$WEBPACK_BUILDER_IMAGE" \
+  --tag "local/gergich" \
+  -
+# syntax=docker/dockerfile:experimental
+
+ARG PATCHSET_TAG
+ARG WEBPACK_BUILDER_TAG
+FROM \$PATCHSET_TAG AS patchset
+FROM \$WEBPACK_BUILDER_TAG
+USER docker
+RUN mkdir -p /home/docker/gergich
+RUN --mount=type=bind,target=/tmp/src,source=/usr/src/app,from=patchset \
+  cp -rf /tmp/src/. /usr/src/app
+EOF
+
+cat <<EOF | docker run --interactive ${inputs[@]} --volume $GERGICH_VOLUME:/home/docker/gergich local/gergich /bin/bash - &
 set -ex
+export COMPILE_ASSETS_NPM_INSTALL=0
+export JS_BUILD_NO_FALLBACK=1
+./build/new-jenkins/linters/run-and-collect-output.sh "gergich capture custom:./build/gergich/compile_assets:Gergich::CompileAssets 'rake canvas:compile_assets'"
 
-# ensure we run the gergich comments with the Lint-Review label
-export GERGICH_REVIEW_LABEL="Lint-Review"
+gergich status
+echo "WEBPACK_BUILD OK!"
+EOF
+WEBPACK_BUILD_PID=$!
 
+cat <<EOF | docker run --interactive ${inputs[@]} --volume $GERGICH_VOLUME:/home/docker/gergich local/gergich /bin/bash - &
+set -ex
 # when parent is not in \$GERRIT_BRANCH (i.e. master)
 if ! git merge-base --is-ancestor HEAD~1 \$GERRIT_BRANCH; then
   message="This commit is built upon commits not currently merged in \$GERRIT_BRANCH. Ensure that your dependent patchsets are merged first!\\n"
   gergich comment "{\"path\":\"/COMMIT_MSG\",\"position\":1,\"severity\":\"warn\",\"message\":\"\$message\"}"
 fi
 
-# we need to remove the hooks because compile_assets calls yarn install which will
-# try to create the .git commit hooks
-> ./script/install_hooks
-export COMPILE_ASSETS_NPM_INSTALL=0
-export COMPILE_ASSETS_API_DOCS=0
-export COMPILE_ASSETS_STYLEGUIDE=0
-export JS_BUILD_NO_FALLBACK=1
-gergich capture custom:./build/gergich/compile_assets:Gergich::CompileAssets "rake canvas:compile_assets"
+./build/new-jenkins/linters/run-and-collect-output.sh "gergich capture custom:./build/gergich/xsslint:Gergich::XSSLint 'node script/xsslint.js'"
+./build/new-jenkins/linters/run-and-collect-output.sh "gergich capture i18nliner 'rake i18n:check'"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/brakeman"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/tatl_tael"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/stylelint"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/rlint"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/eslint"
+./build/new-jenkins/linters/run-and-collect-output.sh "bundle exec ruby script/lint_commit_message"
 
-gergich capture custom:./build/gergich/xsslint:Gergich::XSSLint "node script/xsslint.js"
-gergich capture i18nliner "rake i18n:check"
-bundle exec ruby script/brakeman
-bundle exec ruby script/tatl_tael
-bundle exec ruby script/stylelint
-bundle exec ruby script/rlint
-bundle exec ruby script/eslint
-bundle exec ruby script/lint_commit_message
+gergich status
+echo "LINTER OK!"
+EOF
+LINTER_PID=$!
 
-plugins_list=\$(cat config/plugins_list)
-for gem in \$plugins_list
-do
-  echo \$gem
-  rm -r gems/plugins/\$gem
-done
-yarn install
+cat <<EOF | docker run --interactive ${inputs[@]} --volume $GERGICH_VOLUME:/home/docker/gergich local/gergich /bin/bash - &
+set -ex
+readarray -t PLUGINS_LIST < config/plugins_list
+rm -rf \$(printf 'gems/plugins/%s ' "\${PLUGINS_LIST[@]}")
+
+export DISABLE_POSTINSTALL=1
+./build/new-jenkins/linters/run-and-collect-output.sh "yarn install"
+
 if ! git diff --exit-code yarn.lock; then
   message="yarn.lock changes need to be checked in. Make sure you run 'yarn install' without private canvas-lms plugins installed."
   gergich comment "{\"path\":\"yarn.lock\",\"position\":1,\"severity\":\"error\",\"message\":\"\$message\"}"
 fi
 
-bundle exec rake doc:api css:styleguide
-if ! git diff --exit-code lib/api_scope_mapper.rb; then
-  message="lib/api_scope_mapper.rb changes need to be checked in. Make sure you run 'bundle exec rake doc:api' without private canvas-lms plugins installed."
-  gergich comment "{\"path\":\"lib/api_scope_mapper.rb\",\"position\":1,\"severity\":\"error\",\"message\":\"\$message\"}"
-fi
-
-git status
 gergich status
+echo "YARN_LOCK OK!"
+EOF
+YARN_LOCK_PID=$!
+
+wait $WEBPACK_BUILD_PID
+wait $LINTER_PID
+wait $YARN_LOCK_PID
+
+cat <<EOF | docker run --interactive ${inputs[@]} --volume $GERGICH_VOLUME:/home/docker/gergich local/gergich /bin/bash -
+set -ex
+export GERGICH_REVIEW_LABEL="Lint-Review"
+gergich status
+
 if [[ "\$GERGICH_PUBLISH" == "1" ]]; then
   # we need to do this because it forces gergich to not use git (because no git repo is there).
   # and being that we rebased, the commit hash changes, so this will make it use the variables passed in
