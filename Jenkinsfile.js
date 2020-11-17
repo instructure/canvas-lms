534411171427d0da3fd18206aa89560599944af1
Aaron Ogata <aogata@instructure.com>
2020-11-17 17:08:45 +0000

    restructure JS jobs for ruby-runner change
    refs DE-376
    
    With the ruby-runner change that is coming, the final image will no longer include the node_modules/ assets, so we have to checkout the WEBPACK_BUILDER separately and copy in the source from the final PATCHSET_TAG.
    
    Change-Id: Ic3c048500e00858bfa39244b308112cbc721783d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/252666
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kyle Rosenbaum <krosenbaum@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -65,8 +65,11 @@ pipeline {
   options { ansiColor('xterm') }
 
   environment {
-    COMPOSE_FILE = 'docker-compose.new-jenkins.canvas.yml:docker-compose.new-jenkins-karma.yml'
+    COMPOSE_DOCKER_CLI_BUILD=1
+    COMPOSE_FILE = 'docker-compose.new-jenkins-js.yml'
+    DOCKER_BUILDKIT=1
     FORCE_FAILURE = configuration.forceFailureJS()
+    PROGRESS_NO_TRUNC=1
     SENTRY_URL="https://sentry.insops.net"
     SENTRY_ORG="instructure"
     SENTRY_PROJECT="master-javascript-build"
@@ -85,7 +88,6 @@ pipeline {
 
                 checkoutRepo("canvas-lms", refspecToCheckout, 1)
 
-                sh './build/new-jenkins/docker-with-flakey-network-protection.sh pull $PATCHSET_TAG'
                 sh 'docker-compose build'
               }
             }
