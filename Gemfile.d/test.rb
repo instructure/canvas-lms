886380fe5ee778eac9232b1a036f63f5ff06c80e
Aaron Ogata <aogata@instructure.com>
2020-11-18 15:06:44 -0800

    bump gergich gem
    Change-Id: Ib6c71a9a7abc2c1bdc0715988ac1f3da0d8834cf
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/253150
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -19,7 +19,7 @@ group :test do
   gem 'rails-dom-testing', '2.0.3'
   gem 'rails-controller-testing', '1.0.5'
 
-  gem 'gergich', '1.2.0', require: false
+  gem 'gergich', '1.2.1', require: false
   gem 'dotenv', '2.7.5', require: false
   gem 'testingbot', require: false
   gem 'brakeman', require: false
