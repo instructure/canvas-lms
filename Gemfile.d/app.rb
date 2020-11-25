f2a210574f1c3a6adbaf927e1c0cd4dcc489fbd2
Ethan Vizitei <evizitei@instructure.com>
2020-11-25 09:41:45 -0600

    bump inst jobs version
    refs FOO-1237
    
    Change-Id: Ic225560efccfb17e68847d36932375f24348c3cb
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/253728
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -74,9 +74,9 @@ gem 'inst_statsd', '2.1.6'
   gem 'statsd-ruby', '1.4.0', require: false
   gem 'aroi', '0.0.7', require: false
   gem 'dogstatsd-ruby', '4.7.0'
-gem 'inst-jobs', '1.0.3'
-  gem 'fugit', '1.3.3', require: false
-    gem 'et-orbi', '1.2.2', require: false
+gem 'inst-jobs', '1.0.4'
+  gem 'fugit', '1.4.0', require: false
+    gem 'et-orbi', '1.2.4', require: false
 gem 'inst-jobs-autoscaling', '2.0.0'
   gem 'aws-sdk-autoscaling', '1.32.0', require: false
 gem 'inst-jobs-statsd', '2.0.0'
