fcfe6fe18e2a4fdef7ca2ab8ff1542d15814bb8e
Pat Renner <prenner@instructure.com>
2020-11-25 12:25:19 -0600

    add outcome_friendly_description model
    closes OUT-4017
    flag=none
    
    Test plan:
    - run migrations
    > dc run --rm web bundle exec rake db:migrate
    - open rails console
    - create a learning outcome if you dont already have one in
    your account
    - verify you can create a friendly description
    OutcomeFriendlyDescription.create! context: Account.default,
    description: 'great!', learning_outcome: LearningOutcome.first
    - verify you can create one friendly description
    per context and learning outcome
      > create a second learning outcome if you dont have one
    in your account
      > via the rails console:
     OutcomeFriendlyDescription.create! context: Account.default,
    description: 'great!', learning_outcome: LearningOutcome.last
     > confirm a RecordInvalid error is not raised
    
    Change-Id: I65f2771bf46312e1ea4b4120eed714c7814dcd91
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/253751
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Brian Watson <bwatson@instructure.com>
    Product-Review: Pat Renner <prenner@instructure.com>
    Reviewed-by: Michael Brewer-Davis <mbd@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>

---
+++ spec/models/outcome_friendly_description_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2020 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+
+describe OutcomeFriendlyDescription, type: :model do
+
+  let_once(:account) { account_model }
+  let_once(:outcome) { outcome_model }
+  let(:description) { 'description' }
+  let(:creation_params) { { context: account, description: description, learning_outcome: outcome } }
+
+  describe 'validations' do
+    subject { OutcomeFriendlyDescription.create!(creation_params) }
+
+    it { is_expected.to validate_presence_of :context }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_length_of(:description).is_at_most(255) }
+    it { is_expected.to validate_length_of(:description).is_at_least(1) }
+    it { is_expected.to validate_uniqueness_of(:learning_outcome_id).scoped_to(:context_type, :context_id) }
+  end
+
+  it_behaves_like "soft deletion" do
+    subject { OutcomeFriendlyDescription }
+
+    let(:creation_arguments) { [creation_params, creation_params.merge(context: course_model)] }
+  end
+
+  describe 'root_account_id' do
+    subject { OutcomeFriendlyDescription.create!(description: 'A', context: @context, learning_outcome: outcome_model) }
+
+    before do
+      @root_account = account_model
+    end
+
+    context 'sets root account from course context' do
+      before do
+        @context = course_model(account: @root_account)
+      end
+
+      it { expect(subject.root_account_id).to eq(@context.root_account_id) }
+    end
+
+    context 'sets root account from account context' do
+      before do
+        @context = account_model(parent_account: @root_account)
+      end
+
+      it { expect(subject.root_account_id).to eq(@context.root_account_id) }
+    end
+  end
+end
