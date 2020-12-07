304ce2211124ba2910b3759baf029d344336d02b
Rob Orton <rob@instructure.com>
2020-12-06 18:10:17 -0700

    Add fix_html
    refs LS-1680
    flag = none
    
    test plan:
    - quiz looks good
    
    Change-Id: If6c44ac655a56bf2ce782a2a599b93270ee9fba0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/254438
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Tested-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- lib/quiz_math_data_fixup.rb
+++ lib/quiz_math_data_fixup.rb
@@ -63,109 +63,64 @@ module QuizMathDataFixup
   end
 
   def fixup_question_data(data)
-    data[:answers]&.each_with_index do |answer, index|
-      data[:answers][index] = fixup_answer(answer)
+    %i(neutral_comments_html correct_comments_html incorrect_comments_html).each do |key|
+      if data[key].present?
+        data[key] = fixup_html(data[key])
+      end
+    end
+    data[:answers].each_with_index do |answer, index|
+      %i(html comments_html).each { |key| answer[key] = fixup_html(answer[key]) if answer[key].present? }
+      data[:answers][index] = answer
     end
     data
   end
 
-  def fixup_answer(answer)
-    answer_changed = false
-    [%i[html text], %i[comments_html comments]].each do |shtml, stext|
-      max_len = shtml == :html ? 16_384 : 5_120 # max allowable length for the data field
-      # inline LaTeX is contained w/in <span class="math_equation_latex">,
-      # which is probably there because Canvas replaced an equation image
-      # while the new_math_equation_handling flag was on
-      # deal with MathJax generated children that weren't children of .math_equation_latex
+  def fixup_html(html)
+    return html unless html
+    html = Nokogiri::HTML::DocumentFragment.parse(html)
+    html.search('[id^="MathJax"]').each(&:remove)
+    if html.children.length == 1 && html.children[0].node_type == Nokogiri::XML::Node::TEXT_NODE
+      m = %r{equation_images\/([^\s]+)}.match(html.content)
+      if m && m[1]
+        code = URI.unescape(URI.unescape(m[1]))
+        html =
+          "<img class='equation_image' src='/equation_images/#{m[1]}' alt='LaTeX: #{
+            code
+          }' title='#{code} data-equation-content='#{code}>"
+      end
+      return html
+    end
+    html.search('.math_equation_latex').each do |latex|
+      # find MathJax generated children, extract the eq's mathml
+      # incase we need it later, then remove them
+      mjnodes =
+        html.search('[class^="MathJax"]')
 
-      if (answer[shtml] && answer[shtml].length > 0)
-        html = answer[shtml]
-        html = Nokogiri::HTML::DocumentFragment.parse(html)
-        html.search('[id^="MathJax"]').each(&:remove)
-        if html.children.length == 1 && html.children[0].node_type == Nokogiri::XML::Node::TEXT_NODE
-          m = %r{equation_images\/([^\s]+)}.match(html.content)
-          if m && m[1]
-            code = URI.unescape(URI.unescape(m[1]))
-            answer[shtml] =
-              "<img class='equation_image' src='/equation_images/#{m[1]}' alt='LaTeX: #{
-                code
-              }' title='#{code} data-equation-content='#{code}>"
-            answer[stext] = ''
-          end
-          return answer
-        end
-        html.search('.math_equation_latex').each do |latex|
-          # find MathJax generated children, extract the eq's mathml
-          # incase we need it later, then remove them
-          mjnodes =
-            html.search('[class^="MathJax"]')
-
-          if mjnodes.length > 0
-            n = mjnodes.filter('[data-mathml]')[0]
-            mml = n.attribute('data-mathml') if n
-            mjnodes.each(&:remove)
-            answer_changed = true
-          end
-          if (latex.content.length > 0)
-            code = latex.content.gsub(/(^\\\(|\\\)$)/, '')
-            escaped = URI.escape(URI.escape(code))
-            latex.replace(
-              "<img class='equation_image' src='/equation_images/#{escaped}' alt='LaTeX: #{
-                code
-              }' title='#{code}>"
-            )
-            answer_changed = true
-          elsif mml
-            latex.replace(
-              "<math xmlns='http://www.w3.org/1998/Math/MathML' class='math_equation_mml'>#{
-                mml
-              }</math>"
-            )
-            answer_changed = true
-          end
-        end
-        mjnodes = html.search('[class^="MathJax"]')
-
-        if mjnodes.length > 0
-          if mjnodes.length == html.elements.length
-            n = mjnodes.filter('[data-mathml]')[0]
-            mml = n.attribute('data-mathml') if n
-          end
-          mjnodes.each(&:remove)
-          latex = html.search('.math_equation_latex')[0]
-          img = html.search('img.equation_image')
-
-          if latex && latex.content.length > 0
-            latex.content = "\\(#{latex.content}\\)" if latex.content !~ /\\\(.*\\\)/
-          elsif img.length == 0
-            html.inner_html = "<span class='math_equation_mml'>#{mml}</span>"
-          end
-          answer_changed = true
-        end
-        hrnodes = html.search('span.hidden-readable')
-        if hrnodes.length > 0
-          hrnodes.each(&:remove)
-          answer_changed = true
-        end
-
-        if answer_changed
-          answer[shtml] = html.to_s if answer_changed
-          answer[stext] = ''
-        end
-      elsif answer[stext] && answer[stext].length > 0
-        m = %r{equation_images\/([^\s]+)}.match(answer[stext])
-        if m && m[1]
-          code = URI.unescape(URI.unescape(m[1]))
-          answer[shtml] =
-            "<img class='equation_image' src='/equation_images/#{m[1]}' alt='LaTeX: #{
-              code
-            }' title='#{code}>"
-          answer[stext] = ''
-          return answer
-        end
+      if mjnodes.length > 0
+        n = mjnodes.filter('[data-mathml]')[0]
+        mml = n.attribute('data-mathml') if n
+        mjnodes.each(&:remove)
+      end
+      if (latex.content.length > 0)
+        code = latex.content.gsub(/(^\\\(|\\\)$)/, '')
+        escaped = URI.escape(URI.escape(code))
+        latex.replace(
+          "<img class='equation_image' src='/equation_images/#{escaped}' alt='LaTeX: #{
+            code
+          }' title='#{code}>"
+        )
+      elsif mml
+        latex.replace(
+          "<math xmlns='http://www.w3.org/1998/Math/MathML' class='math_equation_mml'>#{
+            mml
+          }</math>"
+        )
       end
     end
-    answer
+
+    html.search('span.hidden-readable').each(&:remove)
+
+    html.to_s
   end
 
   def check_or_fix_quizzes(batch_of_ids)
