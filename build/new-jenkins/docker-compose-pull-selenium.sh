cba7565a764bf8f27cb670437fa29ef8a8463b95
Aaron Ogata <aogata@instructure.com>
2020-12-08 06:24:27 -0800

    move selenium-chrome build to helper job
    refs DE-384
    
    Change-Id: Ibcaa7295774145cef04eef77a3056b8e9b456bfd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/254586
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- build/new-jenkins/docker-compose-pull-selenium.sh
+++ build/new-jenkins/docker-compose-pull-selenium.sh
@@ -5,9 +5,7 @@ set -x -o errexit -o errtrace -o nounset -o pipefail
 # pull docker images (or build them if missing)
 REGISTRY_BASE=starlord.inscloudgate.net/jenkins
 
-./build/new-jenkins/docker-with-flakey-network-protection.sh pull $REGISTRY_BASE/selenium-chrome:"$SELENIUM_VERSION" || \
-  (docker build -t $REGISTRY_BASE/selenium-chrome:"$SELENIUM_VERSION" --build-arg SELENIUM_VERSION="$SELENIUM_VERSION" build/docker-compose/selenium-chrome && \
-  ./build/new-jenkins/docker-with-flakey-network-protection.sh push $REGISTRY_BASE/selenium-chrome:"$SELENIUM_VERSION")
+./build/new-jenkins/docker-with-flakey-network-protection.sh pull $REGISTRY_BASE/selenium-chrome:"$SELENIUM_VERSION"
 
 # pull canvas-rce-api here to avoid flakes, dependency of docker-compose.new-jenkins.selenium.yml
 ./build/new-jenkins/docker-with-flakey-network-protection.sh pull $BUILD_REGISTRY_FQDN/jenkins/canvas-rce-api
