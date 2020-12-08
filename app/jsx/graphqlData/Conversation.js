6c18997839d1e3c8081362463fdd6715be466fdf
Caleb Guanzon <cguanzon@instructure.com>
2020-12-08 12:33:51 -0700

    adds Conversation graphql data query
    flag=none
    
    fixes VICE-1014
    
    test plan:
    
    code review
    graphiql run
    
    Change-Id: I06f3e08dc2450cae22846e68483de1dd6cc304c3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/254638
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Matthew Lemon <mlemon@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ app/jsx/graphqlData/Conversation.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import gql from 'graphql-tag'
+import {shape, string} from 'prop-types'
+import {ConversationMessage} from './ConversationMessage'
+import {ConversationParticipant} from './ConversationParticipant'
+
+export const Conversation = {
+  fragment: gql`
+    fragment Conversation on Conversation {
+      _id
+      contextId
+      contextType
+      subject
+      conversationMessagesConnection {
+        nodes {
+          ...ConversationMessage
+        }
+      }
+      conversationParticipantsConnection {
+        nodes {
+          ...ConversationParticipant
+        }
+      }
+    }
+    ${ConversationMessage.fragment}
+    ${ConversationParticipant.fragment}
+  `,
+
+  shape: shape({
+    _id: string,
+    contextId: string,
+    contextType: string,
+    subject: string,
+    conversationMessagesConnection: ConversationMessage.shape,
+    conversationParticipantsConnection: ConversationParticipant.shape
+  })
+}
