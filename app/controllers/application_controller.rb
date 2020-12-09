dfb4799c955b2f1757841fbd934b6a05a01d1e3a
Ethan Vizitei <evizitei@instructure.com>
2020-12-09 15:35:47 -0600

    keep search-term length out of sentry
    closes FOO-1306
    flag=none
    
    TEST PLAN:
      1) search for users with a term like "a"
      2) you should not get a 500, but a 400
      3) no sentry errors
    
    Change-Id: Idb614e07dd95af6564f73c4480549bd8097167e5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/254768
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1492,6 +1492,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::UnknownFormat, with: :rescue_expected_error_type
   rescue_from Canvas::Security::TokenExpired, with: :rescue_expected_error_type
   rescue_from ActionView::MissingTemplate, with: :rescue_expected_error_type
+  rescue_from SearchTermHelper::SearchTermTooShortError, with: :rescue_expected_error_type
 
   def rescue_expected_error_type(error)
     rescue_exception(error, level: :info)
