bdf2c2b89db4ce3ff3120c184073b493839fea5c
Cody Cutrer <cody@instructure.com>
2020-12-14 14:06:07 -0700

    bump inst-jobs
    Change-Id: I962c69c317791126a8cd29ca3e98ebce8542c18a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/255087
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -74,7 +74,7 @@ gem 'inst_statsd', '2.1.6'
   gem 'statsd-ruby', '1.4.0', require: false
   gem 'aroi', '0.0.7', require: false
   gem 'dogstatsd-ruby', '4.8.2'
-gem 'inst-jobs', '2.1.1'
+gem 'inst-jobs', '2.1.2'
   gem 'fugit', '1.4.0', require: false
     gem 'et-orbi', '1.2.4', require: false
 gem 'inst-jobs-autoscaling', '2.1.0'
