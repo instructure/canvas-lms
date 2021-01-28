468e96a658c882b213bd64023f64f8008159a8f3
Davis Hyer <dhyer@instructure.com>
2021-01-28 16:27:58 -0700

    add PostToolbar component
    fixes VICE-931
    flag=none
    
    test plan:
      - open storybook and navigate to PostToolbar
      - publish button should render all 5 states correctly
        - green when published
        - red when published and hovered
        - red while unpublishing
        - grey when unpublished
        - grey while publishing
      - publish button callbacks work
        - publishing and unpublishing states should be readonly
      - edit button callback should work
      - menu items should render
        - comment toggling text should respect the current
          enablement setting
      - menu item callbacks should work
      - ensure "StudentView" only renders the option to mark
        all as read, similar to current discussions
    
    qa risk: low
    
    Change-Id: If11f99484448a456996bfc0c26a3a4c7c9597f2d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/257708
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jeffrey Johnson <jeffrey.johnson@instructure.com>
    Product-Review: Jeffrey Johnson <jeffrey.johnson@instructure.com>
    Reviewed-by: Jeffrey Johnson <jeffrey.johnson@instructure.com>

---
+++ app/jsx/discussion_posts/PostToolbar/PostToolbar.js
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import PropTypes from 'prop-types'
+import React from 'react'
+import {Flex} from '@instructure/ui-flex'
+import {Menu} from '@instructure/ui-menu'
+import {
+  IconMoreLine,
+  IconMarkAsReadLine,
+  IconTrashLine,
+  IconLockLine,
+  IconUnlockLine,
+  IconUserLine,
+  IconDuplicateLine,
+  IconEditLine
+} from '@instructure/ui-icons'
+import {Button, IconButton} from '@instructure/ui-buttons'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+import I18n from 'i18n!conversations_2'
+import {PublishButton} from './PublishButton'
+
+export function PostToolbar({...props}) {
+  return (
+    <View display="block" padding="small" background="secondary">
+      <Flex width="100%" justifyItems="end">
+        {props.onTogglePublish && (
+          <PublishButton
+            key={props.publishState}
+            initialState={props.publishState}
+            onClick={props.onTogglePublish}
+          />
+        )}
+        {props.onEdit && (
+          <Button renderIcon={IconEditLine} onClick={props.onEdit} margin="0 x-small 0 xxx-small">
+            {I18n.t('Edit')}
+          </Button>
+        )}
+        {renderMenu(props)}
+      </Flex>
+    </View>
+  )
+}
+
+const renderMenu = props => {
+  return (
+    <Menu
+      trigger={
+        <IconButton
+          screenReaderLabel={I18n.t('Manage Discussion')}
+          renderIcon={IconMoreLine}
+          data-testid="discussion-post-menu-trigger"
+        />
+      }
+    >
+      {getMenuConfigs(props).map(config => renderMenuItem({...config}))}
+    </Menu>
+  )
+}
+
+const getMenuConfigs = props => {
+  const options = [
+    {
+      key: 'read-all',
+      icon: <IconMarkAsReadLine />,
+      labelCallback: () => I18n.t('Mark All as Read'),
+      selectionCallback: props.onReadAll
+    }
+  ]
+  if (props.onDelete) {
+    options.push({
+      key: 'delete',
+      icon: <IconTrashLine />,
+      labelCallback: () => I18n.t('Delete'),
+      selectionCallback: props.onDelete
+    })
+  }
+  if (props.onToggleComments && props.commentsEnabled) {
+    options.push({
+      key: 'toggle-comments',
+      icon: <IconLockLine />,
+      labelCallback: () => I18n.t('Close for Comments'),
+      selectionCallback: props.onToggleComments
+    })
+  } else if (props.onToggleComments && !props.commentsEnabled) {
+    options.push({
+      key: 'toggle-comments',
+      icon: <IconUnlockLine />,
+      labelCallback: () => I18n.t('Open for Comments'),
+      selectionCallback: props.onToggleComments
+    })
+  }
+  if (props.onSend) {
+    options.push({
+      key: 'send',
+      icon: <IconUserLine />,
+      labelCallback: () => I18n.t('Send To...'),
+      selectionCallback: props.onSend
+    })
+  }
+  if (props.onCopy) {
+    options.push({
+      key: 'copy',
+      icon: <IconDuplicateLine />,
+      labelCallback: () => I18n.t('Copy To...'),
+      selectionCallback: props.onCopy
+    })
+  }
+  return options
+}
+
+const renderMenuItem = ({selectionCallback, icon, labelCallback, key}) => {
+  return (
+    <Menu.Item key={key} onSelect={selectionCallback}>
+      <Flex>
+        <Flex.Item>{icon}</Flex.Item>
+        <Flex.Item padding="0 0 0 xx-small">
+          <Text>{labelCallback.call()}</Text>
+        </Flex.Item>
+      </Flex>
+    </Menu.Item>
+  )
+}
+
+PostToolbar.propTypes = {
+  /**
+   * Behavior for marking the thread as read
+   */
+  onReadAll: PropTypes.func.isRequired,
+  /**
+   * Behavior for deleting the discussion post.
+   * Providing this function will result in the menu option being rendered.
+   */
+  onDelete: PropTypes.func,
+  /**
+   * Behavior for toggling the ability to comment on the post.
+   * Providing this function will result in the menu option being rendered.
+   */
+  onToggleComments: PropTypes.func,
+  /**
+   * Indicates whether comments have been enabled or not.
+   * Which toggling menu option is rendered is dependent on this prop.
+   */
+  commentsEnabled: PropTypes.bool,
+  /**
+   * Behavior for sending to a recipient.
+   * Providing this function will result in the menu option being rendered.
+   */
+  onSend: PropTypes.func,
+  /**
+   * Behavior for copying a post.
+   * Providing this function will result in the menu option being rendered.
+   */
+  onCopy: PropTypes.func,
+  /**
+   * Behavior for editing a post.
+   * Providing this function will result in the button being rendered.
+   */
+  onEdit: PropTypes.func,
+  /**
+   * Behavior for toggling the published state of the post.
+   * Providing this function will result in the button being rendered.
+   */
+  onTogglePublish: PropTypes.func,
+  /**
+   * Indicates whether the post is published, publishing, or unpublished.
+   * Which state the publish button is in is dependent on this prop.
+   */
+  publishState: PropTypes.oneOf(['published', 'publishing', 'unpublished', 'unpublishing'])
+}
+
+export default PostToolbar
---
+++ app/jsx/discussion_posts/PostToolbar/__tests__/PostToolbar.test.js
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {render, fireEvent} from '@testing-library/react'
+import React from 'react'
+import {PostToolbar} from '../PostToolbar'
+
+const setup = props => {
+  return render(<PostToolbar onReadAll={Function.prototype} {...props} />)
+}
+
+describe('PostToolbar', () => {
+  describe('publish button', () => {
+    it('does not display if callback is not provided', () => {
+      const {queryByText} = setup()
+      expect(queryByText('Published')).toBeFalsy()
+    })
+
+    it('displays if callback is provided', () => {
+      const onTogglePublishMock = jest.fn()
+      const {getByText} = setup({
+        onTogglePublish: onTogglePublishMock,
+        publishState: 'published'
+      })
+      expect(onTogglePublishMock.mock.calls.length).toBe(0)
+      fireEvent.click(getByText('Published'))
+      expect(onTogglePublishMock.mock.calls.length).toBe(1)
+    })
+  })
+
+  describe('edit button', () => {
+    it('does not display if callback is not provided', () => {
+      const {queryByText} = setup()
+      expect(queryByText('Edit')).toBeFalsy()
+    })
+
+    it('displays if callback is provided', () => {
+      const onEditMock = jest.fn()
+      const {queryByText, getByText} = setup({onEdit: onEditMock})
+      expect(queryByText('Edit')).toBeTruthy()
+      expect(onEditMock.mock.calls.length).toBe(0)
+      fireEvent.click(getByText('Edit'))
+      expect(onEditMock.mock.calls.length).toBe(1)
+    })
+  })
+
+  describe('menu options', () => {
+    describe('mark all as read', () => {
+      it('calls provided callback when clicked', () => {
+        const onReadAllMock = jest.fn()
+        const {getByTestId, getByText} = setup({onReadAll: onReadAllMock})
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(onReadAllMock.mock.calls.length).toBe(0)
+        fireEvent.click(getByText('Mark All as Read'))
+        expect(onReadAllMock.mock.calls.length).toBe(1)
+      })
+    })
+
+    describe('delete', () => {
+      it('does not render if the callback is not provided', () => {
+        const {queryByText, getByTestId} = setup()
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(queryByText('Delete')).toBeFalsy()
+      })
+
+      it('calls provided callback when clicked', () => {
+        const onDeleteMock = jest.fn()
+        const {getByTestId, getByText} = setup({onDelete: onDeleteMock})
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(onDeleteMock.mock.calls.length).toBe(0)
+        fireEvent.click(getByText('Delete'))
+        expect(onDeleteMock.mock.calls.length).toBe(1)
+      })
+    })
+
+    describe('toggle comments', () => {
+      it('does not render if the callback is not provided', () => {
+        const {queryByText, getByTestId} = setup()
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(queryByText('Close for Comments')).toBeFalsy()
+        expect(queryByText('Open for Comments')).toBeFalsy()
+      })
+
+      describe('comments are currently enabled', () => {
+        it('renders correct display text', () => {
+          const onToggleCommentsMock = jest.fn()
+          const {queryByText, getByTestId} = setup({
+            onToggleComments: onToggleCommentsMock,
+            commentsEnabled: true
+          })
+          fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+          expect(queryByText('Close for Comments')).toBeTruthy()
+          expect(queryByText('Open for Comments')).toBeFalsy()
+        })
+
+        it('calls provided callback when clicked', () => {
+          const onToggleCommentsMock = jest.fn()
+          const {getByTestId, getByText} = setup({
+            onToggleComments: onToggleCommentsMock,
+            commentsEnabled: true
+          })
+          fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+          expect(onToggleCommentsMock.mock.calls.length).toBe(0)
+          fireEvent.click(getByText('Close for Comments'))
+          expect(onToggleCommentsMock.mock.calls.length).toBe(1)
+        })
+      })
+
+      describe('comments are currently disabled', () => {
+        it('renders correct display text', () => {
+          const onToggleCommentsMock = jest.fn()
+          const {queryByText, getByTestId} = setup({
+            onToggleComments: onToggleCommentsMock,
+            commentsEnabled: false
+          })
+          fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+          expect(queryByText('Open for Comments')).toBeTruthy()
+          expect(queryByText('Close for Comments')).toBeFalsy()
+        })
+
+        it('calls provided callback when clicked', () => {
+          const onToggleCommentsMock = jest.fn()
+          const {getByTestId, getByText} = setup({
+            onToggleComments: onToggleCommentsMock,
+            commentsEnabled: false
+          })
+          fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+          expect(onToggleCommentsMock.mock.calls.length).toBe(0)
+          fireEvent.click(getByText('Open for Comments'))
+          expect(onToggleCommentsMock.mock.calls.length).toBe(1)
+        })
+      })
+    })
+
+    describe('send to', () => {
+      it('does not render if the callback is not provided', () => {
+        const {queryByText, getByTestId} = setup()
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(queryByText('Sent To...')).toBeFalsy()
+      })
+
+      it('calls provided callback when clicked', () => {
+        const onSendMock = jest.fn()
+        const {getByTestId, getByText} = setup({onSend: onSendMock})
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(onSendMock.mock.calls.length).toBe(0)
+        fireEvent.click(getByText('Send To...'))
+        expect(onSendMock.mock.calls.length).toBe(1)
+      })
+    })
+
+    describe('copy to', () => {
+      it('does not render if the callback is not provided', () => {
+        const {queryByText, getByTestId} = setup()
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(queryByText('Copy To...')).toBeFalsy()
+      })
+
+      it('calls provided callback when clicked', () => {
+        const onCopyMock = jest.fn()
+        const {getByTestId, getByText} = setup({onCopy: onCopyMock})
+        fireEvent.click(getByTestId('discussion-post-menu-trigger'))
+        expect(onCopyMock.mock.calls.length).toBe(0)
+        fireEvent.click(getByText('Copy To...'))
+        expect(onCopyMock.mock.calls.length).toBe(1)
+      })
+    })
+  })
+})
