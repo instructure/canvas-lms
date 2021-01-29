a8d1f5ee85819b82bf6a1dc4ea03b3dd6707140b
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-01-29 14:59:53 -0700

    recent activity publish as v2

--- app/models/enrollment/recent_activity.rb
+++ app/models/enrollment/recent_activity.rb
@@ -55,7 +55,7 @@ class Enrollment
       result = all_enrollments_scope.update_all(options)
       
       all_enrollments_scope.ids.each do |id|
-        PipelineService.publish(Enrollment.new(id: id))
+        PipelineService.publish_as_v2(Enrollment.new(id: id))
       end
 
       result
