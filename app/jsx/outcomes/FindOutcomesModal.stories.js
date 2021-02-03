82792c5e86424e77203c1ca17dba816f91fb16b5
Pat Renner <prenner@instructure.com>
2021-02-03 14:48:30 -0600

    add mocks for the 'Find' modal in storybook
    refs OUT-4016
    flag=improved_outcomes_management
    
    test-plan:
    - run storybook
    > yarn storybook
    - go to Outcomes > FindOutcomeModal
    - verify there are 101 groups within Account Standards
    when isCourse is true
    - verify there are 100 groups when isCourse is false
    - verify that when `includeGlobalRootGroup` is true and
    isCourse is false, you see a 'State Standards' folder
    - verify you don't see this folder when isCourse is true
    (we don't show the State Standards folder when viewing
    within a course)
    - visit the 'Query' tab
      - verify changing the childGroupsCount for one of the root
    account groups updates the count within the
    'Account Standards' folder
    
    Change-Id: I4203df60c8b458e8a061573d2e6597f560bceeb4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/258092
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Brewer-Davis <mbd@instructure.com>
    Reviewed-by: Brian Watson <bwatson@instructure.com>
    QA-Review: Brian Watson <bwatson@instructure.com>
    Product-Review: Michael Brewer-Davis <mbd@instructure.com>

--- app/jsx/outcomes/FindOutcomesModal.stories.js
+++ app/jsx/outcomes/FindOutcomesModal.stories.js
@@ -17,17 +17,55 @@
  */
 
 import React from 'react'
+import {MockedProvider} from '@apollo/react-testing'
 import FindOutcomesModal from './FindOutcomesModal'
+import {createCache} from 'jsx/canvas-apollo'
+import OutcomesContext from './contexts/OutcomesContext'
+import {findModalMocks} from './__tests__/mocks'
 
 export default {
   title: 'Examples/Outcomes/FindOutcomesModal',
   component: FindOutcomesModal,
   args: {
     open: true,
-    onCloseHandler: () => {}
-  }
+    onCloseHandler: () => {},
+    isCourse: false,
+    includeGlobalRootGroup: false
+  },
+  // Adding a story decorator will cause the component to re-render when a control change
+  //  is presented. This is necessary becuase useEffect is only called once upon initial render
+  //  and would otherwise not adjust the story when controls are modified
+  decorators: [Story => <Story />]
 }
 
-const Template = args => <FindOutcomesModal {...args} />
+const Template = args => {
+  window.ENV = {}
+  // we only show the global root group for the account context
+  if (args.includeGlobalRootGroup && !args.isCourse) {
+    window.ENV = {
+      GLOBAL_ROOT_OUTCOME_GROUP_ID: 1
+    }
+  }
+  const response = findModalMocks(args)
+  if (args.response) {
+    // Overwrite the result data if it's provided
+    response[0].result.data = args.response
+  }
+  return (
+    <OutcomesContext.Provider
+      value={{env: {contextType: args.isCourse ? 'Course' : 'Account', contextId: '1'}}}
+    >
+      <MockedProvider mocks={response} cache={createCache()}>
+        <FindOutcomesModal {...args} />
+      </MockedProvider>
+    </OutcomesContext.Provider>
+  )
+}
 
 export const Default = Template.bind({})
+
+export const Query = Template.bind({})
+Query.args = {
+  // Allow control of only the result data
+  response: findModalMocks()[0].result.data
+}
