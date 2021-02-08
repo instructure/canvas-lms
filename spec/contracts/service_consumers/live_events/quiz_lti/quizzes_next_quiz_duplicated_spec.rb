cca5710c7bfafd2536f43760d65736848b2a3dfd
Brian Watson <bwatson@instructure.com>
2021-02-08 14:29:45 -0700

    spec: re-enable live events contract tests
    refs INTEROP-6100
    refs OUT-4041
    
    flag=none
    
    Test-plan:
     - retrigger a couple of times, contract tests pass
    
    Change-Id: I0930d73d41dba8561a9ea578bbd0c0f09ada7362
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/258359
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    Reviewed-by: Ryan Norton <rnorton@instructure.com>
    Product-Review: Ryan Norton <rnorton@instructure.com>
    QA-Review: Brian Watson <bwatson@instructure.com>

--- spec/contracts/service_consumers/live_events/quiz_lti/quizzes_next_quiz_duplicated_spec.rb
+++ spec/contracts/service_consumers/live_events/quiz_lti/quizzes_next_quiz_duplicated_spec.rb
@@ -31,8 +31,6 @@ RSpec.describe 'Canvas LMS Live Events', :pact_live_events do
     end
 
     it 'keeps the contract' do
-      skip "INTEROP-6100 (11/5/2020)" unless run_live_events_specs?
-
       live_event.emit_with do
         # arrange
         params = {
