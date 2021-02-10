instructure/canvas-lms

commit 9bf36d129af82e1253e77629d3939bf4a298005b

Scale scoreGiven to scoreMaximum on manual edits
Closes PLAT-6333
flag=none

Test Plan:
1. Create an assignment associated with an LTI 1.3 tool
   with a non-zero points_possible
2. From the tool, use the AGS scores endpoint to create a submission
   for a student in the assignment. Be sure to set the scoreGiven and
   scoreMaximum. The scoreMaximum should not be equal to the assignments
   points_possible.
3. Use the AGS results endpoint to fetch the result of the score.
   Verify the resultScore and resultMaximum match the scoreMaximum
   and scoreGiven values from step 2.
4. In speedgrader, manually update the score to a new value
5. From the tool, fetch the result again. Verify the scoreGiven
   has been changed and scaled to the scoreMaximum. Example:

   Assignment has 5 points possible

   In step 2, the score posted had the following:
     scoreGiven: 0.5
     scoreMaximum: 1

   The gradebook would show a score of 2.5

   In step 4, the score is changed in the speedgrader to
   "4"

   In step 5, the resultGiven should eual "0.8" (The 80% of the
   4/5 score is preserved)
6. Verify the scoreMaximum has not changed
7. Repeat step 2 with the original vaules
8. Verify that gradebook shows the score from step 7, not from
   step 4.
9. Use the AGS results endpoint to fetch the result of the score
   and verify it matches the value send in step 7.

Change-Id: Ic1db1ef0a1b058b2bda2958e67a2ba689236eb4d
Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/252998
Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
Product-Review: Weston Dransfield <wdransfield@instructure.com>
Reviewed-by: Mysti Lilla <mysti@instructure.com>
QA-Review: Evan Battaglia <ebattaglia@instructure.com>

app/models/lti/result.rb
@@ -44,11 +44,43 @@ class Lti::Result < ApplicationRecord
   belongs_to :user, inverse_of: :lti_results
   belongs_to :line_item, inverse_of: :results, foreign_key: :lti_line_item_id, class_name: 'Lti::LineItem'
   belongs_to :root_account, class_name: 'Account'
+  has_one :assignment, through: :submission
 
   before_save :set_root_account
 
+  # Returns the result_score scaled to the
+  # result_maximum. This is required because
+  # a user can update a score manually in the UI.
+  #
+  # In the future it may be worthwhile to persist
+  # the scaled score on the result in an after_save
+  # callback on submission. Doing so would reuquire
+  # working out some performance issues.
+  def scaled_result_score
+    raw_result_score = read_attribute(:result_score)
+
+    return raw_result_score if raw_result_score.blank? || submission.blank?
+
+    # A negative grader_id indicates that no manual
+    # adjustments were made by a Canvas user to the result.
+    # If that's the case, we can just return the result_score
+    # without additional scaling
+    return raw_result_score if submission.grader_id.blank? || submission.grader_id < 0
+
+    # We can also return the result_score if the assignment
+    # has zero points possible
+    return raw_result_score if assignment.points_possible&.zero?
+
+    # The result was manually updated by a Canvas user.
+    # Because the result_maximum may not be the same as the
+    # assignment's points possible, we need to scale the
+    # result_score to the result_maximum
+    (raw_result_score * result_maximum) / assignment.points_possible.to_f
+  end
+  alias result_score scaled_result_score
+
   private
-  
+
   def set_root_account
     self.root_account_id ||= self.submission&.root_account_id || self.line_item&.root_account_id
   end

spec/models/lti/result_spec.rb
@@ -21,7 +21,7 @@
 require_relative '../../spec_helper'
 
 RSpec.describe Lti::Result, type: :model do
-  let_once(:assignment) { assignment_model }
+  let_once(:assignment) { assignment_model(points_possible: 5) }
 
   context 'when validating' do
     let(:result) { lti_result_model assignment: assignment }
@@ -60,6 +60,51 @@ RSpec.describe Lti::Result, type: :model do
         ActiveRecord::RecordInvalid,
         "Validation failed: Grading progress is not included in the list"
       )
+    end
+
+    describe '#scaled_result_score' do
+      subject { result.scaled_result_score }
+
+      let(:tool_id) { 1 }
+      let(:manual_grader_id) { 2 }
+
+      let(:result) do
+        lti_result_model(
+          assignment: assignment,
+          result_maximum: 1,
+          result_score: 0.5,
+        )
+      end
+
+      before { result.submission.update!(grader_id: tool_id * -1) }
+
+      context 'when the score_given has not been manually changed' do
+        it { is_expected.to eq result.result_score }
+      end
+
+      context 'when result_score is blank' do
+        before { result.update!(result_score: 0) }
+
+        it { is_expected.to eq result.result_score }
+      end
+
+      context 'when the submission is blank' do
+        before { result.update!(submission: nil) }
+
+        it { is_expected.to eq result.result_score }
+      end
+
+      context 'when the score_given was manually updated by a user' do
+        before { result.submission.update!(grader_id: 2, score: 4) }
+
+        it { is_expected.to eq 0.1 }
+
+        context 'when the assignment has 0 points_possible' do
+          before { assignment.update!(points_possible: 0) }
+
+          it { is_expected.to eq result.result_score }
+        end
+      end
     end
 
     describe '#result_maximum' do

app/controllers/lti/ims/results_controller.rb
@@ -76,7 +76,7 @@ module Lti::Ims
     def index
       render(json: [], content_type: MIME_TYPE) and return if user.present? && !context.user_is_student?(user)
 
-      results = Lti::Result.active.where(line_item: line_item)
+      results = Lti::Result.active.where(line_item: line_item).preload(:assignment, :submission)
       results = results.where(user: user).preload(:user) if params.key?(:user_id)
       results = Api.paginate(results, self, "#{line_item_url}/results", pagination_args)
       render json: results_collection(results), content_type: MIME_TYPE

spec/controllers/lti/ims/results_controller_spec.rb
@@ -27,7 +27,7 @@ describe Lti::Ims::ResultsController do
   include_context 'advantage services context'
 
   let(:assignment) do
-    opts = {course: course}
+    opts = {course: course, points_possible: 5}
     if tool.present?
       opts[:submission_types] = 'external_tool'
       opts[:external_tool_tag_attributes] = {
@@ -40,7 +40,6 @@ describe Lti::Ims::ResultsController do
   end
   let(:context) { course }
   let(:unknown_context_id) { (Course.maximum(:id) || 0) + 1 }
-  let(:result) { lti_result_model assignment: assignment, line_item: assignment.line_items.first }
   let(:json) { JSON.parse(response.body) }
   let(:params_overrides) do
     {
@@ -50,11 +49,27 @@ describe Lti::Ims::ResultsController do
   end
   let(:scope_to_remove) { 'https://purl.imsglobal.org/spec/lti-ags/scope/result.readonly' }
 
+  let(:result) do
+    lti_result_model(
+      assignment: assignment,
+      line_item: assignment.line_items.first,
+      result_score: 0.5,
+      result_maximum: 1
+    )
+  end
+
   describe '#index action' do
     let(:action) { :index }
 
     before do
-      3.times { lti_result_model line_item: result.line_item, assignment: assignment }
+      3.times do
+        lti_result_model(
+          line_item: result.line_item,
+          assignment: assignment,
+          result_score: 0.5,
+          result_maximum: 1
+        )
+      end
     end
 
     it_behaves_like 'advantage services'
@@ -133,6 +148,19 @@ describe Lti::Ims::ResultsController do
         expect(response.headers['Link']).to include 'rel="next"'
       end
     end
+
+    context 'when the score was manually updated' do
+      before do
+        submission_ids = Lti::Result.where(line_item: result.line_item).pluck(:submission_id)
+        Submission.find(submission_ids).each { |s| s.update!(grader_id: 1) }
+      end
+
+      it 'scales the resultScore to the resultMaximum' do
+        send_request
+        scaled_result = json.find { |r| r['resultMaximum'] == 1 }
+        expect(scaled_result['resultScore']).to eq 0.1
+      end
+    end
   end
 
   describe '#show' do
@@ -147,10 +175,24 @@ describe Lti::Ims::ResultsController do
       expect(response).to have_http_status :ok
     end
 
+    it 'includes the scoreMaximum' do
+      send_request
+      expect(json['resultScore']).to eq 0.5
+    end
+
     it 'formats the result correctly' do
       send_request
       rslt = Lti::Result.find(json['id'].split('/').last.to_i)
       expect(rslt).to eq result
+    end
+
+    context 'when the score was manually updated' do
+      before { result.submission.update!(grader_id: 1) }
+
+      it 'scales the resultScore to the resultMaximum' do
+        send_request
+        expect(json['resultScore']).to eq 0.1
+      end
     end
 
     context 'when result requested not in line_item' do

commit a31ae12824de46d4a7b5017736ad4e911ba5881b

Fix 500 in Lti::Result set by speedgrader
Fixes 500 error introduced in 9bf36d12. The problem occurs when
speedgrader is used to grade a Lti::Result which has been created from
AGS with no resultScore or resultMaximum.

This commit fixes the issue in two ways:

1. ensuring that when we set a grade in speedgrader, we set a
result_maxmimum if on the Lti::Result if one is not set, using the
assignment's points_possible. This is done in SQL because the original
code which used models was apparently explicitly changed to an
`update_all` for performance reasons (preventing inadvertent loads --
see 3ceaceab).

2. for previously created Lti::Result records which may have the issue
of result_score but no result_maximum, scaled_result_score now returns
the raw score (essentially the same as inferring the result_maximum as
above) instead of crashing.

This commit also makes the validation slightly more efficient by not
needlessly calling scaled_result_score().

closes INTEROP-6497
flag=none

Test plan:
1. Create an assignmeent associated with an LTI 1.3 tool
2. Ensure that the validation still works by using the AGS scores
  endpoint to create a submission with a scoreGiven but no scoreMaximum.
  It should fail.
2. Use the AGS scores endpoint to create a submission with no scoreGiven
   and no scoreMaximum.
3. Set a score in speed grader.
4. Use the AGS results endpoint to fetch the result of the score. No 500
  error should be thrown, and the score returned should be the raw score.
  The result maxmium should be the assignment's points_possible.
5. Manually set the lti result's result_maxmimum in the database
  (bypassing validations e.g. with update_all). Check the AGS results
  endpoint again. No 500 error should be thrown, the score returned should
  be the raw score, and result maximum should be null.
6. Follow the test plan given in 9bf36d12 to make sure that
  functionality still works.

Change-Id: I64d8cc0fffc4f1a4845038e5e80e0224d65fbcc2

app/models/lti/result.rb
@@ -30,7 +30,7 @@ class Lti::Result < ApplicationRecord
   self.record_timestamps = false
 
   validates :line_item, :user, presence: true
-  validates :result_maximum, presence: true, unless: proc { |r| r.result_score.blank? }
+  validates :result_maximum, presence: true, unless: proc { |r| r.read_attribute(:result_score).blank? }
   validates :result_score, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :result_maximum, numericality: { greater_than: 0 }, allow_nil: true
   validates :activity_progress,
@@ -59,7 +59,7 @@ class Lti::Result < ApplicationRecord
   def scaled_result_score
     raw_result_score = read_attribute(:result_score)
 
-    return raw_result_score if raw_result_score.blank? || submission.blank?
+    return raw_result_score if raw_result_score.blank? || submission.blank? || result_maximum.blank?
 
     # A negative grader_id indicates that no manual
     # adjustments were made by a Canvas user to the result.
@@ -69,7 +69,7 @@ class Lti::Result < ApplicationRecord
 
     # We can also return the result_score if the assignment
     # has zero points possible
-    return raw_result_score if assignment.points_possible&.zero?
+    return raw_result_score if assignment.points_possible.to_f.zero?
 
     # The result was manually updated by a Canvas user.
     # Because the result_maximum may not be the same as the
@@ -78,6 +78,27 @@ class Lti::Result < ApplicationRecord
     (raw_result_score * result_maximum) / assignment.points_possible.to_f
   end
   alias result_score scaled_result_score
+
+  # Updates score for submission safely (does not allow maximum score to be null, but
+  # if it's not already set, it will set it to assignment's points_possible)
+  def self.update_score_for_submission(submission, score)
+    update_query = <<~SQL
+      UPDATE #{Lti::Result.quoted_table_name} SET
+        result_score = #{connection.quote(score)},
+        result_maximum = COALESCE(
+          result_maximum,
+          (
+            SELECT assignments.points_possible
+            FROM #{Assignment.quoted_table_name} AS assignments,
+            #{Submission.quoted_table_name} AS submissions
+            WHERE assignments.id=submissions.assignment_id
+            AND submissions.id=#{submission.id.to_i}
+          ),
+          0)
+      WHERE submission_id=#{submission.id.to_i}
+    SQL
+    connection.execute(update_query)
+  end
 
   private
 

app/models/submission.rb
@@ -2439,7 +2439,7 @@ class Submission < ActiveRecord::Base
 
   def update_line_item_result
     return unless saved_change_to_score?
-    Lti::Result.where(submission: self).update_all(result_score: score)
+    Lti::Result.update_score_for_submission(self, score)
   end
 
   def delete_ignores

spec/models/lti/result_spec.rb
@@ -79,31 +79,76 @@ RSpec.describe Lti::Result, type: :model do
       before { result.submission.update!(grader_id: tool_id * -1) }
 
       context 'when the score_given has not been manually changed' do
-        it { is_expected.to eq result.result_score }
+        it { is_expected.to eq result.read_attribute(:result_score) }
       end
 
       context 'when result_score is blank' do
         before { result.update!(result_score: 0) }
 
-        it { is_expected.to eq result.result_score }
+        it { is_expected.to eq result.read_attribute(:result_score) }
       end
 
       context 'when the submission is blank' do
         before { result.update!(submission: nil) }
 
-        it { is_expected.to eq result.result_score }
+        it { is_expected.to eq result.read_attribute(:result_score) }
       end
 
       context 'when the score_given was manually updated by a user' do
-        before { result.submission.update!(grader_id: 2, score: 4) }
+        before do
+          result.submission.update!(grader_id: 2, score: 4)
+          result.reload
+        end
 
-        it { is_expected.to eq 0.1 }
+        it { is_expected.to eq 0.8 }
 
         context 'when the assignment has 0 points_possible' do
           before { assignment.update!(points_possible: 0) }
 
-          it { is_expected.to eq result.result_score }
+          it { is_expected.to eq result.read_attribute(:result_score) }
         end
+      end
+
+      context 'when the result_score/result_maximum were originally null but the score was manually updated by a user' do
+        before do
+          result.update(result_maximum: nil, result_score: nil)
+          result.submission.update!(grader_id: 2, score: 4)
+          result.reload
+        end
+
+        it 'returns the raw score' do
+          expect(subject).to eq(4)
+        end
+      end
+
+      context 'when result_maximum is nil but result_score is not (possible through speedgrader)' do
+        # This happens when the original result has no result_maximum or result_score but grading
+        # the submission in speedgrader gives it a result_score. Tests the same
+        # as above but more of a unit test because it doesn't test the behavior
+        # of updating the submission.
+        # This should not be possible anymore (see update_score_for_submission)
+        # but there still may be old records in the database which have this
+        # issue
+        before do
+          result.submission.update!(grader_id: tool_id)
+          described_class.where(id: result.id).update_all(result_maximum: nil, result_score: 0.75)
+          result.reload
+        end
+
+        it 'returns the raw score' do
+          expect(subject).to eq(0.75)
+        end
+      end
+
+      context 'when the assignment points_possible is nil' do
+        # I don't know if this is possible but the old code implied it might be
+        before do
+          result.submission.update!(grader_id: tool_id)
+          assignment.class.where(id: assignment.id).update_all(points_possible: nil)
+          result.reload
+        end
+
+        it { is_expected.to eq result.read_attribute(:result_score) }
       end
     end
 
@@ -212,6 +257,31 @@ RSpec.describe Lti::Result, type: :model do
       submission.assignment.root_account_id = nil
       result = Lti::Result.create!(line_item: line_item_model, user: user_model, created_at: Time.zone.now, updated_at: Time.zone.now, submission: submission)
       expect(result.root_account_id).to eq result.line_item.root_account_id
+    end
+  end
+
+  describe '.update_score_for_submission' do
+    let(:result) { lti_result_model assignment: assignment }
+
+    context 'when result maximum is null' do
+      it "sets and result maxmium to the assignment's points_possible, and sets the score" do
+        expect(assignment.points_possible).to_not eq(nil)
+        expect(result.result_maximum).to eq(nil)
+        Lti::Result.update_score_for_submission(result.submission, 123)
+        result.reload
+        expect(result.result_maximum).to eq(assignment.points_possible)
+        expect(result.result_score).to eq(123)
+      end
+    end
+
+    context 'when result maximum is not null' do
+      it 'sets the score and leaves the score_maximum untouched' do
+        result.update!(result_score: 8, result_maximum: 88)
+        Lti::Result.update_score_for_submission(result.submission, 123)
+        result.reload
+        expect(result.result_maximum).to eq(88)
+        expect(result.result_score).to eq(123)
+      end
     end
   end
 end