00f1f0e9278031da0be27edc9c5aec31a818c3b6
Brian Watson <bwatson@instructure.com>
2021-02-10 12:27:55 -0700

    Add high contrast to storybook
    closes OUT-4252
    
    flag=none
    
    Test-plan: - Click user icon in global toolbar
     - Switch between 'canvas' and 'canvas-high-contrast'
     - Verify that components display in expected contrast
     - Verify that all stories are rendering as expected and that
       'canvas' theme option displays stories as they were before
       this PS
    Change-Id: I0d139e228ee01f2c3bc45d6b20e69acce88cfae1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/258545
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Manoel Quirino <manoel.quirino@instructure.com>
    Reviewed-by: Michael Brewer-Davis <mbd@instructure.com>
    QA-Review: Chrystal Langston <chrystal.langston@instructure.com>
    Product-Review: Jody Sailor

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,31 @@
+import React from 'react'
+import {ApplyTheme} from '@instructure/ui-themeable'
+import '@instructure/canvas-high-contrast-theme'
+import '@instructure/canvas-theme'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-import '@instructure/canvas-theme'
+export const globalTypes = {
+  canvasTheme: {
+    name: 'Canvas Theme',
+    description: 'Default or High Contrast',
+    defaultValue: 'canvas',
+    toolbar: {
+      icon: 'user',
+      items: ['canvas', 'canvas-high-contrast']
+    }
+  }
+}
+
+const canvasThemeProvider = (Story, context) => {
+  const canvasTheme = context.globals.canvasTheme
+  return (
+    <ApplyTheme theme={ApplyTheme.generateTheme(canvasTheme)}>
+      <Story {...context}/>
+    </ApplyTheme>
+  )
+}
+
+export const decorators = [canvasThemeProvider]
