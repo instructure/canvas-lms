94ad92fdb954b74f02cd5f8fe891f67ac871fdee
Ethan Vizitei <evizitei@instructure.com>
2021-02-26 12:21:58 -0600

    extract LoginRegistry from Canvas::Security
    refs FOO-1648
    flag=none
    
    These login audits are a group of functionality
    very specific to the pseudonym login case,
    and they know a few things about the pseudonym model.
    
    Moving it into it's own module makes sense from
    and organizational standpoint, but also makes it
    more reasonable to extract the rest of canvas_security
    into a gem for other gems/engines to depend on.
    
    TEST PLAN:
      1) the pseudonym login process remains unchanged
      2) specs pass
    
    Change-Id: I8f689c9255c33f0ac4d7947e3b0a0592d7900463
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/259557
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Product-Review: Simon Williams <simon@instructure.com>

--- spec/lib/canvas/security_spec.rb
+++ spec/lib/canvas/security_spec.rb
@@ -190,51 +190,6 @@ describe Canvas::Security do
     end
   end
 
-  if Canvas.redis_enabled?
-    describe "max login attempts" do
-      before do
-        Setting.set('login_attempts_total', '2')
-        Setting.set('login_attempts_per_ip', '1')
-        u = user_with_pseudonym :active_user => true,
-          :username => "nobody@example.com",
-          :password => "asdfasdf"
-        u.save!
-        @p = u.pseudonym
-      end
-
-      it "should be limited for the same ip" do
-        expect(Canvas::Security.allow_login_attempt?(@p, "5.5.5.5")).to eq true
-        Canvas::Security.failed_login!(@p, "5.5.5.5")
-        expect(Canvas::Security.allow_login_attempt?(@p, "5.5.5.5")).to eq false
-      end
-
-      it "should have a higher limit for other ips" do
-        Canvas::Security.failed_login!(@p, "5.5.5.5")
-        expect(Canvas::Security.allow_login_attempt?(@p, "5.5.5.6")).to eq true
-        Canvas::Security.failed_login!(@p, "5.5.5.7")
-        expect(Canvas::Security.allow_login_attempt?(@p, "5.5.5.8")).to eq false # different ip but too many total failures
-        expect(Canvas::Security.allow_login_attempt?(@p, nil)).to eq false # no ip but too many total failures
-      end
-
-      it "should not block other users with the same ip" do
-        Canvas::Security.failed_login!(@p, "5.5.5.5")
-        # schools like to NAT hundreds of people to the same IP, so we don't
-        # ever block the IP address as a whole
-        u2 = user_with_pseudonym(:active_user => true, :username => "second@example.com", :password => "12341234")
-        u2.save!
-        expect(Canvas::Security.allow_login_attempt?(u2.pseudonym, "5.5.5.5")).to eq true
-        expect(Canvas::Security.allow_login_attempt?(u2.pseudonym, "5.5.5.6")).to eq true
-      end
-
-      it "should timeout the login block after a waiting period" do
-        Setting.set('login_attempts_ttl', 5.seconds)
-        Canvas::Security.failed_login!(@p, "5.5.5.5")
-        expect(Canvas::Security.time_until_login_allowed(@p, '5.5.5.6')).to eq 0
-        expect(Canvas::Security.time_until_login_allowed(@p, '5.5.5.5')).to be <= 5
-      end
-    end
-  end
-
   describe '.config' do
     before { described_class.instance_variable_set(:@config, nil) }
     after  { described_class.instance_variable_set(:@config, nil) }
