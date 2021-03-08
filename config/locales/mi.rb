50dc007afde85312367cf0763da1d538efb089cb
Jacob Burroughs <jburroughs@instructure.com>
2021-03-08 11:44:18 -0600

    Add ordinal definitions
    These aren't quite perfect (in particular rails offers no mechanism to specify gender,
    so currently these use whatever is the "default" ordinalization for the language),
    but they should allow us to use `number.ordinalize` for things like maintenance windows.
    
    Change-Id: I014ec1b6aa1fecad2ed079bde7b78f8a164b8dd9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/260137
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Jacob Burroughs <jburroughs@instructure.com>

---
+++ config/locales/mi.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# From what I can gather, in Maori regular numerals are used as ordinals
+{
+  he: {
+    number: {
+      nth: {
+        ordinals: lambda do |_key, _options|
+          ''
+        end,
+
+        ordinalized: lambda do |_key, options|
+          options[:number]
+        end
+      }
+    }
+  }
+}
