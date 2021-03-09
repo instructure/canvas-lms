5ac327b0ccba37ed87b2228dd3bf11739cc1a6d7
Caleb Guanzon <cguanzon@instructure.com>
2021-03-09 12:05:35 -0700

    add discussion_posts NoResultsFound component
    flag=none
    fixes VICE-1208
    
    test plan:
    - yarn storybook
    - visit Discussion Posts/NoResultsFound
    - verify you see the NoResultsFound message like the one
       in prod
    
    Change-Id: Ib577d21066905fd9f4c4e285177648b5a2d04400
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/260283
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Matthew Lemon <mlemon@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ app/jsx/discussion_posts/components/NoResultsFound/NoResultsFound.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import I18n from 'i18n!conversations_2'
+import {List, Text} from '@instructure/ui-elements'
+import React from 'react'
+
+export const NoResultsFound = () => {
+  return (
+    <>
+      <Text lineHeight="double">{I18n.t('Your search did not match any entries.')}</Text>
+      <br />
+      <Text lineHeight="double">{I18n.t('Suggestions:')}</Text>
+      <List>
+        <List.Item>
+          <Text lineHeight="double">
+            {I18n.t('Make sure all search terms are spelled correctly.')}
+          </Text>
+        </List.Item>
+        <List.Item>
+          <Text lineHeight="double">
+            {I18n.t('Try different, more general, or fewer keywords.')}
+          </Text>
+        </List.Item>
+        <List.Item>
+          <Text lineHeight="double">{I18n.t('Try disabling the "Unread" filter.')}</Text>
+        </List.Item>
+      </List>
+    </>
+  )
+}
