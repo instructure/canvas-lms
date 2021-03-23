a706bec9e5e7586d65b61295cd9d495e7aa475ae
James Carbine <jamescarbine@gmail.com>
2021-03-22 22:12:28 -0600

    Add createTime to bbb conferences.
    fixes VICE-2029
    
    Use conference settings instead of a database migration.
    
    Change-Id: Ib8bdc0c61aba4ef3380bfbc008c75799969e9581
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/273669
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Rob Orton <rob@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/models/big_blue_button_conference.rb
+++ app/models/big_blue_button_conference.rb
@@ -42,6 +42,14 @@ class BigBlueButtonConference < WebConference
     visible: false
   }
 
+  user_setting_field :create_time, {
+    name: -> { t('create_time', 'Create Time') },
+    description: -> { t('Security setting to restrict join URLs to a conference') },
+    type: :integer,
+    default: false,
+    visible: false
+  }
+
   def initiate_conference
     return conference_key if conference_key && !retouch?
 
@@ -58,19 +66,20 @@ class BigBlueButtonConference < WebConference
     settings[:record] &&= config[:recording_enabled]
     settings[:domain] ||= config[:domain] # save the domain
     current_host = URI(settings[:default_return_url] || "http://www.instructure.com").host
-    send_request(:create, {
-                   :meetingID => conference_key,
-                   :name => title,
-                   :voiceBridge => format("%020d", self.global_id),
-                   :attendeePW => settings[:user_key],
-                   :moderatorPW => settings[:admin_key],
-                   :logoutURL => (settings[:default_return_url] || "http://www.instructure.com"),
-                   :record => settings[:record] ? "true" : "false",
-                   :welcome => settings[:record] ? t("This conference may be recorded.") : "",
-                   "meta_canvas-recording-ready-user" => recording_ready_user,
-                   "meta_canvas-recording-ready-url" => recording_ready_url(current_host)
-                 }) or return nil
+    response = send_request(:create, {
+                              :meetingID => conference_key,
+                              :name => title,
+                              :voiceBridge => format("%020d", global_id),
+                              :attendeePW => settings[:user_key],
+                              :moderatorPW => settings[:admin_key],
+                              :logoutURL => (settings[:default_return_url] || "http://www.instructure.com"),
+                              :record => settings[:record] ? "true" : "false",
+                              :welcome => settings[:record] ? t("This conference may be recorded.") : "",
+                              "meta_canvas-recording-ready-user" => recording_ready_user,
+                              "meta_canvas-recording-ready-url" => recording_ready_url(current_host)
+                            }) or return nil
     @conference_active = true
+    settings[:create_time] = response[:createTime] if response.present?
     save
     conference_key
   end
@@ -207,7 +216,8 @@ class BigBlueButtonConference < WebConference
                      :fullName => user.short_name,
                      :meetingID => conference_key,
                      :password => settings[(type == :user ? :user_key : :admin_key)],
-                     :userID => user.id
+                     :userID => user.id,
+                     :createTime => settings[:create_time]
   end
 
   def end_meeting
