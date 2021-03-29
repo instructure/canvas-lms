6973b4d096203cf18ddcb117bb59bc33c8780afc
Gary Mei <gmei@instructure.com>
2021-03-29 10:25:07 -0500

    show users' annotations for Annotated Document
    This sets the user filter sent to DocViewer to include more than just
    the viewing user's id when the assignment is an Annotated Document.
    
    closes EVAL-1553
    flag=annotated_document_submissions
    
    Test Plan
    - Create an Annotated Document
    - Set it to post manually.
    - As a student, make some annotations and click Submit.
    - As a teacher, navigate to that submission:
      `/courses/:course_id/assignments/:assignment_id/submissions/:user_id`
    - As a teacher, make some annotations. Refresh the page and verify
      the annotations are still there.
    - As the student, view that submission. Verify that the teacher
      annotations do not appear.
    - As the teacher, grade the submission and post it.
    - As the student and the teacher, both users' annotations should be
      visible.
    
    Change-Id: I01bfbc36683ffc9120785df622b60ea98db3d913
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/261978
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Syed Hussain <shussain@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- spec/lib/canvadocs_spec.rb
+++ spec/lib/canvadocs_spec.rb
@@ -738,6 +738,70 @@ describe Canvadocs do
             end
           end
         end
+
+        context 'for an Annotated Document assignment' do
+          before(:each) do
+            assignment.update!(submission_types: 'annotated_document', annotatable_attachment: attachment)
+            assignment.submit_homework(
+              submission.user,
+              submission_type: 'annotated_document',
+              annotated_document_id: attachment.id
+            )
+            submission.reload
+          end
+
+          context 'when the student is viewing' do
+            before(:each) do
+              @current_user = student
+            end
+
+            it 'sets the user_filter to empty if submission is posted' do
+              expect(user_filter).to be_empty
+            end
+
+            it 'sets restrict_annotations_to_user_filter to false if submission is posted' do
+              expect(session_params[:restrict_annotations_to_user_filter]).to be false
+            end
+
+            it 'sets the user_filter to just the student if submission is unposted' do
+              assignment.ensure_post_policy(post_manually: true)
+
+              aggregate_failures do
+                expect(user_filter.length).to be 1
+                expect(user_filter.dig(0, :id)).to eq student.global_id.to_s
+              end
+            end
+
+            it 'sets restrict_annotations_to_user_filter to true if submission is unposted' do
+              assignment.ensure_post_policy(post_manually: true)
+              expect(session_params[:restrict_annotations_to_user_filter]).to be true
+            end
+          end
+
+          context 'when an instructor is viewing' do
+            before(:each) do
+              @current_user = teacher
+            end
+
+            it 'sets the user_filter to empty if submission is posted' do
+              expect(user_filter).to be_empty
+            end
+
+            it 'sets restrict_annotations_to_user_filter to false if submission is posted' do
+              expect(session_params[:restrict_annotations_to_user_filter]).to be false
+            end
+
+            it 'sets the user_filter to empty if submission is unposted' do
+              assignment.ensure_post_policy(post_manually: true)
+              expect(user_filter).to be_empty
+            end
+
+            it 'sets restrict_annotations_to_user_filter to false if submission is unposted' do
+              assignment.ensure_post_policy(post_manually: true)
+              expect(session_params[:restrict_annotations_to_user_filter]).to be false
+            end
+          end
+        end
       end
     end
   end
