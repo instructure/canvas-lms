b4dad3c2a661a70f64dfefff1b772f48d3a981f5
Han Yan <hyan@instructure.com>
2021-03-31 00:16:51 -0500

    retire import_to_quizzes_next FF
    closes QUIZ-8257
    
    flag=quizzes_next
    
    test plan:
    - enable/disable quizzes_next, on Import Content page,
      "Import assessment content as New Quizzes" option will
      be shown/hidden
    
    Change-Id: Ie0b88f1a3940a34512e0627e593258ddf100deb8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/261928
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: Susan Sorensen <susan.sorensen@instructure.com>
    Reviewed-by: Stephen Kacsmark <skacsmark@instructure.com>

--- app/helpers/new_quizzes_features_helper.rb
+++ app/helpers/new_quizzes_features_helper.rb
@@ -20,19 +20,19 @@
 
 module NewQuizzesFeaturesHelper
   def new_quizzes_import_enabled?
-      @context.root_account.feature_allowed?(:quizzes_next) && @context.root_account.feature_enabled?(:import_to_quizzes_next)
+    @context.instance_of?(Course) && @context.feature_allowed?(:quizzes_next)
   end
 
   def new_quizzes_migration_enabled?
-      @context.root_account.feature_allowed?(:quizzes_next) && @context.root_account.feature_enabled?(:new_quizzes_migration)
+    @context.root_account.feature_allowed?(:quizzes_next) && @context.root_account.feature_enabled?(:new_quizzes_migration)
   end
 
   def new_quizzes_import_third_party?
-      @context.root_account.feature_allowed?(:quizzes_next) && @context.root_account.feature_enabled?(:new_quizzes_third_party_imports)
+    @context.root_account.feature_allowed?(:quizzes_next) && @context.root_account.feature_enabled?(:new_quizzes_third_party_imports)
   end
 
   def new_quizzes_migration_default
-      @context.root_account.feature_enabled?(:migrate_to_new_quizzes_by_default) || new_quizzes_require_migration?
+    @context.root_account.feature_enabled?(:migrate_to_new_quizzes_by_default) || new_quizzes_require_migration?
   end
 
   module_function
