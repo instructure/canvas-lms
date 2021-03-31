2b8deecceb4ffc4b1895f5549f987f6c66874dfa
Robin Kuss <rkuss@instructure.com>
2021-03-31 15:21:42 -0500

    moved master_courses rcs specs up one level
    Closes LS-2102
    
    Test Plan:  passes jenkins
    
    Change-Id: I4c66ff7bdcd4940bbd3754a3319a86914f1bcb96
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/262043
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Brian Watson <bwatson@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/master_courses/child_course_quizzes_spec.rb
+++ spec/selenium/master_courses/child_course_quizzes_spec.rb
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require_relative '../../common'
+require_relative '../common'
 
 describe "master courses - child courses - quiz locking" do
   include_context "in-process server selenium tests"
