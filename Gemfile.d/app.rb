32657247bf083f0f6640a0d295b4c49fc1216f34
Cody Cutrer <cody@instructure.com>
2021-04-08 11:41:54 -0600

    bump rqrcode
    to allow ruby 3.0
    
    Change-Id: I1a7b976e54fa978842a80d16e2da4a8c01d40498
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/262499
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -44,7 +44,7 @@ gem 'aws-sdk-sqs', '1.34.0', require: false
 gem 'aws-sdk-kms', '1.39.0', require: false
 gem 'aws-sigv4', '1.2.2', require: false
 gem 'barby', '0.6.8', require: false
-  gem 'rqrcode', '1.1.2', require: false
+  gem 'rqrcode', '1.2.0', require: false
   gem 'chunky_png', '1.3.14', require: false
 gem 'bcrypt', '3.1.16'
 gem 'brotli', '0.2.3', require: false
