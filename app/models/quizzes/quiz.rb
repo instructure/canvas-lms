9ffa3b5452105f2744928a78e955e3a87510d36e
Pat Renner <prenner@instructure.com>
2021-04-22 16:53:55 -0500

    destroy LORs when a quiz changes from an assignment
    closes OUT-4286
    flag=none
    
    test-plan:
    - create a question & question bank with an outcome aligned to it
    - align the question bank to a new quiz and publish the quiz
    - take the quiz as a student
    - verify you have a learning outcome result record for the student
    - edit the quiz type to be a "practice quiz"
    - verify the learning outcome result was soft deleted
    - verify the result doesn't appear in the SLMGB
    - change the quiz type back to assignment
    - verify the result appears in the SLMGB
    
    Change-Id: Ia17360a1ac4508590a19154e824f60194972eff6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/263504
    Product-Review: Augusto Callejas <acallejas@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Brian Watson <bwatson@instructure.com>
    Reviewed-by: Augusto Callejas <acallejas@instructure.com>

--- app/models/quizzes/quiz.rb
+++ app/models/quizzes/quiz.rb
@@ -75,7 +75,10 @@ class Quizzes::Quiz < ActiveRecord::Base
   after_save :clear_availability_cache
   after_save :touch_context
   after_save :regrade_if_published
-
+  # We currently only create LORs for quizzes that are assignments. If we change the quiz_type,
+  # we should destroy or restore the results appropriately
+  after_save :destroy_learning_outcome_results, if: -> { saved_change_to_quiz_type?(from: 'assignment') }
+  after_save :restore_learning_outcome_results, if: -> { saved_change_to_quiz_type?(to: 'assignment') }
   serialize :quiz_data
 
   simply_versioned
@@ -405,6 +408,18 @@ class Quizzes::Quiz < ActiveRecord::Base
     # then this will update the existing submissions to reflect quiz
     # scores in the gradebook.
     self.quiz_submissions.each { |s| s.save! }
+  end
+
+  def update_learning_outcome_results(state)
+    LearningOutcomeResult.for_associated_asset(self).update_all(workflow_state: state, updated_at: Time.zone.now)
+  end
+
+  def destroy_learning_outcome_results
+    delay_if_production.update_learning_outcome_results('deleted')
+  end
+
+  def restore_learning_outcome_results
+    delay_if_production.update_learning_outcome_results('active')
   end
 
   def destroy_related_submissions
