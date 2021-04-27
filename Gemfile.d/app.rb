457f9d2c9e9cc2e1e197cc39b4bd9ccc21d7de41
Cody Cutrer <cody@instructure.com>
2021-04-27 13:00:07 -0600

    add ruby2_keywords gem
    Change-Id: Idd2694370307630a4a4e55a0c6c59143434f2eb5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/263773
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -115,6 +115,7 @@ gem 'retriable', '1.4.1'
 gem 'ritex', '1.0.1', require: false
 gem 'rotp', '6.2.0', require: false
 gem 'ruby-duration', '3.2.3', require: false
+gem 'ruby2_keywords', '0.0.3'
 gem 'rubycas-client', '2.3.9', require: false
 gem 'rubyzip', '2.3.0', require: 'zip'
 gem 'safe_yaml', '1.0.5', require: false
