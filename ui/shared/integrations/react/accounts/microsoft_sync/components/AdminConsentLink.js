58a2bb366283fa64a6d590d71b56dd411daa6f30
Weston Dransfield <wdransfield@instructure.com>
2021-05-04 16:59:09 -0600

    Show admin consent link in account settings
    Closes INTEROP-6713
    flag=microsoft_group_enrollments_syncing
    
    Test Plan
    - Navigate to account settings an enable
      the MSFT sync integration (if it's not already)
    - Enter a valid tenant you have admin rights in.
      For example: canvastest2.onmicrosoft.com (reach
      out to me if you need admin credentials)
    - Verify a message appears at the bottom of the form
      indicating the user should click the link
    - Click the link and verify you are navigated to
      Azure's admin cosent screen for the sync app.
    - Approve the app and verify you are redirected
      to instructure.com
    - Verify the link does not appera in the account
      settings UI if the MSFT sync integration is
      disabled or if the tenant is not entered.
    
    Change-Id: Ie46abf0af3e0bd72c715f85a15db233f9ac5dc53
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/264206
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

---
+++ ui/shared/integrations/react/accounts/microsoft_sync/components/AdminConsentLink.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import I18n from 'i18n!account_settings_jsx_bundle'
+import {Link} from '@instructure/ui-link'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+
+function consentUrl({baseUrl, clientId, redirectUri, tenant}) {
+  const url = new URL(`${baseUrl}/${tenant}/adminconsent`)
+  url.searchParams.set('client_id', clientId)
+  url.searchParams.set('redirect_uri', redirectUri)
+
+  return url.href
+}
+
+const AdminConsentLink = props => {
+  const url = consentUrl(props)
+
+  return (
+    <div role="region" aria-live="polite">
+      {props.enabled && (
+        <>
+          <View display="block" margin="small 0 0 0">
+            <Text>
+              {I18n.t(
+                'After completing the above configuration, please use the following link to grant Canvas access to your Microsoft tenant:'
+              )}
+            </Text>
+          </View>
+          <View display="block">
+            <Text>
+              <Link href={url}>{I18n.t('Grant tenant access')}</Link>
+            </Text>
+          </View>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default AdminConsentLink
