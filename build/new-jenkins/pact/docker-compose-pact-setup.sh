531eda2a1224efa216130e4b3dc0ce77c2159f45
Kyle Rosenbaum <krosenbaum@instructure.com>
2021-05-05 13:51:25 -0700

    add RSPEC_PROCESSES to contract_tests
    refs DE-467
    
    this change will require a followup to remove
    DOCKER_PROCESSES
    
    test plan:
    -run linters
    -linters use expected db on a per thread basis
    
    Change-Id: Ie0fee7d22a335899e6dac5490f4cadf567e4e32f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/264294
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- build/new-jenkins/pact/docker-compose-pact-setup.sh
+++ build/new-jenkins/pact/docker-compose-pact-setup.sh
@@ -3,6 +3,8 @@ set -o errexit -o errtrace -o pipefail -o xtrace
 # ':' is a bash "no-op"
 parallel --will-cite ::: :
 
+DATABASE_PROCESSES=$((${RSPEC_PROCESSES:=1}-1))
+
 # build and start services
 docker-compose build --parallel canvas cassandra postgres redis
 docker-compose up -d canvas cassandra postgres redis
@@ -15,4 +17,4 @@ for service in cassandra:9160 redis:6379; do
 done
 
 # clone databases from canvas_test
-seq $((DOCKER_PROCESSES-1)) | parallel "docker-compose exec -T postgres sh -c 'createdb -U postgres -T canvas_test pact_test{}'"
+seq 0 $DATABASE_PROCESSES | parallel "docker-compose exec -T postgres sh -c 'createdb -U postgres -T canvas_test pact_test{}'"
