20cce7ad2c028863afa1f1966793fd0f0bf9acac
Aaron Ogata <aogata@instructure.com>
2021-05-10 13:13:48 -0700

    remove antiquated linter scripts
    refs DE-670
    
    Remove scripts that are part of an old iteration of Linters that is no longer in use.
    
    Test Plan
    1. Linters passes and runs all appropriate linters
    
    Change-Id: I773bc090c4d028695f81ff9982ccedab66c5228c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/264562
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- build/new-jenkins/linters/run-brakeman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x -o errexit -o errtrace -o nounset -o pipefail
-
-docker-compose --file $(pwd)/docker-compose.new-jenkins.canvas.yml \
-  run --name linter-brakeman --rm canvas bundle exec ruby script/brakeman
