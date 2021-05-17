a29d3f941f650de6d62173d1972c32e36dd9c32c
Michael Ziwisky <mziwisky@instructure.com>
2021-05-17 11:01:30 -0700

    reorganize auth token injection into `initializers` dir
    refs FOO-1938
    
    Change-Id: I809f112772db7db27e443b7988ff11b430696598
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/265090
    Reviewed-by: Ahmad Amireh <ahmad@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Michael Ziwisky <mziwisky@instructure.com>
    Product-Review: Michael Ziwisky <mziwisky@instructure.com>

--- ui/boot/initializers/__tests__/injectAuthTokenIntoForms.test.js
+++ ui/boot/initializers/__tests__/injectAuthTokenIntoForms.test.js
@@ -16,7 +16,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {isCrossSite} from '../index'
+import {isCrossSite} from '../injectAuthTokenIntoForms'
 
 describe('isCrossSite', () => {
   // eslint-disable-next-line no-restricted-globals
