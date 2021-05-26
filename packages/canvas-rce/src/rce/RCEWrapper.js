2f496eb2c4ce22761aff3b6900d4712f6bfbe053
Nate Armstrong <narmstrong@instructure.com>
2021-05-26 10:21:43 -0600

    RCE: Fix Pretty HTML Editor
    Fixed typing html characters like `<` into the pretty
    html editor.
    
    flag=none
    fixes MAT-208
    
    test plan:
    - In the new RCE
    - Add some content to the wysiwyg view
    - Switch to Pretty HTML Editor
    - Type some html like `<p>hi</p>`
    - Switch to raw html editor
    - Type some more html
    - Switch to wysiwyg view
    - All the text should be there
    - Switch back and forth between html editors and make sure
      it all looks good
    
    Change-Id: If3d8dfb9b318fcac5eaa6f783e00e92a636e928a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/265804
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Nate Armstrong <narmstrong@instructure.com>

--- packages/canvas-rce/src/rce/RCEWrapper.js
+++ packages/canvas-rce/src/rce/RCEWrapper.js
@@ -229,7 +229,6 @@ class RCEWrapper extends React.Component {
     this._elementRef = React.createRef()
     this._prettyHtmlEditorRef = React.createRef()
     this._showOnFocusButton = null
-    this._switchedFromWysiwygToPrettyHtml = false
 
     injectTinySkin()
 
@@ -574,8 +573,6 @@ class RCEWrapper extends React.Component {
       case RAW_HTML_EDITOR_VIEW:
         newState = {editorView: newView || WYSIWYG_VIEW}
     }
-    this._switchedFromWysiwygToPrettyHtml =
-      this.state.editorView === WYSIWYG_VIEW && newView === PRETTY_HTML_EDITOR_VIEW
     this.setState(newState, () => {
       if (wasFullscreen) {
         window.setTimeout(() => {
@@ -1437,15 +1434,6 @@ class RCEWrapper extends React.Component {
   renderHtmlEditor() {
     if (!this.props.use_rce_pretty_html_editor) return null
 
-    let code = ''
-    if (this._switchedFromWysiwygToPrettyHtml) {
-      // When changing from WYSIWYG to Pretty HTML we need to get the HTML styled content
-      code = this.mceInstance().getContent({format: 'html'})
-    } else {
-      // Reproducing the raw HTML editor behavior
-      this.mceInstance().getContent({format: 'raw'})
-    }
-
     // the div keeps the editor from collapsing while the code editor is downloaded
     return (
       <Suspense
@@ -1466,15 +1454,10 @@ class RCEWrapper extends React.Component {
           <RceHtmlEditor
             ref={this._prettyHtmlEditorRef}
             height={document[FS_ELEMENT] ? `${window.screen.height}px` : this.state.height}
-            code={code}
+            code={this.getCode()}
             onChange={value => {
               this.getTextarea().value = value
               this.handleTextareaChange()
-              // Enters only once, changes the way how the user can edit the content
-              // like the raw HTML editor
-              if (this._switchedFromWysiwygToPrettyHtml) {
-                this._switchedFromWysiwygToPrettyHtml = false
-              }
             }}
             onFocus={this.handleFocusHtmlEditor}
           />
