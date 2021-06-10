f06b80c38cdd578667016c43182b15ed8abb27dc
Steve Kacsmark <skacsmark@instructure.com>
2021-06-09 20:45:52 -0500

    Don't leak tabs in the API when behind a FF
    flag = none
    
    Test Plan:
      - Verify the Quizzes 2 Item Banks Placement does not appear
        in the tabs api when the relevant feature flag is disabled
      - Verify the Quizzes 2 Item Banks Placement does not appear
        in the courses/<id>/settings Navigation tab when the
        relevant feature flag is disabled
    
    Change-Id: I1c333322c7957642264a78dfa11cefe3e72a524e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/266796
    Reviewed-by: Han Yan <hyan@instructure.com>
    Reviewed-by: James Logan <james.logan@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: Stephen Kacsmark <skacsmark@instructure.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,7 @@ module ApplicationHelper
   include LocaleSelection
   include Canvas::LockExplanation
   include DatadogRumHelper
+  include NewQuizzesFeaturesHelper
 
   def context_user_name_display(user)
     name = user.try(:short_name) || user.try(:name)
@@ -332,11 +333,21 @@ module ApplicationHelper
     stylesheet_link_tag css_url_for(:common), media: "all"
   end
 
+  def quiz_lti_tab?(tab)
+    if tab[:id].is_a?(String) && tab[:id].start_with?('context_external_tool_') && tab[:args] && tab[:args][1]
+      return ContextExternalTool.find_by(id: tab[:args][1])&.quiz_lti?
+    end
+
+    false
+  end
+
   def sortable_tabs
     tabs = @context.tabs_available(@current_user, :for_reordering => true, :root_account => @domain_root_account, :course_subject_tabs => @context.try(:elementary_subject_course?))
     tabs.select do |tab|
       if (tab[:id] == @context.class::TAB_COLLABORATIONS rescue false)
         Collaboration.any_collaborations_configured?(@context) && !@context.feature_enabled?(:new_collaborations)
+      elsif (quiz_lti_tab?(tab) rescue false)
+        new_quizzes_navigation_placements_enabled?
       elsif (tab[:id] == @context.class::TAB_COLLABORATIONS_NEW rescue false)
         @context.feature_enabled?(:new_collaborations)
       elsif (tab[:id] == @context.class::TAB_CONFERENCES rescue false)
