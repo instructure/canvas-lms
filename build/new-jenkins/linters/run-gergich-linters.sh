bfc3a16b637e0f6209be0654125d3dbf53af5c41
James Butters <jbutters@instructure.com>
2021-06-14 15:29:33 -0600

    fix linters code build for plugins
    linters on plugin builds are currently failing because we are trying
    to run git commands from /usr/src/app rather than the plugin
    directory.
    
    flag = none
    closes: DE-713
    
    Test Plan:
    -Build passes
    -plugin build with changes passes
    
    Change-Id: Iad5b6275845e47159910e964993c71f40bf19672
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/267100
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/linters/run-gergich-linters.sh
+++ build/new-jenkins/linters/run-gergich-linters.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 set -ex
-# when parent is not in $GERRIT_BRANCH (i.e. master)
-if ! git merge-base --is-ancestor HEAD~1 origin/$GERRIT_BRANCH; then
-  message="This commit is built upon commits not currently merged in $GERRIT_BRANCH. Ensure that your dependent patchsets are merged first!\\n"
-  gergich comment "{\"path\":\"/COMMIT_MSG\",\"position\":1,\"severity\":\"warn\",\"message\":\"$message\"}"
-fi
+if [ "$GERRIT_PROJECT" == "canvas-lms" ]; then
+  # when parent is not in $GERRIT_BRANCH (i.e. master)
+  if ! git merge-base --is-ancestor HEAD~1 origin/$GERRIT_BRANCH; then
+    message="This commit is built upon commits not currently merged in $GERRIT_BRANCH. Ensure that your dependent patchsets are merged first!\\n"
+    gergich comment "{\"path\":\"/COMMIT_MSG\",\"position\":1,\"severity\":\"warn\",\"message\":\"$message\"}"
+  fi
 
-# when modifying Dockerfile or Dockerfile.jenkins*, Dockerfile.template must also be modified.
-ruby build/dockerfile_writer.rb --env development --compose-file docker-compose.yml,docker-compose.override.yml --in build/Dockerfile.template --out Dockerfile
-ruby build/dockerfile_writer.rb --env jenkins --compose-file docker-compose.yml,docker-compose.override.yml --in build/Dockerfile.template --out Dockerfile.jenkins
-if ! git diff --exit-code Dockerfile; then
-  message="This commit makes changes to Dockerfile but does not update the Dockerfile.template. Ensure your changes are included in build/Dockerfile.template.\\n"
-  gergich comment "{\"path\":\"\Dockerfile\",\"position\":1,\"severity\":\"error\",\"message\":\"$message\"}"
-fi
-if ! git diff --exit-code Dockerfile.jenkins; then
-  message="This commit makes changes to Dockerfile.jenkins but does not update the Dockerfile.template. Ensure your changes are included in build/Dockerfile.template.\\n"
-  gergich comment "{\"path\":\"\Dockerfile.jenkins\",\"position\":1,\"severity\":\"error\",\"message\":\"$message\"}"
+  # when modifying Dockerfile or Dockerfile.jenkins*, Dockerfile.template must also be modified.
+  ruby build/dockerfile_writer.rb --env development --compose-file docker-compose.yml,docker-compose.override.yml --in build/Dockerfile.template --out Dockerfile
+  ruby build/dockerfile_writer.rb --env jenkins --compose-file docker-compose.yml,docker-compose.override.yml --in build/Dockerfile.template --out Dockerfile.jenkins
+  if ! git diff --exit-code Dockerfile; then
+    message="This commit makes changes to Dockerfile but does not update the Dockerfile.template. Ensure your changes are included in build/Dockerfile.template.\\n"
+    gergich comment "{\"path\":\"\Dockerfile\",\"position\":1,\"severity\":\"error\",\"message\":\"$message\"}"
+  fi
+  if ! git diff --exit-code Dockerfile.jenkins; then
+    message="This commit makes changes to Dockerfile.jenkins but does not update the Dockerfile.template. Ensure your changes are included in build/Dockerfile.template.\\n"
+    gergich comment "{\"path\":\"\Dockerfile.jenkins\",\"position\":1,\"severity\":\"error\",\"message\":\"$message\"}"
+  fi
 fi
 
 gergich capture custom:./build/gergich/xsslint:Gergich::XSSLint 'node script/xsslint.js'
