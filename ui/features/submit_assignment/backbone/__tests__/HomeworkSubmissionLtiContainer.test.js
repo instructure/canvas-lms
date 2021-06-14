e19f547c0d9ffb2da6f031e505960543913b04b4
Sean Scally <sean.scally@instructure.com>
2021-06-14 14:46:44 -0700

    Add specs for isValidFileSubmission
    closes INTEROP-6842
    flag=none
    
    test plan: run the new tests
    
    Change-Id: I56bebbe921ee87e4c5db3dda5ec80e1171e5e58d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/267102
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Sean Scally <sean.scally@instructure.com>
    Product-Review: Sean Scally <sean.scally@instructure.com>

---
+++ ui/features/submit_assignment/backbone/__tests__/HomeworkSubmissionLtiContainer.test.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {isValidFileSubmission} from '../HomeworkSubmissionLtiContainer'
+import {findContentExtension} from '../contentExtension'
+import {getEnv} from '../environment'
+
+jest.mock('../contentExtension')
+jest.mock('../environment')
+
+describe('HomeworkSubmissionLtiContainer', () => {
+  afterEach(() => {
+    getEnv.mockReset()
+  })
+
+  describe('#isValidFileSubmission', () => {
+    describe('when there is no configuration for allowed extensions', () => {
+      it('returns true', () => {
+        getEnv.mockReturnValue({})
+
+        expect(isValidFileSubmission({})).toBeTruthy()
+      })
+    })
+
+    describe('when there are no configured allowed extensions', () => {
+      it('returns true', () => {
+        getEnv.mockReturnValue({
+          SUBMIT_ASSIGNMENT: {
+            ALLOWED_EXTENSIONS: []
+          }
+        })
+        expect(isValidFileSubmission({})).toBeTruthy()
+      })
+    })
+
+    describe('when there are allowed extensions', () => {
+      beforeEach(() => {
+        getEnv.mockReturnValue({
+          SUBMIT_ASSIGNMENT: {
+            ALLOWED_EXTENSIONS: ['txt']
+          }
+        })
+      })
+
+      describe('when no extension can be found for the submission', () => {
+        it('returns false', () => {
+          findContentExtension.mockReturnValue(null)
+          expect(isValidFileSubmission({})).toBeFalsy()
+        })
+      })
+
+      describe('when the found extension is in the list of allowed extensions', () => {
+        it('returns true', () => {
+          findContentExtension.mockReturnValue('txt')
+          expect(isValidFileSubmission({})).toBeTruthy()
+        })
+      })
+
+      describe('when the found extension is not in the list of allowed extensions', () => {
+        it('returns false', () => {
+          findContentExtension.mockReturnValue('notallowed')
+          expect(isValidFileSubmission({})).toBeFalsy()
+        })
+      })
+    })
+  })
+})
---
+++ ui/features/submit_assignment/backbone/contentExtension.js
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2021 - present Instructure, Inc.
+//
+// This file is part of Canvas.
+//
+// Canvas is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3 of the License.
+//
+// Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import mime from 'mime-types'
+
+export const extensionFromString = string => {
+  const ext = string?.split('.')?.pop()?.split('?')?.shift()
+  return mime?.types[ext] != null ? ext : null
+}
+
+export const findContentExtension = contentItem =>
+  (contentItem?.mediaType && mime.extension(contentItem.mediaType)) ||
+  extensionFromString(contentItem.url) ||
+  extensionFromString(contentItem.title) ||
+  extensionFromString(contentItem.text)
