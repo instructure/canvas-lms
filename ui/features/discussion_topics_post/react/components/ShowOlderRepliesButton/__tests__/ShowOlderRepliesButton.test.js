e83343c17b726c1e2b08cb41d3463b557c9c3654
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2021-06-17 12:23:14 -0400

    Add ShowOlderRepliesButton component
    Test plan:
      Option 1:
       - Spec pass.
      Option 2:
       - Run storybook and verify that
           the ShowOlderRepliesButton
           complies with Figma.
    
    flag=react_discussions_post
    
    Closes VICE-1585
    
    Change-Id: I16b3c8186f0276e61fd3e2751eda0ff3da002f9d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/267279
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Katrina Hess <khess@instructure.com>
    Reviewed-by: Chawn Neal <chawn.neal@instructure.com>
    QA-Review: Chawn Neal <chawn.neal@instructure.com>

---
+++ ui/features/discussion_topics_post/react/components/ShowOlderRepliesButton/__tests__/ShowOlderRepliesButton.test.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {fireEvent, render} from '@testing-library/react'
+import React from 'react'
+import {ShowOlderRepliesButton} from '../ShowOlderRepliesButton'
+
+const setup = props => {
+  return render(<ShowOlderRepliesButton {...props} />)
+}
+
+describe('ShowOlderRepliesButton', () => {
+  const onClick = jest.fn()
+
+  it('Should show the button text', () => {
+    const {queryByText} = setup()
+
+    expect(queryByText('Show older replies')).toBeTruthy()
+  })
+
+  it('Should be able to click the button', () => {
+    const {getByTestId} = setup({onClick})
+
+    fireEvent.click(getByTestId('show-older-replies-button'))
+    expect(onClick.mock.calls.length).toBe(1)
+  })
+})
