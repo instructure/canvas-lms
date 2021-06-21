9d6863e5afbceda35d2775104c3ace19ac339c51
Cody Cutrer <cody@instructure.com>
2021-06-21 15:53:00 -0600

    always select only the pertinent columns for a in-batches-pluck
    Change-Id: I36eee84ae18e05446ea6836cbc1b69dbf5c4f20a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/267446
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -1133,7 +1133,9 @@ module UsefulBatchEnumerator
   def pluck(*args)
     return to_enum(:pluck, *args) unless block_given?
 
-    @relation.in_batches(strategy: @strategy, load: false, **@kwargs) do |relation|
+    @relation.except(:select)
+      .select(*args)
+      .in_batches(strategy: @strategy, load: false, **@kwargs) do |relation|
       yield relation.pluck(*args)
     end
   end
