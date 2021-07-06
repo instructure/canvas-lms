b882eb403008da5d7c344b9ae370dbeff74ba8f1
Erin Hallmark McMillan <erin@instructure.com>
2021-07-06 15:54:16 -0600

    Update permission references for manage courses
    CLOSES: FOO-2144
    flag=none
    
    Test plan:
    - Courses - manage / update shows the additional considerations
      section
    
    Change-Id: Iede8c3e5015fe23d1e1a8fc67d061876c1c6ad5e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/268500
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

--- ui/features/permissions/react/templates.js
+++ ui/features/permissions/react/templates.js
@@ -2095,12 +2095,6 @@ const courseManagePermissions = generateActionTemplates(
       )
     },
     {
-      title: I18n.t('Blueprint Courses'),
-      description: I18n.t(
-        'If Admin - Manage / Update is enabled, but Blueprint Courses - add / edit / associate / delete is not enabled, users can still sync Blueprint Courses and view Sync history.'
-      )
-    },
-    {
       title: I18n.t('Course Content'),
       description: I18n.t(
         'To manage course content, Courses - manage / update and Course Content - add / edit / delete must be enabled.'
@@ -2207,39 +2201,92 @@ const courseManagePermissions = generateActionTemplates(
   ]
 )
 
-const courseManageAdminPermissions = generateActionTemplates('manage_courses_admin', [
-  {
-    title: I18n.t('Courses - manage / update'),
-    description: I18n.t('Allows user to sync Blueprint Courses.')
-  },
-  {
-    description: I18n.t('Allows user to view Blueprint Sync history.')
-  },
-  {
-    description: I18n.t('Allows user to view and manage courses in the account.')
-  },
-  {
-    description: I18n.t('Allows user to view the Course Setup Checklist button.')
-  },
-  {
-    description: I18n.t('Allows user to access the Navigation tab in Course Settings.')
-  },
-  {
-    description: I18n.t(
-      'Allows user to edit course image, name, course code, time zone, subaccount, term, and other options in Course Details tab.'
-    )
-  },
-  {
-    description: I18n.t(
-      'Allows user to access Student View (test student), Copy this Course, and Permanently Delete Course buttons.'
-    )
-  },
-  {
-    description: I18n.t(
-      'Allows user to view student context cards in announcement and discussion replies.'
-    )
-  }
-])
+const courseManageAdminPermissions = generateActionTemplates(
+  'manage_courses_admin',
+  [
+    {
+      title: I18n.t('Blueprint Courses'),
+      description: I18n.t('Allows user to sync Blueprint Courses.')
+    },
+    {
+      description: I18n.t('Allows user to view Blueprint Sync history.')
+    },
+    {
+      title: I18n.t('Courses (Account Navigation)'),
+      description: I18n.t('Allows user to view and manage courses in the account.')
+    },
+    {
+      title: I18n.t('Course Home Page'),
+      description: I18n.t('Allows user to view the Course Setup Checklist button.')
+    },
+    {
+      title: I18n.t('Course Settings (Course Details)'),
+      description: I18n.t('Allows user to access the Navigation tab.')
+    },
+    {
+      description: I18n.t(
+        'Allows user to edit course image, name, course code, time zone, subaccount, term, and other options in Course Details tab.'
+      )
+    },
+    {
+      description: I18n.t(
+        'Allows user to access Student View (test student), Copy this Course, and Permanently Delete Course buttons.'
+      )
+    },
+    {
+      title: I18n.t('Student Context Cards'),
+      description: I18n.t(
+        'Allows user to view student context cards in announcement and discussion replies.'
+      )
+    }
+  ],
+  [
+    {
+      title: I18n.t('Blueprint Courses'),
+      description: I18n.t(
+        'If this permission is enabled, but Blueprint Courses - add / edit / associate / delete is not enabled, users can still sync Blueprint Courses and view Sync history.'
+      )
+    },
+    {
+      title: I18n.t('Course Content'),
+      description: I18n.t(
+        'To manage course content, Course Content - add / edit / delete must also be enabled.'
+      )
+    },
+    {
+      description: I18n.t(
+        'To view Choose Home Page and Course Setup Checklist buttons, Course Content - view must also be enabled. (Teachers, designers, and TAs can set the home page of a course, regardless of their permissions.)'
+      )
+    },
+    {
+      title: I18n.t('Course Settings'),
+      description: I18n.t(
+        'To cross-list a section, Manage Course Sections - edit must also be enabled.'
+      )
+    },
+    {
+      description: I18n.t('To edit the course SIS ID, SIS Data - manage must also be enabled.')
+    },
+    {
+      title: I18n.t('Courses (Account Navigation)'),
+      description: I18n.t(
+        'To access the Courses link in Account Navigation, Courses - view list must also be enabled.'
+      )
+    },
+    {
+      title: I18n.t('Grades'),
+      description: I18n.t(
+        'To view grades in a course, Grades - view all grades must also be enabled.'
+      )
+    },
+    {
+      title: I18n.t('Student Context Cards'),
+      description: I18n.t(
+        'Student context cards must be enabled for an account by an admin. If this permission is not enabled, users can still view context cards through the Gradebook.'
+      )
+    }
+  ]
+)
 
 const courseFilesAddPermissions = generateActionTemplates(
   'manage_files',
