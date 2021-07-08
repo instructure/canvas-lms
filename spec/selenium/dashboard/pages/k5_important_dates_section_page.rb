3aff82b19b2ce48d2d51a5aa4142de7ecc9ab2ab
Robin Kuss <rkuss@instructure.com>
2021-07-08 10:42:55 -0500

    spec: add mark as important selenium tests
    Closes LS-2367
    
    Test Plan: Passes Jenkins
    flag=important_dates
    
    Change-Id: I346837329ad982019d1c4cc69049bbf3fbbddbf6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/268663
    Reviewed-by: Nate Armstrong <narmstrong@instructure.com>
    QA-Review: Nate Armstrong <narmstrong@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/dashboard/pages/k5_important_dates_section_page.rb
+++ spec/selenium/dashboard/pages/k5_important_dates_section_page.rb
@@ -22,7 +22,25 @@ require_relative '../../common'
 module K5ImportantDatesSectionPageObject
 
   #------------------------- Selectors --------------------------
-  #
+
+  def add_override_selector
+    "#add_due_date"
+  end
+  def date_field_selector
+    ".date_field[data-date-type='due_at']"
+  end
+
+  def edit_assignment_submit_selector
+    "#edit_assignment_form"
+  end
+
+  def edit_discussion_submit_selector
+    "#edit_discussion_form_buttons"
+  end
+
+  def edit_quiz_submit_selector
+    "#quiz_edit_actions"
+  end
 
   def important_date_icon_selector(icon_type)
     "svg[name='#{icon_type}']"
@@ -40,14 +58,30 @@ module K5ImportantDatesSectionPageObject
     "h3:contains('Important Dates')"
   end
 
+  def mark_important_dates_selector
+    "input + label:contains('Mark as important date and show on homeroom sidebar')"
+  end
+
+  def mark_important_dates_input_selector
+    "[name='important_dates']"
+  end
+
   def no_important_dates_image_selector
     "[data-testid='important-dates-panda']"
   end
 
   #------------------------- Elements --------------------------
 
+  def add_override
+    f(add_override_selector)
+  end
+
   def assignment_link(link_text)
     fln(link_text)
+  end
+
+  def date_field
+    ff(date_field_selector)
   end
 
   def important_date_icon(icon_type)
@@ -66,6 +100,14 @@ module K5ImportantDatesSectionPageObject
     fj(important_dates_title_selector)
   end
 
+  def mark_important_dates
+    fj(mark_important_dates_selector)
+  end
+
+  def mark_important_dates_input
+    f(mark_important_dates_input_selector)
+  end
+
   def no_important_dates_image
     f(no_important_dates_image_selector)
   end
@@ -76,10 +118,26 @@ module K5ImportantDatesSectionPageObject
     element_exists?(important_date_icon_selector(icon_name))
   end
 
+  def set_and_tab_out_of_date_field(date_field_index, due_at)
+    date_field[date_field_index].send_keys(format_date_for_view(due_at), :tab)
+  end
+
+  def clear_date_field(date_field_index)
+    date_field[date_field_index].clear
+  end
+
   #----------------------- Click Items -------------------------------
+
+  def click_add_override
+    add_override.click
+  end
 
   def click_important_date_link
     important_date_link.click
+  end
+
+  def click_mark_important_dates
+    mark_important_dates.click
   end
 
   #------------------------------Retrieve Text----------------------#
