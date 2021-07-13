e6226f547f7b75400dbced6dd3295b130fdf232e
Weston Dransfield <wdransfield@instructure.com>
2021-07-13 16:06:36 -0600

    make span content editable on mention
    Closes MAT-289
    
    This change does the following:
    1. Whenever the @ character is used to trigger a
       mention, the marker span gets contenteditable=true
    2. At the same time as 1., the tinymce body gets
       contenteditable=false
    3. Creates an "onexit" callback function to restore
       editability to the body
    4. Restores editability to the body when the @ sign
       is deleted via a backspace, the enter key is pressed
       (might need to change that bit) or the html a user
       switches from wysiwyg -> html editor -> wysiswg
    
    flag=rce_mentions_in_discussions
    
    Test Plan
    1. Navigate to a discussion that in a course that uses
       the new discussions UI and discussion mentions
    2. Trigger an mention with "@" and verify the mentions
       marker span is now contenteditable
    3. Verify the tinymce body is no longer contenteditable
    
    NOTE: steps 1 and 2 are most easily done by creating a
    live expression in the web console with the following:
    
    tinymce.activeEditor.dom.select('#mentions-marker')[0]
      .getAttribute('contenteditable')
    
    and
    
    tinymce.activeEditor.getBody().getAttribute('contenteditable')
    
    4. Trigger a mention and verify you can paste content into it
    5. Triggger a mention, type some text, then hit "enter." Verify
       the tinymce body is editable once again
    6. Trigger mentions, switch to html view, then switch back.
       Verify the tinymce body is editable once again
    7. Trigger a mention, then click somewhere elsein the tinymce
       document. Verify the tinymce body is editable
    
    Change-Id: I5159062ec70bff741363a5e30ffcac44307d0e42
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/269051
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Guilherme Baron <gbaron@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>

---
+++ ui/shared/rce/plugins/canvas_mentions/events.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {makeBodyEditable} from './contentEditable'
+import {MARKER_SELECTOR, MARKER_ID} from './constants'
+
+function shouldRestoreFromKeyEvent(event, editor) {
+  const {which} = event
+  const keyCodes = {
+    backspace: 8,
+    enter: 13
+  }
+
+  // Enter key was pressed
+  if (which === keyCodes.enter) return true
+
+  // Nothing but a backspace key press can restore
+  // body editability at this point
+  if (which !== keyCodes.backspace) return false
+
+  // Backspace key was pressed. Check to see if
+  // the user was attempting to backspace out
+  // of the mention span
+  const {endOffset, startOffset} = editor.selection.getRng()
+  return (endOffset === 1 && startOffset === 1) || (endOffset === 0 && startOffset === 0)
+}
+
+/**
+ * Restores editability to the tinymce body.
+ *
+ * If the content being inserted contains the marker
+ * element, this function is a no op
+ *
+ * @param Event e
+ */
+export const onSetContent = e => {
+  // If the content being inserted is not the marker and
+  // due to a paste
+  if (!e.content.includes(MARKER_ID) && !e.paste) {
+    makeBodyEditable(e.target, MARKER_SELECTOR)
+  }
+}
+
+/**
+ * Restores editability to the tinymce body.
+ *
+ * Only takes effect when the keydown event is
+ * outside of the marker element OR if the keydown
+ * event is a backspace / enter
+ *
+ * @param Event e
+ */
+export const onKeyDown = e => {
+  const editor = e.editor || tinymce.activeEditor
+  // If the user is typing outside the marker,\
+  // or types an "enter" or "backspace" within the marker
+  if (editor.selection.getNode().id !== MARKER_ID || shouldRestoreFromKeyEvent(e, editor)) {
+    makeBodyEditable(editor, MARKER_SELECTOR)
+  }
+}
+
+/**
+ * Restores editability to the tinymce body.
+ *
+ * Only fires if the target of the mousedown event is
+ * outside of the marker element
+ * @param Event e
+ */
+export const onMouseDown = e => {
+  const editor = e.editor || tinymce.activeEditor
+
+  if (e.currentTarget.id !== MARKER_ID) {
+    makeBodyEditable(editor, MARKER_SELECTOR)
+  }
+}
