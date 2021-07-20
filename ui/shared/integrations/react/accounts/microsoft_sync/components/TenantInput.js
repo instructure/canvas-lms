acacbee812ff496f7d5d79ac601452e1ee10bac0
Alexandre Trindade <alexandre.trindade@instructure.com>
2021-07-20 09:18:06 -0300

    Change the Integration tab layout to display as a table
    refs INTEROP-6791
    flag = none
    
    Test plan:
    - Access the Admin > Settings menu
    - Open the Integration tab
    - The content will be displayed as a table
    
    Change-Id: I1d447823c81f3bdde3260b74125840f61f44b90d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/269509
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Daniel Sasaki <dsasaki@instructure.com>

--- ui/shared/integrations/react/accounts/microsoft_sync/components/TenantInput.js
+++ ui/shared/integrations/react/accounts/microsoft_sync/components/TenantInput.js
@@ -16,13 +16,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {IconButton} from '@instructure/ui-buttons'
-import {Heading} from '@instructure/ui-heading'
 import {TextInput} from '@instructure/ui-text-input'
-import {IconInfoLine} from '@instructure/ui-icons'
-import {Flex} from '@instructure/ui-flex'
-import {View} from '@instructure/ui-view'
-import {Tooltip} from '@instructure/ui-tooltip'
 import I18n from 'i18n!account_settings_jsx_bundle'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -30,22 +24,7 @@ import {ScreenReaderContent} from '@instructure/ui-a11y-content'
 
 export default function TenantInput(props) {
   return (
-    <View as="div" margin="small 0 small 0" maxWidth="20rem">
-      <Flex>
-        <Flex.Item>
-          <Heading level="h3">{I18n.t('Tenant Name')}</Heading>
-        </Flex.Item>
-        <Flex.Item>
-          <Tooltip tip={I18n.t('Your Azure Active Directory Tenant Name')} on={['hover', 'focus']}>
-            <IconButton
-              screenReaderLabel={I18n.t('Tooltip info for tenant input')}
-              renderIcon={IconInfoLine}
-              withBackground={false}
-              withBorder={false}
-            />
-          </Tooltip>
-        </Flex.Item>
-      </Flex>
+    <>
       <TextInput
         label={<ScreenReaderContent>{I18n.t('Tenant Name Input Area')}</ScreenReaderContent>}
         type="text"
@@ -54,7 +33,7 @@ export default function TenantInput(props) {
         defaultValue={props.tenant}
         messages={props.messages}
       />
-    </View>
+    </>
   )
 }
 
