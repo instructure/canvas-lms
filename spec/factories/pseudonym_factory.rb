42dedcd140096ffdb02660900261caf5efdc6d93
Evan Battaglia <ebattaglia@instructure.com>
2021-07-21 13:45:12 -0600

    MSFT sync job: add integration_id as lookup option
    Also fixes a bug where users in a course that had a null value for the
    login attribute (SIS id is the one case I know of for sure) would make
    UsersUlvsFinder crash.
    
    closes INTEROP-6927
    flag=microsoft_group_enrollments_syncing
    
    Test plan:
    - have a course with user(s) with pseudonym(s) with an integration id
      equal to the UPN (or email) of remote users in our test tenant
    - set the root account micrososft_sync_login_attribute setting to
      "integration_id"
    - clear the UserMapping table if necessary, and run a sync for the
      course
    - check in the Microsoft admin console, the user(s) should be in the
      team there
    - on one of the users, set the "integration_id" field on the pseudonym
      to null, e.g.
        my_pseudonym.update integration_id: nil
    - clear the UserMapping table again
    - run a sync. the sync should run successfully and the user should not
      be in the Microsoft team anymore. (or, if there are now no teachers
      with mappings, the job will actually fail saying a group can't have
      no owners)
    
    Change-Id: I15d571a663247af4db60b55298ecbbd299a320a6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/269672
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>

--- spec/factories/pseudonym_factory.rb
+++ spec/factories/pseudonym_factory.rb
@@ -53,6 +53,7 @@ module Factories
     account = (opts[:account] ? opts[:account].root_account : Account.default)
     @pseudonym = account.pseudonyms.build(:user => user, :unique_id => username, :password => password, :password_confirmation => password)
     @pseudonym.sis_user_id = opts[:sis_user_id]
+    @pseudonym.integration_id = opts[:integration_id]
     @pseudonym.save_without_session_maintenance
     opts[:username] = opts[:username] + user.id.to_s + '@example.com' unless opts[:username].include? '@'
     @pseudonym.communication_channel = communication_channel(user, opts)
