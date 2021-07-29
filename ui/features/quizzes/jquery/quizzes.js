d18c91514968fe33acc03bb1ab99ff09763cf279
Steve Kacsmark <skacsmark@instructure.com>
2021-07-29 15:29:09 -0500

    Allow Quizzes to save without error
    refs QO-800
    flag = none
    
    Test Plan:
      In Classic Quizzes, create and publish a quiz being sure
      to check "Let Students See Their Quiz Responses", "Let Students See
      The Correct Answers", and setting the show and hide dates.
    
      Changes your language to Hungarian/Magyar, and edit the quiz.
      Attempting to Save the quiz should update, and bring you to the quiz
      landing page.
    
    Change-Id: Ie587a7d18f1d53aef9d09ae41495b354a0429860
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/270668
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: Susan Sorensen <susan.sorensen@instructure.com>
    Reviewed-by: Jorge Arteaga <jorge.arteaga@instructure.com>
    Reviewed-by: Jared Crystal <jcrystal@instructure.com>

--- ui/features/quizzes/jquery/quizzes.js
+++ ui/features/quizzes/jquery/quizzes.js
@@ -1378,9 +1378,10 @@ const ipFilterValidation = {
     if (resp && resp.invalid_ip_filter) {
       const event = 'invalid:ip_filter'
       $('#quiz_ip_filter').triggerHandler(event)
+
+      // Prevent $.fn.formErrors from giving error box with cryptic message.
+      delete resp.invalid_ip_filter
     }
-    // Prevent $.fn.formErrors from giving error box with cryptic message.
-    delete resp.invalid_ip_filter
   }
 }
 
@@ -1519,7 +1520,7 @@ correctAnswerVisibility = {
       const $field = $('#quiz_' + key)
       let date
 
-      if ($field.val().length) {
+      if ($field.val().length && $field.data().date) {
         date = $field.data().date
         data['quiz[' + key + ']'] = $.unfudgeDateForProfileTimezone(date).toISOString()
       } else {
