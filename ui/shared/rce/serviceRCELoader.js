edbc561339b8d88af9c8dc1ab70420ad3eb83281
Ed Schiebel <eschiebel@instructure.com>
2021-08-10 16:05:16 -0400

    Fix missing feature check in serviceRCELoader
    closes MAT-378
    flag=rce_limit_init_render_on_page
    
    test plan:
      - the rce loads with rce_enhancements off
      - the rce loads with rce_enhancements on and rce_limit_init_render_on_page
      - the rce loads with rce_enhancements on and rce_limit_init_render_on_page on
    
    Change-Id: I3df0c5200dbbe1894b221cde5d3d9a3103175a1a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/271123
    Reviewed-by: Gary Mei <gmei@instructure.com>
    Reviewed-by: Jon Scheiding <jon.scheiding@instructure.com>
    QA-Review: Jon Scheiding <jon.scheiding@instructure.com>
    Product-Review: Jon Scheiding <jon.scheiding@instructure.com>
    Tested-by: Ed Schiebel <eschiebel@instructure.com>

--- ui/shared/rce/serviceRCELoader.js
+++ ui/shared/rce/serviceRCELoader.js
@@ -39,7 +39,16 @@ const RCELoader = {
 
     this.loadRCE(RCE => {
       RCE.renderIntoDiv(renderingTarget, propsForRCE, remoteEditor => {
-        remoteEditor.tinymceOn('init', () => callback(textarea, polyfill.wrapEditor(remoteEditor)))
+        if (remoteEditor.tinymceOn) {
+          // remove with rce_limit_init_render_on_page flag
+          remoteEditor.tinymceOn('init', () =>
+            callback(textarea, polyfill.wrapEditor(remoteEditor))
+          )
+        } else {
+          remoteEditor
+            .mceInstance()
+            .on('init', () => callback(textarea, polyfill.wrapEditor(remoteEditor)))
+        }
       })
     })
   },
@@ -209,7 +218,7 @@ const RCELoader = {
       ltiTools: window.INST?.editorButtons,
       autosave,
       instRecordDisabled: ENV.RICH_CONTENT_INST_RECORD_TAB_DISABLED,
-      maxInitRenderedRCEs: !!window.ENV?.FEATURES?.rce_limit_init_render_on_page
+      maxInitRenderedRCEs: window.ENV?.FEATURES?.rce_limit_init_render_on_page
         ? tinyMCEInitOptions.maxInitRenderedRCEs
         : -1,
       highContrastCSS: window.ENV?.url_for_high_contrast_tinymce_editor_css,
