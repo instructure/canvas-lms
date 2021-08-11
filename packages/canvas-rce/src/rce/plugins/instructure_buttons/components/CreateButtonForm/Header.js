c04dab3ab9e6d925e75c81629a14609152dccd77
Juan Chavez <juan.chavez@instructure.com>
2021-08-11 12:26:18 -0400

    Add alt text field to buttons tray
    flag=rce_buttons_and_icons
    
    closes MAT-357
    
    test plan:
    - Open buttons and icons tray
    - Name the button
    - Set an alt text for the button
    - Click Submit
    - Verify that svg was added to the editor with
    the alt text.
    
    Change-Id: I11766f6421d93cf2d2f8c10f3a9887d14196365e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/271188
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Guilherme Baron <gbaron@instructure.com>
    QA-Review: Guilherme Baron <gbaron@instructure.com>
    Product-Review: Juan Chavez <juan.chavez@instructure.com>

--- packages/canvas-rce/src/rce/plugins/instructure_buttons/components/CreateButtonForm/Header.js
+++ packages/canvas-rce/src/rce/plugins/instructure_buttons/components/CreateButtonForm/Header.js
@@ -23,8 +23,36 @@ import {TextInput} from '@instructure/ui-text-input'
 
 import formatMessage from '../../../../../format-message'
 import {Preview} from './Preview'
+import {TextArea} from '@instructure/ui-text-area'
+import {Tooltip} from '@instructure/ui-tooltip'
+import {View} from '@instructure/ui-view'
+import {Button} from '@instructure/ui-buttons'
+import {IconQuestionLine} from '@instructure/ui-icons'
+import {ScreenReaderContent} from '@instructure/ui-a11y-content'
 
 export const Header = ({settings, onChange}) => {
+  const tooltipText = formatMessage('Used by screen readers to describe the content of an image')
+  const textAreaLabel = (
+    <Flex alignItems="center">
+      <Flex.Item>{formatMessage('Alt Text')}</Flex.Item>
+
+      <Flex.Item margin="0 0 0 xx-small">
+        <Tooltip
+          on={['hover', 'focus']}
+          placement="top"
+          tip={
+            <View display="block" id="alt-text-label-tooltip" maxWidth="14rem">
+              {tooltipText}
+            </View>
+          }
+        >
+          <Button icon={IconQuestionLine} size="small" variant="icon">
+            <ScreenReaderContent>{tooltipText}</ScreenReaderContent>
+          </Button>
+        </Tooltip>
+      </Flex.Item>
+    </Flex>
+  )
   return (
     <Flex as="header" direction="column" padding="0 small 0">
       <Flex.Item padding="small">
@@ -40,6 +68,21 @@ export const Header = ({settings, onChange}) => {
             onChange({name})
           }}
           value={settings.name}
+        />
+      </Flex.Item>
+      <Flex.Item padding="small">
+        <TextArea
+          id="button-alt-text"
+          aria-describedby="alt-text-label-tooltip"
+          height="4rem"
+          label={textAreaLabel}
+          onChange={e => {
+            const alt = e.target.value
+            onChange({alt})
+          }}
+          placeholder={formatMessage('(Describe the image)')}
+          resize="vertical"
+          value={settings.alt}
         />
       </Flex.Item>
     </Flex>
