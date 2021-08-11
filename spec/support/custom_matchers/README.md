ccf4f2dd42b16f744f23b25b26a08071d28a3620
Davis Hyer <dhyer@instructure.com>
2021-08-11 09:17:53 -0600

    migrate custom matchers to spec/support/custom_matchers
    flag=none
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: Iee3ef65abb6badd5bfa9f26a8f86ad4e058aa0ac
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/271179
    Reviewed-by: Brian Watson <bwatson@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

---
+++ spec/support/custom_matchers/README.md
@@ -0,0 +1,8 @@
+# Custom Matchers
+RSpec provides a lot of helpful matchers. Unfortunately, they're not always exactly what you need.
+
+Define your own custom matcher here and Canvas will automatically configure it to be available.
+
+Ex: `expect(foo).to my_custom_matcher(bar)`
+
+Check out the [official docs](https://relishapp.com/rspec/rspec-expectations/docs) for more info.
---
+++ spec/support/custom_matchers/and_fragment.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2021 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+RSpec::Matchers.define :and_fragment do |expected|
+  match do |actual|
+    fragment = JSON.parse(URI.decode_www_form_component(URI(actual).fragment))
+    expected_as_strings = RSpec::Matchers::Helpers.cast_to_strings(expected: expected)
+    values_match?(expected_as_strings, fragment)
+  end
+end
