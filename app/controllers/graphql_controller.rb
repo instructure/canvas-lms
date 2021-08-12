7bf1fbf53e4056ebe4ed7225d9242d981764d847
Michael Ziwisky <mziwisky@instructure.com>
2021-08-12 10:30:40 -0700

    don't publicly expose subgraph introspection query in prod
    refs INTEROP-6995
    
    we'll use Apollo Studio or some similar schema registry in production
    environments.  public exposure is just a convenience for local dev -- it
    allows you to run the API Gateway without needing an Apollo Studio graph
    set up for it.
    
    Change-Id: I3c996c76b985e81671d49b25d24db6ed2bfaf674
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/271283
    QA-Review: Michael Ziwisky <mziwisky@instructure.com>
    Product-Review: Michael Ziwisky <mziwisky@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sean Scally <sean.scally@instructure.com>

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -79,7 +79,7 @@ class GraphQLController < ApplicationController
       return !::Account.site_admin.feature_enabled?(:disable_graphql_authentication)
     end
 
-    if action_name == 'subgraph_execute' && sdl_query?
+    if !Rails.env.production? && action_name == 'subgraph_execute' && sdl_query?
       return false
     end
 
