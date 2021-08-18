bae27dc55862c7615b73998cb1650f27b5a16de5
Davis Hyer <dhyer@instructure.com>
2021-08-18 11:43:57 -0600

    spec: add NotificationPreferencesType tests
    refs VICE-1507
    flag=none
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: I67be1ac9157cceeddde4579137ad8e60c33c2273
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/271741
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>

---
+++ spec/graphql/types/notification_preferences_type_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2021 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'spec_helper'
+require_relative "../graphql_spec_helper"
+
+describe Types::NotificationPreferencesType do
+  let(:user) { student_in_course.user }
+  let(:preferences_type) { GraphQLTypeTester.new(user, current_user: user) }
+  let!(:email_channel) { communication_channel(user, {username: 'email@email.com', path_type: CommunicationChannel::TYPE_EMAIL}) }
+  let!(:push_channel) { communication_channel(user, {username: 'push', path_type: CommunicationChannel::TYPE_PUSH}) }
+  let!(:sms_channel) { communication_channel(user, {username: 'sms', path_type: CommunicationChannel::TYPE_SMS}) }
+
+  describe "channels" do
+    it "returns the user's channels" do
+      result = preferences_type.resolve('notificationPreferences { channels { path } }', domain_root_account: Account.default)
+      expect(result).to match_array [email_channel.path, push_channel.path, sms_channel.path]
+    end
+
+    context "push notifications are disabled on the account" do
+      before do
+        allow(Account.default).to receive(:enable_push_notifications?).and_return false
+      end
+
+      it "does not return push channels" do
+        result = preferences_type.resolve('notificationPreferences { channels { path } }', domain_root_account: Account.default)
+        expect(result).to_not include push_channel.path
+        expect(result).to match_array [email_channel.path, sms_channel.path]
+      end
+    end
+
+    context "deprecate_sms feature flag is enabled" do
+      before do
+        Account.site_admin.enable_feature!(:deprecate_sms)
+      end
+
+      after do
+        Account.site_admin.disable_feature!(:deprecate_sms)
+      end
+
+      it "does not return sms channels" do
+        result = preferences_type.resolve('notificationPreferences { channels { path } }', domain_root_account: Account.default)
+        expect(result).to_not include sms_channel.path
+        expect(result).to match_array [email_channel.path, push_channel.path]
+      end
+    end
+
+    context "a channel_id is provided" do
+      it "returns only the specified channel" do
+        result = preferences_type.resolve("notificationPreferences { channels(channelId: \"#{email_channel.id}\") { path } }", domain_root_account: Account.default)
+        expect(result).to match_array [email_channel.path]
+      end
+
+      context "the channel_id is invalid" do
+        it "returns an empty array" do
+          result = preferences_type.resolve("notificationPreferences { channels(channelId: \"0\") { path } }", domain_root_account: Account.default)
+          expect(result).to match_array []
+        end
+      end
+    end
+  end
+end
