5c663cfc1631bf52e27889ccd6dd12f64fbc879a
Adrian Packel <apackel@instructure.com>
2021-08-20 13:01:59 -0500

    Make new "View Options" tab save changes
    refs EVAL-1840
    flag=enhanced_gradebook_filters
    
    Test plan:
    - Turn on the Enhanced Gradebook Filters feature
      - In the Gradebook, open the settings modal and test making changes to
        the items in the "View Options" tab
        - Check that your changes are reflected in the Gradebook once you
          close the modal
      - Test updating multiple items in the View Options tab at once (e.g.,
        colors and "show notes", or all the options at the same time)
      - Test updating settings in other tabs and settings in the View
        Options tab in the same request, and make sure all the changes go
        through
      - Note that the old View menu still has the options that have been
        moved to this panel; they'll be remove in a subsequent commit
    - Turn off the Enhanced Gradebook Filters feature
      - Make sure the individual settings in the View Options menu still
        work as before
    
    Change-Id: Ifd8682a799df52353ae3f280989b9e672eda472f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/272435
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Dustin Cowles <dustin.cowles@instructure.com>
    Reviewed-by: Syed Hussain <shussain@instructure.com>
    QA-Review: Adrian Packel <apackel@instructure.com>
    Product-Review: Jody Sailor

--- ui/features/gradebook/react/default_gradebook/Gradebook.js
+++ ui/features/gradebook/react/default_gradebook/Gradebook.js
@@ -1926,7 +1926,9 @@ class Gradebook extends React.Component {
   }
 
   renderViewOptionsMenu() {
-    // TODO: if enhanced_gradebook_filters is on, don't render the menu at all
+    // TODO: if enhanced_gradebook_filters is enabled, we can skip rendering
+    // this menu when we have the filters in place. Until then, keep rendering
+    // it so we can still filter when we have the flag on.
 
     const mountPoint = document.querySelector("[data-component='ViewOptionsMenu']")
     return (this.viewOptionsMenu = renderComponent(
@@ -2064,8 +2066,7 @@ class Gradebook extends React.Component {
     return {
       allowSortingByModules: modulesEnabled,
       allowViewUngradedAsZero: this.courseFeatures.allowViewUngradedAsZero,
-      // TODO: actually save the updated settings
-      onViewOptionsUpdated: () => Promise.resolve(),
+      onViewOptionsUpdated: this.handleViewOptionsUpdated,
       viewOptions: {
         columnSortSettings: {criterion, direction},
         showNotes: this.isTeacherNotesColumnShown(),
@@ -2074,6 +2075,161 @@ class Gradebook extends React.Component {
         viewUngradedAsZero
       }
     }
+  }
+
+  handleViewOptionsUpdated = ({
+    columnSortSettings: {criterion, direction} = {},
+    showNotes,
+    showUnpublishedAssignments,
+    statusColors: colors,
+    viewUngradedAsZero
+  }) => {
+    // We may have to save changes to more than one endpoint, depending on
+    // which options have changed. Additionally, a couple options require us to
+    // update the grid when they change. Let's sort out which endpoints we
+    // actually need to call and return a single promise encapsulating all of
+    // them.
+    const promises = []
+
+    // Column sort settings have their own endpoint.
+    const {criterion: oldCriterion, direction: oldDirection} =
+      this.getColumnSortSettingsViewOptionsMenuProps()
+    const columnSortSettingsChanged = criterion !== oldCriterion || direction !== oldDirection
+    if (columnSortSettingsChanged) {
+      promises.push(this.saveUpdatedColumnOrder({criterion, direction}))
+    }
+
+    // We save changes to the notes column using the custom column API.
+    if (showNotes !== this.isTeacherNotesColumnShown()) {
+      promises.push(this.saveUpdatedTeacherNotesSetting({showNotes}))
+    }
+
+    // Finally, the remaining options are saved to the user's settings.
+    const {
+      showUnpublishedAssignments: oldShowUnpublished,
+      viewUngradedAsZero: oldViewUngradedAsZero
+    } = this.gridDisplaySettings
+
+    const viewUngradedAsZeroChanged =
+      this.courseFeatures.allowViewUngradedAsZero && oldViewUngradedAsZero !== viewUngradedAsZero
+    const showUnpublishedChanged = oldShowUnpublished !== showUnpublishedAssignments
+    const colorsChanged = !_.isEqual(this.getGridColors(), colors)
+
+    if (colorsChanged || showUnpublishedChanged || viewUngradedAsZeroChanged) {
+      const changedSettings = {
+        colors: colorsChanged ? colors : undefined,
+        showUnpublishedAssignments: showUnpublishedChanged ? showUnpublishedAssignments : undefined,
+        viewUngradedAsZero: viewUngradedAsZeroChanged ? viewUngradedAsZero : undefined
+      }
+      promises.push(this.saveUpdatedUserSettings(changedSettings))
+    }
+
+    return Promise.all(promises)
+      .catch(FlashAlert.showFlashError(I18n.t('There was an error updating view options.')))
+      .finally(() => {
+        // Regardless of which options we changed, we most likely need to
+        // update the columns and grid.
+        this.updateColumns()
+        this.updateGrid()
+      })
+  }
+
+  saveUpdatedColumnOrder = ({criterion, direction}) => {
+    const newSortOrder = {direction, sortType: criterion}
+    const {freezeTotalGrade} = this.getColumnOrder()
+
+    return GradebookApi.updateColumnOrder(this.options.context_id, {
+      ...newSortOrder,
+      freezeTotalGrade
+    }).then(() => {
+      this.setColumnOrder(newSortOrder)
+      const columns = this.gridData.columns.scrollable.map(
+        columnId => this.gridData.columns.definitions[columnId]
+      )
+      columns.sort(this.makeColumnSortFn(newSortOrder))
+      this.gridData.columns.scrollable = columns.map(column => column.id)
+    })
+  }
+
+  saveUpdatedUserSettings = ({colors, showUnpublishedAssignments, viewUngradedAsZero}) => {
+    return this.saveSettings({
+      colors,
+      showUnpublishedAssignments,
+      viewUngradedAsZero
+    }).then(() => {
+      // Make various updates to the grid depending on what changed.  These
+      // triple-equals checks are deliberate: null could be an actual value for
+      // the setting, so we use undefined to indicate that the setting hasn't
+      // changed and hence we don't need to update it.
+
+      if (colors !== undefined) {
+        this.setGridColors(colors)
+        this.renderGridColor()
+      }
+
+      if (showUnpublishedAssignments !== undefined) {
+        this.gridDisplaySettings.showUnpublishedAssignments = showUnpublishedAssignments
+      }
+
+      if (viewUngradedAsZero !== undefined) {
+        this.gridDisplaySettings.viewUngradedAsZero = viewUngradedAsZero
+        this.courseContent.students.listStudents().forEach(student => {
+          this.calculateStudentGrade(student, true)
+        })
+        this.updateAllTotalColumns()
+      }
+    })
+  }
+
+  saveUpdatedTeacherNotesSetting = ({showNotes}) => {
+    let promise
+
+    const existingColumn = this.getTeacherNotesColumn()
+    if (existingColumn != null) {
+      promise = GradebookApi.updateTeacherNotesColumn(this.options.context_id, existingColumn.id, {
+        hidden: !showNotes
+      })
+    } else {
+      promise = GradebookApi.createTeacherNotesColumn(this.options.context_id).then(response => {
+        this.gradebookContent.customColumns.push(response.data)
+        const teacherNotesColumn = this.buildCustomColumn(response.data)
+        this.gridData.columns.definitions[teacherNotesColumn.id] = teacherNotesColumn
+      })
+    }
+
+    return promise.then(() => {
+      if (showNotes) {
+        this.showNotesColumn()
+        this.reorderCustomColumns(this.gradebookContent.customColumns.map(c => c.id))
+      } else {
+        this.hideNotesColumn()
+      }
+    })
+  }
+
+  renderSettingsButton() {
+    const iconSettingsSolid = React.createElement(IconSettingsSolid)
+    const buttonMountPoint = document.getElementById('gradebook-settings-modal-button-container')
+    const buttonProps = {
+      icon: iconSettingsSolid,
+      id: 'gradebook-settings-button',
+      variant: 'icon',
+      onClick: () => {
+        let ref1
+        return (ref1 = this.gradebookSettingsModal.current) != null ? ref1.open() : undefined
+      }
+    }
+    const screenReaderContent = React.createElement(
+      ScreenReaderContent,
+      {},
+      I18n.t('Gradebook Settings')
+    )
+    return (this.gradebookSettingsModalButton = renderComponent(
+      Button,
+      buttonMountPoint,
+      buttonProps,
+      screenReaderContent
+    ))
   }
 
   renderStatusesModal() {
@@ -2619,7 +2775,12 @@ class Gradebook extends React.Component {
         colors
       }
     }
-    return $.ajaxJSON(this.options.settings_update_url, 'PUT', data, successFn, errorFn)
+
+    if (this.options.enhanced_gradebook_filters) {
+      return GradebookApi.saveUserSettings(this.options.context_id, data.gradebook_settings)
+    } else {
+      return $.ajaxJSON(this.options.settings_update_url, 'PUT', data, successFn, errorFn)
+    }
   }
 
   // # Grid Sorting Methods
--- ui/features/gradebook/react/default_gradebook/components/__tests__/Gradebook.test.js
+++ ui/features/gradebook/react/default_gradebook/components/__tests__/Gradebook.test.js
@@ -17,10 +17,16 @@
  */
 
 import React from 'react'
-import {defaultGradebookProps} from '../../__tests__/GradebookSpecHelper'
+import {createGradebook, defaultGradebookProps} from '../../__tests__/GradebookSpecHelper'
 import {render, within} from '@testing-library/react'
 import Gradebook from '../../Gradebook'
+import GradebookApi from '../../apis/GradebookApi'
+import * as FlashAlert from '@canvas/alerts/react/FlashAlert'
+
 import '@testing-library/jest-dom/extend-expect'
+
+jest.mock('../../apis/GradebookApi')
+jest.mock('@canvas/alerts/react/FlashAlert')
 
 describe('Gradebook', () => {
   it('GradebookMenu is rendered', () => {
@@ -28,6 +34,386 @@ describe('Gradebook', () => {
     render(<Gradebook {...defaultGradebookProps} gradebookMenuNode={node} />)
     const {getByText} = within(node)
     expect(node).toContainElement(getByText(/Gradebook/i))
+  })
+
+  describe('#handleViewOptionsUpdated', () => {
+    let gradebook
+
+    beforeEach(() => {
+      gradebook = createGradebook({
+        allow_view_ungraded_as_zero: true,
+        context_id: '100',
+        enhanced_gradebook_filters: true,
+        settings: {
+          show_unpublished_assignments: false
+        },
+        view_ungraded_as_zero: false
+      })
+
+      gradebook.gotAllAssignmentGroups([
+        {
+          id: '2201',
+          position: 1,
+          name: 'Assignments',
+          assignments: [
+            {id: '2301', name: 'assignment1', points_possible: 100, published: true},
+            {id: '2302', name: 'assignment2', points_possible: 50, published: true},
+            {id: '2303', name: 'unpublished', points_possible: 1500, published: false}
+          ]
+        }
+      ])
+
+      gradebook.createGrid = jest.fn()
+      gradebook.renderGridColor = jest.fn()
+      gradebook.updateGrid = jest.fn()
+      gradebook.updateAllTotalColumns = jest.fn()
+
+      gradebook.setColumnOrder({sortType: 'due_date', direction: 'ascending'})
+      gradebook.gotCustomColumns([])
+      gradebook.initGrid()
+
+      GradebookApi.createTeacherNotesColumn = jest.fn().mockResolvedValue({
+        data: {
+          id: '9999',
+          hidden: false,
+          name: 'Notes',
+          position: 1,
+          teacher_notes: true
+        }
+      })
+      GradebookApi.saveUserSettings = jest.fn(() => Promise.resolve())
+      GradebookApi.updateColumnOrder = jest.fn(() => Promise.resolve())
+      GradebookApi.updateTeacherNotesColumn = jest.fn(() => Promise.resolve())
+
+      FlashAlert.showFlashError = jest.fn(() => {})
+    })
+
+    describe('when updating column sort settings', () => {
+      it('calls the updateColumnOrder API function with the updated settings', async () => {
+        await gradebook.handleViewOptionsUpdated({
+          columnSortSettings: {criterion: 'points', direction: 'ascending'}
+        })
+
+        expect(GradebookApi.updateColumnOrder).toHaveBeenCalledWith('100', {
+          direction: 'ascending',
+          sortType: 'points',
+          freezeTotalGrade: false
+        })
+      })
+
+      it('does not call updateColumnOrder if the column settings have not changed', async () => {
+        gradebook.setColumnOrder({sortType: 'due_date', direction: 'ascending'})
+        await gradebook.handleViewOptionsUpdated({
+          columnSortSettings: {criterion: 'due_date', direction: 'ascending'}
+        })
+        expect(GradebookApi.updateColumnOrder).not.toHaveBeenCalled()
+      })
+
+      it('sorts the grid columns when the API call completes', async () => {
+        await gradebook.handleViewOptionsUpdated({
+          columnSortSettings: {criterion: 'points', direction: 'ascending'}
+        })
+        expect(gradebook.gridData.columns.scrollable).toEqual([
+          'assignment_2302',
+          'assignment_2301',
+          'assignment_group_2201',
+          'total_grade'
+        ])
+      })
+
+      it('does not sort the grid columns if the API call fails', async () => {
+        expect.hasAssertions()
+        GradebookApi.updateColumnOrder.mockRejectedValue(new Error('no'))
+
+        try {
+          await gradebook.handleViewOptionsUpdated({
+            columnSortSettings: {criterion: 'points', direction: 'ascending'}
+          })
+        } catch {
+          expect(gradebook.gridData.columns.scrollable).toEqual([
+            'assignment_2301',
+            'assignment_2302',
+            'assignment_group_2201',
+            'total_grade'
+          ])
+        }
+      })
+    })
+
+    describe('when updating teacher notes settings', () => {
+      const createExistingNotesColumn = () =>
+        gradebook.gotCustomColumns([
+          {id: '9999', teacher_notes: true, hidden: false, title: 'Notes'}
+        ])
+
+      describe('when the notes column does not exist', () => {
+        it('calls the createTeacherNotesColumn API function if showNotes is true', async () => {
+          await gradebook.handleViewOptionsUpdated({showNotes: true})
+          expect(GradebookApi.createTeacherNotesColumn).toHaveBeenCalledWith('100')
+        })
+
+        it('adds the Notes column to gradebook', async () => {
+          await gradebook.handleViewOptionsUpdated({showNotes: true})
+          expect(gradebook.listVisibleCustomColumns()).toContainEqual(
+            expect.objectContaining({id: '9999'})
+          )
+        })
+
+        it('does not call createTeacherNotesColumn if showNotes is false', async () => {
+          await gradebook.handleViewOptionsUpdated({showNotes: false})
+          expect(GradebookApi.createTeacherNotesColumn).not.toHaveBeenCalled()
+        })
+
+        describe('when the API call completes', () => {
+          it('shows the notes column', async () => {
+            await gradebook.handleViewOptionsUpdated({showNotes: true})
+            expect(gradebook.listVisibleCustomColumns()).toContainEqual(
+              expect.objectContaining({id: '9999'})
+            )
+          })
+        })
+
+        it('does not update the visibility of the notes column if the API call fails', async () => {
+          expect.hasAssertions()
+          GradebookApi.createTeacherNotesColumn.mockRejectedValue(new Error('.'))
+
+          try {
+            await gradebook.handleViewOptionsUpdated({showNotes: true})
+          } catch {
+            expect(gradebook.listVisibleCustomColumns()).not.toContainEqual(
+              expect.objectContaining({id: '9999'})
+            )
+          }
+        })
+      })
+
+      describe('when the notes column already exists', () => {
+        beforeEach(() => {
+          createExistingNotesColumn()
+        })
+
+        it('calls the updateTeacherNotesColumn API function if showNotes changes', async () => {
+          await gradebook.handleViewOptionsUpdated({showNotes: false})
+          expect(GradebookApi.updateTeacherNotesColumn).toHaveBeenCalledWith('100', '9999', {
+            hidden: true
+          })
+        })
+
+        it('does not call updateTeacherNotesColumn if showNotes has not changed', async () => {
+          await gradebook.handleViewOptionsUpdated({showNotes: true})
+          expect(GradebookApi.updateTeacherNotesColumn).not.toHaveBeenCalled()
+        })
+
+        describe('when the API call completes', () => {
+          it('shows the notes column if showNotes was set to true', async () => {
+            gradebook.hideNotesColumn()
+
+            await gradebook.handleViewOptionsUpdated({showNotes: true})
+            expect(gradebook.listVisibleCustomColumns()).toContainEqual(
+              expect.objectContaining({id: '9999'})
+            )
+          })
+
+          it('hides the notes column if showNotes was set to false', async () => {
+            gradebook.showNotesColumn()
+
+            await gradebook.handleViewOptionsUpdated({showNotes: false})
+            expect(gradebook.listVisibleCustomColumns()).not.toContainEqual(
+              expect.objectContaining({id: '9999'})
+            )
+          })
+        })
+
+        it('does not update the visibility of the notes column if the API call fails', async () => {
+          expect.hasAssertions()
+          GradebookApi.updateTeacherNotesColumn.mockRejectedValue(new Error('.'))
+
+          try {
+            await gradebook.handleViewOptionsUpdated({showNotes: false})
+          } catch {
+            expect(gradebook.listVisibleCustomColumns()).not.toContain(
+              expect.objectContaining({id: '9999'})
+            )
+          }
+        })
+      })
+    })
+
+    describe('when updating items stored in user settings', () => {
+      const updateParams = (overrides = {}) => ({
+        showUnpublishedAssignments: false,
+        statusColors: gradebook.getGridColors(),
+        viewUngradedAsZero: false,
+        ...overrides
+      })
+
+      it('calls the saveUserSettings API function with the changed values', async () => {
+        await gradebook.handleViewOptionsUpdated(
+          updateParams({
+            showUnpublishedAssignments: true,
+            statusColors: {...gradebook.getGridColors(), dropped: '#000000'},
+            viewUngradedAsZero: true
+          })
+        )
+
+        expect(GradebookApi.saveUserSettings).toHaveBeenCalledWith(
+          '100',
+          expect.objectContaining({
+            colors: expect.objectContaining({dropped: '#000000'}),
+            show_unpublished_assignments: true,
+            view_ungraded_as_zero: true
+          })
+        )
+      })
+
+      it('does not call saveUserSettings if no value has changed', async () => {
+        await gradebook.handleViewOptionsUpdated(updateParams())
+        expect(GradebookApi.saveUserSettings).not.toHaveBeenCalled()
+      })
+
+      describe('updating showing unpublished assignments', () => {
+        it('shows unpublished assignments when showUnpublishedAssignments is set to true', async () => {
+          await gradebook.handleViewOptionsUpdated(updateParams({showUnpublishedAssignments: true}))
+          expect(gradebook.gridData.columns.scrollable).toContain('assignment_2303')
+        })
+
+        it('hides unpublished assignments when showUnpublishedAssignments is set to false', async () => {
+          gradebook.gridDisplaySettings.showUnpublishedAssignments = true
+          gradebook.setVisibleGridColumns()
+
+          await gradebook.handleViewOptionsUpdated(
+            updateParams({showUnpublishedAssignments: false})
+          )
+          expect(gradebook.gridData.columns.scrollable).not.toContain('assignment_2303')
+        })
+
+        it('does not update the list of visible assignments if the request fails', async () => {
+          expect.hasAssertions()
+          GradebookApi.saveUserSettings.mockRejectedValue(new Error('!'))
+
+          try {
+            await gradebook.handleViewOptionsUpdated(
+              updateParams({showUnpublishedAssignments: true})
+            )
+          } catch {
+            expect(gradebook.gridData.columns.scrollable).not.toContain('assignment_2303')
+          }
+        })
+      })
+
+      describe('updating view ungraded as zero', () => {
+        it('makes updates to the grid when the request completes', async () => {
+          await gradebook.handleViewOptionsUpdated(updateParams({viewUngradedAsZero: true}))
+          expect(gradebook.updateAllTotalColumns).toHaveBeenCalled()
+          expect(gradebook.gridDisplaySettings.viewUngradedAsZero).toBe(true)
+        })
+
+        it('does not make updates to grid if the request fails', async () => {
+          expect.hasAssertions()
+          GradebookApi.saveUserSettings.mockRejectedValue(new Error(':|'))
+
+          try {
+            await gradebook.handleViewOptionsUpdated(updateParams({viewUngradedAsZero: true}))
+          } catch {
+            expect(gradebook.updateAllTotalColumns).not.toHaveBeenCalled()
+            expect(gradebook.gridDisplaySettings.viewUngradedAsZero).toBe(false)
+          }
+        })
+      })
+
+      describe('updating status colors', () => {
+        it('updates the grid colors when the request completes', async () => {
+          const newColors = {...gradebook.getGridColors(), dropped: '#AAAAAA'}
+
+          await gradebook.handleViewOptionsUpdated(updateParams({statusColors: newColors}))
+          expect(gradebook.getGridColors().dropped).toBe('#AAAAAA')
+        })
+
+        it('does not update the grid colors if the request fails', async () => {
+          expect.hasAssertions()
+          GradebookApi.saveUserSettings.mockRejectedValue(new Error('?'))
+
+          const oldColors = gradebook.getGridColors()
+
+          try {
+            await gradebook.handleViewOptionsUpdated(
+              updateParams({statusColors: {dropped: '#AAAAAA'}})
+            )
+          } catch {
+            expect(gradebook.getGridColors()).toEqual(oldColors)
+          }
+        })
+      })
+    })
+
+    it('does not update the grid until all requests complete', async () => {
+      let resolveSettingsRequest
+
+      GradebookApi.saveUserSettings.mockImplementation(
+        () =>
+          new Promise(resolve => {
+            resolveSettingsRequest = resolve
+          })
+      )
+
+      const promise = gradebook.handleViewOptionsUpdated({
+        columnSortSettings: {criterion: 'points', direction: 'ascending'},
+        showNotes: true,
+        showUnpublishedAssignments: true
+      })
+
+      expect(gradebook.updateGrid).not.toHaveBeenCalled()
+
+      resolveSettingsRequest()
+      await promise
+
+      expect(gradebook.updateGrid).toHaveBeenCalled()
+    })
+
+    describe('when updates have completed', () => {
+      describe('when at least one API call has failed', () => {
+        beforeEach(() => {
+          GradebookApi.saveUserSettings.mockRejectedValue(new Error('...'))
+        })
+
+        it('shows a flash error', async () => {
+          expect.hasAssertions()
+
+          try {
+            await gradebook.handleViewOptionsUpdated({
+              columnSortSettings: {criterion: 'points', direction: 'ascending'},
+              showNotes: true,
+              showUnpublishedAssignments: true
+            })
+          } catch {
+            expect(FlashAlert.showFlashError).toHaveBeenCalled()
+          }
+        })
+
+        it('nevertheless updates the grid', async () => {
+          expect.hasAssertions()
+
+          try {
+            await gradebook.handleViewOptionsUpdated({
+              columnSortSettings: {criterion: 'points', direction: 'ascending'},
+              showNotes: true,
+              showUnpublishedAssignments: true
+            })
+          } catch {
+            expect(gradebook.updateGrid).toHaveBeenCalled()
+          }
+        })
+      })
+
+      it('updates the grid if all requests succeeded', async () => {
+        await gradebook.handleViewOptionsUpdated({
+          columnSortSettings: {criterion: 'points', direction: 'ascending'},
+          showNotes: true,
+          showUnpublishedAssignments: true
+        })
+        expect(gradebook.updateGrid).toHaveBeenCalled()
+      })
+    })
   })
 })
 
