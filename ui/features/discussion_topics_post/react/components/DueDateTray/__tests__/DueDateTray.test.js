5ece5a925ffd36df2db89a4f2c5d73765d73cc52
Matthew Lemon <mlemon@instructure.com>
2021-08-25 18:04:24 -0600

    display individual names in due dates tray
    fixes VICE-1535
    flag=react_discussions_post
    
    Test Plan:
    - Navigate to a discussion topic with multiple due dates
    - Makes sure one of the due dates has individual students
      assigned to it rather than just a section or group
    - Open the Due Dates tray
    - Note that you should see the individual students names rather
      than a rolled up count
    - If you have more than 10 students assigned to a due date you
      should see the first 5 students with a 'x more' button
    - When expanded you should see a 'x less' button
    
    Change-Id: Ia4e1412a7c37d196ebaab30f14f55b895b95bb0d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/272271
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- ui/features/discussion_topics_post/react/components/DueDateTray/__tests__/DueDateTray.test.js
+++ ui/features/discussion_topics_post/react/components/DueDateTray/__tests__/DueDateTray.test.js
@@ -16,12 +16,11 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {AdhocStudents, Student} from '../../../../graphql/AdhocStudents'
+import {AssignmentOverride} from '../../../../graphql/AssignmentOverride'
 import {DueDateTray} from '../DueDateTray'
-
+import {fireEvent, render} from '@testing-library/react'
 import React from 'react'
-
-import {render} from '@testing-library/react'
-
 import {responsiveQuerySizes} from '../../../utils'
 
 jest.mock('../../../utils')
@@ -39,30 +38,25 @@ beforeAll(() => {
 })
 
 const overrides = [
-  {
-    id: 'QXNzaWdebTVubC0x',
-    _id: '1',
-    dueAt: '2021-03-30T23:59:59-06:00',
-    lockAt: '2021-04-03T23:59:59-06:00',
-    unlockAt: '2021-03-24T00:00:00-06:00',
-    title: 'assignment override 1'
-  },
-  {
-    id: 'QXMzaWdebTubeC0x',
+  AssignmentOverride.mock({title: 'assignment override 1', adhocStudents: null}),
+  AssignmentOverride.mock({
+    id: 'QXNzaWdubWVudE92ZXJyaWRlLTI=',
     _id: '2',
+    title: 'assignment override 2',
     dueAt: '2021-03-27T23:59:59-06:00',
     lockAt: '2021-04-07T23:59:59-06:00',
     unlockAt: '2021-03-21T00:00:00-06:00',
-    title: 'assignment override 2'
-  },
-  {
-    id: 'BXMzaWdebTVubC0x',
+    adhocStudents: null
+  }),
+  AssignmentOverride.mock({
+    id: 'QXNzaWdubWVudE92ZXJyaWRlLTM=',
     _id: '3',
+    title: 'assignment override 3',
     dueAt: '2021-05-27T23:59:59-06:00',
     lockAt: '2021-09-03T23:59:59-06:00',
     unlockAt: '2021-03-21T00:00:00-06:00',
-    title: 'assignment override 3'
-  }
+    adhocStudents: null
+  })
 ]
 
 const setup = props => {
@@ -77,24 +71,82 @@ describe('AssignmentDetails', () => {
       }))
     })
 
-    it('displays tray and correctly formatted dates', async () => {
-      const {findByText} = setup()
-      expect(await findByText('Due Dates')).toBeTruthy()
-      expect(await findByText('Mar 31 5:59am')).toBeTruthy()
-      expect(await findByText('assignment override 1')).toBeTruthy()
-      expect(await findByText('Mar 24 6am')).toBeTruthy()
-      expect(await findByText('Apr 4 5:59am')).toBeTruthy()
+    it('displays tray and correctly formatted dates', () => {
+      const {getByText} = setup()
+      expect(getByText('Due Dates')).toBeInTheDocument()
+      expect(getByText('Mar 31 5:59am')).toBeInTheDocument()
+      expect(getByText('assignment override 1')).toBeInTheDocument()
+      expect(getByText('Mar 24 6am')).toBeInTheDocument()
+      expect(getByText('Apr 4 5:59am')).toBeInTheDocument()
     })
 
-    it('correct text is shown when a date is not set', async () => {
-      const nullOverrides = overrides
-      nullOverrides[2].dueAt = null
-      nullOverrides[2].unlockAt = null
-      nullOverrides[2].lockAt = null
-      const {findByText} = setup({assignmentOverrides: nullOverrides})
-      expect(await findByText('No Due Date')).toBeTruthy()
-      expect(await findByText('No Start Date')).toBeTruthy()
-      expect(await findByText('No End Date')).toBeTruthy()
+    it('correct text is shown when a date is not set', () => {
+      const {getByText} = setup({
+        assignmentOverrides: [
+          AssignmentOverride.mock({
+            dueAt: null,
+            unlockAt: null,
+            lockAt: null
+          })
+        ]
+      })
+      expect(getByText('No Due Date')).toBeInTheDocument()
+      expect(getByText('No Start Date')).toBeInTheDocument()
+      expect(getByText('No End Date')).toBeInTheDocument()
+    })
+
+    it('renders the students names if present', () => {
+      const {getByText} = setup({assignmentOverrides: [AssignmentOverride.mock()]})
+      expect(
+        getByText(
+          AdhocStudents.mock()
+            .students.map(student => student.shortName)
+            .join(', ')
+        )
+      ).toBeInTheDocument()
+    })
+
+    it('truncates the student names if there are more than 10', () => {
+      const students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(val =>
+        Student.mock({shortName: `Student${val}`})
+      )
+      const {getByText} = setup({
+        assignmentOverrides: [
+          AssignmentOverride.mock({
+            adhocStudents: AdhocStudents.mock({
+              students
+            })
+          })
+        ]
+      })
+      expect(
+        getByText(
+          students
+            .slice(0, 5)
+            .map(student => student.shortName)
+            .join(', ')
+        )
+      ).toBeInTheDocument()
+      expect(getByText('...')).toBeInTheDocument()
+      expect(getByText('6 more')).toBeInTheDocument()
+    })
+
+    it('allows expanding the student names if there are more than 10', () => {
+      const students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(val =>
+        Student.mock({shortName: `Student${val}`})
+      )
+      const {getByText} = setup({
+        assignmentOverrides: [
+          AssignmentOverride.mock({
+            adhocStudents: AdhocStudents.mock({
+              students
+            })
+          })
+        ]
+      })
+      fireEvent.click(getByText('6 more'))
+      expect(getByText(students.map(student => student.shortName).join(', '))).toBeInTheDocument()
+      expect(getByText('6 less')).toBeInTheDocument()
     })
   })
 
@@ -105,23 +157,27 @@ describe('AssignmentDetails', () => {
       }))
     })
 
-    it('displays tray and correctly formatted dates', async () => {
-      const {findByText} = setup()
-      expect(await findByText('Mar 31 5:59am')).toBeTruthy()
-      expect(await findByText('assignment override 1')).toBeTruthy()
-      expect(await findByText('Mar 24 6am')).toBeTruthy()
-      expect(await findByText('Apr 4 5:59am')).toBeTruthy()
+    it('displays tray and correctly formatted dates', () => {
+      const {getByText} = setup()
+      expect(getByText('Mar 31 5:59am')).toBeInTheDocument()
+      expect(getByText('assignment override 1')).toBeInTheDocument()
+      expect(getByText('Mar 24 6am')).toBeInTheDocument()
+      expect(getByText('Apr 4 5:59am')).toBeInTheDocument()
     })
 
-    it('correct text is shown when a date is not set', async () => {
-      const nullOverrides = overrides
-      nullOverrides[2].dueAt = null
-      nullOverrides[2].unlockAt = null
-      nullOverrides[2].lockAt = null
-      const {findByText} = setup({assignmentOverrides: nullOverrides})
-      expect(await findByText('No Due Date')).toBeTruthy()
-      expect(await findByText('No Start Date')).toBeTruthy()
-      expect(await findByText('No End Date')).toBeTruthy()
+    it('correct text is shown when a date is not set', () => {
+      const {getByText} = setup({
+        assignmentOverrides: [
+          AssignmentOverride.mock({
+            dueAt: null,
+            unlockAt: null,
+            lockAt: null
+          })
+        ]
+      })
+      expect(getByText('No Due Date')).toBeInTheDocument()
+      expect(getByText('No Start Date')).toBeInTheDocument()
+      expect(getByText('No End Date')).toBeInTheDocument()
     })
   })
 })
