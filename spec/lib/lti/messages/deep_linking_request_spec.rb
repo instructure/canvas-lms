c3599d9ab05c181b75740657a75802d56b067269
Xander Moffatt <xmoffatt@instructure.com>
2021-09-03 11:22:24 -0600

    simplify deep_linking_request specs
    refs INTEROP-6833
    flag=none
    
    why:
    * use shared examples instead of repetitive copy paste
    * in prep for adding more placements that support deep linking
    
    test plan:
    * specs still work
    
    Change-Id: I552a2696de7b72ab0494511084a0e571b7c9920b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/272921
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>

--- spec/lib/lti/messages/deep_linking_request_spec.rb
+++ spec/lib/lti/messages/deep_linking_request_spec.rb
@@ -20,7 +20,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper.rb')
 require File.expand_path(File.dirname(__FILE__) + '/lti_advantage_shared_examples')
 
-
 describe Lti::Messages::DeepLinkingRequest do
   include_context 'lti_advantage_shared_examples'
 
@@ -48,237 +47,123 @@ describe Lti::Messages::DeepLinkingRequest do
       expect(subject['deep_link_return_url']).to eq deep_linking_return_url
     end
 
-    context 'when resource type is "collaboration"' do
-      let(:opts) {{resource_type: 'collaboration'}}
-
+    shared_examples_for 'sets deep linking attributes' do
       it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(
-          ltiResourceLink
-        )
+        expect(subject['accept_types']).to match_array accept_types
       end
 
       it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(iframe)
+        expect(
+          subject['accept_presentation_document_targets']
+        ).to match_array accept_presentation_document_targets
       end
 
       it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
-          'application/vnd.ims.lti.v1.ltilink'
-        )
+        expect(subject['accept_media_types']).to eq accept_media_types
       end
 
       it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq true
+        expect(subject['auto_create']).to eq auto_create
       end
 
-      it 'sets "accept_multiple to false"' do
-        expect(subject['accept_multiple']).to eq false
+      it 'sets "accept_multiple"' do
+        expect(subject['accept_multiple']).to eq accept_multiple
+      end
+    end
+
+    context 'when resource type is "collaboration"' do
+      let(:opts) { { resource_type: 'collaboration' } }
+
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[ltiResourceLink] }
+        let(:accept_presentation_document_targets) { %w[iframe] }
+        let(:accept_media_types) { 'application/vnd.ims.lti.v1.ltilink' }
+        let(:auto_create) { true }
+        let(:accept_multiple) { false }
       end
     end
 
     context 'when resource type is "link_selection"' do
-      let(:opts) {{resource_type: 'link_selection'}}
+      let(:opts) { { resource_type: 'link_selection' } }
 
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(
-          ltiResourceLink
-        )
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(
-          iframe
-          window
-        )
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
-          'application/vnd.ims.lti.v1.ltilink'
-        )
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets "accept_multiple to true"' do
-        expect(subject['accept_multiple']).to eq true
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[ltiResourceLink] }
+        let(:accept_presentation_document_targets) { %w[iframe window] }
+        let(:accept_media_types) { 'application/vnd.ims.lti.v1.ltilink' }
+        let(:auto_create) { false }
+        let(:accept_multiple) { true }
       end
     end
 
     context 'when resource type is "assignment_selection"' do
-      let(:opts) {{resource_type: 'assignment_selection'}}
+      let(:opts) { { resource_type: 'assignment_selection' } }
 
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(
-          ltiResourceLink
-        )
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(
-          iframe
-          window
-        )
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
-          'application/vnd.ims.lti.v1.ltilink'
-        )
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets the correct "accept_multiple"' do
-        expect(subject['accept_multiple']).to eq false
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[ltiResourceLink] }
+        let(:accept_presentation_document_targets) { %w[iframe window] }
+        let(:accept_media_types) { 'application/vnd.ims.lti.v1.ltilink' }
+        let(:auto_create) { false }
+        let(:accept_multiple) { false }
       end
     end
 
     context 'when resource type is "homework_submission"' do
-      let(:opts) {{resource_type: 'homework_submission'}}
+      let(:opts) { { resource_type: 'homework_submission' } }
 
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(file ltiResourceLink)
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(iframe)
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq('*/*')
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets the correct "accept_multiple"' do
-        expect(subject['accept_multiple']).to eq false
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[file ltiResourceLink] }
+        let(:accept_presentation_document_targets) { %w[iframe] }
+        let(:accept_media_types) { '*/*' }
+        let(:auto_create) { false }
+        let(:accept_multiple) { false }
       end
     end
 
     context 'when resource type is "migration_selection"' do
-      let(:opts) {{resource_type: 'migration_selection'}}
+      let(:opts) { { resource_type: 'migration_selection' } }
 
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(file)
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(iframe)
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[file] }
+        let(:accept_presentation_document_targets) { %w[iframe] }
+        let(:accept_media_types) do
           'application/vnd.ims.imsccv1p1,application/vnd.ims.imsccv1p2,application/vnd.ims.imsccv1p3,application/zip,application/xml'
-        )
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets the correct "accept_multiple"' do
-        expect(subject['accept_multiple']).to eq false
+        end
+        let(:auto_create) { false }
+        let(:accept_multiple) { false }
       end
     end
 
     context 'when resource type is "editor_button"' do
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(
-          link
-          file
-          html
-          ltiResourceLink
-          image
-        )
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(
-          embed
-          iframe
-          window
-        )
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
-          'image/*,text/html,application/vnd.ims.lti.v1.ltilink,*/*'
-        )
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets "accept_multiple" to true ' do
-        expect(subject['accept_multiple']).to eq true
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[link file html ltiResourceLink image] }
+        let(:accept_presentation_document_targets) { %w[embed iframe window] }
+        let(:accept_media_types) { 'image/*,text/html,application/vnd.ims.lti.v1.ltilink,*/*' }
+        let(:auto_create) { false }
+        let(:accept_multiple) { true }
       end
     end
 
     context 'when resource type is "conference_selection"' do
-      let(:opts) {{resource_type: 'conference_selection'}}
+      let(:opts) { { resource_type: 'conference_selection' } }
 
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(
-          html
-          link
-        )
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(
-          iframe
-          window
-        )
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq(
-          'text/html,*/*'
-        )
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq true
-      end
-
-      it 'sets "accept_multiple" to true ' do
-        expect(subject['accept_multiple']).to eq false
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[html link] }
+        let(:accept_presentation_document_targets) { %w[iframe window] }
+        let(:accept_media_types) { 'text/html,*/*' }
+        let(:auto_create) { true }
+        let(:accept_multiple) { false }
       end
     end
 
     context 'when resource type is "course_assignments_menu"' do
-      let(:opts) {{resource_type: 'course_assignments_menu'}}
+      let(:opts) { { resource_type: 'course_assignments_menu' } }
 
-      before :once do
-        Account.site_admin.enable_feature!(:lti_multiple_assignment_deep_linking)
-      end
-
-      it 'sets "accept_multiple" to true ' do
-        expect(subject['accept_multiple']).to eq true
-      end
-
-      it 'sets the correct "accept_types"' do
-        expect(subject['accept_types']).to match_array %w(ltiResourceLink)
-      end
-
-      it 'sets the correct "auto_create"' do
-        expect(subject['auto_create']).to eq false
-      end
-
-      it 'sets the correct "accept_presentation_document_targets"' do
-        expect(subject['accept_presentation_document_targets']).to match_array %w(iframe window)
-      end
-
-      it 'sets the correct "accept_media_types"' do
-        expect(subject['accept_media_types']).to eq 'application/vnd.ims.lti.v1.ltilink'
+      it_behaves_like 'sets deep linking attributes' do
+        let(:accept_types) { %w[ltiResourceLink] }
+        let(:accept_presentation_document_targets) { %w[iframe window] }
+        let(:accept_media_types) { 'application/vnd.ims.lti.v1.ltilink' }
+        let(:auto_create) { false }
+        let(:accept_multiple) { true }
       end
     end
   end
