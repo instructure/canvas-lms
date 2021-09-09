296a61bda5a359084460245ef07e5574f170dc0f
Rob Orton <rob@instructure.com>
2021-09-08 21:02:29 -0600

    set dashcard js_env variables before branching on ff
    flag=react_discussions_post
    
    Change-Id: I698b6817a4b8a04dc91f66e5df8ff11ec2e47965
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/273200
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- app/controllers/discussion_topics_controller.rb
+++ app/controllers/discussion_topics_controller.rb
@@ -651,6 +651,10 @@ class DiscussionTopicsController < ApplicationController
       @topic.change_read_state('read', @current_user) unless @locked.is_a?(Hash) && !@locked[:can_view]
     end
 
+    if @context.is_a?(Course) && @context.grants_right?(@current_user, session, :manage)
+      set_student_context_cards_js_env
+    end
+
     # Render updated Post UI if feature flag is enabled
     if @context.feature_enabled?(:react_discussions_post)
       topics = groups_and_group_topics if @topic.for_group_discussion?
@@ -807,9 +811,6 @@ class DiscussionTopicsController < ApplicationController
             end
             js_hash[:COURSE_ID] = @context.id if @context.is_a?(Course)
             js_hash[:CONTEXT_ACTION_SOURCE] = :discussion_topic
-            if @context.is_a?(Course) && @context.grants_right?(@current_user, session, :manage)
-              set_student_context_cards_js_env
-            end
 
             append_sis_data(js_hash)
             js_env(js_hash)
