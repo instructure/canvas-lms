642a9f6e595ee8605ded71221a23d8d2018b8809
coder0107git <76132013+coder0107git@users.noreply.github.com>
2021-09-14 17:05:48 -0600

    Delete lib directory

--- lib/active_support/cache/delif.lua
+++
@@ -1,6 +0,0 @@
-if redis.call("get",KEYS[1]) == ARGV[1]
-then
-    return redis.call("del",KEYS[1])
-else
-    return 0
-end
--- lib/tasks/pact.rake
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-if Gem.loaded_specs.has_key?('pact')
-  require 'pact/tasks'
-end
