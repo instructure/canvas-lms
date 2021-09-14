3413aa4a551ced3c0890a9c26cf9253c8a87abd1
coder0107git <76132013+coder0107git@users.noreply.github.com>
2021-09-14 17:03:08 -0600

    Delete db directory

--- db/migrate/20190312131154_make_account_notification_users_not_null.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class MakeAccountNotificationUsersNotNull < ActiveRecord::Migration[5.1]
-  tag :postdeploy
-
-  def change
-    change_column_null :account_notifications, :user_id, false
-  end
-end
