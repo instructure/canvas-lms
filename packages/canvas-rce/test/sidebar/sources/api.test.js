0c2bf3831725a3dcea298c21d64c5915a5b64f67
Weston Dransfield <wdransfield@instructure.com>
2021-09-14 15:12:02 -0600

    Dedup "onDuplicate" params in Button & Icons
    refs MAT-182
    flag=rce_buttons_and_icons
    
    Test Plan:
    - Edit and button & icon
    > Verify the "upload" request made to RCS
      only includes one instance of "onDuplicate"
      parameter
    
    Change-Id: I961514a79eb947e26387ff7ddc8e0fd948f01285
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/273579
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jon Scheiding <jon.scheiding@instructure.com>
    QA-Review: Jon Scheiding <jon.scheiding@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

--- packages/canvas-rce/test/sidebar/sources/api.test.js
+++ packages/canvas-rce/test/sidebar/sources/api.test.js
@@ -548,10 +548,10 @@ describe('sources/api', () => {
     const fileProps = {}
     const apiProps = {}
 
-    it('includes "onDuplicate"', () => {
+    it.only('includes "onDuplicate"', () => {
       fetchMock.mock(uri, '{}')
 
-      return apiSource.preflightUpload({onDuplicate: 'overwrite'}, apiProps).then(() => {
+      return apiSource.preflightUpload(fileProps, {onDuplicate: 'overwrite'}, apiProps).then(() => {
         const body = JSON.parse(fetchMock.lastOptions(uri).body)
         assert.equal(body.onDuplicate, 'overwrite')
       })
