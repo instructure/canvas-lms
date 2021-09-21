3a249bcf76ce863c376ed60bc19cda0e0a9287da
Simon Williams <simon@instructure.com>
2021-09-21 14:16:33 -0500

    allow new quiz publish/unpublish with granular assignment flags
    fixes QUIZ-8868
    flag = granular_permissions_manage_assignments
    
    test plan:
    - enable the feature flag
    - try to publish or publish a new quiz from the quiz index page
    - it should work
    
    Change-Id: Id8b376e266ffcf7371378520bee6d3e3255ef57c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274032
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -794,7 +794,7 @@ class AssignmentsController < ApplicationController
 
   # pulish a N.Q assignment from Quizzes Page
   def publish_quizzes
-    if authorized_action(@context, @current_user, :manage_assignments)
+    if authorized_action(@context, @current_user, [:manage_assignments, :manage_assignments_edit])
       @assignments = @context.assignments.active.where(id: params[:quizzes])
       @assignments.each(&:publish!)
 
@@ -812,7 +812,7 @@ class AssignmentsController < ApplicationController
 
   # unpulish a N.Q assignment from Quizzes Page
   def unpublish_quizzes
-    if authorized_action(@context, @current_user, :manage_assignments)
+    if authorized_action(@context, @current_user, [:manage_assignments, :manage_assignments_edit])
       @assignments = @context.assignments.active.where(id: params[:quizzes], workflow_state: 'published')
       @assignments.each(&:unpublish!)
 
