ba5d00cb9978cbcdd438dc2c0e3ef33be20b2369
Xander Moffatt <xmoffatt@instructure.com>
2021-09-22 12:23:27 -0600

    support lti.get_data postMessage
    refs INTEROP-7087
    flag=lti_platform_storage
    
    why:
    * part of the IMS LTI Platform Storage proposal
    * allows tools to store and fetch data in canvas local storage, since
    tool frame cookies and local storage is disabled in some browsers
    
    test plan:
    * send an lti.put_data postMessage with the correct subject, key, and
    value
    * in the dev tools Storage/Application tab, look in local storage and
    see the key and value
    * send an lti.get_data postMessage with the correct subject and key
    * you should receive a postMessage with the key and value
    
    Change-Id: Ib0f133f19f8bb3068e24155d39f544ac1b0428c9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274218
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>

--- ui/shared/lti/jquery/messages.js
+++ ui/shared/lti/jquery/messages.js
@@ -40,6 +40,7 @@ const SUBJECT_ALLOW_LIST = [
   'lti.showAlert',
   'lti.showModuleNavigation',
   'org.imsglobal.lti.capabilities',
+  'org.imsglobal.lti.get_data',
   'requestFullWindowLaunch',
   'toggleCourseNavigationMenu'
 ]
