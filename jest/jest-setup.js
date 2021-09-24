0f51de4ed3a4e3f7d62cc277d062b6a94a29ecd5
Davis Hyer <dhyer@instructure.com>
2021-09-24 15:50:48 -0600

    provide correct firstAnnouncement prop type
    flag=none
    
    test plan:
      - specs pass
    
    qa risk: low
    
    Change-Id: Ib4b764ced6a09b0630388557ff636aca79deac2a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274444
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: Rob Orton <rob@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -90,7 +90,6 @@ const ignoredErrors = [
   /The prop `courseId` is marked as required in `(LatestAnnouncementLink|PublishButton)`/,
   /The prop `currentUserRoles` is marked as required in `ObserverOptions`/,
   /The prop `dateTime` is marked as required in `FriendlyDatetime`/,
-  /The prop `firstAnnouncement.id` is marked as required in `K5Announcement`/,
   /The prop `focusOnInit` is marked as required in `(FileUpload|TextEntry|UrlEntry)`/,
   /The prop `groupTitle` is marked as required in `(GroupMoveModal|GroupRemoveModal|SearchBreadcrumb)`/,
   /The prop `hasLoaded` is marked as required in `HistoryList`/,
