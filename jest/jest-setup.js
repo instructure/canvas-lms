b14709fbae9fd865e51b79c18ffd65b35ccefd92
Davis Hyer <dhyer@instructure.com>
2021-09-24 09:48:44 -0600

    provide correct postedAt prop type
    flag=none
    
    test plan:
      - specs pass
    
    qa risk: low
    
    Change-Id: Id06f390ffabea0642ed67fc7c60dfabc125e487a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274379
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -64,7 +64,6 @@ const ignoredErrors = [
   /Invalid prop `conversation._id` of type `number` supplied to `ComposeModalManager`/,
   /Invalid prop `editorOptions.plugins` of type `string` supplied to `(ForwardRef|RCEWrapper)`/, // https://instructure.atlassian.net/browse/MAT-453
   /Invalid prop `editorOptions.toolbar\[0\]` of type `string` supplied to `(ForwardRef|RCEWrapper)`/, // https://instructure.atlassian.net/browse/MAT-453
-  /Invalid prop `firstAnnouncement.postedDate` of type `String` supplied to `K5Announcement`/,
   /Invalid prop `heading` of type `object` supplied to `Billboard`/, // https://instructure.atlassian.net/browse/QUIZ-8870
   /Invalid prop `headingAs` of value `h4` supplied to `Billboard`/,
   /Invalid prop `id` of type `number` supplied to `Option`/,
