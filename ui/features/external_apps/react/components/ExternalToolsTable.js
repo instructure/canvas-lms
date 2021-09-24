a3df3175b5460f52930d2e59b90db49ae39df461
August Thornton <august@instructure.com>
2021-09-24 11:25:21 -0600

    add granular permissions for managing LTI tools
    closes FOO-2397
    refs FOO-2396
    flag = granular_permissions_manage_lti
    
    Test plan:
    • At a root account level with the feature flag for:
      "managing LTI tools in Account or Course contexts"
      disabled
    • Verify against Beta / Prod that behavior around
      permissions to add / edit / delete LTI tools remains
      unchanged
    • With the feature flag *Enabled*
    • Verify Manage LTI Add/Edit/Delete is on by default for
      the following role types: TeacherEnrollment, TaEnrollment
      DesignerEnrollment, AccountAdmin
    • Navigate to Account or Course context settings --> Apps tab
    • Verify you are unable to *Add* LTI apps via the |+App|
      button (shouldn't be visible) *without* manage_lti_add
      permission toggled on
    • Verify you are unable to *Edit* LTI apps via the COG menu
      dropdown button (shouldn't be visible) *without*
      manage_lti_edit permission toggled on
        • Note: you are no longer able to edit placements, as this
          ability is tied to the manage_lti_edit permission
    • Verify you are unable to *Delete* LTI apps via the COG menu
      dropdown button (shouldn't be visible) *without*
      manage_lti_delete permission toggled on
    • Verify things work as you would expect with all the manage_lti
      permissions toggled on
    • The only permission tied to the API is on tool creation/config,
      which uses :create_tool_manually for legacy FF off or
      :manage_lti_add for granular FF flipped on
    
    Change-Id: I10be8d2fb88503b0d9a76a9eadc9280895048f8f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274399
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ahmad Amireh <ahmad@instructure.com>
    QA-Review: Ahmad Amireh <ahmad@instructure.com>
    Product-Review: Ahmad Amireh <ahmad@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>

--- ui/features/external_apps/react/components/ExternalToolsTable.js
+++ ui/features/external_apps/react/components/ExternalToolsTable.js
@@ -32,6 +32,9 @@ import splitAssetString from '@canvas/util/splitAssetString'
 
 export default class ExternalToolsTable extends React.Component {
   static propTypes = {
+    canAdd: PropTypes.bool.isRequired,
+    canEdit: PropTypes.bool.isRequired,
+    canDelete: PropTypes.bool.isRequired,
     canAddEdit: PropTypes.bool.isRequired,
     setFocusAbove: PropTypes.func.isRequired
   }
@@ -94,6 +97,9 @@ export default class ExternalToolsTable extends React.Component {
           key={tool.app_id}
           ref={this.setToolRowRef(tool)}
           tool={tool}
+          canAdd={this.props.canAdd}
+          canEdit={this.props.canEdit}
+          canDelete={this.props.canDelete}
           canAddEdit={this.props.canAddEdit}
           setFocusAbove={this.setFocusAbove(t)}
           favoriteCount={rceFavCount}
@@ -110,7 +116,7 @@ export default class ExternalToolsTable extends React.Component {
     const show_lti_favorite_toggles =
       /^account_/.test(ENV.context_asset_string) &&
       !ENV.ACCOUNT?.site_admin &&
-      this.props.canAddEdit
+      (this.props.canAdd || this.props.canEdit || this.props.canDelete || this.props.canAddEdit)
 
     return (
       <div className="ExternalToolsTable">
