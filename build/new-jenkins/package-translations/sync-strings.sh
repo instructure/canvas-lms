17d512c664538646741d70cdb30d65c590a86948
Kyle Rosenbaum <krosenbaum@instructure.com>
2021-09-27 15:25:33 -0700

    cleanup env variable interpolation
    [use-rspecq]
    
    Change-Id: I6eb610e9dda006c2acb5f394dc99777e7bd4331e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274578
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- build/new-jenkins/package-translations/sync-strings.sh
+++ build/new-jenkins/package-translations/sync-strings.sh
@@ -9,7 +9,7 @@ set -x -o errexit -o errtrace -o nounset -o pipefail
 ##
 
 export AWS_ROLE_ARN="arn:aws:iam::307761260553:role/translations-jenkins"
-export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -l '$SSH_USERNAME'"
+export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -l $SSH_USERNAME'
 
 "$(yarn bin)/sync-translations" --ignore-jira --config ./package-translations/sync-config.json
 
@@ -38,7 +38,7 @@ popd
 git config --global user.name "Jenkins"
 git config --global user.email "svc.cloudjenkins@instructure.com"
 
-gitdir=$(git rev-parse --git-dir); scp -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -p -P 29418 "${SSH_USERNAME}@gerrit.instructure.com:hooks/commit-msg" "${gitdir}/hooks/"
+gitdir=$(git rev-parse --git-dir); scp -o StrictHostKeyChecking=no -i /usr/src/sshkeyfile -p -P 29418 '${SSH_USERNAME}@gerrit.instructure.com:hooks/commit-msg' "${gitdir}/hooks/"
 # Commit any changes into a temp branch then push to gerrit
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout -B sync-translations-tmp && \
