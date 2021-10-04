561441d3daf2c2ee2ea41612742500b068557671
Isaac Moore <isaac.moore@instructure.com>
2021-10-04 11:39:03 -0500

    Improve date validation for Pace Plans projection dates
    This change improves the date validation and error handling logic in the
    projection date input controls. It shows an appropriate message if a
    weekend date is selected (but removes the previous behavior of resetting
    the newly selected date to the closest non-weekend date). It also shows
    appropriate messaging if the start date crosses the end date (if one is
    defined).
    
    closes LS-2638, LS-2710
    flag=pace_plans
    
    test plan:
    - enable the `pace_plans` feature flag (and course setting)
    - navigate to `/pace_plans` for the course
    - select 'Show Projections' to reveal the start date field
    - select the settings cog, and enable the 'Skip Weekends' setting
    - select the start date field, and expect weekends not to be selectable
    - disable the Skip Weekends setting
    - select a weekend date in the start date field
    - expect not to see an error message
    - re-enable the Skip Weekends setting
    - expect to see an error message associated with the start date field
    - refresh the page, and expect the error to persist
    - change the date to a non-weekend date (or re-disable Skip Weekends),
      and expect the error to disappear
    
    Change-Id: I348e5092b4bfc73a86713340072e9b156576fc14
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/275114
    Reviewed-by: Jeff Largent <jeff.largent@instructure.com>
    QA-Review: Jeff Largent <jeff.largent@instructure.com>
    Product-Review: Peyton Craighill <pcraighill@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/pace_plans/react/components/header/projected_dates/date_selector.tsx
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback} from 'react'
+import {connect} from 'react-redux'
+// @ts-ignore: TS doesn't understand i18n scoped imports
+import I18n from 'i18n!pace_plan_date_selector'
+import moment from 'moment-timezone'
+
+import PacePlanDateInput from '../../../shared/components/pace_plan_date_input'
+import {StoreState, PacePlan} from '../../../types'
+import {BlackoutDate} from '../../../shared/types'
+import {getPacePlan, getProjectedEndDate, getExcludeWeekends} from '../../../reducers/pace_plans'
+import {getBlackoutDates} from '../../../shared/reducers/blackout_dates'
+import {pacePlanActions as actions} from '../../../actions/pace_plans'
+
+type StoreProps = {
+  pacePlan: PacePlan
+  projectedEndDate?: string
+  weekendsDisabled?: boolean
+  blackoutDates: BlackoutDate[]
+}
+
+type DispatchProps = {
+  setStartDate: typeof actions.setStartDate
+  setEndDate: typeof actions.setEndDate
+}
+
+type PassedProps = {
+  type: 'start' | 'end'
+}
+
+export type PacePlanDateSelectorProps = StoreProps & DispatchProps & PassedProps
+
+export const PacePlanDateSelector = (props: PacePlanDateSelectorProps) => {
+  const startType = props.type === 'start'
+  const enrollmentType = props.pacePlan.context_type === 'Enrollment'
+
+  const validateDay = useCallback(
+    (date: moment.Moment) => {
+      const crossesOtherDate = startType
+        ? date > moment(props.pacePlan.end_date)
+        : date < moment(props.pacePlan.start_date)
+
+      const errors: string[] = []
+      if (crossesOtherDate)
+        errors.push(
+          startType
+            ? I18n.t('The start date for the pace plan must be after the end date.')
+            : I18n.t('The end date for the pace plan must be after the start date.')
+        )
+
+      return errors.join(' ')
+    },
+    [startType, props.pacePlan.start_date, props.pacePlan.end_date]
+  )
+
+  const interaction = enrollmentType || !startType ? 'readonly' : 'enabled'
+
+  const label = startType
+    ? enrollmentType
+      ? I18n.t('Start Date')
+      : I18n.t('Projected Start Date')
+    : enrollmentType
+    ? I18n.t('End Date')
+    : I18n.t('Projected End Date')
+
+  return (
+    <PacePlanDateInput
+      interaction={interaction}
+      dateValue={startType ? props.pacePlan.start_date : props.projectedEndDate}
+      onDateChange={startType ? props.setStartDate : props.setEndDate}
+      validateDay={validateDay}
+      label={label}
+      width="14rem"
+      blackoutDates={props.blackoutDates}
+      weekendsDisabled={props.weekendsDisabled}
+    />
+  )
+}
+
+const mapStateToProps = (state: StoreState) => {
+  return {
+    pacePlan: getPacePlan(state),
+    weekendsDisabled: getExcludeWeekends(state),
+    blackoutDates: getBlackoutDates(state),
+    projectedEndDate: getProjectedEndDate(state)
+  }
+}
+
+export default connect(mapStateToProps, {
+  setStartDate: actions.setStartDate,
+  setEndDate: actions.setEndDate
+})(PacePlanDateSelector)
