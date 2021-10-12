c7b3c7c4d7f5f5dedf600eaa334b865ccb3b2e36
Drake Harper <drake.harper@instructure.com>
2021-10-12 13:09:00 -0600

    Fix announcement message not showing for students
    refs VICE-1995
    flag=react_discussions_post
    
    Test Plan:
     - turn on discussion redesign ff
     - create an announcement with a description
     - enter student view
     - should see announcement body
    
    Change-Id: I9a1eff47093d0f7a89fcddbd8f8f8b4378c14ee7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/275773
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/graphql/types/discussion_type.rb
+++ app/graphql/types/discussion_type.rb
@@ -63,7 +63,7 @@ module Types
 
     field :message, String, null: true
     def message
-      object.message unless object.locked_for?(current_user, check_policies: true)
+      object.message
     end
 
     field :locked_for_user, Boolean, null: false
