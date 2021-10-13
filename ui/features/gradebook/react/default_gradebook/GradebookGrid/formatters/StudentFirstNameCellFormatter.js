c299fba47a19491f041ad386a0840205eb3ced4a
Syed Hussain <shussain@instructure.com>
2021-10-13 12:54:37 -0500

    Show first and last names columns in gradebook
    Last name column has all the same fields as the original student
    column. The first name column only shows the first name.
    
    closes EVAL-1990
    flag=gradebook_show_first_last_names
    
    Test plan:
    With the FF on and the account admin setting to show separate columns
    enabled, as a teacher go to gradebook:
    
    1. Select the "Split Student Names" view option
    - See that the gradebook grid shows separate columns for student last and
    first names
    - Make sure all the column options in the last name column work "Sort by",
    "Secondary Info" and "Show - Inactive/Concluded enrollments" and the
    option to "Display as" is not there
    - See that there are no column options for first name
    - See that the assignment and student search work as before
    2. Unselect the "Split Student Names" view option
    - See that the grid shows the Student column as before
    
    Change-Id: Ic3ffefcd2069eca84823d60bb993d98521c8861d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/276013
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Adrian Packel <apackel@instructure.com>
    QA-Review: Dustin Cowles <dustin.cowles@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- ui/features/gradebook/react/default_gradebook/GradebookGrid/formatters/StudentFirstNameCellFormatter.js
+++ ui/features/gradebook/react/default_gradebook/GradebookGrid/formatters/StudentFirstNameCellFormatter.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 - present Instructure, Inc.
+ * Copyright (C) 2021 - present Instructure, Inc.
  *
  * This file is part of Canvas.
  *
@@ -16,18 +16,16 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import $ from 'jquery'
 import I18n from 'i18n!gradebook'
 import '@canvas/jquery/jquery.instructure_misc_helpers' // $.toSentence
-import {
-  getSecondaryDisplayInfo,
-  getEnrollmentLabel,
-  getOptions,
-  renderCell} from './StudentCellFormatter.utils'
+import htmlEscape from 'html-escape'
+import {getEnrollmentLabel, renderCell} from './StudentCellFormatter.utils'
 
-export default class StudentCellFormatter {
+export default class StudentFirstNameCellFormatter {
   constructor(gradebook) {
-    this.options = getOptions(gradebook)
+    this.options = {
+      courseId: gradebook.options.context_id
+    }
   }
 
   render = (_row, _cell, _value, _columnDef, student /* dataContext */) => {
@@ -35,14 +33,10 @@ export default class StudentCellFormatter {
       return ''
     }
 
-    const primaryInfo = this.options.getSelectedPrimaryInfo()
-    const secondaryInfo = this.options.getSelectedSecondaryInfo()
-
     const options = {
       courseId: this.options.courseId,
-      displayName: primaryInfo === 'last_first' ? student.sortable_name : student.name,
+      displayName: student.first_name,
       enrollmentLabel: getEnrollmentLabel(student),
-      secondaryInfo: getSecondaryDisplayInfo(student, secondaryInfo, this.options),
       studentId: student.id,
       url: `${student.enrollments[0].grades.html_url}#tab-assignments`
     }
---
+++ ui/features/gradebook/react/default_gradebook/GradebookGrid/headers/StudentFirstNameColumnHeader.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {Grid} from '@instructure/ui-grid'
+import {View} from '@instructure/ui-view'
+
+import {Text} from '@instructure/ui-text'
+import I18n from 'i18n!gradebook'
+import ColumnHeader from './ColumnHeader'
+
+export default class StudentFirstNameColumnHeader extends ColumnHeader {
+  static propTypes = {
+    ...ColumnHeader.propTypes
+  }
+
+  static defaultProps = {
+    ...ColumnHeader.defaultProps
+  }
+
+  render() {
+    return (
+      <div
+        className={`Gradebook__ColumnHeaderContent ${this.state.hasFocus ? 'focused' : ''}`}
+        onBlur={this.handleBlur}
+        onFocus={this.handleFocus}
+      >
+        <div style={{flex: 1, minWidth: '1px'}}>
+          <Grid colSpacing="none" hAlign="space-between" vAlign="middle">
+            <Grid.Row>
+              <Grid.Col textAlign="start">
+                <View
+                  className="Gradebook__ColumnHeaderDetail Gradebook__ColumnHeaderDetail--OneLine"
+                  padding="0 0 0 small"
+                >
+                  <Text fontStyle="normal" size="x-small" weight="bold">
+                    {I18n.t('Student First Name')}
+                  </Text>
+                </View>
+              </Grid.Col>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </div>
+    )
+  }
+}
