eb63ab470a5bffc3ff6b1c91b332394833aa8e27
Ethan Vizitei <evizitei@instructure.com>
2021-10-13 15:52:27 -0500

    upgrade to canvas_connect with zeitwerk compatibility
    closes FOO-2433
    flag=none
    
    TEST PLAN:
      1) specs pass
    
    Change-Id: I76d43a1e92cae3c3f32c64c9bf0e98638b73f688
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/275916
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>
    Product-Review: Ethan Vizitei <evizitei@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -60,7 +60,7 @@ gem 'bcrypt', '3.1.16'
 gem 'browser', '5.1.0', require: false
 gem 'builder', '3.2.4'
 gem 'business_time', '0.10.0'
-gem 'canvas_connect', '0.3.11'
+gem 'canvas_connect', '0.3.13'
   gem 'adobe_connect', '1.0.9', require: false
 gem 'canvas_webex', '0.17'
 gem 'crocodoc-ruby', '0.0.1', require: false
