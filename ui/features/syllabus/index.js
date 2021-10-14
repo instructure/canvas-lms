5ee44c0a5594d3cf38daad1d4e261eb1268b531d
Gary Mei <gmei@instructure.com>
2021-10-13 22:51:25 -0500

    use default syllabus text for immersive reader
    closes MAT-479
    flag=more_immersive_reader
    
    Test Plan
    - Merge the commit.
    - Verify that Immersive Reader works for the syllabus when it only
      has the default syllabus text.
    - Verify that Immersive Reader works for the syllabus when a user
      has added text to the syllabus.
    
    Change-Id: If52e67240b154277b6029d93e60ae6ae52daee86
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/275949
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Gary Mei <gmei@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>

--- ui/features/syllabus/index.js
+++ ui/features/syllabus/index.js
@@ -88,8 +88,17 @@ const immersive_reader_mobile_mount_point = document.getElementById(
 if (immersive_reader_mount_point || immersive_reader_mobile_mount_point) {
   import('../../shared/immersive-reader/ImmersiveReader')
     .then(ImmersiveReader => {
-      const content = document.querySelector('#course_syllabus_details').innerHTML
+      const courseSyllabusText = document.querySelector('#course_syllabus').innerHTML
       const title = I18n.t('Course Syllabus')
+      let content
+
+      // We display a default message in #course_syllabus_details when the user
+      // hasn't set any text in the syllabus.
+      if ($.trim(courseSyllabusText)) {
+        content = courseSyllabusText
+      } else {
+        content = document.querySelector('#course_syllabus_details').innerHTML
+      }
 
       if (immersive_reader_mount_point) {
         ImmersiveReader.initializeReaderButton(immersive_reader_mount_point, {content, title})
