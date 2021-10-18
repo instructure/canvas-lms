a279f4e36853fef16f7abab09915f4ad151ab456
Jeffrey Johnson <jeffrey.johnson@instructure.com>
2021-10-18 10:54:41 -0700

    AddressBook for Conversation Inbox
    fixes VICE-2150
    
    Test Plan
    1. Pull down code and run storybook `yarn storybook`
    2. Test the following cases to capture end to end component behavior
    
    Case 1.
    1. Open Dropdown menu with mouse
    2. Both clicking address book button and input should work
    3. Select an item
    4. Item should show up in tags near input
    5. Tags should be dismissable via mouse
    6. Test same scenario with Limit of 1 story to test that limiting works
    
    Case 2.
    1. Use your keyboard to navigate to the input
    2. Focus should open the menu
    2a. Down arrow or Address book button can also open the menu
    3. Use your up and down keys to navigate
    3a. Note: Search filtering will be handlede at the container. Input only
    returns characters to <AddressBook /> consumer at the moment
    4. Select a user using the enter key
    5. Notice shift tabbing should focus tags and allow them to be dismissed
    
    Overall notes:
    1. Nested navigation will be handled at the Container level as each
    click will trigger a new query
    2. Courses and Back button should not appear in tags
    
    Change-Id: I4abad8f2c2b179b534032ce166da4d8df237a004
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/276868
    Reviewed-by: Matthew Lemon <mlemon@instructure.com>
    Reviewed-by: Davis Hyer <dhyer@instructure.com>
    QA-Review: Matthew Lemon <mlemon@instructure.com>
    Product-Review: Matthew Lemon <mlemon@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/inbox/react/components/AddressBook/AddressBookItem.js
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import PropTypes from 'prop-types'
+import React, {useState} from 'react'
+import {View} from '@instructure/ui-view'
+import {Flex} from '@instructure/ui-flex'
+import {TruncateText} from '@instructure/ui-truncate-text'
+import {Text} from '@instructure/ui-text'
+
+export const AddressBookItem = ({
+  children,
+  id,
+  iconBefore,
+  iconAfter,
+  isSelected,
+  hasPopup,
+  onSelect
+}) => {
+  const [isHover, setHover] = useState(false)
+  return (
+    <View
+      as="div"
+      background={isHover || isSelected ? 'brand' : null}
+      padding="xx-small"
+      onMouseEnter={() => {
+        setHover(true)
+      }}
+      onMouseLeave={() => {
+        setHover(false)
+      }}
+    >
+      <li
+        role="menuitem"
+        id={id}
+        style={{listStyle: 'none'}}
+        aria-haspopup={hasPopup}
+        onMouseDown={() => {
+          onSelect()
+        }}
+        data-selected={isSelected || isHover}
+      >
+        <Flex as="div" width="100%" margin="xxx-small none xxx-small xxx-small">
+          {iconBefore && (
+            <Flex.Item align="start" margin="0 small 0 0">
+              {iconBefore}
+            </Flex.Item>
+          )}
+          <Flex.Item align="center" shouldGrow shouldShrink>
+            <TruncateText>
+              <Text color={isHover || isSelected ? 'primary-inverse' : null}>{children}</Text>
+            </TruncateText>
+          </Flex.Item>
+          {iconAfter && (
+            <Flex.Item align="center" margin="0 0 0 small">
+              {iconAfter}
+            </Flex.Item>
+          )}
+        </Flex>
+      </li>
+    </View>
+  )
+}
+
+AddressBookItem.propTypes = {
+  /**
+   * Child text to be displayed in MenuItem
+   */
+  children: PropTypes.node,
+  /**
+   * String used for ID required for a11y
+   */
+  id: PropTypes.string.isRequired,
+  /**
+   * Node to be the icon rendered before the item
+   */
+  iconBefore: PropTypes.node,
+  /**
+   * Node to be the icon rendered after the item
+   */
+  iconAfter: PropTypes.node,
+  /**
+   * Bool to describe if item is selected
+   */
+  isSelected: PropTypes.bool,
+  /**
+   * Bool for popup a11y aria markup
+   */
+  hasPopup: PropTypes.bool,
+  /**
+   * Function to be returned on click
+   */
+  onSelect: PropTypes.func
+}
+
+export default AddressBookItem
