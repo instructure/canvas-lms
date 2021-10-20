c71062841edefe00d4d34bb2c5b9f1cc3626d645
Adrian Packel <apackel@instructure.com>
2021-10-20 15:20:40 -0500

    Add updated "message students who" dialog
    Add a basic component for an updated "Message Students Who" dialog,
    viewable in Storybook but not yet accessible from Gradebook. This is
    meant to give us a base to begin adding functionality and does not
    presume to be the correct or final version of the UI.
    
    closes EVAL-2056
    flag=message_observers_of_students_who
    
    Test plan:
    - Test that the component appears as expected in Storybook
    
    Change-Id: Ice349c68d4c717809d4fcbae4c8cae0a5fe7a5f1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/276525
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Adrian Packel <apackel@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

---
+++ ui/shared/message-students-dialog/react/MessageStudentsWhoDialog.stories.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import MessageStudentsWhoDialog from './MessageStudentsWhoDialog'
+
+const students = [
+  {
+    id: '100',
+    name: 'Adam Jones',
+    sortableName: 'Jones, Adam'
+  },
+  {
+    id: '101',
+    name: 'Betty Ford',
+    sortableName: 'Ford, Betty'
+  },
+  {
+    id: '102',
+    name: 'Charlie Xi',
+    sortableName: 'Xi, Charlie'
+  },
+  {
+    id: '103',
+    name: 'Dana Smith',
+    sortableName: 'Smith, Dana'
+  }
+]
+
+export default {
+  title: 'Examples/Evaluate/Shared/MessageStudentsWhoDialog',
+  component: MessageStudentsWhoDialog,
+  args: {
+    assignment: {
+      gradingType: 'points',
+      id: '100',
+      name: 'Some assignment',
+      nonDigitalSubmission: false
+    },
+    students
+  },
+  argTypes: {
+    onClose: {action: 'closed'}
+  }
+}
+
+const Template = args => <MessageStudentsWhoDialog {...args} />
+export const ScoredAssignment = Template.bind({})
+ScoredAssignment.args = {
+  assignment: {
+    gradingType: 'points',
+    id: '100',
+    name: 'A pointed assignment',
+    nonDigitalSubmission: false
+  },
+  students
+}
+
+export const UngradedAssignment = Template.bind({})
+UngradedAssignment.args = {
+  assignment: {
+    gradingType: 'not_graded',
+    id: '200',
+    name: 'A pointless assignment',
+    nonDigitalSubmission: false
+  },
+  students
+}
+
+export const PassFailAssignment = Template.bind({})
+PassFailAssignment.args = {
+  assignment: {
+    gradingType: 'pass_fail',
+    id: '300',
+    name: 'A pass-fail assignment',
+    nonDigitalSubmission: false
+  },
+  students
+}
+
+export const UnsubmittableAssignment = Template.bind({})
+UnsubmittableAssignment.args = {
+  assignment: {
+    gradingType: 'no_submission',
+    id: '400',
+    name: 'An unsubmittable assignment',
+    nonDigitalSubmission: true
+  },
+  students
+}
