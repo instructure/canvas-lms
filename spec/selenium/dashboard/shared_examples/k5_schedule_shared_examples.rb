282b261001e33e53609ec101f1731a074e1bf44b
Robin Kuss <rkuss@instructure.com>
2021-10-21 06:46:37 -0500

    spec: unskip missing items test for k5
    Unskipped missing items test and made it a little more relevant.  Also
    fixed items to say subject instead of course.
    
    Closes LS-2754
    
    Test Plan: passes Jenkins
    
    Change-Id: Ib9949877007d4c9d1a2edc65fa05a4d9be7f88c5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/276473
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeff Largent <jeff.largent@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/dashboard/shared_examples/k5_schedule_shared_examples.rb
+++ spec/selenium/dashboard/shared_examples/k5_schedule_shared_examples.rb
@@ -85,8 +85,8 @@ shared_examples_for 'k5 schedule' do
     end
   end
 
-  context 'course-scoped schedule tab included items' do
-    it 'shows schedule info for course items' do
+  context 'subject-scoped schedule tab included items' do
+    it 'shows schedule info for subject items' do
       create_dated_assignment(@subject_course, 'today assignment1', @now)
 
       get "/courses/#{@subject_course.id}#schedule"
@@ -95,17 +95,16 @@ shared_examples_for 'k5 schedule' do
       expect(schedule_item.text).to include('today assignment1')
     end
 
-    it 'shows course missing item in dropdown' do
-      skip("waiting for LS-2722 and UI")
+    it 'shows subject missing item in dropdown' do
       create_dated_assignment(@subject_course, 'yesterday assignment1', 1.day.ago(@now))
 
       get "/courses/#{@subject_course.id}#schedule"
 
-      expect(items_missing_exists?).to be_truthy
+      expect(items_missing.text).to eq('Show 1 missing item')
     end
   end
 
-  context 'course-scoped schedule tab excluded items' do
+  context 'subject-scoped schedule tab excluded items' do
     before(:once) do
       course_with_student(
         active_all: true,
@@ -115,21 +114,21 @@ shared_examples_for 'k5 schedule' do
       create_dated_assignment(@course, 'assignment for other course', @now)
     end
 
-    it 'does not show schedule info for non course item' do
+    it 'does not show schedule info for non subject item' do
       get "/courses/#{@subject_course.id}#schedule"
 
       expect(schedule_item_exists?).to be_falsey
     end
 
-    it 'does not show non-course missing item in dropdown' do
+    it 'does not show non-subject missing item in dropdown' do
       get "/courses/#{@subject_course.id}#schedule"
 
       expect(items_missing_exists?).to be_falsey
     end
   end
 
-  context 'course color' do
-    it 'shows the course color on the planner assignment listing' do
+  context 'subject color' do
+    it 'shows the subject color on the planner assignment listing' do
       new_color = '#07AB99'
       @subject_course.update!(course_color: new_color)
       create_dated_assignment(@subject_course, 'assignment for other course', @now)
