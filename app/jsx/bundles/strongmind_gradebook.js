b59a60fc1cc29c91714066d4ab7f32008b266337
Andy Rosenberg and Josh Brito <tesla+andy+josh@strongmind.com>
2021-10-21 14:27:13 -0700

    added minified js

--- app/jsx/bundles/strongmind_gradebook.js
+++ app/jsx/bundles/strongmind_gradebook.js
@@ -1,2 +1,3 @@
-import '../strongmind_gradebook/main.42211cef.js'
+import '../strongmind_gradebook/main.0b5ab256.js'
+import '../strongmind_gradebook/0.1da7876e.chunk.js'
 import '../strongmind_gradebook/main.97294891.css'
