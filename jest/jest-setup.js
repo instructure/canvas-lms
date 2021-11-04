e7d7990b915c7a5ecbb9706e16f0f8dd711297b7
Drake Harper <drake.harper@instructure.com>
2021-11-04 15:38:05 -0600

    Fix screenReaderLabel is marked as required
    flag=none
    
    Test Plan:
     - specs pass
    
    Change-Id: Iec9cfcd9ead0a2fd72a6d2fdab92237aa912657d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277496
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -90,7 +90,6 @@ const ignoredErrors = [
   /The prop `rcsProps.canUploadFiles` is marked as required in `ForwardRef`/,
   /The prop `renderLabel` is marked as required in `(FileDrop|NumberInput|Select)`/,
   /The prop `rootId` is marked as required in `GroupSelectionDrillDown`/,
-  /The prop `screenReaderLabel` is marked as required in `IconButton`/,
   /Unexpected keys "searchPermissions", "filterRoles", "tabChanged", "setAndOpenAddTray" found in preloadedState argument passed to createStore/,
   /Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>/,
   /validateDOMNesting\(...\): %s cannot appear as a child of <%s>/,
