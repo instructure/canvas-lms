5b1420a9a78ee4c6b8ea5e5c8c158d639f840758
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2021-11-04 15:28:16 -0400

    Add avatarName to CoursesListRow.js
    flag=none
    
    test plan:
      - Specs pass
    
    qa risk: low
    
    Change-Id: I941526af5273a0f6b01abf10eda88f9a526489b7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277487
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -77,7 +77,6 @@ const ignoredErrors = [
   /The 'screenReaderOnly' prop must be used in conjunction with 'liveRegion'/,
   /The above error occurred in the <.*> component/,
   /The prop `activeMailbox` is marked as required in `MailboxSelectionDropdown`/,
-  /The prop `avatarName` is marked as required in `UserLink`/,
   /The prop `children` is marked as required in `TruncateText`/,
   /The prop `courseId` is marked as required in `(LatestAnnouncementLink|PublishButton)`/,
   /The prop `currentUserRoles` is marked as required in `ObserverOptions`/,
