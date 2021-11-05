5876c888f73e670f64ca7acdfc5fe7a6d5737780
Drake Harper <drake.harper@instructure.com>
2021-11-05 15:27:05 -0600

    Fix h4 passes to BillBoard headingAs prop
    flag=none
    
    Test Plan:
     - specs pass
    
    Change-Id: If0b5b243282dc39090931ff09c0f55c09b9fa4df
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277564
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -63,7 +63,6 @@ const ignoredErrors = [
   /Invalid prop `editorOptions.plugins` of type `string` supplied to `(ForwardRef|RCEWrapper)`/, // https://instructure.atlassian.net/browse/MAT-453
   /Invalid prop `editorOptions.toolbar\[0\]` of type `string` supplied to `(ForwardRef|RCEWrapper)`/, // https://instructure.atlassian.net/browse/MAT-453
   /Invalid prop `heading` of type `object` supplied to `Billboard`/, // https://instructure.atlassian.net/browse/QUIZ-8870
-  /Invalid prop `headingAs` of value `h4` supplied to `Billboard`/,
   /Invalid prop `returnFocusTo` of type `DeprecatedComponent` supplied to `(CourseHomeDialog|HomePagePromptContainer)`/,
   /Invalid prop `selectedDate` of type `date` supplied to `CanvasDateInput`/,
   /Invalid prop `value` of type `object` supplied to `CanvasSelect`/,
