42bffa8b21e249e8eb494de2813eaa5f8dd5e4f5
Cody Cutrer <cody@instructure.com>
2021-11-08 16:23:30 -0700

    cdc: improve reliability of fixture creation
    Change-Id: I4c458ab1a79ffba18eaec2bf732727f125650d62
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277645
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/cdc_migration_testing/fixtures/web_conference.rb
+++ lib/cdc_migration_testing/fixtures/web_conference.rb
@@ -20,18 +20,23 @@
 
 module CdcFixtures
   def self.create_web_conference
-    ps = PluginSetting.find_or_create_by(name: 'adobe_connect')
-    ps.settings = {}
-    ps.disabled = false
-    ps.save!
+    # the model-generator may have created a record with id 1 already, but
+    # bypassing the sequence, so it'll try to create with id 1 and fail.
+    # just let it try twice
+    PluginSetting.unique_constraint_retry do
+      ps = PluginSetting.where(name: 'adobe_connect').first_or_initialize
+      ps.settings = {}
+      ps.disabled = false
+      ps.save! if ps.changed?
+    end
 
-    return WebConference.new({
-                               title: 'default',
-                               conference_type: 'AdobeConnect',
-                               context_id: 1,
-                               context_type: 'Course',
-                               user_id: 1,
-                               root_account_id: 1,
-                             })
+    WebConference.new(
+      title: 'default',
+      conference_type: 'AdobeConnect',
+      context_id: 1,
+      context_type: 'Course',
+      user_id: 1,
+      root_account_id: 1,
+    )
   end
 end
