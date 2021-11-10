fda1285ce535fbf5569d03a7e25e42e834f1e446
Adrian Packel <apackel@instructure.com>
2021-11-10 15:04:06 -0600

    include final grade override in course copy
    Copy over a course's final_grade_override setting as part of a course
    copy. Note that the Final Grade Override feature flag must be enabled on
    a parent account (not on the course itself) for FGO to be enabled on the
    destination course, since feature flag statuses are not included in
    course copies.
    
    closes EVAL-2071
    flag=none
    
    Test plan:
    - Have a course with final grade override enabled
      - To test copying the setting, the Final Grade Override feature flag
        should be enabled on the course's account, or higher up
    - Copy the course
      - Check that final grade override is enabled on the destination course
    
    Change-Id: I9f0401f4a737deffb4dac9ae4a9294a6fcd3a28d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277992
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Dustin Cowles <dustin.cowles@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>

--- app/models/importers/course_content_importer.rb
+++ app/models/importers/course_content_importer.rb
@@ -504,6 +504,10 @@ module Importers
         post_manually = Canvas::Plugin.value_to_boolean(settings.dig(:default_post_policy, :post_manually))
         course.default_post_policy.update!(post_manually: post_manually)
       end
+
+      if settings.key?(:allow_final_grade_override) && course.account.feature_enabled?(:final_grades_override)
+        course.allow_final_grade_override = settings[:allow_final_grade_override]
+      end
     end
 
     def self.shift_date_options(course, options = {})
