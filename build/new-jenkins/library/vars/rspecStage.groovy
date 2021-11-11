7ca08b988533149f59f87675559576a2d3a9f4c1
James Butters <jbutters@instructure.com>
2021-11-11 06:42:52 -0700

    bump rspecq version after recent rspecq merge
    add back only archive test results if axe build
    
    Change-Id: I9ae9bd1dd57d3ea6bcb1cc941c7a942d94d6f5a4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/277946
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- build/new-jenkins/library/vars/rspecStage.groovy
+++ build/new-jenkins/library/vars/rspecStage.groovy
@@ -151,7 +151,9 @@ def tearDownNode(prefix) {
     archiveArtifacts(artifacts: "tmp/docker-${prefix}-${CI_NODE_INDEX}.log")
   }
 
-  archiveArtifacts allowEmptyArchive: true, artifacts: 'tmp/rspec_results/**/*'
+  if (env.ENABLE_AXE_SELENIUM == '1') {
+    archiveArtifacts allowEmptyArchive: true, artifacts: 'tmp/rspec_results/**/*'
+  }
 
   archiveArtifacts allowEmptyArchive: true, artifacts: "tmp/spec_failures/$prefix/**/*"
   findFiles(glob: "tmp/spec_failures/$prefix/**/index.html").each { file ->
