b7d9dccecab6c66d7630503063ed80e2c9f516bf
Charley Kline <ckline@instructure.com>
2021-11-12 15:23:19 -0600

    Fix regression in jQuery datepicker
    Fixes FOO-2564
    Refs LS-2740
    flag=none
    
    g/276168 introduced several regressions into the functioning of
    the jQuery datepicker. In particular, it re-introduced the use
    of tz.parse to pull the current value back out of the field
    string value, which is extremely error-prone in non-en-US locales.
    It also broke the "running value" display while a user types in a
    free-format value into the field.
    
    This basically reverts most of that commit and implements the fix
    in a different way.
    
    Test plan:
    * Review "steps to reproduce" in the FOO-2564 ticket
    * The picker should now work right
    * Re-run the test plan for g/276168 to make sure that this fix
      didn't re-introduce that bug (I did check this myself)
    
    Change-Id: I7b9c177a84269ba8c410eafd751945f8055d3846
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278202
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/shared/datetime/jquery/DatetimeField.js
+++ ui/shared/datetime/jquery/DatetimeField.js
@@ -112,7 +112,8 @@ export default class DatetimeField {
 
     // process initial value
     if (options.time) {
-      this.setFromDateTime(options.time)
+      this.parseValue(options.time)
+      this.update()
     } else {
       this.setFromValue()
     }
@@ -194,8 +195,10 @@ export default class DatetimeField {
     if (!this.showDate) {
       this.implicitDate = date
       // replace the date portion of what we have with the date we just received
+      // Current computed value is in the 'iso8601' data as an ISO string, so
+      // let's mash the time from that with the new date we're being given,
       const newDate = date.toISOString().substr(0, 10)
-      const oldTime = (this.$field.data('inputdate') || date.toISOString()).substr(10)
+      const oldTime = (this.$field.data('iso8601') || date.toISOString()).substr(10)
       const timeWithNewDate = `${newDate}${oldTime}`
       this.$field.data('inputdate', timeWithNewDate)
       return this.setFromValue()
@@ -216,17 +219,9 @@ export default class DatetimeField {
   setFromValue(e) {
     const inputdate = this.$field.data('inputdate')
     if (typeof e !== 'undefined' && ['focus', 'blur'].includes(e.type) && !inputdate) return
-    if (e?.type === 'change') {
-      this.$field.data('inputdate', null)
-    }
     this.parseValue()
     this.update()
     this.updateSuggest(e?.type === 'keyup') // only show suggestions when typing
-  }
-
-  setFromDateTime(val) {
-    this.parseValue(val)
-    this.update()
   }
 
   normalizeValue(value) {
@@ -260,18 +255,18 @@ export default class DatetimeField {
   }
 
   parseValue(val) {
-    if (this.$field.data('inputdate')) {
+    if (typeof val === 'undefined' && this.$field.data('inputdate')) {
       const inputdate = this.$field.data('inputdate')
       this.datetime = inputdate instanceof Date ? inputdate : new Date(inputdate)
       this.blank = false
       this.invalid = this.datetime === null
+      this.$field.data('inputdate', null)
     } else {
       if (val) {
         this.setFormattedDatetime(val, TIME_FORMAT_OPTIONS)
       }
       const value = this.normalizeValue(this.$field.val())
       this.datetime = tz.parse(value)
-      this.$field.data('inputdate', this.datetime?.toISOString())
       this.blank = !value
       this.invalid = !this.blank && this.datetime === null
     }
