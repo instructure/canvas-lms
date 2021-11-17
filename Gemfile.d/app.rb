460b2f05d608bc7abbf2e5cf5063697f4654d0ea
Evan Battaglia <ebattaglia@instructure.com>
2021-11-16 17:20:21 -0700

    Add region to InstAccess tokens we create
    flag=none
    refs INTEROP-7085
    
    Test plan:
    - in a prod console make sure ApplicationController.region shows the
      region
    - locally, edit ApplicationController region to fake a value
    - locally, in a console make sure ApplicationController.region shows that value
    - have Canvas & API gateway set up using each others' keys
    - go to /api/v1/inst_access_tokens
    - in API gateway source, add console.log(payloadStr) to the end of
      deserializeAndVerifyToken() in src/instAccessTokens.ts
    - bring up API gateway and use that token
    - you should see your region you added in the payload
    
    Change-Id: Idb773f4177dcc4d620d7a70ff1b8921047d71679
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278506
    Reviewed-by: Michael Ziwisky <mziwisky@instructure.com>
    QA-Review: Michael Ziwisky <mziwisky@instructure.com>
    Product-Review: Michael Ziwisky <mziwisky@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -83,7 +83,7 @@ gem 'i18nliner', '0.1.2'
 gem 'icalendar', '2.7.0', require: false
 gem 'diplomat', '2.5.1', require: false
 gem 'ims-lti', '2.3.0', require: 'ims'
-gem 'inst_access', '0.1.0'
+gem 'inst_access', '0.1.1'
 gem 'inst_statsd', '2.2.0'
   gem 'statsd-ruby', '1.4.0', require: false
   gem 'aroi', '0.0.7', require: false
