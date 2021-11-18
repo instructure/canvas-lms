0bcc0164355cd4cad3b32a92de92b43dbb54f90f
Matthew Lemon <mlemon@instructure.com>
2021-11-18 11:17:56 -0700

    enable deep linking for discussion entries
    fixes VICE-2276
    flag=discussions_reporting
    
    Test Plan:
    - With all the correct flags enabled navigate to a discussion
      topic with a url param for a discussion entry id
    - e.g. localhost:3000/courses/1/discussion_topics/1?entry_id=1
    - After the discussion loads it should highlight the appropriate
      discussion entry
    - It should also work for subentries as well
    
    Change-Id: Ib30646e48f84c8687afea098e1b8bfd1af358f77
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278804
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- ui/features/discussion_topics_post/react/DiscussionTopicManager.js
+++ ui/features/discussion_topics_post/react/DiscussionTopicManager.js
@@ -38,7 +38,7 @@ const DiscussionTopicManager = props => {
   const [searchTerm, setSearchTerm] = useState('')
   const [filter, setFilter] = useState('all')
   const [sort, setSort] = useState('desc')
-  const [pageNumber, setPageNumber] = useState(0)
+  const [pageNumber, setPageNumber] = useState(ENV.current_page)
   const [searchPageNumber, setSearchPageNumber] = useState(0)
   const searchContext = {
     searchTerm,
@@ -60,14 +60,16 @@ const DiscussionTopicManager = props => {
   }
 
   // Isolated View State
-  const [isolatedEntryId, setIsolatedEntryId] = useState(null)
+  const [isolatedEntryId, setIsolatedEntryId] = useState(ENV.discussions_deep_link?.root_entry_id)
   const [replyFromId, setReplyFromId] = useState(null)
-  const [isolatedViewOpen, setIsolatedViewOpen] = useState(false)
+  const [isolatedViewOpen, setIsolatedViewOpen] = useState(
+    !!ENV.discussions_deep_link?.root_entry_id
+  )
   const [editorExpanded, setEditorExpanded] = useState(false)
 
   // Highlight State
   const [isTopicHighlighted, setIsTopicHighlighted] = useState(false)
-  const [highlightEntryId, setHighlightEntryId] = useState(null)
+  const [highlightEntryId, setHighlightEntryId] = useState(ENV.discussions_deep_link?.entry_id)
   const [relativeEntryId, setRelativeEntryId] = useState(null)
 
   // Reset search to 0 when inactive
