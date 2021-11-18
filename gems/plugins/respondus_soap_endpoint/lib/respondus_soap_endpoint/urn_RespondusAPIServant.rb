b8e71beae40d8ccfadcaebc0faac4ece1cd97580
Cody Cutrer <cody@instructure.com>
2021-11-18 10:36:50 -0700

    RuboCop: Style/RedundantFileExtensionInRequire
    [skip-stages=Flakey]
    
    auto-corrected
    
    Change-Id: Ie207afc9f417a60b88296380256c36966ae78ab8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278788
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/plugins/respondus_soap_endpoint/lib/respondus_soap_endpoint/urn_RespondusAPIServant.rb
+++ gems/plugins/respondus_soap_endpoint/lib/respondus_soap_endpoint/urn_RespondusAPIServant.rb
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require 'respondus_soap_endpoint/urn_RespondusAPI.rb'
+require 'respondus_soap_endpoint/urn_RespondusAPI'
 require 'benchmark'
 
 module RespondusSoapEndpoint
