e4b61c84e700c84d4194135a5ec04fb5deb38751
Graham Ballantyne <grahamb@sfu.ca>
2021-11-18 11:48:42 -0800

    Remove redis `auth` from unsupported commands
    Change-Id: I253eff7b9b6a8c062071a784dab2b14059ead738
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278814
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sean Scally <sean.scally@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- gems/canvas_cache/lib/canvas_cache/redis.rb
+++ gems/canvas_cache/lib/canvas_cache/redis.rb
@@ -361,7 +361,6 @@ module CanvasCache
       # one-off in script/console if you aren't using twemproxy, or in specs:
       ALLOWED_UNSUPPORTED = %w[
         keys
-        auth
         quit
         flushall
         flushdb
