0591a24eeed0187cc268d828719aaf47779f966c
Juan Chavez <juan.chavez@instructure.com>
2021-11-19 01:44:52 -0400

    Move math to a canvas-rce plugin
    closes MAT-470
    
    flag=new_equation_editor
    
    test plan:
    - Navigate to RCE instance
    - Open equation editor
    - Click a toolbar button
    > Verify that the equation is added to the text area
    > Verify that the editor preserves the text from
    initial and advanced views(use toggle button)
    > Verify that modifying text from advanced view
    updates the equation preview
    
    test plan:
    - Navigate to RCE instance
    - Select an already created equation image
    > Verify that the modal initializes with the selected
    equation
    
    Change-Id: I05255e9cc40441f5a060198a66ac151f63d14fd1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278860
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -268,7 +268,7 @@ class ApplicationController < ActionController::Base
     cc_in_rce_video_tray featured_help_links
     strip_origin_from_quiz_answer_file_references rce_buttons_and_icons important_dates feature_flag_filters k5_parent_support
     conferencing_in_planner remember_settings_tab word_count_in_speed_grader observer_picker lti_platform_storage
-    scale_equation_images
+    scale_equation_images new_equation_editor
   ].freeze
   JS_ENV_ROOT_ACCOUNT_FEATURES = %i[
     responsive_awareness responsive_misc product_tours files_dnd usage_rights_discussion_topics
--- packages/canvas-rce/src/rce/plugins/instructure_equation/clickCallback.js
+++ packages/canvas-rce/src/rce/plugins/instructure_equation/clickCallback.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 - present Instructure, Inc.
+ * Copyright (C) 2021 - present Instructure, Inc.
  *
  * This file is part of Canvas.
  *
@@ -16,7 +16,38 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-export default function (ed, document) {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import formatMessage from '../../../format-message'
+
+export default function (ed, document, _trayProps) {
+  return import('./EquationEditorModal').then(module => {
+    const EquationEditorModal = module.default
+    let container = document.querySelector('.canvas-rce-equation-container')
+    if (!container) {
+      container = document.createElement('div')
+      container.className = 'canvas-rce-equation-container'
+      document.body.appendChild(container)
+    }
+
+    const handleDismiss = () => {
+      ReactDOM.unmountComponentAtNode(container)
+      ed.focus(false)
+    }
+
+    ReactDOM.render(
+      <EquationEditorModal
+        editor={ed}
+        label={formatMessage('Equation Editor')}
+        onModalDismiss={handleDismiss}
+        onModalClose={handleDismiss}
+      />,
+      container
+    )
+  })
+}
+
+export function oldClickCallback(ed, document) {
   const ev = document.createEvent('CustomEvent')
   ev.initCustomEvent('tinyRCE/initEquation', true, true, {ed})
   document.dispatchEvent(ev)
