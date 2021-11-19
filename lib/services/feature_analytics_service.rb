cd8f2c76a70cc302df85e5ddd8386bb68ec201ce
Jeff Largent <jeff.largent@instructure.com>
2021-11-19 10:04:48 -0500

    Compute and send stats about feature evaluations
    This PS adds logic to compute context about a feature flag being
    evaluated at the time it is checked, and pushed to an SQS queue if
    configured. Whether or not to push the result of the evaluation to the
    queue is both sampled and cached so as to not overwhelm the queue or
    produce too much overhead on the Canvas side. The general process is:
    
    1. Feature flag is evaluated
    2. A random value between 0-1 is checked against a threshold stored in
       the dynamic setting feature_analytics.sampling_rate.
    3. If under the threshold, the LocalCache is checked for any recent
       evaluations of the same flag / context / value combo.
    4. If the result isn't already cached, the message is sent to SQS.
    5. If anything unexpectedly goes wrong, an error is sent to Sentry and
       feature evaluation continues normally (no user-facing error).
    
    refs DE-926
    flag = none
    
    Test plan:
      - Set up config in dynamic_settings.yml like the following:
          feature_analytics:
            cache_expiry: '60'
            sampling_rate: '1.0'
            queue_url: <canvas-feature-analytics SQS queue url in inseng>
            region: 'us-west-2'
      - Start canvas from a vaulted shell with inseng credentials
      - Go to a user's dashboard, a few coures pages, etc..
      - Expect to see messages show up in the SQS queue / its consumers
        after a minute or two delay
      - Expect to not see the same feature / context / value combo show up
        on repeated evaluations
    
      - Update the sampling_rate in dynamic_settings.yml to '0.0' and
        restart canvas
      - Flush redis cache
      - Go to the same pages as before
      - Expect to not see any more evaluations show up in SQS
    
    Change-Id: I189e127dd5f31dc5d504bf45d354e93f7208107a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/278885
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Jeff Largent <jeff.largent@instructure.com>
    Product-Review: Jeff Largent <jeff.largent@instructure.com>

---
+++ lib/services/feature_analytics_service.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2013 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require "aws-sdk-sqs"
+
+module Services
+  class FeatureAnalyticsService
+    def self.persist_feature_evaluation(message)
+      Canvas.timeout_protection("feature_analytics_queue", raise_on_timeout: true) do
+        client.send_message({ queue_url: config["queue_url"], message_body: message.to_json })
+      end
+    end
+
+    class << self
+      private
+
+      def client
+        return @client if instance_variable_defined?(:@client)
+
+        client_config = {}
+        client_config[:region] = config["region"] if config["region"]
+        client_config[:credentials] = Canvas::AwsCredentialProvider.new("feature_analytics", config["vault_credential_path"])
+
+        @client = Aws::SQS::Client.new(client_config)
+      end
+
+      def config
+        @config ||= Canvas::DynamicSettings.find("feature_analytics", tree: :private) || {}
+      end
+    end
+  end
+end
