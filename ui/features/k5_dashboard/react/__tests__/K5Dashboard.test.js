b47390a04d84decbd1281299ffcd6060eb2128ea
Jonathan Guardado <jonathan.guardado@instructure.com>
2021-11-22 17:37:05 -0600

    sync missing items on subject cards with observed user
    The effect used to update the link of the missing items in the
    SchedulePage component is now executed every time the observee picker
    changes, even if the planner is not visible, we need to do this as
    the link on the dashboards cards depends on the redux state in the
    same way that the link in the planner does
    
    closes LS-2824
    flag= none
    
    Test plan:
    
    - As an observer with some linked students
    - Go to the dashboard and expect to see the missing items link on the
    dashboard cards showing the right information for the selected
    student
    - Change the selected student in the picker
    - Expect to see a loading skeleton in the assignments row and the
    right information in the missing items link when it finishes loading
    
    Change-Id: I79464e1f3508b1ab98ed8bc814f1bfbddd0c1bbe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/279472
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Nate Armstrong <narmstrong@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>

--- ui/features/k5_dashboard/react/__tests__/K5Dashboard.test.js
+++ ui/features/k5_dashboard/react/__tests__/K5Dashboard.test.js
@@ -823,6 +823,18 @@ describe('K-5 Dashboard', () => {
       document.cookie = `${observedUserCookieName}=4;path=/`
     })
 
+    const opportunities2 = [
+      {
+        id: '3',
+        course_id: '23',
+        name: 'A new Assignment',
+        points_possible: 10,
+        html_url: '/courses/23/assignments/3',
+        due_at: '2021-02-15T05:59:00Z',
+        submission_types: ['online_text_entry']
+      }
+    ]
+
     const getLastRequest = async () => {
       const request = {}
       await waitFor(() => {
@@ -907,6 +919,60 @@ describe('K-5 Dashboard', () => {
         })
         done()
       })
+    })
+
+    it('shows the observee missing items on dashboard cards', async () => {
+      const {getByText, findByRole, getByRole} = render(
+        <K5Dashboard
+          {...defaultProps}
+          currentUserRoles={['user', 'observer', 'teacher']}
+          parentSupportEnabled
+          canAddObservee
+          plannerEnabled
+        />
+      )
+      moxios.uninstall()
+      moxios.install()
+      moxios.stubRequest('/api/v1/dashboard/dashboard_cards?observed_user=4', {
+        status: 200,
+        response: MOCK_CARDS
+      })
+
+      moxios.stubRequest('/api/v1/dashboard/dashboard_cards?observed_user=2', {
+        status: 200,
+        response: MOCK_CARDS_2
+      })
+
+      moxios.stubRequest(/\/api\/v1\/users\/self\/missing_submissions\?.*observed_user_id=4.*/, {
+        status: 200,
+        headers: {link: 'url; rel="current"'},
+        response: opportunities
+      })
+
+      moxios.stubRequest(/\/api\/v1\/users\/self\/missing_submissions\?.*observed_user_id=2.*/, {
+        status: 200,
+        headers: {link: 'url; rel="current"'},
+        response: opportunities2
+      })
+
+      const observerSelect = getByRole('combobox', {name: 'Select a student to view'})
+      act(() => observerSelect.click())
+      act(() => getByText('Student 4').click())
+      expect(
+        await findByRole('link', {
+          name: 'View 2 missing items for course Economics 101',
+          timeout: 5000
+        })
+      ).toBeInTheDocument()
+
+      act(() => observerSelect.click())
+      act(() => getByText('Student 2').click())
+      expect(
+        await findByRole('link', {
+          name: 'View 1 missing items for course Economics 203',
+          timeout: 5000
+        })
+      ).toBeInTheDocument()
     })
   })
 })
