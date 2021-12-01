7eaf217a416ea8d0b1f54f4f3ddce0ced6acbfc8
Charley Kline <ckline@instructure.com>
2021-12-01 15:42:13 -0600

    Add React components for the Create Group Set modal
    Refs FOO-906
    flag=none
    
    The modal dialog workflow for creating a group set (and
    possibly waiting for a backend job to finish assigning
    students to newly-created groups) is done via Rails views
    and Backbone.js code and has a bunch of A11y and R11s
    issues. This commit creates a React component and some
    Storybook code that replaces the UI functionality for
    group set creation.
    
    This does not change any live code in Canvas. It's just
    a new component that will replace existing code once
    we all agree it's working properly.
    
    Test plan:
    * On this branch, yarn run storybook
    * Exercise the "+ Group Set" button and the modal dialog
      that it brings up, paying attention to:
        - the modal's contents should behave just like the
          existing modal workflow as you select different
          options and checkboxes
        - if you select options that require the backend to
          asynchronously assign students to the groups that
          were just created, the overlaid progress display
          works, the messages make sense, and can be
          interacted with in an understandable way
        - if you turn on any of the "fail API" call mockups
          in the Storybook, the resulting UI display makes
          sense and the state recovers
        - the API calls made by the component are correct in
          terms of the body objects (they should match what
          the existing code does)
    * The main export is actually a function that returns a
      Promise whose resolution will trigger a UI change in
      the group set display. The data in that Promise
      resolution should make sense.
    
    Change-Id: I15d7807d37f8a1b7fbf1c98116c73a1112287e5c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/291155
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sean Scally <sean.scally@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

---
+++ ui/shared/groups/react/CreateGroupSetModal/__tests__/SelfSignup.test.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import {GroupContext} from '../context'
+import {SelfSignup} from '../SelfSignup'
+
+function Wrapper({state, props}) {
+  return (
+    <GroupContext.Provider value={state}>
+      <SelfSignup {...props} />
+    </GroupContext.Provider>
+  )
+}
+
+const defaultProps = {onChange: Function.prototype}
+const both = [true, false]
+
+describe('CreateGroupSetModal::SelfSignup::', () => {
+  describe('checkboxes', () => {
+    both.forEach(checked => {
+      const v = checked ? 'checked' : 'unchecked'
+      it(`tracks ${v} Self Signup`, () => {
+        const state = {selfSignup: checked, bySection: false}
+        const {getByTestId} = render(<Wrapper state={state} props={defaultProps} />)
+        expect(getByTestId('checkbox-allow-self-signup').checked).toBe(checked)
+        expect(getByTestId('checkbox-same-section').disabled).toBe(!checked)
+      })
+    })
+
+    both.forEach(checked => {
+      const v = checked ? 'checked' : 'unchecked'
+      it(`tracks ${v} Same Section`, () => {
+        const state = {selfSignup: true, bySection: checked}
+        const {getByTestId} = render(<Wrapper state={state} props={defaultProps} />)
+        expect(getByTestId('checkbox-same-section').checked).toBe(checked)
+      })
+    })
+  })
+
+  describe('onChange', () => {
+    const onChange = jest.fn()
+
+    beforeEach(() => {
+      onChange.mockReset()
+    })
+
+    it('fires handler for Self Signup click', () => {
+      const state = {selfSignup: false, bySection: false}
+      const props = {onChange}
+      const {getByTestId} = render(<Wrapper state={state} props={props} />)
+      fireEvent.click(getByTestId('checkbox-allow-self-signup'))
+      expect(onChange).toHaveBeenCalledTimes(1)
+      expect(onChange).toHaveBeenCalledWith({selfSignup: true, bySection: false})
+    })
+
+    it('fires handler for Same Section click', () => {
+      const state = {selfSignup: true, bySection: false}
+      const props = {onChange}
+      const {getByTestId} = render(<Wrapper state={state} props={props} />)
+      fireEvent.click(getByTestId('checkbox-same-section'))
+      expect(onChange).toHaveBeenCalledTimes(1)
+      expect(onChange).toHaveBeenCalledWith({selfSignup: true, bySection: true})
+    })
+  })
+})
