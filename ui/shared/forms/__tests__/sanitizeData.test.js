e39924475649c8e2f3f4d6fff1f23cca31a6a037
Weston Dransfield <wdransfield@instructure.com>
2021-12-02 12:55:07 -0700

    Sanitize "message" content in backbone forms
    Closes MAT-543
    flag=none
    
    Test Plan:
    1. Create or edit a discussion topic
    2. Switch to the pretty HTML editor
    3. Type in something in a script tag:
       <script>alert('hi!')</script>
    4. Without switching back to the WYSIWYG
       view, save the discussion topic
    5. Verify the script is not executed
    
    Change-Id: Id37d2f1b26f371bb0c6952c9abc19ab7cd843c5b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/280184
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Juan Chavez <juan.chavez@instructure.com>
    QA-Review: Juan Chavez <juan.chavez@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

---
+++ ui/shared/forms/__tests__/sanitizeData.test.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import sanitizeData from '../sanitizeData'
+
+describe('sanitizeData()', () => {
+  let data = {
+    text: '<script>console.log("hi!")</script>'
+  }
+  const dataItems = ['text']
+
+  const subject = () => sanitizeData(data, dataItems)
+
+  it('sanitizes the specified fields', () => {
+    expect(subject()).toMatchObject({
+      text: ''
+    })
+  })
+
+  const sharedExamplesForNoModifiers = () => {
+    it('does not modify the field', () => {
+      expect(subject()).toMatchObject(data)
+    })
+  }
+
+  describe('when partial sanitization is needed', () => {
+    beforeEach(
+      () => (data = {text: "<div>Don't remove me <script>console.log('remove me')</script></div>"})
+    )
+
+    it('only removes the unsafe elements', () => {
+      expect(subject()).toMatchObject({text: "<div>Don't remove me</div>"})
+    })
+  })
+
+  describe('when no sanitization is needed', () => {
+    beforeEach(() => (data = {text: '<h2>hi!</h2>'}))
+
+    sharedExamplesForNoModifiers()
+  })
+
+  describe('when the specified field is blank', () => {
+    beforeEach(() => (data = {foo: 'bar'}))
+
+    sharedExamplesForNoModifiers()
+  })
+})
