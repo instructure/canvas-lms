4395db62015486c54c8da4d9b234b424ac450c56
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2021-12-03 09:24:07 -0500

    Add anonymousAuthor to the Discussion fragment
    closes VICE-2285
    
    flag=react_discussions_post
    flag=discussion_anonymity
    
    test plan:
      - Specs pass
      - Go to a discussion topic.
      - See in the console, Network tab for the GetDiscussionQuery.
      - The field anonymousAuthor should be there and contain
          the appropriate data.
    
    qa risk: low
    
    Change-Id: I0b27ee6ef8a66d43d80a3efc0b71cefd961f7687
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/280241
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/discussion_topics_post/graphql/AnonymousUser.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import gql from 'graphql-tag'
+import {shape, string} from 'prop-types'
+
+export const AnonymousUser = {
+  fragment: gql`
+    fragment AnonymousUser on AnonymousUser {
+      id
+      avatarUrl
+      shortName
+    }
+  `,
+
+  shape: shape({
+    id: string,
+    avatarUrl: string,
+    shortName: string
+  }),
+
+  mock: ({id = '1', avatarUrl = null, shortName = '1'} = {}) => ({
+    id,
+    avatarUrl,
+    shortName,
+    __typename: 'AnonymousUser'
+  })
+}
+
+export const DefaultMocks = {
+  AnonymousUser: () => ({
+    id: '1',
+    avatarUrl: null,
+    shortName: '1'
+  })
+}
