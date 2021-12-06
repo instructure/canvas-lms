2e7a7c9851fdbc8d9739e0bda3616a6ed8c0cf72
Manoel Quirino Neto <manoel.quirino@instructure.com>
2021-12-06 15:10:23 -0300

    Wrap long friendly description words
    closes OUT-4902
    flag=outcomes_friendly_description
    
    Test plan:
      - Enable FF
      - Create a long word friendly description
      - open dev tools and make it mobile view
      - Assert the long word is broken in hyphen
      - Assert create an outcome without description
        and with FD. Assert the break works without
        the gray box
    
    Change-Id: I952dd8ccf9c64aa67b88796dd839b79a79e6b85c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/280372
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Martin Yosifov <martin.yosifov@instructure.com>
    QA-Review: Martin Yosifov <martin.yosifov@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- ui/features/outcome_management/react/Management/OutcomeDescription.js
+++ ui/features/outcome_management/react/Management/OutcomeDescription.js
@@ -30,8 +30,10 @@ const OutcomeDescription = ({description, friendlyDescription, truncated}) => {
   const shouldShowFriendlyDescription = friendlyDescriptionFF && friendlyDescription
   let fullDescription = description
   let truncatedDescription = stripHtmlTags(fullDescription || '')
+  let fullDescriptionIsFriendlyDescription = false
   if (shouldShowFriendlyDescription && (!description || isStudent)) {
     fullDescription = truncatedDescription = friendlyDescription
+    fullDescriptionIsFriendlyDescription = true
   }
   const shouldShowFriendlyDescriptionSection =
     !truncated &&
@@ -77,17 +79,24 @@ const OutcomeDescription = ({description, friendlyDescription, truncated}) => {
             background="secondary"
             data-testid="friendly-description-expanded"
           >
-            <Text>{friendlyDescription}</Text>
+            <Text wrap="break-word">{friendlyDescription}</Text>
           </View>
         </>
       )}
-      {!truncated && fullDescription && (
+
+      {!truncated && fullDescription && !fullDescriptionIsFriendlyDescription && (
         <View
           as="div"
           padding="0 small 0 0"
           data-testid="description-expanded"
           dangerouslySetInnerHTML={{__html: fullDescription}}
         />
+      )}
+
+      {!truncated && fullDescription && fullDescriptionIsFriendlyDescription && (
+        <View as="div" padding="0 small 0 0" data-testid="description-expanded">
+          <Text wrap="break-word">{fullDescription}</Text>
+        </View>
       )}
     </View>
   )
