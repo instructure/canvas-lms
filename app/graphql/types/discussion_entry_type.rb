a4f45d9722c8ad08f0a059202c4ffc3fdbef472d
Matthew Lemon <mlemon@instructure.com>
2021-12-14 12:41:27 -0700

    Update GQL API to return faculty in anonymous discussions
    fixes VICE-2318
    flag=discussion_anonymity
    
    Test Plan:
    - Navigate to graphiql
    - Run the following query on an anonymous discussion:
    
    ```
    query MyQuery {
      legacyNode(_id: <discussion_id>, type: Discussion) {
        ... on Discussion {
          id
          author(courseId: <course_id>) {
            _id
            shortName
            avatarUrl
            id
          }
          editor(courseId: <course_id>) {
            _id
            avatarUrl
            shortName
            id
          }
          anonymousAuthor {
            avatarUrl
            id
            shortName
          }
          discussionEntriesConnection {
            nodes {
              _id
              message
              anonymousAuthor {
                avatarUrl
                id
                shortName
              }
              author(courseId: <course_id>) {
                _id
                id
                avatarUrl
                shortName
                courseRoles(courseId: <course_id>)
              }
              editor(courseId: <course_id>) {
                _id
                avatarUrl
                id
                shortName
                courseRoles(courseId: <course_id>)
              }
            }
          }
        }
      }
    }
    ```
    
    - You should get back the author/editor for faculty
    - You should not get back the author/editor for students
    
    Change-Id: Icb6dfec1be66443cc264dd42b6d77ea024e8deac
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/281221
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Caleb Guanzon <cguanzon@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>

--- app/graphql/types/discussion_entry_type.rb
+++ app/graphql/types/discussion_entry_type.rb
@@ -61,13 +61,27 @@ module Types
       end
     end
 
-    field :author, Types::UserType, null: true
-    def author
+    field :author, Types::UserType, null: true do
+      argument :course_id, ID, required: false, prepare: GraphQLHelpers.relay_or_legacy_id_prepare_func("Course")
+      argument :role_types, [String], "Return only requested base role types", required: false
+      argument :built_in_only, Boolean, "Only return default/built_in roles", required: false
+    end
+    def author(course_id: nil, role_types: nil, built_in_only: true)
       load_association(:discussion_topic).then do |topic|
-        if topic.anonymous?
+        if topic.anonymous? && !course_id
           nil
         else
-          load_association(:user)
+          load_association(:user).then do |user|
+            if !topic.anonymous? || !user
+              user
+            else
+              Loaders::CourseRoleLoader.for(course_id: course_id, role_types: role_types, built_in_only: built_in_only).load(user).then do |roles|
+                if roles&.include?("TeacherEnrollment") || roles&.include?("TaEnrollment")
+                  user
+                end
+              end
+            end
+          end
         end
       end
     end
@@ -92,13 +106,27 @@ module Types
       object.deleted?
     end
 
-    field :editor, Types::UserType, null: true
-    def editor
+    field :editor, Types::UserType, null: true do
+      argument :course_id, ID, required: false, prepare: GraphQLHelpers.relay_or_legacy_id_prepare_func("Course")
+      argument :role_types, [String], "Return only requested base role types", required: false
+      argument :built_in_only, Boolean, "Only return default/built_in roles", required: false
+    end
+    def editor(course_id: nil, role_types: nil, built_in_only: true)
       load_association(:discussion_topic).then do |topic|
-        if topic.anonymous?
+        if topic.anonymous? && !course_id
           nil
         else
-          load_association(:editor)
+          load_association(:editor).then do |user|
+            if !topic.anonymous? || !user
+              user
+            else
+              Loaders::CourseRoleLoader.for(course_id: course_id, role_types: role_types, built_in_only: built_in_only).load(user).then do |roles|
+                if roles&.include?("TeacherEnrollment") || roles&.include?("TaEnrollment")
+                  user
+                end
+              end
+            end
+          end
         end
       end
     end
