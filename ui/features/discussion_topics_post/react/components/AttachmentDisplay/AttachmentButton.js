54a4c4d57922e87125df87634d8a27e9fdffddec
Chawn Neal <chawn.neal@instructure.com>
2021-12-16 08:25:28 -0600

    create AttachmentDisplay components
    refs VICE-2110
    flag=react_discussion_design
    
    Creates the components used in rendering AttachmentDisplay
    Matches designs on the ticket.
    Rendered inside discussion edit.
    
    Test Plan:
    1) Matches design in ticket.
    2) Renders in yarn storybook.
    
    Change-Id: I33ccc98ed091379614d0374d733a5418d2678f39
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/281396
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/discussion_topics_post/react/components/AttachmentDisplay/AttachmentButton.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import I18n from 'i18n!discussion_posts'
+import PropTypes from 'prop-types'
+import React from 'react'
+import {RemovableItem} from './RemovableItem'
+
+import {IconPaperclipLine} from '@instructure/ui-icons'
+import {Link} from '@instructure/ui-link'
+import {Text} from '@instructure/ui-text'
+
+export const AttachmentButton = props => {
+  return (
+    <RemovableItem
+      onRemove={props.onDeleteItem.bind(null, props.attachment?.id)}
+      screenReaderLabel={I18n.t('Remove Attachment')}
+      childrenAriaLabel={I18n.t(`Replace filename.png button`)}
+    >
+      <Link
+        renderIcon={<IconPaperclipLine size="small" />}
+        href={props.attachment?.url}
+        isWithinText={false}
+      >
+        <Text weight="bold">{props.attachment?.display_name}</Text>
+      </Link>
+    </RemovableItem>
+  )
+}
+
+AttachmentButton.propTypes = {
+  /**
+   * This button renders when attachment is given we
+   * use many attributes from it.
+   */
+  attachment: PropTypes.object.isRequired,
+  /**
+   * When we click the close (x button) on the RemovableItem.
+   * Used to delete the attachment.
+   */
+  onDeleteItem: PropTypes.func.isRequired
+}
