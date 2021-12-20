1486d79784f9867690eaa2b85ea8476a3e9e5529
Aaron Ogata <aogata@instructure.com>
2021-12-20 12:41:10 -0800

    bump inst-jobs
    Change-Id: I92b3183e60f15872492921aac7f34a622dc89dd7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/281639
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Migration-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -24,13 +24,13 @@ if CANVAS_RAILS6_0
     gem "tzinfo", "1.2.7"
   gem "switchman", "2.2.2"
     gem "guardrail", "2.0.1"
-  gem "switchman-inst-jobs", "3.2.6"
+  gem "switchman-inst-jobs", "3.2.7"
 else
   gem "rails", "6.1.4.1"
     gem "tzinfo", "2.0.4"
   gem "switchman", "3.0.5"
     gem "guardrail", "3.0.0"
-  gem "switchman-inst-jobs", "4.0.2"
+  gem "switchman-inst-jobs", "4.0.3"
 end
   gem "irb", "1.3.5"
   gem "open4", "1.3.4", require: false
@@ -87,7 +87,7 @@ gem "inst_statsd", "2.2.0"
   gem "statsd-ruby", "1.4.0", require: false
   gem "aroi", "0.0.7", require: false
   gem "dogstatsd-ruby", "4.8.2"
-gem "inst-jobs", "3.0.5"
+gem "inst-jobs", "3.0.6"
   gem "fugit", "1.4.0", require: false
     gem "et-orbi", "1.2.4", require: false
 gem "inst-jobs-autoscaling", "2.1.1"
