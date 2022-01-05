b68a1010c9546ffe28791ba1ac974aa3dbe3e3b3
Chawn Neal <chawn.neal@instructure.com>
2022-01-05 02:09:21 -0600

    set folder_id when theres no current_user
    Previously we set the folder id by current user which in this case is nil.
    The api for adding attachment handles canAttach permissions.
    
    fixes VICE-2386
    flag=react_discussions_post
    
    Test Plan:
     1) Specs are valid and pass.
     The specs access a course without a current_user set
     and there is no error.
    
    Change-Id: I1a760c86c0d48279cb8488d1b82f1e965938d4e8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/282144
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- app/controllers/discussion_topics_controller.rb
+++ app/controllers/discussion_topics_controller.rb
@@ -755,7 +755,7 @@ class DiscussionTopicsController < ApplicationController
                DISCUSSION: {
                  GRADED_RUBRICS_URL: (@topic.assignment ? context_url(@topic.assignment.context, :context_assignment_rubric_url, @topic.assignment.id) : nil),
                  CONTEXT_RUBRICS_URL: can_do(@topic.assignment, @current_user, :update) ? context_url(@topic.assignment.context, :context_rubrics_url) : "",
-                 ATTACHMENTS_FOLDER_ID: @topic.for_assignment? ? @current_user.submissions_folder(@context).id : Folder.unfiled_folder(@context).id
+                 ATTACHMENTS_FOLDER_ID: (@topic.for_assignment? && !@current_user.nil?) ? @current_user.submissions_folder(@context).id : Folder.unfiled_folder(@context).id
                },
                apollo_caching: @current_user &&
                  Account.site_admin.feature_enabled?(:apollo_caching),
