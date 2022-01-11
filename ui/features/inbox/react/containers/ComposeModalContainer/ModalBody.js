f50c1cdee47e3a389938d422f7cc47eb86ffd6d8
Dustin Cowles <dustin.cowles@instructure.com>
2022-01-11 11:03:41 -1000

    create new shared package message-attachments
    This commit is the first step in moving code from Canvas Inbox into a
    shared directory. This will later be imported to Canvas Inbox and the
    duplicate files removed.
    
    Components from Canvas Inbox
    - Attachment
    - AttachmentDisplay
    - RemovableItem
    
    refs EVAL-2063
    flag=none
    
    Test plan:
    - tests pass
    
    Change-Id: I4be9326c77c4699ced7e83c5793f05bb42d265a3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/282232
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- ui/features/inbox/react/containers/ComposeModalContainer/ModalBody.js
+++ ui/features/inbox/react/containers/ComposeModalContainer/ModalBody.js
@@ -16,18 +16,18 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {AttachmentDisplay} from '../../components/AttachmentDisplay/AttachmentDisplay'
 import {ConversationMessage} from '../../../graphql/ConversationMessage'
 import {MessageBody} from '../../components/MessageBody/MessageBody'
 import {PastMessages} from '../../components/PastMessages/PastMessages'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import {AttachmentDisplay} from '@canvas/message-attachments'
 import {Flex} from '@instructure/ui-flex'
 import {Modal} from '@instructure/ui-modal'
 import {View} from '@instructure/ui-view'
 
-const ModalBody = (props) => (
+const ModalBody = props => (
   <Modal.Body padding="none">
     <Flex direction="column" width="100%" height="100%">
       {props.children}
@@ -53,14 +53,14 @@ ModalBody.propTypes = {
   bodyMessages: PropTypes.arrayOf(
     PropTypes.shape({
       text: PropTypes.string,
-      type: PropTypes.string,
+      type: PropTypes.string
     })
   ),
   children: PropTypes.element,
   onBodyChange: PropTypes.func,
   pastMessages: PropTypes.arrayOf(ConversationMessage.shape),
   removeAttachment: PropTypes.func,
-  replaceAttachment: PropTypes.func,
+  replaceAttachment: PropTypes.func
 }
 
 export default ModalBody
