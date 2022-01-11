4b0cb106618ff2bc884c5867d5d0b5a0bfc5d3fe
Dustin Cowles <dustin.cowles@instructure.com>
2022-01-11 11:06:11 -1000

    add file attachment support to message-attachments
    This commit adds shared components for supporting attachment uploads as
    well as attachment utils to contain optional shared logic for handling
    attachment events. Further commits in this series will put these shared
    components to use.
    
    AttachmentUploadSpinner: Allows delaying message sending until all
        attachments have uploaded.
    FileAttachmentUpload: Provides button and input to upload
        attachments and attach to a message.
    util/attachments: Functions for managing attachments.
    
    refs EVAL-2063
    flag=none
    
    Test plan:
    - Tests Pass
    
    Change-Id: I04bf085be3d6da0f0744c46d4d9c1f078625829c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/282571
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

---
+++ ui/shared/message-attachments/react/components/AttachmentUploadSpinner/AttachmentUploadSpinner.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import I18n from 'i18n!conversations_2'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import {Flex} from '@instructure/ui-flex'
+import {Modal} from '@instructure/ui-modal'
+import {Spinner} from '@instructure/ui-spinner'
+import {Text} from '@instructure/ui-text'
+
+export const AttachmentUploadSpinner = props => {
+  return (
+    <Modal
+      open={props.isMessageSending && !!props.pendingUploads.length}
+      label={props.label}
+      shouldCloseOnDocumentClick={false}
+      onExited={() => props.sendMessage()}
+    >
+      <Modal.Body>
+        <Flex direction="column" textAlign="center">
+          <Flex.Item>
+            <Spinner renderTitle={props.label} size="large" />
+          </Flex.Item>
+          <Flex.Item>
+            <Text>{props.message}</Text>
+          </Flex.Item>
+        </Flex>
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default AttachmentUploadSpinner
+
+AttachmentUploadSpinner.defaultProps = {
+  label: I18n.t('Uploading Files'),
+  message: I18n.t('Please wait while we upload attachments.')
+}
+
+AttachmentUploadSpinner.propTypes = {
+  label: PropTypes.string,
+  message: PropTypes.string,
+  sendMessage: PropTypes.func.isRequired,
+  isMessageSending: PropTypes.bool.isRequired,
+  pendingUploads: PropTypes.array.isRequired
+}
