eb1958d05b1b89618e26a0f7bd636d960122421b
Jason Gillett <jason.gillett@instructure.com>
2022-02-04 16:00:29 -0700

    Update Queries to contain page information
    refs: VICE-2446
    flag=react_inbox
    
    test plan:
    - Open the react inbox with the networks tab open
    - Look for the graphql GetAddressBookRecipients query
    - Verify that the PageInfo data object is now visible
    
    In follow-up commits
    - This paginated query variables will be used
    - Tests will be created to test paginated responses.
    
    Change-Id: Iadcea933c409eca85e91f679e812d96f85b59ff9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/284366
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeffrey Johnson <jeffrey.johnson@instructure.com>
    Product-Review: Jeffrey Johnson <jeffrey.johnson@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/inbox/graphql/PageInfo.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {bool, shape, string} from 'prop-types'
+import gql from 'graphql-tag'
+
+export const PageInfo = {
+  fragment: gql`
+    fragment PageInfo on PageInfo {
+      endCursor
+      hasNextPage
+    }
+  `,
+
+  shape: shape({
+    endCursor: string,
+    hasNextPage: bool
+  }),
+
+  mock: ({endCursor = 'MjA', hasNextPage = true} = {}) => ({
+    endCursor,
+    hasNextPage,
+    __typename: 'PageInfo'
+  })
+}
