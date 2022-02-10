b77b97c8d70209edddcb189e0c58dbeec58a260e
Weston Dransfield <wdransfield@instructure.com>
2022-02-10 09:57:26 -0700

    Embed selected image in B&I
    closes MAT-676
    flag=buttons_and_icons_root_account
    
    Test Plan:
    - Enable the listed feature flag ^
    - Re-build canvas-rce and Canvas webpack
    - Naviage to an RCE and open the create button
      and icons tray
    - Set every property using a multi-color image
      for the embedded image
    - Verify all options are reflected in the preview
    - Create the button to embed it in the RCE
    - Verify the options you selected are reflected in
      the image embedded in the RCE
    - Click the imagein the RCE and choose "Edit"
    - Verify you can edit the button and persist changes.
      Especially test changing the embedded image
    
    Change-Id: I0d4af3eb7f083481bb1d81b71ebf8e8eb12dcad4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/284900
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Gonzalo Penaranda <gonzalo.penaranda@instructure.com>
    QA-Review: Gonzalo Penaranda <gonzalo.penaranda@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

--- packages/canvas-rce/src/rce/plugins/instructure_buttons/svg/constants.js
+++ packages/canvas-rce/src/rce/plugins/instructure_buttons/svg/constants.js
@@ -19,6 +19,7 @@
 export const TYPE = 'image/svg+xml-buttons-and-icons'
 
 export const DEFAULT_SETTINGS = {
+  // Basic button & icon settings
   type: TYPE,
   name: '',
   alt: '',
@@ -34,7 +35,15 @@ export const DEFAULT_SETTINGS = {
   textPosition: 'middle',
   encodedImage: '',
   encodedImageType: '',
-  encodedImageName: ''
+  encodedImageName: '',
+  // Embedded image crop settings
+  x: 0,
+  y: 0,
+  translateX: 0,
+  translateY: 0,
+  width: 0,
+  height: 0,
+  transform: ''
 }
 
 export const DEFAULT_OPTIONS = {
