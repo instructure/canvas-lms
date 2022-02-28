bbe2edc9912084b6b8e7fc5e149dc90894c1e596
Davis Hyer <dhyer@instructure.com>
2022-02-28 16:15:44 -0700

    move select value from option to select
    refs LS-3017
    flag=none
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: Ia52680b0e2b5c277f6078ea19737c7107985abfb
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286069
    Reviewed-by: Ahmad Amireh <ahmad@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -85,7 +85,6 @@ const ignoredErrors = [
   /The prop `renderLabel` is marked as required in `(FileDrop|NumberInput|Select)`/,
   /The prop `rootId` is marked as required in `GroupSelectionDrillDown`/,
   /Unexpected keys "searchPermissions", "filterRoles", "tabChanged", "setAndOpenAddTray" found in preloadedState argument passed to createStore/,
-  /Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>/,
   /validateDOMNesting\(...\): %s cannot appear as a child of <%s>/,
   /WARNING: heuristic fragment matching going on!/,
   /Warning: Failed prop type: Expected one of Checkbox in CheckboxGroup but found `View`/,
