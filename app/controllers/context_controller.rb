f4610f6ecc4f4b2a523fe14a3baed846fa5b7ff7
Caleb Guanzon <cguanzon@instructure.com>
2022-02-28 09:41:33 -0700

    add faculty journal link to people page rows
    flag=none
    fixes VICE-2592
    
    test plan:
    turn on faculty journal for your account
    in /accounts/:id/settings
    
    - visit course users page as a Teacher, TA, or Admin
    - click on a ... of a user, click the Faculty Journal
    option
    - verify that you are in the faculty journal of a page
    
    - visit course users page as a student, designer, or observer
    - - verify that the ... of the user has no faculty
    journal option
    
    turn off faculty journal in your account
    
    visit course users page and verify
    that the ... of the user has no faculty
    journal option
    
    Change-Id: Ib32804965eeae4a1c0a39f83e7900ba933ff77ed
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286046
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- app/controllers/context_controller.rb
+++ app/controllers/context_controller.rb
@@ -92,7 +92,8 @@ class ContextController < ApplicationController
         manage_students: manage_students,
         add_users_to_course: can_add_enrollments,
         read_reports: @context.grants_right?(@current_user, session, :read_reports),
-        can_add_groups: can_do(@context.groups.temp_record, @current_user, :create)
+        can_add_groups: can_do(@context.groups.temp_record, @current_user, :create),
+        manage_user_notes: @context.root_account.enable_user_notes && @context.grants_right?(@current_user, :manage_user_notes)
       }
       if @context.root_account.feature_enabled?(:granular_permissions_manage_users)
         js_permissions[:can_allow_course_admin_actions] = manage_admins
