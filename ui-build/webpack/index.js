8c41cb0ff3f3cdc103b0d13c258b197c1e330a0f
Ahmad Amireh <ahmad@instructure.com>
2022-03-02 15:14:32 -0700

    webpack: remove support for virtual i18n! modules
    refs FOO-2696
    flag = none
    
    with the recent changes to how we generate the locale bundles, modules
    no longer have to have a dependency on their (scope) locale bundle since
    they do not exist
    
    with this change, all support for the virtual "i18n!xxx" modules is
    removed, as it's been a convenience carried over from the days of
    require.js but is proving to be more costly as the tooling landscape
    evolves
    
    . test plan .
     ..... .....
    
    assuming you checked out the change that rewrites all imports to "i18n!"
    to use the "useScope" specifier from @canvas/i18n, webpack should still
    run
    
    Change-Id: I6e0db400b46133458592976cc4772365efcde317
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286653
    Tested-by: James Butters <jbutters@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

--- ui-build/webpack/index.js
+++ ui-build/webpack/index.js
@@ -26,7 +26,6 @@ const glob = require('glob')
 const webpack = require('webpack')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin
-const I18nPlugin = require('./i18nPlugin')
 const WebpackHooks = require('./webpackHooks')
 const SourceFileExtensionsPlugin = require('./SourceFileExtensionsPlugin')
 const EncapsulationPlugin = require('webpack-encapsulation-plugin')
@@ -315,9 +314,6 @@ module.exports = {
       endYear: new Date().getFullYear() + 15
     }),
 
-    // handles our custom i18n stuff
-    I18nPlugin,
-
     // allow plugins to extend source files
     new SourceFileExtensionsPlugin({
       context: canvasDir,
@@ -342,7 +338,7 @@ module.exports = {
         path.resolve(canvasDir, 'public/javascripts'),
         path.resolve(canvasDir, 'gems/plugins')
       ],
-      exclude: [/\/node_modules\//, path.resolve(canvasDir, 'ui/shims/dummyI18nResource.js')],
+      exclude: [/\/node_modules\//],
       formatter: require('./encapsulation/ErrorFormatter'),
       rules: require('./encapsulation/moduleAccessRules')
     }),
