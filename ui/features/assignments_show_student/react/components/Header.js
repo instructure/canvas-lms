c4bdfdd0667b68d6a5264c5ffccf1331ec8b130d
Spencer Olson <solson@instructure.com>
2022-03-08 10:38:12 -0600

    don't show unread comment badge when there are no unread comments
    closes EVAL-2314
    flag=assignments_2_student
    
    Test Plan:
    1. Enable "Assignment Enhancements — Student"
    2. Create an assignment and go to the assignment page as a student
    3. Verify the "Add Comment" button does not have a blue bubble with
       the number 0 in the top-right corner.
    
    Change-Id: Ifbcade08294b056c1896bb964a39a859706c1dd5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286554
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Syed Hussain <shussain@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- ui/features/assignments_show_student/react/components/Header.js
+++ ui/features/assignments_show_student/react/components/Header.js
@@ -157,17 +157,12 @@ class Header extends React.Component {
     const popoverMessage = I18n.t(
       'After the first attempt, you cannot leave comments until you submit the assignment.'
     )
-
     return (
       <>
-        <div data-testid="unread_comments_badge">
+        <div>
           <StudentViewContext.Consumer>
-            {context => (
-              <Badge
-                margin="x-small"
-                count={this.props.submission ? this.props.submission.unreadCommentCount : null}
-                countUntil={100}
-              >
+            {context => {
+              const button = (
                 <Button
                   renderIcon={IconChatLine}
                   onClick={this.openCommentsTray}
@@ -178,8 +173,19 @@ class Header extends React.Component {
                     ? I18n.t('View Feedback')
                     : I18n.t('Add Comment')}
                 </Button>
-              </Badge>
-            )}
+              )
+
+              const unreadCount = this.props.submission?.unreadCommentCount
+              if (!unreadCount) return button
+
+              return (
+                <div data-testid="unread_comments_badge">
+                  <Badge pulse margin="x-small" count={unreadCount} countUntil={100}>
+                    {button}
+                  </Badge>
+                </div>
+              )
+            }}
           </StudentViewContext.Consumer>
           {addCommentsDisabled && (
             <Popover
