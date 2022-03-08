160b95876195151edcfbb1993e8649e6531f2d2f
Jacob Burroughs <jburroughs@instructure.com>
2022-03-08 15:00:21 -0600

    Fix vendored gems for rails 6.1
    [skip-crystalball]
    
    Change-Id: Iddc9432298ce756927c356fd319fec0631dcf6c1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286589
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- gems/canvas_cache/spec/canvas_cache/redis_spec.rb
+++ gems/canvas_cache/spec/canvas_cache/redis_spec.rb
@@ -20,7 +20,6 @@
 
 require "spec_helper"
 require "timecop"
-require "active_support/time_with_zone"
 
 describe CanvasCache::Redis do
   include_context "caching_helpers"
@@ -175,7 +174,7 @@ describe CanvasCache::Redis do
 
         it "does not fail cache.delete_matched" do
           override_cache(cache) do
-            expect(Rails.cache.delete_matched("blah")).to eq nil
+            expect { Rails.cache.delete_matched("blah") }.not_to raise_error
           end
         end
 
@@ -271,13 +270,13 @@ describe CanvasCache::Redis do
             cache.fetch(key, force: true) do
               val = +"a1"
               val << cache.fetch(key2, force: true) do
-                Timecop.travel(Time.zone.now + 1.second)
+                Timecop.travel(Time.now.utc + 1.second)
                 # Cheat to cover the missing ActiveSupport::Notifications.subscription in config/inititalizers/cache_store.rb
                 # TODO: remove this hack when initializer is ported to gem and incorporated
                 Thread.current[:last_cache_generate] = 1
                 "b1"
               end
-              Timecop.travel(Time.zone.now + 2.seconds)
+              Timecop.travel(Time.now.utc + 2.seconds)
               # Cheat to cover the missing ActiveSupport::Notifications.subscription in config/inititalizers/cache_store.rb
               # TODO: remove this hack when initializer is ported to gem and incorporated
               Thread.current[:last_cache_generate] = 3
@@ -286,14 +285,14 @@ describe CanvasCache::Redis do
           end
           outer_message = JSON.parse(log_lines.pop)
           expect(outer_message["command"]).to eq("set")
-          expect(outer_message["key"]).to be_ends_with(key)
+          expect(outer_message["key"]).to end_with(key)
           expect(outer_message["request_time_ms"]).to be_a(Float)
           # 3000 (3s) == 2s outer fetch + 1s inner fetch
           expect(outer_message["generate_time_ms"]).to be_within(500).of(3000)
 
           inner_message = JSON.parse(log_lines.pop)
           expect(inner_message["command"]).to eq("set")
-          expect(inner_message["key"]).to be_ends_with(key2)
+          expect(inner_message["key"]).to end_with(key2)
           expect(inner_message["request_time_ms"]).to be_a(Float)
           expect(inner_message["generate_time_ms"]).to be_within(500).of(1000)
         ensure
