ad82c7832d7571dc1607839d135f77c5bb680d8c
Weston Dransfield <wdransfield@instructure.com>
2022-03-10 16:58:20 -0700

    Scroll to images in Icon Maker
    Also make typing not feel slooow
    
    Closes MAT-760
    flag=buttons_and_icons_root_account
    
    Test Plan:
    - Build canvas-rce and Canvas JS bundles
    - Open Icon Maker and scroll to the images section
    - Choose Multicolor images from the list of options
    - Validate the browser scrolls the list of multicolor
      images into view
    - Choose single color images from the list of options
    - Validate the browser scrolls the list of singlecolor
      images into view
    
    Change-Id: I55e4a7b9ea4357e5a6d3c6afafa41b30a2664d1e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286841
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Gonzalo Penaranda <gonzalo.penaranda@instructure.com>
    QA-Review: Gonzalo Penaranda <gonzalo.penaranda@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

--- packages/canvas-rce/src/rce/plugins/instructure_buttons/components/CreateButtonForm/__tests__/Header.test.js
+++ packages/canvas-rce/src/rce/plugins/instructure_buttons/components/CreateButtonForm/__tests__/Header.test.js
@@ -17,25 +17,27 @@
  */
 
 import React from 'react'
-import {fireEvent, render} from '@testing-library/react'
+import {fireEvent, render, waitFor} from '@testing-library/react'
 import {DEFAULT_SETTINGS} from '../../../svg/constants'
 import {Header} from '../Header'
 
 describe('<Header />', () => {
-  it('changes the button name', () => {
+  it('changes the button name', async () => {
     const onChange = jest.fn()
     render(<Header settings={DEFAULT_SETTINGS} onChange={onChange} />)
     const input = document.querySelector('#button-name')
     fireEvent.change(input, {target: {value: 'A b&i name'}})
-    expect(onChange).toHaveBeenCalledWith({name: 'A b&i name'})
+
+    await waitFor(() => expect(onChange).toHaveBeenCalledWith({name: 'A b&i name'}))
   })
 
-  it('changes the button alt text', () => {
+  it('changes the button alt text', async () => {
     const onChange = jest.fn()
     render(<Header settings={DEFAULT_SETTINGS} onChange={onChange} />)
     const input = document.querySelector('#button-alt-text')
     fireEvent.change(input, {target: {value: 'A descriptive text'}})
-    expect(onChange).toHaveBeenCalledWith({alt: 'A descriptive text'})
+
+    await waitFor(() => expect(onChange).toHaveBeenCalledWith({alt: 'A descriptive text'}))
   })
 
   describe('when the name contains html entities', () => {
