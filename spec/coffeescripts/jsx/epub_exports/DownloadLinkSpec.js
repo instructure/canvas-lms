1fa80aba727a486a9acc838ca649bbfaa91c837b
Ahmad Amireh <ahmad@instructure.com>
2022-03-10 06:54:21 -0700

    rewrite i18n! to useScope in QUnit specs
    refs FOO-2696
    flag = none
    
    see the related patch for JavaScripts for context, this is the same but
    for typescripts
    
    also, specs that were stubbing/spying on I18n.Scope prototype methods
    had to be adjusted to reach out to the constructor since modules no
    longer share the same (dynamically generated) instance
    
    Change-Id: Ie0e4a2fcc97edb25f986c810a2196aa490341d7d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/286751
    Tested-by: James Butters <jbutters@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

--- spec/coffeescripts/jsx/epub_exports/DownloadLinkSpec.js
+++ spec/coffeescripts/jsx/epub_exports/DownloadLinkSpec.js
@@ -21,7 +21,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-dom/test-utils'
 import DownloadLink from 'ui/features/epub_exports/react/DownloadLink.js'
-import I18n from 'i18n!epub_exports'
+import { useScope as useI18nScope } from '@canvas/i18n';
+
+const I18n = useI18nScope('epub_exports');
 
 QUnit.module('DownloadLink', {
   setup() {
