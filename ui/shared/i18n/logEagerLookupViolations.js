e44977095b7aa32575392ed011293b31de82fe8d
Ahmad Amireh <ahmad@instructure.com>
2022-03-16 09:51:37 -0600

    setup moment for catalan only when i18n is ready
    fixes FOO-2770
    flag = none
    
    problem was the moment initializer for Catalan was trying to lookup a
    phrase before the translations file was loaded, now it waits for the
    signal
    
    additionally, i've installed a guard around the lookup routine to track
    any such violations and report them to Sentry to help us clean them up
    
    ~ test plan ~
    
    - switch to Catalan and go to account settings
    - verify the page loads
    - (optional) edit ca.js and call "apply" instead of the new function,
      verify the error gets logged to Sentry (u can just use the debugger if
      u don't have Sentry set up locally)
    
    Change-Id: Iaa35b76b17f4b1f0a6ab8ec8050c0020fb36fcc8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/287210
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

---
+++ ui/shared/i18n/logEagerLookupViolations.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {captureException} from '@sentry/browser'
+
+let translationsAvailable = false
+
+class EagerLookupViolationError extends Error {
+  constructor(phrase) {
+    super()
+    this.message = `eager lookup for phrase "${phrase}"`
+    this.name = 'EagerLookupViolationError'
+  }
+}
+
+const trackReadiness = ({detail}) => {
+  if (detail === 'localeFiles') {
+    translationsAvailable = true
+    window.removeEventListener('canvasReadyStateChange', trackReadiness)
+  }
+}
+
+window.addEventListener('canvasReadyStateChange', trackReadiness)
+
+export default function (f) {
+  return function () {
+    if (!translationsAvailable) {
+      const phrase = [].slice.call(arguments, 0, 1)
+      const error = new EagerLookupViolationError(phrase)
+
+      // don't block
+      window.setTimeout(() => {
+        captureException(error)
+      }, 0)
+    }
+
+    return f.apply(this, arguments)
+  }
+}
