b97f29ba03d444808ac348378c727fd161627e95
Jacob Burroughs <jburroughs@instructure.com>
2022-03-22 14:44:28 -0500

    Remove CANVAS_ZEITWERK
    Change-Id: Ia83164d6f145af4d60303e139a13bd2e68d6e7eb
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/287703
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -68,9 +68,9 @@ gem "bcrypt", "3.1.16"
 gem "browser", "5.1.0", require: false
 gem "builder", "3.2.4"
 gem "business_time", "0.10.0"
-gem "canvas_connect", "0.3.15"
+gem "canvas_connect", "0.3.16"
   gem "adobe_connect", "1.0.10", require: false
-gem "canvas_webex", "0.18.1"
+gem "canvas_webex", "0.18.2"
 gem "crocodoc-ruby", "0.0.1", require: false
 gem "ddtrace", "0.42.0", require: false
 gem "docx", "0.6.2"
