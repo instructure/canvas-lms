f112893ac78ce87f318033f4aa693850c3d903b2
Jacob Burroughs <jburroughs@instructure.com>
2022-03-23 15:13:11 -0500

    Bump switchman
    Change-Id: Icbb684949a9c8fb2b472ce6f9fddc0d2833b1ec2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/287846
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -36,7 +36,7 @@ if CANVAS_RAILS6_0
 else
   gem "rails", "6.1.4.6"
     gem "tzinfo", "2.0.4"
-  gem_override "switchman", "3.0.13"
+  gem_override "switchman", "3.0.14"
     gem "guardrail", "3.0.0"
   gem_override "switchman-inst-jobs", "4.0.6"
 end
