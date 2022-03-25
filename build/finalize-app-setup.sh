cbe68d9f65755d3e4a7aaa510f7209b0a1a44d3b
Steven Nunez <octosteve@github.com>
2022-03-25 01:19:49 +0000

    Run Seed Data

--- build/finalize-app-setup.sh
+++ build/finalize-app-setup.sh
@@ -4,6 +4,11 @@ set -e
 
 cp config/database.yml.codespaces config/database.yml &&
 cp config/security.yml.example config/security.yml &&
+cp config/domain.yml.example config/domain.yml &&
 bundle install &&
-bundle exec rake db:migrate &&
+CANVAS_LMS_STATS_COLLECTION=opt_out \
+CANVAS_LMS_ACCOUNT_NAME=learn_good_things_school \
+CANVAS_LMS_ADMIN_EMAIL=admin@example.com \
+CANVAS_LMS_ADMIN_PASSWORD=secret123 \
+    bundle exec rake db:initial_setup &&
 bundle exec rails canvas:compile_assets_dev
