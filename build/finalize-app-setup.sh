0fbbcae8e2606f7ed1909fc44b68a7dd88a0df99
Steven Nunez <octosteve@github.com>
2022-03-25 01:43:44 +0000

    Run yarn install

--- build/finalize-app-setup.sh
+++ build/finalize-app-setup.sh
@@ -6,6 +6,7 @@ cp config/database.yml.codespaces config/database.yml &&
 cp config/security.yml.example config/security.yml &&
 cp config/domain.yml.example config/domain.yml &&
 bundle install &&
+yarn install &&
 CANVAS_LMS_STATS_COLLECTION=opt_out \
 CANVAS_LMS_ACCOUNT_NAME=learn_good_things_school \
 CANVAS_LMS_ADMIN_EMAIL=admin@example.com \
