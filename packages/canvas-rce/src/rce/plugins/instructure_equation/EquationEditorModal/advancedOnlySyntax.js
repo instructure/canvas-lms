ac0b510f711e33f91b9d8830388dfae7b455df19
jake.oeding <jake.oeding@instructure.com>
2022-03-25 16:57:28 -0500

    NEE: force advanced mode for certain commands
    some commands either don't work, don't work well,
    or don't look acceptable in the basic editor. this
    patch adds a mechanism for forcing the user to
    utilize the advanced editor if they are using these
    commands
    
    closes MAT-784
    flag=new_equation_editor
    
    test plan:
    -enable the ff and find an RCE
    -do all of the following for a simple equation, and
     for an advanced only equation. for the advanced,
     the equation editor should immediately flip to the
     advanced editor. the basic should stay on basic.
     1. open EE while you have the equation highlighted
     2. open EE while your cursor is in the middle of the
        equation
     3. open EE from an image already embedded in the RCE
    -open the EE in basic mode, and paste an advanced
     equation into the editor. the EE should flip to
     the advanced mode
    -delete the advanced equation and observe you can
     flip back to the basic mode
    -for all the steps above, when the advanced mode
     is forced, the mode toggle should be disabled and
     a tooltip should appear upon hovering on the toggle
    
    Change-Id: Idee9452f661cbbfb5abe9485bc79462fdab9c0a4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/288129
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

---
+++ packages/canvas-rce/src/rce/plugins/instructure_equation/EquationEditorModal/advancedOnlySyntax.js
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// These commands all work fine with MathJax but either don't work, don't work well
+// (bad UX for editing), or look strange when rendered by Mathlive. Add new ones
+// here if you discover anything else or customers report unexpected experiences.
+
+import {memoize} from 'lodash'
+
+ const advancedOnlyCommands = [
+    'begin',
+    'end',
+    'cases',
+    'cr',
+    'rm',
+    'text',
+    'hbox',
+    'mbox',
+    'unicode',
+    'cal',
+    'frak',
+    'it',
+    'scr',
+    'sf',
+    '#',
+    'def',
+    'newcommand',
+    'operatorname',
+    'DeclareMathOperator',
+    'displaystyle',
+    'textstyle',
+    'scriptstyle',
+    'scriptscriptstyle',
+    'displaylines',
+    'abovewithdelims',
+    'array',
+    'bmatrix',
+    'buildrel',
+    'ddddot',
+    'dddot',
+    'eqalign',
+    'eqalignno',
+    'gcd',
+    'genfrac',
+    'hdashline',
+    'hfil',
+    'hfill',
+    'hfilll',
+    'hline',
+    'idotsint',
+    'iiiint',
+    'injlim',
+    'kern',
+    'label',
+    'LaTeX',
+    'leftroot',
+    'lgroup',
+    'lower',
+    'mathchoice',
+    'mathfrak',
+    'matrix',
+    'mit',
+    'mkern',
+    'mspace',
+    'negmedspace',
+    'negthickspace',
+    'negthinspace',
+    'newline',
+    'nobreakspace',
+    'oldstyle',
+    'overset',
+    'pmatrix',
+    'raise',
+    'rgroup',
+    'rule',
+    'Rule',
+    // Have to escape this because \S in regex
+    // means 'any non whitespace char'
+    '\\\\S',
+    'skew',
+    'space',
+    'tag',
+    'TeX',
+    'underbrace',
+    'uproot',
+    'varinjlim',
+    'varliminf',
+    'varlimsup',
+    'varprojlim',
+    'vcenter',
+    'vmatrix'
+]
+
+const advancedOnlyRegex = new RegExp(advancedOnlyCommands.join('|'), 'm')
+
+const containsAdvancedSyntax = memoize(latex => advancedOnlyRegex.test(latex))
+
+export {advancedOnlyCommands, containsAdvancedSyntax}
