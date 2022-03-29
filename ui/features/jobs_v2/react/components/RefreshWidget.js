008644c14aa74476481fc44084f285cfb4d0e5d2
Jeremy Stanley <jeremy@instructure.com>
2022-03-29 13:31:23 -0600

    jobs_v2: add refresh buttons
    test plan:
     - spinner should be shown next to group and jobs headers
       while the table is loading
     - while the table isn't loading, a refresh button should be
       present and clicking it should initiate a refresh
    
    flag=jobs_v2
    refs DE-1044
    
    Change-Id: I95bef67683234c365743a110c9131a95fe1e7ce5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/288393
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

---
+++ ui/features/jobs_v2/react/components/RefreshWidget.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useScope as useI18nScope} from '@canvas/i18n'
+import React from 'react'
+import {Spinner} from '@instructure/ui-spinner'
+import {IconButton} from '@instructure/ui-buttons'
+import {IconRefreshLine} from '@instructure/ui-icons'
+
+const I18n = useI18nScope('jobs_v2')
+
+export default function RefreshWidget({loading, onRefresh, title}) {
+  if (loading) {
+    return <Spinner size="x-small" margin="0 0 xx-small xx-small" renderTitle={title} />
+  } else {
+    return (
+      <IconButton
+        withBackground={false}
+        withBorder={false}
+        color="secondary"
+        screenReaderLabel={I18n.t('Refresh')}
+        onClick={onRefresh}
+      >
+        <IconRefreshLine />
+      </IconButton>
+    )
+  }
+}
---
+++ ui/features/jobs_v2/react/components/SectionRefreshHeader.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import RefreshWidget from './RefreshWidget'
+import {Heading} from '@instructure/ui-heading'
+import {Flex} from '@instructure/ui-flex'
+
+export default function SectionRefreshHeader({title, loadingTitle, loading, onRefresh}) {
+  return (
+    <>
+      <Flex alignItems="end">
+        <Flex.Item>
+          <Heading level="h2" margin="large 0 small 0">
+            {title}
+          </Heading>
+        </Flex.Item>
+        <Flex.Item padding="large 0 x-small x-small">
+          <RefreshWidget onRefresh={onRefresh} title={loadingTitle} loading={loading} />
+        </Flex.Item>
+      </Flex>
+    </>
+  )
+}
