59c50bdd28f90c9fc636ef020ea2a3ec154078ac
Ahmad Amireh <ahmad@instructure.com>
2022-03-29 15:54:36 -0600

    submit datepicker usage
    fixes FOO-2280
    flag = none
    
    test plan:
    
    1) assign endpoint in dynamic_settings.yml to point to the inseng one
    2) go to assignment and edit a date
    3) refresh the page and verify the endpoint is hit with PUT and the set
       of events
    
    Change-Id: Ib7e62091cd16c3813534dfe29c1a5084e5f9755e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/288429
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Ahmad Amireh <ahmad@instructure.com>
    Product-Review: Ahmad Amireh <ahmad@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -252,6 +252,10 @@ class ApplicationController < ActionController::Base
           @js_env[:API_GATEWAY_URI] = dynamic_settings_tree["api_gateway_uri"]
         end
 
+        if dynamic_settings_tree["frontend_data_collection_endpoint"]
+          @js_env[:DATA_COLLECTION_ENDPOINT] = dynamic_settings_tree["frontend_data_collection_endpoint"]
+        end
+
         @js_env[:flashAlertTimeout] = 1.day.in_milliseconds if @current_user&.prefers_no_toast_timeout?
         @js_env[:KILL_JOY] = @domain_root_account.kill_joy? if @domain_root_account&.kill_joy?
 
