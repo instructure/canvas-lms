852e4a8dcc71b00c9e4d113468d67b2abe3a7890
Eduardo Escobar <eduardo.escobar@instructure.com>
2022-04-01 16:34:47 -0500

    allow selection and deselection of students/observers
    fixes EVAL-2062
    flag=message_observers_of_students_who
    
    test plan:
    1-)enable the FF "Message Observers of Students
      Who..."
    2-)have a published course.
    3-)have a course with a teacher and at least 5 students
    4-)enroll some observers to be observing some of the
      students.
    5-)go to the gradebook, click on the assignment column
      header and select the "Message Students Who" option
      to open the modal.
    6-)ensure all students are selected by default.
    7-)ensure the students checkbox is checked by default.
    8-)ensure every observer is not selected by default.
    9-)ensure the observers checkbox is not checked by
      default.
    10-)click on one of the students pill and ensure
      the icon changes from a "X" icon to an "PLUS" icon
      and the students checkbox state is indeterminate.
    11-)click again on the same student pill and ensure
      the icon changes from an "X" icon to a "PLUS" icon
      and the checkbox state is checked.
    12-)click on every student pill to deselect all the
      students and ensure the checkbox is not checked.
    13-)click on every student pill again to select all
      the students and ensure the checkbox is not checked.
    14-)repeat steps 10..13 using observers instead of
      students.
    
    Change-Id: I1bf244f9185e7385e9ff1483cfe56c176c1993cc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/288766
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Syed Hussain <shussain@instructure.com>
    Product-Review: Jody Sailor

---
+++ ui/shared/message-students-dialog/react/Pill.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {Tag} from '@instructure/ui-tag'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+import {IconAddSolid, IconXSolid} from '@instructure/ui-icons'
+
+const Pill = ({studentId, observerId = null, text, onClick, selected = false}) => {
+  const contents = selected ? (
+    <>
+      <View margin="0 small 0 0">
+        <Text color="primary">{text}</Text>
+      </View>
+      <IconXSolid data-testid="item-selected" />
+    </>
+  ) : (
+    <>
+      <View margin="0 small 0 0">
+        <Text color="secondary">{text}</Text>
+      </View>
+      <IconAddSolid data-testid="item-unselected" color="brand" />
+    </>
+  )
+
+  return <Tag text={contents} onClick={() => onClick(studentId, observerId)} />
+}
+
+export default Pill
