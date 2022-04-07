9c283973ee4f9ccb1f43dfa71c51764373e645c6
Chrystal Langston <chrystal.langston@instructure.com>
2022-04-06 21:49:55 -0400

    Add support for fetching by assignment ids in Canvas
    refs OUT-4998
    
    flag=outcome_service_results_to_canvas
    
    Test Plan:
    - Prereq: Canvas is provisioned with Outcomes Service
      see https://instructure.atlassian.net/l/c/BDA1nXEX
    - OS: Ensure that you have pulled and are running the latest
      master branch
      - Launch rails console `mc run --rm api bundle exec rails c`
      - switch to the shard that Canvas is provisioned
        `switch_to_shard! 'outcomes.docker'`
      - Create AuthoritativeResults records via Rails
        outcome = Outcome.create!(
            external_type: 'learning_outcome',
            external_id: 123,
            title: 'Outcome 1'
        )
        AuthoritativeResult.create!(
            artifact_type: 'quizzes.quiz',
            artifact_id: '1_test',
            user_uuid: SecureRandom.uuid,
            outcome_id: outcome.id,
            points: 1,
            points_possible: 3,
            event_count: 0,
            event_created_at: Time.zone.now,
            event_updated_at: Time.zone.now,
            associated_asset_id: '1',
            associated_asset_type: 'canvas.assignment.quizzes'
        )
        - run the same AuthoritativeResult query as above but
          with associated_asset_id: '2'
    - Canvas:
      - Create a course and navigate to the course
      - From Settings -> Feature Options, enable
        Outcome Service Results to Canvas Reporting Feature
      - Take note of the course id in the URL
      - Launch the rails console and run the below:
        my_course = Course.find(course_id_in_url)
        include CanvasOutcomesHelper
        get_lmgb_results(my_course, '1', 'canvas.assignment.quizzes')
        - confirm that 1 result is returned
        get_lmgb_results(my_course, '1,2', 'canvas.assignment.quizzes')
        - confirm that 2 results are returned
        get_lmgb_results(my_course, '3', 'canvas.assignment.quizzes')
        - confirm that an empty array list is returned
        - turn off the FF
        my_course.disable_feature(:outcome_service_results_to_canvas)
        - try to query OS again
        get_lmgb_results(my_course, '1', 'canvas.assignment.quizzes')
        - confirm that the results returned are nil
    
    Change-Id: Id5a24097ae2ac1a2cad670ccc6d425e85ef2f50d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/289166
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kyle Rosenbaum <krosenbaum@instructure.com>
    QA-Review: Marcus Pompeu <marcus.pompeu@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- spec/helpers/canvas_outcomes_helper_spec.rb
+++ spec/helpers/canvas_outcomes_helper_spec.rb
@@ -17,9 +17,17 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+require_relative "../spec_helper"
+require "webmock/rspec"
 
 describe CanvasOutcomesHelper do
   subject { Object.new.extend CanvasOutcomesHelper }
+
+  around do |example|
+    WebMock.disable_net_connect!(allow_localhost: true)
+    example.run
+    WebMock.enable_net_connect!
+  end
 
   before do
     course_with_teacher_logged_in(active_all: true)
@@ -130,18 +138,33 @@ describe CanvasOutcomesHelper do
       expect(JWT).to receive(:encode).and_return "encoded"
       expect(subject.extract_domain_jwt(account, "")).to eq ["domain", "encoded"]
     end
+
+    describe "if ApplicationController.test_cluster_name is specified" do
+      it "returns a domain using the test_cluster_name domain" do
+        settings = { consumer_key: "key", jwt_secret: "secret", domain: "domain",
+                     beta_domain: "beta.domain" }
+        account.settings[:provision] = { "outcomes" => settings }
+        account.save!
+        expect(JWT).to receive(:encode).and_return "encoded"
+        allow(ApplicationController).to receive(:test_cluster?).and_return(true)
+        allow(ApplicationController).to receive(:test_cluster_name).and_return("beta")
+        expect(subject.extract_domain_jwt(account, "")).to eq ["beta.domain", "encoded"]
+        allow(ApplicationController).to receive(:test_cluster_name).and_return("invalid")
+        expect(subject.extract_domain_jwt(account, "")).to eq [nil, nil]
+      end
+    end
   end
 
-  describe "#get_lmgb_results_jwt" do
+  describe "#get_lmgb_results" do
     context "without account outcome settings" do
       it "returns nil with no provision settings" do
-        expect(subject.get_lmgb_results_jwt(account, "1", "assign.type")).to eq nil
+        expect(subject.get_lmgb_results(account, "1", "assign.type")).to eq nil
       end
 
       it "returns nil with no outcome provision settings" do
         account.settings[:provision] = {}
         account.save!
-        expect(subject.get_lmgb_results_jwt(account, "1", "assign.type")).to eq nil
+        expect(subject.get_lmgb_results(account, "1", "assign.type")).to eq nil
       end
     end
 
@@ -154,35 +177,86 @@ describe CanvasOutcomesHelper do
 
       context "without assignment ids" do
         it "returns nil when assignment ids is nil" do
-          expect(subject.get_lmgb_results_jwt(account, nil, "assign.type")).to eq nil
+          expect(subject.get_lmgb_results(account, nil, "assign.type")).to eq nil
         end
 
         it "returns nil when assignment ids is empty" do
-          expect(subject.get_lmgb_results_jwt(account, "", "assign.type")).to eq nil
+          expect(subject.get_lmgb_results(account, "", "assign.type")).to eq nil
         end
       end
 
       context "without assignment type" do
         it "returns nil when assignment type is nil" do
-          expect(subject.get_lmgb_results_jwt(account, "1", nil)).to eq nil
+          expect(subject.get_lmgb_results(account, "1", nil)).to eq nil
         end
 
         it "returns nil when assignment type is empty" do
-          expect(subject.get_lmgb_results_jwt(account, "1", "")).to eq nil
+          expect(subject.get_lmgb_results(account, "1", "")).to eq nil
         end
       end
 
       context "with outcomes provision settings" do
-        it "returns jwt with multiple assignment ids" do
-          expect(JWT).to receive(:encode).and_return "encoded"
-          expect(subject.get_lmgb_results_jwt(account, "1,2,3", "assign.type")).to eq "encoded"
+        def stub_get_lmgb_results(params)
+          stub_request(:get, "http://domain/api/authoritative_results?#{params}").with({
+                                                                                         headers: {
+                                                                                           Authorization: /\+*/,
+                                                                                           Accept: "*/*",
+                                                                                           "Accept-Encoding": /\+*/,
+                                                                                           "User-Agent": "Ruby"
+                                                                                         }
+                                                                                       })
         end
 
-        it "returns jwt with single assignment" do
-          expect(JWT).to receive(:encode).and_return "encoded"
-          expect(subject.get_lmgb_results_jwt(account, "1", "assign.type")).to eq "encoded"
+        context "with outcome_service_results_to_canvas FF on" do
+          before do
+            @course.enable_feature!(:outcome_service_results_to_canvas)
+          end
+
+          it "raises error on non 2xx response" do
+            stub_get_lmgb_results("associated_asset_id_list=1&associated_asset_type=assign.type").to_return(status: 401, body: '{"valid_jwt":false}')
+            expect { subject.get_lmgb_results(@course, "1", "assign.type") }.to raise_error(RuntimeError, /Error retrieving results from Outcomes Service:/)
+          end
+
+          it "returns results with one assignment id" do
+            stub_get_lmgb_results("associated_asset_id_list=1&associated_asset_type=assign.type").to_return(status: 200, body: '{"results":[{"result":"stuff"}]}')
+            expect(subject.get_lmgb_results(@course, "1", "assign.type")).to eq [{ "result" => "stuff" }]
+          end
+
+          it "returns results with multiple assignment ids" do
+            stub_get_lmgb_results("associated_asset_id_list=1,2&associated_asset_type=assign.type").to_return(status: 200, body: '{"results":[{"result_one":"stuff1"},{"result_two":"stuff2"}]}')
+            expect(subject.get_lmgb_results(@course, "1,2", "assign.type")).to eq [{ "result_one" => "stuff1" }, { "result_two" => "stuff2" }]
+          end
+
+          it "returns empty array when not assignment ids are matched" do
+            stub_get_lmgb_results("associated_asset_id_list=1&associated_asset_type=assign.type.no.match").to_return(status: 200, body: '{"results":[]}')
+            expect(subject.get_lmgb_results(@course, "1", "assign.type.no.match")).to eq []
+          end
+        end
+
+        context "with outcome_service_results_to_canvas FF off" do
+          before do
+            @course.disable_feature!(:outcome_service_results_to_canvas)
+          end
+
+          it "returns nil when FF is off" do
+            expect(subject.get_lmgb_results(@course, nil, "assign.type")).to eq nil
+          end
         end
       end
+    end
+  end
+
+  describe "#build_request_url" do
+    it "add params if present" do
+      params = { param: "stuff" }
+      expect(subject.build_request_url("protocol", "domain", "endpoint", params)).to eq "protocol://domain/endpoint?param=stuff"
+    end
+
+    it "does not add params when not present" do
+      params = {}
+      expect(subject.build_request_url("protocol", "domain", "endpoint", params)).to eq "protocol://domain/endpoint"
+      params = nil
+      expect(subject.build_request_url("protocol", "domain", "endpoint", params)).to eq "protocol://domain/endpoint"
     end
   end
 end
