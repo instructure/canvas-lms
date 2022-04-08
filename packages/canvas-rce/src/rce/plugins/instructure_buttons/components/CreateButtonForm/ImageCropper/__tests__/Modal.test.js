d5a3e815bd7cc9ba5411cde7bc09845f6bc415b0
Juan Chavez <juan.chavez@instructure.com>
2022-04-08 05:54:38 -0400

    IM: Generate cropped/zoomed image via modal
    closes MAT-174
    
    flag=none
    
    test plan:
    - Navigate to a RCE instance
    - Open Icon Maker
    - Select a Course Image
    - Click Crop button
    - Select a shape
    - Zoom the image if you want (from 1.0x to 2.0x)
    - Click Save
    > Verify that the expected cropped image with
    the respective shape/zoom is replaced in image preview
    and icon preview.
    > Verify this combining shapes and zoom ratios.
    
    note: For the moment we are not able to recover the
    original image (before cropping).
    
    Change-Id: I1100a7f84a73be57000617df5fd72f45404126f3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/289264
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    Reviewed-by: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: David Lyons <lyons@instructure.com>

---
+++ packages/canvas-rce/src/rce/plugins/instructure_buttons/components/CreateButtonForm/ImageCropper/__tests__/Modal.test.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import {ImageCropperModal} from '../Modal'
+import userEvent from '@testing-library/user-event'
+
+jest.mock('../imageCropUtils', () => ({
+  createCroppedImageSvg: jest.fn(() =>
+    Promise.resolve({
+      outerHTML: null
+    })
+  )
+}))
+
+describe('ImageCropperModal', () => {
+  beforeAll(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      blob: () => Promise.resolve(new Blob(['somedata'], {type: 'image/svg+xml'}))
+    })
+  })
+
+  it('calls onSubmit function', async () => {
+    const onSubmit = jest.fn()
+    render(
+      <ImageCropperModal open onSubmit={onSubmit} image="data:image/png;base64,asdfasdfjksdf==" />
+    )
+    const button = screen.getByRole('button', {name: /save/i})
+    userEvent.click(button)
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled()
+    })
+  })
+
+  it('call onSubmit function with correct args', async () => {
+    const onSubmit = jest.fn()
+    render(
+      <ImageCropperModal open onSubmit={onSubmit} image="data:image/png;base64,asdfasdfjksdf==" />
+    )
+    const button = screen.getByRole('button', {name: /save/i})
+    userEvent.click(button)
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledWith('data:image/svg+xml;base64,bnVsbA==')
+    })
+  })
+})
