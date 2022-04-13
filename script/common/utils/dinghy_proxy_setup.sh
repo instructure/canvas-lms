77a6fb750be74fc6a797e65ddb89c3540de58df5
Mark McDermott <mmcdermott@instructure.com>
2022-04-13 17:42:26 -0500

    Don't use reserved address for `DNS_IP`
    By having the `DNS_IP` point to a reserved
    IP for the loopback, this causes all DNS lookups in
    new containers to loopback to itself and unable to
    find containers outside their network if the internal
    DNS server(127.0.0.11) cannot find it as it doesn't
    add the namespace of the dinghy-http-proxy container
    
    flag = none
    
    test plan:
    1. Run `source script/common/utils/common.sh`
    2. Run `setup_dinghy_proxy`
    3. Ensure that
       `docker-compose container inspect http-proxy`
       has `DNS: 192.168.42.42`
    4. Ensure that
        `/Library/LaunchDaemons/com.user.lo0-loopback.plist`
       has the correct alias IP.
    5. Change the IP in the
       `/Library/LaunchDaemons/com.user.lo0-loopback.plist`
       and re-run `setup_dinghy_proxy`
    6. Ensure that the script completes and
       fixes the IP in the file
    
    closes: QUIZ-9476
    
    Change-Id: I28a0668678da64e02bf2d05083d34992c3ab229a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/289650
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Alex Slaughter <aslaughter@instructure.com>
    QA-Review: Mark McDermott <mmcdermott@instructure.com>
    Product-Review: Mark McDermott <mmcdermott@instructure.com>

--- script/common/utils/dinghy_proxy_setup.sh
+++ script/common/utils/dinghy_proxy_setup.sh
@@ -7,15 +7,46 @@ function setup_dinghy_proxy {
     -v /var/run/docker.sock:/tmp/docker.sock:ro \
     -v ~/.dinghy/certs:/etc/nginx/certs \
     -p 80:80 -p 443:443 -p 19322:19322/udp \
-    -e DNS_IP=127.0.0.1 -e CONTAINER_NAME=http-proxy \
+    -e DNS_IP=192.168.42.42  -e CONTAINER_NAME=http-proxy \
     --name http-proxy \
     codekitchen/dinghy-http-proxy
 
     sudo mkdir -p /etc/resolver
 
-    echo 'nameserver 127.0.0.1' | sudo tee /etc/resolver/docker > /dev/null
+    echo 'nameserver 192.168.42.42' | sudo tee /etc/resolver/docker > /dev/null
     echo 'port 19322' | sudo tee -a /etc/resolver/docker > /dev/null
+
+    if [[ ! -f /Library/LaunchDaemons/com.user.lo0-loopback.plist ]]; then
+      message "There's no plist for the loopback. Creating plist file"
+      create_plist_for_restart_persistence
+      if [[ "$(loopback_ip_exists)" != "" ]] ; then
+        message "plist file works!"
+      else
+        message "Your plist file failed to add the loopback. " \
+           "We will try the rest of the install but it may fail"
+      fi
+    else
+      CURRENT_IP_VALUE=$(cat /Library/LaunchDaemons/com.user.lo0-loopback.plist | grep -A 1 '<string>alias' | tail -n 1 | sed 's/<[\/]*string>//g'| sed 's/^ *//')
+      if [[ "$(loopback_ip_exists)" != "" ]]  && [[ $CURRENT_IP_VALUE =~ "192.168.42.42" ]]; then
+        message "Loopback interface is set and your plist is okay!"
+      else
+        message "The value currently set in your plist is ${CURRENT_IP_VALUE}\n" \
+           "which will affect how containers communicate outside of canvas. Now updating the file"
+        sudo launchctl unload /Library/LaunchDaemons/com.user.lo0-loopback.plist
+        create_plist_for_restart_persistence
+        sleep 5
+        if [[ "$(loopback_ip_exists)" == "" ]] ; then
+          warning_message "Updating plist and adding to interface failed \n" \
+            "Setting manually but will not persist between reboots"
+          sudo ifconfig lo0 alias 192.168.42.42
+        fi
+      fi
+    fi
   fi
+}
+
+function loopback_ip_exists {
+  echo "$(ifconfig lo0 | grep 'inet 192.168.42.42')"
 }
 
 function dinghy_machine_exists {
@@ -29,4 +60,40 @@ function dinghy_machine_exists {
     fi
   fi
   return 1
+}
+
+function create_plist_for_restart_persistence () {
+  PLIST_FOR_LOOPBACK=$(cat << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC -//Apple Computer//DTD PLIST 1.0//EN http://www.apple.com/DTDs/PropertyList-1.0.dtd >
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.user.lo0-loopback</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>/sbin/ifconfig</string>
+    <string>lo0</string>
+    <string>alias</string>
+    <string>192.168.42.42</string>
+  </array>
+  <key>RunAtLoad</key> <true/>
+  <key>Nice</key>
+  <integer>10</integer>
+  <key>KeepAlive</key>
+  <false/>
+  <key>AbandonProcessGroup</key>
+  <true/>
+  <key>StandardErrorPath</key>
+  <string>/var/log/loopback-alias.log</string>
+  <key>StandardOutPath</key>
+  <string>/var/log/loopback-alias.log</string>
+</dict>
+</plist>
+EOF
+)
+  echo "${PLIST_FOR_LOOPBACK}" | sudo tee /Library/LaunchDaemons/com.user.lo0-loopback.plist > /dev/null
+  chmod 0644 /Library/LaunchDaemons/com.user.lo0-loopback.plist
+  chown root:wheel /Library/LaunchDaemons/com.user.lo0-loopback.plist
+  sudo launchctl load /Library/LaunchDaemons/com.user.lo0-loopback.plist
 }
