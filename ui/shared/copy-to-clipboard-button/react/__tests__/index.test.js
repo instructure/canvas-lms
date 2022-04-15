434b64d1728068de4ca2ab8ee838c07e805016c6
Jeremy Stanley <jeremy@instructure.com>
2022-04-15 15:50:00 -0600

    jobs_v2: refactor CopyToClipboardButton
    also make the success/failure feedback disappear on a timer
    instead of on blur, which is more consistent with what other
    sites do (also the blur doesn't work quite right on Safari)
    
    test plan:
     - the copy-to-clipboard button in the jobs list should
       behave as expected
    
    refs DE-1142
    
    Change-Id: I95bd757bc182c6b16ebcc9c8c5de576b2ad32313
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/289774
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

---
+++ ui/shared/copy-to-clipboard-button/react/__tests__/index.test.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import CopyToClipboardButton from '../index'
+
+describe('CopyToClipboardButton', () => {
+  let originalClipboard
+
+  beforeAll(() => {
+    const writeText = jest.fn(() => Promise.resolve({}))
+    originalClipboard = navigator.clipboard
+    navigator.clipboard = {writeText}
+  })
+
+  afterAll(() => {
+    navigator.clipboard = originalClipboard
+  })
+
+  it('copies value when the button is clicked', () => {
+    const {getByRole} = render(<CopyToClipboardButton value="foobar" />)
+    const button = getByRole('button', {name: 'Copy'})
+    fireEvent.click(button)
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith('foobar')
+  })
+})
