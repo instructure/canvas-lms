acc9c8ed38046e62a5745d7e74ebd74ea33e3aa7
Jackson Howe <jackson.howe@instructure.com>
2022-04-18 14:33:55 -0600

    Grant appointment groups with :manage_calendar permission
    closes LS-3118
    flag = none
    
    Test plan:
     - As a teacher, expect to be able to create and see an AG in the
       calendar
     - As a student, expect to not have permission to create an AG
     - Create a custom role based on teacher where manage_calendar is
       disabled
     - Enroll a user in a course with that custom role
     - Expect to not be able to create AGs
     - Create a course with 2 sections, enroll a teacher in one section
     - Expect to be able to create AGs in both sections
     - On roster page of the multi-section course, open user details
       for the teacher and `limit this user to only see fellow section
       users`
     - Back in the calendar, expect the teacher to be able to create AGs
       only in the section where enrolled as a teacher
    
    Change-Id: I09898c4a20d5fc78b5f93cb2fadc60c71eebed72
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/289853
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -51,7 +51,7 @@ class CalendarsController < ApplicationController
         if ag.grants_right? @current_user, session, :create
           ag_permission = { all_sections: true }
         else
-          section_ids = context.section_visibilities_for(@current_user).pluck(:course_section_id)
+          section_ids = CourseSection.find(context.section_visibilities_for(@current_user).pluck(:course_section_id)).select { |cs| cs.grants_right?(@current_user, session, :manage_calendar) }.pluck(:id)
           ag_permission = { all_sections: false, section_ids: section_ids } if section_ids.any?
         end
       end
