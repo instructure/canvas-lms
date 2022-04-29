69557183f23349a23a61dd4cdd9f3dbfd5208456
Spencer Olson <solson@instructure.com>
2022-04-29 10:13:03 -0500

    Revert "include submitted classic quizzes in what-if scores calcs"
    This reverts commit ad9856354b0eb44f5459a1e8dc54cba436a9741f.
    
    Change-Id: Ia1ba4cf568890d2687351083ba1f6227c20e9c57
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/290819
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Eduardo Escobar <eduardo.escobar@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- ui/shared/k5/react/__tests__/utils.test.js
+++ ui/shared/k5/react/__tests__/utils.test.js
@@ -372,7 +372,6 @@ describe('getAssignmentGroupTotals', () => {
             name: '1',
             points_possible: 10.0,
             grading_type: 'points',
-            submission_types: ['online_text_entry'],
             submission: {
               score: 7.0,
               grade: '7.0',
@@ -386,7 +385,6 @@ describe('getAssignmentGroupTotals', () => {
             name: '2',
             points_possible: 5.0,
             grading_type: 'points',
-            submission_types: ['online_text_entry'],
             submission: {
               score: 5.0,
               grade: '5.0',
@@ -432,7 +430,6 @@ describe('getAssignmentGroupTotals', () => {
             name: '1',
             points_possible: 10.0,
             grading_type: 'points',
-            submission_types: ['online_text_entry'],
             submission: {
               score: 7.0,
               grade: '7.0',
@@ -452,7 +449,6 @@ describe('getAssignmentGroupTotals', () => {
             name: '2',
             points_possible: 10.0,
             grading_type: 'points',
-            submission_types: ['online_text_entry'],
             submission: {
               score: 7.0,
               grade: '7.0',
