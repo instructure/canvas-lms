fcb53c27e7349aa1060d1ed26b186b1e3978bf33
Martin Yosifov <martin.yosifov@instructure.com>
2022-05-02 12:38:22 -0700

    Create alignment summary and header components
    closes OUT-5071
    flag=outcome_alignment_summary
    
    Test plan:
    - Enable Improved Outcomes Management FF
    - Enable Outcome Alignment Summary FF
    - Go to Course > Outcomes and select Alignments Tab
    - Verify that summary header looks like design linked in ticket,
    and has two stat boxes, filter dropdown and search bar
    - Verify that if you hover over or click on info icon you can see
    a tooltip matching desing linked in ticket
    - Verify that you can use keyboard navigation to reach info icon
    and see the tooltip
    - Resize the browser screen and verify that the summary header
    components wrap for smaller screen sizes
    
    Change-Id: Ie37ca51511860f4518d73550f4198c5fdd31b3b3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/291230
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kyle Rosenbaum <krosenbaum@instructure.com>
    Reviewed-by: Angela Gomba <angela.gomba@instructure.com>
    QA-Review: Angela Gomba <angela.gomba@instructure.com>
    Product-Review: Ben Friedman <ben.friedman@instructure.com>

---
+++ ui/features/outcome_management/react/Alignments/__tests__/AlignmentStatItem.test.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import AlignmentStatItem from '../AlignmentStatItem'
+
+describe('AlignmentStatItem', () => {
+  const defaultProps = (props = {}) => ({
+    type: 'outcome',
+    count: 10,
+    percent: 0.75,
+    average: 1.2,
+    ...props
+  })
+
+  it('renders component', () => {
+    const {getByTestId} = render(<AlignmentStatItem {...defaultProps()} />)
+    expect(getByTestId('outcome-alignment-stat-item')).toBeTruthy()
+  })
+
+  it('changes stat name depending on type', () => {
+    const {getByText, queryByText} = render(
+      <AlignmentStatItem {...defaultProps({type: 'artifact'})} />
+    )
+    expect(queryByText(/OUTCOMES/)).not.toBeInTheDocument()
+    expect(getByText(/ALIGNABLE ARTIFACTS/)).toBeInTheDocument()
+  })
+
+  it('changes stat description depending on type', () => {
+    const {getByText, queryByText} = render(
+      <AlignmentStatItem {...defaultProps({type: 'artifact'})} />
+    )
+    expect(queryByText(/Avg. Alignments per Outcome/)).not.toBeInTheDocument()
+    expect(getByText(/Avg. Alignments per Artifact/)).toBeInTheDocument()
+  })
+
+  it('displays count stat', () => {
+    const {getByText} = render(<AlignmentStatItem {...defaultProps({count: 17})} />)
+    expect(getByText(/17 OUTCOMES/)).toBeInTheDocument()
+  })
+
+  it('displays percent stat', () => {
+    const {getByText} = render(<AlignmentStatItem {...defaultProps({percent: 0.67})} />)
+    expect(getByText(/67%/)).toBeInTheDocument()
+  })
+
+  it('rounds percent stat', () => {
+    const {getByText} = render(<AlignmentStatItem {...defaultProps({percent: 0.678})} />)
+    expect(getByText(/68%/)).toBeInTheDocument()
+  })
+
+  it('displays average stat', () => {
+    const {getByText} = render(<AlignmentStatItem {...defaultProps({average: 3.2})} />)
+    expect(getByText(/3.2/)).toBeInTheDocument()
+  })
+
+  it('rounds average stat to 1 digit after decimal point', () => {
+    const {getByText} = render(<AlignmentStatItem {...defaultProps({average: 3.25})} />)
+    expect(getByText(/3.3/)).toBeInTheDocument()
+  })
+
+  it('displays info tooltip if type is artifact', () => {
+    const {getByText, getByTestId} = render(
+      <AlignmentStatItem {...defaultProps({type: 'artifact'})} />
+    )
+    fireEvent.click(getByTestId('outcome-alignment-stat-info-icon'))
+    expect(getByText(/Outcomes may be aligned/)).toBeInTheDocument()
+  })
+
+  it('does not display info tooltip if type is outcome', () => {
+    const {queryByTestId} = render(<AlignmentStatItem {...defaultProps()} />)
+    expect(queryByTestId('outcome-alignment-stat-info-icon')).not.toBeInTheDocument()
+  })
+})
