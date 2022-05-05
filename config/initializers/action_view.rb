13698f0ad9d518f5ac2eee0eb049276a00d6c4e2
Jacob Burroughs <jburroughs@instructure.com>
2022-05-05 11:26:26 -0500

    Just no links header
    Change-Id: I4307d988623e6c913e147af4fb38e3654c2614fd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/291205
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Alex Slaughter <aslaughter@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -17,50 +17,14 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-if Rails.version >= "6.1" && Rails.version < "7.0"
-  # From https://github.com/rails/rails/pull/42056/files
-  module SaferPreloadLinksHeader
-    MAX_HEADER_SIZE = 8_000 # Some HTTP client and proxies have a 8kiB header limit
-
+if Rails.version >= "6.1"
+  module NoLinksHeader
     def send_preload_links_header(preload_links, max_header_size: MAX_HEADER_SIZE)
-      if respond_to?(:request) && request
-        request.send_early_hints("Link" => preload_links.join("\n"))
-      end
-
-      # INST: add `&& !response.sending?` to support template streaming
-      if respond_to?(:response) && response && !response.sending?
-        header = response.headers["Link"]
-        header = header ? header.dup : +""
-
-        # rindex count characters not bytes, but we assume non-ascii characters
-        # are rare in urls, and we have a 192 bytes margin.
-        last_line_offset = header.rindex("\n")
-        last_line_size = if last_line_offset
-                           header.bytesize - last_line_offset
-                         else
-                           header.bytesize
-                         end
-
-        preload_links.each do |link|
-          if link.bytesize + last_line_size + 1 < max_header_size
-            unless header.empty?
-              header << ","
-              last_line_size += 1
-            end
-          else
-            header << "\n"
-            last_line_size = 0
-          end
-          header << link
-          last_line_size += link.bytesize
-        end
-
-        response.headers["Link"] = header
-      end
+      # Intentional noop so we don't bload the headers to be too big
     end
   end
 
   # Directly put it in ActionView::Base in case that has already been loaded
-  ActionView::Helpers::AssetTagHelper.prepend(SaferPreloadLinksHeader)
-  ActionView::Base.prepend(SaferPreloadLinksHeader)
+  ActionView::Helpers::AssetTagHelper.prepend(NoLinksHeader)
+  ActionView::Base.prepend(NoLinksHeader)
 end
