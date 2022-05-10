8ae2c262fd9621cf697e5a6cb47261a7a052bdd6
Kyle Rosenbaum <krosenbaum@instructure.com>
2022-05-10 15:21:51 -0700

    add average calculation method behind feature flag
    refs OUT-5114
    
    test plan:
    - enable improved outcomes management FF && outcome average
    calculation method FF && account and course level outcome mastery scales
    - at the account level, change calculation method to average
    - average calucaltion method saves as expected
    - disable outcome average calculation method FF
    - at the account level, view calculation method
    - error is displayed, mastery caluclation method uses default calucaltion
    method
    - mastery calculation should be empty
    - disable improved outcomes management FF && account and course level
    outcome mastery scales FF
    - enable outcome average calculation method FF
    - create an outcome with a calculation method of average
    - disable outcome average calculation method FF
    - view outcome with average caluclation method
    - calculation method is empty
    
    Change-Id: Ied37e6aae7a76eaf18a3911310ad39c825ab79d6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/291613
    Reviewed-by: Martin Yosifov <martin.yosifov@instructure.com>
    QA-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -56,7 +56,8 @@ class OutcomesController < ApplicationController
           @context.grants_right?(@current_user, session, :manage_proficiency_calculations)
       },
       OUTCOMES_FRIENDLY_DESCRIPTION: Account.site_admin.feature_enabled?(:outcomes_friendly_description),
-      OUTCOME_ALIGNMENT_SUMMARY: @context.root_account.feature_enabled?(:outcome_alignment_summary)
+      OUTCOME_ALIGNMENT_SUMMARY: @context.root_account.feature_enabled?(:outcome_alignment_summary),
+      OUTCOME_AVERAGE_CALCULATION: @context.root_account.feature_enabled?(:outcome_average_calculation)
     )
 
     set_tutorial_js_env
