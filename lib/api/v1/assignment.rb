5ceabfcfd00d439679365120289d5ddc5eb1668b
paulo.chaves <paulo.chaves@instructure.com>
2022-06-13 17:42:29 -0300

    Remove rubric from API if association is deleted
    Closes EVAL-2489
    flag=none
    
    test plan:
    
    - Have an assignment with a rubric associated
    - Duplicate the assignment
    - Open the duplicate and remove the rubric
    - Check the API for the duplicate and note
      that the `rubric` and `rubric_settings`
      attributes are no longer there
    
    Change-Id: I3f2bf84f74bc0ee21da8e35db5536973654bca73
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/293893
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Samuel Lee <samuel.lee@instructure.com>
    Product-Review: Jody Sailor

--- lib/api/v1/assignment.rb
+++ lib/api/v1/assignment.rb
@@ -299,7 +299,7 @@ module Api::V1::Assignment
         end
       end
 
-      if assignment.rubric && !assignment.rubric.deleted?
+      if assignment.active_rubric_association?
         rubric = assignment.rubric
         hash["rubric"] = rubric.data.map do |row|
           row_hash = row.slice(:id, :points, :description, :long_description, :ignore_for_scoring)
