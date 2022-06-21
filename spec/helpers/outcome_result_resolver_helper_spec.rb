8e1d7e3e76a454fb0fb13feb11b9370697569d47
Angela Gomba <angela.gomba@instructure.com>
2022-06-21 08:05:14 -0400

    Resolve OS Outcome Results
    closes OUT-5097
    flag=outcome_service_results_to_canvas
    
    Test Plan:
    - You will need:
      - an environment with:
        - Canvas + OS + Quiz LTI
        - Canvas to OS Live Events
        - Quiz LTI to OS Live Events
       - a course with outcome_service_results_to_canvas enabled
       - a New Quizzes assignment aligned to an outcome
       - a Rubric aligned to that outcome and attached to that quiz
       - an enrolled student
    - Act as the student and take the NQ assignment
    - Stop acting as the student
    - Go to Grades, find the column with the NQ assignment and click
      the three dots to open the dropdown.
    - In the dropdown, select SpeedGrader
    - For the student who took the NQ, use the right hand side to
      grade them using the Rubric
    - After that, a LOR should be created
    - Adjust the first five lines of the script below and execute it
      within the canvas-lms rails console
    
    course_id =
    user_id =
    outcome_id =
    assignment_id =
    
    switch_to_shard! ''
    
    course = Course.find(course_id)
    user = User.find(user_id)
    outcome_id = outcome_id.to_s
    associated_asset = assignment_id.to_s
    
    include CanvasOutcomesHelper
    json = get_lmgb_results(
             course,
             associated_asset,
             'canvas.assignment.quizzes',
             outcome_id,
             user.uuid
           )
    
    include OutcomeResultResolverHelper
    resolve_outcome_results(json)
    
    - Assert that the resulting output is empty
    
    Change-Id: Idcff8ef69f525fc2bad36b667eef144b4618736d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/293952
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Chrystal Langston <chrystal.langston@instructure.com>
    QA-Review: Chrystal Langston <chrystal.langston@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

---
+++ spec/helpers/outcome_result_resolver_helper_spec.rb
@@ -0,0 +1,142 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2022 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+describe OutcomeResultResolverHelper do
+  include OutcomesServiceAuthoritativeResultsHelper
+
+  let(:time) { Time.zone.now }
+
+  before(:once) do
+    names = %w[Keroppi HelloKitty BadtzMaru]
+
+    course_factory
+    course_with_teacher(course: @course)
+
+    @students = create_users(names.map { |name| { name: "User #{name}", sortable_name: "#{name}, User" } }, return_type: :record)
+
+    @students.each do |s|
+      course_with_user("StudentEnrollment", course: @course, user: s)
+    end
+  end
+
+  describe "removes the alignment result" do
+    it "if there is a rubric result for that student, assignment, and outcome" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric({ assignment: @assignment })
+      create_learning_outcome_result_from_rubric @students[0], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 0
+    end
+
+    it "if a rubric result exists for multiple students for the same assignment and outcome" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      create_learning_outcome_result @students[1], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric({ assignment: @assignment })
+      create_learning_outcome_result_from_rubric @students[0], 1.0
+      create_learning_outcome_result_from_rubric @students[1], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 0
+    end
+
+    it "for just one student if a rubric result exists for their assignment and outcome" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      create_learning_outcome_result @students[1], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric({ assignment: @assignment })
+      create_learning_outcome_result_from_rubric @students[0], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+
+    it "for an assignment with multiple outcomes aligned, where one outcome has a rubric result for a student" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric({ assignment: @assignment })
+      create_learning_outcome_result_from_rubric @students[0], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+  end
+
+  describe "doesn't remove the alignment result" do
+    it "if there is no rubric result for that student, assignment, and outcome" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+
+    it "if there is no rubric result for that student" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric({ assignment: @assignment })
+      create_learning_outcome_result_from_rubric @students[1], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+
+    it "if there is no rubric result for that assignment" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[2], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_alignment_with_rubric
+      create_learning_outcome_result_from_rubric @students[2], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+
+    it "if there is no rubric result for that outcome" do
+      create_outcome
+      create_alignment
+      create_learning_outcome_result @students[0], 1.0
+      authoritative_results = authoritative_results_from_db
+
+      create_outcome
+      create_alignment_with_rubric
+      create_learning_outcome_result_from_rubric @students[0], 1.0
+
+      expect(resolve_outcome_results(authoritative_results).size).to eq 1
+    end
+  end
+end
