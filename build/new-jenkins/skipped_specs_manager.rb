4a225ff52b616cf16a77094bb684c547fa9d57a9
Jacob DeWar <jacob.dewar@instructure.com>
2022-06-24 13:40:31 -0400

    Fix JS skipped specs reporting
    closes MAT-905
    flag=none
    Test plan: JS coverage build reports skipped specs
    
    Change-Id: Ibb3855ba23ab53203851f3d15408e826b9f74f0b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/294767
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Jake Oeding <jake.oeding@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

--- build/new-jenkins/skipped_specs_manager.rb
+++ build/new-jenkins/skipped_specs_manager.rb
@@ -83,8 +83,11 @@ class SkippedSpecsManager
   end
 
   def write_to_file
-    puts "writing #{@updated_map.keys.count} spec(s) to '/usr/src/app/out/#{FILENAME}'"
-    File.write("/usr/src/app/out/#{FILENAME}", @updated_map.sort_by { |_spec_key, attrs| -attrs[:duration] }.to_h.to_json)
+    path = @mode == "ruby" ? "/usr/src/app/out/" : "/tmp/"
+    full_path = "#{path}/#{FILENAME}"
+
+    puts "writing #{@updated_map.keys.count} spec(s) to '#{full_path}'"
+    File.write(full_path, @updated_map.sort_by { |_spec_key, attrs| -attrs[:duration] }.to_h.to_json)
   end
 end
 
