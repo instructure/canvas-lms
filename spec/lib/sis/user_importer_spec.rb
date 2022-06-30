ab179e16a491dabc85c8713b8a88dae3f3fdb0c1
Ben Rinaca <brinaca@instructure.com>
2022-06-30 09:27:23 -0700

    catch import errors on user preparation
    refs FOO-2771
    
    some of the hook implementations may raise errors and we want that to
    only fail the single row rather than the whole batch
    
    Change-Id: I7d7ad5f730f8254abe1ad18b326685820206d7e4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/295166
    Reviewed-by: Sean Scally <sean.scally@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Ben Rinaca <brinaca@instructure.com>
    Product-Review: Ben Rinaca <brinaca@instructure.com>

--- spec/lib/sis/user_importer_spec.rb
+++ spec/lib/sis/user_importer_spec.rb
@@ -48,6 +48,66 @@ describe SIS::UserImporter do
     end
   end
 
+  describe SIS::UserImporter::Work do
+    let(:account) { account_model }
+    let(:user_sis_id) { "sis_id" }
+    let(:sis_batch) { account.sis_batches.create! }
+    let(:messages) { [] }
+    let(:user_importer) { SIS::UserImporter::Work.new(sis_batch, account, Rails.logger, messages) }
+    let(:sis_user) do
+      SIS::Models::User.new(user_id: user_sis_id, login_id: "123456", status: "active",
+                            full_name: "User One", email: "user1@example.com", integration_id: "iid1234")
+    end
+
+    context "when a matching login exists" do
+      let(:existing_login) { pseudonym_model(account: account, sis_user_id: user_sis_id) }
+
+      before do
+        existing_login
+      end
+
+      context "when the force_new_user? hook implementation raises an ImportError" do
+        let(:error_message) { "existing user sisplosion" }
+
+        before do
+          allow(user_importer).to receive(:force_new_user?).and_raise(SIS::ImportError.new(error_message))
+          user_importer.add_user(sis_user)
+        end
+
+        it "records a sis batch warning" do
+          user_importer.process_batch
+          expect(messages).not_to be_empty
+          expect(messages.first.message).to eq(error_message)
+        end
+      end
+    end
+
+    context "when no matching login exists" do
+      context "when there is a user found in an implementation of the other_user hook" do
+        let(:other_user) { user_model }
+
+        before do
+          allow(user_importer).to receive(:other_user).and_return(other_user)
+        end
+
+        context "when the force_new_user? hook implementation raises an ImportError" do
+          let(:error_message) { "other user sisplosion" }
+
+          before do
+            allow(user_importer).to receive(:force_new_user?).and_raise(SIS::ImportError.new(error_message))
+            user_importer.add_user(sis_user)
+          end
+
+          it "records a sis batch warning" do
+            user_importer.process_batch
+            expect(messages).not_to be_empty
+            expect(messages.first.message).to eq(error_message)
+          end
+        end
+      end
+    end
+  end
+
   it "must raise ImportError when user doesn't have status" do
     messages = []
     account_model
