536265a104ecaad740d89a1c4a60d1f8c651ec32
Aaron Shafovaloff <ashafovaloff@instructure.com>
2022-07-07 10:28:32 -0500

    Show filter conditions in columns
    Iterative work toward new enhanced filters design.
    
    Test plan:
     - Turn on the enhanced gradebook filters feature flag
     - Ensure your course has sections, student groups, assignment groups,
       and modules
     - Create a filter
       - Grading Periods, Submissions, Start Date, and End Date conditions
         should (always) show
         - Conditions options should show for each of these
     - Apply the filter and refresh the page
       - All the conditions previously shown should show again
    
    flag=enhanced_gradebook_filters
    
    Closes EVAL-2387
    
    Change-Id: I138787cb78079f4cfdff0625d14d8f5548366bc5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/296101
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jody Sailor

--- ui/features/gradebook/react/default_gradebook/components/FilterNavCondition.tsx
+++ ui/features/gradebook/react/default_gradebook/components/FilterNavCondition.tsx
@@ -16,13 +16,9 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useRef} from 'react'
+import React from 'react'
 import {useScope as useI18nScope} from '@canvas/i18n'
-import {Flex} from '@instructure/ui-flex'
 import {SimpleSelect} from '@instructure/ui-simple-select'
-import {ScreenReaderContent} from '@instructure/ui-a11y-content'
-import {IconButton} from '@instructure/ui-buttons'
-import {IconTrashLine} from '@instructure/ui-icons'
 import CanvasDateInput from '@canvas/datetime/react/components/DateInput'
 import moment from 'moment'
 import {MomentInput} from 'moment-timezone'
@@ -38,7 +34,6 @@ import type {
 
 const I18n = useI18nScope('gradebook')
 
-const {Item} = Flex as any
 const {Option, Group: OptionGroup} = SimpleSelect as any
 const formatDate = date => tz.format(date, 'date.formats.medium')
 const dateLabels = {'start-date': I18n.t('Start Date'), 'end-date': I18n.t('End Date')}
@@ -50,14 +45,23 @@ const submissionTypeOptions: SubmissionTypeOption[] = [
   ['has-submissions', I18n.t('Has submissions')]
 ]
 
+const conditionTypeLabels = {
+  'assignment-group': I18n.t('Assignment Groups'),
+  'grading-period': I18n.t('Grading Periods'),
+  module: I18n.t('Modules'),
+  section: I18n.t('Sections'),
+  'student-group': I18n.t('Student Groups'),
+  submission: I18n.t('Submissions'),
+  'start-date': I18n.t('Start Date'),
+  'end-date': I18n.t('End Date')
+}
+
 type Props = {
   assignmentGroups: AssignmentGroup[]
   condition: FilterCondition
-  conditionsInFilter: any
   gradingPeriods: GradingPeriod[]
   modules: Module[]
   onChange: any
-  onDelete: any
   sections: Section[]
   studentGroupCategories: StudentGroupCategoryMap
 }
@@ -66,17 +70,13 @@ type MenuItem = [id: string, name: string]
 
 export default function ({
   condition,
-  conditionsInFilter,
   onChange,
   modules,
   assignmentGroups,
   gradingPeriods,
   sections,
-  onDelete,
   studentGroupCategories
 }: Props) {
-  const divRef = useRef(null)
-
   let items: MenuItem[] = []
   let itemGroups: [string, string, MenuItem[]][] = []
 
@@ -109,165 +109,85 @@ export default function ({
     }
   }
 
-  const otherConditions = conditionsInFilter.filter(condition_ => condition_.id !== condition.id)
-
   return (
-    <Flex justifyItems="space-between" elementRef={el => (divRef.current = el)}>
-      <Item>
+    <>
+      {(items.length > 0 || itemGroups.length > 0) && (
         <SimpleSelect
-          width="95%"
-          renderLabel={<ScreenReaderContent>{I18n.t('Condition type')}</ScreenReaderContent>}
-          placeholder={I18n.t('Select condition type')}
-          value={condition.type || '_'}
+          data-testid="select-condition"
+          key={condition.type} // resets dropdown when condition type is changed
+          renderLabel={conditionTypeLabels[condition.type || 'assignment-group']}
           size="small"
-          onChange={(_event, {value}) =>
+          value={condition.value || '_'}
+          onChange={(_event, {value}) => {
             onChange({
               ...condition,
-              type: value,
-              value: null
+              value
             })
-          }
+          }}
         >
-          {assignmentGroups.length > 0 &&
-            otherConditions.every(c => c.type !== 'assignment-group') && (
-              <Option id={`${condition.id}-assignment-group`} value="assignment-group">
-                {I18n.t('Assignment Group')}
+          {items.map(([id, name]: [string, string]) => {
+            return (
+              <Option key={id} id={`${condition.id}-item-${id}`} value={id}>
+                {name}
               </Option>
-            )}
+            )
+          })}
 
-          {gradingPeriods.length > 0 && otherConditions.every(c => c.type !== 'grading-period') && (
-            <Option id={`${condition.id}-grading-period`} value="grading-period">
-              {I18n.t('Grading Period')}
-            </Option>
-          )}
-
-          {modules.length > 0 && otherConditions.every(c => c.type !== 'module') && (
-            <Option id={`${condition.id}-module`} value="module">
-              {I18n.t('Module')}
-            </Option>
-          )}
-
-          {sections.length > 0 && otherConditions.every(c => c.type !== 'section') && (
-            <Option id={`${condition.id}-section`} value="section">
-              {I18n.t('Section')}
-            </Option>
-          )}
-
-          {Object.keys(studentGroupCategories).length > 0 &&
-            otherConditions.every(c => c.type !== 'student-group') && (
-              <Option id={`${condition.id}-student-group`} value="student-group">
-                {I18n.t('Student Group')}
-              </Option>
-            )}
-
-          {otherConditions.every(c => c.type !== 'submissions') && (
-            <Option id={`${condition.id}-submissions`} value="submissions">
-              {I18n.t('Submissions')}
-            </Option>
-          )}
-
-          {otherConditions.every(c => c.type !== 'start-date') && (
-            <Option id={`${condition.id}-start-date`} value="start-date">
-              {I18n.t('Start Date')}
-            </Option>
-          )}
-
-          {otherConditions.every(c => c.type !== 'end-date') && (
-            <Option id={`${condition.id}-end-date`} value="end-date">
-              {I18n.t('End Date')}
-            </Option>
-          )}
+          {itemGroups.map(([id, name, items_]) => {
+            return (
+              <OptionGroup value={id} renderLabel={name}>
+                {items_.map(([itemId, itemName]: [string, string]) => {
+                  return (
+                    <Option key={itemId} id={`${condition.id}-item-${itemId}`} value={itemId}>
+                      {itemName}
+                    </Option>
+                  )
+                })}
+              </OptionGroup>
+            )
+          })}
         </SimpleSelect>
-      </Item>
-      <Flex>
-        {(items.length > 0 || itemGroups.length > 0) && (
-          <SimpleSelect
-            data-testid="select-condition"
-            key={condition.type} // resets dropdown when condition type is changed
-            renderLabel={<ScreenReaderContent>{I18n.t('Condition')}</ScreenReaderContent>}
-            size="small"
-            placeholder={I18n.t('Select condition')}
-            value={condition.value || '_'}
-            onChange={(_event, {value}) => {
-              onChange({
-                ...condition,
-                value
-              })
-            }}
-          >
-            {items.map(([id, name]: [string, string]) => {
-              return (
-                <Option key={id} id={`${condition.id}-item-${id}`} value={id}>
-                  {name}
-                </Option>
-              )
-            })}
-
-            {itemGroups.map(([id, name, items_]) => {
-              return (
-                <OptionGroup value={id} renderLabel={name}>
-                  {items_.map(([itemId, itemName]: [string, string]) => {
-                    return (
-                      <Option key={itemId} id={`${condition.id}-item-${itemId}`} value={itemId}>
-                        {itemName}
-                      </Option>
-                    )
-                  })}
-                </OptionGroup>
-              )
-            })}
-          </SimpleSelect>
-        )}
-        {['start-date', 'end-date'].includes(condition.type || '') && (
-          <CanvasDateInput
-            size="small"
-            dataTestid="date-input"
-            renderLabel={<ScreenReaderContent>{dateLabels[condition.type!]}</ScreenReaderContent>}
-            selectedDate={condition.value}
-            formatDate={formatDate}
-            interaction="enabled"
-            onSelectedDateChange={(value: MomentInput) => {
-              onChange({
-                ...condition,
-                value: value ? moment(value).toISOString() : null
-              })
-            }}
-          />
-        )}
-        {condition.type === 'submissions' && (
-          <SimpleSelect
-            key={condition.type} // resets dropdown when condition type is changed
-            renderLabel={<ScreenReaderContent>{I18n.t('Condition')}</ScreenReaderContent>}
-            size="small"
-            data-testid="submissions-input"
-            placeholder={I18n.t('Select condition')}
-            value={condition.value || '_'}
-            onChange={(_event, {value}) => {
-              onChange({
-                ...condition,
-                value
-              })
-            }}
-          >
-            {submissionTypeOptions.map(([id, name]: SubmissionTypeOption) => {
-              return (
-                <Option key={id} id={`${condition.id}-item-${id}`} value={id}>
-                  {name}
-                </Option>
-              )
-            })}
-          </SimpleSelect>
-        )}
-        <IconButton
-          data-testid="delete-condition"
-          onClick={() => onDelete(condition, divRef)}
-          screenReaderLabel={I18n.t('Delete condition')}
-          withBackground={false}
-          withBorder={false}
+      )}
+      {['start-date', 'end-date'].includes(condition.type || '') && (
+        <CanvasDateInput
+          size="small"
+          dataTestid="date-input"
+          renderLabel={dateLabels[condition.type!]}
+          selectedDate={condition.value}
+          formatDate={formatDate}
+          interaction="enabled"
+          onSelectedDateChange={(value: MomentInput) => {
+            onChange({
+              ...condition,
+              value: value ? moment(value).toISOString() : null
+            })
+          }}
+        />
+      )}
+      {condition.type === 'submissions' && (
+        <SimpleSelect
+          key={condition.type} // resets dropdown when condition type is changed
+          renderLabel={I18n.t('Submissions')}
+          size="small"
+          data-testid="submissions-input"
+          placeholder={I18n.t('Select condition')}
+          value={condition.value || '_'}
+          onChange={(_event, {value}) => {
+            onChange({
+              ...condition,
+              value
+            })
+          }}
         >
-          <IconTrashLine />
-        </IconButton>
-      </Flex>
-    </Flex>
+          {submissionTypeOptions.map(([id, name]: SubmissionTypeOption) => {
+            return (
+              <Option key={id} id={`${condition.id}-item-${id}`} value={id}>
+                {name}
+              </Option>
+            )
+          })}
+        </SimpleSelect>
+      )}
+    </>
   )
 }
