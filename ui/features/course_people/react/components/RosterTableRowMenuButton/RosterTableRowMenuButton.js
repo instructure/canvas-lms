38408a801e7c41bad01ea384a8bc045e558f2f5f
Nic Nolan <nic.nolan@instructure.com>
2022-08-02 13:11:47 -0700

    create a context menu button for each row of the roster table
    closes VICE-3006
    flag=react_people_page
    
    Test Plan:
    - Tests pass
    - Turn on 'People Page Upgrade' FF
    - Visit a course people page as a teacher for the course
    - A context menu button should appear in the far right
      column of each table row (no onClick effect)
    
    Change-Id: I9ad6ebaad0b9331fb23b551310d2e338b25645c4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/297681
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Chawn Neal <chawn.neal@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/course_people/react/components/RosterTableRowMenuButton/RosterTableRowMenuButton.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {IconButton} from '@instructure/ui-buttons'
+import {IconMoreLine} from '@instructure/ui-icons'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {string} from 'prop-types'
+
+const I18n = useI18nScope('course_people')
+
+const RosterTableRowMenuButton = ({name}) => {
+  return (
+    <IconButton
+      renderIcon={<IconMoreLine />}
+      screenReaderLabel={I18n.t('Manage %{user_name}', {user_name: name})}
+      withBorder={false}
+      withBackground={false}
+    />
+  )
+}
+
+RosterTableRowMenuButton.propTypes = {
+  name: string.isRequired
+}
+
+RosterTableRowMenuButton.defaultProps = {}
+
+export default RosterTableRowMenuButton
