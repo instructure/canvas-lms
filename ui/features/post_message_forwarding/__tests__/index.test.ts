6d4bb475e6a21c9551b689c9dc262e054de14b2c
Xander Moffatt <xmoffatt@instructure.com>
2022-08-04 14:50:19 -0600

    add postMessage forwarder for LTI platform storage
    why:
    * the LTI platform storage requires for security purposes that
    get/put_data requests are sent to the OIDC auth origin
    (eg canvas.docker or canvas.instructure.com), _not_ the
    current canvas domain
    * this new route will redirect to the right OIDC auth origin and
    listen for postMessages from tools, and forward them to the
    main Canvas window for proper processing
    * as well as forward response messages back to the sender
    
    closes INTEROP-7583
    flag=none
    
    test plan:
    * have multiple domains to access canvas locally (eg
    shard2.canvas.docker etc)
    * in config/dynamic_settings.yml, set
    development.config.canvas.canvas.lti_post_message_forwarding_domain
    to "canvas.docker"
    * add this line to ui/features/post_message_forwarding/index.ts
    on line 23: `console.log({PARENT_DOMAIN})`
    * in your browser, go to your other domain, like
    "shard2.canvas.docker/post_message_forwarding"
    * that should redirect to "canvas.docker/post_message_forwarding"
    and include a token in the url
    * you should see `{PARENT_DOMAIN: "shard2.canvas.docker"}`
    in your browser console (or whatever your other domain is)
    * further testing of the forwarding functionality is in a later
    commit where this forwarder is added to canvas proper
    
    Change-Id: I3bd3572a719b618ccd4e33a57a85cea8edfce9d3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298103
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Mark Starkman <mark.starkman@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>
    Reviewed-by: Mark Starkman <mark.starkman@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>

---
+++ ui/features/post_message_forwarding/__tests__/index.test.ts
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {handler} from '../index'
+
+describe('post_message_forwarding', () => {
+  describe('handler', () => {
+    let message: string | object
+    let origin: string
+    let parentDomain: string
+    let windowReferences: object
+    let source: MessageEventSource
+    let parentWindow: Window
+
+    const subject = () =>
+      handler(
+        parentDomain,
+        windowReferences,
+        parentWindow
+      )({data: message, origin, source} as MessageEvent)
+
+    describe('when message is not JSON string or JS object', () => {
+      beforeEach(() => {
+        message = 'abcdefghi'
+      })
+
+      it('returns false', () => {
+        expect(subject()).toBe(false)
+      })
+    })
+
+    describe('incoming message', () => {
+      beforeEach(() => {
+        message = {subject: 'hello_world', key: 'value'}
+        origin = 'https://test.tool.com'
+        parentDomain = 'https://parent.domain.com'
+        windowReferences = {}
+        source = {
+          postMessage: jest.fn(),
+        } as unknown as Window
+        parentWindow = {
+          postMessage: jest.fn(),
+        } as unknown as Window
+      })
+
+      it('stores source window keyed by origin', () => {
+        subject()
+        expect(windowReferences[origin]).toBe(source)
+      })
+
+      it('posts message to top parent', () => {
+        subject()
+        expect(parentWindow.postMessage).toHaveBeenCalled()
+      })
+
+      it('attaches origin to message', () => {
+        subject()
+        expect(parentWindow.postMessage).toHaveBeenCalledWith(
+          {...(message as object), toolOrigin: origin},
+          expect.anything()
+        )
+      })
+
+      it('addresses message to parent domain', () => {
+        subject()
+        expect(parentWindow.postMessage).toHaveBeenCalledWith(expect.anything(), parentDomain)
+      })
+    })
+
+    describe('outgoing message', () => {
+      beforeEach(() => {
+        message = {subject: 'hello_world', key: 'value', toolOrigin: 'https://test.tool.com'}
+        origin = 'https://parent.domain.com'
+        parentDomain = 'https://parent.domain.com'
+        source = {
+          postMessage: jest.fn(),
+        } as unknown as Window
+        windowReferences = {
+          'https://test.tool.com': source,
+        }
+      })
+
+      describe('when message has no toolOrigin', () => {
+        beforeEach(() => {
+          message = {subject: 'hello_world', key: 'value'}
+        })
+
+        it('returns false', () => {
+          expect(subject()).toBe(false)
+        })
+      })
+
+      it('posts message to source window', () => {
+        subject()
+        expect(source.postMessage).toHaveBeenCalled()
+      })
+
+      it('addresses message to toolOrigin', () => {
+        subject()
+        expect(source.postMessage).toHaveBeenCalledWith(expect.anything(), 'https://test.tool.com')
+      })
+
+      it('removes toolOrigin from message', () => {
+        subject()
+        expect(source.postMessage).toHaveBeenCalledWith(
+          {subject: 'hello_world', key: 'value'},
+          expect.anything()
+        )
+      })
+    })
+  })
+})
---
+++ ui/features/post_message_forwarding/index.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import ready from '@instructure/ready'
+
+type Message = {
+  toolOrigin?: string
+  [key: string]: any
+}
+
+export const handler =
+  (PARENT_DOMAIN: string, windowReferences: object, parentWindow: Window | null) =>
+  (e: MessageEvent) => {
+    let message: Message
+    try {
+      message = typeof e.data === 'string' ? JSON.parse(e.data) : e.data
+    } catch (err) {
+      // unparseable message may not be meant for our handlers
+      return false
+    }
+
+    if (e.origin == PARENT_DOMAIN) {
+      const targetOrigin = message.toolOrigin
+      if (!targetOrigin) {
+        return false
+      }
+
+      const targetWindow = windowReferences[targetOrigin]
+      delete message.toolOrigin
+
+      targetWindow?.postMessage(message, targetOrigin)
+    } else {
+      windowReferences[e.origin] = e.source
+      message.toolOrigin = e.origin
+
+      parentWindow?.postMessage(message, PARENT_DOMAIN)
+    }
+  }
+
+ready(() => {
+  const {PARENT_DOMAIN} = window.ENV
+  const windowReferences = {}
+  window.addEventListener('message', handler(PARENT_DOMAIN, windowReferences, window.top))
+})
