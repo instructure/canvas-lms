80b46257766b552b1dcdf2b6fdfe0607c0fe2d85
Aaron Ogata <aogata@instructure.com>
2022-08-04 03:56:48 +0000

    Revert "wrap this.onSearch in constructor"
    This reverts commit 3ac03a4eeedb65c59ddb94aa0c647e14fffc5de4.
    
    Reason for revert: better solution discovered
    
    Change-Id: Iaf09691d9424bffab8cff66af6924ab34c8555f5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/297046
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Aaron Ogata <aogata@instructure.com>

--- ui/features/announcements/react/components/IndexHeader.js
+++ ui/features/announcements/react/components/IndexHeader.js
@@ -74,21 +74,17 @@ export default class IndexHeader extends Component {
     searchInputRef: null
   }
 
-  constructor(props) {
-    super(props)
-
-    this.onSearch = debounce(
-      () => {
-        const term = this.searchInput.value
-        this.props.searchAnnouncements({term})
-      },
-      SEARCH_TIME_DELAY,
-      {
-        leading: false,
-        trailing: true
-      }
-    )
-  }
+  onSearch = debounce(
+    () => {
+      const term = this.searchInput.value
+      this.props.searchAnnouncements({term})
+    },
+    SEARCH_TIME_DELAY,
+    {
+      leading: false,
+      trailing: true
+    }
+  )
 
   onDelete = () => {
     showConfirmDelete({
