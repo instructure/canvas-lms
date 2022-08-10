9ce88c1865743f5992e36ea1c077f1b75c943d9c
Spencer Olson <solson@instructure.com>
2022-08-10 13:42:42 -0500

    remove flag remove_gradebook_student_search_delay
    closes EVAL-2565
    flag=remove_gradebook_student_search_delay
    
    Test Plan:
    - specs pass
    
    Change-Id: Iaf901d89c5af221daa511d1780e1885d517f2636
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298291
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Spencer Olson <solson@instructure.com>
    Product-Review: Spencer Olson <solson@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -458,7 +458,6 @@ class GradebooksController < ApplicationController
 
       publish_to_sis_url: context_url(@context, :context_details_url, anchor: "tab-grade-publishing"),
       re_upload_submissions_url: named_context_url(@context, :submissions_upload_context_gradebook_url, "{{ assignment_id }}"),
-      remove_gradebook_student_search_delay_enabled: Account.site_admin.feature_enabled?(:remove_gradebook_student_search_delay),
       reorder_custom_columns_url: api_v1_custom_gradebook_columns_reorder_url(@context),
       sections: sections_json(visible_sections, @current_user, session, [], allow_sis_ids: true),
       setting_update_url: api_v1_course_settings_url(@context),
--- ui/features/gradebook/react/default_gradebook/gradebook.d.ts
+++ ui/features/gradebook/react/default_gradebook/gradebook.d.ts
@@ -83,7 +83,6 @@ export type GradebookOptions = {
   publish_to_sis_enabled: boolean
   publish_to_sis_url: string
   re_upload_submissions_url: string
-  remove_gradebook_student_search_delay_enabled: boolean
   reorder_custom_columns_url: string
   sections: Section[]
   setting_update_url: string
