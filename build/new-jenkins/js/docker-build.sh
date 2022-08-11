5fd59a290994c6a20e9f6cf7ce5e1a88f60f86a9
Aaron Ogata <aogata@instructure.com>
2022-08-11 11:11:43 -0700

    remove karma-builder image since local Chrome is no longer needed
    refs DE-1334
    
    Change-Id: I3fb5de6c396dc627813953b7cc31c7f40665b713
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298395
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Build-Review: Aaron Ogata <aogata@instructure.com>

--- build/new-jenkins/js/docker-build.sh
+++ build/new-jenkins/js/docker-build.sh
@@ -7,39 +7,15 @@ CRYSTALBALL_MAP=${CRYSTALBALL_MAP:-0}
 
 echo "CRYSTALBALL_MAP VALUE ${CRYSTALBALL_MAP}"
 
-export CACHE_VERSION="2022-03-24.1"
+export CACHE_VERSION="2022-08-15.1"
 
 source ./build/new-jenkins/docker-build-helpers.sh
-
-KARMA_BUILDER_DOCKERFILE_MD5=$(cat Dockerfile.jenkins.karma-builder | md5sum)
 
 ./build/new-jenkins/docker-with-flakey-network-protection.sh pull $WEBPACK_BUILDER_IMAGE
 
 docker tag $WEBPACK_BUILDER_IMAGE local/webpack-builder
 
 BASE_IMAGE_ID=$(docker images --filter=reference=$WEBPACK_BUILDER_IMAGE --format '{{.ID}}')
-
-KARMA_BUILDER_PARTS=(
-  $BASE_IMAGE_ID
-  $KARMA_BUILDER_DOCKERFILE_MD5
-)
-
-declare -A KARMA_BUILDER_TAGS; compute_tags "KARMA_BUILDER_TAGS" $KARMA_BUILDER_PREFIX ${KARMA_BUILDER_PARTS[@]}
-
-KARMA_BUILDER_SELECTED_TAG=""; pull_first_tag "KARMA_BUILDER_SELECTED_TAG" ${KARMA_BUILDER_TAGS[LOAD_TAG]} ${KARMA_BUILDER_TAGS[LOAD_FALLBACK_TAG]}
-
-if [ -z "${KARMA_BUILDER_SELECTED_TAG}" ]; then
-  docker build \
-    --label "WEBPACK_BUILDER_IMAGE=$WEBPACK_BUILDER_IMAGE" \
-    --tag "${KARMA_BUILDER_TAGS[SAVE_TAG]}" \
-    - < Dockerfile.jenkins.karma-builder
-
-  KARMA_BUILDER_SELECTED_TAG=${KARMA_BUILDER_TAGS[SAVE_TAG]}
-
-  add_log "built ${KARMA_BUILDER_SELECTED_TAG}"
-fi
-
-tag_many $KARMA_BUILDER_SELECTED_TAG local/karma-builder ${KARMA_BUILDER_TAGS[SAVE_TAG]}
 
 while docker exec -t general-build-container ps aww | grep graphql; do
   sleep 0.1
@@ -50,7 +26,6 @@ docker cp $(docker ps -qa -f name=general-build-container):/usr/src/app/schema.g
 docker build \
   --build-arg PATCHSET_TAG="$PATCHSET_TAG" \
   --file Dockerfile.jenkins.karma-runner \
-  --label "KARMA_BUILDER_SELECTED_TAG=$KARMA_BUILDER_SELECTED_TAG" \
   --label "PATCHSET_TAG=$PATCHSET_TAG" \
   --label "WEBPACK_BUILDER_IMAGE=$WEBPACK_BUILDER_IMAGE" \
   --tag "$1" \
