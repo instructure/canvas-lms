b865ca54c8d7c85485bbc9e5b9e00d8994040f07
Drake Harper <drake.harper@instructure.com>
2022-08-12 11:50:42 -0600

    Create conference type selector
    refs VICE-3028
    flag=none
    
    Test Plan:
     - renders in storybook
     - matches design
    
    Change-Id: Ia51800f5fca7a14bafc8cc87e3a64b08bb5f7759
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298489
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/conferences/react/components/VideoConferenceTypeSelect/VideoConferenceTypeSelect.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useScope as useI18nScope} from '@canvas/i18n'
+import React, {useState} from 'react'
+import PropTypes, {arrayOf} from 'prop-types'
+
+import {Select} from '@instructure/ui-select'
+import {View} from '@instructure/ui-view'
+
+const I18n = useI18nScope('video_conference')
+
+const VideoConferenceTypeSelect = ({conferenceTypes, onSetConferenceType}) => {
+  const [isShowingOptions, setIsShowingOptions] = useState(false)
+  const [selectedOptionId, setSelectedOptionId] = useState(conferenceTypes[0].type)
+  const [inputValue, setInputValue] = useState(conferenceTypes[0].name)
+  const [highlightedOptionId, setHighlightedOptionId] = useState(null)
+
+  const getOptionById = queryId => {
+    return conferenceTypes.find(({type}) => type === queryId)
+  }
+
+  const handleShowOptions = () => {
+    setIsShowingOptions(true)
+  }
+
+  const handleHideOptions = () => {
+    const option = getOptionById(selectedOptionId)
+    setIsShowingOptions(false)
+    setHighlightedOptionId(null)
+    setInputValue(selectedOptionId ? option?.name : '')
+  }
+
+  const handleBlur = () => {
+    setHighlightedOptionId(null)
+  }
+
+  const handleHighlightOption = (event, {id}) => {
+    event.persist()
+    const option = getOptionById(id)
+    setHighlightedOptionId(id)
+    setInputValue(event.type === 'keydown' ? option?.name : inputValue)
+  }
+
+  const handleSelectOption = (event, {id}) => {
+    const option = getOptionById(id)
+    setSelectedOptionId(id)
+    setInputValue(option?.name || '')
+    setIsShowingOptions(false)
+    onSetConferenceType(option.type)
+  }
+
+  return (
+    <View as="div" margin="medium" data-testid="conference-type-select">
+      <Select
+        renderLabel={I18n.t('Conference Type')}
+        assistiveText={I18n.t('Use arrow keys to navigate options.')}
+        inputValue={inputValue}
+        isShowingOptions={isShowingOptions}
+        onBlur={handleBlur}
+        onRequestShowOptions={handleShowOptions}
+        onRequestHideOptions={handleHideOptions}
+        onRequestHighlightOption={handleHighlightOption}
+        onRequestSelectOption={handleSelectOption}
+      >
+        {conferenceTypes.map(option => {
+          return (
+            <Select.Option
+              id={option.type}
+              key={option.type}
+              isHighlighted={option.type === highlightedOptionId}
+              isSelected={option.type === selectedOptionId}
+            >
+              {option?.name}
+            </Select.Option>
+          )
+        })}
+      </Select>
+    </View>
+  )
+}
+
+VideoConferenceTypeSelect.prototype = {
+  conferenceTypes: arrayOf(PropTypes.object),
+  onSetConferenceType: PropTypes.func
+}
+
+export default VideoConferenceTypeSelect
