750e08804eb8340ca56647ae7148164fd7c66359
Drake Harper <drake.harper@instructure.com>
2022-08-12 11:50:42 -0600

    Create conference type selector
    refs VICE-3028
    flag=none
    
    Test Plan:
     - renders in storybook
     - matches design
    
    Change-Id: Ia51800f5fca7a14bafc8cc87e3a64b08bb5f7759
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298489
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/conferences/react/components/VideoConferenceTypeSelect/VideoConferenceTypeSelect.stories.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+
+import VideoConferenceTypeSelect from './VideoConferenceTypeSelect'
+
+export default {
+  title: 'Examples/Conferences/VideoConferenceTypeSelect',
+  component: VideoConferenceTypeSelect,
+  argTypes: {}
+}
+
+const Template = args => (
+  <VideoConferenceTypeSelect
+    conferenceTypes={args.conferenceTypes}
+    onSetConferenceType={args.onSetConferenceType}
+  />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  conferenceTypes: [
+    {
+      name: 'Adobe Connect',
+      type: 'AdobeConnect',
+      settings: [],
+      free_trial: false,
+      lti_settings: null,
+      contexts: null
+    },
+    {
+      name: 'BigBlueButton',
+      type: 'BigBlueButton',
+      settings: [],
+      free_trial: false,
+      lti_settings: null,
+      contexts: null
+    }
+  ],
+  onSetConferenceType: () => {}
+}
