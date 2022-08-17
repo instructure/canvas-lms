84c60ef94ad1fd15c761867081f83e0d8716d78b
Tucker McKnight <tmcknight@instructure.com>
2022-08-16 18:32:57 -0600

    spec: Remove colon in front of hash keys
    This list was generated from calling YAML.dump,
    which puts a colon in front of the keys.
    
    Removing the colon makes the keys become strings instead
    of symbols. Instead of calling .map to get just the
    keys we want, calling .pluck appears to be faster.
    
    Change-Id: I7e938af1d425365e436ea12e45b2c5d445a4b543
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/298836
    Reviewed-by: Alexandre Trindade <alexandre.trindade@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/models/developer_key_spec.rb
+++ spec/models/developer_key_spec.rb
@@ -530,9 +530,7 @@ describe DeveloperKey do
 
     it "rejects changes to routes.rb if it would break an existing scope" do
       stub_const("CanvasRails::Application", TokenScopesHelper::SpecHelper::MockCanvasRails::Application)
-      all_routes = Set.new(TokenScopes.api_routes.map do |route|
-        { verb: route[:verb], path: route[:path] }
-      end)
+      all_routes = Set.new(TokenScopes.api_routes.pluck(:verb, :path))
 
       modified_scopes = TokenScopesHelper::SpecHelper.last_known_accepted_scopes.reject do |scope|
         all_routes.include? scope
