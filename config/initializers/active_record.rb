9fa3e127ab4fb289edb794a0f2ebd08b8f5c490b
Alejandra Torres <agtorre3@yahoo.com>
2022-08-18 16:09:04 -0700

    QTY-347 fix for stream item retries
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -581,12 +581,11 @@ class ActiveRecord::Base
     # transaction. useful for possible race conditions where we don't want to
     # take a lock (e.g. when we create a submission).
     retries.times do |retry_count|
-      begin
-        result = transaction(:requires_new => true) { uncached { yield(retry_count) } }
-        connection.clear_query_cache
-        return result
-      rescue ActiveRecord::RecordNotUnique
-      end
+      result = transaction(:requires_new => true) { uncached { yield(retry_count) } }
+      connection.clear_query_cache
+      return result
+    rescue ActiveRecord::RecordNotUnique
+      next
     end
     result = transaction(:requires_new => true) { uncached { yield(retries) } }
     connection.clear_query_cache
