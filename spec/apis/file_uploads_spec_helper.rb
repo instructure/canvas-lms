5232d4f248adcada9bbedc27ae4108a38f00bf21
Mysti Lilla <mysti@instructure.com>
2022-09-02 19:37:47 -0600

    Send category in API file json
    fixes MAT-761
    flag=none
    
    [fsc-max-nodes=20]
    [fsc-timeout=30]
    
    Test plan
    - The API for files should include
      the category (any files API)
    
    Change-Id: Icaca84db5ac1ae1e62f66da178ec5e9002c42f0d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/300184
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Mysti Lilla <mysti@instructure.com>

--- spec/apis/file_uploads_spec_helper.rb
+++ spec/apis/file_uploads_spec_helper.rb
@@ -63,7 +63,8 @@ shared_examples_for "file uploads api" do
       "modified_at" => attachment.modified_at.as_json,
       "thumbnail_url" => attachment.has_thumbnail? ? thumbnail_image_url(attachment, attachment.uuid, host: "www.example.com") : nil,
       "mime_class" => attachment.mime_class,
-      "media_entry_id" => attachment.media_entry_id
+      "media_entry_id" => attachment.media_entry_id,
+      "category" => "uncategorized"
     }
 
     if options[:include]&.include?("enhanced_preview_url") && (attachment.context.is_a?(Course) || attachment.context.is_a?(User) || attachment.context.is_a?(Group))
@@ -129,7 +130,8 @@ shared_examples_for "file uploads api" do
       "mime_class" => attachment.mime_class,
       "media_entry_id" => attachment.media_entry_id,
       "canvadoc_session_url" => nil,
-      "crocodoc_session_url" => nil
+      "crocodoc_session_url" => nil,
+      "category" => "uncategorized"
     }
 
     if attachment.context.is_a?(User) || attachment.context.is_a?(Course) || attachment.context.is_a?(Group)
