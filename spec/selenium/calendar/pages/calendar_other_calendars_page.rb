1c3cd7628c4d7702f2e0bf0a2b1220b3cd27cdc1
Jonathan Guardado <jonathan.guardado@instructure.com>
2022-09-05 16:00:30 -0600

    spec: Selenium specs for other calendars section
    Changes added: A new file for the other calendars section specs, new
    specs for the NEW pill, and the functionality of the "Add other
    Calendars" modal.
    
    Closes LS-3384, LS-3385
    flag = account_calendar_events
    
    Test plan:
    - passes Jenkins
    
    Change-Id: I52c79dd5c0fca65f14b2444b6af2ca01c5689ca1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/300250
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jonathan Guardado <jonathan.guardado@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>

---
+++ spec/selenium/calendar/pages/calendar_other_calendars_page.rb
@@ -0,0 +1,153 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2022 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require_relative "../../common"
+
+module CalendarOtherCalendarsPage
+  #------------------------- Selectors --------------------------
+  def open_other_calendars_modal_btn_selector
+    "button[data-testid='add-other-calendars-button']"
+  end
+
+  def other_calendars_container_selector
+    "#other-calendars-list-holder"
+  end
+
+  def other_calendars_context_selector
+    "#other-calendars-context-list > .context_list_context"
+  end
+
+  def sidebar_selector
+    "#right-side-wrapper"
+  end
+
+  def modal_cancel_btn_selector
+    "button[data-testid='footer-close-button']"
+  end
+
+  def modal_save_btn_selector
+    "button[data-testid='save-calendars-button']"
+  end
+
+  def delete_calendar_btn_selector
+    "#other-calendars-context-list > .context_list_context > .buttons-wrapper > .ContextList__DeleteBtn"
+  end
+
+  def modal_empty_state_selector
+    ".accounts-empty-state"
+  end
+
+  def context_list_item_selector(context_id)
+    "#other-calendars-context-list > li[data-context=account_#{context_id}]"
+  end
+
+  def create_new_event_link_selector
+    "#create_new_event_link"
+  end
+
+  def flash_alert_selector
+    ".flashalert-message"
+  end
+
+  def calendar_event_selector
+    ".fc-event"
+  end
+
+  def calendar_body_selector
+    ".fc-body"
+  end
+
+  #------------------------- Elements ---------------------------
+  def open_other_calendars_modal_btn
+    f(open_other_calendars_modal_btn_selector)
+  end
+
+  def other_calendars_container
+    f(other_calendars_container_selector)
+  end
+
+  def other_calendars_context
+    ff(other_calendars_context_selector)
+  end
+
+  def other_calendars_context_labels
+    ff("#{other_calendars_context_selector} > label")
+  end
+
+  def sidebar
+    f(sidebar_selector)
+  end
+
+  def modal_cancel_btn
+    f(modal_cancel_btn_selector)
+  end
+
+  def modal_save_btn
+    f(modal_save_btn_selector)
+  end
+
+  def delete_calendar_btn
+    ff(delete_calendar_btn_selector)
+  end
+
+  def modal_empty_state
+    f(modal_empty_state_selector)
+  end
+
+  def create_new_event_link
+    f(create_new_event_link_selector)
+  end
+
+  def flash_alert
+    f(flash_alert_selector)
+  end
+
+  def calendar_body
+    f(calendar_body_selector)
+  end
+
+  #----------------------- Actions/Methods ----------------------
+  def open_other_calendars_modal
+    open_other_calendars_modal_btn.click
+    wait_for_ajaximations
+  end
+
+  def click_modal_cancel_btn
+    modal_cancel_btn.click
+    wait_for_ajaximations
+  end
+
+  def click_modal_save_btn
+    modal_save_btn.click
+    wait_for_ajaximations
+  end
+
+  def select_other_calendar(context_id)
+    # because clicking the checkbox clicks on a sibling span
+    driver.execute_script("$('input[data-testid=account-#{context_id}-checkbox]').click()")
+  end
+
+  def open_create_new_event_modal
+    create_new_event_link.click
+  end
+
+  def close_flash_alert
+    f("#{flash_alert_selector} button").click
+  end
+end
