73417f49f8b036481f8c31e3504778e98898b241
Aaron Ogata <aogata@instructure.com>
2022-09-21 07:20:27 -0700

    make jenkins-cache file more readable
    refs DE-1379
    
    Change-Id: I3c89afdedb0d65c69166681bf2f04b7f98bbf07b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/301517
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Build-Review: Aaron Ogata <aogata@instructure.com>

--- build/new-jenkins/docker-build.sh
+++ build/new-jenkins/docker-build.sh
@@ -41,7 +41,7 @@ export CACHE_VERSION="2022-09-20.1"
 source ./build/new-jenkins/docker-build-helpers.sh
 
 ./build/new-jenkins/docker-with-flakey-network-protection.sh pull starlord.inscloudgate.net/jenkins/dockerfile:1.0-experimental
-./build/new-jenkins/docker-with-flakey-network-protection.sh pull starlord.inscloudgate.net/jenkins/busybox
+./build/new-jenkins/docker-with-flakey-network-protection.sh pull starlord.inscloudgate.net/jenkins/bash
 
 DOCKER_BUILDKIT=1 docker build --file Dockerfile.jenkins-cache --tag "local/cache-helper-collect-gems" --target cache-helper-collect-gems "$WORKSPACE"
 DOCKER_BUILDKIT=1 docker build --file Dockerfile.jenkins-cache --tag "local/cache-helper-collect-yarn" --target cache-helper-collect-yarn "$WORKSPACE"
