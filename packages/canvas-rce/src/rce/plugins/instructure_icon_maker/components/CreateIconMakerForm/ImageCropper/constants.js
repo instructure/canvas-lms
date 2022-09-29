0eebe745b77ff26ddf51c22075568262ee2c5ca7
Juan Chavez <juan.chavez@instructure.com>
2022-09-29 19:51:55 -0400

    IM: Fix intermittent acceleration in cropper (KB)
    fixes MAT-982
    
    flag=buttons_and_icons_root_account
    flag=buttons_and_icons_cropper
    
    test plan:
    - Navigate to a RCE instance.
    - Open Icon Maker tray (create or edit).
    - Open cropper with an image.
    - Focus the icon preview.
    - Move the image using just the arrow keys.
    > Verify that the acceleration is still working.
    > Verify that when changing the directions
    the acceleration returns to zero.
    > Verify that when holding an arrow key
    makes the velocity increase as expected.
    > Verify this with different browsers,
    especifically Safari (where this was initially
    failing).
    
    Change-Id: Ic0fbe70726a3765d2da2a2ab1ca7fb27b5c33e6b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/302266
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Juan Chavez <juan.chavez@instructure.com>

--- packages/canvas-rce/src/rce/plugins/instructure_icon_maker/components/CreateIconMakerForm/ImageCropper/constants.js
+++ packages/canvas-rce/src/rce/plugins/instructure_icon_maker/components/CreateIconMakerForm/ImageCropper/constants.js
@@ -26,8 +26,8 @@ export const MAX_SCALE_RATIO = 2.0
 export const BUTTON_SCALE_STEP = 0.1
 export const WHEEL_SCALE_STEP = 0.005
 export const WHEEL_EVENT_DELAY = 100
-export const KEY_EVENT_DELAY = 100
-export const KEY_EVENT_STEP = 3
+export const KEY_EVENT_DELAY = 200
+export const KEY_EVENT_ACCELERATION = 3
 export const BUTTON_ROTATION_DEGREES = 90
 export const BACKGROUND_SQUARE_SIZE = 16
 
