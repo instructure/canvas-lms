57af81c058ba8ff53219bef11a42ab7e4a5ea0c4
Sean Scally <sean.scally@instructure.com>
2022-09-30 10:21:23 -0700

    Fix a11y issue in Internal Settings
    We are replacing CondensedButton with Link/Text components to fix a
    potential a11y issue. See g/298407 for a better writeup with more
    context.
    
    refs FOO-3129
    flag=none
    
    test plan:
    * check that the button "Download as" still is visible and works here:
      http://canvas.docker/accounts/site_admin/settings#tab-internal-settings
    
    Change-Id: Ie73fc438ee14eed47acf08954306273e5a6b3010
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/302336
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Sean Scally <sean.scally@instructure.com>
    Product-Review: Sean Scally <sean.scally@instructure.com>

--- ui/features/account_settings/react/internal_settings/table/InternalSettingsTable.tsx
+++ ui/features/account_settings/react/internal_settings/table/InternalSettingsTable.tsx
@@ -23,9 +23,9 @@ import {useScope as useI18nScope} from '@canvas/i18n'
 import {Text} from '@instructure/ui-text'
 import {EditableCodeValue} from './EditableCodeValue'
 import {InternalSettingActionButtons} from './InternalSettingActionButtons'
-import {CondensedButton} from '@instructure/ui-buttons'
 import {Menu} from '@instructure/ui-menu'
 import exportFromJSON from 'export-from-json'
+import {Link} from '@instructure/ui-link'
 
 const I18n = useI18nScope('internal-settings')
 
@@ -107,7 +107,13 @@ export const InternalSettingsTable = (props: InternalSettingsTableProps) => {
   return (
     <div>
       <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <Menu trigger={<CondensedButton>{I18n.t('Download as...')}</CondensedButton>}>
+        <Menu
+          trigger={
+            <Link isWithinText={false} as="button">
+              <Text>{I18n.t('Download as...')}</Text>
+            </Link>
+          }
+        >
           <Item onClick={() => exportFromJSON({...exportParams, exportType: 'json'})}>JSON</Item>
           <Item onClick={() => exportFromJSON({...exportParams, exportType: 'csv'})}>CSV</Item>
         </Menu>
