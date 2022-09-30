b6938ba3a364d6bebc07b315d2fe68305cb3ebc3
Robin Kuss <rkuss@instructure.com>
2022-09-30 10:28:47 -0500

    fix the student enrollment count statsd
    Closes LS-3502
    
    Test Plan: passes jenkins
    
    Change-Id: I992d3529d649186e30a65a3ed724c0ac39ee7143
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/302328
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Luis Oliveira <luis.oliveira@instructure.com>
    QA-Review: Luis Oliveira <luis.oliveira@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2182,7 +2182,7 @@ describe CoursesController do
       it "tracks enrollments for unpaced courses" do
         allow(InstStatsd::Statsd).to receive(:count)
         post "enroll_users", params: { course_id: @course.id, user_list: "\"Sam\" <sam@yahoo.com>, \"Fred\" <fred@yahoo.com>" }
-        expect(InstStatsd::Statsd).to have_received(:count).with("course.unpaced.student_enrollment_count", ActiveRecord::Associations::CollectionProxy).once
+        expect(InstStatsd::Statsd).to have_received(:count).with("course.unpaced.student_enrollment_count", 3).once
       end
 
       it "tracks enrollments for paced courses" do
@@ -2190,7 +2190,7 @@ describe CoursesController do
         @course.enable_course_paces = true
         @course.save!
         post "enroll_users", params: { course_id: @course.id, user_list: "\"Sam\" <sam@yahoo.com>, \"Fred\" <fred@yahoo.com>" }
-        expect(InstStatsd::Statsd).to have_received(:count).with("course.paced.student_enrollment_count", ActiveRecord::Associations::CollectionProxy).once
+        expect(InstStatsd::Statsd).to have_received(:count).with("course.paced.student_enrollment_count", 3).once
       end
     end
   end
