4ce054073e93c365b16e0f14e0e28e0de4a1ec65
Jason Gillett <jason.gillett@instructure.com>
2022-10-11 15:47:53 -0600

    Create Split Screen view FF
    closes VICE-3106
    flag= split_screen_view
    
    Test Plan
    1. Navigate to Site Admin -> settings -> feature options
    2. Verify that split screen view FF is present
    
    Change-Id: Id5a61d7681719fe9866babba8300b973c08b8cb5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/303061
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>

--- app/controllers/discussion_topics_controller.rb
+++ app/controllers/discussion_topics_controller.rb
@@ -787,6 +787,7 @@ class DiscussionTopicsController < ApplicationController
                discussion_topic_menu_tools: external_tools_display_hashes(:discussion_topic_menu),
                rce_mentions_in_discussions: @context.feature_enabled?(:react_discussions_post) && !@topic.anonymous?,
                isolated_view: Account.site_admin.feature_enabled?(:isolated_view),
+               split_screen_view: Account.site_admin.feature_enabled?(:split_screen_view),
                discussion_grading_view: Account.site_admin.feature_enabled?(:discussion_grading_view),
                draft_discussions: Account.site_admin.feature_enabled?(:draft_discussions),
                discussion_anonymity_enabled: @context.feature_enabled?(:react_discussions_post),
