32555e75dd33989aa4ecc4752731fb784c8f9a2d
Jackson Howe <jackson.howe@instructure.com>
2022-10-13 12:23:56 -0600

    Place reset pace button in the changes tray
    The cancel button previously reset unsaved pace changes. In the
    redesign, it closes the modal, so place the reset button in the
    unsaved changes tray. Clicking the button prompts a modal to confirm
    the reset.
    
    closes LS-3524
    flag = course_paces_redesign
    
    Test plan:
     - With the redesign flag enabled, visit a course pace and make a
       few changes; don't publish
     - Open the changes tray
     - Click the 'Reset all' button
     - Click 'Reset'
     - Expect the modal & tray to close and for the changes to be reset
     - Disable the flag and expect the button to not appear
    
    Change-Id: I3510aaeac0e4a40f379104c42d5269f0c99e921a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/303243
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>

---
+++ ui/features/course_paces/react/components/reset_pace_warning_modal.tsx
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+
+import {Button} from '@instructure/ui-buttons'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+
+import Modal from '@canvas/instui-bindings/react/InstuiModal'
+import {useScope as useI18nScope} from '@canvas/i18n'
+
+const I18n = useI18nScope('reset_pace_warning_modal')
+
+const {Body: ModalBody, Footer: ModalFooter} = Modal as any
+
+export type ComponentProps = {
+  open: boolean
+  onCancel: () => void
+  onConfirm: () => void
+}
+
+export const ResetPaceWarningModal: React.FC<ComponentProps> = ({open, onCancel, onConfirm}) => (
+  <Modal
+    size="small"
+    open={open}
+    onDismiss={onCancel}
+    label={I18n.t('Reset all unpublished changes?')}
+  >
+    <ModalBody>
+      <View>
+        <Text>
+          {I18n.t('Your unpublished changes will be reverted to their previously saved state.')}
+        </Text>
+      </View>
+    </ModalBody>
+    <ModalFooter>
+      <View>
+        <Button onClick={onCancel}>{I18n.t('Cancel')}</Button>
+        <Button margin="0 x-small" onClick={onConfirm} color="danger">
+          {I18n.t('Reset')}
+        </Button>
+      </View>
+    </ModalFooter>
+  </Modal>
+)
