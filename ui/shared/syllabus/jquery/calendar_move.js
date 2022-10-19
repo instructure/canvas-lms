b240895ee05d204fb2b695a21899c00a24140ff9
Aaron Shafovaloff <ashafovaloff@instructure.com>
2022-10-19 07:48:00 -0600

    Resolve eslint errors in calendar_move.js
    Test plan:
        - All existing tests pass
    
    flag=none
    
    Refs DE-1426
    
    Change-Id: Id66569b76067d56ef787118fee11dbdf7a0996a0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/303587
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

--- ui/shared/syllabus/jquery/calendar_move.js
+++ ui/shared/syllabus/jquery/calendar_move.js
@@ -32,20 +32,22 @@ function makeDate(date) {
 
 export function changeMonth($month, change) {
   const monthNames = I18n.lookup('date.month_names')
-  var data = {}
-  var current = null
+  let data = {}
+  let month
+  let year
+  let current = null
   if (typeof change === 'string') {
-    var current = $.datepicker.oldParseDate('mm/dd/yy', change)
+    current = $.datepicker.oldParseDate('mm/dd/yy', change)
     if (current) {
       current.setDate(1)
     }
   }
   if (!current) {
-    var month = parseInt($month.find('.month_number').text(), 10)
-    var year = parseInt($month.find('.year_number').text(), 10)
-    var current = new Date(year, month + change - 1, 1)
+    month = parseInt($month.find('.month_number').text(), 10)
+    year = parseInt($month.find('.year_number').text(), 10)
+    current = new Date(year, month + change - 1, 1)
   }
-  var data = {
+  data = {
     month_name: monthNames[current.getMonth() + 1],
     month_number: current.getMonth() + 1,
     year_number: current.getFullYear(),
@@ -60,7 +62,7 @@ export function changeMonth($month, change) {
   date.setDate(date.getDate() - date.getDay() + firstDayOfWeek)
   const firstDayOfSquare = makeDate(date)
   let lastDayOfPreviousMonth = null
-  if (firstDayOfMonth.day != firstDayOfSquare.day) {
+  if (firstDayOfMonth.day !== firstDayOfSquare.day) {
     date.setDate(1)
     date.setMonth(date.getMonth() + 1)
     date.setDate(0)
@@ -94,15 +96,15 @@ export function changeMonth($month, change) {
   $month.find('.calendar_event').remove()
   let idx = 0
   let day = firstDayOfSquare.day
-  var month = firstDayOfSquare.month
-  var year = firstDayOfSquare.year
-  while (day <= lastDayOfSquare.day || month != lastDayOfSquare.month) {
-    var $day = $days.eq(idx)
+  month = firstDayOfSquare.month
+  year = firstDayOfSquare.year
+  while (day <= lastDayOfSquare.day || month !== lastDayOfSquare.month) {
+    const $day = $days.eq(idx)
     if ($day.length > 0) {
       const classes = $day.attr('class').split(' ')
       const class_names = []
       for (let i = 0; i < classes.length; i++) {
-        if (classes[i].indexOf('date_') == 0) {
+        if (classes[i].indexOf('date_') === 0) {
           // no-op
         } else {
           class_names.push(classes[i])
@@ -111,7 +113,7 @@ export function changeMonth($month, change) {
       $day.attr('class', class_names.join(' '))
     }
     $day.show().addClass('visible').parents('tr').show().addClass('visible')
-    var data = {
+    data = {
       day_number: day,
     }
     const month_number = month < 9 ? `0${month + 1}` : month + 1
@@ -137,17 +139,17 @@ export function changeMonth($month, change) {
       $div = $day
     }
     $div.removeClass('current_month other_month next_month previous_month today')
-    if (month == firstDayOfMonth.month) {
+    if (month === firstDayOfMonth.month) {
       $div.addClass('current_month')
     } else {
       $div.addClass('other_month')
-      if (firstDayOfMonth.month == (month + 1) % 12) {
+      if (firstDayOfMonth.month === (month + 1) % 12) {
         $div.addClass('previous_month')
       } else {
         $div.addClass('next_month')
       }
     }
-    if (month == today.month && day == today.day && year == today.year) {
+    if (month === today.month && day === today.day && year === today.year) {
       $div.addClass('today')
     }
     day++
@@ -155,8 +157,8 @@ export function changeMonth($month, change) {
     if (
       (lastDayOfPreviousMonth &&
         day > lastDayOfPreviousMonth.day &&
-        month == lastDayOfPreviousMonth.month) ||
-      (day > lastDayOfMonth.day && month == lastDayOfMonth.month)
+        month === lastDayOfPreviousMonth.month) ||
+      (day > lastDayOfMonth.day && month === lastDayOfMonth.month)
     ) {
       month += 1
       if (month >= 12) {
@@ -167,9 +169,9 @@ export function changeMonth($month, change) {
     }
   }
   while (idx < $days.length) {
-    var $day = $days.eq(idx)
-    $day.parents('tr').hide().removeClass('visible')
-    $day.hide().removeClass('visible')
+    const $day_ = $days.eq(idx)
+    $day_.parents('tr').hide().removeClass('visible')
+    $day_.hide().removeClass('visible')
     idx++
   }
   if (!$month.hasClass('mini_month')) {
