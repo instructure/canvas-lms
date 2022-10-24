9bd9a90008ea5206f59ebc4739e83be7f998d441
Chris Soto <christopher.soto@instructure.com>
2022-10-24 09:29:58 -0600

    add media attachment preview to inbox message
    add shared media attachment react component to display
    media preview when reading inbox messages
    also fixed renamed file not showing in compose modal
    
    fixes EVAL-2491
    flag=react_inbox
    
    test plan:
    - with react_inbox flag navigate to inbox
    - compose new message
    - add a media attachment (kaltura on)
    - send message
    - log in as user you sent message to
    - verify that you can see the media preview for the
      attached media
    - verify that the media preview does not have the
      removable button
    
    Change-Id: I016d68e48d6484087aed6afdb9a4f19c7684dbb9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/303954
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Paulo Chaves <paulo.chaves@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

---
+++ ui/shared/message-attachments/react/components/MediaAttachment/MediaAttachment.tsx
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2022 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react'
+import CanvasMediaPlayer from '@canvas/canvas-media-player'
+import {RemovableItem} from '../RemovableItem/RemovableItem'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {View} from '@instructure/ui-view'
+import {colors} from '@instructure/canvas-theme'
+
+const I18n = useI18nScope('conversations_2')
+
+type MediaAttachmentProps = {
+  onRemoveMediaComment: () => void
+  file: {
+    mediaID: string
+    mediaTracks?: {
+      id: string
+      src: string
+      label: string
+      type: string
+      language: string
+    }[]
+    title: string
+    src?: string
+    mediaSources?: {
+      type: string
+      src: string
+      height: string
+      width: string
+    }[]
+    type: string
+  }
+}
+
+export function MediaAttachment(props: MediaAttachmentProps) {
+  return (
+    <>
+      {props.onRemoveMediaComment ? (
+        <RemovableItem
+          onRemove={props.onRemoveMediaComment}
+          screenReaderLabel={I18n.t('Remove media comment')}
+          childrenAriaLabel={I18n.t('Media comment content')}
+        >
+          <MediaAttachmentPlayer {...props} />
+        </RemovableItem>
+      ) : (
+        <MediaAttachmentPlayer {...props} />
+      )}
+
+      <div
+        style={{
+          width: '20rem',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          color: colors.ash,
+        }}
+      >
+        {props.file.title}
+      </div>
+    </>
+  )
+}
+
+type MediaAttachmentPlayerProps = Omit<MediaAttachmentProps, 'onRemoveMediaComment'>
+
+const MediaAttachmentPlayer = (props: MediaAttachmentPlayerProps) => {
+  const mediaSources = (): string[] => {
+    if (props.file.src) {
+      return [props.file.src]
+    }
+
+    return props.file.mediaSources?.map(media => media.src) ?? []
+  }
+
+  return (
+    <View
+      as="div"
+      borderRadius="large"
+      overflowX="hidden"
+      overflowY="hidden"
+      height="11.25rem"
+      width="20rem"
+      margin="small small small none"
+      position="relative"
+      shadow="above"
+    >
+      <CanvasMediaPlayer
+        fluidHeight={true}
+        resizeContainer={false}
+        media_id={props.file.mediaID}
+        media_sources={mediaSources()}
+        media_tracks={props.file.mediaTracks}
+        type={props.file.type}
+        aria_label={props.file.title}
+      />
+    </View>
+  )
+}
+
+export default MediaAttachment
