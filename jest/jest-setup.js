ed23c178e5254b6ace1ef25ba371802fe80490b2
Davis Hyer <dhyer@instructure.com>
2022-11-01 12:47:52 -0600

    remove unnecessary canUploadFiles warning supression
    refs LS-3571
    flag=none
    
    The test(s) previously causing this error have either been fixed or
    removed. This supression is no longer necessary.
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: I45e08aec1a07873a499209dee79acdd8db907edd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/304494
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -87,7 +87,6 @@ const ignoredErrors = [
   /The prop `groupTitle` is marked as required in `SearchBreadcrumb`/,
   /The prop `id` is marked as required in `(ColHeader|DashboardCard|FormField|Option)`/,
   /The prop `label` is marked as required in `(FormField|FormFieldLayout|Modal)`/,
-  /The prop `rcsProps.canUploadFiles` is marked as required in `ForwardRef`/,
   /The prop `renderLabel` is marked as required in `(NumberInput|Select)`/,
   /Unexpected keys "searchPermissions", "filterRoles", "tabChanged", "setAndOpenAddTray" found in preloadedState argument passed to createStore/,
   /validateDOMNesting\(...\): %s cannot appear as a child of <%s>/,
