6452937ba05ad98745699ac8eb3a4335c8109f04
Sean Scally <sean.scally@instructure.com>
2022-11-02 13:30:23 -0700

    Convert WikiPage from CoffeeScript->JS
    refs FOO-3200
    
    Change-Id: Id0dd3c6c9e9d8228f43e0a02fd3f33ba750b348d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/304611
    Reviewed-by: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>
    QA-Review: Sean Scally <sean.scally@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/coffeescripts/collections/WikiPageRevisionsCollectionSpec.js
+++ spec/coffeescripts/collections/WikiPageRevisionsCollectionSpec.js
@@ -16,7 +16,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import WikiPage from '@canvas/wiki/backbone/models/WikiPage.coffee'
+import WikiPage from '@canvas/wiki/backbone/models/WikiPage'
 import WikiPageRevisionsCollection from 'ui/features/wiki_page_revisions/backbone/collections/WikiPageRevisionsCollection'
 
 QUnit.module('WikiPageRevisionsCollection')
--- spec/coffeescripts/views/wiki/WikiPageDeleteDialogSpec.js
+++ spec/coffeescripts/views/wiki/WikiPageDeleteDialogSpec.js
@@ -16,7 +16,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import WikiPage from '@canvas/wiki/backbone/models/WikiPage.coffee'
+import WikiPage from '@canvas/wiki/backbone/models/WikiPage'
 import WikiPageDeleteDialog from '@canvas/wiki/backbone/views/WikiPageDeleteDialog'
 
 QUnit.module('WikiPageDeleteDialog')
