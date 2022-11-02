cf52863c09f31556c354f3f3e002caa0a189dc8a
Davis Hyer <dhyer@instructure.com>
2022-11-02 11:10:29 -0600

    remove unnecessary NumberInput warning supression
    refs LS-3571
    flag=none
    
    The test(s) previously causing this error have either been fixed or
    removed. This supression is no longer necessary.
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: I7d41a4c0647c1f83cf8674b0a9afce3e212ffd27
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/304577
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -84,7 +84,7 @@ const ignoredErrors = [
   /The prop `groupTitle` is marked as required in `SearchBreadcrumb`/,
   /The prop `id` is marked as required in `(ColHeader|DashboardCard|FormField|Option)`/,
   /The prop `label` is marked as required in `(FormField|FormFieldLayout|Modal)`/,
-  /The prop `renderLabel` is marked as required in `(NumberInput|Select)`/,
+  /The prop `renderLabel` is marked as required in `Select`/,
   /Unexpected keys "searchPermissions", "filterRoles", "tabChanged", "setAndOpenAddTray" found in preloadedState argument passed to createStore/,
   /validateDOMNesting\(...\): %s cannot appear as a child of <%s>/,
   /WARNING: heuristic fragment matching going on!/,
