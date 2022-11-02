bab7757a4a3fe5112e7f279a5860b3c94f06855c
Sonny Huynh <sonny.huynh@strongmind.com>
2022-11-02 15:12:31 -0700

    QTY-1300 Update canvas-lms to work without canvas_shim patch
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>
    Co-authored-by: Christopher Adeszko <christopher.adeszko@strongmind.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application.js.erb, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
