26220f6aae035bc2357cfd5ec2e67a6aef246188
Bobby Buten <bobby.buten@instructure.com>
2022-11-02 14:03:18 -0400

    clean up bundle size reporting
    [build-registry-path=jenkins/canvas-lms/de-1217]
    [change-merged]
    
    refs DE-1217
    flag=none
    
    TEST PLAN:
      - run post-merge build test
        - confirm webpack size is properly run and recorded
    
    Change-Id: I2e84dbd5509276a3ec817d916c56c15e6fb9ef50
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/304585
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Build-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    QA-Review: Bobby Buten <bobby.buten@instructure.com>
    Product-Review: Bobby Buten <bobby.buten@instructure.com>

--- build/new-jenkins/library/vars/webpackStage.groovy
+++ build/new-jenkins/library/vars/webpackStage.groovy
@@ -20,56 +20,22 @@ def calcBundleSizes() {
   sh '''
     docker run --name webpack-linters \
     $PATCHSET_TAG bash -c "./build/new-jenkins/record-webpack-sizes.sh"
-   '''
+  '''
 
-   sh 'docker cp webpack-linters:/tmp/big_bundles.csv tmp/big_bundles.csv'
+  sh 'docker cp webpack-linters:/tmp/big_bundles.csv tmp/big_bundles.csv'
 
-   def bigFileList = []
-   def thingsWeKnowAreWayTooBig = [
-     'account_settings',
-     'assignment_edit',
-     'assignment_index',
-     'calendar',
-     'discussion_topic_edit',
-     'discussion_topics_post',
-     'edit_rubric',
-     'manage_groups',
-     'wiki_page_show'
-   ]
+  def csv = readCSV file: 'tmp/big_bundles.csv'
+  for (def records : csv) {
 
-   def csv = readCSV file: 'tmp/big_bundles.csv'
-   for (def records : csv) {
-
-     // skip auto-generated bundles
-     if (records[0] =~ /^\d/) {
+    // skip auto-generated bundles
+    if (records[0] =~ /^\d/) {
       continue
-     }
+    }
 
-     reportToSplunk('webpack_bundle_size', [
-       'fileName': records[0],
-       'fileSize': records[1],
-       'version': '3a',
-     ])
-
-     int fileSize = records[1].toInteger()
-     // should match maxAssetSize value in ~/ui-build/webpack/index.js
-     if ((fileSize > 1400000) && !thingsWeKnowAreWayTooBig.any{records[0].contains(it)}) {
-       bigFileList.push(records[0])
-     }
-   }
-
-  //  if (!bigFileList.isEmpty()) {
-    //  def authorSlackId = env.GERRIT_EVENT_ACCOUNT_EMAIL ? slackUserIdFromEmail(email: env.GERRIT_EVENT_ACCOUNT_EMAIL, botUser: true, tokenCredentialId: 'slack-user-id-lookup') : ''
-    //  def authorSlackMsg = authorSlackId ? "<@$authorSlackId>" : env.GERRIT_EVENT_ACCOUNT_NAME
-    //  def authorSegment = "Patchset <${env.GERRIT_CHANGE_URL}|#${env.GERRIT_CHANGE_NUMBER}> by ${authorSlackMsg} includes an asset bundle over the recommended max size"
-    //  def bigFiles = bigFileList.join(", ")
-    //  def extra = "Bundle ${bigFiles} is too big and not in our known list of oversized bundles. This patchset may have pushed in over the acceptable limit. Please review the <https://inst.splunkcloud.com/en-US/app/search/report?sid=scheduler_cm9iZXJ0LmJ1dGVuQGluc3RydWN0dXJlLmNvbQ__search__RMD5ca61b6204ef60fe9_at_1650416400_45040_1987C8A1-0299-4A02-A75B-503AB27123E0&s=%2FservicesNS%2Fnobody%2Fsearch%2Fsaved%2Fsearches%2Fwebpack_bundle_size|Webpack Bundle Size> to see how bundles have grown over time."
-    //  def summaryUrl = "${env.BUILD_URL}/build-summary-report"
-
-    //  slackSend(
-    //    channel: '#canvas_builds',
-    //    color: 'warning',
-    //    message: "${authorSegment}. Build <${summaryUrl}|#${env.BUILD_NUMBER}>\n\n$extra"
-    //  )
-  //  }
+    reportToSplunk('webpack_bundle_size', [
+      'fileName': records[0],
+      'fileSize': records[1],
+      'version': '3a',
+    ])
+  }
 }
