e58e44a6a70ff735584716539a3375722fcd6cff
Jackson Howe <jackson.howe@instructure.com>
2022-11-11 12:39:02 -0700

    Translate 'close' text in jqueryui dialog
    The SR-only close text in all jqueryui dialogs was not translated.
    We can't use Canvas' usei18nScope since the jqueryui dialog config
    is done in its own package, but since there's just a single string
    to translate, and that string ('close') is certainly present in
    existing package translations, we'll just pull that existing
    translation instead of setting up a whole new translation pipeline.
    
    closes LS-3553
    flag = none
    
    Test plan:
     - Switch user's language
     - Go to modules, click Add module
     - Inspect the close 'x' button
     - Expect the screenreader text to be the translated version of
       'Close'
    
    Change-Id: Ie3d6801bf7adce790789008954d8e6a65ee3c047
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/305236
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

--- packages/jqueryui/dialog.js
+++ packages/jqueryui/dialog.js
@@ -2,42 +2,56 @@ import $ from 'jquery'
 import h from 'html-escape'
 import './dialog-unpatched'
 
+// Doing this because there's just one string to translate in this package and we don't want
+// to create a whole new translation pipeline for one package. Additionally, we're eventually
+// moving away from jqueryui, and the string 'Close' should always be present in the existing
+// translation files. Find a better solution if we end up with more than just a single string
+// to translate, or if the strings that need translation are more complex.
+const parseCloseString = packageTranslations => packageTranslations?.close_d634289d?.message
+import(`@instructure/translations/lib/${ENV.LOCALE}.json`)
+  .then(packageTranslations => extendDialog(parseCloseString(packageTranslations)))
+  .catch(() =>
+    import(`@instructure/translations/lib/${ENV.LOCALE?.replace(/-/g, '_')}.json`)
+      .then(packageTranslations => extendDialog(parseCloseString(packageTranslations)))
+      .catch(() => extendDialog())
+  )
+
+const extendDialog = (translatedClose = 'close') => {
   // have UI dialogs default to modal:true
   $.ui.dialog.prototype.options.modal = true
 
   // based on d209434 and 83639ec, htmlEscape string titles by default, and
   // support jquery object titles
   function fixTitle(title) {
-    if (!title) return title;
-    return title.jquery ?
-      $('<div />').append(title.eq(0).clone()).html() :
-      h('' + title);
+    if (!title) return title
+    return title.jquery ? $('<div />').append(title.eq(0).clone()).html() : h('' + title)
   }
 
-  var create = $.ui.dialog.prototype._create,
-      setOption = $.ui.dialog.prototype._setOption;
+  const create = $.ui.dialog.prototype._create,
+    setOption = $.ui.dialog.prototype._setOption
 
   $.extend($.ui.dialog.prototype, {
-    _create: function() {
+    _create() {
       if (!this.options.title) {
-        this.options.title = this.element.attr("title");
-        if (typeof this.options.title !== "string")
-          this.options.title = '';
+        this.options.title = this.element.attr('title')
+        if (typeof this.options.title !== 'string') this.options.title = ''
       }
-      this.options.title = fixTitle(this.options.title);
+      this.options.title = fixTitle(this.options.title)
+      this.options.closeText = translatedClose
       this._on({
-        dialogopen: function() {
-          $('#application').attr('aria-hidden', 'true');
+        dialogopen() {
+          $('#application').attr('aria-hidden', 'true')
         },
-        dialogclose: function() {
-          $('#application').attr('aria-hidden', 'false');
-        }
-      });
-      return create.apply(this, arguments);
+        dialogclose() {
+          $('#application').attr('aria-hidden', 'false')
+        },
+      })
+      return create.apply(this, arguments)
     },
 
-    _setOption: function(key, value) {
-      if (key == "title") value = fixTitle(value);
-      return setOption.call(this, key, value);
-    }
-  });
+    _setOption(key, value) {
+      if (key == 'title') value = fixTitle(value)
+      return setOption.call(this, key, value)
+    },
+  })
+}
