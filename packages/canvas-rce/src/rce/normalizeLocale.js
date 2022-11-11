0ae63eb40465ff2bc6f8fec10e51b3c6dee7bba8
Ed Schiebel <eschiebel@instructure.com>
2022-11-11 10:17:21 -0500

    Use generated locale list for normalization
    closes MAT-1096
    flag=none
    
    test plan:
      - the tests pass
    
    Change-Id: Ibd1b51b0268e6cdeb10398ac4123e2f82b10143c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/305220
    Reviewed-by: Joe Hernandez <joe.hernandez@instructure.com>
    QA-Review: Joe Hernandez <joe.hernandez@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- packages/canvas-rce/src/rce/normalizeLocale.js
+++ packages/canvas-rce/src/rce/normalizeLocale.js
@@ -16,89 +16,20 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {getLocaleList} from '../getTranslations'
+
 // keys = "qualified" locales old canvas sent, values = corresponding standard
 // locales that canvas now sends. old locales absent from this mapping (e.g.
 // pt-BR) didn't change.
 const mapping = {
-  'ar-SA': 'ar',
-  'da-DK': 'da',
-  'de-DE': 'de',
-  'en-US': 'en',
-  'es-ES': 'es',
   fa: 'fa-IR',
-  'fr-FR': 'fr',
-  'he-IL': 'he',
-  'hy-AM': 'hy',
-  'ja-JP': 'ja',
-  'ko-KR': 'ko',
-  'mi-NZ': 'mi',
-  'nb-NO': 'nb',
-  'nl-NL': 'nl',
-  'pl-PL': 'pl',
-  'pt-PT': 'pt',
-  'ru-RU': 'ru',
-  'sv-SE': 'sv',
-  'tr-TR': 'tr',
   uk: 'uk-UA',
   'zh-CN': 'zh-Hans',
   'zh-HK': 'zh-Hant',
 }
 
-// these are the recognized standard and custom locales. remember to extend
-// this list when support for a locale is added.
-const recognized = [
-  'ar',
-  'bg',
-  'ca',
-  'cs',
-  'cy',
-  'da',
-  'da-x-k12',
-  'de',
-  'el',
-  'en',
-  'en-AU',
-  'en-AU-x-unimelb',
-  'en-GB',
-  'en-GB-x-lbs',
-  'en-GB-x-ukhe',
-  'es',
-  'es-ES',
-  'fa-IR',
-  'fi',
-  'fr',
-  'fr-CA',
-  'he',
-  'ht',
-  'hu',
-  'hy',
-  'is',
-  'it',
-  'ja',
-  'ko',
-  'mi',
-  'nb',
-  'nb-x-k12',
-  'nl',
-  'nn',
-  'pl',
-  'pt',
-  'pt-BR',
-  'ro',
-  'ru',
-  'sq',
-  'sr',
-  'sl',
-  'sv',
-  'sv-x-k12',
-  'th',
-  'tr',
-  'vi',
-  'zh-Hans',
-  'zh-Hant',
-]
-
 export default function normalizeLocale(locale) {
+  const recognized = getLocaleList()
   if (!locale) {
     // default to english
     return 'en'
@@ -112,6 +43,8 @@ export default function normalizeLocale(locale) {
     // reduce unrecognized custom locales to their base locale
     locale = locale.split('-x-')[0]
     return normalizeLocale(locale)
+  } else if (recognized.indexOf(locale.split('-')[0]) >= 0) {
+    return locale.split('-')[0]
   } else {
     // default to english for unrecognized locales
     return 'en'
