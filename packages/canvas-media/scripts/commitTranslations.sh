8df719f5b8f9eb336af62b0f07ec62093f8f56b2
Ed Schiebel <eschiebel@instructure.com>
2022-11-22 12:43:03 -0500

    Add commitTranslations step to canvas-media
    closes MAT-1107
    flag=none
    
    test plan:
      - the next package-translations build commits changes
        to packages/canvas-media
    or
      - cd to canvas-media and run
        yarn installTranslations
        yarn commitTranslations
      > expect a commit with the new translations
        (the push will probably fail)
    
    Change-Id: I623e9847bdbc82a2a51824424f6c91fbb483b187
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/305927
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jon Scheiding <jon.scheiding@instructure.com>
    QA-Review: Jon Scheiding <jon.scheiding@instructure.com>
    Product-Review: Jon Scheiding <jon.scheiding@instructure.com>

---
+++ packages/canvas-media/scripts/commitTranslations.sh
@@ -0,0 +1,45 @@
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+push() {
+  if [ ! -z "$SUPPRESS_PUSH" ]; then
+    #
+    # You can use with SUPPRESS_PUSH=1 when testing locally, to avoid
+    # errors about not being able to auto-submit
+    #
+    echo "Would have pushed a PS to Gerrit"
+    return 0
+  fi
+
+  OUTPUT=$( git push origin sync-translations-media:refs/for/master%submit,t=learning-materials,l=Verified+1 2>&1 )
+  return $?
+}
+
+git checkout -B sync-translations-media && \
+  git add -A src && \
+  git commit -m "[i18n] Update canvas-media translations." && \
+  push
+
+echo $OUTPUT
+
+git checkout $CURRENT_BRANCH
+
+if [ ! -z "$OUTPUT" ]; then
+  URL=$(echo $OUTPUT | grep -Eo 'https://[^ >]+')
+
+  SLACK_MESSAGE="canvas-media translations have been updated. Publish to NPM needed! \\n $URL"
+
+  if [ ! -z "$SUPPRESS_SLACK" ]; then
+    #
+    # You can use with SUPPRESS_SLACK=1 when testing locally, to avoid
+    # making noise in the Materials team Slack channel
+    #
+    echo "Would have sent a Slack message:"
+    echo $SLACK_MESSAGE
+  else
+    (
+      aws --region us-east-1 sqs send-message \
+        --queue-url https://sqs.us-east-1.amazonaws.com/636161780776/slack-lambda \
+        --message-body "{\"channel\":\"#mat-bots\",\"username\":\"Package Translations\",\"text\":\"$SLACK_MESSAGE\"}"
+    ) || echo "Failed to send Slack message."
+  fi
+fi
