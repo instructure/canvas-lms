5ac145bd640730d4b5e31bd7b379af519e9b407c
Evan Battaglia <ebattaglia@instructure.com>
2022-11-29 00:30:41 -0700

    amend CSP header for LTI 1.3's authorize endpoint
    refs INTEROP-7821
    
    This continues work started in g/306023 to allow LTI tools to embed
    within other trusted (e.g. New Quizzes) LTI tools, by adding the
    parent_frame_context tool's URL in the CSP header.
    
    This commit in particular enables embedded LTI 1.3 launches by setting
    the header for the `authorize` endpoint, which for is called as the last
    step in the LTI 1.3 launch process initiated by retrieve or
    resource_selection.
    
    Test plan:
    - modify the LTI 1.3 test tool dev key to have internal_service=true
    - in the LTI 1.3 test tool's app/views/launch/launch.html.erb add an
      iframe pointing to resource_selection like this:
        <iframe
          src="http://web.canvas-lms.docker/courses/66/external_tools/155/resource_selection?parent_frame_context=155&editor=1"
          style="width: 70%; height: 70%" ></iframe>
      The parent_frame_context parameter should be your LTI 1.3 test tool
      ID. The first tool ID can be any LTI 1.3 tool. If you're like me, the
      only 1.3 tool I have installed locally is the test tool, so you can
      use the same tool.
    - Launch the LTI 1.3 test tool. Your LTI 1.3 tool should successfully
      launch embedded into the test tool. If you embedded the test tool,
      LTInception will have occurred and it will keep launching embedded
      tools inside each other. Hit "stop" on your browser sometime before
      your machine runs out of memory :D
    - Change the iframe you added above to a retrieve URL, e.g.:
        http://web.canvas-lms.docker/courses/66/external_tools/retrieve?url=http%3A%2F%2Flti13testtool.docker%2Flaunch&parent_frame_context=155"
      Where the URL is a URL for an LTI 1.3 tool available in the course,
      and 155 is your LTI 1.3 test tool ID
    - Launch the tool and again observe the embedded tool / LTInception. For
      some reason with retrieve, it seems to stop on its own at a 3-tool
      level, but I don't think that should be a problem because we only need
      a 2-tool level.
    
    refs INTEROP-7821
    
    Change-Id: I5356be12ee1022bd995b6e5391d06fd5294f8af1
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/306179
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>

---
+++ spec/controllers/lti/concerns/parent_frame_shared_examples.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2018 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+shared_examples_for "an endpoint which uses parent_frame_context to set the CSP header" do
+  # When using, `subject` should also be setup make the request, using pfc_tool.id
+  # as the parent_frame_context
+
+  let(:pfc_tool_context) { raise "override when using" }
+
+  let(:pfc_tool) do
+    pfc_tool_context.context_external_tools.create(
+      name: "instructure_tool", consumer_key: "foo", shared_secret: "bar",
+      url: "http://inst-tool.example.com/abc", lti_version: "1.1",
+      developer_key: DeveloperKey.new(
+        root_account: pfc_tool_context.root_account,
+        internal_service: true
+      )
+    )
+  end
+
+  let(:csp_header) { response.headers["Content-Security-Policy"] }
+
+  it "adds the tool URL to the header if the parent_frame_context tool is trusted" do
+    subject
+    expect(response).to be_successful
+    expect(csp_header).to match %r{frame-ancestors [^;]*http://inst-tool.example.com(;| |$)}
+    # Make sure it also has 'self', which is added in application_controller.rb:
+    expect(csp_header).to match(/frame-ancestors [^;]*'self'/)
+  end
+
+  it "doesn't add the URL to the header if the parent_frame_context tool is not trusted" do
+    pfc_tool.developer_key.update! internal_service: false
+    subject
+    expect(response).to be_successful
+    expect(csp_header).to_not include("inst-tool.example.com")
+  end
+
+  it "doesn't add the URL to the header if the parent_frame_context tool is not active" do
+    pfc_tool.update! workflow_state: :deleted
+    subject
+    expect(response).to be_successful
+    expect(csp_header).to_not include("inst-tool.example.com")
+  end
+
+  it "doesn't add the URL to the header if the parent_frame_context tool's URL has unsafe characters" do
+    pfc_tool.update_attribute :url, "http://inst-tool.example.com;default-src"
+    subject
+    expect(response).to be_successful
+    expect(csp_header).to_not include("inst-tool.example.com")
+  end
+
+  it "doesn't add the parent_frame_context tool's URL to the header if it is a data URL" do
+    pfc_tool.update! url: "data:123"
+    subject
+    expect(response).to be_successful
+    expect(csp_header).to_not include("data:abc")
+  end
+end
