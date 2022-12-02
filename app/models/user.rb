79c704beb25686297f12269ce6152b2d0a14544d
Jeremy Stanley <jeremy@instructure.com>
2022-12-02 13:06:00 -0700

    move LTI IDs when merging into an unused User record
    if the destination user doesn't have any enrollments or
    account memberships or an LTI context id (as is the case
    in consortium home_account moves), move the lti_context_id,
    lti_id, and uuid to the new user record. this way new
    enrollments and global nav launches will continue to use
    the previous LTI IDs, and the calendar feed will continue
    to work
    
    test plan:
     - have an auto-merge consortium set up
     - have a user enrolled in courses in two separate shards
       (course A in Account A and course B in Account B)
       with an LTI tool that can be launched in course context
       or global nav
     - use a SIS import with home_account=true to move the user
       to a different shard in the consortium
     - ensure when the tool is launched from any of course A,
       course B, Account A, or Account B, the student's
       original lti_context_id is supplied to the LTI tool
     - probably repeat the above with an LTI 1.3 tool,
       which uses a separate ID column
     - splitting the merged users should move the ids back
       where they came from
    
    (this reverts 569531302733f01ffd62efaf0cc4433f12d8b9bb)
    
    [pin-commit-multiple_root_accounts=0310a2265dde906935c99642d26e72bada1ea573]
    
    flag=none
    refs FOO-3153
    closes FOO-3355
    
    Change-Id: I7d1e61faf2498aff4fc5820609d54050ee1a31de
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/310292
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
   include UserLearningObjectScopes
   include PermissionsHelper
 
-  attr_accessor :previous_id, :gradebook_importer_submissions, :prior_enrollment
+  attr_accessor :previous_id, :gradebook_importer_submissions, :prior_enrollment, :override_lti_id_lock
 
   before_save :infer_defaults
   before_validation :ensure_lti_id, on: :update
@@ -755,6 +755,8 @@ class User < ActiveRecord::Base
 
   def new_registration(form_params = {}); end
 
+  def update_shadow_records_synchronously!; end
+
   # DEPRECATED, override new_registration instead
   def new_teacher_registration(form_params = {})
     new_registration(form_params)
@@ -906,7 +908,7 @@ class User < ActiveRecord::Base
   end
 
   def preserve_lti_id
-    errors.add(:lti_id, "Cannot change lti_id!") if lti_id_changed? && !lti_id_was.nil?
+    errors.add(:lti_id, "Cannot change lti_id!") if lti_id_changed? && !lti_id_was.nil? && !override_lti_id_lock
   end
 
   def ensure_lti_id
