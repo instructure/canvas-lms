ecbeec3496bc7cd21af38686c6e2f21f2ce10706
Chris Soto <christopher.soto@instructure.com>
2022-12-06 09:10:10 -0700

    group file on individual assignment
    when a user uploads a file to their group and uses
    the file for an individual assignment submission,
    teachers were not able to see the file upload in
    speedgrader
    
    fixes EVAL-2749
    flag=none
    
    test plan:
    - create a group and file upload assignment
    - add student to group
    - have student upload a file to the group
    - have student submit to assignment with the group file
    - go to speedgrader and view assignment
    - teacher should be able to see the uploaded files for
      the submission
    
    Change-Id: I2863c1741b91bc9f712266483298a64666abd964
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/306713
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Deborah Kwak <deborah.kwak@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Reviewed-by: Eduardo Escobar <eduardo.escobar@instructure.com>
    QA-Review: Eduardo Escobar <eduardo.escobar@instructure.com>

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1352,7 +1352,7 @@ class Submission < ActiveRecord::Base
     attachments = Attachment.where(id: unassociated_ids)
     attachments.each do |a|
       next unless (a.context_type == "User" && a.context_id == user_id) ||
-                  (a.context_type == "Group" && a.context_id == group_id) ||
+                  (a.context_type == "Group" && (a.context_id == group_id || user.membership_for_group_id?(a.context_id))) ||
                   (a.context_type == "Assignment" && a.context_id == assignment_id && a.available?) ||
                   attachment_fake_belongs_to_group(a)
 
@@ -1783,7 +1783,7 @@ class Submission < ActiveRecord::Base
   def versioned_attachments=(attachments)
     @versioned_attachments = Array(attachments).compact.select do |a|
       (a.context_type == "User" && (a.context_id == user_id || a.user_id == user_id)) ||
-        (a.context_type == "Group" && a.context_id == group_id) ||
+        (a.context_type == "Group" && (a.context_id == group_id || user.membership_for_group_id?(a.context_id))) ||
         (a.context_type == "Assignment" && a.context_id == assignment_id && a.available?) ||
         attachment_fake_belongs_to_group(a)
     end
