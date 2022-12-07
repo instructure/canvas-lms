add0dbd03b9050a13698993cac4f08c90a733ddb
Tucker McKnight <tmcknight@instructure.com>
2022-12-07 15:46:56 -0700

    Refactor dev key scope fixup
    Since a developer key won't save unless all of its bad routes are
    fixed at once, if we discover a new bad route in the future, we'll want
    to re-run all of the prior scope changes on that key at the same time.
    Putting them all in the fixup class itself makes it easier to run all
    of the route changes every time, and gives a straightforward place to
    put new route changes that may come up in the future.
    
    flag = none
    
    Change-Id: I8c7c93d513d82b1c366bbe641c70196933be3363
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/306601
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>

--- db/migrate/20220902000000_fixup_developer_key_scopes.rb
+++ db/migrate/20220902000000_fixup_developer_key_scopes.rb
@@ -21,30 +21,7 @@ class FixupDeveloperKeyScopes < ActiveRecord::Migration[6.1]
   tag :postdeploy
   disable_ddl_transaction!
 
-  SCOPE_CHANGES = {
-    "url:POST|/api/v1/courses/:course_id/pages/:url/duplicate" => "url:POST|/api/v1/courses/:course_id/pages/:url_or_id/duplicate",
-    "url:GET|/api/v1/courses/:course_id/pages/:url" => "url:GET|/api/v1/courses/:course_id/pages/:url_or_id",
-    "url:GET|/api/v1/groups/:group_id/pages/:url" => "url:GET|/api/v1/groups/:group_id/pages/:url_or_id",
-    "url:GET|/api/v1/courses/:course_id/pages/:url/revisions" => "url:GET|/api/v1/courses/:course_id/pages/:url_or_id/revisions",
-    "url:GET|/api/v1/groups/:group_id/pages/:url/revisions" => "url:GET|/api/v1/groups/:group_id/pages/:url_or_id/revisions",
-    "url:GET|/api/v1/courses/:course_id/pages/:url/revisions/latest" => "url:GET|/api/v1/courses/:course_id/pages/:url_or_id/revisions/latest",
-    "url:GET|/api/v1/groups/:group_id/pages/:url/revisions/latest" => "url:GET|/api/v1/groups/:group_id/pages/:url_or_id/revisions/latest",
-    "url:GET|/api/v1/courses/:course_id/pages/:url/revisions/:revision_id" => "url:GET|/api/v1/courses/:course_id/pages/:url_or_id/revisions/:revision_id",
-    "url:GET|/api/v1/groups/:group_id/pages/:url/revisions/:revision_id" => "url:GET|/api/v1/groups/:group_id/pages/:url_or_id/revisions/:revision_id",
-    "url:POST|/api/v1/courses/:course_id/pages/:url/revisions/:revision_id" => "url:POST|/api/v1/courses/:course_id/pages/:url_or_id/revisions/:revision_id",
-    "url:POST|/api/v1/groups/:group_id/pages/:url/revisions/:revision_id" => "url:POST|/api/v1/groups/:group_id/pages/:url_or_id/revisions/:revision_id",
-    "url:PUT|/api/v1/courses/:course_id/pages/:url" => "url:PUT|/api/v1/courses/:course_id/pages/:url_or_id",
-    "url:PUT|/api/v1/groups/:group_id/pages/:url" => "url:PUT|/api/v1/groups/:group_id/pages/:url_or_id",
-    "url:DELETE|/api/v1/courses/:course_id/pages/:url" => "url:DELETE|/api/v1/courses/:course_id/pages/:url_or_id",
-    "url:DELETE|/api/v1/groups/:group_id/pages/:url" => "url:DELETE|/api/v1/groups/:group_id/pages/:url_or_id"
-  }.freeze
-
   def up
-    DeveloperKey.find_ids_in_ranges(batch_size: 1000) do |start_at, end_at|
-      DataFixup::UpdateDeveloperKeyScopes.delay_if_production(
-        priority: Delayed::LOW_PRIORITY,
-        n_strand: ["DataFixup::UpdateDeveloperKeyScopes", Shard.current.database_server.id]
-      ).run(start_at, end_at, SCOPE_CHANGES)
-    end
+    DataFixup::UpdateDeveloperKeyScopes.run
   end
 end
