831ca81d5b9523d7bbeb3b69d210ba0f6fe5b599
CkReal <CkReal@users.noreply.github.com>
2022-12-07 20:29:25 +0900

    Fix broken check_for_docker_desktop in Japanese

--- script/common/utils/docker_desktop_setup.sh
+++ script/common/utils/docker_desktop_setup.sh
@@ -45,7 +45,12 @@ function attempt_start_docker {
 }
 
 function check_for_docker_desktop {
-  if ! mdfind "kMDItemKind == 'Application'" |grep -qE 'Docker.app|Docker\ Desktop.app'; then
+  case $LANG in
+    'ja_JP.UTF-8') item_kind_name='アプリケーション';;
+    *) item_kind_name='Application';;
+  esac
+
+  if ! mdfind "kMDItemKind == $item_kind_name" |grep -qE 'Docker.app|Docker\ Desktop.app'; then
     echo "  Docker Desktop is not installed!"
     echo "  Refer to https://docs.docker.com/docker-for-mac/install/ for help installing."
     echo "  Once Docker Desktop is installed rerun this script."
