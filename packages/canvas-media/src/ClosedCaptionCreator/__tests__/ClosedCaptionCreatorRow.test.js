00f5b2b0733a73dfb1cbab7311fdefa5c8a9cc64
deyvison.penha <deyvison.penha@instructure.com>
2022-12-08 15:27:16 -0300

    Closed caption file that's too large
    This patchset fix the issue when the
    user upload a CC file that is too large
    then the max size available, now we
    show a Alert message error.
    
    fixes MAT-964
    flag=none
    
    Test plan:
    -Open up a page with an RCE
    -Add a video and include a srt file
    -The Max size is 295000 bytes
    -It's show a alert message if the file
    is bigger then max size.
    
    Change-Id: Ifd84967e21731b465ba02f3de7722ccfa4603c73
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/306970
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Allison Howell <allison.howell@instructure.com>

--- packages/canvas-media/src/ClosedCaptionCreator/__tests__/ClosedCaptionCreatorRow.test.js
+++ packages/canvas-media/src/ClosedCaptionCreator/__tests__/ClosedCaptionCreatorRow.test.js
@@ -15,16 +15,17 @@
  * You should have received a copy of the GNU Affero General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import {fireEvent, render} from '@testing-library/react'
+import {fireEvent, render, waitFor} from '@testing-library/react'
 import React from 'react'
 import ClosedCaptionCreatorRow from '../ClosedCaptionCreatorRow'
+import {CC_FILE_MAX_BYTES} from '../../shared/constants'
 
 function makeProps(overrides = {}) {
   return {
     rowId: undefined,
     languages: [
       {id: 'en', label: 'English'},
-      {id: 'fr', label: 'French'}
+      {id: 'fr', label: 'French'},
     ],
     liveRegion: () => document.getElementById('flash_screenreader_holder'),
     uploadMediaTranslations: {
@@ -33,21 +34,21 @@ function makeProps(overrides = {}) {
         NO_FILE_CHOSEN: 'no file chosen',
         SUPPORTED_FILE_TYPES: 'supported file types',
         CLOSED_CAPTIONS_CHOOSE_FILE: 'choose file',
-        CLOSED_CAPTIONS_SELECT_LANGUAGE: 'select language'
+        CLOSED_CAPTIONS_SELECT_LANGUAGE: 'select language',
       },
       SelectStrings: {
         USE_ARROWS: 'Use arrows',
         LIST_COLLAPSED: 'List collapsed.',
         LIST_EXPANDED: 'List expanded.',
-        OPTION_SELECTED: '{option} selected.'
-      }
+        OPTION_SELECTED: '{option} selected.',
+      },
     },
     onDeleteRow: () => {},
     onFileSelected: () => {},
     onLanguageSelected: () => {},
     selectedFile: null,
     selectedLanguage: null,
-    ...overrides
+    ...overrides,
   }
 }
 
@@ -56,19 +57,11 @@ function makeConfiguredProps(overrides = {}) {
     selectedLanguage: {id: 'en', label: 'English'},
     selectedFile: {name: 'thefile.srt'},
     rowId: 'en',
-    ...overrides
+    ...overrides,
   })
 }
 
 describe('ClosedCaptionCreatorRow', () => {
-  const selectFile = (element, file) => {
-    fireEvent.change(element, {
-      target: {
-        file
-      }
-    })
-  }
-
   beforeAll(() => {
     const node = document.createElement('div')
     node.setAttribute('role', 'alert')
@@ -95,6 +88,16 @@ describe('ClosedCaptionCreatorRow', () => {
   })
 
   describe('when editing caption data', () => {
+    const selectFile = async (element, file) => {
+      await waitFor(() =>
+        fireEvent.change(element, {
+          target: {
+            files: [file],
+          },
+        })
+      )
+    }
+
     it('renders normally', () => {
       const {getByText} = render(<ClosedCaptionCreatorRow {...makeProps()} />)
       expect(getByText('select language')).toBeInTheDocument()
@@ -117,21 +120,41 @@ describe('ClosedCaptionCreatorRow', () => {
       expect(getByDisplayValue('French')).toBeInTheDocument()
     })
 
-    it('calls onFileSelected when file is selected', () => {
+    it('calls onFileSelected when file is selected', async () => {
       const onFileSelected = jest.fn()
       const {container} = render(
         <ClosedCaptionCreatorRow
           {...makeProps({
-            onFileSelected
+            onFileSelected,
           })}
         />
       )
       const fileInput = container.querySelector('input[type="file"]')
       const file = new File(['foo'], 'file1.vtt', {type: 'application/vtt'})
-      selectFile(fileInput, [file])
+      await selectFile(fileInput, file)
       // We can validate the event object here but the parent is the one grabbing the file
       // from the input
       expect(onFileSelected).toHaveBeenCalledTimes(1)
+    })
+    it('upload a file that size is bigger than permitted', async () => {
+      const onFileSelected = jest.fn()
+      const {container, getAllByText} = render(
+        <ClosedCaptionCreatorRow
+          {...makeProps({
+            onFileSelected,
+            userLocale: 'en',
+          })}
+        />
+      )
+      const fileInput = container.querySelector('input[type="file"]')
+      // Create a file with a size larger than allowed
+      const buffer = new ArrayBuffer(CC_FILE_MAX_BYTES + 1)
+      const file = new File([buffer], 'file1.vtt', {type: 'application/vtt'})
+      const fileSizeMessageError = `The selected file exceeds the ${CC_FILE_MAX_BYTES} Byte limit`
+
+      await selectFile(fileInput, file)
+
+      expect(getAllByText(fileSizeMessageError).length).toEqual(3)
     })
   })
 })
