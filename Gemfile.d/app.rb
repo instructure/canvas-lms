83d8b099f6a8a1f887008203b0a695f3544e2b2a
Aaron Ogata <aogata@instructure.com>
2022-12-13 07:43:39 -0800

    explicitly require rss gem for ruby 3.x
    refs AE-92
    
    Change-Id: Id9a7d86f3367682c7fd0318c1e1e7ee8c4af33ca
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/307293
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -130,6 +130,7 @@ gem "redcarpet", "3.5.0", require: false
 gem "retriable", "1.4.1"
 gem "ritex", "1.0.1", require: false
 gem "rotp", "6.2.0", require: false
+gem "rss", "0.2.9", require: false
 gem "ruby-duration", "3.2.3", require: false
 gem "ruby2_keywords", "0.0.3"
 gem "rubycas-client", "2.3.9", require: false
