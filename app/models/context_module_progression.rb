fd8ca94299d8f9933f434674960a48c3c353b4f5
Robin Kuss <rkuss@instructure.com>
2022-12-13 14:59:33 -0600

    fix big decimal rounding ahead of ruby 3
    Closes LS-3653
    
    Test Plan: passes jenkins AND:
    
    From Nate's PS for LS-1916 ...
    The best way to test this locally is to:
    - Create a course with a student
    - Create a module and add an assignment to that module
    - Make the assignment worth 10 points
    - Add a module completion requirement for the assignment to score at least 1
    - Publish the module and the assignment
    - In the UI, give the student a grade of 0 for the assignment
    - As the student, see that the item is incomplete
    
    - From the rails console, find the submission and give it a score of 0.3 + 0.3 + 0.3 + 0.1
    submission = Submission.last
    submission.score = 0.3 + 0.3 + 0.3 + 0.1 # 0.9999999999
    submission.save
    
    Change-Id: Iab1d54a11a47544d85f15f69f04cdc1e39980ab4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/307341
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- app/models/context_module_progression.rb
+++ app/models/context_module_progression.rb
@@ -17,6 +17,12 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+#
+class Float
+  def near_enough?(other, epsilon = 1e-6)
+    (self - other.to_f).abs < epsilon.to_f
+  end
+end
 
 class ContextModuleProgression < ActiveRecord::Base
   include Workflow
@@ -278,7 +284,9 @@ class ContextModuleProgression < ActiveRecord::Base
 
     subs.any? do |sub|
       score = get_submission_score(sub)
-      requirement_met = (score.present? && score.to_d >= requirement[:min_score].to_f)
+
+      new_score = score.near_enough?(score.round) ? score.round : score
+      requirement_met = (score.present? && new_score.to_f >= requirement[:min_score].to_f)
       if requirement_met
         remove_incomplete_requirement(requirement[:id])
       else
