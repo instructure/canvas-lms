50feb430037bbc9b9168b692829230474610bf63
Evan Battaglia <ebattaglia@instructure.com>
2023-01-03 12:53:14 -0700

    Bump ims-lti gem (Ruby 3 support)
    refs INTEROP-7884
    
    Test plan:
    - specs
    
    Change-Id: Ib0c69ba8d258759face9e018a0387d5b417a6cdd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/308048
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -84,7 +84,7 @@ gem "i18nliner", "0.2.2", github: "instructure/i18nliner", ref: "ruby3"
   gem "ruby_parser", "3.18.1", require: false
 gem "icalendar", "2.7.0", require: false
 gem "diplomat", "2.6.3", require: false
-gem "ims-lti", "2.3.2", require: "ims"
+gem "ims-lti", "2.3.3", require: "ims"
 gem "rrule", "0.4.4", require: false
 
 gem "inst_access", "0.1.1"
