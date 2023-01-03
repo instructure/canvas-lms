f48aaa8e50f8d560dfaea9ed06448d58cd0ac2a8
Drake Harper <drake.harper@instructure.com>
2023-01-03 10:26:31 -0700

    Fix split screen view deep linking
    fixes VICE-3290
    flag=split_screen_view
    
    Test Plan:
     - turn on split view ff
     - go to a discussion with ?entry_id=<entry id here>
     - discussion page should load with entry highlighted
    
    Change-Id: Id2143b4a540e332eb840ff47df5a86077e39af43
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/308035
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- ui/features/discussion_topics_post/react/DiscussionTopicManager.js
+++ ui/features/discussion_topics_post/react/DiscussionTopicManager.js
@@ -83,13 +83,21 @@ const DiscussionTopicManager = props => {
 
   // Isolated View State
   const [threadParentEntryId, setThreadParentEntryId] = useState(
-    ENV.discussions_deep_link?.root_entry_id
+    ENV.isolated_view
+      ? ENV.discussions_deep_link?.root_entry_id
+      : ENV.discussions_deep_link?.parent_id
   )
   const [replyFromId, setReplyFromId] = useState(null)
   const [isolatedViewOpen, setIsolatedViewOpen] = useState(
     !!ENV.discussions_deep_link?.root_entry_id
   )
-  const [isSplitScreenViewOpen, setSplitScreenViewOpen] = useState(false)
+
+  // split screen view
+  const [isSplitScreenViewOpen, setSplitScreenViewOpen] = useState(
+    !!(ENV.discussions_deep_link?.parent_id
+      ? ENV.discussions_deep_link?.parent_id
+      : ENV.discussions_deep_link?.entry_id)
+  )
   const [isSplitScreenViewOverlayed, setSplitScreenViewOverlayed] = useState(false)
   const [editorExpanded, setEditorExpanded] = useState(false)
 
