340171c15536af68ef37741d8f4ae7618081830e
Isaac Moore <isaac.moore@instructure.com>
2023-01-06 14:22:36 -0600

    Remove `require_dependency` calls in frontend build
    refs AE-31
    
    flag=none
    
    test plan:
    - verify frontend builds
    
    Change-Id: I6f7b097ef2c39628a3b5810180c6136820b34d39
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/308332
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Isaac Moore <isaac.moore@instructure.com>
    Product-Review: Isaac Moore <isaac.moore@instructure.com>

--- lib/canvas/cdn.rb
+++ lib/canvas/cdn.rb
@@ -17,10 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require_dependency "canvas/cdn/s3_uploader"
-require_dependency "canvas/cdn/registry"
-require_dependency "config_file"
-
 module Canvas
   module Cdn
     class << self
@@ -34,7 +30,7 @@ module Canvas
         end
       end
 
-      # Provides an instance of Registry for the current Rails environment.
+      # Provides an instance of Cdn::Registry for the current Rails environment.
       #
       # Set ENV['USE_OPTIMIZED_JS'] to a truthy value to load the optimized
       # version of the JavaScripts even if you're running a development Rails
@@ -47,12 +43,12 @@ module Canvas
                           Rails.env
                         end
 
-          Registry.new(
+          Cdn::Registry.new(
             environment: environment,
             cache: if ActionController::Base.perform_caching
-                     Registry::ProcessCache.new
+                     Cdn::Registry::ProcessCache.new
                    else
-                     Registry::RequestCache.new
+                     Cdn::Registry::RequestCache.new
                    end
           )
         end
--- lib/canvas/cdn/registry.rb
+++ lib/canvas/cdn/registry.rb
@@ -17,9 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require_dependency "canvas/cdn/registry/gulp"
-require_dependency "canvas/cdn/registry/webpack"
-
 module Canvas
   module Cdn
     # A registry of the available javascripts and static assets - like images
