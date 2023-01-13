d1860df7715b0c19ac8e4fb0bda8dfbbf4fffe61
Isaac Moore <isaac.moore@instructure.com>
2023-01-13 11:19:16 -0600

    Add require statements that fix Rails 7.0 build
    fixes AE-114
    flag=none
    
    test plan:
    - rails 7.0 build passes
    
    Change-Id: I3940aece3f2faeec5b12b0f6f7e60095e06eb4a9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/308789
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Isaac Moore <isaac.moore@instructure.com>
    Product-Review: Isaac Moore <isaac.moore@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- lib/canvas/cdn.rb
+++ lib/canvas/cdn.rb
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+require "canvas/cdn/registry"
+
 module Canvas
   module Cdn
     class << self
