93f40502360d0f4e6bb15522a5c4f73e22c8240e
Jacob Burroughs <jburroughs@instructure.com>
2023-01-16 20:33:01 -0600

    Unignore proxy_submitter_id
    Change-Id: I42828505612409f14ff8be6d261fceedd6132375
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/308957
    Reviewed-by: Alex Slaughter <aslaughter@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -22,7 +22,7 @@ require "atom"
 require "anonymity"
 
 class Submission < ActiveRecord::Base
-  self.ignored_columns = %w[has_admin_comment has_rubric_assessment process_attempts context_code proxy_submitter_id]
+  self.ignored_columns = %w[has_admin_comment has_rubric_assessment process_attempts context_code]
 
   include Canvas::GradeValidations
   include CustomValidations
