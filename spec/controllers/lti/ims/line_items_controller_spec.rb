8f542d8af7da004281a2dbb2b441b99c14a3fd23
Evan Battaglia <ebattaglia@instructure.com>
2023-01-18 12:40:41 -0700

    Use Account#domain for LTI AGS ids (URLs)
    This will provide consistency in the IDs for Accounts which can be
    accessed on multiple domains. No matter what domain is used to access
    Canvas, the AGS ids will always use the domain which is considered the
    primary for the account.
    
    closes INTEROP-7813
    flag=consistent_ags_ids_based_on_account_principal_domain
    
    Test plan:
    - Make sure you have a Canvas account which you can access from multiple
      domains. To do this you'll likely need to have MRA setup, and then add
      an additional domain in Account Settings -> Canvas Cloud Information,
      and setup an additional domain with your proxy
      (traefik/dinghy/dory/whatever) as you did when setting up MRA
    - Check the following both with the new feature flag
      (consistent_ags_ids_based_on_account_principal_domain) off and on.
      Hit Canvas on a domain which is the not the account's primary domain.
      For each, check that, for the ID/URL in question:
        - With the flag off, it should include the domain you're accessing
          Canvas on
        - With the flag on, it should include the primary domain
      1. Launch an LTI 1.3 tool in an assignment context (e.g. create an LTI
         1.3 assignment) and check both the "lineitems" and "lineitem"
           fields
      2. Also check the context_memberships_url (NRPS) from the launch
      3. Get an LTI advantage token for your LTI 1.3 tool
         (/api/lti/advantage_token?tool_id=123) and GET the
         /api/lti/courses/2/line_items endpoint, and check the "id" fields
         of the returned results.
      4. Post a score to the /courses/123/line_items/456 (etc.) endpoint --
         see the example requst from the docs at
         https://canvas.instructure.com/doc/api/score.html --
         and check the the "progress" URL (under content_items) and the
         resultUrl returned.
      5. Get the /api/lti/courses/2/line_items/1/results and check the URLs
         in the "id" field and the "scoreOf" field.
      6. Get the /api/lti/courses/2/names_and_roles endpoint and check the
         URL in the "id" field.
    
    Change-Id: I4250323445d00324cf7470b2496683b492dfce27
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/309389
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>

--- spec/controllers/lti/ims/line_items_controller_spec.rb
+++ spec/controllers/lti/ims/line_items_controller_spec.rb
@@ -148,6 +148,28 @@ module Lti
             expect(parsed_response_body).to eq expected_response
           end
 
+          context "when the consistent_ags_ids_based_on_account_principal_domain feature flag is on" do
+            it "uses the Account#domain in the line item id" do
+              course.root_account.enable_feature!(:consistent_ags_ids_based_on_account_principal_domain)
+              allow_any_instance_of(Account).to receive(:domain).and_return("canonical.host")
+              send_request
+              expect(parsed_response_body["id"]).to start_with(
+                "http://canonical.host/api/lti/courses/#{course.id}/line_items/"
+              )
+            end
+          end
+
+          context "when the consistent_ags_ids_based_on_account_principal_domain feature flag is off" do
+            it "uses the host domain in the line item id" do
+              course.root_account.disable_feature!(:consistent_ags_ids_based_on_account_principal_domain)
+              allow_any_instance_of(Account).to receive(:domain).and_return("canonical.host")
+              send_request
+              expect(parsed_response_body["id"]).to start_with(
+                "http://test.host/api/lti/courses/#{course.id}/line_items/"
+              )
+            end
+          end
+
           it "associates the line item with the correct assignment" do
             send_request
             body = parsed_response_body
