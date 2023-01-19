62e9dd0ae1e96ad7c12ff94c0405b39c5ccf95ba
Jacob Burroughs <jburroughs@instructure.com>
2023-01-19 16:28:17 -0600

    Unbreak loading session secret keys
    This re-adds a require_dependency to keep the change small; we should
    do a followon change to remove the require_dependency.
    
    (fixes issue introduced in 618a6e702efbc8f1d16c12170d33ba00df3f6723)
    
    Change-Id: Ib77c904d432cc7b6abaa230dc1b0ea7cfcc3982e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/309211
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    Reviewed-by: Ethan Fox <ethan.fox@instructure.com>
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -19,6 +19,8 @@
 
 # Be sure to restart your server when you modify this file.
 
+require_dependency "setting"
+
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random,
