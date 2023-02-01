693d22add3bd83792239337e617fc5504b9aa404
Jason L Perry <jason.perry@instructure.com>
2023-02-01 11:01:31 -0500

    Update SIS Imports UI to expire error report URL after 1 hour
    In FOO-3152 we limited the duration of the sis error report url to 1
    hour, now we do the same for the UI. This will prevent a user from
    copying and pasting the link into a dangerous place, where it could be
    used to access the report in perpetuity.
    
    This also updates the previous API change to use a shared helper that
    generates the verifier token in the same way for both the API and UI.
    
    flag=none
    Closes FOO-3293
    
    Test Plan:
     - Generate a SIS import with errors (e.g., bad CSV).
     - Copy the URL from the download link in the UI.
     - Verify you can download the file without a browser session.
     - Go have lunch.
     - Verify you cannot download the file without a browser session.
    
    Change-Id: I0e2b2007fbcaca6e0644a35a04adef6745bed891
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/310133
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jesse Poulos <jpoulos@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Jason Perry <jason.perry@instructure.com>

---
+++ app/helpers/sis_import_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module SisImportHelper
+  def sis_import_error_attachment_token(batch, user: @current_user)
+    verification = Attachments::Verification.new(batch.errors_attachment)
+    verification.verifier_for_user(user, expires: 1.hour.from_now)
+  end
+end
