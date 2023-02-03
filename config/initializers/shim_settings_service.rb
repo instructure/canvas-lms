bb7ed6e0948a27a5b89feb1a33fcb65a8db579ed
James Wingert <james.wingert@strongmind.com>
2023-02-03 15:04:28 -0700

    QTY-2067:Remove pipelineservice calls when in development
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>

--- config/initializers/shim_settings_service.rb
+++ config/initializers/shim_settings_service.rb
@@ -1,5 +1,5 @@
 SettingsService::AuthToken.authenticator = ::AccessToken
 
 if ENV['RAILS_ENV'] == 'development'
-  SettingsService.update_settings(id: 1, value: 'false', setting: 'disable_pipeline', object: 'school')
+  SettingsService.update_settings(id: 1, value: 'true', setting: 'disable_pipeline', object: 'school')
 end
