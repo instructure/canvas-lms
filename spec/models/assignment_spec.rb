5f5b4e32e1b0a6009a05b92c58cf194b286c61a0
Chris Soto <christopher.soto@instructure.com>
2023-02-07 08:59:48 -0700

    fix grades badge count update on not_graded change
    currently with peer_reviews_for_a2, if assignment changes to/from
    not_graded, the badge count does not update.
    
    fixes EVAL-2863
    flag=assignments_2_student
    flag=peer_reviews_for_a2
    
    test plan:
    - as teacher, create an assignment with points
    - leave a comment on the assignment for a student
    - as student, verify the badge count is now 1
    - as teacher, change the submission_types (Display Grade as) for the
      assignment to "Not Graded"
    - as student, verify the badge count is now 0
    - as teacher, change the submission_types (Display Grade as) back to
      points
    - as student, verify the badge count is 1 again
    
    Change-Id: I0b9729b1373b2f47303783007e29e96f9109fb25
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/310590
    Reviewed-by: Derek Williams <derek.williams@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Jody Sailor
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -9282,6 +9282,45 @@ describe Assignment do
             expect(assignment).to_not receive(:refresh_course_content_participation_counts)
           end
         end
+
+        context "when changing submission_types for an assignment" do
+          before do
+            Account.site_admin.enable_feature!(:visibility_feedback_student_grades_page)
+          end
+
+          it "unread count changes between 0 and 1 when going to not_graded and any other submission_type" do
+            assignment.grade_student(student1, grade: 10, grader: teacher)
+            expect(student_unread_count_counts).to eq 1
+            assignment.submission_types = "not_graded"
+            assignment.save!
+            run_jobs
+            expect(student_unread_count_counts).to eq 0
+            assignment.submission_types = "online_text_entry"
+            assignment.save!
+            run_jobs
+            expect(student_unread_count_counts).to eq 1
+          end
+
+          it "does call refresh_course_content_participation_counts when changing to a not_graded submission_type" do
+            expect(assignment).to receive(:refresh_course_content_participation_counts).twice
+            assignment.submission_types = "not_graded"
+            assignment.save!
+            assignment.submission_types = "online_text_entry"
+            assignment.save!
+          end
+
+          it "does not call refresh_course_content_participation_counts when not changing to something other than not_graded" do
+            assignment.submission_types = "on_paper"
+            assignment.save!
+            expect(assignment).to_not receive(:refresh_course_content_participation_counts)
+          end
+
+          it "does not call refresh_course_content_participation_counts when changing something other than submission_types" do
+            assignment.title = "New Title"
+            assignment.save!
+            expect(assignment).to_not receive(:refresh_course_content_participation_counts)
+          end
+        end
       end
 
       describe "grade change audit records" do
