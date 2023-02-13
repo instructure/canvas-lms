d3d659daef59fd826f140b62c60a95899526cf28
Juan Chavez <juan.chavez@instructure.com>
2023-02-13 17:45:12 -0400

    Fix language selector dropdown in media player
    flag=none
    
    fixes MAT-1163
    
    test plan:
    - Navigate to a RCE instance.
    - Upload a video to user/course files.
    - Preview video.
    - Click "CC" and "Upload subtitles".
    > Click languages dropdown and verify that the dropdown
    is filled with language items.
    - Select a language and attach a SRT file.
    - Click "Upload".
    > Verify that there is no console errors.
    - Re-open video preview.
    > Verify that when opening "CC" the previously submitted
    subtitles is listed.
    
    Change-Id: Ic4f11902054050ebf15c0593e8b6c2fe079ec862
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/311001
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Juan Chavez <juan.chavez@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/shared/mediaelement/mep-feature-tracks-instructure.js
+++ ui/shared/mediaelement/mep-feature-tracks-instructure.js
@@ -31,7 +31,7 @@
 /* eslint-disable import/no-cycle, @typescript-eslint/no-unused-vars, no-var, vars-on-top */
 
 import {useScope as useI18nScope} from '@canvas/i18n'
-import closedCaptionLanguages from '@instructure/canvas-media'
+import {closedCaptionLanguages} from '@instructure/canvas-media'
 
 const I18n = useI18nScope('mepfeaturetracksinstructure')
 
@@ -767,7 +767,7 @@ const I18n = useI18nScope('mepfeaturetracksinstructure')
   })
 
   mejs.language = {
-    codes: closedCaptionLanguages,
+    codes: closedCaptionLanguages.reduce((result, {id, label}) => ({...result, [id]: label}), {}),
   }
 
   /*
