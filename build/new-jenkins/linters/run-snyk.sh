1017c1469ea859395471abef608c1bac54680a30
Andrea Cirulli <andrea.cirulli@instructure.com>
2023-02-13 19:33:07 +0000

    Revert "pin snyk to 1.1100.0"
    [change-merged]
    [build-registry-path=jenkins/canvas-lms/snyk1103]
    
    This reverts commit 830112257c37e1843941e669af3b847e4c11ca0a.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I9f0cae8f6adf0a2d834f2c97d9fb5cb1c7701c88
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/310317
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    QA-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Product-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Build-Review: Andrea Cirulli <andrea.cirulli@instructure.com>

--- build/new-jenkins/linters/run-snyk.sh
+++ build/new-jenkins/linters/run-snyk.sh
@@ -2,14 +2,12 @@
 
 set -ex
 
-SNYK_PINNED_VERSION="1.1100.0"
-
 for TEST_FILE in Gemfile.*.lock; do
   echo "checking $TEST_FILE with snyk"
 
-  npx snyk@$SNYK_PINNED_VERSION auth $SNYK_TOKEN
-  npx snyk@$SNYK_PINNED_VERSION test --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems || true
-  npx snyk@$SNYK_PINNED_VERSION monitor --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems
+  npx snyk auth $SNYK_TOKEN
+  npx snyk test --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems || true
+  npx snyk monitor --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems
 done
 
 if [[ -z "$TEST_FILE" ]]; then
