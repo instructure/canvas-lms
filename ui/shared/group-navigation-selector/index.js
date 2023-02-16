db69ee4edd432ad27c2daa46c4050a55f4adacd9
Drake Harper <drake.harper@instructure.com>
2023-02-16 10:50:13 -0700

    Fix group select navigation
    fixes VICE-3352
    flag=none
    
    Test Plan:
     - go to a group home page in a course
     - should be able to change groups
     - test on group home page and announcements
    
    Change-Id: I09dd0bd71ebadd0bfac2d2f50de6f2733571ce40
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/311381
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

--- ui/shared/group-navigation-selector/index.js
+++ ui/shared/group-navigation-selector/index.js
@@ -69,12 +69,15 @@ export const GroupNavigationSelector = props => {
     setSelectedOptionId(id)
     setInputValue(option)
     setIsShowingOptions(false)
-    window.location = id
+    const path = window.location.pathname.split('/')
+    path[2] = id
+    window.location = path.join('/')
   }
 
   return (
     <View as="div" padding="0 0 medium 0">
       <Select
+        data-testid="group-selector"
         renderLabel={I18n.t('Select Group')}
         assistiveText={I18n.t('Use arrow keys to navigate options.')}
         inputValue={inputValue}
