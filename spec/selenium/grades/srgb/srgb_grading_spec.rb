82941fb2160254cf27e6ae9c13b81e62979ca652
Kai Bjorkman <kbjorkman@instructure.com>
2023-02-16 09:27:39 -0700

    spec: selenium coverage of srgb proxy submissions
    closes EVAL-2906
    flag=proxy_file_uploads
    
    Test Plan:
        - specs pass
    
    Change-Id: If265a28048944d27ad880321b6c04d74948f3bba
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/311369
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

--- spec/selenium/grades/srgb/srgb_grading_spec.rb
+++ spec/selenium/grades/srgb/srgb_grading_spec.rb
@@ -20,6 +20,7 @@
 require_relative "../../helpers/gradebook_common"
 require_relative "../pages/srgb_page"
 require_relative "../setup/gradebook_setup"
+require_relative "../pages/gradebook_grade_detail_tray_page"
 
 describe "Screenreader Gradebook grading" do
   include_context "in-process server selenium tests"
@@ -45,26 +46,45 @@ describe "Screenreader Gradebook grading" do
     srgb_page.select_student(student)
   end
 
+  let(:proxy_submit) do
+    assignment_1.update!(submission_types: "online_upload")
+    file_attachment = attachment_model(content_type: "application/pdf", context: student)
+    submission = assignment_1.submit_homework(student, submission_type: "online_upload", attachments: [file_attachment])
+    teacher.update!(short_name: "Test Teacher")
+    submission.update!(proxy_submitter: teacher)
+  end
+
+  let(:proxy_permission) do
+    Account.site_admin.enable_feature!(:proxy_file_uploads)
+    teacher_role = Role.get_built_in_role("TeacherEnrollment", root_account_id: Account.default.id)
+    RoleOverride.create!(
+      permission: "proxy_assignment_submission",
+      enabled: true,
+      role: teacher_role,
+      account: @course.root_account
+    )
+  end
+
   context "in Grades section" do
     before do
       course_setup
       login_to_srgb
     end
 
-    it "displays correct Grade for label on assignments", prority: "1" do
+    it "displays correct Grade for label on assignments" do
       srgb_page.select_assignment(assignment_1)
 
       expect(srgb_page.grade_for_label).to include_text("Grade for User - Points Assignment")
     end
 
-    it "displays correct Grade for: label on next assignment", prority: "1" do
+    it "displays correct Grade for: label on next assignment" do
       srgb_page.select_assignment(assignment_1)
       srgb_page.next_assignment_button.click
 
       expect(srgb_page.grade_for_label).to include_text("Grade for User - Percent Assignment")
     end
 
-    it "displays correct points for graded by Points", priority: "1" do
+    it "displays correct points for graded by Points" do
       srgb_page.select_assignment(assignment_1)
       srgb_page.grade_srgb_assignment(srgb_page.main_grade_input, 8)
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -72,7 +92,7 @@ describe "Screenreader Gradebook grading" do
       expect(srgb_page.main_grade_input).to have_value("8")
     end
 
-    it "displays a flash message when an invalid grade is given and preserves the missing tag", priority: "1" do
+    it "displays a flash message when an invalid grade is given and preserves the missing tag" do
       srgb_page.select_assignment(assignment_2)
       srgb_page.grade_srgb_assignment(srgb_page.main_grade_input, "Invalid Grade")
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -81,7 +101,7 @@ describe "Screenreader Gradebook grading" do
       expect_flash_message :error, "Invalid Grade"
     end
 
-    it "does not grade student when tabing over the grade input without editing", priority: "1" do
+    it "does not grade student when tabing over the grade input without editing" do
       srgb_page.select_assignment(assignment_2)
       srgb_page.main_grade_input.click
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -100,7 +120,7 @@ describe "Screenreader Gradebook grading" do
       expect(srgb_page.main_grade_input).to have_value("Excused")
     end
 
-    it "displays correct points for graded by Percent", prority: "1" do
+    it "displays correct points for graded by Percent" do
       srgb_page.select_assignment(assignment_2)
       srgb_page.grade_srgb_assignment(srgb_page.main_grade_input, 8)
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -108,7 +128,7 @@ describe "Screenreader Gradebook grading" do
       expect(srgb_page.main_grade_input).to have_value("80%")
     end
 
-    it "displays correct points for graded by Complete/Incomplete", priority: "1" do
+    it "displays correct points for graded by Complete/Incomplete" do
       srgb_page.select_assignment(assignment_3)
       click_option("#student_and_assignment_grade", "Complete")
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -116,7 +136,7 @@ describe "Screenreader Gradebook grading" do
       expect(f("#grading div.ember-view")).to include_text("10 out of 10")
     end
 
-    it "displays correct points for graded by Letter Grade", prority: "1" do
+    it "displays correct points for graded by Letter Grade" do
       srgb_page.select_assignment(assignment_4)
       srgb_page.grade_srgb_assignment(srgb_page.main_grade_input, 8)
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -124,7 +144,7 @@ describe "Screenreader Gradebook grading" do
       expect(srgb_page.main_grade_input).to have_value("B-")
     end
 
-    it "displays submission details modal with correct grade", priority: "2" do
+    it "displays submission details modal with correct grade" do
       srgb_page.select_assignment(assignment_1)
       srgb_page.grade_srgb_assignment(srgb_page.main_grade_input, 8)
       srgb_page.tab_out_of_input(srgb_page.main_grade_input)
@@ -134,7 +154,7 @@ describe "Screenreader Gradebook grading" do
       expect(fj(".submission_details_grade_form input:visible")).to have_value("8")
     end
 
-    it "updates grade in submission details modal", priority: "2" do
+    it "updates grade in submission details modal" do
       skip("fragile")
       srgb_page.select_assignment(assignment_2)
       replace_content(srgb_page.main_grade_input, 8)
@@ -155,7 +175,7 @@ describe "Screenreader Gradebook grading" do
       course_setup
     end
 
-    it "on late submissions", priority: "2" do
+    it "on late submissions" do
       login_to_srgb
       srgb_page.select_assignment(assignment_1)
       expect(f(".late-pill")).to include_text("LATE")
@@ -163,14 +183,14 @@ describe "Screenreader Gradebook grading" do
       expect(f(".submission_final_grade")).to include_text("Final Grade")
     end
 
-    it "on missing submissions", priority: "2" do
+    it "on missing submissions" do
       assignment_1.submissions.find_by(user: student).update!(late_policy_status: "missing")
       login_to_srgb
       srgb_page.select_assignment(assignment_1)
       expect(f(".missing-pill")).to include_text("MISSING")
     end
 
-    it "on dropped assignments", priority: "2" do
+    it "on dropped assignments" do
       # create an assignment group with drop lowest 1 score rule
       srgb_page.drop_lowest(test_course, 1)
 
@@ -185,7 +205,7 @@ describe "Screenreader Gradebook grading" do
       expect(f(".dropped.muted")).to include_text("This grade is currently dropped for this student.")
     end
 
-    it "on resubmitted assignments", priority: "2" do
+    it "on resubmitted assignments" do
       # grade assignment
       assignment_1.grade_student(student, grade: 8, grader: teacher)
 
@@ -222,11 +242,10 @@ describe "Screenreader Gradebook grading" do
       user_session(@teacher)
     end
 
-    it "assignment in ended gp should be gradable", priority: "1" do
+    it "assignment in ended gp should be gradable" do
       assignment = @course.assignments.create!(due_at: 13.days.ago, title: "assign in ended")
       SRGB.visit(@course.id)
       SRGB.select_grading_period(@gp_ended.title)
-      SRGB.visit(@course.id) # TODO: delete this line when fixing CNVS-37376
       SRGB.select_student(@student)
       SRGB.select_assignment(assignment)
       SRGB.enter_grade(8)
@@ -235,16 +254,36 @@ describe "Screenreader Gradebook grading" do
       expect(Submission.where(assignment_id: assignment.id, user_id: @student.id).first.grade).to eq "8"
     end
 
-    it "assignment in closed gp should not be gradable", priority: "1" do
+    it "assignment in closed gp should not be gradable" do
       assignment = @course.assignments.create!(due_at: 18.days.ago, title: "assign in closed")
       SRGB.visit(@course.id)
       SRGB.select_grading_period(@gp_closed.title)
-      SRGB.visit(@course.id) # TODO: delete this line when fixing CNVS-37376
       SRGB.select_student(@student)
       SRGB.select_assignment(assignment)
 
       expect(SRGB.grading_enabled?).to be false
       expect(Submission.not_placeholder.where(assignment_id: assignment.id, user_id: @student.id).first).to eq nil
+    end
+  end
+
+  context "submit for student" do
+    before do
+      enroll_teacher_and_students
+      proxy_permission
+      assignment_1.update!(submission_types: "online_upload")
+    end
+
+    it "displays submit for student button for file upload assignments" do
+      login_to_srgb
+      srgb_page.select_assignment(assignment_1)
+      expect(srgb_page.submit_for_student_button).to include_text("Submit for Student")
+    end
+
+    it "displays the submitter's identity for proxy submissions" do
+      proxy_submit
+      login_to_srgb
+      srgb_page.select_assignment(assignment_1)
+      expect(srgb_page.proxy_submitter_label).to include_text("Submitted by Test Teacher")
     end
   end
 end
