6b1301052ddfd54b186d2726eb3bec973c7aefbf
Davis Hyer <dhyer@instructure.com>
2023-02-26 11:01:17 -0700

    remove unnecessary View warning supression
    refs LS-3831
    flag=none
    
    The test(s) previously causing this error have either been fixed or
    removed. This supression is no longer necessary.
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: If36027c881beacf18ed5f1de8a5c6094095fbe9b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312010
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -86,7 +86,7 @@ const ignoredErrors = [
 ]
 const globalWarn = global.console.warn
 const ignoredWarnings = [
-  /\[View|Text\] .* in version 8.0.0/i,
+  /\[Text\] .* in version 8.0.0/i,
   /Error getting \/media_objects\/dummy_media_id\/info/,
   /Exactly one focusable child is required/,
   /Please update the following components: %s/,
