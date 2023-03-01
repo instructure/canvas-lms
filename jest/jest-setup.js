011d8ae22cdf0ab0a314c75c33dcc4db041bca8e
Davis Hyer <dhyer@instructure.com>
2023-03-01 08:47:18 -0700

    remove unnecessary UrlEntry warning supression
    refs LS-3831
    flag=none
    
    The test(s) previously causing this error have either been fixed or
    removed. This supression is no longer necessary.
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: Idaff282b02b178b6ae181cddd5219817e0a2d6f5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312181
    Reviewed-by: Yona Appletree <yona.appletree@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -63,7 +63,6 @@ const ignoredErrors = [
   /Render methods should be a pure function of props and state/,
   /The 'screenReaderOnly' prop must be used in conjunction with 'liveRegion'/,
   /The above error occurred in the <.*> component/,
-  /The prop `focusOnInit` is marked as required in `(UrlEntry)`/,
   /The prop `id` is marked as required in `(ColHeader|FormField|Option)`/,
   /WARNING: heuristic fragment matching going on!/,
   /Warning: Failed prop type: Expected one of Checkbox in CheckboxGroup but found `View`/,
