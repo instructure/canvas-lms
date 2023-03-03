5f27150ba14ffadfbe59f3d39530c7fc9cd805b7
Davis Hyer <dhyer@instructure.com>
2023-03-03 12:41:55 -0700

    remove unnecessary heuristic warning supression
    refs LS-3831
    flag=none
    
    The test(s) previously causing this error have either been fixed or
    removed. This supression is no longer necessary.
    
    test plan:
      - tests pass
    
    qa risk: low
    
    Change-Id: I078b51df01866a44d3d2ffa322efa7cf987b4c72
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312437
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -62,7 +62,6 @@ const ignoredErrors = [
   /Render methods should be a pure function of props and state/,
   /The above error occurred in the <.*> component/,
   /The prop `id` is marked as required in `(Option)`/,
-  /WARNING: heuristic fragment matching going on!/,
   /Warning: Failed prop type: Expected one of Checkbox in CheckboxGroup but found `View`/,
   /You seem to have overlapping act\(\) calls/,
 ]
