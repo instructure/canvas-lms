1210638f26c7b1fe89495defed679776a57d755e
Davis Hyer <dhyer@instructure.com>
2023-03-07 19:40:26 -0700

    force clean test logs in canvas-planner
    flag=none
    
    test plan:
      - add a console warn/error to an existing component with tests
      - run tests
        - the tests should now fail with a message pointing you to the jest
          setup
      - add a regex to filter your message and rerun tests
        - tests should pass and your message should be filtered
    
    qa risk: low
    
    Change-Id: Id17a644d4e09aa087926099bdd8ae5c19066fe22
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312723
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

---
+++ packages/canvas-planner/jest/jest-setup.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * We want to ensure errors and warnings get appropriate eyes. If
+ * you are seeing an exception from here, it probably means you
+ * have an unintended consequence from your changes. If you expect
+ * the warning/error, add it to the ignore list below.
+ */
+/* eslint-disable no-console */
+const globalError = global.console.error
+const ignoredErrors = [
+  /A theme registry has already been initialized/,
+  /Each child in a list should have a unique "key" prop/,
+  /Invalid prop `isCompletelyEmpty`/,
+  /The prop `deregisterAnimatable` is marked as required/,
+  /The prop `registerAnimatable` is marked as required/,
+  /The prop `title` is marked as required/,
+  /The prop `toggleMissing` is marked as required/,
+  /The prop `uniqueId` is marked as required/,
+]
+const globalWarn = global.console.warn
+const ignoredWarnings = [
+  /\[(Popover|DateInput|Select|TimeInput|DateTimeInput)\] is deprecated/,
+  /\[Focusable\] Exactly one focusable child is required/,
+  /\[TextInput\] `(label|required|inline)` is deprecated/,
+  /componentWillReceiveProps has been renamed, and is not recommended for use/,
+]
+global.console = {
+  log: console.log,
+  error: error => {
+    if (ignoredErrors.some(regex => regex.test(error))) {
+      return
+    }
+    globalError(error)
+    throw new Error(
+      `Looks like you have an unhandled error. Keep our test logs clean by handling or filtering it. ${error}`
+    )
+  },
+  warn: warning => {
+    if (ignoredWarnings.some(regex => regex.test(warning))) {
+      return
+    }
+    globalWarn(warning)
+    throw new Error(
+      `Looks like you have an unhandled warning. Keep our test logs clean by handling or filtering it. ${warning}`
+    )
+  },
+  info: console.info,
+  debug: console.debug,
+}
+/* eslint-enable no-console */
