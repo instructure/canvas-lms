6f92cb673d5bd2703d8400c08ebf8ea4210fb88d
Davis Hyer <dhyer@instructure.com>
2023-03-07 19:40:26 -0700

    force clean test logs in canvas-planner
    flag=none
    
    test plan:
      - add a console warn/error to an existing component with tests
      - run tests
        - the tests should now fail with a message pointing you to the jest
          setup
      - add a regex to filter your message and rerun tests
        - tests should pass and your message should be filtered
    
    qa risk: low
    
    Change-Id: Id17a644d4e09aa087926099bdd8ae5c19066fe22
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312723
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

--- packages/canvas-planner/jest.config.js
+++ packages/canvas-planner/jest.config.js
@@ -18,8 +18,8 @@
 
 module.exports = {
   transform: {
-    '^.+\\.(js)$': ['babel-jest', { configFile: require.resolve('./babel.config.cjs.js') }],
-    '^.+\\.(css)$': '<rootDir>/jest-themeable-styles'
+    '^.+\\.(js)$': ['babel-jest', {configFile: require.resolve('./babel.config.cjs.js')}],
+    '^.+\\.(css)$': '<rootDir>/jest-themeable-styles',
   },
   reporters: [
     'default',
@@ -28,12 +28,12 @@ module.exports = {
       {
         suiteName: 'Canvas Planner Jest Tests',
         outputDirectory: process.env.TEST_RESULT_OUTPUT_DIR || './coverage',
-        outputName: 'canvas-planner-junit.xml'
-      }
-    ]
+        outputName: 'canvas-planner-junit.xml',
+      },
+    ],
   ],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFiles: ['jest-canvas-mock', './jest-env.js'],
+  setupFiles: ['jest-canvas-mock', './jest-env.js', '<rootDir>/jest/jest-setup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
   testRegex: '/__tests__/.*\\.(test|spec)\\.js$',
@@ -45,8 +45,8 @@ module.exports = {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
+      statements: 80,
+    },
   },
-  testEnvironment: '<rootDir>../../jest/strictTimeLimitEnvironment.js'
+  testEnvironment: '<rootDir>../../jest/strictTimeLimitEnvironment.js',
 }
