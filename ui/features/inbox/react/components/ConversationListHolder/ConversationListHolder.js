e44557f76e26adc82168c4a754ba4e5d05213e1e
Jason Gillett <jason.gillett@instructure.com>
2023-03-08 11:24:49 -0700

    Fix memoization to stop re-rendering
    Refs VICE-3340
    flag=react_inbox
    
    Test Plan
    1. Open up Dev tools. Show re-renders
    1a. have react dev tools extension
    1b. go to the profiler
    1c. click settings and select "show rerender"
    2. Open conversations
    3. scroll to the bottom to load more conversations
    4. Watch the re-rendering of the already rendered conversations
    5. verify that the inner content is not re-rendered.
    
    Change-Id: Idb3b4b2534882f7a7f30d8ebff8609085bda4e1e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312785
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/inbox/react/components/ConversationListHolder/ConversationListHolder.js
+++ ui/features/inbox/react/components/ConversationListHolder/ConversationListHolder.js
@@ -208,28 +208,21 @@ export const ConversationListHolder = ({
   // Render individual menu items
   const renderMenuItem = (conversation, isLast) => {
     return (
-      <View
-        key={`conversation-${conversation._id}`}
-        elementRef={el => {
-          if (isLast) {
-            onItemRefSet(el)
-          }
-        }}
-      >
-        <ConversationListItem
-          id={conversation._id}
-          conversation={conversation}
-          isStarred={conversation?.label === 'starred'}
-          isSelected={selectedMessages.includes(conversation._id)}
-          isUnread={conversation?.workflowState === 'unread'}
-          onSelect={handleItemSelection}
-          onStar={props.onStar}
-          key={conversation._id}
-          onMarkAsRead={props.onMarkAsRead}
-          onMarkAsUnread={props.onMarkAsUnread}
-          textSize={props.textSize}
-        />
-      </View>
+      <ConversationListItem
+        id={conversation._id}
+        conversation={conversation}
+        isStarred={conversation?.label === 'starred'}
+        isSelected={selectedMessages.includes(conversation._id)}
+        isUnread={conversation?.workflowState === 'unread'}
+        onSelect={handleItemSelection}
+        onStar={props.onStar}
+        key={conversation._id}
+        onMarkAsRead={props.onMarkAsRead}
+        onMarkAsUnread={props.onMarkAsUnread}
+        textSize={props.textSize}
+        isLast={isLast}
+        setRef={onItemRefSet}
+      />
     )
   }
 
