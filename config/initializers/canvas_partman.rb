6ade82032538176ab16a3b8fd6e894883142e1d7
Jacob Burroughs <jburroughs@instructure.com>
2023-03-08 07:34:50 -0600

    Revert "Revert "Introduce guardrails for excessive db modifications""
    This reverts commit d2f0962da9cf7dee3cae06286c5b35fecda7e49e.
    
    Fixed now to not break jenkins
    
    [build-registry-path=jenkins/canvas-lms/jburroughs-test]
    [change-merged]
    
    Change-Id: I9e55a234000043dce4e296952279e275bbb281a3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/312747
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>

--- config/initializers/canvas_partman.rb
+++ config/initializers/canvas_partman.rb
@@ -17,3 +17,4 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 CanvasPartman.timeout_seconds = -> { Setting.get("partman_timeout_seconds", "30").to_i }
+CanvasPartman.after_create_callback = ->(parent_class, table) { parent_class.connection.add_guard_excessive_updates(table) }
