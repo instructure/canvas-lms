f2f15eb1b9430ccf79d943d8fa29257db02d91b4
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-03-11 19:28:02 -0700

    Prevent duplicate Gradebook filter presets
    This prevent both duplicate preset names and preset filter conditions.
    
    Test plan:
      - Create a filter preset
      - Try to create another filter preset with the same name
        - Verify that the new preset is not created
    - Try to create another filter preset with the same filter conditions.
        - Verify that the new preset is not created
      - Edit an existing filter preset
        - Try to give it the same name as another existing preset
          - Verify that the filter preset is not saved
        - Try to give it the same filter conditions as another existing
          preset
          - Verify that the filter preset is not saved
    
    Closes EVAL-2616
    
    flag=enhanced_gradebook_filters
    
    Change-Id: Ie84b2adb31214d3d928c7edce17bad8698a8aef9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/313045
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cameron Ray <cameron.ray@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>

--- ui/features/gradebook/react/default_gradebook/components/FilterTray.tsx
+++ ui/features/gradebook/react/default_gradebook/components/FilterTray.tsx
@@ -57,6 +57,7 @@ export default function FilterTray({
   studentGroupCategories,
 }: FilterTrayProps) {
   const saveStagedFilter = useStore(state => state.saveStagedFilter)
+  const stagedFilterPresetName = useStore(state => state.stagedFilterPresetName)
   const updateFilterPreset = useStore(state => state.updateFilterPreset)
   const deleteFilterPreset = useStore(state => state.deleteFilterPreset)
   const applyFilters = useStore(state => state.applyFilters)
@@ -124,7 +125,7 @@ export default function FilterTray({
             applyFilters={applyFilters}
             assignmentGroups={assignmentGroups}
             filterPreset={{
-              name: '',
+              name: stagedFilterPresetName,
               filters: appliedFilters,
               created_at: new Date().toISOString(),
               updated_at: new Date().toISOString(),
@@ -133,8 +134,15 @@ export default function FilterTray({
             gradingPeriods={gradingPeriods}
             modules={modules}
             onCreate={(filterPreset: PartialFilterPreset) => {
-              saveStagedFilter(filterPreset)
               setExpandedFilterPresetId(null)
+              return saveStagedFilter(filterPreset).then(success => {
+                if (success) {
+                  setExpandedFilterPresetId(null)
+                } else {
+                  setExpandedFilterPresetId('new')
+                }
+                return success
+              })
             }}
             onToggle={() =>
               setExpandedFilterPresetId(expandedFilterPresetId === 'new' ? null : 'new')
@@ -152,17 +160,27 @@ export default function FilterTray({
                 applyFilters={applyFilters}
                 assignmentGroups={assignmentGroups}
                 filterPreset={filterPreset}
-                isActive={doFiltersMatch(appliedFilters, filterPreset.filters)}
                 gradingPeriods={gradingPeriods}
+                isActive={doFiltersMatch(appliedFilters, filterPreset.filters)}
+                isExpanded={expandedFilterPresetId === filterPreset.id}
                 modules={modules}
-                onUpdate={updateFilterPreset}
                 onDelete={() => deleteFilterPreset(filterPreset)}
                 onToggle={() =>
                   setExpandedFilterPresetId(
                     expandedFilterPresetId === filterPreset.id ? null : filterPreset.id
                   )
                 }
-                isExpanded={expandedFilterPresetId === filterPreset.id}
+                onUpdate={updatedFilterPreset => {
+                  setExpandedFilterPresetId(null)
+                  return updateFilterPreset(updatedFilterPreset).then(success => {
+                    if (success) {
+                      setExpandedFilterPresetId(null)
+                    } else {
+                      setExpandedFilterPresetId(updatedFilterPreset.id)
+                    }
+                    return success
+                  })
+                }}
                 sections={sections}
                 studentGroupCategories={studentGroupCategories}
               />
