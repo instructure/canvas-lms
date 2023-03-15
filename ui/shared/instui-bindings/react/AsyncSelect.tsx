39c3aeacf9bc37535587c55f0e05363c4773dcc3
Jonathan Guardado <jonathan.guardado@instructure.com>
2023-03-15 12:46:25 -0600

    Refactor ObsererOptions to use valid children
    Use a callback to notify the observer picker when the highlighted
    option changes, so we no longer need to use a function as a child
    to access this value.
    
    refs LS-3837
    flag=none
    
    test plan:
    - Tests pass
    - Expect the observer picker to behave and look as usual in normal
    canvas and k5.
    - Expect not to see errors associated with the observer picker in the
    console
    
    Change-Id: Ic205828bed4882f27ff874b3eef28fd043b19b6d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/313385
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Davis Hyer <dhyer@instructure.com>
    QA-Review: Davis Hyer <dhyer@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

--- ui/shared/instui-bindings/react/AsyncSelect.tsx
+++ ui/shared/instui-bindings/react/AsyncSelect.tsx
@@ -17,7 +17,7 @@
  */
 
 import {useScope as useI18nScope} from '@canvas/i18n'
-import React, {useState, useRef, ReactElement, ReactNode, ChangeEvent} from 'react'
+import React, {useState, useRef, ReactElement, ReactNode, ChangeEvent, useEffect} from 'react'
 import {Alert} from '@instructure/ui-alerts'
 import {Select} from '@instructure/ui-select'
 import {Spinner} from '@instructure/ui-spinner'
@@ -40,6 +40,7 @@ type Props = {
   noOptionsValue?: string
   renderLabel?: string | ReactNode
   onOptionSelected: (event, optionId: string) => void
+  onHighlightedOptionChange?: (optionId: string | null) => void
   onInputChange: (event, value) => void
   onBlur?: (event) => void
   onFocus?: (event) => void
@@ -61,6 +62,7 @@ export default function CanvasAsyncSelect({
   noOptionsLabel = '---',
   noOptionsValue = '',
   onOptionSelected = () => {},
+  onHighlightedOptionChange = () => {},
   onInputChange = () => {},
   onFocus = () => {},
   onBlur = () => {},
@@ -76,6 +78,11 @@ export default function CanvasAsyncSelect({
   const [announcement, setAnnouncement] = useState<ReactNode>('')
   const [hasFocus, setHasFocus] = useState(false)
   const [managedInputValue, setManagedInputValue] = useState('')
+
+  useEffect(() => {
+    onHighlightedOptionChange(highlightedOptionId)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [highlightedOptionId])
 
   function findOptionById(id: string): ReactElement {
     let option
