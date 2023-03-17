8e58b71ca14636b9b0cf3b1bbbd7c1f810b10f90
Jacob Burroughs <jburroughs@instructure.com>
2023-03-17 16:41:07 -0500

    Send postgres warnings to sentry
    And make the errors from guard_excessive_updates likely group
    better for sentry's purposes
    
    refs AE-149
    
    Change-Id: Iea04ebdbb96653b82e1eed7026697fe8419f6879
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/313635
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>

---
+++ db/migrate/20230317213850_better_guard_logs.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+class BetterGuardLogs < ActiveRecord::Migration[7.0]
+  tag :postdeploy
+
+  def up
+    execute(<<~SQL.squish)
+      CREATE OR REPLACE FUNCTION #{connection.quote_table_name("guard_excessive_updates")}() RETURNS TRIGGER AS $BODY$
+      DECLARE
+          record_count integer;
+          max_record_count integer;
+      BEGIN
+          SELECT count(*) FROM oldtbl INTO record_count;
+          max_record_count := COALESCE(setting_as_int('inst.max_update_limit.' || TG_TABLE_NAME), setting_as_int('inst.max_update_limit'), '1000');
+          IF record_count > max_record_count THEN
+            IF current_setting('inst.max_update_fail', true) IS NOT DISTINCT FROM 'true' THEN
+                RAISE EXCEPTION 'guard_excessive_updates: % to %.% failed', TG_OP, TG_TABLE_SCHEMA, TG_TABLE_NAME USING DETAIL = 'Would update ' || record_count || ' records but max is ' || max_record_count;
+            ELSE
+                RAISE WARNING 'guard_excessive_updates: % to %.% was dangerous', TG_OP, TG_TABLE_SCHEMA, TG_TABLE_NAME USING DETAIL = 'Updated ' || record_count || ' records but threshold is ' || max_record_count;
+            END IF;
+          END IF;
+          RETURN NULL;
+      END
+      $BODY$ LANGUAGE plpgsql;
+    SQL
+    set_search_path("guard_excessive_updates")
+  end
+
+  def down
+    execute(<<~SQL.squish)
+      CREATE OR REPLACE FUNCTION #{connection.quote_table_name("guard_excessive_updates")}() RETURNS TRIGGER AS $BODY$
+      DECLARE
+          record_count integer;
+          max_record_count integer;
+      BEGIN
+          SELECT count(*) FROM oldtbl INTO record_count;
+          max_record_count := COALESCE(setting_as_int('inst.max_update_limit.' || TG_TABLE_NAME), setting_as_int('inst.max_update_limit'), '1000');
+          IF record_count > max_record_count THEN
+              IF current_setting('inst.max_update_fail', true) IS NOT DISTINCT FROM 'true' THEN
+                  RAISE EXCEPTION 'guard_excessive_updates: % to %.% failed. Would update % records but max is %', TG_OP, TG_TABLE_SCHEMA, TG_TABLE_NAME, record_count, max_record_count;
+              ELSE
+                  RAISE WARNING 'guard_excessive_updates: % to %.% was dangerous. Updated % records but threshold is %', TG_OP, TG_TABLE_SCHEMA, TG_TABLE_NAME, record_count, max_record_count;
+              END IF;
+          END IF;
+          RETURN NULL;
+      END
+      $BODY$ LANGUAGE plpgsql;
+    SQL
+    set_search_path("guard_excessive_updates")
+  end
+end
