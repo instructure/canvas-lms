27b570549e33055412b566a3ec57484bb2b364e3
Angel Diaz <angel.diaz@strongmind.com>
2023-04-06 15:56:36 -0700

    QTY-2398: do not verify ssl when talking to slack

--- app/controllers/login/oauth_base_controller.rb
+++ app/controllers/login/oauth_base_controller.rb
@@ -81,15 +81,15 @@ class Login::OauthBaseController < ApplicationController
       pseudonym = Pseudonym.active.find_by(integration_id: unique_ids.first)
       HTTParty.post(
         SettingsService.get_settings(object: "school", id: 1)["slack_api_url"],
-        :verify => false,
         headers: {
           "Content-Type": "application"
         },
         body: {
-          'username': SettingsService.get_settings(object: "school", id: 1)["slack_api_user"], 
+          'username': SettingsService.get_settings(object: "school", id: 1)["slack_api_user"],
           'text': "#{unique_ids.first} did not match an integration id. Provider attributes: " +
                   "#{provider_attributes.map do |k, v| "#{k}: #{v}" end.join(", ")}"
-        }.to_json
+        }.to_json,
+        verify: false
       ) unless pseudonym || provider_attributes["is_admin"]
     end
 
