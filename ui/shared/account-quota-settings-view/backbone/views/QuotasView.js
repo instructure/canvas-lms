d8e365bfc7ecc8cc18ea8c7ee57af20fd01360d3
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-04-13 14:43:20 -0600

    decaf ui/shared (3)
    test plan:
      - existing tests pass
    
    flag=none
    
    refs FOO-3470
    
    Change-Id: Ia1030be3812ecb1c6d0a8d07b22a55cdb84cc1b9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/315992
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/shared/account-quota-settings-view/backbone/views/QuotasView.js
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* eslint-disable no-void */
+
+import {extend} from '@canvas/backbone/utils'
+import $ from 'jquery'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import ValidatedFormView from '@canvas/forms/backbone/views/ValidatedFormView'
+import htmlEscape from 'html-escape'
+import template from '../../jst/Quotas.handlebars'
+import '@canvas/rails-flash-notifications'
+
+const I18n = useI18nScope('accounts')
+
+extend(QuotasView, ValidatedFormView)
+
+function QuotasView() {
+  return QuotasView.__super__.constructor.apply(this, arguments)
+}
+
+QuotasView.prototype.template = template
+
+QuotasView.INTEGER_REGEX = /^[+-]?\d+$/
+
+QuotasView.prototype.tag = 'form'
+
+QuotasView.prototype.id = 'default-quotas'
+
+QuotasView.prototype.className = 'form-horizontal account_settings'
+
+QuotasView.prototype.integerFields = [
+  'default_storage_quota_mb',
+  'default_user_storage_quota_mb',
+  'default_group_storage_quota_mb',
+]
+
+QuotasView.prototype.events = {
+  submit: 'submit',
+}
+
+QuotasView.prototype.initialize = function () {
+  this.events || (this.events = [])
+  this.els || (this.els = [])
+  const ref = this.integerFields
+  for (let i = 0, len = ref.length; i < len; i++) {
+    const integerField = ref[i]
+    this.events['input [name="' + integerField + '"]'] = 'validate'
+    this.els['[name="' + integerField + '"]'] = '$' + integerField
+  }
+  this.on('success', this.submitSuccess)
+  this.on('fail', this.submitFail)
+  return QuotasView.__super__.initialize.apply(this, arguments)
+}
+
+QuotasView.prototype.toJSON = function () {
+  const data = QuotasView.__super__.toJSON.apply(this, arguments)
+  data.root_account = this.model.get('root_account')
+  return data
+}
+
+QuotasView.prototype.submitSuccess = function () {
+  return $.flashMessage(I18n.t('default_account_quotas_updated', 'Default account quotas updated'))
+}
+
+QuotasView.prototype.submitFail = function (errors) {
+  let unknownFailure = true
+  const ref = this.integerFields
+  for (let i = 0, len = ref.length; i < len; i++) {
+    const integerField = ref[i]
+    if (integerField in errors) {
+      unknownFailure = false
+    }
+  }
+  if (unknownFailure) {
+    return $.flashError(
+      I18n.t('default_account_quotas_not_updated', 'Default account quotas were not updated')
+    )
+  }
+}
+
+QuotasView.prototype.validateFormData = function (data) {
+  const errors = {}
+  const ref = this.integerFields
+  for (let i = 0, len = ref.length; i < len; i++) {
+    const integerField = ref[i]
+    if (typeof data[integerField] !== 'undefined') {
+      if (!data[integerField].match(this.constructor.INTEGER_REGEX)) {
+        errors[integerField] = [
+          {
+            type: 'integer_required',
+            message: I18n.t('integer_required', 'An integer value is required'),
+          },
+        ]
+      }
+    }
+  }
+  return errors
+}
+
+QuotasView.prototype.validateBeforeSave = function () {
+  return {}
+}
+
+QuotasView.prototype.hideErrors = function () {
+  const control_groups = this.$('div.control-group.error')
+  control_groups.removeClass('error')
+  return control_groups.find('.help-inline').remove()
+}
+
+QuotasView.prototype.showErrors = function (errors) {
+  const ref = this.integerFields
+  const results = []
+  for (let i = 0, len = ref.length; i < len; i++) {
+    const integerField = ref[i]
+    const control_group = this['$' + integerField].closest('div.control-group')
+    const messages = errors[integerField]
+    control_group.toggleClass('error', messages != null)
+    let html
+    if (messages) {
+      const $helpInline = $('<span class="help-inline"></span>')
+      html = (function () {
+        const results1 = []
+        for (let j = 0, len1 = messages.length; j < len1; j++) {
+          const message = messages[j].message
+          results1.push(htmlEscape(message))
+        }
+        return results1
+      })().join('<br/>')
+      $helpInline.html(html)
+      results.push(control_group.find('.controls').append($helpInline))
+    } else {
+      results.push(void 0)
+    }
+  }
+  return results
+}
+
+export default QuotasView
---
+++ ui/shared/external-tools/backbone/models/ExternalTool.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {extend} from '@canvas/backbone/utils'
+import _ from 'underscore'
+import {Model} from '@canvas/backbone'
+import DefaultUrlMixin from '@canvas/backbone/DefaultUrlMixin'
+
+extend(ExternalTool, Model)
+
+function ExternalTool() {
+  return ExternalTool.__super__.constructor.apply(this, arguments)
+}
+
+ExternalTool.mixin(DefaultUrlMixin)
+
+ExternalTool.prototype.initialize = function () {
+  ExternalTool.__super__.initialize.apply(this, arguments)
+  if (_.has(this, 'url')) {
+    return delete this.url
+  }
+}
+
+ExternalTool.prototype.resourceName = 'external_tools'
+
+ExternalTool.prototype.computedAttributes = [
+  {
+    name: 'custom_fields_string',
+    deps: ['custom_fields'],
+  },
+]
+
+ExternalTool.prototype.urlRoot = function () {
+  return '/api/v1/' + this._contextPath() + '/create_tool_with_verification'
+}
+
+ExternalTool.prototype.custom_fields_string = function () {
+  let k, v
+  return function () {
+    const ref = this.get('custom_fields')
+    const results = []
+    for (k in ref) {
+      v = ref[k]
+      results.push(k + '=' + v)
+    }
+    return results
+  }
+    .call(this)
+    .join('\n')
+}
+
+ExternalTool.prototype.launchUrl = function (launchType, options) {
+  if (options == null) {
+    options = {}
+  }
+  const params = (function () {
+    const results = []
+    for (const key in options) {
+      const value = options[key]
+      results.push(key + '=' + value)
+    }
+    return results
+  })()
+  let url =
+    '/' +
+    this._contextPath() +
+    '/external_tools/' +
+    this.id +
+    '/resource_selection?launch_type=' +
+    launchType
+  if (params.length > 0) {
+    url = url + '&' + params.join('&')
+  }
+  return url
+}
+
+ExternalTool.prototype.assetString = function () {
+  return 'context_external_tool_' + this.id
+}
+
+export default ExternalTool
