66ddc00b2163f67f80ae1197c21c02ed098cb94a
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-04-12 21:21:03 -0600

    decaf small .coffee files (6)
    test plan:
      - existing tests pass
    
    flag=none
    
    refs FOO-3470
    
    Change-Id: I6e2f40e1b806942f5d5e737420fd319b914de009
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/315892
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

---
+++ ui/features/assignment_index/backbone/views/AssignmentGroupListView.js
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {extend} from '@canvas/backbone/utils'
+import $ from 'jquery'
+import _ from 'underscore'
+import SortableCollectionView from './SortableCollectionView'
+import AssignmentGroupListItemView from './AssignmentGroupListItemView'
+import template from '../../jst/AssignmentGroupList.handlebars'
+import NoAssignmentsListItem from '../../jst/NoAssignmentsListItem.handlebars'
+
+extend(AssignmentGroupListView, SortableCollectionView)
+
+function AssignmentGroupListView() {
+  this.expand = this.expand.bind(this)
+  this.collapse = this.collapse.bind(this)
+  this.handleExtraClick = this.handleExtraClick.bind(this)
+  this.renderOnReset = this.renderOnReset.bind(this)
+  this.renderIfLoaded = this.renderIfLoaded.bind(this)
+  this.renderItem = this.renderItem.bind(this)
+  this.render = this.render.bind(this)
+  return AssignmentGroupListView.__super__.constructor.apply(this, arguments)
+}
+
+AssignmentGroupListView.optionProperty('course')
+
+AssignmentGroupListView.optionProperty('userIsAdmin')
+
+AssignmentGroupListView.prototype.template = template
+
+AssignmentGroupListView.prototype.itemView = AssignmentGroupListItemView
+
+AssignmentGroupListView.optionProperty('assignment_sort_base_url')
+
+AssignmentGroupListView.prototype.render = function () {
+  return AssignmentGroupListView.__super__.render.call(this, ENV.PERMISSIONS.manage)
+}
+
+AssignmentGroupListView.prototype.renderItem = function (model) {
+  const view = AssignmentGroupListView.__super__.renderItem.apply(this, arguments)
+  model.groupView.collapseIfNeeded()
+  return view
+}
+
+AssignmentGroupListView.prototype.createItemView = function (model) {
+  const options = {
+    parentCollection: this.collection,
+    childKey: 'assignments',
+    groupKey: 'assignment_group_id',
+    groupId: model.id,
+    reorderURL: this.createReorderURL(model.id),
+    noItemTemplate: NoAssignmentsListItem,
+    userIsAdmin: this.userIsAdmin,
+  }
+  // eslint-disable-next-line new-cap
+  return new this.itemView(
+    $.extend(
+      {},
+      this.itemViewOptions || {},
+      {
+        model,
+      },
+      options
+    )
+  )
+}
+
+AssignmentGroupListView.prototype.createReorderURL = function (id) {
+  return this.assignment_sort_base_url + '/' + id + '/reorder'
+}
+
+// TODO: make menu a child view of listitem so that it can be rendered
+// by itself, and so it can manage all of the dialog stuff,
+AssignmentGroupListView.prototype.attachCollection = function () {
+  AssignmentGroupListView.__super__.attachCollection.apply(this, arguments)
+  this.itemViewOptions = {
+    course: this.course,
+  }
+  this.collection.on('render', this.render)
+  this.collection.on('add', this.renderIfLoaded)
+  this.collection.on('remove', this.render)
+  return this.collection.on('change:groupWeights', this.render)
+}
+
+AssignmentGroupListView.prototype.renderIfLoaded = function () {
+  if (this.collection.loadedAll) {
+    return this.render()
+  }
+}
+
+AssignmentGroupListView.prototype.renderOnReset = function () {
+  this.firstResetLanded = true
+  return AssignmentGroupListView.__super__.renderOnReset.apply(this, arguments)
+}
+
+AssignmentGroupListView.prototype.toJSON = function () {
+  const data = AssignmentGroupListView.__super__.toJSON.apply(this, arguments)
+  return _.extend({}, data, {
+    firstResetLanded: this.firstResetLanded,
+  })
+}
+
+AssignmentGroupListView.prototype._initSort = function () {
+  AssignmentGroupListView.__super__._initSort.call(this, {
+    handle: '.sortable-handle',
+  })
+  this.$list.on('sortstart', this.collapse)
+  return this.$list.on('sortstop', this.expand)
+}
+
+AssignmentGroupListView.prototype.handleExtraClick = function (e) {
+  e.stopImmediatePropagation()
+  // FF triggers an extra click when you drop the item, so we want to handle it here
+  return $(e.target).off('click', this.handleExtraClick)
+}
+
+AssignmentGroupListView.prototype.collapse = function (e, ui) {
+  const item = ui.item
+  const id = item.children(':first').attr('data-id')
+  item.find('#assignment_group_' + id + '_assignments').slideUp(100)
+  ui.item.css('height', 'auto')
+  const $toggler = item.find('.element_toggler').first()
+  const arrow = $toggler.find('i').first()
+  return arrow.removeClass('icon-mini-arrow-down').addClass('icon-mini-arrow-right')
+}
+
+AssignmentGroupListView.prototype.expand = function (e, ui) {
+  const item = ui.item
+  const $toggler = item.find('.element_toggler').first()
+  $toggler.on('click', this.handleExtraClick)
+  // remove the extra click handler for browsers that don't trigger the extra click
+  setTimeout(
+    (function (_this) {
+      return function () {
+        return $toggler.off('click', _this.handleExtraClick)
+      }
+    })(this),
+    50
+  )
+  const id = item.children(':first').attr('data-id')
+  const ag = this.collection.findWhere({
+    id,
+  })
+  if (ag && ag.groupView.shouldBeExpanded()) {
+    item.find('#assignment_group_' + id + '_assignments').slideDown(100)
+    const arrow = $toggler.find('i').first()
+    return arrow.addClass('icon-mini-arrow-down').removeClass('icon-mini-arrow-right')
+  }
+}
+
+export default AssignmentGroupListView
---
+++ ui/features/user_observees/backbone/views/UserObserveesView.js
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {extend} from '@canvas/backbone/utils'
+import $ from 'jquery'
+import _ from 'underscore'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import pairingCodeTemplate from '../../jst/PairingCodeUserObservees.handlebars'
+import itemView from './UserObserveeView'
+import PaginatedCollectionView from '@canvas/pagination/backbone/views/PaginatedCollectionView'
+import '@canvas/jquery/jquery.disableWhileLoading'
+import {clearObservedId, savedObservedId} from '@canvas/observer-picker/ObserverGetObservee'
+
+const I18n = useI18nScope('observees')
+
+extend(UserObserveesView, PaginatedCollectionView)
+
+function UserObserveesView() {
+  this.checkEmpty = this.checkEmpty.bind(this)
+  return UserObserveesView.__super__.constructor.apply(this, arguments)
+}
+
+UserObserveesView.prototype.autoFetch = true
+
+UserObserveesView.prototype.itemView = itemView
+
+UserObserveesView.prototype.className = 'user-observees'
+
+UserObserveesView.prototype.template = pairingCodeTemplate
+
+UserObserveesView.prototype.events = {
+  'submit .add-observee-form': 'addObservee',
+  'click .remove-observee': 'removeObservee',
+}
+
+UserObserveesView.prototype.els = _.extend({}, PaginatedCollectionView.prototype.els, {
+  '.add-observee-form': '$form',
+})
+
+UserObserveesView.prototype.initialize = function () {
+  UserObserveesView.__super__.initialize.apply(this, arguments)
+  this.collection.on(
+    'beforeFetch',
+    (function (_this) {
+      return function () {
+        return _this.setLoading(true)
+      }
+    })(this)
+  )
+  this.collection.on(
+    'fetch',
+    (function (_this) {
+      return function () {
+        return _this.setLoading(false)
+      }
+    })(this)
+  )
+  this.collection.on(
+    'fetched:last',
+    (function (_this) {
+      return function () {
+        return _this.checkEmpty()
+      }
+    })(this)
+  )
+  return this.collection.on(
+    'remove',
+    (function (_this) {
+      return function () {
+        return _this.checkEmpty()
+      }
+    })(this)
+  )
+}
+
+UserObserveesView.prototype.checkEmpty = function () {
+  if (this.collection.size() === 0) {
+    return $('<em>')
+      .text(I18n.t('No students being observed'))
+      .appendTo(this.$('.observees-list-container'))
+  }
+}
+
+UserObserveesView.prototype.addObservee = function (ev) {
+  ev.preventDefault()
+  const data = this.$form.getFormData()
+  const d = $.post(this.collection.url(), data)
+  d.done(
+    (function (_this) {
+      return function (model) {
+        if (model.redirect) {
+          if (
+            // eslint-disable-next-line no-alert
+            window.confirm(
+              I18n.t(
+                "In order to complete the process you will be redirected to a login page where you will need to log in with your child's credentials."
+              )
+            )
+          ) {
+            return (window.location = model.redirect)
+          }
+        } else {
+          _this.collection.add([model], {
+            merge: true,
+          })
+          $.flashMessage(
+            I18n.t('observee_added', 'Now observing %{user}', {
+              user: model.name,
+            })
+          )
+          _this.$form.get(0).reset()
+          return _this.focusForm()
+        }
+      }
+    })(this)
+  )
+  return d.error(
+    (function (_this) {
+      return function (response) {
+        _this.$form.formErrors(JSON.parse(response.responseText))
+        return _this.focusForm()
+      }
+    })(this)
+  )
+}
+
+UserObserveesView.prototype.removeObservee = function (ev) {
+  ev.preventDefault()
+  const id = '' + $(ev.target).data('user-id')
+  const user_name = $(ev.target).data('user-name')
+  if (
+    // eslint-disable-next-line no-alert
+    window.confirm(
+      I18n.t('Are you sure you want to stop observing %{name}?', {
+        name: user_name,
+      })
+    )
+  ) {
+    const current_observed_id = savedObservedId(ENV.current_user_id)
+    if (current_observed_id === id) {
+      clearObservedId(ENV.current_user_id)
+    }
+    return this.$form.disableWhileLoading(
+      $.ajaxJSON(
+        '/api/v1/users/self/observees/' + id,
+        'DELETE',
+        {},
+        (function (_this) {
+          return function () {
+            return _this.removedObservee(id, user_name)
+          }
+        })(this)
+      )
+    )
+  }
+}
+
+UserObserveesView.prototype.removedObservee = function (id, name) {
+  this.collection.remove(id)
+  return $.flashMessage(
+    I18n.t('No longer observing %{user}', {
+      user: name,
+    })
+  )
+}
+
+UserObserveesView.prototype.focusForm = function () {
+  let field = this.$form.find(":input[value='']:not(button)").first()
+  if (!field.length) {
+    field = this.$form.find(':input:not(button)')
+  }
+  return field.focus()
+}
+
+UserObserveesView.prototype.setLoading = function (loading) {
+  if (loading) {
+    this.$('.observees-list-container').attr('aria-busy', 'true')
+    return this.$('.loading-indicator').show()
+  } else {
+    this.$('.observees-list-container').attr('aria-busy', 'false')
+    return this.$('.loading-indicator').hide()
+  }
+}
+
+export default UserObserveesView
