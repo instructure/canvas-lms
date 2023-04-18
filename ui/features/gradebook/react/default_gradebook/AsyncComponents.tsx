bb964221b039b85d3860fe8af243bcc94c8f6f37
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-04-18 15:48:47 -0600

    decaf context_modules.coffee
    test plan:
      - existing tests pass
      - substantial comments are preserved
    
    flag=none
    
    refs FOO-3470
    
    Change-Id: I9ac692d1d4aba36d8bab9ae2f894b39db4ae8720
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/316297
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- ui/features/gradebook/react/default_gradebook/AsyncComponents.tsx
+++ ui/features/gradebook/react/default_gradebook/AsyncComponents.tsx
@@ -54,7 +54,6 @@ async function loadPostAssignmentGradesTray() {
 }
 
 async function loadSetDefaultGradeDialog() {
-  // @ts-expect-error
   return (await import('@canvas/grading/jquery/SetDefaultGradeDialog')).default
 }
 
