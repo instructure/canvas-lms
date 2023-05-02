5b09c6e5812245331b1c5230991d504365e2e48f
Ed Schiebel <eschiebel@instructure.com>
2023-05-02 08:41:54 -0600

    don't let users remove auto-subscribed calendars
    closes LS-3986
    flag=auto_subscribe_account_calendars
    
    test plan:
      - have a student in a couple courses in different sub-accounts
      - have one sub-acccount's calendar auto-subscribe (by setting
        account_calendar_subscription_type="auto" on the account)
      - open the calendar as the student
      > expect to see the auto-subscribed calendar in the sidebar
      - hover over that calendar
      > expect no 'x' to remove the calendar
      - click the + to add other calendar to open the Select Calendars
        modal
      > expect the auto-subscribed calendar's checkbox to be disabled
      - check the other account calendar's checkbox and Save Changes
      > expect both calendars in the sidebar
      - hover over each of them
      > expect an 'x' to delete the non-auto-subscribed calendar and
        no 'x' on the auto-subscribed calendar
    
    Change-Id: I7c22eb3a20f3cbfb0dbe04d540e31d585c0a111a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/317430
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/calendar/jquery/sidebar.js
+++ ui/features/calendar/jquery/sidebar.js
@@ -226,6 +226,7 @@ function convertAccountCalendars(accountCalendars) {
     name: oC.name,
     asset_string: oC.asset_string,
     type: oC.type,
+    auto_subscribe: oC.auto_subscribe,
   }))
 }
 
