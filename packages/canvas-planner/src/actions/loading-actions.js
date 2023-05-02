aad39d2e6b88afa0572be84d2acb138a8bb26d8a
Jonathan Guardado <jonathan.guardado@instructure.com>
2023-05-02 14:30:10 -0600

    Add account calendar events to the student planner
    - Include account calendar events in the api/v1/planner/item
    endpoint
    - Planner UI updates to support account contexts in classic and k5
    modes
    
    closes LS-3412
    flag= none
    
    Test plan:
    
    1- As admin, create a sub-account
    2- Create a course in that sub-account with an enrolled student
    3- Go to the root account -> Account Calendars page
    4- Check the sub-account and apply the changes
    5- Go to the calendar and create an event for that sub-account
    - In classic/normal canvas
    6- Login as the student you enrolled in the course
    7- Go to the dashboard and select the list view
    8- Expect to see the calendar event you just created
    9- Expect the event context to be the name of the account and the color
    to match the one you set in the calendar.
    10- As admin, hide the account in the root account -> Account Calendars
    page
    11- Login as the student again
    12- Go to the dashboard and expect not to see the account calendar
    event.
    - In K5
    13- As admin, enable the sub account again.
    14- As student, validate that the account calendar events appear in
    the k5 dashboard schedule tab
    15-As student, validate that the account calendar events don’t appear
    in the k5 course schedule tab
    16-Login as observer and validate steps 7, 8, 14 and 15
    
    Change-Id: I260c2c5978700153a24e59f95d65300cf902389c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/317461
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Allison Howell <allison.howell@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>

--- packages/canvas-planner/src/actions/loading-actions.js
+++ packages/canvas-planner/src/actions/loading-actions.js
@@ -24,7 +24,7 @@ import {
   transformApiToInternalItem,
   observedUserId,
   observedUserContextCodes,
-  buildURL
+  buildURL,
 } from '../utilities/apiUtils'
 import {alert} from '../utilities/alertUtils'
 import formatMessage from '../format-message'
@@ -57,7 +57,7 @@ export const {
   gotWayPastItemDate,
   gotWayFutureItemDate,
   gotCourseList,
-  clearLoading
+  clearLoading,
 } = createActions(
   'START_LOADING_ITEMS',
   'CONTINUE_LOADING_INITIAL_ITEMS',
@@ -118,13 +118,18 @@ export function getFirstNewActivityDate(fromMoment) {
     fromMoment = fromMoment.clone().subtract(6, 'months')
     const observed_user_id = observedUserId(getState())
     const context_codes = observedUserContextCodes(getState())
+    let include
+    if (observed_user_id && !getState().singleCourse) {
+      include = ['account_calendars']
+    }
 
     const url = buildURL('/api/v1/planner/items', {
       start_date: fromMoment.toISOString(),
+      include,
       filter: 'new_activity',
       order: 'asc',
       observed_user_id,
-      context_codes
+      context_codes,
     })
 
     const request = asAxios(getPrefetchedXHR(url)) || axios.get(url)
@@ -195,7 +200,7 @@ function loadPastItems(byScrolling) {
     if (byScrolling) dispatch(scrollIntoPastAction())
     dispatch(
       gettingPastItems({
-        seekingNewActivity: false
+        seekingNewActivity: false,
       })
     )
     dispatch(startLoadingPastSaga())
@@ -213,7 +218,7 @@ export function loadPastButtonClicked() {
 export const loadPastUntilNewActivity = () => dispatch => {
   dispatch(
     gettingPastItems({
-      seekingNewActivity: true
+      seekingNewActivity: true,
     })
   )
   dispatch(startLoadingPastUntilNewActivitySaga())
@@ -223,7 +228,7 @@ export const loadPastUntilNewActivity = () => dispatch => {
 export const loadPastUntilToday = () => dispatch => {
   dispatch(
     gettingPastItems({
-      seekingNewActivity: false
+      seekingNewActivity: false,
     })
   )
   dispatch(startLoadingPastUntilTodaySaga())
@@ -331,8 +336,12 @@ function getWayFutureItem(fromMoment) {
     const state = getState()
     const observed_user_id = observedUserId(state)
     let context_codes
+    let include
     if (observed_user_id) {
       context_codes = getContextCodesFromState(state)
+      if (!state.singleCourse) {
+        include = ['account_calendars']
+      }
     } else {
       context_codes = state.singleCourse ? getContextCodesFromState(state) : undefined
     }
@@ -341,8 +350,9 @@ function getWayFutureItem(fromMoment) {
       observed_user_id,
       context_codes,
       end_date: futureMoment.toISOString(),
+      include,
       order: 'desc',
-      per_page: 1
+      per_page: 1,
     })
     const request = asAxios(getPrefetchedXHR(url)) || axios.get(url)
 
@@ -362,8 +372,12 @@ function getWayPastItem(fromMoment) {
     const state = getState()
     const observed_user_id = observedUserId(state)
     let context_codes
+    let include
     if (observed_user_id) {
       context_codes = getContextCodesFromState(state)
+      if (!getState().singleCourse) {
+        include = ['account_calendars']
+      }
     } else {
       context_codes = state.singleCourse ? getContextCodesFromState(state) : undefined
     }
@@ -372,8 +386,9 @@ function getWayPastItem(fromMoment) {
       observed_user_id,
       context_codes,
       start_date: pastMoment.toISOString(),
+      include,
       order: 'asc',
-      per_page: 1
+      per_page: 1,
     })
     const request = asAxios(getPrefetchedXHR(url)) || axios.get(url)
 
@@ -418,7 +433,7 @@ function fetchParams(loadingOptions) {
     return [nextPageUrl, {}]
   } else {
     const params = {
-      [timeParam]: loadingOptions.fromMoment.toISOString()
+      [timeParam]: loadingOptions.fromMoment.toISOString(),
     }
     if (loadingOptions.mode === 'past') {
       params.order = 'desc'
@@ -431,6 +446,9 @@ function fetchParams(loadingOptions) {
     }
     const observeeId = observedUserId(loadingOptions.getState())
     if (observeeId) {
+      if (!loadingOptions.getState().singleCourse) {
+        params.include = ['account_calendars']
+      }
       params.observed_user_id = observeeId
       params.context_codes = observedUserContextCodes(loadingOptions.getState())
     }
