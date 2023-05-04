0638ee84a78815e5c5196f27a16649239e494b2f
Gary Denne <gary.denne@instructure.com>
2023-05-04 10:45:57 +1000

    Update password_policy.rb
    Updated Common Password List to use Top 100 weak passwords from
    Wikipedia. Previous list was from 2012 and outdated.
    
    closes gh-2203
    closes demo-238
    flag=none
    
    Test Plan:
     - Specs Pass
    
    Change-Id: Ie9ed0ba7ade0c5b545cd82321464cadca5b0fecf
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/318744
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Dustin Cowles <dustin.cowles@instructure.com>
    QA-Review: Dustin Cowles <dustin.cowles@instructure.com>
    Product-Review: Dustin Cowles <dustin.cowles@instructure.com>

--- lib/canvas/password_policy.rb
+++ lib/canvas/password_policy.rb
@@ -56,33 +56,105 @@ module Canvas
       sequences + sequences.map(&:reverse)
     end
 
-    # per http://www.prweb.com/releases/2012/10/prweb10046001.htm
+    # per https://en.wikipedia.org/wiki/Wikipedia:10,000_most_common_passwords
+    # Licensed under CC BY-SA 3.0: https://creativecommons.org/licenses/by-sa/3.0/legalcode
+    # Top 100 common passwords as at May 2023, excluding profanity
     COMMON_PASSWORDS = %w[
-      password
       123456
+      password
       12345678
-      abc123
       qwerty
+      123456789
+      12345
+      1234
+      111111
+      1234567
+      dragon
+      123123
+      baseball
+      abc123
+      football
       monkey
       letmein
-      dragon
-      111111
-      baseball
-      iloveyou
-      trustno1
-      1234567
-      sunshine
-      master
-      123123
-      welcome
+      696969
       shadow
-      ashley
-      football
-      jesus
-      michael
-      ninja
+      master
+      666666
+      qwertyuiop
+      123321
       mustang
-      password1
+      1234567890
+      michael
+      654321
+      superman
+      1qaz2wsx
+      7777777
+      121212
+      000000
+      qazwsx
+      123qwe
+      killer
+      trustno1
+      jordan
+      jennifer
+      zxcvbnm
+      asdfgh
+      hunter
+      buster
+      soccer
+      harley
+      batman
+      andrew
+      tigger
+      sunshine
+      iloveyou
+      2000
+      charlie
+      robert
+      thomas
+      hockey
+      ranger
+      daniel
+      starwars
+      klaster
+      112233
+      george
+      computer
+      michelle
+      jessica
+      pepper
+      1111
+      zxcvbn
+      555555
+      11111111
+      131313
+      freedom
+      777777
+      pass
+      maggie
+      159753
+      aaaaaa
+      ginger
+      princess
+      joshua
+      cheese
+      amanda
+      summer
+      love
+      ashley
+      6969
+      nicole
+      chelsea
+      biteme
+      matthew
+      access
+      yankees
+      987654321
+      dallas
+      austin
+      thunder
+      taylor
+      matrix
     ].freeze
   end
 end
