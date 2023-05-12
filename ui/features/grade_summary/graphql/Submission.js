9af6e93766440d671c2c06b18c95debba7dc4ece
Drake Harper <drake.harper@instructure.com>
2023-05-12 01:04:58 -0600

    Graphql fragments and queries for student grade page
    refs VICE-3474
    refs VICE-3471
    refs VICE-3472
    refs VICE-3473
    
    flag=restrict_quantitative_data
    
    Test Plan:
    - Code is clear
    - Testing will come in implementation
    
    Change-Id: I6f98e679719e1eba97b644c408500cb878079885
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/318039
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>

---
+++ ui/features/grade_summary/graphql/Submission.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import gql from 'graphql-tag'
+import {arrayOf, float, string, bool} from 'prop-types'
+
+import {SubmissionComment} from './SubmissionComment'
+
+export const Submission = {
+  fragment: gql`
+    fragment Submission on Submission {
+      _id
+      gradingStatus
+      grade
+      score
+      gradingPeriodId
+      hideGradeFromStudent
+      updatedAt
+      commentsConnection {
+        nodes {
+          ...SubmissionComment
+        }
+      }
+    }
+    ${SubmissionComment.fragment}
+  `,
+  shape: {
+    _id: string,
+    gradingStatus: string,
+    grade: string,
+    score: float,
+    gradingPeriodId: string,
+    hideGradeFromStudent: bool,
+    updatedAt: string,
+    commentsConnection: arrayOf({
+      nodes: arrayOf({
+        comment: string,
+        createdAt: string,
+        author: {
+          name: string,
+          shortName: string,
+        },
+      }),
+    }),
+  },
+  mock: ({
+    _id = '1',
+    gradingStatus = 'graded',
+
+    grade = 'A',
+    score = 90,
+    gradingPeriodId = '1',
+    hideGradeFromStudent = false,
+    updatedAt = '2019-01-01T00:00:00Z',
+    commentsConnection = {
+      nodes: [
+        {
+          comment: 'Great job!',
+          createdAt: '2019-01-01T00:00:00Z',
+          author: {
+            name: 'John Doe',
+            shortName: 'JD',
+          },
+        },
+      ],
+    },
+  } = {}) => ({
+    _id,
+    gradingStatus,
+    grade,
+    score,
+    gradingPeriodId,
+    hideGradeFromStudent,
+    updatedAt,
+    commentsConnection,
+    __typename: 'Submission',
+  }),
+}
---
+++ ui/features/grade_summary/graphql/queries.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import gql from 'graphql-tag'
+
+import {AssignmentGroup} from './AssignmentGroup'
+import {Assignment} from './Assignment'
+import {GradingStandard} from './GradingStandard'
+
+export const ASSIGNMENTS = gql`
+  query GetAssignments($courseID: ID!, $gradingPeriodID: ID) {
+    legacyNode(_id: $courseID, type: Course) {
+      ... on Course {
+        id
+        name
+        applyGroupWeights
+        assignmentsConnection(filter: {gradingPeriodId: $gradingPeriodID}) {
+          nodes {
+            ...Assignment
+          }
+        }
+        assignmentGroupsConnection {
+          nodes {
+            ...AssignmentGroup
+          }
+        }
+        gradingStandard {
+          ...GradingStandard
+        }
+        gradingPeriodsConnection {
+          nodes {
+            _id
+            title
+            weight
+          }
+        }
+      }
+    }
+  }
+  ${AssignmentGroup.fragment}
+  ${Assignment.fragment}
+  ${GradingStandard.fragment}
+`
