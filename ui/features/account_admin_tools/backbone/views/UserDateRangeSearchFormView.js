fe24be6951376ad06488c38ed0039109be9202ef
Chawn Neal <chawn.neal@instructure.com>
2023-05-25 12:06:39 -0400

    Use Date iso data instead of abbreviation to filter
    fixes VICE-3492
    flag=none
    
    Test plan:
    0) Go to settings and switch language to spanish:
    0a) /accounts/2/settings#tab-settings
    1) have some notification logs.
    2) go to /accounts/2/admin_tools
    3) click 'Ver notificaciones'
    4) enter '2' (like above)
    4a) click 'encontrar'
    4b) remove 2
    4c) click your account name.
    5) use form to search entries before 'Hasta la fecha'
    5a) Your loggs should return.
    6) re-click your account name.
    7) Search before 1 ene 20XX (year before).
    8) Conclusion:
     a) without my commit:
     - the results will stay the same.
     b) with my commit:
     - the logs should no longer return \ be correct.
    
    Notes:
    Whats happening in the ticket error video?
    1st search goes through.
    2nd search fails.
    Whats left is just the original search\filter results.
    
    Why it breaks?
    We pass an abbreviated Enero, ene and the Date
    processor cannot handle that.
    The solution is I grab the full and correct date data,
    and pass the ISO format.
    
    Change-Id: I6f3e7a44fb116ddbc71ce10ff0c50374ad278672
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319147
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

--- ui/features/account_admin_tools/backbone/views/UserDateRangeSearchFormView.js
+++ ui/features/account_admin_tools/backbone/views/UserDateRangeSearchFormView.js
@@ -199,8 +199,18 @@ export default class UserDateRangeSearchFormView extends Backbone.View {
     // Update the params (which fetches the collection)
     const json = this.$el.toJSON()
     delete json.search_term
-    if (!json.start_time) json.start_time = ''
-    if (!json.end_time) json.end_time = ''
+
+    if (!json.start_time) {
+      json.start_time = ''
+    } else {
+      json.start_time = new Date(this.$dateStartSearchField.data('unfudged-date')).toISOString()
+    }
+    if (!json.end_time) {
+      json.end_time = ''
+    } else {
+      json.end_time = new Date(this.$dateEndSearchField.data('unfudged-date')).toISOString()
+    }
+
     return this.collection.setParams(json)
   }
 }
