9599a9a957c72f8c9336eab69067185ab7737dc8
Davis Hyer <dhyer@instructure.com>
2023-05-26 14:19:40 -0600

    upgrade canvas to DateTimeInput v8
    fixes LF-48
    flag=none
    
    test plan:
      - ensure BBB modal options renders correctly
    
    qa risk: low
    
    Change-Id: I22ac472e7eff24fcc501f9e7c97dbb616893192d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319256
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Davis Hyer <dhyer@instructure.com>

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -61,7 +61,7 @@ const ignoredErrors = [
 ]
 const globalWarn = global.console.warn
 const ignoredWarnings = [
-  /Please update the following components: %s/, // https://instructure.atlassian.net/browse/LS-3901 & https://instructure.atlassian.net/browse/LS-3906
+  /Please update the following components: %s/, // https://instructure.atlassian.net/browse/LS-3906
   /value provided is not in a recognized RFC2822 or ISO format/,
 ]
 global.console = {
