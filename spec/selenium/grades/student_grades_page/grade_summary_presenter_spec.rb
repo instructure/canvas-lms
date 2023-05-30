d8e751afac9e20c08911c2d5d00d8b8ec0f54861
Caleb Guanzon <cguanzon@instructure.com>
2023-05-30 11:34:20 -0600

    spec: clone grade_summary_presenter tests into react version
    refs VICE-3532
    
    Change-Id: Ib22453ace84a4bfb35ea4ce6068d9d8fe6acffcf
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319360
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- spec/selenium/grades/student_grades_page/grade_summary_presenter_spec.rb
+++ spec/selenium/grades/student_grades_page/grade_summary_presenter_spec.rb
@@ -43,15 +43,8 @@ describe GradeSummaryPresenter do
   describe "grade summary page" do
     before do
       enroll_teacher_and_students
-    end
-
-    let(:observed_courses) do
-      Array.new(2) { course_factory(active_course: true, active_all: true) }
-    end
-    let(:active_element) { driver.execute_script("return document.activeElement") }
-
-    it "shows the courses dropdown when logged in as observer" do
-      observed_courses.each do |course|
+      @observed_courses = Array.new(2) { course_factory(active_course: true, active_all: true) }
+      @observed_courses.each do |course|
         student_enrollment = course.enroll_student student
         student_enrollment.accept
 
@@ -62,25 +55,54 @@ describe GradeSummaryPresenter do
         )
         observer_enrollment.accept
       end
-
-      user_session(observer)
-      get "/courses/#{observed_courses.first.id}/grades"
-
-      expect(f("#course_select_menu")).to be_displayed
     end
 
-    it "maintains focus on show what-if/revert to original buttons", priority: 2 do
-      student_submission.student_entered_score = 8
-      student_submission.save!
+    let(:active_element) { driver.execute_script("return document.activeElement") }
 
-      user_session(student)
-      get "/courses/#{test_course.id}/grades"
+    context "when :student_grade_summary_upgrade feature flag is OFF" do
+      it "shows the courses dropdown when logged in as observer" do
+        user_session(observer)
+        get "/courses/#{@observed_courses.first.id}/grades"
+        expect(f("#course_select_menu")).to be_displayed
+      end
 
-      f("#student-grades-whatif button").click
-      expect(active_element).to have_attribute("id", "revert-all-to-actual-score")
+      it "maintains focus on show what-if/revert to original buttons", priority: 2 do
+        student_submission.student_entered_score = 8
+        student_submission.save!
 
-      f("#revert-all-to-actual-score").click
-      expect(active_element).to have_class("btn revert_all_scores_link")
+        user_session(student)
+        get "/courses/#{test_course.id}/grades"
+
+        f("#student-grades-whatif button").click
+        expect(active_element).to have_attribute("id", "revert-all-to-actual-score")
+
+        f("#revert-all-to-actual-score").click
+        expect(active_element).to have_class("btn revert_all_scores_link")
+      end
+    end
+
+    context "when :student_grade_summary_upgrade feature flag is ON" do
+      before do
+        Account.site_admin.enable_feature! :student_grade_summary_upgrade
+      end
+
+      it "shows the courses dropdown to observers" do
+        user_session(observer)
+        get "/courses/#{@observed_courses.first.id}/grades"
+        expect(f("#course_select_menu")).to be_displayed
+      end
+
+      it "shows grade summary even with deleted submissions" do
+        # student, teacher, and test_course come from gradebook_common
+        a1, a2 = Array.new(2) { test_course.assignments.create! points_possible: 10 }
+        a1.grade_student student, grade: 10, grader: teacher
+        a2.grade_student student, grade: 10, grader: teacher
+        a2.destroy
+
+        user_session student
+        get "/courses/#{test_course.id}/grades"
+        expect(f("#course_select_menu")).to be_displayed
+      end
     end
   end
 end
