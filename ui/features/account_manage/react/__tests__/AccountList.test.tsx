176fb2b017e8d4f162febed5ee22e6dbc5fb449c
Maya Tyner <maya.tyner@instructure.com>
2023-05-31 16:14:15 -0600

    create pages for admin accounts
    js_bundle new javascript files in accounts page
    testing in jest
    
    fixes FOO-2199
    flag=none
    
    test plan:
    1. api call is made when page is changed via navigation
    2. page index changes when onPageClick is triggered
    3. accounts from api call are shown in list
    4. prev and next buttons only appear as needed
    
    Change-Id: I09a0ba0cded80e6b1af925e3758ba9461fd5fb7c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319501
    Reviewed-by: Charley Kline <ckline@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Maya Tyner <maya.tyner@instructure.com>
    Product-Review: Maya Tyner <maya.tyner@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/account_manage/react/__tests__/AccountList.test.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, waitFor} from '@testing-library/react'
+import {AccountList} from '../AccountList'
+import fetchMock from 'fetch-mock'
+
+describe('AccountLists', () => {
+  const props = {onPageClick: jest.fn()}
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  it('makes an API call when page loads', async () => {
+    fetchMock.get('/api/v1/accounts?per_page=30&page=1', [{id: '1', name: 'acc1'}])
+    const {queryAllByText} = render(<AccountList {...props} pageIndex={1} />)
+    await waitFor(() => expect(queryAllByText('acc1')).toBeTruthy())
+  })
+
+  it('renders an error message when loading accounts fails', async () => {
+    fetchMock.get('/api/v1/accounts?per_page=30&page=1', () => {
+      throw Object.assign(new Error('error'), {code: 402})
+    })
+    const {queryAllByText} = render(<AccountList {...props} pageIndex={1} />)
+    await waitFor(() => expect(queryAllByText('Accounts could not be found')).toBeTruthy())
+  })
+})
