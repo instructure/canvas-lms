7314e7cd1219d2213c893b13a8e877ab20d25674
Isaac Moore <isaac.moore@instructure.com>
2023-06-02 16:54:05 -0500

    Improve CanvasHttp Sentry logging
    This will move the URI out of the event message and into a tag, where it
    is easier to analyze.
    
    refs AE-140
    flag=none
    
    Change-Id: I01394fcbcddf0a1bda881cc0be1ab96d82034df2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319693
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Isaac Moore <isaac.moore@instructure.com>
    Product-Review: Isaac Moore <isaac.moore@instructure.com>

--- gems/canvas_http/lib/canvas_http.rb
+++ gems/canvas_http/lib/canvas_http.rb
@@ -116,19 +116,26 @@ module CanvasHttp
       curr_cert = 0
       num_certs = nil
       http.verify_callback = lambda do |preverify_ok, x509_store_context| # temporary; remove once all offenders have been fixed
-        valid = preverify_ok
-        error = valid ? "" : x509_store_context.error_string
-        num_certs ||= x509_store_context.chain.length
+        Sentry.with_scope do |scope|
+          scope.set_tags(verify_host: "#{uri.host}:#{uri.port}")
 
-        # only check the last certificate (aka the peer certificate)
-        # We can't have OpenSSL and Net::HTTP check this without failing, so manually check it
-        if (curr_cert += 1) == num_certs && valid && !(valid = OpenSSL::SSL.verify_certificate_identity(x509_store_context.current_cert, uri.host))
-          error = "Hostname mismatch"
+          valid = preverify_ok
+          error = valid ? "" : x509_store_context.error_string
+          num_certs ||= x509_store_context.chain.length
+
+          # only check the last certificate (aka the peer certificate)
+          # We can't have OpenSSL and Net::HTTP check this without failing, so manually check it
+          if (curr_cert += 1) == num_certs && valid && !(valid = OpenSSL::SSL.verify_certificate_identity(x509_store_context.current_cert, uri.host))
+            error = "Hostname mismatch"
+          end
+
+          unless valid
+            scope.set_tags(verify_error: error)
+            Sentry.capture_message("Certificate verify failed: #{error}", level: :warning)
+          end
+
+          true # never fail 🦸
         end
-
-        Sentry.capture_message("Certificate verify failed when connecting to #{uri}: #{error}", level: :warning) unless valid
-
-        true # never fail 🦸
       end
       logger.info("CANVAS_HTTP INITIATE REQUEST | url: #{url_str}")
       start_time = Time.now
