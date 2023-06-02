2fbeea191631dfd9499ffe502f67a97b9b51faa2
Caleb Guanzon <cguanzon@instructure.com>
2023-06-02 15:21:01 -0600

    spec: consolidate, toughen, and strengthen observer testing
    refs VICE-3532
    
    - removes redundant test file
    - redoes setup of student_grades_page_observer_spec
    becuse it was testing the wrong thing, also,
    the included context brings too much unnecessary
    overhead. this commit simplifies the setup
    so that future testing in this file can be
    less frustrating
    
    Change-Id: I01c11fd1fd19f2ad0f71b9d26d2ed6fa170d85e0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/319686
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

--- spec/selenium/grades/student_grades_page/student_grades_page_observer_spec.rb
+++ spec/selenium/grades/student_grades_page/student_grades_page_observer_spec.rb
@@ -17,30 +17,77 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-require_relative "../../helpers/gradebook_common"
+require_relative "../../common"
 
 describe "gradebook" do
   include_context "in-process server selenium tests"
-  include GradebookCommon
 
   context "as an observer" do
-    before do
-      user_with_pseudonym
-      course_with_observer user: @user, active_all: true
-      @course.observers = [@observer]
-      assignment_setup_defaults
-      assignment_setup
-      @all_students.each { |s| add_linked_observer(s, @observer) }
+    before :once do
+      @students = Array.new(2) { |i| user_factory(name: "Student #{i}", active_all: true) }
+      @observer = user_factory(name: "Observer", active_all: true)
+      @admin = account_admin_user
 
-      user_session(@teacher)
+      @observed_courses = Array.new(2) { |i| course_factory(active_course: true, active_all: true, course_name: "OC#{i}") }
+      @observed_courses.each_with_index do |course, index|
+        course.enroll_user(@students[0], "StudentEnrollment", enrollment_state: "active")
+        course.enroll_user(@students[1], "StudentEnrollment", enrollment_state: "active")
+        course.enroll_user(@observer, "ObserverEnrollment", associated_user_id: @students[0].id, enrollment_state: "active")
+        course.enroll_user(@observer, "ObserverEnrollment", associated_user_id: @students[1].id, enrollment_state: "active")
+        a = course.assignments.create!(title: "#{course.name} assignment", points_possible: 10)
+        a.grade_student(@students[0], grade: (10 - index).to_s, grader: @admin)
+        a.grade_student(@students[1], grade: (8 - index).to_s, grader: @admin)
+      end
+      @observed_courses[0].assignments.create!(title: "DO NOT GRADE", points_possible: 20)
     end
 
-    it "allows observer to see grade totals" do
-      get "/courses/#{@course.id}/grades/#{@student_2.id}"
-      expect(f(".final_grade .grade")).to include_text("66.67")
+    it "allows observer to see grade totals with and without ungraded assignments" do
+      user_session @observer
+      get "/courses/#{@observed_courses.first.id}/grades/"
+      expect(f(".student_assignment.final_grade").text).to eq "Total\n100%\n10.00 / 10.00"
+      expect(f("tr.group_total").text).to eq "Assignments\n100%\n10.00 / 10.00"
+      expect(f("tr#submission_final-grade").text).to eq "Total\n100%\n10.00 / 10.00"
+
       f("#only_consider_graded_assignments_wrapper").click
-      wait_for_ajax_requests
-      expect(f(".final_grade .grade")).to include_text("12.5")
+      expect(f(".student_assignment.final_grade").text).to eq "Total\n33.33%\n10.00 / 30.00"
+      expect(f("tr.group_total").text).to eq "Assignments\n33.33%\n10.00 / 30.00"
+      expect(f("tr#submission_final-grade").text).to eq "Total\n33.33%\n10.00 / 30.00"
+    end
+
+    it "can change the student filter" do
+      user_session @observer
+      get "/courses/#{@observed_courses.first.id}/grades/"
+      f("#student_select_menu").click
+      fj("li:contains('Student 1')").click
+      fj("button:contains('Apply')").click
+      wait_for_ajaximations
+      expect(f(".student_assignment.final_grade").text).to eq "Total\n80%\n8.00 / 10.00"
+      expect(f("tr.group_total").text).to eq "Assignments\n80%\n8.00 / 10.00"
+      expect(f("tr#submission_final-grade").text).to eq "Total\n80%\n8.00 / 10.00"
+    end
+
+    it "can change the course filter" do
+      user_session @observer
+      get "/courses/#{@observed_courses.first.id}/grades/"
+      f("#course_select_menu").click
+      fj("li:contains('OC1')").click
+      fj("button:contains('Apply')").click
+      wait_for_ajaximations
+      expect(f(".student_assignment.final_grade").text).to eq "Total\n90%\n9.00 / 10.00"
+      expect(f("tr.group_total").text).to eq "Assignments\n90%\n9.00 / 10.00"
+      expect(f("tr#submission_final-grade").text).to eq "Total\n90%\n9.00 / 10.00"
+    end
+
+    it "respect selected user when changing course filter" do
+      user_session @observer
+      get "/courses/#{@observed_courses.first.id}/grades/#{@students[1].id}"
+      f("#course_select_menu").click
+      fj("li:contains('OC1')").click
+      fj("button:contains('Apply')").click
+      wait_for_ajaximations
+      expect(f(".student_assignment.final_grade").text).to eq "Total\n70%\n7.00 / 10.00"
+      expect(f("tr.group_total").text).to eq "Assignments\n70%\n7.00 / 10.00"
+      expect(f("tr#submission_final-grade").text).to eq "Total\n70%\n7.00 / 10.00"
     end
   end
 end
