b3813289e2f2c5649f8627835dff81a252277a8f
Alexander Youngblood <alexander.youngblood@instructure.com>
2023-06-12 18:48:58 -0700

    Make backend create discussion mutation
    refs VICE-2495
    flag=discussion_create
    
    Test plan:
    1. Spec tests pass
    2. Test mutation in GraphiQL
    
    Change-Id: I4170b9b1f45b745d0244ccbc157e1d4ed5d8a216
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/320326
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

---
+++ app/graphql/mutations/create_discussion_topic.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+class Mutations::CreateDiscussionTopic < Mutations::DiscussionBase
+  graphql_name "CreateDiscussionTopic"
+
+  argument :context_id, ID, required: true, prepare: GraphQLHelpers.relay_or_legacy_id_prepare_func("Context")
+  argument :context_type, String, required: true
+  # most arguments inherited from DiscussionBase
+
+  field :discussion_topic, Types::DiscussionType, null: true
+  def resolve(input:)
+    # "context" is already a variable (the context of the mutation)
+    # which is why this is "discussion_topic_context(_id)"
+    discussion_topic_context_id = GraphQLHelpers.parse_relay_or_legacy_id(input[:context_id], "Context")
+
+    # TODO: potentially create ContextType GraphQL type
+    if input[:context_type] == "Course"
+      discussion_topic_context = Course.find(discussion_topic_context_id)
+    elsif input[:context_type] == "Group"
+      discussion_topic_context ||= Group.find(discussion_topic_context_id)
+    else
+      return validation_error(I18n.t("Invalid context type"))
+    end
+
+    return validation_error(I18n.t("Invalid context")) unless discussion_topic_context
+
+    discussion_topic = DiscussionTopic.new(
+      {
+        context_id: discussion_topic_context.id,
+        context_type: input[:context_type],
+        title: input[:title],
+        message: input[:message],
+        workflow_state: input[:published] ? "active" : "unpublished"
+      }
+    )
+
+    return validation_error(I18n.t("Insufficient Permissions")) unless discussion_topic&.grants_right? current_user, :create
+
+    response = {}
+    if discussion_topic.save
+      response[:discussion_topic] = discussion_topic
+    else
+      errors = errors_for(discussion_topic)
+      response[:errors] = errors if errors.any?
+    end
+
+    response
+  rescue ActiveRecord::RecordNotFound
+    raise GraphQL::ExecutionError, "Not found"
+  end
+end
---
+++ spec/graphql/mutations/create_discussion_topic_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+require "spec_helper"
+require_relative "../graphql_spec_helper"
+describe Mutations::CreateDiscussionTopic do
+  before(:once) do
+    course_with_teacher(active_all: true)
+  end
+
+  def execute_with_input(create_input, current_user = @teacher)
+    mutation_command = <<~GQL
+      mutation {
+        createDiscussionTopic(input: {
+          #{create_input}
+        }){
+          discussionTopic {
+            _id
+            contextType
+            title
+            message
+            published
+          }
+          errors {
+            attribute
+            message
+          }
+        }
+      }
+    GQL
+    context = { current_user:, request: ActionDispatch::TestRequest.create }
+    CanvasSchema.execute(mutation_command, context:)
+  end
+
+  it "creates the discussion entry and returns id" do
+    context_type = "Course"
+    title = "Test Title"
+    message = "A message"
+    published = false
+    query = <<~GQL
+      contextId: "#{@course.id}"
+      contextType: "#{context_type}"
+      title: "#{title}"
+      message: "#{message}"
+      published: #{published}
+    GQL
+
+    result = execute_with_input(query)
+    created_discussion_topic = result.dig("data", "createDiscussionTopic", "discussionTopic")
+
+    expect(result["errors"]).to be_nil
+    expect(result.dig("data", "discussionTopic", "errors")).to be_nil
+    expect(created_discussion_topic["contextType"]).to eq context_type
+    expect(created_discussion_topic["title"]).to eq title
+    expect(created_discussion_topic["message"]).to eq message
+    expect(created_discussion_topic["published"]).to eq published
+    expect(DiscussionTopic.where("id = #{created_discussion_topic["_id"]}").count).to eq 1
+  end
+
+  context "errors" do
+    def expect_error(result, message)
+      errors = result["errors"] || result.dig("data", "createDiscussionTopic", "errors")
+      expect(errors).not_to be_nil
+      expect(errors[0]["message"]).to match(/#{message}/)
+    end
+
+    describe "invalid context" do
+      it "returns 'not found' with an incorrect ID" do
+        query = <<~GQL
+          contextId: "1"
+          contextType: "Course"
+        GQL
+        result = execute_with_input(query)
+        expect_error(result, "Not found")
+      end
+
+      it "returns 'invalid context' with an incorrect context type" do
+        query = <<~GQL
+          contextId: "1"
+          contextType: "NotAContextType"
+        GQL
+        result = execute_with_input(query)
+        expect_error(result, "Invalid context type")
+      end
+    end
+  end
+end
