fe2f26cc68fd8dcc7cfaba1464677b11c081eb39
Ryan Hawkins <ryan.hawkins@instructure.com>
2023-06-14 13:10:51 -0600

    Add tests for LTI Configuration Forms
    why:
    - None of these forms had any tests(!) and my next commit will need to
      make some modifications. I'd like to be confident my changes aren't
      breaking existing functionality.
    
    test-plan:
    - No user-facing behavior should change. This commit only adds tests.
      The next commit *also* shouldn't modify user-facing behavior, but it
      might be inevitable with the migration to InstUI components. Hopefully
      not though :cross-fingers:
    
    refs INTEROP-7353
    
    flag=none
    
    Change-Id: I015f3299e0127fd9c4141f6766812e681a303146
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/320673
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>

---
+++ ui/features/external_apps/react/components/__tests__/ConfigurationForm.test.tsx
@@ -0,0 +1,411 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {jest} from '@jest/globals'
+import React from 'react'
+import '@testing-library/jest-dom'
+import $ from 'jquery'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import ConfigurationForm from '../ConfigurationForm'
+
+const handleSubmitMock = jest.fn()
+
+const renderForm = (props: Object) => {
+  return render(<ConfigurationForm {...props} />)
+}
+
+const getSubmitButton = () => {
+  return screen.getByRole('button', {name: /submit/i})
+}
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+// I hate having to mock as opposed to just using testing library
+// but the way that $.screenReaderFlashError is written means that
+// we have to do it this way. Otherwise, we have to insert the node
+// that it's expecting.
+const mockedFlash = jest.fn()
+const originalFlash = $.screenReaderFlashError
+$.screenReaderFlashError = mockedFlash
+
+afterAll(() => {
+  $.screenReaderFlashError = originalFlash
+})
+
+describe('when configuration type is manual', () => {
+  const getNameInput = () => {
+    return screen.getByLabelText(/^name$/i)
+  }
+
+  const getDomainInput = () => {
+    return screen.getByLabelText(/^domain$/i)
+  }
+
+  const getUrlInput = () => {
+    return screen.getByLabelText(/^launch url$/i)
+  }
+
+  const baseProps = {
+    configurationType: 'manual',
+    tool: {},
+    handleSubmit: handleSubmitMock,
+    name: undefined,
+    url: undefined,
+    domain: undefined,
+  }
+
+  it('renders the manual configuration form', () => {
+    renderForm(baseProps)
+    expect(getNameInput()).toBeInTheDocument()
+    expect(getUrlInput()).toBeInTheDocument()
+    expect(getDomainInput()).toBeInTheDocument()
+  })
+
+  it('tries to submit the form with the appropriate values when the submit button is clicked', () => {
+    renderForm(baseProps)
+    userEvent.type(getNameInput(), 'a really cool name')
+    userEvent.type(getUrlInput(), 'https://example.com')
+    userEvent.type(getDomainInput(), 'example.com')
+    userEvent.type(screen.getByLabelText(/consumer key/i), 'key')
+    userEvent.type(screen.getByLabelText(/shared secret/i), 'secret')
+    userEvent.selectOptions(screen.getByLabelText(/privacy/i), 'anonymous')
+    userEvent.click(getSubmitButton())
+    expect(handleSubmitMock).toHaveBeenCalledWith(
+      'manual',
+      {
+        name: 'a really cool name',
+        url: 'https://example.com',
+        domain: 'example.com',
+        consumerKey: 'key',
+        sharedSecret: 'secret',
+        privacyLevel: 'anonymous',
+        customFields: '',
+        description: '',
+        verifyUniqueness: 'true',
+      },
+      expect.anything()
+    )
+  })
+
+  it('uses the default values passed in from props', () => {
+    renderForm({
+      ...baseProps,
+      tool: {
+        name: 'a really cool name',
+        consumer_key: 'key',
+        shared_secret: 'secret',
+        description: 'a great little description',
+      },
+    })
+
+    expect(getNameInput()).toHaveValue('a really cool name')
+    expect(screen.getByLabelText(/consumer key/i)).toHaveValue('key')
+    expect(screen.getByLabelText(/shared secret/i)).toHaveValue('secret')
+    expect(screen.getAllByLabelText(/privacy/i)[0]).toHaveValue('anonymous')
+    expect(screen.getByLabelText(/description/i)).toHaveValue('a great little description')
+  })
+
+  describe('error checking', () => {
+    it('flashes an error when name is empty', () => {
+      renderForm(baseProps)
+      userEvent.click(getSubmitButton())
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect(mockedFlash).toHaveBeenCalled()
+    })
+
+    it('renders an error next to the name input when name is empty', () => {
+      renderForm(baseProps)
+      userEvent.click(getSubmitButton())
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect(screen.getByText('This field is required')).toBeInTheDocument()
+    })
+
+    describe('name has a value', () => {
+      it('flashes an error when url and domain are both empty', () => {
+        renderForm(baseProps)
+        userEvent.type(getNameInput(), 'a really cool name')
+        userEvent.click(getSubmitButton())
+
+        expect(handleSubmitMock).not.toHaveBeenCalled()
+        expect(mockedFlash).toHaveBeenCalled()
+      })
+
+      it('renders an error when url and domain are both empty', () => {
+        renderForm(baseProps)
+        userEvent.type(getNameInput(), 'a really cool name')
+        userEvent.click(getSubmitButton())
+
+        expect(handleSubmitMock).not.toHaveBeenCalled()
+        expect(screen.getAllByText(/Either the url or domain should be set./i)).not.toHaveLength(0)
+      })
+
+      it("doesn't flash an error if just url is set and tries to submit the form", () => {
+        renderForm(baseProps)
+        userEvent.type(getNameInput(), 'a really cool name')
+        userEvent.type(getUrlInput(), 'https://example.com')
+        userEvent.click(getSubmitButton())
+
+        expect(handleSubmitMock).toHaveBeenCalled()
+        expect(mockedFlash).not.toHaveBeenCalled()
+      })
+
+      it("doesn't flash an error if just domain is set and tries to submit the form", () => {
+        renderForm(baseProps)
+        userEvent.type(getNameInput(), 'a really cool name')
+        userEvent.type(getDomainInput(), 'example.com')
+        userEvent.click(getSubmitButton())
+
+        expect(handleSubmitMock).toHaveBeenCalled()
+        expect(mockedFlash).not.toHaveBeenCalled()
+      })
+    })
+  })
+})
+
+describe('when configuration type is url', () => {
+  const baseProps = {
+    configurationType: 'url',
+    tool: {},
+    handleSubmit: handleSubmitMock,
+    name: undefined,
+    url: undefined,
+    domain: undefined,
+  }
+
+  it('renders the url configuration form', () => {
+    renderForm(baseProps)
+    expect(screen.getByLabelText(/config url/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/shared secret/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/consumer key/i)).toBeInTheDocument()
+  })
+
+  it('tries to submit the form with the appropriate values when the submit button is clicked', () => {
+    renderForm(baseProps)
+
+    userEvent.type(screen.getByLabelText(/config url/i), 'https://example.com')
+    userEvent.type(screen.getByLabelText(/consumer key/i), 'key')
+    userEvent.type(screen.getByLabelText(/shared secret/i), 'secret')
+    userEvent.type(screen.getByLabelText(/name/i), 'a really cool name')
+
+    userEvent.click(screen.getByText(/submit/i))
+
+    expect(handleSubmitMock).toHaveBeenCalledWith(
+      'url',
+      {
+        name: 'a really cool name',
+        configUrl: 'https://example.com',
+        consumerKey: 'key',
+        sharedSecret: 'secret',
+        verifyUniqueness: 'true',
+      },
+      expect.anything()
+    )
+    expect($.screenReaderFlashError).not.toHaveBeenCalled()
+  })
+
+  it('uses the default values passed in from props', () => {
+    renderForm({
+      ...baseProps,
+      tool: {
+        name: 'a really cool name',
+        config_url: 'https://example.com',
+        consumer_key: 'key',
+        shared_secret: 'secret',
+      },
+    })
+
+    expect(screen.getByLabelText(/name/i)).toHaveValue('a really cool name')
+    expect(screen.getByLabelText(/config url/i)).toHaveValue('https://example.com')
+    expect(screen.getByLabelText(/consumer key/i)).toHaveValue('key')
+    expect(screen.getByLabelText(/shared secret/i)).toHaveValue('secret')
+  })
+
+  describe('error checking', () => {
+    it('flashes and renders an error when config url is empty', () => {
+      renderForm(baseProps)
+      userEvent.type(screen.getByLabelText(/name/i), 'a great name')
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getByText(/This field is required/i)).toBeInTheDocument()
+    })
+
+    it('flashes and renders error when the name is empty', () => {
+      renderForm(baseProps)
+      userEvent.type(screen.getByLabelText(/config url/i), 'https://example.com')
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getByText(/This field is required/i)).toBeInTheDocument()
+    })
+
+    it('flashes and renders multiple errors when both fields are empty', () => {
+      renderForm(baseProps)
+
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getAllByText(/This field is required/i)).toHaveLength(2)
+    })
+  })
+})
+
+describe('when configuration type is xml', () => {
+  const baseProps = {
+    configurationType: 'xml',
+    tool: {},
+    handleSubmit: handleSubmitMock,
+    name: undefined,
+    url: undefined,
+    domain: undefined,
+  }
+
+  it('renders the xml configuration form', () => {
+    renderForm(baseProps)
+    expect(screen.getByLabelText(/xml configuration/i)).toBeInTheDocument()
+  })
+
+  it('tries to submit the form with the appropriate values when the submit button is clicked', () => {
+    renderForm(baseProps)
+
+    userEvent.type(screen.getByLabelText(/xml configuration/i), 'some for sure real xml')
+    userEvent.type(screen.getByLabelText(/name/i), 'a really cool name')
+    userEvent.type(screen.getByLabelText(/shared secret/i), 'secret')
+    userEvent.type(screen.getByLabelText(/consumer key/i), 'key')
+
+    userEvent.click(screen.getByText(/submit/i))
+
+    expect(handleSubmitMock).toHaveBeenCalledWith(
+      'xml',
+      {
+        name: 'a really cool name',
+        xml: 'some for sure real xml',
+        consumerKey: 'key',
+        sharedSecret: 'secret',
+        verifyUniqueness: 'true',
+      },
+      expect.anything()
+    )
+    expect($.screenReaderFlashError).not.toHaveBeenCalled()
+  })
+
+  it('uses the default values passed in from props', () => {
+    renderForm({
+      ...baseProps,
+      tool: {
+        name: 'a really cool name',
+        xml: 'some for sure real xml',
+        consumer_key: 'key',
+        shared_secret: 'secret',
+      },
+    })
+
+    expect(screen.getByLabelText(/name/i)).toHaveValue('a really cool name')
+    expect(screen.getByLabelText(/xml configuration/i)).toHaveValue('some for sure real xml')
+    expect(screen.getByLabelText(/consumer key/i)).toHaveValue('key')
+    expect(screen.getByLabelText(/shared secret/i)).toHaveValue('secret')
+  })
+
+  describe('error checking', () => {
+    it('flashes and renders an error when xml configuration is empty', () => {
+      renderForm(baseProps)
+      userEvent.type(screen.getByLabelText(/name/i), 'a great name')
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getByText(/This field is required/i)).toBeInTheDocument()
+    })
+
+    it('flashes and renders error when the name is empty', () => {
+      renderForm(baseProps)
+      userEvent.type(screen.getByLabelText(/xml configuration/i), 'some for sure real xml')
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getByText(/This field is required/i)).toBeInTheDocument()
+    })
+
+    it('flashes and renders multiple errors when both fields are empty', () => {
+      renderForm(baseProps)
+
+      userEvent.click(screen.getByText(/submit/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect($.screenReaderFlashError).toHaveBeenCalled()
+      expect(screen.getAllByText(/This field is required/i)).toHaveLength(2)
+    })
+  })
+})
+
+describe('when configuration type is lti2', () => {
+  const baseProps = {
+    configurationType: 'lti2',
+    tool: {
+      registration_url: '',
+    },
+    handleSubmit: handleSubmitMock,
+    name: undefined,
+    url: undefined,
+    domain: undefined,
+  }
+
+  const getRegUrlInput = () => {
+    return screen.getByLabelText(/registration url/i)
+  }
+
+  it('renders the lti2 configuration form', () => {
+    renderForm(baseProps)
+    expect(getRegUrlInput()).toBeInTheDocument()
+  })
+
+  it('tries to submit the form with the appropriate values when the submit button is clicked', () => {
+    renderForm(baseProps)
+    userEvent.type(getRegUrlInput(), 'https://example.com')
+
+    userEvent.click(screen.getByText(/launch registration tool/i))
+
+    expect(handleSubmitMock).toHaveBeenCalledWith(
+      'lti2',
+      {
+        registrationUrl: 'https://example.com',
+      },
+      expect.anything()
+    )
+  })
+
+  describe('error checking', () => {
+    it("renders an error if the registration url hasn't been filled out", () => {
+      renderForm(baseProps)
+
+      userEvent.click(screen.getByText(/launch registration tool/i))
+
+      expect(handleSubmitMock).not.toHaveBeenCalled()
+      expect(screen.getByLabelText(/this field is required/i)).toBeInTheDocument()
+    })
+  })
+})
