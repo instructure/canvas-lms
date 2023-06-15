2c514474a17c87d73287dc13c2c53472a26da675
Alexander Youngblood <alexander.youngblood@instructure.com>
2023-06-15 11:02:38 -0700

    Create index.js for discussion create/edit page
    closes VICE-3587
    flag=discussion_create
    
    Test plan:
    1. Jest tests pass
    2. Verify Apollo client is in page body
    
    Change-Id: I29c85bf7c5a160cccf3abd53a63caa5fdf8d8e17
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/320641
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

---
+++ ui/features/discussion_topic_edit_v2/react/index.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useState, useEffect} from 'react'
+import {ApolloProvider, createClient} from '@canvas/apollo'
+
+import {useScope as useI18nScope} from '@canvas/i18n'
+import LoadingIndicator from '@canvas/loading-indicator'
+import ErrorBoundary from '@canvas/error-boundary'
+import errorShipUrl from '@canvas/images/ErrorShip.svg'
+import GenericErrorPage from '@canvas/generic-error-page'
+
+const I18n = useI18nScope('discussion_topics_edit')
+
+export const DiscussionTopicEdit = _props => {
+  const [client, setClient] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setClient(createClient())
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return <LoadingIndicator />
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <ErrorBoundary
+        errorComponent={
+          <GenericErrorPage
+            imageUrl={errorShipUrl}
+            errorCategory={I18n.t('Discussion Topic Edit Error Page')}
+          />
+        }
+      >
+        <div>Page coming soon</div>
+      </ErrorBoundary>
+    </ApolloProvider>
+  )
+}
