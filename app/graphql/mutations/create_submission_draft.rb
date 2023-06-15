2dae2759cef1b609b568a13038ec7b3aa8b8a1f0
Cameron Ray <cameron.ray@instructure.com>
2023-06-15 16:35:34 -0400

    Optimize CreateSubmissionDraft GraphQL Mutation DB Queries
    closes EVAL-3267
    flag=none
    
    test plan:
    - specs pass
    
    Change-Id: Id5b2e6326457a427ec8881efcd4b1a8d32ca0275
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/320661
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Reviewed-by: Christopher Soto <christopher.soto@instructure.com>
    QA-Review: Cameron Ray <cameron.ray@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

--- app/graphql/mutations/create_submission_draft.rb
+++ app/graphql/mutations/create_submission_draft.rb
@@ -129,17 +129,10 @@ def find_submission(submission_id)
 end
 
 def get_and_verify_attachments!(file_ids)
-  submittable_attachments = current_user.submittable_attachments do |scope|
-    scope.left_joins(:replaced_attachments)
-  end
+  attachment_ids = get_attachment_ids(file_ids)
 
-  attachments = submittable_attachments.where(id: file_ids).or(
-    # table alias for self-referencing relations is <relation_name>_<table_name>
-    submittable_attachments.where(replaced_attachments_attachments: { id: file_ids })
-  )
-
-  validate_file_ids!(file_ids, attachments)
-  attachments = attachments.to_a.uniq
+  validate_file_ids!(file_ids, attachment_ids)
+  attachments = Attachment.active.where(id: attachment_ids)
 
   attachments.each do |attachment|
     verify_authorized_action!(attachment, :read)
@@ -148,15 +141,7 @@ def get_and_verify_attachments!(file_ids)
   attachments
 end
 
-def validate_file_ids!(file_ids, attachments)
-  current_and_replaced_ids =
-    attachments
-    .pluck(:id, "replaced_attachments_attachments.id")
-    .each_with_object(Set.new) do |(id, replaced_id), ids|
-      ids << id.to_s
-      ids << replaced_id.to_s if replaced_id
-    end
-
+def validate_file_ids!(file_ids, current_and_replaced_ids)
   file_ids.each do |file_id|
     next if current_and_replaced_ids.include?(file_id)
 
@@ -175,4 +160,61 @@ def verify_allowed_extensions!(assignment, attachments)
     attachment_extension = attachment.after_extension || ""
     assignment.allowed_extensions.include?(attachment_extension.downcase)
   end
+end
+
+def get_attachment_ids(file_ids)
+  return [] if file_ids.empty?
+
+  joined_file_ids = file_ids.to_a.join(",")
+  sql = <<~SQL.squish
+    SELECT
+      p.a_id,
+      p.ra_id
+    FROM
+      (
+        (
+          SELECT
+            a.id as a_id,
+            ra.id as ra_id,
+            a.context_id,
+            a.context_type
+          FROM
+            #{Attachment.quoted_table_name} AS a
+            LEFT JOIN #{Attachment.quoted_table_name} AS ra ON ra.replacement_attachment_id = a.id
+          WHERE
+            a.id in (#{joined_file_ids}) AND
+            a.file_state = 'available'
+        )
+        UNION ALL
+        (
+          SELECT
+            a.id,
+            ra.id,
+            a.context_id,
+            a.context_type
+          FROM
+            #{Attachment.quoted_table_name} AS a
+            JOIN #{Attachment.quoted_table_name} AS ra ON ra.replacement_attachment_id = a.id
+          WHERE
+            ra.id in (#{joined_file_ids}) AND
+            a.file_state = 'available'
+        )
+      ) as p
+    WHERE
+      ( p.context_type = 'User' AND p.context_id = #{current_user.id} )
+      OR
+      ( p.context_type = 'Group' AND exists (
+          SELECT 1
+          FROM #{GroupMembership.quoted_table_name} gm
+              JOIN #{Group.quoted_table_name} g ON g.id = gm.group_id
+          WHERE
+              gm.user_id = #{current_user.id} AND
+              gm.workflow_state = 'accepted' AND
+              g.workflow_state <> 'deleted'
+        )
+      );
+  SQL
+
+  result = ActiveRecord::Base.connection.execute(sql)
+  result.values.flatten.compact.uniq.map(&:to_s)
 end
