9b3a73420447c3e2d8df0512057336244502db32
Chris Soto <christopher.soto@instructure.com>
2023-06-23 09:01:34 -0600

    ind-gradebook enhancement curve grades modal
    this is an iterative commit for the enhanced individual gradebook work
    currently under feature flag. this commit adds the curve grades modal
    functionality. it reuses the jquery curve grades modal right now, but
    hopeful future plans to create an inst ui component for this.
    
    closes EVAL-3226
    flag=individual_gradebook_enhancements
    
    test plan:
    - build & tests pass
    
    Change-Id: I37e4b07c43ec1e73c90ae13c790b8b66ea15bc8e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/321224
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/features/enhanced_individual_gradebook/react/components/AssignmentInformation/CurveGradesModal.tsx
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $ from 'jquery'
+import React from 'react'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {Button} from '@instructure/ui-buttons'
+import {AssignmentConnection, SubmissionConnection, SubmissionGradeChange} from '../../../types'
+import {mapToSubmissionGradeChange} from '../../../utils/gradebookUtils'
+import {Submission} from '../../../../../api.d'
+
+async function loadCurveGradesDialog() {
+  return (await import('@canvas/grading/jquery/CurveGradesDialog')).default
+}
+
+const I18n = useI18nScope('enhanced_individual_gradebook')
+
+type StudentAssignmentMap = {
+  [userId: string]: {
+    [assignmentId: string]: SubmissionConnection
+  }
+}
+type Props = {
+  assignment: AssignmentConnection
+  submissions: SubmissionConnection[]
+  contextUrl?: string | null
+  handleGradeChange: (updatedSubmissions: SubmissionGradeChange[]) => void
+}
+export function CurveGradesModal({assignment, contextUrl, submissions, handleGradeChange}: Props) {
+  const studentAssignmentMap = submissions.reduce((studentMap, submission) => {
+    const {userId, assignmentId} = submission
+    studentMap[userId] = {
+      [`assignment_${assignmentId}`]: submission,
+    }
+    return studentMap
+  }, {} as StudentAssignmentMap)
+
+  const assignmentDetails = {
+    id: assignment.id,
+    points_possible: assignment.pointsPossible,
+    grading_type: assignment.gradingType,
+    name: assignment.name,
+  }
+
+  if (!contextUrl) {
+    return null
+  }
+
+  const showDialog = async () => {
+    const CurveGradesDialog = await loadCurveGradesDialog()
+    const dialogProps = {
+      assignment: assignmentDetails,
+      students: studentAssignmentMap,
+      context_url: contextUrl,
+    }
+    const dialog = new CurveGradesDialog(dialogProps)
+    dialog.show(() => {})
+  }
+
+  $.subscribe('submissions_updated', submissions => {
+    const mappedSubmissions: SubmissionGradeChange[] = submissions.map((submission: Submission) =>
+      mapToSubmissionGradeChange(submission)
+    )
+    handleGradeChange(mappedSubmissions)
+  })
+
+  return (
+    <Button color="secondary" onClick={showDialog}>
+      {I18n.t('Curve Grades')}
+    </Button>
+  )
+}
