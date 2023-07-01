df28887f6f478d33f5079fcc94066fb25be7da6e
Tucker McKnight <tmcknight@instructure.com>
2023-07-01 14:03:21 -0600

    Rename the dev key "rewrite" specs to just "dev key" specs
    Also remove the references to the "new dev key" setting and
    feature flag, which are not used anymore.
    
    flag = none
    
    Change-Id: I204d63f2479e03b65de1e52731aac224527a45da
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/321888
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -19,8 +19,6 @@
 #
 
 class Setting < Switchman::UnshardedRecord
-  SITE_ADMIN_ACCESS_TO_NEW_DEV_KEY_FEATURES = "site_admin_access_to_new_dev_key_features"
-
   def self.skip_cache
     @skip_cache, old_enabled = true, @skip_cache
     yield
