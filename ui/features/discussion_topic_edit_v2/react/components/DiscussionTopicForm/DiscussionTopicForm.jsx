4974ca174ec1548b2955a336b69188c0450f72e1
Alexander Youngblood <alexander.youngblood@instructure.com>
2023-07-10 13:16:46 -0700

    Add Phase 1 components to DiscussionTopicForm
    Testing the anonymity selector via Jest is
    blocked by InstUI's RadioInput(Group) components
    being difficult to manipulate. Tests will
    come in a later patchset once I figure out
    how to make them work (Jason and I have tried
    a lot!)
    
    Also, the DateTimeInput component is from
    InstUI v8 at the time of this commit, FYI.
    
    closes VICE-2469
    closes VICE-2470
    refs VICE-2471
    refs VICE-2472
    refs VICE-2473
    refs VICE-2474
    closes VICE-2475
    closes VICE-2476
    closes VICE-2477
    closes VICE-2478
    flag=discussion_create
    
    Test plan:
    1. Jest tests pass
    2. Open form in Storybook, enter title
    and see that the background changes color
    
    Change-Id: I029cb1686b2dcab56f3af7a75c5290f5f0077b43
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/322402
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jody Sailor
    QA-Review: Alexander Youngblood <alexander.youngblood@instructure.com>
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>

--- ui/features/discussion_topic_edit_v2/react/components/DiscussionTopicForm/DiscussionTopicForm.jsx
+++ ui/features/discussion_topic_edit_v2/react/components/DiscussionTopicForm/DiscussionTopicForm.jsx
@@ -16,28 +16,70 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import PropTypes from 'prop-types'
+import AnonymousResponseSelector from '@canvas/discussions/react/components/AnonymousResponseSelector/AnonymousResponseSelector'
+import {useScope as usei18NScope} from '@canvas/i18n'
+
 import {View} from '@instructure/ui-view'
 import {TextInput} from '@instructure/ui-text-input'
 import {FormFieldGroup} from '@instructure/ui-form-field'
 import {Button} from '@instructure/ui-buttons'
-import {useScope as usei18NScope} from '@canvas/i18n'
+import {RadioInput, RadioInputGroup} from '@instructure/ui-radio-input'
+import {Text} from '@instructure/ui-text'
+import {Checkbox} from '@instructure/ui-checkbox'
+import {SimpleSelect} from '@instructure/ui-simple-select'
+import {DateTimeInput} from '@instructure/ui-date-time-input'
+import CanvasMultiSelect from '@canvas/multi-select'
+import CanvasRce from '@canvas/rce/react/CanvasRce'
 
 const I18N = usei18NScope('discussion_create')
 
-export default function DiscussionTopicForm({onSubmit}) {
+export default function DiscussionTopicForm({isEditing, isStudent, sections, groups, onSubmit}) {
   const [title, setTitle] = useState('')
   const [titleValidationMessages, setTitleValidationMessages] = useState([
     {text: '', type: 'success'},
   ])
 
+  const [rceContent, setRceContent] = useState('')
+
+  const [sectionsToPostTo, setSectionsToPostTo] = useState(['all-sections'])
+
+  const [discussionAnonymousState, setDiscussionAnonymousState] = useState('off')
+  const [anonymousAuthorState, setAnonymousAuthorState] = useState(false)
+  const [respondBeforeReply, setRespondBeforeReply] = useState(false)
+  const [enablePodcastFeed, setEnablePodcastFeed] = useState(false)
+  const [includeRepliesInFeed, setIncludeRepliesInFeed] = useState(false)
+  const [isGraded, setIsGraded] = useState(false)
+  const [allowLiking, setAllowLiking] = useState(false)
+  const [onlyGradersCanLike, setOnlyGradersCanLike] = useState(false)
+  const [addToTodo, setAddToTodo] = useState(false)
+  const [todoDate, setTodoDate] = useState(null)
+  const [isGroupDiscussion, setIsGroupDiscussion] = useState(false)
+  const [groupSet, setGroupSet] = useState(null)
+
+  const [availableFrom, setAvailableFrom] = useState(null)
+  const [availableUntil, setAvailableUntil] = useState(null)
+  const [availabiltyValidationMessages, setAvailabilityValidationMessages] = useState([
+    {text: '', type: 'success'},
+  ])
+
+  // To be implemented in phase 2, kept as a reminder
+  // const [pointsPossible, setPointsPossible] = useState(0)
+  // const [displayGradeAs, setDisplayGradeAs] = useState('letter')
+  // const [assignmentGroup, setAssignmentGroup] = useState('')
+  // const [peerReviewAssignment, setPeerReviewAssignment] = useState('off')
+  // const [assignTo, setAssignTo] = useState('')
+  // const [dueDate, setDueDate] = useState(0)
+
   const validateTitle = newTitle => {
     if (newTitle.length > 255) {
-      setTitleValidationMessages([{text: 'Title must be less than 255 characters.', type: 'error'}])
+      setTitleValidationMessages([
+        {text: I18N.t('Title must be less than 255 characters.'), type: 'error'},
+      ])
       return false
     } else if (newTitle.length === 0) {
-      setTitleValidationMessages([{text: 'Title must not be empty.', type: 'error'}])
+      setTitleValidationMessages([{text: I18N.t('Title must not be empty.'), type: 'error'}])
       return false
     } else {
       setTitleValidationMessages([{text: '', type: 'success'}])
@@ -45,18 +87,58 @@ export default function DiscussionTopicForm({onSubmit}) {
     }
   }
 
+  const validateAvailability = (newAvailableFrom, newAvailableUntil) => {
+    if (newAvailableFrom === null || newAvailableUntil === null) {
+      setAvailabilityValidationMessages([{text: '', type: 'success'}])
+      return true
+    } else if (newAvailableUntil < newAvailableFrom) {
+      setAvailabilityValidationMessages([
+        {text: I18N.t('Date must be after date available.'), type: 'error'},
+      ])
+      return false
+    } else {
+      setAvailabilityValidationMessages([{text: '', type: 'success'}])
+      return true
+    }
+  }
+
   const validateFormFields = () => {
-    // Will add more conditions in future tickets
-    return validateTitle(title)
+    return validateTitle(title) && validateAvailability(availableFrom, availableUntil)
   }
 
   const submitForm = shouldPublish => {
     if (validateFormFields()) {
-      onSubmit({title, message: '<p>Lorem ipsum</p>', shouldPublish})
+      onSubmit({
+        title,
+        message: rceContent,
+        sectionsToPostTo,
+        discussionAnonymousState,
+        anonymousAuthorState,
+        respondBeforeReply,
+        enablePodcastFeed,
+        includeRepliesInFeed,
+        isGraded,
+        allowLiking,
+        onlyGradersCanLike,
+        addToTodo,
+        todoDate,
+        isGroupDiscussion,
+        groupSet,
+        availableFrom,
+        availableUntil,
+        shouldPublish,
+      })
       return true
     }
     return false
   }
+
+  const rceRef = useRef()
+  const textAreaId = useRef(`discussion-message-body`)
+
+  const allSectionsOption = {id: 'all-sections', label: 'All Sections'}
+
+  const inputWidth = '50%'
 
   return (
     <>
@@ -66,14 +148,256 @@ export default function DiscussionTopicForm({onSubmit}) {
           type={I18N.t('text')}
           placeholder={I18N.t('Topic Title')}
           value={title}
-          onChange={(_, value) => {
+          onChange={(_event, value) => {
             validateTitle(value)
             const newTitle = value.substring(0, 255)
             setTitle(newTitle)
           }}
           messages={titleValidationMessages}
+          autoFocus={true}
+          width={inputWidth}
         />
-        <View display="block" textAlign="end">
+        <CanvasRce
+          textareaId={textAreaId.current}
+          onFocus={() => {}}
+          onBlur={() => {}}
+          onInit={() => {}}
+          ref={rceRef}
+          onContentChange={content => {
+            setRceContent(content)
+          }}
+          editorOptions={{}}
+          height={300}
+          defaultContent=""
+        />
+        {!isGraded && !isGroupDiscussion && (
+          <View display="block" padding="medium none">
+            <CanvasMultiSelect
+              label={I18N.t('Post to')}
+              assistiveText={I18N.t(
+                'Select sections to post to. Type or use arrow keys to navigate. Multiple selections are allowed.'
+              )}
+              selectedOptionIds={sectionsToPostTo}
+              onChange={value => {
+                if (
+                  !sectionsToPostTo.includes(allSectionsOption.id) &&
+                  value.includes(allSectionsOption.id)
+                ) {
+                  setSectionsToPostTo([allSectionsOption.id])
+                } else {
+                  setSectionsToPostTo(value)
+                }
+              }}
+              width={inputWidth}
+            >
+              {[allSectionsOption, ...sections].map(({id, label}) => (
+                <CanvasMultiSelect.Option id={id} value={`opt-${id}`} key={id}>
+                  {label}
+                </CanvasMultiSelect.Option>
+              ))}
+            </CanvasMultiSelect>
+          </View>
+        )}
+        <Text size="large">{I18N.t('Options')}</Text>
+        <View display="block" margin="medium 0">
+          <RadioInputGroup
+            name="anonymous"
+            description={I18N.t('Anonymous Discussion')}
+            value={discussionAnonymousState}
+            onChange={(_event, value) => {
+              if (value !== 'off') {
+                setIsGraded(false)
+                setIsGroupDiscussion(false)
+                setGroupSet(null)
+              }
+              setDiscussionAnonymousState(value)
+            }}
+            disabled={isEditing}
+          >
+            <RadioInput
+              key="off"
+              value="off"
+              label={I18N.t(
+                'Off: student names and profile pictures will be visible to other members of this course'
+              )}
+            />
+            <RadioInput
+              key="partial_anonymity"
+              value="partial_anonymity"
+              label={I18N.t(
+                'Partial: students can choose to reveal their name and profile picture'
+              )}
+            />
+            <RadioInput
+              key="full_anonymity"
+              value="full_anonymity"
+              label={I18N.t('Full: student names and profile pictures will be hidden')}
+            />
+          </RadioInputGroup>
+          {!isEditing && discussionAnonymousState === 'partial_anonymity' && isStudent && (
+            <View display="block" margin="medium 0">
+              <AnonymousResponseSelector
+                username={ENV.current_user.display_name}
+                setAnonymousAuthorState={setAnonymousAuthorState}
+                discussionAnonymousState={discussionAnonymousState}
+              />
+            </View>
+          )}
+        </View>
+        <FormFieldGroup description="" rowSpacing="small">
+          <Checkbox
+            label={I18N.t('Participants must respond to the topic before viewing other replies')}
+            value="must-respond-before-viewing-replies"
+            checked={respondBeforeReply}
+            onChange={() => setRespondBeforeReply(!respondBeforeReply)}
+          />
+          <Checkbox
+            label={I18N.t('Enable podcast feed')}
+            value="enable-podcast-feed"
+            checked={enablePodcastFeed}
+            onChange={() => {
+              setIncludeRepliesInFeed(!enablePodcastFeed && includeRepliesInFeed)
+              setEnablePodcastFeed(!enablePodcastFeed)
+            }}
+          />
+          {enablePodcastFeed && (
+            <View display="block" padding="none none none large">
+              <Checkbox
+                label={I18N.t('Include student replies in podcast feed')}
+                value="include-student-replies-in-podcast-feed"
+                checked={includeRepliesInFeed}
+                onChange={() => setIncludeRepliesInFeed(!includeRepliesInFeed)}
+              />
+            </View>
+          )}
+          {discussionAnonymousState === 'off' && (
+            <Checkbox
+              label={I18N.t('Graded')}
+              value="graded"
+              checked={isGraded}
+              onChange={() => setIsGraded(!isGraded)}
+            />
+          )}
+          <Checkbox
+            label={I18N.t('Allow liking')}
+            value="allow-liking"
+            checked={allowLiking}
+            onChange={() => {
+              setOnlyGradersCanLike(!allowLiking && onlyGradersCanLike)
+              setAllowLiking(!allowLiking)
+            }}
+          />
+          {allowLiking && (
+            <View display="block" padding="none none none large">
+              <FormFieldGroup description="" rowSpacing="small">
+                <Checkbox
+                  label={I18N.t('Only graders can like')}
+                  value="only-graders-can-like"
+                  checked={onlyGradersCanLike}
+                  onChange={() => setOnlyGradersCanLike(!onlyGradersCanLike)}
+                />
+              </FormFieldGroup>
+            </View>
+          )}
+          {!isGraded && (
+            <Checkbox
+              label={I18N.t('Add to student to-do')}
+              value="add-to-student-to-do"
+              checked={addToTodo}
+              onChange={() => {
+                setTodoDate(!addToTodo ? todoDate : null)
+                setAddToTodo(!addToTodo)
+              }}
+            />
+          )}
+          {addToTodo && (
+            <View display="block" padding="none none none large">
+              <DateTimeInput
+                dateRenderLabel=""
+                timeRenderLabel=""
+                prevMonthLabel={I18N.t('previous')}
+                nextMonthLabel={I18N.t('next')}
+                onChange={(_event, newDate) => setTodoDate(newDate)}
+                value={todoDate}
+                invalidDateTimeMessage={I18N.t('Invalid date and time')}
+                layout="columns"
+              />
+            </View>
+          )}
+          {discussionAnonymousState === 'off' && (
+            <Checkbox
+              label={I18N.t('This is a Group Discussion')}
+              value="group-discussion"
+              checked={isGroupDiscussion}
+              onChange={() => {
+                setGroupSet(!isGroupDiscussion ? '' : groupSet)
+                setIsGroupDiscussion(!isGroupDiscussion)
+              }}
+            />
+          )}
+          {discussionAnonymousState === 'off' && isGroupDiscussion && (
+            <View display="block" padding="none none none large">
+              <SimpleSelect
+                renderLabel={I18N.t('Group Set')}
+                defaultValue=""
+                value={groupSet}
+                onChange={(_event, newChoice) => setGroupSet(newChoice.value)}
+                placeholder={I18N.t('Select Group')}
+                width={inputWidth}
+              >
+                {groups.map(({id, label}) => (
+                  <SimpleSelect.Option key={id} id={`opt-${id}`} value={id}>
+                    {label}
+                  </SimpleSelect.Option>
+                ))}
+              </SimpleSelect>
+            </View>
+          )}
+        </FormFieldGroup>
+        {isGraded ? (
+          <div>Graded options here</div>
+        ) : (
+          <FormFieldGroup description="" width={inputWidth}>
+            <DateTimeInput
+              description={I18N.t('Available from')}
+              dateRenderLabel=""
+              timeRenderLabel=""
+              prevMonthLabel={I18N.t('previous')}
+              nextMonthLabel={I18N.t('next')}
+              value={availableFrom}
+              onChange={(_event, newAvailableFrom) => {
+                validateAvailability(newAvailableFrom, availableUntil)
+                setAvailableFrom(newAvailableFrom)
+              }}
+              datePlaceholder={I18N.t('Select Date')}
+              invalidDateTimeMessage={I18N.t('Invalid date and time')}
+              layout="columns"
+            />
+            <DateTimeInput
+              description={I18N.t('Until')}
+              dateRenderLabel=""
+              timeRenderLabel=""
+              prevMonthLabel={I18N.t('previous')}
+              nextMonthLabel={I18N.t('next')}
+              value={availableUntil}
+              onChange={(_event, newAvailableUntil) => {
+                validateAvailability(availableFrom, newAvailableUntil)
+                setAvailableUntil(newAvailableUntil)
+              }}
+              datePlaceholder={I18N.t('Select Date')}
+              invalidDateTimeMessage={I18N.t('Invalid date and time')}
+              messages={availabiltyValidationMessages}
+              layout="columns"
+            />
+          </FormFieldGroup>
+        )}
+        <View
+          display="block"
+          textAlign="end"
+          borderWidth="small none none none"
+          margin="xx-large none"
+          padding="large none"
+        >
           <Button type="button" color="secondary">
             {I18N.t('Cancel')}
           </Button>
@@ -95,9 +419,17 @@ export default function DiscussionTopicForm({onSubmit}) {
 }
 
 DiscussionTopicForm.propTypes = {
+  isEditing: PropTypes.bool,
+  isStudent: PropTypes.bool,
+  sections: PropTypes.arrayOf(PropTypes.string),
+  groups: PropTypes.arrayOf(PropTypes.string),
   onSubmit: PropTypes.func,
 }
 
 DiscussionTopicForm.defaultProps = {
+  isEditing: false,
+  isStudent: false,
+  sections: [],
+  groups: [],
   onSubmit: () => {},
 }
