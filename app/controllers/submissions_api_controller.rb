09e295e012ad8d6b92a87d457d8af9a3005e2fb6
Tucker McKnight <tmcknight@instructure.com>
2023-07-12 13:03:51 -0600

    Document the online_quiz submission type
    When viewing a list of submissions or a single submission
    in the API, online_quiz can be one of the values for submission_type.
    
    flag = none
    
    Change-Id: Ifb19c4d9cf63a9f9e6540bc78bc12423693fec31
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/322595
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

--- app/controllers/submissions_api_controller.rb
+++ app/controllers/submissions_api_controller.rb
@@ -87,7 +87,7 @@
 #           "items": { "$ref": "SubmissionComment" }
 #         },
 #         "submission_type": {
-#           "description": "The types of submission ex: ('online_text_entry'|'online_url'|'online_upload'|'media_recording'|'student_annotation')",
+#           "description": "The types of submission ex: ('online_text_entry'|'online_url'|'online_upload'|'online_quiz'|'media_recording'|'student_annotation')",
 #           "example": "online_text_entry",
 #           "type": "string",
 #           "allowableValues": {
@@ -95,6 +95,7 @@
 #               "online_text_entry",
 #               "online_url",
 #               "online_upload",
+#               "online_quiz",
 #               "media_recording",
 #               "student_annotation"
 #             ]
