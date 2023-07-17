764838e8a162dc32a5ad1b194787fbebb264addc
Cody Cutrer <cody@instructure.com>
2023-07-17 14:28:57 -0600

    remove old monkeypatch for an optimization now in upstream rails
    Change-Id: I7fa12ac47729d65c683ff0cd1a20625a3827ac10
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/322900
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -2113,43 +2113,6 @@ Rails.application.config.after_initialize do
   end
 end
 
-# this can be removed if/when https://github.com/rails/rails/pull/43036 is merged
-# and we get up to date on that rails version
-module Serialization
-  # significant change: replace attributes.keys with attribute_names
-  def serializable_hash(options = nil)
-    # these lines are from ActiveRecord::Serialization
-    options = options.try(:dup) || {}
-
-    options[:except] = Array(options[:except]).map(&:to_s)
-    options[:except] |= Array(self.class.inheritance_column)
-
-    # the rest of this method is from ActiveModel::Serialization
-    attribute_names = self.attribute_names
-    if (only = options[:only])
-      attribute_names &= Array(only).map(&:to_s)
-    elsif (except = options[:except])
-      attribute_names -= Array(except).map(&:to_s)
-    end
-
-    hash = {}
-    attribute_names.each { |n| hash[n] = read_attribute_for_serialization(n) }
-
-    Array(options[:methods]).each { |m| hash[m.to_s] = send(m) }
-
-    serializable_add_includes(options) do |association, records, opts|
-      hash[association.to_s] = if records.respond_to?(:to_ary)
-                                 records.to_ary.map { |a| a.serializable_hash(opts) }
-                               else
-                                 records.serializable_hash(opts)
-                               end
-    end
-
-    hash
-  end
-end
-ActiveRecord::Base.include(Serialization)
-
 module UserContentSerialization
   def serializable_hash(options = nil)
     result = super
