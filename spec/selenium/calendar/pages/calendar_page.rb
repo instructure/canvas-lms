113d32749457f0b2936c79cbb010c65aad47be0c
Robin Kuss <rkuss@instructure.com>
2023-07-20 12:49:11 -0500

    spec: new tests for recurring calendar events
    Closes LF-474
    
    Test Plan: passes jenkins
    
    Change-Id: I5600b1558ad70a629fbf21b2277dbab1a0383642
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/323333
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Juan Chavez <juan.chavez@instructure.com>
    QA-Review: Juan Chavez <juan.chavez@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/calendar/pages/calendar_page.rb
+++ spec/selenium/calendar/pages/calendar_page.rb
@@ -33,8 +33,24 @@ module CalendarPage
     "#edit_calendar_event_form #calendar_event_title"
   end
 
+  def edit_event_date_input_selector
+    "[data-testid='edit-calendar-event-form-date']"
+  end
+
   def edit_event_modal_submit_btn_selector
     "#edit_calendar_event_form button.event_button"
+  end
+
+  def events_in_month_view_selector
+    ".fc-event"
+  end
+
+  def frequency_picker_selector
+    "[data-testid='frequency-picker']"
+  end
+
+  def submit_button_selector
+    "[data-testid='edit-calendar-event-submit-button']"
   end
 
   def use_section_dates_checkbox_selector
@@ -42,12 +58,20 @@ module CalendarPage
   end
 
   #------------------------- Elements ---------------------------
+  def all_events_in_month_view
+    ff(events_in_month_view_selector)
+  end
+
   def create_new_event_btn
     f(create_new_event_btn_selector)
   end
 
   def delete_confirm_button
     fxpath(delete_confirm_button_selector)
+  end
+
+  def edit_event_date_input
+    f(edit_event_date_input_selector)
   end
 
   def edit_event_title_input
@@ -58,13 +82,30 @@ module CalendarPage
     f(edit_event_modal_submit_btn_selector)
   end
 
+  def frequency_picker
+    f(frequency_picker_selector)
+  end
+
+  def submit_button
+    f(submit_button_selector)
+  end
+
   def use_section_dates_checkbox
     f(use_section_dates_checkbox_selector)
   end
 
   #----------------------- Actions/Methods ----------------------
+  def add_calendar_event_title(title_text)
+    replace_content(edit_event_title_input, title_text)
+  end
+
   def click_delete_confirm_button
     delete_confirm_button.click
+  end
+
+  def click_submit_button
+    submit_button.click
+    wait_for_ajaximations
   end
 
   def create_new_calendar_event
@@ -72,8 +113,17 @@ module CalendarPage
     wait_for_ajaximations
   end
 
-  def add_calendar_event_title(title_text)
-    replace_content(edit_event_title_input, title_text)
+  def enter_new_event_date(date_text)
+    replace_content(edit_event_date_input, date_text)
+    edit_event_date_input.send_keys(:enter)
+  end
+
+  def frequency_picker_value
+    element_value_for_attr(frequency_picker, "value")
+  end
+
+  def select_frequency_option(option_text)
+    click_option(frequency_picker_selector, option_text)
   end
 
   def submit_calendar_event_changes
