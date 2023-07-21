fc9b67d9f77f3bcf9a9fa740ee3d11850f4bff0e
rohan.chugh <rohan.chugh@instructure.com>
2023-07-20 20:14:45 -0500

    add mutations for custom grade statuses
    add upsert mutation for custom and standard grade statuses
    add delete mutation for custom grade statuses
    
    closes EVAL-3240
    
    flag=grading_scheme_updates
    flag=custom_gradebook_statuses
    
    test plan:
    - ensure FFs are enabled
    - go to graphiql
    - click on add new mutation
    - ensure that "deleteCustomGradeStatus", "upsertCustomGradeStatus",
      and "upsertStandardGradeStatus" are available mutations in the list
    - create a new custom grade status with the upsert query, leaving
      the id blank, ensure it returns the new status
    - either using graphiql through account->custom grade statuses
      connection or the rails console, ensure the new status was created
      (CustomGradeStatus.find(id) in rails console)
    - update that status with the same query but specifying the id of the
      newly created status and changing the color and name, ensure it
      returns the updated status
    - using graphiql or the rails console, ensure the status was updated
    - delete the status with the delete mutation, ensure it returns the
      deleted status's id and that the status is soft deleted with a
      workflow state of deleted in the database
    - ensure that the status is no longer returned in the list of custom
      grade statuses from graphiql or in rails console with the command
      Account.find(2).custom_grade_statuses.active (mimics the graphql
      query)
    - do the same for standard grade statuses aside from delete since
      there is no mutation for that. ensure that when updating, you only
      update the color but still pass in the name and when creating,
      the name is one of the following: "missing", "late", "resubmitted",
       "dropped", "excused", "extended"
    
    Change-Id: I867842b42105e05a600db8440d42958e7db35880
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/323309
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Christopher Soto <christopher.soto@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Christopher Soto <christopher.soto@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

---
+++ app/graphql/mutations/upsert_standard_grade_status.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+module Mutations
+  class UpsertStandardGradeStatus < BaseMutation
+    argument :color, String, required: true
+    argument :id, ID, required: false
+    argument :name, String, required: true
+    field :standard_grade_status, Types::StandardGradeStatusType, null: true
+
+    def resolve(input:)
+      raise GraphQL::ExecutionError, "custom gradebook statuses feature flag is disabled" unless Account.site_admin.feature_enabled?(:custom_gradebook_statuses)
+
+      root_account = context[:request].env["canvas.domain_root_account"] || LoadAccount.default_domain_root_account
+      standard_grade_status = input[:id] ? StandardGradeStatus.find_by!(id: input[:id], status_name: input[:name]) : StandardGradeStatus.new(root_account:, status_name: input[:name])
+      # TODO: add multi-tenant shard test
+      if standard_grade_status.update(color: input[:color])
+        { standard_grade_status: }
+      else
+        errors_for(standard_grade_status)
+      end
+    rescue ActiveRecord::RecordNotFound
+      raise GraphQL::ExecutionError, "standard grade status not found"
+    end
+  end
+end
