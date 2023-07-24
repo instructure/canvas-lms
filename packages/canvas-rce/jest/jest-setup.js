45d3364f64a172be4fd7c42790f4bd3a991a11a9
Jackson Howe <jackson.howe@instructure.com>
2023-07-24 13:32:21 -0600

    Fail canvas-rce jest tests with console errors/warnings
    An exception list is created for the existing errors and warnings
    so that we can block new errors moving forward.
    
    refs LF-535
    flag = none
    
    Test plan:
     - Without this commit, run `yarn test:jest` in packages/canvas-rce
     - Expect to see a bunch of console errors/warnings
     - With this commit loaded, run `yarn test:jest` again
     - Expect to see much fewer errors/ warnings
     - Add a new error to a test
     - Run `yarn test:jest` once more
     - Expect the test to fail
    
    Change-Id: Ieff22934df9dede55cd4dc8aa5379edb18610cf5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/323449
    Reviewed-by: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Jake Oeding <jake.oeding@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- packages/canvas-rce/jest/jest-setup.js
+++ packages/canvas-rce/jest/jest-setup.js
@@ -21,6 +21,110 @@
 import {StyleSheetTestUtils} from 'aphrodite'
 import {filterUselessConsoleMessages} from '@instructure/js-utils'
 
+/**
+ * We want to ensure errors and warnings get appropriate eyes. If
+ * you are seeing an exception from here, it probably means you
+ * have an unintended consequence from your changes. If you expect
+ * the warning/error, add it to the ignore list below.
+ */
+/* eslint-disable no-console */
+const globalError = global.console.error
+const ignoredErrors = [
+  /An update to %s inside a test was not wrapped in act/,
+  /Can't perform a React state update on an unmounted component/,
+  /Unknown event handler property `%s`/,
+  /React does not recognize the `%s` prop on a DOM element/,
+  /A component is changing a controlled input of type %s to be uncontrolled/,
+  /A component is changing an uncontrolled input of type %s to be controlled./,
+  /The prop `editor` is marked as required in `IconMakerTray`/,
+  /The prop `renderLabel` is marked as required in `SimpleSelect`/,
+  /The prop `sortBy.order` is marked as required in `Images`/,
+  /SyntaxError: unknown pseudo-class selector ':host>/,
+  /The 'screenReaderOnly' prop must be used in conjunction with 'liveRegion'/,
+  /You seem to have overlapping act\(\) calls/,
+  /The prop `onLinkClick` is marked as required in `DocumentsPanel`/,
+  /Invalid prop `documents.searchString` of type `string` supplied to `DocumentsPanel`/,
+  /The prop `onImageClick` is marked as required in `ImageList`/,
+  /The prop `sortBy.order` is marked as required in `DocumentsPanel`/,
+  /The prop `sortBy.order` is marked as required in `SavedIconMakerList`/,
+  /Invalid prop `images.searchString` of type `string` supplied to `SavedIconMakerList`/,
+  /The prop `onFileSelect` is marked as required in `RceFileBrowser`/,
+  /The prop `refreshToken` is marked as required in `RceFileBrowser`/,
+  /The prop `editor` is marked as required in `ImageSection`/,
+  /The prop `onImageEmbed` is marked as required in `Images`/,
+  /Invalid prop `media.searchString` of type `string` supplied to `MediaPanel`/,
+  /The prop `label` is marked as required in `FormField`/,
+  /The prop `label` is marked as required in `FormFieldLayout`/,
+  /Can't call %s on a component that is not yet mounted./,
+  /The prop `dimensionsState.usePercentageUnits` is marked as required in `DimensionsInput`/,
+  /The prop `videoOptions.naturalHeight` is marked as required in `VideoOptionsTray`/,
+  /The prop `renderLabel` is marked as required in `Select`/,
+  /The prop `renderLabel` is marked as required in `NumberInput`/,
+  /The prop `onLinkClick` is marked as required in `LinkSet`/,
+  /The prop `onMediaEmbed` is marked as required in `MediaPanel`/,
+  /The prop `sortBy.order` is marked as required in `MediaPanel`/,
+  /Invalid prop `images.searchString` of type `string` supplied to `Images`/,
+  /Invalid prop `source` of type `object` supplied to `RceFileBrowser`/,
+  /unknown pseudo-class selector'/,
+  /Invalid URL: undefined/,
+  /failed updating video captions/,
+  /The prop `state.compressed` is marked as required in `ImageOptions`/,
+  /The prop `state.image` is marked as required in `ImageOptions`/,
+  /The prop `data.icon` is marked as required in `SingleColor`/,
+  /The content `content` is marked as required in `LinkOptionsTray`/,
+  /The prop `host` is marked as required in `RceFileBrowser`/,
+  /The prop `jwt` is marked as required in `RceFileBrowser`/,
+  /The prop `source` is marked as required in `RceFileBrowser`/,
+  /The prop `contextType` is marked as required in `DocumentsPanel`/,
+  /The prop `media.course.files\[0].title` is marked as required in `MediaPanel`/,
+  /The prop `contextType` is marked as required in `MediaPanel`/,
+  /The prop `audioOptions.titleText` is marked as required in `AudioOptionsTray`/,
+  /The prop `audioOptions.id` is marked as required in `AudioOptionsTray`/,
+  /The prop `trayProps` is marked as required in `AudioOptionsTray`/,
+  /Invalid prop `audioOptions` of type `boolean` supplied to `AudioOptionsTray`/,
+  /The prop `context` is marked as required in `RceFileBrowser`/,
+  /Invalid value for prop %s on <%s> tag./,
+  /Invalid prop `audioOptions` of type `boolean` supplied to `AudioOptionsTray`/,
+  /Not implemented: navigation/,
+  /Not implemented: HTMLFormElement.prototype.submit/,
+  /The prop `images.course.files\[0].content_type` is marked as required in `Images`/,
+  /The prop `canvasOrigin` is marked as required in `Upload`/,
+  /Cannot read properties of undefined \(reading 'canvadoc_session_url'\)/,
+]
+const globalWarn = global.console.warn
+const ignoredWarnings = [
+  /componentWillReceiveProps has been renamed/,
+  /Store interaction failed/,
+  /Unsupported LTI 1.3 Content Item/,
+  /Found bad LTI MRU data/,
+  /Cannot save LTI MRU list/,
+  /clicked sidebar (link|image) without a focused editor/,
+]
+global.console = {
+  log: console.log,
+  error: error => {
+    if (ignoredErrors.some(regex => regex.test(error))) {
+      return
+    }
+    globalError(error)
+    throw new Error(
+      `Looks like you have an unhandled error. Keep our test logs clean by handling or filtering it. ${error}`
+    )
+  },
+  warn: warning => {
+    if (ignoredWarnings.some(regex => regex.test(warning))) {
+      return
+    }
+    globalWarn(warning)
+    throw new Error(
+      `Looks like you have an unhandled warning. Keep our test logs clean by handling or filtering it. ${warning}`
+    )
+  },
+  info: console.info,
+  debug: console.debug,
+}
+/* eslint-enable no-console */
+
 filterUselessConsoleMessages(console)
 StyleSheetTestUtils.suppressStyleInjection()
 
