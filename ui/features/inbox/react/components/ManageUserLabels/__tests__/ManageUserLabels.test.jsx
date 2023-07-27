303171dd6e9d1234fcf36e7fbe587313670ff910
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2023-07-27 11:56:56 -0400

    Create ManageUserLabels component
    refs VICE-3679
    flag=none
    
    test plan:
      - Specs pass.
      - Use storybook to open the ManageUserLabels component
          and test it's functionality.
      - Try adding labels, removing them, trying to duplicate
          them, etc.
      - Add button should not be enabled if the input is
          empty.
      - Save/Close button will reset the state and that's
          intended.
      - Both of those are supposed to close the component
          but does not happen in Storybook because the
          open state will be controlled by an upper component.
    
    qa risk: low
    
    Change-Id: I60f64548f97ffa2f2c3c42bccb339b86c80f4a01
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/323735
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/inbox/react/components/ManageUserLabels/__tests__/ManageUserLabels.test.jsx
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import {ManageUserLabels} from '../ManageUserLabels'
+
+const createProps = overrides => {
+  return {
+    open: true,
+    labels: ['Assignment Info', 'Important'],
+    onCreate: jest.fn(),
+    onDelete: jest.fn(),
+    onClose: jest.fn(),
+    ...overrides,
+  }
+}
+
+describe('ManageUserLabels', () => {
+  it('renders', () => {
+    const {container} = render(<ManageUserLabels {...createProps()} />)
+
+    expect(container).toBeInTheDocument()
+  })
+
+  it('renders the correct number of labels', () => {
+    const {getAllByTestId} = render(<ManageUserLabels {...createProps()} />)
+
+    expect(getAllByTestId('label')).toHaveLength(2)
+  })
+
+  it('correctly adds labels', () => {
+    const props = createProps()
+    const {getByLabelText, getByTestId, getAllByTestId, getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'New Label'}})
+    fireEvent.click(getByTestId('add-label'))
+
+    expect(getAllByTestId('label')).toHaveLength(3)
+    expect(getByText('New Label')).toBeInTheDocument()
+  })
+
+  it('shows error if trying to add a label that already exists', () => {
+    const props = createProps()
+    const {getByLabelText, getByTestId, getAllByTestId, getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'Important'}})
+    fireEvent.click(getByTestId('add-label'))
+
+    expect(getAllByTestId('label')).toHaveLength(2)
+    expect(getByText('The specified label already exists. Please enter a different label name.')).toBeInTheDocument()
+  })
+
+  it('correctly deletes labels', () => {
+    const props = createProps()
+    const {getAllByTestId} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.click(getAllByTestId('delete-label')[0])
+
+    expect(getAllByTestId('label')).toHaveLength(1)
+  })
+
+  it('correctly closes the modal', () => {
+    const props = createProps()
+    const {getAllByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.click(getAllByText('Close')[1])
+
+    expect(props.onClose).toHaveBeenCalled()
+  })
+
+  it('calls onCreate when the Save button is clicked', () => {
+    const props = createProps()
+    const {getByLabelText, getByTestId, getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'Test 1'}})
+    fireEvent.click(getByTestId('add-label'))
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'Test 2'}})
+    fireEvent.click(getByTestId('add-label'))
+
+    fireEvent.click(getByText('Save'))
+
+    expect(props.onCreate).toHaveBeenCalled()
+    expect(props.onCreate).toHaveBeenCalledWith(['Test 1', 'Test 2'])
+  })
+
+  it('does not calls onCreate when the Save button is clicked and no labels are added', () => {
+    const props = createProps()
+    const {getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.click(getByText('Save'))
+
+    expect(props.onCreate).not.toHaveBeenCalled()
+  })
+
+  it('calls onDelete when the Save button is clicked', () => {
+    const props = createProps()
+    const {getAllByTestId, getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.click(getAllByTestId('delete-label')[0])
+
+    fireEvent.click(getByText('Save'))
+
+    expect(props.onDelete).toHaveBeenCalled()
+    expect(props.onDelete).toHaveBeenCalledWith(['Assignment Info'])
+  })
+
+  it('does not calls onDelete when the Save button is clicked and no labels are deleted', () => {
+    const props = createProps()
+    const {getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.click(getByText('Save'))
+
+    expect(props.onDelete).not.toHaveBeenCalled()
+  })
+
+  it('does not calls onDelete when the Save button is clicked and unsaved labels are deleted', () => {
+    const props = createProps()
+    const {getByLabelText, getByTestId, getAllByTestId, getByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'Beta'}})
+    fireEvent.click(getByTestId('add-label'))
+    fireEvent.click(getAllByTestId('delete-label')[1])
+
+    fireEvent.click(getByText('Save'))
+
+    expect(props.onDelete).not.toHaveBeenCalled()
+  })
+
+  it('component resets to its initial state when the modal is closed', () => {
+    const props = createProps()
+    const {getByLabelText, getByTestId, getAllByTestId, getByText, getAllByText} = render(<ManageUserLabels {...props} />)
+
+    fireEvent.change(getByLabelText('Label Name'), {target: {value: 'New Label'}})
+    fireEvent.click(getByTestId('add-label'))
+
+    expect(getAllByTestId('label')).toHaveLength(3)
+    expect(getByText('New Label')).toBeInTheDocument()
+
+    fireEvent.click(getAllByText('Close')[1])
+
+    expect(getAllByTestId('label')).toHaveLength(2)
+  })
+})
