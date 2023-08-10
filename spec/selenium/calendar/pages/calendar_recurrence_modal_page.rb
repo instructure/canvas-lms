1d421f3a82bb8cfd515fccca0f4885a6cf6e8819
Robin Kuss <rkuss@instructure.com>
2023-08-10 10:50:13 -0500

    spec: add recurring events test for custom modal
    Closes LF-477
    
    Test Plan: Passes Jenkins
    
    Change-Id: I6c8bad1880ac42f0032d227f73ff7206e24cf51d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/324881
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/calendar/pages/calendar_recurrence_modal_page.rb
+++ spec/selenium/calendar/pages/calendar_recurrence_modal_page.rb
@@ -21,6 +21,10 @@ require_relative "../../common"
 
 module CalendarRecurrenceModelPage
   #------------------------- Selectors --------------------------
+  def after_radio_button_selector
+    "//*[@aria-label='Custom Repeating Event']//label[../input[@value = 'AFTER']]"
+  end
+
   def cancel_button_selector
     "//*[@aria-label='Custom Repeating Event']//button[.//*[. = 'Cancel']]"
   end
@@ -29,15 +33,48 @@ module CalendarRecurrenceModelPage
     "[aria-label='Custom Repeating Event']"
   end
 
+  def day_of_week_input_selector(day)
+    "##{day}"
+  end
+
+  def day_of_week_label_selector(day)
+    "//label[*//input[@id = '#{day}']]"
+  end
+
   def done_button_selector
     "//*[@aria-label='Custom Repeating Event']//button[.//*[. = 'Done']]"
+  end
+
+  def on_radio_button_selector
+    "//*[@aria-label='Custom Repeating Event']//label[../input[@value = 'ON']]"
+  end
+
+  def recurrence_end_count_input_selector
+    "[data-testid='recurrence-end-count-input']"
+  end
+
+  def recurrence_end_date_selector
+    "//*[@aria-label='Custom Repeating Event']//input[@aria-haspopup = 'listbox' and @role = 'combobox']"
+  end
+
+  def repeat_month_mode_selector
+    "[data-testid='repeat-month-mode']"
   end
 
   def repeat_frequency_selector
     "[data-testid='repeat-frequency']"
   end
 
+  def repeat_interval_selector
+    "[data-testid='repeat-interval']"
+  end
+
   #------------------------- Elements ---------------------------
+
+  def after_radio_button
+    fxpath(after_radio_button_selector)
+  end
+
   def cancel_button
     fxpath(cancel_button_selector)
   end
@@ -46,21 +83,76 @@ module CalendarRecurrenceModelPage
     f(custom_recurrence_modal_selector)
   end
 
+  def day_selection_checkbox(day)
+    fxpath(day_of_week_label_selector(day))
+  end
+
+  def day_of_week_input(day)
+    f(day_of_week_input_selector(day))
+  end
+
   def done_button
     fxpath(done_button_selector)
+  end
+
+  def on_radio_button
+    fxpath(on_radio_button_selector)
+  end
+
+  def recurrence_end_count_input
+    f(recurrence_end_count_input_selector)
+  end
+
+  def recurrence_end_date
+    fxpath(recurrence_end_date_selector)
+  end
+
+  def repeat_month_mode
+    f(repeat_month_mode_selector)
   end
 
   def repeat_frequency
     f(repeat_frequency_selector)
   end
 
+  def repeat_interval
+    f(repeat_interval_selector)
+  end
+
   #----------------------- Actions/Methods ----------------------
+  def click_after_radio_button
+    after_radio_button.click
+  end
+
   def click_cancel_button
     cancel_button.click
   end
 
+  def click_day_selection_checkbox(day)
+    day_selection_checkbox(day).click
+  end
+
   def click_done_button
     done_button.click
+  end
+
+  def click_on_radio_button
+    on_radio_button.click
+  end
+
+  def enter_recurrence_end_date(date)
+    recurrence_end_date.send_keys([:control, "a"], :backspace)
+    replace_content(recurrence_end_date, date, tab_out: true)
+  end
+
+  def enter_repeat_interval(interval)
+    repeat_interval.send_keys([:control, "a"], :backspace)
+    replace_content(repeat_interval, interval, tab_out: true)
+  end
+
+  def enter_recurrence_end_count(end_count)
+    recurrence_end_count_input.send_keys([:control, "a"], :backspace)
+    replace_content(recurrence_end_count_input, end_count, tab_out: true)
   end
 
   def recurrence_modal_exists?
@@ -69,6 +161,18 @@ module CalendarRecurrenceModelPage
 
   def repeat_frequency_picker_value
     element_value_for_attr(repeat_frequency, "value")
+  end
+
+  def repeat_interval_value
+    element_value_for_attr(repeat_interval, "value")
+  end
+
+  def recurrence_end_count_value
+    element_value_for_attr(recurrence_end_count_input, "value")
+  end
+
+  def select_repeat_month_mode(mode)
+    click_option(repeat_month_mode_selector, mode)
   end
 
   def select_repeat_frequency(frequency)
