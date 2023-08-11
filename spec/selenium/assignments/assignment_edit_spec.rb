eeba676342351e0ba2f537473b37e62009629454
Ed Schiebel <eschiebel@instructure.com>
2023-08-11 09:34:39 -0400

    Fix jquery date-time picker regression
    closes LF-573
    flag=none
    
    test plan:
      - edit an assignment
      - change the due date using the popup calendar
      > expect it to work
      - change the due date by typing into the input box
      > expec it to work too
    
    Change-Id: I472457cee21326508c617186b1d9d7913ace88c0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/324984
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- spec/selenium/assignments/assignment_edit_spec.rb
+++ spec/selenium/assignments/assignment_edit_spec.rb
@@ -151,4 +151,29 @@ describe "assignment" do
       expect(@new_quiz.reload.omit_from_final_grade).to be true
     end
   end
+
+  context "due date" do
+    before do
+      course_with_teacher(active_all: true)
+      user_session(@teacher)
+    end
+
+    it "fills the due date field from a selection in the popup calendar" do
+      time = DateTime.new(2023, 8, 9, 12, 0, 0, 0, 0) # this is a Wednesday
+      Timecop.freeze(time) do
+        @assignment = @course.assignments.create!(due_at: time, points_possible: 10)
+
+        AssignmentCreateEditPage.visit_assignment_edit_page(@course.id, @assignment.id)
+        AssignmentCreateEditPage.due_date_picker_btn.click
+        expect(AssignmentCreateEditPage.due_date_picker_popup).to be_displayed
+
+        # click the next day (Thursday the 10th)
+        f("td.ui-datepicker-current-day + td").click
+        AssignmentCreateEditPage.due_date_picker_done_btn.click
+        expect(AssignmentCreateEditPage.due_date_input.attribute("value")).to eq(
+          format_time_for_datepicker(DateTime.new(2023, 8, 10, 12, 0, 0, 0, 0))
+        )
+      end
+    end
+  end
 end
