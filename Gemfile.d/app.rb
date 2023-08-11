d40d00f5216059551069b1adc52600ca82213c90
anmg <a.gangan@gmail.com>
2023-08-11 09:39:50 +0300

    Update app.rb

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -105,7 +105,7 @@ gem "oj", "3.10.16"
 gem "outrigger", "3.0.1"
 gem "parallel", "1.22.1", require: false
 gem "pdf-reader", "2.5.0"
-gem "pg_query", "2.2.0"
+gem "pg_query", "2.2.1"
 gem "prawn-emoji", "~> 5.3", require: false
 gem "prawn-rails", "1.3.0"
   gem "matrix", "0.4.2" # Used to be a default gem but is no more, but prawn depends on it implicitly
