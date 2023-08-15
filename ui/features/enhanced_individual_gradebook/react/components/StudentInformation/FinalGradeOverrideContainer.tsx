0ee6a4ad5a04d36960f4405602d34d49a3dfa613
rohan.chugh <rohan.chugh@instructure.com>
2023-08-15 14:50:56 -0500

    move final grade text box to shared component
    move the final grade override text box from enhanced individual
    gradebook to a shared component to be reused in the traditional
    gradebook total grade override tray
    
    closes EVAL-3416
    
    flag=individual_gradebook_enhancements
    
    test plan:
    - have a course with a student and a grading scheme and grading periods
    - enable final grade override
    - go to the enhanced individual gradebook (either by enabling the ff
      or adding ?version=individual_enhanced to the url of the normal srgb)
    - choose a student and scroll to the override text box
    - enter a grade to change it
    - if a numeric grade was entered, the letter grade should appear in the
      box and the numeric percentage should appear to the right side of the
      box if grading schemes are enabled
    - if grading schemes are not enabled, it should show the numeric grade
      in the text box
    - if a letter grade was entered, the letter grade should appear in the
      box and the lower bound numeric percentage of the grading scheme
      should appear to the right side of the box if grading schemes
      are enabled
    - do this for a grading period, all grading periods, and a course
      without grading periods
    
    Change-Id: If2e3e35ab7d0494f06ee079182ed537c15caf94f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/325289
    Reviewed-by: Christopher Soto <christopher.soto@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Christopher Soto <christopher.soto@instructure.com>
    Product-Review: Jody Sailor

---
+++ ui/features/enhanced_individual_gradebook/react/components/StudentInformation/FinalGradeOverrideContainer.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {View} from '@instructure/ui-view'
+import React, {useState} from 'react'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {DeprecatedGradingScheme, FinalGradeOverride} from '@canvas/grading/grading'
+import {updateFinalGradeOverride} from '@canvas/grading/FinalGradeOverrideApi'
+import {ApiCallStatus} from '../../../types'
+import {FinalGradeOverrideTextBox} from '@canvas/final-grade-override'
+import GradeOverrideInfo from '@canvas/grading/GradeEntry/GradeOverrideInfo'
+
+const I18n = useI18nScope('enhanced_individual_gradebook')
+type Props = {
+  finalGradeOverride?: FinalGradeOverride
+  gradingScheme?: DeprecatedGradingScheme | null
+  enrollmentId: string
+  onSubmit: (finalGradeOverride: FinalGradeOverride) => void
+  gradingPeriodId?: string
+  pointsBasedGradingSchemesFeatureEnabled: boolean
+}
+function FinalGradeOverrideContainer({
+  finalGradeOverride,
+  gradingScheme,
+  enrollmentId,
+  onSubmit,
+  gradingPeriodId,
+  pointsBasedGradingSchemesFeatureEnabled,
+}: Props) {
+  const [apiCallStatus, setApiCallStatus] = useState<ApiCallStatus>(ApiCallStatus.NOT_STARTED)
+
+  const handleFinalGradeOverrideChange = async (grade: GradeOverrideInfo) => {
+    if (grade.grade?.percentage == null) {
+      return
+    }
+    const castedEnteredGrade = {percentage: grade.grade.percentage}
+    setApiCallStatus(ApiCallStatus.PENDING)
+    const updatedScore = await updateFinalGradeOverride(
+      enrollmentId,
+      gradingPeriodId,
+      castedEnteredGrade
+    )
+    setApiCallStatus(ApiCallStatus.COMPLETED)
+    if (updatedScore) {
+      if (gradingPeriodId) {
+        onSubmit({
+          ...finalGradeOverride,
+          gradingPeriodGrades: {
+            ...finalGradeOverride?.gradingPeriodGrades,
+            [gradingPeriodId]: updatedScore,
+          },
+        })
+      } else {
+        onSubmit({...finalGradeOverride, courseGrade: updatedScore})
+      }
+    }
+  }
+  return (
+    <>
+      <View as="div">
+        <label htmlFor="final-grade-override-input">
+          <strong>{I18n.t('Final Grade Override')}</strong>
+        </label>
+      </View>
+      <FinalGradeOverrideTextBox
+        finalGradeOverride={finalGradeOverride}
+        gradingScheme={gradingScheme}
+        gradingPeriodId={gradingPeriodId}
+        pointsBasedGradingSchemesFeatureEnabled={pointsBasedGradingSchemesFeatureEnabled}
+        onGradeChange={handleFinalGradeOverrideChange}
+        disabled={apiCallStatus === ApiCallStatus.PENDING}
+        showPercentageLabel={true}
+      />
+    </>
+  )
+}
+
+export default FinalGradeOverrideContainer
