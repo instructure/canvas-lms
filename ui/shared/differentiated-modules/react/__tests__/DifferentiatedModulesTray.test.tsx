c065a2428b52ff100920a34f6f99ccf7ba895057
jake.oeding <jake.oeding@instructure.com>
2023-08-18 14:21:30 -0400

    add tabs to differentiated modules tray
    closes LF-610
    flag=differentiated_modules
    qa risK: low
    
    test plan:
    -go to the modules page
    -open a module menu, click "assign to"
    >expect the tray to open to the assign to tab
    -click the settings tab
    >confirm that it switches and loads the settings tab
    
    Change-Id: I2d71f278ddf47aedc60a0226f796f58516201650
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/325618
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Jake Oeding <jake.oeding@instructure.com>

---
+++ ui/shared/differentiated-modules/react/__tests__/DifferentiatedModulesTray.test.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import DifferentiatedModulesTray, {
+  DifferentiatedModulesTrayProps,
+} from '../DifferentiatedModulesTray'
+
+describe('DifferentiatedModulesTray', () => {
+  const props: DifferentiatedModulesTrayProps = {
+    open: true,
+    onDismiss: () => {},
+    initialTab: 'assign-to',
+    assignOnly: true,
+  }
+
+  const renderComponent = (overrides = {}) =>
+    render(<DifferentiatedModulesTray {...props} {...overrides} />)
+
+  it('should render', () => {
+    const {getByText} = renderComponent()
+    expect(getByText('Edit Module Settings')).toBeInTheDocument()
+  })
+
+  it('calls onDismiss when close button is clicked', () => {
+    const onDismiss = jest.fn()
+    const {getByRole} = renderComponent({onDismiss})
+    getByRole('button', {name: /close/i}).click()
+    expect(onDismiss).toHaveBeenCalled()
+  })
+
+  it('does not render tabs when assignOnly is true', () => {
+    const {queryByTestId} = renderComponent()
+    expect(queryByTestId('assign-to-panel')).not.toBeInTheDocument()
+    expect(queryByTestId('settings-panel')).not.toBeInTheDocument()
+  })
+
+  it('renders tabs when assignOnly is false', () => {
+    const {getByTestId} = renderComponent({assignOnly: false})
+    expect(getByTestId('assign-to-panel')).toBeInTheDocument()
+    expect(getByTestId('settings-panel')).toBeInTheDocument()
+  })
+})
