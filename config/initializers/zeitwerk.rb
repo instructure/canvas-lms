625081ffffd9ec9d03990f8b1a8b6d28b0553f0b
Tucker McKnight <tmcknight@instructure.com>
2023-08-22 15:56:41 -0600

    spec: remove tests related to CDC
    Change-Id: I604ae5d9b9152de85a8389ddf8fcb002f41f25b3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/325871
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -26,9 +26,7 @@ Rails.autoloaders.main.ignore(
   # we don't want zeitwerk to try to eager_load some "Version" constant from any plugins
   "#{__dir__}/../../gems/plugins/simply_versioned/lib/simply_versioned/gem_version.rb",
   "#{__dir__}/../../gems/plugins/account_reports/lib/account_reports/version.rb",
-  "#{__dir__}/../../gems/plugins/moodle_importer/lib/moodle_importer/version.rb",
-  "#{__dir__}/../../lib/cdc_migration_testing", # these aren't really modules we use in canvas runtime
-  "#{__dir__}/../../gems/plugins/*/lib/cdc_migration_testing"
+  "#{__dir__}/../../gems/plugins/moodle_importer/lib/moodle_importer/version.rb"
 )
 
 Rails.autoloaders.each do |autoloader|
