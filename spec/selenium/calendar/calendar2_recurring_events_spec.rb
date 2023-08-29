af9a4ae7b0c3c430305c493c45efd6a2031b8e63
Robin Kuss <rkuss@instructure.com>
2023-08-29 11:07:12 -0500

    add more calendar recurrent event tests
    Closes LF-605
    
    Test Plan: passes Jenkins
    
    Change-Id: Icfadcbe84b81ff3c9bac899789f7117ac736ff8b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/326644
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/calendar/calendar2_recurring_events_spec.rb
+++ spec/selenium/calendar/calendar2_recurring_events_spec.rb
@@ -400,7 +400,7 @@ describe "recurring events" do
     it "deletes 'this event' from a series" do
       get "/calendar"
 
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -408,17 +408,17 @@ describe "recurring events" do
       event_series_this_event.click
       event_series_delete_button.click
       wait_for_ajax_requests
-      expect(events_in_a_series.length).to eq 2
+      expect(events_in_a_series("event in a series").length).to eq 2
 
       # make sure it was actually deleted and not just removed from the interface
       get("/calendar")
-      expect(events_in_a_series.length).to eq 2
+      expect(events_in_a_series("event in a series").length).to eq 2
     end
 
     it "deletes 'this event and all following' from a series" do
       get "/calendar"
 
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -426,17 +426,17 @@ describe "recurring events" do
       event_series_following_events.click
       event_series_delete_button.click
       wait_for_ajax_requests
-      expect(events_in_a_series.length).to eq 1
+      expect(events_in_a_series("event in a series").length).to eq 1
 
       # make sure it was actually deleted and not just removed from the interface
       get("/calendar")
-      expect(events_in_a_series.length).to eq 1
+      expect(events_in_a_series("event in a series").length).to eq 1
     end
 
     it "deletes 'all events' from a series" do
       get "/calendar"
 
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -444,11 +444,11 @@ describe "recurring events" do
       event_series_all_events.click
       event_series_delete_button.click
       wait_for_ajax_requests
-      expect(calendar_content).not_to contain_jqcss(events_in_a_series_selector)
+      expect(calendar_content).not_to contain_jqcss(events_in_a_series_selector("event in a series"))
 
       # make sure it was actually deleted and not just removed from the interface
       get("/calendar")
-      expect(calendar_content).not_to contain_jqcss(events_in_a_series_selector)
+      expect(calendar_content).not_to contain_jqcss(events_in_a_series_selector("event in a series"))
     end
   end
 
@@ -643,7 +643,7 @@ describe "recurring events" do
       get "/calendar"
 
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[0].click
@@ -661,7 +661,7 @@ describe "recurring events" do
       click_edit_confirm_button
       wait_for_ajaximations
 
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 2
     end
 
@@ -669,7 +669,7 @@ describe "recurring events" do
       get "/calendar"
 
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
       events[1].click
       click_edit_event_button
@@ -680,18 +680,17 @@ describe "recurring events" do
       event_series_this_event.click
       click_edit_confirm_button
       wait_for_ajaximations
-      upd_events = updated_events_in_a_series
+      upd_events = events_in_a_series("event updated in a series")
       expect(upd_events.length).to eq 1
 
-      # events_in_a_series returns all events with the original title
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 2
     end
 
     it "updates this and following events in the series" do
       get "/calendar"
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -705,11 +704,11 @@ describe "recurring events" do
       click_edit_confirm_button
       wait_for_ajaximations
 
-      upd_events = updated_events_in_a_series
+      upd_events = events_in_a_series("event updated in a series")
       expect(upd_events.length).to eq 2
 
-      # events_in_a_series returns all events with the original title
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
+
       expect(events.length).to eq 1
     end
 
@@ -717,7 +716,7 @@ describe "recurring events" do
       get "/calendar"
 
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -731,8 +730,7 @@ describe "recurring events" do
       click_close_edit_button
       wait_for_ajaximations
 
-      events = events_in_a_series
-
+      events = events_in_a_series("event in a series")
       # events_in_a_series returns all events with the original title
       expect(events.length).to eq 3
     end
@@ -741,7 +739,7 @@ describe "recurring events" do
       get "/calendar"
 
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       events.each do |e|
         e.click
         expect(event_details_modal.text).to include("Unnamed Course")
@@ -758,7 +756,7 @@ describe "recurring events" do
       click_edit_confirm_button
       wait_for_ajaximations
 
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
 
       events.each do |e|
         e.click
@@ -771,7 +769,7 @@ describe "recurring events" do
       get "/calendar"
 
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -790,8 +788,7 @@ describe "recurring events" do
       click_edit_confirm_button
       wait_for_ajaximations
 
-      events = events_in_a_series
-
+      events = events_in_a_series("event in a series")
       # events_in_a_series returns all events with the original title
       expect(events.length).to eq 1
     end
@@ -799,7 +796,7 @@ describe "recurring events" do
     it "cancels edit from update page" do
       get "/calendar"
       wait_for_ajaximations
-      events = events_in_a_series
+      events = events_in_a_series("event in a series")
       expect(events.length).to eq 3
 
       events[1].click
@@ -819,10 +816,310 @@ describe "recurring events" do
       click_update_cancel_button
       wait_for_ajaximations
 
-      events = events_in_a_series
-
+      events = events_in_a_series("event in a series")
       # events_in_a_series returns all events with the original title
       expect(events.length).to eq 3
+    end
+  end
+
+  context "calendar agenda view recurring events interactions" do
+    before :once do
+      course_with_teacher(active_all: true, new_user: true)
+    end
+
+    before do
+      user_session(@teacher)
+    end
+
+    it "creates custom 'every 5 days' event and sees events in agenda view" do
+      get "/calendar2#view_name=agenda&view_start=2023-07-20"
+      wait_for_ajaximations
+
+      create_new_calendar_event
+      enter_event_title("agenda event")
+      select_frequency_option("Custom")
+
+      enter_repeat_interval(5)
+
+      click_done_button
+      click_submit_button
+
+      expect(agenda_events("agenda event").length).to eq(5)
+    end
+  end
+
+  context "series split views" do
+    before :once do
+      course_with_teacher(active_all: true, new_user: true)
+    end
+
+    before do
+      user_session(@teacher)
+      target_day = Date.parse("2023-07-20")
+      create_calendar_event_series(@course, "event in a series", target_day, 1.hour, "FREQ=DAILY;INTERVAL=1;COUNT=6")
+    end
+
+    it "shows all events in list after time change split" do
+      get "/calendar2#view_name=month&view_start=2023-07-01"
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      expect(events.length).to eq 6
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Daily, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 12am - 1am")
+
+      click_edit_event_button
+      replace_content(edit_calendar_event_end_input, "12:00 PM")
+      edit_calendar_event_end_input.send_keys :return
+
+      replace_content(edit_calendar_event_start_input, "11:00 AM")
+      edit_calendar_event_start_input.send_keys :return
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+      events = events_in_a_series("event in a series")
+
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Jul 20, 12am - 1am")
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 11am - 12pm")
+    end
+
+    it "shows all events in list after RRULE split" do
+      get "/calendar2#view_name=month&view_start=2023-07-01"
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      expect(events.length).to eq 6
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Daily, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 12am - 1am")
+
+      click_edit_event_button
+      select_frequency_option("Custom")
+      select_repeat_frequency("Week")
+      click_day_selection_checkbox("Su")
+      click_day_selection_checkbox("We")
+      click_day_selection_checkbox("Th")
+      click_done_button
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+      events = events_in_a_series("event in a series")
+
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Jul 20, 12am - 1am")
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Weekly on Wed, Thu, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 26, 12am - 1am")
+    end
+
+    it "shows title change doesn't split series" do
+      get "/calendar2#view_name=month&view_start=2023-07-01"
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      expect(events.length).to eq 6
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Daily, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 12am - 1am")
+
+      click_edit_event_button
+      enter_event_title("updated event title")
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+      events = events_in_a_series("updated event title")
+
+      expect(events.length).to eq 3
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Daily, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 12am - 1am")
+    end
+  end
+
+  context "calendar events series in week view" do
+    before :once do
+      course_with_teacher(active_all: true, new_user: true)
+    end
+
+    before do
+      user_session(@teacher)
+      target_day = Date.parse("2023-07-20")
+      create_calendar_event_series(@course, "event in a series", target_day, 1.hour, "FREQ=DAILY;INTERVAL=1;COUNT=6")
+    end
+
+    it "shows appropriate events in week view" do
+      get "/calendar2#view_name=week&view_start=2023-07-16"
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      expect(events.length).to eq 3
+    end
+
+    it "shows events series that have been split with an edit in week view" do
+      get "/calendar2#view_name=week&view_start=2023-07-16"
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      expect(events.length).to eq 3
+
+      events[1].click
+      click_edit_event_button
+      expect(frequency_picker_value).to eq("Daily, 6 times")
+
+      replace_content(edit_calendar_event_end_input, "12:00 PM")
+      edit_calendar_event_end_input.send_keys :return
+
+      replace_content(edit_calendar_event_start_input, "11:00 AM")
+      edit_calendar_event_start_input.send_keys :return
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+
+      events = events_in_a_series("event in a series")
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Daily, 1 time")
+      expect(edit_event_rule_text.text).to include("Jul 20, 12am - 1am")
+
+      events[1].click
+      expect(edit_event_rule_text.text).to include("Daily, 5 times")
+      expect(edit_event_rule_text.text).to include("Jul 21, 11am - 12pm")
+    end
+
+    it "shows events series with RRULE change in week view" do
+      target_day = Date.parse("2023-08-06")
+      create_calendar_event_series(@course, "daily series for week", target_day, 1.hour, "FREQ=DAILY;INTERVAL=1;COUNT=6")
+
+      get "/calendar2#view_name=week&view_start=2023-08-06"
+      wait_for_ajaximations
+
+      events = events_in_a_series("daily series for week")
+      expect(events.length).to eq 6
+
+      events[3].click
+      click_edit_event_button
+      select_frequency_option("Custom")
+      select_repeat_frequency("Week")
+      click_day_selection_checkbox("We")
+      click_day_selection_checkbox("Th")
+      click_day_selection_checkbox("Fr")
+      click_done_button
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+
+      events = events_in_a_series("daily series for week")
+      events[0].click
+
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Aug 6, 12am - 1am")
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Weekly on Thu, Fri, 6 times")
+      expect(edit_event_rule_text.text).to include("Aug 10, 12am - 1am")
+    end
+  end
+
+  context "agenda view series split views" do
+    before :once do
+      course_with_teacher(active_all: true, new_user: true)
+    end
+
+    before do
+      user_session(@teacher)
+      target_day = Date.parse("2023-07-20")
+      create_calendar_event_series(@course, "event in a series", target_day, 1.hour, "FREQ=DAILY;INTERVAL=1;COUNT=6")
+    end
+
+    it "shows events series in agenda view after time change split" do
+      get "/calendar2#view_name=agenda&view_start=2023-07-20"
+      wait_for_ajaximations
+
+      events = agenda_events("event in a series")
+      expect(events.length).to eq 6
+
+      events[3].click
+      click_edit_event_button
+      expect(frequency_picker_value).to eq("Daily, 6 times")
+
+      replace_content(edit_calendar_event_end_input, "12:00 PM")
+      edit_calendar_event_end_input.send_keys :return
+
+      replace_content(edit_calendar_event_start_input, "11:00 AM")
+      edit_calendar_event_start_input.send_keys :return
+
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+
+      events = agenda_events("event in a series")
+      expect(events.length).to eq 6
+
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Jul 20, 12am - 1am")
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Daily, 3 times")
+      expect(edit_event_rule_text.text).to include("Jul 23, 11am - 12pm")
+    end
+
+    it "shows events series in agenda view with RRULE change" do
+      get "/calendar2#view_name=agenda&view_start=2023-07-20"
+      wait_for_ajaximations
+
+      events = agenda_events("event in a series")
+      expect(events.length).to eq 6
+
+      events[3].click
+      click_edit_event_button
+      select_frequency_option("Custom")
+      select_repeat_frequency("Week")
+      click_day_selection_checkbox("Su")
+      click_day_selection_checkbox("Th")
+      click_day_selection_checkbox("Fr")
+      click_done_button
+      click_submit_button
+
+      event_series_following_events.click
+      click_edit_confirm_button
+      wait_for_ajaximations
+
+      events = agenda_events("event in a series")
+
+      events[0].click
+      expect(edit_event_rule_text.text).to include("Jul 20, 12am - 1am")
+
+      events[3].click
+      expect(edit_event_rule_text.text).to include("Weekly on Thu, Fri, 6 times")
+      expect(edit_event_rule_text.text).to include("Jul 27, 12am - 1am")
     end
   end
 end
