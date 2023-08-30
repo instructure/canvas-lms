613c65da4a98c384af97a7c9b721a1e148fcbd7c
Isaac Moore <isaac.moore@instructure.com>
2023-08-30 14:50:57 -0500

    Defer constraint checking when creating account 0
    In newer postgres versions, this migration will fail, because of the
    foreign key of `root_account_id` -> `id`. For this reason, we will
    defer constraint checking when making this insert.
    
    fixes AE-474
    flag=none
    
    test plan:
    - with the fix:
      - the migration completes successfully on newer postgres
    - without the fix:
      - the migration does not complete successfully on newer postgres
    
    Change-Id: I1ea7b874aea4d3cae979248edafd249675fb4221
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/326597
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Build-Review: Cody Cutrer <cody@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Isaac Moore <isaac.moore@instructure.com>
    Product-Review: Isaac Moore <isaac.moore@instructure.com>

--- db/migrate/20230911171412_make_accounts_root_account_fk_deferrable.rb
+++ db/migrate/20230911171412_make_accounts_root_account_fk_deferrable.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-# Copyright (C) 2020 - present Instructure, Inc.
+# Copyright (C) 2023 - present Instructure, Inc.
 #
 # This file is part of Canvas.
 #
@@ -16,21 +16,25 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
-class CreateAccount0 < ActiveRecord::Migration[5.2]
-  tag :predeploy
+class MakeAccountsRootAccountFkDeferrable < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  tag :postdeploy
 
   def up
-    fk_name = connection.foreign_key_for(:accounts, to_table: :accounts).name
-
-    # self-referential; so defer constraint checking until the end of the transaction
-    defer_constraints(fk_name) do
-      Account.create_with(name: "Dummy Root Account", workflow_state: "deleted", root_account_id: 0)
-             .find_or_create_by!(id: 0)
-    end
+    alter_constraint(
+      :accounts,
+      find_foreign_key(:accounts, :accounts, column: :root_account_id),
+      deferrable: true
+    )
   end
 
   def down
-    Account.where(id: 0).delete_all
+    alter_constraint(
+      :accounts,
+      find_foreign_key(:accounts, :accounts, column: :root_account_id),
+      deferrable: false
+    )
   end
 end
