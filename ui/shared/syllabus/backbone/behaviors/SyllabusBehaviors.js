1de1c2dbff894ea58a70a7aba5f1e7e438cf64bb
Mysti Lilla <mysti@instructure.com>
2023-09-01 13:38:10 -0600

    Change syllabus edit to button with aria-expanded
    fixes LF-661
    flag=none
    
    Test plan
    - Go to the syllabus page in a course
    - Check the edit button and ensure it's actually
      a button (and is read as such in a screenreader)
    - Verify it reads something ("collapsed" in Safari)
      to let you know the edit portion is not visible
    
    Change-Id: I13236217018652bf807e135e984285a3236c8eb9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/326798
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Daniel Sasaki <dsasaki@instructure.com>

--- ui/shared/syllabus/backbone/behaviors/SyllabusBehaviors.js
+++ ui/shared/syllabus/backbone/behaviors/SyllabusBehaviors.js
@@ -207,6 +207,7 @@ const bindToEditSyllabus = function (course_summary_enabled) {
   $edit_course_syllabus_form.on('edit', () => {
     $edit_course_syllabus_form.show()
     $edit_syllabus_link.hide()
+    $edit_syllabus_link.attr('aria-expanded', 'true')
     $course_syllabus.hide()
     $course_syllabus_details.hide()
     easy_student_view.hide()
@@ -227,6 +228,7 @@ const bindToEditSyllabus = function (course_summary_enabled) {
   $edit_course_syllabus_form.on('hide_edit', () => {
     $edit_course_syllabus_form.hide()
     $edit_syllabus_link.show()
+    $edit_syllabus_link.attr('aria-expanded', 'false')
     $course_syllabus.show()
     easy_student_view.show()
     const text = $.trim($course_syllabus.html())
