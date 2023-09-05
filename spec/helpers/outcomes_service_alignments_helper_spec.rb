928f65b342870cd49df53f9eca927e23b5d9a821
Martin Yosifov <martin.yosifov@instructure.com>
2023-09-05 12:55:45 -0700

    Add Pagination Support for Alignment Summary calls to OS API
    closes OUT-5873
    flag=outcome_alignment_summary_with_new_quizzes
    
    Test plan:
    - Start Canvas, Outcomes-Service, Quiz LTI and Quiz API
    - Create course and enable "New Quizzes included on
    Outcome Alignment Summary Tab FF"
    - Add or Create at least 26 outcomes in the course
    - Create new quiz and align all outcomes to the quiz
    - Select Alignment Summary Tab and verify that that it
    displays all aligned outcomes (scroll down until all
    outcomes are loaded)
    - Expand the outcome description of random set of aligned
    outcomes and verify that it displays the new quiz alignment
    - Filter outcomes "With Alignments", search for outcomes,
    expand the descriptions of a random set of outcomes returned
    from search and verify that it displays the new quiz alignment
    
    Note: OS API returns up to 25 results per page; that's why
    we need 26 outcomes to verify that AS supports pagination
    
    Change-Id: I80d8bb5c173eae0be986b4aae1d82aa056775b04
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/326973
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Wilmer Corrales <wilmer.corrales@instructure.com>
    QA-Review: Angela Gomba <angela.gomba@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- spec/helpers/outcomes_service_alignments_helper_spec.rb
+++ spec/helpers/outcomes_service_alignments_helper_spec.rb
@@ -40,9 +40,17 @@ describe OutcomesServiceAlignmentsHelper do
   let(:cache_key) { [:os_aligned_outcomes, :account_uuid, account.uuid, :context_uuid, @course.uuid, :context_id, @course.id] }
   let(:minified_response_with_active_outcomes) { mock_minified_aligned_outcomes_response(mock_aligned_outcomes_response([@outcome1], [@new_quiz1, @new_quiz2])) }
   let(:minified_response_with_active_quizzes) { mock_minified_aligned_outcomes_response(mock_aligned_outcomes_response([@outcome1, @outcome2], [@new_quiz1])) }
+  let(:first_page_results_with_outcomes) { { results: minified_response_with_outcomes, total_pages: 2 } }
+  let(:first_page_results_without_outcomes) { { results: minified_response_no_outcomes, total_pages: 1 } }
+  let(:context) { @course }
+  let(:params) { { context_id: context.id, context_uuid: context.uuid } }
+  let(:scope) { "outcome_alignments.show" }
+  let(:endpoint) { "api/alignments" }
+  let(:domain) { "domain" }
+  let(:jwt) { "jwt" }
 
-  def stub_get_aligned_outcomes(context)
-    stub_request(:get, "http://domain/api/alignments?context_uuid=#{context.uuid}&context_id=#{context.id}")
+  def stub_get_aligned_outcomes(context: @course, page: 1, per_page: 25)
+    stub_request(:get, "http://domain/api/alignments?context_uuid=#{context.uuid}&context_id=#{context.id}&page=#{page}&per_page=#{per_page}")
       .with({
               headers: {
                 Authorization: /\+*/,
@@ -89,9 +97,10 @@ describe OutcomesServiceAlignmentsHelper do
 
   describe "#get_os_aligned_outcomes" do
     before do
-      settings = { consumer_key: "key", jwt_secret: "secret", domain: "domain" }
+      settings = { consumer_key: "key", jwt_secret: "secret", domain: }
       account.settings[:provision] = { "outcomes" => settings }
       account.save!
+      stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json)
     end
 
     context "without context" do
@@ -105,27 +114,36 @@ describe OutcomesServiceAlignmentsHelper do
     end
 
     context "with context" do
-      it "returns outcomes" do
-        stub_get_aligned_outcomes(@course).to_return(status: 200, body: response_with_outcomes.to_json)
-        expect(subject.get_os_aligned_outcomes(@course)).to eq minified_response_with_outcomes
+      it "calls #make_paginated_request" do
+        expect(subject).to receive(:make_paginated_request).with(context:, endpoint:, scope:, params:).and_return([response_with_outcomes])
+        subject.get_os_aligned_outcomes(@course)
       end
 
-      it "returns empty response if no outcomes" do
-        stub_get_aligned_outcomes(@course).to_return(status: 200, body: response_no_outcomes.to_json)
-        expect(subject.get_os_aligned_outcomes(@course)).to eq minified_response_no_outcomes
-      end
+      context "pagination" do
+        it "returns all results when single page of results" do
+          stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 1 })
+          expect(subject.get_os_aligned_outcomes(@course)).to eq minified_response_with_outcomes
+        end
 
-      it "raises error on non 2xx response" do
-        stub_get_aligned_outcomes(@course).to_return(status: 401, body: '{"valid_jwt":false}')
-        expect { subject.get_os_aligned_outcomes(@course) }.to raise_error(CanvasOutcomesHelper::OSFetchError)
+        it "returns all results when multiple pages of results" do
+          outcomes = (1..60).map { |num| outcome_model(context: @course, title: "outcome #{num} aligned in OS") }
+
+          mocked_response = mock_aligned_outcomes_response(outcomes, [@new_quiz1, @new_quiz2])
+          mocked_response_1 = mock_aligned_outcomes_response(outcomes.slice(0, 25), [@new_quiz1, @new_quiz2])
+          mocked_response_2 = mock_aligned_outcomes_response(outcomes.slice(25, 25), [@new_quiz1, @new_quiz2])
+          mocked_response_3 = mock_aligned_outcomes_response(outcomes.slice(50, 10), [@new_quiz1, @new_quiz2])
+          mocked_minified_response = mock_minified_aligned_outcomes_response(mocked_response)
+
+          stub_get_aligned_outcomes.to_return(status: 200, body: mocked_response_1.to_json, headers: { "Total-Pages": 3 })
+          stub_get_aligned_outcomes(page: 2).to_return(status: 200, body: mocked_response_2.to_json, headers: { "Total-Pages": 3 })
+          stub_get_aligned_outcomes(page: 3).to_return(status: 200, body: mocked_response_3.to_json, headers: { "Total-Pages": 3 })
+
+          expect(subject.get_os_aligned_outcomes(@course)).to eq mocked_minified_response
+        end
       end
     end
 
     context "caching" do
-      before do
-        stub_get_aligned_outcomes(@course).to_return(status: 200, body: response_with_outcomes.to_json)
-      end
-
       it "scopes cache key per account uuid, course uuid and course id" do
         enable_cache do
           subject.get_os_aligned_outcomes(@course)
@@ -133,7 +151,7 @@ describe OutcomesServiceAlignmentsHelper do
         end
       end
 
-      it "minifies response before caching" do
+      it "converts results to hash before caching" do
         enable_cache do
           subject.get_os_aligned_outcomes(@course)
           expect(Rails.cache.fetch(cache_key)).to eq minified_response_with_outcomes
@@ -165,6 +183,80 @@ describe OutcomesServiceAlignmentsHelper do
       it "returns only outcomes with alignments to new quizzes that are active in Canvas" do
         @new_quiz2.destroy
         expect(subject.get_active_os_alignments(@course)).to eq minified_response_with_active_quizzes
+      end
+    end
+  end
+
+  describe "#get_paginated_results" do
+    it "returns hashed results and total pages" do
+      stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 2 })
+      expect(subject.get_paginated_results(context:, domain:, endpoint:, jwt:, params:))
+        .to eq first_page_results_with_outcomes
+    end
+
+    it "returns empty hash and total pages if no results" do
+      stub_get_aligned_outcomes.to_return(status: 200, body: response_no_outcomes.to_json)
+      expect(subject.get_paginated_results(context:, domain:, endpoint:, jwt:, params:))
+        .to eq first_page_results_without_outcomes
+    end
+
+    it "raises error on non 2xx response" do
+      stub_get_aligned_outcomes.to_return(status: 401, body: '{"valid_jwt":false}')
+      expect { subject.get_paginated_results(context:, domain:, endpoint:, jwt:, params:) }
+        .to raise_error(CanvasOutcomesHelper::OSFetchError)
+    end
+
+    it "raises error on invalid JSON response" do
+      stub_get_aligned_outcomes.to_return(status: 200, body: "")
+      expect { subject.get_paginated_results(context:, domain:, endpoint:, jwt:, params:) }
+        .to raise_error(CanvasOutcomesHelper::OSFetchError)
+    end
+
+    it "throws error if call to API endpoint fails" do
+      expect(CanvasHttp).to receive(:get).and_raise("failed call").exactly(3).times
+      expect { subject.get_paginated_results(context:, domain:, endpoint:, jwt:, params:) }
+        .to raise_error(CanvasOutcomesHelper::OSFetchError)
+    end
+  end
+
+  describe "#make_paginated_request" do
+    before do
+      settings = { consumer_key: "key", jwt_secret: "secret", domain: }
+      account.settings[:provision] = { "outcomes" => settings }
+      account.save!
+    end
+
+    context "when outcomes service is not properly provisioned" do
+      it "returns nil if missing domain" do
+        allow_any_instance_of(CanvasOutcomesHelper).to receive(:extract_domain_jwt).and_return([nil, jwt])
+        expect(subject.make_paginated_request(context:, scope:, endpoint:, params:)).to be_nil
+      end
+
+      it "returns nil if missing jwt_secret" do
+        allow_any_instance_of(CanvasOutcomesHelper).to receive(:extract_domain_jwt).and_return([domain, nil])
+        expect(subject.make_paginated_request(context:, scope:, endpoint:, params:)).to be_nil
+      end
+    end
+
+    context "threading" do
+      it "does not make threaded request when single page of results" do
+        stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 1 })
+        expect(Parallel).not_to receive(:map)
+        subject.make_paginated_request(context:, scope:, endpoint:, params:)
+      end
+
+      it "makes threaded request when multiple pages of results" do
+        stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 2 })
+        stub_get_aligned_outcomes(page: 2).to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 2 })
+        expect(Parallel).to receive(:map)
+        subject.make_paginated_request(context:, scope:, endpoint:, params:)
+      end
+
+      it "throws error if call to API endpoint fails during a threaded request" do
+        stub_get_aligned_outcomes.to_return(status: 200, body: response_with_outcomes.to_json, headers: { "Total-Pages": 2 })
+        stub_get_aligned_outcomes(page: 2).to_return(status: [500, "Internal Server Error"])
+        expect { subject.make_paginated_request(context:, scope:, endpoint:, params:) }
+          .to raise_error(CanvasOutcomesHelper::OSFetchError)
       end
     end
   end
