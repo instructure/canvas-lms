60dc505ce6990e13c45cba43e4b426ee50707431
jonathan <jonathan.guardado@instructure.com>
2023-09-05 11:02:45 -0600

    “Assign to” tab UI on differentiated modules tray
    Add the access type options to the “Assign to” tab
    
    closes LF-591
    flag= differentiated_modules
    
    Test plan
    
    - With the differentiated_modules FF on
    - go to the modules page of a course
    - open the module menu
    - click on the Assign to option
    - Expect to see the two access type radio buttons and the UI to match
    the design
    
    Change-Id: Ibbf329e3a690ec00150bcde5d0d5757fa288ef90
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/327008
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Jake Oeding <jake.oeding@instructure.com>
    Product-Review: Jonathan Guardado <jonathan.guardado@instructure.com>

--- ui/shared/differentiated-modules/react/AssignToPanel.tsx
+++ ui/shared/differentiated-modules/react/AssignToPanel.tsx
@@ -16,9 +16,17 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
+import React, {useState} from 'react'
 import {Flex} from '@instructure/ui-flex'
 import Footer from './Footer'
+// @ts-expect-error -- TODO: remove once we're on InstUI 8
+import {RadioInputGroup, RadioInput} from '@instructure/ui-radio-input'
+import {Text} from '@instructure/ui-text'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {View} from '@instructure/ui-view'
+import {ScreenReaderContent} from '@instructure/ui-a11y-content'
+
+const I18n = useI18nScope('differentiated_modules')
 
 // Doing this to avoid TS2339 errors-- remove once we're on InstUI 8
 const {Item: FlexItem} = Flex as any
@@ -28,13 +36,66 @@ export interface AssignToPanelProps {
   onDismiss: () => void
 }
 
+interface Option {
+  value: string
+  getLabel: () => string
+  getDescription: () => string
+}
+
+const options: Option[] = [
+  {
+    value: 'everyone',
+    getLabel: () => I18n.t('Everyone'),
+    getDescription: () => I18n.t('This module will be visible to everyone.'),
+  },
+  {
+    value: 'custom',
+    getLabel: () => I18n.t('Custom Access'),
+    getDescription: () =>
+      I18n.t('Create custom access and optionally set Lock Until date for each group.'),
+  },
+]
+
 export default function AssignToPanel({height, onDismiss}: AssignToPanelProps) {
+  const [selectedOption, setSelectedOption] = useState<string>(options[0].value)
+
   return (
-    <Flex direction="column" justifyItems="space-between" height={height}>
-      <FlexItem shouldGrow={true} padding="small">
-        <div>Assign To</div>
+    <Flex direction="column" justifyItems="start" height={height}>
+      <FlexItem padding="medium medium small">
+        <Text>
+          {I18n.t('By default everyone in this course has assigned access to this module.')}
+        </Text>
       </FlexItem>
-      <FlexItem>
+      <FlexItem padding="x-small medium" overflowX="hidden">
+        <RadioInputGroup description={I18n.t('Select Access Type')} name="access_type">
+          {options.map(option => (
+            <Flex key={option.value}>
+              <FlexItem align="start">
+                <View as="div" margin="none">
+                  <RadioInput
+                    data-testid={`${option.value}-option`}
+                    value={option.value}
+                    checked={selectedOption === option.value}
+                    onClick={() => setSelectedOption(option.value)}
+                    label={<ScreenReaderContent>{option.getLabel()}</ScreenReaderContent>}
+                  />
+                </View>
+              </FlexItem>
+              <FlexItem>
+                <View as="div" margin="none">
+                  <Text>{option.getLabel()}</Text>
+                </View>
+                <View as="div" margin="none">
+                  <Text color="secondary" size="small">
+                    {option.getDescription()}
+                  </Text>
+                </View>
+              </FlexItem>
+            </Flex>
+          ))}
+        </RadioInputGroup>
+      </FlexItem>
+      <FlexItem margin="auto none none none">
         <Footer onDismiss={onDismiss} onUpdate={() => {}} />
       </FlexItem>
     </Flex>
---
+++ ui/shared/differentiated-modules/react/__tests__/AssignToPanel.test.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import AssignToPanel, {AssignToPanelProps} from '../AssignToPanel'
+
+describe('AssignToPanel', () => {
+  const props: AssignToPanelProps = {
+    height: '500px',
+    onDismiss: () => {},
+  }
+
+  const renderComponent = (overrides = {}) => render(<AssignToPanel {...props} {...overrides} />)
+
+  it('renders', () => {
+    const {getByText} = renderComponent()
+    expect(
+      getByText('By default everyone in this course has assigned access to this module.')
+    ).toBeInTheDocument()
+  })
+
+  it('renders options', () => {
+    const {getByTestId} = renderComponent()
+    expect(getByTestId('everyone-option')).toBeInTheDocument()
+    expect(getByTestId('custom-option')).toBeInTheDocument()
+  })
+
+  it('renders everyone as the default option', () => {
+    const {getByTestId} = renderComponent()
+    expect(getByTestId('everyone-option')).toBeChecked()
+    expect(getByTestId('custom-option')).not.toBeChecked()
+  })
+})
