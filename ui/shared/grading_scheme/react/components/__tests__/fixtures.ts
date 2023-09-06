74c6eee7b5e799411b703ce63088d945e55ee7fa
Chris Soto <christopher.soto@instructure.com>
2023-09-06 10:00:39 -0600

    disable edit & delete grading scheme buttons
    currently when in the context of course settings, the edit and delete
    buttons are enabled and functional for account level grading schemes.
    this change will disable the buttons for admin grading schemes if the
    admin is in the context of course settings.
    
    fixes EVAL-3427
    flag=none
    
    test plan:
    - create an account level grading scheme and a course level grading
      scheme
    - open http://canvas.docker/accounts/<id>/grading_settings/schemes and
      verify that the edit/delete buttons for the account level grading
      schemes are NOT disabled
    - go to the course settings for the course that the course level grading
      scheme is in and verify that the edit/delete buttons for the account
      level grading schemes are disabled while the course level grading
      scheme buttons are NOT disabled
      http://canvas.docker/courses/<id>/settings#tab-details
    - within the same page, select both a course and an account level
      grading scheme from the dropdown & verify that when the modal opens,
      the buttons are disabled for the account grading scheme and enabled
      for the course grading scheme.
    - create an assignment and change the "Display Grade As" option to
      "Letter Grade"
    - The same grading scheme components should be displayed
    - Verify the steps again where course level grading scheme buttons are
      NOT disabled while the account level grading scheme buttons are
      disabled.
    
    Change-Id: I1a82ddb56df6eb10539809fe924e1657627201ac
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/327025
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Cameron Ray <cameron.ray@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Samuel Lee <samuel.lee@instructure.com>
    Product-Review: Ravi Koll <ravi.koll@instructure.com>

---
+++ ui/shared/grading_scheme/react/components/__tests__/fixtures.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {GradingScheme, GradingSchemeTemplate} from '@canvas/grading_scheme'
+
+export const AccountGradingSchemes: GradingScheme[] = [
+  {
+    assessed_assignment: false,
+    context_id: '1',
+    context_type: 'Course',
+    context_name: 'Test Course',
+    data: [{name: 'A', value: 90}],
+    id: '1',
+    permissions: {manage: true},
+    points_based: false,
+    scaling_factor: 1,
+    title: 'Grading Scheme 1',
+  },
+  {
+    assessed_assignment: false,
+    context_id: '1',
+    context_type: 'Account',
+    context_name: 'Test Account',
+    data: [{name: 'A', value: 90}],
+    id: '2',
+    permissions: {manage: true},
+    points_based: false,
+    scaling_factor: 1,
+    title: 'Grading Scheme 2',
+  },
+  {
+    assessed_assignment: false,
+    context_id: '1',
+    context_type: 'Course',
+    context_name: 'Test Course',
+    data: [{name: 'A', value: 90}],
+    id: '3',
+    permissions: {manage: true},
+    points_based: false,
+    scaling_factor: 1,
+    title: 'Grading Scheme 3',
+  },
+]
+
+export const DefaultGradingScheme: GradingSchemeTemplate = {
+  data: [{name: 'A', value: 90}],
+  points_based: false,
+  scaling_factor: 1,
+  title: 'Default Grading Scheme',
+}
