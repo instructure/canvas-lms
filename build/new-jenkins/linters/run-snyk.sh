e4146b77e38b6c7f2df624e433c2b9f429957a6b
Andrea Cirulli <andrea.cirulli@instructure.com>
2023-09-07 08:11:42 -0600

    update snyk org
    [canvas-builds-refspec=8f775a14385cfd426592eecdd14d0226f3cac9d1]
    [build-registry-path=jenkins/canvas-lms-snyk-test]
    [change-merged]
    
    Change-Id: I4b33786da4dc0a03c4638931df0a03c54950c72b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/327107
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hargiss <mhargiss@instructure.com>
    QA-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Product-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Build-Review: Andrea Cirulli <andrea.cirulli@instructure.com>

--- build/new-jenkins/linters/run-snyk.sh
+++ build/new-jenkins/linters/run-snyk.sh
@@ -6,8 +6,8 @@ for TEST_FILE in Gemfile.*.lock; do
   echo "checking $TEST_FILE with snyk"
 
   npx snyk auth $SNYK_TOKEN
-  npx snyk test --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems || true
-  npx snyk monitor --severity-threshold=low --file=$TEST_FILE --org=instructure --project-name=canvas-lms:ruby --packageManager=rubygems
+  npx snyk test --severity-threshold=low --file=$TEST_FILE --org=canvas-lms --project-name=canvas-lms:ruby --packageManager=rubygems || true
+  npx snyk monitor --severity-threshold=low --file=$TEST_FILE --org=canvas-lms --project-name=canvas-lms:ruby --packageManager=rubygems
 done
 
 if [[ -z "$TEST_FILE" ]]; then
