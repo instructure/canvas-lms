8d0a2bbca8f3ee308151e5e1fb81870cfd2c5fae
Jeremy Stanley <jeremy@instructure.com>
2023-09-12 14:49:29 -0600

    use qualified name for vector type
    rather than assuming it's in `public`, in case someone installs
    it elsewhere rather than using our own EnableVectorExtension migration
    
    refs ADV-22
    
    Change-Id: I7ee86103cc63a4738076d1df8ec56c6ec51ba82f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/327503
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- db/migrate/20230901164455_add_wiki_page_embeddings.rb
+++ db/migrate/20230901164455_add_wiki_page_embeddings.rb
@@ -26,7 +26,7 @@ class AddWikiPageEmbeddings < ActiveRecord::Migration[7.0]
   def change
     create_table :wiki_page_embeddings do |t|
       t.references :wiki_page, null: false, foreign_key: true
-      t.column :embedding, "public.vector", limit: 1536, null: false
+      t.column :embedding, "#{connection.extension("vector").schema}.vector", limit: 1536, null: false
       t.timestamps null: false
       t.references :root_account, foreign_key: { to_table: :accounts }, index: false, null: false
     end
