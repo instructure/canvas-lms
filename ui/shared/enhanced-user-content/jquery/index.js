4086fab99f46162c3f7bbc0efc518a96d6b424f5
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-09-15 15:53:20 -0600

    Stop intercepting early clicks
    test plan:
      - build passes
    
    flag=none
    
    Change-Id: I57b81d77f2167b6c953a215b5e52d4625b6ee407
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/327934
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/shared/enhanced-user-content/jquery/index.js
+++ ui/shared/enhanced-user-content/jquery/index.js
@@ -132,26 +132,6 @@ function enhanceUserJQueryWidgetContent() {
     .end()
 }
 
-function retriggerEarlyClicks() {
-  // handle all of the click events that were triggered before the dom was ready (and thus weren't handled by jquery listeners)
-  if (window._earlyClick) {
-    // unset the onclick handler we were using to capture the events
-    document.removeEventListener('click', window._earlyClick)
-    if (window._earlyClick.clicks) {
-      // wait to fire the "click" events till after all of the event hanlders loaded at dom ready are initialized
-      setTimeout(function () {
-        $.each(_.uniq(window._earlyClick.clicks), function () {
-          // cant use .triggerHandler because it will not bubble,
-          // but we do want to preventDefault, so this is what we have to do
-          const event = $.Event('click')
-          event.preventDefault()
-          $(this).trigger(event)
-        })
-      }, 1)
-    }
-  }
-}
-
 function ellipsifyBreadcrumbs() {
   // this next block of code adds the ellipsis on the breadcrumb if it overflows one line
   const $breadcrumbs = $('#breadcrumbs')
@@ -644,7 +624,6 @@ const setDialogCloseText = () => {
 
 export default function enhanceTheEntireUniverse() {
   ;[
-    retriggerEarlyClicks,
     ellipsifyBreadcrumbs,
     bindKeyboardShortcutsHelpPanel,
     warnAboutRolesBeingSwitched,
