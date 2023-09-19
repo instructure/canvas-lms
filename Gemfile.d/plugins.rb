70c7dea46830c196fe5fd30827a755ec8e39cff4
Cody Cutrer <cody@instructure.com>
2023-09-19 17:29:39 -0600

    use `gemfile_root` for relative operations in gemfile fragments
    instead of Bundler.root
    
    Change-Id: I3f8c09f4b13e8f55e359b87e93c454ef571ce09a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/328182
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Build-Review: Cody Cutrer <cody@instructure.com>

--- Gemfile.d/plugins.rb
+++ Gemfile.d/plugins.rb
@@ -26,11 +26,11 @@ CANVAS_INLINE_PLUGINS = %w[
   simply_versioned
 ].freeze
 
-Dir[File.join(File.dirname(__FILE__), "../gems/plugins/*")].each do |plugin_dir|
+gemfile_root.glob("../gems/plugins/*") do |plugin_dir|
   next unless File.directory?(plugin_dir)
 
   gem_name = File.basename(plugin_dir)
   next unless CANVAS_INCLUDE_PLUGINS || CANVAS_INLINE_PLUGINS.include?(gem_name)
 
-  gem(gem_name, path: plugin_dir)
+  gem(gem_name, path: plugin_dir.relative_path_from(gemfile_root))
 end
