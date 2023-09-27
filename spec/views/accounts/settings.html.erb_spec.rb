b0215fb118f29a067dc635a2673ecb8b7644b757
Jason Perry <jason.perry@instructure.com>
2023-09-27 14:56:09 -0400

    Don't show dummy course for templates in account settings
    Fixes FOO-3791
    flag=none
    
    Test Plan:
      - Have course templates enabled
      - Go to a sub account's settings page
      - In the drop down for course templates, there should be no
        option for "Unnamed Course"
    
    Change-Id: I6ea3d4b0241d56ea3992678f5728e6ee4c8a10b2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/328837
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jason Perry <jason.perry@instructure.com>
    QA-Review: Jason Perry <jason.perry@instructure.com>

--- spec/views/accounts/settings.html.erb_spec.rb
+++ spec/views/accounts/settings.html.erb_spec.rb
@@ -671,6 +671,21 @@ describe "accounts/settings" do
       expect(select.css("option").pluck("value")).to eq ["", "0"]
     end
 
+    it "does not show dummy course for sub accounts" do
+      account.courses.create!(id: 0, workflow_state: "deleted", name: "Unnamed Course")
+      a2 = account.sub_accounts.create!
+      a2.update(course_template_id: 0)
+      view_context(a2, admin)
+      assign(:context, a2)
+      assign(:account, a2)
+
+      render
+      doc = Nokogiri::HTML5(response.body)
+      options = doc.css("#account_course_template_id option[selected]")
+      expect(options.map(&:text)).to_not include("Unnamed Course")
+      expect(options.count).to eq 1
+    end
+
     it "disables if you don't have permission" do
       c = account.courses.create!(template: true)
       account.role_overrides.create!(role: admin.account_users.first.role, permission: :edit_course_template, enabled: false)
