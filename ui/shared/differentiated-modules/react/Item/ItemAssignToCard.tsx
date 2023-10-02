88c06381e86b99cca6af48a9d0efcdb3ff36eefe
Ed Schiebel <eschiebel@instructure.com>
2023-10-02 17:17:22 -0400

    Add dates and their validation to the moduil item assign to tray
    closes LF-750
    flag=differenciated_modules
    
    test plan:
      - from an assignment, quiz, or nq module item, select
        Assign To... from the kabob menu
      - the first card will fill with the current date time
      - change dates to make them invalid
      > expect the tray's Save button to become disabled
      - changes date to make them valid
      > expect the Save button to become enabled
      - add a card and repeat
      - make one card's dates invalid
      - delete the card
      > expect the Save button to become enabled
    
    Change-Id: I71cb1d08c12fddfa8f682505c1144507b9f69ef9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/329213
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- ui/shared/differentiated-modules/react/Item/ItemAssignToCard.tsx
+++ ui/shared/differentiated-modules/react/Item/ItemAssignToCard.tsx
@@ -16,14 +16,15 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useCallback} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {View} from '@instructure/ui-view'
-// import AssigneeSelector from '../AssigneeSelector'
 import {DateTimeInput} from '@instructure/ui-date-time-input'
 import {IconButton} from '@instructure/ui-buttons'
 import {ScreenReaderContent} from '@instructure/ui-a11y-content'
 import {IconTrashLine} from '@instructure/ui-icons'
 import {useScope as useI18nScope} from '@canvas/i18n'
+import DateValidator from '@canvas/datetime/DateValidator'
+// import AssigneeSelector from '../AssigneeSelector'
 
 const I18n = useI18nScope('differentiated_modules')
 
@@ -36,22 +37,97 @@ function AssigneeSelector({cardId}: {cardId: string}) {
   )
 }
 
+function arrayEquals(a: any[], b: any[]) {
+  return a.length === b.length && a.every((v, i) => v === b[i])
+}
+
+export interface DateValidatorInputArgs {
+  lock_at?: string
+  unlock_at?: string
+  due_at?: string
+  set_type?: string
+  course_section_id?: string
+  student_ids?: string[]
+}
+
 export type ItemAssignToCardProps = {
   cardId: string
-  // courseId: string
-  // moduleItemId: string
   onDelete?: (cardId: string) => void
+  onValidityChange?: (cardId: string, isValid: boolean) => void
 }
 
 export default function ItemAssignToCard({
   cardId,
-  // courseId,
-  // moduleItemId,
   onDelete,
+  onValidityChange,
 }: ItemAssignToCardProps) {
+  const [dateValidator] = useState<DateValidator>(
+    new DateValidator({
+      date_range: {...ENV.VALID_DATE_RANGE},
+      hasGradingPeriods: ENV.HAS_GRADING_PERIODS,
+      gradingPeriods: ENV.active_grading_periods,
+      userIsAdmin: (ENV.current_user_roles || []).includes('admin'),
+      postToSIS: ENV.POST_TO_SIS && ENV.DUE_DATE_REQUIRED_FOR_ACCOUNT,
+    })
+  )
+  const [dueDate, setDueDate] = useState<string | undefined>(new Date().toISOString())
+  const [availableFromDate, setAvailableFromDate] = useState<string | undefined>(
+    new Date().toISOString()
+  )
+  const [availableToDate, setAvailableToDate] = useState<string | undefined>(
+    new Date().toISOString()
+  )
+  const [validationErrors, setValidationErrors] = useState<Record<string, string>>({})
+
   const handleDelete = useCallback(() => {
     onDelete?.(cardId)
   }, [cardId, onDelete])
+
+  const handleDueDateChange = useCallback(
+    (_event: React.SyntheticEvent, value: string | undefined) => {
+      setDueDate(value)
+    },
+    []
+  )
+
+  const handleAvailableFromDateChange = useCallback(
+    (_event: React.SyntheticEvent, value: string | undefined) => {
+      setAvailableFromDate(value)
+    },
+    []
+  )
+
+  const handleAvailableToDateChange = useCallback(
+    (_event: React.SyntheticEvent, value: string | undefined) => {
+      setAvailableToDate(value)
+    },
+    []
+  )
+
+  useEffect(() => {
+    const data: DateValidatorInputArgs = {
+      due_at: dueDate,
+      unlock_at: availableFromDate,
+      lock_at: availableToDate,
+      student_ids: [],
+      course_section_id: '2',
+    }
+    const newErrors = dateValidator.validateDatetimes(data)
+    const newBadDates = Object.keys(newErrors)
+    const oldBadDates = Object.keys(validationErrors)
+    if (!arrayEquals(newBadDates, oldBadDates)) {
+      onValidityChange?.(cardId, newBadDates.length === 0)
+      setValidationErrors(newErrors)
+    }
+  }, [
+    availableFromDate,
+    availableToDate,
+    cardId,
+    dateValidator,
+    dueDate,
+    onValidityChange,
+    validationErrors,
+  ])
 
   return (
     <View
@@ -84,22 +160,29 @@ export default function ItemAssignToCard({
         </div>
       )}
       <AssigneeSelector cardId={cardId} />
+
       <View as="div" margin="small none">
         <DateTimeInput
+          allowNonStepInput={true}
+          dateFormat="MMM D, YYYY"
           description={
             <ScreenReaderContent>{I18n.t('Choose a due date and time')}</ScreenReaderContent>
           }
-          dateRenderLabel={I18n.t('Date')}
+          dateRenderLabel={I18n.t('Due Date')}
           timeRenderLabel={I18n.t('Time')}
-          invalidDateTimeMessage={I18n.t('Invalid date!')}
+          invalidDateTimeMessage={I18n.t('Invalid date')}
           prevMonthLabel={I18n.t('Previous month')}
           nextMonthLabel={I18n.t('Next month')}
-          defaultValue={new Date().toISOString()}
+          value={dueDate}
           layout="columns"
+          messages={validationErrors.due_at ? [{type: 'error', text: validationErrors.due_at}] : []}
+          onChange={handleDueDateChange}
         />
       </View>
       <View as="div" margin="small none">
         <DateTimeInput
+          allowNonStepInput={true}
+          dateFormat="MMM D, YYYY"
           description={
             <ScreenReaderContent>
               {I18n.t('Choose an available from date and time')}
@@ -107,15 +190,21 @@ export default function ItemAssignToCard({
           }
           dateRenderLabel={I18n.t('Available from')}
           timeRenderLabel={I18n.t('Time')}
-          invalidDateTimeMessage={I18n.t('Invalid date!')}
+          invalidDateTimeMessage={I18n.t('Invalid date')}
           prevMonthLabel={I18n.t('Previous month')}
           nextMonthLabel={I18n.t('Next month')}
-          defaultValue={new Date().toISOString()}
+          value={availableFromDate}
           layout="columns"
+          messages={
+            validationErrors.unlock_at ? [{type: 'error', text: validationErrors.unlock_at}] : []
+          }
+          onChange={handleAvailableFromDateChange}
         />
       </View>
       <View as="div" margin="small none">
         <DateTimeInput
+          allowNonStepInput={true}
+          dateFormat="MMM D, YYYY"
           description={
             <ScreenReaderContent>
               {I18n.t('Choose an available to date and time')}
@@ -123,11 +212,15 @@ export default function ItemAssignToCard({
           }
           dateRenderLabel={I18n.t('Until')}
           timeRenderLabel={I18n.t('Time')}
-          invalidDateTimeMessage={I18n.t('Invalid date!')}
+          invalidDateTimeMessage={I18n.t('Invalid date')}
           prevMonthLabel={I18n.t('Previous month')}
           nextMonthLabel={I18n.t('Next month')}
-          defaultValue={new Date().toISOString()}
+          value={availableToDate}
           layout="columns"
+          messages={
+            validationErrors.lock_at ? [{type: 'error', text: validationErrors.lock_at}] : []
+          }
+          onChange={handleAvailableToDateChange}
         />
       </View>
     </View>
