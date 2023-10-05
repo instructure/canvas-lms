0f30bae7fb8f9b70928c20c2c741807396407f31
jonathan <jonathan.guardado@instructure.com>
2023-10-05 12:56:16 -0600

    Persist selected student/section assignment overrides
    Create/update assignment overrides. If the override exists, the
    request includes the assignment override id back, so the endpoint
    does not need to delete and create it again.
    
    closes LF-643
    flag= differentiated_modules
    
    Test plan:
    
    - In a course with several sections and students, create a module.
    - Go to the course modules page.
    - Open the module menu.
    - Click on the “Assign to” option.
    - Click on the “Custom Access” option.
    - Select some students and sections
    - Click on update module
    - Wait for the assignment overrides get saved
    - Go to the “Assign to” tab again
    - Select “Custom Access”,
     - Expect the selection you saved to be shown
    - Click on the Everyone Radio button
    - Click on update module
    - Go to the “Assign to” tab again
    - Select “Custom Access”,
    - Expect the Assign to input to be empty
    
    Change-Id: I18414801e0b938c1265648aa3b6485159ac57e97
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/329539
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jonathan Guardado <jonathan.guardado@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>

--- ui/shared/differentiated-modules/react/__tests__/AssignToPanel.test.tsx
+++ ui/shared/differentiated-modules/react/__tests__/AssignToPanel.test.tsx
@@ -17,8 +17,10 @@
  */
 
 import React from 'react'
-import {render} from '@testing-library/react'
+import {act, render} from '@testing-library/react'
 import AssignToPanel, {AssignToPanelProps} from '../AssignToPanel'
+import {ASSIGNMENT_OVERRIDES_DATA, SECTIONS_DATA, STUDENTS_DATA} from './mocks'
+import fetchMock from 'fetch-mock'
 
 describe('AssignToPanel', () => {
   const props: AssignToPanelProps = {
@@ -27,6 +29,29 @@ describe('AssignToPanel', () => {
     height: '500px',
     onDismiss: () => {},
   }
+
+  const ASSIGNMENT_OVERRIDES_URL = `/api/v1/courses/${props.courseId}/modules/${props.moduleId}/assignment_overrides`
+  const SECTIONS_URL = `/api/v1/courses/${props.courseId}/sections`
+  const STUDENTS_URL = `api/v1/courses/${props.courseId}/users?enrollment_type=student`
+
+  beforeAll(() => {
+    if (!document.getElementById('flash_screenreader_holder')) {
+      const liveRegion = document.createElement('div')
+      liveRegion.id = 'flash_screenreader_holder'
+      liveRegion.setAttribute('role', 'alert')
+      document.body.appendChild(liveRegion)
+    }
+  })
+
+  beforeEach(() => {
+    fetchMock.getOnce(SECTIONS_URL, SECTIONS_DATA)
+    fetchMock.getOnce(STUDENTS_URL, STUDENTS_DATA)
+    fetchMock.getOnce(ASSIGNMENT_OVERRIDES_URL, [])
+  })
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
 
   const renderComponent = (overrides = {}) => render(<AssignToPanel {...props} {...overrides} />)
 
@@ -47,5 +72,94 @@ describe('AssignToPanel', () => {
     const {getByTestId} = renderComponent()
     expect(getByTestId('everyone-option')).toBeChecked()
     expect(getByTestId('custom-option')).not.toBeChecked()
+  })
+
+  describe('AssigneeSelector', () => {
+    it('selects multiple options', async () => {
+      const {findByTestId, findByText, getAllByTestId, getByTestId} = renderComponent()
+      act(() => getByTestId('custom-option').click())
+      const assigneeSelector = await findByTestId('assignee_selector')
+      act(() => assigneeSelector.click())
+      const option1 = await findByText(SECTIONS_DATA[0].name)
+      act(() => option1.click())
+      act(() => assigneeSelector.click())
+      const option2 = await findByText(SECTIONS_DATA[2].name)
+      act(() => option2.click())
+      expect(getAllByTestId('assignee_selector_selected_option').length).toBe(2)
+    })
+
+    it('clears selection', async () => {
+      const {findByTestId, getByTestId, queryAllByTestId, findByText} = renderComponent()
+      act(() => getByTestId('custom-option').click())
+      const assigneeSelector = await findByTestId('assignee_selector')
+      act(() => assigneeSelector.click())
+      const option = await findByText(STUDENTS_DATA[0].name)
+      act(() => option.click())
+      expect(queryAllByTestId('assignee_selector_selected_option').length).toBe(1)
+      act(() => getByTestId('clear_selection_button').click())
+      expect(queryAllByTestId('assignee_selector_selected_option').length).toBe(0)
+    })
+
+    it('shows existing assignmentOverrides as the default selection', async () => {
+      fetchMock.getOnce(ASSIGNMENT_OVERRIDES_URL, ASSIGNMENT_OVERRIDES_DATA, {
+        overwriteRoutes: true,
+      })
+      const assignedSections = ASSIGNMENT_OVERRIDES_DATA.filter(
+        override => override.course_section !== undefined
+      )
+      const {getAllByTestId, findByText, getByTestId} = renderComponent()
+      act(() => getByTestId('custom-option').click())
+      expect(await findByText(ASSIGNMENT_OVERRIDES_DATA[0].students![0].name)).toBeInTheDocument()
+      expect(getAllByTestId('assignee_selector_selected_option').length).toBe(
+        ASSIGNMENT_OVERRIDES_DATA[0].students!.length + assignedSections.length
+      )
+    })
+  })
+
+  describe('on update', () => {
+    it('creates new assignment overrides', async () => {
+      fetchMock.put(ASSIGNMENT_OVERRIDES_URL, {})
+      const {findByTestId, findByText, getByTestId, getByRole, findAllByText} = renderComponent()
+      act(() => getByTestId('custom-option').click())
+      const assigneeSelector = await findByTestId('assignee_selector')
+      act(() => assigneeSelector.click())
+      const option1 = await findByText(SECTIONS_DATA[0].name)
+      act(() => option1.click())
+
+      getByRole('button', {name: 'Update Module'}).click()
+      expect((await findAllByText('Module access updated successfully.'))[0]).toBeInTheDocument()
+      const requestBody = fetchMock.lastOptions(ASSIGNMENT_OVERRIDES_URL)?.body
+      const expectedPayload = JSON.stringify({
+        overrides: [{course_section_id: SECTIONS_DATA[0].id}],
+      })
+      expect(requestBody).toEqual(expectedPayload)
+    })
+
+    it('updates existing assignment overrides', async () => {
+      fetchMock.getOnce(ASSIGNMENT_OVERRIDES_URL, ASSIGNMENT_OVERRIDES_DATA, {
+        overwriteRoutes: true,
+      })
+      fetchMock.put(ASSIGNMENT_OVERRIDES_URL, {})
+      const studentsOverride = ASSIGNMENT_OVERRIDES_DATA[0]
+      const existingOverride = ASSIGNMENT_OVERRIDES_DATA[1]
+      const {findByTestId, findByText, getByTestId, getByRole, findAllByText} = renderComponent()
+      act(() => getByTestId('custom-option').click())
+      const assigneeSelector = await findByTestId('assignee_selector')
+      act(() => assigneeSelector.click())
+      const option1 = await findByText(existingOverride.course_section?.name!)
+      // removing the existing section override
+      act(() => option1.click())
+
+      getByRole('button', {name: 'Update Module'}).click()
+      expect((await findAllByText('Module access updated successfully.'))[0]).toBeInTheDocument()
+      const requestBody = fetchMock.lastOptions(ASSIGNMENT_OVERRIDES_URL)?.body
+      // it sends back the student list override, including the assignment override id
+      const expectedPayload = JSON.stringify({
+        overrides: [
+          {id: studentsOverride.id, student_ids: studentsOverride.students!.map(({id}) => id)},
+        ],
+      })
+      expect(requestBody).toEqual(expectedPayload)
+    })
   })
 })
