b77f271c2591c678c3339f547fdf65b50b6ee76b
Robin Kuss <rkuss@instructure.com>
2023-10-16 10:50:03 -0500

    Unskip selective release tests
    These tests have not failed in multiple jenkins runs and local
    flakey spec testing.
    
    Closes LF-931
    
    Test Plan: passes Jenkins
    
    Change-Id: I7d108d9585a64161a601176bb22c18e9eab54024
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/330463
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/context_modules/shared_examples/module_selective_release_shared_examples.rb
+++ spec/selenium/context_modules/shared_examples/module_selective_release_shared_examples.rb
@@ -222,8 +222,7 @@ shared_examples_for "selective release module tray requirements" do |context|
     end
   end
 
-  # FOO-3934 became flaky after InstUI 8 upgrade
-  skip "adds two requirements for complete all requirements with sequential order FOO-3934" do
+  it "adds two requirements for complete all requirements with sequential order" do
     get @mod_url
 
     scroll_to_the_top_of_modules_page
@@ -244,8 +243,7 @@ shared_examples_for "selective release module tray requirements" do |context|
     expect(require_sequential_progress(@module.id).attribute("textContent")).to eq("true")
   end
 
-  # FOO-3934 became flaky after InstUI 8 upgrade
-  skip "adds a requirement and validates complete one requirement pill FOO-3934" do
+  it "adds a requirement and validates complete one requirement pill" do
     get @mod_url
 
     scroll_to_the_top_of_modules_page
@@ -261,8 +259,7 @@ shared_examples_for "selective release module tray requirements" do |context|
     validate_correct_pill_message(@module.id, "Complete One Item")
   end
 
-  # FOO-3934 became flaky after InstUI 8 upgrade
-  skip "cancels a requirement session FOO-3934" do
+  it "cancels a requirement session" do
     get @mod_url
 
     scroll_to_the_top_of_modules_page
