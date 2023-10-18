6c88fc14e8da812f7e79469fd1019514742147f0
Jeremy Stanley <jeremy@instructure.com>
2023-10-18 14:53:12 -0600

    scope smart search to course for now
    and API-ify it while we're at it
    
    test plan:
     - smart search API should be documented
       and should function as shown
    
    flag=smart_search
    closes ADV-24
    closes ADV-43
    
    Change-Id: If5bc14338a15d6cd75e0a107916b7fd5d4165e8e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/330693
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jonathan Featherstone <jfeatherstone@instructure.com>
    QA-Review: Jonathan Featherstone <jfeatherstone@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/controllers/smart_search_controller.rb
+++ app/controllers/smart_search_controller.rb
@@ -16,14 +16,68 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# @API Smart Search
+# @beta
+#
+# API for AI-powered course content search. NOTE: This feature has limited availability at present.
+#
+# @model SearchResult
+#     {
+#       "id": "SearchResult",
+#       "description": "Reference to an object that matches a smart search",
+#       "properties": {
+#         "content_id": {
+#           "description": "The ID of the matching object.",
+#           "example": 2,
+#           "type": "integer",
+#           "format": "int64"
+#         },
+#         "content_type": {
+#           "description": "The type of the matching object.",
+#           "example": "WikiPage",
+#           "type": "string"
+#         },
+#         "title": {
+#           "description": "The title of the matching object.",
+#           "example": "Nicolaus Copernicus",
+#           "type": "string"
+#         },
+#        "body": {
+#          "description": "The body of the matching object.",
+#          "example": "Nicolaus Copernicus was a Renaissance-era mathematician and astronomer who...",
+#          "type": "string"
+#         },
+#         "html_url": {
+#           "description": "The Canvas URL of the matching object.",
+#           "example": "https://canvas.example.com/courses/123/pages/nicolaus-copernicus",
+#           "type": "string"
+#         },
+#         "distance": {
+#           "description": "The distance between the search query and the result. Smaller numbers indicate closer matches.",
+#           "example": "0.212",
+#           "type": "number"
+#         }
+#       }
+#     }
+#
 class SmartSearchController < ApplicationController
+  include Api::V1::SearchResult
+
+  before_action :require_context, only: :search
   before_action :require_user
 
   # TODO: Other ways of tuning results?
   MIN_DISTANCE = 0.70
 
-  def index
-    # TODO: permission check?
+  # @API Search course content
+  # Find course content using a meaning-based search
+  #
+  # @argument q [String, required]
+  #   The search query
+  #
+  # @returns [SearchResult]
+  def search
+    return render_unauthorized_action unless @context.grants_right?(@current_user, session, :read)
     return render_unauthorized_action unless OpenAi.smart_search_available?(@domain_root_account)
     return render json: { error: "missing 'q' param" }, status: :bad_request unless params.key?(:q)
 
@@ -38,27 +92,15 @@ class SmartSearchController < ApplicationController
         # Prototype query using "neighbor". Embedding is now on join table so manual SQL for now
         # wiki_pages = WikiPage.nearest_neighbors(:embedding, embedding, distance: "inner_product")
         # response[:results].concat( wiki_pages.select { |x| x.neighbor_distance >= MIN_DISTANCE }.first(MAX_RESULT))
-
         # Wiki Pages
-        # TODO: Enforce enrollment types
-        scope = WikiPage
-                .select(WikiPage.send(:sanitize_sql, ["wiki_pages.*, MIN(wpe.embedding <=> ?) AS distance", embedding.to_s]))
-                .joins("INNER JOIN #{Enrollment.quoted_table_name} e ON wiki_pages.context_type = 'Course' AND wiki_pages.context_id = e.course_id")
-                .joins("INNER JOIN #{EnrollmentState.quoted_table_name} es ON e.id = es.enrollment_id")
-                .joins("INNER JOIN #{WikiPageEmbedding.quoted_table_name} wpe ON wiki_pages.id = wpe.wiki_page_id")
-                .where(e: {
-                         user_id: @current_user,
-                         type: %w[TeacherEnrollment TaEnrollment DesignerEnrollment StudentViewEnrollment]
-                       },
-                       es: {
-                         restricted_access: false,
-                         state: %w[active invited pending_invited pending_active]
-                       })
-                .where.not(e: { workflow_state: "deleted" })
-                .group("wiki_pages.id")
-                .order("distance ASC")
-        wiki_pages = Api.paginate(scope, self, smart_search_query_url)
-        response[:results].concat(wiki_pages_search_json(wiki_pages))
+        scope = @context.wiki_pages.not_deleted
+        scope = WikiPages::ScopedToUser.new(@context, @current_user, scope).scope
+                                       .select(WikiPage.send(:sanitize_sql, ["wiki_pages.*, MIN(wpe.embedding <=> ?) AS distance", embedding.to_s]))
+                                       .joins("INNER JOIN #{WikiPageEmbedding.quoted_table_name} wpe ON wiki_pages.id = wpe.wiki_page_id")
+                                       .group("wiki_pages.id")
+                                       .order("distance ASC")
+        wiki_pages = Api.paginate(scope, self, api_v1_course_smart_search_query_url(@context))
+        response[:results].concat(search_results_json(wiki_pages))
       end
 
       render json: response
