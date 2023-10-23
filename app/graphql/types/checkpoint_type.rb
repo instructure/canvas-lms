26b45607e32f096f7053c5fd294f4b764ce9d794
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2023-10-23 15:50:54 -0400

    Add checkpoint fields to Assignment Type
    refs VICE-3835
    flag=discussion_checkpoints
    
    test plan:
      - Specs pass.
      - You can test it with this query:
    query MyQuery {
      course(id: "1") {
        _id
        assetString
        name
        assignmentsConnection(filter: {userId: "1"}) {
          nodes {
            _id
            name
            checkpointed
            checkpoints {
              name
              label
              pointsPossible
              dueAt
              onlyVisibleToOverrides
            }
          }
        }
      }
    }
    
    qa risk: low
    
    Change-Id: I485a50ec4e58fde273f1c0ab69a317f311329073
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/330979
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

---
+++ app/graphql/types/checkpoint_type.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Types
+  class CheckpointType < ApplicationObjectType
+    alias_method :checkpoint, :object
+
+    field :name, String, null: true
+
+    field :label,
+          String,
+          "the label of the checkpoint",
+          null: false
+    def label
+      checkpoint.checkpoint_label
+    end
+
+    field :points_possible,
+          Float,
+          "the checkpoint is out of this many points",
+          null: false
+
+    field :due_at,
+          DateTimeType,
+          "when this checkpoint is due for 'Everyone'",
+          null: true
+
+    field :only_visible_to_overrides,
+          Boolean,
+          "specifies that this checkpoint is only assigned to students for whom an override applies",
+          null: false
+  end
+end
