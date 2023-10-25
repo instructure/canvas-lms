a242a12e3803a33e5086d9a935f0ed0c2913434a
August Thornton <august@instructure.com>
2023-10-25 13:32:52 -0700

    add temp enroll pairing param to enrollments api create
    closes FOO-3988
    flag = temporary_enrollments
    
    Test plan:
     • Enable the temporary enrollments feature flag
     • Exercise the enrollments API endpoint:
        • /doc/api/enrollments.html#method.enrollments_api.create
     • In order to create a temporary enrollment for a user, pass in the
       temporary_enrollments_source_user_id and
       temporary_enrollments_pairing_id params with a value of the
       provider id and the pairing id respectively
      • Note: the pairing id will be generated by the temporary enrollment
        pairing API endpoint
    
    Change-Id: Ibb7dc6db44371d1bec14df67ce9585c9884bec77
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/331331
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

--- app/controllers/enrollments_api_controller.rb
+++ app/controllers/enrollments_api_controller.rb
@@ -705,25 +705,19 @@ class EnrollmentsApiController < ApplicationController
         value_to_boolean(params[:enrollment][:limit_privileges_to_course_section])
     end
 
-    if params[:enrollment][:temporary_enrollment_source_user_id].present?
-      temporary_enrollment_source_user_id = params[:enrollment][:temporary_enrollment_source_user_id]
-    end
-
     params[:enrollment].slice!(
       :enrollment_state,
       :section,
       :limit_privileges_to_course_section,
       :associated_user_id,
+      :temporary_enrollment_source_user_id,
+      :temporary_enrollment_pairing_id,
       :role,
       :start_at,
       :end_at,
       :self_enrolled,
       :no_notify
     )
-
-    if @domain_root_account&.feature_enabled?(:temporary_enrollments)
-      params[:enrollment][:temporary_enrollment_source_user_id] = temporary_enrollment_source_user_id
-    end
 
     SubmissionLifecycleManager.with_executing_user(@current_user) do
       @enrollment = @context.enroll_user(user, type, params[:enrollment].merge(allow_multiple_enrollments: true))
