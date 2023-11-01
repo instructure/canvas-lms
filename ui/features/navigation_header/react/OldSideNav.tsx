aabfe97489e7107525e2a0989cd1ce552fcca3b7
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-11-01 10:13:45 -0600

    Use functional component for OldSideNav
    test plan:
      - smoke test old side nav
    
    refs FOO-3642
    
    flag=none
    
    Change-Id: I342f8bb345eeec7ed440f39b649747d952644069
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/331816
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Martin Ochoa <martin.ochoa@instructure.com>
    Reviewed-by: Gustavo Bernardes <gustavo.bernardes@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/navigation_header/react/OldSideNav.tsx
+++ ui/features/navigation_header/react/OldSideNav.tsx
@@ -17,15 +17,14 @@
  */
 
 import $ from 'jquery'
+import React, {useEffect, useState, useCallback} from 'react'
 import {useScope as useI18nScope} from '@canvas/i18n'
-import React from 'react'
 import {Tray} from '@instructure/ui-tray'
 import {CloseButton} from '@instructure/ui-buttons'
 import {View} from '@instructure/ui-view'
 import {Spinner} from '@instructure/ui-spinner'
-import preventDefault from '@canvas/util/preventDefault'
 import tourPubSub from '@canvas/tour-pubsub'
-import {getTrayLabel} from './utils'
+import {getTrayLabel, getTrayPortal} from './utils'
 import NavigationBadges from './NavigationBadges'
 
 const I18n = useI18nScope('Navigation')
@@ -46,194 +45,147 @@ type ActiveItem = 'dashboard' | 'accounts' | 'courses' | 'groups' | 'profile' | 
 
 const itemsWithResources = ['courses', 'groups', 'accounts', 'profile', 'history', 'help'] as const
 
-// give the trays that slide out from the the nav bar
-// a place to mount. It has to be outside the <div id=application>
-// to aria-hide everything but the tray when open.
-let portal: HTMLDivElement | undefined
-function getPortal() {
-  if (!portal) {
-    portal = document.createElement('div')
-    portal.id = 'nav-tray-portal'
-    // the <header> has z-index: 100. This has to be behind it,
-    portal.setAttribute('style', 'position: relative; z-index: 99;')
-    document.body.appendChild(portal)
-  }
-  return portal
-}
-
 function noop() {}
 
-type Props = {}
+const Navigation = () => {
+  const [activeItem, setActiveItem] = useState<ActiveItem>('dashboard')
+  const [isTrayOpen, setIsTrayOpen] = useState(false)
+  const [noFocus, setNoFocus] = useState(false)
+  const [overrideDismiss, setOverrideDismiss] = useState(false)
+  const [type, setType] = useState<ActiveItem | null>(null)
 
-type State = {
-  activeItem: ActiveItem
-  isTrayOpen: boolean
-  noFocus: boolean
-  overrideDismiss: boolean
-  type: ActiveItem | null
-}
-
-export default class Navigation extends React.Component<Props, State> {
-  openPublishUnsubscribe: () => void = noop
-
-  overrideDismissUnsubscribe: () => void = noop
-
-  closePublishUnsubscribe: () => void = noop
-
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      activeItem: 'dashboard',
-      isTrayOpen: false,
-      noFocus: false,
-      overrideDismiss: false,
-      type: null,
+  useEffect(() => {
+    if (!isTrayOpen) {
+      // when tray is closed, set active item based on current path
+      const path = window.location.pathname
+      const matchData = path.match(EXTERNAL_TOOLS_REGEX) || path.match(ACTIVE_ROUTE_REGEX)
+      const item = (matchData && matchData[1]) as ActiveItem | null
+      setActiveItem(item || 'dashboard')
     }
-  }
+  }, [isTrayOpen])
 
-  componentDidMount() {
-    /**
-     * Mount up stuff to our existing DOM elements, yes, it's not very
-     * React-y, but it is workable and maintainable, plus it doesn't require
-     * us to trash what Rails has already rendered.
-     */
+  useEffect(() => {
+    $(`.${ACTIVE_CLASS}`).removeClass(ACTIVE_CLASS).removeAttr('aria-current')
+    $(`#global_nav_${activeItem}_link`)
+      .closest('li')
+      .addClass(ACTIVE_CLASS)
+      .attr('aria-current', 'page')
+  }, [activeItem])
 
-    // ////////////////////////////////
-    // / Click Events
-    // ////////////////////////////////
-    itemsWithResources.forEach(type => {
-      $(`#global_nav_${type}_link`).on(
-        'click',
-        preventDefault(this.handleMenuClick.bind(this, type))
-      )
+  const openTray = useCallback((type_: ActiveItem, focus: boolean = false) => {
+    setType(type_)
+    setNoFocus(focus)
+    setIsTrayOpen(true)
+    setActiveItem(type_)
+  }, [])
+
+  const closeTray = useCallback(() => {
+    setIsTrayOpen(false)
+    setNoFocus(false)
+    setTimeout(() => setType(null), 150)
+  }, [])
+
+  const handleMenuClick = useCallback(
+    (type_: ActiveItem) => {
+      if (isTrayOpen && activeItem === type_) {
+        closeTray()
+      } else if (isTrayOpen && activeItem !== type_) {
+        openTray(type_)
+      } else {
+        openTray(type_)
+      }
+    },
+    [activeItem, closeTray, isTrayOpen, openTray]
+  )
+
+  useEffect(() => {
+    itemsWithResources.forEach(type_ => {
+      $(`#global_nav_${type_}_link`).on('click', (event: Event) => {
+        event.preventDefault()
+        handleMenuClick(type_)
+      })
     })
-    this.openPublishUnsubscribe = tourPubSub.subscribe<{
+
+    return () => {
+      itemsWithResources.forEach(type_ => {
+        $(`#global_nav_${type_}_link`).off('click')
+      })
+    }
+  }, [handleMenuClick])
+
+  /*
+  begin student tour code
+  */
+  useEffect(() => {
+    const openPublishUnsubscribe = tourPubSub.subscribe<{
       type: ActiveItem
       noFocus: boolean
-    }>('navigation-tray-open', ({type, noFocus}) => {
-      this.openTray(type, noFocus)
-
-      // If we're already open for the specified type
-      // send a message that we are open.
-      if (this.state.isTrayOpen && this.state.type === type) {
-        tourPubSub.publish('navigation-tray-opened', type)
+    }>('navigation-tray-open', ({type: type_, noFocus: noFocus_}) => {
+      openTray(type_, noFocus_)
+      if (isTrayOpen && type_ === type) {
+        tourPubSub.publish('navigation-tray-opened', type_)
       }
     })
-    this.closePublishUnsubscribe = tourPubSub.subscribe('navigation-tray-close', () => {
-      this.closeTray()
-    })
-    this.overrideDismissUnsubscribe = tourPubSub.subscribe(
+
+    const closePublishUnsubscribe = tourPubSub.subscribe('navigation-tray-close', closeTray)
+
+    const overrideDismissUnsubscribe = tourPubSub.subscribe(
       'navigation-tray-override-dismiss',
-      tf => {
-        this.setState({overrideDismiss: Boolean(tf)})
-      }
+      tf => setOverrideDismiss(Boolean(tf))
     )
-  }
 
-  componentWillUnmount() {
-    this.openPublishUnsubscribe && this.openPublishUnsubscribe()
-    this.overrideDismissUnsubscribe && this.overrideDismissUnsubscribe()
-    this.closePublishUnsubscribe && this.closePublishUnsubscribe()
-  }
-
-  componentDidUpdate(_prevProps: Props, prevState: State) {
-    if (prevState.activeItem !== this.state.activeItem) {
-      $(`.${ACTIVE_CLASS}`).removeClass(ACTIVE_CLASS).removeAttr('aria-current')
-      $(`#global_nav_${this.state.activeItem}_link`)
-        .closest('li')
-        .addClass(ACTIVE_CLASS)
-        .attr('aria-current', 'page')
+    return () => {
+      openPublishUnsubscribe()
+      overrideDismissUnsubscribe()
+      closePublishUnsubscribe()
     }
-  }
+  }, [isTrayOpen, type, closeTray, openTray])
+  /*
+  end student tour code
+  */
 
-  determineActiveLink() {
-    const path = window.location.pathname
-    const matchData = path.match(EXTERNAL_TOOLS_REGEX) || path.match(ACTIVE_ROUTE_REGEX)
-    const activeItem = (matchData && matchData[1]) as ActiveItem | null
-    if (!activeItem) {
-      this.setState({activeItem: 'dashboard'})
-    } else {
-      this.setState({activeItem})
-    }
-  }
-
-  handleMenuClick(type: ActiveItem) {
-    if (this.state.isTrayOpen && this.state.activeItem === type) {
-      this.closeTray()
-    } else if (this.state.isTrayOpen && this.state.activeItem !== type) {
-      this.openTray(type)
-    } else {
-      this.openTray(type)
-    }
-  }
-
-  openTray(type: ActiveItem, noFocus: boolean = false) {
-    // Sometimes we don't want the tray to capture focus,
-    // so we specify that here.
-    this.setState({type, noFocus, isTrayOpen: true, activeItem: type})
-  }
-
-  closeTray = () => {
-    this.determineActiveLink()
-    // Regardless of whether it captured focus before,
-    // we should make sure it does on future openings.
-    this.setState({isTrayOpen: false, noFocus: false}, () => {
-      setTimeout(() => {
-        this.setState({type: null})
-      }, 150)
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <Tray
-          key={this.state.type}
-          label={getTrayLabel(this.state.type)}
-          size="small"
-          open={this.state.isTrayOpen}
-          // We need to override closing trays
-          // so the tour can properly go through them
-          // without them unexpectedly closing.
-          onDismiss={this.state.overrideDismiss ? noop : this.closeTray}
-          shouldCloseOnDocumentClick={true}
-          shouldContainFocus={!this.state.noFocus}
-          mountNode={getPortal()}
-          themeOverride={{smallWidth: '28em'}}
-          onEntered={() => {
-            tourPubSub.publish('navigation-tray-opened', this.state.type)
-          }}
-        >
-          <div className={`navigation-tray-container ${this.state.type}-tray`}>
-            <CloseButton
-              placement="end"
-              onClick={this.closeTray}
-              screenReaderLabel={I18n.t('Close')}
-            />
-            <div className="tray-with-space-for-global-nav">
-              <React.Suspense
-                fallback={
-                  <View display="block" textAlign="center">
-                    <Spinner
-                      size="large"
-                      margin="large auto"
-                      renderTitle={() => I18n.t('Loading')}
-                    />
-                  </View>
-                }
-              >
-                {this.state.type === 'courses' && <CoursesTray />}
-                {this.state.type === 'groups' && <GroupsTray />}
-                {this.state.type === 'accounts' && <AccountsTray />}
-                {this.state.type === 'profile' && <ProfileTray />}
-                {this.state.type === 'history' && <HistoryTray />}
-                {this.state.type === 'help' && <HelpTray closeTray={this.closeTray} />}
-              </React.Suspense>
-            </div>
+  return (
+    <>
+      <Tray
+        key={type}
+        label={getTrayLabel(type)}
+        size="small"
+        open={isTrayOpen}
+        // We need to override closing trays
+        // so the tour can properly go through them
+        // without them unexpectedly closing.
+        onDismiss={overrideDismiss ? noop : closeTray}
+        shouldCloseOnDocumentClick={true}
+        shouldContainFocus={!noFocus}
+        mountNode={getTrayPortal()}
+        themeOverride={{smallWidth: '28em'}}
+        onEntered={() => {
+          tourPubSub.publish('navigation-tray-opened', type)
+        }}
+      >
+        <div className={`navigation-tray-container ${type}-tray`}>
+          <CloseButton placement="end" onClick={closeTray} screenReaderLabel={I18n.t('Close')} />
+          <div className="tray-with-space-for-global-nav">
+            <React.Suspense
+              fallback={
+                <View display="block" textAlign="center">
+                  <Spinner size="large" margin="large auto" renderTitle={() => I18n.t('Loading')} />
+                </View>
+              }
+            >
+              {type === 'courses' && <CoursesTray />}
+              {type === 'groups' && <GroupsTray />}
+              {type === 'accounts' && <AccountsTray />}
+              {type === 'profile' && <ProfileTray />}
+              {type === 'history' && <HistoryTray />}
+              {type === 'help' && <HelpTray closeTray={closeTray} />}
+            </React.Suspense>
           </div>
-        </Tray>
-        <NavigationBadges />
-      </>
-    )
-  }
+        </div>
+      </Tray>
+      <NavigationBadges />
+    </>
+  )
 }
+
+export default Navigation
