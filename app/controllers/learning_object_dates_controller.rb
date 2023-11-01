0a250d37ecae2ec65e6a71ffc1d229ab639cee91
Jackson Howe <jackson.howe@instructure.com>
2023-11-01 14:33:11 -0600

    Move date_details API to its own controller
    Since this API will be used for more than just assignments and
    quizzes.
    
    closes LF-973
    flag = differentiated_modules
    
    Test plan:
     - Create an assignment with some overrides (including at least 1 ADHOC
       override)
     - GET /api/v1/courses/:course_id/assignments/:id/date_details
     - Expect to see the assignment's id, lock_at, unlock_at, due_at, and
       only_visible_to_overrides columns
     - Expect a list of overrides
     - For the ADHOC override, expect to see an attribute "students" with
       a list of student id and name objects
     - Repeat for a classic quiz using
       GET /api/v1/courses/:course_id/quizzes/:id/date_details
    
    Change-Id: Ia8f7d51b7c8904980341d180bd1e4881f031c754
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/331842
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

---
+++ app/controllers/learning_object_dates_controller.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+# @API Learning Object Dates
+#
+# API for accessing date-related attributes on assignments and quizzes.
+#
+# @model LearningObjectDates
+#     {
+#       "id": "LearningObjectDates",
+#       "description": "",
+#       "properties": {
+#         "id": {
+#           "description": "the ID of the learning object",
+#           "example": 4,
+#           "type": "integer"
+#         },
+#         "due_at": {
+#           "description": "the due date for the learning object. returns null if not present or applicable",
+#           "example": "2012-07-01T23:59:00-06:00",
+#           "type": "datetime"
+#         },
+#         "lock_at": {
+#           "description": "the lock date (learning object is locked after this date). returns null if not present",
+#           "example": "2012-07-01T23:59:00-06:00",
+#           "type": "datetime"
+#         },
+#         "unlock_at": {
+#           "description": "the unlock date (learning object is unlocked after this date). returns null if not present",
+#           "example": "2012-07-01T23:59:00-06:00",
+#           "type": "datetime"
+#         },
+#         "only_visible_to_overrides": {
+#           "description": "whether the learning object is only visible to overrides",
+#           "example": false,
+#           "type": "boolean"
+#         },
+#         "overrides": {
+#           "description": "paginated list of AssignmentOverride objects",
+#           "type": "array",
+#           "items": { "$ref": "AssignmentOverride" }
+#         }
+#       }
+#     }
+class LearningObjectDatesController < ApplicationController
+  before_action :require_feature_flag # remove when differentiated_modules flag is removed
+  before_action :require_user
+  before_action :require_context
+  before_action :check_authorized_action
+
+  include Api::V1::LearningObjectDates
+  include Api::V1::AssignmentOverride
+
+  # @API Get a learning object's date information
+  #
+  # Get a learning object's date-related information, including due date, availability dates,
+  # override status, and a paginated list of all assignment overrides for the item.
+  #
+  # Note: this API is still under development and will not function until the feature is enabled.
+  #
+  # @returns LearningObjectDates
+  def show
+    route = polymorphic_url([:api_v1, @context, asset, :date_details])
+    overrides = Api.paginate(asset.assignment_overrides.active, self, route)
+    render json: {
+      **learning_object_dates_json(asset, @current_user, session),
+      overrides: assignment_overrides_json(overrides, @current_user, include_student_names: true)
+    }
+  end
+
+  private
+
+  def require_feature_flag
+    not_found unless Account.site_admin.feature_enabled? :differentiated_modules
+  end
+
+  def check_authorized_action
+    render_unauthorized_action unless @context.grants_any_right?(@current_user, :manage_content, :manage_course_content_edit)
+  end
+
+  def asset
+    @asset ||= if params[:assignment_id]
+                 @context.active_assignments.find(params[:assignment_id])
+               elsif params[:quiz_id]
+                 @context.active_quizzes.find(params[:quiz_id])
+               end
+  end
+end
