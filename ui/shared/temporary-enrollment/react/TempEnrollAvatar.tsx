a16ce34fc6f8710e4cfc371a07829e5451b60f30
Michael Hulse <michael.hulse@instructure.com>
2023-11-06 20:30:15 -0800

    update temp enrollment UI to match Figma spec
    This commit improves the styling of the temporary enrollments
    modal to match the Figma design. This includes adjustments to
    the modal’s spacing, alignment, and responsive behaviors.
    
    Introduced the TempEnrollAvatar component to encapsulate the
    common avatar presentation logic, ensuring a consistent look
    and feel across the application as per the design spec.
    
    refs FOO-3975
    flag=temporary_enrollments
    
    test plan:
    - tests pass
    
    Change-Id: Iddef8e22b0fdae9acd10e93e8f2f0bb4ec8d38f0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/332266
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/shared/temporary-enrollment/react/TempEnrollAvatar.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {ReactNode} from 'react'
+import {Flex} from '@instructure/ui-flex'
+import {Text} from '@instructure/ui-text'
+import {Avatar} from '@instructure/ui-avatar'
+import {User} from './types'
+import {useScope as useI18nScope} from '@canvas/i18n'
+
+const I18n = useI18nScope('temporary_enrollment')
+
+type Props = {
+  user: User
+  children?: ReactNode
+}
+
+export function TempEnrollAvatar(props: Props) {
+  return (
+    <Flex gap="x-small">
+      <Flex.Item>
+        <Avatar
+          size="small"
+          name={props.user.name}
+          src={props.user.avatar_url}
+          data-fs-exclude={true}
+          data-heap-redact-attributes="name"
+          alt={I18n.t('Avatar for %{name}', {name: props.user.name})}
+        />
+      </Flex.Item>
+      <Flex.Item shouldGrow={true}>
+        <Text>{props.children ?? props.user.name}</Text>
+      </Flex.Item>
+    </Flex>
+  )
+}
