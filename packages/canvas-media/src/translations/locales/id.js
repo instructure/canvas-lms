e10aa0d4b938ee70f98c58776fc64bad54550414
Jenkins <svc.cloudjenkins@instructure.com>
2023-11-08 04:51:12 +0000

    [i18n] Update canvas-media translations.
    Change-Id: Ifc558a21f3efdfb48331b030f5bfc145aaa38798
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/332351
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ packages/canvas-media/src/translations/locales/id.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import formatMessage from '../../format-message'
+
+const locale = {
+  "afrikaans_da0fe6ee": { "message": "Afrikaan" },
+  "albanian_21ed929e": { "message": "Albania" },
+  "arabic_c5c87acd": { "message": "Arab" },
+  "armenian_12da6118": { "message": "Armenia" },
+  "belarusian_b2f19c76": { "message": "Belarus" },
+  "bulgarian_feccab7e": { "message": "Bulgaria" },
+  "catalan_16f6b78f": { "message": "Catalan" },
+  "chinese_111d37f6": { "message": "Mandarin" },
+  "chinese_simplified_7f0bd370": { "message": "Mandarin Tiongkok" },
+  "chinese_traditional_8a7f759d": { "message": "Mandarin Taiwan" },
+  "croatian_d713d655": { "message": "Kroasia" },
+  "czech_9aa2cbe4": { "message": "Cheska" },
+  "danish_c18cdac8": { "message": "Denmark" },
+  "dutch_6d05cee5": { "message": "Belanda" },
+  "english_australia_dc405d82": { "message": "Inggris (Australia)" },
+  "english_c60612e2": { "message": "Inggris" },
+  "english_canada_12688ee4": { "message": "Inggris (Kanada)" },
+  "english_united_kingdom_a613f831": { "message": "Inggris (Britania Raya)" },
+  "estonian_5e8e2fa4": { "message": "Estonia" },
+  "file_name_8fd421ff": { "message": "Nama file" },
+  "filipino_33339264": { "message": "Filipina" },
+  "finnish_4df2923d": { "message": "Finlandia" },
+  "french_33881544": { "message": "Prancis" },
+  "french_canada_c3d92fa6": { "message": "Prancis (Kanada)" },
+  "galician_7e4508b5": { "message": "Galisia" },
+  "german_3ec99bbb": { "message": "Jerman" },
+  "greek_65c5b3f7": { "message": "Yunani" },
+  "haitian_creole_7eb4195b": { "message": "Creole Haiti" },
+  "hebrew_88fbf778": { "message": "Ibrani" },
+  "hindi_9bcd4b34": { "message": "Hindi" },
+  "hungarian_fc7d30c9": { "message": "Hongaria" },
+  "icelandic_9d6d35de": { "message": "Islandia" },
+  "indonesian_5f6accd6": { "message": "Indonesia" },
+  "irish_567e109f": { "message": "Irlandia" },
+  "italian_bd3c792d": { "message": "Italia" },
+  "japanese_b5721ca7": { "message": "Jepang" },
+  "korean_da812d9": { "message": "Korea" },
+  "latvian_2bbb6aab": { "message": "Latvia" },
+  "lithuanian_5adcbe24": { "message": "Lithuania" },
+  "loading_25990131": { "message": "Memuat..." },
+  "macedonian_6ed541af": { "message": "Makedonia" },
+  "malay_f5dddce4": { "message": "Melayu" },
+  "maltese_916925e8": { "message": "Malta" },
+  "maori_new_zealand_5380a95f": { "message": "Māori (Selandia Baru)" },
+  "no_preview_is_available_for_this_file_f940114a": {
+    "message": "Pratinjau tidak tersedia untuk file ini."
+  },
+  "norwegian_53f391ec": { "message": "Norwegia" },
+  "norwegian_bokmal_ad5843fa": { "message": "Norwegia Bokmål" },
+  "norwegian_nynorsk_c785f8a6": { "message": "Norwegia Nynorsk" },
+  "persian_a8cadb95": { "message": "Persia" },
+  "polish_4cf2ecaf": { "message": "Polandia" },
+  "portuguese_9c212cf4": { "message": "Portugis" },
+  "romanian_13670c1e": { "message": "Rumania" },
+  "russian_1e3e197": { "message": "Rusia" },
+  "serbian_7187f1f2": { "message": "Serbia" },
+  "slovak_69f48e1b": { "message": "Slovak" },
+  "slovenian_30ae5208": { "message": "Slovenia" },
+  "spanish_de9de5d6": { "message": "Spanyol" },
+  "swahili_5caeb4ba": { "message": "Swahili" },
+  "swedish_59a593ca": { "message": "Swedia" },
+  "tagalog_74906db7": { "message": "Tagalog" },
+  "thai_8f9bc548": { "message": "Thai" },
+  "turkish_5b69578b": { "message": "Turki" },
+  "ukrainian_945b00b7": { "message": "Ukraina" },
+  "vietnamese_e7a76583": { "message": "Vietnam" },
+  "welsh_42ab94b1": { "message": "Wales" },
+  "yiddish_f96986df": { "message": "Yiddish" }
+}
+
+
+formatMessage.addLocale({id: locale})
