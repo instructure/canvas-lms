c9ba9b531daddb54e1cb6b9c9cf48ec14733a75b
Jason Gillett <jason.gillett@instructure.com>
2023-11-15 17:28:42 -0700

    Allow DT_mutation to update assignment
    closes VICE-3891
    flag=discussion_create
    
    Test Plan
    1. open the graphiql interface
    2. Update a graded discussion's assignment
    3. use the updateDiscussionTopic mutation
    3a to change assignment attributes
    3b to remove an assigment (set_assigment to false )
    3c restore an assignment ( should retain assignment id )
    
    Steps for a very thorough QA
    Do each step in graphiql and legacy modal
    Compare for differences
    1. update topic to become graded
    2. Update topic to become ungraded
    3. Updatetopic  to become graded with other changes
    4. Update all topic's assignment variables
    5. update topic properties while removing graded
    
    Change-Id: I524f615f43782033448aabb1067a14bc977895fe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/332869
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- app/graphql/mutations/update_discussion_topic.rb
+++ app/graphql/mutations/update_discussion_topic.rb
@@ -27,6 +27,8 @@ class Mutations::UpdateDiscussionTopic < Mutations::DiscussionBase
 
   field :discussion_topic, Types::DiscussionType, null: false
   def resolve(input:)
+    @current_user = current_user
+
     discussion_topic = DiscussionTopic.find(input[:discussion_topic_id])
     raise GraphQL::ExecutionError, "insufficient permission" unless discussion_topic.grants_right?(current_user, :update)
 
@@ -52,12 +54,85 @@ class Mutations::UpdateDiscussionTopic < Mutations::DiscussionBase
     process_common_inputs(input, discussion_topic.is_announcement, discussion_topic)
     process_future_date_inputs(input[:delayed_post_at], input[:lock_at], discussion_topic)
 
-    return errors_for(discussion_topic) unless discussion_topic.save
+    # Take care of Assignment update information
+    if input[:assignment]
+      assignment_id = (discussion_topic&.assignment&.id || discussion_topic.old_assignment_id)
+
+      if assignment_id
+        # If a current or old assignment exists already, then update it
+        unless discussion_topic.root_topic_id?
+          # The UpdateAssignment mutation requires an id, so we need to add it to the input
+          updated_assignment_args = input[:assignment].to_h.merge(
+            id: assignment_id.to_s
+          )
+          set_discussion_assignment_association(updated_assignment_args, discussion_topic)
+
+          # Instantiate and execute UpdateAssignment mutation
+          assignment_mutation = Mutations::UpdateAssignment.new(object: nil, context:, field: nil)
+          assignment_result = assignment_mutation.resolve(input: updated_assignment_args)
+
+          if assignment_result[:errors]
+            return { errors: assignment_result[:errors] }
+          end
+        end
+      elsif input[:assignment][:set_assignment].nil? || input[:assignment][:set_assignment]
+        # Create a new Assignment if set_assignment doesn't exist or is true.
+
+        course_id = discussion_topic.course.id
+        assignment_name = discussion_topic.title
+
+        # Update the input hash with course_id and name. They are required for the CreateAssignment mutation
+        assignment_input = input[:assignment].to_h.merge({
+                                                           course_id: course_id.to_s,
+                                                           name: assignment_name
+                                                         })
+
+        # Instantiate and execute CreateAssignment mutation
+        assignment_create_mutation = Mutations::CreateAssignment.new(object:, context:, field: nil)
+        assignment_create_result = assignment_create_mutation.resolve(input: assignment_input)
+
+        if assignment_create_result[:errors]
+          return { errors: assignment_create_result[:errors] }
+        end
+
+        discussion_topic.assignment = assignment_create_result[:assignment]
+      end
+    end
+
+    return errors_for(discussion_topic) unless discussion_topic.save!
+
+    discussion_topic.assignment = assignment_result[:assignment] if assignment_result && assignment_result[:assignment]
 
     {
       discussion_topic:
     }
   rescue ActiveRecord::RecordNotFound
     raise GraphQL::ExecutionError, "not found"
+  end
+end
+
+def set_discussion_assignment_association(assignment_params, discussion_topic)
+  # Determine if the assignment is being deleted
+  is_deleting_assignment = assignment_params.key?(:set_assignment) && !assignment_params[:set_assignment]
+
+  if is_deleting_assignment && !discussion_topic&.assignment.nil?
+    assignment = discussion_topic.assignment
+    discussion_topic.assignment = nil
+    assignment.discussion_topic = nil
+    assignment.destroy
+  elsif (assignment = discussion_topic.assignment || discussion_topic.old_assignment)
+    # Update assignment_params
+    assignment_params[:state] = discussion_topic.published? ? "published" : "unpublished"
+    assignment_params[:name] = discussion_topic.title
+
+    # If a topic doesn't have a group_category_id or has submissions, then the assignment group_category_id should be nil
+    unless discussion_topic.try(:group_category_id) || assignment.has_submitted_submissions?
+      assignment_params[:group_category_id] = nil
+    end
+    # Finalize assignment restoration
+    discussion_topic.assignment = assignment
+    discussion_topic.sync_assignment
+    # This save is required to prevent an extra discussion_topic from being created in the updateAssignment
+    assignment.save!
   end
 end
