23bafd07321d12af4a856c73c04b0fefe3ccea75
Jeremy Stanley <jeremy@instructure.com>
2023-11-17 11:05:11 -0700

    add manage_impact permission
    test plan:
    "Impact - Manage" should appear in account permissions
    
    flag=none
    closes FOO-4060
    
    Change-Id: Id9baf994fecbed3a120e6554e11415491899df91
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/333161
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

---
+++ ui/features/permissions/react/templates/manage_impact.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {generateActionTemplates} from '../generateActionTemplates'
+
+const I18n = useI18nScope('permissions_templates_80')
+
+export const template = generateActionTemplates(
+  [
+    {
+      title: I18n.t('Impact - Manage'),
+      description: I18n.t(
+        'Allows an account administrator to manage the Impact service integration.'
+      ),
+    },
+  ],
+  [
+    {
+      title: I18n.t('Impact'),
+      description: I18n.t('Impact is an add-on to Canvas LMS. Contact your CSM if interested.'),
+    },
+  ]
+)
