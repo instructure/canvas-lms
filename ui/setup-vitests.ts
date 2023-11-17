e1384057d11f5e8f0ebff9f616fe037a9f8b1a5b
Aaron Shafovaloff <ashafovaloff@instructure.com>
2023-11-17 05:54:42 -0700

    Install Vitest
    This does not yet change our build process
    
    Demo:
      - yarn run test:vitest:watch ui/features/theme_editor/
    
    Test plan:
      - build passes
    
    Closes CFA-17
    
    flag=none
    
    Change-Id: I6eac4684af62525829e36f5dd0c64bb80c638ca3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/333130
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Martin Ochoa <martin.ochoa@instructure.com>
    Reviewed-by: Gustavo Bernardes <gustavo.bernardes@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Build-Review: Andrea Cirulli <andrea.cirulli@instructure.com>

---
+++ ui/setup-vitests.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import '@testing-library/jest-dom'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+import {vi} from 'vitest'
+
+Enzyme.configure({adapter: new Adapter()})
+
+vi.stubGlobal('ENV', {
+  FEATURES: {},
+})
+
+vi.stubGlobal(
+  'IntersectionObserver',
+  class IntersectionObserver {
+    observe() {}
+
+    unobserve() {}
+  }
+)
+
+vi.stubGlobal(
+  'ResizeObserver',
+  class ResizeObserver {
+    observe() {}
+
+    unobserve() {}
+
+    disconnect() {}
+  }
+)
+
+vi.stubGlobal('matchMedia', () => ({
+  matches: false,
+  addListener() {},
+  removeListener() {},
+  onchange() {},
+  media: '',
+}))
+
+vi.stubGlobal('jest', vi)
