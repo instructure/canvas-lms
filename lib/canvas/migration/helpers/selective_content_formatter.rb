ae08ac98223b0014ed00877bb0920fc1fba02ea7
Matheus <luis.oliveira@instructure.com>
2023-11-21 21:54:33 -0300

    check user permissions for blueprint setting migration
    closes LF-940
    flag=none
    
    Test Plan:
    - Create a BP course
    - With an admin user with no
      manage master courses permissions
    - Create a second course and try to
      copy the BP one into it
    - Check that the BP settings migration
      option shows up greyed out under import
      all_content
      (oh  and check there is a tooltip there explaining it)
    - Check that it does not show up as an option
      on selective import
    - Check that even if you enable the checkbox
      and use it, the settings are not imported
      regardless
    
    Change-Id: I622b00cfcf010a9a656c590782c22952c940f1c0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/333488
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Luis Oliveira <luis.oliveira@instructure.com>

--- lib/canvas/migration/helpers/selective_content_formatter.rb
+++ lib/canvas/migration/helpers/selective_content_formatter.rb
@@ -326,7 +326,8 @@ module Canvas::Migration::Helpers
           content_list << { type: "syllabus_body", property: "#{property_prefix}[all_syllabus_body]", title: COURSE_SYLLABUS_TYPE.call }
           content_list << { type: "course_paces", property: "#{property_prefix}[all_course_paces]", title: COURSE_PACE_TYPE.call } if source.course_paces.primary.not_deleted.any?
 
-          if @migration && MasterCourses::MasterTemplate.is_master_course?(source) && MasterCourses::MasterTemplate.blueprint_eligible?(@migration.context)
+          if @migration && MasterCourses::MasterTemplate.is_master_course?(source) && MasterCourses::MasterTemplate.blueprint_eligible?(@migration.context) &&
+             (@migration.context.account.grants_any_right?(@migration.user, :manage_courses, :manage_courses_admin) && @migration.context.account.grants_right?(@migration.user, :manage_master_courses))
             content_list << { type: "blueprint_settings", property: "#{property_prefix}[all_blueprint_settings]", title: BLUEPRINT_SETTING_TYPE.call }
           end
 
