e1713f65f1848e10395944086e9c93fb4045f4c0
Xander Moffatt <xmoffatt@instructure.com>
2023-12-15 10:49:03 -0700

    allow sort/filter of inherited dev keys table
    why:
    * directly copies the admin keys table,
    which is not ideal
    
    refs INTEROP-8357
    flag=none
    
    test plan:
    * go to Account -> Developer Keys and click Inherited,
    and play around with sorting by the
    various headers
    * may need to add lots of keys to Site Admin
    * confirm that they behave as expected
    * delete a key - the focus should move to the previous key, or the
    + Key button if the key was first in the list
    * add `{ max_per_page: 3}` to the API.paginate call for the endpoint
    that populates the table
    (app/controllers/developer_keys_controller.rb:44)
    * reload the page - there should only be 3 keys
    * sort the keys by name
    * click Show All Keys
    * the entire list should show and still be sorted by name
    * bonus: with a CMC (parent/child root account) setup
      * create some keys in the parent root account
      * the Inherited tab in the child root account should have two tables,
      and both should be sortable/filterable
    
    Change-Id: If0f894025e7aa89d9b981163235a53f1f1d6f8e4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/335390
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>

---
+++ ui/features/developer_keys_v2/react/__tests__/Inheritedtable.test.jsx
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2023 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import InheritedTable from '../InheritedTable'
+
+describe('InheritedTable', () => {
+  const idFor = n => `1000000000000${n}`
+
+  const devKeyList = (numKeys = 10) => {
+    return [...Array(numKeys).keys()].map(n => ({
+      id: idFor(n),
+      name: `key-${n}`,
+      email: `email-${n}`,
+      access_token_count: n * 2,
+      is_lti_key: n == 3,
+      api_key: 'abc12345678',
+      created_at: '2023-12-12T20:36:50Z',
+      visible: true,
+      developer_key_account_binding: {
+        workflow_state: n == 5 ? 'off' : 'on',
+      },
+    }))
+  }
+
+  const component = (keys, props = {}) => {
+    return render(
+      <InheritedTable
+        label="Inherited Keys"
+        prefix="inherited"
+        developerKeysList={keys || devKeyList()}
+        store={{dispatch: () => {}}}
+        actions={{
+          makeVisibleDeveloperKey: () => {},
+          makeInvisibleDeveloperKey: () => {},
+          activateDeveloperKey: () => {},
+          deactivateDeveloperKey: () => {},
+          deleteDeveloperKey: () => {},
+          editDeveloperKey: () => {},
+          developerKeysModalOpen: () => {},
+          ltiKeysSetLtiKey: () => {},
+          setBindingWorkflowState: () => {},
+        }}
+        ctx={{
+          params: {
+            contextId: '',
+          },
+        }}
+        {...props}
+      />
+    )
+  }
+
+  it('renders table with placeholder text if no keys are given', () => {
+    const wrapper = component([])
+    expect(wrapper.getByRole('table')).toBeInTheDocument()
+    expect(wrapper.getByText('Nothing here yet')).toBeInTheDocument()
+  })
+
+  it('renders a DeveloperKey for each key', () => {
+    const wrapper = component()
+    expect(wrapper.getAllByRole('row')).toHaveLength(11)
+  })
+
+  describe('when sorting table', () => {
+    const firstRow = wrapper => wrapper.getAllByRole('row')[1]
+
+    it('defaults to descending id', () => {
+      const wrapper = component()
+
+      expect(firstRow(wrapper)).toHaveTextContent('9')
+    })
+
+    it('allows sorting by name', () => {
+      const wrapper = component()
+
+      userEvent.click(wrapper.getByText('Name')) // ascending
+      expect(firstRow(wrapper)).toHaveTextContent('key-0')
+
+      userEvent.click(wrapper.getByText('Name')) // descending
+      expect(firstRow(wrapper)).toHaveTextContent('key-9')
+    })
+
+    it('allows sorting by id', () => {
+      const wrapper = component()
+
+      userEvent.click(wrapper.getByText('Id')) // ascending
+      expect(firstRow(wrapper)).toHaveTextContent(idFor(0))
+
+      userEvent.click(wrapper.getByText('Id')) // descending
+      expect(firstRow(wrapper)).toHaveTextContent(idFor(9))
+    })
+
+    it('allows sorting by type', () => {
+      const wrapper = component()
+
+      userEvent.click(wrapper.getByText('Type')) // ascending
+      expect(firstRow(wrapper)).toHaveTextContent('key-0')
+
+      userEvent.click(wrapper.getByText('Type')) // descending
+      expect(firstRow(wrapper)).toHaveTextContent('key-3')
+    })
+
+    it('allows sorting by state', () => {
+      const wrapper = component()
+
+      userEvent.click(wrapper.getByText('State')) // ascending
+      expect(firstRow(wrapper)).toHaveTextContent('off')
+
+      userEvent.click(wrapper.getByText('State')) // descending
+      expect(firstRow(wrapper)).toHaveTextContent('on')
+    })
+  })
+
+  describe('when filtering table', () => {
+    const waitForDebounce = () => new Promise(resolve => setTimeout(resolve, 400))
+
+    it('filters by selecting type', () => {
+      const wrapper = component()
+
+      userEvent.click(wrapper.getByRole('combobox'))
+      userEvent.click(wrapper.getByRole('option', {name: 'LTI Keys'}))
+      expect(wrapper.getAllByRole('row')).toHaveLength(2)
+    })
+
+    it('filters by searching for name', async () => {
+      const wrapper = component()
+
+      userEvent.type(wrapper.getByRole('searchbox'), 'key-1')
+      await waitForDebounce()
+      expect(wrapper.getAllByRole('row')).toHaveLength(2)
+    })
+
+    it('filters by searching for id', async () => {
+      const wrapper = component()
+
+      userEvent.type(wrapper.getByRole('searchbox'), idFor(1))
+      await waitForDebounce()
+      console.log(wrapper.getAllByRole('row').map(r => r.textContent))
+      expect(wrapper.getAllByRole('row')).toHaveLength(2)
+    })
+  })
+})
