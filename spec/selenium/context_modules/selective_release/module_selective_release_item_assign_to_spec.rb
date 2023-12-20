22317b9fc0430a4900948ccaec44962cd7df1884
Robin Kuss <rkuss@instructure.com>
2023-12-20 15:53:11 -0600

    add tests for assign to tray
    Closes LF-1054
    
    Test Plan: passes Jenkins
    
    Change-Id: Icf3fae6ace1d2318b9e2acd12496029e033862e0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/335833
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/context_modules/selective_release/module_selective_release_item_assign_to_spec.rb
+++ spec/selenium/context_modules/selective_release/module_selective_release_item_assign_to_spec.rb
@@ -36,10 +36,13 @@ describe "selective_release module item assign to tray" do
   include K5DashboardCommonPageObject
   include K5Common
 
+  before(:once) do
+    Account.site_admin.enable_feature! :differentiated_modules
+    course_with_teacher(active_all: true)
+  end
+
   context "using assign to tray for newly created items" do
     before(:once) do
-      Account.site_admin.enable_feature! :differentiated_modules
-      course_with_teacher(active_all: true)
       @course.context_modules.create!(name: "module1")
 
       @course.enable_feature! :quizzes_next
@@ -64,7 +67,7 @@ describe "selective_release module item assign to tray" do
       module_item = ContentTag.last
 
       manage_module_item_button(module_item).click
-      click_manage_module_item_assign_to
+      click_manage_module_item_assign_to(module_item)
 
       expect(item_tray_exists?).to be true
       expect(icon_type_exists?("Assignment")).to be true
@@ -79,7 +82,7 @@ describe "selective_release module item assign to tray" do
       module_item = ContentTag.last
 
       manage_module_item_button(module_item).click
-      click_manage_module_item_assign_to
+      click_manage_module_item_assign_to(module_item)
 
       expect(item_tray_exists?).to be true
       expect(icon_type_exists?("Quiz")).to be true
@@ -94,11 +97,142 @@ describe "selective_release module item assign to tray" do
       module_item = ContentTag.last
 
       manage_module_item_button(module_item).click
-      click_manage_module_item_assign_to
+      click_manage_module_item_assign_to(module_item)
 
       expect(item_tray_exists?).to be true
       expect(icon_type_exists?("Quiz")).to be true
       expect(item_type_text.text).to eq("Quiz")
+    end
+  end
+
+  context "assign to tray values" do
+    before(:once) do
+      module_setup
+      @module_item1 = ContentTag.find_by(context_id: @course.id, context_module_id: @module.id, content_type: "Assignment", content_id: @assignment1.id)
+      @module.update!(workflow_state: "active")
+      @student1 = student_in_course(course: @course, active_all: true, name: "Student 1").user
+      @student2 = student_in_course(course: @course, active_all: true, name: "Student 2").user
+    end
+
+    before do
+      user_session(@teacher)
+    end
+
+    it "shows tray and Everyone pill when accessing tray for an item that has no overrides" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      expect(item_tray_exists?).to be true
+      expect(module_item_assign_to_card[0]).to be_displayed
+      expect(assign_to_in_tray("Remove Everyone")[0]).to be_displayed
+    end
+
+    it "changes pills when new card is added" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      expect(item_tray_exists?).to be true
+
+      click_add_assign_to_card
+      expect(element_exists?(assign_to_in_tray_selector("Remove Everyone"))).to be_falsey
+      expect(assign_to_in_tray("Remove Everyone else")[0]).to be_displayed
+    end
+
+    it "changes first card pill to Everyone when second card deleted" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      click_add_assign_to_card
+      expect(assign_to_in_tray("Remove Everyone else")[0]).to be_displayed
+      click_delete_assign_to_card(1)
+      expect(assign_to_in_tray("Remove Everyone")[0]).to be_displayed
+    end
+
+    it "first card pill stays Everyone when student added to first card" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      select_module_item_assignee(0, @student1.name)
+
+      expect(assign_to_in_tray("Remove Everyone")[0]).to be_displayed
+      expect(assign_to_in_tray("Remove #{@student1.name}")[0]).to be_displayed
+    end
+
+    it "second card selection does not contain student when student added to first card" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+      select_module_item_assignee(0, @student1.name)
+
+      click_add_assign_to_card
+      option_elements = INSTUI_Select_options(module_item_assignee[1])
+      option_names = option_elements.map(&:text)
+      expect(option_names).not_to include(@student1.name)
+      expect(option_names).to include(@student2.name)
+    end
+
+    it "shows existing enrollments when accessing module item tray" do
+      @module_item1.assignment.assignment_overrides.create!(set_type: "ADHOC")
+      @module_item1.assignment.assignment_overrides.first.assignment_override_students.create!(user: @student1)
+
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      expect(module_item_assign_to_card[0]).to be_displayed
+      expect(module_item_assign_to_card[1]).to be_displayed
+
+      expect(assign_to_in_tray("Remove Everyone else")[0]).to be_displayed
+      expect(assign_to_in_tray("Remove #{@student1.name}")[0]).to be_displayed
+    end
+
+    it "allows for item assignment for newly-created module item" do
+      go_to_modules
+
+      add_new_module_item_and_yield("#assignments_select", "Assignment", "[ Create Assignment ]", "New Assignment Title")
+      latest_module_item = ContentTag.last
+
+      manage_module_item_button(latest_module_item).click
+      click_manage_module_item_assign_to(latest_module_item)
+
+      expect(item_tray_exists?).to be true
+      expect(module_item_assign_to_card[0]).to be_displayed
+      expect(assign_to_in_tray("Remove Everyone")[0]).to be_displayed
+
+      select_module_item_assignee(0, @student1.name)
+      expect(assign_to_in_tray("Remove #{@student1.name}")[0]).to be_displayed
+    end
+
+    it "can fill out due dates and times on card" do
+      go_to_modules
+
+      manage_module_item_button(@module_item1).click
+      click_manage_module_item_assign_to(@module_item1)
+
+      expect(item_tray_exists?).to be true
+
+      update_due_date(0, "12/31/2022")
+      update_due_time(0, "5:00 PM")
+      update_available_date(0, "12/27/2022")
+      update_available_time(0, "8:00 AM")
+      update_until_date(0, "1/7/2023")
+      update_until_time(0, "9:00 PM")
+
+      expect(assign_to_due_date(0).attribute("value")).to eq("Dec 31, 2022")
+      expect(assign_to_due_time(0).attribute("value")).to eq("5:00 PM")
+      expect(assign_to_date_and_time[0].text).to include("Saturday, December 31, 2022 5:00 PM")
+      expect(assign_to_date_and_time[1].text).to include("Tuesday, December 27, 2022 8:00 AM")
+      expect(assign_to_date_and_time[2].text).to include("Saturday, January 7, 2023 9:00 PM")
     end
   end
 end
--- spec/selenium/helpers/items_assign_to_tray.rb
+++ spec/selenium/helpers/items_assign_to_tray.rb
@@ -21,6 +21,38 @@ require_relative "../common"
 
 module ItemsAssignToTray
   #------------------------------ Selectors -----------------------------
+  def add_assign_to_card_selector
+    "[data-testid='add-card']"
+  end
+
+  def assign_to_button_selector(button_title)
+    "button[title='#{button_title}']"
+  end
+
+  def assign_to_card_delete_button_selector
+    "//button[.//*[contains(text(), 'Delete')]]"
+  end
+
+  def assign_to_date_and_time_selector
+    "[data-testid='clearable-date-time-input']"
+  end
+
+  def assign_to_date_selector
+    "//*[@data-testid = 'clearable-date-time-input']//*[contains(@class,'-dateInput')]//input"
+  end
+
+  def assign_to_option_selector(assignee)
+    "//li[.//*[contains(text(), '#{assignee}')]]"
+  end
+
+  def assign_to_time_selector
+    "//*[@data-testid = 'clearable-date-time-input']//*[contains(@class, '-select')]//input"
+  end
+
+  def module_item_assignee_selector
+    "#{module_item_assign_to_card_selector} [data-testid='assignee_selector']"
+  end
+
   def icon_type_selector(icon_type)
     "[name='Icon#{icon_type}']"
   end
@@ -29,11 +61,66 @@ module ItemsAssignToTray
     "[data-testid='item-type-text']"
   end
 
+  def module_item_assign_to_card_selector
+    "[data-testid='item-assign-to-card']"
+  end
+
   def module_item_edit_tray_selector
     "[data-testid='module-item-edit-tray']"
   end
 
+  def assign_to_in_tray_selector(button_title)
+    "#{module_item_assign_to_card_selector} #{assign_to_button_selector(button_title)}"
+  end
+
   #------------------------------ Elements ------------------------------
+  def add_assign_to_card
+    f(add_assign_to_card_selector)
+  end
+
+  def assign_to_card_delete_button
+    ffxpath(assign_to_card_delete_button_selector)
+  end
+
+  def assign_to_date
+    ffxpath(assign_to_date_selector)
+  end
+
+  def assign_to_date_and_time
+    ff(assign_to_date_and_time_selector)
+  end
+
+  def assign_to_available_from_date(card_number = 0)
+    assign_to_date[1 + card_number]
+  end
+
+  def assign_to_available_from_time(card_number = 0)
+    assign_to_time[1 + card_number]
+  end
+
+  def assign_to_due_date(card_number = 0)
+    assign_to_date[0 + card_number]
+  end
+
+  def assign_to_due_time(card_number = 0)
+    assign_to_time[0 + card_number]
+  end
+
+  def assign_to_in_tray(button_title)
+    ff(assign_to_in_tray_selector(button_title))
+  end
+
+  def assign_to_time
+    ffxpath(assign_to_time_selector)
+  end
+
+  def assign_to_until_date(card_number = 0)
+    assign_to_date[2 + card_number]
+  end
+
+  def assign_to_until_time(card_number = 0)
+    assign_to_time[2 + card_number]
+  end
 
   def icon_type(icon_type)
     f(icon_type_selector(icon_type))
@@ -43,11 +130,27 @@ module ItemsAssignToTray
     f(item_type_text_selector)
   end
 
+  def module_item_assign_to_card
+    ff(module_item_assign_to_card_selector)
+  end
+
+  def module_item_assignee
+    ff(module_item_assignee_selector)
+  end
+
   def module_item_edit_tray
     f(module_item_edit_tray_selector)
   end
 
   #------------------------------ Actions ------------------------------
+
+  def click_add_assign_to_card
+    add_assign_to_card.click
+  end
+
+  def click_delete_assign_to_card(card_number)
+    assign_to_card_delete_button[card_number].click
+  end
 
   def icon_type_exists?(icon_type)
     element_exists?(icon_type_selector(icon_type))
@@ -55,5 +158,34 @@ module ItemsAssignToTray
 
   def item_tray_exists?
     element_exists?(module_item_edit_tray_selector)
+  end
+
+  def select_module_item_assignee(card_number, assignee)
+    # module_item_assignee[card_number].click
+    click_option(module_item_assignee[card_number], assignee)
+  end
+
+  def update_due_date(card_number, due_date)
+    replace_content(assign_to_due_date(card_number), due_date, tab_out: true)
+  end
+
+  def update_due_time(card_number, due_time)
+    replace_content(assign_to_due_time(card_number), due_time, tab_out: true)
+  end
+
+  def update_available_date(card_number, available_date)
+    replace_content(assign_to_available_from_date(card_number), available_date, tab_out: true)
+  end
+
+  def update_available_time(card_number, available_time)
+    replace_content(assign_to_available_from_time(card_number), available_time, tab_out: true)
+  end
+
+  def update_until_date(card_number, until_date)
+    replace_content(assign_to_until_date(card_number), until_date, tab_out: true)
+  end
+
+  def update_until_time(card_number, until_time)
+    replace_content(assign_to_until_time(card_number), until_time, tab_out: true)
   end
 end
