5a72bb49b527a43cb0f64eaf14d2228302edabe7
Spencer Olson <solson@instructure.com>
2023-12-21 10:23:57 -0600

    move broadcast policy to assignment
    closes VICE-4023
    flag=discussion_checkpoints
    
    This change prevents us from sending notifications out for SubAssignments,
    but continues to send out notifications for Assignments.
    
    Test Plan
    - specs pass
    
    Change-Id: I62ddd33e24fe5143142afc006cce8a9242ca2f59
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/335835
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Chawn Neal <chawn.neal@instructure.com>

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -23,6 +23,60 @@ class Assignment < AbstractAssignment
 
   before_save :before_soft_delete, if: -> { will_save_change_to_workflow_state?(to: "deleted") }
 
+  set_broadcast_policy do |p|
+    p.dispatch :assignment_due_date_changed
+    p.to do |assignment|
+      # everyone who is _not_ covered by an assignment override affecting due_at
+      # (the AssignmentOverride records will take care of notifying those users)
+      excluded_ids = participants_with_overridden_due_at.to_set(&:id)
+      BroadcastPolicies::AssignmentParticipants.new(assignment, excluded_ids).to
+    end
+    p.whenever do |assignment|
+      BroadcastPolicies::AssignmentPolicy.new(assignment)
+                                         .should_dispatch_assignment_due_date_changed?
+    end
+    p.data { course_broadcast_data }
+
+    p.dispatch :assignment_changed
+    p.to do |assignment|
+      BroadcastPolicies::AssignmentParticipants.new(assignment).to
+    end
+    p.whenever do |assignment|
+      BroadcastPolicies::AssignmentPolicy.new(assignment)
+                                         .should_dispatch_assignment_changed?
+    end
+    p.data { course_broadcast_data }
+
+    p.dispatch :assignment_created
+    p.to do |assignment|
+      BroadcastPolicies::AssignmentParticipants.new(assignment).to
+    end
+    p.whenever do |assignment|
+      BroadcastPolicies::AssignmentPolicy.new(assignment)
+                                         .should_dispatch_assignment_created?
+    end
+    p.data { course_broadcast_data }
+    p.filter_asset_by_recipient do |assignment, user|
+      assignment.overridden_for(user, skip_clone: true)
+    end
+
+    p.dispatch :submissions_posted
+    p.to do |assignment|
+      assignment.course.participating_instructors
+    end
+    p.whenever do |assignment|
+      BroadcastPolicies::AssignmentPolicy.new(assignment)
+                                         .should_dispatch_submissions_posted?
+    end
+    p.data do |record|
+      if record.posting_params_for_notifications.present?
+        record.posting_params_for_notifications.merge(course_broadcast_data)
+      else
+        course_broadcast_data
+      end
+    end
+  end
+
   def find_checkpoint(sub_assignment_tag)
     sub_assignments.find_by(sub_assignment_tag:)
   end
