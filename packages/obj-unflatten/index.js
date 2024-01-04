47f102ab41268b94ecbf7fa9e2d4e72bea778ba0
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-01-04 08:38:50 -0700

    replace underscore shim usage in obj-unflatten
    refs CFA-33
    
    flag=none
    
    Change-Id: Ib5667b313229bd3b099e206b9f658c3f3a2fc859
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/336663
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- packages/obj-unflatten/index.js
+++ packages/obj-unflatten/index.js
@@ -15,56 +15,60 @@
 // You should have received a copy of the GNU Affero General Public License along
 // with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import _ from 'underscore'
+import {reduce} from 'lodash'
 
-// turns {'foo[bar]': 1} into {foo: {bar: 1}}
+/**
+ * Converts an object with nested key strings into an object with nested structure.
+ * Example: turns {'foo[bar]': 1} into {foo: {bar: 1}}
+ * @param {Object} obj - The object with nested key strings.
+ * @returns {Object} The unflattened object with nested structure.
+ */
+
 export default function unflatten(obj) {
-  return _(obj).reduce((newObj, val, key) => {
-    let keys = key.split('][')
-    let lastKey = keys.length - 1
+  return reduce(
+    obj,
+    (newObj, val, key) => {
+      let keys = key.split('][')
+      let lastKey = keys.length - 1
 
-    // If the first keys part contains [ and the last ends with ], then []
-    // are correctly balanced.
-    if (/\[/.test(keys[0]) && /\]$/.test(keys[lastKey])) {
-      // Remove the trailing ] from the last keys part.
-      keys[lastKey] = keys[lastKey].replace(/\]$/, '')
+      // If the first keys part contains [ and the last ends with ], then []
+      // are correctly balanced.
+      if (/\[/.test(keys[0]) && /\]$/.test(keys[lastKey])) {
+        // Remove the trailing ] from the last keys part.
+        keys[lastKey] = keys[lastKey].replace(/\]$/, '')
 
-      // Split first keys part into two parts on the [ and add them back onto
-      // the beginning of the keys array.
-      keys = keys
-        .shift()
-        .split('[')
-        .concat(keys)
-      lastKey = keys.length - 1
-    } else {
-      // Basic 'foo' style key.
-      lastKey = 0
-    }
-
-    if (lastKey) {
-      // Complex key, build deep object structure based on a few rules:
-      // * The 'cur' pointer starts at the object top-level.
-      // * [] = array push (n is set to array length), [n] = array if n is
-      //   numeric, otherwise object.
-      // * If at the last keys part, set the value.
-      // * For each keys part, if the current level is undefined create an
-      //   object or array based on the type of the next keys part.
-      // * Move the 'cur' pointer to the next level.
-      // * Rinse & repeat.
-      let i = 0
-      let cur = newObj
-      while (i <= lastKey) {
-        key = keys[i] === '' ? cur.length : keys[i]
-
-        cur = cur[key] =
-          i < lastKey ? cur[key] || (keys[i + 1] && isNaN(keys[i + 1]) ? {} : []) : val
-        i++
+        // Split first keys part into two parts on the [ and add them back onto
+        // the beginning of the keys array.
+        keys = keys.shift().split('[').concat(keys)
+        lastKey = keys.length - 1
+      } else {
+        // Basic 'foo' style key.
+        lastKey = 0
       }
-    } else {
-      // Simple key, even simpler rules, since only scalars and shallow
-      // arrays are allowed.
 
-      if (_.isArray(newObj[key])) {
+      if (lastKey) {
+        // Complex key, build deep object structure based on a few rules:
+        // * The 'cur' pointer starts at the object top-level.
+        // * [] = array push (n is set to array length), [n] = array if n is
+        //   numeric, otherwise object.
+        // * If at the last keys part, set the value.
+        // * For each keys part, if the current level is undefined create an
+        //   object or array based on the type of the next keys part.
+        // * Move the 'cur' pointer to the next level.
+        // * Rinse & repeat.
+        let i = 0
+        let cur = newObj
+        while (i <= lastKey) {
+          key = keys[i] === '' ? cur.length : keys[i]
+
+          cur = cur[key] =
+            // eslint-disable-next-line no-restricted-globals
+            i < lastKey ? cur[key] || (keys[i + 1] && isNaN(keys[i + 1]) ? {} : []) : val
+          i++
+        }
+        // Simple key, even simpler rules, since only scalars and shallow
+        // arrays are allowed.
+      } else if (Array.isArray(newObj[key])) {
         // val is already an array, so push on the next value.
         newObj[key].push(val)
       } else if (newObj[key] != null) {
@@ -75,8 +79,9 @@ export default function unflatten(obj) {
         // val is a scalar.
         newObj[key] = val
       }
-    }
 
-    return newObj
-  }, Object.create(null))
+      return newObj
+    },
+    Object.create(null)
+  )
 }
