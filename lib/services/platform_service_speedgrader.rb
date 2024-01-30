0ddd58dcda3e3117f71459031d90293c10e96679
Keith T. Garner <kgarner@instructure.com>
2024-01-30 10:21:33 -0600

    add dynamic config for speedgrader service
    flag=none
    
    Change-Id: I3bcaaf94cddcf5f087fc5cc38ce3b713b83074e9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/339233
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Keith Garner <kgarner@instructure.com>
    Product-Review: Keith Garner <kgarner@instructure.com>

---
+++ lib/services/platform_service_speedgrader.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Services
+  class PlatformServiceSpeedgrader
+    def self.launch_url
+      config["launch_url"]
+    end
+
+    class << self
+      private
+
+      def config
+        @config ||= DynamicSettings.find("platform_service_speedgrader", tree: :private) || {}
+      end
+    end
+  end
+end
