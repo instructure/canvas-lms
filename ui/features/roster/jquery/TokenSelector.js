2ab517f0bfaa61edb56ebce4d2804d619d15e86d
Angela Gomba <angela.gomba@instructure.com>
2024-01-30 19:29:37 -0500

    Use .active selector instead of :focus
    When upgrading to jQuery 1.8, we can no longer select for the
    focused item on the list using the :focus selector. Instead we
    can use the .active selector.
    
    refs FOO-4220
    flag=none
    
    Test Plan:
    - Jenkins Passes
    - I tested this in UI to make sure the list behaved the same
      with different types of inputs and results
    
    Change-Id: Iad62ff135d2b97d5417d7489ba6ec5ccc5847777
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/339137
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jen Smith <jen.smith@instructure.com>
    QA-Review: Jen Smith <jen.smith@instructure.com>
    Product-Review: Jen Smith <jen.smith@instructure.com>

--- ui/features/roster/jquery/TokenSelector.js
+++ ui/features/roster/jquery/TokenSelector.js
@@ -130,7 +130,7 @@ export default class TokenSelector {
     if (this.uiLocked) {
       return true
     }
-    if (this.$menu.find('.no-results').length > 0 && [13, 'Enter'].includes(keyCode)) {
+    if (this.isShowingNoResults() && [13, 'Enter'].includes(keyCode)) {
       return e.preventDefault()
     }
 
@@ -273,10 +273,17 @@ export default class TokenSelector {
   blur() {
     // It seems we can't check focus while it is being changed, so check it later.
     return setTimeout(() => {
-      if (!this.input.hasFocus() && !(this.$container.find(':focus').length > 0)) {
+      if (
+        !this.input.hasFocus() &&
+        (!(this.$container.find('.active').length > 0) || this.isShowingNoResults())
+      ) {
         return this.close()
       }
     }, 0)
+  }
+
+  isShowingNoResults() {
+    return this.$menu.find('.no-results').length > 0
   }
 
   listExpanded() {
