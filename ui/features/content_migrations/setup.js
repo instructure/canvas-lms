dc14177acb7146563e9186e82e01cd2c1cf972c6
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-02-06 12:48:35 -0700

    replaces SourceFileExtensionsPlugin
    instead of writing another webpack/rspack plugin,
    I remove the need for it
    
    Canvas is now aware of insertion points
    for string-key-based plugins
    
    test plan:
      - confirm that plugins are loading
    
    refs CFA-32
    
    Change-Id: I998aa4d2f63cfc00f610e48a03259f09a2358c5b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/339867
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    Build-Review: Isaac Moore <isaac.moore@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/content_migrations/setup.js
+++ ui/features/content_migrations/setup.js
@@ -47,6 +47,7 @@ import ExternalContentReturnView from '@canvas/external-tools/backbone/views/Ext
 import ExternalTool from '@canvas/external-tools/backbone/models/ExternalTool'
 import OverwriteAssessmentContentView from '@canvas/content-migrations/backbone/views/subviews/OverwriteAssessmentContentView'
 import ImportQuizzesNextView from '@canvas/content-migrations/backbone/views/ImportQuizzesNextView'
+import extensions from '@canvas/bundles/extensions'
 import processMigrationContentItem from './processMigrationContentItem'
 import {subscribe} from 'jquery-tinypubsub'
 
@@ -323,4 +324,13 @@ function __guard__(value, transform) {
   return typeof value !== 'undefined' && value !== null ? transform(value) : undefined
 }
 
-export default Array.from(ENV.EXTERNAL_TOOLS).map(et => registerExternalTool(et))
+Array.from(ENV.EXTERNAL_TOOLS).forEach(et => registerExternalTool(et))
+
+const loadExtension = extensions['ui/features/content_migrations/setup.js']?.()
+if (loadExtension) {
+  loadExtension
+    .then(extension => extension.default())
+    .catch(error => {
+      throw new Error(`Error loading extension for ui/features/content_migrations/setup.js`, error)
+    })
+}
