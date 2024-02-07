ccfb162bae8be90983b7c0ac65ac1d9fbab12ca5
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-02-07 13:33:01 -0700

    remove require statement; replace with import
    Change-Id: I465eddfaf9bbc8bd7d2421def195f074fb04ef59
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/339966
    Reviewed-by: Samuel Lee <samuel.lee@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/course_paces/react/shared/create_store.ts
+++ ui/features/course_paces/react/shared/create_store.ts
@@ -20,12 +20,13 @@ import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
 import {thunk} from 'redux-thunk'
 import defaultReducers from '../reducers/reducers'
+// @ts-expect-error
+import {createLogger} from 'redux-logger'
 
 export default (reducers = defaultReducers) => {
   const middlewares: any[] = [thunk]
 
   if (process.env.NODE_ENV === `development`) {
-    const {createLogger} = require(`redux-logger`) // tslint:disable-line
     const logger = createLogger({
       diff: true,
       duration: true,
