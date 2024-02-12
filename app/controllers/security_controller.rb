75b4b48016ef06b8f68de0d79d6f7abbf5f1e593
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-02-12 15:08:38 -0700

    Use Rails helper for LTI registration URL
    Previously, we were constructing this URL using some string
    interpolation because the route didn't exist yet. It does now, so we
    should stop string interpolating.
    
    flag=lti_dynamic_registration
    
    test-plan:
    - The specs cover this
    
    Change-Id: Ic9e215d5746dc990e6d1f5e69f77eadf1a10d2a9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/340310
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Product-Review: Ryan Hawkins <ryan.hawkins@instructure.com>

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -83,8 +83,7 @@ class SecurityController < ApplicationController
     render json: {
       issuer: Canvas::Security.config["lti_iss"],
       authorization_endpoint: lti_authorize_redirect_url(host: Lti::Oidc.auth_domain(account_domain)),
-      # TODO: use a path helper for this when this path gets created
-      registration_endpoint: "#{HostUrl.protocol}://#{account_domain}/api/lti/registrations",
+      registration_endpoint: create_lti_registration_url(host: account_domain),
       jwks_uri: lti_jwks_url(host: Lti::Oidc.auth_domain(account_domain)),
       token_endpoint: oauth2_token_url(host: Lti::Oidc.auth_domain(account_domain)),
       token_endpoint_auth_methods_supported: ["private_key_jwt"],
