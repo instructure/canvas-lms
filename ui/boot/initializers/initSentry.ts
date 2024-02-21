38b63a0b7adce0a68e4f6916aebbf551409e77eb
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-02-21 15:34:47 -0700

    Don't send ChunkLoadError errors to Sentry
    Change-Id: I6431405e60df019eaf6513a9c01788991be4d29d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341000
    Reviewed-by: Isaac Moore <isaac.moore@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/boot/initializers/initSentry.ts
+++ ui/boot/initializers/initSentry.ts
@@ -46,6 +46,7 @@ export function initSentry() {
       release: sentrySettings.revision,
 
       denyUrls,
+      ignoreErrors: ['ChunkLoadError'],
       integrations,
 
       sampleRate: errorsSampleRate,
