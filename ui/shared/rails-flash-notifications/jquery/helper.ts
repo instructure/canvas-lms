ffeddbb3ff6bf57aed6eef5edb84e9b6d40b8747
Michael Hulse <michael.hulse@instructure.com>
2024-02-21 14:58:09 -0800

    improve html for flash message and test coverage
    Fixed sinon clock interference with jQuery animation timing.
    Turned off jQuery fade to ensure precise timing for flash
    message disappearance.
    
    unblocking jquery upgrade path
    
    refs FOO-4258
    flag=none
    
    test plan: tests pass
    
    [skip-crystalball]
    
    Change-Id: I98aabaf60abe2f35b66ac797d9300c4055606f91
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341004
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jen Smith <jen.smith@instructure.com>

--- ui/shared/rails-flash-notifications/jquery/helper.ts
+++ ui/shared/rails-flash-notifications/jquery/helper.ts
@@ -93,17 +93,23 @@ class RailsFlashNotificationsHelper {
 
   generateNodeHTML(type, content) {
     const icon = this.getIconType(type)
+    const escapedType = htmlEscape(type)
+    const escapedIcon = htmlEscape(icon)
+    const escapedContent = this.escapeContent(content)
+    const closeButtonLabel = htmlEscape(I18n.t('Close'))
 
-    // See generateScreenreaderNodeHtml for SR features
-    return `<div class="ic-flash-${htmlEscape(
-      type
-    )} flash-message-container" aria-hidden="true"><div class="ic-flash__icon"><i class="icon-${htmlEscape(
-      icon
-    )}"></i></div>${this.escapeContent(
-      content
-    )}<button type="button" class="Button Button--icon-action close_link" aria-label="${htmlEscape(
-      I18n.t('Close')
-    )}"><i class="icon-x"></i></button></div>`
+    // see generateScreenreaderNodeHtml() for SR features
+    return `
+      <div class="ic-flash-${escapedType} flash-message-container" aria-hidden="true">
+        <div class="ic-flash__icon">
+          <i class="icon-${escapedIcon}"></i>
+        </div>
+        ${escapedContent}
+        <button type="button" class="Button Button--icon-action close_link" aria-label="${closeButtonLabel}">
+          <i class="icon-x"></i>
+        </button>
+      </div>
+    `.trim()
   }
 
   getIconType(type) {
