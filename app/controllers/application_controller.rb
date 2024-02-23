1ededa0dbc3a9a791ee9e5ba22c4add314891098
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-02-23 13:21:26 -0700

    Allow description for submission_type_selection
    why:
    - To allow tools at this placement to specify an additional bit of
      description other than just title.
    - Additionally, we've removed support for the
      submission_type_selection_launch_points configuration option.
      It was determined this is no longer required.
    
    flag=none
    
    fixes INTEROP-8478
    
    test-plan:
    - Note: This is very similar to the plan in d78cbba.
    - Install the LTI 1.3 test tool (or any other tool) in the
      submission_type_selection placement.
    - Modify the configuration for the submission_type_selection placement
      so that it now includes a description property. You can do this by
      editing the JSON on the dev keys page or through the Rails console
      with something like:
    
    ```ruby
    tc = DeveloperKey.find(123456).tool_configuration
    tc.settings["extensions"].first["settings"]["placements"].find {|p|
    p.placement == "submission_type_selection}["description"] = "foobarbaz"
    tc.save!
    ```
    - It should save without causing issues.
    - Go to the new Assignment page, open the web console, then type
      `ENV.SUBMISSION_TYPE_SELECTION_TOOLS` and look for the tool you
      modified. A description property should now be present with the
      description you just put in.
    
    Change-Id: I0a032eec4202c725dea6dcea7450101c6db124b6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341194
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    QA-Review: Evan Battaglia <ebattaglia@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -530,8 +530,8 @@ class ApplicationController < ActionController::Base
     end
     hash[:base_title] = tool.default_label(I18n.locale) if custom_settings.include?(:base_title)
     hash[:external_url] = tool.url if custom_settings.include?(:external_url)
-    if type == :submission_type_selection && tool.submission_type_selection[:submission_type_selection_launch_points].present?
-      hash[:submission_type_selection_launch_points] = tool.submission_type_selection[:submission_type_selection_launch_points]
+    if type == :submission_type_selection && tool.submission_type_selection[:description].present?
+      hash[:description] = tool.submission_type_selection[:description]
     end
 
     hash
