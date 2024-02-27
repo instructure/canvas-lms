34308055c3fc3d147a0b2b76b420e029c4d12651
Tucker McKnight <tmcknight@instructure.com>
2024-02-27 14:39:15 -0700

    Remove the lti_unique_tool_form_ids feature flag
    This flag has been on for all accounts for about 90 days now;
    we do not expect to have to revert it. (This is a hidden flag
    that customers have not been able to toggle themselves.)
    
    This can affect open source users, who may not have been aware
    of this flag before. LTI launch iframes will now have an
    id with a unique, three-digit number in it. (E.g. "#tool_content_456".)
    If someone was previously scraping the page and finding the iframe with
    the CSS selector #tool_content, they can now find that with [id^=tool_content].
    
    test plan:
    - a page with multiple tool placements on it,
      like the assignment edit page with a tool
      in the assignment's description, loads all
      tools that you expect it to.
    
    fixes INTEROP-8304
    
    flag = lti_unique_tool_form_ids
    
    Change-Id: I840e1096f59f17851e0b0e8591e69a7a4e0f62a2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341592
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    QA-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1070,16 +1070,9 @@ RSpec.describe ApplicationController do
             end
 
             context "ENV.LTI_TOOL_FORM_ID" do
-              it "with the lti_unique_tool_form_ids flag on, sets a random id" do
-                course.account.enable_feature!(:lti_unique_tool_form_ids)
+              it "sets a random id" do
                 expect(controller).to receive(:random_lti_tool_form_id).and_return("1")
                 expect(controller).to receive(:js_env).with(LTI_TOOL_FORM_ID: "1")
-                controller.send(:content_tag_redirect, course, content_tag, nil)
-              end
-
-              it "with the lti_unique_tool_form_ids flag off, does not set a random it" do
-                course.account.disable_feature!(:lti_unique_tool_form_ids)
-                expect(controller).not_to receive(:js_env).with(LTI_TOOL_FORM_ID: anything)
                 controller.send(:content_tag_redirect, course, content_tag, nil)
               end
             end
