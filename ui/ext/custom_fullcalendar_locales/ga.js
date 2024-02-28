37bcd569aaa486b5a02dbb5f98127c1f6be7307e
Jason Perry <jason.perry@instructure.com>
2024-02-28 14:07:05 -0500

    Add support for the Irish (Gaeilge) Language (ga)
    Closes FOO-4233
    flag=none
    
    Test plan:
    * Recompile all assets including I18n
    * Set your dev user language to Irish
    * Verify that most Canvas pages appear to be translated in Irish
    * Check the calendar for correct month/day names
    * Check the jQuery datepicker for correct month/day names
    * Check the calendar and datepicker for correct first day of the week
      (Monday or Dé Luain)
    * Also verify that the datepicker runs in 24-hour time
    
    Change-Id: I2acaae53c0ca241fd3ea374be906b57c3c47f488
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341680
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Jason Perry <jason.perry@instructure.com>
    Product-Review: Jason Perry <jason.perry@instructure.com>

---
+++ ui/ext/custom_fullcalendar_locales/ga.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $ from 'jquery'
+
+$.fullCalendar.lang('ga', {
+  buttonText: {
+    month: 'Mí',
+    week: 'Seachtain',
+    day: 'lá',
+    list: 'liosta',
+  },
+  allDayHtml: 'an lá<br/>ar fad',
+  eventLimitText: 'níos mó',
+})
