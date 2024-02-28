4eb1288a45a1170edaa7f59d9dbc379cb8e448fa
alvaro.talavera <alvaro.talavera@instructure.com>
2024-02-28 18:13:28 -0300

    Fix Module Header when there are no modules
    closes LF-1331
    
    flag=instui_header
    
    test plan:
    - go to the modules page
    - test with a course with no modules
    - test with a course with modules
    - button expand/collapse should not appear when no modules
    
    Change-Id: I43410c6978d6fbc4156b4154a0e092dff52678f9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/341790
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Alvaro Talavera <alvaro.talavera@instructure.com>

--- ui/shared/context-modules/jquery/index.jsx
+++ ui/shared/context-modules/jquery/index.jsx
@@ -63,6 +63,7 @@ import {
   scrollTo,
   setExpandAllButton,
   setExpandAllButtonHandler,
+  setExpandAllButtonVisible,
   updateProgressionState,
   openExternalTool,
 } from './utils'
@@ -74,6 +75,7 @@ import DifferentiatedModulesTray from '../differentiated-modules'
 import ItemAssignToTray from '../differentiated-modules/react/Item/ItemAssignToTray'
 import {parseModule, parseModuleList} from '../differentiated-modules/utils/moduleHelpers'
 import {addModuleElement} from '../utils/moduleHelpers'
+import ContextModulesHeader from '../react/ContextModulesHeader'
 
 if (!('INST' in window)) window.INST = {}
 
@@ -1330,6 +1332,7 @@ modules.initModuleManagement = function (duplicate) {
           $('.duplicate_module_link').die()
           $('.duplicate_item_link').die()
           if (!ENV.FEATURES.instui_header) {
+            // not using with instui header, clicks are handled differently
             $('.add_module_link').die()
           }
           $('.edit_module_link').die()
@@ -1386,7 +1389,7 @@ modules.initModuleManagement = function (duplicate) {
             $toFocus.focus()
             const $contextModules = $('#context_modules .context_module')
             if (!$contextModules.length) {
-              $('#expand_collapse_all').hide()
+              setExpandAllButtonVisible(false)
               updatePublishMenuDisabledState(true)
             }
           })
@@ -1717,10 +1720,10 @@ modules.initModuleManagement = function (duplicate) {
   }
 
   if (ENV.FEATURES.instui_header) {
-    setTimeout(() => { // export for react, use timer to avoid race conditions.
-      document.add_module_link_handler = add_module_link_handler
-    }, 300)
+    // export "new module" handler for react
+    document.add_module_link_handler = add_module_link_handler
   } else {
+    // adds the "new module" button click handler
     $(document).on('click', '.add_module_link', add_module_link_handler)
   }
 
@@ -2182,6 +2185,11 @@ $(document).ready(function () {
     modules.evaluateItemCyoe($item)
   })
 
+  if (ENV.FEATURES.instui_header) {
+    // render the new INSTUI header component
+    renderHeaderComponent()
+  }
+
   let $currentElem = null
   const hover = function ($elem) {
     if ($elem.hasClass('context_module')) {
@@ -2301,6 +2309,7 @@ $(document).ready(function () {
     // "n" opens up the Add Module form
     $document.keycodes('n', event => {
       if (ENV.FEATURES.instui_header) {
+        // handles the "new module" button action on keypress
         $('#context-modules-header-add-module-button:visible').click()
       } else {
         $('.add_module_link:visible:first').click()
@@ -2362,26 +2371,26 @@ $(document).ready(function () {
   const $contextModules = $('#context_modules .context_module')
   if (!$contextModules.length) {
     $('#no_context_modules_message').show()
-    $('#expand_collapse_all').hide()
+    setExpandAllButtonVisible(false)
     $('#context_modules_sortable_container').addClass('item-group-container--is-empty')
   }
   $contextModules.each(function () {
     updateProgressionState($(this))
   })
 
-  if (ENV.FEATURES.instui_header) {
-    setTimeout(() => {
-      setExpandAllButton()
-    }, 300)
-  }
-  else {
-    setExpandAllButton()
+  setExpandAllButton()
+
+  if (!ENV.FEATURES.instui_header) {
+    // set the click handler for the expand/collapse all button 
+    // if the instui header is not enabled
     setExpandAllButtonHandler()
   }
 
   if (!ENV.FEATURES.instui_header) {
+    // menu tools click handler for the old UI
     $('.menu_tray_tool_link').click(openExternalTool)
   }
+
   monitorLtiMessages()
 
   function renderCopyToTray(open, contentSelection, returnFocusTo) {
@@ -2412,6 +2421,13 @@ $(document).ready(function () {
       />,
       document.getElementById('direct-share-mount-point')
     )
+  }
+
+  function renderHeaderComponent() {
+    const root = $('#context-modules-header-root')
+    if (root) {
+      ReactDOM.render(<ContextModulesHeader {...root.data('props')} />, root[0])
+    }
   }
 
   $(document).on('click', '.module_copy_to', event => {
