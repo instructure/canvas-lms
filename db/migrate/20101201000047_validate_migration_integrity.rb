428811d92508137e4b7bf59a0d47a86638e70648
Cody Cutrer <cody@instructure.com>
2024-03-05 14:33:49 -0700

    squash old migrations 201905
    Change-Id: Ib7db6cdf828e498b32d321e3b1e75c70ec7b8acb
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/342152
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20101201000047_validate_migration_integrity.rb
+++ db/migrate/20101201000047_validate_migration_integrity.rb
@@ -30,7 +30,7 @@ class ValidateMigrationIntegrity < ActiveRecord::Migration[4.2]
 
   def self.up
     initial_migration_version = "20101210192618"
-    last_squashed_migration_version = "20190429145838"
+    last_squashed_migration_version = "20190530174534"
 
     versions = if $canvas_rails == "7.1"
                  ActiveRecord::SchemaMigration.new(ActiveRecord::Base.connection).versions
