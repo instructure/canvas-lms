8ce6604bce1d5c95071d99e55dd590ea2d9451b7
Cody Cutrer <cody@instructure.com>
2024-03-05 15:02:20 -0700

    squash old migrations 201908
    Change-Id: If898e88bafbc0cee3959fcb26190a41b24c0496b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/342162
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20190806214554_add_view_feature_flags_permission_overrides.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddViewFeatureFlagsPermissionOverrides < ActiveRecord::Migration[5.2]
-  tag :predeploy
-
-  def up
-    DataFixup::AddRoleOverridesForNewPermission.run(:manage_feature_flags, :view_feature_flags)
-  end
-end
