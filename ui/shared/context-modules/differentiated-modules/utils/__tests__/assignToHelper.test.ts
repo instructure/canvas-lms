6a6e03453326d8f4d8967f56cac7358549e447b4
Sarah Gerard <sarah.gerard@instructure.com>
2024-03-06 14:11:26 -0800

    fix clear date inputs for everyone cards
    closes LF-1333
    flag = differentiated_modules
    
    test plan:
    - have an assignment with an 'everyone' or 'everyone
      else' card
    - with the item assign to tray, add a due date and
      availability dates to the everyone card and save
    - go back into the assign to tray and clear any of the
      dates you just added
    - save and then open the tray again, expect the dates
      to have cleared
    
    Change-Id: If941b0501364149984dad8445d3673c7eb8ef213
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/342261
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Sarah Gerard <sarah.gerard@instructure.com>

---
+++ ui/shared/context-modules/differentiated-modules/utils/__tests__/assignToHelper.test.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+  type DateDetailsPayload,
+  type ItemAssignToCardSpec,
+  DateDetailsOverride,
+} from '../../react/Item/types'
+import {generateDateDetailsPayload} from '../assignToHelper'
+
+describe('generateDateDetailsPayload', () => {
+  it('returns the correct payload when everyone due/unlock/lock dates are removed', () => {
+    const cards: ItemAssignToCardSpec[] = [
+      {
+        key: 'everyone_card',
+        isValid: true,
+        hasAssignees: true,
+        selectedAssigneeIds: ['everyone'] as string[],
+      } as ItemAssignToCardSpec,
+    ]
+    const expectedPayload = <DateDetailsPayload>{
+      due_at: null,
+      unlock_at: null,
+      lock_at: null,
+      assignment_overrides: [] as DateDetailsOverride[],
+      only_visible_to_overrides: false,
+    }
+    expect(generateDateDetailsPayload(cards)).toEqual(expectedPayload)
+  })
+})
