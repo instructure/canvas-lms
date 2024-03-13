ba8468d09e2cd64fef1e9139115ea54b6cb7952e
Cody Cutrer <cody@instructure.com>
2024-03-13 09:46:23 -0600

    migrations: fix grading_periods.grading_period_group_id type
    Change-Id: I64c8c6b4a872cf198e679b28c0ffeac48acd8ba0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/342857
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Migration-Review: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

---
+++ db/migrate/20240313154359_fix_grading_period_group_id_type.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+class FixGradingPeriodGroupIdType < ActiveRecord::Migration[7.0]
+  tag :predeploy
+
+  def up
+    change_column :grading_periods, :grading_period_group_id, :bigint
+  end
+
+  def down
+    change_column :grading_periods, :grading_period_group_id, :integer
+  end
+end
