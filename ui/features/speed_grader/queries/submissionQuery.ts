dd2bea8806654aea0a9db1ca0a1d813eef3c9519
Drake Harper <drake.harper@instructure.com>
2024-03-18 14:25:32 -0600

    show createdAt in submission comment
    flag=platform_service_speedgrader
    refs EVAL-4004
    
    Test Plan:
    - load SG2
    - should see created at time in submission comments
    
    Change-Id: I33ec4210059375faa03e99c8c6f7e44a1876f34f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/343123
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Spencer Olson <solson@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

--- ui/features/speed_grader/queries/submissionQuery.ts
+++ ui/features/speed_grader/queries/submissionQuery.ts
@@ -51,6 +51,7 @@ const SUBMISSION_QUERY = gql`
               id
               comment
               attempt
+              createdAt
               author {
                 name
                 updatedAt
