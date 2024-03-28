28150b7e199632a3ffc3e53af74450b7236928f0
Aaron Ogata <aogata@instructure.com>
2024-03-28 07:25:49 -0700

    remove dead code breaking in ruby 3.2
    refs AE-855
    
    This code never worked correctly as the =~ operation on an object always returns nil, and it has been removed in Ruby 3.2.
    
    Based off of the regex, it appears that the original intention was to use `input[:recipients]` as one possible input from the specs is `course_12_students`.
    
    Remove the code for now as it has never worked, and allow the owning team to restore it later to reduce risk.
    
    Change-Id: I5813b9badf0aa1c0a8f0164a8d71d7465ae24870
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/343973
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- app/graphql/mutations/create_conversation.rb
+++ app/graphql/mutations/create_conversation.rb
@@ -49,18 +49,6 @@ class Mutations::CreateConversation < Mutations::BaseMutation
     context_id = context&.id
     shard = context ? context.shard : Shard.current
 
-    # TODO: Refactor this, it doesnt work anymore. recipient =~ /\A(course_\d+)(?:_([a-z]+))?$/  returns nil
-    # It was also built with recipient = User object instead of MessagbleUser
-    recipients.each do |recipient|
-      if recipient =~ /\A(course_\d+)(?:_([a-z]+))?$/ && [nil, "students", "observers"].include?(Regexp.last_match(2)) &&
-         !Context.find_by_asset_string(Regexp.last_match(1)).try(:grants_right?, @current_user, session, :send_messages_all)
-        return validation_error(
-          I18n.t("Recipients restricted by role"),
-          attribute: "recipients"
-        )
-      end
-    end
-
     if context.blank? && !@current_user.associated_root_accounts.first.try(:grants_right?, @current_user, session, :read_roster)
       return validation_error(
         I18n.t("Context cannot be blank"),
