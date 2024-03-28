c827a6d803cf882761cf62f28ad949fd34fb1968
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-03-28 16:05:08 -0600

    Support default: enabled in Dynamic Registration
    why:
    - The traditional Canvas format supports a property called "default".
      This has been around since practically the beginning of Canvas, which
      explains the poor name.
    - This option causes the tool to be hidden from the course_navigation
      placement. Teachers and admins can re-enable it if they like in the
      course settings page. Note that it only affects the course_navigation
      placement in both DR and manually created installations/configurations.
    - As some tools make great use of this option, we chose to make it
      available in Dynamic Registration.
    
    closes INTEROP-8543
    
    flag=lti_dynamic_registration
    
    test-plan:
    - Go to install yaltt using Dynamic Registration.
    - Install it at the course navigation placement, as well as a few
      others.
    - Check the registration that was just created in the database. Under
      the messages portion of the lti_tool_configuration, you should *not*
      see a "default: disabled" key for the "course_navigation" placement.
      We specifically omit it if it isn't sent.
    - Now switch yaltt to the `default_disabled` branch. This modifies it to
      send this new parameter, as well as some other schema files. Make sure
      you run a new build for the lti-model package
      and reinstall any new dependencies in the frontend
      and backend containers.
    - Go install yaltt again, using the same setup as last time.
    - Now, go check the registration in the database again. There *should*
      be an entry `default: "disabled"` associated with the
      course_navigation placement.
    - Rebuild your API docs using `rake doc:api`. You should see Dynamic
      Registration show up as an option under the external tools item in the
      sidebar. Click on it and make sure that this new extension property is
      documented and that it all makes sense.
    
    Change-Id: I4a67da77449946a0aee9efdf0eaf7af8092a1a45
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/344024
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Mark Starkman <mark.starkman@instructure.com>

--- app/controllers/lti/ims/dynamic_registration_controller.rb
+++ app/controllers/lti/ims/dynamic_registration_controller.rb
@@ -207,7 +207,7 @@ module Lti
           :token_endpoint_auth_method,
           { "https://purl.imsglobal.org/spec/lti-tool-configuration" => [
             :domain,
-            { messages: [:type, :target_link_uri, :label, :icon_uri, { custom_parameters: ArbitraryStrongishParams::ANYTHING }, { roles: [] }, { placements: [] }] },
+            { messages: [:type, :target_link_uri, :label, :icon_uri, { custom_parameters: ArbitraryStrongishParams::ANYTHING }, { roles: [] }, { placements: [] }, Lti::IMS::Registration::COURSE_NAV_DEFAULT_ENABLED_EXTENSION] },
             { claims: [] },
             :target_link_uri,
             { custom_parameters: ArbitraryStrongishParams::ANYTHING },
