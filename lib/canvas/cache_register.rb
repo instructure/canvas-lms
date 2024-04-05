e0f19eda4f45f9505e863da290265f65a4ddd756
Sarah Gerard <sarah.gerard@instructure.com>
2024-04-05 12:46:22 -0700

    Support discussions/wiki pages in AssignmentOverrideApplicator
    not many changes to the AOA were necessary to support new
    learning objects, most of the changes are name changes to
    make it clear that other objects are supported now and
    additional test coverage.
    
    closes LF-1048
    flag=differentiated_modules
    
    test plan:
    - tests pass
    - since there's no way to create overrides in the UI yet for
      these objects, to test manually you'll have to make overrides
      in a rails console.
    - ensure your override has a user attached to it, either ADHOC
      or section
    - run AssignmentOverrideApplicator.overrides_for_assignment_and_user(
      learning_object, user) for both a wiki page with an override
      and ungraded discussion with an override, and the user which the
      override applies to
    - ensure you see the correct override appear
    - add a graded discussion to a context module that has overrides
    - on the discussion's assignment, run
      discussion.assignment.all_assignment_overrides
    - expect to see the context module's override
    - in a course with mastery paths enabled, create a page and check
      'allow in mastery paths'
    - add that page to a module with overrides
    - on the page's assignment, run
      wiki_page.assignment.all_assignment_overrides
    - expect to see the context module's override appear
    
    Change-Id: I4a82d091d8c92b5e0ac6cae5596c6953b982f8d5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/344611
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    QA-Review: Jackson Howe <jackson.howe@instructure.com>
    Product-Review: Sarah Gerard <sarah.gerard@instructure.com>

--- lib/canvas/cache_register.rb
+++ lib/canvas/cache_register.rb
@@ -41,7 +41,9 @@ module Canvas
       "User" => %w[enrollments groups account_users todo_list submissions user_services k5_user potential_unread_submission_ids],
       "AbstractAssignment" => %w[availability conditional_release needs_grading],
       "Assignment" => %w[availability conditional_release needs_grading],
-      "Quizzes::Quiz" => %w[availability]
+      "Quizzes::Quiz" => %w[availability],
+      "DiscussionTopic" => %w[availability],
+      "WikiPage" => %w[availability]
     }.freeze
 
     PREFER_MULTI_CACHE_TYPES = {
