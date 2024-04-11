61ce64c964ea321a2a49c6d29605dda94bb3e693
Sleyder Zuleta <sleyder.zuleta@instructure.com>
2024-04-11 14:19:56 -0500

    remove used locations for default GS
    remove used locations for default GS because
    is not necessary anymore
    
    fixes EVAL-4085
    fixes EVAL-4031
    flag=none
    
    test plan:
    - validate the GS UI is loading default scheme
    
    Change-Id: I19d75541c4f1be401e0f3a7ee506ec7c02aa7c78
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/345075
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Reviewed-by: Cameron Ray <cameron.ray@instructure.com>
    Reviewed-by: Derek Williams <derek.williams@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Derek Williams <derek.williams@instructure.com>

--- spec/controllers/grading_schemes_json_controller_spec.rb
+++ spec/controllers/grading_schemes_json_controller_spec.rb
@@ -498,50 +498,6 @@ describe GradingSchemesJsonController, type: :request do
         expect(course_ids).to include(another_course.id)
       end
     end
-
-    describe "#default_used_locations" do
-      let_once(:data) { [["A", 94], ["F", 0]] }
-
-      before(:once) do
-        @root_account = Account.default
-        course_with_teacher(active_all: true, account: @root_account)
-        @admin = account_admin_user(account: @root_account)
-        @student = user_factory(active_user: true)
-        @sub_account = @root_account.sub_accounts.create!
-        course_with_teacher(account: @root_account)
-        @enrollment.update(workflow_state: "active")
-        @root_account.update(grading_standard_id: nil)
-        @sub_account.update(grading_standard_id: nil)
-        @course.update(grading_standard_id: nil)
-        3.times do
-          assignment = @course.assignments.create!(title: "hi", grading_standard_id: nil, grading_type: "letter_grade")
-          assignment.submissions.create!(user: @student, workflow_state: "graded")
-        end
-      end
-
-      it "returns courses and assignments where the grading standard is used" do
-        user_session(@admin)
-        get "/accounts/#{@account.id}/grading_schemes/default_used_locations", as: :json
-        locations = response.parsed_body
-
-        expect(locations.size).to eq(1)
-        expect(locations.first["id"]).to eq(@course.id)
-        expect(locations.first["assignments"].size).to eq(3)
-      end
-
-      it "does not return courses without graded assignments" do
-        another_course = course_factory
-        another_course.assignments.create!(title: "hi")
-        @course.assignments.create!(title: "hi")
-
-        user_session(@admin)
-        get "/accounts/#{@account.id}/grading_schemes/default_used_locations", as: :json
-        locations = response.parsed_body
-
-        course_ids = locations.pluck("id")
-        expect(course_ids).not_to include(another_course.id)
-      end
-    end
   end
 
   context "course teacher" do
