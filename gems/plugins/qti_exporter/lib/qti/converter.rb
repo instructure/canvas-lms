ac91d0d79567a51d243133fcafa5a86bbdeacd14
Adrian Diaz <adrian.diaz@instructure.com>
2024-04-16 11:47:58 -0600

    preserve qti new quizzes identifier
    closes QUIZ-13486
    flag = common_cartridge_qti_new_quizzes_import
    
    Test plan: specs should pass
    
    Change-Id: Ib370b8e0a32a1e503a10b0798e8861e4ee0c3e8e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/343664
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: James Logan <james.logan@instructure.com>
    QA-Review: James Logan <james.logan@instructure.com>
    Product-Review: Marissa Pio Roda <marissa.pioroda@instructure.com>

--- gems/plugins/qti_exporter/lib/qti/converter.rb
+++ gems/plugins/qti_exporter/lib/qti/converter.rb
@@ -128,7 +128,7 @@ module Qti
 
       begin
         manifest_file = File.join(@dest_dir_2_1, MANIFEST_FILE)
-        @quizzes[:assessments] = Qti.convert_assessments(manifest_file, @settings.merge({ converted_questions: questions }))
+        @quizzes[:assessments] = Qti.convert_assessments(manifest_file, @settings.merge({ converted_questions: questions, package_root: @package_root }))
       rescue => e
         message = "Error processing assessment QTI data: #{$!}: #{$!.backtrace.join("\n")}"
         add_error "qti_assessments", message, @questions, e
