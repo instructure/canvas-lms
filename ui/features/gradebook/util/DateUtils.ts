bc505ce6fb78a33074a937f13d1d0c719c2c2289
Spencer Olson <solson@instructure.com>
2024-04-18 10:36:50 -0500

    fix gradebook end date filtering
    closes EVAL-4047
    flag=none
    
    Test Plan:
    1. Create several assignments in a course, make sure they have due
       dates.
    2. Navigate to the Gradebook.
    3. Select the Start & End date gradebook filter.
    4. Observe that assignments that are due on the end date display in the
       search results.
    5. Create a saved filter with start and end dates. Apply the saved
       filter.
    6. Observe that assignments that are due on the end date display in the
       search results.
    
    Change-Id: I44666798430067771ad30cc715934f216148ea3a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/345595
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sleyder Zuleta <sleyder.zuleta@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Spencer Olson <solson@instructure.com>
    Product-Review: Ravi Koll <ravi.koll@instructure.com>

---
+++ ui/features/gradebook/util/DateUtils.ts
@@ -0,0 +1,27 @@
+//
+// Copyright (C) 2024 - present Instructure, Inc.
+//
+// This file is part of Canvas.
+//
+// Canvas is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3 of the License.
+//
+// Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import moment from 'moment'
+import type {MomentInput} from 'moment-timezone'
+
+export function isoDateFromInput(dateType: 'start-date' | 'end-date', input: MomentInput) {
+  if (!input) return undefined
+
+  const date = moment(input)
+  const shiftedDate = dateType === 'end-date' ? date.endOf('day') : date.startOf('day')
+  return shiftedDate.toISOString()
+}
