dcf1f20abe779974719792e3589e28fea31f9b59
Jeremy Stanley <jeremy@instructure.com>
2024-04-29 08:25:05 -0600

    smart search: fix unnecessary indexing issue
    test plan: a no-op index job should not be enqueued every time
    someone visits the search page, because it attempted to
    compare 1 to "1"
    
    flag=smart_search
    refs ADV-72
    
    Change-Id: Id382fce33c0863dc597186508806d3f6c6c2803d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/346361
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jonathan Featherstone <jfeatherstone@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -3693,7 +3693,7 @@ class Course < ActiveRecord::Base
 
   add_setting :default_due_time, inherited: true
   add_setting :conditional_release, default: false, boolean: true, inherited: true
-  add_setting :search_embedding_version
+  add_setting :search_embedding_version, arbitrary: true
 
   def elementary_enabled?
     account.enable_as_k5_account?
