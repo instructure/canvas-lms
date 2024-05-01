e3b56092319e5487c9893db83973c76818a746d5
Angel Diaz <angel.diaz@strongmind.com>
2024-05-01 16:28:55 -0700

    (S) QTY-6895: add routes back but keep scope for users

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,14 @@ CanvasRails::Application.routes.draw do
     resources(:settings, controller: :enrollment_settings)
   end
 
+  scope '/users/:user_id' do
+    resources(:assignments) do
+      resources(:settings, controller: :student_assignment_settings)
+    end
+
+    resources(:settings, controller: :user_settings)
+  end
+
   resources :submission_comments, only: [:update, :destroy]
 
   resources :epub_exports, only: [:index]
