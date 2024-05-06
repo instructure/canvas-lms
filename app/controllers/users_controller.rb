0354b085568d1227b1d452f081688fede5ec35d4
Angel Diaz <angel.diaz@strongmind.com>
2024-05-06 09:52:13 -0700

    (B) QTY-7414: Properly catch and send validation errors for user creation to avoid exceptions for API calls
    Co-authored-by: Alixandra Arehart <alixandra.arehart@strongmind.com>
    Co-authored-by: Boston Essary <boston.essary@strongmind.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2328,7 +2328,9 @@ class UsersController < ApplicationController
       @pseudonym = @context.pseudonyms.where(:sis_user_id => sis_user_id, :workflow_state => 'deleted').first
       if @pseudonym
         @pseudonym.workflow_state = 'active'
-        @pseudonym.save!
+        unless @pseudonym.save
+          return render(:json => {:errors => {:pseudonym => @pseudonym.errors}}, :status => 422)
+        end
         @user = @pseudonym.user
         @user.workflow_state = 'registered'
         @user.update_account_associations
