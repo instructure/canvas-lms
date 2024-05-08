1ccdfe9c0ae46575dd8e6314b198fe5de66a6d28
Charley Kline <ckline@instructure.com>
2024-05-08 11:25:41 -0500

    Fix TS errors in ui/features/speed_grader
    Closes CFA-60
    flag=none
    
    Another stop on the way to zero tsc errors in the codebase.
    
    Test plan:
    * tsc no longer generates errors in this bundle
    * build passes
    
    Change-Id: I8f28beaa436dfcf9946b4d246d361f19fc1429b4
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347057
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

--- packages/html-escape/index.js
+++ packages/html-escape/index.js
@@ -73,7 +73,7 @@ const ENTITIES = {
 }
 
 /**
- * @param {string} str - The string to be escaped.
+ * @param {string|undefined} str - The string to be escaped.
  * @returns {string} The escaped string with HTML special characters replaced by entities.
  *
  * @example
@@ -83,6 +83,7 @@ const ENTITIES = {
  * // safeString would be: "&lt;script&gt;alert('XSS')&lt;/script&gt;"
  */
 export function htmlEscape(str) {
+  if (typeof str === 'undefined') return ''
   return str.replace(/[&<>"'\/`=]/g, c => ENTITIES[c])
 }
 
@@ -92,7 +93,7 @@ export function htmlEscape(str) {
  * the input as is. This utility function is intended to help prevent XSS
  * attacks by sanitizing strings or to safely convert numbers to strings.
  *
- * @param {string|number} strOrNumber - The input to be escaped or converted to a string.
+ * @param {string|number|undefined} strOrNumber - The input to be escaped or converted to a string.
  * @returns {string|*} - If the input is a string or number, returns an escaped
  * string. Otherwise, returns the input unchanged.
  *
@@ -111,14 +112,19 @@ export function htmlEscape(str) {
  * This is a no-op!
  * Don’t call htmlEscape with an object.
  */
+
 export default function escape(strOrNumber) {
-  if (typeof strOrNumber === 'string') {
-    return htmlEscape(strOrNumber)
-  } else if (typeof strOrNumber === 'number') {
-    return escape(strOrNumber.toString())
+  switch (typeof strOrNumber) {
+    case 'string':
+    case 'undefined':
+      return htmlEscape(strOrNumber)
+    case 'number':
+      return escape(strOrNumber.toString())
+    default:
+      return strOrNumber
   }
-  return strOrNumber
 }
+
 escape.SafeString = SafeString
 
 INST.htmlEscape = escape
