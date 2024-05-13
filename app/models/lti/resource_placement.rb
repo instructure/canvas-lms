bf5fd90a8f5135ff08ab995409e3dc7b7fb8a599
Dustin Cowles <dustin.cowles@instructure.com>
2024-05-13 16:28:07 -0700

    create top_navigation LTI placement
    closes ADV-81
    flag=top_navigation_placement
    
    Test plan:
    - NOTE: You must restart the web container after running Setting.set
    
    - Edit an existing LTI tool and add a `top_navigation` placement
      ```
          {
            "text": "Test Tool",
            "icon_url": "https://foo.bar/icon.png",
            "placement": "top_navigation",
            "message_type": "LtiResourceLinkRequest",
            "target_link_uri": "https://foo.bar/launch"
          }
      ```
    - When saving the tool config, ensure a flash message is displayed
      ```
        Warning: the top_navigation placement is only allowed for...
      ```
    
    - Add the tool to the allow list by developer key in a rails console
      ```
        Setting.set("top_navigation_allowed_dev_keys", "global-developer-key")
      ```
    - Save the tool config again and ensure no flash message is displayed
    
    - Remove the tool from the dev key allow list and add the domain
      ```
        Setting.set("top_navigation_allowed_dev_keys", "")
        Setting.set("top_navigation_allowed_launch_domains", "domain")
      ```
    - Save the config again and ensure no flash message is displayed
    
    - Repeat for the submission_type_selection placement as shared code has
      changed in this commit.
    
    - UI elements to further test the new placement will come in a later commit
    
    Change-Id: If56bd194aaea789c263e76c88a2c31edd312e5e3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347351
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Dustin Cowles <dustin.cowles@instructure.com>

--- app/models/lti/resource_placement.rb
+++ app/models/lti/resource_placement.rb
@@ -40,7 +40,13 @@ module Lti
     LEGACY_DEFAULT_PLACEMENTS = [ASSIGNMENT_SELECTION, LINK_SELECTION].freeze
 
     # Placements restricted so not advertised in the UI
-    NON_PUBLIC_PLACEMENTS = [:submission_type_selection].freeze
+    NON_PUBLIC_PLACEMENTS = %i[submission_type_selection top_navigation].freeze
+
+    # These placements require tools to be on an allow list
+    RESTRICTED_PLACEMENTS = %i[submission_type_selection top_navigation].freeze
+
+    # These placements can be marked as pinned, which will influence their visibility in the UI
+    PINNABLE_PLACEMENTS = %i[top_navigation].freeze
 
     PLACEMENTS_BY_MESSAGE_TYPE = {
       LtiAdvantage::Messages::ResourceLinkRequest::MESSAGE_TYPE => %i[
@@ -62,6 +68,7 @@ module Lti
         file_index_menu
         file_menu
         global_navigation
+        top_navigation
         homework_submission
         link_selection
         migration_selection
