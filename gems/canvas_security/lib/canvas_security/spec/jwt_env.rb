928729d7f54b219e82db6779062278159ae2266f
Cody Cutrer <cody@instructure.com>
2024-05-15 11:36:31 -0600

    allow generating non-encrypted service JWTs
    refs AE-821
    
    Change-Id: I4fa27a176956feb91e286d149e79954a3dd94a46
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347461
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/canvas_security/lib/canvas_security/spec/jwt_env.rb
+++ gems/canvas_security/lib/canvas_security/spec/jwt_env.rb
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+require "timecop"
+
 RSpec.shared_context "services JWT wrapper" do
   def build_wrapped_token(user_id, real_user_id: nil, encoding_secret: fake_signing_secret)
     payload = { sub: user_id }
