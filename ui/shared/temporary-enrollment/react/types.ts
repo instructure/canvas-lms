c17e77ef396edf655e838b162c37e71a2ce041b3
Michael Hulse <michael.hulse@instructure.com>
2024-05-15 02:47:10 -0700

    bump max per-page for temp enroll assign enrollments fetch
    This commit updates the current maximum limit for the temporary
    enrollments assign component enrollments API call from 50 to
    100; this should temporarily help accounts that have 1 to 100
    active enrollments.
    
    closes FOO-4467
    flag=temporary_enrollments
    
    test plan:
    - enable temporary enrollments feature flag
    - find a recipient of temporary enrollment
    - navigate to the assign screen
    - verify that the max per-page is 100 for courses fetched
    - this doesn’t add pagination, just a max per-page, which should
      satisfy the use case for now
    
    [skip-crystalball]
    
    Change-Id: Ib581caaff5e72bf5a128805357edfa28aee2454a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347396
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>

--- ui/shared/temporary-enrollment/react/types.ts
+++ ui/shared/temporary-enrollment/react/types.ts
@@ -19,7 +19,7 @@
 export const MODULE_NAME = 'TempEnroll'
 
 export const ITEMS_PER_PAGE = 100
-export const MAX_ALLOWED_COURSES_PER_PAGE = 50
+export const MAX_ALLOWED_COURSES_PER_PAGE = 100
 
 export const TOOLTIP_MAX_WIDTH: string = '15rem'
 export const ENROLLMENT_TREE_SPACING: string = '1.75rem'
