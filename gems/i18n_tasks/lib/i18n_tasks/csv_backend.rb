30c82ce26361ad070f2d682052ba912b50284fed
Cody Cutrer <cody@instructure.com>
2024-05-16 15:50:06 -0600

    fix build issue
    Change-Id: I282c1ff73508b495f15183202e33e08dc5ffa8b5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347590
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/i18n_tasks/lib/i18n_tasks/csv_backend.rb
+++ gems/i18n_tasks/lib/i18n_tasks/csv_backend.rb
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+require "csv"
+
 module I18nTasks
   module CsvBackend
     def load_csv(filename)
