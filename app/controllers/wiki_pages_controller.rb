09c7099e7543962626d479416cf6b6ae958d1dad
Dustin Cowles <dustin.cowles@instructure.com>
2024-05-16 17:06:57 -0700

    remove per-page drawer layout implementation
    This removes the experimental per-page implementations of the
    ContentTypeExternalToolDrawer component in favor of the global
    implementation introduced as part of the top_navigation placement.
    
    refs: ADV-104
    flag=none
    
    Test Plan:
    - Assignments page loads without errors
    - Wiki Page loads without errors
    - The ContentTypeExternalToolDrawer component is not
      rendered on either page.
    
    Change-Id: I19a69e4a4c0b926f6f1b88c7560fda6708dae85c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347598
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Dustin Cowles <dustin.cowles@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -171,7 +171,7 @@ class WikiPagesController < ApplicationController
   def wiki_pages_js_env(context)
     set_k5_mode # we need this to run now, even though we haven't hit the render hook yet
     @wiki_pages_env ||= {
-      wiki_page_menu_tools: filtered_wiki_page_menu_tools,
+      wiki_page_menu_tools: external_tools_display_hashes(:wiki_page_menu),
       wiki_index_menu_tools: external_tools_display_hashes(:wiki_index_menu),
       DISPLAY_SHOW_ALL_LINK: tab_enabled?(context.class::TAB_PAGES, no_render: true) && !@k5_details_view,
       CAN_SET_TODO_DATE: context.grants_any_right?(@current_user, session, :manage_content, :manage_course_content_edit),
@@ -183,22 +183,5 @@ class WikiPagesController < ApplicationController
     end
     js_env(@wiki_pages_env)
     @wiki_pages_env
-  end
-
-  def filtered_wiki_page_menu_tools
-    tools = external_tools_display_hashes(:wiki_page_menu)
-    return tools unless tools.present? && @context.is_a?(Course)
-
-    # we do not support tray launch method on this page without the drawer
-    unless @domain_root_account&.feature_enabled?(:external_tool_drawer)
-      tools.reject! { |tool| tool[:launch_method] == "tray" }
-    end
-
-    # students should only see menu tools that launch in the tray
-    if context.user_is_student?(@current_user, include_fake_student: true, include_all: true)
-      tools.select! { |tool| tool[:launch_method] == "tray" }
-    end
-
-    tools.presence || []
   end
 end
