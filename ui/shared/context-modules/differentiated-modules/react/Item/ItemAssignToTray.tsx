33eaecb4b47c4dc45219dc5ac02b3a17830af2a5
Jackson Howe <jackson.howe@instructure.com>
2024-05-21 09:05:41 -0600

    Change selective release tray headings to h2
    to be more consistent with rest of canvas.
    
    closes LF-1679
    flag = differentiated_modules
    
    Test plan:
     - Open the module and item trays
     - Expect the header (at the very top of the tray) to be an h2
    
    Change-Id: I5d63cd800034f1aab17635df33e8b788acd537e3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347918
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

--- ui/shared/context-modules/differentiated-modules/react/Item/ItemAssignToTray.tsx
+++ ui/shared/context-modules/differentiated-modules/react/Item/ItemAssignToTray.tsx
@@ -263,7 +263,7 @@ export default function ItemAssignToTray({
           placement="end"
           offset="small"
         />
-        <Heading as="h3">
+        <Heading level="h2">
           {icon} {itemName}
         </Heading>
         <View data-testid="item-type-text" as="div" margin="medium 0 0 0">
