e219b30a3774b30fbe25a1eae38bf3d882e25221
Martin Ochoa <martin.ochoa@instructure.com>
2024-05-21 10:44:11 -0500

    implement TopNav in statistics view
    This is an implementation for TopNav into Statistics view.
    
    Note: Single breadcrumb error may appear but should be fixed
    in ticket FOO-4473.
    
    closes FOO-4078
    
    Test plan:
    - Enable the instui-nav flag
    - Go to Admin / Statistics / url: '/accounts/:id/statistics'
    - New TopNav should appear
    
    Change-Id: I7af28c23bf239762693c9fce6e3d535aa5fbf12d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/347924
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Martin Ochoa <martin.ochoa@instructure.com>

--- ui/features/account_statistics/jquery/index.js
+++ ui/features/account_statistics/jquery/index.js
@@ -21,6 +21,7 @@ import $ from 'jquery'
 import '@canvas/jquery/jquery.ajaxJSON'
 import 'jqueryui/dialog'
 import replaceTags from '@canvas/util/replaceTags'
+import {initializeTopNavPortal} from '@canvas/top-navigation/react/TopNavPortal'
 
 const I18n = useI18nScope('accounts.statistics')
 
@@ -61,6 +62,7 @@ function populateDialog(data_points, axis, $link) {
 }
 
 $(document).ready(() => {
+  initializeTopNavPortal(document.getElementById('react-instui-topnav'))
   $(document).on('click', '.over_time_link', function (event) {
     event.preventDefault()
     const $link = $(this)
