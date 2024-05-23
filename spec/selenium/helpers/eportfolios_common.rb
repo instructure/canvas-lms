73c13a2eb8d2b2afd82cf7c7dcc34a99552fba52
davidwenzlick <david.wenzlick@instructure.com>
2024-05-23 16:02:21 -0400

    Add sticky footer to account and course.
    closes OUT-6263 OUT-6336
    flag=none
    
    [skip-crystalball]
    
    Test Plan
    - Navigate to account settings  and verify a sticky footer
    - Navigate to course settings and verify a sticky footer
    
    Change-Id: I9398d32022bbf0351d94ed208f91887a2513512b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/348210
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Angela Gomba <angela.gomba@instructure.com>
    QA-Review: Angela Gomba <angela.gomba@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- spec/selenium/helpers/eportfolios_common.rb
+++ spec/selenium/helpers/eportfolios_common.rb
@@ -22,13 +22,19 @@ require_relative "../common"
 module EportfoliosCommon
   def create_eportfolio
     get "/dashboard/eportfolios"
-    f(".add_eportfolio_link").click
+    target = f(".add_eportfolio_link")
+    scroll_into_view(target)
+    target.click
     wait_for_animations
     replace_content f("#eportfolio_name"), "student content"
   end
 
   def validate_eportfolio(is_public = false)
-    f("#eportfolio_public").click if is_public
+    if is_public
+      target = f("#eportfolio_public")
+      scroll_into_view(target)
+      target.click
+    end
     expect_new_page_load { f("#eportfolio_submit").click }
     eportfolio = Eportfolio.find_by_name("student content")
     expect(eportfolio).to be_valid
