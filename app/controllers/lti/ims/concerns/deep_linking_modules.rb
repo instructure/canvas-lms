e3f84baf7b0a4b11110889b12fe3cee7869735cb
Steve McGee <steve.mcgee@instructure.com>
2024-05-24 17:04:26 -0600

    no new Lti::ResourceLink on assignment edit
    why: when an assignment already has a primary_resource_link a new
         Lti::ResourceLink should not be created when launching the lti
         tool again from the submission_type_selection placement
    
    closes INTEROP-8599
    flag=none
    
    test plan:
    - from a rails console check the Lti::ResourceLink.count and the
      assignment.primary_resource_link
    - in the browser from an assignment that has an lti 1.3 tool from
      submission_type_selection already click the closing 'x' on the
      resource card, re-launch the tool, and walk through the process
      again (update the title or something), and save the assignment.
    - in the rails console check the Lti::Resource.count again and there
      should not be an increase from before, and check that the
      assignment.primary_resource_link has updated as expected.
    
    Change-Id: Id2af3d29eb59a17b4ab73beea2e7d7e11c92135e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/348372
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Steve Mcgee <steve.mcgee@instructure.com>

--- app/controllers/lti/ims/concerns/deep_linking_modules.rb
+++ app/controllers/lti/ims/concerns/deep_linking_modules.rb
@@ -27,7 +27,7 @@ module Lti::IMS::Concerns
       # a resource link for the newly-created assignment later, when we actually
       # create the assignment. The assignment_selection placement, therefore, should
       # not be creating resource links.
-      return true if for_placement?(:assignment_selection)
+      return true if for_placement?(:assignment_selection) || for_placement?(:submission_type_selection)
 
       add_item_to_existing_module = return_url_parameters[:context_module_id].present?
       create_new_module? || add_item_to_existing_module || add_assignment?
