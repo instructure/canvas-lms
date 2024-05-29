18169f4c1993564ab9a32ef4527ce477d55fc94f
Chris Soto <christopher.soto@instructure.com>
2024-05-29 11:00:09 -0600

    add permissions check to new rubric pages
    this commit adds a permissions check to the new rubric pages. if the
    user does not have the correct permissions, they will be redirected to
    /rubrics page if they are on the rubric form page, or if they are
    already on the /rubrics page they will have limited functionality.
    
    closes EVAL-4204
    flag=enhanced_rubrics
    
    test plan:
    - create a user in a TA role
    - set the permissions for TA role to not have the edit/add/delete rubric
      permissions
    - log in as the TA user
    - navigate to /rubrics
    - verify that the options popover button is not visible as well as the
      Create New Rubric button
    - try to navigate directly to /rubrics/new
    - verify that you are redirected back to /rubrics (the url still might
      show /rubrics/new but the page content should be the /rubrics page)
    
    Change-Id: I0109306f4489602f38ab93b27d2e58144bfd00cc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/348587
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Derek Williams <derek.williams@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Melissa Kruger <melissa.kruger@instructure.com>

--- ui/shared/global/env/EnvAccounts.d.ts
+++ ui/shared/global/env/EnvAccounts.d.ts
@@ -33,6 +33,7 @@ export interface EnvAccountsAdminTools {
     can_read_roster: boolean
     can_view_temporary_enrollments: boolean
     manage_grading_schemes: boolean
+    manage_rubrics: boolean
     logging:
       | false
       | {
