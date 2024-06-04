cf144b041db73558b6eea46e0b677561898d11de
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-06-04 12:33:15 -0600

    LIME: Create Permission Confirmation Screen
    why:
    - To let users toggle what permissions a tool is allowed to have
    
    closes INTEROP-8590
    
    flag=lti_registrations_page
    
    test-plan:
    - Enable the registrations page flag
    - Add a new tool and use Yaltt for the dynamic registration
    - Select a couple different permissions to give yaltt in it's install
      dialog
    - Click install
    - Verify that the requested permissions match what you selected and that
      you can enable/disable/reenable said permissions.
    
    Change-Id: I682ee7a56f616d5f7f32aa278c8264cb5e299e5c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/349436
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Paul Gray <paul.gray@instructure.com>
    QA-Review: Paul Gray <paul.gray@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>

---
+++ ui/features/lti_registrations/manage/dynamic_registration_wizard/components/PermissionConfirmation.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {useScope as useI18nScope} from '@canvas/i18n'
+import {i18nLtiScope} from '../../model/LtiScope'
+import {Heading} from '@instructure/ui-heading'
+import {Text} from '@instructure/ui-text'
+import {Flex} from '@instructure/ui-flex'
+import {Checkbox} from '@instructure/ui-checkbox'
+import type {RegistrationOverlayStore} from '../../registration_wizard/registration_settings/RegistrationOverlayState'
+import type {LtiImsRegistration} from '../../model/lti_ims_registration/LtiImsRegistration'
+import htmlEscape from '@instructure/html-escape'
+
+const I18n = useI18nScope('lti_registration.wizard')
+
+export type PermissionConfirmationProps = {
+  registration: LtiImsRegistration
+  overlayStore: RegistrationOverlayStore
+}
+
+export const PermissionConfirmation = ({
+  registration,
+  overlayStore,
+}: PermissionConfirmationProps) => {
+  const [{state, ...actions}, setState] = React.useState(overlayStore.getState())
+  React.useEffect(
+    () =>
+      overlayStore.subscribe(s => {
+        setState(s)
+      }),
+    [overlayStore]
+  )
+
+  return (
+    <Flex direction="column" alignItems="start" gap="medium">
+      <Heading>{I18n.t('Permissions')}</Heading>
+      <Text
+        dangerouslySetInnerHTML={{
+          __html: I18n.t(
+            "*%{toolName}* is requesting permission to perform the following actions. We have chosen the app's recommended default settings. Please note that altering these defaults might impact the app's performance.",
+            {toolName: htmlEscape(registration.client_name), wrapper: '<strong>$1</strong>'}
+          ),
+        }}
+      />
+      <Flex direction="column" alignItems="center" gap="small">
+        {registration.scopes.map(scope => {
+          return (
+            <Checkbox
+              data-testid={scope}
+              key={scope}
+              variant="toggle"
+              label={i18nLtiScope(scope)}
+              checked={!state.registration.disabledScopes?.includes(scope)}
+              onChange={() => {
+                actions.toggleDisabledScope(scope)
+              }}
+            />
+          )
+        })}
+      </Flex>
+    </Flex>
+  )
+}
