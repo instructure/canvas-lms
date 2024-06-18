08b8933d78a988981d25f94da631b99695ac9b7c
juan.chavez <juan.chavez@instructure.com>
2024-06-18 18:45:34 -0400

    spec: Write selenium tests for Sync to SIS
    closes LX-1737
    
    flag=selective_release_ui_api
    flag=selective_release_backend
    
    test plan:
    > Passes Jenkins.
    
    Change-Id: I27c809ba4c09f5ddc8d49f5cc08a0e5b76f34766
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/350522
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Juan Chavez <juan.chavez@instructure.com>

--- spec/selenium/assignments/assignments_sync_to_sis_spec.rb
+++ spec/selenium/assignments/assignments_sync_to_sis_spec.rb
@@ -23,6 +23,8 @@ require_relative "../helpers/public_courses_context"
 require_relative "../helpers/files_common"
 require_relative "../helpers/admin_settings_common"
 require_relative "../../helpers/selective_release_common"
+require_relative "../helpers/items_assign_to_tray"
+require_relative "../assignments/page_objects/assignment_create_edit_page"
 
 describe "assignments sync to sis" do
   include_context "in-process server selenium tests"
@@ -32,6 +34,7 @@ describe "assignments sync to sis" do
   include CustomScreenActions
   include CustomSeleniumActions
   include SelectiveReleaseCommon
+  include ItemsAssignToTray
 
   # NOTE: due date testing can be found in assignments_overrides_spec
 
@@ -57,6 +60,7 @@ describe "assignments sync to sis" do
     let(:valid_name) { "Name" }
     let(:points) { "10" }
     let(:differentiate) { false }
+    let(:due_date) { 3.years.from_now }
     let(:due_date_valid) { "#{format_date_for_view(3.years.from_now)} at 11:59pm" }
     let(:short_date) { format_date_for_view(3.years.from_now) }
     let(:error) { "" }
@@ -151,45 +155,136 @@ describe "assignments sync to sis" do
       let(:error) { "Please add a due date" }
       let(:settings_enable) { { sis_require_assignment_due_date: { value: true } } }
 
-      it "validates due date while sis is on" do
-        submit_blocked_with_errors
-        set_value(due_date_input_fields.first, due_date_valid)
-        submit_assignment_form
-        check_due_date_table("Everyone", short_date)
-      end
-
-      it "does not validate when sis is off" do
-        f("#assignment_post_to_sis").click
-        submit_assignment_form
-        check_due_date_table("Everyone")
-      end
-
-      describe "differentiated assignment" do
-        let(:differentiate) { true }
-        let(:section_to_set) { "Section B" }
-
-        before do
-          assign_section_due_date
+      context "with selective release flags on" do
+        before(:once) do
+          differentiated_modules_on
         end
 
-        def assign_section_due_date
-          f("#add_due_date").click
-          due_date_fields = ff(".Container__DueDateRow-item")
-          assign_to_section(due_date_fields.last, section_to_set)
+        it "validates due date while sis is on", :ignore_js_errors do
+          # LX-1857: Does not show any error, but not submits, we should verify an error here
+          AssignmentCreateEditPage.save_assignment
+
+          AssignmentCreateEditPage.click_manage_assign_to_button
+
+          wait_for_assign_to_tray_spinner
+          keep_trying_until { expect(item_tray_exists?).to be_truthy }
+
+          expect(assign_to_date_and_time[0].text).to include("Please add a due date")
+
+          update_due_date(0, format_date_for_view(due_date, "%-m/%-d/%Y"))
+          update_due_time(0, "11:59 PM")
+          click_save_button("Apply")
+          keep_trying_until { expect(element_exists?(module_item_edit_tray_selector)).to be_falsey }
+
+          AssignmentCreateEditPage.save_assignment
+
+          check_due_date_table("Everyone", short_date)
         end
 
-        it "checks each due date when on" do
+        it "does not validate when sis is off" do
+          AssignmentCreateEditPage.select_post_to_sis_checkbox
+          AssignmentCreateEditPage.save_assignment
+
+          check_due_date_table("Everyone")
+        end
+
+        describe "differentiated assignment" do
+          let(:differentiate) { true }
+          let(:section_to_set) { "Section B" }
+
+          it "checks each due date when on", :ignore_js_errors do
+            # LX-1857: Does not show any error, but not submits, we should verify an error here
+            AssignmentCreateEditPage.save_assignment
+
+            AssignmentCreateEditPage.click_manage_assign_to_button
+
+            wait_for_assign_to_tray_spinner
+            keep_trying_until { expect(item_tray_exists?).to be_truthy }
+
+            expect(assign_to_date_and_time[0].text).to include("Please add a due date")
+
+            update_due_date(0, format_date_for_view(due_date, "%-m/%-d/%Y"))
+            update_due_time(0, "11:59 PM")
+            click_add_assign_to_card
+
+            expect(assign_to_date_and_time[3].text).to include("Please add a due date")
+
+            select_module_item_assignee(1, section_to_set)
+            update_due_date(1, format_date_for_view(due_date, "%-m/%-d/%Y"))
+            update_due_time(1, "11:59 PM")
+            click_save_button("Apply")
+            keep_trying_until { expect(element_exists?(module_item_edit_tray_selector)).to be_falsey }
+
+            AssignmentCreateEditPage.save_assignment
+
+            check_due_date_table(section_to_set, short_date)
+          end
+
+          it "does not check when sis is off", :ignore_js_errors do
+            skip("LX-1856: Tray is not using the checkbox value")
+            AssignmentCreateEditPage.select_post_to_sis_checkbox
+            AssignmentCreateEditPage.click_manage_assign_to_button
+
+            wait_for_assign_to_tray_spinner
+            keep_trying_until { expect(item_tray_exists?).to be_truthy }
+            click_add_assign_to_card
+            select_module_item_assignee(1, section_to_set)
+            click_save_button("Apply")
+            keep_trying_until { expect(element_exists?(module_item_edit_tray_selector)).to be_falsey }
+
+            AssignmentCreateEditPage.save_assignment
+
+            check_due_date_table(section_to_set)
+            check_due_date_table("Everyone else")
+          end
+        end
+      end
+
+      context "with selective release flags off" do
+        before(:once) do
+          differentiated_modules_off
+        end
+
+        it "validates due date while sis is on" do
           submit_blocked_with_errors
-          due_date_input_fields.each { |h| set_value(h, due_date_valid) }
-          f("#edit_assignment_form .btn-primary[type=submit]").click
-          check_due_date_table(section_to_set, short_date)
+          set_value(due_date_input_fields.first, due_date_valid)
+          submit_assignment_form
+          check_due_date_table("Everyone", short_date)
         end
 
-        it "does not check when sis is off" do
+        it "does not validate when sis is off" do
           f("#assignment_post_to_sis").click
           submit_assignment_form
-          check_due_date_table(section_to_set)
-          check_due_date_table("Everyone else")
+          check_due_date_table("Everyone")
+        end
+
+        describe "differentiated assignment" do
+          let(:differentiate) { true }
+          let(:section_to_set) { "Section B" }
+
+          before do
+            assign_section_due_date
+          end
+
+          def assign_section_due_date
+            f("#add_due_date").click
+            due_date_fields = ff(".Container__DueDateRow-item")
+            assign_to_section(due_date_fields.last, section_to_set)
+          end
+
+          it "checks each due date when on" do
+            submit_blocked_with_errors
+            due_date_input_fields.each { |h| set_value(h, due_date_valid) }
+            f("#edit_assignment_form .btn-primary[type=submit]").click
+            check_due_date_table(section_to_set, short_date)
+          end
+
+          it "does not check when sis is off" do
+            f("#assignment_post_to_sis").click
+            submit_assignment_form
+            check_due_date_table(section_to_set)
+            check_due_date_table("Everyone else")
+          end
         end
       end
     end
