fb4eb58fb4196bfc19998f7332215a301a07ae62
Derek Williams <derek.williams@instructure.com>
2024-06-20 14:30:38 -0400

    remove sleeps from VideoCaptionService
    sleeps were causing workers to sit around and do nothing
    for long periods of time.  This was causing the workers to
    not be able to process other jobs in the queue.
    
    flag=speedgrader_studio_media_capture
    
    test plan:
    - specs pass
    
    Change-Id: Ia7e99592d666c2f770514624cc8ab4eaef215756
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/350664
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Cameron Ray <cameron.ray@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -23,13 +23,14 @@ class MediaObject < ActiveRecord::Base
   include SearchTermHelper
 
   AUTO_CAPTION_STATUSES = {
-    processing: "Processing",
-    failed_initial_validation: "Error - Something went wrong",
-    failed_handoff: "Error - Failed to communicate with captioning service",
-    failed_request: "Error - Failed to request",
-    failed_captions: "Error - Caption request failed",
-    failed_to_pull: "Error - Captions not found",
-    complete: "Complete",
+    processing: -> { I18n.t("Processing") },
+    failed_initial_validation: -> { I18n.t("Error - Something went wrong") },
+    failed_handoff: -> { I18n.t("Error - Failed to communicate with captioning service") },
+    failed_request: -> { I18n.t("Error - Failed to request") },
+    non_english_captions: -> { I18n.t("Error - Non-English detected") },
+    failed_captions: -> { I18n.t("Error - Caption request failed") },
+    failed_to_pull: -> { I18n.t("Error - Captions not found") },
+    complete: -> { I18n.t("Complete") },
   }.freeze
 
   belongs_to :user
@@ -47,7 +48,7 @@ class MediaObject < ActiveRecord::Base
   belongs_to :root_account, class_name: "Account"
 
   validates :media_id, :workflow_state, presence: true
-  validates :auto_caption_status, inclusion: { in: AUTO_CAPTION_STATUSES.values }, allow_nil: true
+  validates :auto_caption_status, inclusion: { in: AUTO_CAPTION_STATUSES.keys }, allow_nil: true
   has_many :media_tracks, ->(media_object) { where(attachment_id: [nil, media_object.attachment_id]).order(:locale) }, dependent: :destroy, inverse_of: :media_object
   has_many :attachments_by_media_id, class_name: "Attachment", primary_key: :media_id, foreign_key: :media_entry_id, inverse_of: :media_object_by_media_id
   before_create :create_attachment
