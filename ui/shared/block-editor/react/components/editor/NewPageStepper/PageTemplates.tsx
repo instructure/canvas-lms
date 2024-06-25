b3572cc17476498ddbe12d118f13f8f90412c29c
Jacob DeWar <jacob.dewar@instructure.com>
2024-06-25 08:44:14 -0400

    Add template to Block Editor
    closes RCX-2036
    flag=block_editor
    
    Test Plan:
    - Open new Block Editor
    - Select Template
    * Only one works
    - Load template
    
    Change-Id: I3a4356fd37c5733cf854ee72e1bd319f5ae330f9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/350990
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- ui/shared/block-editor/react/components/editor/NewPageStepper/PageTemplates.tsx
+++ ui/shared/block-editor/react/components/editor/NewPageStepper/PageTemplates.tsx
@@ -16,16 +16,99 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
-import {Alert} from '@instructure/ui-alerts'
+import React, {useRef, useEffect, useCallback} from 'react'
+import {CondensedButton} from '@instructure/ui-buttons'
+import {View, type ViewProps} from '@instructure/ui-view'
+import {Img} from '@instructure/ui-img'
+import {Text} from '@instructure/ui-text'
+import {Grid} from '@instructure/ui-grid'
+import {Flex} from '@instructure/ui-flex'
 
-const PageTemplates = () => {
+interface PageTemplatesProps {
+  selectedTemplate: string
+  onSelectTemplate: (templateId: string) => void
+}
+
+const PageTemplates = ({selectedTemplate = 'template-1', onSelectTemplate}: PageTemplatesProps) => {
+  const firstTemplateRef = useRef<HTMLButtonElement | null>(null)
+  const secondTemplateRef = useRef<HTMLButtonElement | null>(null)
+
+  useEffect(() => {
+    if (selectedTemplate === 'template-1') {
+      firstTemplateRef.current?.focus()
+    } else {
+      secondTemplateRef.current?.focus()
+    }
+  }, [selectedTemplate])
+
+  const handleSelectTemplate = useCallback(
+    (e: React.KeyboardEvent<ViewProps> | React.MouseEvent<ViewProps, MouseEvent>) => {
+      const target = e.target as HTMLElement
+      onSelectTemplate(target.id)
+    },
+    [onSelectTemplate]
+  )
+
   return (
-    <div>
-      <Alert variant="warning" margin="small">
-        Sorry, we don&apos;t do page templates yet
-      </Alert>
-    </div>
+    <Flex direction="column" gap="small">
+      <Text size="large">Browse Templates</Text>
+      <Text size="small">
+        Select from a variety of pre-designed page layouts that are ready to be filled with your
+        content.
+      </Text>
+      <Text size="large">Home Pages</Text>
+      <Text size="small">
+        A home page is the main or introductory page of a course. The home page is typically the
+        first page you see. It serves as a central hub, guiding visitors to other parts of the
+        course.
+      </Text>
+      <View
+        as="div"
+        position="relative"
+        width="100%"
+        maxWidth="100%"
+        padding="small"
+        borderRadius="large large none none"
+        background="secondary"
+      >
+        <Flex gap="small">
+          <View
+            as="div"
+            borderWidth={selectedTemplate === 'template-1' ? 'medium' : 'none'}
+            borderColor="brand"
+            padding="xxx-small"
+            borderRadius="large large large large"
+          >
+            <CondensedButton
+              id="template-1"
+              onClick={handleSelectTemplate}
+              elementRef={(el: Element | null) => {
+                if (el) firstTemplateRef.current = el as HTMLButtonElement
+              }}
+            >
+              <Img src="/images/block_editor/template-1.png" alt="" width="230px" height="350px" />
+            </CondensedButton>
+          </View>
+          <View
+            as="div"
+            borderWidth={selectedTemplate === 'template-2' ? 'medium' : 'none'}
+            borderColor="brand"
+            padding="xxx-small"
+            borderRadius="large large large large"
+          >
+            <CondensedButton
+              id="template-2"
+              onClick={handleSelectTemplate}
+              elementRef={(el: Element | null) => {
+                if (el) secondTemplateRef.current = el as HTMLButtonElement
+              }}
+            >
+              <Img src="/images/block_editor/template-2.png" alt="" width="230px" height="350px" />
+            </CondensedButton>
+          </View>
+        </Flex>
+      </View>
+    </Flex>
   )
 }
 
