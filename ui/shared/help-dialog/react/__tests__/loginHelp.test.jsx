6bd7239f5e0528e0cc03248177158d5fee439e25
Michael Hulse <michael.hulse@instructure.com>
2024-07-08 21:27:47 -0700

    load login help menu modal when using vanity URL
    closes FOO-4558
    flag=none
    
    test plan:
    - visit an instance which has a vanity URL
    - navigate to the login page using the vanity URL
    - click on the Help link
    - expect Help modal to open
    - repeat above steps for a non-vanity URL
    - ensure that all unit tests pass
    
    Change-Id: Ic27e8270a3fa8facc60c0d5cf60338a3e61c95b9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/352234
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Maya Tyner <maya.tyner@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>

---
+++ ui/shared/help-dialog/react/__tests__/loginHelp.test.jsx
@@ -0,0 +1,151 @@
+// Copyright (C) 2020 - present Instructure, Inc.
+//
+// This file is part of Canvas.
+//
+// Canvas is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3 of the License.
+//
+// Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import React from 'react'
+import {render, screen, waitFor, cleanup} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {QueryProvider} from '@canvas/query'
+import LoginHelp, {renderLoginHelp} from '../loginHelp'
+
+jest.mock('@canvas/do-fetch-api-effect', () => ({
+  __esModule: true,
+  default: jest.fn(() =>
+    Promise.resolve({
+      json: [{}],
+      link: null,
+    })
+  ),
+}))
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+  jest.clearAllMocks()
+})
+
+afterEach(() => {
+  document.body.innerHTML = ''
+  cleanup()
+})
+
+describe('renderLoginHelp', () => {
+  it('renders', () => {
+    render(
+      <QueryProvider>
+        <LoginHelp linkText="Help" />
+      </QueryProvider>
+    )
+    expect(screen.getByText('Help')).toBeInTheDocument()
+  })
+
+  it('opens modal when link is clicked', () => {
+    render(
+      <QueryProvider>
+        <LoginHelp linkText="Help" />
+      </QueryProvider>
+    )
+    userEvent.click(screen.getByText('Help'))
+    expect(screen.getByText('Login Help for Canvas LMS')).toBeInTheDocument()
+  })
+
+  it('closes modal when close button is clicked', async () => {
+    render(
+      <QueryProvider>
+        <LoginHelp linkText="Help" />
+      </QueryProvider>
+    )
+    userEvent.click(screen.getByText('Help'))
+    expect(screen.getByText('Login Help for Canvas LMS')).toBeInTheDocument()
+    await userEvent.click(screen.getByTestId('login-help-close-button'))
+    await waitFor(() => {
+      expect(screen.queryByText('Login Help for Canvas LMS')).not.toBeInTheDocument()
+    })
+  })
+
+  it('renders modal label correctly', () => {
+    render(
+      <QueryProvider>
+        <LoginHelp linkText="Help" />
+      </QueryProvider>
+    )
+    userEvent.click(screen.getByText('Help'))
+    expect(screen.getByText('Login Help for Canvas LMS')).toBeInTheDocument()
+  })
+
+  describe('renderLoginHelp()', () => {
+    it('renders modal with link text for simple anchor tag', async () => {
+      const anchorElement = document.createElement('a')
+      anchorElement.href = '#'
+      anchorElement.textContent = 'Help'
+      document.body.appendChild(anchorElement)
+      renderLoginHelp(anchorElement)
+      userEvent.click(screen.getByText('Help'))
+      await waitFor(() => {
+        expect(screen.getByText('Login Help for Canvas LMS')).toBeInTheDocument()
+      })
+    })
+
+    it('renders modal with link text for anchor tag with span child, including hidden span', async () => {
+      const anchorElement = document.createElement('a')
+      const spanElement = document.createElement('span')
+      spanElement.textContent = 'Help'
+      anchorElement.appendChild(spanElement)
+      const extraSpanElement = document.createElement('span')
+      extraSpanElement.textContent = 'Links to an external site.'
+      extraSpanElement.style.display = 'none'
+      anchorElement.appendChild(extraSpanElement)
+      document.body.appendChild(anchorElement)
+      // simulate clicking the span element and pass event.target to renderLoginHelp
+      anchorElement.addEventListener('click', event => {
+        expect(event.target.textContent).toBe('Help')
+        renderLoginHelp(event.target)
+      })
+      userEvent.click(spanElement) // not perfect, but it works
+      await waitFor(() => {
+        expect(screen.getByText('Login Help for Canvas LMS')).toBeInTheDocument()
+      })
+    })
+
+    it('throws an error if the provided element is neither an anchor element nor a direct child of an anchor element', () => {
+      const divElement = document.createElement('div')
+      document.body.appendChild(divElement)
+      expect(() => renderLoginHelp(divElement)).toThrow(
+        'Element must be an <a> element or a descendant of an <a> element'
+      )
+    })
+
+    it('renders help link when the provided element is an anchor element', async () => {
+      const anchorElement = document.createElement('a')
+      anchorElement.textContent = 'Help'
+      document.body.appendChild(anchorElement)
+      renderLoginHelp(anchorElement)
+      await waitFor(() => {
+        expect(screen.getByText('Help')).toBeInTheDocument()
+      })
+    })
+
+    it('renders help link when the provided element is a direct child of an anchor element', async () => {
+      const anchorElement = document.createElement('a')
+      const spanElement = document.createElement('span')
+      spanElement.textContent = 'Help'
+      anchorElement.appendChild(spanElement)
+      document.body.appendChild(anchorElement)
+      renderLoginHelp(anchorElement)
+      await waitFor(() => {
+        expect(screen.getByText('Help')).toBeInTheDocument()
+      })
+    })
+  })
+})
