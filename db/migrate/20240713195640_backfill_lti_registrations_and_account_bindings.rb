2a7ecc4806bd5d310cdb9568b1cbadea7792a305
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-07-15 14:19:54 -0600

    backfill Lti::RegistrationAccountBindings
    why:
    - So that the state of our newer data model is in sync with what's
      actually in the database.
    
    closes INTEROP-8502
    
    flag=none
    
    test-plan:
    - Have an API dev key and an LTI 1.3 dev key installed at the root account
      and site admin level. Ensure that the LTI 1.3 keys have an already existing
      Lti::RegistrationAccountBinding. If they were created semi-recently,
      there should already be an active record hook that did it for you.
      If they don't, create some new keys or create bindings for them
      manually in the Rails console.
    - Run the migration.
    - Nothing in your database should have changed.
    - Now delete the Lti::RegistrationAccountBinding associated with the LTI
      1.3 keys.
    - Run the migration again. The bindings should have been (re)created.
    - Ensure that no such binding was created for the API key.
    
    Change-Id: If57d299e550366131f2229101585ffa82609de69
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/352646
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Migration-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Product-Review: Ryan Hawkins <ryan.hawkins@instructure.com>

--- db/migrate/20240713195640_backfill_lti_registrations_and_account_bindings.rb
+++ db/migrate/20240713195640_backfill_lti_registrations_and_account_bindings.rb
@@ -17,10 +17,12 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-class BackfillLtiRegistrations < ActiveRecord::Migration[7.1]
+class BackfillLtiRegistrationsAndAccountBindings < ActiveRecord::Migration[7.1]
   tag :postdeploy
 
   def change
-    DataFixup::CreateLtiRegistrationsFromDeveloperKeys.delay_if_production(priority: Delayed::LOWER_PRIORITY).run
+    strand = "backfill_lti_registrations_and_account_bindings/shard_#{Shard.current.id}"
+    DataFixup::CreateLtiRegistrationsFromDeveloperKeys.delay_if_production(priority: Delayed::LOWER_PRIORITY, strand:).run
+    DataFixup::Lti::BackfillLtiRegistrationAccountBindings.delay_if_production(priority: Delayed::LOWER_PRIORITY, strand:).run
   end
 end
---
+++ spec/lib/data_fixup/lti/backfill_lti_registration_account_bindings_spec.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+RSpec.describe DataFixup::Lti::BackfillLtiRegistrationAccountBindings do
+  let(:dev_key) { dev_key_model_1_3(account:) }
+  let(:account) { account_model }
+
+  it "backfills the lti_registration_account_bindings table" do
+    # Asserts that we skip any syncing to avoid extraneous updates to the dev key on
+    # reg account binding creation
+    expect(DeveloperKeyAccountBinding).not_to receive(:update)
+    dev_key
+    expect { described_class.run }.to change { Lti::RegistrationAccountBinding.count }.by(1)
+    expect(Lti::RegistrationAccountBinding.last.account).to eq(account)
+    expect(Lti::RegistrationAccountBinding.last.registration).to eq(dev_key.lti_registration)
+  end
+
+  it "copies the workflow state of the dev key account binding" do
+    dev_key.account_binding_for(account).update_column(:workflow_state, "off")
+    expect { described_class.run }.to change { Lti::RegistrationAccountBinding.count }.by(1)
+    expect(Lti::RegistrationAccountBinding.last.workflow_state).to eq(dev_key.account_binding_for(account).workflow_state)
+  end
+
+  context "an error occurs while saving a new account binding" do
+    let(:scope) { double("scope") }
+
+    it "captures the error using Sentry" do
+      expect(Sentry).to receive(:with_scope).and_yield(scope)
+      expect(Sentry).to receive(:capture_message)
+        .with("DataFixup#backfill_lti_registration_account_bindings", { level: :warning })
+      expect(scope).to receive(:set_tags).with(developer_key_account_binding_id: dev_key.account_binding_for(account).global_id)
+      expect(scope).to receive(:set_context)
+        .with("exception", { name: "StandardError", message: "whoops!" })
+
+      allow(Lti::RegistrationAccountBinding).to receive(:create!).and_raise(StandardError.new("whoops!"))
+
+      dev_key
+
+      expect { described_class.run }.not_to raise_error
+    end
+  end
+
+  context "the dev key doesn't have an lti registration" do
+    before do
+      dev_key.update!(lti_registration: nil, skip_lti_sync: true)
+    end
+
+    it "doesn't create a binding" do
+      expect { described_class.run }.not_to change { Lti::RegistrationAccountBinding.count }
+      expect(dev_key.reload.lti_registration).to be_nil
+    end
+  end
+
+  context "when there are also API keys" do
+    let(:api_key) { dev_key_model(account:) }
+
+    it "backfills the lti_registration_account_bindings table" do
+      dev_key
+      api_key
+      expect { described_class.run }.to change { Lti::RegistrationAccountBinding.count }.by(1)
+      expect(Lti::RegistrationAccountBinding.last.account).to eq(account)
+      expect(Lti::RegistrationAccountBinding.last.registration).to eq(dev_key.lti_registration)
+      expect(api_key.reload.lti_registration).to be_nil
+    end
+  end
+
+  context "when there are some LTI keys that already have a registration account binding" do
+    let(:other_key) { dev_key_model_1_3(account:) }
+    let(:binding) { Lti::RegistrationAccountBinding.create!(developer_key_account_binding: DeveloperKeyAccountBinding.find_in_account_priority(account.account_chain, other_key), account:, registration: other_key.lti_registration) }
+
+    it "doesn't create any new bindings and runs successfully" do
+      binding
+      expect { described_class.run }.not_to change { Lti::RegistrationAccountBinding.count }
+    end
+  end
+
+  context "when dealing with inherited account bindings" do
+    let(:site_admin_key) do
+      key = dev_key_model_1_3(account: Account.site_admin)
+      key.update!(account: nil)
+      key
+    end
+
+    it "backfills successfully" do
+      site_admin_key
+      expect { described_class.run }.to change { Lti::RegistrationAccountBinding.count }.by(1)
+      expect(site_admin_key.reload.lti_registration.account_binding_for(Account.site_admin)).to eq(Lti::RegistrationAccountBinding.last)
+    end
+
+    context "and there's a site admin and root account level binding" do
+      let(:root_account_binding) { DeveloperKeyAccountBinding.create!(account:, developer_key: site_admin_key, workflow_state: "on", skip_lime_sync: true) }
+
+      before do
+        site_admin_key.account_binding_for(Account.site_admin).update!(workflow_state: "allow", skip_lime_sync: true)
+        root_account_binding
+      end
+
+      it "backfills successfully" do
+        expect { described_class.run }.to change { Lti::RegistrationAccountBinding.count }.by(2)
+      end
+    end
+  end
+end
