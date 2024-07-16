3603f76ed8df4dd778f0512c2e7f2d42d2972a12
Tucker McKnight <tmcknight@instructure.com>
2024-07-16 17:11:15 -0600

    Fixup to create Lti::Registrations for existing dev keys
    fixes INTEROP-8501
    
    flag = lti_registrations_page
    
    test plan:
    - Create some LTI developer keys in the UI, or use the developer
      keys from existing LTI tools that you have installed
    - Make note of their IDs
    - Find those developer keys in the rails console and manually
      delete their lti_registration association
      - DeveloperKey.find(<id>).update(lti_registration: nil, skip_lti_sync: true)
    - Enable the lti_registrations_page feature flag
    - Go to the Settings > Apps page, and see that your LTI tools do not
      snow up there
    - Make sure the jobs container is running
    - Run rake db:migrate
    - Refresh the Settings > Apps page; the LTI tools should show up now
    
    Change-Id: I4729f620f2b49981b652d99c8b961bd927364d9f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/352890
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    QA-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

---
+++ lib/data_fixup/create_lti_registrations_from_developer_keys.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+module DataFixup
+  module CreateLtiRegistrationsFromDeveloperKeys
+    def self.run
+      DeveloperKey.where(workflow_state: "active", is_lti_key: true, lti_registration: nil).preload(:tool_configuration, :ims_registration).find_each do |developer_key|
+        registration_values = {
+          admin_nickname: developer_key.name,
+          account: developer_key.account,
+          internal_service: developer_key.internal_service,
+          name: developer_key.tool_configuration.configuration["title"],
+          developer_key:,
+          ims_registration: developer_key.ims_registration, # can be nil
+          skip_lti_sync: true,
+        }
+
+        begin
+          registration = ::Lti::Registration.create!(registration_values)
+
+          developer_key.lti_registration = registration if registration
+          developer_key.save!
+        rescue => e
+          Sentry.with_scope do |scope|
+            scope.set_tags(developer_key_id: developer_key.global_id)
+            scope.set_context("exception", { name: e.class.name, message: e.message })
+            Sentry.capture_message("DataFixup#create_lti_registrations_from_developer_keys", level: :warning)
+          end
+        end
+      end
+    end
+  end
+end
