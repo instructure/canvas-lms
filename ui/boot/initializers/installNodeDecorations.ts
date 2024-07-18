68834f7d0c671e459cabcb5e67416adaf344431d
Charley Kline <ckline@instructure.com>
2024-07-18 17:48:13 -0500

    Make visibleTextContent a global property on DOM nodes
    Refs FOO-4582
    flag=none
    
    Michael wrote a clever little function that works like
    Node.textContent to get the inner text of a DOM node,
    but excludes anything that is display=none, so as not
    to confuse the returned value if it has, say, some
    screenreader-only stuff inside it.
    
    I thought it would be generally useful so this commit
    arranges to make it globally available on any DOM node
    inside Canvas. The addition of the new property on
    Node happens during UI boot time so it can just be used
    naturally.
    
    Test plan:
    * automated tests pass
    * you agree this is a nifty and useful addition
    
    Change-Id: I5246c5c545d4bdca3cd344ae5c41a5b5e71998bc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/353077
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/boot/initializers/installNodeDecorations.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// This adds a new property to the DOM Node prototype that returns the visible
+// text content of the node. node.visibleTextContent will work just like node.textContent
+// but will omit any non-displayed text nodes (e.g. text nodes inside hidden elements
+// such as screenreader content.)
+
+// exported for tests only
+export const getVisibleTextContent = (element: Node): string =>
+  Array.from(element.childNodes)
+    .filter(
+      node =>
+        node.nodeType === Node.TEXT_NODE ||
+        (node instanceof HTMLElement && getComputedStyle(node).display !== 'none')
+    )
+    .map(node => node.textContent?.trim())
+    // remove empty strings using Boolean operation
+    .filter(Boolean)
+    .join(' ')
+    .trim()
+
+declare global {
+  interface Node {
+    visibleTextContent: string
+  }
+}
+
+export function up(): void {
+  Object.defineProperty(Node.prototype, 'visibleTextContent', {
+    get() {
+      return getVisibleTextContent(this)
+    },
+    enumerable: true,
+  })
+}
