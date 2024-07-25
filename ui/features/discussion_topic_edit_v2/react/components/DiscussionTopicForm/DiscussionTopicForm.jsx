f7e63810c5459c2c22cc86eb71a252374a5a4dbb
Adam Molnar <adam.molnar@instructure.com>
2024-07-25 09:10:28 +0000

    remove "Allow participants to comment" from announcements
    closes VICE-4455
    flag=discussion_create
    
    test plan:
    - Turn on "Disable comments for announcements" in course setting
    - (also try the same checkbox in account settings)
    - the "Allow participants to comment" setting
     should not be in the announcement edit page
    
    Change-Id: I11698bd5caee1b0118e34d79b7b67994c54ce7c6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/353448
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Dora Csolakov <dora.csolakov@instructure.com>
    Product-Review: Tamás Balogh <tamas.balogh@instructure.com>
    Reviewed-by: Daniel Matyas Vincze <daniel.vincze@instructure.com>

--- ui/features/discussion_topic_edit_v2/react/components/DiscussionTopicForm/DiscussionTopicForm.jsx
+++ ui/features/discussion_topic_edit_v2/react/components/DiscussionTopicForm/DiscussionTopicForm.jsx
@@ -407,6 +407,7 @@ function DiscussionTopicForm({
     shouldShowPodcastFeedOption,
     shouldShowCheckpointsOptions,
     shouldShowAssignToForUngradedDiscussions,
+    shouldShowAllowParticipantsToCommentOption,
   } = useShouldShowContent(
     isGraded,
     isAnnouncement,
@@ -858,7 +859,7 @@ function DiscussionTopicForm({
             />
           )}
           <FormFieldGroup description="" rowSpacing="small">
-            {shouldShowAnnouncementOnlyOptions && (
+            {shouldShowAllowParticipantsToCommentOption && (
               <Checkbox
                 label={I18n.t('Allow Participants to Comment')}
                 value="enable-participants-commenting"
