9035abd34bb70f573ddc24753b5319ea8d1e1341
Ed Schiebel <eschiebel@instructure.com>
2024-07-26 09:17:15 -0600

    Add tests for the block editor utility functions
    some functions are not tested because they require mocking
    too much of the craft api. esp. "query"
    
    closes RCX-2150
    flag=none
    
    test plan: passes jenkins
    
    Change-Id: Ic8b4f026210215c7babbf601985fea3d67a2e4d9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/353582
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

---
+++ ui/shared/block-editor/react/utils/__tests__/getNodeIndex.test.ts
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {getNodeIndex, getSectionLocation} from '../getNodeIndex'
+
+describe('getNodeIndex', () => {
+  let currDescendants: string[] = []
+
+  describe('getNodeIndex', () => {
+    it('returns the index of the node in the list of descendants', () => {
+      const node = {id: '1', data: {parent: 'ROOT'}}
+      currDescendants = ['2', '1', '3']
+      const query = {
+        node: (_id: string) => ({
+          descendants: () => currDescendants,
+        }),
+      }
+      // @ts-expect-error
+      expect(getNodeIndex(node, query)).toEqual(1)
+    })
+  })
+
+  describe('getSectionLocation', () => {
+    it('should return "alone" if node is the only section', () => {
+      const node = {id: '1', data: {parent: 'ROOT'}}
+      currDescendants = ['1']
+      const query = {
+        node: (_id: string) => ({
+          descendants: () => currDescendants,
+        }),
+      }
+      // @ts-expect-error
+      expect(getSectionLocation(node, query)).toEqual('alone')
+    })
+
+    it('should return "top" if node is the first section', () => {
+      const node = {id: '1', data: {parent: 'ROOT'}}
+      currDescendants = ['1', '2']
+      const query = {
+        node: (_id: string) => ({
+          descendants: () => currDescendants,
+        }),
+      }
+      // @ts-expect-error
+      expect(getSectionLocation(node, query)).toEqual('top')
+    })
+
+    it('should return "bottom" if node is the last section', () => {
+      const node = {id: '1', data: {parent: 'ROOT'}}
+      currDescendants = ['2', '1']
+      const query = {
+        node: (_id: string) => ({
+          descendants: () => currDescendants,
+        }),
+      }
+      // @ts-expect-error
+      expect(getSectionLocation(node, query)).toEqual('bottom')
+    })
+
+    it('should return "middle" if node is in the middle of sections', () => {
+      const node = {id: '1', data: {parent: 'ROOT'}}
+      currDescendants = ['2', '1', '3']
+      const query = {
+        node: (_id: string) => ({
+          descendants: () => currDescendants,
+        }),
+      }
+      // @ts-expect-error
+      expect(getSectionLocation(node, query)).toEqual('middle')
+    })
+  })
+})
---
+++ ui/shared/block-editor/react/utils/__tests__/kb.test.ts
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {KeyboardEvent} from 'react'
+import {
+  isAnyModifierKeyPressed,
+  getCaretPosition,
+  isCaretAtEnd,
+  setCaretToEnd,
+  shouldAddNewNode,
+  removeLastParagraphTag,
+  shouldDeleteNode,
+} from '../kb'
+
+const makeKeyboardEvent = (opts = {}) => {
+  return {
+    ctrlKey: false,
+    metaKey: false,
+    shiftKey: false,
+    altKey: false,
+    ...opts,
+  } as KeyboardEvent
+}
+
+describe('keyboard utilities', () => {
+  describe('isAnyModifierKeyPressed', () => {
+    it('should return false if no modifier key is pressed', () => {
+      expect(isAnyModifierKeyPressed(makeKeyboardEvent())).toBe(false)
+    })
+
+    it('should return true if any modifier key is pressed', () => {
+      expect(isAnyModifierKeyPressed(makeKeyboardEvent({ctrlKey: true}))).toBe(true)
+      expect(isAnyModifierKeyPressed(makeKeyboardEvent({metaKey: true}))).toBe(true)
+      expect(isAnyModifierKeyPressed(makeKeyboardEvent({shiftKey: true}))).toBe(true)
+      expect(isAnyModifierKeyPressed(makeKeyboardEvent({altKey: true}))).toBe(true)
+    })
+  })
+
+  describe('getCaretPosition', () => {
+    it('should return the 0 if there is no selection', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const pos = getCaretPosition(div)
+      expect(pos).toBe(0)
+    })
+
+    it('should return the caret position if there is a selection', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const textNode = div.firstChild as Text
+      const range = document.createRange()
+      range.setStart(textNode, 5)
+      range.setEnd(textNode, 5)
+      const sel = window.getSelection()
+      sel?.removeAllRanges()
+      sel?.addRange(range)
+      const pos = getCaretPosition(div)
+      expect(pos).toBe(5)
+    })
+  })
+
+  describe('isCaretAtEnd', () => {
+    it('returns true if the caret is at the end of the text', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const textNode = div.firstChild as Text
+      const range = document.createRange()
+      range.setStart(textNode, 11)
+      range.setEnd(textNode, 11)
+      const sel = window.getSelection()
+      sel?.removeAllRanges()
+      sel?.addRange(range)
+      expect(isCaretAtEnd(div)).toBe(true)
+    })
+
+    it('returns false if the caret is not at the end of the text', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const textNode = div.firstChild as Text
+      const range = document.createRange()
+      range.setStart(textNode, 5)
+      range.setEnd(textNode, 5)
+      const sel = window.getSelection()
+      sel?.removeAllRanges()
+      sel?.addRange(range)
+      expect(isCaretAtEnd(div)).toBe(false)
+    })
+  })
+
+  describe('setCaretToEnd', () => {
+    it('should set the caret to the end of the text', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const textNode = div.firstChild as Text
+      const range = document.createRange()
+      range.setStart(textNode, 0)
+      range.setEnd(textNode, 0)
+      const sel = window.getSelection()
+      sel?.removeAllRanges()
+      sel?.addRange(range)
+      expect(isCaretAtEnd(div)).toBe(false)
+
+      setCaretToEnd(div)
+      expect(isCaretAtEnd(div)).toBe(true)
+    })
+  })
+
+  describe('shouldAddNewNode', () => {
+    it('should return true if this is the 2nd Enter key press', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      setCaretToEnd(div)
+      expect(shouldAddNewNode(makeKeyboardEvent({key: 'Enter', currentTarget: div}), 'Enter')).toBe(
+        true
+      )
+    })
+
+    it('should return false if there is no text content', () => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      setCaretToEnd(div)
+      expect(shouldAddNewNode(makeKeyboardEvent({key: 'Enter', currentTarget: div}), 'Enter')).toBe(
+        false
+      )
+    })
+
+    it('should return false if a modifier key is pressed', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      setCaretToEnd(div)
+      expect(
+        shouldAddNewNode(
+          makeKeyboardEvent({key: 'Enter', currentTarget: div, ctrlKey: true}),
+          'Enter'
+        )
+      ).toBe(false)
+    })
+
+    it('should return false if the caret is not at the end of the text', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const textNode = div.firstChild as Text
+      const range = document.createRange()
+      range.setStart(textNode, 5)
+      range.setEnd(textNode, 5)
+      const sel = window.getSelection()
+      sel?.removeAllRanges()
+      sel?.addRange(range)
+      expect(shouldAddNewNode(makeKeyboardEvent({key: 'Enter', currentTarget: div}), 'Enter')).toBe(
+        false
+      )
+    })
+
+    it('should return false if this is not the 2nd Enter key press', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      setCaretToEnd(div)
+      expect(shouldAddNewNode(makeKeyboardEvent({key: 'Enter', currentTarget: div}), 'a')).toBe(
+        false
+      )
+    })
+  })
+
+  describe('removeLastParagraphTag', () => {
+    it('should remove the last paragraph tag', () => {
+      const div = document.createElement('div')
+      div.innerHTML = '<p id="p1">hello</p><p id="p2">world</p>'
+      document.body.appendChild(div)
+      removeLastParagraphTag(div)
+      expect(div.innerHTML).toBe('<p id="p1">hello</p>')
+    })
+
+    it('should do nothing if there are no paragraph tags', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      removeLastParagraphTag(div)
+      expect(div.innerHTML).toBe('hello world')
+    })
+  })
+
+  describe('shouldDeleteNode', () => {
+    it('should return true if the delete key is pressed in an empty element', () => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      expect(shouldDeleteNode(makeKeyboardEvent({key: 'Backspace', currentTarget: div}))).toBe(true)
+    })
+
+    it('should return false if the delete key is not pressed', () => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      expect(shouldDeleteNode(makeKeyboardEvent({key: 'a', currentTarget: div}))).toBe(false)
+    })
+
+    it('should return false if the element is not empty', () => {
+      const div = document.createElement('div')
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      expect(shouldDeleteNode(makeKeyboardEvent({key: 'Backspace', currentTarget: div}))).toBe(
+        false
+      )
+    })
+  })
+
+  describe('addNewNodeAsNextSibling', () => {
+    // requires mocking to much of craft.js
+  })
+
+  describe('deleteNodeAndSelecctPrevSibling', () => {
+    // requires mocking to much of craft.js
+  })
+})
