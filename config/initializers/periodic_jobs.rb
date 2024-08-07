9cd3b0706a2137919fab91ba70d83806bec6e681
Jacob Burroughs <jburroughs@instructure.com>
2024-08-07 10:24:56 -0500

    Fix attachments cron typo
    fixes gh-2383
    fixes CANVAS-MN3K
    
    Change-Id: I791edeea22a58b39e6fc3a072e12cd9b68f57def
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/354524
    Reviewed-by: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- config/initializers/periodic_jobs.rb
+++ config/initializers/periodic_jobs.rb
@@ -182,12 +182,12 @@ Rails.configuration.after_initialize do
   end
 
   unless ApplicationController.test_cluster?
-    Delayed::Periodic.cron "Attachment::GarbageCollector::ContentExportAndMigrationContextType.delete_content", "37 1 * * *" do
-      with_each_shard_by_database(Attachment::GarbageCollector::ContentExportAndMigrationContextType, :delete_content, jitter: 30.minutes, local_offset: true)
+    Delayed::Periodic.cron "Attachments::GarbageCollector::ContentExportAndMigrationContextType.delete_content", "37 1 * * *" do
+      with_each_shard_by_database(Attachments::GarbageCollector::ContentExportAndMigrationContextType, :delete_content, jitter: 30.minutes, local_offset: true)
     end
 
-    Delayed::Periodic.cron "Attachment::GarbageCollector::ContentExportContextType.delete_content", "37 3 * * *" do
-      with_each_shard_by_database(Attachment::GarbageCollector::ContentExportContextType, :delete_content, jitter: 30.minutes, local_offset: true)
+    Delayed::Periodic.cron "Attachments::GarbageCollector::ContentExportContextType.delete_content", "37 3 * * *" do
+      with_each_shard_by_database(Attachments::GarbageCollector::ContentExportContextType, :delete_content, jitter: 30.minutes, local_offset: true)
     end
   end
 
