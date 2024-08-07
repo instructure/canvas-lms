8ae5202fb7382479487d1c38c30b32c7ee40ed98
Derek Williams <derek.williams@instructure.com>
2024-08-07 13:16:53 -0400

    restrict media object captions to submission comments
    currently we request captions on all media object, but
    we only want to do this for submission comments
    
    flag=speedgrader_studio_media_capture
    
    test plan:
    - create a submission comment in speed grader
    - verify that a job was created to request captions
    
    Change-Id: Id71158482339efc438b88c1f297a9656df879a6d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/354536
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cameron Ray <cameron.ray@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>
    QA-Review: Derek Williams <derek.williams@instructure.com>

--- app/models/submission_comment.rb
+++ app/models/submission_comment.rb
@@ -69,6 +69,7 @@ class SubmissionComment < ActiveRecord::Base
   before_save :set_edited_at
   after_save :update_participation
   after_save :check_for_media_object
+  after_save :request_captions, if: :needs_captions?
   after_update :publish_other_comments_in_this_group
   after_update :post_submission_for_finalized_draft, if: -> { saved_change_to_draft?(from: true, to: false) }
   after_destroy :delete_other_comments_in_this_group
@@ -160,6 +161,12 @@ class SubmissionComment < ActiveRecord::Base
     media_comment_id && media_comment_type
   end
 
+  def media_object
+    return nil unless media_comment?
+
+    MediaObject.by_media_id(media_comment_id).first
+  end
+
   def self.serialize_media_comment(media_comment_id)
     media_object = MediaObject.by_media_id(media_comment_id).first
     return nil unless media_object.present?
@@ -180,6 +187,10 @@ class SubmissionComment < ActiveRecord::Base
                                       user: author,
                                       context: author)
     end
+  end
+
+  def request_captions
+    media_object&.generate_captions
   end
 
   on_create_send_to_streams do
@@ -498,6 +509,11 @@ class SubmissionComment < ActiveRecord::Base
   end
 
   private
+
+  def needs_captions?
+    obj = media_object
+    obj.present? && obj.auto_caption_status.nil? && obj.media_id.present? && obj.media_type.include?("video") && obj.media_tracks.where(kind: "subtitles").none?
+  end
 
   def updating_user_present?
     # For newly-created comments, the updating user is always the commenter
