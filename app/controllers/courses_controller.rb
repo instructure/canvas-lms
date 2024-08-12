17875dda52fe3531027a1be38a3c605cd9fa03f9
Angel Diaz <angel.diaz@strongmind.com>
2024-08-12 09:54:40 -0700

    (B) QTY-9035: Rename param to params
    Co-authored-by: Jothan Kelepolo <jothan.kelepolo@strongmind.com>
    Co-authored-by: Alixandra Arehart <Alixandra.Arehart@strongmind.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2450,7 +2450,7 @@ class CoursesController < ApplicationController
       @course.send_later_if_production_enqueue_args(:touch_content_if_public_visibility_changed,
         { :priority => Delayed::LOW_PRIORITY }, changes)
       Rails.logger.info("Referrer: #{request.referer}")
-      course_saved = @course.errors.none? && (param[:update_timezone] ? @course.save_with_account_times : @course.save)
+      course_saved = @course.errors.none? && (params[:update_timezone] ? @course.save_with_account_times : @course.save)
       if course_saved
         Auditors::Course.record_updated(@course, @current_user, changes, source: logging_source)
         @current_user.touch
