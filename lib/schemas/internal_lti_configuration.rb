523469c087f0df3c175b08d1ad2806c81324190e
Xander Moffatt <xmoffatt@instructure.com>
2024-08-13 15:10:43 -0600

    define internal shape of LTI 1.3 config
    why:
    * to have a single source of truth for the shape of 1.3 JSON config,
    both external (LtiConfiguration), and internal (InternalLtiConfiguration)
    
    * update current schema for LtiConfiguration to match the format
    of the new InternalLtiConfiguration schema
    * provide transformation methods between the two formats
    * use newer version of LtiConfiguration schema validator
    in place of Schemas::Lti::ToolConfiguration
    * expose transformation and validation via API endpoints
    
    closes INTEROP-8762
    flag=none
    
    test plan:
    * installing a 1.3 developer key still succeeds in every scenario that
    it did before this commit
    * specific things to test:
      * not including required properties in the config, like extensions,
      settings, scopes
      * including vendor extensions (not the canvas platform)
      * including every possible config option, and a bunch of different
      placements
    * make API requests to the new /configuration/validate endpoint with
      both valid and invalid configurations, and with both url and
      lti_configuration
    
    Change-Id: I3fa5b6f7c5010a4a1fc16a80e198dc979569e282
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/354802
    Reviewed-by: Paul Gray <paul.gray@instructure.com>
    QA-Review: Paul Gray <paul.gray@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>

---
+++ lib/schemas/internal_lti_configuration.rb
@@ -0,0 +1,187 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Schemas
+  # Represents the "internal" JSON schema used to configure an LTI 1.3 tool,
+  # as stored in Lti::ToolConfiguration and used in Lti::Registration.
+  class InternalLtiConfiguration < Base
+    # Transforms a hash conforming to the LtiConfiguration schema into
+    # a hash conforming to the InternalLtiConfiguration schema.
+    def self.from_lti_configuration(lti_config)
+      config = lti_config.deep_dup.deep_symbolize_keys
+
+      extensions = config.delete(:extensions)
+      canvas_ext, vendor_extensions = extensions.map(&:deep_symbolize_keys).partition { |ext| ext[:platform] == "canvas.instructure.com" }
+      canvas_ext = canvas_ext.first || {}
+      config[:vendor_extensions] = vendor_extensions if vendor_extensions.present?
+
+      canvas_ext.delete(:platform)
+      launch_settings = canvas_ext.delete(:settings)&.deep_symbolize_keys || {}
+      placements = launch_settings&.delete(:placements) || []
+
+      config.merge({
+        **canvas_ext,
+        launch_settings:,
+        placements:,
+      }.deep_symbolize_keys)
+    end
+
+    def schema
+      {
+        type: "object",
+        required: %w[
+          title
+          description
+          target_link_uri
+          oidc_initiation_url
+          scopes
+          redirect_uris
+          placements
+          launch_settings
+        ],
+        properties: {
+          **self.class.base_properties,
+          redirect_uris: { type: "array", items: { type: "string" }, minItems: 1 },
+          domain: { type: "string" },
+          tool_id: { type: "string" },
+          privacy_level: { type: "string", enum: %w[public email_only name_only anonymous] },
+          launch_settings: {
+            type: "object",
+            properties: self.class.base_settings_properties
+          },
+          placements: self.class.placements_schema,
+          # vendor_extensions: extensions with platform != "canvas.instructure.com", only currently copied during content migration. not present on 1.3 tools.
+        }
+      }.freeze
+    end
+
+    def self.base_properties
+      {
+        title: { type: "string", description: "Overridable by 'text' in settings and placements" },
+        description: { type: "string", description: "Displayed only in assignment_selection and link_selection" },
+        custom_fields: { oneOf: [{ type: "object" }, { type: "string" }], description: "Overridable in settings and placements. String for legacy purposes." },
+        target_link_uri: { type: "string", description: "Overridable in settings and placements" },
+        oidc_initiation_url: { type: "string" },
+        oidc_initiation_urls: { type: "object" },
+        public_jwk_url: { type: "string" },
+        public_jwk: { type: "object" },
+        scopes: { type: "array", items: { type: "string", enum: [*TokenScopes::LTI_SCOPES.keys, *TokenScopes::LTI_HIDDEN_SCOPES.keys] } },
+      }.freeze
+    end
+
+    def self.base_settings_properties
+      {
+        message_type: { type: "string", enum: %w[LtiDeepLinkingRequest LtiResourceLinkRequest] },
+        text: { type: "string" },
+        labels: { type: "object" },
+        custom_fields: { type: "object" },
+        selection_height: { type: "number" },
+        selection_width: { type: "number" },
+        launch_height: { type: "number", description: "Not standard everywhere yet" },
+        launch_width: { type: "number", description: "Not standard everywhere yet" },
+        icon_url: { type: "string" },
+        canvas_icon_class: { type: "string" },
+        required_permissions: { type: "string" },
+        windowTarget: { type: "string", enum: %w[_blank] },
+        display_type: { type: "string", enum: %w[default full_width full_width_in_context in_nav_context borderless] },
+        url: { type: "string", description: "Defers to target_link_uri for 1.3 tools" },
+        target_link_uri: { type: "string" },
+        visibility: { type: "string", enum: %w[admins members public] },
+        prefer_sis_email: { type: "boolean", description: "1.1 only" },
+        oauth_compliant: { type: "boolean", description: "1.1 only" },
+        **placement_specific_settings_properties,
+      }.freeze
+    end
+
+    def self.placements_schema
+      {
+        type: "array",
+        items: {
+          type: "object",
+          required: %w[placement],
+          properties: {
+            placement: { type: "string", enum: ::Lti::ResourcePlacement::PLACEMENTS.map(&:to_s) },
+            enabled: { type: "boolean" },
+            **base_settings_properties
+          },
+          allOf: [
+            {
+              if: {
+                properties: { placement: { const: "submission_type_selection" } }
+              },
+              then: {
+                properties: {
+                  description: { type: "string", maxLength: 255, errorMessage: "description must be a string with a maximum length of 255" },
+                  require_resource_selection: { type: "boolean" },
+                }
+              }
+            },
+            {
+              if: {
+                properties: { placement: { const: "account_navigation" } }
+              },
+              then: {
+                properties: {
+                  root_account_only: { type: "boolean" },
+                  default: { type: "string", enum: %w[disabled enabled] },
+                }
+              }
+            },
+            {
+              if: {
+                properties: { placement: { const: "global_navigation" } }
+              },
+              then: {
+                properties: { icon_svg_path_64: { type: "string" } }
+              }
+            },
+            {
+              if: {
+                properties: { placement: { const: "file_menu" } }
+              },
+              then: {
+                properties: { accept_media_types: { type: "string" } }
+              }
+            },
+            {
+              if: {
+                properties: { placement: { const: "editor_button" } }
+              },
+              then: {
+                properties: { use_tray: { type: "boolean" } }
+              }
+            }
+          ]
+
+        }
+      }.freeze
+    end
+
+    # These can be set in the base-level settings, but only apply to specific placements
+    def self.placement_specific_settings_properties
+      {
+        icon_svg_path_64: { type: "string", description: "Used only by global_navigation" },
+        default: { type: "string", enum: %w[disabled enabled], description: "Used only by account_navigation and course_navigation" },
+        accept_media_types: { type: "string", description: "Used only by file_menu" },
+        use_tray: { type: "boolean", description: "Used only by editor_button" },
+      }.freeze
+    end
+  end
+end
---
+++ spec/lib/schemas/internal_lti_configuration_spec.rb
@@ -0,0 +1,329 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require_relative "../../lti_1_3_tool_configuration_spec_helper"
+
+describe Schemas::InternalLtiConfiguration do
+  # introduces `settings` (hard-coded JSON LtiConfiguration)
+  # and `internal_configuration` (hard-coded JSON InternalLtiConfiguration)
+  include_context "lti_1_3_tool_configuration_spec_helper"
+
+  describe "#validate" do
+    subject { Schemas::InternalLtiConfiguration.new.validate(json).to_a }
+
+    let(:json) { internal_configuration }
+    let(:error_details) { subject.first["details"] }
+    let(:error_message) { subject.first["error"] }
+
+    context "with minimal invalid configuration" do
+      let(:json) do
+        { title: "Hello World!", description: "hello there", target_link_uri: "url", scopes: [], redirect_uris: ["url"] }
+      end
+
+      it "returns errors" do
+        expect(subject).not_to be_empty
+      end
+
+      it "includes specific errors" do
+        expect(error_details["missing_keys"]).to include "oidc_initiation_url"
+      end
+    end
+
+    context "with full valid configuration" do
+      it "returns no errors" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "windowTarget" do
+      let(:window_target) { nil }
+      let(:json) do
+        internal_configuration.tap do |c|
+          c[:launch_settings][:windowTarget] = window_target
+        end
+      end
+
+      context "when not set" do
+        before do
+          json[:launch_settings].delete(:windowTarget)
+        end
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "when _blank" do
+        let(:window_target) { "_blank" }
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "when another value" do
+        let(:window_target) { "_self" }
+
+        it "is invalid" do
+          expect(error_message).to include "windowTarget"
+        end
+      end
+    end
+
+    context "default" do
+      let(:default) { nil }
+      let(:json) do
+        internal_configuration.tap do |c|
+          c[:launch_settings][:default] = default
+        end
+      end
+
+      context "when not set" do
+        before do
+          json[:launch_settings].delete(:default)
+        end
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      %w[disabled enabled].each do |value|
+        context "when #{value}" do
+          let(:default) { value }
+
+          it "is valid" do
+            expect(subject).to be_empty
+          end
+        end
+      end
+
+      context "when another value" do
+        let(:default) { "invalid" }
+
+        it "is invalid" do
+          expect(error_message).to include "default"
+        end
+      end
+    end
+
+    context "visibility" do
+      let(:visibility) { nil }
+      let(:json) do
+        internal_configuration.tap do |c|
+          c[:launch_settings][:visibility] = visibility
+        end
+      end
+
+      context "when not set" do
+        before do
+          json[:launch_settings].delete(:visibility)
+        end
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      %w[admins members public].each do |value|
+        context "when #{value}" do
+          let(:visibility) { value }
+
+          it "is valid" do
+            expect(subject).to be_empty
+          end
+        end
+      end
+
+      context "when another value" do
+        let(:visibility) { "invalid" }
+
+        it "is invalid" do
+          expect(error_message).to include "visibility"
+        end
+      end
+    end
+
+    context "privacy_level" do
+      let(:privacy_level) { nil }
+      let(:json) do
+        internal_configuration.tap do |c|
+          c[:privacy_level] = privacy_level
+        end
+      end
+
+      context "when not set" do
+        before do
+          json.delete(:privacy_level)
+        end
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      %w[anonymous name_only email_only public].each do |value|
+        context "when #{value}" do
+          let(:privacy_level) { value }
+
+          it "is valid" do
+            expect(subject).to be_empty
+          end
+        end
+      end
+
+      context "when another value" do
+        let(:privacy_level) { "invalid" }
+
+        it "is invalid" do
+          expect(error_message).to include "privacy_level"
+        end
+      end
+    end
+
+    context "placements" do
+      context "when invalid" do
+        let(:json) do
+          internal_configuration.tap do |c|
+            c[:placements] << { placement: "invalid" }
+          end
+        end
+
+        it "is invalid" do
+          expect(error_message).to include "placements"
+        end
+      end
+    end
+
+    context "scopes" do
+      context "when invalid" do
+        let(:json) do
+          internal_configuration.tap do |c|
+            c[:scopes] << "invalid"
+          end
+        end
+
+        it "is invalid" do
+          expect(error_message).to include "scopes"
+        end
+      end
+
+      context "when empty" do
+        let(:json) do
+          internal_configuration.tap do |c|
+            c[:scopes] = []
+          end
+        end
+
+        it "is valid" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "when not present" do
+        let(:json) do
+          internal_configuration.tap do |c|
+            c.delete(:scopes)
+          end
+        end
+
+        it "is invalid" do
+          expect(error_message).to include "scopes"
+        end
+      end
+    end
+
+    context "root_account_only" do
+      context "in launch_settings" do
+        let(:json) do
+          internal_configuration.tap do |c|
+            c[:launch_settings][:root_account_only] = true
+          end
+        end
+
+        it "returns no errors" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "in account_navigation" do
+        let(:json) do
+          super().merge(
+            {
+              placements: [
+                { placement: "account_navigation", root_account_only: true }
+              ]
+            }
+          )
+        end
+
+        it "returns no errors" do
+          expect(subject).to be_empty
+        end
+      end
+
+      context "in other placement" do
+        let(:json) do
+          super().merge(
+            {
+              placements: [
+                { placement: "course_navigation", root_account_only: true }
+              ]
+            }
+          )
+        end
+
+        it "returns no errors" do
+          expect(subject).to be_empty
+        end
+      end
+    end
+  end
+
+  describe ".from_lti_configuration" do
+    subject { Schemas::InternalLtiConfiguration.from_lti_configuration(settings) }
+
+    # make tweaks to transformed config to match validations
+    let(:internal_lti_config) do
+      subject.tap do |config|
+        config.delete(:vendor_extensions)
+        config[:redirect_uris] = [config[:target_link_uri]]
+      end
+    end
+
+    it "returns a valid InternalLtiConfiguration" do
+      expect(Schemas::InternalLtiConfiguration.new.validate(internal_lti_config).to_a).to be_empty
+    end
+
+    it "returns an InternalLtiConfiguration with the same values as the LtiConfiguration" do
+      expect(internal_lti_config).to eq(internal_configuration)
+    end
+
+    context "with no canvas extension" do
+      before do
+        settings[:extensions] = []
+      end
+
+      it "does not error" do
+        expect { subject }.not_to raise_error
+      end
+    end
+  end
+end
