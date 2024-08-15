5fb281aa1aeca90352d0fecd66112f9e1410aaa4
August Thornton <august@instructure.com>
2024-08-15 13:57:30 -0700

    Refactor permission registration
    closes FOO-4659
    flag = none
    
    test plan |
    - permissions should behave as before
    - we now have a single source of truth for permissions
      that is much more obvious and understandable
    - don't clutter role overrides with permissions
    - registration has be refactored to be more DRY
    - /permissions should work as before and plugins should
      be able to register permissions in the same way,
      however we settled on one uniform way to do it
    
    Change-Id: I9e2f541643713065dfada692b4eb30230a9896e7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/355239
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: August Thornton <august@instructure.com>

---
+++ spec/initializers/permissions_registry_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+describe "Permission Registry" do
+  before do
+    Permissions.instance_variable_set(:@permissions, nil)
+  end
+
+  it "registers the correct permissions" do
+    Rails.application.config.to_prepare_blocks.each(&:call)
+
+    registered_permissions = Permissions.retrieve
+
+    expect(registered_permissions[:become_user]).to include(
+      label: an_instance_of(Proc),
+      label_v2: an_instance_of(Proc),
+      account_only: :root,
+      true_for: %w[AccountAdmin],
+      available_to: %w[AccountAdmin AccountMembership]
+    )
+  end
+
+  it "calls Permissions.retrieve after initialization" do
+    expect(Permissions).to receive(:retrieve).and_call_original
+
+    Rails.application.config.after_initialize { Permissions.retrieve.freeze }
+  end
+
+  it "freezes the retrieved permissions" do
+    allow(Permissions).to receive(:retrieve).and_return({ some_permission: "value" })
+
+    Rails.application.config.after_initialize { Permissions.retrieve.freeze }
+
+    expect(Permissions.retrieve).to be_frozen
+  end
+end
