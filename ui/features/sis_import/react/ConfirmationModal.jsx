ad5edfbd0e7930cd5d5307e9577cff1b62166125
davidwenzlick <david.wenzlick@instructure.com>
2024-08-20 15:57:57 -0400

    Add visual cues to dangerous SIS import actions
    closes OUT-6541
    flag=none
    
    Test Plan:
    - Create a new term so you do not clobber any of your existing data
    - Navigate to the sis import page for the account
    - Follow the "please see this documentation link" to generate a csv
      for courses. Use the sis id for the term you created above.
    - Verify that there is an inst ui alert warning under the "This is a
      full batch update" option.
    - Select the batch update and then the term you created above.
    - Click process data
    - Verify that a modal appears warning you about batch updates.
    - Verify if you click the X or outside the modal, the form is not
      subitted.
    - Verify if you click confim, the SIS import works as expected
    - Verify that the modal does not appear if you are not doing a
      batch update (do a sis import with out the batch update box
      checked)
    
    Change-Id: I73109b417e70a0dcd98a593b03025dd119ca753e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/355463
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

---
+++ ui/features/sis_import/react/ConfirmationModal.jsx
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Modal } from '@instructure/ui-modal'
+import { Button } from '@instructure/ui-buttons'
+import { Heading } from '@instructure/ui-heading'
+import { CloseButton } from '@instructure/ui-buttons'
+import { Alert } from '@instructure/ui-alerts'
+import {useScope as useI18nScope} from '@canvas/i18n'
+
+const I18n = useI18nScope('SIS_Import')
+
+export function BatchImportAlert({margin}) {
+  return (
+    <Alert
+      variant="warning"
+      margin={margin}
+    >
+      <p>
+        {
+          I18n.t(
+            `If selected, this will delete everything for this term, which includes all courses and enrollments
+                that are not in the selected import file above. See the documentation for details.`
+          )
+        }
+      </p>
+    </Alert>
+  )
+}
+
+export function ConfirmationModal({ isOpen, onSubmit, onRequestClose }) {
+  return (
+    <Modal
+      as="form"
+      open={isOpen}
+      onDismiss={onRequestClose}
+      size="small"
+      label={I18n.t('Confirm SIS Import Changes')}
+      shouldCloseOnDocumentClick
+    >
+      <Modal.Header>
+        <CloseButton
+          placement="end"
+          offset="small"
+          onClick={onRequestClose}
+          screenReaderLabel={I18n.t('Close')}
+        />
+        <Heading>{I18n.t('Confirm Changes')}</Heading>
+      </Modal.Header>
+      <Modal.Body>
+        <BatchImportAlert margin='small' />
+        <div>
+          {I18n.t('Please confirm you want to move forward with these changes.')}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button id="confirmation_modal_cancel" onClick={onRequestClose} margin="0 x-small 0 0">
+          {I18n.t('Cancel')}
+        </Button>
+        <Button id="confirmation_modal_confirm" color="primary" onClick={onSubmit}>{I18n.t('Confirm')}</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export function renderBatchImportAlert(margin) {
+  ReactDOM.render(
+    <BatchImportAlert margin={margin}/>,
+    document.getElementById('batch_import_instructions')
+  );
+}
+
+export function openModal(onSubmit, onRequestClose) {
+  ReactDOM.render(
+    <ConfirmationModal
+      isOpen={true}
+      onSubmit={onSubmit}
+      onRequestClose={onRequestClose}
+    />,
+    document.getElementById('confirmation_modal_root')
+  );
+}
