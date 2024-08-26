a89b8c2fa1de73c3afa0dd1e6d6838fc31d55439
Matheus <luis.oliveira@instructure.com>
2024-08-26 14:14:02 -0300

    Adds track permissions to attachments
    fixes RCX-2071
    flag=none
    
    Test plan:
    - Create a Canvas course with a video and course copy it
    - Act as a TA or designer in the resulting course and navigate
      to the files tab and select the video.
    - Select the “CC” option at the bottom right. Select a language,
      a subtitle file from your computer and upload it.
    - Check the captions were submitted properly.
    
    Change-Id: I670c2f1f90f339fd888b1ab10c57d1a233d97630
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/355984
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Luis Oliveira <luis.oliveira@instructure.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1532,6 +1532,44 @@ class Attachment < ActiveRecord::Base
         Account.site_admin.feature_enabled?(:differentiated_files)
     end
     can :manage_assign_to
+
+    #################### Begin legacy permission block #########################
+    given do |user|
+      !root_account.feature_enabled?(:granular_permissions_manage_course_content) &&
+        ((self.user && self.user == user) || context&.grants_right?(user, :manage_content))
+    end
+    can :add_captions and can :delete_captions
+
+    given do |user|
+      !root_account.feature_enabled?(:granular_permissions_manage_course_content) &&
+        Account.site_admin.feature_enabled?(:media_links_use_attachment_id) && grants_right?(user, :update)
+    end
+    can :add_captions and can :delete_captions
+    ##################### End legacy permission block ##########################
+
+    given do |user|
+      root_account.feature_enabled?(:granular_permissions_manage_course_content) && grants_right?(user, :update)
+    end
+    can :add_captions
+
+    given do |user|
+      root_account.feature_enabled?(:granular_permissions_manage_course_content) && grants_right?(user, :update)
+    end
+    can :delete_captions
+  end
+
+  def context_root_account(user = nil)
+    # Granular Permissions
+    #
+    # The primary use case for this method is for accurately checking
+    # feature flag enablement, given a user and the calling context.
+    # We want to prefer finding the root_account through the context
+    # of the authorizing resource or fallback to the user's active
+    # pseudonym's residing account.
+    return context.account if context.is_a?(User)
+
+    # return nil and don't raise if receiver doesn't respond to :root_account
+    context.try(:root_account) || user&.account
   end
 
   def clear_permissions(run_at)
