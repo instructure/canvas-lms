3cb2bc8c51747192a8e91d6c570a4d3d98956380
Cameron Ray <cameron.ray@instructure.com>
2024-08-26 12:48:33 -0400

    Add submission comment bank mutations
    flag=platform_service_speedgrader
    refs EVAL-3866
    
    Test plan
    - Tests pass
    
    Change-Id: I1f195d5462c39ac60e63f915c40ef461347c417e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/355978
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/features/speed_grader/mutations/comment_bank/createCommentBankItemMutation.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {z} from 'zod'
+import {executeQuery} from '@canvas/query/graphql'
+import gql from 'graphql-tag'
+
+export const CREATE_COMMENT_MUTATION = gql`
+  mutation CreateCommentBankItem($courseId: ID!, $comment: String!) {
+    createCommentBankItem(input: {courseId: $courseId, comment: $comment}) {
+      commentBankItem {
+        _id
+        comment
+      }
+      errors {
+        attribute
+        message
+      }
+    }
+  }
+`
+
+export const ZCreateSubmissionCommentParams = z.object({
+  courseId: z.string(),
+  comment: z.string(),
+})
+
+type CreateSubmissionCommentParams = z.infer<typeof ZCreateSubmissionCommentParams>
+
+export async function createCommentBankItem({
+  courseId,
+  comment,
+}: CreateSubmissionCommentParams): Promise<any> {
+  const result = executeQuery<any>(CREATE_COMMENT_MUTATION, {
+    courseId,
+    comment,
+  })
+
+  return result
+}
---
+++ ui/features/speed_grader/mutations/comment_bank/deleteCommentBankItemMutation.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {z} from 'zod'
+import {executeQuery} from '@canvas/query/graphql'
+import gql from 'graphql-tag'
+
+export const DELETE_COMMENT_MUTATION = gql`
+  mutation DeleteCommentBankItem($id: ID!) {
+    deleteCommentBankItem(input: {id: $id}) {
+      commentBankItemId
+      errors {
+        attribute
+        message
+      }
+    }
+  }
+`
+
+export const ZDeleteSubmissionCommentParams = z.object({
+  id: z.string(),
+})
+
+type DeleteSubmissionCommentParams = z.infer<typeof ZDeleteSubmissionCommentParams>
+
+export async function deleteCommentBankItem({id}: DeleteSubmissionCommentParams): Promise<any> {
+  const result = executeQuery<any>(DELETE_COMMENT_MUTATION, {
+    id,
+  })
+
+  return result
+}
