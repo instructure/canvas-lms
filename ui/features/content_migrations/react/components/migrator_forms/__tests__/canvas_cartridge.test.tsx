a29b4fdcab6211f7bc7fad5e99a70a55a387c84d
tbarna <tamas.barna@instructure.com>
2024-08-28 15:27:05 +0200

    Use FileDrop for File input
    refs CLAB-87
    
    flag=instui_for_import_page
    
    test plan:
     - Move to Site admin settings page and under the
       feature options tab enable the
       "Use InstUI for the course import page" feature
     - Navigate to Import content page and select
       Canvas course export package,
       Unzip .zip file into folder,
       Common Cartridge 1.x Package,
       Moodle 1.9/2.x or
       QTI .zip file
     - You should see the upload area with this text:
       "Choose File Drag and drop or browse your files"
     - After you click it you could only select the allowed
       file types
     - When you select/drop a correct file you can see the file
       name under the area
     - When you drop a not allowed file type you see the
       "Invalid file type" under the area
    
    Change-Id: I1e269f689ab672679178205c5c6505af0655fbdd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/356130
    Reviewed-by: Dániel Török <daniel.torok@instructure.com>
    QA-Review: Dániel Török <daniel.torok@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Kata Virag Keri <kata.keri@instructure.com>

--- ui/features/content_migrations/react/components/migrator_forms/__tests__/canvas_cartridge.test.tsx
+++ ui/features/content_migrations/react/components/migrator_forms/__tests__/canvas_cartridge.test.tsx
@@ -66,8 +66,7 @@ describe('CanvasCartridgeImporter', () => {
   it('disable inputs while uploading', async () => {
     renderComponent({isSubmitting: true})
     await waitFor(() => {
-      expect(screen.getByRole('button', {name: 'Choose File'})).toBeInTheDocument()
-      expect(screen.getByRole('button', {name: 'Choose File'})).toBeDisabled()
+      expect(screen.getByTestId('migrationFileUpload')).toBeDisabled()
       expect(screen.getByRole('button', {name: 'Cancel'})).toBeDisabled()
       expect(screen.getByRole('button', {name: /Adding.../})).toBeDisabled()
       expect(screen.getByRole('radio', {name: /All content/})).toBeDisabled()
