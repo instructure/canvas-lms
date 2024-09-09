746622a211b291f5b387d4f83c53ba71c36d4c50
Spencer Last <slast@instructure.com>
2024-09-09 12:06:13 -0700

    Fix Limited Access Account's File Assignment Submission
    Test Plan:
    - Test in Canvas instance with InstFS enabled.
    - Enable "Limited "Allow Limited Access for Students" feature flag
    - Enable "Enable Limited Access for Students for this account" setting
      on an account.
    - In course within that account:
      - Create assignment with file upload type
      - As student, make a submission to that assignment. The file upload
        and submission process should both work.
    - With same student, make submission to similar assignment in course
      outside the account with limited access. That should work.
    - Should not be able to access any uploaded files via API as this student.
    
    flag=allow_limited_access_for_students
    refs: PFS-23930
    
    Change-Id: I83c87897c6613ca361eb90f87f5cc2d9f4805fe3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/357024
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Chrystal Langston <chrystal.langston@instructure.com>
    QA-Review: Chrystal Langston <chrystal.langston@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -171,7 +171,14 @@ class FilesController < ApplicationController
     api_show api_create_success api_file_status api_update destroy icon_metadata reset_verifier
   ]
   before_action :check_limited_access_contexts, only: %i[index]
-  before_action :check_limited_access_for_students, only: %i[show api_index api_show]
+  before_action :check_limited_access_for_students, only: %i[show api_index]
+  before_action :forbid_api_calls_for_limited_access_students, only: :api_show
+
+  def forbid_api_calls_for_limited_access_students
+    if @current_user&.student_in_limited_access_account? && request.referer.nil?
+      render_unauthorized_action
+    end
+  end
 
   include Api::V1::Attachment
   include Api::V1::Avatar
