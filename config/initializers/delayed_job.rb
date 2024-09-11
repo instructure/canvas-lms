938f67049b404432cc00f647c1bcf04ecb828d78
Mark Ng <mark@markng.co.uk>
2024-09-11 16:51:40 -0700

    BEL-3136 include the client to fix errors.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
+require "aws-sdk-cloudwatch"
 
 Delayed::Job.include(JobLiveEventsContext)
 
