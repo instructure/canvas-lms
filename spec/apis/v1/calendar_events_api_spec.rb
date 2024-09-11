3d162b493fa14ead8c66c41d44aa8e253cab2835
Jason Gillett <jason.gillett@instructure.com>
2024-09-11 14:55:26 -0600

    Update checkpoint creator services to use new parent_override
    closes EGG-79
    flag=discussion_checkpoint
    
    Test plan
    1. Create a checkpointed discussion
    2. Check that the assignment_override on the
    sub_assignment has a parent_override relationship
    2a that parent_override should belong to the parent_assignment
    2b. should have the same set, unlock_at, and lock_at times
    
    Change-Id: I4ad6ad92ee8aedad24ce87a0a7fc7533cfae8394
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/357253
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Caleb Guanzon <cguanzon@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>

--- spec/apis/v1/calendar_events_api_spec.rb
+++ spec/apis/v1/calendar_events_api_spec.rb
@@ -4586,9 +4586,8 @@ describe CalendarEventsApiController, type: :request do
         expect(@checkpoint_1.all_day).to be_falsey
         expect(@checkpoint_1.all_day_date).to eq DateTime.parse("2024-08-03 05:15:00").to_date
 
-        assignment_override_model(assignment: @checkpoint_1,
-                                  set: @course.default_section,
-                                  due_at: DateTime.parse("2024-08-05 23:59:00"))
+        @override = create_section_override_for_assignment(@checkpoint_1, due_at: DateTime.parse("2024-08-05 23:59:00"))
+
         expect(@override.all_day).to be_truthy
         expect(@override.all_day_date).to eq DateTime.parse("2024-08-05 23:59:00").to_date
 
@@ -4614,10 +4613,8 @@ describe CalendarEventsApiController, type: :request do
         @checkpoint_1.save!
         expect(@checkpoint_1.all_day).to be_truthy
         expect(@checkpoint_1.all_day_date).to eq DateTime.parse("2024-08-03 23:59:00").to_date
+        @override = create_section_override_for_assignment(@checkpoint_1, due_at: DateTime.parse("2024-08-05 05:15:00"))
 
-        assignment_override_model(assignment: @checkpoint_1,
-                                  set: @course.default_section,
-                                  due_at: DateTime.parse("2024-08-05 05:15:00"))
         expect(@override.all_day).to be_falsey
         expect(@override.all_day_date).to eq DateTime.parse("2024-08-05 05:15:00").to_date
 
