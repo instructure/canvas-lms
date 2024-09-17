b39ce44e56caec98614fd8ac8fc9d5809178145e
Michael Hulse <michael.hulse@instructure.com>
2024-09-17 15:19:26 -0700

    fix test/build for g/357587
    [skip-crystalball]
    
    Change-Id: Ia4e898f98122abdb7e4a828e29890a27dab48619
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/357710
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/views/communication_channels/confirm.html.erb_spec.rb
+++ spec/views/communication_channels/confirm.html.erb_spec.rb
@@ -169,7 +169,7 @@ describe "communication_channels/confirm" do
       expect(transfer_button["href"]).to eq registration_confirmation_path(@communication_channel.confirmation_code, enrollment: @enrollment.uuid, transfer_enrollment: 1)
       login_button = page.css("#login.btn").first
       expect(login_button).not_to be_nil
-      expect(login_button["href"]).to eq login_url(enrollment: @enrollment.uuid, pseudonym_session: { unique_id: "jt@instructure.com" }, expected_user_id: @pseudonym1.user_id, host: "test.host")
+      expect(login_button["href"]).to eq login_url(enrollment: @enrollment.uuid, expected_user_id: @pseudonym1.user_id, login_hint: "jt@instructure.com")
     end
 
     context "open registration" do
