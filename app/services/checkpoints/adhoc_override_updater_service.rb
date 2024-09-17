e6b24500708e9588b969e2b93a2c935228d28bff
Omar Gerardo Soto-Fortuño <omar.soto@instructure.com>
2024-09-17 18:41:19 -0400

    Update parent assignment override behavior
    closes EGG-72
    flag=discussion_checkpoints
    
    Test plan:
     - Test passes
    
    Change-Id: I40c3f556998ba6207d17122b0659028cee53e790
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/357713
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Dave Wenzlick <david.wenzlick@instructure.com>
    QA-Review: Dave Wenzlick <david.wenzlick@instructure.com>
    Product-Review: Omar Soto-Fortuño <omar.soto@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>

--- app/services/checkpoints/adhoc_override_updater_service.rb
+++ app/services/checkpoints/adhoc_override_updater_service.rb
@@ -29,21 +29,27 @@ class Checkpoints::AdhocOverrideUpdaterService < Checkpoints::AdhocOverrideCommo
     existing_student_ids = override.assignment_override_students.pluck(:user_id)
     student_ids_to_delete = existing_student_ids - valid_student_ids
 
-    apply_overridden_dates(override, @override, shell_override: false)
+    parent_override = override.parent_override
 
-    override.title = get_title(student_ids: valid_student_ids)
-    build_override_students(override:, student_ids: valid_student_ids)
-    override.save! if override.changed? || override.changed_student_ids.any?
+    destroy_students(override:, student_ids: student_ids_to_delete)
+    destroy_students(override: parent_override, student_ids: student_ids_to_delete)
 
-    override.assignment_override_students.where(user_id: student_ids_to_delete).destroy_all if student_ids_to_delete.any?
-
-    parent_override = existing_parent_adhoc_override
-    parent_override.title = get_title(student_ids: valid_student_ids)
-    build_override_students(override: parent_override, student_ids: valid_student_ids)
-    parent_override.save! if parent_override.changed? || parent_override.changed_student_ids.any?
-
-    parent_override.assignment_override_students.where(user_id: student_ids_to_delete).destroy_all if student_ids_to_delete.any?
+    update_override(override:, student_ids: valid_student_ids)
+    update_override(override: parent_override, student_ids: valid_student_ids, shell_override: true)
 
     override
+  end
+
+  def destroy_students(override:, student_ids:)
+    return if student_ids.blank?
+
+    override.assignment_override_students.where(user_id: student_ids).destroy_all
+  end
+
+  def update_override(override:, student_ids:, shell_override: false)
+    override.title = get_title(student_ids:)
+    build_override_students(override:, student_ids:)
+    apply_overridden_dates(override, @override, shell_override:)
+    override.save! if override.changed? || override.changed_student_ids.any?
   end
 end
