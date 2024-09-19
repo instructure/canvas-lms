cfd5cb2a408737f9886515fecafce24ef650e2b2
Angela Gomba <angela.gomba@instructure.com>
2024-09-19 09:22:28 -0400

    Strip html tags from Submission Comments in Inbox
    closes EGG-87
    flag=rce_lite_enabled_speedgrader_comments
    
    Test Plan:
    - In a course with at least one student, create an Assignment
    - In SpeedGrader, use the RCE Lite to add a submission comment
      for the student and use different formatting
    - Act as a Student and go to the Inbox
    - Use the dropdown and switch from Inbox to Submission Comments
    - Observe the text preview in the left side does not render
      the submission comment with html tags
    - Open the message with the submission comment
    - Observe the submission comment is properly formatted
    
    Change-Id: I8d96cc55ce62019a7705ad848d6602340096e759
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/357499
    Reviewed-by: Omar Soto-Fortuño <omar.soto@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Martin Yosifov <martin.yosifov@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

--- ui/features/inbox/react/components/ConversationListHolder/ConversationListItem.jsx
+++ ui/features/inbox/react/components/ConversationListHolder/ConversationListItem.jsx
@@ -38,6 +38,7 @@ import {useScope as useI18nScope} from '@canvas/i18n'
 import {colors} from '@instructure/canvas-theme'
 import {Tooltip} from '@instructure/ui-tooltip'
 import {Heading} from '@instructure/ui-heading'
+import {stripHtmlTags} from '@canvas/util/TextHelper'
 
 const I18n = useI18nScope('conversations_2')
 
@@ -237,7 +238,7 @@ export const ConversationListItem = ({...props}) => {
                 </Grid.Col>
                 <Grid.Col>
                   <Text color="secondary" size={props.textSize} data-testid="last-message-content">
-                    {truncateText(props.conversation.lastMessageContent)}
+                    {truncateText(stripHtmlTags(props.conversation.lastMessageContent))}
                   </Text>
                 </Grid.Col>
                 <Grid.Col width="auto">
--- ui/features/inbox/react/components/MessageDetailItem/__tests__/MessageDetailItem.test.jsx
+++ ui/features/inbox/react/components/MessageDetailItem/__tests__/MessageDetailItem.test.jsx
@@ -97,7 +97,7 @@ describe('MessageDetailItem', () => {
     expect(getByText('Apr 20, 2021 at 2:31pm')).toBeInTheDocument()
   })
 
-  it('renders with an xss attempt', () => {
+  it('renders and xss attempt gets sanitized', () => {
     const props = {
       conversationMessage: {
         author: {name: 'Tom Thompson', shortName: 'Tom Thompson'},
@@ -106,16 +106,66 @@ describe('MessageDetailItem', () => {
           {name: 'Billy Harris', shortName: 'Billy Harris'},
         ],
         createdAt: 'Tue, 20 Apr 2021 14:31:25 UTC +00:00',
-        body: "<script>alert('XSS')</script>",
+        body: "<p>Attempting to attack!<script>alert('XSS')</script></p>",
       },
       contextName: 'Fake Course 1',
     }
 
-    const {getByText} = render(<MessageDetailItem {...props} />)
+    const {container, getByText} = render(<MessageDetailItem {...props} />)
 
     expect(getByText('Tom Thompson')).toBeInTheDocument()
     expect(getByText(', Billy Harris')).toBeInTheDocument()
-    expect(getByText("<script>alert('XSS')</script>")).toBeInTheDocument()
+    expect(container.querySelector('script')).not.toBeInTheDocument()
+    expect(getByText('Attempting to attack!')).toBeInTheDocument()
+    expect(getByText('Fake Course 1')).toBeInTheDocument()
+    expect(getByText('Apr 20, 2021 at 2:31pm')).toBeInTheDocument()
+  })
+
+  it('renders and does not display html tags', () => {
+    const props = {
+      conversationMessage: {
+        author: {name: 'Tom Thompson', shortName: 'Tom Thompson'},
+        recipients: [
+          {name: 'Tom Thompson', shortName: 'Tom Thompson'},
+          {name: 'Billy Harris', shortName: 'Billy Harris'},
+        ],
+        createdAt: 'Tue, 20 Apr 2021 14:31:25 UTC +00:00',
+        body: '<p>Formatted text</p>',
+      },
+      contextName: 'Fake Course 1',
+    }
+
+    const {queryByText, getByText} = render(<MessageDetailItem {...props} />)
+
+    expect(getByText('Tom Thompson')).toBeInTheDocument()
+    expect(getByText(', Billy Harris')).toBeInTheDocument()
+    expect(queryByText('<p>Formatted text</p>')).not.toBeInTheDocument()
+    expect(getByText('Formatted text')).toBeInTheDocument()
+    expect(getByText('Fake Course 1')).toBeInTheDocument()
+    expect(getByText('Apr 20, 2021 at 2:31pm')).toBeInTheDocument()
+  })
+
+  it('renders and preserves new lines', () => {
+    const props = {
+      conversationMessage: {
+        author: {name: 'Tom Thompson', shortName: 'Tom Thompson'},
+        recipients: [
+          {name: 'Tom Thompson', shortName: 'Tom Thompson'},
+          {name: 'Billy Harris', shortName: 'Billy Harris'},
+        ],
+        createdAt: 'Tue, 20 Apr 2021 14:31:25 UTC +00:00',
+        body: 'Text\nOn a new line',
+      },
+      contextName: 'Fake Course 1',
+    }
+
+    const {container, getByText} = render(<MessageDetailItem {...props} />)
+
+    expect(getByText('Tom Thompson')).toBeInTheDocument()
+    expect(getByText(', Billy Harris')).toBeInTheDocument()
+    // replaces \n with <br>
+    expect(container.querySelector('br')).toBeInTheDocument()
+    expect(getByText(/Text\s*On a new line/)).toBeInTheDocument()
     expect(getByText('Fake Course 1')).toBeInTheDocument()
     expect(getByText('Apr 20, 2021 at 2:31pm')).toBeInTheDocument()
   })
