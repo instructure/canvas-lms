984a364cda16945dd2d80763d262cda1174616ba
Cody Cutrer <cody@instructure.com>
2024-10-10 08:44:53 -0600

    expand list of allowed otpions for AcademicBenchmark imports
    fixes OUT-6918
    
    Change-Id: Ica0294ec6ceb8a7b80bb2e948dbf6414e635e7e8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/359734
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- gems/plugins/academic_benchmark/lib/academic_benchmark/outcome_data/base.rb
+++ gems/plugins/academic_benchmark/lib/academic_benchmark/outcome_data/base.rb
@@ -19,23 +19,82 @@
 
 module AcademicBenchmark::OutcomeData
   class Base
-    Options = Struct.new(:authority,
-                         :publication,
-                         :partner_id,
-                         :partner_key,
-                         :converter_class,
-                         :document,
-                         :import_immediately,
-                         :migration_type,
-                         :archive_file,
-                         :no_archive_file,
-                         :skip_import_notification,
-                         :skip_job_progress,
-                         :content_migration,
-                         :content_migration_id,
-                         :user_id,
-                         :migration_options,
-                         keyword_init: true)
+    # This used to be an OpenStruct
+    # Many of these options are likely not relevant to AcademicBenchmark,
+    # but were added to this list from a review of the surrounding codebase,
+    # and determining what things might possibly get added to the settings
+    # hash passed to this point, including from the AcademicBenchmark plugin's
+    # settings, ContentMigration controller (both keys referenced in code, and
+    # documented API parameters), methods in ContentMigration, and more.
+    # Ideally this struct would only contain options relevant to AcademicBenchmark,
+    # and the initializer would slice the incoming hash to those options, but
+    # given the apparent lack of specs in this area of the code, it seems safer
+    # to just try and allow more possible options, instead of trying to find
+    # everything that might be used.
+    Options = Struct.new(
+      :api_key,
+      :api_url,
+      :archive_file,
+      :attachment_id,
+      :attachment_path_id_lookup,
+      :authority,
+      :base_download_dir,
+      :common_core_guid,
+      :content_export_id,
+      :content_migration,
+      :content_migration_id,
+      :converter_class,
+      :copy_options,
+      :course_archive_download_url,
+      :date_shift_options,
+      :document,
+      :domain_substitution_map,
+      :export_archive_path,
+      :file_url,
+      :folder_id,
+      :id_prepender,
+      :import_blueprint_settings,
+      :import_immediately,
+      :import_in_progress_notice,
+      :import_quizzes_next,
+      :imported_assets,
+      :importer_skips,
+      :initiated_source,
+      :insert_into_module_id,
+      :insert_into_module_position,
+      :insert_into_module_type,
+      :job_ids,
+      :last_error,
+      :master_course_export_id,
+      :master_migration_id,
+      :migration_ids_to_import,
+      :migration_options,
+      :migration_type,
+      :move_to_assignment_group_id,
+      :no_archive_file,
+      :no_selective_import,
+      :overwrite_questions,
+      :overwrite_quizzes,
+      :partner_id,
+      :partner_key,
+      :prefer_existing_tools,
+      :provides,
+      :publication,
+      :question_bank_id,
+      :question_bank_name,
+      :required_options_validator,
+      :requires_file_upload,
+      :skip_import_notification,
+      :skip_job_progress,
+      :source_course_id,
+      :strand,
+      :unzipped_file_path,
+      :user_id,
+      :valid_contexts,
+      :worker,
+      :worker_class,
+      keyword_init: true
+    )
 
     def initialize(options = {})
       @options = Options.new(**options)
