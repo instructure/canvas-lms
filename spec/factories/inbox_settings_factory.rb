1d643ebf4aed48f5e82f7d043389b3f51faf941b
Daniel Vincze <daniel.vincze@instructure.com>
2024-10-17 16:55:09 +0200

    fix: send ooo message to all
    fixes VICE-4724
    flag=inbox_settings
    
    Test plan:
    - turn on feature flag
    - set users ooo settings
    - message user from at least 2 other users
    - each of them should receive ooo msg
    - re messaging user should not resend ooo msg
    - unless ooo settings changed
    
    Change-Id: I5e94f33f8b02fffd7979bfc55f4d02b4c4810272
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360324
    Reviewed-by: Ádám Molnár <adam.molnar@instructure.com>
    QA-Review: Ádám Molnár <adam.molnar@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Mario Hegyi <mario.hegyi@instructure.com>
    Product-Review: Tamás Balogh <tamas.balogh@instructure.com>

---
+++ spec/factories/inbox_settings_factory.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2011 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Factories
+  def inbox_settings_factory(opts = {})
+    Inbox::Repositories::InboxSettingsRepository.save_inbox_settings(
+      user_id: opts[:user_id] || @user&.id,
+      root_account_id: opts[:root_account_id] || Account.default.id,
+      use_signature: opts[:use_signature] || true,
+      signature: opts[:signature] || "Signature",
+      use_out_of_office: opts[:use_out_of_office] || true,
+      out_of_office_first_date: opts[:out_of_office_first_date] || Time.now,
+      out_of_office_last_date: opts[:out_of_office_last_date] || (Time.now + 1.week),
+      out_of_office_subject: opts[:out_of_office_subject] || "Out of office",
+      out_of_office_message: opts[:out_of_office_message] || "I'm out of office for a week"
+    )
+  end
+end
