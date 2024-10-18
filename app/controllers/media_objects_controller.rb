2508008927ca4ec680237a2c95c0f6a097408edc
Mysti Lilla <mysti@instructure.com>
2024-10-18 16:40:30 -0600

    Fix media redirect blocking access
    fixes RCX-2556
    flag=rce_linked_file_urls
    
    Test plan
    - Ensure the authenticated_iframe_content
      feature flag shows is on
    - Set up a video in a new quiz question
    - Copy the quiz question to an item bank
    - In a different course, add that item
      into a new quiz
    - As a person who only has access to
      the second course, ensure they can
      access the video file
    
    Change-Id: Ieb1dbee6d2ec52a6f6387a6e84011ec0b0867a84
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360475
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jorge Andres <jorge.andres@instructure.com>
    Product-Review: Jorge Andres <jorge.andres@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    QA-Review: Griffin Zody <griffin.zody@instructure.com>

--- app/controllers/media_objects_controller.rb
+++ app/controllers/media_objects_controller.rb
@@ -84,7 +84,7 @@ class MediaObjectsController < ApplicationController
   protect_from_forgery only: %i[create_media_object media_object_redirect media_object_inline media_object_thumbnail], with: :exception
 
   def services_jwt_auth_allowed
-    params[:action] == "iframe_media_player" && Account.site_admin.feature_enabled?(:rce_linked_file_urls)
+    %w[media_object_redirect iframe_media_player].include?(params[:action]) && Account.site_admin.feature_enabled?(:rce_linked_file_urls)
   end
 
   # @{not an}API Show Media Object Details
