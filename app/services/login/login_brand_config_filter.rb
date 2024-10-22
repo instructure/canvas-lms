65af4b2e3b495b17c6dc6e6f615503bb6b7b33d1
Michael Hulse <michael.hulse@instructure.com>
2024-10-22 09:59:24 -0700

    hide unused theme options for login page
    closes AE-1296
    flag=login_registration_ui_identity
    
    [skip-crystalball]
    
    test plan:
     1. login to Canvas as an admin
     2. visit /accounts/<id>/brand_configs
     3. open a theme in the theme editor
     4. under the “Edit” tab in the sidebar, expect these form
        controls to show in the “Login Screen” section:
    
    Background Color (ic-brand-Login-body-bgd-color)
    Background Image (ic-brand-Login-body-bgd-image)
    Body Shadow (ic-brand-Login-body-bgd-shadow-color)
    Login Logo (ic-brand-Login-logo)
    Top Box Background (ic-brand-Login-Content-bgd-color)
    Top Box Border (ic-brand-Login-Content-border-color)
    Inner Box Background (ic-brand-Login-Content-inner-bgd)
    Inner Box Border (ic-brand-Login-Content-inner-border)
    Form Background (ic-brand-Login-Content-inner-body-bgd)
    Form Border (ic-brand-Login-Content-inner-body-border)
    Login Label (ic-brand-Login-Content-label-text-color)
    Login Link Color (ic-brand-Login-Content-password-text-color)
    Login Footer Link (ic-brand-Login-footer-link-color)
    Login Footer Link Hover (ic-brand-Login-footer-link-color-hover)
    Login Instructure Logo (ic-brand-Login-instructure-logo)
    
     5. click the “Exit” button
     6. navigate to
        /accounts/<account_id>/settings#tab-features
     7. enable “New Login/Registration UI for Identity”
     8. visit /accounts/<id>/brand_configs
     9. open a theme in the theme editor
    10. under the “Edit” tab in the sidebar, expect these form
        controls to show in the “Login Screen” section:
    
    Login Logo (ic-brand-Login-logo)
    Background Image (ic-brand-Login-body-bgd-image)
    Background Color (ic-brand-Login-body-bgd-color)
    
    11. expect/ensure other brand config paths, such as API
        responses or data access outside the theme editor,
        remain unaffected
    
    Change-Id: I8257ad65ccdc5fcceb50d3539caff8c97fbe9ac7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360786
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ app/services/login/login_brand_config_filter.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2023 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# this filter is temporary and should be removed once the features/new_login UI
+# is fully adopted and old/unused login brand config variables are no longer needed
+class Login::LoginBrandConfigFilter
+  ALLOWED_LOGIN_VARS = %w[
+    ic-brand-Login-logo
+    ic-brand-Login-body-bgd-image
+    ic-brand-Login-body-bgd-color
+  ].freeze
+
+  def self.filter(variable_schema)
+    variable_schema.each do |group|
+      next unless group["group_key"] == "login"
+
+      group["variables"].reject! do |variable|
+        variable_name = variable["variable_name"]
+        variable_name.start_with?("ic-brand-Login") && !ALLOWED_LOGIN_VARS.include?(variable_name)
+      end
+    end
+
+    variable_schema
+  end
+end
