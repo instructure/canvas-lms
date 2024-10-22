7acc7ee329a1d5a20d7cea378856581f6429e6b8
Keith T. Garner <kgarner@instructure.com>
2024-10-22 13:15:59 -0500

    add caching to heavy queries for selective release
    This adds caching to queries that are commonly used to determine
    visibilty, somtimes multiple times in the same call. For now, we'll
    cache up to a minute to give the database some breathing room.
    
    This also removes unused functions from the visibility services and
    replaces a function with one provided by rails.
    
    flag=none
    
    test plan:
      - specs pass
      - determine if the queries are being cached by looking at the logs
    
    Change-Id: I30cc97ea614a6fc9578582dc3d97be45e8427365
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360794
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Keith Garner <kgarner@instructure.com>

--- app/services/ungraded_discussion_visibility/ungraded_discussion_visibility_service.rb
+++ app/services/ungraded_discussion_visibility/ungraded_discussion_visibility_service.rb
@@ -19,6 +19,7 @@
 
 module UngradedDiscussionVisibility
   class UngradedDiscussionVisibilityService
+    extend VisibilityHelpers::Common
     class << self
       def discussion_topics_visible_to_student_in_course(course_id:, user_id:)
         raise ArgumentError, "course_id cannot be nil" if course_id.nil?
@@ -72,35 +73,6 @@ module UngradedDiscussionVisibility
         discussion_topics_visible(discussion_topic_id_params: discussion_topic_id, user_id_params: user_ids)
       end
 
-      def discussion_topics_visible_to_students_by_topics(discussion_topic_ids:, user_ids:)
-        raise ArgumentError, "discussion_topic_ids cannot be nil" if discussion_topic_ids.nil?
-        raise ArgumentError, "discussion_topic_ids must be an array" unless discussion_topic_ids.is_a?(Array)
-        raise ArgumentError, "user_ids cannot be nil" if user_ids.nil?
-        raise ArgumentError, "user_ids must be an array" unless user_ids.is_a?(Array)
-
-        discussion_topics_visible(discussion_topic_id_params: discussion_topic_ids, user_id_params: user_ids)
-      end
-
-      def discussion_topic_visible_to_students_in_course(discussion_topic_id:, user_ids:, course_id:)
-        raise ArgumentError, "course_id cannot be nil" if course_id.nil?
-        raise ArgumentError, "course_id must not be an array" if course_id.is_a?(Array)
-        raise ArgumentError, "discussion_topic_id cannot be nil" if discussion_topic_id.nil?
-        raise ArgumentError, "discussion_topic_id must not be an array" if discussion_topic_id.is_a?(Array)
-        raise ArgumentError, "user_ids cannot be nil" if user_ids.nil?
-        raise ArgumentError, "user_ids must be an array" unless user_ids.is_a?(Array)
-
-        discussion_topics_visible(course_id_params: course_id, discussion_topic_id_params: discussion_topic_id, user_id_params: user_ids)
-      end
-
-      def discussion_topic_visible_in_course(discussion_topic_id:, course_id:)
-        raise ArgumentError, "course_id cannot be nil" if course_id.nil?
-        raise ArgumentError, "course_id must not be an array" if course_id.is_a?(Array)
-        raise ArgumentError, "discussion_topic_id cannot be nil" if discussion_topic_id.nil?
-        raise ArgumentError, "discussion_topic_id must not be an array" if discussion_topic_id.is_a?(Array)
-
-        discussion_topics_visible(course_id_params: course_id, discussion_topic_id_params: discussion_topic_id)
-      end
-
       private
 
       def discussion_topics_visible(course_id_params: nil, user_id_params: nil, discussion_topic_id_params: nil)
@@ -108,74 +80,74 @@ module UngradedDiscussionVisibility
           raise ArgumentError, "at least one non nil course_id, user_id, or discussion_topic_id_params is required (for query performance reasons)"
         end
 
-        if Account.site_admin.feature_enabled?(:selective_release_optimized_services_v3)
-          UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository.full_discussion_topic_visibility_query(course_id_params:, user_id_params:, discussion_topic_id_params:)
-        elsif Account.site_admin.feature_enabled?(:selective_release_optimized_services_v2)
-          visible_discussion_topics = []
+        service_cache_fetch(service: name,
+                            course_id_params:,
+                            user_id_params:,
+                            additional_id_params: discussion_topic_id_params) do
+          if Account.site_admin.feature_enabled?(:selective_release_optimized_services_v3)
+            UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository.full_discussion_topic_visibility_query(course_id_params:, user_id_params:, discussion_topic_id_params:)
+          elsif Account.site_admin.feature_enabled?(:selective_release_optimized_services_v2)
+            visible_discussion_topics = []
 
-          # add discussion topics visible to everyone
-          discussion_topics_visible_to_all = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                             .find_discussion_topics_visible_to_everyone(course_id_params:, user_id_params:, discussion_topic_id_params:)
-          visible_discussion_topics |= discussion_topics_visible_to_all
+            # add discussion topics visible to everyone
+            discussion_topics_visible_to_all = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                               .find_discussion_topics_visible_to_everyone(course_id_params:, user_id_params:, discussion_topic_id_params:)
+            visible_discussion_topics |= discussion_topics_visible_to_all
 
-          # add discussion topics assigned to sections, students, or the course
-          discussion_topics_assigned_to_others = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                 .find_discussion_topics_assigned_to_others(course_id_params:, user_id_params:, discussion_topic_id_params:)
+            # add discussion topics assigned to sections, students, or the course
+            discussion_topics_assigned_to_others = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                   .find_discussion_topics_assigned_to_others(course_id_params:, user_id_params:, discussion_topic_id_params:)
 
-          visible_discussion_topics | discussion_topics_assigned_to_others
-        else
-          visible_discussion_topics = []
-
-          # add discussion topics visible to everyone
-          discussion_topics_visible_to_all = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                             .find_discussion_topics_visible_to_everyone(course_id_params:, user_id_params:, discussion_topic_id_params:)
-          visible_discussion_topics |= discussion_topics_visible_to_all
-
-          if Account.site_admin.feature_enabled?(:selective_release_optimized_services)
-            # add discussion topics visible to sections (and related module section overrides) without unassign overrides
-            discussion_topics_assigned_to_sections = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                     .find_discussion_topics_assigned_to_sections(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics |= discussion_topics_assigned_to_sections
+            visible_discussion_topics | discussion_topics_assigned_to_others
           else
-            # add discussion topics visible to sections (and related module section overrides)
-            discussion_topics_visible_to_sections = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                    .find_discussion_topics_visible_to_sections(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics |= discussion_topics_visible_to_sections
+            visible_discussion_topics = []
 
-            # remove discussion topics for students with unassigned section overrides
-            discussion_topics_with_unassigned_section_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                                  .find_discussion_topics_with_unassigned_section_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics -= discussion_topics_with_unassigned_section_overrides
+            # add discussion topics visible to everyone
+            discussion_topics_visible_to_all = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                               .find_discussion_topics_visible_to_everyone(course_id_params:, user_id_params:, discussion_topic_id_params:)
+            visible_discussion_topics |= discussion_topics_visible_to_all
+
+            if Account.site_admin.feature_enabled?(:selective_release_optimized_services)
+              # add discussion topics visible to sections (and related module section overrides) without unassign overrides
+              discussion_topics_assigned_to_sections = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                       .find_discussion_topics_assigned_to_sections(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics |= discussion_topics_assigned_to_sections
+            else
+              # add discussion topics visible to sections (and related module section overrides)
+              discussion_topics_visible_to_sections = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                      .find_discussion_topics_visible_to_sections(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics |= discussion_topics_visible_to_sections
+
+              # remove discussion topics for students with unassigned section overrides
+              discussion_topics_with_unassigned_section_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                                    .find_discussion_topics_with_unassigned_section_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics -= discussion_topics_with_unassigned_section_overrides
+            end
+
+            if Account.site_admin.feature_enabled?(:selective_release_optimized_services)
+              # add discussion topics visible due to ADHOC overrides (and related module ADHOC overrides) without unassign overrides
+              discussion_topics_assigned_to_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                              .find_discussion_topics_assigned_to_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics |= discussion_topics_assigned_to_adhoc_overrides
+            else
+              # add discussion topics visible due to ADHOC overrides (and related module ADHOC overrides)
+              discussion_topics_visible_to_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                             .find_discussion_topics_visible_to_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics |= discussion_topics_visible_to_adhoc_overrides
+
+              # remove discussion topics for students with unassigned ADHOC overrides
+              discussion_topics_with_unassigned_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                                  .find_discussion_topics_with_unassigned_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
+              visible_discussion_topics -= discussion_topics_with_unassigned_adhoc_overrides
+            end
+
+            # add discussion topics visible due to course overrides
+            discussion_topics_visible_to_course_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
+                                                            .find_discussion_topics_visible_to_course_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
+
+            visible_discussion_topics | discussion_topics_visible_to_course_overrides
           end
-
-          if Account.site_admin.feature_enabled?(:selective_release_optimized_services)
-            # add discussion topics visible due to ADHOC overrides (and related module ADHOC overrides) without unassign overrides
-            discussion_topics_assigned_to_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                            .find_discussion_topics_assigned_to_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics |= discussion_topics_assigned_to_adhoc_overrides
-          else
-            # add discussion topics visible due to ADHOC overrides (and related module ADHOC overrides)
-            discussion_topics_visible_to_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                           .find_discussion_topics_visible_to_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics |= discussion_topics_visible_to_adhoc_overrides
-
-            # remove discussion topics for students with unassigned ADHOC overrides
-            discussion_topics_with_unassigned_adhoc_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                                .find_discussion_topics_with_unassigned_adhoc_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
-            visible_discussion_topics -= discussion_topics_with_unassigned_adhoc_overrides
-          end
-
-          # add discussion topics visible due to course overrides
-          discussion_topics_visible_to_course_overrides = UngradedDiscussionVisibility::Repositories::UngradedDiscussionVisibleToStudentRepository
-                                                          .find_discussion_topics_visible_to_course_overrides(course_id_params:, user_id_params:, discussion_topic_id_params:)
-
-          visible_discussion_topics | discussion_topics_visible_to_course_overrides
         end
-      end
-
-      def empty_id_hash(ids)
-        # [1,2,3] => {1:[],2:[],3:[]}
-        ids.zip(ids.map { [] }).to_h
       end
     end
   end
