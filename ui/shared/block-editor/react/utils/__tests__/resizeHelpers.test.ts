3258937059bb60ceb07392b485b5c7c4d5ced6d8
Ed Schiebel <eschiebel@instructure.com>
2024-10-23 13:31:06 -0600

    Fix sizing if ImageBlock
    closes RCX-2585
    flag=block_editor
    
    test plan:
      - create 2 columns
      - make one column tall (add a slew of icons?)
      - add an image to the other column
      - set size to percent and drag to make 100% tall
      > expect the image to fill the column
      - change among auto, pixel, and percent
      - resize the image
      > expect it to behave
      - look at the preview
      > expect it to behave
    
    Change-Id: I9107dc07aea4de83cdb9c2f8b2a04b6e00b5a684
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360874
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

---
+++ ui/shared/block-editor/react/utils/__tests__/resizeHelpers.test.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {changeSizeVariant, percentSize} from '../resizeHelpers'
+
+describe('resizeHelpers', () => {
+  describe('changeSizeVariant', () => {
+    it('returns the current size of the element if the new size variant is auto', () => {
+      const elem = {} as HTMLElement
+      elem.getBoundingClientRect = jest.fn().mockReturnValue({width: 100, height: 100})
+      expect(changeSizeVariant(elem, 'auto')).toEqual({width: 100, height: 100})
+    })
+
+    it('returns the current size of the element if the new size variant is pixel', () => {
+      const elem = {} as HTMLElement
+      elem.getBoundingClientRect = jest.fn().mockReturnValue({width: 100, height: 100})
+      expect(changeSizeVariant(elem, 'pixel')).toEqual({width: 100, height: 100})
+    })
+
+    it('returns the current size of the element as a percentage of the parent if the new size variant is percent', () => {
+      const parent = {
+        clientWidth: 200,
+        clientHeight: 200,
+      } as HTMLElement
+      const elem = {} as HTMLElement
+      // @ts-expect-error
+      elem.offsetParent = parent
+      elem.getBoundingClientRect = jest.fn().mockReturnValue({width: 100, height: 100})
+
+      expect(changeSizeVariant(elem, 'percent')).toEqual({width: 50, height: 50})
+    })
+  })
+
+  describe('percentSize', () => {
+    it('returns the percentage of the parent width and height that the element occupies', () => {
+      expect(percentSize(200, 200, 100, 100)).toEqual({width: 50, height: 50})
+    })
+
+    it('returns 100% if the element is within 7px of the parent width', () => {
+      expect(percentSize(200, 200, 193, 100)).toEqual({width: 100, height: 50})
+    })
+
+    it('returns 100% if the element is within 7px of the parent height', () => {
+      expect(percentSize(200, 200, 100, 193)).toEqual({width: 50, height: 100})
+    })
+  })
+})
