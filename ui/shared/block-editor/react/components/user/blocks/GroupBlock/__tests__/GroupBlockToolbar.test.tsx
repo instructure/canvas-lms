7021aed3f4586bf4b58bbdc0e6366fb92ba2c7dd
Ed Schiebel <eschiebel@instructure.com>
2024-10-24 14:42:22 -0600

    Update GroupBlock toolbar
    closes RCX-2590
    flag=block_editor
    
    test plan:
      - create a blank page
      - add an icon to the empty column
      - up up so the column is selected
      > expect a toolbar with a color icon and Alignment Options button
      - click the color button
      > expect a color popup with a Group Color tab and a
        Group Background Tab
      - change colors
      - click cancel
      > expect no colors to have been changed
      - open the color popup, change colors and click Apply
      > expect the icon color and the column background color
        to be what you selected
      - open the color popup
      > expect the current colors to be checked
    
      - click on the Alignment Options button
      > expect a popup menu
      > expect the defaults to be checked
        - Align Horizonally, Align to start, Align to top
      - change some stuff
      > expect a "Reset Default Alignment" button to appear
      - click it
      > expect the menu checks to reset
      - change alignments and click Apply
      > expect the column's content to be aligned accordingly
        - it helps to put some icons + a group with some more icons
          to see what's happening
      - open the menu
      > expect the current selections to be checked
      > expect "Reset Default Alignment" button to be there
    
    Change-Id: Ifc4c32295d00596093256fce69beb9ac2913dd58
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/360999
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Luis Oliveira <luis.oliveira@instructure.com>
    Reviewed-by: Luis Oliveira <luis.oliveira@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

--- ui/shared/block-editor/react/components/user/blocks/GroupBlock/__tests__/GroupBlockToolbar.test.tsx
+++ ui/shared/block-editor/react/components/user/blocks/GroupBlock/__tests__/GroupBlockToolbar.test.tsx
@@ -18,7 +18,6 @@
 
 import React from 'react'
 import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {useNode} from '@craftjs/core'
 import {GroupBlock, GroupBlockToolbar} from '..'
@@ -48,105 +47,7 @@ describe('GroupBlockToolbar', () => {
   it('should render', () => {
     const {getByText} = render(<GroupBlockToolbar />)
 
-    expect(getByText('Layout direction')).toBeInTheDocument()
-    expect(getByText('Align Horizontally')).toBeInTheDocument()
-    expect(getByText('Align Vertically')).toBeInTheDocument()
-  })
-
-  describe('layout direction', () => {
-    it('checks the right layout direction', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Layout direction').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const colMenuItem = screen.getByText('Column')
-      const rowMenuItem = screen.getByText('Row')
-
-      expect(colMenuItem).toBeInTheDocument()
-      expect(rowMenuItem).toBeInTheDocument()
-
-      const li = colMenuItem.closest('li') as HTMLLIElement
-      expect(li.querySelector('svg[name="IconCheck"]')).toBeInTheDocument()
-    })
-
-    it('changes the direction prop', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Layout direction').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const rowMenuItem = screen.getByText('Row')
-      await userEvent.click(rowMenuItem)
-
-      expect(mockSetProp).toHaveBeenCalled()
-      expect(props.layout).toBe('row')
-    })
-  })
-
-  describe('horizontal alignment', () => {
-    it('checks the right alignment', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Align Horizontally').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const startMenuItem = screen.getByText('Align to start')
-      const centerMenuItem = screen.getByText('Align to center')
-      const endMenuItem = screen.getByText('Align to end')
-
-      expect(startMenuItem).toBeInTheDocument()
-      expect(centerMenuItem).toBeInTheDocument()
-      expect(endMenuItem).toBeInTheDocument()
-
-      const startLi = startMenuItem.closest('li') as HTMLLIElement
-      expect(startLi.querySelector('svg[name="IconCheck"]')).toBeInTheDocument()
-    })
-
-    it('changes the alignment prop', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Align Horizontally').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const centerMenuItem = screen.getByText('Align to center')
-      await userEvent.click(centerMenuItem)
-
-      expect(mockSetProp).toHaveBeenCalled()
-      expect(props.alignment).toBe('center')
-    })
-  })
-
-  describe('vertical alignment', () => {
-    it('checks the right alignment', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Align Vertically').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const startMenuItem = screen.getByText('Align to start')
-      const centerMenuItem = screen.getByText('Align to center')
-      const endMenuItem = screen.getByText('Align to end')
-
-      expect(startMenuItem).toBeInTheDocument()
-      expect(centerMenuItem).toBeInTheDocument()
-      expect(endMenuItem).toBeInTheDocument()
-
-      const startLi = startMenuItem.closest('li') as HTMLLIElement
-      expect(startLi.querySelector('svg[name="IconCheck"]')).toBeInTheDocument()
-    })
-
-    it('changes the vertical alignment prop', async () => {
-      const {getByText} = render(<GroupBlockToolbar />)
-
-      const btn = getByText('Align Vertically').closest('button') as HTMLButtonElement
-      await userEvent.click(btn)
-
-      const centerMenuItem = screen.getByText('Align to center')
-      await userEvent.click(centerMenuItem)
-
-      expect(mockSetProp).toHaveBeenCalled()
-      expect(props.verticalAlignment).toBe('center')
-    })
+    expect(getByText('Color')).toBeInTheDocument()
+    expect(getByText('Alignment Options')).toBeInTheDocument()
   })
 })
