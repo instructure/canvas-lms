18be1db482778100de79dffe3b2139cf82b9e889
Luis Hilario <luis.hilario@instructure.com>
2024-10-28 16:55:47 -0600

    Add progressbar to show storage use in Files page
    closes RCX-2510
    flag=files-a11y-rewrite
    
    Test Plan:
    - Go to Site Admin Feature Flags;
    - Enable Files a11y Rewrite flag;
    - Go to Course Files, you should see the progress bar with the
      respective porcentage filled as well as a text with said value.
    
    Change-Id: I70bc05dba6aeebebde391221e1d01a624594a708
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/361264
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Luis Hilario <luis.hilario@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/files_v2/react/components/FilesApp.tsx
+++ ui/features/files_v2/react/components/FilesApp.tsx
@@ -17,8 +17,17 @@
  */
 
 import React from 'react'
+import {useQuery} from '@tanstack/react-query'
 import {Heading} from '@instructure/ui-heading'
 import {useScope as useI18nScope} from '@canvas/i18n'
+import {ProgressBar} from '@instructure/ui-progress'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+import {Flex} from '@instructure/ui-flex'
+import filesEnv from '@canvas/files/react/modules/filesEnv'
+import {showFlashError} from '@canvas/alerts/react/FlashAlert'
+import formatMessage from '../../../../../packages/canvas-media/src/format-message'
+import friendlyBytes from '@canvas/files/util/friendlyBytes'
 
 const I18n = useI18nScope('files_v2')
 
@@ -26,11 +35,61 @@ interface FilesAppProps {
   contextAssetString: string
 }
 
+const fetchQuota = async (contextType: string, contextId: string) => {
+  const response = await fetch(`/api/v1/${contextType}/${contextId}/files/quota`)
+  if (!response.ok) {
+    throw new Error('Failed to fetch quota data')
+  }
+  return response.json()
+}
+
 const FilesApp: React.FC<FilesAppProps> = ({contextAssetString}) => {
+  const contextType = filesEnv.contextType
+  const contextId = filesEnv.contextId
   const isUserContext = contextAssetString?.startsWith('user_')
+
+  const {data, error} = useQuery(['quota'], () => fetchQuota(contextType, contextId))
+
+  const renderFilesUsageValue = (percentage: number) => {
+    return (
+      <Text>
+        {I18n.t('%{percentUsed} of %{quota} used', {
+          percentUsed: I18n.n(percentage, {percentage: true}),
+          quota: friendlyBytes(data?.quota) || 0,
+        })}
+      </Text>
+    )
+  }
+
+  const renderFilesUsageBar = () => {
+    if (error) {
+      showFlashError(I18n.t('An error occurred while loading files usage data'))(error)
+    }
+
+    const {quota_used = 0, quota = 1} = data || {quota_used: 0, quota: 1}
+    const percentage = Math.round((quota_used / quota) * 100)
+
+    return (
+      <ProgressBar
+        meterColor="brand"
+        screenReaderLabel={formatMessage('File Storage Quota Used')}
+        formatScreenReaderValue={() => renderFilesUsageValue(percentage)}
+        renderValue={renderFilesUsageValue(percentage)}
+        size="x-small"
+        valueMax={quota}
+        valueNow={quota_used}
+      />
+    )
+  }
+
   return (
     <div>
       <Heading level="h1">{isUserContext ? I18n.t('All My Files') : I18n.t('Files')}</Heading>
+      <View as="div">
+        <Flex>
+          <Flex.Item size="50%">{renderFilesUsageBar()}</Flex.Item>
+        </Flex>
+      </View>
     </div>
   )
 }
