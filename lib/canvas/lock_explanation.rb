9bdcdab7b595ed7391612807770bbf49be593442
Mysti Lilla <mysti@instructure.com>
2024-10-29 16:39:10 -0600

    Remove dead jQuery
    Test plan
    - Specs pass
    
    Change-Id: I1b7be0d81f061436373531dac7d0955ec42db7ff
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/361359
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Mysti Lilla <mysti@instructure.com>

--- lib/canvas/lock_explanation.rb
+++ lib/canvas/lock_explanation.rb
@@ -23,7 +23,7 @@ module Canvas
     def lock_explanation(hash, type, context = nil, options = {})
       include_js = options.fetch(:include_js, true)
       url_options = options.key?(:only_path) ? { only_path: options[:only_path] } : {}
-      # Any additions to this function should also be made in javascripts/content_locks.js
+      # Any additions to this function should also be made in javascripts/lock_reason.js
       if hash[:lock_at]
         case type
         when "quiz"
