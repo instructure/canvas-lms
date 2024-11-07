4ed2e7af73b3c0a5c7a27c1048f0d5356e9aad3e
Jeremy Stanley <jeremy@instructure.com>
2024-11-07 10:41:11 -0700

    don't return expired global announcements in observer alerts
    test plan:
     - create some global announcements, some that will remain
       visible for awhile, and some that expire soon
     - have an observer linked to a student who can see the
       global announcements
     - use /api/v1/users/self/observer_alerts/<student_id> to
       retrieve observer alerts for the student
     - the global announcement should stop appearing after it
       expires
    
    flag=none
    closes FOO-4902
    
    Change-Id: I861a8c6c63b3750c6984df690e45e97ef9cde612
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362142
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/controllers/observer_alerts_api_controller.rb
+++ app/controllers/observer_alerts_api_controller.rb
@@ -31,11 +31,16 @@ class ObserverAlertsApiController < ApplicationController
                  .order(id: :desc)
                  .preload(:context)
 
-    # avoid n+1, all alerts are for the same student, we don't need to check each one.
-    all_alerts = [] unless all_alerts.first&.users_are_still_linked?
+    if all_alerts.first&.users_are_still_linked?
+      account_notification_ids = all_alerts.where(context_type: "AccountNotification").select(:context_id)
+      expired_account_notifications = AccountNotification.where(id: account_notification_ids).where(end_at: ...Time.zone.now).select(:id)
+      all_alerts = all_alerts.where.not(context_id: expired_account_notifications, context_type: "AccountNotification")
+    else
+      # avoid n+1, all alerts are for the same student, we don't need to check each one.
+      all_alerts = []
+    end
 
     alerts = Api.paginate(all_alerts, self, api_v1_observer_alerts_by_student_url)
-
     render json: alerts.map { |alert| observer_alert_json(alert, @current_user, session) }
   end
 
