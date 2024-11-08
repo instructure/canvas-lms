8881af7f1f75102820f1c49abd1b22dbb0eeaa28
Daniel Kis-Pal <daniel.kispal@instructure.com>
2024-11-08 13:25:44 +0100

    Optimize Student Competency Report generation
    The previous implementation caused PostgreSQL to use the GEQO optimizer
    due to an excessive number of joins, resulting in a suboptimal execution
    plan for the customer. By removing one join (accounts table) and adding
    the necessary information in Ruby, we can keep the number of joins below
    the geqo_threshold parameter (currently set to 12).
    
    Account information is straightforward to query as it uses an INNER JOIN
    and only adds two attributes (id, name). One of these attributes can be
    fetched from the courses table, while the other must be added manually.
    
    refs: OUT-6626
    flag=improved_outcomes_report_generation
    
    Test plan:
    - Before merging
    -- Check if FF is available in local dev environment.
    -- Check if Student Competency Report generation finishes
    
    - After merging
    -- Generate reports on BETA for customers
    -- Generate reports with FF turned on for the same customers
    -- Compare generated reports if there is any records/columns missing
    
    Change-Id: If3020eade77335388486769619633e8f3673339a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362212
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Balázs Koskóci <balazs.koskoci@instructure.com>
    QA-Review: Balázs Koskóci <balazs.koskoci@instructure.com>
    Product-Review: Balázs Koskóci <balazs.koskoci@instructure.com>

--- gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
+++ gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
@@ -71,7 +71,15 @@ module AccountReports
     # returns rows for each assignment that is linked to an outcome,
     # whether or not it has been submitted or graded
     def student_assignment_outcome_map
-      write_outcomes_report(self.class.student_assignment_outcome_headers, student_assignment_outcome_map_scope)
+      if Account.site_admin.feature_enabled?(:improved_outcomes_report_generation)
+        write_outcomes_report(
+          self.class.student_assignment_outcome_headers,
+          improved_student_assignment_outcome_map_scope,
+          { post_process_record: method(:post_process_student_assignment_outcome_map_scope_record) }
+        )
+      else
+        write_outcomes_report(self.class.student_assignment_outcome_headers, student_assignment_outcome_map_scope)
+      end
     end
 
     def self.outcome_result_headers
@@ -120,6 +128,20 @@ module AccountReports
     end
 
     private
+
+    def post_process_student_assignment_outcome_map_scope_record(record_hash, cache)
+      # It used to be an INNER JOIN with `accounts` table thus if acc_id is nil, or
+      # account does not exist is invalid case (raise exception)
+
+      acc_id = record_hash["account id"]
+      raise ActiveRecord::RecordInvalid if acc_id.nil?
+
+      account = cache.fetch(acc_id) { Account.find_by(id: acc_id) }
+      raise ActiveRecord::RecordInvalid if account.nil?
+
+      cache[account.id] = account
+      record_hash.merge("account name" => account.name)
+    end
 
     def student_assignment_outcome_map_scope
       parameters = {
@@ -172,6 +194,89 @@ module AccountReports
                                INNER JOIN #{Course.quoted_table_name} c ON c.id = e.course_id
                                  AND c.root_account_id = :root_account_id
                                INNER JOIN #{Account.quoted_table_name} acct ON acct.id = c.account_id
+                               INNER JOIN #{CourseSection.quoted_table_name} s ON s.id = e.course_section_id
+                               INNER JOIN #{Assignment.quoted_table_name} a ON (a.context_id = c.id
+                                                            AND a.context_type = 'Course'
+                                                            AND a.type = 'Assignment')
+                               INNER JOIN #{ContentTag.quoted_table_name} ct ON (ct.content_id = a.id
+                                                              AND ct.content_type = 'Assignment')
+                               INNER JOIN #{LearningOutcome.quoted_table_name} lo ON lo.id = ct.learning_outcome_id
+                               INNER JOIN #{ContentTag.quoted_table_name} lol ON lol.content_id = lo.id
+                                 AND lol.context_id = :account_id
+                                 AND lol.context_type = 'Account'
+                                 AND lol.tag_type = 'learning_outcome_association'
+                                 AND lol.workflow_state != 'deleted'
+                               INNER JOIN #{LearningOutcomeGroup.quoted_table_name} g ON g.id = lol.associated_asset_id
+                                 AND lol.associated_asset_type = 'LearningOutcomeGroup'
+                                 LEFT JOIN #{LearningOutcomeResult.quoted_table_name} r ON (r.user_id=pseudonyms.user_id
+                                   AND r.content_tag_id = ct.id)
+                                 LEFT JOIN #{Submission.quoted_table_name} sub ON sub.assignment_id = a.id
+                                   AND sub.user_id = pseudonyms.user_id AND sub.workflow_state <> 'deleted'
+                                   AND sub.workflow_state <> 'unsubmitted'
+                             SQL
+                             .where(<<~SQL.squish)
+                               ct.tag_type = 'learning_outcome' AND ct.workflow_state <> 'deleted'
+                               AND (r.id IS NULL OR (r.workflow_state <> 'deleted' AND r.artifact_type IS NOT NULL AND r.artifact_type <> 'Submission'))
+                             SQL
+
+      unless @include_deleted
+        students = students.where("pseudonyms.workflow_state<>'deleted' AND c.workflow_state IN ('available', 'completed')")
+      end
+
+      students = add_course_sub_account_scope(students, "c")
+      add_term_scope(students, "c")
+    end
+
+    def improved_student_assignment_outcome_map_scope
+      parameters = {
+        account_id: account.id,
+        root_account_id: root_account.id
+      }
+      students = root_account.pseudonyms.except(:preload)
+                             .select(<<~SQL.squish)
+                               pseudonyms.id,
+                               u.sortable_name        AS "student name",
+                               pseudonyms.user_id     AS "student id",
+                               pseudonyms.sis_user_id AS "student sis id",
+                               a.title                AS "assignment title",
+                               a.id                   AS "assignment id",
+                               sub.submitted_at       AS "submission date",
+                               sub.score              AS "submission score",
+                               lo.short_description   AS "learning outcome name",
+                               lo.id                  AS "learning outcome id",
+                               lo.display_name        AS "learning outcome friendly name",
+                               lo.data                AS "learning outcome data",
+                               g.title                AS "learning outcome group title",
+                               g.id                   AS "learning outcome group id",
+                               r.attempt              AS "attempt",
+                               r.hide_points          AS "learning outcome points hidden",
+                               r.score                AS "outcome score",
+                               r.possible             AS "learning outcome points possible",
+                               r.mastery              AS "learning outcome mastered",
+                               c.name                 AS "course name",
+                               c.id                   AS "course id",
+                               c.sis_source_id        AS "course sis id",
+                               s.name                 AS "section name",
+                               s.id                   AS "section id",
+                               s.sis_source_id        AS "section sis id",
+                               e.workflow_state       AS "enrollment state",
+                               lo.context_id          AS "outcome context id",
+                               lo.context_type        AS "outcome context type",
+                               c.account_id           AS "account id"
+                             SQL
+                             .joins(Pseudonym.send(:sanitize_sql, [<<~SQL.squish, parameters]))
+                               INNER JOIN #{User.quoted_table_name} u ON pseudonyms.user_id = u.id
+                               INNER JOIN (
+                                 SELECT user_id, course_id, course_section_id, workflow_state
+                                 FROM #{Enrollment.quoted_table_name}
+                                 WHERE type = 'StudentEnrollment'
+                                 AND root_account_id = :root_account_id
+                                 #{"AND workflow_state <> 'deleted'" unless @include_deleted}
+                                 GROUP BY user_id, course_id, course_section_id, workflow_state
+                               ) e ON pseudonyms.user_id = e.user_id
+                               INNER JOIN #{Course.quoted_table_name} c ON c.id = e.course_id
+                                 AND c.root_account_id = :root_account_id
+                                 AND c.account_id IS NOT NULL
                                INNER JOIN #{CourseSection.quoted_table_name} s ON s.id = e.course_section_id
                                INNER JOIN #{Assignment.quoted_table_name} a ON (a.context_id = c.id
                                                             AND a.context_type = 'Course'
@@ -577,6 +682,7 @@ module AccountReports
     end
 
     def write_outcomes_report(headers, canvas_scope, config_options = {})
+      improved_outcomes_report_generation_enabled = Account.site_admin.feature_enabled?(:improved_outcomes_report_generation)
       config_options[:empty_scope_message] ||= "No outcomes found"
       config_options[:new_quizzes_scope] ||= []
       host = root_account.domain
@@ -595,16 +701,45 @@ module AccountReports
           csv << headers.map { |h| row[h] }
         end
 
-        total = os_scope.length + canvas_scope.except(:select).count
-        GuardRail.activate(:primary) { AccountReport.where(id: @account_report.id).update_all(total_lines: total) }
+        if improved_outcomes_report_generation_enabled
+          # Use post_process_record function to execute any transformation logic on records
+          # Return processed record
+          # Raise ActiveRecord:RecordInvalid to skip record
+          post_process_record = config_options[:post_process_record]
+          # post_process_record_cache is provided on each loop for the post_process_record function.
+          # Use this hash to cache data between loops, e.g. skip querying data repeatedly from db
+          # the hash MUST to be changed in place, otherwise it will not save changes
+          post_process_record_cache = {}
+          omitted_row_count = 0
+        else
+          total = os_scope.length + canvas_scope.except(:select).count
+          GuardRail.activate(:primary) { AccountReport.where(id: @account_report.id).update_all(total_lines: total) }
+        end
 
         os_index = 0
+
         canvas_scope.find_each do |canvas_row|
-          until canvas_next?(canvas_row, os_scope, os_index)
+          record_hash = canvas_row.attributes
+
+          if improved_outcomes_report_generation_enabled
+            begin
+              record_hash = post_process_record.call(record_hash, post_process_record_cache) if post_process_record
+            rescue ActiveRecord::RecordInvalid
+              omitted_row_count += 1
+              next
+            end
+          end
+
+          until canvas_next?(record_hash, os_scope, os_index)
             write_row.call(os_scope[os_index])
             os_index += 1
           end
-          write_row.call(canvas_row.attributes)
+          write_row.call(record_hash)
+        end
+
+        if improved_outcomes_report_generation_enabled
+          total = os_scope.length + canvas_scope.except(:select).count - omitted_row_count
+          GuardRail.activate(:primary) { AccountReport.where(id: @account_report.id).update_all(total_lines: total) }
         end
 
         while os_index < os_scope.length
