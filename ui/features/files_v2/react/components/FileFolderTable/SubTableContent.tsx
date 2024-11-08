622d80bc4e795b434d5a5bc5769c0495a06cbdf5
Jacob DeWar <jacob.dewar@instructure.com>
2024-11-08 08:54:10 -0500

    Display Table Rows
    refs RCX-2514
    flag=files_a11y_rewrite
    
    Test Plan:
     - Go to Files page
     - Rows are loaded with fake data
     - Table is displayed as expected
     * Verify against figma designs
    
     - Shrink table to tablet/mobile view
     * Table changes to stacked view
    
    Change-Id: I443f88e32a583a76058b1359b86504f7669f6ff9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362217
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

---
+++ ui/features/files_v2/react/components/FileFolderTable/SubTableContent.tsx
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {FileDrop} from '@instructure/ui-file-drop'
+import {View} from '@instructure/ui-view'
+import {Heading} from '@instructure/ui-heading'
+import {Text} from '@instructure/ui-text'
+import {IconUploadLine} from '@instructure/ui-icons'
+import {Spinner} from '@instructure/ui-spinner'
+import {Flex} from '@instructure/ui-flex'
+import {useScope as useI18nScope} from '@canvas/i18n'
+
+const I18n = useI18nScope('files_v2')
+
+interface SubTableContentProps {
+  isLoading: boolean
+  isEmpty: boolean
+}
+
+const SubTableContent = ({isLoading, isEmpty}: SubTableContentProps) => {
+  return isLoading ? (
+    <Flex as="div" alignItems="center" justifyItems="center" padding="medium">
+      <Spinner renderTitle={I18n.t('Loading data')} />
+    </Flex>
+  ) : (
+    <View as="div" padding="large none none none">
+      {isEmpty && (
+        <FileDrop
+          renderLabel={
+            <View as="div" padding="xx-large large" background="primary">
+              <IconUploadLine size="large" />
+              <Heading margin="medium 0 small 0">{I18n.t('Drag a file here, or')}</Heading>
+              <Text color="brand">{I18n.t('Choose a file to upload')}</Text>
+            </View>
+          }
+        />
+      )}
+    </View>
+  )
+}
+
+export default SubTableContent
