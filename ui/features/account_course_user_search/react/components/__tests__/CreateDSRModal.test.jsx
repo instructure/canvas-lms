6d9ca80aa6ff7db907d4678458d597aa4091c3e4
jake.oeding <jake.oeding@instructure.com>
2024-11-13 16:56:32 -0500

    eslint dsr modal tests
    refs AE-1377
    
    Change-Id: I1ebac43e8e91a66143fd40d3133a4c20cb860548
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362698
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    QA-Review: Jake Oeding <jake.oeding@instructure.com>
    Product-Review: Jake Oeding <jake.oeding@instructure.com>

--- ui/features/account_course_user_search/react/components/__tests__/CreateDSRModal.test.jsx
+++ ui/features/account_course_user_search/react/components/__tests__/CreateDSRModal.test.jsx
@@ -1,9 +1,27 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import {render, fireEvent, waitFor} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import {Tooltip} from '@instructure/ui-tooltip'
 import {IconButton} from '@instructure/ui-buttons'
-import {IconEditLine, IconMasqueradeLine, IconMessageLine, IconExportLine} from '@instructure/ui-icons'
+import {IconExportLine} from '@instructure/ui-icons'
 import {useScope as useI18nScope} from '@canvas/i18n'
 import CreateDSRModal from '../CreateDSRModal'
 import axios from '@canvas/axios'
@@ -31,27 +49,29 @@ describe('CreateDSRModal', () => {
     render(
       <CreateDSRModal accountId={mockAccountId} user={mockUser} afterSave={afterSave} {...props}>
         <span>
-              <Tooltip
-                data-testid="user-list-row-tooltip"
-                renderTip={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})}
-              >
-                <IconButton
-                  withBorder={false}
-                  withBackground={false}
-                  size="small"
-                  screenReaderLabel={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})}
-                >
-                  <IconExportLine title={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})} />
-                </IconButton>
-              </Tooltip>
-            </span>
+          <Tooltip
+            data-testid="user-list-row-tooltip"
+            renderTip={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})}
+          >
+            <IconButton
+              withBorder={false}
+              withBackground={false}
+              size="small"
+              screenReaderLabel={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})}
+            >
+              <IconExportLine
+                title={I18n.t('Create DSR Request for %{name}', {name: mockUser.name})}
+              />
+            </IconButton>
+          </Tooltip>
+        </span>
       </CreateDSRModal>
     )
 
   it('should not show latest request if there is none', async () => {
-    axios.get.mockResolvedValueOnce({ status: 204, data: {} })
+    axios.get.mockResolvedValueOnce({status: 204, data: {}})
 
-    const { queryByText, getByTitle } = renderComponent()
+    const {queryByText, getByTitle} = renderComponent()
     fireEvent.click(getByTitle('Create DSR Request for John Doe'))
 
     await waitFor(() => {
@@ -69,7 +89,7 @@ describe('CreateDSRModal', () => {
       },
     })
 
-    const { getByText, getByTitle } = renderComponent()
+    const {getByText, getByTitle} = renderComponent()
     fireEvent.click(getByTitle('Create DSR Request for John Doe'))
 
     await waitFor(() => {
@@ -87,11 +107,13 @@ describe('CreateDSRModal', () => {
       },
     })
 
-    const { getByText, queryByText, getByTitle } = renderComponent()
+    const {getByText, queryByText, getByTitle} = renderComponent()
     fireEvent.click(getByTitle('Create DSR Request for John Doe'))
 
     await waitFor(() => {
-      expect(getByText((_, element) => element.textContent === 'Latest DSR: In progress')).toBeInTheDocument()
+      expect(
+        getByText((_, element) => element.textContent === 'Latest DSR: In progress')
+      ).toBeInTheDocument()
       expect(queryByText('Download:')).not.toBeInTheDocument()
     })
   })
