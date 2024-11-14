cdd8443797ec9002c1ae151d1ee302d68f1d5f4f
Gergely Máté <gergely.mate@instructure.com>
2024-11-14 19:05:30 +0100

    Crystalball skip predictions for erb changes
    closes DE-1702
    
    flag = none
    
    test plan:
      - make a change to an .erb template and trigger the Jenkins build
      - check build logs:
        - message about
          "Ignoring Crystalball prediction due to .erb file changes"
        - the full test suite should be run
      - revert all .erb changes
      - check build logs:
        - no message about ignoring Crystalball prediction
        - Crystalball prediction should be used by rspecq
          (this won't happen if there are build config changes
           in the same commit)
    
    Change-Id: If79fab1c2975a78fffb28ddda7221029de53df2c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362724
    QA-Review: Gergely Máté <gergely.mate@instructure.com>
    Product-Review: Gergely Máté <gergely.mate@instructure.com>
    Reviewed-by: James Butters <jbutters@instructure.com>
    Build-Review: James Butters <jbutters@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- build/new-jenkins/library/vars/filesChangedStage.groovy
+++ build/new-jenkins/library/vars/filesChangedStage.groovy
@@ -45,6 +45,10 @@ def hasGraphqlFiles(buildConfig) {
   return buildConfig[STAGE_NAME].value('graphqlFiles')
 }
 
+def hasErbFiles(buildConfig) {
+  return buildConfig[STAGE_NAME].value('erbFiles')
+}
+
 def hasJsFiles(buildConfig) {
   return buildConfig[STAGE_NAME_POST_BUILD].value('jsFiles')
 }
@@ -69,6 +73,7 @@ def preBuild(stageConfig) {
   stageConfig.value('groovyFiles', git.changedFiles(['.*.groovy', 'Jenkinsfile.*'], 'HEAD^'))
   stageConfig.value('yarnFiles', git.changedFiles(['package.json', 'yarn.lock'], 'HEAD^'))
   stageConfig.value('graphqlFiles', git.changedFiles(['app/graphql'], 'HEAD^'))
+  stageConfig.value('erbFiles', git.changedFiles(['.erb'], 'HEAD^'))
   stageConfig.value('addedOrDeletedSpecFiles', sh(script: 'git diff --name-only --diff-filter=AD HEAD^..HEAD | grep "_spec.rb"', returnStatus: true) == 0)
 
   dir(env.LOCAL_WORKDIR) {
