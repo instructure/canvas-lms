f38e7562bdf33e7eecfaedb541f01eb9e9cc4b67
Jeremy Stanley <jeremy@instructure.com>
2024-11-15 12:45:31 -0700

    fail diffed sis import after 5 consecutive over-threshold batches
    to prevent schools from unknowingly going without diffing for an
    extended period of time
    
    test plan:
     - do an import with a new diffing data set identifier
     - attempt to import an empty package with the same identifier
       and a low row count threshold; this should end up doing
       nothing because the diffing threshold is exceeded
     - repeat this upload five more times
     - the last attempt should fail with a "remaster required"
       error
     - performing a diffing remaster should get imports working again
    
    flag=none
    closes FOO-4925
    
    Change-Id: Iaa02c04c50d4698067dc3c1e438518c7ebda503f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362839
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jeremy Perkins <jperkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>

--- spec/models/sis_batch_spec.rb
+++ spec/models/sis_batch_spec.rb
@@ -1160,7 +1160,7 @@ test_4,TC 104,Test Course 104,,term1,active
       expect(b4.generated_diff_id).to_not be_nil
     end
 
-    it "does not diff against a batch after too many skipped intermediate batches" do
+    it "requires a remaster after too many skipped over-threshold batches" do
       Setting.set("sis_diffing_max_skip", "2")
 
       process_csv_data(
@@ -1192,8 +1192,19 @@ test_4,TC 104,Test Course 104,,term1,active
         diffing_data_set_identifier: "foobar"
       )
 
-      expect(nb.diffing_threshold_exceeded).to be true
-      expect(nb.processing_warnings.first.last).to include("Diffing not performed because too many batches were skipped. Please do a remaster to re-enable diffing.")
+      expect(nb).to be_failed_with_messages
+      expect(nb.processing_errors.first.last).to eq "Too many consecutive batches exceeded the change threshold. A remaster is required."
+
+      fb = process_csv_data(
+        [
+          %(course_id,short_name,long_name,account_id,term_id,status
+            test_1,TC 101,Test Course 101,,term1,active
+            test_4,TC 104,Test Course 104,,term1,active)
+        ],
+        diffing_data_set_identifier: "foobar",
+        diffing_remaster: true
+      )
+      expect(fb).to be_imported
     end
 
     it "marks files separately when created for diffing" do
