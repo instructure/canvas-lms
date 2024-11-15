6e7fceb645c855ae7d410dec3b60677ecb5e267e
Drake Harper <drake.harper@instructure.com>
2024-11-15 11:04:52 -0700

    Check permission at course level if available
    flag=none
    closes LX-2113
    
    Test Plan:
    - before pulling code
    - enable prosopite for n+1 scanning
    
    - as a teacher
    - run the sample query
    - verify logs show n+1
    - note the completion time for the request
    
    - pull code
    - run the sample query
    - verify there is no n+1
    - completion time should be reduced
    
    Sample Query:
    query Selective_Release_GetStudentsQuery(
     $courseId: ID!,
     $cursor: String
    ) {
      __typename
      legacyNode(_id: $courseId, type: Course) {
        ... on Course {
          id
          name
          enrollmentsConnection(
            filter: {types: StudentEnrollment, states: [invited, active]}
            first: 100
            after: $cursor
          ) {
            edges {
              cursor
              node {
                user {
                  id: _id
                  name
                  sisId
                }
              }
            }
          }
        }
      }
    }
    
    Sample Variables:
    {
      "courseId": "8",
      "cursor": null
    }
    
    Change-Id: Ic04ad5cba28a03f6762cb12520c58dd1bc8499f9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362826
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -108,6 +108,7 @@ module Types
     def sis_id
       domain_root_account = context[:domain_root_account]
       if domain_root_account.grants_any_right?(context[:current_user], :read_sis, :manage_sis) ||
+         context[:course]&.grants_any_right?(context[:current_user], :read_sis, :manage_sis) ||
          object.grants_any_right?(context[:current_user], :read_sis, :manage_sis)
         Loaders::AssociationLoader.for(User, :pseudonyms)
                                   .load(object)
@@ -127,6 +128,7 @@ module Types
     def integration_id
       domain_root_account = context[:domain_root_account]
       if domain_root_account.grants_any_right?(context[:current_user], :read_sis, :manage_sis) ||
+         context[:course]&.grants_any_right?(context[:current_user], :read_sis, :manage_sis) ||
          object.grants_any_right?(context[:current_user], :read_sis, :manage_sis)
         Loaders::AssociationLoader.for(User, :pseudonyms)
                                   .load(object)
