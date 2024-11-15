6fb2a32f0694bd11b239e7cb3ad1418e07bd25bc
Jeremy Stanley <jeremy@instructure.com>
2024-11-15 12:45:31 -0700

    fail diffed sis import after 5 consecutive over-threshold batches
    to prevent schools from unknowingly going without diffing for an
    extended period of time
    
    test plan:
     - do an import with a new diffing data set identifier
     - attempt to import an empty package with the same identifier
       and a low row count threshold; this should end up doing
       nothing because the diffing threshold is exceeded
     - repeat this upload five more times
     - the last attempt should fail with a "remaster required"
       error
     - performing a diffing remaster should get imports working again
    
    flag=none
    closes FOO-4925
    
    Change-Id: Iaa02c04c50d4698067dc3c1e438518c7ebda503f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/362839
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jeremy Perkins <jperkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jeremy Stanley <jeremy@instructure.com>

--- app/models/sis_batch.rb
+++ app/models/sis_batch.rb
@@ -288,8 +288,13 @@ class SisBatch < ActiveRecord::Base
     require "sis"
     download_zip
     diff_result = generate_diff
-    if diff_result == :empty_diff_file
+    case diff_result
+    when :empty_diff_file
       finish(true)
+      return
+    when :too_many_over_threshold_batches
+      SisBatch.add_error(nil, "Too many consecutive batches exceeded the change threshold. A remaster is required.", sis_batch: self, failure: true)
+      finish(false)
       return
     end
 
@@ -321,16 +326,11 @@ class SisBatch < ActiveRecord::Base
     previous_batch ||= account.sis_batches
                               .succeeded.where(diffing_data_set_identifier:).order(:created_at).first
 
-    # avoid unpleasant surprises if, for example, the file size check suddenly passes after failing for years
-    # and we end up diffing against a very old import
-    if previous_batch && account.sis_batches
-                                .where(diffing_data_set_identifier:)
-                                .where("id > ? AND id < ?", previous_batch.id, id)
-                                .count > Setting.get("sis_diffing_max_skip", "5").to_i
-      self.diffing_threshold_exceeded = true
-      SisBatch.add_error(nil, "Diffing not performed because too many batches were skipped. Please do a remaster to re-enable diffing.", sis_batch: self)
-      return
-    end
+    # require a remaster if too many consecutive batches exceeded the threshold
+    return :too_many_over_threshold_batches if previous_batch && account.sis_batches
+                                                                        .where(diffing_data_set_identifier:)
+                                                                        .where("id > ? AND id < ?", previous_batch.id, id)
+                                                                        .count > Setting.get("sis_diffing_max_skip", "5").to_i
 
     previous_zip = previous_batch.try(:download_zip)
     return unless previous_zip
