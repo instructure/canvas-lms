a9fc8a8e3edba0cdd19fbebc0172f4aae2093dc9
Jacob DeWar <jacob.dewar@instructure.com>
2024-11-20 11:27:21 -0500

    Fetch usage data only if proper permissions
    closes RCX-2695
    flag=files_a11y_rewrite
    
    Test Plan:
     - Go to Course + User files
     * Usage bar displayed as expected
    
     - Go to Course Files as student, or someone who doesn't have any Files permissions
     * No usage bar is displayed
     * No errors flash for failure to fetch usage data
    
    Change-Id: I3cae3a093f9b75005f1ceaaad13e2738e24b1340
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/363286
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

--- ui/features/files_v2/react/components/FilesApp.tsx
+++ ui/features/files_v2/react/components/FilesApp.tsx
@@ -20,13 +20,8 @@ import React from 'react'
 import {useQuery} from '@tanstack/react-query'
 import {Heading} from '@instructure/ui-heading'
 import {useScope as useI18nScope} from '@canvas/i18n'
-import {ProgressBar} from '@instructure/ui-progress'
-import {Text} from '@instructure/ui-text'
 import {View} from '@instructure/ui-view'
 import filesEnv from '@canvas/files/react/modules/filesEnv'
-import {showFlashError} from '@canvas/alerts/react/FlashAlert'
-import formatMessage from '../../../../../packages/canvas-media/src/format-message'
-import friendlyBytes from '@canvas/files/util/friendlyBytes'
 import {Flex} from '@instructure/ui-flex'
 import {canvas} from '@instructure/ui-theme-tokens'
 import {Responsive} from '@instructure/ui-responsive'
@@ -35,16 +30,9 @@ import TopLevelButtons from './TopLevelButtons'
 import FileFolderTable from './FileFolderTable'
 
 import {FAKE_FOLDERS_AND_FILES} from '../../data/FakeData'
+import FilesUsageBar from './FilesUsageBar'
 
 const I18n = useI18nScope('files_v2')
-
-const fetchQuota = async (contextType: string, contextId: string) => {
-  const response = await fetch(`/api/v1/${contextType}/${contextId}/files/quota`)
-  if (!response.ok) {
-    throw new Error('Failed to fetch quota data')
-  }
-  return response.json()
-}
 
 const fetchFilesAndFolders = async (contextType: string, contextId: string) => {
   // TODO fetch real data
@@ -65,38 +53,14 @@ const FilesApp = ({isUserContext, size}: FilesAppProps) => {
     return filesEnv.userHasPermission({contextType, contextId}, permission)
   }
 
-  const _userCanAddFilesForContext = canManageFilesForContext('manage_files_add')
+  const userCanAddFilesForContext = canManageFilesForContext('manage_files_add')
   const userCanEditFilesForContext = canManageFilesForContext('manage_files_edit')
-  const _userCanDeleteFilesForContext = canManageFilesForContext('manage_files_delete')
+  const userCanDeleteFilesForContext = canManageFilesForContext('manage_files_delete')
+  const userCanManageFilesForContext =
+    userCanAddFilesForContext || userCanEditFilesForContext || userCanDeleteFilesForContext
 
-  const {data, error} = useQuery(['quota'], () => fetchQuota(contextType, contextId))
   const tableResult = useQuery(['files'], () => fetchFilesAndFolders(contextType, contextId))
 
-  const renderFilesUsageBar = () => {
-    if (error) {
-      showFlashError(I18n.t('An error occurred while loading files usage data'))(error as Error)
-    }
-
-    const {quota_used = 0, quota = 1} = data || {quota_used: 0, quota: 1}
-    const percentage = Math.round((quota_used / quota) * 100)
-
-    const filesUsageString = I18n.t('%{percentUsed} of %{quota} used', {
-      percentUsed: I18n.n(percentage, {percentage: true}),
-      quota: friendlyBytes(data?.quota) || 0,
-    })
-
-    return (
-      <ProgressBar
-        meterColor="brand"
-        screenReaderLabel={formatMessage('File Storage Quota Used')}
-        formatScreenReaderValue={() => filesUsageString}
-        renderValue={<Text>{filesUsageString}</Text>}
-        size="x-small"
-        valueMax={quota}
-        valueNow={quota_used}
-      />
-    )
-  }
   return (
     <View as="div">
       <Flex justifyItems="center" padding="medium none none none">
@@ -129,9 +93,13 @@ const FilesApp = ({isUserContext, size}: FilesAppProps) => {
         isLoading={tableResult.isLoading}
         userCanEditFilesForContext={userCanEditFilesForContext}
       />
-      <Flex padding="small none none none">
-        <Flex.Item size="50%">{renderFilesUsageBar()}</Flex.Item>
-      </Flex>
+      {userCanManageFilesForContext && (
+        <Flex padding="small none none none">
+          <Flex.Item size="50%">
+            <FilesUsageBar contextId={contextId} contextType={contextType} />
+          </Flex.Item>
+        </Flex>
+      )}
     </View>
   )
 }
--- ui/features/files_v2/react/components/__tests__/FilesApp.test.tsx
+++ ui/features/files_v2/react/components/__tests__/FilesApp.test.tsx
@@ -19,62 +19,51 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import FilesApp from '../FilesApp'
-import {useQuery} from '@tanstack/react-query'
-import {showFlashError} from '@canvas/alerts/react/FlashAlert'
-import friendlyBytes from '@canvas/files/util/friendlyBytes'
-
-// Mock the `useQuery` hook from TanStack Query
-jest.mock('@tanstack/react-query', () => ({
-  useQuery: jest.fn(),
-}))
-
-jest.mock('@canvas/alerts/react/FlashAlert', () => ({
-  showFlashError: jest.fn().mockReturnValue(jest.fn()),
-}))
+import {MockedQueryClientProvider} from '@canvas/test-utils/query'
+import {QueryClient} from '@tanstack/react-query'
+import fetchMock from 'fetch-mock'
+import filesEnv from '@canvas/files/react/modules/filesEnv'
 
 describe('FilesApp', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+    fetchMock.get(/.*\/files\/quota/, {quota_used: 500, quota: 1000})
+    // @ts-expect-error
+    filesEnv.userHasPermission = jest.fn().mockReturnValue(true)
   })
 
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  const renderComponent = (contextAssetString: string) => {
+    const queryClient = new QueryClient()
+    return render(
+      <MockedQueryClientProvider client={queryClient}>
+        <FilesApp contextAssetString={contextAssetString} />
+      </MockedQueryClientProvider>
+    )
+  }
+
   it('renders "Files" when contextAssetString starts with "course_"', () => {
-    useQuery.mockReturnValue({data: null, isLoading: false, error: null})
-    render(<FilesApp contextAssetString="course_12345" />)
+    renderComponent('course_12345')
 
     const headingElement = screen.getByText('Files', {exact: true})
     expect(headingElement).toBeInTheDocument()
   })
 
   it('renders "All My Files" when contextAssetString starts with "user_"', () => {
-    useQuery.mockReturnValue({data: null, isLoading: false, error: null})
-    render(<FilesApp contextAssetString="user_67890" />)
+    renderComponent('user_67890')
 
     const headingElement = screen.getByText(/All My Files/i)
     expect(headingElement).toBeInTheDocument()
   })
 
-  it('displays error message if quota fetch fails', () => {
-    useQuery.mockReturnValue({data: null, isLoading: false, error: new Error('Failed to fetch')})
-    render(<FilesApp contextAssetString="user_67890" />)
+  it('does not render progress bar without permission', () => {
+    // @ts-expect-error
+    filesEnv.userHasPermission = jest.fn().mockReturnValue(false)
+    renderComponent('course_12345')
 
-    expect(showFlashError).toHaveBeenCalledWith('An error occurred while loading files usage data')
-  })
-
-  it('renders progress bar with quota data when fetch is successful', async () => {
-    useQuery.mockReturnValue({
-      data: {quota_used: 500, quota: 1000},
-      isLoading: false,
-      error: null,
-    })
-
-    render(<FilesApp contextAssetString="user_67890" />)
-
-    const quota = friendlyBytes('1000')
-    const percentageText = await screen.findByText(`50% of ${quota} used`)
-    expect(percentageText).toBeInTheDocument()
-
-    const progressBar = screen.getByLabelText(/File Storage Quota Used/i)
-    expect(progressBar).toHaveAttribute('aria-valuemax', '1000')
-    expect(progressBar).toHaveAttribute('aria-valuenow', '500')
+    expect(fetchMock.calls()).toHaveLength(0)
   })
 })
