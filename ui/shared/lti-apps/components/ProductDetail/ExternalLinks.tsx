c1b7ee1d6262a6d9992eea7eaeb08739f431af3e
Westin Humble <westin.humble@instructure.com>
2024-11-20 14:56:30 -0500

    LTI Config Detail Redesign
    test plan: Click on any product on discover page, and detail page should match most recent designs (linked in below ticket).
    
    flag=lti_registrations_page
    
    fixes LRN-8807
    
    Change-Id: I912ce0315da2a797dafe98a9a28bbc0e2fa8a4c7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/363302
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Márton Németh <marton.nemeth@instructure.com>
    QA-Review: Márton Németh <marton.nemeth@instructure.com>
    Product-Review: Márton Németh <marton.nemeth@instructure.com>

---
+++ ui/shared/lti-apps/components/ProductDetail/ExternalLinks.tsx
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useScope as useI18nScope} from '@canvas/i18n'
+import React from 'react'
+import {Flex} from '@instructure/ui-flex'
+import {Text} from '@instructure/ui-text'
+import {Link} from '@instructure/ui-link'
+import {IconExternalLinkLine} from '@instructure/ui-icons'
+import type {Product} from '../../models/Product'
+
+const I18n = useI18nScope('lti_registrations')
+
+interface ExternalLinksProps {
+  product: Product
+}
+
+const ExternalLinks = (props: ExternalLinksProps) => {
+  const {product} = props
+
+  return (
+    <div>
+      <Flex direction="column" gap="medium">
+        <Flex direction="column">
+          <Flex.Item margin="0 0 small 0">
+            <Text weight="bold" size="x-large">
+              {I18n.t('Privacy')}
+            </Text>
+          </Flex.Item>
+          {product.privacy_policy_url ? (
+            <Flex direction="column">
+              <Flex.Item>
+                <Link href={product.privacy_policy_url} isWithinText={false}>
+                  <Text weight="bold">
+                    {I18n.t('Partner Privacy Policy')} <IconExternalLinkLine />
+                  </Text>
+                </Link>
+              </Flex.Item>
+            </Flex>
+          ) : (
+            <Flex.Item>
+              {I18n.t('The privacy information for this provider is not currently available.')}
+            </Flex.Item>
+          )}
+        </Flex>
+        <Flex direction="column">
+          <Flex.Item margin="0 0 small 0">
+            <Text weight="bold" size="x-large">
+              {I18n.t('Terms of Service')}
+            </Text>
+          </Flex.Item>
+          {product.terms_of_service_url ? (
+            <Flex direction="column">
+              <Flex.Item>
+                <Link href={product.terms_of_service_url} isWithinText={false}>
+                  <Text weight="bold">
+                    {I18n.t('Terms of Service')} <IconExternalLinkLine />
+                  </Text>
+                </Link>
+              </Flex.Item>
+            </Flex>
+          ) : (
+            <Flex.Item>
+              {I18n.t(
+                'The terms of service documentation for this provider is not currently available.'
+              )}
+            </Flex.Item>
+          )}
+        </Flex>
+        <Flex direction="column">
+          <Flex.Item margin="0 0 small 0">
+            <Text weight="bold" size="x-large">
+              {I18n.t('Accessibility')}
+            </Text>
+          </Flex.Item>
+          {product.accessibility_url ? (
+            <Flex direction="column">
+              <Flex.Item>
+                <Link href={product.accessibility_url} isWithinText={false}>
+                  <Text weight="bold">
+                    {I18n.t('Accessibility Documentation')} <IconExternalLinkLine />
+                  </Text>
+                </Link>
+              </Flex.Item>
+            </Flex>
+          ) : (
+            <Flex.Item>
+              {I18n.t(
+                'The accessibility documentation for this provider is not currently available.'
+              )}
+            </Flex.Item>
+          )}
+        </Flex>
+      </Flex>
+    </div>
+  )
+}
+
+export default ExternalLinks
--- ui/shared/lti-apps/components/common/__tests__/carousels.test.tsx
+++ ui/shared/lti-apps/components/common/__tests__/carousels.test.tsx
@@ -20,53 +20,8 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import ProductCarousel from '../Carousels/ProductCarousel'
-import BadgeCarousel from '../Carousels/BadgeCarousel'
 import ImageCarousel from '../Carousels/ImageCarousel'
-
-import type {Product, Company, LtiDetail, Lti, Badges, Tag} from '../../../models/Product'
-
-const company: Company = {
-  id: 2,
-  name: 'Instructure',
-  company_url: 'google.com',
-}
-
-const lti_configurations: LtiDetail = {
-  lti_13: {services: ['gk'], placements: ['dr']},
-  lti_11: {services: ['gk'], placements: ['dr']},
-}
-
-const tool_integration_configurations: Lti = {
-  lti_13: [{id: 3, integration_type: 'dr', url: 'google.com', unified_tool_id: '5'}],
-  lti_11: [{id: 4, integration_type: 'gk', url: 'google.com', unified_tool_id: '6'}],
-}
-
-const badges: Badges[] = [{name: 'badge1', image_url: 'google.com', link: 'google.com'}]
-
-const tags: Tag[] = [{id: '1', name: 'tag1'}]
-
-const product: Product[] = [
-  {
-    id: '1',
-    global_product_id: '1',
-    name: 'Product 1',
-    company,
-    logo_url: 'google.com',
-    tagline: 'Product 1 tagline',
-    description: 'Product 1 description',
-    updated_at: '2024-01-01',
-    tool_integration_configurations,
-    lti_configurations,
-    integration_resources: {comments: null, resources: []},
-    badges,
-    screenshots: ['greatimage'],
-    terms_of_service_url: 'google.com',
-    privacy_policy_url: 'google.com',
-    accessibility_url: 'google.com',
-    support_link: 'google.com',
-    tags,
-  },
-]
+import {product, company} from './data'
 
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
@@ -104,20 +59,6 @@ describe('Carousels render as expected', () => {
 
     const {getByText} = render(<ProductCarousel products={product} companyName={company.name} />)
     expect(getByText('Product 1')).toBeInTheDocument()
-  })
-
-  it('BadgeCarousel renders as expected', () => {
-    const isMaxMobile = false
-    const isMaxTablet = true
-
-    const {getByText} = render(
-      <BadgeCarousel
-        isMaxMobile={isMaxMobile}
-        isMaxTablet={isMaxTablet}
-        badges={product[0].badges}
-      />
-    )
-    expect(getByText('badge1')).toBeInTheDocument()
   })
 
   it('ImageCarousel renders as expected', () => {
