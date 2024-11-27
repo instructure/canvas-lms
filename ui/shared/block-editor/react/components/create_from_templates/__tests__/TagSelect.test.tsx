6f7b3d866ef6d95b64336d4d255e370eda609b36
Ed Schiebel <eschiebel@instructure.com>
2024-11-27 15:02:20 -0700

    Add filtering to the block editor starting page
    closes RCX-2560
    flag=block_editor
    
    test plan:
      - search and filter
      > expect the templates to be filtered
      - click the 'x' in the search box
      > expect the search string to be cleared
      - click the 'x' in some filter pills
      > expect them to be removed and the matching templates
        to be removed
      - click "clear all filters"
      > expect all the pills to come back
    
    Change-Id: I889f217d635ffddb3456c41e2339019ee44e2599
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/363889
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Ed Schiebel <eschiebel@instructure.com>

---
+++ ui/shared/block-editor/react/components/create_from_templates/__tests__/TagSelect.test.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import {TagSelect, AvailableTags} from '../TagSelect'
+
+const renderComponent = (props = {}) => {
+  return render(
+    <TagSelect
+      onChange={jest.fn()}
+      selectedTags={Object.keys(AvailableTags)}
+      interaction="enabled"
+      {...props}
+    />
+  )
+}
+
+const isMenuItemChecked = (li: HTMLLIElement): boolean => {
+  return li.querySelector('svg[name="IconCheck"]') !== null
+}
+
+describe('TagSelect', () => {
+  it('renders', () => {
+    const {getByText} = renderComponent()
+    expect(getByText('Apply Filters')).toBeInTheDocument()
+  })
+
+  it('shows the menu', () => {
+    const {getByText} = renderComponent()
+    const trigger = getByText('Apply Filters').closest('button')
+    trigger?.click()
+    expect(getByText('Home')).toBeInTheDocument()
+    expect(getByText('Resource')).toBeInTheDocument()
+    expect(getByText('Module Overview')).toBeInTheDocument()
+    expect(getByText('Introduction')).toBeInTheDocument()
+    expect(getByText('General Content')).toBeInTheDocument()
+  })
+
+  it('checks the selected tags', () => {
+    const selectedTags = ['home', 'resource', 'intro']
+    const {getByText} = renderComponent({selectedTags})
+    const trigger = getByText('Apply Filters').closest('button')
+    trigger?.click()
+
+    for (const tag of Object.keys(AvailableTags)) {
+      const li = getByText(AvailableTags[tag]).closest('li')
+      const isChecked = selectedTags.includes(tag)
+      expect(isMenuItemChecked(li as HTMLLIElement)).toBe(isChecked)
+    }
+  })
+
+  it('calls onChange when a tag is selected', () => {
+    const onChange = jest.fn()
+    const selectedTags = ['resource']
+    const {getByText} = renderComponent({selectedTags, onChange})
+    const trigger = getByText('Apply Filters').closest('button')
+    trigger?.click()
+
+    const li = getByText('Home')
+    li?.click()
+    expect(onChange).toHaveBeenCalledWith(['resource', 'home'])
+  })
+})
---
+++ ui/shared/block-editor/react/components/create_from_templates/__tests__/TemplateCardSkeleton.test.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import {type BlockTemplate} from '../../../types'
+import TemplateCardSkeleton from '../TemplateCardSekeleton'
+
+const blocktemplate = {
+  id: 'xyzzy',
+  name: 'Template Name',
+  description: 'Template Description',
+} as BlockTemplate
+
+const renderComponent = (props = {}) => {
+  return render(
+    <TemplateCardSkeleton
+      template={blocktemplate}
+      createAction={jest.fn()}
+      quickLookAction={jest.fn()}
+      inLayout="grid"
+      {...props}
+    />
+  )
+}
+
+describe('TemplateCardSkeleton', () => {
+  it('renders', () => {
+    const {getByText, getByTestId} = renderComponent()
+    const skeleton = getByTestId('template-card-skeleton')
+    expect(skeleton).toBeInTheDocument()
+    expect(skeleton).toHaveClass('grid')
+    expect(getByText('Quick Look')).toBeInTheDocument()
+    expect(getByText('Customize')).toBeInTheDocument()
+  })
+
+  it('renders in rows layout', () => {
+    const {getByTestId} = renderComponent({inLayout: 'rows'})
+    const skeleton = getByTestId('template-card-skeleton')
+    expect(skeleton).toHaveClass('rows')
+  })
+
+  it('includes the template name in the aria-label', () => {
+    const {getByTestId} = renderComponent()
+    const skeleton = getByTestId('template-card-skeleton')
+    expect(skeleton).toHaveAttribute('aria-label', 'Template Name template')
+  })
+
+  it('includes the template description in the aria-describedby', () => {
+    const {container, getByTestId} = renderComponent()
+    const skeleton = getByTestId('template-card-skeleton')
+    expect(skeleton).toHaveAttribute('aria-describedby')
+    const aria_describedby = skeleton.getAttribute('aria-describedby')
+    const describedby = container.querySelector(`#${aria_describedby}`)
+    expect(describedby).toBeInTheDocument()
+    expect(describedby).toHaveTextContent('Template Description')
+  })
+
+  describe('The blank page template', () => {
+    it('renders', () => {
+      const {getByText, getByTestId} = renderComponent({template: {id: 'blank_page'}})
+      const skeleton = getByTestId('template-card-skeleton')
+      expect(getByText('New Blank Page')).toBeInTheDocument()
+      expect(skeleton).toHaveClass('blank-card')
+    })
+
+    it('had a curl', () => {
+      const {getByTestId} = renderComponent({template: {id: 'blank_page'}})
+      const skeleton = getByTestId('template-card-skeleton')
+      expect(skeleton.querySelector('.curl')).toBeInTheDocument()
+    })
+
+    it('does not render the Quick Look button', () => {
+      const {queryByText} = renderComponent({template: {id: 'blank_page'}})
+      expect(queryByText('Quick Look')).not.toBeInTheDocument()
+    })
+  })
+})
