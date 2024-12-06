27e99d8debd2fe59b197d2bba6541685ff037bed
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-12-06 08:08:05 -0700

    test: convert some QUnit tests to Jest
    Change-Id: Id6472007b097fb369f76af467e17820eff374331
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/364720
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/features/manage_groups/backbone/views/__tests__/GroupCategoriesView.test.js
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2013 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $ from 'jquery'
+import 'jquery-migrate'
+import GroupCategoriesView from '../GroupCategoriesView'
+import GroupCategoryCollection from '@canvas/groups/backbone/collections/GroupCategoryCollection'
+import GroupCategory from '@canvas/groups/backbone/models/GroupCategory'
+import fakeENV from '@canvas/test-utils/fakeENV'
+
+describe('GroupCategoriesView', () => {
+  let view
+  let categories
+  let wrapper
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+    fakeENV.setup()
+    ENV.group_categories_url = '/api/v1/courses/1/group_categories'
+    ENV.permissions = {can_add_groups: true}
+
+    categories = new GroupCategoryCollection([
+      {
+        id: 1,
+        name: 'group set 1',
+      },
+      {
+        id: 2,
+        name: 'group set 2',
+      },
+    ])
+    jest.spyOn(categories, 'fetch').mockReturnValue([])
+
+    view = new GroupCategoriesView({collection: categories})
+    view.render()
+
+    wrapper = document.createElement('div')
+    wrapper.id = 'fixtures'
+    document.body.appendChild(wrapper)
+    view.$el.appendTo($('#fixtures'))
+  })
+
+  afterEach(() => {
+    fakeENV.teardown()
+    jest.useRealTimers()
+    view.remove()
+    document.body.removeChild(wrapper)
+  })
+
+  it('renders tab and panel elements', () => {
+    // find the tabs
+    expect(view.$el.find('.collectionViewItems > li').length).toBe(2)
+    // find the panels
+    expect(view.$el.find('#tab-1').length).toBe(1)
+    expect(view.$el.find('#tab-2').length).toBe(1)
+  })
+
+  it('adds new GroupCategory and displays new tab and panel', () => {
+    categories.add(
+      new GroupCategory({
+        id: 3,
+        name: 'Newly Added',
+      })
+    )
+    expect(view.$el.find('.collectionViewItems > li').length).toBe(3)
+    expect(view.$el.find('#tab-3').length).toBe(1)
+  })
+
+  it('removes GroupCategory and removes tab and panel', () => {
+    categories.remove(categories.models[0])
+    expect(view.$el.find('.collectionViewItems > li').length).toBe(1)
+    expect(view.$el.find('#tab-1').length).toBe(0)
+    categories.remove(categories.models[0])
+    expect(view.$el.find('.collectionViewItems > li').length).toBe(0)
+    expect(view.$el.find('#tab-2').length).toBe(0)
+  })
+
+  it('loads tab panel content when tab is activated', () => {
+    // verify the content is not present before being activated
+    expect($('#tab-2').children().length).toBe(0)
+    // activate
+    view.$el.find('.group-category-tab-link:last').click()
+    expect($('#tab-2').children().length).toBeGreaterThan(0)
+  })
+})
---
+++ ui/shared/media-comments/jquery/__tests__/mediaCommentThumbnail.test.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2014 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $ from 'jquery'
+import 'jquery-migrate'
+import '../mediaCommentThumbnail'
+
+const awhile = () => new Promise(resolve => setTimeout(resolve, 2))
+
+describe('mediaCommentThumbnail', () => {
+  let $fixtures
+
+  beforeEach(() => {
+    window.INST = window.INST || {}
+    window.INST.kalturaSettings = {
+      resource_domain: 'example.com',
+      partner_id: '12345',
+    }
+
+    $fixtures = document.createElement('div')
+    $fixtures.id = 'fixtures'
+    document.body.appendChild($fixtures)
+
+    const mediaComment = $(`
+      <a
+        id="media_comment_23"
+        class="instructure_inline_media_comment video_comment"
+        href="/media_objects/23"
+        data-author="Tom"
+        data-created_at="Oct 22 at 7:10pm"
+      >
+        this is a media comment
+      </a>
+    `)
+    $('#fixtures').append(mediaComment)
+  })
+
+  afterEach(() => {
+    window.INST.kalturaSettings = null
+    document.body.removeChild($fixtures)
+  })
+
+  it('creates a thumbnail span with a background image URL generated from kaltura settings and media id', async () => {
+    // emulating the call from enhanceUserContent() in instructure.js
+    $('.instructure_inline_media_comment', '#fixtures').mediaCommentThumbnail('normal')
+    await awhile()
+    expect($('.media_comment_thumbnail', '#fixtures').length).toBe(1)
+    const bgImage = $('.media_comment_thumbnail', '#fixtures').first().css('background-image')
+    expect(bgImage).toContain(
+      `https://example.com/p/12345/thumbnail/entry_id/23/width/140/height/100/bgcolor/000000/type/2/vid_sec/5`
+    )
+  })
+
+  it('creates screenreader text describing media comment', async () => {
+    $('.instructure_inline_media_comment', '#fixtures').mediaCommentThumbnail('normal')
+    await awhile()
+    const screenreaderText = document.querySelector(
+      '.media_comment_thumbnail .screenreader-only'
+    ).innerText
+    expect(screenreaderText).toBe('Play media comment by Tom from Oct 22 at 7:10pm.')
+  })
+
+  it('creates generic screenreader text if no authoring info provided', async () => {
+    $('#fixtures').html('')
+    $('#fixtures').append(
+      $(`
+      <a
+        id="media_comment_23"
+        class="instructure_inline_media_comment video_comment"
+        href="/media_objects/23"
+      >
+        this is a media comment
+      </a>
+    `)
+    )
+    $('.instructure_inline_media_comment', '#fixtures').mediaCommentThumbnail('normal')
+    await awhile()
+    const screenreaderText = document.querySelector(
+      '.media_comment_thumbnail .screenreader-only'
+    ).innerText
+    expect(screenreaderText).toBe('Play media comment.')
+  })
+})
