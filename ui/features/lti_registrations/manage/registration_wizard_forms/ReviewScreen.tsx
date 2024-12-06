e34dab43b4998730074f3ec5724e79ed3155a4f6
Ryan Hawkins <ryan.hawkins@instructure.com>
2024-12-06 14:30:43 -0700

    Fix Misc LIME Phase 2 Issues
    what:
    - Specify a better error message for the domain on the launch settings
      screen
    - Default to an undefined admin nickname rather than an empty string.
      This plays better with the assumptions our code makes, as otherwise
      we think the admin genuinely gave us an empty string and we get some
      weird values, like on the Review Screen where we just have an empty admin
      nickname, even though we should default to the tool nickname.
    - Don't pass the admin nickname to the IconConfirmation screen as the
      name of the tool. It doesn't match with actual behavior, where the
      generated icon will depend on the tool's name, not the admin nickname.
      Also prevents a weird scenario where an empty admin nickname would
      result in the "I can't find that" image being displayed.
    - On the review screen, if there are no requested permissions and/or no
      placements with icons, display an appropriate message, instead of just
      leaving a blank space under the header.
    
    fixes INTEROP-9005
    fixes INTEROP-9007
    
    flag=lti_registrations_next
    
    test-plan:
    - Verify that each of these things are actually fixed on the Apps
      installation page.
    
    Change-Id: I685c65171b161a4b74c4bddb7eba754ffdfcd5ee
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/364789
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Paul Gray <paul.gray@instructure.com>
    QA-Review: Paul Gray <paul.gray@instructure.com>
    Product-Review: Ryan Hawkins <ryan.hawkins@instructure.com>

--- ui/features/lti_registrations/manage/registration_wizard_forms/ReviewScreen.tsx
+++ ui/features/lti_registrations/manage/registration_wizard_forms/ReviewScreen.tsx
@@ -56,7 +56,7 @@ export type ReviewScreenProps = {
   scopes: LtiScope[]
   privacyLevel: LtiPrivacyLevel
   placements: LtiPlacement[]
-  nickname: string
+  nickname?: string
   description?: string
   labels: Partial<Record<LtiPlacement, string>>
   iconUrls: Partial<Record<LtiPlacementWithIcon, string>>
@@ -108,11 +108,17 @@ export const ReviewScreen = ({
             itemSpacing="small"
             size="small"
           >
-            {scopes.map(scope => (
-              <List.Item key={scope}>
-                <Text size="small">{i18nLtiScope(scope)}</Text>
-              </List.Item>
-            ))}
+            {scopes.length === 0 ? (
+              <Text size="small" fontStyle="italic">
+                {I18n.t('No permissions requested')}
+              </Text>
+            ) : (
+              scopes.map(scope => (
+                <List.Item key={scope}>
+                  <Text size="small">{i18nLtiScope(scope)}</Text>
+                </List.Item>
+              ))
+            )}
           </List>
         </View>
         <IconButton
@@ -159,7 +165,9 @@ export const ReviewScreen = ({
               <Text size="small" weight="bold">
                 {I18n.t('Administration Nickname')}:{' '}
               </Text>
-              <Text size="small">{nickname ?? ''}</Text>
+              <Text size="small" fontStyle={nickname ? 'normal' : 'italic'}>
+                {nickname ?? I18n.t('No nickname provided.')}
+              </Text>
             </div>
             <div>
               <Text size="small" weight="bold">
@@ -199,55 +207,13 @@ export const ReviewScreen = ({
           onClick={onEditNaming}
         />
       </ReviewSection>
-      <ReviewSection>
-        <View>
-          <Heading level="h4">{I18n.t('Icon URLs')}</Heading>
-          <Flex direction="column" gap="x-small" margin="small 0 0 0">
-            {placements
-              .filter((p: LtiPlacement): p is LtiPlacementWithIcon =>
-                LtiPlacementsWithIcons.includes(p as LtiPlacementWithIcon)
-              )
-              .map(p => {
-                let status: string
-                const url = iconUrls[p]
-                const placementDefaultUrl = defaultPlacementIconUrls[p]
-                const usingDefaultIcon = // We're using the placement's default URL
-                  (!url && !!placementDefaultUrl) ||
-                  // We're using the top-level default Icon URL
-                  (!url && !!defaultIconUrl) ||
-                  // Our configured URL matches the placement's default URL, and
-                  // both URL's are actually present
-                  (url === placementDefaultUrl && !!url && !!placementDefaultUrl) ||
-                  // Our configured URL matches the top-level default URL, and both
-                  // URL's are actually present
-                  (url === defaultIconUrl && !!url && !!defaultIconUrl)
-                if (usingDefaultIcon) {
-                  status = I18n.t('Default Icon')
-                } else if (url) {
-                  status = I18n.t('Custom Icon')
-                } else if (!url && p === 'editor_button') {
-                  status = I18n.t('Generated Icon')
-                } else {
-                  status = I18n.t('No Icon')
-                }
-
-                return (
-                  <div key={p}>
-                    <Text size="small" weight="bold">
-                      {i18nLtiPlacement(p)}:{' '}
-                    </Text>
-                    <Text size="small">{status}</Text>
-                  </div>
-                )
-              })}
-          </Flex>
-        </View>
-        <IconButton
-          renderIcon={IconEditLine}
-          screenReaderLabel={I18n.t('Edit Icon URLs')}
-          onClick={onEditIconUrls}
-        />
-      </ReviewSection>
+      <IconUrlsReviewSection
+        placements={placements}
+        iconUrls={iconUrls}
+        defaultPlacementIconUrls={defaultPlacementIconUrls}
+        defaultIconUrl={defaultIconUrl}
+        onEditIconUrls={onEditIconUrls}
+      />
     </RegistrationModalBody>
   )
 }
@@ -376,6 +342,88 @@ export const LaunchSettingsSection = React.memo(
           renderIcon={IconEditLine}
           screenReaderLabel={I18n.t('Edit Launch Settings')}
           onClick={onEditLaunchSettings}
+        />
+      </ReviewSection>
+    )
+  }
+)
+
+export type IconUrlsReviewSectionProps = {
+  placements: LtiPlacement[]
+  iconUrls: ReviewScreenProps['iconUrls']
+  defaultPlacementIconUrls: ReviewScreenProps['defaultPlacementIconUrls']
+  defaultIconUrl?: string
+  onEditIconUrls: () => void
+}
+
+export const IconUrlsReviewSection = React.memo(
+  ({
+    placements,
+    iconUrls,
+    defaultPlacementIconUrls,
+    defaultIconUrl,
+    onEditIconUrls,
+  }: IconUrlsReviewSectionProps) => {
+    const placementsWithIcons = placements.filter((p): p is LtiPlacementWithIcon =>
+      LtiPlacementsWithIcons.includes(p as LtiPlacementWithIcon)
+    )
+
+    const body = (() => {
+      if (placementsWithIcons.length === 0) {
+        return (
+          <Text size="small" fontStyle="italic">
+            {I18n.t('No placements with icons')}
+          </Text>
+        )
+      } else {
+        return placementsWithIcons.map(placement => {
+          let status: string
+          const iconUrl = iconUrls[placement]
+          const defaultPlacementIconUrl = defaultPlacementIconUrls[placement]
+          const usingDefaultIcon = // We're using the placement's default URL
+            (!iconUrl && !!defaultPlacementIconUrl) ||
+            // We're using the top-level default Icon URL
+            (!iconUrl && !!defaultIconUrl) ||
+            // Our configured URL matches the placement's default URL, and
+            // both URL's are actually present
+            (iconUrl === defaultPlacementIconUrl && !!iconUrl && !!defaultPlacementIconUrl) ||
+            // Our configured URL matches the top-level default URL, and both
+            // URL's are actually present
+            (iconUrl === defaultIconUrl && !!iconUrl && !!defaultIconUrl)
+          if (usingDefaultIcon) {
+            status = I18n.t('Default Icon')
+          } else if (iconUrl) {
+            status = I18n.t('Custom Icon')
+          } else if (!iconUrl && placement === 'editor_button') {
+            status = I18n.t('Generated Icon')
+          } else {
+            status = I18n.t('No Icon')
+          }
+
+          return (
+            <div key={placement}>
+              <Text size="small" weight="bold">
+                {i18nLtiPlacement(placement)}:{' '}
+              </Text>
+              <Text size="small">{status}</Text>
+            </div>
+          )
+        })
+      }
+    })()
+
+    return (
+      <ReviewSection>
+        <View>
+          <Heading level="h4">{I18n.t('Icon URLs')}</Heading>
+          <Flex direction="column" gap="x-small" margin="small 0 0 0">
+            {body}
+          </Flex>
+        </View>
+        <IconButton
+          renderIcon={IconEditLine}
+          screenReaderLabel={I18n.t('Edit Icon URLs')}
+          onClick={onEditIconUrls}
         />
       </ReviewSection>
     )
