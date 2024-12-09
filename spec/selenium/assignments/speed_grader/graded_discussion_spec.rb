c945388d13ef2b125a559d004f639e1335a8eb83
Jacob DeWar <jacob.dewar@instructure.com>
2024-12-09 10:06:41 -0500

    spec: skip flaky gradebook spec
    refs EGG-411
    
    Test Plan: Jekins
    
    Change-Id: I95d69c752e2a27c597726a996fb70470d5018b56
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/364957
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Pedro Gutierrez <pedro.gutierrez@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

--- spec/selenium/assignments/speed_grader/graded_discussion_spec.rb
+++ spec/selenium/assignments/speed_grader/graded_discussion_spec.rb
@@ -153,7 +153,8 @@ describe "Screenreader Gradebook grading" do
       end
     end
 
-    it "next tab keeps focus" do
+    # EGG-411
+    xit "next tab keeps focus" do
       get "/courses/#{@course.id}/gradebook/speed_grader?assignment_id=#{@checkpointed_assignment.id}&student_id=#{@student2.id}&entry_id=#{@entry.id}"
 
       in_frame("speedgrader_iframe") do
