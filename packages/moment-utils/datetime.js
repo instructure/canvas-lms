3e89e5a8a7e5fec804307b35f315cdd7264f92f8
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-12-11 14:16:16 -0700

    refactor: remove circular dependency from moment-utils
    Change-Id: I78590553363ba0a61afb3f6b37d6fe7cf1985445
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/365277
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- packages/moment-utils/datetime.js
+++ packages/moment-utils/datetime.js
@@ -19,10 +19,73 @@
 import moment from 'moment-timezone'
 import timezone from 'timezone'
 import en_US from 'timezone/en_US'
-import parseDateTimeWithMoment, {
-  specifiesTimezone,
-  toRFC3339WithoutTZ,
-} from '@instructure/moment-utils'
+
+import getFormats from './formats'
+
+const config = {customI18nFormats: []}
+
+export function loadI18nFormats(customI18nFormats) {
+  config.customI18nFormats = customI18nFormats
+}
+
+// Parse a DateTime string according to any of the pre-defined formats. The
+// formats may come from Moment itself, or from the locale dictionary for
+// date and time.
+//
+// See ./formats.js for the possible formats.
+//
+// @param  {Object.<{
+//           t: (String) -> String,
+//           lookup: (String) -> String
+//         }>}
+//         An object that can query the locale dictionary for datetime formats.
+//
+// @param  {String}
+//         The string to parse.
+//
+// @return {Moment?}
+//         A moment instance in case the string could be parsed.
+export function parseDateTimeWithMoment(input, locale) {
+  const formats = getFormats(config)
+  const momentInstance = createDateTimeMoment(input, formats, locale)
+
+  return momentInstance.isValid() ? momentInstance : null
+}
+
+// Check a moment instance to see if its format (and input value) explicitly
+// specify a timezone. This query is useful to know whether the date needs to
+// be unfudged in case it does NOT specify a timezone (e.g. using tz-parse).
+export function specifiesTimezone(m) {
+  return !!(m._f.match(/Z/) && m._pf.unusedTokens.indexOf('Z') === -1)
+}
+
+export function toRFC3339WithoutTZ(m) {
+  return moment(m).locale('en').format('YYYY-MM-DD[T]HH:mm:ss')
+}
+
+// wrap's moment() for parsing datetime strings. assumes the string to be
+// parsed is in the profile timezone unless if contains an offset string
+// *and* a format token to parse it, and unfudges the result.
+export function createDateTimeMoment(input, format, locale) {
+  // ensure first argument is a string and second is a format or an array
+  // of formats
+  if (typeof input !== 'string' || !(typeof format === 'string' || Array.isArray(format))) {
+    throw new Error(
+      'createDateTimeMoment only works on string+format(s). just use moment() directly for any other signature'
+    )
+  }
+
+  const m = moment.apply(null, [input, format, locale])
+
+  if (m._pf.unusedTokens.length > 0) {
+    // we didn't use strict at first, because we want to accept when
+    // there's unused input as long as we're using all tokens. but if the
+    // best non-strict match has unused tokens, reparse with strict
+    return moment.apply(null, [input, format, locale, true])
+  } else {
+    return m
+  }
+}
 
 export function isMidnight(date) {
   if (date === null) {
