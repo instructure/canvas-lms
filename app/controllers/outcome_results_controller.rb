27e4f172792032d2111bc09575203010e58b4f6d
Szvetlin Tanyi <szvetlin.tanyi@instructure.com>
2024-12-13 12:28:08 +0100

    Rails cache key to include outcome ids
    The Rails cache did not include the outcome ids and in result wrong results
    were returned when filtering by outcome ids.
    
    flag = none
    
    refs: OUT-7094
    
    Test plan:
    - call the api/v1/courses/:course_id/outcome_rollups?outcome_ids=:outcome_id
     endpoint with the appropriate params
    
    Change-Id: I309be5788e177591e391a70c014b0b4bb5617a9f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/365432
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Martin Dubai <m.dubai@instructure.com>
    QA-Review: Martin Dubai <m.dubai@instructure.com>
    Product-Review: Martin Dubai <m.dubai@instructure.com>

--- app/controllers/outcome_results_controller.rb
+++ app/controllers/outcome_results_controller.rb
@@ -464,7 +464,11 @@ class OutcomeResultsController < ApplicationController
     #   app/controllers/quizzes/quizzes_controller.rb
     #   app/controllers/quizzes_next/quizzes_api_controller.rb
     #   app/controllers/application_controller.rb
-    [results_type, "context_uuid", @context.uuid, "current_user_uuid", @current_user.uuid, "account_uuid", @domain_root_account.uuid]
+
+    # If there are outcome ids in the params we need to take them into consideration when caching
+    outcome_ids_key = params[:outcome_ids].split(",").sort.join("|") if params[:outcome_ids].present?
+
+    [results_type, "context_uuid", @context.uuid, "current_user_uuid", @current_user.uuid, "account_uuid", @domain_root_account.uuid, outcome_ids_key].compact
   end
 
   # used in sLMGB/LMGB
