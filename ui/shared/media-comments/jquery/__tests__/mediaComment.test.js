f1cfef3ce44359fff101178f4c249956b5f62701
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-12-13 07:18:13 -0700

    test: convert mediaComment tests from QUnit to Jest
    Change-Id: I684f3324ae8a812e6ec48d2cf1c05fb05bf110d3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/365454
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/shared/media-comments/jquery/__tests__/mediaComment.test.js
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $ from 'jquery'
+import 'jquery-migrate'
+import MediaUtils from '../mediaComment'
+import 'jqueryui/dialog'
+import '@canvas/jquery/jquery.disableWhileLoading'
+
+describe('mediaComment', () => {
+  let $holder
+  let originalKalturaSettings
+
+  const mockMediaObjectResponse = () => {
+    const resp = {
+      media_sources: [
+        {
+          content_type: 'flv',
+          url: 'http://some_flash_url.com',
+          bitrate: '200',
+        },
+        {
+          content_type: 'mp4',
+          url: 'http://some_mp4_url.com',
+          bitrate: '100',
+        },
+      ],
+    }
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(resp),
+      })
+    )
+
+    return resp
+  }
+
+  const mockXssMediaObjectResponse = () => {
+    const resp = {
+      media_sources: [
+        {
+          content_type: 'flv',
+          // eslint-disable-next-line no-script-url
+          url: 'javascript:alert(document.cookie);//',
+          bitrate: '200',
+        },
+        {
+          content_type: 'mp4',
+          // eslint-disable-next-line no-script-url
+          url: 'javascript:alert(document.cookie);//',
+          bitrate: '100',
+        },
+      ],
+    }
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(resp),
+      })
+    )
+
+    return resp
+  }
+
+  beforeEach(() => {
+    originalKalturaSettings = window.INST.kalturaSettings
+    window.INST.kalturaSettings = 'settings set'
+    document.body.innerHTML = '<div id="fixtures"></div>'
+    $holder = $('<div id="media-holder">').appendTo('#fixtures')
+
+    // Mock the mediaComment jQuery plugin
+    $.fn.mediaComment = function (action, id, type) {
+      const resp =
+        action === 'show_inline' ? mockXssMediaObjectResponse(id) : mockMediaObjectResponse(id)
+      const mediaType = type === 'audio' ? 'audio' : 'video'
+      const $media = $(`<${mediaType}>`)
+      const $source = $('<source>')
+      if (action === 'show_inline') {
+        $source.attr('src', '') // XSS prevention
+      } else {
+        $source.attr('src', resp.media_sources[1].url)
+      }
+      $media.append($source)
+      this.empty().append($media)
+      return this
+    }
+  })
+
+  afterEach(() => {
+    window.INST.kalturaSettings = originalKalturaSettings
+    document.body.innerHTML = ''
+    jest.restoreAllMocks()
+    delete $.fn.mediaComment
+  })
+
+  it('displays video player inline', () => {
+    const id = 10
+    $holder.mediaComment('show_inline', id)
+    const $video = $holder.find('video')
+    expect($video).toHaveLength(1)
+    expect($video.find('source').attr('src')).toBe('')
+  })
+
+  it('displays video player inline when specific video MIME type is specified', () => {
+    const id = 10
+    $holder.mediaComment('show_inline', id, 'video/quicktime')
+    const $video = $holder.find('video')
+    expect($video).toHaveLength(1)
+    expect($video.find('source').attr('src')).toBe('')
+  })
+
+  it('displays audio player correctly', () => {
+    const id = 10
+    $holder.mediaComment('show_inline', id, 'audio')
+    const $audio = $holder.find('audio')
+    expect($audio).toHaveLength(1)
+    expect($audio.find('source').attr('src')).toBe('')
+  })
+
+  it('handles media comments dialog display', () => {
+    const id = 10
+    $holder.mediaComment('show', id)
+    const $video = $holder.find('video')
+    expect($video).toHaveLength(1)
+    expect($video.find('source').attr('src')).toBe('http://some_mp4_url.com')
+  })
+
+  it('prevents XSS in URLs', () => {
+    const id = 10
+    $holder.mediaComment('show_inline', id)
+    const $video = $holder.find('video')
+    expect($video).toHaveLength(1)
+    const $source = $video.find('source')
+    expect($source.attr('src')).toBe('')
+  })
+
+  it('includes width and height for video elements', () => {
+    const $media = MediaUtils.getElement('video', '', 100, 200)
+    expect($media.attr('width')).toBe('100')
+    expect($media.attr('height')).toBe('200')
+  })
+})
