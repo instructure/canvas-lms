b9bb2c3a00fa73adf283c01fc19758d66408b933
viktor.szpisjak <viktor.szpisjak@instructure.com>
2024-12-19 10:56:52 +0100

    Re-implement the content selection logic for import
    closes CLAB-875
    flag=instui_for_import_page
    
    test plan:
     - Turn on FF
     - Have a complicated course with various resource
       like, assignments, assignment groups, discussion,
       graded discussions, classic quiz, pages ect...
     - Export it
     - Import it again into empty courses and see if
       content selection work correctly
     ---
     - Play with the content selection modal, there are
       checked, unchecked and indeterminate states for
       the checkboxes
     - There are linked resources, like graded discussions
       have an assignment and a discussions, and if one of
       them is checked, the other also should be.
       Linked issues update also should update the
       parents/children state
    
    Change-Id: I12d7c94c8a34b195d33247f890a51f40067d470e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/366142
    Reviewed-by: Rajmund Csehil <rajmund.csehil@instructure.com>
    Reviewed-by: Tamas Barna <tamas.barna@instructure.com>
    QA-Review: Rajmund Csehil <rajmund.csehil@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Viktor Szpisják <viktor.szpisjak@instructure.com>

--- ui/features/content_migrations/react/components/utils.ts
+++ ui/features/content_migrations/react/components/utils.ts
@@ -16,6 +16,10 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import type {CheckboxTreeNode} from '@canvas/content-migrations'
+import type {Item} from './content_selection_modal'
+import type {GenericItemResponse, SelectiveDataRequest} from './types'
+
 export const humanReadableSize = (size: number): string => {
   const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   let i = 0
@@ -28,6 +32,104 @@ export const humanReadableSize = (size: number): string => {
 
 export const timeout = (delay: number) => {
   return new Promise(resolve => setTimeout(resolve, delay))
+}
+
+export const generateSelectiveDataResponse = (
+  migrationId: string,
+  userId: string,
+  checkboxTreeNodes: Record<string, CheckboxTreeNode>
+): SelectiveDataRequest => {
+  const nonRootElements: Record<string, Record<string, '1'>> = {}
+  const rootElements: Record<string, '1'> = {}
+
+  Object.values(checkboxTreeNodes).forEach(item => {
+    if (
+      item.migrationId &&
+      (item.checkboxState === 'indeterminate' || item.checkboxState === 'checked')
+    ) {
+      nonRootElements[item.type] ||= {}
+      nonRootElements[item.type][item.migrationId] = '1'
+    }
+
+    // These are the all_discussions, all_assignments, all_* elements
+    if (!item.migrationId && item.checkboxState === 'checked') {
+      const match = item.id.match(/\[(.*?)\]/)
+      const value = match ? match[1] : null
+      if (value) {
+        rootElements[value] = '1'
+      }
+    }
+  })
+
+  return {
+    id: migrationId,
+    user_id: userId,
+    workflow_state: 'waiting_for_select',
+    copy: {...rootElements, ...nonRootElements},
+  }
+}
+
+export const mapToCheckboxTreeNodes = (items: Item[], parentId?: string): Record<string, CheckboxTreeNode> => {
+  const checkboxTreeNodes: Record<string, CheckboxTreeNode> = {}
+
+  items.forEach(item => {
+    const {id, label, type, children, linkedId, checkboxState, migrationId} = item
+    checkboxTreeNodes[id] = {
+      id,
+      label,
+      type,
+      checkboxState,
+      childrenIds: children ? children.map(child => child.id) : [],
+    }
+
+    if (linkedId) {
+      checkboxTreeNodes[id].linkedId = linkedId
+    }
+
+    if (parentId) {
+      checkboxTreeNodes[id].parentId = parentId
+    }
+
+    if (migrationId) {
+      checkboxTreeNodes[id].migrationId = migrationId
+    }
+
+    if (children) {
+      Object.assign(checkboxTreeNodes, mapToCheckboxTreeNodes(children, id))
+    }
+  })
+
+  return checkboxTreeNodes
+}
+
+export const responseToItem = ({
+  type,
+  title,
+  property,
+  sub_items,
+  linked_resource,
+  migration_id,
+}: GenericItemResponse, translator: { t: Function }): Item => {
+  const base: Item = {
+    id: property,
+    label:
+      sub_items && sub_items.length > 0
+        ? translator.t('%{title} (%{count})', {title, count: sub_items.length})
+        : title,
+    checkboxState: 'unchecked',
+    migrationId: migration_id,
+    type,
+  }
+
+  if (sub_items && sub_items.length > 0) {
+    base.children = sub_items.map(sub_item => responseToItem(sub_item, translator))
+  }
+
+  if (linked_resource) {
+    base.linkedId = `copy[${linked_resource.type}][id_${linked_resource.migration_id}]`
+  }
+
+  return base
 }
 
 export {parseDateToISOString} from '@canvas/content-migrations'
