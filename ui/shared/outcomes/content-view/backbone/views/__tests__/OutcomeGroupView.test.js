1e45c691c52d39bc260fa733781899e47d2a9c8c
Michael Hulse <michael.hulse@instructure.com>
2024-12-18 20:14:12 -0800

    test: convert OutcomeGroupView tests from QUnit to Jest
    Change-Id: If33f97ae9496423a3462c8a68b3e80f40b96198f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/366130
    QA-Review: Michael Hulse <michael.hulse@instructure.com>
    Product-Review: Michael Hulse <michael.hulse@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/shared/outcomes/content-view/backbone/views/__tests__/OutcomeGroupView.test.js
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'jquery-migrate'
+import fakeENV from '@canvas/test-utils/fakeENV'
+import OutcomeContentBase from '../OutcomeContentBase'
+import OutcomeGroup from '../../../../backbone/models/OutcomeGroup'
+import OutcomeGroupView from '../OutcomeGroupView'
+
+// Stub RCE initialization
+OutcomeContentBase.prototype.readyForm = () => {}
+
+const createView = opts => {
+  const view = new OutcomeGroupView(opts)
+  view.$el.appendTo(document.getElementById('fixtures'))
+  return view.render()
+}
+
+describe('OutcomeGroupView', () => {
+  let container
+  let outcomeGroup
+
+  beforeEach(() => {
+    container = document.createElement('div')
+    container.id = 'fixtures'
+    document.body.appendChild(container)
+    fakeENV.setup()
+  })
+
+  afterEach(() => {
+    container.remove()
+    fakeENV.teardown()
+  })
+
+  describe('as a teacher', () => {
+    beforeEach(() => {
+      ENV.PERMISSIONS = {manage_outcomes: true}
+      outcomeGroup = new OutcomeGroup({
+        context_type: 'Course',
+        url: 'www.example.com',
+        context_id: 1,
+        parent_outcome_group: {subgroups_url: 'www.example.com'},
+        description: 'blah',
+        can_edit: true,
+      })
+    })
+
+    it('renders placeholder text properly for new outcome groups', () => {
+      const view = createView({
+        state: 'add',
+        model: outcomeGroup,
+      })
+      const titleInput = view.$('input[name="title"]')[0]
+      expect(titleInput.getAttribute('placeholder')).toBe('New Outcome Group')
+      view.remove()
+    })
+
+    it('validates title is present', () => {
+      const view = createView({
+        state: 'add',
+        model: outcomeGroup,
+      })
+      view.$('#outcome_group_title').val('')
+      expect(view.isValid()).toBe(false)
+      expect(view.errors.title).toBeTruthy()
+      view.remove()
+    })
+
+    it('displays move, edit, and delete buttons', () => {
+      const view = createView({
+        state: 'show',
+        model: outcomeGroup,
+      })
+      const moveButton = view.$('.move_group_button')
+      expect(moveButton).toHaveLength(1)
+      view.remove()
+    })
+
+    it('hides move, edit, and delete buttons when read only', () => {
+      const view = createView({
+        state: 'show',
+        model: outcomeGroup,
+        readOnly: true,
+      })
+      const moveButton = view.$('.move_group_button')
+      expect(moveButton).toHaveLength(0)
+      view.remove()
+    })
+  })
+
+  describe('as a student', () => {
+    beforeEach(() => {
+      ENV.PERMISSIONS = {manage_outcomes: false}
+      outcomeGroup = new OutcomeGroup({
+        context_type: 'Course',
+        url: 'www.example.com',
+        context_id: 1,
+        parent_outcome_group: {subgroups_url: 'www.example.com'},
+        description: 'blah',
+        can_edit: false,
+      })
+    })
+
+    it('does not display move, edit, and delete buttons', () => {
+      const view = createView({
+        state: 'show',
+        model: outcomeGroup,
+      })
+      const moveButton = view.$('.move_group_button')
+      expect(moveButton).toHaveLength(0)
+      view.remove()
+    })
+  })
+})
