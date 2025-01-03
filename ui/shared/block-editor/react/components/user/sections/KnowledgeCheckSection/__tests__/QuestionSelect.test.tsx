4026931db4143af6a45aba8c31dc64ed770a9770
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-01-03 12:38:24 -0700

    refactor: improve TS coverage on QuestionSelect test
    Change-Id: Ie4e836009965fca63307fb3b2f632a81f2823ede
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/366685
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/shared/block-editor/react/components/user/sections/KnowledgeCheckSection/__tests__/QuestionSelect.test.tsx
+++ ui/shared/block-editor/react/components/user/sections/KnowledgeCheckSection/__tests__/QuestionSelect.test.tsx
@@ -20,12 +20,113 @@ import React from 'react'
 import {render, screen, fireEvent, waitFor} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import QuestionSelect from '../QuestionSelect'
-import {
+
+import {type QuestionProps} from '../types'
+
+const testUnsupportedQuestion: QuestionProps = {
+  id: '1',
+  entry_editable: false,
+  entry_type: 'question',
+  points_possible: 1,
+  position: 1,
+  properties: {},
+  status: 'published',
+  stimulus_quiz_entry_id: '1',
+  entry: {
+    id: '1',
+    title: 'Unsupported Question',
+    answer_feedback: {},
+    calculator_type: 'basic',
+    feedback: {
+      neutral: '',
+      correct: '',
+      incorrect: '',
+    },
+    interaction_data: {
+      true_choice: 'True',
+      false_choice: 'False',
+    },
+    interaction_type_slug: 'true_false',
+    item_body: null,
+    properties: {},
+    scoring_algorithm: 'none',
+    scoring_data: {
+      value: true,
+    },
+  },
+}
+
+const testSupportedQuestion: QuestionProps = {
+  id: '2',
+  entry_editable: false,
+  entry_type: 'question',
+  points_possible: 1,
+  position: 2,
+  properties: {},
+  status: 'published',
+  stimulus_quiz_entry_id: '2',
+  entry: {
+    id: '2',
+    title: 'True or false?',
+    answer_feedback: {},
+    calculator_type: 'basic',
+    feedback: {
+      neutral: '',
+      correct: '',
+      incorrect: '',
+    },
+    interaction_data: {
+      true_choice: 'True',
+      false_choice: 'False',
+    },
+    interaction_type_slug: 'true_false',
+    item_body: 'The sky is blue',
+    properties: {},
+    scoring_algorithm: 'none',
+    scoring_data: {
+      value: true,
+    },
+  },
+}
+
+const testMissingValuesQuestion: QuestionProps = {
+  id: '3',
+  entry_editable: false,
+  entry_type: 'question',
+  points_possible: 1,
+  position: 3,
+  properties: {},
+  status: 'published',
+  stimulus_quiz_entry_id: '3',
+  entry: {
+    id: '3',
+    title: null,
+    answer_feedback: {},
+    calculator_type: 'basic',
+    feedback: {
+      neutral: '',
+      correct: '',
+      incorrect: '',
+    },
+    interaction_data: {
+      true_choice: 'True',
+      false_choice: 'False',
+    },
+    interaction_type_slug: 'true_false',
+    item_body: null,
+    properties: {},
+    scoring_algorithm: 'none',
+    scoring_data: {
+      value: true,
+    },
+  },
+}
+
+const testQuestions: QuestionProps[] = [
   testUnsupportedQuestion,
   testSupportedQuestion,
   testMissingValuesQuestion,
-  testQuestions,
-} from './testQuestions'
+]
 
 describe('QuestionSelect', () => {
   const mockOnSelect = jest.fn()
@@ -41,7 +142,6 @@ describe('QuestionSelect', () => {
   })
 
   it('calls onSelect when a QuestionToggle is clicked', async () => {
-    // @ts-expect-error
     render(<QuestionSelect onSelect={mockOnSelect} questions={testQuestions} />)
     const toggle = screen.getByText(testQuestions[0].entry.title ?? '')
     fireEvent.click(toggle)
@@ -49,28 +149,25 @@ describe('QuestionSelect', () => {
   })
 
   it('renders the correct question titles', () => {
-    // @ts-expect-error
     render(<QuestionSelect onSelect={mockOnSelect} questions={testQuestions} />)
-    expect(screen.getByText(testUnsupportedQuestion.entry.title)).toBeInTheDocument()
-    expect(screen.getByText(testSupportedQuestion.entry.title)).toBeInTheDocument()
+    expect(screen.getByText(testUnsupportedQuestion.entry.title ?? '')).toBeInTheDocument()
+    expect(screen.getByText(testSupportedQuestion.entry.title ?? '')).toBeInTheDocument()
     expect(screen.getByText(`Question ${testMissingValuesQuestion.position}`)).toBeInTheDocument()
   })
 
   it('filters questions based on title', () => {
-    // @ts-expect-error
     render(<QuestionSelect onSelect={mockOnSelect} questions={testQuestions} />)
     const input = screen.getByPlaceholderText('Search questions...')
     fireEvent.change(input, {target: {value: 'true or false?'}})
-    expect(screen.queryByText(testUnsupportedQuestion.entry.title)).not.toBeInTheDocument()
-    expect(screen.getByText(testSupportedQuestion.entry.title)).toBeInTheDocument()
+    expect(screen.queryByText(testUnsupportedQuestion.entry.title ?? '')).not.toBeInTheDocument()
+    expect(screen.getByText(testSupportedQuestion.entry.title ?? '')).toBeInTheDocument()
   })
 
   it('filters questions based on body', () => {
-    // @ts-expect-error
     render(<QuestionSelect onSelect={mockOnSelect} questions={testQuestions} />)
     const input = screen.getByPlaceholderText('Search questions...')
     fireEvent.change(input, {target: {value: 'blue'}})
-    expect(screen.queryByText(testUnsupportedQuestion.entry.title)).not.toBeInTheDocument()
-    expect(screen.getByText(testSupportedQuestion.entry.title)).toBeInTheDocument()
+    expect(screen.queryByText(testUnsupportedQuestion.entry.title ?? '')).not.toBeInTheDocument()
+    expect(screen.getByText(testSupportedQuestion.entry.title ?? '')).toBeInTheDocument()
   })
 })
