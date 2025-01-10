330c6c80bb094e06c25f2c2a6896d52911de553d
Xander Moffatt <xmoffatt@instructure.com>
2025-01-10 14:02:35 -0700

    send LTI 1.3 ContextCopy notice in course copy
    why
    * allows 1.3 tools to be properly notified when
    a course copy/content migration happens
    * partial support for LTI 1.3 Course Copy! more to come later
    
    closes INTEROP-9064
    flag=lti_context_copy_notice
    
    test plan:
    * add this line to app/services/lti/platform_notification_service.rb
    at the end of `send_notice_batch`:
    ```
    CanvasHttp.post(notice_handler.url, body: webhook_body,
    content_type: "application/json")
    ```
    * add an endpoint to your 1.3 test tool with these two snippets:
      * in routes.rb: `post 'content_migration' => 'application#test'`
      * in application_controller.rb:
      ```
      def test
        p "asdf"
        # change this URL to point to your Canvas!
        jwk_url = "http://canvas.docker/api/lti/security/jwks"
        key = PublicJwkService.new(jwk_url).public_jwk_set
        p params[:notices].pluck(:jwt)
           .map { |jwt| JSON::JWT.decode(jwt, key) }
        render json: { message: "success" }
      end
      ```
    * create a NoticeHandler in your Canvas rails console for the account
    and tool you want, something like this (change the url as needed):
    ```
    Lti::NoticeHandler.create!(
      context_external_tool_id: 1,
      account_id: 2,
      notice_type: "LtiContextCopyNotice",
      max_batch_size: 10,
      url: "http://lti13testtool.docker/content_migration"
    )
    ```
    * create a new course in the UI, go to it, and click Import Content
    * select Copy a Canvas Course and choose an existing course
    * make sure the migration succeeds
    * check the logs for the "asdf" message from the test tool and
    subsequent decoded notice JWT
    * it should correctly describe the new course and include the old
    course's lti_context_id in the origin_contexts claim
    
    Change-Id: I4d9dd58ab85307782462d0f19d9b6f5e6318a738
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367632
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>

---
+++ spec/models/lti/pns/lti_context_copy_notice_builder_spec.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require_relative "../../../spec_helper"
+
+RSpec.describe Lti::Pns::LtiContextCopyNoticeBuilder, type: :model do
+  let(:account) { account_model }
+  let(:developer_key) do
+    dk = DeveloperKey.new(
+      scopes: ["https://purl.imsglobal.org/spec/lti/scope/noticehandlers"],
+      account:
+    )
+    dk.save!
+    dk
+  end
+  let(:tool) do
+    ContextExternalTool.new(
+      name: "Test Tool",
+      url: "https://www.test.tool.com",
+      consumer_key: "key",
+      shared_secret: "secret",
+      settings: { "platform" => "canvas" },
+      account:,
+      developer_key:,
+      root_account: account
+    )
+  end
+
+  let(:params) do
+    {
+      course:,
+      source_course:,
+      copied_at:
+    }
+  end
+  let(:course) { course_model(account:) }
+  let(:source_course) { course_model(account:) }
+  let(:copied_at) { Time.now.utc.iso8601 }
+
+  describe "#initialize" do
+    it "raises an error if course is missing" do
+      expect { described_class.new(params.except(:course)) }.to raise_error(ArgumentError, "Missing required parameter: course")
+    end
+
+    it "raises an error if copied_at is missing" do
+      expect { described_class.new(params.except(:copied_at)) }.to raise_error(ArgumentError, "Missing required parameter: copied_at")
+    end
+  end
+
+  describe "#build" do
+    subject { described_class.new(params).build(tool) }
+
+    before do
+      allow(LtiAdvantage::Messages::JwtMessage).to receive(:create_jws).and_return("signed_jwt")
+      allow(Rails.application.routes.url_helpers).to receive(:lti_notice_handlers_url).and_return("https://example.com/notice_handler")
+    end
+
+    it "returns a notice" do
+      Timecop.freeze do
+        expect(subject).to eq({ jwt: "signed_jwt" })
+        expect(LtiAdvantage::Messages::JwtMessage).to have_received(:create_jws).with(
+          {
+            "aud" => developer_key.global_id.to_s,
+            "azp" => developer_key.global_id.to_s,
+            "exp" => Time.zone.now.to_i + 3600,
+            "iat" => Time.zone.now.to_i,
+            "iss" => "https://canvas.instructure.com",
+            "nonce" => anything,
+            "https://purl.imsglobal.org/spec/lti/claim/deployment_id" => tool.deployment_id.to_s,
+            "https://purl.imsglobal.org/spec/lti/claim/context" =>
+            {
+              id: course.lti_context_id,
+              label: course.course_code,
+              title: course.name,
+              type: ["http://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering"]
+            },
+            "https://purl.imsglobal.org/spec/lti/claim/notice" =>
+            {
+              "id" => anything,
+              "timestamp" => copied_at,
+              "type" => "LtiContextCopyNotice"
+            },
+            "https://purl.imsglobal.org/spec/lti/claim/origin_contexts" => [source_course.lti_context_id],
+            "https://purl.imsglobal.org/spec/lti/claim/version" => "1.3.0",
+          },
+          anything
+        )
+      end
+    end
+
+    context "when source_course is absent" do
+      let(:params) { { course:, copied_at: } }
+
+      it "does not include origin_contexts" do
+        subject
+        expect(LtiAdvantage::Messages::JwtMessage).to have_received(:create_jws).with(
+          hash_excluding("https://purl.imsglobal.org/spec/lti/claim/origin_contexts"),
+          anything
+        )
+      end
+    end
+  end
+end
