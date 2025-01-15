afcc4c5a996da7f62b50ffbf22fc647e20f1e030
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-01-15 13:47:46 -0700

    Use createRoot in account_course_user_search
    flag=none
    refs CFA-70
    
    Test Plan:
    - specs pass
    
    Change-Id: Ie1b021ed5e14e09b58c0c136600464457fd09b47
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367606
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- ui/features/account_course_user_search/index.jsx
+++ ui/features/account_course_user_search/index.jsx
@@ -18,7 +18,7 @@
 
 import $ from 'jquery'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import App from './react/index'
 import router from './react/router'
 import configureStore from './react/store/configureStore'
@@ -71,13 +71,14 @@ ready(() => {
   initializeTopNavPortal({getBreadCrumbSetter: setFunction})
 
   const content = document.getElementById('content')
+  const root = createRoot(content)
+
   store.subscribe(() => {
     const tabState = store.getState().tabList
     const selectedTab = tabState.tabs[tabState.selected]
     updateDocumentTitleBreadcrumbAndActiveTab(selectedTab, () => setBreadCrumb)
 
-    // eslint-disable-next-line no-restricted-properties
-    ReactDOM.render(<App {...props} />, content)
+    root.render(<App {...props} />)
   })
 
   router.start(store)
