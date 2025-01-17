14c924dd112b51cb048adee2dd7adcd5eb50ebef
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-01-16 17:43:17 -0700

    skip fickle CreateFromTemplate tests
    Change-Id: I0c6d1fbb6a6109150cea69a02538d0909bb4c2e6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367705
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/shared/block-editor/react/__tests__/CreateFromTemplate.test.tsx
+++ ui/shared/block-editor/react/__tests__/CreateFromTemplate.test.tsx
@@ -55,7 +55,7 @@ jest.mock('@craftjs/core', () => {
 })
 
 const renderComponent = async () => {
-  const rendered = render(<CreateFromTemplate course_id="1" noBlocks={true}/>)
+  const rendered = render(<CreateFromTemplate course_id="1" noBlocks={true} />)
 
   await waitFor(() => {
     expect(document.querySelectorAll('.block-template-preview-card')).toHaveLength(totalPages)
@@ -111,7 +111,8 @@ describe('CreateFromTemplate', () => {
     )
   })
 
-  it('filters on the tags', async () => {
+  // fickle; passes individually
+  it.skip('filters on the tags', async () => {
     await renderComponent()
     user.click(getByTagText('General Content').closest('button') as HTMLButtonElement)
 
@@ -133,9 +134,9 @@ describe('CreateFromTemplate', () => {
   it('filters on the menu of tags', async () => {
     await renderComponent()
 
-    expect(document.querySelectorAll('[role="menuitemcheckbox"][aria-checked="true"]')).toHaveLength(
-      0,
-    )
+    expect(
+      document.querySelectorAll('[role="menuitemcheckbox"][aria-checked="true"]'),
+    ).toHaveLength(0)
 
     user.click(screen.getByText('Apply Filters').closest('button') as HTMLButtonElement)
 
