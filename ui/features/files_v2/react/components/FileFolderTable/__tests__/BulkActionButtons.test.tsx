4edbe103badad8a667138c7b22a0bdfe44659b8e
alvaro.talavera <alvaro.talavera@instructure.com>
2025-01-19 17:26:43 -0300

    Create Bulk Actions Buttons
    closes RCX-2527
    
    flag=files_a11y_rewrite
    
    test plan:
    - course files page
    - select at least two rows
    - check that the bulk actions menu appears
    - check for requested permission (edit, delete)
      related buttons must be shown or hidden
    
    Change-Id: Iac288eb8a5e8c8953812e24819fe1c5598879f99
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367794
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Alvaro Talavera <alvaro.talavera@instructure.com>

---
+++ ui/features/files_v2/react/components/FileFolderTable/__tests__/BulkActionButtons.test.tsx
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, screen, fireEvent, waitFor} from '@testing-library/react'
+import BulkActionButtons from '../BulkActionButtons'
+
+let defaultProps: any
+describe('BulkActionButtons', () => {
+  beforeEach(() => {
+    defaultProps = {
+      selectedRows: new Set(['1', '2']),
+      totalRows: 10,
+      userCanEditFilesForContext: true,
+      userCanDeleteFilesForContext: true,
+    }
+  })
+
+  it('renders component with all options enabled', async () => {
+    render(<BulkActionButtons {...defaultProps} />)
+    expect(screen.getByText('2 of 10 selected')).toBeInTheDocument()
+    expect(screen.getByTestId('bulk-actions-delete-button')).toBeInTheDocument()
+
+    const moreButton = screen.getByTestId('bulk-actions-more-button')
+    fireEvent.click(moreButton)
+
+    await waitFor(() => {
+      expect(screen.getByTestId('bulk-actions-manage-access-button')).toBeInTheDocument()
+      expect(screen.getByTestId('bulk-actions-move-button')).toBeInTheDocument()
+    })
+  })
+
+  it('does not render delete button when userCanDeleteFilesForContext is false', () => {
+    defaultProps.userCanDeleteFilesForContext = false
+    render(<BulkActionButtons {...defaultProps} />)
+    expect(screen.queryByTestId('bulk-actions-delete-button')).toBeNull()
+  })
+
+  it('does not render manage access and move to when userCanEditFilesForContext is false', async () => {
+    defaultProps.userCanEditFilesForContext = false
+    render(<BulkActionButtons {...defaultProps} />)
+
+    const moreButton = screen.getByTestId('bulk-actions-more-button')
+    fireEvent.click(moreButton)
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('bulk-actions-manage-access-button')).toBeNull()
+      expect(screen.queryByTestId('bulk-actions-move-button')).toBeNull()
+    })
+  })
+
+  it('renders disabled buttons when no selection', async () => {
+    defaultProps.selectedRows = new Set()
+    render(<BulkActionButtons {...defaultProps} />)
+    expect(screen.getByText('0 selected')).toBeInTheDocument()
+
+    const button = screen.getByTestId('bulk-actions-more-button')
+    expect(button.closest('button')).toHaveAttribute('disabled')
+  })
+})
