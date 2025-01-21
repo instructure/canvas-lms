c6a5aac55f04c41a55ea7d009fecc6b350e9d4e0
Maya Tyner <maya.tyner@instructure.com>
2025-01-21 09:07:52 -0700

    update NewCourseModal for 5VPAT
    this commit also includes a new selenium
    test for required fields only
    
    refs FOO-4956
    flag=none
    
    test-plan:
    - navigate to accounts/:id
    - add new course
    - can create course with only req fields
    - can create course with all fields
    - when required fields aren't filled
    	- focus set to empty fields
    - when create fails
    	- error message and focus on field
    
    Change-Id: Ie6ca22a7a5247cf0381b1eb84e82d89ebd88ccb7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367881
    Reviewed-by: Adrián Grúber <adrian.gruber@instructure.com>
    QA-Review: Adrián Grúber <adrian.gruber@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Maya Tyner <maya.tyner@instructure.com>

--- ui/features/account_course_user_search/react/components/NewCourseModal.tsx
+++ ui/features/account_course_user_search/react/components/NewCourseModal.tsx
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import React, {useState} from 'react'
 import {node} from 'prop-types'
 import Modal from '@canvas/instui-bindings/react/InstuiModal'
@@ -26,24 +25,39 @@ import {TextInput} from '@instructure/ui-text-input'
 import {propType as termsPropType} from '../store/TermsStore'
 import SearchableSelect from './SearchableSelect'
 import {useScope as createI18nScope} from '@canvas/i18n'
-import {Text} from '@instructure/ui-text'
 import CoursesStore from '../store/CoursesStore'
 import AccountsTreeStore from '../store/AccountsTreeStore'
 import {showFlashAlert, showFlashError} from '@canvas/alerts/react/FlashAlert'
-import preventDefault from '@canvas/util/preventDefault'
 import {flatten} from 'lodash'
 import {clearDashboardCache} from '../../../../shared/dashboard-card/dashboardCardQueries'
+import * as z from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {useForm, Controller} from 'react-hook-form'
+import {getFormErrorMessage} from '@canvas/forms/react/react-hook-form/utils'
+import {Term, Course} from '../../../../api'
+
+type Account = {
+  id: string,
+  name: string,
+  subAccounts: Account[],
+}
+
+type TermList = {
+  data: Term[],
+  loading: boolean
+}
 
 const I18n = createI18nScope('account_course_user_search')
 
 const nonBreakingSpace = '\u00a0'
-const renderAccountOptions = (accounts = [], depth = 0) =>
+
+const renderAccountOptions = (accounts: Account[] = [], depth = 0) : {id: string, name: string}[] =>
   flatten(
     accounts.map(account =>
       [{id: account.id, name: Array(2 * depth + 1).join(nonBreakingSpace) + account.name}].concat(
-        renderAccountOptions(account.subAccounts || [], depth + 1),
-      ),
-    ),
+        renderAccountOptions(account.subAccounts || [], depth + 1)
+      )
+    )
   )
 
 NewCourseModal.propTypes = {
@@ -51,31 +65,35 @@ NewCourseModal.propTypes = {
   children: node.isRequired,
 }
 
-const emptyData = {name: '', course_code: ''}
+const validationSchema = z.object({
+  name: z
+    .string()
+    .min(1, I18n.t('Course name is required.')),
+  reference_code: z.string().min(1, I18n.t('Reference code is required.')),
+})
 
-export default function NewCourseModal({terms, children}) {
+export default function NewCourseModal({terms, children}: {terms: TermList, children: React.ReactNode}) {
   const [isOpen, setIsOpen] = useState(false)
-  const [data, setData] = useState(emptyData)
-  const [errors, setErrors] = useState({})
+  const [account, setAccount] = useState('')
+  const [enrollmentTerm, setEnrollmentTerm] = useState('')
+  const defaultValues = {name: '', reference_code: ''}
+  const {
+    formState: {errors},
+    control,
+    handleSubmit,
+    setFocus,
+  } = useForm({defaultValues, resolver: zodResolver(validationSchema)})
 
   const accountTree = AccountsTreeStore.getTree()
 
   function closeModal() {
     setIsOpen(false)
-    setData(emptyData)
-    setErrors({})
+    setAccount('')
+    setEnrollmentTerm('')
   }
 
-  function onSubmit() {
-    const errs = {}
-    if (!data.name) errs.name = I18n.t('Course name is required')
-    if (!data.course_code) errs.course_code = I18n.t('Reference code is required')
-    if (Object.keys(errs).length > 0) {
-      setErrors(errs)
-      return
-    }
-
-    const successHandler = createdCourse => {
+  function onSubmit({name, reference_code}: typeof defaultValues) {
+    const successHandler = (createdCourse: Course) => {
       closeModal()
       showFlashAlert({
         type: 'success',
@@ -92,25 +110,25 @@ export default function NewCourseModal({terms, children}) {
       }
     }
 
-    const errorHandler = showFlashError(
-      I18n.t('Something went wrong creating the course. Please try again.')
-    )
-
-    CoursesStore.create({course: data}, successHandler, errorHandler)
-  }
-
-  function onChange(field) {
-    return function (e, value) {
-      setData(oldState => ({...oldState, [field]: value.id || value}))
-      setErrors({})
+    const errorHandler = () => {
+      showFlashError(
+        I18n.t('Something went wrong creating the course. Please try again.')
+      )()
+      setFocus('name')
     }
+
+    const accountValue = account === '' ? undefined : account
+    const enrollmentTermValue = enrollmentTerm === '' ? undefined : enrollmentTerm
+    const data = {name: name, course_code: reference_code, account_id: accountValue, enrollment_term_id: enrollmentTermValue}
+    CoursesStore.create({course: data}, successHandler, errorHandler)
   }
 
   return (
     <span>
       <Modal
         as="form"
-        onSubmit={preventDefault(onSubmit)}
+        noValidate={true}
+        onSubmit={handleSubmit(onSubmit)}
         open={isOpen}
         onDismiss={closeModal}
         onOpen={() => AccountsTreeStore.loadTree()}
@@ -120,27 +138,37 @@ export default function NewCourseModal({terms, children}) {
       >
         <Modal.Body>
           <FormFieldGroup layout="stacked" rowSpacing="small" description="">
-            <TextInput
-              renderLabel={I18n.t('Course Name')}
-              value={data.name}
-              onChange={onChange('name')}
-              isRequired={true}
-              messages={errors.name && [{type: 'error', text: errors.name}]}
+            <Controller
+              name="name"
+              control={control}
+              render={({field}) => (
+                <TextInput
+                  {...field}
+                  data-testid="courseName"
+                  renderLabel={I18n.t('Course Name')}
+                  isRequired={true}
+                  messages={getFormErrorMessage(errors, 'name')}
+                />
+              )}
             />
-
-            <TextInput
-              renderLabel={I18n.t('Reference Code')}
-              value={data.course_code}
-              onChange={onChange('course_code')}
-              isRequired={true}
-              messages={errors.course_code && [{type: 'error', text: errors.course_code}]}
+            <Controller
+              name="reference_code"
+              control={control}
+              render={({field}) => (
+                <TextInput
+                  {...field}
+                  data-testid="referenceCode"
+                  renderLabel={I18n.t('Reference Code')}
+                  isRequired={true}
+                  messages={getFormErrorMessage(errors, 'reference_code')}
+                />
+              )}
             />
-
             <SearchableSelect
               id="accountSelector"
               label={I18n.t('Subaccount')}
               isLoading={accountTree.loading}
-              onChange={onChange('account_id')}
+              onChange={(_e, option) => setAccount(option.id)}
             >
               {renderAccountOptions(accountTree.accounts).map(account => (
                 <SearchableSelect.Option key={account.id} id={account.id} value={account.id}>
@@ -148,12 +176,11 @@ export default function NewCourseModal({terms, children}) {
                 </SearchableSelect.Option>
               ))}
             </SearchableSelect>
-
             <SearchableSelect
               id="termSelector"
               label={I18n.t('Enrollment Term')}
               isLoading={terms.loading}
-              onChange={onChange('enrollment_term_id')}
+              onChange={(_e, option) => setEnrollmentTerm(option.id)}
             >
               {(terms.data || []).map(term => (
                 <SearchableSelect.Option key={term.id} id={term.id} value={term.id}>
@@ -165,16 +192,16 @@ export default function NewCourseModal({terms, children}) {
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={closeModal}>{I18n.t('Cancel')}</Button> &nbsp;
-          <Button type="submit" color="primary">
+          <Button data-testid="submitBtn" type="submit" color="primary">
             {I18n.t('Add Course')}
           </Button>
         </Modal.Footer>
       </Modal>
       {React.Children.map(children, child =>
+        React.isValidElement(child) &&
         // when you click whatever is the child element to this, open the modal
-        React.cloneElement(child, {
-          onClick: (...args) => {
-            if (child.props.onClick) child.props.onClick(...args)
+        React.cloneElement(child as React.ReactElement<any>, {
+          onClick: () => {
             setIsOpen(true)
           },
         })
