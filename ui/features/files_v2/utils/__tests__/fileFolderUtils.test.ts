cd39b55c45129b7c1962f4a6a4b8d6c2931e495d
Jacob DeWar <jacob.dewar@instructure.com>
2025-01-22 10:37:03 -0500

    Clear table selection on pagination
    closes RCX-2856
    flag=files_a11y_rewrite
    
    Test Plan:
     - Have 51+ files/folders in a course
     - Go to Course Files
     - Select some items in the table
     - Go to Page 2
     * Verify the Select All checkbox is in None state
    
     - Also fixed an issue with File and Folder key collision
     - Have a File and Folder with same id in course
     - Go to Course Files
     * No console error warning about key collision
    
    Change-Id: Ie4d49d2b0c9bd0e789673f64c463c9bdb1b5464e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/368043
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Luis Hilario <luis.hilario@instructure.com>
    QA-Review: Luis Hilario <luis.hilario@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

---
+++ ui/features/files_v2/utils/__tests__/fileFolderUtils.test.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {isFile, getUniqueId} from '../fileFolderUtils'
+import {FAKE_FILES, FAKE_FOLDERS} from '../../fixtures/fakeData'
+
+describe('isFile', () => {
+  it('returns true for a file', () => {
+    expect(isFile(FAKE_FILES[0])).toBe(true)
+  })
+
+  it('returns false for a folder', () => {
+    expect(isFile(FAKE_FOLDERS[0])).toBe(false)
+  })
+})
+
+describe('getUniqueId', () => {
+  it('returns the correct key for a file', () => {
+    expect(getUniqueId(FAKE_FILES[0])).toBe(FAKE_FILES[0].uuid)
+  })
+
+  it('returns the correct key for a folder', () => {
+    expect(getUniqueId(FAKE_FOLDERS[0])).toBe(FAKE_FOLDERS[0].id)
+  })
+})
