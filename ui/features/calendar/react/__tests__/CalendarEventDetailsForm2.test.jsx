db577f8a945f340f408f180ae800fc79704f4d57
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-01-21 20:10:00 -0700

    split out, stabilize CalendarEventDetailsForm2.test.jsx (refs CFA-71)
    Change-Id: Ic50de707363fb9784370f9d4d83888251eda4910
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367982
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/calendar/react/__tests__/CalendarEventDetailsForm2.test.jsx
+++ ui/features/calendar/react/__tests__/CalendarEventDetailsForm2.test.jsx
@@ -20,12 +20,12 @@ import React from 'react'
 import $ from 'jquery'
 import moment from 'moment-timezone'
 import {act, fireEvent, render, waitFor} from '@testing-library/react'
-import {screen} from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
-import {eventFormProps, conference, userContext, courseContext, accountContext} from './mocks'
+import {eventFormProps} from './mocks'
 import CalendarEventDetailsForm from '../CalendarEventDetailsForm'
 import commonEventFactory from '@canvas/calendar/jquery/CommonEvent/index'
 import * as UpdateCalendarEventDialogModule from '@canvas/calendar/react/RecurringEvents/UpdateCalendarEventDialog'
+import fakeENV from '@canvas/test-utils/fakeENV'
 
 jest.mock('@canvas/calendar/jquery/CommonEvent/index')
 jest.mock('@canvas/calendar/react/RecurringEvents/UpdateCalendarEventDialog')
@@ -48,58 +48,34 @@ const setTime = (component, testid, time) => {
   return clock
 }
 
-const testTimezone = async (timezone, inputDate, expectedDate, time) => {
-  defaultProps.timezone = timezone
-  const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-  const date = changeValue(component, 'edit-calendar-event-form-date', inputDate)
-  expect(date.value).toBe('Thu, Jul 14, 2022')
-  if (time) setTime(component, 'event-form-end-time', time)
-  component.getByText('Submit').click()
-
-  waitFor(() =>
-    expect(defaultProps.event.save).toHaveBeenCalledWith(
-      expect.objectContaining({
-        'calendar_event[start_at]': expectedDate,
-      }),
-      expect.any(Function),
-      expect.any(Function),
-    ),
-  )
-  await waitFor(() => expect(defaultProps.closeCB).toHaveBeenCalled())
-}
-
-const testBlackoutDateSuccess = () => {
-  const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-  component.getByText('Add to Course Pacing blackout dates').click()
-  component.getByText('Submit').click()
-  expect(defaultProps.event.save).toHaveBeenCalledWith(
-    expect.objectContaining({
-      'calendar_event[blackout_date]': true,
-    }),
-    expect.any(Function),
-    expect.any(Function),
-  )
-  defaultProps.event.contextInfo = userContext
-  defaultProps.event.blackout_date = false
-}
-
-const expectFieldsToBeEnabled = (component, fieldNames) => {
-  fieldNames.forEach(fieldName => expect(component.getByText(fieldName)).toBeEnabled())
-}
-
-const expectFieldsToBeDisabled = (component, fieldNames) => {
-  fieldNames.forEach(fieldName => expect(component.getByLabelText(fieldName)).toBeDisabled())
-}
-
 describe('CalendarEventDetailsForm', () => {
   beforeEach(() => {
     defaultProps = eventFormProps()
-    commonEventFactory.mockImplementation(
-      jest.requireActual('@canvas/calendar/jquery/CommonEvent/index').default,
-    )
+    fakeENV.setup({
+      FEATURES: {
+        calendar_series: true,
+        account_level_blackout_dates: true,
+        course_paces: true,
+        k5_course_welcome_pages: true,
+        important_dates: true,
+      },
+      TIMEZONE: 'America/Denver',
+    })
+    commonEventFactory.mockImplementation(() => ({
+      possibleContexts: () => [],
+      isNewEvent: () => false,
+      startDate: () => moment(),
+      endDate: () => moment(),
+      allDay: () => false,
+      multipleDates: () => false,
+      calendarEvent: {
+        important_dates: false,
+        blackout_date: false,
+      },
+      save: jest.fn().mockResolvedValue({}),
+    }))
     $.ajaxJSON = (_url, _method, _params, onSuccess, _onError) => {
-      onSuccess({})
+      onSuccess([])
     }
     jest
       .spyOn(UpdateCalendarEventDialogModule, 'renderUpdateCalendarEventDialog')
@@ -107,6 +83,7 @@ describe('CalendarEventDetailsForm', () => {
   })
 
   afterEach(() => {
+    fakeENV.teardown()
     jest.clearAllMocks()
   })
 
@@ -204,236 +181,5 @@ describe('CalendarEventDetailsForm', () => {
     const errMessage = component.queryByText('This date is invalid.')
     expect(errMessage).not.toBeInTheDocument()
     expect(component.getByRole('button', {name: 'Submit'})).toBeEnabled()
-  })
-
-  it('renders and updates an event with conferencing when it is available', async () => {
-    defaultProps.event.webConference = conference
-    const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-    expect(component.getByText('Conferencing')).toBeInTheDocument()
-    component.getByText('Submit').click()
-    expect(defaultProps.event.save).toHaveBeenCalledWith(
-      expect.objectContaining({
-        'calendar_event[web_conference][conference_type]': 'BigBlueButton',
-        'calendar_event[web_conference][name]': 'BigBlueButton',
-      }),
-      expect.any(Function),
-      expect.any(Function),
-    )
-  })
-
-  it('can remove conferences when conferencing is available', async () => {
-    defaultProps.event.webConference = conference
-    const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-    component.getByText('Remove conference: Conference').click()
-    component.getByText('Submit').click()
-    expect(defaultProps.event.save).toHaveBeenCalledWith(
-      expect.objectContaining({
-        'calendar_event[web_conference]': '',
-      }),
-      expect.any(Function),
-      expect.any(Function),
-    )
-  })
-
-  it('renders and updates an event with important dates checkbox when context is a k5 subject', async () => {
-    const event = defaultProps.event
-    event.contextInfo.k5_course = true
-    const component = render(<CalendarEventDetailsForm {...defaultProps} event={event} />)
-
-    component.getByText('Mark as Important Date').click()
-    component.getByText('Submit').click()
-    expect(defaultProps.event.save).toHaveBeenCalledWith(
-      expect.objectContaining({
-        'calendar_event[important_dates]': true,
-      }),
-      expect.any(Function),
-      expect.any(Function),
-    )
-  })
-
-  it('renders and updates an event with important dates checkbox when context is a k5 account', async () => {
-    const event = defaultProps.event
-    event.contextInfo.k5_account = true
-    const component = render(<CalendarEventDetailsForm {...defaultProps} event={event} />)
-
-    component.getByText('Mark as Important Date').click()
-    component.getByText('Submit').click()
-    expect(defaultProps.event.save).toHaveBeenCalledWith(
-      expect.objectContaining({
-        'calendar_event[important_dates]': true,
-      }),
-      expect.any(Function),
-      expect.any(Function),
-    )
-  })
-
-  it('can create a blackout date event for a course with course pacing enabled', async () => {
-    ENV.FEATURES.account_level_blackout_dates = true
-    defaultProps.event.contextInfo = courseContext
-    testBlackoutDateSuccess()
-  })
-
-  it('can create a blackout date event for an account with course pacing enabled', async () => {
-    ENV.FEATURES.account_level_blackout_dates = true
-    defaultProps.event.contextInfo = accountContext
-    testBlackoutDateSuccess()
-  })
-
-  it('does not render blackout checkbox when the feature flag is off', async () => {
-    ENV.FEATURES.account_level_blackout_dates = false
-    defaultProps.event.contextInfo = courseContext
-    const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-    expect(
-      component.queryByRole('checkbox', {name: 'Add to Course Pacing blackout dates'}),
-    ).not.toBeInTheDocument()
-    defaultProps.event.contextInfo = userContext
-  })
-
-  it('does not render blackout checkbox in a user context', async () => {
-    const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-    expect(
-      component.queryByRole('checkbox', {name: 'Add to Course Pacing blackout dates'}),
-    ).not.toBeInTheDocument()
-  })
-
-  it('only enables relevant fields when blackout date checkbox is checked', async () => {
-    ENV.FEATURES.account_level_blackout_dates = true
-    const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-
-    expectFieldsToBeEnabled(component, [
-      'Title',
-      'Location',
-      'Date',
-      'From',
-      'To',
-      'Calendar',
-      'More Options',
-      'Submit',
-    ])
-
-    component.getByText('Calendar').click()
-    component.getByText('Geometry').click()
-    component.getByText('Add to Course Pacing blackout dates').click()
-
-    expectFieldsToBeEnabled(component, ['Title', 'Date', 'Calendar', 'More Options', 'Submit'])
-    expectFieldsToBeDisabled(component, ['Location', 'From', 'To'])
-  })
-
-  it('does not render the location input for child (section) events', () => {
-    const props = {...defaultProps}
-    props.event.calendarEvent.parent_event_id = '133'
-    const {getByText, queryByText} = render(<CalendarEventDetailsForm {...props} />)
-    expect(getByText('Title')).toBeInTheDocument()
-    expect(queryByText('Location')).not.toBeInTheDocument()
-  })
-
-  describe('frequency picker', () => {
-    beforeEach(() => {
-      defaultProps.event.isNewEvent = () => true
-      commonEventFactory.mockImplementation(() => defaultProps.event)
-    })
-
-    afterEach(() => {
-      defaultProps.event.isNewEvent = () => false
-      jest.resetModules()
-    })
-
-    it('renders when creating', async () => {
-      const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-      expect(component.queryByRole('combobox', {name: 'Frequency'})).toBeInTheDocument()
-    })
-
-    it('renders when editing', async () => {
-      defaultProps.event.isNewEvent = () => false
-      const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-      expect(component.queryByRole('combobox', {name: 'Frequency'})).toBeInTheDocument()
-    })
-
-    it('with option selected contains RRULE on submit', async () => {
-      const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-      component.getByText('Frequency').click() // open the dropdown
-      component.getByText('Daily').click() // select the option
-      component.getByText('Submit').click()
-      expect(defaultProps.closeCB).toHaveBeenCalled()
-      expect(defaultProps.event.save).toHaveBeenCalledWith(
-        expect.objectContaining({
-          'calendar_event[rrule]': 'FREQ=DAILY;INTERVAL=1;COUNT=365',
-        }),
-        expect.any(Function),
-        expect.any(Function),
-      )
-    })
-
-    it('with not-repeat option selected does not contain RRULE on submit', async () => {
-      const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-      component.getByText('Submit').click()
-
-      expect(defaultProps.event.save).toHaveBeenCalledWith(
-        expect.not.objectContaining({
-          'calendar_event[rrule]': expect.anything(),
-        }),
-        expect.any(Function),
-        expect.any(Function),
-      )
-    })
-
-    it('with custom option selected opens the modal', async () => {
-      const component = render(<CalendarEventDetailsForm {...defaultProps} />)
-      component.getByText('Frequency').click()
-      component.getByText('Custom...').click()
-      const modal = await component.findByText('Custom Repeating Event')
-      expect(modal).toBeInTheDocument()
-    })
-
-    it('does not reset when the date changes', async () => {
-      const props = {...defaultProps}
-      props.event = commonEventFactory(null, [userContext, courseContext])
-      const nextDate = props.event.startDate().clone().add(1, 'day').format('ddd, MMM D, YYYY')
-
-      const component = render(<CalendarEventDetailsForm {...props} />)
-      component.getByText('Frequency').click()
-      component.getByText('Daily').click()
-      await waitFor(() => expect(component.queryByDisplayValue('Daily')).toBeInTheDocument())
-      changeValue(component, 'edit-calendar-event-form-date', nextDate)
-      expect(component.queryByDisplayValue('Daily')).toBeInTheDocument()
-    })
-
-    it('tracks day of week when the date changes', async () => {
-      const props = {...defaultProps}
-      props.event = commonEventFactory(null, [userContext, courseContext])
-      const d = moment('2023-08-28') // a monday
-      props.event.date = d.toDate()
-      props.event.startDate = jest.fn(() => moment(props.event.date))
-      const nextDate = d.clone().add(1, 'day').format('ddd, MMM D, YYYY')
-
-      const component = render(<CalendarEventDetailsForm {...props} />)
-      component.getByText('Frequency').click()
-      screen.getByText('Weekly on Monday').click()
-      await waitFor(() =>
-        expect(component.queryByDisplayValue('Weekly on Monday')).toBeInTheDocument(),
-      )
-      changeValue(component, 'edit-calendar-event-form-date', nextDate)
-      expect(component.queryByDisplayValue('Weekly on Tuesday')).toBeInTheDocument()
-    })
-
-    it('does not change the custom frequency when the date changes', async () => {
-      const props = {...defaultProps}
-      props.event = commonEventFactory(null, [userContext, courseContext])
-      const d = moment('2023-08-28') // a monday
-      props.event.date = d.toDate()
-      props.event.startDate = jest.fn(() => moment(props.event.date))
-      props.event.object.rrule = 'FREQ=WEEKLY;BYDAY=MO;INTERVAL=1;COUNT=5'
-      const nextDate = d.clone().add(1, 'day').format('ddd, MMM D, YYYY')
-
-      const component = render(<CalendarEventDetailsForm {...props} />)
-      expect(component.queryByDisplayValue('Weekly on Mon, 5 times')).toBeInTheDocument()
-
-      changeValue(component, 'edit-calendar-event-form-date', nextDate)
-      expect(component.queryByDisplayValue('Weekly on Mon, 5 times')).toBeInTheDocument()
-    })
   })
 })
