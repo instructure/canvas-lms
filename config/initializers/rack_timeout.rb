0801626e8170719d189626397a7367946d90e876
Mark Ng <mark@markng.co.uk>
2025-01-24 10:11:04 -0800

    BEL-4804 Send errors for slow transaction that we do not wish to terminate yet
    Dealing with the last of these grade_passback errors, we're not getting Rack Timeouts
    for some of them. This will produce a different exception and send it to sentry, but
    not kill the transaction.

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,11 @@
 Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 55
+class SlowTransactionError < RuntimeError; end
+Rack::Timeout.register_state_change_observer(:check_for_slow_requests) do |env|
+  unless env["sentry_sent"]
+    info = env[::Rack::Timeout::ENV_INFO_KEY]
+    if info.service && info.service > 15
+      Sentry.capture_exception(SlowTransactionError.new)
+      env["sentry_sent"] = true
+    end
+  end
+end
