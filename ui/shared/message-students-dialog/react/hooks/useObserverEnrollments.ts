44ed382416e4d7dff1dcc50dffa169b8745571ad
Spencer Olson <solson@instructure.com>
2025-01-30 11:20:18 -0600

    paginate observer enrollments query
    closes EVAL-4731
    flag=message_observers_of_students_who
    
    Test Plan:
    * Prerequisites: multiple observers in a course
    
    0. Change MAX_ENROLLMENTS_PER_PAGE to 1 in
       ui/shared/message-students-dialog/graphql/Queries.ts
    1. Enable message_observers_of_students_who
    2. Go to a course gradebook
    3. Open 'Message Students Who...'
    4. Notice that the observers are shown
    
    Change-Id: If796477e85ff20f0e3a695fd58f712d023872171
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369458
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Derek Williams <derek.williams@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    QA-Review: Spencer Olson <solson@instructure.com>
    Product-Review: Spencer Olson <solson@instructure.com>

---
+++ ui/shared/message-students-dialog/react/hooks/useObserverEnrollments.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useMemo} from 'react'
+import {useAllPages} from '@canvas/query'
+import {Student} from '../MessageStudentsWhoDialog'
+import {executeQuery} from '@canvas/query/graphql'
+import {
+  OBSERVER_ENROLLMENTS_QUERY,
+  ObserverEnrollmentQueryResult,
+} from '@canvas/message-students-dialog/graphql/Queries'
+
+export const useObserverEnrollments = (courseId: string | undefined, students: Student[]) => {
+  const studentIds = students.map(student => student.id)
+  const keyStudentIds = studentIds.sort().join(',')
+  const {data, isLoading, isFetchingNextPage} = useAllPages({
+    enabled: courseId !== undefined,
+    queryKey: ['ObserversForStudents', courseId, keyStudentIds],
+    queryFn: async ({pageParam}) => {
+      return executeQuery<ObserverEnrollmentQueryResult>(OBSERVER_ENROLLMENTS_QUERY, {
+        courseId,
+        cursor: pageParam,
+        studentIds,
+      })
+    },
+    getNextPageParam: lastPage => {
+      const {hasNextPage, endCursor} = lastPage.course.enrollmentsConnection.pageInfo
+
+      if (hasNextPage && endCursor) {
+        return endCursor
+      }
+
+      return null
+    },
+    meta: {fetchAtLeastOnce: true},
+  })
+
+  const loading = isLoading || isFetchingNextPage
+  const observerEnrollments = useMemo(
+    () => data?.pages.flatMap(page => page?.course?.enrollmentsConnection?.nodes) ?? [],
+    [data],
+  )
+
+  return {loading, observerEnrollments}
+}
