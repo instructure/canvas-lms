125c06cbf6415fa0a8be59156a681869f4c6fd98
Jeremy Stanley <jeremy@instructure.com>
2025-01-30 13:41:23 -0700

    fix orphaned feature flag cleanup
    make sure it doesn't delete too much
    
    test plan:
     - orphan a feature flag
     - run the datafixup
     - the orphaned flag should be gone
     - all the others should still be there
    
    refs FOO-4982
    
    Change-Id: I1db42218eb53c807599d03d00d5d65b0cdf9ad8c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/368882
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    Product-Review: Jeremy Stanley <jeremy@instructure.com>

--- spec/lib/data_fixup/delete_orphaned_feature_flags_spec.rb
+++ spec/lib/data_fixup/delete_orphaned_feature_flags_spec.rb
@@ -20,30 +20,49 @@
 require "spec_helper"
 
 describe DataFixup::DeleteOrphanedFeatureFlags do
-  it "deletes orphaned User feature flags" do
-    feature = Feature.definitions.values.find { |f| f.applies_to == "User" }.feature
+  before :once do
+    @account_feature = Feature.definitions.values.find { |f| f.applies_to == "Account" }.feature
+    @course_feature = Feature.definitions.values.find { |f| f.applies_to == "Course" }.feature
+    @user_feature = Feature.definitions.values.find { |f| f.applies_to == "User" }.feature
+
+    account = account_model(root_account: Account.default)
+    @account_flag = account.feature_flags.create!(feature: @account_feature, state: "on")
+    course = course_factory
+    @course_flag = course.feature_flags.create!(feature: @course_feature, state: "on")
     user = user_factory
-    ff = user.feature_flags.create!(feature:, state: "on")
+    @user_flag = user.feature_flags.create!(feature: @user_feature, state: "on")
+  end
+
+  def check_existing_flags
+    expect(@account_flag.reload).to be_present
+    expect(@course_flag.reload).to be_present
+    expect(@user_flag.reload).to be_present
+  end
+
+  it "deletes orphaned User feature flags" do
+    user = user_factory
+    ff = user.feature_flags.create!(feature: @user_feature, state: "on")
     FeatureFlag.where(id: ff).update_all(context_id: -1) # easier than hard-deleting and dealing with FK constraints
     described_class.run
     expect { ff.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    check_existing_flags
   end
 
   it "deletes orphaned Course feature flags" do
-    feature = Feature.definitions.values.find { |f| f.applies_to == "Course" }.feature
     course = course_factory
-    ff = course.feature_flags.create!(feature:, state: "on")
+    ff = course.feature_flags.create!(feature: @course_feature, state: "on")
     FeatureFlag.where(id: ff).update_all(context_id: -1)
     described_class.run
     expect { ff.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    check_existing_flags
   end
 
   it "deletes orphaned Account feature flags" do
-    feature = Feature.definitions.values.find { |f| f.applies_to == "Account" }.feature
     account = account_model(root_account: Account.default)
-    ff = account.feature_flags.create!(feature:, state: "on")
+    ff = account.feature_flags.create!(feature: @account_feature, state: "on")
     FeatureFlag.where(id: ff).update_all(context_id: -1)
     described_class.run
     expect { ff.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    check_existing_flags
   end
 end
