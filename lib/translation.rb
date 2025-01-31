591792f7254497a9d1e43723629eeed530e2a5fd
Aaron Ogata <aogata@instructure.com>
2025-01-31 13:25:51 -0800

    convert translation caching to Rails.cache
    refs AE-1579
    
    Change-Id: I4813bfc73e95d253578f62370700b09f426a4114
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369013
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Product-Review: Aaron Ogata <aogata@instructure.com>

--- lib/translation.rb
+++ lib/translation.rb
@@ -236,29 +236,18 @@ module Translation
         return languages
       end
 
-      # Check if Redis is present. If yes, then we try to read the key from Redis
-      if Canvas.redis_enabled?
-        # Try to read the key
-        cached_languages = Canvas.redis.get(language_cache_key)
-        unless cached_languages.nil?
-          return JSON.parse(cached_languages)
-        end
-      end
-
-      # Translate our language controls
-      translated = []
-      languages.each do |language|
-        language[:name] = create(src_lang: "en", tgt_lang: locale, text: language[:name])
-        translated << language
-      end
-
-      # Cache the translation for new loads
-      if Canvas.redis_enabled?
+      Rails.cache.fetch(language_cache_key) do
         Rails.logger.info "Caching supported language translation: #{locale}}}}"
-        Canvas.redis.set(language_cache_key, translated.to_json)
-      end
 
-      translated
+        # Translate our language controls
+        translated = []
+        languages.each do |language|
+          language[:name] = create(src_lang: "en", tgt_lang: locale, text: language[:name])
+          translated << language
+        end
+
+        translated
+      end
     end
 
     def language_matches_user_locale?(user, text)
