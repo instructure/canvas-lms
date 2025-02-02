06dd026ef5c9f5b3bdeca64f64a33726b634a85f
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-02-02 09:28:00 -0700

    split out and stabilize ItemAssignToCard tests (refs CFA-71)
    Change-Id: I0bd5bd390e65d3ce57a8d934117d31a399f9fffd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369034
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sleyder Zuleta <sleyder.zuleta@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/shared/context-modules/differentiated-modules/react/Item/__tests__/ItemAssignToCard.3.test.tsx
+++ ui/shared/context-modules/differentiated-modules/react/Item/__tests__/ItemAssignToCard.3.test.tsx
@@ -21,7 +21,6 @@ import {render, fireEvent, screen, waitFor} from '@testing-library/react'
 import ItemAssignToCard, {type ItemAssignToCardProps} from '../ItemAssignToCard'
 import {SECTIONS_DATA, STUDENTS_DATA} from '../../__tests__/mocks'
 import fetchMock from 'fetch-mock'
-import userEvent from '@testing-library/user-event'
 import {queryClient} from '@canvas/query'
 import {MockedQueryProvider} from '@canvas/test-utils/query'
 
@@ -51,7 +50,9 @@ const renderComponent = (overrides: Partial<ItemAssignToCardProps> = {}) =>
   )
 
 const withWithGradingPeriodsMock = () => {
+  window.ENV = window.ENV || {}
   window.ENV.HAS_GRADING_PERIODS = true
+  window.ENV.current_user_is_admin = false
   window.ENV.active_grading_periods = [
     {
       id: '2',
@@ -89,6 +90,10 @@ describe('ItemAssignToCard', () => {
   })
 
   beforeEach(() => {
+    window.ENV = window.ENV || {}
+    window.ENV.HAS_GRADING_PERIODS = false
+    window.ENV.active_grading_periods = []
+    window.ENV.current_user_is_admin = false
     fetchMock.get(SECTIONS_URL, SECTIONS_DATA)
     queryClient.setQueryData(['students', props.courseId, {per_page: 100}], STUDENTS_DATA)
     fetchMock.get(ASSIGNMENT_OVERRIDES_URL, [])
@@ -97,110 +102,9 @@ describe('ItemAssignToCard', () => {
 
   afterEach(() => {
     fetchMock.restore()
-  })
-
-  it('renders all disabled when date falls in a closed grading period for teacher', () => {
-    withWithGradingPeriodsMock()
-
-    const due_at = '2024-05-05T00:00:00-06:00'
-    const original_due_at = '2024-05-05T00:00:00-06:00'
-    const {getByLabelText} = renderComponent({due_at, original_due_at})
-    expect(getByLabelText('Due Date')).toHaveValue('May 5, 2024')
-    expect(getByLabelText('Due Date')).toBeDisabled()
-  })
-
-  it('renders all fields when date falls in a closed grading period for admin', () => {
-    withWithGradingPeriodsMock()
-    window.ENV.current_user_is_admin = true
-
-    const due_at = '2024-05-05T00:00:00-06:00'
-    const original_due_at = '2024-05-05T00:00:00-06:00'
-    const {getByLabelText} = renderComponent({due_at, original_due_at})
-    expect(getByLabelText('Due Date')).toHaveValue('May 5, 2024')
-    expect(getByLabelText('Due Date')).not.toBeDisabled()
-  })
-
-  it.skip('renders error when date change to a closed grading period for teacher', async () => {
-    // Flakey spec
-    withWithGradingPeriodsMock()
+    window.ENV.HAS_GRADING_PERIODS = false
+    window.ENV.active_grading_periods = []
     window.ENV.current_user_is_admin = false
-
-    const due_at = '2024-05-17T00:00:00-06:00'
-    const original_due_at = '2024-05-17T00:00:00-06:00'
-    const {getByLabelText, getAllByText, getAllByRole} = renderComponent({due_at, original_due_at})
-
-    const dateInput = getByLabelText('Due Date')
-    fireEvent.change(dateInput, {target: {value: 'May 4, 2024'}})
-    getAllByRole('option', {name: '4 May 2024'})[0].click()
-
-    await waitFor(async () => {
-      expect(dateInput).toHaveValue('May 4, 2024')
-      expect(getAllByText(/Please enter a due date on or after/).length).toBeGreaterThanOrEqual(1)
-    })
-  })
-
-  describe('when course dates are set', () => {
-    beforeAll(() => {
-      window.ENV.VALID_DATE_RANGE = {
-        start_at: {date: '2025-02-09T00:00:00-06:00', date_context: 'course'},
-        end_at: {date: '2025-04-22T23:59:59-06:00', date_context: 'course'},
-      }
-      window.ENV.SECTION_LIST = [
-        {
-          id: '1',
-          override_course_and_term_dates: false,
-          start_at: '2025-02-09T00:00:00-06:00',
-          end_at: '2025-06-22T23:59:59-06:00',
-        },
-      ]
-    })
-
-    afterAll(() => {
-      window.ENV.VALID_DATE_RANGE = undefined
-      window.ENV.SECTION_LIST = undefined
-    })
-
-    it('renders error when date is outside of course dates', async () => {
-      const {getByLabelText, getAllByRole, getAllByText} = renderComponent()
-      const dateInput = getByLabelText('Due Date')
-      fireEvent.change(dateInput, {target: {value: 'May 4, 2025'}})
-      getAllByRole('option', {name: '4 May 2025'})[0].click()
-
-      await waitFor(async () => {
-        expect(dateInput).toHaveValue('May 4, 2025')
-        expect(getAllByText(/Due date cannot be after course end/).length).toBeGreaterThanOrEqual(1)
-      })
-    })
-
-    it('does not render error when date is outside of course dates but assignees are ADHOC', async () => {
-      const {getByLabelText, getAllByRole, queryByText} = renderComponent({
-        customAllOptions: [{id: 'student-1', value: 'John'}],
-        selectedAssigneeIds: ['student-1'],
-      })
-      const dateInput = getByLabelText('Due Date')
-      fireEvent.change(dateInput, {target: {value: 'May 4, 2025'}})
-      getAllByRole('option', {name: '4 May 2025'})[0].click()
-
-      await waitFor(async () => {
-        expect(dateInput).toHaveValue('May 4, 2025')
-        expect(queryByText(/Due date cannot be after course end/)).not.toBeInTheDocument()
-      })
-    })
-
-    it('does not render error when date is outside of course dates but inside section dates', async () => {
-      const {getByLabelText, getAllByRole, queryByText} = renderComponent({
-        customAllOptions: [{id: 'section-1', value: 'Section 1'}],
-        selectedAssigneeIds: ['section-1'],
-      })
-      const dateInput = getByLabelText('Due Date')
-      fireEvent.change(dateInput, {target: {value: 'May 4, 2025'}})
-      getAllByRole('option', {name: '4 May 2025'})[0].click()
-
-      await waitFor(async () => {
-        expect(dateInput).toHaveValue('May 4, 2025')
-        expect(queryByText(/Due date cannot be after course end/)).not.toBeInTheDocument()
-      })
-    })
   })
 
   describe('when course and user timezones differ', () => {
