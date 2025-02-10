709aa78b6fc8f2a730ad83416e730bbe5d45b987
Angela Gomba <angela.gomba@instructure.com>
2025-02-10 16:04:19 -0500

    Remove visibility caching
    We are receiving mulitple SWs due to caching. After more
    investigation and discussion, caching will be added back in
    LX-2224
    
    refs LX-2221 VICE-4948
    flag=none
    
    Test Plan:
    - Specs pass
    
    Change-Id: Ibeadcc29436100a2c219576b699cb960d8e287cd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369886
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Chrystal Langston <chrystal.langston@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- app/services/visibility_helpers/common.rb
+++ app/services/visibility_helpers/common.rb
@@ -20,8 +20,10 @@
 module VisibilityHelpers
   module Common
     def service_cache_fetch(service:, course_ids: nil, user_ids: nil, additional_ids: nil, &)
-      key = service_cache_key(service:, course_ids:, user_ids:, additional_ids:)
-      Rails.cache.fetch(key, expires_in: 1.minute, &)
+      # TODO: LX-2224 Add Visibility Caching back with Dynamic Expiration
+      # key = service_cache_key(service:, course_ids:, user_ids:, additional_ids:)
+      # Rails.cache.fetch(key, expires_in: 1.minute, &)
+      yield
     end
 
     private
