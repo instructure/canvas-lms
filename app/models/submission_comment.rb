6b7d91913d84ea509249785b2bd190745e5205a3
Derek Williams <derek.williams@instructure.com>
2025-02-11 10:55:11 -0500

    add feature flag for auto captioning in submission comments
    closes EVAL-4735
    flag=submission_comment_media_auto_captioning
    
    test plan:
    - turn on submission_comment_media_auto_captioning FF
    - make a media submission comment in speed grader with
        the updated studio media capture component
        (speedgrader_studio_media_capture FF enabled)
    - verify that the comment is auto captioned
    
    - turn off submission_comment_media_auto_captioning FF
    - make a media submission comment in speed grader with
        the updated studio media capture component
        (speedgrader_studio_media_capture FF enabled)
    - verify that the comment is not auto captioned
    
    Change-Id: Id0f8bf1c2eae1b5ff24146ae0236ee78a1afb5b3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369981
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Derek Williams <derek.williams@instructure.com>
    Reviewed-by: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

--- app/models/submission_comment.rb
+++ app/models/submission_comment.rb
@@ -69,7 +69,7 @@ class SubmissionComment < ActiveRecord::Base
   before_save :set_edited_at
   after_save :update_participation
   after_save :check_for_media_object
-  after_save :request_captions
+  after_save :request_captions, if: -> { root_account&.feature_enabled?(:submission_comment_media_auto_captioning) }
   after_update :publish_other_comments_in_this_group
   after_update :post_submission_for_finalized_draft, if: -> { saved_change_to_draft?(from: true, to: false) }
   after_destroy :delete_other_comments_in_this_group
