ec5eee429fdcdc3c7fc41351a3c9221ee944dce8
Tyler Rhodes <tyler.rhodes@strongmind.com>
2025-02-11 00:24:40 +0000

    QTY-13820: Change SlowTransactionError to 50 seconds

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,23 +1,32 @@
 Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 50
 class SlowTransactionError < RuntimeError
-  attr :backtrace
-  def initialize(backtrace)
+  attr_reader :backtrace, :duration
+
+  def initialize(backtrace, duration)
     @backtrace = backtrace
+    @duration = duration
+  end
+
+  def message
+    "Request took too long (#{duration}s)."
   end
 end
+
 Rack::Timeout.register_state_change_observer(:check_for_slow_requests) do |env|
   unless env["sentry_sent"]
     info = env[::Rack::Timeout::ENV_INFO_KEY]
     request_id = info.id
-    if info.service && info.service > 30
+    slow_threshold = 50
+    actual_time = info.service
+
+    if actual_time && actual_time > slow_threshold
       env["sentry_sent"] = true
       begin
         slow_thread = Thread.list.find { |thread| thread.thread_variable_get("request_id") == request_id}
-        slow_thread.thread_variable_get('sentry_hub').capture_exception(SlowTransactionError.new(slow_thread.backtrace))
+        slow_thread.thread_variable_get('sentry_hub').capture_exception(SlowTransactionError.new(slow_thread.backtrace, actual_time))
       rescue
          # Ignored
       end
     end
   end
 end
-
