f7e80b6009ef78f4b6c333988c3ee3e3a2b76f7c
Juan Chavez <juan.chavez@instructure.com>
2025-02-12 18:27:34 -0400

    Copy To Tray
    closes RCX-2537
    
    flag=files_a11y_rewrite
    
    test plan:
    - Navigate to course files page.
    - Select a file and click 'Copy To...'
    menu item.
    > Verify that the tray looks like the
    designs.
    - Search and select a course.
    - Search and select a module.
    - Select a place.
    > Verify that if you select 'Before...'
    or 'After...' places, the assignments
    selector will appear.
    - If wanted select an assignment.
    - Click Copy button.
    > Verify that flash alerts appear.
    > Verify that the file was copied in
    the correct place.
    ALSO VERIFY...:
    > That when clicking Cancel button
    nothing happens.
    > That the warning message can be
    closed.
    > That when the user tries to copy
    without selecting a course, an error
    message will appear and the input will
    be focused.
    
    Change-Id: I690636a660b964d27d6a3e819298af1c5866f1c8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/370502
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Juan Chavez <juan.chavez@instructure.com>

---
+++ ui/features/files_v2/react/components/FileFolderTable/DirectShareCourseTray/ModulePositionPicker.tsx
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {SyntheticEvent, useCallback, useEffect, useState} from 'react'
+import {useCourseModuleItemApi} from '@canvas/direct-sharing/react/effects/useModuleCourseSearchApi'
+import {SimpleSelect} from '@instructure/ui-simple-select'
+import {useScope as createI18nScope} from '@canvas/i18n'
+import {Spinner} from '@instructure/ui-spinner'
+import {View} from '@instructure/ui-view'
+import {FormMessage} from '@instructure/ui-form-field'
+
+const I18n = createI18nScope('files_v2')
+
+type ModulePositionPickerProps = {
+  courseId: string
+  moduleId: string
+  onSelectPosition: (position: number | null) => void
+}
+
+type Position = {
+  type: string
+  label: string
+}
+
+type PositionType = 'first' | 'before' | 'after' | 'last'
+
+type ModuleItem = {
+  id: string
+  title: string
+}
+
+// Copied from ui/shared/positions/positions.js
+export const positions: {[key in PositionType]: Position} = {
+  first: {
+    type: 'absolute',
+    get label() {
+      return I18n.t('At the Top')
+    },
+  },
+  before: {
+    type: 'relative',
+    get label() {
+      return I18n.t('Before...')
+    },
+  },
+  after: {
+    type: 'relative',
+    get label() {
+      return I18n.t('After...')
+    },
+  },
+  last: {
+    type: 'absolute',
+    get label() {
+      return I18n.t('At the Bottom')
+    },
+  },
+}
+
+const ModulePositionPicker = ({
+  courseId,
+  moduleId,
+  onSelectPosition,
+}: ModulePositionPickerProps) => {
+  const [moduleItems, setModuleItems] = useState<ModuleItem[]>([])
+  const [position, setPosition] = useState<PositionType>('first')
+  const [offset, setOffset] = useState(0)
+  const [siblingPosition, setSiblingPosition] = useState(1)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setSiblingPosition(1)
+    onSelectPosition(1 + offset)
+    // eslint-disable-next-line react-compiler/react-compiler
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [courseId, moduleId])
+
+  const setFetchError = useCallback(() => setError(I18n.t('Error retrieving module items')), [])
+
+  useCourseModuleItemApi({
+    success: setModuleItems,
+    error: setFetchError,
+    loading: setIsLoading,
+    fetchAllPages: true,
+    params: {contextId: courseId, moduleId},
+  })
+
+  const handleSetPosition = useCallback(
+    (_e: SyntheticEvent, {value}: {value?: string | number | undefined}) => {
+      const positionKey = value as PositionType
+      setPosition(positionKey)
+      switch (positionKey) {
+        case 'first':
+          setOffset(0)
+          setSiblingPosition(1)
+          onSelectPosition(1)
+          break
+        case 'last':
+          setOffset(0)
+          setSiblingPosition(1)
+          onSelectPosition(null)
+          break
+        case 'after':
+          setOffset(1)
+          // + 1 for the offset that won't be set yet by the time we need it
+          onSelectPosition(siblingPosition + 1)
+          break
+        case 'before':
+          setOffset(0)
+          onSelectPosition(siblingPosition)
+          break
+      }
+    },
+    [onSelectPosition, siblingPosition],
+  )
+
+  const handleSetSibling = useCallback(
+    (_e: SyntheticEvent, {value}: {value?: string | number | undefined}) => {
+      const pos = (value as number) + 1
+      setSiblingPosition(pos)
+      onSelectPosition(pos + offset)
+    },
+    [offset, onSelectPosition],
+  )
+
+  const renderSelectPlace = useCallback(() => {
+    const messages = error ? [{text: error, type: 'newError'} as FormMessage] : []
+    return (
+      <SimpleSelect
+        data-testid="select-position"
+        renderLabel={I18n.t('Place')}
+        value={position}
+        onChange={handleSetPosition}
+        messages={messages}
+      >
+        {Object.keys(positions).map(positionKey => {
+          return (
+            <SimpleSelect.Option id={positionKey} key={positionKey} value={positionKey}>
+              {positions[positionKey as PositionType].label}
+            </SimpleSelect.Option>
+          )
+        })}
+      </SimpleSelect>
+    )
+  }, [error, handleSetPosition, position])
+
+  const renderSelectSibling = useCallback(() => {
+    if (isLoading)
+      return <Spinner renderTitle={I18n.t('Loading additional items...')} size="x-small" />
+
+    return (
+      <SimpleSelect
+        data-testid="select-sibling"
+        renderLabel={I18n.t('Module Item')}
+        onChange={handleSetSibling}
+      >
+        {moduleItems.map((item, index) => (
+          <SimpleSelect.Option id={item.id} key={item.id} value={index}>
+            {item.title}
+          </SimpleSelect.Option>
+        ))}
+      </SimpleSelect>
+    )
+  }, [handleSetSibling, isLoading, moduleItems])
+
+  return (
+    <>
+      <View as="div" margin="0 0 small 0">
+        {renderSelectPlace()}
+      </View>
+
+      {positions[position]?.type === 'relative' && (
+        <View as="div" margin="0 0 small 0" textAlign="center">
+          {renderSelectSibling()}
+        </View>
+      )}
+    </>
+  )
+}
+export default ModulePositionPicker
---
+++ ui/features/files_v2/react/components/FileFolderTable/DirectShareCourseTray/__tests__/DirectShareCourseTray.test.tsx
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, fireEvent, act, screen} from '@testing-library/react'
+import fetchMock from 'fetch-mock'
+import useManagedCourseSearchApi from '@canvas/direct-sharing/react/effects/useManagedCourseSearchApi'
+import useModuleCourseSearchApi, {
+  useCourseModuleItemApi,
+} from '@canvas/direct-sharing/react/effects/useModuleCourseSearchApi'
+import {FAKE_FILES} from '../../../../../fixtures/fakeData'
+import DirectShareCourseTray from '../DirectShareCourseTray'
+import userEvent from '@testing-library/user-event'
+
+jest.mock('@canvas/direct-sharing/react/effects/useManagedCourseSearchApi')
+jest.mock('@canvas/direct-sharing/react/effects/useModuleCourseSearchApi')
+
+const defaultProps = {
+  open: true,
+  onDismiss: jest.fn(),
+  courseId: '1',
+  file: FAKE_FILES[0],
+}
+
+const renderComponent = (props = {}) =>
+  render(<DirectShareCourseTray {...defaultProps} {...props} />)
+
+describe('DirectShareCourseTray', () => {
+  let ariaLive: HTMLElement
+
+  beforeAll(() => {
+    ariaLive = document.createElement('div')
+    ariaLive.id = 'flash_screenreader_holder'
+    ariaLive.setAttribute('role', 'alert')
+    document.body.appendChild(ariaLive)
+  })
+
+  afterAll(() => {
+    if (ariaLive) ariaLive.remove()
+  })
+
+  beforeEach(() => {
+    ;(useManagedCourseSearchApi as jest.Mock).mockImplementationOnce(({success}) => {
+      success([
+        {id: 'abc', name: 'abc', course_code: '1', term: 'default term'},
+        {id: 'cde', name: 'cde', course_code: '1', term: 'default term'},
+      ])
+    })
+  })
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  it('shows the overwrite warning', () => {
+    renderComponent()
+    expect(
+      screen.getByText(/^Importing the same course content more than once/),
+    ).toBeInTheDocument()
+  })
+
+  it('calls the onDismiss property', () => {
+    renderComponent()
+    fireEvent.click(screen.getByText(/cancel/i))
+    expect(defaultProps.onDismiss).toHaveBeenCalled()
+  })
+
+  it('starts a copy operation and reports status', async () => {
+    fetchMock.postOnce('path:/api/v1/courses/abc/content_migrations', {
+      id: '8',
+      workflow_state: 'running',
+    })
+    fetchMock.getOnce('path:/api/v1/courses/abc/modules', [])
+    renderComponent()
+    const input = await screen.getByLabelText(/select a course/i)
+    await userEvent.click(input)
+    await userEvent.type(input, 'abc')
+    await userEvent.click(screen.getByText('abc'))
+    await userEvent.click(screen.getByTestId('direct-share-course-copy'))
+    const mockCall = fetchMock.lastCall()
+    const fetchOptions = mockCall?.[1] || {}
+    expect(fetchOptions.method).toBe('POST')
+    expect(JSON.parse(fetchOptions.body?.toString() || '')).toMatchObject({
+      migration_type: 'course_copy_importer',
+      select: {attachments: ['178']},
+      settings: {source_course_id: '1'},
+    })
+    expect(screen.getAllByText(/start/i)[0]).toBeInTheDocument()
+    await act(() => {
+      fetchMock.flush(true)
+    })
+    expect(screen.getAllByText(/success/i)[0]).toBeInTheDocument()
+  })
+
+  it('deletes the module and removes the position selector when a new course is selected', async () => {
+    ;(useModuleCourseSearchApi as jest.Mock).mockImplementationOnce(({success}) => {
+      success([
+        {id: '1', name: 'Module 1'},
+        {id: '2', name: 'Module 2'},
+      ])
+    })
+    renderComponent()
+    let input = await screen.getByLabelText(/select a course/i)
+    await userEvent.click(input)
+    await userEvent.type(input, 'abc')
+    await userEvent.click(screen.getByText('abc'))
+
+    input = await screen.getByLabelText(/select a module/i)
+    await userEvent.click(input)
+    await userEvent.type(input, 'Module 1')
+    await userEvent.click(screen.getByText('Module 1'))
+    expect(screen.getByTestId('select-position')).toBeInTheDocument()
+    ;(useManagedCourseSearchApi as jest.Mock).mockImplementationOnce(({success}) => {
+      success([{id: 'ghi', name: 'foo', course_code: '3', term: 'default term'}])
+    })
+    ;(useCourseModuleItemApi as jest.Mock).mockClear()
+
+    input = await screen.getByLabelText(/select a course/i)
+    await userEvent.click(input)
+    await userEvent.type(input, 'f')
+    await userEvent.click(screen.getByText('foo'))
+    expect(screen.queryByTestId('select-position')).not.toBeInTheDocument()
+    expect(useCourseModuleItemApi).not.toHaveBeenCalled()
+  })
+
+  describe('errors', () => {
+    it('shows an error when user tries to submit without a selected course', async () => {
+      renderComponent()
+      await userEvent.click(screen.getByTestId('direct-share-course-copy'))
+      expect(screen.getByLabelText(/a course needs to be selected/i)).toBeInTheDocument()
+    })
+
+    it('reports an error if the fetch fails', async () => {
+      fetchMock.postOnce('path:/api/v1/courses/abc/content_migrations', 400)
+      fetchMock.getOnce('path:/api/v1/courses/abc/modules', [])
+      renderComponent()
+      const input = await screen.getByLabelText(/select a course/i)
+      await userEvent.click(input)
+      await userEvent.type(input, 'abc')
+      await userEvent.click(screen.getByText('abc'))
+      await userEvent.click(screen.getByTestId('direct-share-course-copy'))
+      await act(() => {
+        fetchMock.flush(true)
+      })
+      expect(screen.getAllByText(/start/i)[0]).toBeInTheDocument()
+      expect(screen.getAllByText(/failed/i)[0]).toBeInTheDocument()
+    })
+  })
+})
