ec4085300e9ed931a2a90f01d3b222635de42ab4
Angela Gomba <angela.gomba@instructure.com>
2025-02-21 14:54:20 -0500

    5VPAT: Edit Group Set Validation
    closes EGG-367
    flag=none
    
    Test Plan:
    - Create a Group Set
    - Once created, use the three-dot menu to edit it and open the
      group set edit modal
    - Check Allow self-sign up to enable the group limit input
    - Test the following cases for the group limit input and observe
      an error is shown for each and the user cannot submit the form:
      - non-numerical value, i.e. "abc"
      - number less than 2
      - non-integer, i.e. 3.5
    - Enter a valid number and save. Reopen the modal to verify it
      persisted
    - Clear the group set name input
    - Attempt to save the form and observe an error is shown saying
      a name is required
    - Enter an invalid number for the group limit input again and
      attempt to save
    - Observe the errors for both inputs remain and the focus is
      brought to the name input
    - Enter a valid name and attempt to save
    - Observe focus is brought to the group limit input
    - Clear the group limit input and save the form
    - Observe it saves successfully
    
    Change-Id: Ic0dd49134f5cc0a5dfb8271629d4c7fe1ff25b5c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/370763
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Martin Yosifov <martin.yosifov@instructure.com>
    QA-Review: Martin Yosifov <martin.yosifov@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

---
+++ ui/shared/groups/react/GroupSetNameInput.tsx
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useState} from 'react'
+import {Flex} from '@instructure/ui-flex'
+import {FormMessage} from '@instructure/ui-form-field/types/FormPropTypes'
+import {IconWarningSolid} from '@instructure/ui-icons'
+import {TextInput} from '@instructure/ui-text-input'
+import {Text} from '@instructure/ui-text'
+import {View} from '@instructure/ui-view'
+import {useScope as createI18nScope} from '@canvas/i18n'
+
+const I18n = createI18nScope('editGroup')
+
+export type GroupSetNameInputProps = {
+  id: string,
+  initialValue?: string,
+  getShouldShowEmptyNameError?: () => boolean,
+  setShouldShowEmptyNameError?: (shouldShow: boolean) => void,
+}
+
+const GroupSetNameInput = ({
+  id,
+  initialValue = '',
+  getShouldShowEmptyNameError,
+  setShouldShowEmptyNameError,
+}: GroupSetNameInputProps) => {
+  const [nameValue, setNameValue] = useState<string>(initialValue)
+  const [errorMessages, setErrorMessages] = useState<FormMessage[]>([])
+
+  const formatErrorMessage = (message: string) => (
+    <Flex as="div" alignItems="center">
+      <Flex as="div" alignItems="center" margin="0 xx-small 0 0">
+        <IconWarningSolid color="error" />
+      </Flex>
+      <Text size="small" color="danger">
+        {message}
+      </Text>
+    </Flex>
+  )
+
+  const handleFocus = (_event: React.FocusEvent<HTMLInputElement>) => {
+    if (getShouldShowEmptyNameError && getShouldShowEmptyNameError()) {
+      // reset the value
+      if (setShouldShowEmptyNameError) setShouldShowEmptyNameError(false)
+      const errorText = formatErrorMessage(I18n.t('Name is required'))
+      setErrorMessages([{ text: errorText, type: 'error' }])
+    }
+  }
+
+  const validateInput = (_event: React.FocusEvent<HTMLInputElement>) => {
+    if (nameValue) {
+      let errorText
+      if (nameValue.length > 255) {
+        errorText = formatErrorMessage(I18n.t('Name must be 255 characters or less'))
+      }
+      if (errorText) setErrorMessages([{ text: errorText, type: 'error' }])
+    }
+  }
+
+  const handleInputChange = (_event: React.ChangeEvent<HTMLInputElement>, value: string) => {
+    setErrorMessages([])
+    setNameValue(value)
+  }
+
+  return (
+    <View as='div' margin='small xx-small'>
+      <TextInput
+        id={`category_${id}_name`}
+        renderLabel=""
+        messages={errorMessages}
+        name='name'
+        value={nameValue}
+        onFocus={handleFocus}
+        onBlur={validateInput}
+        onChange={handleInputChange}
+        width='250px'
+        data-testid={`category_${id}_name`}
+      />
+    </View>
+  )
+}
+
+export default GroupSetNameInput
