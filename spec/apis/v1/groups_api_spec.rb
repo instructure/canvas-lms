700df0ea0395090147d59aa19d532616bac1249a
Jason Gillett <jason.gillett@instructure.com>
2025-02-25 08:32:08 -0700

    Fix broken spec
    flag=differentiation_tags
    
    Test plan
    - Specs pass
    
    Change-Id: Idd2a06ae209fae298ec6e1c2cc0288230df5dd84
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/371193
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Roberto Noguera <roberto.noguera@instructure.com>
    QA-Review: Roberto Noguera <roberto.noguera@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

--- spec/apis/v1/groups_api_spec.rb
+++ spec/apis/v1/groups_api_spec.rb
@@ -1046,6 +1046,7 @@ describe "Groups API", type: :request do
     end
 
     it "bulks add users to non collaborative groups" do
+      Account.default.enable_feature!(:differentiation_tags)
       course_with_teacher(active_all: true)
       category = @course.group_categories.create(name: "category", non_collaborative: true)
       @group = @course.groups.create!(name: "G1", group_category: category, non_collaborative: true)
