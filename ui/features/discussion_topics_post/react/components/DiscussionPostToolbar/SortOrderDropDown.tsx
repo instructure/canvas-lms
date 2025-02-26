4232e0789fdfa40ce730695ebc2a94b69c9e3de9
Roland Beres <roland.beres@instructure.com>
2025-02-26 15:29:50 +0100

    Revert "Fix View and Sort by dropdown's width"
    This commit reverts 4c853e8433b7a638d47aa74e2ed34a9e205c6bf7
    
    Change-Id: Ic52b283a137be98d6e0be899e40dd5dce217528f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/371291
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Theo Sorriaux <theo.sorriaux@instructure.com>
    Product-Review: Tamás Balogh <tamas.balogh@instructure.com>
    QA-Review: Tamás Balogh <tamas.balogh@instructure.com>

--- ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/SortOrderDropDown.tsx
+++ ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/SortOrderDropDown.tsx
@@ -35,10 +35,9 @@ interface SortFilterDropDownProps {
   isLocked: boolean
   selectedSortType: string
   onSortClick?: () => void
-  width?: string
 }
 
-const SortOrderDropDown: React.FC<SortFilterDropDownProps> = ({ isLocked, selectedSortType, onSortClick, width }) => {
+const SortOrderDropDown: React.FC<SortFilterDropDownProps> = ({ isLocked, selectedSortType, onSortClick }) => {
   const [actualSortType, setActualSortType] = useState(selectedSortType)
 
   useEffect(() => {
@@ -69,7 +68,6 @@ const SortOrderDropDown: React.FC<SortFilterDropDownProps> = ({ isLocked, select
         defaultValue={actualSortType}
         onChange={handleSortOrderTypeChange}
         disabled={isLocked}
-        width={width}
       >
         <SimpleSelect.Group renderLabel={I18n.t('Sort by')}>
           {Object.entries(getSortConfig()).map(([viewOption, viewOptionLabel]) => (
@@ -78,6 +76,7 @@ const SortOrderDropDown: React.FC<SortFilterDropDownProps> = ({ isLocked, select
               id={viewOption}
               key={viewOption}
               value={viewOption}
+              renderBeforeLabel={viewOption === actualSortType ? <IconCheckLine /> : <span />}
             >
               {viewOptionLabel()}
             </SimpleSelect.Option>
