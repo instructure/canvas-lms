96abca99e6839f1bac32d52c7211de31624b0732
David Varga <d.varga@instructure.com>
2025-02-28 12:32:20 +0100

    fix Asset Processor routes to use plural names
    refs INTEROP-8961 INTEROP-8962
    flag=lti_asset_processor
    
    test plan:
    - hit the new routes
    
    Change-Id: I6ef5b69168aae12f197f82812ce5dd4699d4270d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/371466
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    QA-Review: David Varga <d.varga@instructure.com>
    Product-Review: David Varga <d.varga@instructure.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2925,8 +2925,8 @@ CanvasRails::Application.routes.draw do
 
     # Asset Service & Asset Report Service (LTI Asset Processor Specs)
     scope(controller: "lti/ims/asset_processor") do
-      post "asset_processor/:asset_processor_id/report", action: :create_report, as: :lti_asset_processor_create_report
-      get "asset_processor/:asset_processor_id/asset/:asset_id", action: :lti_asset_show, as: :lti_asset_processor_asset_show
+      post "asset_processors/:asset_processor_id/reports", action: :create_report, as: :lti_asset_processor_create_report
+      get "asset_processors/:asset_processor_id/assets/:asset_id", action: :lti_asset_show, as: :lti_asset_processor_asset_show
     end
 
     # Dynamic Registration Service
