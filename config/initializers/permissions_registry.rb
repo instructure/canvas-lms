15eaa4d023992cf4ccc26f79ad133baf87b6b0a3
Bryant Larsen <blarsen@instructure.com>
2025-03-06 15:56:04 -0700

    Course SiNoA in its own group
    -- refs OREO-4070
    -- We discovered a visual issue that we believe is due to re-using the same group that is used by admin permissions.
    -- also expanding the available too.
    -- better naming, and no group.
    
    Change-Id: I94c0939b60f641b627de2f274747251ff26803d5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/371997
    Reviewed-by: Asia Nugent <asia.nugent@instructure.com>
    QA-Review: Bryant Larsen <blarsen@instructure.com>
    Product-Review: Bryant Larsen <blarsen@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- config/initializers/permissions_registry.rb
+++ config/initializers/permissions_registry.rb
@@ -1683,12 +1683,9 @@ Rails.application.config.to_prepare do
         account_allows: ->(a) { a.feature_enabled?(:k20_students_in_need_of_attention) }
       },
       view_students_in_need_in_course: {
-        label: -> { I18n.t("Students in Need of Attention In Courses") },
-        group: "view_advanced_analytics",
-        group_label: -> { I18n.t("Intelligent Insights") },
-        available_to: %w[AccountAdmin AccountMembership TeacherEnrollment],
+        label: -> { I18n.t("Intelligent Insights - Students in Need of Attention - Course Level") },
+        available_to: %w[AccountAdmin AccountMembership TeacherEnrollment TaEnrollment],
         true_for: %w[AccountAdmin],
-        account_only: true,
         account_allows: ->(a) { a.feature_enabled?(:k20_students_in_need_of_attention) && a.feature_enabled?(:students_in_need_of_attention_course_level_beta) } ## AND they are part of the beta group.
       },
       view_course_readiness: {
