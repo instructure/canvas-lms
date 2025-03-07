87ab241cd395bbf473382ee7caeaf0029b4ed0df
balazs.buri <balazs.buri@instructure.com>
2025-03-07 13:21:18 +0100

    Scroll to Highlight through layout shift
    refs: EGG-802
    
    flag=discussion_checkpoints
    
    Test Plan:
    - Prereq.: To easily test this feature without full
      RCE support in your local env, you can do the
      following:
      - use https://placebear.com/<WIDTH>/<HEIGTH>
        urls to embed images in the discussion post.
      - use your somewhat functional RCE to embed the
        image by choosing the RAW HTML option
        (button looks like: </>) and then inserting ex.:
        "<img src='https://placebear.com/200/300' />"
      - Make sure you vary the image sizes a little bit
        to prevent caching. Ex.: 200x300, 200x301, etc.
    - Turn on the discussion checkpoints feature flag
      and enroll multiple students into a published
      course
    - As a teacher, Create a graded discussion and enable
      the checkpoints feature making sure to embed a medium
      sized image into the description of the discussion.
    
    - As a student in the course reply to the main post
      and include an image or video embedded in your
      reply as well.
    
    - Repeat previous step for two or three more students.
    
    - As the teacher open speedgrader
       - How to find: Go to assignments, the discussion
         should be there as it is graded. Click the
         "three dots" icon and select "SpeedGrader".
    
    - Open developer console in your browser and in
      the network tab, disable cache.
    
    - Still in speedgrader, toggle between the students
      clicking the arrows on the top right
    
    - Verify that the students' posts are scrolled into
      view even when the images load at a later time.
    
    Change-Id: I982bfa7ff04871a6678df25457e80908f10f6543
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/372023
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Peipei Zhou <peipei.zhou@instructure.com>
    Reviewed-by: Peipei Zhou <peipei.zhou@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

---
+++ ui/features/discussion_topics_post/react/components/Highlight/__tests__/ScrollToHighlight.test.js
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {scrollToHighlight} from '../ScrollToHighlight'
+
+class MockElement {
+    constructor(){
+        this.offsetTop = 0
+    }
+}
+
+class MockWindow {
+    constructor(){
+        this._eventListeners = {}
+        this._animationFrameCallback = () => {}
+        this._now = 0
+        this.scrollY = 0
+        this.Date = { now: () => this._now }
+        this.addEventListener = jest.fn().mockImplementation(
+            (event, callback) =>
+                this._eventListeners[event] = (injectedEventObject =>
+                callback(injectedEventObject)
+            )
+        )
+        this.removeEventListener = jest.fn().mockImplementation(
+            event => delete this._eventListeners[event])
+        this.scrollTo = jest.fn().mockImplementation((x, y) => this.scrollY = y)
+        this.document = {
+            activeElement: 5,
+            createElement: () => new MockElement()
+        }
+        this.requestAnimationFrame = async callback => this._animationFrameCallback = callback
+    }
+    async offestTimeMS(ms = 10) {
+        if (ms === 0) return
+        this._now += ms
+        await new Promise(resolve => setTimeout(resolve, ms))
+        this._animationFrameCallback()
+    }
+}
+
+describe ('ScrollToHighlight', () => {
+
+    it('is noop if element is falsy, returns early', async () => {
+        const mockWindow = new MockWindow()
+        mockWindow.offestTimeMS(1)
+        expect(await scrollToHighlight(undefined, mockWindow)).toBe('NO_ELEMENT_TO_SCROLL_TO')
+        expect(mockWindow.removeEventListener).toHaveBeenCalledTimes(0)
+    })
+
+    it('finishes and cleans up event listeners', async () => {
+        const element = document.createElement('div')
+        const mockWindow = new MockWindow()
+        scrollToHighlight(element, mockWindow).then(_result => {
+            expect(mockWindow.removeEventListener).toHaveBeenCalledTimes(3)
+        })
+    })
+
+    it('scrolls to element', async () => {
+        const mockWindow = new MockWindow()
+        const element = mockWindow.document.createElement('div')
+        element.offsetTop = 10000
+        scrollToHighlight(element, mockWindow)
+        await mockWindow.offestTimeMS(1)
+        const scrollYAtThisPointInTime = mockWindow.scrollY
+        expect(scrollYAtThisPointInTime).toBeGreaterThan(0)
+        expect(scrollYAtThisPointInTime).toBeLessThan(element.offsetTop)
+        await mockWindow.offestTimeMS(10)
+        expect(mockWindow.scrollY).toBeGreaterThan(scrollYAtThisPointInTime)
+    })
+
+    it('scrolls UP to element', async () => {
+        const mockWindow = new MockWindow()
+        const element = mockWindow.document.createElement('div')
+        mockWindow.scrollTo(0, 10000)
+        scrollToHighlight(element, mockWindow)
+        await mockWindow.offestTimeMS(1)
+        const scrollYAtThisPointInTime = mockWindow.scrollY
+        expect(scrollYAtThisPointInTime).toBeGreaterThan(0)
+        expect(scrollYAtThisPointInTime).toBeLessThan(10000)
+        await mockWindow.offestTimeMS(10)
+    })
+
+    describe ('Aborting', () => {
+        [
+            {eventName: 'wheel'},
+            {eventName: 'mousedown'},
+            {eventName: 'keydown', eventData: {key: 'Home'}},
+            {eventName: 'keydown', eventData: {key: 'End'}},
+            {eventName: 'keydown', eventData: {key: 'PageUp'}},
+            {eventName: 'keydown', eventData: {key: 'PageDown'}},
+            {eventName: 'keydown', eventData: {key: 'ArrowUp'}},
+            {eventName: 'keydown', eventData: {key: 'ArrowDown'}},
+        ].forEach(async (userInput) => {
+            it(`aborts behavior on ${userInput.eventName}
+                ${userInput.eventData ? JSON.stringify(userInput.eventData) : ''}`,
+                async () => {
+                    const mockWindow = new MockWindow()
+                    const element = mockWindow.document.createElement('div')
+                    element.offsetTop = 10000
+                    let result = null
+                    scrollToHighlight(element, mockWindow).then(_result => {
+                        result = _result
+                    })
+                    mockWindow._eventListeners[userInput.eventName](userInput.eventData)
+                    await mockWindow.offestTimeMS(1)
+                    await new Promise(resolve => setTimeout(resolve, 0))
+                    expect(result).toBe('SCROLL_ABORTED')
+                })
+        })
+    })
+})
