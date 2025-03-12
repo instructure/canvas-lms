27cd8a5d733d71af69e4833ff1a4e1da0bf638f5
Daniel, Torok <daniel.torok@instructure.com>
2025-03-12 13:02:02 +0100

    [RCX] Replace jquery media player in Modules
    flag=consolidated_media_player
    refs RCX-3136
    
    test-plan
    - enable ff
    - upload a file directly to a module
    - click on the video under the module
    - verify that the new studio player loads
    
    Change-Id: Iead366837c482dd00ab11b30c027d0cb9e488458
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/372457
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Zoltan Dalmadi <zoltan.dalmadi@instructure.com>

--- spec/selenium/files/new_files_spec.rb
+++ spec/selenium/files/new_files_spec.rb
@@ -348,29 +348,54 @@ describe "better_file_browsing" do
       expect(@kaltura).to receive(:media_sources).and_return([{ attachment_id: @att.id, content_type: "video/mp4", url: "/a.mp4" }])
     end
 
-    it "will show CC options normally", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
-      get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
-      wait_for_ajaximations
-      expect(f('[title="Captions/Subtitles"]')).to be_present
+    context "when consolidated_media_player feature is disabled" do
+      before do
+        Account.site_admin.disable_feature! :consolidated_media_player
+      end
+
+      it "will show CC options normally", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
+        get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
+        wait_for_ajaximations
+        expect(f('[title="Captions/Subtitles"]')).to be_present
+      end
+
+      it "shows caption inheritance tooltip", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
+        @mo.media_tracks.create!(kind: "subtitles", locale: "en", content: "subs")
+        @another_att = Attachment.create! filename: "file.mp4", context: @course, media_entry_id: "mediaentryid", uploaded_data: stub_file_data("test.m4v", "asdf", "video/mp4")
+        get "/courses/#{@course.id}/files/#{@another_att.id}/file_preview"
+        wait_for_ajaximations
+        expect(f(".mejs-captions-selector .track-tip-container")).to be_present
+      end
+
+      it "will hide CC options for locked attachments", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
+        mt = MasterCourses::MasterTemplate.set_as_master_course(@bp_course)
+        cs = MasterCourses::ChildSubscription.create! child_course: @course, master_template: mt
+        MasterCourses::ChildContentTag.create! content_type: "Attachment", content_id: @att.id, migration_id: "matchedmigid", child_subscription: cs
+        mct = MasterCourses::MasterContentTag.create! master_template: mt, content: @bogus_parent_att, restrictions: { content: true }
+        mct.update! migration_id: "matchedmigid"
+        get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
+        wait_for_ajaximations
+        expect(f(".mejs-controls")).not_to contain_jqcss('[title="Captions/Subtitles"]')
+      end
     end
 
-    it "shows caption inheritance tooltip", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
-      @mo.media_tracks.create!(kind: "subtitles", locale: "en", content: "subs")
-      @another_att = Attachment.create! filename: "file.mp4", context: @course, media_entry_id: "mediaentryid", uploaded_data: stub_file_data("test.m4v", "asdf", "video/mp4")
-      get "/courses/#{@course.id}/files/#{@another_att.id}/file_preview"
-      wait_for_ajaximations
-      expect(f(".mejs-captions-selector .track-tip-container")).to be_present
-    end
+    context "when consolidated_media_player feature is enabled" do
+      before do
+        Account.site_admin.enable_feature! :consolidated_media_player
+      end
 
-    it "will hide CC options for locked attachments", upgrade_files_v2: "waiting for deployment (RCX-2974)" do
-      mt = MasterCourses::MasterTemplate.set_as_master_course(@bp_course)
-      cs = MasterCourses::ChildSubscription.create! child_course: @course, master_template: mt
-      MasterCourses::ChildContentTag.create! content_type: "Attachment", content_id: @att.id, migration_id: "matchedmigid", child_subscription: cs
-      mct = MasterCourses::MasterContentTag.create! master_template: mt, content: @bogus_parent_att, restrictions: { content: true }
-      mct.update! migration_id: "matchedmigid"
-      get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
-      wait_for_ajaximations
-      expect(f(".mejs-controls")).not_to contain_jqcss('[title="Captions/Subtitles"]')
+      it "will not show CC button when no CC is uploaded" do
+        get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
+        wait_for_selector('[aria-label="Video Player"]')
+        expect(f("#media_preview")).not_to contain_jqcss('[aria-label="Captions"]')
+      end
+
+      it "will show CC button when a CC is uploaded" do
+        @mo.media_tracks.create!(kind: "subtitles", locale: "en", content: "subs")
+        get "/courses/#{@course.id}/files/#{@att.id}/file_preview"
+        wait_for_selector('[aria-label="Video Player"]')
+        expect(f('[aria-label="Captions"]')).to be_present
+      end
     end
   end
 
