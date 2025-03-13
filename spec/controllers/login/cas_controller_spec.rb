14d3d1e29ac6243a606814513e8436ca40f275b4
Weston Dransfield <wdransfield@instructure.com>
2025-03-13 14:05:53 -0600

    Stop emitting original auth metrics
    Closes Refs AE-1860
    flag=none
    
    Test Plan:
    -specs pass
    
    Change-Id: Ie1f9d2cada8b47580f1a7d20bae48f0b70257f56
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/372624
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hargiss <mhargiss@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Product-Review: Weston Dransfield <wdransfield@instructure.com>

--- spec/controllers/login/cas_controller_spec.rb
+++ spec/controllers/login/cas_controller_spec.rb
@@ -244,10 +244,6 @@ describe Login::CasController do
     expect(Time.now.utc - start).to be < 1
     expect(session[:sentinel]).to be true
     expect(InstStatsd::Statsd).to have_received(:distributed_increment).with(
-      "auth.create.failure", tags: { auth_type: ap.auth_type.to_s, auth_provider_id: ap.global_id, domain: request.host, reason: :timeout }
-    )
-
-    expect(InstStatsd::Statsd).to have_received(:distributed_increment).with(
       "auth.create.failure.v2", tags: { auth_type: ap.auth_type.to_s, auth_provider_id: ap.global_id, domain: request.host, reason: :timeout }
     )
   end
