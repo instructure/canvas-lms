8f6c8820c0b4e22e3bb7fec7a8eaf486d2268e8d
Maya Tyner <maya.tyner@instructure.com>
2025-03-20 15:58:12 -0400

    swap RQD description dialog with InstUI modal
    flag=none
    refs FOO-5257
    
    test-plan:
    - enable Restrict Quantitative Data FF
    - visit accounts/<id>/settings
    - click tooltip beside 'Restrict view of quantitative data'
    	- EXPECT description to render
    	- EXPECT close button to close modal`
    
    Change-Id: If33338c254d85891ead559bc236383c5b763bc9f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/373317
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Adrián Grúber <adrian.gruber@instructure.com>
    QA-Review: Adrián Grúber <adrian.gruber@instructure.com>
    Product-Review: Maya Tyner <maya.tyner@instructure.com>

--- ui/features/account_settings/react/components/RQDModal.tsx
+++ ui/features/account_settings/react/components/RQDModal.tsx
@@ -16,24 +16,23 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {Heading} from '@instructure/ui-heading'
 import {Modal} from '@instructure/ui-modal'
 import {useScope as createI18nScope} from '@canvas/i18n'
-import {Heading} from '@instructure/ui-heading'
 import {CloseButton} from '@instructure/ui-buttons'
+import {Text} from '@instructure/ui-text'
 
-const I18n = createI18nScope('account_reports')
+const I18n = createI18nScope('account_settings')
 
 interface Props {
-  descHTML: string
-  title: string
   closeModal: () => void
 }
 
-export default function ReportDescription(props: Props) {
+export default function RQDModal(props: Props) {
   return (
-    <Modal label={I18n.t('Report Description')} open size="medium">
+    <Modal size="small" open label={I18n.t('Restrict Quantitative Data')}>
       <Modal.Header>
-        <Heading>{props.title}</Heading>
+        <Heading>{I18n.t('Restrict Quantitative Data')}</Heading>
         <CloseButton
           data-testid="close-button"
           placement="end"
@@ -42,7 +41,13 @@ export default function ReportDescription(props: Props) {
           screenReaderLabel={I18n.t('Close')}
         />
       </Modal.Header>
-      <Modal.Body dangerouslySetInnerHTML={{__html: props.descHTML}} />
+      <Modal.Body>
+        <Text data-testid="rqd-modal-text">
+          {I18n.t(
+            "When selected, this setting will limit the view of new courses' quantitative (numeric) grading data. Students and observers will only see qualitative data, which includes letter grades and comments.",
+          )}
+        </Text>
+      </Modal.Body>
     </Modal>
   )
 }
