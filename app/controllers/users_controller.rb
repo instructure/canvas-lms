bb1febf79fabd68597d6f115f09c13a1fa2450f6
Maya Tyner <maya.tyner@instructure.com>
2025-03-25 17:10:01 -0400

    allow courses created in other subaccounts with FF enabled
    when a teacher/student has enrollments in other subaccounts
    allow courses to be created in other subaccounts
    
    simplified the RESTRICT_MCC check down to checking
    if the FF enabled and if this is an admin user
    
    flag=none
    fixes FOO-5261
    
    test-plan:
    - passes new tests
    - follow steps in the attached ticket
    
    Change-Id: I5a338bf94cf671829839541c40195d3e4a3fc15a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/373842
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Maya Tyner <maya.tyner@instructure.com>
    Product-Review: Maya Tyner <maya.tyner@instructure.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -526,10 +526,16 @@ class UsersController < ApplicationController
     js_env({ K5_USER: k5_user && !k5_disabled }, true)
 
     # things needed on both k5 and classic dashboards
-    create_permission_root_account = @current_user.create_courses_right(@domain_root_account)
+    create_permission_root_account = @current_user.create_courses_right(@domain_root_account, check_subaccounts: true)
     create_permission_mcc_account = @current_user.create_courses_right(@domain_root_account.manually_created_courses_account)
     create_permission_alternate_account = @current_user.alternate_account_for_course_creation && @current_user.create_courses_right(@current_user.alternate_account_for_course_creation)
 
+    mcc_only = if create_permission_alternate_account
+                 # admin can always create courses in other accounts/subaccounts
+                 false
+               else
+                 !(create_permission_root_account && @domain_root_account.feature_enabled?(:create_course_subaccount_picker))
+               end
     js_env({
              PREFERENCES: {
                dashboard_view: @current_user.dashboard_view(@domain_root_account),
@@ -542,7 +548,7 @@ class UsersController < ApplicationController
              ALLOW_ELEMENTARY_DASHBOARD: k5_disabled && k5_user,
              CREATE_COURSES_PERMISSIONS: {
                PERMISSION: create_permission_alternate_account || create_permission_root_account || create_permission_mcc_account,
-               RESTRICT_TO_MCC_ACCOUNT: !!(!create_permission_root_account && create_permission_mcc_account) && !(@domain_root_account.feature_enabled?(:create_course_subaccount_picker) && create_permission_alternate_account),
+               RESTRICT_TO_MCC_ACCOUNT: mcc_only,
              },
              OBSERVED_USERS_LIST: observed_users_list,
              CAN_ADD_OBSERVEE: @current_user
