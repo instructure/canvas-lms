bd66aea04b9a5e036abc8f0645f5d6aecb0e68fa
Drake Harper <drake.harper@instructure.com>
2025-03-25 16:46:44 -0600

    spec: Add some tests to modules rewrite
    flag=modules_page_rewrite
    refs LX-2382
    
    Test Plan:
    - specs pass
    
    Change-Id: I4787d7f487ff7b06ccacb99a586805068a62ddc8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/373849
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

---
+++ ui/features/context_modules_v2/react/components/__tests__/ModuleItemSupplementalInfo.test.tsx
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {fireEvent, render} from '@testing-library/react'
+import {ContextModuleProvider} from '../../hooks/useModuleContext'
+import ModuleItemSupplementalInfo from '../ModuleItemSupplementalInfo'
+import type {ModuleContent, CompletionRequirement} from '../../utils/types'
+
+const defaultCompletionRequirement: CompletionRequirement = {
+  id: '19',
+  type: 'must_view',
+  completed: false,
+}
+
+const currentDate = new Date().toISOString()
+const defaultContent: ModuleContent = {
+  id: '19',
+  title: 'Test Module Item',
+  dueAt: currentDate,
+  pointsPossible: 100,
+}
+
+const setUp = (content: ModuleContent = defaultContent, completionRequirement: CompletionRequirement | null = defaultCompletionRequirement) => {
+  return render(
+    <ContextModuleProvider courseId="1" isMasterCourse={true} isChildCourse={false}>
+      <ModuleItemSupplementalInfo
+        content={content}
+        completionRequirement={completionRequirement ?? undefined}
+        contentTagId="19"
+      />
+    </ContextModuleProvider>
+  )
+}
+
+describe('ModuleItemSupplementalInfo', () => {
+    it('renders', () => {
+      const container = setUp()
+      expect(container.container).toBeInTheDocument()
+      expect(container.getAllByText('|')).toHaveLength(2)
+    })
+
+    it('does not render', () => {
+        const container = setUp({...defaultContent, dueAt: undefined, pointsPossible: undefined}, null)
+        expect(container.container).toBeInTheDocument()
+        expect(container.queryByText(new Date(currentDate).toLocaleDateString())).not.toBeInTheDocument()
+        expect(container.queryAllByText('|')).toHaveLength(0)
+      })
+
+    describe('due at', () => {
+      it('renders', () => {
+        const container = setUp(defaultContent, null)
+        expect(container.container).toBeInTheDocument()
+        expect(container.getByText(new Date(currentDate).toLocaleDateString())).toBeInTheDocument()
+        expect(container.getAllByText('|')).toHaveLength(1)
+      })
+
+      it('does not render', () => {
+        const container = setUp({...defaultContent, dueAt: undefined})
+        expect(container.container).toBeInTheDocument()
+        expect(container.queryByText(new Date(currentDate).toLocaleDateString())).not.toBeInTheDocument()
+        expect(container.queryAllByText('|')).toHaveLength(1)
+      })
+    })
+
+    describe('points possible', () => {
+      it('renders', () => {
+        const container = setUp()
+        expect(container.container).toBeInTheDocument()
+        expect(container.getByText('100 pts')).toBeInTheDocument()
+      })
+
+      it('does not render', () => {
+        const container = setUp({...defaultContent, pointsPossible: undefined})
+        expect(container.container).toBeInTheDocument()
+        expect(container.queryByText('100 pts')).not.toBeInTheDocument()
+      })
+    })
+
+
+})
