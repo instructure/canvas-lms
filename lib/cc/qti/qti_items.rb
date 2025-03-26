ac6a2a60398e1975e912f03aafd7a410005ce96e
Mohammad Hajjaj <mohammad.hajjaj@instructure.com>
2025-03-26 10:40:15 +0300

    Add passage meta field in qti for 'text_only_question' questions
    refs QUIZ-15404
    
    flag=none
    
    Test plan:
    1- create a classic quiz with question type 'text_only_question'
    2- in course settings, export the course
    3- download the exported package
    4- open the qti file with any text editor
    5- notice the 'passage' meta field is added with value 'true'
    
    Change-Id: I97fd14f237d052b78848535203dbfcbbec48db72
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/373858
    Reviewed-by: Zoltan Hegedus <zoltan.hegedus@instructure.com>
    Reviewed-by: Gergely Knipl <gergely.knipl@instructure.com>
    QA-Review: Peter Zaj <peter.zaj@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Mohammad Hajjaj <mohammad.hajjaj@instructure.com>

--- lib/cc/qti/qti_items.rb
+++ lib/cc/qti/qti_items.rb
@@ -118,6 +118,9 @@ module CC
                 meta_field(qm_node, "question_type", question["question_type"])
                 meta_field(qm_node, "points_possible", question["points_possible"])
                 meta_field(qm_node, "original_answer_ids", question["answers"].pluck("id").join(","))
+                if question["question_type"] == "text_only_question"
+                  meta_field(qm_node, "passage", "true")
+                end
                 if question[:is_quiz_question]
                   meta_field(qm_node, "assessment_question_identifierref", aq_mig_id(question))
                 end
