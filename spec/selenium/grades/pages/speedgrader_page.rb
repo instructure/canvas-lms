4358e992529dd78de3e79c834d2bc4a6f22a4470
Caleb Guanzon <cguanzon@instructure.com>
2025-03-31 10:56:20 -0600

    streamline getDefaultDiscussionView logic
    [fsc-timeout=60]
    [fsc-node-count=10]
    
    flag=discussions_speedgrader_revisit
    flag=discussion_checkpoints
    
    fixes EGG-995
    fixes EGG-975
    
    as part of 975, tests around
    this area was strengthened with
    wait_for explicit polling
    
    test plan:
    - discussion_checkpoints ff OFF
    - discussions_speedgrader_revisit ff ON
    - as 2 students, reply to a non-checkpointed
    graded discussion
    - as a teacher, visit speedgrader, set your speedgrader
    preference for discussion view to "Show replies in context"
    - save
    - verify you see the full discussion
    - reload
    - verify you still see the full discussion
    
    Change-Id: I006f21230fa4b381fa671f7d4f837013eccd1ebe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/374328
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Reviewed-by: Chawn Neal <chawn.neal@instructure.com>
    Product-Review: Caleb Guanzon <cguanzon@instructure.com>

--- spec/selenium/grades/pages/speedgrader_page.rb
+++ spec/selenium/grades/pages/speedgrader_page.rb
@@ -705,5 +705,38 @@ class Speedgrader
       f(".flashalert-message button").click
       Speedgrader.comment_library_close_button.click
     end
+
+    def wait_for_speedgrader_iframe(timeout = 5)
+      wait_for(method: nil, timeout:) { f("#speedgrader_iframe") }
+    end
+
+    def wait_for_discussions_iframe(timeout = 5)
+      wait_for(method: nil, timeout:) { f("#discussion_preview_iframe") }
+    end
+
+    def wait_for_first_reply_button(timeout = 5)
+      wait_for(method: nil, timeout:) { f("[data-testid='discussions-first-reply-button']") }
+    end
+
+    # expects #speedgrader_iframe and #discussion_preview_iframe to be loaded
+    def wait_for_all_speedgrader_iframes_to_load
+      Speedgrader.wait_for_speedgrader_iframe
+      in_frame("speedgrader_iframe") do
+        Speedgrader.wait_for_discussions_iframe
+        in_frame("discussion_preview_iframe") do
+          wait_for_ajaximations
+          yield if block_given?
+        end
+      end
+    end
+
+    # the parent speedgrader_iframe is #speedgrader_iframe, this is the left pane of the speedgrader
+    def wait_for_parent_speedgrader_iframe_to_load
+      Speedgrader.wait_for_speedgrader_iframe
+      in_frame("speedgrader_iframe") do
+        wait_for_ajaximations
+        yield if block_given?
+      end
+    end
   end
 end
