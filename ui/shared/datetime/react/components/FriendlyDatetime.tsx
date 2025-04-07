ff6b59438689e84ede26540bdc67b4b1b8c0855a
Sarah Gerard <sarah.gerard@instructure.com>
2025-04-07 15:04:50 -0700

    Fix screenreader content for created and last modified
    this removes extra screen reader content from the created
    at and last modified at columns to ensure that focus
    does not jump when using screenreaders on the table.
    
    closes RCX-3291
    flag=files_a11y_rewrite
    
    test plan:
    - using voice over, you can use control+option+left/right
      arrows to navigate through a row's columns
    - ensure that when reading the created at columns and last
      modified at columns, screen reader focus does not jump.
    - ensure that the format of the dates has not changed.
    - ensure format of the dates looks correct in mobile view
      (should be mm/dd/yy format)
    
    Change-Id: I0a9719e38e4a67481122b0cfa418f891a9115b69
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/375004
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob DeWar <jacob.dewar@instructure.com>
    QA-Review: Jacob DeWar <jacob.dewar@instructure.com>
    Product-Review: Kata Virag Keri <kata.keri@instructure.com>

--- ui/shared/datetime/react/components/FriendlyDatetime.tsx
+++ ui/shared/datetime/react/components/FriendlyDatetime.tsx
@@ -32,6 +32,7 @@ type Props = {
   prefix?: string
   prefixMobile?: string
   showTime?: boolean
+  includeScreenReaderContent?: boolean
 }
 
 class FriendlyDatetime extends Component<Props> {
@@ -42,6 +43,7 @@ class FriendlyDatetime extends Component<Props> {
     prefix: '',
     prefixMobile: null,
     showTime: false,
+    includeScreenReaderContent: true,
   }
 
   // The original render function is really slow because of all
@@ -51,7 +53,7 @@ class FriendlyDatetime extends Component<Props> {
   render = memoize(
     () => {
       // Separate props not used by the `time` element
-      const {prefixMobile, showTime, ...timeElementProps} = this.props
+      const {prefixMobile, showTime, includeScreenReaderContent, ...timeElementProps} = this.props
 
       let datetime = this.props.dateTime
       if (!datetime) {
@@ -89,26 +91,37 @@ class FriendlyDatetime extends Component<Props> {
         fixedPrefixMobile += ' '
       }
 
+      const visibleElements = (
+        <>
+          <span className="visible-desktop">
+            {/* something like: Mar 6, 2014 */}
+            {fixedPrefix + friendly}
+          </span>
+          <span className="hidden-desktop">
+            {/* something like: 3/3/2014 */}
+            {(fixedPrefixMobile || '') + fudged.toLocaleDateString()}
+          </span>
+        </>
+      )
+
       return (
         <span data-testid="friendly-date-time">
-          <ScreenReaderContent>{fixedPrefix + friendly}</ScreenReaderContent>
-
-          <time
-            {...timeProps}
-            ref={c => {
-              this.time = c
-            }}
-            aria-hidden="true"
-          >
-            <span className="visible-desktop">
-              {/* something like: Mar 6, 2014 */}
-              {fixedPrefix + friendly}
-            </span>
-            <span className="hidden-desktop">
-              {/* something like: 3/3/2014 */}
-              {(fixedPrefixMobile || '') + fudged.toLocaleDateString()}
-            </span>
-          </time>
+          {includeScreenReaderContent ? (
+            <>
+              <ScreenReaderContent>{fixedPrefix + friendly}</ScreenReaderContent>
+              <time
+                {...timeProps}
+                ref={c => {
+                  this.time = c
+                }}
+                aria-hidden="true"
+              >
+                {visibleElements}
+              </time>
+            </>
+          ) : (
+            visibleElements
+          )}
         </span>
       )
     },
