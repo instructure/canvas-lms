5b670e7b3a95c7397c2401bbc575ee9decfa1d1e
Jacob DeWar <jacob.dewar@instructure.com>
2025-04-17 08:19:18 -0400

    Delay create folder alert until after focus was returned
    fixes RCX-3287
    flag=files_a11y_rewrite
    
    Test Plan:
     - Go to Files
     - Turn screenreader on
     - Create a new Folder
     - Focus is retuned to Create Folder button
     * Screenreader reads out success alert
    
     - Open create folder modal
     - Cause api call to error
     * Use Dev Tools to block url
     - Try to create folder
     * Error alert appears as normal
    
    Change-Id: I723b202105a6923264a33c0225f8625c51fb1d32
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/376052
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Jacob DeWar <jacob.dewar@instructure.com>

--- ui/features/files_v2/react/components/FilesHeader/CreateFolderButton.tsx
+++ ui/features/files_v2/react/components/FilesHeader/CreateFolderButton.tsx
@@ -16,12 +16,13 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import {IconAddLine} from '@instructure/ui-icons'
 import {useScope as createI18nScope} from '@canvas/i18n'
 import {Button} from '@instructure/ui-buttons'
 import CreateFolderModal from './CreateFolderModal'
 import {createPortal} from 'react-dom'
+import {showFlashSuccess} from '@canvas/alerts/react/FlashAlert'
 
 const I18n = createI18nScope('files_v2')
 
@@ -31,6 +32,7 @@ interface CreateFolderButtonProps {
 
 const CreateFolderButton = ({buttonDisplay}: CreateFolderButtonProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const buttonRef = useRef<Button | null>(null)
 
   const handleOpenModal = () => {
     setIsModalOpen(true)
@@ -39,14 +41,25 @@ const CreateFolderButton = ({buttonDisplay}: CreateFolderButtonProps) => {
   const handleCloseModal = () => {
     setIsModalOpen(false)
   }
+
+  const handleExitedModal = (wasSuccessful?: boolean) => {
+    buttonRef.current?.focus()
+    if (wasSuccessful) {
+      setTimeout(() => {
+        showFlashSuccess(I18n.t('Folder created successfully'))()
+      }, 2000)
+    }
+  }
   return (
     <>
-      {
-        createPortal(
-          <CreateFolderModal isOpen={isModalOpen} onRequestClose={handleCloseModal} />,
-          document.body,
-        )
-      }
+      {createPortal(
+        <CreateFolderModal
+          isOpen={isModalOpen}
+          onRequestClose={handleCloseModal}
+          onExited={handleExitedModal}
+        />,
+        document.body,
+      )}
       <Button
         color="secondary"
         renderIcon={<IconAddLine />}
@@ -54,6 +67,7 @@ const CreateFolderButton = ({buttonDisplay}: CreateFolderButtonProps) => {
         onClick={handleOpenModal}
         data-testid="create-folder-button"
         elementRef={elt => elt?.setAttribute('aria-label', I18n.t('Add Folder'))}
+        ref={buttonRef}
       >
         {I18n.t('Folder')}
       </Button>
