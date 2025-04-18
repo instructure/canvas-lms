550f73ebf53f90a5f249f7b8e3cedd32acdf7cf7
Robin Kuss <rkuss@instructure.com>
2025-04-18 10:48:46 -0500

    spec: create shared example infrastructure for module perf
    Closes LX-2563
    
    Test Plan: passes jenkins
    
    flag=modules_perf
    
    [fsc-node-count=30]
    [fsc-timeout=60]
    
    Change-Id: I4abb5dc17c89ff2e91984d4256d80d44bd2449f5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/376169
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    QA-Review: Ed Schiebel <eschiebel@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>

--- spec/selenium/context_modules/performance_update/context_modules_teacher_spec.rb
+++ spec/selenium/context_modules/performance_update/context_modules_teacher_spec.rb
@@ -28,7 +28,11 @@ require_relative "../../helpers/public_courses_context"
 require_relative "../page_objects/modules_index_page"
 require_relative "../page_objects/modules_settings_tray"
 require_relative "../../helpers/items_assign_to_tray"
+require_relative "../../dashboard/pages/k5_dashboard_page"
+require_relative "../../dashboard/pages/k5_dashboard_common_page"
+require_relative "../../../helpers/k5_common"
 require_relative "../shared_examples/context_modules_teacher_shared_examples"
+require_relative "../shared_examples/modules_performance_teacher_shared_examples"
 
 describe "context modules" do
   include_context "in-process server selenium tests"
@@ -36,6 +40,9 @@ describe "context modules" do
   include ModulesIndexPage
   include ModulesSettingsTray
   include ItemsAssignToTray
+  include K5Common
+  include K5DashboardPageObject
+  include K5DashboardCommonPageObject
 
   context "as a teacher", priority: "1" do
     before(:once) do
@@ -158,5 +165,41 @@ describe "context modules" do
         expect(copy_to_tray_exists?).to be true
       end
     end
+  end
+
+  context "as a teacher with many module items on the modules page" do
+    before(:once) do
+      course_with_teacher(active_all: true)
+
+      @course.account.enable_feature!(:modules_perf)
+      Setting.set("module_perf_threshold", -1)
+
+      @module_list = big_course_setup
+      @course.reload
+    end
+
+    before do
+      user_session(@teacher)
+    end
+
+    it_behaves_like "module performance for teachers", :context_modules
+    it_behaves_like "module performance for teachers", :course_homepage
+  end
+
+  context "as a canvas for elementary teacher with many module items", :ignore_js_errors do
+    before(:once) do
+      teacher_setup
+      @subject_course.account.enable_feature!(:modules_perf)
+      Setting.set("module_perf_threshold", -1)
+      @course = @subject_course
+      @module_list = big_course_setup
+      @subject_course.reload
+    end
+
+    before do
+      user_session(@teacher)
+    end
+
+    it_behaves_like "module performance for teachers", :canvas_for_elementary
   end
 end
