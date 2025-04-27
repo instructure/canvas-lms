5918d4e5946f72378f9a71ff6bad8a5b9f471e9d
alvaro.talavera <alvaro.talavera@instructure.com>
2025-04-27 15:13:26 -0300

    refactor discussion form options
    closes VICE-5043
    
    flag=none
    
    test plan:
    - as a teacher go to discussions
    - create a new discussion and test the options funcionality
    - edit a discussion and test the options checkboxes
    - announcements are also discussions so it need to be tested
    - check that the contrast is ok for the checkboxes and radios
      of the options
    
    Change-Id: I633251402f8e3e8c707136999dd4a031af507b8e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/377331
    Reviewed-by: Murilo Paiva <murilo.paiva@instructure.com>
    QA-Review: Adam Szabo <adam.szabo@instructure.com>
    Product-Review: Tamás Balogh <tamas.balogh@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/selenium/rcs/discussions_post_grades_to_sis_spec.rb
+++ spec/selenium/rcs/discussions_post_grades_to_sis_spec.rb
@@ -43,7 +43,7 @@ describe "sync grades to sis" do
   it "does not display Sync to SIS option when feature not configured", priority: "1" do
     Account.default.set_feature_flag!("post_grades", "off")
     get "/courses/#{@course.id}/discussion_topics/new"
-    f("#use_for_grading").click
+    f("label[for='use_for_grading']").click
     expect(f("#content")).not_to contain_css("#assignment_post_to_sis")
   end
 
@@ -51,7 +51,7 @@ describe "sync grades to sis" do
     before do
       get "/courses/#{@course.id}/discussion_topics/new"
       f("#discussion-title").send_keys("New Discussion Title")
-      f("#use_for_grading").click
+      f("label[for='use_for_grading']").click
       f("#assignment_post_to_sis").click
       wait_for_ajaximations
       click_option("#assignment_group_id", "Assignment Group")
