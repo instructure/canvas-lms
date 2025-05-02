a80e39b87d79eb3a86d98df5437f8ff607604789
dan-costin <dan.costin@instructure.com>
2025-05-02 14:42:32 +0300

    Add validation errors in assignment and submission queries
    Flag: project_lhotse 
    Closes: EVAL-5406
    
    Test Plan
    1. In GraphiQL, run the following query:
    ```
    query assignmentQuery {
      assignment(id: "1") {
        autoGradeAssignmentErrors
      }
    }
    ```
    3. You should receive a response similar to:
    ```
    {
      "data": {
        "assignment": {
          "autoGradeAssignmentErrors": []
        }
      }
    }
    ```
    
    Change-Id: I322e403daf30af9fdca5747e1deec1fa5c915923
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/376915
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Dan Costin <dan.costin@instructure.com>
    Product-Review: Cameron Ray <cameron.ray@instructure.com>

---
+++ spec/graphql/graph_ql_helpers/auto_grade_eligibility_helper_spec.rb
@@ -0,0 +1,157 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+describe GraphQLHelpers::AutoGradeEligibilityHelper do
+  before :once do
+    course_with_teacher(active_all: true)
+    @student = student_in_course(course: @course).user
+  end
+
+  describe ".validate_assignment" do
+    let!(:assignment) { assignment_model(course: @course) }
+
+    let!(:rubric) do
+      rubric_model(
+        context: @course,
+        data: [
+          {
+            points: 5,
+            description: "Criterion 1",
+            long_description: "Criterion 1 description",
+            ratings: [
+              {
+                points: 5,
+                description: "Excellent",
+                long_description: "Excellent performance"
+              }
+            ]
+          }
+        ]
+      )
+    end
+
+    context "when CedarClient is disabled" do
+      it "returns a grading assistance error" do
+        rubric_association_model(association: assignment, rubric:)
+        assignment.reload
+
+        allow(CedarClient).to receive(:enabled?).and_return(false)
+        issues = described_class.validate_assignment(assignment:)
+        expect(issues).to include("Grading Assistance is not available right now.")
+      end
+    end
+
+    context "when no rubric is attached" do
+      it "returns a missing rubric error" do
+        allow(CedarClient).to receive(:enabled?).and_return(true)
+        issues = described_class.validate_assignment(assignment:)
+        expect(issues).to include("No rubric is attached to this assignment.")
+      end
+    end
+
+    context "when a rating is missing long_description" do
+      it "returns a rating description error" do
+        rubric.data[0][:ratings][0][:long_description] = nil
+        rubric.save!
+
+        ra = rubric_association_model(
+          association: assignment,
+          rubric:,
+          purpose: "grading",
+          use_for_grading: true
+        )
+        ra.save!
+        assignment.reload
+
+        allow(CedarClient).to receive(:enabled?).and_return(true)
+        expect(assignment.rubric_association).to be_present
+        expect(assignment.rubric_association.rubric).to eq(rubric)
+
+        issues = described_class.validate_assignment(assignment:)
+        expect(issues).to include("Rubric is missing rating description.")
+      end
+    end
+  end
+
+  describe ".validate_submission" do
+    let!(:assignment) do
+      rubric = rubric_model(context: @course)
+      assignment = assignment_model(course: @course)
+      rubric_association_model(association: assignment, rubric:)
+      assignment
+    end
+
+    context "with a valid submission" do
+      it "returns no issues" do
+        submission = submission_model(
+          user: @student,
+          assignment:,
+          body: "This is a valid essay submission.",
+          submission_type: "online_text_entry",
+          attachments: []
+        )
+        issues = described_class.validate_submission(submission:)
+        expect(issues).to be_empty
+      end
+    end
+
+    context "when submission is blank" do
+      it "returns missing submission error" do
+        submission = submission_model(
+          user: @student,
+          assignment:,
+          body: "",
+          submission_type: "online_text_entry",
+          attachments: []
+        )
+        issues = described_class.validate_submission(submission:)
+        expect(issues).to include("No essay submission found.")
+      end
+    end
+
+    context "when submission has less than 5 words" do
+      it "returns word count error" do
+        submission = submission_model(
+          user: @student,
+          assignment:,
+          body: "Too short",
+          submission_type: "online_text_entry",
+          attachments: []
+        )
+        issues = described_class.validate_submission(submission:)
+        expect(issues).to include("Submission must be at least 5 words.")
+      end
+    end
+
+    context "when submission type is not text entry" do
+      it "returns submission type error" do
+        submission = submission_model(
+          user: @student,
+          assignment:,
+          body: "This is a valid essay submission.",
+          submission_type: "online_upload",
+          attachments: []
+        )
+        issues = described_class.validate_submission(submission:)
+        expect(issues).to include("Submission must be a text entry type.")
+      end
+    end
+  end
+end
