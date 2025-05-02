bc30a84cc8d093d84e9bb57575953d22a28b32cd
Robin Kuss <rkuss@instructure.com>
2025-05-02 14:12:17 -0500

    spec: add pagination tests for modules perf
    Closes LX-2615
    
    flag=modules_perf
    
    [fsc-node-count=30]
    [fsc-timeout=60]
    
    Test Plan: passes Jenkins
    
    Change-Id: I1ff12250b7ad89c37eab52b59a72420cfee10f1c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/377277
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

--- spec/selenium/context_modules/performance_update/context_modules_teacher_spec.rb
+++ spec/selenium/context_modules/performance_update/context_modules_teacher_spec.rb
@@ -32,8 +32,8 @@ require_relative "../../dashboard/pages/k5_dashboard_page"
 require_relative "../../dashboard/pages/k5_dashboard_common_page"
 require_relative "../../../helpers/k5_common"
 require_relative "../shared_examples/context_modules_teacher_shared_examples"
-require_relative "../shared_examples/modules_performance_teacher_shared_examples"
 require_relative "../shared_examples/module_show_all_o_less_shared_examples"
+require_relative "../shared_examples/modules_performance_shared_examples"
 
 describe "context modules" do
   include_context "in-process server selenium tests"
@@ -193,8 +193,8 @@ describe "context modules" do
       user_session(@teacher)
     end
 
-    it_behaves_like "module performance for teachers", :context_modules
-    it_behaves_like "module performance for teachers", :course_homepage
+    it_behaves_like "module performance with module items", :context_modules
+    it_behaves_like "module performance with module items", :course_homepage
   end
 
   context "as a canvas for elementary teacher with many module items", :ignore_js_errors do
@@ -211,6 +211,6 @@ describe "context modules" do
       user_session(@teacher)
     end
 
-    it_behaves_like "module performance for teachers", :canvas_for_elementary
+    it_behaves_like "module performance with module items", :canvas_for_elementary
   end
 end
