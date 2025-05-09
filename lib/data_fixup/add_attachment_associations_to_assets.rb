bb5351ca4964bf109cd99e4fa17db662ec3e8f00
Kasra Rismanchi <kasra.rismanchi@instructure.com>
2025-05-09 13:48:45 +0200

    data fixup for attachment_association on syllabus_body
    flag=none
    test plan:
     - tests passing
     - create a course syllabus with attachments on its body
       make sure disable_file_verifiers_in_public_syllabus ff is disabled
     - through rails console check that the attachmetnts that are linked
       in syllabus do not have attachment associations
     - run the migration, check the attachment for their
       attachment association, they should apear and user_id
       must be nil for them
    
    closes: GROW-36
    Change-Id: If2984a222bd7629f5005a4e1b7e8bc485a162620
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/377946
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    QA-Review: Mysti Lilla <mysti@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Kasra Rismanchi <kasra.rismanchi@instructure.com>

---
+++ lib/data_fixup/add_attachment_associations_to_assets.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2024 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require "nokogiri"
+module DataFixup::AddAttachmentAssociationsToAssets
+  CONTENT_MAP = [
+    { Course => :syllabus_body },
+  ].freeze
+  def self.process_model_and_create_attachment_association(model, field, batch_ids)
+    model.where(id: batch_ids).find_each do |active_record|
+      next unless (field && active_record[field]) || active_record.is_a?(Quizzes::Quiz)
+
+      if active_record.is_a?(AssessmentQuestion) || active_record.is_a?(Quizzes::QuizQuestion)
+        html = active_record.question_data.to_hash["question_text"]
+        UserContent.associate_attachments_to_rce_object(html, active_record, blank_user: true)
+      elsif active_record.is_a?(Quizzes::Quiz)
+        active_record.quiz_data & map do |question|
+          question_html = question["question_text"]
+          UserContent.associate_attachments_to_rce_object(question_html, active_record, blank_user: true)
+          next unless question["answers"]
+
+          question["answers"] = question["answers"].map do |a|
+            answer_html = a["text"]
+            UserContent.associate_attachments_to_rce_object(answer_html, active_record, blank_user: true)
+          end
+        end
+      else
+        UserContent.associate_attachments_to_rce_object(active_record[field], active_record, ((field == :syllabus_body) ? "syllabus_body" : nil), blank_user: true)
+      end
+    end
+  end
+
+  def self.run
+    CONTENT_MAP.each do |model_map|
+      model_map.each do |model, field|
+        model.where(
+          "#{field} LIKE ? OR #{field} LIKE ?",
+          "%/media_attachments_iframe/%",
+          "%/files/%"
+        ).find_ids_in_batches do |batch_ids|
+          delay_if_production(
+            priority: Delayed::LOW_PRIORITY,
+            n_strand: ["DataFixup::AddAttachmentAssociationsToAssets", Shard.current.database_server.id]
+          ).process_model_and_create_attachment_association(model, field, batch_ids)
+        end
+      end
+    end
+  end
+end
