487027fccf7f1aa38adbf7f0d9b747707110443e
Martin Yosifov <martin.yosifov@instructure.com>
2025-05-12 17:59:57 +0300

    Fix "View User Tags" element accessibility issues
    closes EGG-1119
    flag=differentiation_tags
    
    Test plan:
    - Enable Differentiation Tags
    - Create a course with student
    - Go to Course -> People
    - Create differentiation tag and assign it to the student
    - Start a screen reader
    - Tab to the tag icon next to the student's name
    - Verify that the screen reader:
    -- sees the element as a button
    -- reads "View {student's name} User Tags"
    
    Change-Id: I526712b6f13832da094e3acec9dceda101a9f24f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/378138
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Angela Gomba <angela.gomba@instructure.com>
    QA-Review: Angela Gomba <angela.gomba@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

--- spec/selenium/people/differentiation_tag_management_spec.rb
+++ spec/selenium/people/differentiation_tag_management_spec.rb
@@ -144,13 +144,13 @@ describe "Differentiation Tag Management" do
           it "Stops displaying differentiation tag UI if the setting is turned off" do
             expect(f("body")).to contain_jqcss("input[type='checkbox'][aria-label^='Select']")
             expect(f("body")).to contain_jqcss("button[data-testid='user-diff-tag-manager-tag-as-button']")
-            expect(f("body")).to contain_jqcss("a[aria-label='View user tags']")
+            expect(f("body")).to contain_jqcss("a[aria-label^='View '][aria-label$=' user tags']")
             @course.account.settings[:allow_assign_to_differentiation_tags] = { value: false }
             @course.account.save!
             refresh_page
             expect(f("body")).not_to contain_jqcss("input[type='checkbox'][aria-label^='Select']")
             expect(f("body")).not_to contain_jqcss("button[data-testid='user-diff-tag-manager-tag-as-button']")
-            expect(f("body")).not_to contain_jqcss("a[aria-label='View user tags']")
+            expect(f("body")).not_to contain_jqcss("a[aria-label^='View '][aria-label$=' user tags']")
           end
 
           it "properly handles paginated users" do
@@ -437,7 +437,7 @@ describe "Differentiation Tag Management" do
           expect(fj("[data-testid='tag-info']:contains('#{@multiple_tags_2.name}')").text).to include("0 students")
 
           # remove the user from the tag
-          f("a[aria-label='View user tags']").click
+          f("a[aria-label='View #{@student.name} user tags']").click
           wait_for_ajaximations
           f("button[data-testid='user-tag-#{@multiple_tags_1.id}']").click
           wait_for_ajaximations
