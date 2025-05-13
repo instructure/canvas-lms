2d7436eec63eeb1997c31dd50753e00aea86976c
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-05-13 11:55:03 -0600

    split out EditView6 test file
    Change-Id: I5acb80cc455f17b830e738f78c8bb33cdbf5265a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/378279
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/__tests__/DiscussionPostToolbar1.test.jsx
+++ ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/__tests__/DiscussionPostToolbar1.test.jsx
@@ -74,7 +74,11 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
-const setup = (props, mocks, discussionManagerProviderValues = {translationLanguages: {current: []}}) => {
+const setup = (
+  props,
+  mocks,
+  discussionManagerProviderValues = {translationLanguages: {current: []}},
+) => {
   return render(
     <MockedProvider mocks={mocks}>
       <AlertManagerContext.Provider
@@ -108,21 +112,36 @@ describe('DiscussionPostToolbar', () => {
 
   describe('Splitscreen Button', () => {
     it('should call updateUserDiscussionsSplitscreenView mutation when clicked', async () => {
+      // Reset mocks to ensure clean state
+      jest.clearAllMocks()
+
+      // Mock functions
+      const setUserSplitScreenPreference = jest.fn()
+      const closeView = jest.fn()
+
       const {getByTestId} = setup(
         {
-          setUserSplitScreenPreference: jest.fn(),
+          setUserSplitScreenPreference,
           userSplitScreenPreference: false,
-          closeView: jest.fn(),
+          closeView,
         },
         updateUserDiscussionsSplitscreenViewMock({discussionsSplitscreenView: true}),
       )
 
+      // Get and click the button
       const splitscreenButton = getByTestId('splitscreenButton')
       fireEvent.click(splitscreenButton)
 
-      await waitFor(() => {
-        expect(onSuccessStub.mock.calls).toHaveLength(1)
-      })
+      // Wait for the success callback to be called
+      await waitFor(
+        () => {
+          expect(onSuccessStub).toHaveBeenCalled()
+        },
+        {timeout: 2000},
+      )
+
+      // Verify the preference was updated
+      expect(setUserSplitScreenPreference).toHaveBeenCalled()
     })
   })
 
@@ -202,146 +221,6 @@ describe('DiscussionPostToolbar', () => {
         isAdmin: false,
       })
       expect(container.queryByTestId('groups-menu-button')).toBeNull()
-    })
-  })
-
-  describe('Anonymous Indicator Avatar', () => {
-    describe('discussion is anonymous', () => {
-      it('should render discussionAnonymousState is not null', () => {
-        ENV.current_user_roles = ['student']
-        const container = setup({
-          discussionAnonymousState: 'full_anonymity',
-        })
-        expect(container.queryByTestId('anonymous_avatar')).toBeTruthy()
-      })
-    })
-
-    describe('discussion is not anonymous', () => {
-      it('should render discussionAnonymousState is null', () => {
-        ENV.current_user_roles = ['student']
-        const container = setup({
-          discussionAnonymousState: null,
-        })
-        expect(container.queryByTestId('anonymous_avatar')).toBeNull()
-      })
-    })
-  })
-
-  describe('Assign To', () => {
-    it('renders the Assign To button if user can manageAssignTo and in a course discussion', () => {
-      const {getByRole} = setup({
-        manageAssignTo: true,
-        showAssignTo: true,
-      })
-      expect(getByRole('button', {name: 'Assign To'})).toBeInTheDocument()
-    })
-
-    it('does not render the Assign To button if in speedGrader', () => {
-      jest.mock('../../../utils/constants', () => ({
-        ...jest.requireActual('../../../utils/constants'),
-        isSpeedGraderInTopUrl: true,
-      }))
-
-      const container = setup({
-        manageAssignTo: true,
-        discussionTopic: {
-          _id: '1',
-          contextType: 'Course',
-          groupSet: null,
-          assignment: true,
-        },
-      })
-      expect(container.queryByTestId('assign-to-button')).toBeNull()
-    })
-
-    it('does not render the Assign To button if user can not manageAssignTo', () => {
-      const {queryByRole} = setup({
-        manageAssignTo: false,
-        contextType: 'Course',
-      })
-      expect(queryByRole('button', {name: 'Assign To'})).not.toBeInTheDocument()
-    })
-
-    it('does not render the Assign To button if a group discussion', () => {
-      const {queryByText} = setup({
-        manageAssignTo: true,
-        contextType: 'Group',
-      })
-      expect(queryByText('Assign To')).not.toBeInTheDocument()
-    })
-
-    it('does not render the Assign To button if an ungraded group discussion in course context', () => {
-      const {queryByTestId} = setup({
-        manageAssignTo: true,
-        contextType: 'Course',
-        isGraded: false,
-        isGroupDiscussion: true,
-      })
-      expect(queryByTestId('manage-assign-to')).not.toBeInTheDocument()
-    })
-  })
-
-  describe('Translate Button', () => {
-    describe('when translationLanguages is empty', () => {
-      it('does not render the translate button', () => {
-        const {queryByTestId} = setup()
-        expect(queryByTestId('translate-button')).toBeNull()
-      })
-    })
-
-    describe('when the discussion topic is an announcement', () => {
-      it('does not render the translate button', () => {
-        const {queryByTestId} = setup({isAnnouncement: true}, null, {
-          translationLanguages: {current: ['en', 'es']}
-        })
-        expect(queryByTestId('translate-button')).toBeNull()
-      })
-    })
-
-    describe('when the improvement flag is turned on', () => {
-      beforeEach(() => {
-        ENV.ai_translation_improvements = true
-      })
-
-      afterEach(() => {
-        ENV.ai_translation_improvements = false
-      })
-
-      it('does render the translate button with improved text', () => {
-        const {getByText} = setup(null, null, {
-          translationLanguages: {current: ['en', 'es']}
-        })
-
-        expect(getByText('Translate Discussion')).toBeTruthy()
-      })
-
-      it('does render the translate button with improved text when the translation controls are on', () => {
-        const {getByText} = setup(null, null, {
-          translationLanguages: {current: ['en', 'es']},
-          showTranslationControl: true,
-        })
-
-        expect(getByText('Turn off Translation')).toBeTruthy()
-      })
-    })
-
-    it('does render the translate button', () => {
-      const {getByTestId} = setup(null, null, {
-        translationLanguages: {current: ['en', 'es']}
-      })
-
-      expect(getByTestId('translate-button')).toBeTruthy()
-    })
-
-    it('does call setShowTranslationControl when clicked', () => {
-      const setShowTranslationControl = jest.fn()
-      const {getByTestId} = setup(null, null, {
-        translationLanguages: {current: ['en', 'es']},
-        setShowTranslationControl,
-      })
-      const translateButton = getByTestId('translate-button')
-      fireEvent.click(translateButton)
-      expect(setShowTranslationControl).toHaveBeenCalled()
     })
   })
 })
