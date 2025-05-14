80ca4741c734f03373e3c19889b896904f8ba675
Jacob DeWar <jacob.dewar@instructure.com>
2025-05-14 15:50:24 -0400

    Refetch only after all file uploads complete
    closes RCX-3264
    flag=files_a11y_rewrite
    
    Test Plan:
     * Test with 2 types of upload failures:
     * Name conflicts (409 error)
     * Other upload error - ex: upload a 0 byte file
    
     - Upload one file
     * Table and quota are refetched after upload
    
     - Upload multiple files
     * Refetches once, after all uploads complete
    
     - Upload multiple files, have first file fail
     * Refetches once, after all uploads complete
    
     - Upload multiple files, have last file fail
     * Refetches once, after all uploads complete
    
     - Upload multiple files, with 1+ failures
     * Refetches once, after all uploads complete
     - Remove errored files with X button
     * No extra refetches done
    
    Change-Id: I73dbdb9e72666f00d414c9ae98797be9bbc0f6a3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/378406
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: June Eoh <jungsun.eoh@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Reviewed-by: Ferenc Marcsó <ferenc.marcso@instructure.com>
    Product-Review: Ferenc Marcsó <ferenc.marcso@instructure.com>

--- ui/features/files_v2/react/components/FilesHeader/__tests__/CurrentUploads.test.tsx
+++ ui/features/files_v2/react/components/FilesHeader/__tests__/CurrentUploads.test.tsx
@@ -17,7 +17,9 @@
  */
 
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitFor, act} from '@testing-library/react'
+import {queryClient} from '@canvas/query'
+import {MockedQueryClientProvider} from '@canvas/test-utils/query'
 import CurrentUploads from '../CurrentUploads'
 import FileUploader from '@canvas/files/react/modules/FileUploader'
 import UploadQueue from '@canvas/files/react/modules/UploadQueue'
@@ -56,27 +58,214 @@ beforeEach(() => {
   mockGetAllUploaders.mockReturnValue([makeUploader('foo.txt')])
 })
 
+const error = {response: {status: 409}}
+const nonDuplicateError = {response: {status: 500}}
+
+const renderComponent = () => {
+  return render(
+    <MockedQueryClientProvider client={queryClient}>
+      <CurrentUploads />
+    </MockedQueryClientProvider>,
+  )
+}
+
 describe('CurrentUploads', () => {
   it('renders', () => {
-    render(<CurrentUploads />)
+    renderComponent()
     expect(screen.getByTestId('current-uploads')).toBeInTheDocument()
   })
 
   it("doesn't render", () => {
     // Return empty array for this test
     mockGetAllUploaders.mockReturnValue([])
-    render(<CurrentUploads />)
+    renderComponent()
     expect(screen.queryByTestId('current-uploads')).not.toBeInTheDocument()
   })
 
   it('catches file conflicts and shows rename form', () => {
-    const error = {response: {status: 409}}
     // Return uploader with error for this test
     mockGetAllUploaders.mockReturnValue([makeUploader('foo.txt', error)])
-    render(<CurrentUploads />)
+    renderComponent()
     expect(screen.getByText('File failed to upload. Please try again.')).toBeInTheDocument()
     expect(screen.getByTestId('rename-replace-button')).toBeInTheDocument()
     expect(screen.getByTestId('rename-skip-button')).toBeInTheDocument()
     expect(screen.getByTestId('rename-change-button')).toBeInTheDocument()
+  })
+
+  // The mockGetAllUploaders implementations aren't 100% accurate but are close enough
+  // The beginning and end of the process is the most important, so some intermediate updates are left out
+  describe('quota and files queries', () => {
+    let changeCallback: () => void
+
+    // simulates UploadQueue.onChange
+    // which triggers handleUploadQueueChange
+    const executeCallbackTimes = (times: number) => {
+      for (let i = 0; i < times; i++) {
+        act(() => changeCallback())
+      }
+    }
+
+    beforeEach(() => {
+      changeCallback = () => {}
+      jest.spyOn(queryClient, 'refetchQueries')
+
+      mockAddChangeListener.mockImplementation((callback: () => void) => {
+        changeCallback = callback
+      })
+    })
+
+    it('are refetched once when sole upload completes', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt')])
+        // progress 0 -> 1
+        .mockImplementationOnce(() => [makeUploader('foo.txt')])
+        .mockImplementation(() => [])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledWith({
+          queryKey: ['quota'],
+          type: 'active',
+        })
+        expect(queryClient.refetchQueries).toHaveBeenCalledWith({
+          queryKey: ['files'],
+          type: 'active',
+        })
+      })
+    })
+
+    it('are refeched once when multiple uploads complete', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [makeUploader('bar.txt')])
+        .mockImplementation(() => [])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when sole upload fails', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt')])
+        // progress 0 -> 1
+        .mockImplementationOnce(() => [makeUploader('foo.txt')])
+        .mockImplementation(() => [makeUploader('foo.txt', nonDuplicateError)])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when all uploads fail', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [
+          makeUploader('foo.txt', nonDuplicateError),
+          makeUploader('bar.txt'),
+        ])
+        .mockImplementation(() => [
+          makeUploader('foo.txt', nonDuplicateError),
+          makeUploader('bar.txt', nonDuplicateError),
+        ])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are not refetched again when failed upload is removed', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt')])
+        .mockImplementationOnce(() => [makeUploader('foo.txt', nonDuplicateError)])
+        .mockImplementation(() => [])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when last upload errors', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [makeUploader('bar.txt', nonDuplicateError)])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when first upload errors', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [
+          makeUploader('foo.txt', nonDuplicateError),
+          makeUploader('bar.txt'),
+        ])
+        .mockImplementationOnce(() => [makeUploader('foo.txt', nonDuplicateError)])
+
+      renderComponent()
+      executeCallbackTimes(3)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when resolving name conflicts', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [makeUploader('foo.txt', error)])
+        .mockImplementationOnce(() => [
+          makeUploader('foo.txt', error),
+          makeUploader('bar.txt', error),
+        ])
+        .mockImplementationOnce(() => [makeUploader('bar.txt', error)])
+        .mockImplementation(() => [])
+
+      renderComponent()
+      executeCallbackTimes(5)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
+
+    it('are refetched once when dealing with mixed errors', async () => {
+      mockGetAllUploaders
+        .mockImplementationOnce(() => [makeUploader('foo.txt'), makeUploader('bar.txt')])
+        .mockImplementationOnce(() => [makeUploader('foo.txt', error)])
+        .mockImplementationOnce(() => [
+          makeUploader('foo.txt', error),
+          makeUploader('bar.txt', nonDuplicateError),
+        ])
+        .mockImplementationOnce(() => [makeUploader('bar.txt', error)])
+        .mockImplementation(() => [])
+
+      renderComponent()
+      executeCallbackTimes(5)
+
+      await waitFor(() => {
+        expect(queryClient.refetchQueries).toHaveBeenCalledTimes(2)
+      })
+    })
   })
 })
