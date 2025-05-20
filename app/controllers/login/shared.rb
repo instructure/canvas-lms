4e45253eb112b23d0093e186a5c408cbb9173443
Cody Cutrer <cody@instructure.com>
2025-05-20 16:53:15 -0600

    implement OIDC 3rd party initiated login
    closes AE-2097
    
    it basically already worked, but this brings it completely into spec:
     * use `iss` param if provided to find the correct auth provider config
     * accept `target_link_uri` param to set after-login location
     * accept POST
    
    Change-Id: I9840d5d93d9dcff9ba08a2191f9eaaececadf5d7
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/378927
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    QA-Review: Weston Dransfield <wdransfield@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- app/controllers/login/shared.rb
+++ app/controllers/login/shared.rb
@@ -38,7 +38,7 @@ module Login::Shared
             session[:return_to] = url
           elsif (target_account == domain_root_account) ||
                 (target_account && target_account != domain_root_account &&
-                pseudonym.works_for_account?(target_account, true))
+                pseudonym&.works_for_account?(target_account, true))
             token = SessionToken.new(pseudonym.global_id,
                                      current_user_id: pseudonym.global_user_id).to_s
             uri.query&.concat("&")
