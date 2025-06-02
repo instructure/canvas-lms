3b5eb382acedbcbe0db15c145966a8170e0fd172
akemeny <akemeny@instructure.com>
2025-06-02 14:38:01 +0200

    Update field_name definition to varchar
    flag = none
    
    Test plan:
    1. Run the migration.
    2. Turn on disable_file_verifiers_in_public_syllabus feature flag
       (only needed for the QA).
    3. Have a course with some course images. Modify the syllabus by
       adding in a few images. Save it.
    4. Check in the Rails console to see if you have the proper
       AttachmentAssociation records created.
    
    refs GROW-76
    
    Change-Id: If46491447a1e7c14a47f82a162a154fba5feddc2
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/379847
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    QA-Review: Mysti Lilla <mysti@instructure.com>
    Product-Review: Andras Kemeny <akemeny@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

--- spec/models/attachment_association_spec.rb
+++ spec/models/attachment_association_spec.rb
@@ -50,17 +50,17 @@ describe AttachmentAssociation do
   end
 
   describe "#update_associations" do
-    def fetch_list_with_field_name(field_name)
-      AttachmentAssociation.where(context: course, field_name:).pluck(:attachment_id)
+    def fetch_list_with_field_name(context_concern)
+      AttachmentAssociation.where(context: course, context_concern:).pluck(:attachment_id)
     end
 
-    def make_association_update(attachment_ids, field_name, user = teacher)
+    def make_association_update(attachment_ids, context_concern, user = teacher)
       AttachmentAssociation.update_associations(
         course,
         attachment_ids,
         user,
         nil,
-        field_name
+        context_concern
       )
     end
 
@@ -110,7 +110,7 @@ describe AttachmentAssociation do
           AttachmentAssociation.update_associations(course, [@attachment.id], teacher, nil, "syllabus_body")
         end
 
-        aa = AttachmentAssociation.find_by(context: course, field_name: "syllabus_body")
+        aa = AttachmentAssociation.find_by(context: course, context_concern: "syllabus_body")
         expect(aa.attachment_id).to eql @attachment.global_id
         expect(aa.context_id).to eql course.local_id
       end
@@ -120,6 +120,7 @@ describe AttachmentAssociation do
   describe "#verify_access" do
     before do
       course.root_account.enable_feature!(:disable_file_verifiers_in_public_syllabus)
+      course.root_account.enable_feature!(:file_association_access)
     end
 
     def make_associations
