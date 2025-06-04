8609d295e9d618ce5ab14ec574f0d20e78daa453
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-06-03 22:01:43 -0600

    replace enzyme with testing-library in GradesDisplay.spec.jsx
    Change-Id: Id496f450961762a376c0554591f51b3c1a8108df
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/380188
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Daniel Kis-Pal <daniel.kispal@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/shared/planner/components/GradesDisplay/__tests__/GradesDisplay.spec.jsx
+++ ui/shared/planner/components/GradesDisplay/__tests__/GradesDisplay.spec.jsx
@@ -17,13 +17,8 @@
  */
 
 import React from 'react'
-import {shallow} from 'enzyme'
-import {Heading} from '@instructure/ui-heading'
-import {Text} from '@instructure/ui-text'
-import {Spinner} from '@instructure/ui-spinner'
-import {Link} from '@instructure/ui-link'
+import {render} from '@testing-library/react'
 import GradesDisplay from '../index'
-import ErrorAlert from '../../ErrorAlert'
 
 describe('GradesDisplay', () => {
   const mockCourses = [
@@ -57,41 +52,36 @@ describe('GradesDisplay', () => {
   ]
 
   it('renders course grades with proper heading and structure', () => {
-    const wrapper = shallow(<GradesDisplay courses={mockCourses} />)
+    const {getByText, getByRole} = render(<GradesDisplay courses={mockCourses} />)
 
-    // Check for "My Grades" heading component
-    const heading = wrapper.find(Heading)
-    expect(heading).toHaveLength(1)
+    expect(getByText('My Grades')).toBeInTheDocument()
 
-    // Check that all courses are rendered as links
-    const links = wrapper.find(Link)
-    expect(links).toHaveLength(3)
-
-    // Check course links have correct hrefs
-    expect(links.at(0).prop('href')).toBe('/courses/1/grades')
-    expect(links.at(1).prop('href')).toBe('/courses/2/grades')
-    expect(links.at(2).prop('href')).toBe('/courses/3/grades')
+    expect(getByRole('link', {name: 'Ticket to Ride 101'})).toHaveAttribute(
+      'href',
+      '/courses/1/grades',
+    )
+    expect(getByRole('link', {name: 'Ingenious 101'})).toHaveAttribute('href', '/courses/2/grades')
+    expect(getByRole('link', {name: 'Settlers of Catan 201'})).toHaveAttribute(
+      'href',
+      '/courses/3/grades',
+    )
   })
 
   it('displays proper grade scores and handles invalid scores', () => {
-    const wrapper = shallow(<GradesDisplay courses={mockCourses} />)
+    const {getAllByTestId} = render(<GradesDisplay courses={mockCourses} />)
 
-    const scoreTexts = wrapper.find('[data-testid="my-grades-score"]')
+    const scoreTexts = getAllByTestId('my-grades-score')
     expect(scoreTexts).toHaveLength(3)
 
-    // Check props instead of rendered text since shallow doesn't render children
-    expect(scoreTexts.at(0).prop('children')).toBe('No Grade')
-    expect(scoreTexts.at(1).prop('children')).toBe('42.34%')
-    expect(scoreTexts.at(2).prop('children')).toBe('No Grade')
+    expect(scoreTexts[0]).toHaveTextContent('No Grade')
+    expect(scoreTexts[1]).toHaveTextContent('42.34%')
+    expect(scoreTexts[2]).toHaveTextContent('No Grade')
   })
 
   it('renders grading period caveat when courses have grading periods', () => {
-    const wrapper = shallow(<GradesDisplay courses={mockCourses} />)
+    const {getByText} = render(<GradesDisplay courses={mockCourses} />)
 
-    // Look for caveat text by checking Text components with italic style
-    const italicTexts = wrapper.find(Text).filterWhere(text => text.prop('fontStyle') === 'italic')
-    expect(italicTexts).toHaveLength(1)
-    expect(italicTexts.at(0).prop('children')).toBe('*Only most recent grading period shown.')
+    expect(getByText('*Only most recent grading period shown.')).toBeInTheDocument()
   })
 
   it('does not render caveat if no courses have grading periods', () => {
@@ -107,60 +97,42 @@ describe('GradesDisplay', () => {
         enrollmentType: 'StudentEnrollment',
       },
     ]
-    const wrapper = shallow(<GradesDisplay courses={coursesWithoutGradingPeriods} />)
+    const {queryByText} = render(<GradesDisplay courses={coursesWithoutGradingPeriods} />)
 
-    const caveatText = wrapper.find(Text).filterWhere(text => text.prop('fontStyle') === 'italic')
-    expect(caveatText).toHaveLength(0)
+    expect(queryByText('*Only most recent grading period shown.')).not.toBeInTheDocument()
   })
 
   it('renders a loading spinner when loading', () => {
-    const wrapper = shallow(<GradesDisplay loading={true} courses={mockCourses} />)
+    const {getByText, queryByText, queryByRole} = render(
+      <GradesDisplay loading={true} courses={mockCourses} />,
+    )
 
-    const spinner = wrapper.find(Spinner)
-    expect(spinner).toHaveLength(1)
-    expect(spinner.prop('size')).toBe('small')
+    expect(getByText('Grades are loading')).toBeInTheDocument()
 
-    // Should not render course grades when loading
-    const links = wrapper.find(Link)
-    expect(links).toHaveLength(0)
-
-    // Should not render caveat when loading
-    const caveatText = wrapper.find(Text).filterWhere(text => text.prop('fontStyle') === 'italic')
-    expect(caveatText).toHaveLength(0)
+    expect(queryByRole('link')).not.toBeInTheDocument()
+    expect(queryByText('*Only most recent grading period shown.')).not.toBeInTheDocument()
   })
 
   it('renders an ErrorAlert if there is an error loading grades', () => {
     const mockCoursesSimple = [
       {id: '1', shortName: 'Ticket to Ride 101', color: 'blue', href: '/courses/1'},
     ]
-    const wrapper = shallow(
+    const {getByText, queryByTestId} = render(
       <GradesDisplay courses={mockCoursesSimple} loadingError="There was an error" />,
     )
 
-    const errorAlert = wrapper.find(ErrorAlert)
-    expect(errorAlert).toHaveLength(1)
-    expect(errorAlert.prop('error')).toBe('There was an error')
-    expect(errorAlert.prop('children')).toBe('Error loading grades')
-
-    // Should still render heading when there's an error
-    const heading = wrapper.find(Heading)
-    expect(heading).toHaveLength(1)
-
-    // Should not render grades when there's an error
-    const scoreTexts = wrapper.find('[data-testid="my-grades-score"]')
-    expect(scoreTexts).toHaveLength(0)
+    expect(getByText('Error loading grades')).toBeInTheDocument()
+    expect(getByText('My Grades')).toBeInTheDocument()
+    expect(queryByTestId('my-grades-score')).not.toBeInTheDocument()
   })
 
   it('applies course color to border styling', () => {
-    const wrapper = shallow(<GradesDisplay courses={mockCourses} />)
+    const {container} = render(<GradesDisplay courses={mockCourses} />)
 
-    // Check that course styling divs have the proper border colors
-    const styledDivs = wrapper
-      .find('div')
-      .filterWhere(div => div.prop('style') && div.prop('style').borderBottomColor)
+    const styledDivs = container.querySelectorAll('div[style*="border-bottom-color"]')
 
-    expect(styledDivs.at(0).prop('style').borderBottomColor).toBe('blue')
-    expect(styledDivs.at(1).prop('style').borderBottomColor).toBe('green')
-    expect(styledDivs.at(2).prop('style').borderBottomColor).toBe('red')
+    expect(styledDivs[0]).toHaveStyle('border-bottom-color: rgb(0, 0, 255)')
+    expect(styledDivs[1]).toHaveStyle('border-bottom-color: rgb(0, 128, 0)')
+    expect(styledDivs[2]).toHaveStyle('border-bottom-color: rgb(255, 0, 0)')
   })
 })
