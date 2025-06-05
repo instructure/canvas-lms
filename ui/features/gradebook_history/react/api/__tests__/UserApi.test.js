1de773f8eacbe78c3b66ff89e1611f5c76e2d767
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-06-04 23:06:51 -0600

    replace sinon with msw in UserApi test
    Change-Id: Iaba42c8a87bf54fe65bc0ec01c5aca321b6ef78d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/380502
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/gradebook_history/react/api/__tests__/UserApi.test.js
+++ ui/features/gradebook_history/react/api/__tests__/UserApi.test.js
@@ -16,135 +16,133 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import axios from '@canvas/axios'
+import {setupServer} from 'msw/node'
+import {http, HttpResponse} from 'msw'
 import UserApi from '../UserApi'
-import sinon from 'sinon'
 
-const sandbox = sinon.createSandbox()
-const deepEqual = (x, y) => expect(x).toEqual(y)
-const strictEqual = (x, y) => expect(x).toEqual(y)
+const server = setupServer()
 
 let courseId
-let getStub
+let requestedUrls = []
+let requestedParams = []
 
 describe('UserApi', () => {
+  beforeAll(() => server.listen())
+  afterEach(() => {
+    server.resetHandlers()
+    requestedUrls = []
+    requestedParams = []
+  })
+  afterAll(() => server.close())
+
   beforeEach(() => {
     courseId = 525600
-    getStub = sandbox.stub(axios, 'get').returns(
-      Promise.resolve({
-        response: {},
+    server.use(
+      http.get('*', ({request}) => {
+        const url = new URL(request.url)
+        requestedUrls.push(url.pathname)
+        const params = {}
+        url.searchParams.forEach((value, key) => {
+          if (key.endsWith('[]')) {
+            const cleanKey = key.slice(0, -2)
+            if (!params[cleanKey]) params[cleanKey] = []
+            params[cleanKey].push(value)
+          } else {
+            params[key] = value
+          }
+        })
+        // Handle enrollment_state if it's not in the URL but should be empty array
+        if (!('enrollment_state' in params) && url.searchParams.has('enrollment_type[]')) {
+          params.enrollment_state = []
+        }
+        requestedParams.push(params)
+        return HttpResponse.json({response: {}})
       }),
     )
   })
 
-  afterEach(() => {
-    // restore the environment as it was before
-    sandbox.restore()
-  })
-
-  test('getUsersByName for graders searches by teachers and TAs in a course', function () {
+  test('getUsersByName for graders searches by teachers and TAs in a course', async function () {
     const searchTerm = 'Norval'
     const url = `/api/v1/courses/${courseId}/users`
-    const params = {
-      params: {
-        search_term: searchTerm,
-        enrollment_type: ['teacher', 'ta'],
-        enrollment_state: [],
-      },
-    }
-    const promise = UserApi.getUsersByName(courseId, 'graders', searchTerm)
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-      deepEqual(getStub.firstCall.args[1], params)
+    await UserApi.getUsersByName(courseId, 'graders', searchTerm)
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe(url)
+    expect(requestedParams[0]).toEqual({
+      search_term: searchTerm,
+      enrollment_type: ['teacher', 'ta'],
+      enrollment_state: [],
     })
   })
 
-  test('getUsersByName for students searches by students', function () {
+  test('getUsersByName for students searches by students', async function () {
     const searchTerm = 'Norval'
     const url = `/api/v1/courses/${courseId}/users`
-    const params = {
-      params: {
-        search_term: searchTerm,
-        enrollment_type: ['student', 'student_view'],
-        enrollment_state: [],
-      },
-    }
-    const promise = UserApi.getUsersByName(courseId, 'students', searchTerm)
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-      deepEqual(getStub.firstCall.args[1], params)
+    await UserApi.getUsersByName(courseId, 'students', searchTerm)
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe(url)
+    expect(requestedParams[0]).toEqual({
+      search_term: searchTerm,
+      enrollment_type: ['student', 'student_view'],
+      enrollment_state: [],
     })
   })
 
-  test('getUsersByName restricts results by enrollment state if specified', function () {
+  test('getUsersByName restricts results by enrollment state if specified', async function () {
     const searchTerm = 'Norval'
     const enrollmentState = ['completed']
     const url = `/api/v1/courses/${courseId}/users`
-    const params = {
-      params: {
-        search_term: searchTerm,
-        enrollment_type: ['student', 'student_view'],
-        enrollment_state: enrollmentState,
-      },
-    }
-    const promise = UserApi.getUsersByName(courseId, 'students', searchTerm, ['completed'])
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-      deepEqual(getStub.firstCall.args[1], params)
+    await UserApi.getUsersByName(courseId, 'students', searchTerm, ['completed'])
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe(url)
+    expect(requestedParams[0]).toEqual({
+      search_term: searchTerm,
+      enrollment_type: ['student', 'student_view'],
+      enrollment_state: enrollmentState,
     })
   })
 
-  test('getUsersByName does not restrict results by enrollment state if argument omitted', function () {
+  test('getUsersByName does not restrict results by enrollment state if argument omitted', async function () {
     const searchTerm = 'Norval'
     const url = `/api/v1/courses/${courseId}/users`
-    const params = {
-      params: {
-        search_term: searchTerm,
-        enrollment_type: ['student', 'student_view'],
-        enrollment_state: [],
-      },
-    }
-    const promise = UserApi.getUsersByName(courseId, 'students', searchTerm)
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-      deepEqual(getStub.firstCall.args[1], params)
+    await UserApi.getUsersByName(courseId, 'students', searchTerm)
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe(url)
+    expect(requestedParams[0]).toEqual({
+      search_term: searchTerm,
+      enrollment_type: ['student', 'student_view'],
+      enrollment_state: [],
     })
   })
 
-  test('getUsersByName does not restrict results by enrollment state if passed an empty array', function () {
+  test('getUsersByName does not restrict results by enrollment state if passed an empty array', async function () {
     const searchTerm = 'Norval'
     const url = `/api/v1/courses/${courseId}/users`
-    const params = {
-      params: {
-        search_term: searchTerm,
-        enrollment_type: ['student', 'student_view'],
-        enrollment_state: [],
-      },
-    }
-    const promise = UserApi.getUsersByName(courseId, 'students', searchTerm, [])
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-      deepEqual(getStub.firstCall.args[1], params)
+    await UserApi.getUsersByName(courseId, 'students', searchTerm, [])
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe(url)
+    expect(requestedParams[0]).toEqual({
+      search_term: searchTerm,
+      enrollment_type: ['student', 'student_view'],
+      enrollment_state: [],
     })
   })
 
-  test('getUsersNextPage makes a request with given url', function () {
+  test('getUsersNextPage makes a request with given url', async function () {
     const url = 'https://example.com/users?page=2'
-    const promise = UserApi.getUsersNextPage(url)
 
-    return promise.then(() => {
-      strictEqual(getStub.callCount, 1)
-      strictEqual(getStub.firstCall.args[0], url)
-    })
+    await UserApi.getUsersNextPage(url)
+
+    expect(requestedUrls).toHaveLength(1)
+    expect(requestedUrls[0]).toBe('/users')
   })
 })
