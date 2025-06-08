8e087b5e0f32420d2d664bebd448310bc82d284a
Aaron Shafovaloff <ashafovaloff@instructure.com>
2025-06-07 20:06:33 -0600

    Convert defaultProps to TypeScript defaults in 3 components
    Change-Id: Ibb60d312c94b010bde05af7bfb1d78a94241cbf9
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/380873
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

--- ui/features/assignments_show_student/react/components/MarkAsDoneButton.tsx
+++ ui/features/assignments_show_student/react/components/MarkAsDoneButton.tsx
@@ -18,7 +18,6 @@
 
 import {useScope as createI18nScope} from '@canvas/i18n'
 import {Button} from '@instructure/ui-buttons'
-import {bool, func, string} from 'prop-types'
 import {IconCompleteLine, IconEmptyLine} from '@instructure/ui-icons'
 import React, {useState} from 'react'
 import {SET_MODULE_ITEM_COMPLETION} from '@canvas/assignments/graphql/student/Mutations'
@@ -26,7 +25,21 @@ import {useMutation} from '@apollo/client'
 
 const I18n = createI18nScope('assignments_2_file_upload')
 
-function MarkAsDoneButton({done: initialDone, itemId, moduleId, onError, onToggle}) {
+interface MarkAsDoneButtonProps {
+  done: boolean
+  itemId: string
+  moduleId: string
+  onError?: () => void
+  onToggle?: () => void
+}
+
+function MarkAsDoneButton({
+  done: initialDone,
+  itemId,
+  moduleId,
+  onError = () => {},
+  onToggle = () => {},
+}: MarkAsDoneButtonProps) {
   const [done, setDone] = useState(initialDone)
   const [setItemCompletion] = useMutation(SET_MODULE_ITEM_COMPLETION, {
     onCompleted: () => {
@@ -48,25 +61,12 @@ function MarkAsDoneButton({done: initialDone, itemId, moduleId, onError, onToggl
       color={done ? 'success' : 'secondary'}
       data-testid="set-module-item-completion-button"
       id="set-module-item-completion-button"
-      onClick={setItemCompletion}
-      renderIcon={done ? IconCompleteLine : IconEmptyLine}
+      onClick={() => setItemCompletion()}
+      renderIcon={done ? <IconCompleteLine /> : <IconEmptyLine />}
     >
       {done ? I18n.t('Done') : I18n.t('Mark as done')}
     </Button>
   )
-}
-
-MarkAsDoneButton.propTypes = {
-  done: bool.isRequired,
-  itemId: string.isRequired,
-  moduleId: string.isRequired,
-  onError: func,
-  onToggle: func,
-}
-
-MarkAsDoneButton.defaultProps = {
-  onError: () => {},
-  onToggle: () => {},
 }
 
 export default MarkAsDoneButton
