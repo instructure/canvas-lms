ce4b1da218dcd66f57194e86ff36839f9c103571
Mysti Lilla <mysti@instructure.com>
2025-06-09 18:21:50 -0600

    Disable adding the file verifier into asset_map
    refs GROW-7
    flag=disable_adding_uuid_verifier_in_api
    
    Test plan
    - Specs pass
    
    Change-Id: Ib7c8ea366af2e73a51bc43ba1412944af310a0cd
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/380980
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Product-Review: Keith Garner <kgarner@instructure.com>
    QA-Review: Mysti Lilla <mysti@instructure.com>

--- app/models/content_migration.rb
+++ app/models/content_migration.rb
@@ -1376,15 +1376,15 @@ class ContentMigration < ActiveRecord::Base
 
   def asset_map_url(generate_if_needed: false)
     generate_asset_map if !asset_map_attachment && generate_if_needed
-    asset_map_attachment && file_download_url(
-      asset_map_attachment,
-      {
-        verifier: asset_map_attachment.uuid,
-        download: "1",
-        download_frd: "1",
-        host: context.root_account.domain(ApplicationController.test_cluster_name)
-      }
-    )
+    return nil unless asset_map_attachment
+
+    options = {
+      download: "1",
+      download_frd: "1",
+      host: context.root_account.domain(ApplicationController.test_cluster_name)
+    }
+    options[:verifier] = asset_map_attachment.uuid unless context.root_account.feature_enabled?(:disable_adding_uuid_verifier_in_api)
+    file_download_url(asset_map_attachment, options)
   end
 
   def asset_map_v2?
