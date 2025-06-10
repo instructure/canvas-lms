a159df67ae466f406616cdecc24a5d687b5048fb
Charley Kline <ckline@instructure.com>
2025-06-10 16:40:20 -0500

    Ensure proper loading time of backbone code
    Fixes FOO-5509
    Reverts g/380868
    flag=none
    
    The previous fix for this bug did not address an issue
    with how React Router does its "lazy loading" of components.
    Empirically (via generous console.log messages), the
    createBrowserRouter() function which creates all the lazy
    routes does in fact execute all of the import functions in
    the `lazy=` props IMMEDIATELY upon execution. Apparently
    the "lazy" part is that it does not actually render the
    React components themselves until the route is executed.
    
    This finding would seem to create some additional research
    work to see if loading the React Router could be optimized
    a bit more, or if we are just not making the best use of it
    for the way we are invoking it. I don't think we want to
    load every single bundle's index.tsx file every time!
    
    In any event, this fixes the immediate problem by not
    executing the Backbone.js initialization code until frd
    after the corresponding React component has run (which
    renders null in this case anyway). That ensures that all
    the translations are available when the Backbone and
    Handlebars code does execute.
    
    Also caught that the "Edit Assignment" page title was not
    being translated, ever, because of the way the original
    Backbone and Handlebars code was trying to call I18n.t on
    a previously-computed variable (which will never work).
    
    Test plan:
    * Switch to a non-English locale
    * Edit an assignment (/courses/:id/assignments/:id/edit)
    * The ENTIRE page should be in the locale language
    
    Change-Id: Ida9c5fb21f8a07e0ddb72d9a3f09fdc4a98bf633
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381053
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Adrián Grúber <adrian.gruber@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

--- ui/features/assignment_edit/index.js
+++ ui/features/assignment_edit/index.js
@@ -17,7 +17,7 @@
  */
 
 // manage groups is for the add_group_category dialog
-import ready from '@instructure/ready'
+import {useEffect} from 'react'
 import Assignment from '@canvas/assignments/backbone/models/Assignment'
 import EditHeaderView from './backbone/views/EditHeaderView'
 import EditView from './backbone/views/EditView'
@@ -34,16 +34,17 @@ import LockManager from '@canvas/blueprint-courses/react/components/LockManager/
 import renderEditAssignmentsApp from './react/index'
 import {renderEnhancedRubrics} from './react/AssignmentRubric'
 
-ready(() => {
-  window.addEventListener('load', () => {
+function loadBackboneComponents() {
+  function maybeScrollToTarget() {
     const params = new URLSearchParams(window.location.search)
     const targetId = params.get('scrollTo')
     const target = document.getElementById(targetId)
 
-    if (target) {
-      target.scrollIntoView({behavior: 'smooth'})
-    }
-  })
+    if (target) target.scrollIntoView({behavior: 'smooth'})
+  }
+
+  if (document.readyState === 'complete') maybeScrollToTarget()
+  else window.addEventListener('load', maybeScrollToTarget, {once: true})
 
   if (ENV.ASSIGNMENT_EDIT_ENHANCEMENTS_TEACHER_VIEW) {
     const div = document.createElement('div')
@@ -53,9 +54,10 @@ ready(() => {
     lockManager.init({itemType: 'assignment', page: 'edit'})
     const lockedItems = lockManager.isChildContent() ? lockManager.getItemLocks() : {}
 
-    ENV.ASSIGNMENT.assignment_overrides = ENV.ASSIGNMENT_OVERRIDES
+    if (ENV.ASSIGNMENT) ENV.ASSIGNMENT.assignment_overrides = ENV.ASSIGNMENT_OVERRIDES
 
     const userIsAdmin = ENV.current_user_is_admin
+    const canEditGrades = ENV.PERMISSIONS?.can_edit_grades ?? false
 
     const assignment = new Assignment(ENV.ASSIGNMENT)
     assignment.urlRoot = ENV.URL_ROOT
@@ -76,7 +78,7 @@ ready(() => {
       parentModel: assignment,
       preventNotGraded: assignment.submissionTypesFrozen(),
       lockedItems,
-      canEditGrades: ENV.PERMISSIONS.can_edit_grades,
+      canEditGrades,
     })
     const groupCategorySelector = new GroupCategorySelector({
       parentModel: assignment,
@@ -113,7 +115,7 @@ ready(() => {
         }),
       },
       lockedItems: assignment.id ? lockedItems : {}, // if no id, creating a new assignment
-      canEditGrades: ENV.PERMISSIONS.can_edit_grades || !assignment.gradedSubmissionsExist(),
+      canEditGrades: canEditGrades || !assignment.gradedSubmissionsExist(),
     })
 
     const editHeaderView = new EditHeaderView({
@@ -127,8 +129,14 @@ ready(() => {
     editHeaderView.render()
     renderEnhancedRubrics()
   }
-})
+}
 
 export function Component() {
+  useEffect(() => {
+    // Need to make sure the DOM has settled down before loading the Backbone
+    // stuff, because it in turn wants to render stuff into the DOM and we need
+    // to make sure everything is in place before that happens.
+    requestAnimationFrame(loadBackboneComponents)
+  }, [])
   return null
 }
