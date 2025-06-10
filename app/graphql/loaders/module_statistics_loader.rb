33c03fcbef32d05b56db2695ed567c5a90b8e063
viktor.szpisjak <viktor.szpisjak@instructure.com>
2025-06-10 15:21:05 +0200

    Add support for every assignment type for module stat calculation
    flag=modules_page_rewrite_student_view
    refs LX-2710, LX-2776
    
    Test Plan:
    - Turn on FF
    - Create a new course with multiple module
    - To the modules, create assignments, classic quiz
      new quiz, graded discussions, checkpoint discussions
    - Set some due at dates, some should be in late
      to trigger missing
    - Validate if the statistics calculates with every
      listed module item type
    - Also validate from the logs, that there is max
      SQL query executed to get the data
    
    Change-Id: I42275e26cd880ee000421cc5d9b3085d418fe7ac
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381004
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: Viktor Szpisják <viktor.szpisjak@instructure.com>

--- app/graphql/loaders/module_statistics_loader.rb
+++ app/graphql/loaders/module_statistics_loader.rb
@@ -24,6 +24,10 @@
 #
 module Loaders
   class ModuleStatisticsLoader < GraphQL::Batch::Loader
+    ASSIGNMENT = "Assignment"
+    DISCUSSION_TOPIC = "DiscussionTopic"
+    CLASSIC_QUIZ = "Quizzes::Quiz"
+
     def initialize(current_user:)
       super()
       @current_user = current_user
@@ -32,76 +36,71 @@ module Loaders
     def perform(context_modules)
       return if context_modules.empty? || @current_user.nil?
 
-      # Get all assignment ids for each module
-      module_ids = context_modules.map(&:id)
-      assignment_ids_by_module = {}
+      submissions_by_modules = query_submissions_by_modules(context_modules)
 
-      # Get all assignment_id => module_id mapping
-      ContentTag.assignments_for_modules(module_ids)
-                .where.not(content_id: nil)
-                .pluck(:context_module_id, :content_id)
-                .each do |module_id, content_id|
-                  assignment_ids_by_module[module_id] ||= []
-                  assignment_ids_by_module[module_id] << content_id
-                end
-
-      # Calculate overdue assignments
-      overdue_counts = calculate_overdue(assignment_ids_by_module)
-
-      # Get the latest due date for each module
-      latest_due_dates = calculate_latest_due_at(assignment_ids_by_module)
-
-      # Fulfill the loader with statistics for each module
-      context_modules.each do |mod|
-        module_id = mod.id
-
-        fulfill(mod, {
-                  missing_assignment_count: overdue_counts[module_id] || 0,
-                  latest_due_at: latest_due_dates[module_id]
-                })
+      context_modules.each do |context_module|
+        fulfill(context_module, submissions_by_modules[context_module.id] || [])
       end
     end
 
     private
 
-    def calculate_overdue(assignment_ids_by_module)
-      result = {}
+    def query_submissions_by_modules(context_modules)
+      module_ids = context_modules.map(&:id)
 
-      assignment_ids_by_module.each do |module_id, assignment_ids|
-        next if assignment_ids.empty?
-
-        # Find submissions that are missing/overdue using the scope from the Submission model
-        count = @current_user.submissions
-                             .except(:order)
-                             .where(assignment_id: assignment_ids)
-                             .merge(Assignment.published)
-                             .missing
-                             .distinct
-                             .count
-
-        result[module_id] = count if count > 0
+      content_type_groups = ContentTag.not_deleted
+                                      .where(context_module_id: module_ids)
+                                      .where.not(content_id: nil)
+                                      .pluck(:content_type, :content_id, :context_module_id)
+                                      .group_by { |arr| arr[0] }
+                                      .transform_values do |items|
+        items.group_by { |i| i[1] }.transform_values { |group| group.to_set { |i| i[2] } }
       end
 
-      result
-    end
+      # {"Assignment"=>{content_id_1=>#<Set: {context_module_id_1, context_module_id_2}>, "Quizzes::Quiz"=>{content_id_1=>#<Set: {context_module_id_1}>}
 
-    def calculate_latest_due_at(assignment_ids_by_module)
-      result = {}
-
-      assignment_ids_by_module.each do |module_id, assignment_ids|
-        next if assignment_ids.empty?
-
-        # Get the latest due date from all submissions for assignments in this module
-        latest_date = @current_user.submissions
-                                   .except(:order)
-                                   .where(assignment_id: assignment_ids)
-                                   .where.not(cached_due_date: nil)
-                                   .maximum(:cached_due_date)
-
-        result[module_id] = latest_date if latest_date
+      assignment_ids_by_modules = content_type_groups.each_with_object({}) do |(content_type, content_and_modules_pair), result|
+        case content_type
+        when ASSIGNMENT # New Quizzes are included here
+          result.merge!(content_and_modules_pair)
+        when DISCUSSION_TOPIC
+          DiscussionTopic.find(content_and_modules_pair.keys).each do |discussion|
+            result[discussion.assignment_id] = content_and_modules_pair[discussion.id] if discussion.assignment_id
+          end
+        when CLASSIC_QUIZ
+          Quizzes::Quiz.find(content_and_modules_pair.keys).each do |quiz|
+            result[quiz.assignment_id] = content_and_modules_pair[quiz.id] if quiz.assignment_id
+          end
+        end
       end
 
-      result
+      # Transform every content_id to assignment_id
+      # {assignment_id_1=>context_module_id_1, assignment_id_2=>context_module_id_1, assignment_id_3=>context_module_id_2}
+
+      # Remove parent assignments from discussion checkpoints
+      assignments_ids = Assignment.where(id: assignment_ids_by_modules.keys, has_sub_assignments: false).pluck(:id)
+      # Load sub assignments from discussion checkpoints
+      sub_assignments = SubAssignment.where(parent_assignment_id: assignment_ids_by_modules.keys)
+      merged_assignment_ids = assignments_ids + sub_assignments.map(&:id)
+
+      # Generate mapping for sub_assignments because they are directly not attached to module items
+      sub_assignment_ids_by_modules = sub_assignments.to_h do |sub_assignment|
+        module_id = assignment_ids_by_modules[sub_assignment.parent_assignment_id]
+        [sub_assignment.id, module_id]
+      end
+
+      merged_assignment_ids_by_modules = assignment_ids_by_modules.merge(sub_assignment_ids_by_modules)
+
+      @current_user
+        .submissions
+        .where(assignment_id: merged_assignment_ids)
+        .joins(:assignment)
+        .merge(AbstractAssignment.published)
+        .each_with_object(Hash.new { |h, k| h[k] = [] }) do |submission, grouped|
+          merged_assignment_ids_by_modules[submission.assignment_id].each do |module_id|
+            grouped[module_id] << submission
+          end
+        end
     end
   end
 end
