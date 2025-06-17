1b2074a56ae418cb95a95b86251700b5bd6ad643
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-17 10:23:09 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 10
    [fsc-timeout=60]
    [fsc-node-count=10]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: I215939127f5c539f4c0b6aca766914cbcaadd765
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381537
    Reviewed-by: László Sipula <laszlo.sipula@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_grade_edit_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_grade_edit_spec.rb
@@ -24,12 +24,21 @@ require_relative "../pages/gradebook_page"
 require_relative "../pages/grading_curve_page"
 require_relative "../setup/gradebook_setup"
 
-describe "Gradebook editing grades" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Gradebook editing grades" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
   include GradebookSetup
 
   before(:once) do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
     gradebook_data_setup
     show_sections_filter(@teacher)
   end
@@ -443,4 +452,9 @@ describe "Gradebook editing grades" do
       end
     end
   end
+end
+
+describe "Gradebook editing grades" do
+  it_behaves_like "Gradebook editing grades", true
+  it_behaves_like "Gradebook editing grades", false
 end
--- spec/selenium/grades/gradebook/gradebook_group_weight_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_group_weight_spec.rb
@@ -20,9 +20,21 @@
 require_relative "../../helpers/gradebook_common"
 require_relative "../pages/gradebook_page"
 
-describe "Gradebook - group weights" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Gradebook - group weights" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
+
+  before :once do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
+  end
 
   def student_totals
     totals = ff(".total-cell")
@@ -138,4 +150,9 @@ describe "Gradebook - group weights" do
       expect(Gradebook.content_selector).not_to contain_jqcss(".total-cell .icon-off")
     end
   end
+end
+
+describe "Gradebook - group weights" do
+  it_behaves_like "Gradebook - group weights", true
+  it_behaves_like "Gradebook - group weights", false
 end
