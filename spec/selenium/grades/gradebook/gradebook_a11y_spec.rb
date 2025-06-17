7846869e667dc1e0cd707466b0831c7c7be3fc74
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-17 10:20:32 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 3
    [fsc-timeout=120]
    [fsc-node-count=10]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: Ica55df0e6a224901b4a6d2e8ec3fcb4c105d47de
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381312
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    Reviewed-by: Chris Soto <christopher.soto@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_a11y_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_a11y_spec.rb
@@ -22,7 +22,10 @@ require_relative "../../helpers/color_common"
 require_relative "../pages/gradebook_page"
 require_relative "../pages/gradebook_cells_page"
 
-describe "Gradebook" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Gradebook" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
   include ColorCommon
@@ -31,6 +34,12 @@ describe "Gradebook" do
   let(:students) { @course.students }
 
   before :once do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
     gradebook_data_setup
   end
 
@@ -435,4 +444,9 @@ describe "Gradebook" do
       check_element_has_focus Gradebook.assignment_header_menu_trigger_element(assignment.title)
     end
   end
+end
+
+describe "Gradebook" do
+  it_behaves_like "Gradebook", true
+  it_behaves_like "Gradebook", false
 end
