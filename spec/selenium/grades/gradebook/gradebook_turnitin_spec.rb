f530d62dd462b694d141940b0afbc57b2fb24056
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-17 10:25:45 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 17
    [fsc-timeout=60]
    [fsc-node-count=10]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: If4967a0ac0191cca6dc2afd73654e8274bd6f2da
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381553
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_turnitin_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_turnitin_spec.rb
@@ -20,11 +20,22 @@
 require_relative "../../helpers/gradebook_common"
 require_relative "../pages/gradebook_page"
 
-describe "Gradebook - turnitin" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Gradebook - turnitin" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
 
-  before(:once) { gradebook_data_setup }
+  before(:once) do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
+    gradebook_data_setup
+  end
 
   before { user_session(@teacher) }
 
@@ -86,4 +97,9 @@ describe "Gradebook - turnitin" do
     icons = ff(".gradebook-cell-turnitin")
     expect(icons).to have_size 2
   end
+end
+
+describe "Gradebook - turnitin" do
+  it_behaves_like "Gradebook - turnitin", true
+  it_behaves_like "Gradebook - turnitin", false
 end
