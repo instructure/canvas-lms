763cce9f84ff5c2387705a7245da9a88b3d32baa
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-17 10:25:21 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 16
    [fsc-timeout=60]
    [fsc-node-count=10]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: Ib180e21fe28ba79be45270ec4e2ebec067fe213b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381552
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_total_column_options_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_total_column_options_spec.rb
@@ -20,11 +20,22 @@
 require_relative "../../helpers/gradebook_common"
 require_relative "../pages/gradebook_page"
 
-describe "Gradebook - total column menu options" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Gradebook - total column menu options" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
 
-  before(:once) { gradebook_data_setup }
+  before :once do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
+    gradebook_data_setup
+  end
 
   before { user_session(@teacher) }
 
@@ -142,4 +153,9 @@ describe "Gradebook - total column menu options" do
       expect(Gradebook.gradebook_slick_header_columns[1]).to match("Total")
     end
   end
+end
+
+describe "Gradebook - total column menu options" do
+  it_behaves_like "Gradebook - total column menu options", true
+  it_behaves_like "Gradebook - total column menu options", false
 end
