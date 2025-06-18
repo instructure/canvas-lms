b141b744e04d381c6454318299d6ca6c15bbbf05
Mysti Lilla <mysti@instructure.com>
2025-06-18 12:46:29 -0600

    Move WikiPage attachment association creation to on save
    refs GROW-84
    flag=file_association_access
    [skip-crystalball]
    
    Test plan
    - Specs pass
    
    Change-Id: If7a114ad558f0bd90a1637fd055e9e1e7a07b923
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381742
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Andras Kemeny <akemeny@instructure.com>
    QA-Review: Andras Kemeny <akemeny@instructure.com>
    Product-Review: Andras Kemeny <akemeny@instructure.com>

--- app/controllers/wiki_pages_api_controller.rb
+++ app/controllers/wiki_pages_api_controller.rb
@@ -213,13 +213,8 @@ class WikiPagesApiController < ApplicationController
     end
 
     new_page = @page.duplicate
+    new_page.saving_user = @current_user
     new_page.save!
-
-    UserContent.associate_attachments_to_rce_object(
-      new_page.body,
-      new_page,
-      user: @current_user
-    )
 
     render json: wiki_page_json(new_page, @current_user, session)
   end
@@ -383,13 +378,8 @@ class WikiPagesApiController < ApplicationController
       allowed_fields << :estimated_duration_attributes if @context.is_a?(Course) && @context.horizon_course?
       update_params = get_update_params(allowed_fields)
       assign_todo_date
+      @page.saving_user = @current_user
       if !update_params.is_a?(Symbol) && @page.update(update_params) && process_front_page
-        UserContent.associate_attachments_to_rce_object(
-          @page.body,
-          @page,
-          user: @current_user
-        )
-
         log_asset_access(@page, "wiki", @wiki, "participate")
         render json: wiki_page_json(@page, @current_user, session)
       else
@@ -475,13 +465,8 @@ class WikiPagesApiController < ApplicationController
     if perform_update
       assign_todo_date
       update_params = get_update_params(allowed_fields)
+      @page.saving_user = @current_user
       if !update_params.is_a?(Symbol) && @page.update(update_params) && process_front_page
-        UserContent.associate_attachments_to_rce_object(
-          @page.body,
-          @page,
-          user: @current_user
-        )
-
         # This ensures the context module's UI items are updated
         @page.context_module_tags.each { |content_tag| content_tag.context_module&.touch }
 
