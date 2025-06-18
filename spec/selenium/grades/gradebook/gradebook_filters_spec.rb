f22c825263e763e863793c4e98c76103d4e6a0de
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-18 08:57:34 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 9.1
    [fsc-timeout=60]
    [fsc-node-count=10]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: Idbbe50535bef67084ec20913e7b815521926e73b
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381536
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

--- spec/selenium/grades/gradebook/gradebook_filters_spec.rb
+++ spec/selenium/grades/gradebook/gradebook_filters_spec.rb
@@ -24,11 +24,23 @@ require_relative "../setup/gradebook_setup"
 
 require_relative "../pages/gradebook_cells_page"
 
-describe "Filter" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "Filter" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
   include GradebookSetup
   include GroupsCommon
+
+  before :once do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
+  end
 
   context "by Module" do
     before(:once) do
@@ -133,4 +145,9 @@ describe "Filter" do
       expect(Gradebook::Cells.get_grade(@student_2, @first_assignment)).to eq "1"
     end
   end
+end
+
+describe "Filter" do
+  it_behaves_like "Filter", true
+  it_behaves_like "Filter", false
 end
