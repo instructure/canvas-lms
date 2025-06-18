121e72c4bd6c0813d661690bea46eaff485b20bd
Robin Kuss <rkuss@instructure.com>
2025-06-18 13:46:44 -0500

    spec: add expand/collapse mod student view tests
    Closes LX-2830
    
    Test Plan: passes Jenkins
    
    Change-Id: I84ca767ddd4d9bfd83fd172334a34e8f37afd2d3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381743
    Reviewed-by: Ed Schiebel <eschiebel@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Product-Review: Robin Kuss <rkuss@instructure.com>
    QA-Review: Felipe Leon <felipe.leon@instructure.com>
    Reviewed-by: Felipe Leon <felipe.leon@instructure.com>

--- spec/selenium/context_modules_v2/page_objects/modules2_index_page.rb
+++ spec/selenium/context_modules_v2/page_objects/modules2_index_page.rb
@@ -21,12 +21,21 @@ require_relative "../../common"
 
 module Modules2IndexPage
   #------------------------------ Selectors -----------------------------
-  def student_modules_container_selector
-    "[data-testid='modules-rewrite-student-container']"
+
+  def collapse_all_modules_button_selector
+    "button[aria-label='Collapse All']"
   end
 
-  def teacher_modules_container_selector
-    "[data-testid='modules-rewrite-container']"
+  def edit_item_modal_selector
+    "[data-testid='edit-item-modal']"
+  end
+
+  def expand_all_modules_button_selector
+    "button[aria-label='Expand All']"
+  end
+
+  def manage_module_item_button_selector(module_item_id)
+    "[data-testid='module-item-action-menu_#{module_item_id}']"
   end
 
   def manage_module_item_container_selector(module_item_id)
@@ -37,58 +46,70 @@ module Modules2IndexPage
     "[data-testid='module-action-menu_#{module_id}']"
   end
 
-  def manage_module_item_button_selector(module_item_id)
-    "[data-testid='module-item-action-menu_#{module_item_id}']"
-  end
-
-  def module_item_action_menu_selector
-    "[data-testid='module-item-action-menu']"
+  def module_header_expand_toggle_selector
+    "[data-testid='module-header-expand-toggle']"
   end
 
   def module_item_action_menu_link_selector(tool_text)
     "[role=menuitem]:contains('#{tool_text}')"
   end
 
-  def edit_item_modal_selector
-    "[data-testid='edit-item-modal']"
-  end
-
-  def send_to_modal_modal_selector
-    "[data-testid='send-to-item-modal']"
-  end
-
-  def send_to_modal_input_selector
-    "#content-share-user-search"
-  end
-
-  def module_header_expand_toggle_selector
-    "[data-testid='module-header-expand-toggle']"
-  end
-
   def module_item_title_selector
     "[data-testid='module-item-title']"
+  end
+
+  def module_item_action_menu_selector
+    "[data-testid='module-item-action-menu']"
   end
 
   def page_body
     f("body")
   end
 
-  #------------------------------ Elements ------------------------------
-
-  def student_modules_container
-    f(student_modules_container_selector)
+  def send_to_modal_input_selector
+    "#content-share-user-search"
   end
 
-  def teacher_modules_container
-    f(teacher_modules_container_selector)
+  def send_to_modal_modal_selector
+    "[data-testid='send-to-item-modal']"
+  end
+
+  def student_modules_container_selector
+    "[data-testid='modules-rewrite-student-container']"
+  end
+
+  def teacher_modules_container_selector
+    "[data-testid='modules-rewrite-container']"
+  end
+
+  #------------------------------ Elements ------------------------------
+
+  def collapse_all_modules_button
+    f(collapse_all_modules_button_selector)
+  end
+
+  def edit_item_modal
+    f(edit_item_modal_selector)
+  end
+
+  def expand_all_modules_button
+    f(expand_all_modules_button_selector)
+  end
+
+  def manage_module_item_button(module_item_id)
+    f(manage_module_item_button_selector(module_item_id))
+  end
+
+  def manage_module_item_container(module_item_id)
+    f(manage_module_item_container_selector(module_item_id))
   end
 
   def module_action_menu(module_id)
     f(module_action_menu_selector(module_id))
   end
 
-  def manage_module_item_button(module_item_id)
-    f(manage_module_item_button_selector(module_item_id))
+  def module_header_expand_toggles
+    ff(module_header_expand_toggle_selector)
   end
 
   def module_item_action_menu
@@ -99,12 +120,12 @@ module Modules2IndexPage
     fj(module_item_action_menu_link_selector(tool_text))
   end
 
-  def edit_item_modal
-    f(edit_item_modal_selector)
+  def module_item_titles
+    ff(module_item_title_selector)
   end
 
-  def manage_module_item_container(module_item_id)
-    f(manage_module_item_container_selector(module_item_id))
+  def send_to_form_selected_elements
+    ff("button[type='button']", send_to_modal_input_container)
   end
 
   def send_to_modal
@@ -119,17 +140,14 @@ module Modules2IndexPage
     fxpath("../..", send_to_modal_input)
   end
 
-  def send_to_form_selected_elements
-    ff("button[type='button']", send_to_modal_input_container)
+  def student_modules_container
+    f(student_modules_container_selector)
   end
 
-  def module_header_expand_toggle
-    f(module_header_expand_toggle_selector)
+  def teacher_modules_container
+    f(teacher_modules_container_selector)
   end
 
-  def module_item_title
-    f(module_item_title_selector)
-  end
   #------------------------------ Actions -------------------------------
 
   def set_rewrite_flag(rewrite_status: true)
@@ -152,7 +170,8 @@ module Modules2IndexPage
 
   def course_modules_setup(student_view: false)
     student_view ? set_rewrite_student_flag : set_rewrite_flag
-    @quiz = @course.assignments.create!(title: "quiz assignment", submission_types: "online_quiz")
+    @quiz = @course.quizzes.create!(title: "some quiz")
+    @quiz.publish!
     @assignment = @course.assignments.create!(title: "assignment 1", submission_types: "online_text_entry")
     @assignment2 = @course.assignments.create!(title: "assignment 2",
                                                submission_types: "online_text_entry",
@@ -162,10 +181,10 @@ module Modules2IndexPage
 
     @module1 = @course.context_modules.create!(name: "module1")
     @module2 = @course.context_modules.create!(name: "module2")
-    @module1.add_item({ id: @assignment.id, type: "assignment" })
-    @module1.add_item({ id: @assignment2.id, type: "assignment" })
-    @module2.add_item({ id: @assignment3.id, type: "assignment" })
-    @module2.add_item({ id: @quiz.id, type: "quiz" })
+    @module_item1 = @module1.add_item({ id: @assignment.id, type: "assignment" })
+    @module_item2 = @module1.add_item({ id: @assignment2.id, type: "assignment" })
+    @module_item3 = @module2.add_item({ id: @assignment3.id, type: "assignment" })
+    @module_item4 = @module2.add_item({ id: @quiz.id, type: "quiz" })
 
     @course.reload
   end
