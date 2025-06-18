601bb40bd76c500a931d42ac6b7cd5658fd7cf92
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-06-18 09:01:03 +0200

    spec: Enable tests for performance_improvements_for_gradebook FF vol 20.1
    [fsc-timeout=60]
    [fsc-node-count=20]
    [fsc-max-nodes=20]
    
    refs EVAL-5543
    flag=performance_improvements_for_gradebook
    
    Test plan: auto-tested
    
    Change-Id: I5abb61956f3217c61363e124af68993f0c7c77f5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381556
    Reviewed-by: László Sipula <laszlo.sipula@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/selenium/outcomes/outcome_gradebook_spec.rb
+++ spec/selenium/outcomes/outcome_gradebook_spec.rb
@@ -21,10 +21,22 @@ require_relative "../grades/pages/gradebook_page"
 require_relative "../grades/setup/gradebook_setup"
 require_relative "../helpers/gradebook_common"
 
-describe "outcome gradebook" do
+# NOTE: We are aware that we're duplicating some unnecessary testcases, but this was the
+# easiest way to review, and will be the easiest to remove after the feature flag is
+# permanently removed. Testing both flag states is necessary during the transition phase.
+shared_examples "outcome gradebook" do |ff_enabled|
   include_context "in-process server selenium tests"
   include GradebookCommon
   include GradebookSetup
+
+  before :once do
+    # Set feature flag state for the test run - this affects how the gradebook data is fetched, not the data setup
+    if ff_enabled
+      Account.site_admin.enable_feature!(:performance_improvements_for_gradebook)
+    else
+      Account.site_admin.disable_feature!(:performance_improvements_for_gradebook)
+    end
+  end
 
   context "as a teacher" do
     before(:once) do
@@ -707,4 +719,9 @@ describe "outcome gradebook" do
       end
     end
   end
+end
+
+describe "outcome gradebook" do
+  it_behaves_like "outcome gradebook", true
+  it_behaves_like "outcome gradebook", false
 end
