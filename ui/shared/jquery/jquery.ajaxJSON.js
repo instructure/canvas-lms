ec64222cab3f90db7a4b489d248b3d10b5f26fb0
Evan Battaglia <ebattaglia@instructure.com>
2025-06-20 07:06:24 +0000

    add debugging output to leaky ajaxJSON tests
    refs INTEROP-9281
    flag=none
    
    Unhandled rejections stemming from this function often cause errors
    in later tests which are extremely difficult to track down.
    
    Note that the blocks inside `if (process.env.NODE_ENV)` get compiled out
    and don't appear in development / production (I tested this by adding
    `if (process.env.NODE_ENV) { console.log("uniquestr") }` to some
    javascript running on a page, loading the page locally, and saving and
    checking the HAR file for "uniquestr")
    
    Test plan:
    - check out a previous commit with a flaky spec and produce a slakey
      spec by editing StudentFirstNameCellFormatter.test.js and commenting
      out this line:
        jest.spyOn(gradebook, 'saveSettings').mockImplentation()
    - run the spec in a variety of ways:
      1) run directly, without any further changes
      2) add jest.spyOn(gradebook, 'saveSettings'), but no
         mockImplementation()
      3) add a test at the end
        it('sleeps', () => new Promise((res, rej) => setTimeout(res, 5000)))
      4) run with --no-randomize
      5) run with --detectOpenHandles
      6) run with several combinations of these.
    - In particular, makie sure to run with the sleep test and
      --no-randomize, both with and without --detectOpenHandles
    - In each case, if the test run fails, it should also give a
      stack trace with Gradebook.saveSettings and
      StudentFirstNameCellFormatter.test.js (with a line number other than
      the sleep test) in the output.
    
    Change-Id: I6d279e7c1c17e247e9b9342457e80b0189cd532d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381885
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    QA-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Product-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>

--- ui/shared/jquery/jquery.ajaxJSON.js
+++ ui/shared/jquery/jquery.ajaxJSON.js
@@ -47,6 +47,12 @@ $.ajaxJSON = function (url, submit_type, data = {}, success, error, options) {
     submit_type = 'POST'
     data.authenticity_token = authenticity_token()
   }
+
+  let debugStack = undefined
+  if (process.env.NODE_ENV === 'test') {
+    debugStack = new Error().stack
+  }
+
   const ajaxError = function (xhr, textStatus, errorThrown) {
     if (textStatus === 'abort') {
       return // request aborted, do nothing
@@ -65,6 +71,18 @@ $.ajaxJSON = function (url, submit_type, data = {}, success, error, options) {
       $.ajaxJSON.ignoredXHRs.push(xhr)
     }
     if (error && $.isFunction(error)) {
+      if (process.env.NODE_ENV === 'test') {
+        try {
+          const blurb =
+            'An unstubbed ajaxJSON request was made and has likely caused an unrelated test to fail. Please inspect the following stacktrace to find the offending test and either skip it and create a ticket with the appropriate team or fix it yourself. We recommend using Mock Service Worker (MSW).'
+          const msg = `${blurb}\nstack: ${new Error().stack}\n*** initiator: ${debugStack}`
+          if (data && !data.stack) {
+            data.stack = msg
+          } else {
+            console.error(msg)
+          }
+        } catch (_e) {}
+      }
       error(data, xhr, textStatus, errorThrown)
     } else {
       $.ajaxJSON.unhandledXHRs.push(xhr)
