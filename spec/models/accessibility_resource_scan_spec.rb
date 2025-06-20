267251e98bc178a62a55756f1a99d5e2a29c143c
Peter Popovics <peter.popovics@instructure.com>
2025-06-20 18:38:54 +0200

    Add the accessibility dashboard models
    refs LMA-71
    flag=none
    
    test plan:
    - specs pass
    
    Change-Id: I97638a6ddd980004020d87e24cd7faedf02b3223
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/381946
    Reviewed-by: Martin Dubai <m.dubai@instructure.com>
    QA-Review: Martin Dubai <m.dubai@instructure.com>
    Product-Review: Martin Dubai <m.dubai@instructure.com>
    Migration-Review: Martin Dubai <m.dubai@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

---
+++ spec/models/accessibility_resource_scan_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+describe AccessibilityResourceScan do
+  subject { described_class.new }
+
+  it_behaves_like "has a single accessibility context"
+
+  describe "defaults" do
+    it "sets the default workflow_state to queued" do
+      expect(subject.workflow_state).to eq "queued"
+    end
+  end
+
+  describe "factories" do
+    it "has a valid factory" do
+      expect(accessibility_resource_scan_model).to be_valid
+    end
+  end
+
+  describe "validations" do
+    context "when course is missing" do
+      before { subject.course = nil }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors[:course]).to include("can't be blank")
+      end
+    end
+
+    describe "workflow_state" do
+      context "when workflow_state is missing" do
+        before { subject.workflow_state = nil }
+
+        it "is invalid" do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:workflow_state]).to include("can't be blank")
+        end
+      end
+
+      context "when workflow_state is not in the allowed list" do
+        before { subject.workflow_state = "invalid_state" }
+
+        it "is invalid" do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:workflow_state]).to include("is not included in the list")
+        end
+      end
+    end
+  end
+end
