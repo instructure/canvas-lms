bc9a83d8590c7d128e9038cb2ed50282ec088c7b
Peipei Zhou <peipei.zhou@instructure.com>
2025-06-30 11:56:10 +0200

    handle AssessmentQuestionBank alignment url
    closes EGG-1437
    flag=none
    
    Test plan:
    - create a course
    - create a new quiz
    - create a question bank
    - add a question and align an outcome
    - add the question bank to the new quiz
    - publish the new quiz and the course
    - enroll student
    - as student take the quiz
    - `api/v1/courses/<course_id>/outcome_rollups?include[]=alignments`
        api endpoint should return 200 with result
    
    Change-Id: Iadac371d51a9ce4f49ae10b9b56835c20c0b9a73
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/382583
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- lib/api/v1/outcome_results.rb
+++ lib/api/v1/outcome_results.rb
@@ -139,8 +139,7 @@ module Api::V1::OutcomeResults
   def outcome_results_include_alignments_json(alignments)
     alignments.map do |alignment|
       hash = { id: alignment.asset_string, name: alignment.title }
-      html_url = alignment.is_a?(LiveAssessments::Assessment) ? "" : polymorphic_url([alignment.context, alignment])
-      hash[:html_url] = html_url if html_url
+      hash[:html_url] = outcome_alignment_html_url(alignment)
       hash
     end
   end
@@ -150,9 +149,19 @@ module Api::V1::OutcomeResults
       {
         id: a.asset_string,
         name: a.title,
-        html_url: a.is_a?(LiveAssessments::Assessment) ? "" : polymorphic_url([a.context, a]),
+        html_url: outcome_alignment_html_url(a),
         submission_types: a.try(:submission_types) || "magic_marker"
       }
+    end
+  end
+
+  def outcome_alignment_html_url(alignment)
+    if alignment.nil? || alignment.is_a?(LiveAssessments::Assessment)
+      ""
+    elsif alignment.is_a?(AssessmentQuestionBank)
+      course_question_bank_url(course_id: alignment.context.id, id: alignment.id)
+    else
+      polymorphic_url([alignment.context, alignment])
     end
   end
 
