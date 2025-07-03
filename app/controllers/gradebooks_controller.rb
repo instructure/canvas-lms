3b81cbe478e46b3522660746c1a5ecd09f446f55
dan-costin <dan.costin@instructure.com>
2025-07-03 10:11:00 +0300

    Pass grading_assistance_file_uploads feature flag to SG2
    flag=grading_assistance_file_uploads
    Closes: EVAL-5575
    
    Test Plan:
    - Make sure that grading_assistance_file_uploads is enabled in Site Admin feature options.
    - Open Dev console from Speed Grader Page
    - window.ENV.GRADING_ASSISTANCE_FILE_UPLOADS_ENABLED should return true
    
    Change-Id: I9b8fa3be9cccf8b31b75a81005ea53a0ad63714f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/382597
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cameron Ray <cameron.ray@instructure.com>
    QA-Review: Giani Statie <giani.statie@instructure.com>
    Product-Review: Giani Statie <giani.statie@instructure.com>

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -1086,7 +1086,8 @@ class GradebooksController < ApplicationController
         late_policy: @context.late_policy&.as_json(include_root: false),
         gradebook_group_filter_id: @current_user.get_latest_preference_setting_by_key(:gradebook_settings, @context.global_id, "filter_rows_by", "student_group_ids"),
         can_view_audit_trail: @assignment.present? && @assignment.can_view_audit_trail?(@current_user),
-        PROJECT_LHOTSE_ENABLED: @context.feature_enabled?(:project_lhotse)
+        PROJECT_LHOTSE_ENABLED: @context.feature_enabled?(:project_lhotse),
+        GRADING_ASSISTANCE_FILE_UPLOADS_ENABLED: Account.site_admin.feature_enabled?(:grading_assistance_file_uploads)
       }
       js_env(env)
 
