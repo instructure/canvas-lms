2bc093a23e6097ecbbb580c1adb96c23e3082b67
Sarah Gerard <sarah.gerard@instructure.com>
2025-07-09 12:44:23 -0700

    Preserve navigation state after 401 redirect
    this change only applies to navigation actions, including
    opening a folder, breadcrumbs, search, and all my files
    
    closes RCX-3342
    flag=files_a11y_rewrite
    
    test plan:
    - in one tab, have files open
    - in another tab, log out of canvas
    - try each of the navigational actions listed in the ticket
    - open a folder, breadcrumb, search, and all my files
    - after you are redirected to the login page, login
    - expect to be navigated back to the page you were trying to
      get to.
    
    Change-Id: Ie3f68a2f8d7d1a49b5fa21f983e94536f5293867
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/383386
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Kata Virag Keri <kata.keri@instructure.com>

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -154,22 +154,27 @@ class FoldersController < ApplicationController
     return render_json_unauthorized unless Account.site_admin.feature_enabled?(:files_a11y_rewrite)
 
     @folder = Folder.find(params[:id])
-    return unless authorized_action(@folder, @current_user, :read_contents)
-
-    items, opts, all_item_count = paginated_folders_and_files(api_v1_list_folders_and_files_url)
-    headers["X-Total-Items"] = all_item_count.to_s
-    render json: folders_or_files_json(items, @current_user, session, opts)
+    if authorized_action(@folder, @current_user, :read_contents)
+      items, opts, all_item_count = paginated_folders_and_files(api_v1_list_folders_and_files_url)
+      headers["X-Total-Items"] = all_item_count.to_s
+      render json: folders_or_files_json(items, @current_user, session, opts)
+    else
+      store_location(request.referer)
+    end
   end
 
   # internal API
   def list_all_folders_and_files
     return render_json_unauthorized unless Account.site_admin.feature_enabled?(:files_a11y_rewrite)
-    return unless authorized_action(@context, @current_user, :read_files)
 
-    base_url = polymorphic_url([:api, :v1, @context, :folders_and_files])
-    items, opts, all_item_count = paginated_folders_and_files(base_url)
-    headers["X-Total-Items"] = all_item_count.to_s
-    render json: folders_or_files_json(items, @current_user, session, opts)
+    if authorized_action(@context, @current_user, :read_files)
+      base_url = polymorphic_url([:api, :v1, @context, :folders_and_files])
+      items, opts, all_item_count = paginated_folders_and_files(base_url)
+      headers["X-Total-Items"] = all_item_count.to_s
+      render json: folders_or_files_json(items, @current_user, session, opts)
+    else
+      store_location(request.referer)
+    end
   end
 
   # Setup additional options based on context and permissions
@@ -312,6 +317,8 @@ class FoldersController < ApplicationController
       raise ActiveRecord::RecordNotFound if folders.blank?
 
       render json: folders_json(folders, @current_user, session, can_view_hidden_files:, context: @context)
+    else
+      store_location(request.referer)
     end
   end
 
