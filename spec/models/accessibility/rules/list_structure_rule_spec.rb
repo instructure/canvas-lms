1cec88f0d4d3c4d3b8550c7149d6f9e63ed1f597
Viktor Barna <viktor.barna@instructure.com>
2025-07-21 16:22:27 +0200

    fix list-structure rule generation issue
    the list-sturcture rule did not work, the response was not correct.
    in this change this issue is fixed alongside with some small details,
    like response messages or button text
    
    refs LMA-175
    flag=accessibility_tab_enable
    
    test plan:
    - create a list-structure violation
    - open up accessibility tray tab
    - Press Fix → Preview Problem → Apply → Undo → Apply →
    Save and Next → Issue shall be gone
    - (you can test with out with ordered and unordered list as well)
    
    Change-Id: I102fdd2fb86e70b1170653485898732602bad02a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/384305
    Reviewed-by: Attila Toth <attila.toth@instructure.com>
    QA-Review: Martin Dubai <m.dubai@instructure.com>
    Product-Review: Martin Dubai <m.dubai@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- spec/models/accessibility/rules/list_structure_rule_spec.rb
+++ spec/models/accessibility/rules/list_structure_rule_spec.rb
@@ -87,6 +87,24 @@ describe Accessibility::Rules::ListStructureRule do
       expect(fixed_html.delete("\n")).to eq(expected_html)
     end
 
+    it "Resolves unordered items with extra space" do
+      input_html = "<p>* List </p><p>* List </p><p>* List </p>"
+      expected_html = "<ul><li>List </li><li>List </li><li>List </li></ul>"
+
+      fixed_html = fix_issue(:list_structure, input_html, "./*", "true")
+
+      expect(fixed_html.delete("\n")).to eq(expected_html)
+    end
+
+    it "Resolves ordered with extra space" do
+      input_html = "<p>1. List </p><p>2. List </p><p>3. List </p>"
+      expected_html = "<ol><li>List </li><li>List </li><li>List </li></ol>"
+
+      fixed_html = fix_issue(:list_structure, input_html, "./*", "true")
+
+      expect(fixed_html.delete("\n")).to eq(expected_html)
+    end
+
     it "Replaces the ordered part of the list even when nested in an element" do
       input_html = "<p><strong>1. Text</strong> Text</p><p><strong>2. Text</strong> Text</p><p><strong>3. Text</strong> Text</p>"
       expected_html = "<ol><li><strong>Text</strong> Text</li><li><strong>Text</strong> Text</li><li><strong>Text</strong> Text</li></ol>"
@@ -170,7 +188,7 @@ describe Accessibility::Rules::ListStructureRule do
 
   context "form" do
     it "returns the proper form" do
-      expect(Accessibility::Rules::ListStructureRule.form(nil).label).to eq("Format as list")
+      expect(Accessibility::Rules::ListStructureRule.form(nil).label).to eq("Reformat")
     end
   end
 end
