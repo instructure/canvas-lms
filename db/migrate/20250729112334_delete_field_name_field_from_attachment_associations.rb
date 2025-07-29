02c37e86b30d4aede7f1a890c844097f831f09c4
akemeny <akemeny@instructure.com>
2025-07-29 14:16:05 +0200

    [migration] Drop field_name in attachment_associations
    flag = none
    
    Test plan: No test plan necessary.
    
    refs GROW-74
    
    Change-Id: I3fb9059fa49905f1a265c1bfa19faad012b07612
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/384934
    Reviewed-by: Mysti Lilla <mysti@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Jake Oeding <jake.oeding@instructure.com>
    QA-Review: Andras Kemeny <akemeny@instructure.com>
    Product-Review: Andras Kemeny <akemeny@instructure.com>

---
+++ db/migrate/20250729112334_delete_field_name_field_from_attachment_associations.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+class DeleteFieldNameFieldFromAttachmentAssociations < ActiveRecord::Migration[7.1]
+  tag :postdeploy
+  disable_ddl_transaction!
+
+  def change
+    remove_column :attachment_associations, :field_name # rubocop:disable Migration/RemoveColumn,Rails/ReversibleMigration
+    drop_enum :enum_attachment_associations_field_name
+  end
+end
