1066ab9e8740f3a526737fcfc18fc6fdf3e5e724
Daniel, Torok <daniel.torok@instructure.com>
2025-07-30 15:24:38 +0200

    Block Editor: introduce context for Preview
    refs RCX-3681
    flag=block_content_editor
    
    test plan:
    - open a page for edit
    - verify preview button is gray
    - click preview button
    - verify it toggles to filled state
    
    Change-Id: I9713ff64c5b27367afefc372df86f908f252439c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/385074
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ferenc Marcsó <ferenc.marcso@instructure.com>
    QA-Review: Ferenc Marcsó <ferenc.marcso@instructure.com>
    Product-Review: Dániel Török <daniel.torok@instructure.com>

---
+++ ui/shared/block-content-editor/react/Toolbar/__tests__/Toolbar.test.tsx
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {Editor} from '@craftjs/core'
+import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Toolbar} from '../Toolbar'
+import {useBlockContentEditorContext} from '../../BlockContentEditorContext'
+
+const mockSetMode = jest.fn()
+
+jest.mock('../../BlockContentEditorContext', () => ({
+  __esModule: true,
+  useBlockContentEditorContext: jest.fn(),
+}))
+
+function renderToolbar() {
+  return render(
+    <Editor>
+      <Toolbar />
+    </Editor>,
+  )
+}
+
+function setupMockContext(mode: string = 'default') {
+  ;(useBlockContentEditorContext as jest.Mock).mockReturnValue({
+    editor: {
+      mode,
+      setMode: mockSetMode,
+    },
+  })
+}
+
+describe('Toolbar', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('when toolbar is in default mode', () => {
+    beforeEach(() => {
+      setupMockContext('default')
+    })
+
+    it('should switch to preview mode when clicked', async () => {
+      const user = userEvent.setup()
+      const {getByRole} = renderToolbar()
+      const previewButton = getByRole('button', {name: /preview/i})
+
+      await user.click(previewButton)
+
+      expect(mockSetMode).toHaveBeenCalledWith('preview')
+    })
+  })
+
+  describe('when toolbar is in preview mode', () => {
+    beforeEach(() => {
+      setupMockContext('preview')
+    })
+
+    it('should switch to default mode when clicked', async () => {
+      const user = userEvent.setup()
+      const {getByRole} = renderToolbar()
+      const previewButton = getByRole('button', {name: /preview/i})
+
+      await user.click(previewButton)
+
+      expect(mockSetMode).toHaveBeenCalledWith('default')
+    })
+  })
+})
