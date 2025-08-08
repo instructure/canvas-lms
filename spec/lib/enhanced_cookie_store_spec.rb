3dbcefa4f728f7e749aaa73ab99607aa3e512be8
Jacob Burroughs <jburroughs@instructure.com>
2025-08-07 19:00:31 -0500

    Remove legacy session cookie
    fixes AE-2703
    
    Change-Id: I674d80a9aefeb8fd1892a52ce2ff9fd020277436
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/385692
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- spec/lib/enhanced_cookie_store_spec.rb
+++ spec/lib/enhanced_cookie_store_spec.rb
@@ -19,12 +19,12 @@
 
 require_relative "../spec_helper"
 
-describe "SamesiteTransitionCookieStore" do
+describe "EnhancedCookieStore" do
   describe "#unmarshal" do
     it "doesn't explode with malformed data" do
       app = double("SomeAppThing")
       options = { secret: "038c3fa7b8f07362f0a79db7e717eada" } # <- fake secret
-      store = SamesiteTransitionCookieStore.new(app, options)
+      store = EnhancedCookieStore.new(app, options)
       expect(Canvas::Errors).to receive(:capture_exception) do |type, e, level|
         expect(level).to eq(:info)
         expect(e.class).to be(ArgumentError)
