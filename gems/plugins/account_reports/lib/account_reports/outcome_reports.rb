1bf6521d2c530b0b97d12ac2f98d9ce7fa978535
davidwenzlick <david.wenzlick@instructure.com>
2025-08-08 15:08:51 -0400

    Filter inst pseudonym from outcome result report
    closes AE-2610
    
    Test plan
    - specs pass
    - I also tested this locally
    
    Change-Id: I263bb6cd7908851c52b409c90af8921870bd2647
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/385779
    Reviewed-by: Michael Hargiss <mhargiss@instructure.com>
    Reviewed-by: Weston Dransfield <wdransfield@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Dave Wenzlick <david.wenzlick@instructure.com>
    Product-Review: Dave Wenzlick <david.wenzlick@instructure.com>

--- gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
+++ gems/plugins/account_reports/lib/account_reports/outcome_reports.rb
@@ -126,7 +126,7 @@ module AccountReports
         account_id: account.id,
         root_account_id: root_account.id
       }
-      students = root_account.pseudonyms.except(:preload)
+      students = root_account.pseudonyms.not_instructure_identity.except(:preload)
                              .select(<<~SQL.squish)
                                pseudonyms.id,
                                u.sortable_name        AS "student name",
@@ -441,6 +441,7 @@ module AccountReports
     end
 
     def outcome_results_scope
+      inst_identity = Pseudonym.column_names.include?("is_inst_id")
       students = account.learning_outcome_links.active
                         .select(<<~SQL.squish)
                           distinct on (#{outcome_order}, p.id, s.id, r.id, qr.id, q.id, a.id, subs.id, qs.id, aq.id)
@@ -488,6 +489,7 @@ module AccountReports
                           INNER JOIN #{ContentTag.quoted_table_name} ct ON r.content_tag_id = ct.id
                           INNER JOIN #{User.quoted_table_name} u ON u.id = r.user_id
                           INNER JOIN #{Pseudonym.quoted_table_name} p on p.user_id = r.user_id
+                            #{"AND p.is_inst_id = false" if inst_identity}
                           INNER JOIN #{Course.quoted_table_name} c ON r.context_id = c.id
                           INNER JOIN #{Account.quoted_table_name} acct ON acct.id = c.account_id
                           INNER JOIN #{Enrollment.quoted_table_name} e ON e.type = 'StudentEnrollment' and e.root_account_id = #{account.root_account.id}
