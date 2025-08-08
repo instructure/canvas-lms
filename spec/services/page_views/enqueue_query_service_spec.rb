10465b2c6e46c0b58c53e4942e9a62defa954cdc
Peter Michaleczky <peter.michaleczky@instructure.com>
2025-08-08 12:48:45 +0200

    add endpoints to support new async PageView 5 API queries
    Three new endpoints have been added to PageViewsController to support
    the new async PageView query endpoints for CSV download:
    - `/api/v1/users/:user_id/page_views/query`: to enqueue a new
      PageView query with parameters
    - `/api/v1/users/:user_id/page_views/query/:query_id`: polling
      running queries status
    - `/api/v1/users/:user_id/page_views/query/:query_id/results`:
      fetch the results
    
    The CR includes a new PageViewService module which interacts with
    PV5 API. The new service loads pv5 configuration and the access
    token from credentials.
    Existing Pv4Client, PageViews model are remain unchanged.
    
    flag=none
    refs: PVIEW-790, PVIEW-751
    
    **Test Plan**
    
    1. Make sure pv5.yml is created, and PV5 API access token is saved in
       the vault.
    2. POST /api/v1/users/:user_id/page_views/query.
    3. The request should be 201 Created and contain the polling address
       in the Location header.
    4. Request the received polling URI until you receive 200 OK with
       "finished" status, save the results_url.
    5. Request the results URL. The response should be the gzipped result
       in jsonl or csv format.
    
    Change-Id: I0022490b887699a332a4adcdec34d6a4c2b0aa9f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/385719
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Gergely Schmidt <gergely.schmidt@instructure.com>
    Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Csaba Sipőcz <csaba.sipocz@instructure.com>

---
+++ spec/services/page_views/enqueue_query_service_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+describe PageViews::EnqueueQueryService do
+  let(:configuration) { instance_double(PageViews::Configuration, uri: URI.parse("http://pv5.instructure.com"), access_token: "token") }
+  let(:service) { PageViews::EnqueueQueryService.new(configuration) }
+  let(:account) { instance_double(Account, id: 1, uuid: "abc") }
+  let(:user) { instance_double(User, global_id: 1, shard: Shard.default, root_account_ids: [account.id]) }
+
+  before do
+    allow(Account).to receive(:find_cached).with(1).and_return(account)
+    allow(user).to receive(:is_a?).with(User).and_return(true)
+  end
+
+  it "returns query's job ID" do
+    response = double(code: 201, header: { "Location" => "http://pv5.instructure.com/api/v5/pageviews/query/123456" })
+    allow(CanvasHttp).to receive(:post).and_yield(response)
+
+    query = service.call("2025-03-01", "2025-06-01", user, "csv")
+
+    expect(query).to eq("123456")
+  end
+
+  it "raises ArgumentError when query params are invalid" do
+    allow(CanvasHttp).to receive(:post)
+
+    [
+      ["2025-03-01", "2025-02-01", user, "csv"],
+      ["2025-03-01", "2025-03-10", user, "csv"],
+      ["2025-03--", "2025-02-01", user, "csv"],
+      ["2025-03-01", "", user, "csv"],
+      ["2025-03-01", "2025-04-01", nil, "csv"],
+      ["2025-03-01", "2025-04-01", user, "xml"],
+      ["2025-03-01T10:30:45", "2025-04-01", user, "csv"],
+      ["2025-03-01", "2025-04-01T15:20:30", user, "csv"]
+    ].each do |start_date, end_date, user, format|
+      expect do
+        service.call(start_date, end_date, user, format)
+        expect(CanvasHttp).not_to have_received(:post)
+      end.to raise_error(ArgumentError)
+    end
+  end
+
+  it "raises InvalidRequestError when PV5 API returns query parameters are invalid" do
+    response = double(code: 400, body: '{"errors": ["invalid root account uuid"]}')
+    allow(CanvasHttp).to receive(:post).and_yield(response)
+
+    expect do
+      service.call("2024-12-01", "2025-01-01", user, "jsonl")
+    end.to raise_error(PageViews::Common::InvalidRequestError) do |error|
+      expect(error.message).to eq("Invalid request: invalid root account uuid")
+    end
+  end
+
+  it "raises NotFoundError when PV5 API returns not found" do
+    response = double(code: 404)
+    allow(CanvasHttp).to receive(:post).and_yield(response)
+
+    expect do
+      service.call("2024-12-01", "2025-01-01", user, "jsonl")
+    end.to raise_error(PageViews::Common::NotFoundError) do |error|
+      expect(error.message).to eq("Resource not found")
+    end
+  end
+
+  it "raises InternalServerError when PV5 API returns internal server error" do
+    response = double(code: 500)
+    allow(CanvasHttp).to receive(:post).and_yield(response)
+
+    expect do
+      service.call("2024-12-01", "2025-01-01", user, "csv")
+    end.to raise_error(PageViews::Common::InternalServerError) do |error|
+      expect(error.message).to eq("Internal server error")
+    end
+  end
+
+  it "includes request id in headers" do
+    expected_request_id = SecureRandom.uuid
+    allow(RequestContext::Generator).to receive(:request_id).and_return(expected_request_id)
+    allow(CanvasHttp).to receive(:post).and_yield(double(code: 201, header: { "Location" => "http://pv5.instructure.com/api/v5/pageviews/query/123456" }))
+
+    service.call("2025-03-01", "2025-06-01", user, "csv")
+
+    expect(CanvasHttp).to have_received(:post).with(anything, hash_including("X-Request-Context-Id" => expected_request_id), anything)
+  end
+end
