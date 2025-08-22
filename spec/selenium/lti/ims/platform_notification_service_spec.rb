57b0e9ec4fa05bbd5d14c9d869eebb639ccab6b7
Tucker McKnight <tmcknight@instructure.com>
2025-08-22 17:04:25 -0600

    spec: Implement a mock LTI tool for Selenium testing
    This adds a few things for testing an LTI tool launch in Canvas,
    as well as a Selenium test that does the launch.
    
    First, in spec/selenium/test_setup/test_only_routes.rb, there is now
    a place to define endpoints that will only be available in Selenium
    tests.
    
    The endpoints defined in there mimic the endpoints that an LTI 1.3
    tool would define, in order to get a basic tool launch happening in
    Canvas.
    
    The /test/mock_lti/ui endpoints shows an HTML page with a single button,
    which will send a request to subscribe to the notification service when
    clicked.
    
    A Selenium test using these new endpoints is also part of this commit.
    
    [skip-crystalball]
    
    test plan:
    - specs pass
    
    flag = none
    
    fixes INTEROP-9239
    
    Change-Id: Ic184aeec0b48f4bcd941172fac22f76910eaa4cc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/386938
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: David Varga <d.varga@instructure.com>
    QA-Review: David Varga <d.varga@instructure.com>
    Product-Review: Tucker Mcknight <tmcknight@instructure.com>

---
+++ spec/selenium/lti/ims/platform_notification_service_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require_relative "../../common"
+require_relative "page_objects/mock_lti_tool_ui"
+require_relative "../../assignments/page_objects/assignment_create_edit_page"
+
+describe "Platform Notification Service" do
+  include_context "in-process server selenium tests"
+
+  let(:course) { course_model }
+  let(:base_url) { "http://#{HostUrl.default_host}/test/mock_lti" }
+  let(:registration) do
+    configuration_params = {
+      scopes: ["https://purl.imsglobal.org/spec/lti/scope/noticehandlers"],
+      redirect_uris: ["#{base_url}/ui"],
+      placements: [{
+        placement: "ActivityAssetProcessor",
+        message_type: "LtiDeepLinkingRequest",
+      }],
+      oidc_initiation_url: "#{base_url}/login",
+      public_jwk_url: "#{base_url}/jwks",
+      domain: HostUrl.default_host,
+    }
+
+    lti_registration_with_tool(account: course.account, configuration_params:)
+  end
+
+  before do
+    # With an asset processor tool installed in a course
+    teacher = user_model
+    account_admin_user(name: "A User", account: course.account)
+    course.enroll_teacher(teacher, enrollment_state: "active")
+    user_session(teacher)
+
+    Lti::UpdateRegistrationService.call(
+      id: registration.id,
+      account: course.account,
+      updated_by: teacher,
+      binding_params: { workflow_state: "on" }
+    )
+
+    key = OpenSSL::PKey::RSA.new 2048
+    allow(Lti::KeyStorage).to receive(:present_key).and_return(key)
+
+    allow_any_instance_of(Lti::IMS::NoticeHandlersController).to receive(:verify_access_token).and_return(true)
+    allow_any_instance_of(Lti::IMS::NoticeHandlersController).to receive(:developer_key).and_return(registration.developer_key)
+    allow_any_instance_of(Lti::IMS::NoticeHandlersController).to receive(:verify_access_scope).and_return(true)
+  end
+
+  it "registers a Notice Handler" do
+    # Visit the new assignment page
+    get("/courses/#{course.id}/assignments/new")
+
+    # Set assignment type to text submission
+    AssignmentCreateEditPage.select_text_entry_submission_type
+
+    # Click the asset processor button
+    scroll_into_view(AssignmentCreateEditPage.add_asset_processor_button)
+    AssignmentCreateEditPage.add_asset_processor_button.click
+    f("[data-testid='asset-processor-card']").click
+
+    # Expect the iframe modal to have loaded
+    expect(f("[role='dialog'] h2").text).to eq("Add A Document Processing App")
+
+    in_frame(AssignmentCreateEditPage.lti_tool_iframe) do
+      MockLtiToolUi.notification_button.click
+    end
+
+    # After clicking the mock tool's notification_button, this LTI registration
+    # should have an lti_notice_handler created on it.
+    tool_deployment = registration.deployments.first
+    expect(tool_deployment.lti_notice_handlers.first.url)
+      .to eq("#{base_url}/subscription_handler")
+  end
+end
---
+++ spec/selenium/test_setup/test_only_routes.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+module SeleniumDriverSetup
+  module TestOnlyRoutes
+    def self.create_routes(app)
+      app.routes.disable_clear_and_finalize = true
+
+      app.routes.draw do
+        post "/test/mock_lti/ui", to: "test/mock_lti#ui"
+        post "/test/mock_lti/login", to: "test/mock_lti#login"
+        get "/test/mock_lti/jwks", to: "test/mock_lti#jwks"
+        post "/test/mock_lti/subscription_handler", to: "test/mock_lti#subscription_handler"
+      end
+    end
+  end
+end
