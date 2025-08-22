b26fb831baea7eb623989620f61df001d8f61e59
Drake Harper <drake.harper@instructure.com>
2025-08-22 09:51:07 -0600

    Add pagination to CourseWork widget
    flag=widget_dashboard
    closes LX-3202
    
    Test Plan:
    - create a course with 5+ assignments
    - as a student in the course load widget_dashboard
    - should see the course work widget paginated
    
    Change-Id: Ifc3c0cd885926d5ded44c32c00eecf0630cc88fe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/386893
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Drake Harper <drake.harper@instructure.com>

---
+++ ui/features/widget_dashboard/react/components/widgets/CourseWorkWidget/CourseWorkItem.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {Flex} from '@instructure/ui-flex'
+import {View} from '@instructure/ui-view'
+import {Text} from '@instructure/ui-text'
+import {Link} from '@instructure/ui-link'
+import {Tag} from '@instructure/ui-tag'
+import {useScope as createI18nScope} from '@canvas/i18n'
+import {formatDueDate, getTypeInfo} from './utils'
+import type {CourseWorkItem as CourseWorkItemType} from '../../../hooks/useCourseWork'
+
+const I18n = createI18nScope('widget_dashboard')
+
+interface CourseWorkItemProps {
+  item: CourseWorkItemType
+}
+
+export function CourseWorkItem({item}: CourseWorkItemProps) {
+  return (
+    <Flex.Item key={item.id} overflowY="hidden">
+      <View as="div" margin="small" background="primary">
+        <Flex gap="small" alignItems="start">
+          <Flex.Item shouldShrink>
+            <Tag
+              text={getTypeInfo(item.type).label}
+              color={getTypeInfo(item.type).color}
+              margin="0 0 x-small 0"
+            />
+          </Flex.Item>
+          <Flex.Item shouldGrow>
+            <Flex direction="column" gap="xx-small">
+              <Link
+                href={item.htmlUrl}
+                isWithinText={false}
+                data-testid={`course-work-item-link-${item.id}`}
+              >
+                <Text weight="bold" size="small">
+                  {item.title}
+                </Text>
+              </Link>
+              <Text size="x-small" color="secondary">
+                {item.course.name}
+              </Text>
+              <Text size="x-small" color="secondary">
+                {formatDueDate(item.dueAt)}
+                {item.points != null && ` • ${I18n.t('%{points} pts', {points: item.points})}`}
+              </Text>
+            </Flex>
+          </Flex.Item>
+        </Flex>
+      </View>
+    </Flex.Item>
+  )
+}
---
+++ ui/features/widget_dashboard/react/components/widgets/CourseWorkWidget/utils.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useScope as createI18nScope} from '@canvas/i18n'
+import {startOfToday, getTomorrow} from '../../../utils/dateUtils'
+import type {CourseWorkItem} from '../../../hooks/useCourseWork'
+
+const I18n = createI18nScope('widget_dashboard')
+
+function dueDateTime(dueDate: Date, day: string): string {
+  return I18n.t('Due %{day} at %{time}', {
+    day,
+    time: dueDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}),
+  })
+}
+
+export function formatDueDate(dueAt: string | null): string {
+  if (!dueAt) return I18n.t('No due date')
+
+  const dueDate = new Date(dueAt)
+  const today = startOfToday()
+  const tomorrow = getTomorrow()
+
+  const dueToday = dueDate.toDateString() === today.toDateString()
+  const dueTomorrow = dueDate.toDateString() === tomorrow.toDateString()
+
+  if (dueToday) {
+    return dueDateTime(dueDate, I18n.t('today'))
+  }
+  if (dueTomorrow) {
+    return dueDateTime(dueDate, I18n.t('tomorrow'))
+  }
+  return I18n.t('Due %{date} at %{time}', {
+    date: dueDate.toLocaleDateString(),
+    time: dueDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}),
+  })
+}
+
+export function getTypeInfo(type: CourseWorkItem['type']) {
+  switch (type) {
+    case 'assignment':
+      return {color: 'info' as const, label: I18n.t('Assignment')}
+    case 'quiz':
+      return {color: 'warning' as const, label: I18n.t('Quiz')}
+    case 'discussion':
+      return {color: 'success' as const, label: I18n.t('Discussion')}
+    default:
+      return {color: 'primary' as const, label: I18n.t('Item')}
+  }
+}
