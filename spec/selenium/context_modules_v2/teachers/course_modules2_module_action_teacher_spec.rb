331b535c6b940c6e0912bcfa1ec908ae48e07653
June-eoh <jungsun.eoh@instructure.com>
2025-08-26 10:32:55 -0700

    spec: add tests for module edit and requirement for teacher view
    closes LX-3198
    
    Test Plan:
    Jenkins runs and passes
    
    Change-Id: I0829d0b4f691ea37fd2fe454a1097042f084f3da
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387215
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Felipe Leon <felipe.leon@instructure.com>
    QA-Review: Felipe Leon <felipe.leon@instructure.com>
    Product-Review: June Eoh <jungsun.eoh@instructure.com>

---
+++ spec/selenium/context_modules_v2/teachers/course_modules2_module_action_teacher_spec.rb
@@ -0,0 +1,157 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+require_relative "../../helpers/context_modules_common"
+require_relative "../page_objects/modules2_index_page"
+require_relative "../page_objects/modules2_action_tray"
+require_relative "../../helpers/items_assign_to_tray"
+require_relative "../../helpers/assignments_common"
+require_relative "../shared_examples/course_modules2_shared"
+describe "context modules", :ignore_js_errors do
+  include_context "in-process server selenium tests"
+  include ContextModulesCommon
+  include Modules2IndexPage
+  include ItemsAssignToTray
+  include AssignmentsCommon
+  include Modules2ActionTray
+
+  before :once do
+    modules2_teacher_setup
+  end
+
+  before do
+    user_session(@teacher)
+  end
+
+  context "module action menu edit" do
+    before do
+      go_to_modules
+      wait_for_ajaximations
+    end
+
+    it "edits a module name" do
+      module_action_menu(@module1.id).click
+      module_item_action_menu_link("Edit").click
+      expect(module_settings_tray).to be_displayed
+
+      new_module_name = "Updated Module Name"
+      expect(input_module_name).to be_displayed
+      fill_in_module_name(new_module_name)
+      click_save_module_tray_change
+      expect(context_module(@module1.id).attribute("data-module-name")).to eq(new_module_name)
+    end
+  end
+
+  context "uses tray to edit module requirements" do
+    before :once do
+      @module4 = @course.context_modules.create!(name: "module with no items")
+      @module5 = @course.context_modules.create!(name: "module with requirements")
+      @required_hw = @course.assignments.create!(title: "assignment 1")
+      @required_quiz = @course.quizzes.create!(title: "quiz 1")
+      @tag_1 = @module5.add_item({ id: @required_hw.id, type: "assignment" })
+      @tag_2 = @module5.add_item({ id: @required_quiz.id, type: "quiz" })
+      @module5.completion_requirements = { @tag_1.id => { type: "must_view" }, @tag_2.id => { type: "must_view" } }
+      @module5.save!
+    end
+
+    before do
+      go_to_modules
+      wait_for_ajaximations
+    end
+
+    it "sets complete all as default selection and shows one requirement card" do
+      module_action_menu(@module1.id).click
+      module_item_action_menu_link("Edit").click
+      click_add_requirement_button
+      expect(is_checked(complete_all_radio_checked)).to be true
+      expect(module_requirement_card.length).to eq(1)
+    end
+
+    it "selects complete all requirements and clicks sequential order" do
+      expect(element_exists?(module_header_complete_all_pill_selector(@module1.id))).to be false
+      module_action_menu(@module1.id).click
+      module_item_action_menu_link("Edit").click
+      click_add_requirement_button
+      expect(sequential_order_checkbox).to be_displayed
+      sequential_order_checkbox.click
+      click_save_module_tray_change
+      expect(context_module_completion_requirement(@module1.id).text).to include("Complete All Items")
+    end
+
+    it "selects complete one requirement" do
+      module_action_menu(@module1.id).click
+      module_item_action_menu_link("Edit").click
+      click_add_requirement_button
+      select_complete_one_radio
+      expect(is_checked(complete_one_radio_checked)).to be true
+      expect(module_requirement_card.length).to eq(1)
+      expect(element_exists?(sequential_order_checkbox_selector, true)).to be false
+    end
+
+    it "does not show Requirements button for module with no items" do
+      module_action_menu(@module4.id).click
+      module_item_action_menu_link("Edit").click
+      expect(element_exists?(add_requirement_button_selector)).to be false
+    end
+
+    it "cancels a requirement session" do
+      module_action_menu(@module1.id).click
+      module_item_action_menu_link("Edit").click
+      click_add_requirement_button
+      expect(close_tray_button).to be_displayed
+      cancel_tray_button.click
+      wait_for_ajaximations
+      expect(element_exists?(context_module_completion_requirement_selector(@module1.id))).to be false
+    end
+
+    it "update complete all requirement to complete one" do
+      module_action_menu(@module5.id).click
+      module_item_action_menu_link("Edit").click
+      select_complete_one_radio
+      click_save_module_tray_change
+      expect(context_module_completion_requirement(@module5.id).text).to include("Complete One Item")
+    end
+
+    it "updates requirement type and shows on modules page" do
+      module_action_menu(@module5.id).click
+      module_item_action_menu_link("Edit").click
+      select_requirement_type_option(0, "Mark as done")
+      select_requirement_type_option(1, "Submit the assignment")
+      click_save_module_tray_change
+      ignore_relock
+      module_header_expand_toggles.last.click
+      expect(context_module_item_todo(@module5.content_tags[0].id, "Mark as done")).to be_present
+      expect(context_module_item_todo(@module5.content_tags[1].id, "Submit quiz")).to be_present
+    end
+
+    it "switches between requirement count radios with arrow keys" do
+      module_action_menu(@module2.id).click
+      module_item_action_menu_link("Edit").click
+      click_add_requirement_button
+      select_complete_one_radio
+      expect(is_checked(complete_one_radio_checked)).to be true
+      driver.action.send_keys(:arrow_up).perform
+      expect(is_checked(complete_all_radio_checked)).to be true
+      driver.action.send_keys(:arrow_down).perform
+      expect(is_checked(complete_one_radio_checked)).to be true
+    end
+
+    it_behaves_like "course_module2 module tray requirements", :context_modules
+    it_behaves_like "course_module2 module tray requirements", :course_homepage
+  end
+end
