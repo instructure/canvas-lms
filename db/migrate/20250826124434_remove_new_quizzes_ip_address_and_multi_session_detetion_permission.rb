dd48ef99661e1637a7f23651e46514ec980389db
Sipos Bálint <balint.sipos@instructure.com>
2025-08-26 17:24:17 +0200

    chore: Revert the role override for become_user with ip_address
    and detect_multiple_session permissions in Canvas
    
    closes: QUIZ-16283
    
    flag = none
    
    [skip-crystalball]
    
    test plan = see in ticket comments.
    
    Change-Id: I1533872ba46fa5e7af69b6b98ccd158650b92e70
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387194
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Richárd Varró <richard.varro@instructure.com>
    QA-Review: Rnad Alkhalafat <rnad.alkhalafat@instructure.com>
    Product-Review: András Maróy <andras.maroy@instructure.com>

---
+++ db/migrate/20250826124434_remove_new_quizzes_ip_address_and_multi_session_detetion_permission.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+class RemoveNewQuizzesIpAddressAndMultiSessionDetetionPermission < ActiveRecord::Migration[7.2]
+  tag :postdeploy
+
+  def up
+    DataFixup::RemoveRoleOverridesForNewPermission
+      .delay_if_production(priority: Delayed::LOW_PRIORITY)
+      .run(:become_user,
+           [:new_quizzes_view_ip_address, :new_quizzes_multiple_session_detection],
+           skip_role_type: "AccountAdmin")
+  end
+
+  def down
+    DataFixup::AddRoleOverridesForNewPermission
+      .delay_if_production(priority: Delayed::LOW_PRIORITY)
+      .run(:become_user, :new_quizzes_view_ip_address)
+
+    DataFixup::AddRoleOverridesForNewPermission
+      .delay_if_production(priority: Delayed::LOW_PRIORITY)
+      .run(:become_user, :new_quizzes_multiple_session_detection)
+  end
+end
