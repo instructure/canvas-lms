4bf0d947f73fc6e788f8440caf2d08712bd5382e
Jozsef Kercso <jozsef.kercso@instructure.com>
2025-08-27 14:44:28 +0200

    API documentation: Fix broken links
    The linkchecker tool of the new doc portal reported some new broken
    links. They are broken due a hard-coded '/doc/api' prefix in the source
    code files. They work with the old doc portal but not any more in the
    new one.
    
    The aim of this patchset is to update these links so they work in both
    the old and the new API portal.
    
    Refs: SAS-4460
    
    Flag: none
    
    Test plan:
    - Generated new HTML (`rake doc:api`) and Markdown documentation
    (`OUTPUT_FORMAT=markdown rake doc:api`) and compared each file
    to its original version to ensure no unintended changes were introduced.
    - Opened the new documentation in a web browser and verified that the
    links are working.
    
    Change-Id: I1113c85e5e48cc561083b94c5c5feeb2dddced15
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387283
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Evan Battaglia <ebattaglia@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>
    QA-Review: Jozsef Kercso <jozsef.kercso@instructure.com>

--- app/controllers/lti/ims/scores_controller.rb
+++ app/controllers/lti/ims/scores_controller.rb
@@ -20,7 +20,7 @@
 module Lti::IMS
   # @API Score
   #
-  # Score API for 1EdTech (IMS) <a href="/doc/api/file.assignment_tools.html">Assignment and Grade Services</a>.
+  # Score API for 1EdTech (IMS) <a href="file.assignment_tools.html">Assignment and Grade Services</a>.
   #
   # @model Score
   #     {
