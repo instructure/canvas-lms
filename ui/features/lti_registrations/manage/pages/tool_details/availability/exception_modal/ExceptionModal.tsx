e61278fc3f8823d790dede45878d722646c030d8
Tucker McKnight <tmcknight@instructure.com>
2025-08-27 23:31:37 -0600

    Limit registration exceptions UI to only add 100 at a time
    The backend API will only allow 100 availability exceptions to be
    added at once. So the UI should also be limited to 100 at a time.
    
    test plan:
    - Edit the ExceptionModal.tsx file (the one that's changed in this commit)
      and change the number 100 to something smaller like 3 for easier testing
    - Enable the lti_registrations_next flag
    - Go to Admin > Apps
    - On one of your existing app registrations, click "Add Exception"
    - Choose 3 accounts or courses
    - Try selecting a fourth account or course, and see that you get a browser
      modal saying that you can't do that
    - Click the trash icon on one of your selected accounts/courses, then try
      adding another one, and see that it works
    - Click an account/course that you already selected, and see that it doesn't
      prompt the error modal to show up
    
    flag = lti_registrations_next
    
    fixes INTEROP-9475
    
    Change-Id: Ia8250961ef6329e2c7e2074bfd9012c5b01eddce
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387366
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Alexis Nast <alexis.nast@instructure.com>

--- ui/features/lti_registrations/manage/pages/tool_details/availability/exception_modal/ExceptionModal.tsx
+++ ui/features/lti_registrations/manage/pages/tool_details/availability/exception_modal/ExceptionModal.tsx
@@ -98,7 +98,16 @@ export const ExceptionModal = ({
       control => control.context.context.id === context.context.id,
     )
     if (!existingControl) {
-      setContextControlForm(prev => [...prev, {context, available: false}])
+      const maxControlsSelectable = 100
+      if (contextControlForm.length >= maxControlsSelectable) {
+        alert(
+          I18n.t('Sorry, you can only add %{maxControlsSelectable} exceptions at a time.', {
+            maxControlsSelectable,
+          }),
+        )
+      } else {
+        setContextControlForm(prev => [...prev, {context, available: false}])
+      }
     }
     setBrowserOpen(false)
   }
