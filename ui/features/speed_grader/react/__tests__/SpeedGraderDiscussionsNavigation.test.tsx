bda79b6bf64bad9049f83c5ec5ad2290c3f6ed5f
Caleb Guanzon <cguanzon@instructure.com>
2025-09-02 09:03:32 -0600

    spec: move selenium test to jest
    fixes EGG-411
    
    the whole selenium test file is flaky.
    this is because they do too much and
    sets up over 20 entries
    
    moved button tests to jest
    the other selenium tests are ok to delete
    since the components are deprecated
    
    Change-Id: I0fabb954a45b6208adbc605aee2333206ae28e1c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387737
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Chawn Neal <chawn.neal@instructure.com>
    QA-Review: Chawn Neal <chawn.neal@instructure.com>
    Product-Review: Kyle Rosenbaum <krosenbaum@instructure.com>

--- ui/features/speed_grader/react/__tests__/SpeedGraderDiscussionsNavigation.test.tsx
+++ ui/features/speed_grader/react/__tests__/SpeedGraderDiscussionsNavigation.test.tsx
@@ -17,18 +17,77 @@
  */
 
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import {SpeedGraderDiscussionsNavigation} from '../SpeedGraderDiscussionsNavigation'
 
 const setup = () => {
   return render(<SpeedGraderDiscussionsNavigation />)
 }
 
+const setupMockIframes = () => {
+  const mockContentWindow = {
+    postMessage: jest.fn(),
+  }
+
+  const mockDiscussionIframe = {
+    contentWindow: mockContentWindow,
+  }
+
+  const mockSpeedgraderIframe = {
+    contentDocument: {
+      getElementById: jest.fn().mockReturnValue(mockDiscussionIframe),
+    },
+  }
+
+  jest.spyOn(document, 'getElementById').mockReturnValue(mockSpeedgraderIframe as any)
+
+  return mockContentWindow
+}
+
 describe('SpeedGraderDiscussionsNavigation', () => {
+  beforeEach(() => {
+    // Mock the DOM structure that the component expects
+    document.body.innerHTML = `
+      <iframe id="speedgrader_iframe">
+        <iframe id="discussion_preview_iframe"></iframe>
+      </iframe>
+    `
+  })
+
+  afterEach(() => {
+    document.body.innerHTML = ''
+  })
+
   it('renders', () => {
     const {getByText} = setup()
 
     expect(getByText('Previous Reply')).toBeInTheDocument()
     expect(getByText('Next Reply')).toBeInTheDocument()
+  })
+
+  it('calls nextStudentReply when Next Reply button is clicked', () => {
+    const {getByTestId} = setup()
+    const mockContentWindow = setupMockIframes()
+    const nextButton = getByTestId('discussions-next-reply-button')
+
+    fireEvent.click(nextButton)
+
+    expect(mockContentWindow.postMessage).toHaveBeenCalledWith(
+      {subject: 'DT.nextStudentReply'},
+      '*',
+    )
+  })
+
+  it('calls previousStudentReply when Previous Reply button is clicked', () => {
+    const {getByTestId} = setup()
+    const mockContentWindow = setupMockIframes()
+    const previousButton = getByTestId('discussions-previous-reply-button')
+
+    fireEvent.click(previousButton)
+
+    expect(mockContentWindow.postMessage).toHaveBeenCalledWith(
+      {subject: 'DT.previousStudentReply'},
+      '*',
+    )
   })
 })
