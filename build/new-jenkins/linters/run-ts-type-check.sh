b161da2d6e38238768b05c2c042c1bc30a853aef
Evan Battaglia <ebattaglia@instructure.com>
2025-09-02 11:04:10 +0000

    jenkins: Explain possible codegen failures better
    If a commit uses a new field that is not in schema.graphql yet, and you
    just need to run the graphql rake task, codegen will fail. This will
    give the failure a little more context.
    
    See also g/387692
    
    Test plan:
    - modify any gql query to request a field that doesn't exist
    - run build/new-jenkins/linters/run-ts-type-check.sh and check the error
      message and check exit_status
    
    flag=none
    
    Change-Id: I36e01f5399ccc6ba16921996d8b06ccd892cf58a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387693
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Build-Review: James Belleau <james.belleau@instructure.com>
    Reviewed-by: David Varga <d.varga@instructure.com>
    QA-Review: David Varga <d.varga@instructure.com>
    Product-Review: Evan Battaglia <ebattaglia@instructure.com>

--- build/new-jenkins/linters/run-ts-type-check.sh
+++ build/new-jenkins/linters/run-ts-type-check.sh
@@ -12,7 +12,11 @@ echo "Generating plugin extensions..."
 node ui-build/webpack/generatePluginExtensions.js || { echo "Generating plugin extensions failed"; exit_status=1; }
 
 echo "Generate graphql codegen..."
-yarn run graphql:codegen || { echo "Running graphql codegen types failed"; exit_status=1; }
+yarn run graphql:codegen || {
+    echo "Running graphql codegen types failed"
+    echo "Note: codegen works off of schema.graphql, so make sure this file is up-to-date with the ruby code by running 'rake graphql:schema'"
+    exit_status=1
+}
 
 echo "Running TypeScript type check..."
 node_modules/.bin/tsc -p tsconfig.json --noEmit || { echo "TypeScript type checking failed"; exit_status=1; }
