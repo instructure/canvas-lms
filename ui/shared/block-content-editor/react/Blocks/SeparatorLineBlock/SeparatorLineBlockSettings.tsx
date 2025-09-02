c50c6438aef4a930112c9e42f92afd82f0e1f05e
Ádám Máté <adam.mate@instructure.com>
2025-09-02 11:21:42 +0200

    BlockEditor: Implement Separator Block thickness setting
    refs RCX-3899
    flag=block_content_editor
    
    test-plan:
    -Add a SeparatorBlock to the BCE and verify default rendering.
    -Thickness change works as expected
    -Thickness is saved/loaded
    
    Change-Id: I930bc2c70a4c15813d8f6c49616f9fe6e7694f0a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387686
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Tamas Barna <tamas.barna@instructure.com>
    QA-Review: Tamas Barna <tamas.barna@instructure.com>
    Product-Review: Kata Virag Keri <kata.keri@instructure.com>

--- ui/shared/block-content-editor/react/Blocks/SeparatorLineBlock/SeparatorLineBlockSettings.tsx
+++ ui/shared/block-content-editor/react/Blocks/SeparatorLineBlock/SeparatorLineBlockSettings.tsx
@@ -22,16 +22,27 @@ import {useScope as createI18nScope} from '@canvas/i18n'
 import {ColorPickerWrapper} from '../BlockItems/ColorPickerWrapper'
 import {SeparatorLineBlockProps} from './SeparatorLineBlock'
 import {SettingsSectionToggle} from '../BlockItems/SettingsSectionToggle/SettingsSectionToggle'
+import {BorderWidthValues} from '@instructure/emotion'
+import {RadioInput, RadioInputGroup} from '@instructure/ui-radio-input'
+import {Flex} from '@instructure/ui-flex'
 
 const I18n = createI18nScope('block_content_editor')
+
+const THICKNESS_OPTIONS: {label: string; value: BorderWidthValues}[] = [
+  {label: I18n.t('Small'), value: 'small'},
+  {label: I18n.t('Medium'), value: 'medium'},
+  {label: I18n.t('Large'), value: 'large'},
+]
 
 export const SeparatorLineBlockSettings = () => {
   const {
     actions: {setProp},
     separatorColor,
+    thickness,
     backgroundColor,
   } = useNode(node => ({
     separatorColor: node.data.props.separatorColor,
+    thickness: node.data.props.thickness,
     backgroundColor: node.data.props.backgroundColor,
   }))
 
@@ -47,8 +58,14 @@ export const SeparatorLineBlockSettings = () => {
     })
   }
 
+  const handleThicknessChange = (value: BorderWidthValues) => {
+    setProp((props: SeparatorLineBlockProps) => {
+      props.thickness = value
+    })
+  }
+
   return (
-    <View as="div" padding="small">
+    <View as="div">
       <SettingsSectionToggle
         title={I18n.t('Color settings')}
         collapsedLabel={I18n.t('Expand color settings')}
@@ -64,13 +81,35 @@ export const SeparatorLineBlockSettings = () => {
           baseColorLabel={I18n.t('Background')}
         />
       </SettingsSectionToggle>
-      <ColorPickerWrapper
-        label={I18n.t('Separator')}
-        value={separatorColor}
-        baseColor={backgroundColor}
-        onChange={handleSeparatorColorChange}
-        baseColorLabel={I18n.t('Background')}
-      />
+
+      <SettingsSectionToggle
+        title={I18n.t('Separator settings')}
+        collapsedLabel={I18n.t('Expand separator settings')}
+        expandedLabel={I18n.t('Collapse separator settings')}
+        defaultExpanded={true}
+        includeSeparator={false}
+      >
+        <Flex direction="column" gap="medium">
+          <ColorPickerWrapper
+            label={I18n.t('Separator')}
+            value={separatorColor}
+            baseColor={backgroundColor}
+            onChange={handleSeparatorColorChange}
+            baseColorLabel={I18n.t('Background')}
+          />
+
+          <RadioInputGroup
+            name="separator-line-block-settings-thickness"
+            description={I18n.t('Separator size')}
+            value={thickness}
+            onChange={(_event, value) => handleThicknessChange(value as BorderWidthValues)}
+          >
+            {THICKNESS_OPTIONS.map(option => (
+              <RadioInput key={option.value} label={option.label} value={option.value} />
+            ))}
+          </RadioInputGroup>
+        </Flex>
+      </SettingsSectionToggle>
     </View>
   )
 }
