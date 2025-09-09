1480c2dff68b4801ddfa42513d97a357809f7a44
Katalin Kiss <katalin.kiss@instructure.com>
2025-09-09 15:03:50 +0200

    BCE: Label colors should contrast well with the block background
    Updates the color of block's title label for accessibility.
    Fallback to the style of block types if no valid contrasting color
    is found.
    
    closes RCX-3753
    flag=block_content_editor
    
    test plan:
    - Open Page with BCE
    - Have a block with title field
    - Change block's background color in settings tray
    - The label for the title field should stay readable
    - Label only visible in edit mode
    
    Change-Id: I49a9a27f0b7a32fb7db013a622f74c5eabdacf27
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/388337
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Dániel Török <daniel.torok@instructure.com>
    QA-Review: Dániel Török <daniel.torok@instructure.com>
    Product-Review: Dániel Török <daniel.torok@instructure.com>

--- ui/shared/block-content-editor/react/Blocks/BlockItems/Title/TitleEdit.tsx
+++ ui/shared/block-content-editor/react/Blocks/BlockItems/Title/TitleEdit.tsx
@@ -18,18 +18,29 @@
 
 import {ChangeEvent} from 'react'
 import {TextInput} from '@instructure/ui-text-input'
+import {Tag} from '@instructure/ui-tag'
+import {Text} from '@instructure/ui-text'
 import {useScope as createI18nScope} from '@canvas/i18n'
 import {TitleEditProps} from './types'
 
 const I18n = createI18nScope('block_content_editor')
 
-export const TitleEdit = ({title, onTitleChange, focusHandler}: TitleEditProps) => {
+export const TitleEdit = ({title, onTitleChange, focusHandler, labelColor}: TitleEditProps) => {
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => onTitleChange(event.target.value)
+  const labelText = I18n.t('Block title')
 
   return (
     <TextInput
       elementRef={el => focusHandler?.(el as HTMLElement | null)}
-      renderLabel={I18n.t('Block title')}
+      renderLabel={
+        labelColor ? (
+          <Text weight="weightImportant" color="primary" themeOverride={{primaryColor: labelColor}}>
+            {labelText}
+          </Text>
+        ) : (
+          <Tag size="medium" text={labelText} />
+        )
+      }
       placeholder={I18n.t('Start typing...')}
       value={title}
       onChange={handleOnChange}
