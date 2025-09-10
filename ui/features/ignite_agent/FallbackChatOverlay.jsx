d65e11d31f941a1b36904f65a8210d7a70cf7d6c
Csaba Balla-Somogyi <csaba.ballasomogyi@instructure.com>
2025-09-10 20:25:48 +0200

    initial setup for Ignite Agent module federation integration
    refs IAGENT-145
    flag=ignite_agent_enabled
    
    test plan:
    - Enable the Ignite Agent feature flag
    - Start up the ignite agent locally
    - Navigate to any page in canvas-lms, the agent button should be visible
    - Click on the agent button, this should load
    the federated module and open the chat window
    
    Change-Id: I7093c199dccc5146ab6d207227af2fed3a1d724e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387635
    Product-Review: Kevin Rood <kevin.rood@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jorge Andres <jorge.andres@instructure.com>
    QA-Review: Jorge Andres <jorge.andres@instructure.com>

---
+++ ui/features/ignite_agent/FallbackChatOverlay.jsx
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {useScope as createI18nScope} from '@canvas/i18n'
+const I18n = createI18nScope('QRMobileLogin')
+
+import React from 'react'
+import {View} from '@instructure/ui-view'
+import {Text} from '@instructure/ui-text'
+import {IconButton} from '@instructure/ui-buttons'
+import {IconXSolid} from '@instructure/ui-icons'
+import {Flex} from '@instructure/ui-flex'
+
+/**
+ * Fallback sidebar component that renders when the agent fails to load
+ * @param {object} props - The component props
+ * @param {Error} props.error - The error that occurred
+ * @param {Function} props.onClose - Function to call when closing the sidebar
+ */
+export function FallbackChatOverlay({error, onClose}) {
+  return (
+    <View
+      as="div"
+      width="320px"
+      shadow="above"
+      borderRadius="medium"
+      background="primary"
+      borderWidth="medium"
+      borderColor="danger"
+      position="fixed"
+      insetInlineEnd="20px"
+      insetBlockEnd="20px"
+      style={{zIndex: 9999}}
+      padding="medium"
+    >
+      <Flex alignItems="center" justifyItems="space-between">
+        <Flex.Item shouldGrow shouldShrink>
+          <Text size="medium">
+            {I18n.t('An unexpected error happened while loading the Ignite Agent.')}
+          </Text>
+        </Flex.Item>
+        <Flex.Item margin="0 0 0 medium">
+          <IconButton
+            onClick={onClose}
+            screenReaderLabel={I18n.t('Close')}
+            renderIcon={IconXSolid}
+            size="small"
+            color="danger"
+            withBackground={true}
+            withBorder={false}
+          />
+        </Flex.Item>
+      </Flex>
+    </View>
+  )
+}
