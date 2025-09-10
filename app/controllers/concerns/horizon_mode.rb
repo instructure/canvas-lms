c8821ae46e47240e98a28e7a2a53ace11a7efb84
Adam_Mikulas <adam.mikulas@instructure.com>
2025-09-10 12:55:54 +0200

    Add career query params to redirect_to
    refs: CLXG-12
    flag=none
    
    test plan:
        - go to a career course settings page
        - delete the course
    
    Change-Id: I00abafc6e4a72d980710870eb8b3fd4c81616c2e
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/387801
    Reviewed-by: Adrián Grúber <adrian.gruber@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Attila Sedon <attila.sedon@instructure.com>
    QA-Review: Attila Sedon <attila.sedon@instructure.com>

--- app/controllers/concerns/horizon_mode.rb
+++ app/controllers/concerns/horizon_mode.rb
@@ -57,17 +57,47 @@ module HorizonMode
   def add_career_params
     yield
 
-    return unless @context.is_a?(Account) && @context.horizon_account?
-
-    horizon_params = { content_only: "true", instui_theme: "career", force_classic: "true" }
+    return unless should_add_horizon_params?
 
     location = response.location
-    uri = URI(location)
+    return unless location
 
+    response.location = add_horizon_params_to_url(location)
+  end
+
+  def redirect_to(options = {}, response_options = {})
+    if should_add_horizon_params?
+      if options.is_a?(String) && !options.include?("/career/")
+        options = add_horizon_params_to_url(options)
+      elsif options.is_a?(Hash) && !options.key?(:force_classic)
+        options = options.merge(horizon_params)
+      end
+    end
+
+    super
+  end
+
+  def should_add_horizon_params?
+    return false unless @context
+
+    if @context.is_a?(Account)
+      @context.horizon_account?
+    elsif @context.is_a?(Course)
+      @context.horizon_course?
+    else
+      false
+    end
+  end
+
+  def add_horizon_params_to_url(url)
+    uri = URI(url)
     query = Rack::Utils.parse_query(uri.query).merge(horizon_params.stringify_keys)
     uri.query = query.to_query
+    uri.to_s
+  end
 
-    response.location = uri.to_s
+  def horizon_params
+    { content_only: "true", instui_theme: "career", force_classic: "true" }.symbolize_keys
   end
 
   def rewrite_path_for_career
