ed93acaba0d53442d768f8244416f1bcc59b5b3d
theo sorriaux <theo.sorriaux@instructure.com>
2025-09-18 13:58:14 +0200

    Add nutrition facts to discussion translation
    We're adding the nutrition facts modal to discussion.
    This PS creates a base component and an initial implementation
    
    closes VICE-5515
    flag=cedar_translation
    
    Test plan
    - Go to a discussion topic
    - hover the AI button
    - it should behave as expected
    - it should display the correct information
    
    Change-Id: I07535803e760d65675eb7dabf8dadb5b52d2a027
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/389301
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Roland Beres <roland.beres@instructure.com>
    QA-Review: Norbert Zsolt Bakó <norbert.bako@instructure.com>
    Product-Review: Bence Árpási <bence.arpasi@instructure.com>

---
+++ ui/shared/nutrition-facts/react/constants.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {useScope as createI18nScope} from '@canvas/i18n'
+const I18n = createI18nScope('nutrition_facts')
+export const STATIC_TEXT = {
+  title: "",
+  dataPermissionLevelsTitle: I18n.t('Data Permission Levels'),
+  dataPermissionLevelsCurrentFeatureText: I18n.t('Current Feature:'),
+  dataPermissionLevelsCloseIconButtonScreenReaderLabel: I18n.t('Close'),
+  dataPermissionLevelsCloseButtonText: I18n.t('Close'),
+  dataPermissionLevelsModalLabel: I18n.t('This is a Data Permission Levels modal'),
+  dataPermissionLevelsTriggerText: I18n.t('Data Permission Levels'),
+  nutritionFactsModalLabel: I18n.t('This is a modal for AI facts'),
+  nutritionFactsTitle: I18n.t('Nutrition Facts'),
+  nutritionFactsCloseButtonText: I18n.t('Close'),
+  nutritionFactsCloseIconButtonScreenReaderLabel: I18n.t('Close'),
+  nutritionFactsTriggerText: I18n.t('Nutrition Facts'),
+}
