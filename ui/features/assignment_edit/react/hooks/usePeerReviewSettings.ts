ae011f2c1d3ed219e8b40e0d5cae1ec0f4a28757
Martin Yosifov <martin.yosifov@instructure.com>
2025-09-24 11:24:53 -0700

    Update Peer Review Details in Assignment Create Page
    closes EGG-1638
    flag=peer_review_allocation_and_grading
    
    Test Plan:
    - Recompile css assets by running the following command:
    docker-compose run --rm web yarn brandable_css
    - Create a course and enable the Peer Review Allocation
    and Grading feature flag
    - Create an assignment, select "Require Peer Reviews" checkbox
    and verify that the peer review details section looks like the
    designs in the ticket except that the "Require Peer Reviews"
    is a checkbox instead of a toggle
    - Enter a negative number of reviews, select another input and
    verify that error message is displayed
    - Enter a negative number of points, select another input and
    verify that error message is displayed
    - Expand "Advanced Per Review Configurations" and verify that
    each toggle can be enabled/disabled independently
    - Uncheck then recheck the "Require Peer Reviews" checkbox
    and verify that previously entered values are reset
    
    Change-Id: I6adde797eb1acca69aa2d3f8675abf9a09da8280
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/389612
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Roberto Noguera <roberto.noguera@instructure.com>
    QA-Review: Roberto Noguera <roberto.noguera@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

---
+++ ui/features/assignment_edit/react/hooks/usePeerReviewSettings.ts
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useState, useEffect} from 'react'
+import {useScope as createI18nScope} from '@canvas/i18n'
+
+const I18n = createI18nScope('peer_review_settings')
+
+export const MAX_NUM_PEER_REVIEWS = 10
+
+export const usePeerReviewSettings = () => {
+  const [reviewsRequired, setReviewsRequired] = useState<string>('1')
+  const [totalPoints, setTotalPoints] = useState<string>('0')
+  const [errorMessageReviewsRequired, setErrorMessageReviewsRequired] = useState<
+    string | undefined
+  >(undefined)
+  const [pointsPerReview, setPointsPerReview] = useState<string>('0')
+  const [errorMessagePointsPerReview, setErrorMessagePointsPerReview] = useState<
+    string | undefined
+  >(undefined)
+  const [allowPeerReviewAcrossMultipleSections, setAllowPeerReviewAcrossMultipleSections] =
+    useState<boolean>(false)
+  const [allowPeerReviewWithinGroups, setAllowPeerReviewWithinGroups] = useState<boolean>(false)
+  const [usePassFailGrading, setUsePassFailGrading] = useState<boolean>(false)
+  const [anonymousPeerReviews, setAnonymousPeerReviews] = useState<boolean>(false)
+  const [submissionsRequiredBeforePeerReviews, setSubmissionsRequiredBeforePeerReviews] =
+    useState<boolean>(false)
+
+  useEffect(() => {
+    setTotalPoints(calculateTotalPoints())
+  }, [reviewsRequired, pointsPerReview]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleReviewsRequiredChange = (
+    _event: React.ChangeEvent<HTMLInputElement>,
+    value: string,
+  ) => {
+    setReviewsRequired(value)
+
+    // Clear error message if value becomes valid
+    const numReviews = stringToNumber(value)
+    if (numReviews !== undefined && numReviews >= 0) {
+      setErrorMessageReviewsRequired(undefined)
+    }
+  }
+
+  const validateReviewsRequired = (event: React.FocusEvent<HTMLInputElement>) => {
+    let errorMessage
+
+    const inputElement = event.target
+    const numReviewsRequired = stringToNumber(reviewsRequired)
+
+    // Check if input is empty but user had entered invalid value (e.g. "-1e")
+    if (reviewsRequired === '' && inputElement.validity && !inputElement.validity.valid) {
+      errorMessage = I18n.t('Please enter a valid number.')
+    } else if (
+      numReviewsRequired === undefined ||
+      reviewsRequired === '' ||
+      reviewsRequired === '0'
+    ) {
+      errorMessage = I18n.t('Number of peer reviews is required.')
+    } else if (!Number.isInteger(numReviewsRequired)) {
+      errorMessage = I18n.t('Number of peer reviews must be a whole number.')
+    } else if (numReviewsRequired < 0) {
+      errorMessage = I18n.t('Number of peer reviews cannot be negative.')
+    } else if (numReviewsRequired > MAX_NUM_PEER_REVIEWS) {
+      errorMessage = I18n.t('Number of peer reviews cannot exceed %{max}.', {
+        max: MAX_NUM_PEER_REVIEWS,
+      })
+    }
+
+    setErrorMessageReviewsRequired(errorMessage)
+  }
+
+  const handlePointsPerReviewChange = (
+    _event: React.ChangeEvent<HTMLInputElement>,
+    value: string,
+  ) => {
+    setPointsPerReview(value)
+
+    const numPoints = stringToNumber(value)
+    if (numPoints !== undefined && numPoints >= 0) {
+      setErrorMessagePointsPerReview(undefined)
+    }
+  }
+
+  const validatePointsPerReview = (event: React.FocusEvent<HTMLInputElement>) => {
+    let errorMessage
+
+    const inputElement = event.target
+    const numPoints = stringToNumber(pointsPerReview)
+
+    if (pointsPerReview === '' && inputElement.validity && !inputElement.validity.valid) {
+      errorMessage = I18n.t('Please enter a valid number.')
+    } else if (numPoints !== undefined && numPoints < 0) {
+      errorMessage = I18n.t('Points per review cannot be negative.')
+    }
+
+    setErrorMessagePointsPerReview(errorMessage)
+  }
+
+  const calculateTotalPoints = () => {
+    const numReviewsRequired = stringToNumber(reviewsRequired)
+    const numPointsPerReview = stringToNumber(pointsPerReview)
+    if (
+      !numReviewsRequired ||
+      !numPointsPerReview ||
+      errorMessageReviewsRequired ||
+      errorMessagePointsPerReview
+    )
+      return '0'
+
+    const total = numReviewsRequired * numPointsPerReview
+    return total % 1 === 0 ? total.toString() : total.toFixed(2)
+  }
+
+  const handleCrossSectionsCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAllowPeerReviewAcrossMultipleSections(e.target.checked)
+  }
+
+  const handleInterGroupCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAllowPeerReviewWithinGroups(e.target.checked)
+  }
+
+  const handleUsePassFailCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsePassFailGrading(e.target.checked)
+  }
+
+  const handleAnonymityCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnonymousPeerReviews(e.target.checked)
+  }
+
+  const handleSubmissionRequiredCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubmissionsRequiredBeforePeerReviews(e.target.checked)
+  }
+
+  const resetFields = () => {
+    setReviewsRequired('1')
+    setPointsPerReview('0')
+    setErrorMessageReviewsRequired(undefined)
+    setErrorMessagePointsPerReview(undefined)
+    setAllowPeerReviewAcrossMultipleSections(false)
+    setAllowPeerReviewWithinGroups(false)
+    setUsePassFailGrading(false)
+    setAnonymousPeerReviews(false)
+    setSubmissionsRequiredBeforePeerReviews(false)
+  }
+
+  const stringToNumber = (value: string): number | undefined => {
+    const num = Number(value)
+    return Number.isNaN(num) ? undefined : num
+  }
+
+  return {
+    reviewsRequired,
+    handleReviewsRequiredChange,
+    validateReviewsRequired,
+    errorMessageReviewsRequired,
+    pointsPerReview,
+    handlePointsPerReviewChange,
+    validatePointsPerReview,
+    errorMessagePointsPerReview,
+    totalPoints,
+    allowPeerReviewAcrossMultipleSections,
+    handleCrossSectionsCheck,
+    allowPeerReviewWithinGroups,
+    handleInterGroupCheck,
+    usePassFailGrading,
+    handleUsePassFailCheck,
+    anonymousPeerReviews,
+    handleAnonymityCheck,
+    submissionsRequiredBeforePeerReviews,
+    handleSubmissionRequiredCheck,
+    resetFields,
+  }
+}
