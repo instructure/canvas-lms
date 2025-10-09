a5106d4a0c2dc1841e8c4f3a1c184169523c9083
Daniel Kis-Pal <daniel.kispal@instructure.com>
2025-10-09 09:49:29 +0000

    Revert "Fix Comment library item count in SG1"
    This reverts commit 54d61e9b745c4dc0cb3abdde9c6ab550ab1f8794.
    
    Reason for revert: Failing CI
    
    Change-Id: I7924f70745157e292b9236dd3379996201dd396c
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/391075
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Reviewed-by: Rajmund Csehil <rajmund.csehil@instructure.com>
    Reviewed-by: Ferenc Marcsó <ferenc.marcso@instructure.com>
    Reviewed-by: László Sipula <laszlo.sipula@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/speed_grader/react/CommentLibrary/__tests__/LibraryManager.test.jsx
+++ ui/features/speed_grader/react/CommentLibrary/__tests__/LibraryManager.test.jsx
@@ -23,7 +23,6 @@ import {createCache} from '@canvas/apollo-v3'
 import doFetchApi from '@canvas/do-fetch-api-effect'
 import {
   commentBankItemMocks,
-  commentBankItemMocksV2,
   makeDeleteCommentMutation,
   makeCreateMutationMock,
   searchMocks,
@@ -87,7 +86,7 @@ describe('LibraryManager', () => {
 
   describe('query', () => {
     it('renders a loading spinner while loading', () => {
-      const {getByText} = render()
+      const {getByText} = render(defaultProps())
       expect(getByText('Loading comment library')).toBeInTheDocument()
     })
 
@@ -106,31 +105,6 @@ describe('LibraryManager', () => {
       fireEvent.click(getByText('Open Comment Library'), {detail: 1})
       fireEvent.click(getByText('Comment item 0'), {detail: 1})
       expect(setFocusToTextAreaMock).toHaveBeenCalled()
-    })
-  })
-
-  describe('query with v2 mocks', () => {
-    // Could not move this into the describe above, somehow mocking
-    // got confused...
-    it('fetches all pages', async () => {
-      const mocks = commentBankItemMocksV2()
-      const {getByText, rerender} = render({
-        mocks,
-      })
-      await flushAllTimersAndPromises()
-      expect(getByText('15')).toBeInTheDocument()
-
-      // Simulate typing "search" in the comment area
-      render({
-        props: defaultProps({commentAreaText: 'search'}),
-        mocks,
-        func: rerender,
-      })
-
-      await flushAllTimersAndPromises()
-      expect(getByText('Comment item 2')).toBeInTheDocument()
-
-      expect(getByText('15')).toBeInTheDocument()
     })
   })
 
