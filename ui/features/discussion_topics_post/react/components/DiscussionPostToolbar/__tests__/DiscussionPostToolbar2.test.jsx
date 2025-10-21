02ca4f9ffd12edfedb8897f21555eecb6612b701
Dora, Csolakov <dora.csolakov@instructure.com>
2025-10-21 09:51:51 +0200

    Move the Translate area below the toggle button
    refs VICE-5467
    
    Change-Id: If2c2a8b978eca09d33cb755a4ba1cb61f43ee8e0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/392136
    Reviewed-by: Norbert Zsolt Bakó <norbert.bako@instructure.com>
    QA-Review: Norbert Zsolt Bakó <norbert.bako@instructure.com>
    Product-Review: Bence Árpási <bence.arpasi@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/__tests__/DiscussionPostToolbar2.test.jsx
+++ ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/__tests__/DiscussionPostToolbar2.test.jsx
@@ -225,7 +225,7 @@ describe('DiscussionPostToolbar', () => {
         const {getByText} = setup({isAnnouncement: true}, null, {
           translationLanguages: {current: ['en', 'es']},
         })
-        expect(getByText('Open Translate')).toBeTruthy()
+        expect(getByText('Enable Translation')).toBeTruthy()
       })
     })
 
@@ -243,7 +243,7 @@ describe('DiscussionPostToolbar', () => {
           translationLanguages: {current: ['en', 'es']},
         })
 
-        expect(getByText('Open Translate')).toBeTruthy()
+        expect(getByText('Enable Translation')).toBeTruthy()
       })
 
       it('does render the translate button with improved text when the translation controls are on', () => {
@@ -252,7 +252,7 @@ describe('DiscussionPostToolbar', () => {
           showTranslationControl: true,
         })
 
-        expect(getByText('Close Translate')).toBeTruthy()
+        expect(getByText('Disable Translation')).toBeTruthy()
         expect(getByTestId('translate-button')).toHaveAttribute(
           'data-action-state',
           'disableTranslation',
