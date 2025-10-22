4f4a496cf5364aef6b67a4e56da7ff1f24494f2b
theo sorriaux <theo.sorriaux@instructure.com>
2025-10-22 12:10:29 +0200

    Cleanup Sagemaker
    We're removing sagemaker as a translation source.
    This PS ensures there's no leftover in the codebase
    Sagemaker was our default fallback engine, we switch to cedar
    also, awstranslate is now able to die silently.
    
    flag=translation
    closes VICE-4924
    
    Test plan
    - Have only the translation ff on
    - try to translate a discussion
    - look at the langage select input
    - You might recognise cedar's list of langage
    
    - try to enable ai improvement ff
    - remove the credentials from your vault
    - rebase canvas
    - try to access discussion
    - discussion should work
    - translation should not available
    
    Change-Id: I6cd1bf5801f89678c8c3f52dede0b870ba134599
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/392293
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Balázs André <andre.balazs@instructure.com>
    Build-Review: James Butters <jbutters@instructure.com>
    QA-Review: Norbert Zsolt Bakó <norbert.bako@instructure.com>
    Product-Review: Theo Sorriaux <theo.sorriaux@instructure.com>

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -39,7 +39,6 @@ gem "aws-sdk-kinesis", "~> 1.45", require: false
 gem "aws-sdk-s3", "~> 1.119", require: false
 gem "aws-sdk-sns", "~> 1.60", require: false
 gem "aws-sdk-sqs", "~> 1.53", require: false
-gem "aws-sdk-sagemakerruntime", "~> 1.61", require: false
 gem "aws-sdk-translate", "~> 1.77", require: false
 gem "rqrcode", "~> 3.0", require: false
 gem "bcrypt", "~> 3.1"
@@ -50,7 +49,6 @@ gem "business_time", "0.13.0"
 gem "canvas_connect", "0.3.16"
 gem "canvas_link_migrator", "~> 1.0"
 gem "canvas_webex", "0.18.2"
-gem "cld", "~> 0.13"
 gem "code_ownership", "~> 1.33"
 gem "datadog", "~> 2.1", require: false
 gem "docx", "~> 0.8"
@@ -91,7 +89,6 @@ gem "outrigger", "~> 3.0"
 gem "parallel", "~> 1.23", require: false
 gem "pdf-reader", "~> 2.11"
 gem "pg_query", "~> 6.0", require: false
-gem "pragmatic_segmenter", "~> 0.3"
 gem "prawn-emoji", "~> 6.0", require: false
 gem "prawn-rails", "~> 1.4"
 gem "prosopite", "~> 2.1"
