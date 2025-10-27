6195ef38f89095ba7352149dfbcf97a8e30263a6
Jacob Burroughs <jburroughs@instructure.com>
2025-10-27 10:47:48 -0500

    Improve nontransaction linter for change_table
    fixes AE-2127
    
    Change-Id: Ie2c17e2fcb2009bf4f79863a36e394ca024e705d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/392606
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- gems/rubocop-canvas/lib/rubocop_canvas/cops/migration/non_transactional.rb
+++ gems/rubocop-canvas/lib/rubocop_canvas/cops/migration/non_transactional.rb
@@ -21,107 +21,153 @@ module RuboCop
   module Cop
     module Migration
       class NonTransactional < Base
+        include RuboCop::Canvas::CurrentDef
+        include RuboCop::Canvas::NonTransactional
+
+        def on_class(node)
+          @class_node = node
+          @non_transactional = non_transactional?(node)
+        end
+
+        RESTRICT_ON_SEND = %i[add_index add_column add_foreign_key add_reference create_table remove_foreign_key remove_index drop_table].freeze
+
         def on_send(node)
-          _receiver, method_name, *args = *node
+          _receiver, method_name = *node
 
           case method_name
-          when :disable_ddl_transaction!
-            @disable_ddl_transaction = true
           when :add_index
-            check_add_index(node, args)
-          when :add_column, :add_foreign_key
-            check_add_column(node, args)
+            check_add_index(node)
           when :add_reference
-            check_add_index(node, args)
-            check_reference_args(args)
-          when :create_table
-            check_create_table(node, args)
-          when :remove_foreign_key, :remove_index, :drop_table
-            check_remove_foreign_key(node, args)
+            check_add_reference(node)
+          when :add_column, :add_foreign_key, :create_table
+            check_if_not_exists(node)
+          when :remove_column, :remove_foreign_key, :remove_index, :drop_table
+            check_if_exists(node)
           end
         end
 
-        ALGORITHM = AST::Node.new(:sym, [:algorithm])
-        INDEX = AST::Node.new(:sym, [:index])
-        IF_NOT_EXISTS = AST::Node.new(:sym, [:if_not_exists])
-        IF_EXISTS = AST::Node.new(:sym, [:if_exists])
+        def_node_matcher :create_table, <<~PATTERN
+          (block (send nil? :create_table $_ ...) ...)
+        PATTERN
 
-        def check_create_table(node, args)
-          check_add_index(node, args)
+        def_node_matcher :change_table, <<~PATTERN
+          (block (send nil? :change_table $_ ...) ...)
+        PATTERN
 
-          return unless node.parent.is_a?(RuboCop::AST::BlockNode)
+        def_node_search :algorithm_concurrently?, <<~PATTERN
+          (pair (sym :algorithm) (sym :concurrently))
+        PATTERN
 
-          block_arg = node.parent.arguments.first
-          node.parent.body.each_child_node do |child|
-            next unless child.send_type? && child.receiver && child.receiver.children.first == block_arg.name
+        def_node_search :index_argument, <<~PATTERN
+          (pair (sym :index) $_)
+        PATTERN
 
-            case child.method_name
-            when :references
-              check_reference_args(child.arguments)
-            when :index
-              check_add_index(child, child.arguments)
-            end
+        def_node_matcher :false?, <<~PATTERN
+          (false)
+        PATTERN
+
+        def_node_search :if_not_exists?, <<~PATTERN
+          (pair (sym :if_not_exists) true)
+        PATTERN
+
+        def_node_search :if_exists?, <<~PATTERN
+          (pair (sym :if_exists) true)
+        PATTERN
+
+        def_node_search :ct_column, <<~PATTERN
+          $(send lvar {:column :foreign_key :bigint :binary :boolean :date :datetime :decimal :float :integer :json :string :text :time :timestamp :virtual :blob :numeric} ...)
+        PATTERN
+
+        def_node_search :ct_index, <<~PATTERN
+          $(send lvar :index ...)
+        PATTERN
+
+        def_node_search :ct_references, <<~PATTERN
+          $(send lvar :references ...)
+        PATTERN
+
+        def_node_search :ct_removal, <<~PATTERN
+          $(send lvar {:remove :remove_index} ...)
+        PATTERN
+
+        def_node_matcher :unless_exists_conditional?, <<~PATTERN
+          ^(if (send _ {:column_exists? :index_exists? :foreign_key_exists? :check_constraint_exists?} _) nil? _)
+        PATTERN
+
+        def_node_matcher :if_exists_conditional?, <<~PATTERN
+          ^(if (send _ {:column_exists? :index_exists? :foreign_key_exists? :check_constraint_exists?} _) _ nil?)
+        PATTERN
+
+        def on_block(node)
+          if create_table(node)
+            check_batch_table_ops(node)
+          elsif change_table(node)
+            @in_change_table = true
+            check_batch_table_ops(node)
+            @in_change_table = false
           end
         end
 
-        def check_reference_args(args)
-          options = args.last
-          return unless options&.hash_type?
-
-          options.each_pair do |key, value|
-            next unless key == INDEX
-            next if value.false_type?
-
-            check_add_index(value, [value])
+        def check_add_reference(node)
+          arg = index_argument(node).first
+          if arg.nil? || !false?(arg)
+            check_add_index(arg || node)
           end
         end
 
-        def check_add_index(node, args)
-          options = args.last
-          options = nil unless options&.hash_type?
-
-          algorithm = options&.children&.find do |pair|
-            pair.children.first == ALGORITHM
-          end
-          algorithm_name = algorithm&.children&.last&.children&.first
-
-          if algorithm_name == :concurrently && !@disable_ddl_transaction
+        def check_add_index(node)
+          if algorithm_concurrently?(node) && !@non_transactional
             add_offense(node,
                         message: "Concurrent index adds require `disable_ddl_transaction!`",
                         severity: :error)
           end
 
-          check_add_column(node, args)
+          check_if_not_exists(node)
         end
 
-        def check_add_column(node, args)
-          options = args.last
-          options = nil unless options&.hash_type?
-
-          if_not_exists = options&.children&.find do |pair|
-            pair.children.first == IF_NOT_EXISTS
-          end
-          value = if_not_exists&.children&.last&.type
-          if @disable_ddl_transaction && value != :true # rubocop:disable Lint/BooleanSymbol
+        def check_if_not_exists(node)
+          if @in_change_table
+            if if_not_exists?(node)
+              add_offense(node,
+                          message: "Inside batch table operations, use `unless t.column_exists?(:name)` not `if_not_exists: true`",
+                          severity: :error)
+            end
+            unless unless_exists_conditional?(node)
+              add_offense(node,
+                          message: "Non-transactional migrations should be idempotent; add `unless t.column_exists?(:name)` or equivalent",
+                          severity: :error)
+            end
+          elsif @non_transactional && !if_not_exists?(node)
             add_offense(node,
                         message: "Non-transactional migrations should be idempotent; add `if_not_exists: true`",
                         severity: :error)
           end
         end
 
-        def check_remove_foreign_key(node, args)
-          options = args.last
-          options = nil unless options.hash_type?
-
-          if_not_exists = options&.children&.find do |pair|
-            pair.children.first == IF_EXISTS
-          end
-          value = if_not_exists&.children&.last&.type
-          if @disable_ddl_transaction && value != :true # rubocop:disable Lint/BooleanSymbol
+        def check_if_exists(node)
+          if @in_change_table
+            if if_exists?(node)
+              add_offense(node,
+                          message: "Inside batch table operations, use `if t.column_exists?(:name)` not `if_exists: true`",
+                          severity: :error)
+            end
+            unless if_exists_conditional?(node)
+              add_offense(node,
+                          message: "Non-transactional migrations should be idempotent; add `if t.column_exists?(:name)` or equivalent",
+                          severity: :error)
+            end
+          elsif @non_transactional && !if_exists?(node)
             add_offense(node,
                         message: "Non-transactional migrations should be idempotent; add `if_exists: true`",
                         severity: :error)
           end
+        end
+
+        def check_batch_table_ops(node)
+          ct_index(node).each { |subnode| check_add_index(subnode) }
+          ct_references(node).each { |subnode| check_add_reference(subnode) }
+          ct_column(node).each { |subnode| check_if_not_exists(subnode) }
+          ct_removal(node).each { |subnode| check_if_exists(subnode) }
         end
       end
     end
