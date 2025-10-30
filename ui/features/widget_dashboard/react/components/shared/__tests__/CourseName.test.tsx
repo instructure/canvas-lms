e162c4e33120b1b152edf74bc4b5967f52aba4c5
Felipe Leon <felipe.leon@instructure.com>
2025-10-30 10:35:34 -0500

    Fix: Text on Course grades widget not truncated properly
    On smaller screen sizes, the text elements in the Course
    Grade widget does not truncate. Instead, a horizontal
    scrollbar appears within course name, and the course
    grade text is partially cut off.
    
    Test plan:
    
    1. Create a course with a long course name: longer than 300
       characters.
    2. Add a student to the course
    3. Act as the student added
    	Verify: In Dashboard "Course grades" section The name
    		of the course is truncated.
    		The horizontal scrollbar is not shown
    		The text is not cutted off
    		When the course name is hover a Tooltip showing
    		the complete name is displayed.
    
    4. With key navigation go to the course code name.
    	Verify: The tooltip with the complete course name is shown.
    
    flag= widget_dashboard
    Closes: LX-3422
    
    Change-Id: I9e38ce1e23938f8370be546c848a36214f6d49b5
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/392925
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: June Eoh <jungsun.eoh@instructure.com>
    QA-Review: June Eoh <jungsun.eoh@instructure.com>
    Product-Review: Felipe Leon <felipe.leon@instructure.com>

---
+++ ui/features/widget_dashboard/react/components/shared/__tests__/CourseName.test.tsx
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {CourseName} from '../CourseName'
+
+// Mock TruncateText to simulate truncation behavior in tests
+jest.mock('@instructure/ui-truncate-text', () => ({
+  TruncateText: ({
+    children,
+    onUpdate,
+    maxLines,
+  }: {
+    children: string
+    onUpdate?: (truncated: boolean) => void
+    maxLines?: number
+  }) => {
+    React.useEffect(() => {
+      if (onUpdate && children && children.length > 100) {
+        onUpdate(true)
+      }
+    }, [children, onUpdate])
+
+    const displayText =
+      children && children.length > 100 ? `${children.substring(0, 70)}...` : children
+
+    return displayText
+  },
+}))
+
+describe('CourseName', () => {
+  describe('Component rendering', () => {
+    it('should render with tooltip for long course name', async () => {
+      const longName =
+        'This is a Very Long Course Name That Should Be Truncated Because It Exceeds The Maximum Number of Characters Allowed for Two Lines'
+      const truncatedName =
+        'This is a Very Long Course Name That Should Be Truncated Because It Ex...'
+
+      const user = userEvent.setup()
+
+      render(<CourseName courseName={longName} />)
+
+      // Verify truncated text is displayed
+      expect(screen.getByText(truncatedName)).toBeInTheDocument()
+
+      // The tooltip wrapper should be focusable and have tabIndex 0
+      const truncatedTextElement = screen.getByText(truncatedName)
+      const tooltipWrapper = truncatedTextElement.closest('[tabindex="0"]')
+      expect(tooltipWrapper).toBeInTheDocument()
+      expect(tooltipWrapper).toHaveAttribute('tabIndex', '0')
+
+      // Hover over the tooltip wrapper to trigger the tooltip
+      if (tooltipWrapper) {
+        await user.hover(tooltipWrapper)
+      }
+
+      // After hovering, the full name should appear in the tooltip
+      // We need to find the tooltip specifically (not the ScreenReaderContent)
+      const elementList = await screen.findAllByText(longName)
+
+      // The tooltip will be inside an element with an id starting with 'Tooltip___'
+      const tooltip = elementList.find(element => {
+        const tooltipContainer = element.closest('[id^="Tooltip___"]')
+        return tooltipContainer !== null
+      })
+      expect(tooltip).toBeInTheDocument()
+    })
+
+    it('should not render with tooltip for normal course name length', () => {
+      const shortName = 'Introduction to Programming'
+      render(<CourseName courseName={shortName} />)
+
+      expect(screen.getByText(shortName)).toBeInTheDocument()
+
+      // Check that there's no focusable wrapper (should not find element with tabindex="0")
+      const textElement = screen.getByText(shortName)
+      const tooltipWrapper = textElement.closest('[tabindex="0"]')
+      expect(tooltipWrapper).not.toBeInTheDocument()
+    })
+  })
+})
