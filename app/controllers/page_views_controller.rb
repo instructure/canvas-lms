644f12e0b66aa70c24d8a90330c62fd3213a2587
eva.tapai <eva.tapai@instructure.com>
2025-11-07 09:32:13 +0100

    Add @hint YARD tag for API doc hint blocks
    Implements custom @hint YARD tag to display styled hint and
    disclaimer blocks in API documentation. Supports multiple
    styles (info, warning, danger, success) and generates
    appropriate output for both HTML and markdown formats.
    
    Changes:
    - Define @hint YARD tag in doc/api/api_routes.rb
    - Add hint_section handler in doc/api/tags/setup.rb
    - Create format-specific markdown setup following existing
      pattern (doc/api/tags/markdown/setup.rb)
    - Add hint.erb templates for both html and markdown
    - Add CSS styling for HTML hint message boxes
    - Update PageViewsController to use @hint for disclaimer
    
    Test Plan:
      - Run rake doc:api (HTML generation)
      - Run OUTPUT_FORMAT=markdown rake doc:api
      - Verify hint blocks render with correct styling in HTML
      - Verify GitBook syntax {% hint style="..." %} in markdown
      - Check PageViewsController disclaimer displays correctly
    
    refs SAS-4589
    
    flag = none
    
    Change-Id: I6e84476d54d18667132701c0eed0a081fcc80aaf
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/393152
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: David Varga <d.varga@instructure.com>
    QA-Review: David Varga <d.varga@instructure.com>
    Product-Review: Eva Tapai K. <eva.tapai@instructure.com>

--- app/controllers/page_views_controller.rb
+++ app/controllers/page_views_controller.rb
@@ -258,16 +258,17 @@ class PageViewsController < ApplicationController
   # similar to the available CSV download. Page views are returned in
   # descending order, newest to oldest.
   #
-  # **Disclaimer**: The data is a best effort attempt, and is not guaranteed
-  # to be complete or wholly accurate. This data is meant to be used for
-  # rollups and analysis in the aggregate, not in isolation for auditing,
-  # or other high-stakes analysis involving examining single users or
-  # small samples. Page Views data is generated from the Canvas logs files,
-  # not a transactional database, there are many places along the way
-  # data can be lost and/or duplicated (though uncommon). Additionally,
-  # given the size of this data, our processes ensure that errors can be
-  # rectified at any point in time, with corrections integrated as soon as
-  # they are identified and processed.
+  # @hint info
+  #   **Disclaimer**: The data is a best effort attempt, and is not guaranteed
+  #   to be complete or wholly accurate. This data is meant to be used for
+  #   rollups and analysis in the aggregate, not in isolation for auditing,
+  #   or other high-stakes analysis involving examining single users or
+  #   small samples. Page Views data is generated from the Canvas logs files,
+  #   not a transactional database, there are many places along the way
+  #   data can be lost and/or duplicated (though uncommon). Additionally,
+  #   given the size of this data, our processes ensure that errors can be
+  #   rectified at any point in time, with corrections integrated as soon as
+  #   they are identified and processed.
   #
   # @argument start_time [DateTime]
   #   The beginning of the time range from which you want page views.
