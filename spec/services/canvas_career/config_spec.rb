e086fa8345db637b45cfc44ce31c0dcd02eccfb7
Jackson Howe <jackson.howe@instructure.com>
2025-11-10 11:24:24 -0700

    Pass experience_preferences to horizon MF
    flag = none
    refs CLX-3291
    
    Test plan:
     - Open Canvas Career and run `window.REMOTES.canvas_career_config`
     - Expect skillspace and learner_assist to be true
     - In a rails console, find the root account and run
       `account.settings[:horizon_academic_mode] = true; account.save!`
     - Expect skillspace and learner_assist to be false
    
    Change-Id: I216903e5eddb14dc0847ee8856319467200beba8
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/393800
    Reviewed-by: Robin Kuss <rkuss@instructure.com>
    QA-Review: Robin Kuss <rkuss@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Jackson Howe <jackson.howe@instructure.com>

---
+++ spec/services/canvas_career/config_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module CanvasCareer
+  describe Config do
+    let(:root_account) { Account.default }
+    let(:request) { double("request", base_url: "https://canvascareer.instructure.com") }
+    let(:dynamic_settings_yaml) do
+      {
+        "public_app_config" => {
+          "hosts" => {}
+        }
+      }.to_yaml
+    end
+
+    before do
+      dynamic_settings = double("DynamicSettings")
+      allow(DynamicSettings).to receive(:find).and_call_original
+      allow(DynamicSettings).to receive(:find).with(tree: :private).and_return(dynamic_settings)
+      allow(dynamic_settings).to receive(:[]).and_return(nil)
+      allow(dynamic_settings).to receive(:[]).with("canvas_career.yml", failsafe: nil).and_return(dynamic_settings_yaml)
+    end
+
+    describe "public_app_config" do
+      context "when horizon_academic_mode is not set" do
+        it "returns experience preferences with skillspace and learner_assist enabled" do
+          config = Config.new(root_account)
+          result = config.public_app_config(request)
+
+          expect(result["hosts"]["canvas"]).to eq("https://canvascareer.instructure.com")
+          expect(result["experience_preferences"]).to eq({
+                                                           features: {
+                                                             notebook: true,
+                                                             skillspace: true,
+                                                             learner_assist: true,
+                                                           }
+                                                         })
+        end
+      end
+
+      context "when horizon_academic_mode is true" do
+        it "returns experience preferences with rubrics enabled" do
+          root_account.settings[:horizon_academic_mode] = true
+          root_account.save!
+
+          config = Config.new(root_account)
+          result = config.public_app_config(request)
+
+          expect(result["hosts"]["canvas"]).to eq("https://canvascareer.instructure.com")
+          expect(result["experience_preferences"]).to eq({
+                                                           features: {
+                                                             notebook: true,
+                                                             skillspace: false,
+                                                             learner_assist: false,
+                                                           }
+                                                         })
+        end
+      end
+    end
+  end
+end
