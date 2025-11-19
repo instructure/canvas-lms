cff0e018a9f2ee122b7a5a4edc25d9e2ca5b4f34
Adam Molnar <adam.molnar@instructure.com>
2025-11-19 11:57:28 +0100

    create FF for Career Chart view
    closes CLXB-293
    flag=none
    test plan:
    - new FF appears in the root acc. settings
    - new FF is being sent to Canvas Career
    
    Change-Id: I7db68d68f30b1480fcf03601b6c1b767a8500333
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/394592
    Reviewed-by: Ildiko Gera <ildiko.gera@instructure.com>
    QA-Review: Ildiko Gera <ildiko.gera@instructure.com>
    Product-Review: Ildiko Gera <ildiko.gera@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -65,6 +65,7 @@ class CareerController < ApplicationController
       horizon_course_navigation
       horizon_course_redesign
       horizon_course_index_page
+      horizon_chart_view
     ].index_with { |feature| @domain_root_account.feature_enabled?(feature) }
   end
 end
