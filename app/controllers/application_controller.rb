5ddec9a9443ced8d1b86f4d266b033830b8e04fb
jorge.andres <jorge.andres@instructure.com>
2025-11-19 19:39:21 -0600

    Fix Ignite Agent injection in preview and oauth mode
    flag=ignite_agent_enabled
    Refs: AGN-184
    
    test plan:
    - Open the agent, the oauth flow should start
    - There should be no agent loading spinner in the
    "authorize" pop-up
    - Navigate to a course with a submitted assignment
    - Open SpeedGrader for that assignment
    - Confirm the submission is displayed in preview mode
    - Observe the UI, only one Agent button is injected
    
    Change-Id: I9ea9b2cd8a10947a8caff1b25e6f2624cee63362
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/394681
    Reviewed-by: Botond Tóth <botond.toth@instructure.com>
    QA-Review: Csaba Balla-Somogyi <csaba.ballasomogyi@instructure.com>
    Product-Review: Csaba Balla-Somogyi <csaba.ballasomogyi@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3561,6 +3561,8 @@ class ApplicationController < ActionController::Base
   end
 
   def add_ignite_agent_bundle?
+    return false if params[:preview] == "true"
+    return false if controller_name == "oauth2_provider"
     return false unless @domain_root_account&.feature_enabled?(:ignite_agent_enabled)
     return true if @domain_root_account&.grants_right?(@current_user, session, :manage_account_settings)
     return false unless @current_user&.feature_enabled?(:ignite_agent_enabled_for_user)
