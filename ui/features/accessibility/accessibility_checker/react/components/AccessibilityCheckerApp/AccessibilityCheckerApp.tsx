dbafd3e88b64d473e334c148990f75a42ee98da6
kadir.saglam <kadir.saglam@instructure.com>
2025-11-21 12:24:12 +0100

    fix: a11y checker filter issues
    - clear filters button works as intented
    - date filtering is corrected
    - archived state removed from filters
    - search term matches only the resource name
    
    flag=a11y_checker
    closes RCX-4421
    
    test plan:
    - Clear filter button clears the filters and data is refreshed
    - To date filter should be inclusive
      * If the resource last edit date is 20 Oct 2025, when you set To Date filter to this date it should appear in the results
    - Archived state is not visible in filters
    - Search term does partial matching to the resource name
    
    Change-Id: I989ada5ae19c43e25f1b8bc177cf2fbf992c33ab
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/394797
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Zsolt Mester <zsolt.mester@instructure.com>
    Reviewed-by: Zsolt Mester <zsolt.mester@instructure.com>
    Product-Review: Kadir Saglam <kadir.saglam@instructure.com>

--- ui/features/accessibility/accessibility_checker/react/components/AccessibilityCheckerApp/AccessibilityCheckerApp.tsx
+++ ui/features/accessibility/accessibility_checker/react/components/AccessibilityCheckerApp/AccessibilityCheckerApp.tsx
@@ -16,7 +16,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {useCallback, useMemo, useEffect} from 'react'
+import {useCallback, useMemo, useEffect, useRef} from 'react'
 import {useShallow} from 'zustand/react/shallow'
 import {View} from '@instructure/ui-view'
 
@@ -48,17 +48,24 @@ export const AccessibilityCheckerApp: React.FC = () => {
   const appliedFilters = useMemo(() => getAppliedFilters(filters || {}), [filters])
 
   const accessibilityScanDisabled = window.ENV.SCAN_DISABLED
+  const hasInitializedFilters = useRef(false)
 
   useEffect(() => {
     const parsedFetchParams = parseFetchParams()
     if (parsedFetchParams.filters && !filters) {
       setFilters(parsedFetchParams.filters as Filters)
+    } else {
+      hasInitializedFilters.current = true
     }
   }, [])
 
   useDeepCompareEffect(() => {
     const fetchParams = parseFetchParams()
-    if (fetchParams.filters && !filters) return // wait for filters to be set from query params
+    if (fetchParams.filters && !filters && !hasInitializedFilters.current) {
+      return // wait for filters to be set from query params on initial load
+    }
+
+    hasInitializedFilters.current = true
 
     if (!accessibilityScanDisabled) {
       const parsedFetchParams = {...fetchParams, filters, page: 1}
