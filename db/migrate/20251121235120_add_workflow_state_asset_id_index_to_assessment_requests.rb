e5b57ee049a2b8a9c4749e2e55b922ce7a42f65f
Chrystal Langston <chrystal.langston@instructure.com>
2025-11-21 19:57:40 -0500

    Add composite index to AssessmentRequest
    This PS is 2 of 3 for EGG-1859.  This PS adds an index that will
    allow the queries introduced in PS1 to be more performant.  See
    comment on ticket for query plan and summary of analysis
    conducted.
    
    refs EGG-1859
    
    flag=peer_review_allocation_and_grading
    
    Test plan:
    - run migration
    docker compose run --rm web bin/rails db:migrate
    - assert index is created
    - reverse the migration by running
    docker-compose run --rm web bash
      bundle exec rake db:migrate:down VERSION=20251121235120
    - assert the index is removed
    
    Change-Id: I3c887dafd8040dbbd6a5cd675c43bb45a90bbac3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/394872
    Migration-Review: Cody Cutrer <cody@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Mark Starkman <mark.starkman@instructure.com>
    QA-Review: Mark Starkman <mark.starkman@instructure.com>
    Product-Review: Jason Gillett <jason.gillett@instructure.com>

---
+++ db/migrate/20251121235120_add_workflow_state_asset_id_index_to_assessment_requests.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2025 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+class AddWorkflowStateAssetIdIndexToAssessmentRequests < ActiveRecord::Migration[8.0]
+  tag :predeploy
+  disable_ddl_transaction!
+
+  def change
+    add_index :assessment_requests,
+              [:workflow_state, :asset_id],
+              name: "index_assessment_requests_on_workflow_state_and_asset_id",
+              algorithm: :concurrently,
+              if_not_exists: true
+  end
+end
