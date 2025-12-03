593a1f6953495bab2257423ffc4fdb17588271aa
Martin Yosifov <martin.yosifov@instructure.com>
2025-12-03 12:26:54 -0800

    Create Peer Review Grading View in SpeedGrader2
    This is PS 1 of 2 for EGG-1678. It exposes the
    Peer Review Allocation and Grading feature flag
    to SpeedGrader2 via the ENV variable.
    
    closes EGG-1678
    flag=peer_review_allocation_and_grading
    
    Test plan:
    - Create a course and enable the "Peer Review
    Allocation and Grading" and "Performance and
    Usability Upgrades for SpeedGrader" feature flags
    - Create an assignment and go to SpeedGrader
    - Open devtools -> console and verify that
    ENV.PEER_REVIEW_ALLOCATION_AND_GRADING_ENABLED is true
    - Disable the Peer Review Allocation and
    Grading feature flag and verify that
    ENV.PEER_REVIEW_ALLOCATION_AND_GRADING_ENABLED is false
    
    Change-Id: Id18416a50f4754fb16ff950cdcd42c106e960014
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/395641
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: Martin Yosifov <martin.yosifov@instructure.com>

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -1114,6 +1114,7 @@ class GradebooksController < ApplicationController
         MULTISELECT_FILTERS_ENABLED: multiselect_filters_enabled?,
         gradebook_section_filter_id: filtered_sections,
         COMMENT_BANK_PER_ASSIGNMENT_ENABLED: Account.site_admin.feature_enabled?(:comment_bank_per_assignment),
+        PEER_REVIEW_ALLOCATION_AND_GRADING_ENABLED: @context.feature_enabled?(:peer_review_allocation_and_grading),
         show_inactive_enrollments: gradebook_settings(@context.global_id)&.[]("show_inactive_enrollments") == "true",
         show_concluded_enrollments: @context.completed? || gradebook_settings(@context.global_id)&.[]("show_concluded_enrollments") == "true",
       }
