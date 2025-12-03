be6717448ab5b3fd2855c6676d5e34905fdaa212
Spencer Olson <solson@instructure.com>
2025-12-03 16:14:46 -0600

    Revert "support a list of db passwords"
    This reverts commit dde300bfd52e5295b7a479d040b09d565fdfa3a9.
    
    Change-Id: I909c2c78785dc53fffdeeac8913beb13d1feb3bc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/395655
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- spec/lib/postgresql_adapter_spec.rb
+++ spec/lib/postgresql_adapter_spec.rb
@@ -62,17 +62,4 @@ describe ActiveRecord::ConnectionAdapters::PostgreSQLAdapter do
   ensure
     conn&.disconnect!
   end
-
-  it "can handle an array of passwords, falling back to a successful connection" do
-    db_config = ActiveRecord::Base.connection_pool.db_config
-    config = db_config.configuration_hash.dup
-    # password might be nil on purpose so can't use Array(), but it might also be an array,
-    # so we need to flatten
-    config[:password] = (["wrong_password"] + [config[:password]]).flatten
-    db_config = ActiveRecord::DatabaseConfigurations::HashConfig.new(db_config.env_name, db_config.name, config)
-    conn = db_config.new_connection
-    conn.execute("SELECT 1")
-  ensure
-    conn&.disconnect!
-  end
 end
