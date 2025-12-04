2fdab62fb785defd065d14182984772cc0241e7f
balazs.buri <balazs.buri@instructure.com>
2025-12-04 09:22:14 +0100

    add Contributing Score Header
    refs: OUTC-390
    
    flag=improved_lmgb
    
    Test Plan:
    - Have a course with data for the improved LMGB
      - an assignment with associated outcome
      - a student took the assignment
      - The attempt has been graded and outcome
         score has been given
    - Go to ILMGB
    - Open the header menu of the outcome and
       click "Show contributing scores"
    - Open the contributing score column's header
       menu and click "Open in Speedgrader"
    - It navigates to speedgrader to the correct
       assignment
       - Assert that the name in the contributing score
          header matches the name shown in
          speedgrader
    
    Change-Id: I71a01077b05d0be93fdfb6b994fc4db662402bca
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/395671
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Peipei Zhou <peipei.zhou@instructure.com>
    QA-Review: Peipei Zhou <peipei.zhou@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>

---
+++ ui/features/learning_mastery_v2/react/components/grid/__tests__/ContributingScoreHeader.test.tsx
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {ContributingScoreHeader, ContributingScoreHeaderProps} from '../ContributingScoreHeader'
+import {ContributingScoreAlignment} from '../../../hooks/useContributingScores'
+
+const mockOpenWindow = jest.fn()
+jest.mock('@canvas/util/globalUtils', () => ({
+  ...jest.requireActual('@canvas/util/globalUtils'),
+  openWindow: (url: string, target?: string) => mockOpenWindow(url, target),
+}))
+
+describe('ContributingScoreHeader', () => {
+  const mockAlignment: ContributingScoreAlignment = {
+    alignment_id: '1',
+    associated_asset_id: '100',
+    associated_asset_name: 'Assignment 1',
+    associated_asset_type: 'assignment',
+    html_url: '/courses/1/assignments/100',
+  }
+
+  const defaultProps = (): ContributingScoreHeaderProps => {
+    return {
+      alignment: mockAlignment,
+      courseId: '1',
+    }
+  }
+
+  beforeEach(() => {
+    mockOpenWindow.mockClear()
+  })
+
+  it('renders the alignment name', () => {
+    const {getByText} = render(<ContributingScoreHeader {...defaultProps()} />)
+    expect(getByText('Assignment 1')).toBeInTheDocument()
+  })
+
+  it('renders a menu with "Open in Speedgrader" option', async () => {
+    const user = userEvent.setup({pointerEventsCheck: 0})
+    const {getByText} = render(<ContributingScoreHeader {...defaultProps()} />)
+    await user.click(getByText('Contributing Score Menu'))
+    expect(getByText('Open in Speedgrader')).toBeInTheDocument()
+  })
+
+  it('opens speedgrader in a new tab when "Open in Speedgrader" is clicked', async () => {
+    const user = userEvent.setup({pointerEventsCheck: 0})
+    const {getByText} = render(<ContributingScoreHeader {...defaultProps()} />)
+    await user.click(getByText('Contributing Score Menu'))
+    await user.click(getByText('Open in Speedgrader'))
+    expect(mockOpenWindow).toHaveBeenCalledWith(
+      '/courses/1/gradebook/speed_grader?assignment_id=100',
+      '_blank',
+    )
+  })
+
+  it('constructs the correct speedgrader URL with courseId and assignment_id', async () => {
+    const user = userEvent.setup({pointerEventsCheck: 0})
+    const props: ContributingScoreHeaderProps = {
+      alignment: {
+        ...mockAlignment,
+        associated_asset_id: '250',
+      },
+      courseId: '42',
+    }
+    const {getByText} = render(<ContributingScoreHeader {...props} />)
+    await user.click(getByText('Contributing Score Menu'))
+    await user.click(getByText('Open in Speedgrader'))
+    expect(mockOpenWindow).toHaveBeenCalledWith(
+      '/courses/42/gradebook/speed_grader?assignment_id=250',
+      '_blank',
+    )
+  })
+})
