511da20453a8bc1d05e483a0ce7b4d46aa617992
Sarah Gerard <sarah.gerard@instructure.com>
2025-12-05 13:35:28 -0800

    Honor course/section end dates for course pacing
    this ensure that when course_pace_time_selection is on,
    course and section end dates are still respected in the
    UI. Due dates cannot be past the course end date, and this
    is consistent with the overrides created by course pacing.
    This updates the end date display to mimic the start date
    display when there is a course/section end date (warning
    but able to edit for default/section paces, read only
    for student paces)
    
    refs LX-3522
    flag=course_pace_time_selection
    
    test plan:
    - have course_pace_time_selection flag on
    - without the change, create a course with course pacing
      enabled and multiple assignments in that pace
    - have a studen in the course
    - add number of days to complete in the course pace
    - add an end date to the course in the near future
    - notice that due dates for the later assignments will extend
      past the course end date
    - with the change, ensure that the assignment due dates are
      compressed to be before the course end date
    - ensure in the default/section paces, the end date is still
      editable but shows the same warning text as the start date
    - in the student pace, the end date and pace length should be
      read only.
    - ensure if you remove the course end date from course settings,
      then the end date for all paces is editable
    
    Change-Id: I57b8ce086f396b3df0c83d7918e5c8aafb9152ef
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/395893
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Eric Saupe <eric.saupe@instructure.com>
    QA-Review: Eric Saupe <eric.saupe@instructure.com>
    Product-Review: Sarah Gerard <sarah.gerard@instructure.com>

--- ui/features/course_paces/react/components/pace_modal/__tests__/time_selection.test.tsx
+++ ui/features/course_paces/react/components/pace_modal/__tests__/time_selection.test.tsx
@@ -24,6 +24,7 @@ import TimeSelection from '../TimeSelection'
 import {PRIMARY_PACE, STUDENT_PACE} from '../../../__tests__/fixtures'
 import {CoursePace, Pace} from 'features/course_paces/react/types'
 import keycode from 'keycode'
+import fakeENV from '@canvas/test-utils/fakeENV'
 
 const responsiveSize = 'small'
 
@@ -38,9 +39,22 @@ const coursePace: CoursePace = {
   ...PRIMARY_PACE,
   start_date: '2021-09-01',
   time_to_complete_calendar_days: 90,
+  end_date_context: 'hypothetical',
 }
 
 describe('Pace Modal TimeSelection', () => {
+  beforeEach(() => {
+    fakeENV.setup({
+      FEATURES: {
+        course_pace_time_selection: true,
+      },
+    })
+  })
+
+  afterEach(() => {
+    fakeENV.teardown()
+  })
+
   it('displays the correct start date', () => {
     renderConnected(
       <TimeSelection
@@ -183,5 +197,281 @@ describe('Pace Modal TimeSelection', () => {
     const startDateLabel = queryByTestId('start-date-label')
 
     expect(startDateLabel).not.toBeInTheDocument()
+  })
+
+  describe('End Date Behavior', () => {
+    it('displays end date from course/section for enrollment pace', () => {
+      const enrollmentPaceWithCourseEnd: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'course',
+      }
+
+      renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceWithCourseEnd}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      expect(screen.getByTestId('end-date-readonly')).toHaveTextContent('November 15, 2021')
+    })
+
+    it('displays end date caption "Determined by course end date" for enrollment pace with course end date', () => {
+      const enrollmentPaceWithCourseEnd: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'course',
+      }
+      const appliedCourse = {...appliedPace, type: 'Course'}
+
+      const {container} = renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceWithCourseEnd}
+          appliedPace={appliedCourse as Pace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      expect(container).toHaveTextContent('Determined by course end date')
+    })
+
+    it('displays end date caption "Determined by section end date" for enrollment pace with section end date', () => {
+      const enrollmentPaceWithSectionEnd: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'term',
+      }
+      const appliedSection = {...appliedPace, type: 'Section'}
+
+      const {container} = renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceWithSectionEnd}
+          appliedPace={appliedSection as Pace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      expect(container).toHaveTextContent('Determined by section end date')
+    })
+
+    it('shows warning caption for course pace with course end date', () => {
+      const coursePaceWithEnd: CoursePace = {
+        ...PRIMARY_PACE,
+        end_date: '2021-12-15',
+        end_date_context: 'course',
+      }
+
+      const {container} = renderConnected(
+        <TimeSelection
+          coursePace={coursePaceWithEnd}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      expect(container).toHaveTextContent(
+        'Determined by course end date. Changing this date will not save, but you can view the effects of a new end date by changing the date here.',
+      )
+    })
+
+    it('shows warning caption for section pace with section end date', () => {
+      const sectionPaceWithEnd: CoursePace = {
+        ...PRIMARY_PACE,
+        context_type: 'Section',
+        end_date: '2021-12-15',
+        end_date_context: 'term',
+      }
+
+      const {container} = renderConnected(
+        <TimeSelection
+          coursePace={sectionPaceWithEnd}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      expect(container).toHaveTextContent(
+        'Determined by section end date. Changing this date will not save, but you can view the effects of a new end date by changing the date here.',
+      )
+    })
+
+    it('End date input is not available for Student Pace', () => {
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={STUDENT_PACE}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const endDateInput = queryByTestId('end-date-input')
+
+      expect(endDateInput).not.toBeInTheDocument()
+    })
+
+    it('End date readonly label is shown for Student Pace', () => {
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={STUDENT_PACE}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const endDateReadonly = queryByTestId('end-date-readonly')
+
+      expect(endDateReadonly).toBeInTheDocument()
+    })
+
+    it('displays editable end date for course/section paces', () => {
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={coursePace}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const endDateInput = queryByTestId('end-date-input')
+
+      expect(endDateInput).toBeInTheDocument()
+    })
+  })
+
+  describe('Time to Complete Behavior', () => {
+    it('displays read-only time to complete for enrollment pace with course end date', () => {
+      const enrollmentPaceWithCourseEnd: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'course',
+      }
+
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceWithCourseEnd}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const timeToCompleteReadonly = queryByTestId('time-to-complete-readonly')
+      const weeksInput = queryByTestId('weeks-number-input')
+
+      expect(timeToCompleteReadonly).toBeInTheDocument()
+      expect(weeksInput).not.toBeInTheDocument()
+    })
+
+    it('displays read-only time to complete for enrollment pace with section end date', () => {
+      const enrollmentPaceWithSectionEnd: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'term',
+      }
+
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceWithSectionEnd}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const timeToCompleteReadonly = queryByTestId('time-to-complete-readonly')
+      const weeksInput = queryByTestId('weeks-number-input')
+
+      expect(timeToCompleteReadonly).toBeInTheDocument()
+      expect(weeksInput).not.toBeInTheDocument()
+    })
+
+    it('displays editable time to complete for enrollment pace with hypothetical end date', () => {
+      const enrollmentPaceHypothetical: CoursePace = {
+        ...STUDENT_PACE,
+        end_date: '2021-11-15',
+        end_date_context: 'hypothetical',
+      }
+
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={enrollmentPaceHypothetical}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const timeToCompleteReadonly = queryByTestId('time-to-complete-readonly')
+      const weeksInput = queryByTestId('weeks-number-input')
+
+      expect(timeToCompleteReadonly).not.toBeInTheDocument()
+      expect(weeksInput).toBeInTheDocument()
+    })
+
+    it('displays editable time to complete for course/section paces', () => {
+      const {queryByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={coursePace}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const weeksInput = queryByTestId('weeks-number-input')
+      const daysInput = queryByTestId('days-number-input')
+
+      expect(weeksInput).toBeInTheDocument()
+      expect(daysInput).toBeInTheDocument()
+    })
+
+    it('calculates compressed time to complete when course end date is earlier than projected end', () => {
+      // Set up a pace where assignments would project past the course end date
+      const compressedPace: CoursePace = {
+        ...coursePace,
+        start_date: '2021-09-01',
+        end_date: '2021-10-15', // Course ends earlier
+        end_date_context: 'course',
+        time_to_complete_calendar_days: 90, // Would extend to ~November 30 without compression
+      }
+
+      const {getByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={compressedPace}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const weeksInput = getByTestId('weeks-number-input')
+      const daysInput = getByTestId('days-number-input')
+
+      // Should show compressed time (Sept 1 to Oct 15 = ~6 weeks, 2 days)
+      expect(weeksInput).toHaveValue(6)
+      expect(daysInput).toHaveValue(2)
+    })
+
+    it('calculates uncompressed time to complete when course end date is later than projected end', () => {
+      const uncompressedPace: CoursePace = {
+        ...coursePace,
+        start_date: '2021-09-01',
+        end_date: '2021-12-31', // Course ends later
+        end_date_context: 'course',
+        time_to_complete_calendar_days: 30, // Would end ~October 1
+      }
+
+      const {getByTestId} = renderConnected(
+        <TimeSelection
+          coursePace={uncompressedPace}
+          appliedPace={appliedPace}
+          responsiveSize={responsiveSize}
+        />,
+      )
+
+      const weeksInput = getByTestId('weeks-number-input')
+      const daysInput = getByTestId('days-number-input')
+
+      // Should show uncompressed time based on assignments (30 days = 4 weeks, 2 days)
+      expect(weeksInput).toHaveValue(4)
+      expect(daysInput).toHaveValue(2)
+    })
   })
 })
