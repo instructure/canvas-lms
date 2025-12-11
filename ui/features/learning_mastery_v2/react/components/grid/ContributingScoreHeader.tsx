d4f23c9e92b2b5a8f5f4e123b25ad244701ebfd7
Peipei Zhou <peipei.zhou@instructure.com>
2025-12-11 10:58:35 +0100

    update column header button's icon
    closes OUTC-468
    flag=improved_lmgb
    
    Test plan:
    - go to lmgb
    - check student column header, outcomes column header,
      contributing score column header
    - the header button's icon should be a vertical 3-dots
    
    Change-Id: Ie4d97c5c952502e516a0082be680fd0a43378bda
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/396321
    Reviewed-by: Szvetlin Tanyi <szvetlin.tanyi@instructure.com>
    QA-Review: Szvetlin Tanyi <szvetlin.tanyi@instructure.com>
    Product-Review: Sam Garza <sam.garza@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- ui/features/learning_mastery_v2/react/components/grid/ContributingScoreHeader.tsx
+++ ui/features/learning_mastery_v2/react/components/grid/ContributingScoreHeader.tsx
@@ -16,18 +16,15 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 import React from 'react'
-import {Text} from '@instructure/ui-text'
 import {Flex} from '@instructure/ui-flex'
-import {IconButton} from '@instructure/ui-buttons'
-import {IconArrowOpenDownLine, IconArrowUpLine, IconArrowDownLine} from '@instructure/ui-icons'
+import {IconArrowUpLine, IconArrowDownLine} from '@instructure/ui-icons'
 import {Menu} from '@instructure/ui-menu'
-import {View} from '@instructure/ui-view'
-import {TruncateText} from '@instructure/ui-truncate-text'
 import {useScope as createI18nScope} from '@canvas/i18n'
 import {openWindow} from '@canvas/util/globalUtils'
-import {CELL_HEIGHT, COLUMN_WIDTH, SortBy, SortOrder} from '../../utils/constants'
+import {SortBy, SortOrder} from '../../utils/constants'
 import {ContributingScoreAlignment} from '../../hooks/useContributingScores'
 import {Sorting} from '../../types/shapes'
+import {ColumnHeader} from './ColumnHeader'
 
 const I18n = createI18nScope('learning_mastery_gradebook')
 
@@ -58,67 +55,48 @@ export const ContributingScoreHeader: React.FC<ContributingScoreHeaderProps> = (
     sorting.setSortOrder(SortOrder.DESC)
   }
 
-  return (
-    <View
-      background="secondary"
-      as="div"
-      width={COLUMN_WIDTH}
-      borderWidth="large 0 medium 0"
-      data-testid="outcome-header"
+  const speedGraderMenuItem = (
+    <Menu.Item
+      key="speedgrader"
+      onClick={() =>
+        openWindow(
+          `/courses/${courseId}/gradebook/speed_grader?assignment_id=${alignment.associated_asset_id}`,
+          '_blank',
+        )
+      }
     >
-      <Flex alignItems="center" justifyItems="space-between" height={CELL_HEIGHT}>
-        <Flex.Item size="80%" padding="0 0 0 small">
-          <TruncateText>
-            <Text weight="bold">{alignment.associated_asset_name}</Text>
-          </TruncateText>
-        </Flex.Item>
-        <Flex.Item padding="0 small 0 0">
-          <Menu
-            placement="bottom"
-            trigger={
-              <IconButton
-                withBorder={false}
-                withBackground={false}
-                size="small"
-                screenReaderLabel={I18n.t('Contributing Score Menu')}
-              >
-                <IconArrowOpenDownLine />
-              </IconButton>
-            }
-          >
-            <Menu.Item
-              onClick={() =>
-                openWindow(
-                  `/courses/${courseId}/gradebook/speed_grader?assignment_id=${alignment.associated_asset_id}`,
-                  '_blank',
-                )
-              }
-            >
-              {I18n.t('Open in Speedgrader')}
-            </Menu.Item>
-            <Menu.Group label={I18n.t('Sort')}>
-              <Menu.Item
-                onSelect={handleSortAscending}
-                selected={isCurrentlySelected && sorting.sortOrder === SortOrder.ASC}
-              >
-                <Flex gap="x-small">
-                  <IconArrowUpLine spacing="small" />
-                  {I18n.t('Ascending scores')}
-                </Flex>
-              </Menu.Item>
-              <Menu.Item
-                onSelect={handleSortDescending}
-                selected={isCurrentlySelected && sorting.sortOrder === SortOrder.DESC}
-              >
-                <Flex gap="x-small">
-                  <IconArrowDownLine spacing="small" />
-                  {I18n.t('Descending scores')}
-                </Flex>
-              </Menu.Item>
-            </Menu.Group>
-          </Menu>
-        </Flex.Item>
-      </Flex>
-    </View>
+      {I18n.t('Open in Speedgrader')}
+    </Menu.Item>
+  )
+
+  const sortMenuGroup = (
+    <Menu.Group key="sort" label={I18n.t('Sort')}>
+      <Menu.Item
+        onSelect={handleSortAscending}
+        selected={isCurrentlySelected && sorting.sortOrder === SortOrder.ASC}
+      >
+        <Flex gap="x-small">
+          <IconArrowUpLine spacing="small" />
+          {I18n.t('Ascending scores')}
+        </Flex>
+      </Menu.Item>
+      <Menu.Item
+        onSelect={handleSortDescending}
+        selected={isCurrentlySelected && sorting.sortOrder === SortOrder.DESC}
+      >
+        <Flex gap="x-small">
+          <IconArrowDownLine spacing="small" />
+          {I18n.t('Descending scores')}
+        </Flex>
+      </Menu.Item>
+    </Menu.Group>
+  )
+
+  return (
+    <ColumnHeader
+      title={alignment.associated_asset_name}
+      optionsMenuTriggerLabel={I18n.t('Contributing Score Menu')}
+      optionsMenuItems={[speedGraderMenuItem, sortMenuGroup]}
+    />
   )
 }
---
+++ ui/features/learning_mastery_v2/react/components/grid/DraggableColumnHeader.tsx
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React, {useMemo} from 'react'
+import {ColumnHeader, ColumnHeaderProps} from './ColumnHeader'
+
+export interface DraggableColumnHeaderProps extends ColumnHeaderProps {
+  connectDragSource?: __ReactDnd.ConnectDragSource
+  connectDropTarget?: __ReactDnd.ConnectDropTarget
+  isDragging?: boolean
+}
+
+export const DraggableColumnHeader: React.FC<DraggableColumnHeaderProps> = ({
+  title,
+  optionsMenuTriggerLabel,
+  optionsMenuItems,
+  connectDragSource,
+  connectDropTarget,
+  isDragging,
+}) => {
+  const headerStyle = useMemo(
+    () => ({
+      opacity: isDragging ? 0.5 : 1,
+      cursor: 'grab',
+      transition: 'opacity 0.15s ease-in-out',
+    }),
+    [isDragging],
+  )
+
+  const headerContent = (
+    <div style={headerStyle}>
+      <ColumnHeader
+        title={title}
+        optionsMenuTriggerLabel={optionsMenuTriggerLabel}
+        optionsMenuItems={optionsMenuItems}
+      />
+    </div>
+  )
+
+  return (
+    <>
+      {connectDragSource && connectDropTarget
+        ? connectDragSource(connectDropTarget(headerContent))
+        : headerContent}
+    </>
+  )
+}
