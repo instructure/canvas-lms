fde25b92a0c735ca267a1adf060f6c65aef13a1a
Ryan Hawkins <ryan.hawkins@instructure.com>
2026-01-07 23:13:10 +0000

    Copy course-level tools context control with them
    why:
    - To ensure that course copy still works properly when this flag is
      turned on while still preventing teachers from using course copy to
      circumvent permissions. They can also copy controls if that control's
      associated tool was also copied over.
    
    closes INTEROP-10171
    
    flag=lti_registrations_next
    
    test-plan:
    - Have a course with a LTI 1.3 tool installed in it.
        - Copy said course to another course.
        - Ensure that the copied course has the same LTI 1.3 tool installed.
        - Ensure that the new tool shows up as a new deployment on the
          tool's details page.
    - Have an account with an LTI 1.3 tool installed in it that's disabled
      for everyone by default.
        - Put an exception in a course in that account enabling it for just
          that one course.
        - Copy everything from that course to another course.
        - Ensure that the tool does *not* show up. This means the exception
          wasn't copied, which is what we want.
    - Perform a selective import of an LTI 1.3 tool installed at the course
      level. Only select the tool.
        - Make sure that the tool is imported selectively and shows up on
          the tool's details page.
    
    Change-Id: I6acd61271b0541c677500dd2a521e05440d42f44
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/397434
    Reviewed-by: Tucker Mcknight <tmcknight@instructure.com>
    QA-Review: Tucker Mcknight <tmcknight@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Scott Morgan <scott.morgan@instructure.com>
    Reviewed-by: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>
    Product-Review: Paul Gray <paul.gray@instructure.com>

--- lib/cc/lti_context_controls.rb
+++ lib/cc/lti_context_controls.rb
@@ -22,7 +22,11 @@ module CC
   # Exports all Lti::ContextControls that are in the course being exported.
   module LtiContextControls
     def add_lti_context_controls(document = nil)
-      return nil unless Lti::ContextControl.active.where(course: @course).exists?
+      controls_to_export = Lti::ContextControl.preload(:deployment).where(course: @course).active.select do |context_control|
+        export_control?(context_control)
+      end
+
+      return nil if controls_to_export.empty?
 
       if document
         controls_file = nil
@@ -39,9 +43,7 @@ module CC
         "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
         "xsi:schemaLocation" => "#{CCHelper::CANVAS_NAMESPACE} #{CCHelper::XSD_URI}"
       ) do |controls_node|
-        Lti::ContextControl.preload(:deployment).where(course: @course).active.find_each do |context_control|
-          next unless export_control?(context_control)
-
+        controls_to_export.each do |context_control|
           add_lti_context_control(context_control, controls_node)
         end
       end
@@ -58,31 +60,12 @@ module CC
         identifier: create_key(context_control)
       ) do |context_control_node|
         context_control_node.available context_control.available
-        # The deployment URL will be used later to associate a copied context control
-        # with the appropriate deployment in the destination course. Note that this
-        # URL for 1.3 tools is the same as the target_link_uri on the tool configuration.
-        context_control_node.deployment_url context_control.deployment.url
-        # Let's us know if the control's tool is being copied as well, so we can handle
-        # selective import correctly.
-        if export_object?(context_control.deployment)
-          context_control_node.deployment_migration_id create_key(context_control.deployment)
-        else
-          # This only makes sense to include if the deployment is not being copied, as if it is being
-          # copied, we know the preferred_deployment_id can't be used as it's a course level tool.
-          context_control_node.preferred_deployment_id context_control.deployment.global_id
-        end
+        context_control_node.deployment_migration_id create_key(context_control.deployment)
       end
     end
 
     def export_control?(control)
-      control.deployment.active? &&
-        (export_object?(control.deployment) ||
-          (!tool_deployed_in_course?(control.deployment) && export_symbol?(:all_course_settings)))
-    end
-
-    def tool_deployed_in_course?(tool)
-      # Avoid loading context into memory unnecessarily
-      tool.context_id == @course.id && tool.context_type == "Course"
+      control.deployment.active? && export_object?(control.deployment)
     end
   end
 end
