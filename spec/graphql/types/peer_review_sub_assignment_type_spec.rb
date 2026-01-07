66616cef9192347d68c048d996559b9074201313
Martin Yosifov <martin.yosifov@instructure.com>
2026-01-07 12:02:59 -0800

    Add peer_review submission type to graphql
    closes EGG-2098
    flag=peer_review_allocation_and_grading
    
    Test plan:
    - Create a course and enable Peer Review Allocation
    and Grading feature flag
    - Create an assignment with peer reviews
    - In rails console, find id of the peer review sub assignment
    pr_id = PeerReviewSubAssignment.last.id
    - Open canvas graphiql and run the following query:
    query MyQuery {
      peerReviewSubAssignment(id: "peer review sub assignment id") {
        submissionTypes
        submissionsConnection {
          nodes {
            submissionType
          }
        }
      }
    }
    - Verify that the query runs successfully
    - Verify that the submissionTypes include "peer_review"
    - Verify that the submissionType is "peer_review"
    
    Change-Id: I2585191a1b85ebdc475a7b70b616aec0e728ec12
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/397530
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Mark Starkman <mark.starkman@instructure.com>
    QA-Review: Mark Starkman <mark.starkman@instructure.com>
    Product-Review: Martin Yosifov <martin.yosifov@instructure.com>

--- spec/graphql/types/peer_review_sub_assignment_type_spec.rb
+++ spec/graphql/types/peer_review_sub_assignment_type_spec.rb
@@ -63,6 +63,109 @@ describe Types::PeerReviewSubAssignmentType do
     end
   end
 
+  describe "submission types" do
+    it "includes peer_review as valid submission types" do
+      expect(peer_review_type.resolve("submissionTypes")).to eq ["peer_review"]
+    end
+
+    it "returns peer_review submissions type via GraphQL without UnresolvedValueError" do
+      query = <<~GQL
+        query($id: ID!) {
+          peerReviewSubAssignment(id: $id) {
+            submissionTypes
+          }
+        }
+      GQL
+
+      result = CanvasSchema.execute(
+        query,
+        variables: { id: @peer_review_sub_assignment.id.to_s },
+        context: { current_user: teacher }
+      )
+
+      expect(result["errors"]).to be_nil
+      expect(result.dig("data", "peerReviewSubAssignment", "submissionTypes")).to eq ["peer_review"]
+    end
+
+    context "with peer review submissions" do
+      it "returns peer_review submission type via submissionsConnection" do
+        parent_assignment = course.assignments.create!(
+          title: "Parent Assignment with Submissions",
+          peer_reviews: true,
+          peer_review_count: 2,
+          points_possible: 10
+        )
+        peer_review_sub_assignment = peer_review_model(parent_assignment:)
+
+        student1 = student_in_course(course:, active_all: true).user
+        student2 = student_in_course(course:, active_all: true).user
+
+        parent_assignment.submit_homework(student, body: "assessor work")
+        submission1 = parent_assignment.submit_homework(student1, body: "student1 work")
+        submission2 = parent_assignment.submit_homework(student2, body: "student2 work")
+        parent_assignment.reload
+
+        assessment_request1 = AssessmentRequest.create!(
+          user: student1,
+          asset: submission1,
+          assessor_asset: parent_assignment.submissions.find_by(user: student),
+          assessor: student,
+          workflow_state: "completed"
+        )
+        assessment_request2 = AssessmentRequest.create!(
+          user: student2,
+          asset: submission2,
+          assessor_asset: parent_assignment.submissions.find_by(user: student),
+          assessor: student,
+          workflow_state: "completed"
+        )
+
+        comment1 = SubmissionComment.create!(
+          submission: submission1,
+          author: student,
+          comment: "Good work!",
+          created_at: 1.hour.ago
+        )
+        comment2 = SubmissionComment.create!(
+          submission: submission2,
+          author: student,
+          comment: "Needs improvement",
+          created_at: 1.hour.ago
+        )
+
+        assessment_request1.submission_comments << comment1
+        assessment_request2.submission_comments << comment2
+
+        PeerReview::PeerReviewSubmitterService.new(
+          parent_assignment:,
+          assessor: student
+        ).call
+
+        query = <<~GQL
+          query($id: ID!) {
+            peerReviewSubAssignment(id: $id) {
+              submissionsConnection {
+                nodes {
+                  submissionType
+                }
+              }
+            }
+          }
+        GQL
+
+        result = CanvasSchema.execute(
+          query,
+          variables: { id: peer_review_sub_assignment.id.to_s },
+          context: { current_user: teacher }
+        )
+
+        expect(result["errors"]).to be_nil
+        submission_type = result.dig("data", "peerReviewSubAssignment", "submissionsConnection", "nodes").pluck("submissionType")
+        expect(submission_type).to include("peer_review")
+      end
+    end
+  end
+
   describe "permissions" do
     it "respects read permissions via assignment endpoint" do
       other_course = course_factory
--- ui/shared/lti-asset-processor/queries/getCourseAssignmentsAssetReports.ts
+++ ui/shared/lti-asset-processor/queries/getCourseAssignmentsAssetReports.ts
@@ -85,6 +85,7 @@ export const ZSubmissionType = z.enum([
   'online_text_entry',
   'online_upload',
   'online_url',
+  'peer_review',
   'student_annotation',
   'wiki_page',
 ])
