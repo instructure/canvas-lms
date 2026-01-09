a3faaa14100f2ebe5019d2bd6740a572172f734c
David Varga <d.varga@instructure.com>
2026-01-09 12:20:15 +0100

    Fix Assignment hiding on long running CPF migrations
    Because of long running migrations
    we want to hide (prevent editing)
    TP from assignment once
    tool_proxy is migrated and not wait for
    AP or report creation
    
    refs INTEROP-10155
    flag=lti_asset_processor_tii_migration
    
    test-plan:
    - Have an Assignment with CPF
    - set `migrated_to_context_external_tool` on TP
    but make sure assignment has no AP yet
    - Edit the Assignemnt and save
    - Before the change you should see error
    but after the change no error should be shown
    
    Change-Id: Icb7d347b95757f64495f41be7b7c37a517ebefaa
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/397560
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Csaba Csuzdi <csaba.csuzdi@instructure.com>
    QA-Review: Csaba Csuzdi <csaba.csuzdi@instructure.com>
    Product-Review: David Varga <d.varga@instructure.com>

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1736,12 +1736,10 @@ describe Assignment do
         end
       end
 
-      context "and the tool proxy has been migrated to a ContextExternalTool" do
+      context "and the tool proxy migration has started" do
         before do
           migrated_tool = @course.context_external_tools.create!(name: "migrated tool", url: "http://www.example.com", consumer_key: "key", shared_secret: "secret")
           message_handler.resource_handler.tool_proxy.update!(migrated_to_context_external_tool: migrated_tool)
-          # Add an LTI asset processor to satisfy the migrated? check
-          @assignment.lti_asset_processors.create!(context_external_tool: migrated_tool, custom: { migrated_from_cpf: "true" })
         end
 
         it "returns nil instead of the message handler" do
