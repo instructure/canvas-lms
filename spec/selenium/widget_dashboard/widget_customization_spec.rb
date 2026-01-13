97437c97721c2215d022826c2c28cb39c86b87ec
June-eoh <jungsun.eoh@instructure.com>
2026-01-13 16:49:37 -0700

    spec: add and remove new widgets to dashboard
    closes LX-3568
    flag=widget_dashboard
    [fsc-timeout=50]
    
    test Plan:
    Jenkins runs and passes
    
    Change-Id: I9226b811844efebdfbb2d8dbccc6bb1415f1f570
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/398000
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Drake Harper <drake.harper@instructure.com>
    QA-Review: Drake Harper <drake.harper@instructure.com>
    Product-Review: June Eoh <jungsun.eoh@instructure.com>

--- spec/selenium/widget_dashboard/widget_customization_spec.rb
+++ spec/selenium/widget_dashboard/widget_customization_spec.rb
@@ -37,11 +37,10 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
   it "enters and exits edit mode with save and cancel buttons" do
     go_to_dashboard
 
-    expect(customize_dashboard_button).to be_displayed
-    customize_dashboard_button.click
+    click_widget_customize_button
     expect(save_customize_button).to be_displayed
     expect(cancel_customize_button).to be_displayed
-    expect(widget_drag_handle("course-work-combined-widget")).to be_displayed
+    expect(widget_drag_handle("course-work-combined")).to be_displayed
 
     cancel_customize_button.click
     expect(customize_dashboard_button).to be_displayed
@@ -50,11 +49,10 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
   context "Rearrange widget via context menu" do
     it "moves widgets up and down using context menu options" do
       go_to_dashboard
-      expect(customize_dashboard_button).to be_displayed
-      customize_dashboard_button.click
+      click_widget_customize_button
 
-      expect(widget_drag_handle("people-widget")).to be_displayed
-      widget_drag_handle("people-widget").click
+      expect(widget_drag_handle("people")).to be_displayed
+      widget_drag_handle("people").click
       expect(widget_reorder_menu_option("Move up")).to be_displayed
       widget_reorder_menu_option("Move up").click
       wait_for_ajaximations
@@ -64,12 +62,12 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
       expect(column_2_widgets[0].attribute("data-testid")).to eq("widget-container-people-widget")
       expect(column_2_widgets[1].attribute("data-testid")).to eq("widget-container-announcements-widget")
 
-      widget_drag_handle("people-widget").click
+      widget_drag_handle("people").click
       expect(widget_reorder_menu_option("Move down")).to be_displayed
       widget_reorder_menu_option("Move down").click
       verify_reordered_widget_up_down
 
-      save_customize_button.click
+      click_save_customize_button
       verify_reordered_widget_up_down
 
       refresh_page
@@ -78,11 +76,10 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
 
     it "moves widget to top and bottom using context menu option" do
       go_to_dashboard
-      expect(customize_dashboard_button).to be_displayed
-      customize_dashboard_button.click
+      click_widget_customize_button
 
-      expect(widget_drag_handle("course-work-combined-widget")).to be_displayed
-      widget_drag_handle("course-work-combined-widget").click
+      expect(widget_drag_handle("course-work-combined")).to be_displayed
+      widget_drag_handle("course-work-combined").click
       expect(widget_reorder_menu_option("Move to bottom")).to be_displayed
       widget_reorder_menu_option("Move to bottom").click
       wait_for_ajaximations
@@ -92,12 +89,12 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
       expect(column_1_widgets[0].attribute("data-testid")).to eq("widget-container-course-grades-widget")
       expect(column_1_widgets[1].attribute("data-testid")).to eq("widget-container-course-work-combined-widget")
 
-      widget_drag_handle("course-work-combined-widget").click
+      widget_drag_handle("course-work-combined").click
       expect(widget_reorder_menu_option("Move to top")).to be_displayed
       widget_reorder_menu_option("Move to top").click
       verify_reordered_widget_top_bottom
 
-      save_customize_button.click
+      click_save_customize_button
       verify_reordered_widget_top_bottom
 
       refresh_page
@@ -106,11 +103,10 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
 
     it "moves widgets between columns using context menu options" do
       go_to_dashboard
-      expect(customize_dashboard_button).to be_displayed
-      customize_dashboard_button.click
+      click_widget_customize_button
 
-      expect(widget_drag_handle("announcements-widget")).to be_displayed
-      widget_drag_handle("announcements-widget").click
+      expect(widget_drag_handle("announcements")).to be_displayed
+      widget_drag_handle("announcements").click
       expect(widget_reorder_menu_option("Move left bottom")).to be_displayed
       widget_reorder_menu_option("Move left bottom").click
       wait_for_ajaximations
@@ -121,12 +117,12 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
       expect(column_2_widgets.length).to eq(1)
       expect(column_1_widgets[2].attribute("data-testid")).to eq("widget-container-announcements-widget")
 
-      widget_drag_handle("course-grades-widget").click
+      widget_drag_handle("course-grades").click
       expect(widget_reorder_menu_option("Move right top")).to be_displayed
       widget_reorder_menu_option("Move right top").click
       verify_reordered_widget_btw_columns
 
-      save_customize_button.click
+      click_save_customize_button
       verify_reordered_widget_btw_columns
 
       refresh_page
@@ -135,21 +131,97 @@ describe "student dashboard widget customization tests", :ignore_js_errors do
 
     it "disables context menu options when they are not applicable" do
       go_to_dashboard
-      expect(customize_dashboard_button).to be_displayed
-      customize_dashboard_button.click
+      click_widget_customize_button
       wait_for_ajaximations
 
-      widget_drag_handle("course-work-combined-widget").click
+      widget_drag_handle("course-work-combined").click
       expect(widget_reorder_menu_option("Move up").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move to top").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move left top").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move left bottom").attribute("aria-disabled")).to eq("true")
 
-      widget_drag_handle("people-widget").click
+      widget_drag_handle("people").click
       expect(widget_reorder_menu_option("Move down").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move to bottom").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move right top").attribute("aria-disabled")).to eq("true")
       expect(widget_reorder_menu_option("Move right bottom").attribute("aria-disabled")).to eq("true")
+    end
+  end
+
+  context "Remove widget" do
+    it "removes a widget" do
+      go_to_dashboard
+      click_widget_customize_button
+
+      click_widget_remove_button("announcements")
+      verify_widget_is_removed("announcements", 2)
+
+      click_save_customize_button
+      verify_widget_is_removed("announcements", 2)
+      refresh_page
+      verify_widget_is_removed("announcements", 2)
+    end
+
+    it "restores removed widget when canceling edit mode" do
+      go_to_dashboard
+      click_widget_customize_button
+      click_widget_remove_button("announcements")
+      verify_widget_is_removed("announcements", 2)
+
+      cancel_customize_button.click
+      expect(widget_container("announcements")).to be_displayed
+    end
+  end
+
+  context "Add widget" do
+    it "adds a widget via the add widget modal" do
+      go_to_dashboard
+      click_widget_customize_button
+
+      expect(element_exists?(widget_container_selector("todo_list"))).to be_falsey
+      click_add_widget_button
+      expect(add_widget_modal_add_button("todo_list")).to be_displayed
+      add_widget_modal_add_button("todo_list").click
+      expect(widget_container("todo_list")).to be_displayed
+      click_save_customize_button
+
+      refresh_page
+      wait_for_ajaximations
+      expect(widget_container("todo_list")).to be_displayed
+    end
+
+    it "shows added status for widget that is already on dashboard" do
+      go_to_dashboard
+      click_widget_customize_button
+
+      click_add_widget_button
+      expect(add_widget_modal_added_button("course_grades")).to be_displayed
+      expect(add_widget_modal_added_button("course_grades")).to have_attribute("disabled")
+    end
+
+    it "closes add widget modal without adding when close button is clicked" do
+      go_to_dashboard
+      verify_default_widget_count
+
+      click_widget_customize_button
+      click_add_widget_button
+      expect(add_widget_modal_close_button).to be_displayed
+      add_widget_modal_close_button.click
+
+      expect(element_exists?(add_widget_modal_selector)).to be_falsey
+      verify_default_widget_count
+    end
+
+    it "does not add widgets when canceling edit mode" do
+      go_to_dashboard
+      click_widget_customize_button
+      click_add_widget_button
+
+      expect(add_widget_modal_add_button("todo_list")).to be_displayed
+      add_widget_modal_add_button("todo_list").click
+      expect(widget_container("todo_list")).to be_displayed
+      cancel_customize_button.click
+      verify_default_widget_count
     end
   end
 end
