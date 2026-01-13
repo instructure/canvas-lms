a58ae530718adbe1270bfd109587d21445160ffb
mohammad.hajjaj <mohammad.hajjaj@instructure.com>
2026-01-13 16:21:11 +0300

    fix launch_presentation_return_url launch param for Native launch
    refs QUIZ-17043
    
    flag = new_quizzes_native_experience
    
    Test Plan:
    1- Open Quizzes page
    2- open a new quiz
    3- click the return button
    4- it should redirect back to the quizzes page instead of assignments page
    
    Change-Id: I283583432320ec7260f2b2f5af4ecf807eaefdb3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/397898
    Reviewed-by: Márton Gréczi <marton.greczi@instructure.com>
    QA-Review: Márton Gréczi <marton.greczi@instructure.com>
    Product-Review: Márton Gréczi <marton.greczi@instructure.com>
    Reviewed-by: Bálint Sipos <balint.sipos@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- lib/new_quizzes/launch_data_builder.rb
+++ lib/new_quizzes/launch_data_builder.rb
@@ -154,9 +154,10 @@ module NewQuizzes
     end
 
     def return_url
-      # Generate return URL - for native launches, this could be used by the tool
-      # to navigate back to Canvas after completion
-      @controller&.named_context_url(@context, :context_external_content_success_url, "external_tool_redirect", include_host: true)
+      # Generate return URL - match LTI launch behavior by using set_return_url
+      # This intelligently determines the best return URL based on the referer
+      # (quizzes page, gradebook, modules, etc.)
+      @controller&.set_return_url
     end
 
     def roles
