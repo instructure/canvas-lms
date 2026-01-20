8fb7394f66e2eec76fe7c88ace383c9fd00e11aa
rohanchugh14 <rohan.chugh@instructure.com>
2026-01-20 13:28:28 -0600

    remove horizon account check before tenant logic
    remove horizon account check before tenant deletion and provisioning
    logic to avoid a race condition that stopped tenants from being created
    because the horizon_account? check was being called before the account
    setting was updated and cache invalidated
    
    closes DOMSVC-817
    
    [skip-crystalball]
    
    flag=horizon_course_setting
    
    test plan:
    - set up an account that can be turned into a career account (no
      courses)
    - ensure they are not already provisioned in pine/redwood
    - enable career on the account
    - ensure they are provisioned in pine/redwood
    - disable career on the account
    - ensure they are deleted in pine/redwood
    - ensure that this only happens when the first/last career account is
      enabled/disabled, so i.e.:
      - if you have one account enabled and enable
        another, it should not send a request to provision the root account
      - and if you have 2 accounts enabled and disable one, it should not
        send a request to delete the root account
    
    Change-Id: Ib228e1d5088d2586a62c35e7f232d1d621bc040d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/398621
    Reviewed-by: Jackson Howe <jackson.howe@instructure.com>
    Reviewed-by: Andrea Cirulli <andrea.cirulli@instructure.com>
    QA-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Product-Review: Andrea Cirulli <andrea.cirulli@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -2885,8 +2885,6 @@ class Account < ActiveRecord::Base
   end
 
   def provision_horizon_tenants(root_account, current_user)
-    return unless horizon_account?
-
     [PineClient, RedwoodClient].each do |client|
       next unless client.enabled?
 
@@ -2895,8 +2893,6 @@ class Account < ActiveRecord::Base
   end
 
   def delete_horizon_tenants(root_account, current_user)
-    return if horizon_account?
-
     [PineClient, RedwoodClient].each do |client|
       next unless client.enabled?
 
