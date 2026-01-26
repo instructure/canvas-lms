d3952d2de101ad7f74cbea83c0115d5e385bda79
Cody Cutrer <cody@instructure.com>
2026-01-26 09:04:50 -0700

    Squash old migrations 202507
    refs AE-3340
    
    Change-Id: Ib3d3bf1f811e98c79d928a906c11605dbc334abe
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/399167
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    Migration-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20101201000094_validate_migration_integrity.rb
+++ db/migrate/20101201000094_validate_migration_integrity.rb
@@ -31,7 +31,7 @@ class ValidateMigrationIntegrity < ActiveRecord::Migration[7.0]
 
   def up
     initial_migration_version = "20101210192618"
-    last_squashed_migration_version = "20250624000755"
+    last_squashed_migration_version = "20250731223635"
 
     versions = ActiveRecord::Base.schema_migration.versions
     initial_migration_has_run = versions.include?(initial_migration_version)
