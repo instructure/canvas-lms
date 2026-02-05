abe7516e93ecdbbf40160984f60abbcc4ae3a306
jake.oeding <jake.oeding@instructure.com>
2026-02-05 10:53:04 -0500

    add bookmarks association to user
    refs AE-3299
    
    Change-Id: I12780f5bb489ec8f39be946d968bc2b17a1d6941
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/400242
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    QA-Review: Jake Oeding <jake.oeding@instructure.com>
    Product-Review: Jake Oeding <jake.oeding@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -232,6 +232,7 @@ class User < ActiveRecord::Base
   has_many :discussion_topics, -> { where(type: nil) }
   has_many :submission_comments, foreign_key: "author_id", inverse_of: :author
   has_many :all_comment_bank_items, class_name: "CommentBankItem"
+  has_many :bookmarks, class_name: "Bookmarks::Bookmark"
 
   has_one :profile, class_name: "UserProfile"
   has_many :profile_links, through: :profile, source: :links
