c34873077c5fe834b517f3ac28504f97c007c53a
Aaron Shafovaloff <ashafovaloff@instructure.com>
2026-02-15 14:09:37 -0700

    typescriptify ui/features/quiz_log_auditing
    refs CFA-338
    flag=none
    
    Change-Id: I04a589d78ce99897d2e714bb7b430a96d11df6cc
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/401104
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hulse <michael.hulse@instructure.com>
    QA-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>
    Product-Review: Aaron Shafovaloff <ashafovaloff@instructure.com>

---
+++ ui/features/quiz_log_auditing/global.d.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type {GlobalEnv} from '@canvas/global/env/GlobalEnv.d'
+
+declare global {
+  const ENV: GlobalEnv & {
+    quiz_url?: string
+    questions_url?: string
+    submission_url?: string
+    events_url?: string
+    can_view_answer_audits?: boolean
+  }
+}
+
+export {}
