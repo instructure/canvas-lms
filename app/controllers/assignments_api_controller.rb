321ac0f3c3a3c40d7bdad4c029d39f11a785d6a9
Jozsef Kercso <jozsef.kercso@instructure.com>
2026-02-24 19:49:57 +0100

    Fix broken link in API doc
    Fix the reference to quizzes.html
    
    Flag: none
    Test plan:
    - generate the API doc and check if the link works
    both in the old (html) and the new (markdown)
    documentation.
    
    Change-Id: I61cf07e8602de38087abce2b49b7ac5ba2b0923d
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/402277
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hogenson <michael.hogenson@instructure.com>
    Reviewed-by: Eva Tapai K. <eva.tapai@instructure.com>
    QA-Review: Jozsef Kercso <jozsef.kercso@instructure.com>
    Product-Review: Jozsef Kercso <jozsef.kercso@instructure.com>

--- app/controllers/assignments_api_controller.rb
+++ app/controllers/assignments_api_controller.rb
@@ -838,7 +838,7 @@ class AssignmentsApiController < ApplicationController
   #   Optional information:
   #   When the root account has the feature `newquizzes_on_quiz_page` enabled
   #   and this argument is set to "Quiz" the response will be serialized into a
-  #   quiz format({file:doc/api/quizzes.html#Quiz});
+  #   {file:quizzes.html#Quiz quiz format};
   #   When this argument isn't specified the response will be serialized into an
   #   assignment format;
   #
