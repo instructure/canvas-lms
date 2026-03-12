1097223c945cba88da343338315eb0b9e2273f01
Theo Sorriaux <theo.sorriaux@instructure.com>
2026-03-12 18:16:01 +0100

    Fix translation button in discussion
    Discussion's Translation button shows up
    even if the flag is off,
    this PS intends to fix it
    
    closes VICE-5911
    flag=translation
    
    Test plan
    - Disable and enable translation ff
    - go to any discussion
    - the button should behave accordingly
    
    Change-Id: I98d64f4a96d51914bc1e67536749e754db506ac6
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/403896
    Reviewed-by: Roland Beres <roland.beres@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Theo Sorriaux <theo.sorriaux@instructure.com>
    Product-Review: Theo Sorriaux <theo.sorriaux@instructure.com>

--- ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/DiscussionPostToolbar.tsx
+++ ui/features/discussion_topics_post/react/components/DiscussionPostToolbar/DiscussionPostToolbar.tsx
@@ -97,12 +97,13 @@ export const DiscussionPostToolbar = props => {
   const {translationLanguages, setShowTranslationControl, showTranslationControl} = useContext(
     DiscussionManagerUtilityContext,
   )
-
   const [showTranslate, setShowTranslate] = useState(false)
 
   const isTranslateAll = useTranslationStore(state => state.translateAll)
   const setActiveLanguage = useTranslationStore(state => state.setActiveLanguage)
   const clearTranslateAll = useTranslationStore(state => state.clearTranslateAll)
+  // @ts-expect-error TS2339 (typescriptify)
+  const discussionTranslationAvailable = ENV.discussion_translation_available
 
   const clearButton = () => {
     return getClearButton({
@@ -437,17 +438,19 @@ export const DiscussionPostToolbar = props => {
                   >
                     {renderSort(responsiveProps?.sortOrder?.width)}
                   </Flex.Item>
-                  {translationLanguages.current.length > 0 && !isSpeedGraderInTopUrl && (
-                    <Flex.Item
-                      // @ts-expect-error TS18049 (typescriptify)
-                      padding={responsiveProps.padding}
-                      margin={responsiveProps?.translation?.margin}
-                      shouldGrow={responsiveProps?.translation?.shouldGrow}
-                      shouldShrink={responsiveProps?.translation?.shouldShrink}
-                    >
-                      {renderTranslate()}
-                    </Flex.Item>
-                  )}
+                  {discussionTranslationAvailable &&
+                    translationLanguages.current.length > 0 &&
+                    !isSpeedGraderInTopUrl && (
+                      <Flex.Item
+                        // @ts-expect-error TS18049 (typescriptify)
+                        padding={responsiveProps.padding}
+                        margin={responsiveProps?.translation?.margin}
+                        shouldGrow={responsiveProps?.translation?.shouldGrow}
+                        shouldShrink={responsiveProps?.translation?.shouldShrink}
+                      >
+                        {renderTranslate()}
+                      </Flex.Item>
+                    )}
                 </Flex>
               </Flex.Item>
             </Flex>
