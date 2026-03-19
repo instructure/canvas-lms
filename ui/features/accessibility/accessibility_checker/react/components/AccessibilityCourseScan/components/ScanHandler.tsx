5b31b1707f9ed983d6501d9e83c72f52c4676108
viktor.szpisjak <viktor.szpisjak@instructure.com>
2026-03-19 15:52:01 +0100

    Add 'what we look for' explanation to a11y checker UI
    closes RCX-4924
    
    Test plan:
     - No regression
     - Check the popover is matching with the design
       (design in the ticket)
    
    Change-Id: I0febaf43f39b34284d91060d9ee2d60749f286b3
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/404500
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ferenc Marcsó <ferenc.marcso@instructure.com>
    QA-Review: Ferenc Marcsó <ferenc.marcso@instructure.com>
    Product-Review: Viktor Szpisják <viktor.szpisjak@instructure.com>

--- ui/features/accessibility/accessibility_checker/react/components/AccessibilityCourseScan/components/ScanHandler.tsx
+++ ui/features/accessibility/accessibility_checker/react/components/AccessibilityCourseScan/components/ScanHandler.tsx
@@ -16,18 +16,101 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
+import React, {useState} from 'react'
 import {View} from '@instructure/ui-view'
 import {Flex} from '@instructure/ui-flex'
-import {Button} from '@instructure/ui-buttons'
+import {Button, CloseButton, IconButton} from '@instructure/ui-buttons'
 import {Heading} from '@instructure/ui-heading'
 import {Text} from '@instructure/ui-text'
 import {Responsive} from '@instructure/ui-responsive'
 import {useScope as createI18nScope} from '@canvas/i18n'
 import {responsiveQuerySizes} from '@canvas/breakpoints'
 import useDateTimeFormat from '@canvas/use-date-time-format-hook'
+import {IconExternalLinkLine, IconQuestionLine} from '@instructure/ui-icons'
+import {Popover} from '@instructure/ui-popover'
+import {Link} from '@instructure/ui-link'
+import {ScreenReaderContent} from '@instructure/ui-a11y-content'
 
 const I18n = createI18nScope('accessibility_scan')
+
+const WhatWeLookForPopover = () => {
+  const [isShowingContent, setIsShowingContent] = useState(false)
+
+  return (
+    <Popover
+      placement="bottom"
+      isShowingContent={isShowingContent}
+      shouldContainFocus
+      shouldCloseOnDocumentClick={true}
+      onHideContent={() => setIsShowingContent(false)}
+      on="click"
+      screenReaderLabel={I18n.t('What we look for')}
+      renderTrigger={() => (
+        <IconButton
+          onClick={() => setIsShowingContent(!isShowingContent)}
+          screenReaderLabel={I18n.t('What we look for')}
+          renderIcon={() => <IconQuestionLine size="x-small" />}
+          withBackground={false}
+          withBorder={false}
+          size="small"
+          shape="circle"
+        />
+      )}
+    >
+      <Flex direction="column" padding="medium" width="22rem" gap="mediumSmall">
+        <Flex.Item>
+          <Flex justifyItems="space-between" alignItems="center">
+            <Flex.Item>
+              <Heading level="h3" margin="none">
+                {I18n.t('What we look for')}
+              </Heading>
+            </Flex.Item>
+            <Flex.Item>
+              <CloseButton
+                margin="x-small"
+                screenReaderLabel={I18n.t('Close')}
+                onClick={() => setIsShowingContent(false)}
+              />
+            </Flex.Item>
+          </Flex>
+        </Flex.Item>
+        <Flex.Item>
+          <Text>
+            {I18n.t(
+              'The Course Accessibility Checker scans Pages, Assignments, Discussions, Announcements, and Syllabi for common accessibility errors typically occurring in content created with he Rich Text Editor or other text editors.',
+            )}
+          </Text>
+        </Flex.Item>
+        <Flex.Item>
+          <Text>
+            {I18n.t(
+              'We look for errors in image alt text, table and list formatting, headings, text contrast (in content created with the RCE) and duplicate links.',
+            )}
+          </Text>
+        </Flex.Item>
+        <Flex.Item>
+          <Text>
+            {I18n.t(
+              'We don’t check new quizzes, videos, and uploaded files like PDFs, or linked external content like Google docs. We don’t look for errors with interactivity and complex layouts created outside of the RCE.',
+            )}
+          </Text>
+        </Flex.Item>
+        <Flex.Item>
+          <Link
+            href="https://community.instructure.com/en/kb/articles/664351-how-do-i-use-the-accessibility-checker-in-canvas"
+            target="_blank"
+            rel="noopener noreferrer"
+            iconPlacement="end"
+            renderIcon={<IconExternalLinkLine size="x-small" />}
+          >
+            {I18n.t('Read the documentation')}
+            <ScreenReaderContent>{I18n.t('- Opens in a new tab.')}</ScreenReaderContent>
+          </Link>
+        </Flex.Item>
+      </Flex>
+    </Popover>
+  )
+}
 
 interface CourseScanWrapperProps {
   children: React.ReactNode
@@ -74,9 +157,14 @@ export const ScanHandler: React.FC<CourseScanWrapperProps> = ({
                   {I18n.t('Course Accessibility Checker')}
                 </Heading>
                 {formattedDate && (
-                  <Text size="small" color="secondary">
-                    {I18n.t('Last checked %{date}', {date: formattedDate})}
-                  </Text>
+                  <Flex alignItems="center" gap="x-small">
+                    <Flex.Item>
+                      <Text size="small" color="secondary">
+                        {I18n.t('Last checked %{date}', {date: formattedDate})} |{' '}
+                        {I18n.t('What we look for?')} <WhatWeLookForPopover />
+                      </Text>
+                    </Flex.Item>
+                  </Flex>
                 )}
               </Flex.Item>
               {buttonLabel && (
