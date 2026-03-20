18646458865706df60c4f4667825d9135ae9c2e0
Maya Tyner <maya.tyner@instructure.com>
2026-03-20 13:14:26 -0400

    update docs for querying by assignment
    refs FOO-6194
    flag=none
    
    test-plan:
    - none
    
    Change-Id: Id00e698d14388d5a5a0fc9771cd55eaa7bfa60f0
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/404608
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Charley Kline <ckline@instructure.com>
    QA-Review: Charley Kline <ckline@instructure.com>
    Product-Review: Charley Kline <ckline@instructure.com>

--- app/controllers/grade_change_audit_api_controller.rb
+++ app/controllers/grade_change_audit_api_controller.rb
@@ -219,7 +219,7 @@ class GradeChangeAuditApiController < AuditorApiController
   # @API Advanced query
   #
   # List grade change events satisfying all given parameters. Teachers may query for events in courses they teach.
-  # Queries without +course_id+ require account administrator rights.
+  # Queries without +course_id+ or +assignment_id+ require account administrator rights.
   #
   # At least one of +course_id+, +assignment_id+, +student_id+, or +grader_id+ must be specified.
   #
