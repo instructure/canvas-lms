15e3024b670b6884b7903e6aae82765698ea4530
Eric Saupe <eric.saupe@instructure.com>
2026-04-06 10:21:14 -0700

    Move Study Tools button to page header
    Study Tools button now renders in the
    breadcrumbs bar and mobile nav header
    instead of inline on wiki/file pages.
    Uses InstUI ai-primary IconButton style.
    
    closes LX-3944
    flag=study_assist
    
    test plan:
    - enable study_assist feature flag
    - open a wiki page in a course
    - verify AI button appears in breadcrumb bar
    - click button, verify tray opens
    - open a file show page
    - verify button appears there too
    - check mobile view shows the button
    
    Change-Id: I43c7675417af2892150a73501e967861d59bf12f
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/405839
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Sarah Gerard <sarah.gerard@instructure.com>
    QA-Review: Sarah Gerard <sarah.gerard@instructure.com>
    Product-Review: Eric Saupe <eric.saupe@instructure.com>

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -128,12 +128,14 @@ class WikiPagesController < ApplicationController
         @mark_done = MarkDonePresenter.new(self, @context, params["module_item_id"], @current_user, @page)
         @padless = true
         if @context.feature_enabled?(:study_assist)
+          @show_study_assist = true
           js_env[:FEATURES][:study_assist] = true
           js_env({
                    WIKI_PAGE_ID: @page.url,
                    JOURNEY_URL: CanvasCareer::Config.new(@domain_root_account).public_app_config(request).dig("hosts", "journey"),
                    STUDY_ASSIST_TOOLS: study_assist_enabled_tools
                  })
+          js_bundle :study_assist
         end
       end
 
