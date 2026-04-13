408f3689af882506a17b099bca705f7279719937
Steve McGee <steve.mcgee@instructure.com>
2026-04-12 22:35:26 -0600

    Remove api_rate_limits feature flag
    closes INTEROP-10131
    flag=api_rate_limits
    
    test plan:
    - all automated tests should still pass
    
    Change-Id: I1835151cf55e2dfbad028c332bd54633c7a26b27
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/406343
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
    QA-Review: Steven McGee <steve.mcgee@instructure.com>
    Product-Review: Steven McGee <steve.mcgee@instructure.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -559,7 +559,6 @@ class ApplicationController < ActionController::Base
     account_level_mastery_scales
     ams_root_account_integration
     ams_advanced_content_organization
-    api_rate_limits
     buttons_and_icons_root_account
     canvas_apps_sub_account_access
     cookie_consent_necessary
