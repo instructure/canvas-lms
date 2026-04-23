bf605b677f144607de9ec9aaf31110598141fd08
June-eoh <jungsun.eoh@instructure.com>
2026-04-22 17:33:53 -0700

    spec: Reorganize widget_dashboard specs into subdirs
    closes LX-4127
    flag=widget_dashboard
    [fsc-timeout=50]
    [fsc-max-nodes=20]
    
    test Plan:
    Jenkins runs and passes
    
    Change-Id: I94d290f08e786bd0788e1f56f1a5088a8df16dec
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/407204
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jason Gillett <jason.gillett@instructure.com>
    QA-Review: Jason Gillett <jason.gillett@instructure.com>
    Product-Review: June Eoh <jungsun.eoh@instructure.com>

--- spec/selenium/widget_dashboard/student_experience/dashboard_toggle_spec.rb
+++ spec/selenium/widget_dashboard/student_experience/dashboard_toggle_spec.rb
@@ -18,8 +18,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 require_relative "page_objects/widget_dashboard_page"
-require_relative "../helpers/student_dashboard_common"
-require_relative "../dashboard/pages/dashboard_page"
+require_relative "../../helpers/student_dashboard_common"
+require_relative "../../dashboard/pages/dashboard_page"
 
 describe "widget dashboard toggle", :ignore_js_errors do
   include_context "in-process server selenium tests"
